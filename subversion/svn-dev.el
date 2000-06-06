2d5c5432ac4c078c96440f6301b23383dc2f5ac2
Karl Fogel <kfogel@apache.org>
2000-06-06 17:47:11 +0000

    New files.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@836593 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subversion/svn-dev.el
@@ -0,0 +1,13 @@
+;;;; Emacs Lisp help for writing Subversion code. ;;;;
+
+;;; In C files, put something like this to load this file automatically:
+;;;
+;;; 
+;;;
+;;;
+
+
+(c-set-style "gnu")
+(setq indent-tabs-mode nil)
+
+(progn (message "loaded") (sit-for 1))
