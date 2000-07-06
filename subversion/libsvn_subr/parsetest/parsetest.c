47e1451d808fe1c35dc305c5369c774b3d14cf60
Ben Collins-Sussman <sussman@apache.org>
2000-07-06 03:50:13 +0000

    debugging parser test
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@836665 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/parsetest/parsetest.c
+++ subversion/libsvn_subr/parsetest/parsetest.c
@@ -1,5 +1,31 @@
 
 /* Testing svn_parse() */
 
+#include <svn_parse.h>
+#include <stdio.h>
+
 void
 main ()
+{
+  ap_hash_t *configdata;
+  ap_pool_t *pool;
+  svn_string_t *myfile;
+
+  /* Initialize APR (Apache pools) */
+  if (ap_initialize () != APR_SUCCESS)
+    {
+      printf ("ap_initialize() failed.\n");
+      exit (1);
+    }
+  if (ap_create_pool (&pool, NULL) != APR_SUCCESS)
+    {
+      printf ("ap_create_pool() failed.\n");
+      exit (1);
+    }
+
+  myfile = svn_string_create ("configfile", pool);
+
+  configdata = svn_parse (myfile, pool);
+
+  
+}
