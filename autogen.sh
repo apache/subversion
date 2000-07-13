79d2be5116d84a27d46b99bb43043cb9025f71c9
Karl Fogel <kfogel@apache.org>
2000-07-13 05:57:35 +0000

    more autoconfiscation work
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@836729 13f79535-47bb-0310-9956-ffa450edef68

--- autogen.sh
+++ autogen.sh
@@ -1,16 +1,16 @@
 ### Run this to produce everything needed for configuration. ###
 
-# Produce config.h.in
-autoheader --verbose
-
 # Produce all the `Makefile.in's
-automake --verbose --add-missing
+automake --add-missing
 
 # Produce aclocal.m4, so autoconf gets the automake macros it needs
 aclocal
 
 # Produce ./configure
 autoconf
+
+# Produce config.h.in
+autoheader
 
 echo "Don't run configure yet!  Once we autoconfiscation is working, "
 echo "this message will go away."
