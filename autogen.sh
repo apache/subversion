43f2a0cacbdc8bc13ab7a2074ddb1d17119e6ef2
Karl Fogel <kfogel@apache.org>
2000-07-14 21:47:06 +0000

    Deal with new apr/ situation.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@836743 13f79535-47bb-0310-9956-ffa450edef68

--- autogen.sh
+++ autogen.sh
@@ -12,7 +12,28 @@ autoconf
 # Produce config.h.in
 autoheader
 
+# Meta-configure apr/ subdir
+# Meta-configure apr/ subdir
+if [ -d apr ]; then
+  (cd apr; ./buildconf)
+else
+  echo ""
+  echo "You don't have an apr/ subdirectory here.  Please get one:"
+  echo ""
+  echo "   cvs -d :pserver:anoncvs@www.apache.org:/home/cvspublic login"
+  echo "      (password 'anoncvs')"
+  echo ""
+  echo "   cvs -d :pserver:anoncvs@www.apache.org:/home/cvspublic \\"
+  echo "          checkout -d apr apache-2.0/src/lib/apr"
+  echo ""
+  echo "Run that right here in the top-level of the Subversion tree."
+  echo ""
+  exit 1
+fi
+
 echo ""
-echo "Note: *Don't* run configure yet!  (Once autoconfiscation is complete, "
-echo "this message will go away.)"
+echo "You can run ./configure now."
+echo ""
+echo "(Note that autoconfiscation is still in progress, so not everything"
+echo "will be built.)"
 echo ""
