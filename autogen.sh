70134b7dfdbef03fb679b04056cd434160964701
Ben Collins-Sussman <sussman@apache.org>
2000-08-03 20:03:15 +0000

    autogen.sh checks for automake >= 1.4 now
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@836822 13f79535-47bb-0310-9956-ffa450edef68

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,14 @@
 ### Run this to produce everything needed for configuration. ###
 
+# First check that we're using at least automake 1.4, lest
+#  we get infinite loops from the command `SUBDIRS = . tests'
+automake --version | perl -ne 'if (/\(GNU automake\) ([0-9].[0-9])/) {print;  if ($1 < 1.4) {exit 1;}}'
+
+if [ $? -ne 0 ]; then
+    echo "Error: you need automake 1.4 or later.  Please upgrade."
+    exit 1
+fi
+
 # Produce all the `Makefile.in's, verbosely, and create neat missing things
 # like `libtool', `install-sh', etc.
 automake --add-missing --verbose
@@ -34,6 +43,5 @@ fi
 echo ""
 echo "You can run ./configure now."
 echo ""
-echo "(Note that autoconfiscation is still in progress, so not everything"
-echo "will be built.)"
+echo "(Note that this software is still a work in progress.)"
 echo ""
