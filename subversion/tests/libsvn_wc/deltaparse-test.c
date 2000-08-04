a45c60ec3422370bd334d3d1b4a17a80783121e0
Ben Collins-Sussman <sussman@apache.org>
2000-08-04 18:50:42 +0000

    deltaparser started
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@836835 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subversion/tests/libsvn_wc/deltaparse-test.c
@@ -0,0 +1,46 @@
+/* 
+   This file began as James Clark's simple demonstration of how to use
+   the expat library.  :)
+*/
+
+
+#include <stdio.h>
+#include "svn_types.h"
+#include "xmlparse.h"
+
+
+int main()
+{
+  char buf[BUFSIZ];
+  int done;
+  svn_delta_t my_delta;
+
+  /* Create the parser */
+  XML_Parser parser = XML_ParserCreate (NULL);
+
+  /* All callbacks should receive the delta structure */
+  XML_SetUserData (parser, &my_delta);
+
+  /* Register subversion-specific callbacks with the parser */
+  XML_SetElementHandler (parser, svn_xml_startElement, svn_xml_endElement);
+  XML_SetCharacterDataHandler (parser, svn_xml_DataHandler);
+
+  do {
+    /* Buffer a big chunk of stream */
+    size_t len = fread (buf, 1, sizeof(buf), stdin);
+    done = len < sizeof(buf);
+
+    /* Parse the buffer */
+    if (! XML_Parse (parser, buf, len, done)) {
+      fprintf (stderr,
+               "%s at line %d\n",
+               XML_ErrorString (XML_GetErrorCode (parser)),
+               XML_GetCurrentLineNumber (parser));
+      return 1;
+    }
+    /* Lather.  Rinse.  Repeat. */
+  } while (!done);
+
+  XML_ParserFree (parser);
+  return 0;
+}
