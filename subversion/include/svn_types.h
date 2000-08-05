c48a631d09a226e66c71002fe1d31ec81b9d47b0
Karl Fogel <kfogel@apache.org>
2000-08-05 19:37:10 +0000

    Moved all delta definitions from svn_types.h to svn_delta.h.
    Namespace-protect enum values with "svn_".
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@836856 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_types.h
+++ subversion/include/svn_types.h
@@ -212,122 +212,10 @@ typedef unsigned long svn_token_t;
 
 
 
-/* ******** Deltas and friends. ******** */
-
-/* These are the in-memory tree deltas; you can convert them to and
- * from XML.
- * 
- * The XML representation has certain space optimizations.  For
- * example, if an ancestor is omitted, it means the same path at the
- * same version (taken from the surrounding delta context).  We may
- * well decide to use corresponding optimizations here -- an absent
- * svn_ancestor_t object means use the path and ancestor from the
- * delta, etc -- or we may not.  In any case it doesn't affect the
- * definitions of these data structures.  However, once we do know
- * what interpretive conventions we're using in code, we should
- * probably record them here.
- */
-
-/* Note that deltas are constructed and deconstructed streamily.  That
- * way when you do a checkout of comp-tools, for example, the client
- * doesn't wait for an entire 200 meg tree delta to arrive before
- * doing anything.
- *
- * The delta being {de}constructed is passed along as one of the
- * arguments to the XML parser callbacks; the callbacks use the
- * existing delta, plus whatever the parser just saw that caused the
- * callback to be invoked, to figure out what to do next.
- */
-
-typedef size_t svn_version_t;   /* Would they ever need to be signed? */
-typedef int pdelta_t;           /* todo: for now */
-typedef int vdelta_t;           /* todo: for now */
-
-
-/* A property diff */
-typedef struct svn_pdelta_t {
-  enum {
-    set = 1,
-    delete
-  } kind;
-  svn_string_t *name;
-  svn_string_t *value;
-  struct svn_pdelta_t *next;
-} svn_pdelta_t;
-
-
-/* A binary diff */
-typedef struct svn_vdelta_t {
-  int todo;
-} svn_vdelta_t;
-
-
-/* An edit is an action and some content.  This is the content. */
-typedef struct svn_edit_content_t
-{
-  enum { 
-    file_type = 1,
-    directory_type
-  } kind;                           /* what kind of object is this? */
-
-  /* An ancestor is a path rooted from a version. */
-  svn_string_t *ancestor_path;      /* If NULL, this object is `new'. */
-  svn_version_t ancestor_version;
-
-  svn_boolean_t prop_delta;         /* flag: upcoming prop delta data */
-  svn_boolean_t text_delta;         /* flag: upcoming text delta data */
-  struct svn_delta_t *tree_delta;   /* A further tree delta, or NULL. */
-} svn_edit_content_t;
-
-
-/* A tree delta is a list of edits.  This is an edit. */
-typedef struct svn_edit_t
-{
-  enum { 
-    action_delete = 1,            /* Delete a file or directory. */
-    action_new,                   /* Create a new file or directory. */
-    action_replace,               /* Replace an existing file or dir */
-  } kind;
-  svn_string_t *name;             /* name to add/del/replace */
-  svn_edit_content_t *content;    /* the object we're adding/replacing */
-} svn_edit_t;
-
-
-/* This is a tree delta. */
-typedef struct svn_delta_t
-{
-  svn_string_t *source_root;   /* Directory to which this delta applies */
-  svn_version_t base_version;  /* Base version of this directory */
-  svn_edit_t *edit;            /* latest edit we're holding */
-} svn_delta_t;
-
-
-
-/* An enumerated type that can indicates one of the Subversion-delta
-   XML tag categories; needed for walking & building a
-   delta-in-progress */
-
-typedef enum
-{
-  svn_XML_treedelta = 1,
-  svn_XML_edit,
-  svn_XML_editcontent,
-  svn_XML_propdelta,
-  svn_XML_textdelta
-  
-} svn_XML_elt_t
-
-
-
-/* A skelta is just a tree delta with empty pdeltas and vdeltas. */
-typedef svn_delta_t svn_skelta_t;
-
-/* A line-based diff is just a huge wad of text. */
-typedef svn_string_t svn_diff_t;
-
 #endif  /* SVN_TYPES_H */
 
 
+
 /* --------------------------------------------------------------
  * local variables:
  * eval: (load-file "../svn-dev.el")
