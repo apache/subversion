fdce0d69a1da7cd4ec2d70da5fbbf06b36a5f05b
jimb <jimb@unknown>
2000-08-08 00:16:35 +0000

    Add interface to text deltas.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@836877 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_delta.h
+++ subversion/include/svn_delta.h
@@ -55,8 +55,157 @@
 #define SVN_DELTA_H
 
 #include <apr_pools.h>
+#include <apr_file_io.h>
 #include <xmlparse.h>
 #include "svn_types.h"
+
+
+/* Text deltas.  */
+
+/* A text delta represents the difference between two strings of
+   bytes, the `source' string and the `target' string.  Given a source
+   string and a target string, we can compute a text delta; given a
+   source string and a delta, we can reconstruct the target string.
+   However, note that deltas are not reversible: you cannot always
+   reconstruct the source string given the target string and delta.
+
+   Since text deltas can be very large, we make it possible to
+   generate them in pieces.  Each piece, represented by an
+   `svn_delta_window_t' structure, describes how to produce the next
+   section of the target string, based on either a supplied source
+   string, or a prior region of the target string.
+
+   We begin delta generation by calling `svn_delta_files' or
+   `svn_delta_strings' on the files or strings we want to compare.
+   That returns us an `svn_delta_stream_t' object.  We then call
+   `svn_next_delta_window' on the stream object repeatedly; each call
+   generates a new `svn_delta_window_t' object which describes the
+   next portion of the target string.  When `svn_next_delta_window'
+   returns zero, we are done building the target string.  */
+
+/* An `svn_delta_window' object describes how to reconstruct a section
+   of the target string.  It contains a series of instructions which
+   assemble new target string text by pulling together substrings from:
+     - the source file,
+     - the target file text so far, and
+     - a string of new data.  */
+
+/* A single text delta instruction.  */
+typedef struct svn_delta_insn_t {
+  enum {
+    /* Append the LEN bytes at OFFSET in the source string to the
+       target.  It must be the case that 0 <= OFFSET < OFFSET + LEN <=
+       size of source string.  */
+    svn_delta_source,
+
+    /* Append the LEN bytes at OFFSET in the target file, to the
+       target file.  It must be the case that 0 <= OFFSET < current
+       size of the target string.
+
+       However!  OFFSET + LEN may be *beyond* the end of the existing
+       target data.  "Where the heck does the text come from, then?"
+       If you start at OFFSET, and append LEN bytes one at a time,
+       it'll work out --- you're adding new bytes to the end at the
+       same rate you're reading them from the middle.  Thus, if your
+       current target text is "abcdefgh", and you get an
+       `svn_delta_target' instruction whose OFFSET is 6 and whose LEN
+       is 7, the resulting string is "abcdefghghghghg".  */
+    svn_delta_target,
+
+    /* Append the LEN bytes at OFFSET in the window's NEW string to
+       the target file.  It must be the case that 0 <= OFFSET < OFFSET
+       + LEN <= length of NEW.  */
+    svn_delta_new
+  } op;
+  
+  ap_off_t offset;
+  ap_off_t length;
+} svn_delta_insn_t;
+
+
+/* How to produce the next stretch of the target string.  */
+typedef struct svn_delta_window_t {
+
+  /* The number of instructions in this window.  */
+  int num_insns;
+  
+  /* The instructions for this window.  */
+  svn_delta_insn_t *insns;
+
+  /* New data, for use by any `svn_delta_new' instructions.  */
+  svn_string_t *new;
+
+} svn_delta_window_t;
+
+
+/* A delta stream --- this is the hat from which we pull a series of
+   svn_delta_window_t objects, which, taken in order, describe the
+   entire target string.  This type is defined within libsvn_delta, and
+   opaque outside that library.  */
+typedef struct svn_delta_stream_t *svn_delta_stream_t;
+
+
+/* Set *WINDOW to a pointer to the next window from the delta stream
+   STREAM.  When we have completely reconstructed the target string,
+   set *WINDOW to zero.  */
+extern svn_error_t *svn_next_delta_window (svn_delta_stream_t *stream,
+                                           svn_delta_window_t **window);
+
+/* Free the delta window WINDOW.  */
+extern void svn_free_delta_window (svn_delta_window_t *window);
+
+/* Set *STREAM to a pointer to a delta stream that will turn the text
+   from SOURCE into the text from TARGET.
+
+   SOURCE and TARGET are both functions which act like the Unix `read'
+   system call, given SOURCE_DATA or TARGET_DATA as their first
+   argument.  We will need to compute deltas for text drawn from
+   files, memory, sockets, and so on; the data may be huge --- too
+   large to read into memory at one time.  Using `read'-like functions
+   allows us to process the data as we go.  When we call
+   `svn_next_delta_window' on STREAM, it will call upon its SOURCE and
+   TARGET `read'-like functions to gather as much data as it needs.  */
+typedef svn_error_t *(*svn_delta_read_fn_t) (void *data,
+                                           char *buffer,
+                                           ap_off_t len);
+
+extern svn_error_t *svn_text_delta (svn_delta_read_fn_t *source,
+                                    void *source_data,
+                                    svn_delta_read_fn_t *target,
+                                    void *target_data,
+                                    svn_delta_stream_t **stream);
+
+/* Free the delta stream STREAM.  */
+extern void svn_free_delta_stream (svn_delta_stream_t *stream);
+
+/* Given a delta stream STREAM, set *FN and *DATA to a `read'-like
+   function that will return a VCDIFF-format byte stream.
+   (Do we need a `free' function for disposing of DATA somehow?)  */
+extern svn_error_t *svn_delta_to_vcdiff (svn_delta_stream_t *stream,
+                                         svn_delta_read_fn_t **fn,
+                                         void **data);
+
+/* Given FN and DATA, a `read'-like function and data pointer that
+   yield a VCDIFF-format byte stream, set *STREAM to a pointer to a
+   delta stream carrying the data from the VCDIFF stream.  */
+extern svn_error_t *svn_vcdiff_to_delta (svn_delta_read_fn_t *fn,
+                                         void *data,
+                                         svn_delta_stream_t **stream);
+
+
+
+/* Property deltas.  */
+
+/* A property diff */
+typedef struct svn_pdelta_t {
+  enum {
+    svn_prop_set = 1,
+    svn_prop_delete
+  } kind;
+  svn_string_t *name;
+  svn_string_t *value;
+  struct svn_pdelta_t *next;
+} svn_pdelta_t;
 
 
 /* These are the in-memory tree-delta stackframes; they are used to
@@ -120,27 +269,6 @@ typedef struct svn_delta_stackframe_t
   struct svn_delta_stackframe_t *previous;
 
 } svn_delta_stackframe_t
-
-
-
-
-/* A property diff */
-typedef struct svn_pdelta_t {
-  enum {
-    svn_prop_set = 1,
-    svn_prop_delete
-  } kind;
-  svn_string_t *name;
-  svn_string_t *value;
-  struct svn_pdelta_t *next;
-} svn_pdelta_t;
-
-
-/* A binary diff */
-typedef struct svn_vdelta_t {
-  int todo;
-} svn_vdelta_t;
-
 
 
 
