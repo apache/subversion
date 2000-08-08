1d0c89d311225096ac2d72c9fe10da4a3ee05f42
Karl Fogel <kfogel@apache.org>
2000-08-08 23:31:12 +0000

    Use SVN_WC instead of WC, and export it.
    (Change suggested by Branko <branko.cibej@hermes.si>).
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@836916 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svn-dev.sh
+++ subversion/svn-dev.sh
@@ -6,16 +6,23 @@ echo "   'source svn-dev.sh'"
 echo "instead of running it the standard way; it wants to affect parent env."
 echo ""
 
-if [ -d ${HOME}/projects/subversion ]; then
-   WC=${HOME}/projects/subversion
-elif [ -d ${HOME}/src/subversion ]; then
-   WC=${HOME}/src/subversion
+if [ "X${SVN_WC}" = "X" ]; then
+  if [ -d ${HOME}/projects/subversion ]; then
+     SVN_WC=${HOME}/projects/subversion
+  elif [ -d ${HOME}/src/subversion ]; then
+     SVN_WC=${HOME}/src/subversion
+  else
+     echo "I need to know where your working copy is."
+     echo "Please set the SVN_WC environment variable to that directory."
+     exit 1
+  fi
 else
-   echo "I need to know where your working copy is."
-   echo "Please modify me accordingly (subversion/subversion/svn-dev.sh)."
-   exit 1
+  if [ ! -d ${SVN_WC} ]; then
+    echo "${SVN_WC} does not exist, update your SVN_WC environment variable."
+  fi
 fi
 
-LD_LIBRARY_PATH=.:${WC}/subversion/libsvn_string:${WC}/subversion/libsvn_subr:${WC}/subversion/libsvn_delta:${WC}/subversion/libsvn_wc:${WC}/subversion/apr:${LD_LIBRARY_PATH}
+LD_LIBRARY_PATH=.:${SVN_WC}/subversion/libsvn_string:${SVN_WC}/subversion/libsvn_subr:${SVN_WC}/subversion/libsvn_delta:${SVN_WC}/subversion/libsvn_wc:${SVN_WC}/subversion/apr:${LD_LIBRARY_PATH}
 
+export SVN_WC
 export LD_LIBRARY_PATH
