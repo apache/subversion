0319b50c632694c83096190ad66db3c509d86caa
jimb <dev-null@apache.org>
2000-08-30 00:36:29 +0000

    Major doc fixes.
    (svn_delta_read_fn_t): Moved to its own page at the top; this isn't
    specific to text or tree deltas.
    
    Rename text delta functions and types, to avoid confusion with tree
    delta stuff.
    (svn_txdelta_op_t): Renamed from svn_delta_op_t.
    (svn_txdelta_source, svn_txdelta_target, svn_txdelta_new): Renamed from
    svn_delta_source, svn_delta_target, and svn_delta_new.
    (svn_txdelta_window_t): Renamed from svn_delta_window_t.
    (svn_txdelta_free_window): Renamed from svn_free_delta_window.
    (svn_txdelta_window_handler_t): Renamed from svn_text_delta_window_handler_t.
    
    Reinstate the text delta creation and vcdiff handling interfaces.
    (svn_txdelta_stream_t): New type.
    (svn_txdelta_next_window, svn_txdelta, svn_txdelta_free,
    svn_txdelta_to_vcdiff, svn_make_vcdiff_parser, svn_vcdiff_parse): New
    functions.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@837080 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_delta.h
+++ subversion/include/svn_delta.h
@@ -60,6 +60,33 @@
 #include "svn_string.h"
 #include "svn_error.h"
 
+
+
+/* Stuff used throughout the delta interface.  */
+
+/* A typedef for functions resembling the POSIX `read' system call,
+   representing a incoming stream of bytes, in caller-pulls form.
+
+   We will need to compute text deltas for data drawn from files,
+   memory, sockets, and so on.  We will need to read tree deltas from
+   various sources.  The data may be huge --- too large to read into
+   memory at one time.  Using a `read'-like function like this to
+   represent the input data allows us to process the data as we go.
+
+   BATON is some opaque structure representing what we're reading.
+   Whoever provided the function gets to use BATON however they
+   please.
+
+   BUFFER is a buffer to hold the data, and *LEN indicates how many
+   bytes to read.  Upon return, the function should set *LEN to the
+   number of bytes actually read, or zero at the end of the data
+   stream.  */
+typedef svn_error_t *svn_delta_read_fn_t (void *baton,
+                                          char *buffer,
+                                          apr_off_t *len,
+                                          apr_pool_t *pool);
+
+
 
 /* Text deltas.  */
 
@@ -70,37 +97,42 @@
    However, note that deltas are not reversible: you cannot always
    reconstruct the source string given the target string and delta.
 
-   Since text deltas can be very large, we make it possible to
-   generate them in pieces.  Each piece, represented by an
-   `svn_delta_window_t' structure, describes how to produce the next
-   section of the target string.
+   Since text deltas can be very large, the interface here allows us
+   to produce and consume them in pieces.  Each piece, represented by
+   an `svn_txdelta_window_t' structure, describes how to produce the
+   next section of the target string.
 
-   We begin delta generation by calling `svn_text_delta' on the
-   strings we want to compare.  That returns us an
-   `svn_delta_stream_t' object.  We then call `svn_next_delta_window'
-   on the stream object repeatedly; each call generates a new
-   `svn_delta_window_t' object which describes the next portion of the
-   target string.  When `svn_next_delta_window' returns zero, we are
-   done building the target string.  */
+   To compute a new text delta:
 
-/* An `svn_delta_window' object describes how to reconstruct a section
-   of the target string.  It contains a series of instructions which
-   assemble new target string text by pulling together substrings from:
+   - We call `svn_txdelta' on the strings we want to compare.  That
+     returns us an `svn_txdelta_stream_t' object.
+
+   - We then call `svn_txdelta_next_window' on the stream object
+     repeatedly.  Each call returns a new `svn_txdelta_window_t'
+     object, which describes the next portion of the target string.
+     When `svn_txdelta_next_window' returns zero, we are done building
+     the target string.  */
+
+
+/* An `svn_txdelta_window_t' object describes how to reconstruct a
+   contiguous section of the target string.  It contains a series of
+   instructions which assemble the new target string text by pulling
+   together substrings from:
      - the source file,
      - the target file text so far, and
      - a string of new data (accessible to this window only).  */
 
 /* A single text delta instruction.  */
-typedef struct svn_delta_op_t {
+typedef struct svn_txdelta_op_t {
   enum {
     /* Append the LEN bytes at OFFSET in the source string to the
        target.  It must be the case that 0 <= OFFSET < OFFSET + LEN <=
        size of source string.  */
-    svn_delta_source,
+    svn_txdelta_source,
 
     /* Append the LEN bytes at OFFSET in the target file, to the
-       target file.  It must be the case that 0 <= OFFSET < current
-       size of the target string.
+       target.  It must be the case that 0 <= OFFSET < current size of
+       the target string.
 
        However!  OFFSET + LEN may be *beyond* the end of the existing
        target data.  "Where the heck does the text come from, then?"
@@ -109,92 +141,253 @@ typedef struct svn_delta_op_t {
        same rate you're reading them from the middle.  Thus, if your
        current target text is "abcdefgh", and you get an
        `svn_delta_target' instruction whose OFFSET is 6 and whose LEN
-       is 7, the resulting string is "abcdefghghghghg".  */
-    svn_delta_target,
+       is 7, the resulting string is "abcdefghghghghg".  This trick is
+       actually useful in encoding long runs of consecutive
+       characters, long runs of CR/LF pairs, etc.  */
+    svn_txdelta_target,
 
     /* Append the LEN bytes at OFFSET in the window's NEW string to
-       the target file.  It must be the case that 0 <= OFFSET < OFFSET
-       + LEN <= length of NEW.  */
-    svn_delta_new
+       the target.  It must be the case that 0 <= OFFSET < OFFSET +
+       LEN <= length of NEW.  */
+    svn_txdelta_new
   } action_code;
   
   apr_off_t offset;
   apr_off_t length;
-} svn_delta_op_t;
+} svn_txdelta_op_t;
 
 
 /* How to produce the next stretch of the target string.  */
-typedef struct svn_delta_window_t {
+typedef struct svn_txdelta_window_t {
 
   /* The number of instructions in this window.  */
   int num_ops;
   
   /* The instructions for this window.  */
-  svn_delta_op_t *ops;
+  svn_txdelta_op_t *ops;
 
   /* New data, for use by any `svn_delta_new' instructions.  */
   svn_string_t *new;
 
   /* The sub-pool that this window is living in, needed for
-     svn_free_delta_window() */
+     svn_txdelta_free_window() */
   apr_pool_t *pool;
 
-} svn_delta_window_t;
+} svn_txdelta_window_t;
 
 
-
-/* A function resembling the POSIX `read' system call --- BATON is some
-   opaque structure indicating what we're reading, BUFFER is a buffer
-   to hold the data, and *LEN indicates how many bytes to read.  Upon
-   return, the function should set *LEN to the number of bytes
-   actually read, or zero at the end of the data stream.
-
-   We will need to compute deltas for text drawn from files, memory,
-   sockets, and so on; the data may be huge --- too large to read into
-   memory at one time.  Using `read'-like functions allows us to
-   process the data as we go.  */
-typedef svn_error_t *svn_delta_read_fn_t (void *baton,
-                                          char *buffer,
-                                          apr_off_t *len,
-                                          apr_pool_t *pool);
+/* A delta stream --- this is the hat from which we pull a series of
+   svn_txdelta_window_t objects, which, taken in order, describe the
+   entire target string.  This type is defined within libsvn_delta, and
+   opaque outside that library.  */
+typedef struct svn_txdelta_stream_t svn_txdelta_stream_t;
 
 
-/* A function to consume a series of delta windows.  This function
-   will typically apply each delta window to produce some file, or
-   save it somewhere.  */
-typedef svn_error_t *(svn_text_delta_window_handler_t)
-     (svn_delta_window_t *window, void *baton);
+/* Set *WINDOW to a pointer to the next window from the delta stream
+   STREAM.  When we have completely reconstructed the target string,
+   set *WINDOW to zero.  */
+extern svn_error_t *svn_txdelta_next_window (svn_txdelta_window_t **window,
+                                             svn_txdelta_stream_t *stream);
 
+
+/* Free the delta window WINDOW.  */
+extern void svn_txdelta_free_window (svn_txdelta_window_t *window);
+     
+
+/* Set *STREAM to a pointer to a delta stream that will turn the byte
+   string from SOURCE into the byte stream from TARGET.
+
+   SOURCE_FN and TARGET_FN are both `read'-like functions; see the
+   description of `svn_delta_read_fn_t' above.  When we call
+   `svn_txdelta_next_window' on *STREAM, it will call upon SOURCE_FN
+   and TARGET_FN to gather as much data as it needs.
+
+   Do any necessary allocation in a sub-pool of POOL.  */
+extern svn_error_t *svn_txdelta (svn_txdelta_stream_t **stream,
+                                 svn_delta_read_fn_t *source_fn,
+                                 void *source_baton,
+                                 svn_delta_read_fn_t *target_fn,
+                                 void *target_baton,
+                                 apr_pool_t *pool);
+
+
+/* Free the delta stream STREAM.  */
+extern void svn_txdelta_free (svn_txdelta_stream_t *stream);
+
+
+
+/* Generating and consuming VCDIFF-format text deltas.  */
+
+/* Given a delta stream STREAM, set *READ_FN and *READ_BATON to a
+   `read'-like function that will return a VCDIFF-format byte stream.
+   Do all allocation for the conversion in pool.  */
+extern svn_error_t *svn_txdelta_to_vcdiff (svn_delta_read_fn_t **read_fn,
+                                           void **read_baton,
+                                           svn_txdelta_stream_t *stream,
+                                           apr_pool_t *pool);
+     
+
+/* A function to consume a series of delta windows, in caller-pushes
+   form.  This function will typically apply each delta window to
+   produce some file, or save it somewhere.  */
+typedef svn_error_t *(svn_txdelta_window_handler_t)
+                     (svn_txdelta_window_t *window, void *baton);
+
+
+/* Return a vcdiff parser object, PARSER, in caller-pushes form.  If
+   we're receiving a vcdiff-format byte stream, one block of bytes at
+   a time, we can pass each block in succession to `svn_vcdiff_parse',
+   with PARSER as the other argument.  PARSER keeps track of where we
+   are in the stream; each time we've received enough data for a
+   complete svn_txdelta_window_t, we pass it to HANDLER, along with
+   HANDLER_BATON.  */
+extern svn_vcdiff_parser_t *svn_make_vcdiff_parser
+                            (svn_txdelta_window_handler_t *handler,
+                             void *handler_baton);
+
+
+/* Parse the LEN bytes at BUFFER as the next block of data in the
+   VCDIFF-format stream being parsed by PARSER.  When we've
+   accumulated enough data for a complete window, call PARSER's
+   HANDLER function.
+
+   If LEN is zero, that indicates the end of the VCDIFF data stream.
+   You *must* call svn_vcdiff_parse with LEN == 0 at the end of your
+   data stream; otherwise, the parser may still have buffered data it
+   hasn't passed to HANDLER yet.  */
+extern svn_error_t *svn_vcdiff_parse (svn_vcdiff_parser_t *parser,
+                                      const char *buffer,
+                                      apr_off_t len);
 
 
 /* Traversing tree deltas.  */
 
-/* A structure of callback functions the parser will invoke as it
-   reads in the delta.  */
+
+/* In Subversion, we've got various producers and consumers of tree
+   deltas.
+
+   In processing a `commit' command:
+   - The client examines its working copy data, and produces a tree
+     delta describing the changes to be committed.
+   - The client networking library consumes that delta, and sends them
+     across the wire as an equivalent series of WebDAV requests.
+   - The Apache WebDAV module receives those requests and produces a
+     tree delta --- hopefully equivalent to the one the client
+     produced above.
+   - The Subversion server module consumes that delta and commits an
+     appropriate transaction to the filesystem.
+
+   In processing an `update' command, the process is reversed:
+   - The Subversion server module talks to the filesystem and produces
+     a tree delta describing the changes necessary to bring the
+     client's working copy up to date.
+   - The Apache WebDAV module consumes this delta, and assembles a
+     WebDAV reply representing the appropriate changes.
+   - The client networking library receives that WebDAV reply, and
+     produces a tree delta --- hopefully equivalent to the one the
+     Subversion server produced above.
+   - The working copy library consumes that delta, and makes the
+     appropriate changes to the working copy.
+
+   The simplest approach would be to represent tree deltas as the
+   obvious data structure.  To do an update, the server would
+   construct a delta structure, the working copy library would apply
+   it to the working copy; WebDAV's job would simply be to get the
+   structure across the net intact.
+
+   However, we expect that these deltas will occasionally be too large
+   to fit in a typical workstation's swap area.  For example, in
+   checking out a 200Mb source tree, the entire source tree is
+   represented by a single tree delta.  So it's important to handle
+   deltas that are too large to fit in swap all at once.
+
+   So instead of representing the tree delta explicitly, we define a
+   standard way for a consumer to process each piece of a tree delta
+   as soon as the producer creates it.  The `svn_delta_walk_t'
+   structure is a set of callback functions to be defined by a delta
+   consumer, and invoked by a delta producer.  Each invocation of a
+   callback function describes a piece of the delta --- a file's
+   contents changing, something being renamed, etc.  */
+
+
+/* A structure full of callback functions the delta source will invoke
+   as it produces the delta.  */
 typedef struct svn_delta_walk_t
 {
-  /* In the following callback functions:
+  /* Here's how to use these functions to express a tree delta.
 
-     - NAME is a single path component, not a full directory name.  The
-       caller should use its PARENT_BATON pointers to keep track of
-       the current complete subdirectory name, if necessary.
+     The delta consumer implements the callback functions described in
+     this structure, and the delta producer invokes them.  So the
+     caller (producer) is pushing tree delta data at the callee
+     (consumer).
 
-     - WALK_BATON is the baton for the overall delta walk.  It is the
-       same value passed to `svn_delta_parse'.
+     At the start of traversal, the consumer provides two batons:
 
-     - PARENT_BATON is the baton for the current directory, whose
-       entries we are adding/removing/replacing.
+     - WALK_BATON, a baton global to the entire delta walk.  The
+       producer should pass this value as the WALK_BATON argument to
+       every callback.
 
-     - If ANCESTOR_PATH is non-zero, then ANCESTOR_PATH and
-       ANCESTOR_VERSION indicate the ancestor of the resulting
-       object.
+     - DIR_BATON, a directory baton representing the root of the tree
+       we're applying the delta to.  The producer can use this to
+       describe changes to that root directory, or use `add_directory'
+       and `replace_directory' to obtain batons for other directories
+       in the tree, as explained below.
 
-     - PDELTA is a property delta structure, describing either changes
-       to the existing object's properties (for the `replace_FOO'
-       functions), or a new object's property list as a delta against
-       the empty property list (for the `add_FOO' functions).
+     In the case of `svn_delta_parse', these would be the WALK_BATON
+     and DIR_BATON arguments.  Other producers will work differently.
 
-     So there.  */
+     Most of the callbacks work in the obvious way:
+
+         delete
+         add_file        add_directory    
+         replace_file    replace_directory
+
+     These all take a PARENT_BATON argument, indicating the directory
+     the change takes place in, and a NAME argument, giving the name
+     of the file, directory, or directory entry to change.  (NAME is
+     always a single path component, never a full directory path.)
+
+     While the consumer provides a directory baton for the root of the
+     tree being changed (as explained above), the `add_directory' and
+     `replace_directory' callbacks provide batons for other
+     directories.  Like the callbacks above, they take a PARENT_BATON
+     and a single path component NAME, and then return a new baton for
+     the subdirectory being created / modified --- CHILD_BATON.  The
+     producer can then use CHILD_BATON to make further changes in that
+     subdirectory.
+
+     So, if we already have subdirectories named `foo' and `foo/bar',
+     then the producer can create a new file named `foo/bar/baz.c' by
+     calling:
+        replace_directory (ROOT, "foo") --- yielding a baton F for `foo'
+        replace_directory (F, "bar") --- yielding a baton B for `foo/bar'
+        add_file (B, "baz.c")
+     
+     The producer needn't use the batons in any particular order.  It
+     would be fine for the producer to then call:
+        add_file (ROOT, "qux.c") --- to create `qux.c' in the root
+     and then
+        add_file (F, "quux.c") --- to create `foo/quux.c'.
+     The only restriction is that the producer must call
+     `add_directory' or `replace_directory', so as to obtain a baton
+     for each directory before making changes there.
+
+     When the producer is finished making changes to a directory, it
+     should call `finish_directory'.  This lets the consumer do any
+     necessary cleanup, and free the baton's storage.
+
+     The `add_file' and `replace_file' callbacks each return a baton
+     for the file being created or changed.  This baton can then be
+     passed to `apply_textdelta' to change the file's contents, or
+     `change_file_prop' to change the file's properties.  When the
+     producer is finished making changes to a file, it should call
+     `finish_file', to let the consumer clean up and free the baton.
+
+     The `add_file', `add_directory', `replace_file', and
+     `replace_directory' functions all take arguments ANCESTOR_PATH
+     and ANCESTOR_VERSION.  If ANCESTOR_PATH is non-zero, then
+     ANCESTOR_PATH and ANCESTOR_VERSION indicate the ancestor of the
+     resulting object.  */
 
 
   /* Deleting things.  */
@@ -256,11 +449,11 @@ typedef struct svn_delta_walk_t
                                       svn_string_t *name,
                                       svn_string_t *value);
 
-  /* We are done processing a subdirectory, whose baton is CHILD_BATON
+  /* We are done processing a subdirectory, whose baton is DIR_BATON
      (set by add_directory or replace_directory).  We won't be using
      the baton any more, so whatever resources it refers to may now be
      freed.  */
-  svn_error_t *(*finish_directory) (void *child_baton);
+  svn_error_t *(*finish_directory) (void *dir_baton);
 
 
   /* Creating and modifying files.  */
