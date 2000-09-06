7849430ae86154824e07c57278de6cc01e8b7c83
Karl Fogel <kfogel@apache.org>
2000-09-06 00:39:28 +0000

    * svn_xml.h: new file, declares `get_attribute_value'. * xml.c: new file, part of libsvn_subr. (get_attribute_value): moved to here from xml_parse.c, in anticipation of use in the working copy library.  Also, switched order of arguments (all callers changed).
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@837153 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subversion/include/svn_xml.h
@@ -0,0 +1,65 @@
+/*  svn_xml.h:  xml code shared by various Subversion libraries.
+ *
+ * ================================================================
+ * Copyright (c) 2000 CollabNet.  All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * 3. The end-user documentation included with the redistribution, if
+ * any, must include the following acknowlegement: "This product includes
+ * software developed by CollabNet (http://www.Collab.Net)."
+ * Alternately, this acknowlegement may appear in the software itself, if
+ * and wherever such third-party acknowlegements normally appear.
+ * 
+ * 4. The hosted project names must not be used to endorse or promote
+ * products derived from this software without prior written
+ * permission. For written permission, please contact info@collab.net.
+ * 
+ * 5. Products derived from this software may not use the "Tigris" name
+ * nor may "Tigris" appear in their names without prior written
+ * permission of CollabNet.
+ * 
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL COLLABNET OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ====================================================================
+ * 
+ * This software may consist of voluntary contributions made by many
+ * individuals on behalf of CollabNet.
+ */
+
+
+
+#ifndef SVN_XML_H
+
+
+/* Return the value associated with NAME in expat attribute array ATTS,
+ * else return NULL.  (There could never be a NULL attribute value in
+ * the XML, although the empty string is possible.)
+ * 
+ * ATTS is an array of c-strings: even-numbered indexes are names,
+ * odd-numbers hold values.  If all is right, it should end on an
+ * even-numbered index pointing to NULL.
+ */
+const char *get_attribute_value (char *name, const char **atts);
+
+
+#endif /* SVN_XML_H */
