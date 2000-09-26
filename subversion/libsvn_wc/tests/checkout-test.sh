d3e8d1dcb5106ef5024cd3e38e63d683afd2ab89
Karl Fogel <kfogel@apache.org>
2000-09-26 21:33:34 +0000

    Take care of reference counting within baton constructors and destructors, as much as possible:
    (make_dir_baton, make_file_baton): bump parent dir baton's ref count.
    (free_file_baton): new func.
    (add_directory, add_file): don't bump ref counts.
    (close_file): call free_file_baton when done.
    
    Note: above has either caused or (I suspect) revealed an allocation
    bug.  See the comment in free_file_baton.  Suggestions welcome... I've
    spent all day trying to figure out how freeing a file baton's subpool
    can affect its parent dir baton chain, which is apparently what's
    happening.  Anyone want to play Captain Obvious? :-)
    
    (debug_dir_baton): new temporary func.
    (svn_pool_create): use apr_make_sub_pool().
    
    * checkout-test.sh: just make one tree, for easier debugging.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@837367 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/tests/checkout-test.sh
+++ subversion/libsvn_wc/tests/checkout-test.sh
@@ -8,14 +8,17 @@ if [ ! -x ./checkout-test ]; then
 fi
 
 # First get rid of any remnants of the last run.
+rm -rf this a
 for num in 1 2; do
    rm -rf ${num}/this
 done
 
 # Check out all the deltas.
-for num in 1 2; do
-   echo ./checkout-test checkout-${num}.delta ${num}/this/is/a/test/dir
-   ./checkout-test checkout-${num}.delta ${num}/this/is/a/test/dir
-done
+# for num in 1 2; do
+#    echo ./checkout-test checkout-${num}.delta ${num}/this/is/a/test/dir
+#    ./checkout-test checkout-${num}.delta ${num}/this/is/a/test/dir
+# done
+
+./checkout-test checkout-1.delta this/is/a/test/dir
 
 # That's it.  Right now, we test by inspecting them by hand.
