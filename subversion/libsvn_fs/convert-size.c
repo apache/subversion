6b06e15b9b3b22257ab5144f388ea7a67ef4ba88
jimb <dev-null@apache.org>
2000-09-29 00:08:11 +0000

    First cut at skeleton parsing code
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@837383 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subversion/libsvn_fs/convert-size.c
@@ -0,0 +1,139 @@
+/* convert-size.c --- implementation of size/string conversion functions
+ *
+ * ================================================================
+ * Copyright (c) 2000 Collab.Net.  All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * 3. The end-user documentation included with the redistribution, if
+ * any, must include the following acknowlegement: "This product includes
+ * software developed by Collab.Net (http://www.Collab.Net/)."
+ * Alternately, this acknowlegement may appear in the software itself, if
+ * and wherever such third-party acknowlegements normally appear.
+ * 
+ * 4. The hosted project names must not be used to endorse or promote
+ * products derived from this software without prior written
+ * permission. For written permission, please contact info@collab.net.
+ * 
+ * 5. Products derived from this software may not use the "Tigris" name
+ * nor may "Tigris" appear in their names without prior written
+ * permission of Collab.Net.
+ * 
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL COLLABNET OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ====================================================================
+ * 
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of Collab.Net.
+ */
+
+#include "apr.h"
+#include "convert-size.h"
+
+
+/* Converting text to numbers.  */
+
+apr_size_t
+svn_fs__getsize (const char *data, apr_size_t len,
+		 char **endptr,
+		 apr_size_t max)
+{
+  /* We can't detect overflow by simply comparing value against max,
+     since multiplying value by ten can overflow in strange ways if
+     max is close to the limits of apr_size_t.  For example, suppose
+     that max is 54, and apr_size_t is six bits long; its range is
+     0..63.  If we're parsing the number "482", then value will be 48
+     after parsing the first two digits.  48 * 10 = 480.  But 480
+     doesn't fit in an apr_size_t, so it'll be truncated to 480 mod 64
+     = 32, which is less than max, so we'd fail to recognize the
+     overflow.
+
+     So we do the check for overflow before we multiply value and add
+     in the new digit.  */
+  int max_prefix = max / 10;
+  int max_digit = max % 10;
+  int i;
+  apr_size_t value = 0;
+
+  for (i = 0; i < len && '0' <= data[i] && data[i] <= '9'; i++)
+    {
+      int digit = data[i] - '0';
+
+      /* Check for overflow.  */
+      if (value > max_prefix
+	  || (value == max_prefix && digit > max_digit))
+	{
+	  *endptr = 0;
+	  return 0;
+	}
+
+      value = (value * 10) + digit;
+    }
+
+  /* There must be at least one digit there.  */
+  if (i == 0)
+    {
+      *endptr = 0;
+      return 0;
+    }
+  else
+    {
+      *endptr = data + i;
+      return value;
+    }
+}
+
+
+
+/* Converting numbers to text.  */
+
+int
+svn_fs__putsize (char *data, apr_size_t len, apr_size_t value)
+{
+  int i;
+
+  /* Generate the digits, least-significant first.  */
+  do 
+    {
+      if (i >= len)
+	return 0;
+
+      data[i] = (value % 10) + '0';
+      value /= 10;
+      i++;
+    }
+  while (value > 0);
+
+  /* Put the digits in most-significant-first order.  */
+  {
+    int left, right;
+
+    for (left = 0, right = i-1; left < right; left++, right--)
+      {
+	char t = data[left];
+	data[left] = data[right];
+	data[right] = t;
+      }
+  }
+
+  return i;
+}
