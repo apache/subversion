33aa97a117e751044046e9dd6c0f2ee2ed4275c7
Ben Collins-Sussman <sussman@apache.org>
2000-10-02 21:11:14 +0000

    * wc.h (svn_wc_get_entry_ancestry):  new prototype
    * adm_crawler.c : finish writing, but it still depends on a bunch of
      things in entries.c that don't exist yet.  Therefore it doesn't yet
      compile. :)
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@837418 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/adm_crawler.c
+++ subversion/libsvn_wc/adm_crawler.c
@@ -61,7 +61,8 @@
 
 
 
-/* Local stack used by the crawler */
+/* Local "stack" objects used by the crawler to keep track of dir
+   batons. */
 struct stack_object
 {
   svn_string_t *path;   /* A working copy directory */
@@ -69,10 +70,11 @@ struct stack_object
 
   struct stack_object *next;
   struct stack_object *previous;
-}
+};
 
 
-/* Put a new stack object containing PATH and BATON on top of STACK. */
+/* Create a new stack object containing PATH and BATON and push it on
+   top of STACK. */
 static void
 append_stack (struct stack_object **stack,
               svn_string_t *path,
@@ -102,10 +104,9 @@ remove_stack (struct stack_object **stack)
 }
 
 
-/* A posix-like read function of type svn_read_fn_t.
+/* A posix-like read function of type svn_read_fn_t (see svn_io.h).
 
-   Given an open APR FILEHANDLE, read LEN bytes into BUFFER.
-*/
+   Given an already-open APR FILEHANDLE, read LEN bytes into BUFFER.  */
 static svn_error_t *
 posix_file_reader (void *filehandle,
                    char *buffer,
@@ -115,24 +116,26 @@ posix_file_reader (void *filehandle,
   apr_status_t stat;
 
   /* Recover our filehandle */
-  apr_file_t *xmlfile = (apr_file_t *) baton;
+  apr_file_t *the_file = (apr_file_t *) filehandle;
 
-  stat = apr_full_read (xmlfile, buffer,
+  stat = apr_full_read (the_file, buffer,
                         (apr_size_t) *len,
                         (apr_size_t *) len);
   
   if (stat && (stat != APR_EOF)) 
     return
       svn_error_create (stat, 0, NULL, pool,
-                        "my_read_func: error reading xmlfile");
+                        "adm_crawler.c (posix_file_reader): file read error");
   
   return SVN_NO_ERROR;  
 }
 
 
 
-/* Decide the entry has been added, deleted, or modified by setting
-   the value of NEW_P, MODIFIED_P, or DELETE_P. */
+/* Given the NAME, TYPE, and HASH attribute info for a directory
+   entry, decide if the entry has been added, deleted, or modified.
+   Return these flags by setting the value of NEW_P, MODIFIED_P, or
+   DELETE_P. */
 static svn_error_t *
 set_entry_flags (svn_string_t *current_entry_name,
                  int current_entry_type,
@@ -144,11 +147,13 @@ set_entry_flags (svn_string_t *current_entry_name,
 {
   void *value;
 
-  /* Examine the hash for a "new" xml attribute */
+  /* Examine the hash for a "new" xml attribute.  If this attribute
+     doesn't exists, then the hash value will be NULL.  */
   value = apr_hash_get (current_entry_hash, "new", 3);
   *new_p = value ? TRUE : FALSE;
 
-  /* Examine the hash for a  "delete" xml attribute */
+  /* Examine the hash for a "delete" xml attribute in the same
+     manner. */
   value = apr_hash_get (current_entry_hash, "delete", 6);
   *delete_p = value ? TRUE : FALSE;
 
@@ -161,65 +166,137 @@ set_entry_flags (svn_string_t *current_entry_name,
 
 
 
-/* Given a PATH, return an editor-supplied baton which allows one to
-   edit entries there. */
-static void *
-do_dir_replaces (svn_string_t *path, struct stack_object *stack)
+/* Given a PATH, return NEWEST_BATON which allows one to edit entries
+   there.  Fetch and store (in STACK) any previous directory batons
+   necessary to create the one for PATH (..using calls from EDITOR.)  */
+static svn_error_t *
+do_dir_replaces (svn_string_t *path,
+                 struct stack_object **stack,
+                 svn_delta_edit_fns_t *editor,
+                 void *edit_baton,
+                 void **newest_baton)
 {
-  /* Logic:
+  svn_error_t *err;
+  struct stack_object *stackptr;  /* The current stack object we're
+                                     examining */
 
-     1.  Walk down the stack until you find a non-NULL baton.  (If you
-         reach the bottom of the stack and still can't find one, then
-         call replace_root() and store the root baton in the stack
-         bottom.)
+  stackptr = *stack;   /* Start at the top of the stack */
 
-     2.  Walk *up* the stack now from this non-NULL position, issuing
-         multiple "replace_directory()" calls and storing batons in
-         the stack.  Stop when you fill in the top of the stack.
+  while (1)  /* Walk down the stack until we find a non-NULL dir baton. */
+    {
+      if (stackptr->baton != NULL) 
+        /* Found an existing directory baton! */
+        break;
+      
+      if (stackptr->previous)  
+        stackptr = stackptr->previous;  /* decend. */
+      else
+        {
+          /* Can't descend?  We must have reached stack_bottom, which
+             is just an empty placeholder.  */
 
-     3.  Return the final directory baton at the top of the stack.  */
+          /* Move up the stack to the "root" stackframe and fetch the
+             root baton. */
+          void *root_baton;
 
-  return 0;
+          err = editor->replace_root (edit_baton, &root_baton);  
+          if (err) return err;
+          
+          stackptr->baton = root_baton;
+          break;
+        }
+    }
+
+  /* Now that we're outside the while() loop, our stackptr is pointing
+     to the frame with the "youngest" directory baton. */
+
+  while (1)  /* Now walk _up_ the stack, creating & storing batons. */
+    {
+      if (stackptr->next)
+        {
+          void *dir_baton;
+
+          /* Move up the stack */
+          stackptr = stackptr->next;
+          
+          /* Get a baton for this directory */
+          err = 
+            editor->replace_directory (stackptr->path, /* current dir */
+                                       stackptr->previous->path, /* parent */
+                                       ?, ?, /* Ancestry??? */
+                                       &dir_baton);
+          if (err) return err;
+
+          stackptr->baton = dir_baton;
+        }
+      
+      else 
+        /* Can't move up the stack anymore?  We must be at the top
+           of the stack.  We're all done. */
+        break;
+    }
+
+  /* Return (by reference) the youngest directory baton, the one that
+     goes with our youngest PATH */
+  *newest_baton = stackptr->baton;
+
+  return SVN_NO_ERROR;
 }
 
 
 
+
 /* Examine both the local and text-base copies of a file FILENAME, and
-   push a text-delta to EDITOR using the supplied FILE_BATON. */
+   push a text-delta to EDITOR using the already-opened FILE_BATON.
+   (FILENAME is presumed to be a full path ending with a filename. ) */
 static svn_error_t *
 do_apply_textdelta (svn_string_t *filename,
                     svn_delta_edit_fns_t *editor,
-                    void *file_baton)
+                    void *file_baton,
+                    apr_pool_t *pool)
 {
+  svn_error_t *err;
+  apr_status_t status;
+
   svn_txdelta_window_handler_t *window_handler;
   void *window_handler_baton;
+
   svn_txdelta_stream_t *txdelta_stream;
   svn_txdelta_window_t *txdelta_window;
-  apr_status_t status;
+
   apr_file_t *localfile = NULL;
   apr_file_t *textbasefile = NULL;
 
   /* Apply a textdelta to the file baton, getting a window
      consumer routine and baton */
-  err = (* (editor->apply_textdelta)) (file_baton,
-                                       &window_handler,
-                                       &window_handler_baton);
+  err = editor->apply_textdelta (file_baton,
+                                 &window_handler,
+                                 &window_handler_baton);
   if (err) return err;
 
   /* Open two filehandles, one for local file and one for text-base file. */
-  /* TODO, using *filename and svn_wc__open_ routines. */
+  status = apr_open (&localfile, filename->data,
+                     APR_READ, APR_OS_DEFAULT, pool);
+  if (status)
+    return svn_error_create (status, 0, NULL, pool,
+                             "do_apply_textdelta: error opening local file");
 
+  err = svn_wc__open_text_base (&textbasefile, filename, APR_READ, pool);
+  if (err) return err;
+                                
   /* Create a text-delta stream object that pulls data out of the two
      files. */
   err= svn_txdelta (&txdelta_stream, 
-                    posix_file_reader, (void *) localfile,
-                    posix_file_reader, (void *) textbasefile,
+                    posix_file_reader, localfile,
+                    posix_file_reader, textbasefile,
                     pool);
   if (err) return err;
   
   /* Grab a window from the stream, "push" it at the consumer routine,
-     then free it.  Repeat until there are no more windows. */
-  while (txdelta_window)
+     then free it.  (When we run out of windows, TXDELTA_WINDOW will
+     be set to NULL, and then still passed to window_handler(),
+     thereby notifying window_handler that we're all done.)  */
+  do
     {
       err = svn_txdelta_next_window (&txdelta_window, txdelta_stream);
       if (err) return err;
@@ -228,13 +305,21 @@ do_apply_textdelta (svn_string_t *filename,
       if (err) return err;
       
       svn_txdelta_free_window (txdelta_window);
-    }
 
-  err = (* (window_handler)) (NULL, window_handler_baton);
-  if (err) return err;
+    } while (txdelta_window);
+
 
   /* Free the stream */
   svn_txdelta_free (txdelta_stream);
+
+  /* Close the two files */
+  status = apr_close (localfile);
+  if (status)
+    return svn_error_create (status, 0, NULL, pool,
+                             "do_apply_textdelta: error closing local file");
+
+  err = svn_wc__close_text_base (textbasefile, filename, 0, pool);
+  if (err) return err;
 
   return SVN_NO_ERROR;
 }
@@ -257,7 +342,8 @@ static svn_error_t *
 process_subdirectory (svn_string_t *path,
                       void *dir_baton,
                       svn_delta_edit_fns_t *editor,
-                      struct stack_object  *stack,
+                      void *edit_baton,
+                      struct stack_object *stack,
                       apr_pool_t *pool)
 {
   svn_error_t *err;
@@ -274,7 +360,7 @@ process_subdirectory (svn_string_t *path,
   svn_boolean_t modified_p;
   svn_boolean_t delete_p;
 
-  /* Add the current path and baton to the top of the stack. */
+  /* Push the current path and baton to the top of the stack. */
   append_stack (&stack, path, dir_baton, pool);
 
   /* Start looping over each entry in this directory */
@@ -306,19 +392,23 @@ process_subdirectory (svn_string_t *path,
 
               /* Do what's necesary to get a baton for current directory */
               if (! dir_baton)
-                dir_baton = do_dir_replaces (path);
+                {
+                  err = do_dir_replaces (path, &stack, editor, edit_baton,
+                                         &dir_baton);
+                  if (err) return err;
+                }
 
               /* Add the new directory, getting a new dir baton.  */
-              err = (* (editor->add_directory)) (current_entry_name,
-                                                 dir_baton,
-                                                 ancestor_path,
-                                                 ancestor_version,
-                                                 &new_dir_baton);
+              err = editor->add_directory (current_entry_name,
+                                           dir_baton,
+                                           ancestor_path,
+                                           ancestor_version,
+                                           &new_dir_baton);
               if (err) return err;
 
               /* Recurse into it, using the new dir_baton. */
               err = process_subdirectory (current_entry_name, new_dir_baton,
-                                          editor, stack, pool);
+                                          editor, edit_baton, stack, pool);
               if (err) return err;
             }
 
@@ -330,23 +420,28 @@ process_subdirectory (svn_string_t *path,
               
               /* Do what's necesary to get a baton for current directory */
               if (! dir_baton)
-                dir_baton = do_dir_replaces (path);
+                {
+                  err = do_dir_replaces (path, &stack, editor, edit_baton,
+                                         &dir_baton);
+                  if (err) return err;
+                }
               
               /* Add a new file, getting a file baton */
-              err = (* (editor->add_file)) (current_entry_name,
-                                            dir_baton,
-                                            ancestor_path,
-                                            ancestor_ver,
-                                            &file_baton);
+              err = editor->add_file (current_entry_name,
+                                      dir_baton,
+                                      ancestor_path,
+                                      ancestor_ver,
+                                      &file_baton);
               if (err) return err;
               
               /* Send the text-delta to this file baton */
               err = do_apply_textdelta (current_entry_name,
                                         editor,
-                                        file_baton);
+                                        file_baton,
+                                        pool);
               
               /* Close the file baton. */
-              err = (* (editor->close_file)) (file_baton);
+              err = editor->close_file (file_baton);
               if (err) return err;
             }
         }
@@ -355,10 +450,14 @@ process_subdirectory (svn_string_t *path,
         {
           /* Do what's necesary to get a baton for current directory */
           if (! dir_baton)
-            dir_baton = do_dir_replaces (path);
+            {
+              err = do_dir_replaces (path, &stack, editor, edit_baton,
+                                     &dir_baton);
+              if (err) return err;
+            }
 
           /* Delete the entry */
-          err = (* (editor->delete)) (current_entry_name, dir_baton);
+          err = editor->delete (current_entry_name, dir_baton);
           if (err) return err;
         }
 
@@ -370,21 +469,26 @@ process_subdirectory (svn_string_t *path,
 
           /* Do what's necesary to get a baton for current directory */
           if (! dir_baton)
-            dir_baton = do_dir_replaces (path);
+            {
+              err = do_dir_replaces (path, &stack, editor, edit_baton,
+                                     &dir_baton);
+              if (err) return err;
+            }
           
           /* Replace the file, getting a file baton */
-          err = (* (editor->replace_file)) (current_entry_name,
-                                            dir_baton,
-                                            ancestor_path,
-                                            ancestor_ver,
-                                            &file_baton);
+          err = editor->replace_file (current_entry_name,
+                                      dir_baton,
+                                      ancestor_path,
+                                      ancestor_ver,
+                                      &file_baton);
           if (err) return err;
 
           /* Send the text-delta to this file baton */
-          err = do_apply_textdelta (current_entry_name, editor, file_baton);
+          err = do_apply_textdelta (current_entry_name, editor,
+                                    file_baton, pool);
 
           /* Close the file baton. */
-          err = (* (editor->close_file)) (file_baton);
+          err = editor->close_file (file_baton);
           if (err) return err;
         }
 
@@ -394,7 +498,7 @@ process_subdirectory (svn_string_t *path,
              will force a call to do_dir_replaces() and get the
              _correct_ dir baton for the child directory.  */
           err = process_subdirectory (current_entry_name, NULL,
-                                      editor, stack, pool);
+                                      editor, edit_baton, stack, pool);
         }
 
     } while (current_entry_name)
@@ -406,7 +510,7 @@ process_subdirectory (svn_string_t *path,
   directory. */
   if (stack->baton)
     {
-      err = (* (editor->close_dir)) (stack->baton);
+      err = editor->close_dir (stack->baton);
       if (err) return err;
     }
 
@@ -426,29 +530,36 @@ process_subdirectory (svn_string_t *path,
 
    Do a depth-first crawl of the local changes in a working copy,
    beginning at ROOT_DIRECTORY (absolute path).  Communicate all local
-   changes (both textual and tree) to the supplied EDIT_FNS object.
+   changes (both textual and tree) to the supplied EDIT_FNS object
+   (coupled with the supplied EDIT_BATON).
 
-   (Presumably, the client library will someday grab EDIT_FNS from
-   libsvn_ra, and then pass it to this routine.  This is how local
-   changes in the working copy are ultimately translated into network
-   requests.)  */
+   (Presumably, the client library will someday grab EDIT_FNS and
+   EDIT_BATON from libsvn_ra, and then pass it to this routine.  This
+   is how local changes in the working copy are ultimately translated
+   into network requests.)  */
 
 svn_error_t *
 svn_wc_crawl_local_mods (svn_string_t *root_directory,
                          svn_delta_edit_fns_t *edit_fns,
+                         void *edit_baton,
                          apr_pool_t *pool)
 {
   svn_error_t *err;
-  struct stack_object stack_bottom;
+  struct stack_object *stack_bottom;
 
-  stack_bottom.path = NULL;
-  stack_bottom.baton = NULL;
+  /* Create the bottommost stack object -- it will always have NULL
+     values in its fields.  */
+  stack_bottom = apr_pcalloc (pool, sizeof(struct stack_object));
   
   /* Start the crawler! */
+
+  /* Note that the first thing the crawler will do is push a new stack
+     object onto the stack with PATH="root_directory" and BATON=NULL.  */
   err = process_subdirectory (svn_string_t *root_directory,
                               NULL,             /* No baton to start with. */
                               edit_fns,
-                              &stack_bottom,
+                              edit_baton,
+                              stack_bottom,
                               pool);
 
   if (err) return err;
