af55f67110de20ae389c01f82918e20147340f7f
Karl Fogel <kfogel@apache.org>
2000-10-03 23:24:58 +0000

    (svn_wc__file_affected_time): new func. (set_entry): set a timestamp attribute. (do_parse): note to self.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@837447 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/wc.h
+++ subversion/libsvn_wc/wc.h
@@ -65,6 +65,24 @@
 svn_error_t *svn_wc__check_wc (svn_string_t *path, apr_pool_t *pool);
 
 
+/* Set *APR_TIME to the later of PATH's (a regular file) mtime or ctime.
+ *
+ * Unix traditionally distinguishes between "mod time", which is when
+ * someone last modified the contents of the file, and "change time",
+ * when someone changed something else about the file (such as
+ * permissions).
+ *
+ * Since Subversion versions both kinds of information, our timestamp
+ * comparisons have to notice either kind of change.  That's why this
+ * function gives the time of whichever kind came later.  APR will
+ * hopefully make sure that both ctime and mtime always have useful
+ * values, even on OS's that do things differently. (?)
+ */
+svn_error_t *svn_wc__file_affected_time (apr_time_t *apr_time,
+                                         svn_string_t *path,
+                                         apr_pool_t *pool);
+
+
 /* Set *MODIFIED_P to non-zero if FILENAME has been locally modified,
    else set to zero. */
 svn_error_t *svn_wc__file_modified_p (svn_boolean_t *modified_p,
