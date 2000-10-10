90e5d0642355fe40d96d70e6ed61127edeb29c6d
Greg Hudson <ghudson@apache.org>
2000-10-10 08:30:14 +0000

    Produce svndiff output.  This should give people a way to create some svndiff output to stuff into the test XML deltas.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@837571 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_delta/tests/svndiff-test.c
+++ subversion/libsvn_delta/tests/svndiff-test.c
@@ -8,6 +8,7 @@
 #include "svn_delta.h"
 #include "svn_error.h"
 
+
 /* NOTE: Does no error-checking.  */
 static svn_error_t *
 read_from_file (void *baton, char *buffer, apr_size_t *len, apr_pool_t *pool)
@@ -21,13 +22,15 @@ read_from_file (void *baton, char *buffer, apr_size_t *len, apr_pool_t *pool)
   return SVN_NO_ERROR;
 }
 
-static svn_error_t *
-count_bytes (void *baton, const char *data, apr_size_t *len,
-	     apr_pool_t *pool)
-{
-  apr_size_t *count = (int *) baton;
 
-  *count += *len;
+/* NOTE: Does no error-checking.  */
+static svn_error_t *
+write_to_file (void *baton, const char *data, apr_size_t *len,
+               apr_pool_t *pool)
+{
+  FILE *fp = baton;
+
+  *len = fwrite (data, 1, *len, fp);
   return SVN_NO_ERROR;
 }
 
@@ -41,8 +44,6 @@ main (int argc, char **argv)
   svn_txdelta_window_t *window;
   svn_txdelta_window_handler_t *handler;
   void *baton;
-  apr_size_t new_data = 0, total = 0;
-  int num_ops = 0, nwindows = 0;
 
   source_file = fopen (argv[1], "rb");
   target_file = fopen (argv[2], "rb");
@@ -51,25 +52,16 @@ main (int argc, char **argv)
   svn_txdelta (&stream, read_from_file, source_file,
 	       read_from_file, target_file, NULL);
 
-  svn_txdelta_to_svndiff (count_bytes, &total, NULL, &handler, &baton);
+  svn_txdelta_to_svndiff (write_to_file, stdout, NULL, &handler, &baton);
   do {
     svn_txdelta_next_window (&window, stream);
     handler (window, baton);
-    if (window != NULL)
-      {
-        new_data += window->new->len;
-        num_ops += window->num_ops;
-        nwindows++;
-      }
     svn_txdelta_free_window (window);
   } while (window != NULL);
 
   svn_txdelta_free (stream);
   fclose (source_file);
   fclose (target_file);
-
-  printf("%d %d %lu %lu\n", nwindows, num_ops, (unsigned long) new_data,
-         (unsigned long) total);
 
   apr_terminate();
   exit (0);
