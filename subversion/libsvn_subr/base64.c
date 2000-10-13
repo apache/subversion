341e00958cec1ef2ab15746304746f7bc6766602
Greg Hudson <ghudson@apache.org>
2000-10-13 19:48:15 +0000

    Greg Stein suggested he might want to base64-encode property values.  The write filter interfaces would be rather inconvenient for that, so reorganize the code and add some string interfaces which assume you have all the data at once but don't force you to go through callback hell. (svn_base64_encode_string, svn_base64_decode_string): New, simpler interfaces. * base64.c (encode_bytes, encode_partial_group, decode_bytes): New helper functions to factor out encoding and decoding of byte strings.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@837621 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/base64.c
+++ subversion/libsvn_subr/base64.c
@@ -79,7 +79,7 @@ struct encode_baton {
    six-bit units which are treated as lookups into base64tab for the
    bytes of the output group.  */
 static APR_INLINE void
-encode_group (const unsigned char *in, unsigned char *out)
+encode_group (const unsigned char *in, char *out)
 {
   out[0] = base64tab[in[0] >> 2];
   out[1] = base64tab[((in[0] & 0x3) << 4) | (in[1] >> 4)];
@@ -88,61 +88,77 @@ encode_group (const unsigned char *in, unsigned char *out)
 }
 
 
+/* Base64-encode a byte string which may or may not be the totality of
+   the data being encoded.  INBUF and *INBUFLEN carry the leftover
+   data from call to call, and *LINELEN carries the length of the
+   current output line.  Make INBUF have room for three characters and
+   initialize *INBUFLEN and *LINELEN to 0.  Output will be appended to
+   STR.  */
+static void
+encode_bytes (svn_string_t *str, const char *data, apr_size_t len,
+              unsigned char *inbuf, int *inbuflen, int *linelen)
+{
+  char group[4];
+  const char *p = data, *end = data + len;
+
+  /* Keep encoding three-byte groups until we run out.  */
+  while (*inbuflen + (end - p) >= 3)
+    {
+      memcpy (inbuf + *inbuflen, p, 3 - *inbuflen);
+      p += (3 - *inbuflen);
+      encode_group (inbuf, group);
+      svn_string_appendbytes (str, group, 4);
+      *inbuflen = 0;
+      *linelen += 4;
+      if (*linelen == BASE64_LINELEN)
+        {
+          svn_string_appendcstr (str, "\n");
+          *linelen = 0;
+        }
+    }
+
+  /* Tack any extra input onto *INBUF.  */
+  memcpy (inbuf + *inbuflen, p, end - p);
+  *inbuflen += (end - p);
+}
+
+/* Encode leftover data, if any, and possibly a final newline,
+   appending to STR.  LEN must be in the range 0..2.  */
+static void
+encode_partial_group (svn_string_t *str, const char *extra, int len,
+                      int linelen)
+{
+  unsigned char ingroup[3];
+  char outgroup[4];
+
+  if (len > 0)
+    {
+      memcpy (ingroup, extra, len);
+      memset (ingroup + len, 0, 3 - len);
+      encode_group (ingroup, outgroup);
+      memset (outgroup + (len + 1), '=', 4 - (len + 1));
+      svn_string_appendbytes (str, outgroup, 4);
+      linelen += 4;
+    }
+  if (linelen > 0)
+    svn_string_appendcstr (str, "\n");
+}
+
+/* Write handler for svn_base64_encode.  */
 static svn_error_t *
 encode_data (void *baton, const char *data, apr_size_t *len, apr_pool_t *pool)
 {
   struct encode_baton *eb = baton;
   apr_pool_t *subpool = svn_pool_create (pool);
   svn_string_t *encoded = svn_string_create ("", subpool);
-  const unsigned char *p = (unsigned char *) data;
-  const unsigned char *end = (unsigned char *) data + *len;
-  unsigned char group[4];
   apr_size_t enclen;
   svn_error_t *err = SVN_NO_ERROR;
 
-  if (*len == 0)
-    {
-      /* We may need to encode a partial group.  The way we do this is
-	 to zero-fill the remainder of the input group, encode it, and
-	 then replace the unnecessary bytes of the output group with
-	 '=' characters.  Also finish up with a newline if we aren't
-	 at the beginning of a line.  */
-      if (eb->buflen > 0)
-	{
-	  memset (eb->buf + eb->buflen, 0, 3 - eb->buflen);
-	  encode_group (eb->buf, group);
-	  memset (group + (eb->buflen + 1), '=', 4 - (eb->buflen + 1));
-	  svn_string_appendbytes (encoded, group, 4);
-	  eb->linelen += 4;
-	}
-      if (eb->linelen != 0)
-	svn_string_appendcstr (encoded, "\n");
-    }
+  /* Decode this block of data, or finish up if there is no more.  */
+  if (*len > 0)
+    encode_bytes (encoded, data, *len, eb->buf, &eb->buflen, &eb->linelen);
   else
-    {
-      /* This code looks a little weird but it gets all the edge cases
-	 right without extra logic.  Keep copying bytes of the input
-	 into eb->buf and encoding them as long as we have groups of
-	 three.  */
-      while (eb->buflen + (end - p) >= 3)
-	{
-	  memcpy (eb->buf + eb->buflen, p, 3 - eb->buflen);
-	  p += (3 - eb->buflen);
-	  encode_group (eb->buf, group);
-	  svn_string_appendbytes (encoded, group, 4);
-	  eb->buflen = 0;
-	  eb->linelen += 4;
-	  if (eb->linelen == BASE64_LINELEN)
-	    {
-	      svn_string_appendcstr (encoded, "\n");
-	      eb->linelen = 0;
-	    }
-	}
-
-      /* Tack any extra input onto eb->buf.  */
-      memcpy (eb->buf + eb->buflen, p, end - p);
-      eb->buflen += (end - p);
-    }
+    encode_partial_group (encoded, eb->buf, eb->buflen, eb->linelen);
 
   /* Write the output, clean up, go home.  */
   enclen = encoded->len;
@@ -177,13 +193,26 @@ svn_base64_encode (svn_write_fn_t *output, void *output_baton,
 }
 
 
+svn_string_t *
+svn_base64_encode_string (svn_string_t *str, apr_pool_t *pool)
+{
+  svn_string_t *encoded = svn_string_create ("", pool);
+  unsigned char ingroup[3];
+  int ingrouplen = 0, linelen = 0;
+
+  encode_bytes (encoded, str->data, str->len, ingroup, &ingrouplen, &linelen);
+  encode_partial_group (encoded, ingroup, ingrouplen, linelen);
+  return encoded;
+}
+
+
 
 /* Base64-encoded input --> binary output */
 
 struct decode_baton {
   svn_write_fn_t *output;
   void *output_baton;
-  unsigned char buf[4];         /* Bytes waiting to be decoded */
+  char buf[4];                  /* Bytes waiting to be decoded */
   int buflen;                   /* Number of bytes waiting */
   svn_boolean_t done;		/* True if we already saw an '=' */
   apr_pool_t *pool;
@@ -203,59 +232,68 @@ decode_group (const unsigned char *in, unsigned char *out)
 }
 
 
-static svn_error_t *
-decode_data (void *baton, const char *data, apr_size_t *len, apr_pool_t *pool)
+/* Decode a byte string which may or may not be the total amount of
+   data being decoded.  INBUF and *INBUFLEN carry the leftover bytes
+   fro call to call, and *DONE keeps track of whether we've seen an
+   '=' which terminates the encoded data.  Have room for four bytes in
+   INBUF and initialize *INBUFLEN to 0 and *DONE to FALSE.  Output
+   will be appended to STR.  */
+static void
+decode_bytes (svn_string_t *str, const char *data, apr_size_t len,
+              char *inbuf, int *inbuflen, svn_boolean_t *done)
 {
-  struct decode_baton *db = baton;
-  apr_pool_t *subpool = svn_pool_create (pool);
-  svn_string_t *decoded = svn_string_create ("", subpool);
-  const char *p = data;
-  const char *end = data + *len;
-  const char *find;
-  unsigned char group[3];
-  apr_size_t declen;
-  svn_error_t *err = SVN_NO_ERROR;
+  const char *p, *find;
+  unsigned char group[4];
 
-  if (*len == 0)
-    {
-      err = db->output (db->output_baton, NULL, len, pool);
-      apr_destroy_pool (subpool);
-      apr_destroy_pool (db->pool);
-      return err;
-    }
-
-  /* Keep adding characters to the input group (in db->buf) and
-     decoding groups whenever we hit 4.  If there is a partial group
-     at the end, an '=' will mark it.  If the input is invalid, just
-     spit out something plausible; we're not too worried about
-     nonconformant base64 encoders.  */
-  while (p < end && !db->done)
+  for (p = data; !*done && p < data + len; p++)
     {
       if (*p == '=')
 	{
 	  /* We are at the end and have to decode a partial group.  */
-	  if (db->buflen >= 2)
+	  if (*inbuflen >= 2)
 	    {
-	      memset (db->buf + db->buflen, 0, 4 - db->buflen);
-	      decode_group (db->buf, group);
-	      svn_string_appendbytes (decoded, group, db->buflen - 1);
+	      memset (inbuf + *inbuflen, 0, 4 - *inbuflen);
+	      decode_group (inbuf, group);
+	      svn_string_appendbytes (str, group, *inbuflen - 1);
 	    }
-	  db->done = TRUE;
+	  *done = TRUE;
 	}
       else
 	{
 	  find = strchr (base64tab, *p);
 	  if (find != NULL)
-	    db->buf[db->buflen++] = find - base64tab;
-	  if (db->buflen == 4)
+	    inbuf[(*inbuflen)++] = find - base64tab;
+	  if (*inbuflen == 4)
 	    {
-	      decode_group (db->buf, group);
-	      svn_string_appendbytes (decoded, group, 3);
-	      db->buflen = 0;
+	      decode_group (inbuf, group);
+	      svn_string_appendbytes (str, group, 3);
+	      *inbuflen = 0;
 	    }
 	}
-      p++;
     }
+}
+
+static svn_error_t *
+decode_data (void *baton, const char *data, apr_size_t *len, apr_pool_t *pool)
+{
+  struct decode_baton *db = baton;
+  apr_pool_t *subpool;
+  svn_string_t *decoded;
+  apr_size_t declen;
+  svn_error_t *err = SVN_NO_ERROR;
+
+  if (*len == 0)
+    {
+      /* No more data to decode; pass that on to db->output and clean up.  */
+      err = db->output (db->output_baton, NULL, len, pool);
+      apr_destroy_pool (db->pool);
+      return err;
+    }
+
+  /* Decode this block of data.  */
+  subpool = svn_pool_create (pool);
+  decoded = svn_string_create ("", subpool);
+  decode_bytes (decoded, data, *len, db->buf, &db->buflen, &db->done);
 
   /* Write the output, clean up, go home.  */
   declen = decoded->len;
@@ -281,6 +319,19 @@ svn_base64_decode (svn_write_fn_t *output, void *output_baton,
   db->pool = subpool;
   *decode = decode_data;
   *decode_baton = db;
+}
+
+
+svn_string_t *
+svn_base64_decode_string (svn_string_t *str, apr_pool_t *pool)
+{
+  svn_string_t *decoded = svn_string_create ("", pool);
+  char ingroup[4];
+  int ingrouplen = 0;
+  svn_boolean_t done = FALSE;
+
+  decode_bytes (decoded, str->data, str->len, ingroup, &ingrouplen, &done);
+  return decoded;
 }
 
 
