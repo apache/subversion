04398f1ba8b2780bdd896faac74583ca78a43468
Karl Fogel <kfogel@apache.org>
2000-10-17 21:47:15 +0000

    (svn_path_canonicalize): semicolon assumed the position.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@837690 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_path.h
+++ subversion/include/svn_path.h
@@ -133,7 +133,7 @@ int svn_path_isempty (const svn_string_t *path, enum svn_path_style style);
    (At some future point, this may make other semantically inoperative
    transformations.) */
 static void svn_path_canonicalize (svn_string_t *path,
-                                   enum svn_path_style style)
+                                   enum svn_path_style style);
 
 
 #endif /* SVN_PATHS_H */
