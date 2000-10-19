eda0f30de3ba6bad1f563bda45de8ee573e8e031
Karl Fogel <kfogel@apache.org>
2000-10-19 21:54:04 +0000

    (svn_wc__adm_path, svn_wc__locked, svn_wc__log_commit): new funcs.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@837732 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/lock.c
+++ subversion/libsvn_wc/lock.c
@@ -87,6 +87,34 @@ svn_wc__unlock (svn_string_t *path, apr_pool_t *pool)
 }
 
 
+svn_error_t *
+svn_wc__locked (svn_boolean_t *locked, svn_string_t *path, apr_pool_t *pool)
+{
+  svn_error_t *err;
+  enum svn_node_kind kind;
+  svn_string_t *lockfile
+    = svn_wc__adm_path (path, 0, pool, SVN_WC__ADM_LOCK, NULL);
+                                             
+  err = svn_io_check_path (lockfile, &kind, pool);
+  if (err)
+    return err;
+  else if (kind == svn_file_kind)
+    *locked = 1;
+  else if (kind == svn_invalid_kind)
+    *locked = 0;
+  else
+    return svn_error_createf (SVN_ERR_WC_LOCKED,
+                              0,
+                              NULL,
+                              pool,
+                              "svn_wc__locked: "
+                              "lock file is not a regular file (%s)",
+                              path->data);
+    
+  return SVN_NO_ERROR;
+}
+
+
 
 /* 
  * local variables:
