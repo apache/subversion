543a6851561e397b0cf2cc015f4b64e7531e435b
Karl Fogel <kfogel@apache.org>
2000-10-20 02:24:50 +0000

    Demonstrate commits of modified files.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@837745 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/client/tests/svn-test.sh
+++ subversion/client/tests/svn-test.sh
@@ -18,16 +18,25 @@ ${SVN_PROG} checkout                                      \
       --version 1                                         \
       --ancestor-path ${ANCESTOR_PATH}
 
-### Adding.
+### Modifying some existing files.
 echo
-echo "Adding a file."
-touch this/newfile1
-${SVN_PROG} add this/newfile1
+echo "Modifying A/D/G/pi."
+echo "fish" >> this/A/D/G/pi
 
 echo
-echo "Adding another file."
-touch this/A/B/E/newfile2
-${SVN_PROG} add this/A/B/E/newfile2
+echo "Modifying A/mu."
+echo "moo" >> this/A/mu
+
+### Adding.
+# echo
+# echo "Adding newfile1."
+# touch this/newfile1
+# ${SVN_PROG} add this/newfile1
+# 
+# echo
+# echo "Adding A/B/E/newfile2."
+# touch this/A/B/E/newfile2
+# ${SVN_PROG} add this/A/B/E/newfile2
 
 ### Updating.
 # echo
@@ -41,17 +50,17 @@ ${SVN_PROG} add this/A/B/E/newfile2
 #              --xml-file ../../../tests-common/xml/up1b-inline.xml)
                       
 ### Deleting.
-echo
-echo "Deleting a versioned file, with --force."
-${SVN_PROG} delete --force this/A/D/H/omega
-
-echo
-echo "Deleting one of the added files, without --force."
-${SVN_PROG} delete this/A/B/E/newfile2
+# echo
+# echo "Deleting versioned file A/D/H/omega, with --force."
+# ${SVN_PROG} delete --force this/A/D/H/omega
+# 
+# echo
+# echo "Deleting added files A/B/E/newfile2, without --force."
+# ${SVN_PROG} delete this/A/B/E/newfile2
  
 ### Committing.
 ## Disable commits until they're working.
-# echo
-# echo "Committing."
-# echo
-# (cd this; ../${SVN_PROG} commit --xml-file ../commit-1.xml --version 1; cd ..)
+echo
+echo "Committing."
+echo
+(cd this; ../${SVN_PROG} commit --xml-file ../commit-1.xml --version 2; cd ..)
