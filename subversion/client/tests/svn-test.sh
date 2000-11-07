6c6e4a09148883f255effc1018b555c25d058395
Karl Fogel <kfogel@apache.org>
2000-11-07 23:38:20 +0000

    Preserve local mods by patching:
    * libsvn_client/tests/svn-test.sh: make more changes in the updated
    tree t2, commit them.
    
    * libsvn_wc/get_editor.c (close_file): if working file present, update
    by patching.
    
    * libsvn_wc/log.c (start_handler): implement SVN_WC__LOG_RUN_CMD.
    (run_cmd_under_directory): removed, unnecessary right now.
    
    Note: this change breaks "make check", because the final update in
    client/tests/t2 fails, apparently due to some ascii nulls in the XML
    file resulting from the second commit (run "make check" in
    subversion/client/, then look at subversion/client/tests/commit-2.xml,
    line 51).
    
    Am committing anyway for the sake of reproduction, but will commit
    another change immediately to restore "make check"'s success.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@837905 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/client/tests/svn-test.sh
+++ subversion/client/tests/svn-test.sh
@@ -4,6 +4,7 @@ SVN_PROG=../svn
 TEST_DIR_1=t1
 TEST_DIR_2=t2
 COMMIT_RESULTFILE_1=commit-1.xml
+COMMIT_RESULTFILE_2=commit-2.xml
 ANCESTOR_PATH=anni       # See if Greg Stein notices. :-) 
 
 # Remove the testing tree
@@ -18,7 +19,7 @@ echo "Checking out ${TEST_DIR_1}."
 ${SVN_PROG} checkout                                      \
       -d ${TEST_DIR_1}                                    \
       --xml-file ../../tests-common/xml/co1-inline.xml    \
-      --revision 1                                         \
+      --revision 1                                        \
       --ancestor-path ${ANCESTOR_PATH}
 
 ### Copy the pristine checked-out tree, so we can test updates later.
@@ -26,10 +27,10 @@ cp -R -p ${TEST_DIR_1} ${TEST_DIR_2}
 
 ### Modify some existing files.
 echo "Modifying ${TEST_DIR_1}/A/D/G/pi."
-echo "fish" >> ${TEST_DIR_1}/A/D/G/pi
+echo "in t1, adding a second line to A/D/G/pi" >> ${TEST_DIR_1}/A/D/G/pi
 
 echo "Modifying ${TEST_DIR_1}/A/mu."
-echo "moo" >> ${TEST_DIR_1}/A/mu
+echo "in t1, adding a second line to A/mu" >> ${TEST_DIR_1}/A/mu
 
 ### Add.
 echo "Adding ${TEST_DIR_1}/newfile1."
@@ -55,15 +56,48 @@ echo "Committing changes in ${TEST_DIR_1}."
  ../${SVN_PROG} commit --xml-file ../${COMMIT_RESULTFILE_1} --revision 2;   \
  cd ..)
 
-### Show the results of the commit to the user.
-# cat ${COMMIT_RESULTFILE_1}
-
 ### Update.
 echo "Updating ${TEST_DIR_2} from changes in ${TEST_DIR_1}."
 (cd ${TEST_DIR_2};                                           \
  ../${SVN_PROG} update --xml-file ../${COMMIT_RESULTFILE_1}  \
                 --revision 2;                                 \
  cd ..)
+
+### Modify some more files.
+echo "Modifying ${TEST_DIR_1}/A/D/G/pi."
+echo "in t2, adding a third line to A/D/G/pi" >> ${TEST_DIR_2}/A/D/G/pi
+echo "in t2, adding a fourth line to A/D/G/pi" >> ${TEST_DIR_2}/A/D/G/pi
+echo "in t2, adding a fifth line to A/D/G/pi" >> ${TEST_DIR_2}/A/D/G/pi
+echo "in t2, adding a sixth line to A/D/G/pi" >> ${TEST_DIR_2}/A/D/G/pi
+echo "in t2, adding a seventh line to A/D/G/pi" >> ${TEST_DIR_2}/A/D/G/pi
+echo "in t2, adding a eighth line to A/D/G/pi" >> ${TEST_DIR_2}/A/D/G/pi
+echo "in t2, adding a ninth line to A/D/G/pi" >> ${TEST_DIR_2}/A/D/G/pi
+echo "in t2, adding a tenth line to A/D/G/pi" >> ${TEST_DIR_2}/A/D/G/pi
+
+echo "Modifying ${TEST_DIR_1}/A/mu."
+echo "in t2, adding a third line to A/mu" >> ${TEST_DIR_2}/A/mu
+echo "in t2, adding a fourth line to A/mu" >> ${TEST_DIR_2}/A/mu
+echo "in t2, adding a fifth line to A/mu" >> ${TEST_DIR_2}/A/mu
+echo "in t2, adding a sixth line to A/mu" >> ${TEST_DIR_2}/A/mu
+echo "in t2, adding a seventh line to A/mu" >> ${TEST_DIR_2}/A/mu
+echo "in t2, adding a eighth line to A/mu" >> ${TEST_DIR_2}/A/mu
+echo "in t2, adding a ninth line to A/mu" >> ${TEST_DIR_2}/A/mu
+echo "in t2, adding a tenth line to A/mu" >> ${TEST_DIR_2}/A/mu
+
+### Commit.
+echo "Committing changes, this time in ${TEST_DIR_2}."
+(cd ${TEST_DIR_2};                                               \
+ ../${SVN_PROG} commit --xml-file ../${COMMIT_RESULTFILE_2} --revision 3;   \
+ cd ..)
+
+
+### Update.
+echo "Updating ${TEST_DIR_1} from changes in ${TEST_DIR_2}."
+(cd ${TEST_DIR_1};                                           \
+ ../${SVN_PROG} update --xml-file ../${COMMIT_RESULTFILE_2}  \
+                --revision 3;                                 \
+ cd ..)
+
 
 ### Diff the two trees.  The only differences should be in timestamps
 ### and some of the version numbers (see README), so ignore those.
