91bd8d25c9bec4c1765ccc94b4fbf720a57f895c
bkorb <bkorb@unknown>
2000-11-13 01:40:58 +0000

    tabularize the command dispatch
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@837988 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subversion/client/st-cmd.c
@@ -0,0 +1,73 @@
+/*
+ *  st-cmd.c - Display status information in current directory
+ *
+ *  svn is free software copyrighted by CollabNet.
+ *  
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *  3. Neither the name ``CollabNet'' nor the name of any other
+ *     contributor may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *  
+ *  svn IS PROVIDED BY CollabNet ``AS IS'' AND ANY EXPRESS
+ *  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED.  IN NO EVENT SHALL CollabNet OR ANY OTHER CONTRIBUTORS
+ *  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ *  BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ *  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ====================================================================
+ * 
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of CollabNet.
+ */
+
+/* ==================================================================== */
+
+
+
+/*** Includes. ***/
+
+#include "svn_wc.h"
+#include "svn_client.h"
+#include "svn_string.h"
+#include "svn_path.h"
+#include "svn_delta.h"
+#include "svn_error.h"
+#include "cl.h"
+
+
+/*** Code. ***/
+
+svn_error_t *
+svn_cl__status( int argc, char** argv, apr_pool_t* pool )
+{
+  svn_revnum_t revision = SVN_INVALID_REVNUM;
+  svn_string_t *xml_file = NULL;
+  svn_string_t *target = NULL;
+  svn_string_t *ancestor_path = NULL;
+  svn_boolean_t force = 0;
+  svn_error_t *err = NULL;
+  apr_hash_t *statushash;
+
+  svn_cl__parse_options (argc, argv, STATUS_COMMAND,
+                 &xml_file, &target, &revision, &ancestor_path, &force,
+                 pool);
+
+  err = svn_client_status (&statushash, target, pool);
+  if (! err) 
+    svn_cl__print_status_list (statushash, pool);
+
+  return err;
+}
