d4291e3ea03b3a3a57eeb79345a557be90edbba0
Ben Collins-Sussman <sussman@apache.org>
2000-11-16 18:48:06 +0000

    Duh, add the new file.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@838042 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subversion/client/propget-cmd.c
@@ -0,0 +1,82 @@
+/*
+ *  propget-cmd.c - Display status information in current directory
+ *
+ *  svn is free software copyrighted by CollabNet.
+ *  
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *  3. Neither the name ``CollabNet'' nor the name of any other
+ *     contributor may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *  
+ *  svn IS PROVIDED BY CollabNet ``AS IS'' AND ANY EXPRESS
+ *  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED.  IN NO EVENT SHALL CollabNet OR ANY OTHER CONTRIBUTORS
+ *  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ *  BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ *  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ====================================================================
+ * 
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of CollabNet.
+ */
+
+/* ==================================================================== */
+
+
+
+/*** Includes. ***/
+
+#include "svn_wc.h"
+#include "svn_client.h"
+#include "svn_string.h"
+#include "svn_path.h"
+#include "svn_delta.h"
+#include "svn_error.h"
+#include "cl.h"
+
+
+/*** Code. ***/
+
+svn_error_t *
+svn_cl__propget( int argc, char** argv, apr_pool_t* pool,
+                 svn_cl__opt_state_t *p_opt_state )
+{
+  svn_error_t *err = NULL;
+  svn_string_t *name, *value;
+  apr_hash_t *prop_hash = apr_make_hash (pool);
+
+  name = GET_OPT_STATE(p_opt_state, name);
+
+  err = svn_wc_prop_get (&value,
+                         name,
+                         GET_OPT_STATE(p_opt_state, target),
+                         pool);
+
+  if (! err) 
+    {
+      apr_hash_set (prop_hash, name->data, name->len, value); 
+      svn_cl__print_prop_hash (prop_hash, pool);
+    }      
+
+  return err;
+}
+
+
+/* 
+ * local variables:
+ * eval: (load-file "../svn-dev.el")
+ * end: 
+ */
