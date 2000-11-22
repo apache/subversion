92ea5942bc408c484dc9e67432098f31b7179297
Greg Stein <gstein@apache.org>
2000-11-22 21:10:23 +0000

    add the -C switch to support changing licenses in files
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@838098 13f79535-47bb-0310-9956-ffa450edef68

--- tools/check-license.py
+++ tools/check-license.py
@@ -2,16 +2,17 @@
 #
 # check if a file has the proper license in it
 #
-# USAGE: check-license.py file1 file2 ... fileN
+# USAGE: check-license.py [-C] file1 file2 ... fileN
 #
 # If the license cannot be found, then the filename is printed to stdout.
 # Typical usage:
 #    $ find . -name "*.[ch]" | xargs check-license.py > bad-files
 #
-# todo: add a switch for rewriting the file with the right license
+# -C switch is used to change licenses. Typical usage:
+#    $ find . -name "*.[ch]" | xargs check-license.py -C
 #
 
-LICENSE = '''\
+OLD_LICENSE = '''\
  * ================================================================
  * Copyright (c) 2000 CollabNet.  All rights reserved.
  * 
@@ -58,14 +59,40 @@ LICENSE = '''\
  * individuals on behalf of CollabNet.
 '''
 
+NEW_LICENSE = '''\
+ * ================================================================
+ * Copyright (c) 2000 CollabNet.  All rights reserved.
+ *
+ * Mumbo jumbo goes here
+ *
+ * ====================================================================
+ * 
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of CollabNet.
+'''
+
 import sys
 import string
 
 def check_file(fname):
   s = open(fname).read()
-  if string.find(s, LICENSE) == -1:
+  if string.find(s, OLD_LICENSE) == -1:
     print fname
 
+def change_license(fname):
+  s = open(fname).read()
+  if string.find(s, OLD_LICENSE) == -1:
+    print 'ERROR: missing old license:', fname
+  else:
+    s = string.replace(s, OLD_LICENSE, NEW_LICENSE)
+    open(fname, 'w').write(s)
+    print 'Changed:', fname
+
 if __name__ == '__main__':
-  for f in sys.argv[1:]:
-    check_file(f)
+  if sys.argv[1] == '-C':
+    print 'Changing license text...'
+    for f in sys.argv[2:]:
+      change_license(f)
+  else:
+    for f in sys.argv[1:]:
+      check_file(f)
