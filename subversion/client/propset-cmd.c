afa634d8d322bdc7a862965563d61f0b1bb61657
Karl Fogel <kfogel@apache.org>
2000-11-25 05:50:10 +0000

    * main.c (read_from_file): new helper func. (main): if see valfile, read value right away, don't defer.
    * propset-cmd.c (svn_cl__propset): use opt_state->value, which will
    have been set in main no matter what.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@838116 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/client/propset-cmd.c
+++ subversion/client/propset-cmd.c
@@ -35,48 +35,7 @@ svn_cl__propset (svn_cl__opt_state_t *opt_state,
                  apr_pool_t *pool)
 {
   svn_error_t *err;
-  char buf[BUFSIZ];
   int i;
-
-  /* kff todo: this may get moved into main, since reading values from
-     files is a generic operation that many commands may need to
-     support. */
-  if (opt_state->valfile)
-    {
-      /* Load the whole file into `value'.  
-         
-         What?  Don't look at me like that.  
-         
-         Don't forget that our entire property implementation happens
-         "in-memory" right now.  And we're not just talking about
-         single property name/value pairs; whole *lists* of pairs move
-         from disk to memory and back. */
-      svn_error_t *err2;
-      apr_status_t status;
-      apr_size_t len = BUFSIZ;
-      apr_file_t *the_file = NULL;
-      
-      /* kff todo: possibly blowing away a previous setting of
-         opt_state->value here.  But that redundancy should be checked
-         earlier (see other comment above) anyway. */
-      opt_state->value = svn_string_create ("", pool);
-
-      status = apr_open (&the_file, opt_state->valfile->data,
-                         APR_READ, APR_OS_DEFAULT, pool);
-      if (status)
-        return svn_error_createf (status, 0, NULL, pool,
-                                  "svn_cl__propset:  failed to open '%s'",
-                                  opt_state->valfile->data);
-      
-      do {
-        err2 = svn_io_file_reader (the_file, buf, &len, pool);
-        if (err2) return err2;
-
-        svn_string_appendbytes (opt_state->value, buf, len);
-
-      } while (len != 0);
-    }
-
 
   if (! strcmp (opt_state->value->data, ""))
     /* The user wants to delete the property. */
