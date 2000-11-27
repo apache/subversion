d16f8d8a5a954c753034632eefc4377a3ec1ba9b
Greg Stein <gstein@apache.org>
2000-11-27 13:03:17 +0000

    * liveprops.c, dav_svn.h: sync with updated mod_dav.h: prototype for   find_liveprop hook now takes a dav_resource (rather than a request_rec)
    * liveprops.c (dav_svn_find_liveprop, dav_svn_insert_all_liveprops): add
      check on the passed resource to ensure it is "ours" before looking for or
      inserting props.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@838123 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/mod_dav_svn/dav_svn.h
+++ subversion/mod_dav_svn/dav_svn.h
@@ -103,7 +103,8 @@ struct dav_resource_private {
 
 
 void dav_svn_gather_propsets(apr_array_header_t *uris);
-int dav_svn_find_liveprop(request_rec *r, const char *ns_uri, const char *name,
+int dav_svn_find_liveprop(const dav_resource *resource,
+                          const char *ns_uri, const char *name,
                           const dav_hooks_liveprop **hooks);
 void dav_svn_insert_all_liveprops(request_rec *r, const dav_resource *resource,
                                   int insvalue, ap_text_header *phdr);
