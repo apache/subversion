473c101919bcd1b161ab75a9422e0f1cbaf85ec8
jimb <dev-null@apache.org>
2000-12-10 04:24:08 +0000

    Hey nonny nonny.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@838193 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subversion/libsvn_fs/trail.c
@@ -0,0 +1,120 @@
+/* trail.c : backing out of aborted Berkeley DB transactions
+ *
+ * ====================================================================
+ * Copyright (c) 2000 CollabNet.  All rights reserved.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at http://subversion.tigris.org/license-1.html.
+ * If newer versions of this license are posted there, you may use a
+ * newer version instead, at your option.
+ * ====================================================================
+ */
+
+#include "db.h"
+#include "apr_pools.h"
+#include "svn_fs.h"
+#include "fs.h"
+#include "err.h"
+#include "trail.h"
+
+
+/* A single action to be undone.  Actions are chained so that later
+   actions point to earlier actions.  Thus, walking the chain and
+   applying the functions should undo actions in the reverse of the
+   order they were performed.  */
+struct undo {
+  void (*func) (void *baton);
+  void *baton;
+  struct undo *prev;
+};
+
+
+static svn_error_t *
+begin_trail (trail_t **trail_p,
+	     svn_fs_t *fs,
+	     apr_pool_t *pool)
+{
+  trail_t *trail = apr_palloc (pool, sizeof (*trail));
+
+  trail->pool = pool;
+  trail->undo = 0;
+  SVN_ERR (DB_WRAP (fs, "beginning Berkeley DB transaction",
+		    txn_begin (fs->env, 0, &trail->db_txn, 0)));
+
+  *trail_p = trail;
+  return 0;
+}
+
+
+static svn_error_t *
+abort_trail (trail_t *trail,
+	     svn_fs_t *fs)
+{
+  struct undo *undo;
+
+  /* Revert any in-memory changes we made as part of this transaction.  */
+  for (undo = trail->undo; undo; undo = undo->prev)
+    undo->func (undo->baton);
+
+  SVN_ERR (DB_WRAP (fs, "aborting Berkeley DB transaction",
+		    txn_abort (trail->db_txn)));
+ 
+  apr_destroy_pool (trail->pool);
+
+  return 0;
+}
+
+
+svn_error_t *
+svn_fs__retry_txn (svn_fs_t *fs,
+		   svn_error_t *(*txn_body) (void *baton, trail_t *trail),
+		   void *baton,
+		   apr_pool_t *pool)
+{
+  for (;;)
+    {
+      trail_t *trail;
+      svn_error_t *svn_err;
+      
+      SVN_ERR (begin_trail (&trail, fs, pool));
+
+      /* Do the body of the transaction.  */
+      svn_err = (*txn_body) (baton, trail);
+
+      if (! svn_err)
+	{
+	  /* The transaction succeeded!  Commit it.  */
+	  SVN_ERR (DB_WRAP (fs,
+			    "committing Berkeley DB transaction",
+			    txn_commit (trail->db_txn, 0)));
+	  return 0;
+	}
+
+      /* Is this a real error, or do we just need to retry?  */
+      if (svn_err->apr_err != SVN_ERR_BERKELEY_DB
+	  || svn_err->src_err != DB_LOCK_DEADLOCK)
+	{
+	  /* Ignore any error returns.  The first error is more valuable.  */
+	  abort_trail (trail, fs);
+	  return svn_err;
+	}
+
+      /* We deadlocked.  Abort the transaction, and try again.  */
+      SVN_ERR (abort_trail (trail, fs));
+    }
+}
+
+
+void
+svn_fs__record_undo (trail_t *trail,
+		     void (*func) (void *baton),
+		     void *baton)
+{
+  struct undo *undo = apr_palloc (trail->pool, sizeof (*undo));
+
+  undo->func = func;
+  undo->baton = baton;
+  undo->prev = trail->undo;
+  trail->undo = undo;
+}
---
+++ subversion/libsvn_fs/trail.h
@@ -0,0 +1,79 @@
+/* trail.h : internal interface to backing out of aborted Berkeley DB txns
+ *
+ * ====================================================================
+ * Copyright (c) 2000 CollabNet.  All rights reserved.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at http://subversion.tigris.org/license-1.html.
+ * If newer versions of this license are posted there, you may use a
+ * newer version instead, at your option.
+ * ====================================================================
+ */
+
+#ifndef SVN_LIBSVN_FS_TRAIL_H
+#define SVN_LIBSVN_FS_TRAIL_H
+
+#include "db.h"
+#include "apr_pools.h"
+
+
+/* A `trail' structure provides a context for a Berkeley DB transaction,
+   helping us ensure that:
+   - changes made to in-memory structures during an aborted transaction
+     are undone, leaving data structures ready for a retry, and
+   - memory allocated during an aborted transaction is freed, so we
+     don't chew up memory as we retry.  */
+
+
+struct trail_t
+{
+  /* A Berkeley DB transaction.  */
+  DB_TXN *db_txn;
+
+  /* A pool to allocate things in as part of that transaction --- a
+     subpool of the one passed to `begin_trail'.  We destroy this pool
+     if we abort the transaction, and leave it around otherwise.  */
+  apr_pool_t *pool;
+
+  /* A record of the side-effects to be undone if we abort db_txn.  */
+  struct undo *undo;
+};
+typedef struct trail_t trail_t;
+
+
+/* Try a Berkeley DB transaction repeatedly until it doesn't deadlock.
+
+   That is:
+   - Begin a new Berkeley DB transaction, DB_TXN, in the filesystem FS.
+   - Allocate a subpool of POOL, TXN_POOL.
+   - Start a new trail, TRAIL based on DB_TXN and TXN_POOL.
+   - Apply TXN_BODY to BATON and TRAIL.  TXN_BODY should try to do
+     some series of DB operations which needs to be atomic, using
+     TRAIL->db_txn as the transaction.  If a DB operation deadlocks, or if
+     any other kind of error happens, TXN_BODY should simply return
+     with an appropriate svn_error_t, E.
+   - If E is a Berkeley DB error indicating that a deadlock occurred,
+     unroll the operations recorded in TRAIL->undo, free TRAIL->pool,
+     abort TRAIL->db_txn, and retry the operation.
+   - Otherwise, return what TXN_BODY returned.
+
+   One benefit of using this function is that it makes it easy to
+   ensure that whatever transactions a filesystem function starts, it
+   either aborts or commits before it returns.  If we don't somehow
+   complete all our transactions, later operations could deadlock.  */
+svn_error_t *svn_fs__retry_txn (svn_fs_t *fs,
+				svn_error_t *(*txn_body) (void *baton,
+							  trail_t *trail),
+				void *baton,
+				apr_pool_t *pool);
+
+
+/* Record than a change to an in-memory structure took place as part
+   of TRAIL which can be undone by applying FUNC to BATON.  */
+void svn_fs__record_undo (trail_t *trail,
+			  void (*func) (void *baton),
+			  void *baton);
+
+
+#endif /* SVN_LIBSVN_FS_TRAIL_H */
