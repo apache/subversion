140b70ec7af2e453b9653d8b3181bad2ff2ca893
jimb <dev-null@apache.org>
2000-12-12 02:06:40 +0000

    * libsvn_fs/nodes-table.c (svn_fs__new_node_id, last_key_before, svn_fs__new_successor_id): New functions.  (Actually, these were just moved from the now-dead node.c.) * libsvn_fs/nodes-table.h (svn_fs__new_successor_id): Make ID const.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@838197 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs/nodes-table.c
+++ subversion/libsvn_fs/nodes-table.c
@@ -346,3 +346,233 @@ svn_fs__put_rep (svn_fs_t *fs,
 
   return 0;
 }
+
+
+
+/* Choosing node revision ID's.  */
+
+
+svn_error_t *
+svn_fs__new_node_id (svn_fs_id_t **id_p,
+		     svn_fs_t *fs,
+		     DB_TXN *db_txn,
+		     apr_pool_t *pool)
+{
+  int db_err;
+  DBC *cursor = 0;
+  DBT key, value;
+  svn_fs_id_t *id;
+
+  /* Create a database cursor.  */
+  SVN_ERR (DB_WRAP (fs, "choosing new node ID (creating cursor)",
+		    fs->nodes->cursor (fs->nodes, db_txn, &cursor, 0)));
+
+  /* Find the last entry in the `nodes' table, and increment its node
+     number.  */
+  db_err = cursor->c_get (cursor,
+			  svn_fs__result_dbt (&key),
+			  svn_fs__nodata_dbt (&value),
+			  DB_LAST);
+  if (db_err)
+    {
+      /* Free the cursor.  Ignore any error value --- the error above
+	 is more interesting.  */
+      cursor->c_close (cursor);
+
+      if (db_err == DB_NOTFOUND)
+	/* The root directory should always be present, at least.  */
+	return
+	  svn_error_createf
+	  (SVN_ERR_FS_CORRUPT, 0, 0, fs->pool,
+	   "root directory missing from `nodes' table, in filesystem `%s'",
+	   fs->env_path);
+      
+      SVN_ERR (DB_WRAP (fs, "choosing new node ID (finding last entry)",
+			db_err));
+    }
+  svn_fs__track_dbt (&key, pool);
+
+  /* Try to parse the key as a node revision ID.  */
+  id = svn_fs_parse_id (key.data, key.size, pool);
+  if (! id
+      || svn_fs_id_length (id) < 2)
+    {
+      cursor->c_close (cursor);
+      return svn_fs__err_corrupt_nodes_key (fs);
+    }
+
+  /* We've got the value; close the cursor.  */
+  SVN_ERR (DB_WRAP (fs, "choosing new node ID (closing cursor)",
+		    cursor->c_close (cursor)));
+
+  /* Given the ID of the last node revision, what's the ID of the
+     first revision of an entirely new node?  */
+  id[0]++;
+  id[1] = 1;
+  id[2] = -1;
+
+  *id_p = id;
+  return 0;
+}
+
+
+/* Find the last entry before KEY in the btree table DB.
+
+   KEY must be initialized as for any normal Berkeley DB operation.
+   The settings of KEY->flags and KEY's other members control how the
+   value is returned.
+
+   If DB_TXN is non-zero, perform the operation as part of that
+   Berkeley DB transaction.  */
+static int
+last_key_before (DB *db,
+		 DB_TXN *db_txn,
+		 DBT *key)
+{
+  int db_err;
+  DBC *cursor;
+  DBT temp_key, value;
+
+  /* Create a cursor into the table.  */
+  DB_ERR (db->cursor (db, db_txn, &cursor, 0));
+
+  /* Position CURSOR to the first table entry at or after KEY.
+     Don't bother retrieving the key or value we find there.  */
+  svn_fs__nodata_dbt (&temp_key);
+  temp_key.data = key->data;
+  temp_key.size = key->size;
+  svn_fs__nodata_dbt (&value);
+  db_err = cursor->c_get (cursor, &temp_key, &value, DB_SET_RANGE);
+  if (db_err && db_err != DB_NOTFOUND)
+    {
+      cursor->c_close (cursor);
+      return db_err;
+    }
+
+  /* If db_err == 0, we found the first table entry at or after KEY;
+     the record we want comes immediately before that.
+
+     If db_err == DB_NOTFOUND, then we couldn't find any entry at or
+     after KEY, so the record we want must be the last record in the
+     table.  */
+  db_err = cursor->c_get (cursor, key, svn_fs__nodata_dbt (&value),
+			  db_err == DB_NOTFOUND ? DB_LAST : DB_PREV);
+  if (db_err)
+    {
+      cursor->c_close (cursor);
+      return db_err;
+    }
+
+  /* We're finished with the cursor now.  */
+  DB_ERR (cursor->c_close (cursor));
+
+  return 0;
+}
+
+
+svn_error_t *
+svn_fs__new_successor_id (svn_fs_id_t **successor_p,
+			  svn_fs_t *fs,
+			  const svn_fs_id_t *id,
+			  DB_TXN *db_txn, 
+			  apr_pool_t *pool)
+{
+  int id_len = svn_fs_id_length (id);
+  svn_fs_id_t *new_id;
+  DBT key, value;
+  int db_err;
+
+  /* Make sure ID is really a node revision ID.  */
+  if (id_len & 1)
+    return svn_fs__err_corrupt_id (fs, id);
+
+  /* Set NEW_ID to the next node revision after ID.  Allocate some
+     extra room, in case we need to construct a branch ID below.  */
+  new_id = apr_palloc (pool, sizeof (*new_id) * (id_len + 3) * sizeof (*id));
+  memcpy (new_id, id, (id_len + 1) * sizeof (*id)); /* copy the -1 */
+  new_id[id_len - 1]++;		/* increment the revision number */
+
+  /* Check to see if there already exists a node whose ID is NEW_ID.  */
+  db_err = fs->nodes->get (fs->nodes, db_txn,
+			   svn_fs__id_to_dbt (&key, new_id, pool),
+			   svn_fs__nodata_dbt (&value),
+			   0);
+  if (db_err == DB_NOTFOUND)
+    {
+      /* NEW_ID isn't currently in use, so return that.  */
+      *successor_p = new_id;
+      return 0;
+    }
+  else
+    SVN_ERR (DB_WRAP (fs, "checking for next node revision", db_err));
+
+  /* Okay, the next revision of ID already exists, so we'll need to make
+     a new branch.  What's the next available branch number?
+
+     The sort order for the nodes table says that all revisions of a
+     node come together, followed by all branches from any revision of
+     that node; the branches are sorted by the revision they branch
+     from, and then by branch number.
+
+     So, if our node revision ID is N.V, then all its branches will
+     come immediately before the first branch from N.(V+1).  So we
+     find the last node in the table before node ID N.(V+1).1.1; that
+     node is (perhaps a branch from) the last branch from N.V.
+
+     NEW_ID is currently N.(V+1); stick on the ".1.1".  */
+  new_id[id_len + 0] = 1;
+  new_id[id_len + 1] = 1;
+  new_id[id_len + 2] = -1;
+  SVN_ERR (DB_WRAP (fs, "checking for next node branch",
+		    last_key_before (fs->nodes, db_txn,
+				     svn_fs__result_dbt (&key))));
+  svn_fs__track_dbt (&key, pool);
+
+  {
+    svn_fs_id_t *last_branch_id = svn_fs_parse_id (key.data, key.size, pool);
+    int last_branch_len;
+
+    if (! last_branch_id)
+      return svn_fs__err_corrupt_nodes_key (fs);
+
+    last_branch_len = svn_fs_id_length (last_branch_id);
+
+    /* Only node revision ID's may appear as keys in the `nodes' table.  */
+    if (last_branch_len & 1)
+      return svn_fs__err_corrupt_nodes_key (fs);
+
+    /* If the last key before NEW_ID is just another revision of node
+       N (specifically, the last revision), then there are no branches
+       yet.  */
+    if (last_branch_len == id_len)
+      {
+	/* The first branch from N.V is N.V.1.1.  */
+	memcpy (new_id, id, id_len * sizeof (*id));
+	new_id[id_len + 0] = 1;
+	new_id[id_len + 1] = 1;
+	new_id[id_len + 2] = -1;
+
+	*successor_p = new_id;
+	return 0;
+      }
+
+    /* If the last key before NEW_ID is a branch off of ID, then
+       choose the next branch number.  */
+    else if (last_branch_len > id_len)
+      {
+	/* The last key has the form N.V.B... so the first revision
+	   on our new branch is N.V.(B+1).1.  */
+	memcpy (new_id, last_branch_id, (id_len + 1) * sizeof (*id));
+	new_id[id_len + 0]++;
+	new_id[id_len + 1] = 1;
+	new_id[id_len + 2] = -1;
+
+	*successor_p = new_id;
+	return 0;
+      }
+
+    /* Otherwise, something strange is going on.  */
+    else
+      return svn_fs__err_corrupt_nodes_key (fs);
+  }
+}
--- subversion/libsvn_fs/nodes-table.h
+++ subversion/libsvn_fs/nodes-table.h
@@ -73,7 +73,7 @@ svn_error_t *svn_fs__new_node_id (svn_fs_id_t **id_p,
    the successor is a new branch from ID.  */
 svn_error_t *svn_fs__new_successor_id (svn_fs_id_t **successor_p,
 				       svn_fs_t *fs,
-				       svn_fs_id_t *id,
+				       const svn_fs_id_t *id,
 				       DB_TXN *db_txn, 
 				       apr_pool_t *pool);
 
