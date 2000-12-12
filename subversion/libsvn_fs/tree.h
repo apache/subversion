1ea6db6088324dccf8b5b819ed66de8d204b591a
jimb <dev-null@apache.org>
2000-12-12 20:24:07 +0000

    This should include the use of locked facilities, fences, barbed wire, and locked file cabinets.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@838210 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subversion/libsvn_fs/tree.h
@@ -0,0 +1,26 @@
+/* tree.h : internal interface to tree node functions
+ *
+ * ====================================================================
+ * Copyright (c) 2000 CollabNet.  All rights reserved.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at http://subversion.tigris.org/license-1.html.
+ * If newer versions of this license are posted there, you may use a
+ * newer version instead, at your option.
+ * ====================================================================
+ */
+
+#ifndef SVN_LIBSVN_FS_TREE_H
+#define SVN_LIBSVN_FS_TREE_H
+
+
+/* Set *ROOT_P to a node referring to the root of TXN in FS, as part
+   of TRAIL.  */
+svn_error_t *svn_fs__txn_root_node (svn_fs_node_t **root_p,
+				    svn_fs_t *fs,
+				    const char *txn,
+				    trail_t *trail);
+
+
+#endif /* SVN_LIBSVN_FS_TREE_H */
