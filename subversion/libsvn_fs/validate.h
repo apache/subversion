f9f781b21863a4d32526ce1918396b935754f8c0
Branko Cibej <brane@apache.org>
2000-12-16 23:04:43 +0000

    So the boat was left to drift down the stream as it would, till it glided gently in among the waving rushes.  And then the little sleeves were carefully rolled up, and the little arms were plunged in elbow-deep, to get hold of the rushes a good long way down before breaking them off -- and for a while Alice forgot all bout the sheep and the knitting, as she bent over the side of the boat, with just the ends of her tangled hair dipping into the water -- while with bright eager eyes she caught at one bunch after another of the darling scented rushes.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@838229 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subversion/libsvn_fs/validate.h
@@ -0,0 +1,41 @@
+/* validate.h : internal interface to structure validators
+ *
+ * ====================================================================
+ * Copyright (c) 2000 CollabNet.  All rights reserved.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at http://subversion.tigris.org/license-1.html.
+ * If newer versions of this license are posted there, you may use a
+ * newer version instead, at your option.
+ * ====================================================================
+ */
+
+#ifndef SVN_LIBSVN_FS_VALIDATE_H
+#define SVN_LIBSVN_FS_VALIDATE_H
+
+#include "skel.h"
+
+/* Validating node and node revision IDs. */
+
+/* Count the number of components in the ID, and check its syntax.
+   Return 0 if the syntax is incorrect. */
+int svn_fs__count_id_components (const char *data, apr_size_t data_len);
+
+
+
+/* Validating skels. */
+
+/* Validate the structure of a PROPLIST. */
+int svn_fs__is_valid_proplist (skel_t *skel);
+
+
+#endif /* SVN_LIBSVN_FS_VALIDATE_H */
+
+
+
+/* 
+ * local variables:
+ * eval: (load-file "../svn-dev.el")
+ * end:
+ */
