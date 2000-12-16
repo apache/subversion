e417c69d6987239a40d17471bd9053dbcc2356a0
Branko Čibej <brane@apache.org>
2000-12-16 23:05:22 +0000

    "I only hope the boat won't tipple over!" she said to herself.  "Oh, /what/ a lovely one! Only I couldn't quite reach it."  And it certainly /did/ seem a little provoking ("almost as if it happened on purpose," she thought) that, though she managed to pick plenty of beautiful rushes as the boat glided by, there was always a more lovely one that she couldn't reach.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@838230 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subversion/libsvn_fs/validate.c
@@ -0,0 +1,78 @@
+/* validate.c : internal structure validators
+ *
+ * ====================================================================
+ * Copyright (c) 2000 CollabNet.  All rights reserved.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at http://subversion.tigris.org/license-1.html.
+ * If newer versions of this license are posted there, you may use a
+ * newer version instead, at your option.
+ * ====================================================================
+ */
+
+#include "validate.h"
+
+
+
+/* Validating node and node revision IDs. */
+
+int
+svn_fs__count_id_components (const char *data, apr_size_t data_len)
+{
+  int i;
+  int id_len = 1;
+  int last_start = 0;
+
+  for (i = 0; i < data_len; i++)
+    if (data[i] == '.')
+      {
+        /* There must be at least one digit before and after each dot.  */
+        if (i == last_start)
+          return 0;
+        last_start = i + 1;
+        id_len++;
+      }
+    else if ('0' <= data[i] && data[i] <= '9')
+      ;
+    else
+      return 0;
+
+  /* Make sure there was at least one digit in the last number.  */
+  if (i == last_start)
+    return 0;
+
+  return id_len;
+}
+
+
+
+/* Validating SKELs. */
+
+int
+svn_fs__is_valid_proplist (skel_t *skel)
+{
+  int len = svn_fs__list_length (skel);
+
+  if (len >= 0
+      && (len & 1) == 0)
+    {
+      skel_t *elt;
+
+      for (elt = skel->children; elt; elt = elt->next)
+	if (! elt->is_atom)
+	  return 0;
+
+      return 1;
+    }
+
+  return 0;
+}
+
+
+
+/* 
+ * local variables:
+ * eval: (load-file "../svn-dev.el")
+ * end:
+ */
