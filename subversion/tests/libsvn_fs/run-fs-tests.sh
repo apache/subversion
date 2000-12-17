db3ceedd152bb81a0eee671cdf18ecedf6786155
Branko Čibej <brane@apache.org>
2000-12-17 00:13:50 +0000

    Added fs-test to the list of tests. Remove database files created by fs-test before running the tests.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@838239 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/libsvn_fs/run-fs-tests.sh
+++ subversion/tests/libsvn_fs/run-fs-tests.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 #### run-fs-tests.sh --- run filesystem test programs
 
+## Remove database files cretaed by the tests.
+if [ -d test-repo ]; then
+  rm -fr test-repo;
+fi
+
 > ./tests.log
-for test_pgm in skel-test; do
+for test_pgm in skel-test fs-test; do
   if ./${test_pgm} >> ./tests.log; then :; else
     echo "Test program \`${test_pgm}' exited with status $?" >&2
     exit 1
