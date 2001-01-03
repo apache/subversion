fed9f148f44f2bc4b5705fd9e1a8a45ca1986dbb
Ben Collins-Sussman <sussman@apache.org>
2001-01-03 20:50:37 +0000

    New commit-tracing editor.  To be composed with our normal commit editor.
    * trace-commit.c:  new editor for tracing commits, a variant of
      trace-update.c
    
    * cl.h (svn_cl__get_trace_commit_editor):  new declaration
    
    * Makefile.am:  compile trace-update.c
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@838312 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/client/cl.h
+++ subversion/client/cl.h
@@ -141,6 +141,14 @@ svn_cl__get_trace_update_editor (const svn_delta_edit_fns_t **editor,
                                  svn_string_t *initial_path,
                                  apr_pool_t *pool);
 
+/* Returns an editor that prints out events in a commit. */
+svn_error_t *
+svn_cl__get_trace_commit_editor (const svn_delta_edit_fns_t **editor,
+                                 void **edit_baton,
+                                 svn_string_t *initial_path,
+                                 apr_pool_t *pool);
+
+
 #endif /* SVN_CL_H */
 
 
