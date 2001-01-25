d5fc97bb72cdf16a822441fa0d17a89da18ff1fe
Greg Stein <gstein@apache.org>
2001-01-25 13:44:38 +0000

    "There can be only one."
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@838398 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subversion/svnadmin/main.c
@@ -0,0 +1,19 @@
+/*
+ * main.c: Subversion server administration tool.
+ *
+ * ====================================================================
+ * Copyright (c) 2000 CollabNet.  All rights reserved.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at http://subversion.tigris.org/license-1.html.
+ * If newer versions of this license are posted there, you may use a
+ * newer version instead, at your option.
+ * ====================================================================
+ */
+
+int
+main (int argc, const char * const *argv)
+{
+  return 0;
+}
