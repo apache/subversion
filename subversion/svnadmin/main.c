566bf514e662c0805818d408a25c28cfe63bacad
Greg Stein <gstein@apache.org>
2001-02-05 04:27:10 +0000

    quick hack to create an SVN environment
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@838469 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svnadmin/main.c
+++ subversion/svnadmin/main.c
@@ -12,8 +12,53 @@
  * ====================================================================
  */
 
+#include <apr_general.h>
+#include <apr_pools.h>
+
+#define APR_WANT_STDIO
+#define APR_WANT_STRFUNC
+#include <apr_want.h>
+
+#include "svn_types.h"
+#include "svn_error.h"
+#include "svn_fs.h"
+
 int
 main (int argc, const char * const *argv)
 {
-  return 0;
+  apr_pool_t *pool;
+  svn_fs_t *fs;
+  svn_error_t *err;
+
+  /* ### this whole thing needs to be cleaned up once client/main.c
+     ### is refactored. for now, let's just get the tool up and
+     ### running. */
+  if (argc != 2 || strcmp(argv[1], "create") != 0)
+    {
+      fprintf(stderr, "USAGE: %s create\n", argv[0]);
+      exit(1);
+    }
+
+  apr_initialize ();
+  pool = svn_pool_create (NULL);
+
+  fs = svn_fs_new(pool);
+  err = svn_fs_create_berkeley(fs, ".");
+  if (err != NULL)
+    {
+      svn_handle_error(err, stderr, FALSE);
+      return EXIT_FAILURE;
+    }
+
+  err = svn_fs_close_fs(fs);
+  if (err != NULL)
+    {
+      svn_handle_error(err, stderr, FALSE);
+      return EXIT_FAILURE;
+    }
+
+  apr_destroy_pool (pool);
+  apr_terminate();
+
+  return EXIT_SUCCESS;
 }
