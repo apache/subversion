353a15d8c2783d3df9d62b68d76b4bca8aa861c9
Greg Stein <gstein@apache.org>
2001-02-05 13:20:43 +0000

    add a placeholder for some activity manipulation functions
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@838479 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subversion/mod_dav_svn/activity.c
@@ -0,0 +1,27 @@
+/*
+ * activity.c: DeltaV activity handling
+ *
+ * ====================================================================
+ * Copyright (c) 2000 CollabNet.  All rights reserved.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at http://subversion.tigris.org/license-1.html.
+ * If newer versions of this license are posted there, you may use a
+ * newer version instead, at your option.
+ * ====================================================================
+ */
+
+
+
+#include <httpd.h>
+#include <mod_dav.h>
+
+#include "dav_svn.h"
+
+
+/* 
+ * local variables:
+ * eval: (load-file "../svn-dev.el")
+ * end:
+ */
