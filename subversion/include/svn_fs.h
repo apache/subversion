716bdd6bb1ae597e29b4a7218c4a8e9674367dad
Karl Fogel <kfogel@apache.org>
2001-02-06 23:26:30 +0000

    Start tree editor interface for committing changes to the repository:
    (svn_fs_commit_hook_t): New prototype
    (svn_fs_get_editor): New function.
    
    * libsvn_fs/editor.c: New file.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@838489 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_fs.h
+++ subversion/include/svn_fs.h
@@ -864,6 +864,45 @@ svn_error_t *svn_fs_file_delta (svn_txdelta_stream_t **stream_p,
 
 
 
+/*** Making changes to a filesystem, editor-style. ***/
+
+/* Hook function type for commits.  When a filesystem commit happens,
+ * one of these should be invoked on the NEW_REVISION that resulted
+ * from the commit, the LOG_MSG (kff todo: should that be a stream?
+ * Nah, string is prolly fine.), and the BATON that was provided with
+ * the hook originally.
+ *
+ * See svn_fs_get_editor for an example user.
+ */
+typedef svn_error_t *svn_fs_commit_hook_t (svn_revnum_t new_revision,
+                                           svn_string_t *log_msg,
+                                           void *baton);
+
+
+/* Return an EDITOR and EDIT_BATON to commit changes to BASE_REVISION
+ * of FS.  The directory baton returned by (*EDITOR)->begin_edit is
+ * for the root of the tree; all edits must start at the top and
+ * descend. 
+ *
+ * Calling (*EDITOR)->close_edit completes the commit.  Before
+ * close_edit returns, but after the commit has succeeded, it will
+ * invoke HOOK with the new revision number, log message, and
+ * HOOK_BATON as arguments.  If HOOK returns an error, that error will
+ * be returned from close_edit, else close_edit will return
+ * successfully (unless it encountered an error before invoking
+ * HOOK).
+ */
+svn_error_t *svn_fs_get_editor (svn_delta_edit_fns_t **editor,
+                                void **edit_baton,
+                                svn_fs_t *fs,
+                                svn_revnum_t base_revision,
+                                svn_fs_commit_hook_t hook,
+                                void *hook_baton,
+                                apr_pool_t *pool);
+
+
+
+
 /* Non-historical properties.  */
 
 /* [[Yes, do tell.]] */
