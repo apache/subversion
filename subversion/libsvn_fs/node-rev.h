105af888cc7dcc16cf3e9631c1aa3a78327c931d
jimb <dev-null@apache.org>
2001-02-09 22:13:32 +0000

    * libsvn_fs/node-rev.h (svn_fs__get_node_revision, svn_fs__put_node_revision, svn_fs__create_node, svn_fs__create_successor, svn_fs__stable_node): Accept a trail, instead of a Berkeley DB transaction and a pool.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@838530 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs/node-rev.h
+++ subversion/libsvn_fs/node-rev.h
@@ -17,71 +17,66 @@
 #include "db.h"
 #include "svn_fs.h"
 #include "skel.h"
+#include "trail.h"
 
 
 /* Set *SKEL_P to the NODE-REVISION skel for the node ID in FS, as
-   part of the Berkeley DB transaction DB_TXN.  Allocate the skel, and
-   do any other temporary allocation in POOL.  */
+   part of TRAIL.  Allocate the skel, and do any other temporary
+   allocation in TRAIL->pool.  */
 svn_error_t *svn_fs__get_node_revision (skel_t **skel_p,
 					svn_fs_t *fs,
                                         const svn_fs_id_t *id,
-                                        DB_TXN *db_txn,
-                                        apr_pool_t *pool);
+					trail_t *trail);
 
 
 /* Store SKEL as the NODE-REVISION skel for the node revision whose id
-   is ID in FS, as part of the Berkeley DB transaction DB_TXN.  Do any
-   necessary temporary allocation in POOL.
+   is ID in FS, as part of TRAIL.  Do any necessary temporary
+   allocation in TRAIL->pool.
 
    After this call, the node table manager assumes that NODE's
    contents will change frequently.  */
 svn_error_t *svn_fs__put_node_revision (svn_fs_t *fs,
 					const svn_fs_id_t *id,
                                         skel_t *skel,
-                                        DB_TXN *db_txn,
-					apr_pool_t *pool);
+					trail_t *trail);
 
 
 /* Create an entirely new, mutable node in the filesystem FS, whose
-   NODE-REVISION skel is SKEL, as part of the Berkeley DB transaction
-   DB_TXN.  Set *ID_P to the new node revision's ID.  Use POOL for any
-   temporary allocation.
+   NODE-REVISION skel is SKEL, as part of TRAIL.  Set *ID_P to the new
+   node revision's ID.  Use TRAIL->pool for any temporary allocation.
 
    After this call, the node table manager assumes that the new node's
    contents will change frequently.  */
 svn_error_t *svn_fs__create_node (svn_fs_id_t **id_p,
 				  svn_fs_t *fs,
 				  skel_t *skel,
-				  DB_TXN *db_txn,
-				  apr_pool_t *pool);
+				  trail_t *trail);
 
 
 /* Create a mutable node in FS which is an immediate successor of
-   OLD_ID, whose contents are NEW_SKEL, as part of the Berkeley DB
-   transaction DB_TXN.  Set *NEW_ID_P to the new node revision's ID.
-   Use POOL for any temporary allocation.
+   OLD_ID, whose contents are NEW_SKEL, as part of TRAIL.  Set
+   *NEW_ID_P to the new node revision's ID.  Use TRAIL->pool for any
+   temporary allocation.
 
    After this call, the deltification code assumes that the new node's
    contents will change frequently.  */
 svn_error_t *svn_fs__create_successor (svn_fs_id_t **new_id_p,
 				       svn_fs_t *fs,
-				       DB_TXN *db_txn,
 				       svn_fs_id_t *old_id,
 				       skel_t *new_skel,
-				       apr_pool_t *pool);
+				       trail_t *trail);
 
 
 /* Indicate that the contents of the node ID in FS are expected to be
-   stable now, as part of the Berkeley DB transaction DB_TXN.  This
-   suggests to the deltification code that it could be effective to
-   represent other nodes' contents as deltas against this node's
-   contents.  This does not change the contents of the node.
+   stable now, as part of TRAIL.  This suggests to the deltification
+   code that it could be effective to represent other nodes' contents
+   as deltas against this node's contents.  This does not change the
+   contents of the node.
 
-   Do any necessary temporary allocation in POOL.  */
+   Do any necessary temporary allocation in TRAIL->pool.  */
 svn_error_t *svn_fs__stable_node (svn_fs_t *fs,
 				  svn_fs_id_t *id,
-				  DB_TXN *db_txn,
-				  apr_pool_t *pool);
+				  trail_t *trail);
 
 
 #endif /* SVN_LIBSVN_FS_NODE_REV_H */
