4542905c41d6435885f2172c9f179c199efdf25f
Branko Čibej <brane@apache.org>
2001-02-10 02:20:59 +0000

    Make this compile.
    * ra-commit.c: Added missing BASE-REVISION parameter in call to
    get_commit_editor.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@838535 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/libsvn_ra_dav/ra-commit.c
+++ subversion/tests/libsvn_ra_dav/ra-commit.c
@@ -66,7 +66,7 @@ main (int argc, char **argv)
      ### and tossing svn_wc_close_commit */
 
   err = (*plugin->get_commit_editor)(session_baton, &editor, &edit_baton,
-                                     NULL, NULL, NULL, NULL);
+                                     -1, NULL, NULL, NULL, NULL);
   if (err)
     goto error;
 
