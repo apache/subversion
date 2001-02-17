87bae3099d34049f3b351768c8284b1935ee3d7c
Greg Stein <gstein@apache.org>
2001-02-17 11:05:38 +0000

    clean out some cruft
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@838622 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/client.h
+++ subversion/libsvn_client/client.h
@@ -18,48 +18,10 @@
 
 
 #include <apr_pools.h>
-#include <apr_dso.h>
+
 #include "svn_types.h"
 #include "svn_string.h"
 #include "svn_error.h"
-#include "svn_path.h"
-#include "svn_xml.h"
-#include "svn_wc.h"
-#include "svn_ra.h"
-
-
-/* ---------------------------------------------------------------- */
-
-/*** RA library stuff ***/
-
-/* "What's RA stuff doing in the private header of libsvn_client?"
-
-   Remember that there's no such thing as "libsvn_ra"; just specific
-   implementations like libsvn_ra_dav, libsvn_ra_local, etc.  The
-   public header `svn_ra.h' defines the vtable that any RA library
-   needs to implement.
-
-   Meanwhile, libsvn_client has internal methods of loading and
-   storing these vtables.  That's what this section is about.  */
-
-
-
-/* Structure representing a loaded RA library. */
-typedef struct svn_client__ra_library_t
-{
-  const svn_ra_plugin_t *plugin;  /* the library's "vtable" */
-  apr_dso_handle_t *dso;          /* the whole library */
-
-} svn_client__ra_library_t;
-
-
-
-/* Return a loaded RA library which can handle URL, alloc'd from
-   POOL. */
-svn_error_t *
-svn_client__get_ra_library (const svn_client__ra_library_t **library,
-                            const char *URL,
-                            apr_pool_t *pool);
 
 
 /* ---------------------------------------------------------------- */
