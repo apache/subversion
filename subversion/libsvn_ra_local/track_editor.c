c1cedf686e610320d28a601bd884387da1e8bae5
Karl Fogel <kfogel@apache.org>
2001-02-17 00:46:09 +0000

    (svn_ra_local__get_commit_track_editor): Comment fix.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@838615 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_ra_local/track_editor.c
+++ subversion/libsvn_ra_local/track_editor.c
@@ -201,7 +201,7 @@ svn_ra_local__get_commit_track_editor (svn_delta_edit_fns_t **editor,
         change_file_prop
         change_dir_prop
      
-     I'm pretty sure delete_entry is a no-op too, so it is
+     I'm pretty sure delete_entry is a no-op too, so it is also
      unimplemented.  It's true the delete is part of the commit, but
      that entity's record is expunged afterwards, so there's no point
      bumping its revision number.  So, delete_entry is left in the
