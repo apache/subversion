983a6a70afca9e0b1573b4111afb6cfb1dc4fa5b
C. Michael Pilato <cmpilato@apache.org>
2001-02-28 17:34:05 +0000

    * tests/libsvn_fs/fs-test.c
      (common_test_prologue): New helper function for opening filesystem
      and init'ing an FS object.
    
      (open_berkeley_filesystem, trivial_transaction, reopen_trivial_transaction,
      create_file_transaction, list_live_transactions): Changed to use new helper
      common_test_prologue.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@838757 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/libsvn_fs/fs-test.c
+++ subversion/tests/libsvn_fs/fs-test.c
@@ -90,23 +90,41 @@ check_filesystem_root_id (void *fs_baton, trail_t *trail)
 }
 
 
+
+/* The Prologue: stuff to do at the beginning of most txn-based tests.
+   See in-line comments at each step to find out what this does. */
+static int
+common_test_prologue (svn_fs_t **fs)
+{
+  const char *ignored;
+
+  /* Make sure the filesystem exists. */
+  if (create_berkeley_filesystem (&ignored) != 0)
+    return fail();
+
+  /* Init a new FS structure */
+  *fs = svn_fs_new (pool);
+  if (*fs == NULL)
+    return fail();
+
+  /* Open our db tables, and hook them up to our FS structure */
+  if (SVN_NO_ERROR != svn_fs_open_berkeley (*fs, repository))
+    return fail();
+
+  return 0;
+}
+
+
+
 static int
 open_berkeley_filesystem (const char **msg)
 {
   svn_fs_t *fs;
-  const char *ignored;
 
   *msg = "open Berkeley DB filesystem";
 
-  /* Make sure the FS exists. */
-  if (create_berkeley_filesystem (&ignored) != 0)
-    return fail();
-
-  fs = svn_fs_new (pool);
-  if (fs == NULL)
-    return fail();
-
-  if (SVN_NO_ERROR != svn_fs_open_berkeley (fs, repository))
+  /* Do our common `startup stuff' */
+  if (common_test_prologue (&fs))
     return fail();
 
   if (SVN_NO_ERROR != svn_fs__retry_txn (fs, check_filesystem_root_id,
@@ -130,6 +148,8 @@ open_berkeley_filesystem (const char **msg)
 }
 
 
+
+
 /* Safe to call this multiple times -- only creates first txn once. */
 static int
 trivial_transaction (const char **msg)
@@ -137,25 +157,16 @@ trivial_transaction (const char **msg)
   svn_fs_t *fs;
   svn_fs_txn_t *txn;
   svn_error_t *err;
-  const char *ignored;
   static int made_first_txn_already = 0;
 
   *msg = "begin a txn, check its name, then immediately close it";
 
   if (! made_first_txn_already)
     {
-      /* Make sure the FS exists. */
-      if (create_berkeley_filesystem (&ignored) != 0)
+      /* Do our common `startup stuff' */
+      if (common_test_prologue (&fs))
         return fail();
-      
-      /* Open the FS. */
-      fs = svn_fs_new (pool);
-      if (fs == NULL)
-        return fail();
-      
-      if (SVN_NO_ERROR != svn_fs_open_berkeley (fs, repository))
-        return fail();
-      
+
       /* Begin a transaction. */
       if (SVN_NO_ERROR != svn_fs_begin_txn (&txn, fs, 0, pool))
         return fail();
@@ -196,16 +207,8 @@ reopen_trivial_transaction (const char **msg)
 
   *msg = "reopen and check the transaction name";
 
-  /* Make sure the FS exists. */
-  if (create_berkeley_filesystem (&ignored) != 0)
-    return fail();
-
-  /* Open the FS. */
-  fs = svn_fs_new (pool);
-  if (fs == NULL)
-    return fail();
-
-  if (SVN_NO_ERROR != svn_fs_open_berkeley (fs, repository))
+  /* Do our common `startup stuff' */
+  if (common_test_prologue (&fs))
     return fail();
 
   /* Make sure the transaction exists. */
@@ -234,23 +237,14 @@ create_file_transaction (const char **msg)
   svn_fs_t *fs;
   svn_fs_txn_t *txn;
   svn_error_t *err;
-  const char *ignored;
   static int made_txn_already = 0;
 
   *msg = "begin a txn, get the txn root, and add a file!";
 
   if (! made_txn_already)
     {
-      /* Make sure the FS exists. */
-      if (create_berkeley_filesystem (&ignored) != 0)
-        return fail();
-      
-      /* Open the FS. */
-      fs = svn_fs_new (pool);
-      if (fs == NULL)
-        return fail();
-      
-      if (SVN_NO_ERROR != svn_fs_open_berkeley (fs, repository))
+      /* Do our common `startup stuff' */
+      if (common_test_prologue (&fs))
         return fail();
       
       /* Begin a transaction. */
@@ -307,16 +301,8 @@ list_live_transactions (const char **msg)
 
   *msg = "list active transactions";
 
-  /* Make sure the FS exists. */
-  if (create_berkeley_filesystem (&ignored) != 0)
-    return fail();
-
-  /* Open the FS. */
-  fs = svn_fs_new (pool);
-  if (fs == NULL)
-    return fail();
-
-  if (SVN_NO_ERROR != svn_fs_open_berkeley (fs, repository))
+  /* Do our common `startup stuff' */
+  if (common_test_prologue (&fs))
     return fail();
 
   /* Make sure the transaction exists. */
