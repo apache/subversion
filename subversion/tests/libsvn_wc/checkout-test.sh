88a08e0e5bc3a6689a1b038a0fa1eb33202b58eb
C. Michael Pilato <cmpilato@apache.org>
2001-03-02 21:47:30 +0000

    Patches submitted by Yoshiki Hayashi
    * tests/libsvn_fs/run-fs-tests.sh
    
      Change output format to match other make check target.  Always
      return 0 since other targets do so.
    
    * tests/libsvn_wc/Makefile.am
    
      (check): Home made check target to avoid using automake's one.
    
    * tests/libsvn_wc/checkout-test.sh
    
      Output what has happened.  cmpilato tweaked this patch to print
      'SUCCESS' instead of 'DONE' for consistency with other tests.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@838802 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/libsvn_wc/checkout-test.sh
+++ subversion/tests/libsvn_wc/checkout-test.sh
@@ -21,6 +21,9 @@ done
 #    ./checkout-test checkout-${num}.delta ${num}/this/is/a/test/dir
 # done
 
+echo
+echo -n "Running all sub-tests in checkout-test"...
 ./checkout-test ${TEST_DELTA} this
+echo "SUCCESS"
 
 # That's it.  Right now, we test by inspecting them by hand.
