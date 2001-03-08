ffe0318b59992f48c9b480b8e3e84305fe5f9d1e
Ben Collins-Sussman <sussman@apache.org>
2001-03-08 18:00:17 +0000

    * svn_ra.h (get_commit_editor): take base_path argument, for the same   reason that svn_fs_get_editor now does.  Namely, the client already   knows the "grandaddy" dir where it will call replace root.  It's   silly to force the editor-driver to always begin changes at the very   top of a revision tree.
    * ra_local.c (open, close, get_latest_revnum, get_commit_editor): use
      SVN_ERR macro for readability.  Uncomment some things.
    
      (get_commit_editor): take base_path argument.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@838864 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_ra_local/ra_plugin.c
+++ subversion/libsvn_ra_local/ra_plugin.c
@@ -86,7 +86,6 @@ open (void **session_baton,
       svn_string_t *repository_URL,
       apr_pool_t *pool)
 {
-  svn_error_t *err;
   svn_ra_local__session_baton_t *baton;
 
   /* When we close the session_baton later, we don't necessarily want
@@ -94,10 +93,10 @@ open (void **session_baton,
      there. */
   apr_pool_t *subpool = svn_pool_create (pool);
 
-  /* Allocate the session_baton itself in this subpool */ 
+  /* Allocate the session_baton the parent pool */ 
   baton = apr_pcalloc (pool, sizeof(*baton));
 
-  /* And let all other session_baton data use the same subpool */
+  /* And let all other session_baton data use session's subpool */
   baton->pool = subpool;
   baton->repository_URL = repository_URL;
   baton->fs = svn_fs_new (subpool);
@@ -105,11 +104,10 @@ open (void **session_baton,
   /* Look through the URL, figure out which part points to the
      repository, and which part is the path *within* the
      repository. */
-  err = svn_ra_local__split_URL (&(baton->repos_path),
-                                 &(baton->fs_path),
-                                 baton->repository_URL,
-                                 subpool);
-  if (err) return err;
+  SVN_ERR (svn_ra_local__split_URL (&(baton->repos_path),
+                                    &(baton->fs_path),
+                                    baton->repository_URL,
+                                    subpool));
 
   /* Temporary... for debugging only.  Obviously, this library should
      never print to stdout! */
@@ -117,10 +115,8 @@ open (void **session_baton,
           baton->repos_path->data,
           baton->fs_path->data);
 
-
   /* Open the filesystem at located at environment `repos_path' */
-  err = svn_fs_open_berkeley (baton->fs, baton->repos_path->data);
-  if (err) return err;
+  SVN_ERR (svn_fs_open_berkeley (baton->fs, baton->repos_path->data));
 
   /* Return the session baton */
   *session_baton = baton;
@@ -132,14 +128,11 @@ open (void **session_baton,
 static svn_error_t *
 close (void *session_baton)
 {
-  svn_error_t *err;
-  
   svn_ra_local__session_baton_t *baton = 
     (svn_ra_local__session_baton_t *) session_baton;
 
   /* Close the repository filesystem */
-  err = svn_fs_close_fs (baton->fs);
-  if (err) return err;
+  SVN_ERR (svn_fs_close_fs (baton->fs));
 
   /* Free all memory allocated during this ra session.  */
   apr_pool_destroy (baton->pool);
@@ -154,13 +147,10 @@ static svn_error_t *
 get_latest_revnum (void *session_baton,
                    svn_revnum_t *latest_revnum)
 {
-  svn_error_t *err;
-
   svn_ra_local__session_baton_t *baton = 
     (svn_ra_local__session_baton_t *) session_baton;
 
-  /*  err = svn_fs_youngest_rev (latest_revnum, baton->fs); */
-  if (err) return err;
+  SVN_ERR (svn_fs_youngest_rev (latest_revnum, baton->fs, baton->pool));
 
   return SVN_NO_ERROR;
 }
@@ -173,12 +163,12 @@ get_commit_editor (void *session_baton,
                    const svn_delta_edit_fns_t **editor,
                    void **edit_baton,
                    svn_revnum_t base_revision,
+                   svn_string_t *base_path,
                    svn_string_t *log_msg,
                    svn_ra_close_commit_func_t close_func,
                    svn_ra_set_wc_prop_func_t set_func,
                    void *close_baton)
 {
-  svn_error_t *err;
   svn_delta_edit_fns_t *commit_editor, *tracking_editor;
   const svn_delta_edit_fns_t *composed_editor;
   void *commit_editor_baton, *tracking_editor_baton, *composed_editor_baton;
@@ -198,42 +188,29 @@ get_commit_editor (void *session_baton,
                                           sizeof(*(closer->target_array)));
 
   /* Get the filesystem commit-editor */     
-  /*  err = svn_fs_get_editor (&commit_editor,
-                           &commit_editor_baton,
-                           sess_baton->fs,
-                           base_revision,
-                           log_msg,
-                           cleanup_commit, 
-                           closer,
-                           sess_baton->pool); */
-  if (err) return err;
+  SVN_ERR (svn_fs_get_editor (&commit_editor, &commit_editor_baton,
+                              sess_baton->fs, 
+                              base_revision, base_path,
+                              log_msg,
+                              cleanup_commit, closer,
+                              sess_baton->pool));
 
   /* Get the commit `tracking' editor, telling it to store committed
-     targets inside our closer */
-  err = svn_ra_local__get_commit_track_editor (&tracking_editor,
-                                               &tracking_editor_baton,
-                                               sess_baton->pool,
-                                               closer);
-  if (err) return err;
+     targets inside our `closer' object. */
+  SVN_ERR (svn_ra_local__get_commit_track_editor (&tracking_editor,
+                                                  &tracking_editor_baton,
+                                                  sess_baton->pool,
+                                                  closer));
 
-  /* TEMPORARY:  shut up compile warnings until we're able to pass
-     non-bogus values to the composition function...
+  /* Set up a pipeline between the editors, creating a wrapper editor. */
+  svn_delta_compose_editors (&composed_editor, &composed_editor_baton,
+                             commit_editor, commit_editor_baton,
+                             tracking_editor, tracking_editor_baton,
+                             sess_baton->pool);
 
-  err = svn_delta_compose_editors (&composed_editor,
-                                   &composed_editor_baton,
-                                   commit_editor,
-                                   commit_editor_baton,
-                                   tracking_editor,
-                                   tracking_editor_baton,
-                                   sess_baton->pool);
-  if (err) return err;
-
-  */
-
-  /* Give the magic composed-editor thingie back to the client */
+  /* Give the magic composed-editor back to the client */
   *editor = composed_editor;
   *edit_baton = composed_editor_baton;
-
   return SVN_NO_ERROR;
 }
 
@@ -281,7 +258,7 @@ static const svn_ra_reporter_t ra_local_reporter =
 static const svn_ra_plugin_t ra_local_plugin = 
 {
   "ra_local",
-  "RA module for accessing file:// URLs",
+  "RA module for accessing repository on local disk. (file:// URLs)",
   open,
   close,
   get_latest_revnum,
@@ -304,7 +281,7 @@ svn_ra_local_init (int abi_version,
   *url_type = "file";
   *plugin = &ra_local_plugin;
 
-  /* are we ever going to care about abi_version? */
+  /* ben sez:  todo:  check that abi_version >=1. */
 
   return SVN_NO_ERROR;
 }
