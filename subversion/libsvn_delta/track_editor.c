45cb66376739fc969cf0fab9f989002570d65e45
Ben Collins-Sussman <sussman@apache.org>
2001-03-12 18:43:43 +0000

    The actual move of track_editor.c
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@838901 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_delta/track_editor.c
+++ subversion/libsvn_delta/track_editor.c
@@ -12,8 +12,34 @@
  * ====================================================================
  */
 
-#include "ra_local.h"
+#include "svn_path.h"
+#include "svn_delta.h"
+#include "svn_ra.h"
+#include "apr_tables.h"
 
+/* Philosophy:  how does the track editor know when to store a path as
+   a "committed target"?
+
+   Here is the logic used by the commit-editor-driver
+   (svn_wc_crawl_local_mods): 
+   
+   Store a path if:
+
+      - an entry is marked for addition
+      - an entry is marked for deletion
+      - a file's text or props are modified
+      - a dir's props are modified.
+
+   Since the track editor will be driven by crawl_local_mods, it needs
+   to line itself up along these semantics.  This means storing a
+   target inside:
+
+      - add_file & add_dir
+      - delete_entry
+      - apply_textdelta or change_file_prop
+      - change_dir_prop
+
+ */
 
 
 
@@ -25,7 +51,10 @@ struct edit_baton
 {
   apr_pool_t *pool;
   svn_string_t *initial_path;
-  svn_ra_local__commit_closer_t *closer;
+
+  /* An already-intitialized array, ready to store (svn_string_t *)
+     objects */
+  apr_array_header_t *array;
 };
 
 
@@ -34,6 +63,9 @@ struct dir_baton
   struct edit_baton *edit_baton;
   struct dir_baton *parent_dir_baton;
   svn_string_t *path;
+  
+  /* Has this path been stored in the array already? */
+  int stored;
 };
 
 
@@ -41,7 +73,25 @@ struct file_baton
 {
   struct dir_baton *parent_dir_baton;
   svn_string_t *path;
+
+  /* Has this path been stored in the array already? */
+  int stored;
 };
+
+
+
+/*** Helpers ***/
+
+/* Store PATH in EB's array. */
+static void
+store_path (svn_string_t *path, struct edit_baton *eb)
+{
+  svn_string_t **receiver;
+  
+  receiver = (svn_string_t **) apr_array_push (eb->array);
+  *receiver = path;
+}
+
 
 
 
@@ -81,6 +131,9 @@ add_directory (svn_string_t *name,
   child_d->path = svn_string_dup (parent_d->path, child_d->edit_baton->pool);
   svn_path_add_component (child_d->path, name, svn_path_local_style);
 
+  store_path (child_d->path, parent_d->edit_baton);
+  child_d->stored = TRUE;
+
   *child_baton = child_d;
 
   return SVN_NO_ERROR;
@@ -107,33 +160,7 @@ replace_directory (svn_string_t *name,
   return SVN_NO_ERROR;
 }
 
-static svn_error_t *
-close_file (void *file_baton)
-{
-  struct file_baton *fb = file_baton;
-  svn_string_t **receiver;
 
-  receiver = (svn_string_t **) apr_array_push
-    (fb->parent_dir_baton->edit_baton->closer->target_array);
-  *receiver = fb->path;
-
-  return SVN_NO_ERROR;
-}
-
-
-static svn_error_t *
-close_edit (void *edit_baton)
-{
-  /* kff todo: Ben, I wasn't sure what to do here.  At first, I
-     thought we'd run over the items in the target_array, invoking the
-     close_func() and set_func() on them.  But now that I think about
-     it, that's the province of the true commit editor, not the
-     tracking editor -- after all, it's the commit editor that knows
-     the new revision number.  Anyway, if this func here ends up not
-     doing anything, we should remove it altogether. */
-
-  return SVN_NO_ERROR;
-}
 
 
 static svn_error_t *
@@ -150,6 +177,9 @@ add_file (svn_string_t *name,
   child_fb->parent_dir_baton = parent_d;
   child_fb->path = svn_string_dup (parent_d->path, parent_d->edit_baton->pool);
   svn_path_add_component (child_fb->path, name, svn_path_local_style);
+
+  store_path (child_fb->path, parent_d->edit_baton);
+  child_fb->stored = TRUE;
 
   *file_baton = child_fb;
 
@@ -177,14 +207,81 @@ replace_file (svn_string_t *name,
 }
 
 
+static svn_error_t *
+delete_entry (svn_string_t *name,
+              void *parent_baton)
+{
+  struct dir_baton *parent_d = parent_baton;
+  svn_string_t *path = svn_string_dup (parent_d->path,
+                                       parent_d->edit_baton->pool);
+  svn_path_add_component (path, name, svn_path_local_style);
+  
+  store_path (path, parent_d->edit_baton);  
+
+  return SVN_NO_ERROR;
+}
+
+
+static svn_error_t *
+change_dir_prop (void *dir_baton,
+                 svn_string_t *name,
+                 svn_string_t *value)
+{
+  struct dir_baton *db = dir_baton;
+
+  if (! db->stored)
+    {
+      store_path (db->path, db->edit_baton);  
+      db->stored = TRUE;
+    }
+  
+  return SVN_NO_ERROR;
+}
+
+
+static svn_error_t *
+change_file_prop (void *file_baton,
+                  svn_string_t *name,
+                  svn_string_t *value)
+{
+  struct file_baton *fb = file_baton;
+
+  if (! fb->stored)
+    {
+      store_path (fb->path, fb->parent_dir_baton->edit_baton);  
+      fb->stored = TRUE;
+    }
+  
+  return SVN_NO_ERROR;
+}
+
+
+static svn_error_t *
+apply_textdelta (void *file_baton, 
+                 svn_txdelta_window_handler_t **handler,
+                 void **handler_baton)
+{
+  struct file_baton *fb = file_baton;
+  
+  if (! fb->stored)
+    {
+      store_path (fb->path, fb->parent_dir_baton->edit_baton);  
+      fb->stored = TRUE;
+    }
+  
+  return SVN_NO_ERROR;
+}
+
+
+
 
 /*** exported routine ***/
 
 svn_error_t *
-svn_ra_local__get_commit_track_editor (svn_delta_edit_fns_t **editor,
-                                       void **edit_baton,
-                                       apr_pool_t *pool,
-                                       svn_ra_local__commit_closer_t *closer)
+svn_delta_get_commit_track_editor (svn_delta_edit_fns_t **editor,
+                                   void **edit_baton,
+                                   apr_pool_t *pool,
+                                   apr_array_header_t *array)
 {
   struct edit_baton *eb = apr_pcalloc (pool, sizeof (*eb));
   svn_delta_edit_fns_t *track_editor = svn_delta_default_editor (pool);
@@ -195,27 +292,23 @@ svn_ra_local__get_commit_track_editor (svn_delta_edit_fns_t **editor,
         set_target_revision
         close_directory
         window_handler
-        apply_textdelta
-        change_file_prop
-        change_dir_prop
      
-     I'm pretty sure delete_entry is a no-op too, so it is also
-     unimplemented.  It's true the delete is part of the commit, but
-     that entity's record is expunged afterwards, so there's no point
-     bumping its revision number.  So, delete_entry is left in the
-     default implementation as well. */
+  */
   track_editor->replace_root = replace_root;
   track_editor->add_directory = add_directory;
   track_editor->replace_directory = replace_directory;
   track_editor->add_file = add_file;
   track_editor->replace_file = replace_file;
-  track_editor->close_file = close_file;
-  track_editor->close_edit = close_edit;
+  track_editor->delete_entry = delete_entry;
+  track_editor->change_dir_prop = change_dir_prop;
+  track_editor->change_file_prop = change_file_prop;
+  track_editor->apply_textdelta = apply_textdelta;
+
 
   /* Set up the edit baton. */
   eb->pool = pool;
   eb->initial_path = svn_string_create ("", pool);
-  eb->closer = closer;
+  eb->array = array;
 
   *editor = track_editor;
   *edit_baton = eb;
