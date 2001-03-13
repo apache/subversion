4ea6b2eff88b10d42cd3b8c9b439288f93033f17
Brian Fitzpatrick <fitz@apache.org>
2001-03-13 01:45:34 +0000

    Changed push_implicit_dot_target(targets, pool) to         svn_cl__push_implicit_dot_target(targets, pool) throughout for         namespace protection as suggested by gstein
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@838909 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/client/update-cmd.c
+++ subversion/client/update-cmd.c
@@ -38,7 +38,7 @@ svn_cl__update (svn_cl__opt_state_t *opt_state,
   int i;
 
   /* Add "." if user passed 0 arguments */
-  push_implicit_dot_target(targets, pool);
+  svn_cl__push_implicit_dot_target(targets, pool);
 
   for (i = 0; i < targets->nelts; i++)
     {
