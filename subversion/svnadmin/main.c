e597c1f909230901ba9d810b42d4e79bba848653
Greg Stein <gstein@apache.org>
2001-03-13 00:51:25 +0000

    More hacking to list out transactions.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@838903 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svnadmin/main.c
+++ subversion/svnadmin/main.c
@@ -29,36 +29,68 @@ main (int argc, const char * const *argv)
   apr_pool_t *pool;
   svn_fs_t *fs;
   svn_error_t *err;
+  int is_create;
+  int is_lstxn;
+  const char *path = ".";
 
   /* ### this whole thing needs to be cleaned up once client/main.c
      ### is refactored. for now, let's just get the tool up and
      ### running. */
-  if (argc != 2 || strcmp(argv[1], "create") != 0)
+  if (argc < 2 || argc > 3 ||
+      !((is_create = strcmp(argv[1], "create") == 0)
+        || (is_lstxn = strcmp(argv[1], "lstxn") == 0)))
     {
-      fprintf(stderr, "USAGE: %s create\n", argv[0]);
-      exit(1);
+      fprintf(stderr,
+              "USAGE: %s COMMAND [ REPOS-PATH ]\n"
+              "  COMMAND is one of: create, lstxn\n",
+              argv[0]);
+      return EXIT_FAILURE;
     }
+
+  if (argc == 3)
+    path = argv[2];
 
   apr_initialize ();
   pool = svn_pool_create (NULL);
 
   fs = svn_fs_new(pool);
-  err = svn_fs_create_berkeley(fs, ".");
-  if (err != NULL)
+  if (is_create)
     {
-      svn_handle_error(err, stderr, FALSE);
-      return EXIT_FAILURE;
+      err = svn_fs_create_berkeley(fs, path);
+      if (err) goto error;
+    }
+  else if (is_lstxn)
+    {
+      char **txns;
+
+      err = svn_fs_open_berkeley(fs, path);
+      if (err) goto error;
+
+      err = svn_fs_list_transactions(&txns, fs, pool);
+      if (err) goto error;
+
+      while (*txns != NULL)
+        printf("%s\n", *txns++);
     }
 
   err = svn_fs_close_fs(fs);
-  if (err != NULL)
-    {
-      svn_handle_error(err, stderr, FALSE);
-      return EXIT_FAILURE;
-    }
+  if (err) goto error;
 
   apr_pool_destroy (pool);
   apr_terminate();
 
   return EXIT_SUCCESS;
+
+ error:
+  svn_handle_error(err, stderr, FALSE);
+  return EXIT_FAILURE;
 }
+
+
+
+
+/* 
+ * local variables:
+ * eval: (load-file "../svn-dev.el")
+ * end:
+ */
