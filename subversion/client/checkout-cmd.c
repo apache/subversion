83934c31c454a1381f3131a58e3fa65e56519227
Brian Fitzpatrick <fitz@apache.org>
2001-03-15 08:39:06 +0000

    *checkout-cmd.c: Added enormous comment re: checking out multiple  repositories, now feeding multiples to the svn_client_checkout  command, and have (gladly) ditched all that ancestor-path  foolishness.
    *cl.h: removed svn_cl__target_dir_opt from svn_cl__longopt_t since it
     now has 'd' as its shortopt
    
    *main.c: - checkout command now takes 1 or more non-file targets.
             - fixed checkout helpstring
             - deep-sixed ancestor-path related code as it's now a
               non-file arg to checkout
             - renamed --target-dir to --destination and made -d the
               short-opt (see README)
    
             - non-file arg descriptions that have -1 in them (like the
               one for svn checkout) require at least one arg. Seems right
               to me... I don't see the case where we would ever have 0,
               1, or many non-file args. This can always change in the future.
    
    *tests/svn-test.sh: updated to accomodate new calling conventions.
    *tests/svn-test2.sh: updated to accomodate new calling conventions.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@838936 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/client/checkout-cmd.c
+++ subversion/client/checkout-cmd.c
@@ -37,25 +37,72 @@ svn_cl__checkout (svn_cl__opt_state_t *opt_state,
   const svn_delta_edit_fns_t *trace_editor;
   void *trace_edit_baton;
   svn_error_t *err;
+  int i;
 
-  err = svn_cl__get_trace_update_editor (&trace_editor,
-                                         &trace_edit_baton,
-                                         opt_state->target,
-                                         pool);
-  if (err)
-    return err;
+  /* TODO Fixme: This only works for one repo checkout at a shot.  In
+     CVS, when we checkout one project and give it a destination
+     directory, it dumps it in the directory. If you check out more
+     than one, it dumps each project into its own directory *inside*
+     the one that you specified with the -d flag. So, for example, we
+     have project A:
 
+         A/one_mississippi.txt
+         A/two_mississippi.txt
+         A/three_mississippi.txt
+     
+     And project B:
 
-  err = svn_client_checkout (NULL, NULL,
-                             trace_editor, trace_edit_baton,
-                             opt_state->ancestor_path,
-                             opt_state->target,
-                             opt_state->revision,
-                             opt_state->xml_file,
-                             pool);
-  if (err)
-    return err;
+         B/cat
+         B/dog
+         B/pig
 
+     If I do 'cvs -d :pserver:fitz@subversion.tigris.org:/cvs co -d foo A', I get the following:
+
+         foo/one_mississippi.txt
+         foo/two_mississippi.txt
+         foo/three_mississippi.txt
+
+     But if I do this 'cvs -d :pserver:fitz@subversion.tigris.org:/cvs
+     co -d foo A B', I get the following:
+
+         foo/A/one_mississippi.txt
+         foo/A/two_mississippi.txt
+         foo/A/three_mississippi.txt
+         foo/B/cat
+         foo/B/dog
+         foo/B/pig
+      
+    Makes sense, right? Right. Note that we have no provision for this
+    right now and we need to support it. My vote is that we stop
+    iterating over opt_state->args here and just pass the args into
+    svn_client_checkout and let it decide what to do based on
+    (args->nelts == 1) or (args->nelts > 1). -Fitz
+
+   */
+  for (i = 0; i < opt_state->args->nelts; i++)
+    {
+      svn_string_t *repos_url
+        = ((svn_string_t **) (opt_state->args->elts))[0];
+      
+      err = svn_cl__get_trace_update_editor (&trace_editor,
+                                             &trace_edit_baton,
+                                             opt_state->target,
+                                             pool);
+      if (err)
+        return err;
+      
+      
+      err = svn_client_checkout (NULL, NULL,
+                                 trace_editor, trace_edit_baton,
+                                 repos_url,
+                                 opt_state->target,
+                                 opt_state->revision,
+                                 opt_state->xml_file,
+                                 pool);
+      if (err)
+        return err;
+      
+    }
   return SVN_NO_ERROR;
 }
 
