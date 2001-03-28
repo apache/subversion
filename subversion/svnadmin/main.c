202b88f9c0a3729c587df8b61bad3fd6e4dad015
Karl Fogel <kfogel@apache.org>
2001-03-28 03:33:36 +0000

    Print revision trees.  Now we can inspect repositories from the command line:
    (print_tree, usage): New funcs.
    (main): Added "youngest" and "lsrevs" commands.
    Use new usage() func to print help.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@839068 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svnadmin/main.c
+++ subversion/svnadmin/main.c
@@ -23,37 +23,131 @@
 #include "svn_error.h"
 #include "svn_fs.h"
 
+
+
+
+/*** Tree printing. ***/
+
+/* Print the tree at ROOT:PATH, indenting by INDENTATION spaces.
+   Use POOL for any allocation.  */
+static svn_error_t *
+print_tree (svn_fs_root_t *root,
+            const char *path,
+            int indentation,
+            apr_pool_t *pool)
+{
+  apr_hash_t *entries;
+  apr_hash_index_t *hi;
+  
+  SVN_ERR (svn_fs_dir_entries (&entries, root, path, pool));
+
+  for (hi = apr_hash_first (entries); hi; hi = apr_hash_next (hi))
+    {
+      const void *key;
+      apr_size_t keylen;
+      void *val;
+      svn_fs_dirent_t *this_entry;
+      const char *this_full_path;
+      int is_dir;
+      int i;
+ 
+      apr_hash_this (hi, &key, &keylen, &val);
+      this_entry = val;
+
+      this_full_path = apr_psprintf (pool, "%s/%s", path, this_entry->name);
+
+      /* Indent. */
+      for (i = 0; i < indentation; i++)
+        printf (" ");
+
+      printf ("%s", this_entry->name);
+      
+      SVN_ERR (svn_fs_is_dir (&is_dir, root, this_full_path, pool));
+      if (is_dir)
+        {
+          printf ("/\n");  /* trailing slash for dirs */
+          print_tree (root, this_full_path, indentation + 1, pool);
+        }
+      else   /* assume it's a file */
+        {
+          apr_off_t len;
+          SVN_ERR (svn_fs_file_length (&len, root, this_full_path, pool));
+          printf ("[%ld]\n", (long int) len);
+        }
+    }
+
+  return SVN_NO_ERROR;
+}
+
+
+
+
+/*** Argument parsing and usage. ***/
+void
+usage (const char *progname, int exit_code)
+{
+  fprintf
+    (exit_code ? stderr : stdout,
+     "usage: %s COMMAND REPOS_PATH [LOWER_REV [UPPER_REV]]\n"
+     "\n"
+     "Commands are: \n"
+     "  - create   REPOS_PATH\n"
+     "  - youngest REPOS_PATH\n"
+     "  - lstxn    REPOS_PATH\n"
+     "  - lsrevs   REPOS_PATH [LOWER_REV [UPPER_REV]]\n"
+     "      If no revision is given, all revision trees are printed.\n"
+     "      If just LOWER_REV is given, that revision trees is printed.\n"
+     "      If two revisions are given, that range is printed, inclusive.\n"
+     "      (Printing a revision tree shows its structure and file sizes.)\n"
+     "\n",
+     progname);
+
+  exit (exit_code);
+}
+
+
+
+/*** Main. ***/
+
 int
 main (int argc, const char * const *argv)
 {
   apr_pool_t *pool;
   svn_fs_t *fs;
   svn_error_t *err;
-  int is_create;
-  int is_lstxn;
-  const char *path = ".";
+  int               /* commands */
+    is_create = 0,
+    is_youngest = 0,
+    is_lstxn = 0,
+    is_lsrevs = 0;
+  const char *path = NULL;
 
   /* ### this whole thing needs to be cleaned up once client/main.c
      ### is refactored. for now, let's just get the tool up and
      ### running. */
-  if (argc < 2 || argc > 3 ||
-      !((is_create = strcmp(argv[1], "create") == 0)
-        || (is_lstxn = strcmp(argv[1], "lstxn") == 0)))
+
+  if (argc < 3)
     {
-      fprintf(stderr,
-              "USAGE: %s COMMAND [ REPOS-PATH ]\n"
-              "  COMMAND is one of: create, lstxn\n",
-              argv[0]);
+      usage (argv[0], 1);
       return EXIT_FAILURE;
     }
 
-  if (argc == 3)
-    path = argv[2];
+  path = argv[2];
+
+  if (! ((is_create = strcmp(argv[1], "create") == 0)
+         || (is_youngest = strcmp(argv[1], "youngest") == 0)
+         || (is_lstxn = strcmp(argv[1], "lstxn") == 0)
+         || (is_lsrevs = strcmp(argv[1], "lsrevs") == 0)))
+    {
+      usage (argv[0], 1);
+      return EXIT_FAILURE;
+    }
 
   apr_initialize ();
   pool = svn_pool_create (NULL);
 
   fs = svn_fs_new(pool);
+
   if (is_create)
     {
       err = svn_fs_create_berkeley(fs, path);
@@ -71,6 +165,60 @@ main (int argc, const char * const *argv)
 
       while (*txns != NULL)
         printf("%s\n", *txns++);
+    }
+  else if (is_youngest)
+    {
+      svn_revnum_t youngest_rev;
+
+      err = svn_fs_open_berkeley(fs, path);
+      if (err) goto error;
+
+      svn_fs_youngest_rev (&youngest_rev, fs, pool);
+      printf ("%ld\n", (long int) youngest_rev);
+    }
+  else if (is_lsrevs)
+    {
+      svn_revnum_t
+        lower = SVN_INVALID_REVNUM,
+        upper = SVN_INVALID_REVNUM,
+        this;
+
+      err = svn_fs_open_berkeley(fs, path);
+      if (err) goto error;
+
+      /* Do the args tell us what revisions to inspect? */
+      if (argv[3])
+        {
+          lower = (svn_revnum_t) atoi (argv[3]);
+          if (argv[4])
+            upper = (svn_revnum_t) atoi (argv[4]);
+        }
+
+      /* Fill in for implied args. */
+      if (lower == SVN_INVALID_REVNUM)
+        {
+          lower = 0;
+          svn_fs_youngest_rev (&upper, fs, pool);
+        }
+      else if (upper == SVN_INVALID_REVNUM)
+        upper = lower;
+
+      /* Loop, printing revisions. */
+      for (this = lower; this <= upper; this++)
+        {
+          svn_fs_root_t *this_root;
+          apr_pool_t *this_pool = svn_pool_create (pool);
+
+          err = svn_fs_revision_root (&this_root, fs, this, this_pool);
+          if (err) goto error;
+
+          printf ("Revision %ld:\n", (long int) this);
+          printf ("===============\n");
+          print_tree (this_root, "", 1, this_pool);
+          printf ("\n");
+
+          apr_pool_destroy (this_pool);
+        }
     }
 
   err = svn_fs_close_fs(fs);
