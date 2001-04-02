20b5c6dac46e59c9386b5a23e83fe7f99ac98303
Kevin Meyer <kevin@apache.org>
2001-04-02 14:47:45 +0000

    Subversion now warns on repeated addition of the same entry.
    * libsvn_client/add.c (svn_client_add) - Handle (as a warning), the (new)
    				specific return of SVN_ERR_WC_ENTRY_EXISTS returned from
    				svn_wc_add_file and svn_wc_add_dir.
    
    * libsvn_wc/entries.c (interpret_changes) - Return SVN_ERR_WC_ENTRY_EXITS
    				when adding an already existing entry.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@839129 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/add.c
+++ subversion/libsvn_client/add.c
@@ -33,18 +33,26 @@ svn_error_t *
 svn_client_add (svn_string_t *path, apr_pool_t *pool)
 {
   enum svn_node_kind kind;
+  svn_error_t * err;
 
   SVN_ERR (svn_io_check_path (path, &kind, pool));
   
   if (kind == svn_node_file)
-    SVN_ERR (svn_wc_add_file (path, pool));
+    err = svn_wc_add_file (path, pool);
   else if (kind == svn_node_dir)
-    SVN_ERR (svn_wc_add_directory (path, pool));
+    err = svn_wc_add_directory (path, pool);
   else
     return
       svn_error_createf (SVN_ERR_WC_PATH_NOT_FOUND, 0, NULL, pool,
                          "No such object: %s", path->data);
 
+  if (err)
+    {
+      if (err->apr_err == SVN_ERR_WC_ENTRY_EXISTS)
+          svn_handle_warning(err, "svn warning: Cannot add because \"%s\"", err->message);
+      else
+          return err;
+    }
   return SVN_NO_ERROR;
 }
 
