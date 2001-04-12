2a379005770d31f3e8991c6adf41ef335f280472
Ben Collins-Sussman <sussman@apache.org>
2001-04-12 13:58:44 +0000

    Correctly object-orient this script, as suggested by gstein.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@839163 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/clients/cmdline/svn_entry.py
+++ subversion/tests/clients/cmdline/svn_entry.py
@@ -33,41 +33,34 @@
 
 import xml.parsers.expat  # you may need to install this package
 
-
-# Global list: this is stupid.  The expat module doesn't allow us to
-# pass a baton into the start-tag handler, so we have to re-use this
-# global list.
-
-entry_list = []
-
-# The entry 'struct'
 class svn_entry:
   "An object that represents an entry from an 'entries' file."
-  # constructor
-  def __init__(self, name):
+
+  def __init__(self, name):   # constructor
     self.name = name
     self.atts = {}
 
-# When we get a new tag, append a new entry object to the list.
-def handle_start_tag(name, attrs):
-  "Expat callback that receives a new open-tag."
-  if attrs.has_key('name'):
-    entry = svn_entry(attrs['name'])
-    entry.atts = attrs       # todo:  make revision/ancestry inherit.
-    entry_list.append(entry)
+class svn_entryparser:
+  "A class to parse an 'entries' file."
+
+  def __init__(self):   # constructor
+    self.entry_list = []
+    self.parser = xml.parsers.expat.ParserCreate()
+    self.parser.StartElementHandler = self.handle_start_tag
+
+  def handle_start_tag(self, name, attrs):
+    "Expat callback that receives a new open-tag."
+    if attrs.has_key('name'):
+      entry = svn_entry(attrs['name']) # create new entry object
+      entry.atts = attrs       # todo:  make revision/ancestry inherit.
+      self.entry_list.append(entry)
 
 # The main exported routine
 def get_entries(path):
   "Parse the entries file at PATH and return a list of svn_entry objects."
 
-  entry_list = []         # this is global; fix this.
+  entryparser = svn_entryparser() # make a parser instance
   fp = open(path, 'r')
-
-  parser = xml.parsers.expat.ParserCreate()
-  parser.StartElementHandler = handle_start_tag
-  parser.ParseFile(fp)
-
+  entryparser.parser.ParseFile(fp)
   fp.close()
-  entry_list_copy = entry_list # return a copy of the global list; fix this.
-  return entry_list_copy
-
+  return entryparser.entry_list
