027b6093eb0da210dd1084483614052da52f50b4
Ben Collins-Sussman <sussman@apache.org>
2001-04-16 20:52:41 +0000

    Move 'reporter' functionality (for updates) into libsvn_repos.
    * reporter.c: new file, copied over from libsvn_ra_local/update.c.
    
    * ra_local.h (svn_ra_local__set_path, svn_ra_local__finish_report,
      svn_ra_local__report_baton_t): remove declarations
    
    * svn_repos.h (svn_repos_set_path, svn_repos_finish_report,
      svn_repos_report_baton_t): move here, with new names.
    
    * ra_plugin.c (do_update, svn_ra_reporter_t): use reporter functions
      with their new svn_repos prefix.
    
    * libsvn_repos/Makefile.am:  build reporter.c
    
    * libsvn_ra_local/Makefile.am:  stop building update.c
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@839199 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_ra_local/ra_local.h
+++ subversion/libsvn_ra_local/ra_local.h
@@ -75,37 +75,6 @@ typedef struct svn_ra_local__commit_closer_t
 
 
 
-/* A structure used by the routines within the `reporter' vtable,
-   driven by the client as it describes its working copy revisions. */
-typedef struct svn_ra_local__report_baton_t
-{
-  /* The transaction being built in the repository, a mirror of the
-     working copy. */
-  svn_fs_t *fs;
-  svn_fs_txn_t *txn;
-  svn_fs_root_t *txn_root;
-
-  /* The location under which all reporting will happen. */
-  svn_string_t *base_path;
-
-  /* finish_report() calls svn_fs_dir_delta(), and uses this arg to
-     decide which revision to compare the transaction against. */
-  svn_revnum_t revnum_to_update_to;
-
-  /* The working copy editor driven by svn_fs_dir_delta(). */
-  const svn_delta_edit_fns_t *update_editor;
-  void *update_edit_baton;
-
-  /* This hash describes the mixed revisions in the transaction; it
-     maps pathnames (char *) to revision numbers (svn_revnum_t). */
-  apr_hash_t *path_rev_hash;
-
-  /* Pool from the session baton. */
-  apr_pool_t *pool;
-
-} svn_ra_local__report_baton_t;
-
-
 
 
 /** Private routines **/
@@ -142,18 +111,6 @@ svn_ra_local__checkout (svn_fs_t *fs,
                         apr_pool_t *pool);
 
 
-
-/* The implementations of the `reporter' vtable routines (for
-   updates). */
-
-svn_error_t *
-svn_ra_local__set_path (void *report_baton,
-                        svn_string_t *path,
-                        svn_revnum_t revision);
-
-
-svn_error_t *
-svn_ra_local__finish_report (void *report_baton);
 
 
 
--- subversion/libsvn_repos/reporter.c
+++ subversion/libsvn_repos/reporter.c
@@ -1,5 +1,5 @@
 /*
- * update.c : `reporter' vtable routines
+ * reporter.c : `reporter' vtable routines for updates.
  *
  * ====================================================================
  * Copyright (c) 2000-2001 CollabNet.  All rights reserved.
@@ -12,21 +12,19 @@
  * ====================================================================
  */
 
-#include "ra_local.h"
+#include "svn_repos.h"
+#include "svn_fs.h"
+#include "svn_path.h"
 
 
-
-/* The client reports that its copy of DIR_PATH is at REVISION.  Make
-   the transaction reflect this. */
 svn_error_t *
-svn_ra_local__set_path (void *report_baton,
-                        svn_string_t *path,
-                        svn_revnum_t revision)
+svn_repos_set_path (void *report_baton,
+                    svn_string_t *path,
+                    svn_revnum_t revision)
 {
   svn_fs_root_t *from_root;
   svn_string_t *from_path;
-  svn_ra_local__report_baton_t *rbaton 
-    = (svn_ra_local__report_baton_t *) report_baton;
+  svn_repos_report_baton_t *rbaton = (svn_repos_report_baton_t *) report_baton;
   svn_revnum_t *rev_ptr = apr_palloc (rbaton->pool, sizeof(*rev_ptr));
 
   /* If this is the very first call, no txn exists yet. */
@@ -53,35 +51,33 @@ svn_ra_local__set_path (void *report_baton,
 
   else  /* this is not the first call to set_path. */ 
     {
-    /* Create the "from" root and path. */
-    SVN_ERR (svn_fs_revision_root (&from_root, rbaton->fs,
-                                   revision, rbaton->pool));
-    from_path = svn_string_dup (rbaton->base_path, rbaton->pool);
-    svn_path_add_component (from_path, path, svn_path_repos_style);
-    
-    /* Copy into our txn. */
-    SVN_ERR (svn_fs_copy (from_root, from_path->data,
-                          rbaton->txn_root, from_path->data, rbaton->pool));
-    
-    /* Remember this path in our hashtable. */
-    *rev_ptr = revision;
-    apr_hash_set (rbaton->path_rev_hash, from_path->data,
-                  from_path->len, rev_ptr);    
-  }
+      /* Create the "from" root and path. */
+      SVN_ERR (svn_fs_revision_root (&from_root, rbaton->fs,
+                                     revision, rbaton->pool));
+      from_path = svn_string_dup (rbaton->base_path, rbaton->pool);
+      svn_path_add_component (from_path, path, svn_path_repos_style);
+      
+      /* Copy into our txn. */
+      SVN_ERR (svn_fs_copy (from_root, from_path->data,
+                            rbaton->txn_root, from_path->data, rbaton->pool));
+      
+      /* Remember this path in our hashtable. */
+      *rev_ptr = revision;
+      apr_hash_set (rbaton->path_rev_hash, from_path->data,
+                    from_path->len, rev_ptr);    
+    }
 
   return SVN_NO_ERROR;
 }
 
 
 
-/* Make the filesystem compare the transaction to a revision and have
-   it drive an update editor.  Then abort the transaction. */
+
 svn_error_t *
-svn_ra_local__finish_report (void *report_baton)
+svn_repos_finish_report (void *report_baton)
 {
   svn_fs_root_t *rev_root;
-  svn_ra_local__report_baton_t *rbaton 
-    = (svn_ra_local__report_baton_t *) report_baton;
+  svn_repos_report_baton_t *rbaton = (svn_repos_report_baton_t *) report_baton;
 
   /* Get the root of the revision we want to update to. */
   SVN_ERR (svn_fs_revision_root (&rev_root, rbaton->fs,
