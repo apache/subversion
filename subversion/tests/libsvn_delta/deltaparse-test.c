0f3dc74824f99a1f87770a2dea06db9eaededf84
Kevin Meyer <kevin@apache.org>
2001-04-18 18:38:25 +0000

    Renable (dynamic) building of the test tree.
        * tests/libsvn_subr/Makefile.am - Add @SVN_LIBSVN_FS_LIBS@ if building
            the filesystem.  (This is because of the new fs-helpers.c in
            libsvn_test.la)
    
        * tests/libsvn_delta/Makefile.am - Add @SVN_LIBSVN_FS_LIBS@ if building
            the filesystem.  (This is because of the new fs-helpers.c in
            libsvn_test.la)
    
        * tests/libsvn_wc/Makefile.am - Add @SVN_LIBSVN_FS_LIBS@ if building
            the filesystem.  (This is because of the new fs-helpers.c in
            libsvn_test.la)
    
        * tests/libsvn_delta/deltaparse-test.c - Add empty definition of
            test_funcs variable, required by the new combined libsvn_test.la
    
        * tests/libsvn_wc/commit-test.c - Add empty definition of
            test_funcs variable, required by the new combined libsvn_test.la
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@839228 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/libsvn_delta/deltaparse-test.c
+++ subversion/tests/libsvn_delta/deltaparse-test.c
@@ -24,7 +24,8 @@
 #include "svn_delta.h"
 #include "svn_test.h"   /* For svn_test_get_editor() */
 
-
+/* libsvn_test.la requires this symbol */ 
+svn_error_t *(*test_funcs[])(const char **msg, apr_pool_t *p) = {};
 
 
 int
