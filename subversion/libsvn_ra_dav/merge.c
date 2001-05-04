cdd394ea9e26acb87bd27ebcc7a7b56527b63354
Greg Stein <gstein@apache.org>
2001-05-04 10:47:29 +0000

    For requests which need to parse XML responses, the code was following a particular template. Shift all of that code to a utility function.
    Then, add recognition of XML parsing errors in the response.
    
    * ra_dav.h (svn_ra_dav__parsed_request): new declaration
    
    * fetch.c (reporter_finish_report): use new function
    * options.c (svn_ra_dav__get_activity_url): use new function
    * merge.c (svn_ra_dav__merge_activity): use new function
    
    * util.c (svn_ra_dav__parsed_request): implement from unification of the
        pattern from fetch.c, options.c and merge.c. and XML parse error
        handling.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@839274 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_ra_dav/merge.c
+++ subversion/libsvn_ra_dav/merge.c
@@ -499,33 +499,9 @@ svn_error_t * svn_ra_dav__merge_activity(
     apr_pool_t *pool)
 {
   merge_ctx_t mc = { 0 };
-  http_req *req;
-  hip_xml_parser *parser;
-  int rv;
-  int code;
   const char *body;
   svn_string_t *path_str;
   int i;
-
-  /* create/prep the request */
-  req = http_request_create(ras->sess, "MERGE", repos_url);
-  if (req == NULL)
-    {
-      return svn_error_createf(SVN_ERR_RA_CREATING_REQUEST, 0, NULL, pool,
-                               "Could not create a MERGE request (%s)",
-                               repos_url);
-    }
-
-  body = apr_psprintf(pool,
-                      "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
-                      "<D:merge xmlns:D=\"DAV:\">"
-                      "<D:source><D:href>%s</D:href></D:source>"
-                      "<D:no-auto-merge/><D:no-checkout/>"
-                      "<D:prop>"
-                      "<D:checked-in/><D:version-name/><D:resourcetype/>"
-                      "</D:prop>"
-                      "</D:merge>", activity_url);
-  http_set_request_body_buffer(req, body);
 
   mc.pool = pool;
   mc.base_href = repos_url;
@@ -543,49 +519,22 @@ svn_error_t * svn_ra_dav__merge_activity(
   /* ### damn it */
   mc.vsn_url_name = svn_string_create(SVN_RA_DAV__LP_VSN_URL, pool);
 
-  /* create a parser to read the MERGE response body */
-  parser = hip_xml_create();
-  hip_xml_push_handler(parser, merge_elements,
-                       validate_element, start_element, end_element, &mc);
-  http_add_response_body_reader(req, http_accept_2xx, hip_xml_parse_v, parser);
+  body = apr_psprintf(pool,
+                      "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+                      "<D:merge xmlns:D=\"DAV:\">"
+                      "<D:source><D:href>%s</D:href></D:source>"
+                      "<D:no-auto-merge/><D:no-checkout/>"
+                      "<D:prop>"
+                      "<D:checked-in/><D:version-name/><D:resourcetype/>"
+                      "</D:prop>"
+                      "</D:merge>", activity_url);
 
-  /* run the request and get the resulting status code. */
-  rv = http_request_dispatch(req);
-  code = http_get_status(req)->code;
-  http_request_destroy(req);
+  SVN_ERR( svn_ra_dav__parsed_request(ras, "MERGE", repos_url, body, NULL,
+                                      merge_elements, validate_element,
+                                      start_element, end_element, &mc,
+                                      pool) );
 
-  if (rv != HTTP_OK)
-    {
-      /* ### need to be more sophisticated with reporting the failure */
-
-      switch (rv)
-        {
-        case HTTP_CONNECT:
-          /* ### need an SVN_ERR here */
-          return svn_error_createf(APR_EGENERAL, 0, NULL, pool,
-                                   "Could not connect to server "
-                                   "(%s, port %d).",
-                                   ras->root.host, ras->root.port);
-
-        case HTTP_AUTH:
-          return svn_error_create(SVN_ERR_NOT_AUTHORIZED, 0, NULL, pool,
-                                  "Authentication failed on server.");
-
-        default:
-          return svn_error_createf(SVN_ERR_RA_REQUEST_FAILED, 0, NULL, pool,
-                                   "The MERGE request failed (#%d) (%s)",
-                                   rv, repos_url);
-        }
-    }
-
-  if (code != 200)
-    {
-      /* ### need an SVN_ERR here */
-      return svn_error_createf(APR_EGENERAL, 0, NULL, pool,
-                               "The MERGE status was %d, but expected 200.",
-                               code);
-    }
-
+  /* is there an error stashed away in our context? */
   if (mc.err != NULL)
     return mc.err;
 
