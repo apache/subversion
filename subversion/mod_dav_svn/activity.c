bdccb89f68036b90d6ab5c5833021655e69cd628
Greg Stein <gstein@apache.org>
2001-05-05 11:03:38 +0000

    Update apr_sdbm_open() calls to latest interface.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@839287 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/mod_dav_svn/activity.c
+++ subversion/mod_dav_svn/activity.c
@@ -40,7 +40,8 @@ const char *dav_svn_get_txn(const dav_svn_repos *repos,
 
   pathname = svn_string_create(repos->fs_path, repos->pool);
   svn_path_add_component_nts(pathname, ACTIVITY_DB, svn_path_local_style);
-  status = apr_dbm_open(&dbm, pathname->data, APR_DBM_READONLY, repos->pool);
+  status = apr_dbm_open(&dbm, pathname->data, APR_DBM_READONLY,
+                        APR_OS_DEFAULT, repos->pool);
   if (status != APR_SUCCESS)
     {
       /* ### let's just assume that any error means the DB doesn't exist,
@@ -78,7 +79,8 @@ dav_error *dav_svn_store_activity(const dav_svn_repos *repos,
 
   pathname = svn_string_create(repos->fs_path, repos->pool);
   svn_path_add_component_nts(pathname, ACTIVITY_DB, svn_path_local_style);
-  status = apr_dbm_open(&dbm, pathname->data, APR_DBM_RWCREATE, repos->pool);
+  status = apr_dbm_open(&dbm, pathname->data, APR_DBM_RWCREATE,
+                        APR_OS_DEFAULT, repos->pool);
   if (status != APR_SUCCESS)
     {
       /* ### return an error */
