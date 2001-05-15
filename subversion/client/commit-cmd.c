0bc603616d8a68847cbff9bd3792303f31154e39
C. Michael Pilato <cmpilato@apache.org>
2001-05-15 14:12:55 +0000

    * client/commit-cmd.c
      (svn_cl__commit): Hack up a beautiful path for the trace editor to use that
      *should* work with multi-arg commits.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@839353 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/client/commit-cmd.c
+++ subversion/client/commit-cmd.c
@@ -34,10 +34,13 @@ svn_cl__commit (apr_getopt_t *os,
                 svn_cl__opt_state_t *opt_state,
                 apr_pool_t *pool)
 {
-  svn_error_t *err;
   apr_array_header_t *targets;
+  apr_array_header_t *condensed_targets;
   svn_string_t *message;
-  int i;
+  svn_string_t *base_dir;
+  svn_string_t *cur_dir;
+  const svn_delta_edit_fns_t *trace_editor;
+  void *trace_edit_baton;
 
   /* Take our message from ARGV or a FILE */
   if (opt_state->filedata) 
@@ -48,29 +51,38 @@ svn_cl__commit (apr_getopt_t *os,
   targets = svn_cl__args_to_target_array (os, pool);
 
   /* Add "." if user passed 0 arguments */
-  svn_cl__push_implicit_dot_target(targets, pool);
+  svn_cl__push_implicit_dot_target (targets, pool);
 
-  for (i = 0; i < targets->nelts; i++)
-    {
-      svn_string_t *target = ((svn_string_t **) (targets->elts))[i];
-      const svn_delta_edit_fns_t *trace_editor;
-      void *trace_edit_baton;
-      
-      err = svn_cl__get_trace_commit_editor (&trace_editor,
-                                             &trace_edit_baton,
-                                             target, pool);
-      if (err) return err;
-      
-      err = svn_client_commit (NULL, NULL,
-                               trace_editor, trace_edit_baton,
-                               targets,
-                               message,
-                               opt_state->xml_file,
-                               opt_state->revision,
-                               pool);
-      if (err)
-        return err;
-    }
+  /* Get the current working directory as an absolute path. */
+  SVN_ERR (svn_path_get_absolute (&cur_dir,
+                                  svn_string_create (".", pool),
+                                  pool));
+
+  /* Condense the targets (like commit does)... */
+  SVN_ERR (svn_path_condense_targets (&base_dir,
+                                      &condensed_targets,
+                                      targets,
+                                      pool));
+
+  /* ...so we can have a common parent path to pass to the trace
+     editor.  Note that what we are actually passing here is the
+     difference between the absolute path of the current working
+     directory and the absolute path of the common parent directory
+     used in the commit (give or take a slash :-). */
+  SVN_ERR (svn_cl__get_trace_commit_editor 
+           (&trace_editor,
+            &trace_edit_baton,
+            svn_string_create (&(base_dir->data[cur_dir->len + 1]), pool),
+            pool));
+
+  /* Commit. */
+  SVN_ERR (svn_client_commit (NULL, NULL,
+                              trace_editor, trace_edit_baton,
+                              targets,
+                              message,
+                              opt_state->xml_file,
+                              opt_state->revision,
+                              pool));
 
   return SVN_NO_ERROR;
 }
