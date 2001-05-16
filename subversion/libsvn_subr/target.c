c2781eead2180319dbb01c42d327a4cf64a7c849
C. Michael Pilato <cmpilato@apache.org>
2001-05-16 18:58:31 +0000

    Resolution for IssueZilla issue #386
    * include/svn_path.h
    * include/target.c
    
      (svn_path_remove_redundancies): New function.
    
    * client/update-cmd.c
    
      (svn_cl__update): Remove redundancies from the target list,
      preserving the order of those targets, using new
      svn_path_remove_redundancies function.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@839369 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/target.c
+++ subversion/libsvn_subr/target.c
@@ -252,6 +252,99 @@ svn_path_condense_targets (svn_string_t **pbasedir,
   return SVN_NO_ERROR;
 }
 
+
+svn_error_t *
+svn_path_remove_redundancies (apr_array_header_t **pcondensed_targets,
+                              const apr_array_header_t *targets,
+                              apr_pool_t *pool)
+{
+  apr_pool_t *temp_pool;
+  apr_array_header_t *abs_targets;
+  apr_array_header_t *rel_targets;
+  int i;
+
+  if ((targets->nelts <= 0) || (! pcondensed_targets))
+    {
+      /* No targets or no place to store our work means this function
+         really has nothing to do. */
+      if (pcondensed_targets)
+        *pcondensed_targets = NULL;
+      return SVN_NO_ERROR;
+    }
+
+  /* Initialize our temporary pool. */
+  temp_pool = svn_pool_create (pool);
+
+  /* Create our list of absolute paths for our "keepers" */
+  abs_targets = apr_array_make (temp_pool, targets->nelts, 
+                                sizeof (svn_string_t *));
+
+  /* Create our list of untainted paths for our "keepers" */
+  rel_targets = apr_array_make (pool, targets->nelts,
+                                sizeof (svn_string_t *));
+
+  /* For each target in our list we do the following:
+
+     1.  Calculate its absolute path (ABS_PATH).
+     2.  See if any of the keepers in ABS_TARGETS is a parent of, or
+         is the same path as, ABS_PATH.  If so, we ignore this
+         target.  If not, however, add this target's absolute path to
+         ABS_TARGETS and its original path to REL_TARGETS.
+  */
+  for (i = 0; i < targets->nelts; i++)
+    {
+      svn_string_t *rel_path = ((svn_string_t **)targets->elts)[i];
+      svn_string_t *abs_path;
+      int j;
+      svn_boolean_t keep_me;
+
+      /* Get the absolute path for this target. */
+      SVN_ERR (svn_path_get_absolute (&abs_path, rel_path, temp_pool));
+
+      /* For each keeper in ABS_TARGETS, see if this target is the
+         same as or a child of that keeper. */
+      keep_me = TRUE;
+      for (j = 0; j < abs_targets->nelts; j++)
+        {
+          svn_string_t *keeper = ((svn_string_t **)abs_targets->elts)[j];
+          
+          /* Quit here if we find this path already in the keepers. */
+          if (svn_string_compare (keeper, abs_path))
+            {
+              keep_me = FALSE;
+              break;
+            }
+          
+          /* Quit here if this path is a child of one of the keepers. */
+          if (svn_path_is_child (keeper, abs_path, temp_pool))
+            { 
+              keep_me = FALSE;
+              break;
+            }
+        }
+
+      /* If this is a new keeper, add its absolute path to ABS_TARGETS
+         and its original path to REL_TARGETS. */
+      if (keep_me)
+        {
+          (* ((svn_string_t **) apr_array_push (abs_targets))) = abs_path;
+          (* ((svn_string_t **) apr_array_push (rel_targets))) = rel_path;
+        }
+    }
+  
+  /* Destroy our temporary pool. */
+  svn_pool_destroy (temp_pool);
+
+  /* Make sure we return the list of untainted keeper paths. */
+  *pcondensed_targets = rel_targets;
+  
+  return SVN_NO_ERROR;
+}
+      
+
+  
+
+
 
 /* 
  * local variables:
