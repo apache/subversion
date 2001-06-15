b52f277db21236bd00c153523fe6c6eb9de07de1
Karl Fogel <kfogel@apache.org>
2001-06-15 16:23:36 +0000

    Finish conversion to Greg Stein's new error system:
    * svn_error.h: Get rid of old system, add comments for new one.
    (svn_strerror): Document new behavior.
    
    * svn_error.c: Got rid of top-level conditionals.
    (svn_handle_error): Use the new system.
    (svn_strerror): Removed old implementation of this function.
    (set_error_hash, initialize_svn_error_descriptions): Removed.
    
    * svn_error_codes.h: Added explanatory comment.  Reformatted error
    list and removed redundant comments.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@839534 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_error_codes.h
+++ subversion/include/svn_error_codes.h
@@ -11,10 +11,26 @@
  * ====================================================================
  */
 
-/*
- * Process this file if we're building an error array, or if we have
- * not defined the enumerated constants yet.
- */
+/* What's going on here?
+ 
+   In order to define error codes and their associated description
+   strings in the same place, we overload the SVN_ERRDEF() macro with
+   two definitions below.  Both take two arguments, an error code name
+   and a description string.  One definition of the macro just throws
+   away the string and defines enumeration constants using the error
+   code names -- that definition is used by the header file that
+   exports error codes to the rest of Subversion.  The other
+   definition creates a static table mapping the enum codes to their
+   corresponding strings -- that definition is used by the C file that
+   implements svn_strerror().
+ 
+   The header and C files both include this file, using #defines to
+   control which version of the macro they get.  
+*/
+
+
+/* Process this file if we're building an error array, or if we have
+   not defined the enumerated constants yet.  */
 #if defined(SVN_ERROR_BUILD_ARRAY) || !defined(SVN_ERROR_ENUM_DEFINED)
 
 
@@ -45,251 +61,241 @@ extern "C" {
 
 #endif
 
-/* 
-   Define custom Subversion error numbers, in the range reserved for
+/* Define custom Subversion error numbers, in the range reserved for
    that in APR: from APR_OS_START_USEERR to APR_OS_START_SYSERR (see
-   apr_errno.h).
-
-*/
+   apr_errno.h).  */
 SVN_ERROR_START
 
-  SVN_ERRDEF(SVN_ERR_NOT_AUTHORIZED, "Not authorized")
-  SVN_ERRDEF(SVN_ERR_PLUGIN_LOAD_FAILURE, "Failure loading plugin")
-  SVN_ERRDEF(SVN_ERR_UNKNOWN_FS_ACTION, "Unknown fs action")
-  SVN_ERRDEF(SVN_ERR_UNEXPECTED_EOF, "Unexpected end of file")
-  SVN_ERRDEF(SVN_ERR_MALFORMED_FILE, "Malformed file")
-  SVN_ERRDEF(SVN_ERR_INCOMPLETE_DATA, "Incomplete data")
+  SVN_ERRDEF (SVN_ERR_NOT_AUTHORIZED,
+              "Not authorized")
 
-  /* The xml delta we got was not well-formed. */
-  SVN_ERRDEF(SVN_ERR_MALFORMED_XML, "XML data was not well-formed")
+  SVN_ERRDEF (SVN_ERR_PLUGIN_LOAD_FAILURE,
+              "Failure loading plugin")
 
-  /* A working copy "descent" crawl came up empty */
-  SVN_ERRDEF(SVN_ERR_UNFRUITFUL_DESCENT, "WC descent came up empty")
+  SVN_ERRDEF (SVN_ERR_UNKNOWN_FS_ACTION,
+              "Unknown fs action")
 
-  /* A bogus filename was passed to a routine */
-  SVN_ERRDEF(SVN_ERR_BAD_FILENAME, "Bogus filename")
+  SVN_ERRDEF (SVN_ERR_UNEXPECTED_EOF,
+              "Unexpected end of file")
 
-  /* Trying to use an as-yet unsupported feature. */
-  SVN_ERRDEF(SVN_ERR_UNSUPPORTED_FEATURE, "Trying to use an unsupported feature")
+  SVN_ERRDEF (SVN_ERR_MALFORMED_FILE,
+              "Malformed file")
 
-  /* There's no such xml tag attribute */
-  SVN_ERRDEF(SVN_ERR_XML_ATTRIB_NOT_FOUND, "No such XML tag attribute")
+  SVN_ERRDEF (SVN_ERR_INCOMPLETE_DATA,
+              "Incomplete data")
 
-  /* A delta-pkg is missing ancestry. */
-  SVN_ERRDEF(SVN_ERR_XML_MISSING_ANCESTRY, "<delta-pkg> is missing ancestry")
+  SVN_ERRDEF (SVN_ERR_MALFORMED_XML,
+              "XML data was not well-formed")
 
-  /* A binary data encoding was specified which we don't know how to decode. */
-  SVN_ERRDEF(SVN_ERR_XML_UNKNOWN_ENCODING, "Unrecognized binary data encoding; can't decode")
+  SVN_ERRDEF (SVN_ERR_UNFRUITFUL_DESCENT,
+              "WC descent came up empty")
 
-  /* Not one of the valid kinds in svn_node_kind. */
-  SVN_ERRDEF(SVN_ERR_UNKNOWN_NODE_KIND, "Unknown svn_node_kind")
+  SVN_ERRDEF (SVN_ERR_BAD_FILENAME,
+              "Bogus filename")
 
-  /* Can't do this update or checkout, because something was in the way. */
-  SVN_ERRDEF(SVN_ERR_WC_OBSTRUCTED_UPDATE, "Obstructed update; unversioned item in the way")
+  SVN_ERRDEF (SVN_ERR_UNSUPPORTED_FEATURE,
+              "Trying to use an unsupported feature")
 
-  /* A mismatch popping the wc unwind stack. */
-  SVN_ERRDEF(SVN_ERR_WC_UNWIND_MISMATCH, "Mismatch popping the WC unwind stack")
+  SVN_ERRDEF (SVN_ERR_XML_ATTRIB_NOT_FOUND,
+              "No such XML tag attribute")
 
-  /* Trying to pop an empty unwind stack. */
-  SVN_ERRDEF(SVN_ERR_WC_UNWIND_EMPTY, "Attempt to pop empty WC unwind stack")
+  SVN_ERRDEF (SVN_ERR_XML_MISSING_ANCESTRY,
+              "<delta-pkg> is missing ancestry")
 
-  /* Trying to unlock when there's non-empty unwind stack. */
-  SVN_ERRDEF(SVN_ERR_WC_UNWIND_NOT_EMPTY, "Attempt to unlock with non-empty unwind stack")
+  SVN_ERRDEF (SVN_ERR_XML_UNKNOWN_ENCODING,
+              "Unrecognized binary data encoding; can't decode")
 
-  /* What happens if a non-blocking call to svn_wc__lock() encounters
-     another lock. */
-  SVN_ERRDEF(SVN_ERR_WC_LOCKED, "Attempted to lock an already-locked dir")
+  SVN_ERRDEF (SVN_ERR_UNKNOWN_NODE_KIND,
+              "Unknown svn_node_kind")
 
-  /* Something's wrong with the log file format. */
-  SVN_ERRDEF(SVN_ERR_WC_BAD_ADM_LOG, "Logfile is corrupted")
+  SVN_ERRDEF (SVN_ERR_WC_OBSTRUCTED_UPDATE,
+              "Obstructed update; unversioned item in the way")
 
-  /* Unable to find a file or dir in the working copy. */
-  SVN_ERRDEF(SVN_ERR_WC_PATH_NOT_FOUND, "Can't find a working copy path")
+  SVN_ERRDEF (SVN_ERR_WC_UNWIND_MISMATCH,
+              "Mismatch popping the WC unwind stack")
 
-  /* Unable to find an entry.  Not always a fatal error, by the way. */
-  SVN_ERRDEF(SVN_ERR_WC_ENTRY_NOT_FOUND, "Can't find an entry")
+  SVN_ERRDEF (SVN_ERR_WC_UNWIND_EMPTY,
+              "Attempt to pop empty WC unwind stack")
 
-  /* Entry already exists when adding a file. */
-  SVN_ERRDEF(SVN_ERR_WC_ENTRY_EXISTS, "Entry already exists")
+  SVN_ERRDEF (SVN_ERR_WC_UNWIND_NOT_EMPTY,
+              "Attempt to unlock with non-empty unwind stack")
 
-  /* Unable to get revision for an entry. */
-  SVN_ERRDEF(SVN_ERR_WC_ENTRY_MISSING_REVISION, "Entry has no revision")
+  SVN_ERRDEF (SVN_ERR_WC_LOCKED,
+              "Attempted to lock an already-locked dir")
 
-  /* Unable to get ancestry for an entry. */
-  SVN_ERRDEF(SVN_ERR_WC_ENTRY_MISSING_ANCESTRY, "Entry has no ancestor")
+  SVN_ERRDEF (SVN_ERR_WC_BAD_ADM_LOG,
+              "Logfile is corrupted")
 
-  /* Entry has some invalid attribute value. */
-  SVN_ERRDEF(SVN_ERR_WC_ENTRY_ATTRIBUTE_INVALID, "Entry has an invalid attribute")
+  SVN_ERRDEF (SVN_ERR_WC_PATH_NOT_FOUND,
+              "Can't find a working copy path")
 
-  /* Bogus attributes are trying to be merged into an entry */
-  SVN_ERRDEF(SVN_ERR_WC_ENTRY_BOGUS_MERGE, "Bogus entry attributes during entry merge")
+  SVN_ERRDEF (SVN_ERR_WC_ENTRY_NOT_FOUND,
+              "Can't find an entry")
 
-  /* Working copy is not up-to-date w.r.t. the repository. */
-  SVN_ERRDEF(SVN_ERR_WC_NOT_UP_TO_DATE, "Working copy os not up-to-date")
+  SVN_ERRDEF (SVN_ERR_WC_ENTRY_EXISTS,
+              "Entry already exists")
 
-  /* A recursive directory removal left locally modified files
-     behind. */
-  SVN_ERRDEF(SVN_ERR_WC_LEFT_LOCAL_MOD, "Left locally modified or unversioned files")
+  SVN_ERRDEF (SVN_ERR_WC_ENTRY_MISSING_REVISION,
+              "Entry has no revision")
 
-  /* No unique names available for tmp files. */
-  SVN_ERRDEF(SVN_ERR_IO_UNIQUE_NAMES_EXHAUSTED, "Ran out of unique names")
+  SVN_ERRDEF (SVN_ERR_WC_ENTRY_MISSING_ANCESTRY,
+              "Entry has no ancestor")
 
-  /* If a working copy conflict is found (say, during a commit) */
-  SVN_ERRDEF(SVN_ERR_WC_FOUND_CONFLICT, "Found a conflict in working copy")
+  SVN_ERRDEF (SVN_ERR_WC_ENTRY_ATTRIBUTE_INVALID,
+              "Entry has an invalid attribute")
 
-  /* The working copy is in an invalid state */
-  SVN_ERRDEF(SVN_ERR_WC_CORRUPT, "Working copy is corrupt")
+  SVN_ERRDEF (SVN_ERR_WC_ENTRY_BOGUS_MERGE,
+              "Bogus entry attributes during entry merge")
 
-  /* A general filesystem error.  */
-  SVN_ERRDEF(SVN_ERR_FS_GENERAL, "General filesystem error")
+  SVN_ERRDEF (SVN_ERR_WC_NOT_UP_TO_DATE,
+              "Working copy os not up-to-date")
 
-  /* An error occurred while trying to close a Subversion filesystem.
-     This status code is meant to be returned from APR pool cleanup
-     functions; since that interface doesn't allow us to provide more
-     detailed information, this is all you'll get.  */
-  SVN_ERRDEF(SVN_ERR_FS_CLEANUP, "Error closing filesystem")
+  SVN_ERRDEF (SVN_ERR_WC_LEFT_LOCAL_MOD,
+              "Left locally modified or unversioned files")
 
-  /* You called svn_fs_newfs or svn_fs_open, but the filesystem object
-     you provided already refers to some filesystem.  You should allocate
-     a fresh filesystem object with svn_fs_new, and use that instead.  */
-  SVN_ERRDEF(SVN_ERR_FS_ALREADY_OPEN, "Filesystem is already open")
+  SVN_ERRDEF (SVN_ERR_IO_UNIQUE_NAMES_EXHAUSTED,
+              "Ran out of unique names")
 
-  /* You tried to perform an operation on a filesystem object which
-     hasn't been opened on any actual database yet.  You need to call
-     `svn_fs_open_berkeley', `svn_fs_create_berkeley', or something
-     like that.  */
-  SVN_ERRDEF(SVN_ERR_FS_NOT_OPEN, "Filesystem is not open")
+  SVN_ERRDEF (SVN_ERR_WC_FOUND_CONFLICT,
+              "Found a conflict in working copy")
 
-  /* The filesystem has been corrupted.  The filesystem library found
-     improperly formed data in the database.  */
-  SVN_ERRDEF(SVN_ERR_FS_CORRUPT, "Filesystem is corrupt")
+  SVN_ERRDEF (SVN_ERR_WC_CORRUPT,
+              "Working copy is corrupt")
 
-  /* The name given is not a valid directory entry name, or filename.  */
-  SVN_ERRDEF(SVN_ERR_FS_PATH_SYNTAX, "Invalid filesystem path syntax")
+  SVN_ERRDEF (SVN_ERR_FS_GENERAL,
+              "General filesystem error")
 
-  /* The filesystem has no revision by the given number.  */
-  SVN_ERRDEF(SVN_ERR_FS_NO_SUCH_REVISION, "Invalid filesystem revision number")
+  SVN_ERRDEF (SVN_ERR_FS_CLEANUP,
+              "Error closing filesystem")
 
-  /* The filesystem has no transaction with the given name.  */
-  SVN_ERRDEF(SVN_ERR_FS_NO_SUCH_TRANSACTION, "Invalid filesystem transaction name")
+  SVN_ERRDEF (SVN_ERR_FS_ALREADY_OPEN,
+              "Filesystem is already open")
 
-  /* A particular entry was not found in a directory. */
-  SVN_ERRDEF(SVN_ERR_FS_NO_SUCH_ENTRY, "Filesystem directory has no such entry")
+  SVN_ERRDEF (SVN_ERR_FS_NOT_OPEN,
+              "Filesystem is not open")
 
-  /* A particular representation was not found. */
-  SVN_ERRDEF(SVN_ERR_FS_NO_SUCH_REPRESENTATION, "Filesystem has no such representation")
+  SVN_ERRDEF (SVN_ERR_FS_CORRUPT,
+              "Filesystem is corrupt")
 
-  /* A particular string was not found. */
-  SVN_ERRDEF(SVN_ERR_FS_NO_SUCH_STRING, "Filesystem has no such string")
+  SVN_ERRDEF (SVN_ERR_FS_PATH_SYNTAX,
+              "Invalid filesystem path syntax")
 
-  /* There is no file by the given name.  */
-  SVN_ERRDEF(SVN_ERR_FS_NOT_FOUND, "Filesystem has no such file")
+  SVN_ERRDEF (SVN_ERR_FS_NO_SUCH_REVISION,
+              "Invalid filesystem revision number")
 
-  /* The given node revision id does not exist.  */
-  SVN_ERRDEF(SVN_ERR_FS_ID_NOT_FOUND, "Filesystem has no such node-rev-id")
+  SVN_ERRDEF (SVN_ERR_FS_NO_SUCH_TRANSACTION,
+              "Invalid filesystem transaction name")
 
-  /* The given string does not represent a node or node revision id. */
-  SVN_ERRDEF(SVN_ERR_FS_NOT_ID, "String does not represent a node or node-rev-id")
+  SVN_ERRDEF (SVN_ERR_FS_NO_SUCH_ENTRY,
+              "Filesystem directory has no such entry")
 
-  /* The name given does not refer to a directory.  */
-  SVN_ERRDEF(SVN_ERR_FS_NOT_DIRECTORY, "Name does not refer to a filesystem directory")
+  SVN_ERRDEF (SVN_ERR_FS_NO_SUCH_REPRESENTATION,
+              "Filesystem has no such representation")
 
-  /* The name given does not refer to a file.  */
-  SVN_ERRDEF(SVN_ERR_FS_NOT_FILE, "Name does not refer to a filesystem file")
+  SVN_ERRDEF (SVN_ERR_FS_NO_SUCH_STRING,
+              "Filesystem has no such string")
 
-  /* The name given is not a single path component.  */
-  SVN_ERRDEF(SVN_ERR_FS_NOT_SINGLE_PATH_COMPONENT, "Name is not a single path component")
+  SVN_ERRDEF (SVN_ERR_FS_NOT_FOUND,
+              "Filesystem has no such file")
 
-  /* The caller attempted to change or delete a node which is not mutable.  */
-  SVN_ERRDEF(SVN_ERR_FS_NOT_MUTABLE, "Attempt to change immutable filesystem node")
+  SVN_ERRDEF (SVN_ERR_FS_ID_NOT_FOUND,
+              "Filesystem has no such node-rev-id")
 
-  /* You tried to create a new file in a filesystem revision, but the
-     file already exists. */
-  SVN_ERRDEF(SVN_ERR_FS_ALREADY_EXISTS, "File already exists in revision")
+  SVN_ERRDEF (SVN_ERR_FS_NOT_ID,
+              "String does not represent a node or node-rev-id")
 
-  /* Tried to remove a non-empty directory. */
-  SVN_ERRDEF(SVN_ERR_FS_DIR_NOT_EMPTY, "Attempt to remove non-empty filesystem directory")
+  SVN_ERRDEF (SVN_ERR_FS_NOT_DIRECTORY,
+              "Name does not refer to a filesystem directory")
 
-  /* You tried to remove the root directory of a filesystem revision,
-     or create another node named /.  */
-  SVN_ERRDEF(SVN_ERR_FS_ROOT_DIR, "Attempt to remove or recreate fs root dir")
+  SVN_ERRDEF (SVN_ERR_FS_NOT_FILE,
+              "Name does not refer to a filesystem file")
 
-  /* The root object given is not a transaction root.  */
-  SVN_ERRDEF(SVN_ERR_FS_NOT_TXN_ROOT, "Object is not a transaction root")
+  SVN_ERRDEF (SVN_ERR_FS_NOT_SINGLE_PATH_COMPONENT,
+              "Name is not a single path component")
 
-  /* The root object given is not a revision root.  */
-  SVN_ERRDEF(SVN_ERR_FS_NOT_REVISION_ROOT, "Object is not a revision root")
+  SVN_ERRDEF (SVN_ERR_FS_NOT_MUTABLE,
+              "Attempt to change immutable filesystem node")
 
-  /* The transaction could not be committed, because of a conflict with
-     a prior change.  */
-  SVN_ERRDEF(SVN_ERR_FS_CONFLICT, "Merge conflict during commit")
+  SVN_ERRDEF (SVN_ERR_FS_ALREADY_EXISTS,
+              "File already exists in revision")
 
-  /* This TXN's base root is not the same as that of the youngest
-     revision in the repository.  */
-  SVN_ERRDEF(SVN_ERR_TXN_OUT_OF_DATE, "Transaction is out of date")
+  SVN_ERRDEF (SVN_ERR_FS_DIR_NOT_EMPTY,
+              "Attempt to remove non-empty filesystem directory")
 
-  /* The error is a Berkeley DB error.  `src_err' is the Berkeley DB
-     error code, and `message' is an error message.  */
-  SVN_ERRDEF(SVN_ERR_BERKELEY_DB, "Berkeley DB error")
+  SVN_ERRDEF (SVN_ERR_FS_ROOT_DIR,
+              "Attempt to remove or recreate fs root dir")
+
+  SVN_ERRDEF (SVN_ERR_FS_NOT_TXN_ROOT,
+              "Object is not a transaction root")
+
+  SVN_ERRDEF (SVN_ERR_FS_NOT_REVISION_ROOT,
+              "Object is not a revision root")
+
+  SVN_ERRDEF (SVN_ERR_FS_CONFLICT,
+              "Merge conflict during commit")
+
+  SVN_ERRDEF (SVN_ERR_TXN_OUT_OF_DATE,
+              "Transaction is out of date")
+
+  SVN_ERRDEF (SVN_ERR_BERKELEY_DB,
+              "Berkeley DB error")
 
   /* ### need to come up with a mechanism for RA-specific errors */
 
-  /* a bad URL was passed to the repository access layer */
-  SVN_ERRDEF(SVN_ERR_RA_ILLEGAL_URL, "Bad URL passed to RA layer")
+  SVN_ERRDEF (SVN_ERR_RA_ILLEGAL_URL,
+              "Bad URL passed to RA layer")
 
   /* These RA errors are specific to ra_dav */
 
-    /* the repository access layer could not initialize the socket layer */
-    SVN_ERRDEF(SVN_ERR_RA_SOCK_INIT, "RA layer failed to init socket layer")
+    SVN_ERRDEF (SVN_ERR_RA_SOCK_INIT,
+                "RA layer failed to init socket layer")
 
-    /* the repository access layer could not lookup the hostname */
-    SVN_ERRDEF(SVN_ERR_RA_HOSTNAME_LOOKUP, "RA layer failed hostname lookup")
+    SVN_ERRDEF (SVN_ERR_RA_HOSTNAME_LOOKUP,
+                "RA layer failed hostname lookup")
 
-    /* could not create an HTTP request */
-    SVN_ERRDEF(SVN_ERR_RA_CREATING_REQUEST, "RA layer failed to create HTTP request")
+    SVN_ERRDEF (SVN_ERR_RA_CREATING_REQUEST,
+                "RA layer failed to create HTTP request")
 
-    /* a request to the server failed in some way */
-    SVN_ERRDEF(SVN_ERR_RA_REQUEST_FAILED, "RA layer's server request failed")
+    SVN_ERRDEF (SVN_ERR_RA_REQUEST_FAILED,
+                "RA layer's server request failed")
 
-    /* error making an Activity for the commit to the server */
-    SVN_ERRDEF(SVN_ERR_RA_MKACTIVITY_FAILED, "RA layer failed to make an activity for commit")
+    SVN_ERRDEF (SVN_ERR_RA_MKACTIVITY_FAILED,
+                "RA layer failed to make an activity for commit")
 
-    /* could not delete a resource on the server */
-    SVN_ERRDEF(SVN_ERR_RA_DELETE_FAILED, "RA layer failed to delete server resource")
+    SVN_ERRDEF (SVN_ERR_RA_DELETE_FAILED,
+                "RA layer failed to delete server resource")
 
   /* End of ra_dav errors */
 
   /* These RA errors are specific to ra_local */
   
-    /* the given URL does not seem to point to a versioned resource */
-    SVN_ERRDEF(SVN_ERR_RA_NOT_VERSIONED_RESOURCE, "URL is not a versioned resource")
+    SVN_ERRDEF (SVN_ERR_RA_NOT_VERSIONED_RESOURCE,
+                "URL is not a versioned resource")
 
-    /* the update reporter was given a bogus first path. */
-    SVN_ERRDEF(SVN_ERR_RA_BAD_REVISION_REPORT, "Bogus revision report")
+    SVN_ERRDEF (SVN_ERR_RA_BAD_REVISION_REPORT,
+                "Bogus revision report")
  
   /* End of ra_local errors */
 
-  /* an unsuitable container-pool was passed to svn_make_pool() */
-  SVN_ERRDEF(SVN_ERR_BAD_CONTAINING_POOL, "Bad parent pool passed to svn_make_pool()")
+  SVN_ERRDEF (SVN_ERR_BAD_CONTAINING_POOL,
+              "Bad parent pool passed to svn_make_pool()")
 
-  /* the server was misconfigured: a pathname to an SVN FS was not supplied */
-  SVN_ERRDEF(SVN_ERR_APMOD_MISSING_PATH_TO_FS, "Apache has no path to an SVN filesystem")
+  SVN_ERRDEF (SVN_ERR_APMOD_MISSING_PATH_TO_FS,
+              "Apache has no path to an SVN filesystem")
 
-  /* the specified URI refers to our namespace, but was malformed */
-  SVN_ERRDEF(SVN_ERR_APMOD_MALFORMED_URI, "Apache got a malformed URI")
+  SVN_ERRDEF (SVN_ERR_APMOD_MALFORMED_URI,
+              "Apache got a malformed URI")
 
-  /* A test in the test suite failed.  The test suite uses this
-     error internally.  */
-  SVN_ERRDEF(SVN_ERR_TEST_FAILED, "Test failed")
+  SVN_ERRDEF (SVN_ERR_TEST_FAILED,
+              "Test failed")
 
   /* BEGIN Client errors */
 
-  /* Generic arg parsing error */
-  SVN_ERRDEF(SVN_ERR_CL_ARG_PARSING_ERROR, "Client error in parsing arguments")
+  SVN_ERRDEF (SVN_ERR_CL_ARG_PARSING_ERROR,
+              "Client error in parsing arguments")
 
-  /* An operation was attempted on the administrative subdirectory
-     (i.e., user tried to import it, or update it, or...) */
-  SVN_ERRDEF(SVN_ERR_CL_ADM_DIR_RESERVED, "Attempted command in administrative dir")
+  SVN_ERRDEF (SVN_ERR_CL_ADM_DIR_RESERVED,
+              "Attempted command in administrative dir")
 
   /* END Client errors */
   
