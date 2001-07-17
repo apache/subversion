cd81ad766cedc6bbd49424aedf74d43ae8df0c65
Karl Fogel <kfogel@apache.org>
2001-07-17 22:55:25 +0000

    (svn_client_status): If no such entry, just return without attempting to fill in more status information.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@839728 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/status.c
+++ subversion/libsvn_client/status.c
@@ -64,45 +64,50 @@ svn_client_status (apr_hash_t **statushash,
 
   /* Get a URL out of the working copy. */
   SVN_ERR (svn_wc_entry (&entry, path, pool));
-  URL = entry->ancestor->data;
+
+  if (entry)
+    {
+      URL = entry->ancestor->data;
 
   /* Get the RA vtable that matches URL. */
-  SVN_ERR (svn_ra_init_ra_libs (&ra_baton, pool));
-  err = svn_ra_get_ra_library (&ra_lib, ra_baton, URL, pool);
+      SVN_ERR (svn_ra_init_ra_libs (&ra_baton, pool));
+      err = svn_ra_get_ra_library (&ra_lib, ra_baton, URL, pool);
 
   /* If we got a valid RA layer by looking up the URL, then proceed.
      If the URL was bogus, or just has no RA match, that's okay.
      Leave the repository revnum fields invalid.  (Perhaps the wc came
      from xml or something.) */
-  if (err && (err->apr_err != SVN_ERR_RA_ILLEGAL_URL))
-    return err;
+      if (err && (err->apr_err != SVN_ERR_RA_ILLEGAL_URL))
+        return err;
 
-  if (! err)
-    {
-      apr_hash_index_t *hi;
-      svn_revnum_t latest_revnum;
-
-      /* Open an RA session to URL, get latest revnum, close session. */
-      SVN_ERR (ra_lib->open (&session, svn_stringbuf_create (URL, pool), pool));
-      SVN_ERR (ra_lib->get_latest_revnum (session, &latest_revnum));
-      SVN_ERR (ra_lib->close (session));
-
-      /* Write the latest revnum into each status structure. */
-      for (hi = apr_hash_first (hash); hi; hi = apr_hash_next (hi))
+      if (! err)
         {
-          const void *key;
-          void *val;
-          apr_size_t klen;
-          svn_wc_status_t *status;
+          apr_hash_index_t *hi;
+          svn_revnum_t latest_revnum;
 
-          apr_hash_this (hi, &key, &klen, &val);
-          status = (svn_wc_status_t *) val;
-          status->repos_rev = latest_revnum;
-        } 
+          /* Open an RA session to URL, get latest revnum, close session. */
+          SVN_ERR (ra_lib->open (&session, svn_stringbuf_create (URL, pool),
+                                 pool));
+          SVN_ERR (ra_lib->get_latest_revnum (session, &latest_revnum));
+          SVN_ERR (ra_lib->close (session));
+
+          /* Write the latest revnum into each status structure. */
+          for (hi = apr_hash_first (hash); hi; hi = apr_hash_next (hi))
+            {
+              const void *key;
+              void *val;
+              apr_size_t klen;
+              svn_wc_status_t *status;
+
+              apr_hash_this (hi, &key, &klen, &val);
+              status = (svn_wc_status_t *) val;
+              status->repos_rev = latest_revnum;
+            } 
+        }
     }
-  
+
   *statushash = hash;
-  
+
   return SVN_NO_ERROR;
 }
 
