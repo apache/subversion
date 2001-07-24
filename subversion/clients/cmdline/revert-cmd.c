250370c5cbabdd88aa9a3004a0c0db6a6d63fd3b
C. Michael Pilato <cmpilato@apache.org>
2001-07-24 15:00:43 +0000

    Initial code for 'svn revert' subcommand.
    * clients/cmdline/main.c
    
      (svn_cl__cmd_table):  Added 'revert' subcommand.
    
    * clients/cmdline/cl.h
    
      (svn_cl__revert):  New function prototype.
    
    * include/svn_client.h
    
      (svn_client_revert): New function prototype.
    
    * clients/cmdline/revert-cmd.c
    * libsvn_client/revert.c
    
      New file(s).
    
    * libsvn_wc/adm_ops.c
    
      (svn_wc_revert): New function.
    
    * include/svn_wc.h
    
      (svn_wc_revert): New function prototype.
    
    * clients/cmdline/cleanup-cmd.c
    
      Drop the unnecessary #includes.
    
    * tests/clients/cmdline/basic-tests.py
    
      (basic_revert): New test function.
    
      (test_list): Add reference to new test basic_revert().
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@839772 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/clients/cmdline/revert-cmd.c
+++ subversion/clients/cmdline/revert-cmd.c
@@ -1,5 +1,5 @@
 /*
- * cleanup-cmd.c -- Subversion cleanup command
+ * revert-cmd.c -- Subversion revert command
  *
  * ====================================================================
  * Copyright (c) 2000-2001 CollabNet.  All rights reserved.
@@ -29,9 +29,9 @@
 /*** Code. ***/
 
 svn_error_t *
-svn_cl__cleanup (apr_getopt_t *os,
-                 svn_cl__opt_state_t *opt_state,
-                 apr_pool_t *pool)
+svn_cl__revert (apr_getopt_t *os,
+                svn_cl__opt_state_t *opt_state,
+                apr_pool_t *pool)
 {
   apr_array_header_t *targets;
   int i;
@@ -45,15 +45,15 @@ svn_cl__cleanup (apr_getopt_t *os,
     for (i = 0; i < targets->nelts; i++)
       {
         svn_stringbuf_t *target = ((svn_stringbuf_t **) (targets->elts))[i];
-
-        SVN_ERR (svn_client_cleanup (target, pool));
+        
+        SVN_ERR (svn_client_revert (target, pool));
       }
   else
     {
-      svn_cl__subcommand_help ("cleanup", pool);
+      svn_cl__subcommand_help ("revert", pool);
       return svn_error_create (SVN_ERR_CL_ARG_PARSING_ERROR, 0, 0, pool, "");
     }
-
+  
   return SVN_NO_ERROR;
 }
 
