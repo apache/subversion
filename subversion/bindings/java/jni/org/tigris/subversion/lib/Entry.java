4d8b509afb05c2f345976e1fc315851aaeaac89d
XelaRellum <dev-null@apache.org>
2001-08-09 17:24:41 +0000

    First checkin of the Java interface definitions and very basic classes. Caution: right now these files are only interfaces and they are not at the right place in the directory yet.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@839875 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subversion/bindings/java/jni/org/tigris/subversion/lib/Entry.java
@@ -0,0 +1,41 @@
+package org.tigris.subversion.lib;
+
+/**
+ * ====================================================================
+ * Copyright (c) 2000-2001 CollabNet.  All rights reserved.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at http://subversion.tigris.org/license-1.html.
+ * If newer versions of this license are posted there, you may use a
+ * newer version instead, at your option.
+ * ====================================================================
+ *
+ * @author Alexander.Mueller@littleblue.de
+ * @version 0.0
+ */
+
+public interface Entry {
+    public final static int SCHEDULE_NORMAL=0;
+    public final static int SCHEDULE_ADD=1;
+    public final static int SCHEDULE_DELETE=2;
+    public final static int SCHEDULE_REPLACE=3;
+    public final static int SCHEDULE_UNADD=4;
+    public final static int SCHEDULE_UNDELETE=5;
+
+    public final static int EXISTENCE_NORMAL=0;
+    public final static int EXISTENCE_ADDED=1;
+    public final static int EXISTENCE_DELETED=2;
+
+    public final static int NODEKIND_NONE = 0;
+    public final static int NODEKIND_FILE = 1;
+    public final static int NODEKIND_DIR = 2;
+    public final static int NODEKIND_UNKNOWN = 3;
+
+    public void setRevision(Revision revision);
+    public Revision getRevision();
+    public void setAncestor(String ancestor);
+    public String getAncestor();
+    public void setNodeKind(int nodeKind);
+    public int getNodeKind();
+}
---
+++ subversion/bindings/java/jni/org/tigris/subversion/lib/TextdeltaHandler.java
@@ -0,0 +1,25 @@
+package org.tigris.subversion.lib;
+
+/**
+ * ====================================================================
+ * Copyright (c) 2000-2001 CollabNet.  All rights reserved.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at http://subversion.tigris.org/license-1.html.
+ * If newer versions of this license are posted there, you may use a
+ * newer version instead, at your option.
+ * ====================================================================
+ *
+ * @author Alexander.Mueller@littleblue.de
+ * @version 0.0
+ *  Remark: this class corresponds to the subversion c api type
+ * 'svn_txdelta_window_handler_t'
+ */
+
+import org.tigris.subversion.SubversionException;
+
+public interface TextdeltaHandler {
+  public void handleTextdeltaWindow(TextdeltaWindow window, Object baton)
+    throws SubversionException;
+}
