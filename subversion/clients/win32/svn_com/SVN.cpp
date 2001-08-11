6a6734938c56567edb3ac23eef275169891d2faf
Kevin Meyer <kevin@apache.org>
2001-08-11 13:41:22 +0000

    Fix the win32 build.  This fixes a Microsoft Bug, involving duplicate definitions for InlineIsEqualGUID.  For more information see: http://support.microsoft.com/support/kb/articles/q243/2/98.asp
    Submitted by:	Cynic <cynic@mail.cz>
    Reviewed by:	Kevin Pilch-Bisson <kevin@pilch-bisson.net>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@839914 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/clients/win32/svn_com/SVN.cpp
+++ subversion/clients/win32/svn_com/SVN.cpp
@@ -31,7 +31,7 @@ CSVNWorkingCopy::InterfaceSupportsErrorInfo(REFIID riid)
 	};
 	for (int i=0; i < sizeof(arr) / sizeof(arr[0]); i++)
 	{
-		if (InlineIsEqualGUID(*arr[i],riid))
+		if (::ATL::InlineIsEqualGUID(*arr[i],riid))
 			return S_OK;
 	}
 	return S_FALSE;
