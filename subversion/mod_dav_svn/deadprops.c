1c07b7be9bb6af12ffb8f23b43e1191df94c38f4
Greg Stein <gstein@apache.org>
2001-08-14 03:51:45 +0000

    BIG HACK. Map the incoming names from mod_dav's current scheme (NAMESPACE-ID ":" NAME) into a "normal" string for use in the FS. Also take care of the property values that mod_dav expects: they begin with a language value and contain embedded NUL characters.
    This is a temporary solution. The final solution (in the next day,
    hopefully) will be a reworking of the interface between mod_dav and its
    backend modules (e.g. mod_dav_svn).
    
    (dav_db{}): add a working buffer for the various remapping
    
    (fix_name): new function to tweak a property name from the mod_dav style to
      the FS style.
    
    (dav_svn_db_open): prep the working buffer
    
    (dav_svn_db_fetch): if the "METADATA" pseudo-property is requested, then
      return a fake one. (we no longer store this property). remap the name via
      fix_name(). format the result value properly.
    
    (dav_svn_db_store): don't store the "METADATA" pseudo-property. remap the
      name. handle the value reformat.
    
    (dav_svn_db_remove, dav_svn_db_exists): remap the name.
    
    (get_key): pass in db to do the hack-work. remap the name from the FS style
      to the mod_dav style.
    
    (dav_svn_db_firstkey, dav_svn_db_nextkey): pass new param to get_key.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@839933 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/mod_dav_svn/deadprops.c
+++ subversion/mod_dav_svn/deadprops.c
@@ -29,7 +29,31 @@ struct dav_db {
   /* the resource's properties that we are sequencing over */
   apr_hash_t *props;
   apr_hash_index_t *hi;
+
+  /* ### part of hack on prop name changes/mapping */
+  svn_stringbuf_t *work;
 };
+
+/* ### temp hack for fixing prop names */
+static void fix_name(dav_db *db, svn_string_t *propname)
+{
+  /* all properties have no prefix, or they have an svn: prefix */
+  if (*propname->data == ':')
+    {
+      /* not in a namespace, so drop the ':' */
+      ++propname->data;
+      --propname->len;
+    }
+  else
+    {
+      const char *s = ap_strchr_c(propname->data, ':');
+
+      svn_stringbuf_set(db->work, "svn:");
+      svn_stringbuf_appendcstr(db->work, s + 1);
+      propname->data = db->work->data;
+      propname->len = db->work->len;
+    }
+}
 
 static dav_error *dav_svn_db_open(apr_pool_t *p, const dav_resource *resource,
                                   int ro, dav_db **pdb)
@@ -61,6 +85,9 @@ static dav_error *dav_svn_db_open(apr_pool_t *p, const dav_resource *resource,
   db->resource = resource;
   db->p = p;
 
+  /* ### temp hack */
+  db->work = svn_stringbuf_ncreate("", 0, p);
+
   /* ### use RO and node's mutable status to look for an error? */
 
   *pdb = db;
@@ -79,6 +106,19 @@ static dav_error *dav_svn_db_fetch(dav_db *db, dav_datum key,
   svn_string_t propname = { key.dptr, key.dsize };
   svn_stringbuf_t *propval;
   svn_error_t *serr;
+
+  if (strcmp(key.dptr, "METADATA") == 0)
+    {
+#define THE_METADATA "\004\000\000\002DAV:\0svn:" /* plus '\0' */
+      pvalue->dptr = (char *)THE_METADATA;
+      pvalue->dsize = sizeof(THE_METADATA);
+      return NULL;
+    }
+
+  /* ### temp hack to remap the name */
+  fix_name(db, &propname);
+
+  /* ### if db->props exists, then try in there first */
 
   /* Working Baseline, Baseline, or (Working) Version resource */
   if (db->resource->baselined)
@@ -107,6 +147,14 @@ static dav_error *dav_svn_db_fetch(dav_db *db, dav_datum key,
 
   pvalue->dptr = propval->data;
   pvalue->dsize = propval->len;
+
+  /* ### temp hack. fix the return value */
+  svn_stringbuf_setempty(db->work);
+  svn_stringbuf_appendbytes(db->work, "", 1);
+  svn_stringbuf_appendbytes(db->work, propval->data, propval->len);
+  pvalue->dptr = db->work->data;
+  pvalue->dsize = db->work->len + 1;    /* include null term */
+
   return NULL;
 }
 
@@ -117,6 +165,15 @@ static dav_error *dav_svn_db_store(dav_db *db, dav_datum key, dav_datum value)
   svn_error_t *serr;
 
   /* ### hope node is open, and it is mutable */
+
+  /* don't store this */
+  if (strcmp(key.dptr, "METADATA") == 0)
+    return NULL;
+
+  /* ### temp hack to remap the name. fix the value (skip lang; toss null). */
+  fix_name(db, &propname);
+  ++propval.data;
+  propval.len -= 2;
 
   /* Working Baseline or Working (Version) Resource */
   if (db->resource->baselined)
@@ -143,6 +200,9 @@ static dav_error *dav_svn_db_remove(dav_db *db, dav_datum key)
 
   /* ### hope node is open, and it is mutable */
 
+  /* ### temp hack to remap the name */
+  fix_name(db, &propname);
+
   /* Working Baseline or Working (Version) Resource */
   if (db->resource->baselined)
     serr = svn_fs_change_txn_prop(db->resource->info->root.txn,
@@ -167,6 +227,9 @@ static int dav_svn_db_exists(dav_db *db, dav_datum key)
   svn_stringbuf_t *propval;
   svn_error_t *serr;
 
+  /* ### temp hack to remap the name */
+  fix_name(db, &propname);
+
   /* Working Baseline, Baseline, or (Working) Version resource */
   if (db->resource->baselined)
     if (db->resource->type == DAV_RESOURCE_TYPE_WORKING)
@@ -186,7 +249,8 @@ static int dav_svn_db_exists(dav_db *db, dav_datum key)
   return serr == NULL && propval != NULL;
 }
 
-static void get_key(apr_hash_index_t *hi, dav_datum *pkey)
+static void get_key(apr_hash_index_t *hi, dav_datum *pkey,
+                    dav_db *db)
 {
   if (hi == NULL)
     {
@@ -201,6 +265,21 @@ static void get_key(apr_hash_index_t *hi, dav_datum *pkey)
       apr_hash_this(hi, &name, &namelen, NULL);
       pkey->dptr = (char *)name;        /* hope the caller doesn't change */
       pkey->dsize = namelen;
+
+      /* ### temp hack for name remapping. a prop with the svn: prefix will
+         ### get a namespace index of 1. others will get "no namespace" */
+      if (strncmp(name, "svn:", 4) == 0)
+        {
+          svn_stringbuf_set(db->work, "1:");
+          svn_stringbuf_appendcstr(db->work, (const char *)name + 4);
+        }
+      else
+        {
+          svn_stringbuf_set(db->work, ":");
+          svn_stringbuf_appendcstr(db->work, name);
+        }
+      pkey->dptr = db->work->data;
+      pkey->dsize = db->work->len + 1;  /* include null term */
     }
 }
 
@@ -233,7 +312,7 @@ static dav_error *dav_svn_db_firstkey(dav_db *db, dav_datum *pkey)
   db->hi = apr_hash_first(db->p, db->props);
 
   /* fetch the first key */
-  get_key(db->hi, pkey);
+  get_key(db->hi, pkey, db);
 
   return NULL;
 }
@@ -245,7 +324,7 @@ static dav_error *dav_svn_db_nextkey(dav_db *db, dav_datum *pkey)
     db->hi = apr_hash_next(db->hi);
 
   /* fetch the key */
-  get_key(db->hi, pkey);
+  get_key(db->hi, pkey, db);
 
   return NULL;
 }
