b3ad55d211076f94d918bdd75f44fb8141674292
C. Michael Pilato <cmpilato@apache.org>
2001-08-28 16:07:09 +0000

    * schedule_tests.py
      Changed syntax from `func ()' to `func()'.
    
      (nested_adds): Changed function description.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@840040 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/clients/cmdline/schedule_tests.py
+++ subversion/tests/clients/cmdline/schedule_tests.py
@@ -47,19 +47,19 @@ def add_files():
 
   # Bootstrap
   sbox = sandbox(add_files)
-  wc_dir = os.path.join (svntest.main.general_wc_dir, sbox)
+  wc_dir = os.path.join(svntest.main.general_wc_dir, sbox)
 
   if svntest.actions.make_repo_and_wc(sbox):
     return 1
 
   # Create some files, then schedule them for addition
-  delta_path = os.path.join (wc_dir, 'delta')
-  zeta_path = os.path.join (wc_dir, 'A', 'B', 'zeta')
-  epsilon_path = os.path.join (wc_dir, 'A', 'D', 'G', 'epsilon')
+  delta_path = os.path.join(wc_dir, 'delta')
+  zeta_path = os.path.join(wc_dir, 'A', 'B', 'zeta')
+  epsilon_path = os.path.join(wc_dir, 'A', 'D', 'G', 'epsilon')
   
-  svntest.main.file_append (delta_path, "This is the file 'delta'.")
-  svntest.main.file_append (zeta_path, "This is the file 'zeta'.")
-  svntest.main.file_append (epsilon_path, "This is the file 'epsilon'.")
+  svntest.main.file_append(delta_path, "This is the file 'delta'.")
+  svntest.main.file_append(zeta_path, "This is the file 'zeta'.")
+  svntest.main.file_append(epsilon_path, "This is the file 'epsilon'.")
   
   svntest.main.run_svn(None, 'add', delta_path, zeta_path, epsilon_path)
   
@@ -82,7 +82,7 @@ def add_files():
                        'repos_rev' : '1'}])
   expected_output_tree = svntest.tree.build_generic_tree(status_list)
 
-  return svntest.actions.run_and_verify_status (wc_dir, expected_output_tree)
+  return svntest.actions.run_and_verify_status(wc_dir, expected_output_tree)
 
 
 #----------------------------------------------------------------------
@@ -92,19 +92,19 @@ def add_directories():
 
   # Bootstrap
   sbox = sandbox(add_directories)
-  wc_dir = os.path.join (svntest.main.general_wc_dir, sbox)
+  wc_dir = os.path.join(svntest.main.general_wc_dir, sbox)
 
   if svntest.actions.make_repo_and_wc(sbox):
     return 1
 
   # Create some directories, then schedule them for addition
-  X_path = os.path.join (wc_dir, 'X')
-  Y_path = os.path.join (wc_dir, 'A', 'C', 'Y')
-  Z_path = os.path.join (wc_dir, 'A', 'D', 'H', 'Z')
+  X_path = os.path.join(wc_dir, 'X')
+  Y_path = os.path.join(wc_dir, 'A', 'C', 'Y')
+  Z_path = os.path.join(wc_dir, 'A', 'D', 'H', 'Z')
   
-  os.mkdir (X_path)
-  os.mkdir (Y_path)
-  os.mkdir (Z_path)
+  os.mkdir(X_path)
+  os.mkdir(Y_path)
+  os.mkdir(Z_path)
   
   svntest.main.run_svn(None, 'add', X_path, Y_path, Z_path)
   
@@ -127,49 +127,49 @@ def add_directories():
                        'repos_rev' : '1'}])
   expected_output_tree = svntest.tree.build_generic_tree(status_list)
 
-  return svntest.actions.run_and_verify_status (wc_dir, expected_output_tree)
+  return svntest.actions.run_and_verify_status(wc_dir, expected_output_tree)
 
 
 #----------------------------------------------------------------------
 
 def nested_adds():
-  "add some directories, and files and dirs in those directories"
+  "add some nested files and directories"
 
   # Bootstrap
   sbox = sandbox(nested_adds)
-  wc_dir = os.path.join (svntest.main.general_wc_dir, sbox)
+  wc_dir = os.path.join(svntest.main.general_wc_dir, sbox)
 
   if svntest.actions.make_repo_and_wc(sbox):
     return 1
 
   # Create some directories then schedule them for addition
-  X_path = os.path.join (wc_dir, 'X')
-  Y_path = os.path.join (wc_dir, 'A', 'C', 'Y')
-  Z_path = os.path.join (wc_dir, 'A', 'D', 'H', 'Z')
+  X_path = os.path.join(wc_dir, 'X')
+  Y_path = os.path.join(wc_dir, 'A', 'C', 'Y')
+  Z_path = os.path.join(wc_dir, 'A', 'D', 'H', 'Z')
 
-  os.mkdir (X_path)
-  os.mkdir (Y_path)
-  os.mkdir (Z_path)
+  os.mkdir(X_path)
+  os.mkdir(Y_path)
+  os.mkdir(Z_path)
 
   # Now, create some files and directories to put into our newly added
   # directories
-  P_path = os.path.join (X_path, 'P')
-  Q_path = os.path.join (Y_path, 'Q')
-  R_path = os.path.join (Z_path, 'R')
+  P_path = os.path.join(X_path, 'P')
+  Q_path = os.path.join(Y_path, 'Q')
+  R_path = os.path.join(Z_path, 'R')
 
-  os.mkdir (P_path)
-  os.mkdir (Q_path)
-  os.mkdir (R_path)
+  os.mkdir(P_path)
+  os.mkdir(Q_path)
+  os.mkdir(R_path)
   
-  delta_path = os.path.join (X_path, 'delta')
-  epsilon_path = os.path.join (Y_path, 'epsilon')
-  upsilon_path = os.path.join (Y_path, 'upsilon')
-  zeta_path = os.path.join (Z_path, 'zeta')
+  delta_path = os.path.join(X_path, 'delta')
+  epsilon_path = os.path.join(Y_path, 'epsilon')
+  upsilon_path = os.path.join(Y_path, 'upsilon')
+  zeta_path = os.path.join(Z_path, 'zeta')
 
-  svntest.main.file_append (delta_path, "This is the file 'delta'.")
-  svntest.main.file_append (epsilon_path, "This is the file 'epsilon'.")
-  svntest.main.file_append (upsilon_path, "This is the file 'upsilon'.")
-  svntest.main.file_append (zeta_path, "This is the file 'zeta'.")
+  svntest.main.file_append(delta_path, "This is the file 'delta'.")
+  svntest.main.file_append(epsilon_path, "This is the file 'epsilon'.")
+  svntest.main.file_append(upsilon_path, "This is the file 'upsilon'.")
+  svntest.main.file_append(zeta_path, "This is the file 'zeta'.")
 
   # Finally, let's try some recursive adds of our new files and directories
   svntest.main.run_svn(None, 'add', '--recursive', X_path, Y_path, Z_path)
@@ -228,7 +228,7 @@ def nested_adds():
                        'repos_rev' : '1'}])
   expected_output_tree = svntest.tree.build_generic_tree(status_list)
 
-  return svntest.actions.run_and_verify_status (wc_dir, expected_output_tree)
+  return svntest.actions.run_and_verify_status(wc_dir, expected_output_tree)
 
 
 #----------------------------------------------------------------------
@@ -238,16 +238,16 @@ def delete_files():
 
   # Bootstrap
   sbox = sandbox(delete_files)
-  wc_dir = os.path.join (svntest.main.general_wc_dir, sbox)
+  wc_dir = os.path.join(svntest.main.general_wc_dir, sbox)
 
   if svntest.actions.make_repo_and_wc(sbox):
     return 1
 
   # Schedule some files for deletion
-  iota_path = os.path.join (wc_dir, 'iota')
-  mu_path = os.path.join (wc_dir, 'A', 'mu')
-  rho_path = os.path.join (wc_dir, 'A', 'D', 'G', 'rho')
-  omega_path = os.path.join (wc_dir, 'A', 'D', 'H', 'omega')
+  iota_path = os.path.join(wc_dir, 'iota')
+  mu_path = os.path.join(wc_dir, 'A', 'mu')
+  rho_path = os.path.join(wc_dir, 'A', 'D', 'G', 'rho')
+  omega_path = os.path.join(wc_dir, 'A', 'D', 'H', 'omega')
   
   svntest.main.run_svn(None, 'del', iota_path, mu_path, rho_path, omega_path)
     
@@ -258,7 +258,7 @@ def delete_files():
       item[3]['status'] = 'D '
   expected_output_tree = svntest.tree.build_generic_tree(status_list)
 
-  return svntest.actions.run_and_verify_status (wc_dir, expected_output_tree)
+  return svntest.actions.run_and_verify_status(wc_dir, expected_output_tree)
 
 #----------------------------------------------------------------------
 
@@ -267,15 +267,15 @@ def delete_dirs():
 
   # Bootstrap
   sbox = sandbox(delete_dirs)
-  wc_dir = os.path.join (svntest.main.general_wc_dir, sbox)
+  wc_dir = os.path.join(svntest.main.general_wc_dir, sbox)
 
   if svntest.actions.make_repo_and_wc(sbox):
     return 1
 
   # Schedule some directories for deletion (this is recursive!)
-  E_path = os.path.join (wc_dir, 'A', 'B', 'E')
-  F_path = os.path.join (wc_dir, 'A', 'B', 'F')
-  H_path = os.path.join (wc_dir, 'A', 'D', 'H')
+  E_path = os.path.join(wc_dir, 'A', 'B', 'E')
+  F_path = os.path.join(wc_dir, 'A', 'B', 'F')
+  H_path = os.path.join(wc_dir, 'A', 'D', 'H')
 
   # Finally, let's try some recursive adds of our new files and directories
   svntest.main.run_svn(None, 'del', E_path, F_path, H_path)
@@ -284,17 +284,17 @@ def delete_dirs():
   status_list = svntest.actions.get_virginal_status_list(wc_dir, '1')
   for item in status_list:
     if item[0] == E_path \
-    or item[0] == os.path.join (E_path, 'alpha') \
-    or item[0] == os.path.join (E_path, 'beta') \
+    or item[0] == os.path.join(E_path, 'alpha') \
+    or item[0] == os.path.join(E_path, 'beta') \
     or item[0] == F_path \
     or item[0] == H_path \
-    or item[0] == os.path.join (H_path, 'chi') \
-    or item[0] == os.path.join (H_path, 'omega') \
-    or item[0] == os.path.join (H_path, 'psi'):
+    or item[0] == os.path.join(H_path, 'chi') \
+    or item[0] == os.path.join(H_path, 'omega') \
+    or item[0] == os.path.join(H_path, 'psi'):
       item[3]['status'] = 'D '
   expected_output_tree = svntest.tree.build_generic_tree(status_list)
 
-  return svntest.actions.run_and_verify_status (wc_dir, expected_output_tree)
+  return svntest.actions.run_and_verify_status(wc_dir, expected_output_tree)
 
 
 #----------------------------------------------------------------------
