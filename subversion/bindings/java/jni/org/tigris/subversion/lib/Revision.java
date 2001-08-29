fe791b7c4df92132061cb1d88cc426ef428ae5b2
Karl Fogel <kfogel@apache.org>
2001-08-29 21:57:29 +0000

    * tools/check-license.py: Change to reflect new license.
    * everything with a license: Updated with text acknowledging
    contributors and advising how to track contribution history.
    Suggested by C. Scott Ananian <cananian@lesser-magoo.lcs.mit.edu>.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@840050 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/java/jni/org/tigris/subversion/lib/Revision.java
+++ subversion/bindings/java/jni/org/tigris/subversion/lib/Revision.java
@@ -9,6 +9,10 @@ package org.tigris.subversion.lib;
  * are also available at http://subversion.tigris.org/license-1.html.
  * If newer versions of this license are posted there, you may use a
  * newer version instead, at your option.
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals.  For exact contribution history, see the revision
+ * history and logs, available at http://subversion.tigris.org/.
  * ====================================================================
  *
  */
