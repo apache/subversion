3f4fe476f6b834b365dee2a5ced48a0d76609cbb
C. Michael Pilato <cmpilato@apache.org>
2001-09-06 07:24:51 +0000

    Bug fixes for status when used with no network connection.
    * status.c 
    
      (get_youngest_from_ra): New helper function.
    
      (svn_client_status): Use new helper function (improved readability).
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@840132 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/status.c
+++ subversion/libsvn_client/status.c
@@ -35,6 +35,66 @@
 #include "svn_io.h"
 
 
+
+/*** Helper functions. ***/
+
+/* Open an RA session to URL to retrieve the YOUNGEST revision.  If
+   any errors occur, *YOUNGEST will NOT be assigned to.  Use PATH to
+   find authentication information, and use POOL for any necessary
+   allocations.  RA layer errors will be considered non-fatal, all
+   others will be passed up the caller.  */
+static svn_error_t *
+get_youngest_from_ra (svn_revnum_t *youngest,
+                      svn_stringbuf_t *path,
+                      const char *URL,
+                      apr_pool_t *pool)
+{ 
+  svn_error_t *err;
+  void *ra_baton, *session;
+  svn_ra_plugin_t *ra_lib;
+  svn_revnum_t latest_revnum;
+  svn_boolean_t close_session = FALSE, rev_known = FALSE;
+  svn_client_auth_t *auth_obj;
+
+  /* Get the RA vtable that matches URL. */
+  SVN_ERR (svn_ra_init_ra_libs (&ra_baton, pool));
+  err = svn_ra_get_ra_library (&ra_lib, ra_baton, URL, pool);
+
+  /* If we got a valid RA layer by looking up the URL, then proceed.
+     If the URL was bogus, or just has no RA match, that's okay.
+     We'll just not touch *YOUNGEST. */
+  if (err)
+    {
+      if (err->apr_err != SVN_ERR_RA_ILLEGAL_URL)
+        return err;
+      else
+        return SVN_NO_ERROR;
+    }
+ 
+  err = svn_client_authenticate (&session, ra_lib,
+                                 svn_stringbuf_create (URL, pool),
+                                 path, auth_obj, pool);
+  if (! err)
+    close_session = TRUE;
+
+  if (! err)
+    err = ra_lib->get_latest_revnum (session, &latest_revnum);
+
+  if (! err)
+    rev_known = TRUE;
+
+  if (close_session)
+    {
+      ra_lib->close (session);
+      if (auth_obj->storage_callback)
+        SVN_ERR (auth_obj->storage_callback (auth_obj->storage_baton));
+    }
+  
+  if (rev_known)
+    *youngest = latest_revnum;
+
+  return SVN_NO_ERROR;
+}
 
 
 /*** Public Interface. ***/
@@ -51,102 +111,66 @@ svn_client_status (apr_hash_t **statushash,
                    svn_client_auth_t *auth_obj,
                    apr_pool_t *pool)
 {
-  svn_error_t *err;
-  void *ra_baton, *session;
-  svn_ra_plugin_t *ra_lib;
   svn_wc_entry_t *entry;
   svn_stringbuf_t *parent = NULL;
-  const char *URL;
+  svn_revnum_t latest_revnum = SVN_INVALID_REVNUM;
   apr_hash_t *hash = apr_hash_make (pool);
+  apr_hash_index_t *hi;
 
   /* Ask the wc to give us a list of svn_wc_status_t structures. */
-  err = svn_wc_statuses (hash, path, descend, pool);
-  if (err) return err;
+  SVN_ERR (svn_wc_statuses (hash, path, descend, pool));
   
   /* Each status structure in the hash now has all fields filled in
-   *except* the repos_rev field, which is SVN_INVALID_REVNUM.
-   
-   Attempt to contact the repos and get the latest revnum. */
+     *except* the repos_rev field, which is SVN_INVALID_REVNUM.
+     Attempt to contact the repos and get the latest revnum. */
 
-  /* Get a URL out of the working copy. */
+  /* Get PATH's entry from the working copy. */
   SVN_ERR (svn_wc_entry (&entry, path, pool));
+  if (! entry)
+    return svn_error_createf 
+      (SVN_ERR_WC_ENTRY_NOT_FOUND, 0, NULL, pool,
+       "svn_client_status: '%s' is not a versioned resource", 
+       path->data);
 
-  if ((entry) && (entry->kind != svn_node_dir) && (! entry->ancestor))
+  /* If the entry is not a directory, we'll be looking at its parent
+     for authentication stuffs. */
+  if (entry->kind != svn_node_dir)
     {
-      /* If this entry has no ancestry and isn't a directory
-         (perhaps because it was just added) we'll get the
-         ancestry from its parent. */
       parent = svn_stringbuf_dup (path, pool);
       svn_path_remove_component (parent, svn_path_local_style);
-      SVN_ERR (svn_wc_entry (&entry, parent, pool));
     }
 
-  if ((entry) && (entry->ancestor))
+  /* If the entry has no ancestry, get the ancestry from its parent. */
+  if ((! entry->ancestor) && (parent))
     {
-      URL = entry->ancestor->data;
+      SVN_ERR (svn_wc_entry (&entry, parent, pool));
+      if (! entry)
+        return svn_error_createf 
+          (SVN_ERR_WC_ENTRY_NOT_FOUND, 0, NULL, pool,
+           "svn_client_status: parent '%s' is not a versioned resource", 
+           parent->data);
+    }
 
-      /* Get the RA vtable that matches URL. */
-      SVN_ERR (svn_ra_init_ra_libs (&ra_baton, pool));
-      err = svn_ra_get_ra_library (&ra_lib, ra_baton, URL, pool);
+  /* If we have a valid URL, open an RA session to it, get latest
+     revnum, close session. Don't throw network errors; just treat
+     them as non-fatal. */
+  if (entry->ancestor)
+    SVN_ERR (get_youngest_from_ra (&latest_revnum,
+                                   parent ? parent : path,
+                                   entry->ancestor->data,
+                                   pool));
 
-      /* If we got a valid RA layer by looking up the URL, then proceed.
-         If the URL was bogus, or just has no RA match, that's okay.
-         Leave the repository revnum fields invalid.  (Perhaps the wc came
-         from xml or something.) */
-      if (err && (err->apr_err != SVN_ERR_RA_ILLEGAL_URL))
-        return err;
-
-      if (! err)
-        {
-          svn_error_t *ra_err;
-          apr_hash_index_t *hi;
-          svn_revnum_t latest_revnum;
-          svn_boolean_t rev_unknown = FALSE;
-
-          /* Open an RA session to URL, get latest revnum, close
-             session. 
-
-             Don't throw network errors;  just treat them as
-             non-fatal. */
-
-          /* ### svn_client_authenticate only accepts directory paths */
-          if (!parent && entry->kind != svn_node_dir)
-            {
-              parent = svn_stringbuf_dup (path, pool);
-              svn_path_remove_component (parent, svn_path_local_style);
-            }
-          ra_err = svn_client_authenticate (&session, 
-                                            ra_lib,
-                                            svn_stringbuf_create (URL, pool),
-                                            (parent ? parent : path),
-                                            auth_obj, pool);
-          if (ra_err) rev_unknown = TRUE;
-            
-          ra_err = ra_lib->get_latest_revnum (session, &latest_revnum);
-          if (ra_err) rev_unknown = TRUE;
-
-          ra_err = ra_lib->close (session);
-          if (ra_err) rev_unknown = TRUE;
-
-          if (auth_obj->storage_callback)
-            SVN_ERR (auth_obj->storage_callback (auth_obj->storage_baton));
-
-          /* Write the latest revnum into each status structure. */
-          for (hi = apr_hash_first (pool, hash); hi; hi = apr_hash_next (hi))
-            {
-              const void *key;
-              void *val;
-              apr_size_t klen;
-              svn_wc_status_t *status;
-              
-              apr_hash_this (hi, &key, &klen, &val);
-              status = (svn_wc_status_t *) val;
-              if (rev_unknown)
-                status->repos_rev = (svn_revnum_t) SVN_INVALID_REVNUM;
-              else
-                status->repos_rev = latest_revnum;
-            } 
-        }
+  /* Write the latest revnum into each status structure. */
+  for (hi = apr_hash_first (pool, hash); hi; hi = apr_hash_next (hi))
+    {
+      const void *key;
+      void *val;
+      apr_size_t klen;
+      svn_wc_status_t *status;
+      
+      apr_hash_this (hi, &key, &klen, &val);
+      status = (svn_wc_status_t *) val;
+      status->repos_rev = latest_revnum;
     }
 
   *statushash = hash;
