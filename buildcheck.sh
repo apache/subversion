522d47645036e340b1acfd5f38e64a55dfd6fbf7
Greg Stein <gstein@apache.org>
2001-10-29 19:57:58 +0000

    Add the check for Neon earlier in the process. We keep the configure one for uesrs.
    buildcheck.sh: move NEON_WANTED here and do a simple check. We assume
      developers (who are the only people to run this script) know how to
      handle a failure.
    
    configure.in: grab NEON_WANTED from buildcheck.sh
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@840417 13f79535-47bb-0310-9956-ffa450edef68

--- buildcheck.sh
+++ buildcheck.sh
@@ -5,27 +5,27 @@ echo "buildcheck: checking installation..."
 # autoconf 2.50 or newer
 ac_version=`autoconf --version 2>/dev/null|head -1|sed -e 's/^[^0-9]*//' -e 's/[a-z]* *$//'`
 if test -z "$ac_version"; then
-echo "buildcheck: autoconf not found."
-echo "            You need autoconf version 2.50 or newer installed"
-echo "            to build Apache from CVS."
-exit 1
+  echo "buildcheck: autoconf not found."
+  echo "            You need autoconf version 2.50 or newer installed"
+  echo "            to build Apache from CVS."
+  exit 1
 fi
 IFS=.; set $ac_version; IFS=' '
 if test "$1" = "2" -a "$2" -lt "50" || test "$1" -lt "2"; then
-echo "buildcheck: autoconf version $ac_version found."
-echo "            You need autoconf version 2.50 or newer installed."
-exit 1
-else
-echo "buildcheck: autoconf version $ac_version (ok)"
+  echo "buildcheck: autoconf version $ac_version found."
+  echo "            You need autoconf version 2.50 or newer installed."
+  exit 1
 fi
+
+echo "buildcheck: autoconf version $ac_version (ok)"
 
 # libtool 1.4 or newer
 libtool=$(which glibtool libtool 2>/dev/null | head -1)
 lt_pversion=`$libtool --version 2>/dev/null|sed -e 's/^[^0-9]*//' -e 's/[- ].*//'`
 if test -z "$lt_pversion"; then
-echo "buildcheck: libtool not found."
-echo "            You need libtool version 1.4 or newer installed"
-exit 1
+  echo "buildcheck: libtool not found."
+  echo "            You need libtool version 1.4 or newer installed"
+  exit 1
 fi
 lt_version=`echo $lt_pversion|sed -e 's/\([a-z]*\)$/.\1/'`
 IFS=.; set $lt_version; IFS=' '
@@ -35,12 +35,25 @@ if test "$1" = "1"; then
       lt_status="bad"
    fi
 fi
-if test $lt_status = "good"; then
-   echo "buildcheck: libtool version $lt_pversion (ok)"
-   exit 0
+if test $lt_status != "good"; then
+  echo "buildcheck: libtool version $lt_pversion found."
+  echo "            You need libtool version 1.4 or newer installed"
+  exit 1
 fi
 
-echo "buildcheck: libtool version $lt_pversion found."
-echo "            You need libtool version 1.4 or newer installed"
+echo "buildcheck: libtool version $lt_pversion (ok)"
 
-exit 1
+# check for the correct version of Neon
+NEON_WANTED=0.17.2
+if test -d ./neon; then
+  NEON_VERSION="`./ac-helpers/get-neon-ver.sh neon`"
+  if test "$NEON_WANTED" != "$NEON_VERSION"; then
+    echo "buildcheck: neon version $NEON_VERSION found in ./neon/."
+    echo "            You need neon $NEON_WANTED."
+    exit 1
+  fi
+
+  echo "buildcheck: neon version $NEON_VERSION (ok)"
+fi
+
+exit 0
