86157ac0895fb05943b8f4aef6370553947ca345
daniel <daniel@unknown>
2001-11-04 17:17:39 +0000

    * ./ac-helpers/gnu-diff.sh
     Akinori Musha's fixes. They don't just find 'gdiff' or 'diff', they look a
     lot cleaner as well!
    
    * ./ac-helpers/gnu-patch.sh
    
     Akinori Musha's fixes in the same manner as above. It now tries 'gpatch' and
     'patch' to find a GNU compatible patch program.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@840467 13f79535-47bb-0310-9956-ffa450edef68

--- ac-helpers/gnu-patch.sh
+++ ac-helpers/gnu-patch.sh
@@ -4,34 +4,20 @@
 # Print the full path to this program, else print "".
 #
 
-gnu_patch_path=""
-pathlist=$PATH
-final="no"
+IFS=':'
 
-# Could this loop *be* any uglier?  
-# I can't believe it's the 21st century, and I'm using bourne.
-
-while test "$final" != "";  do
-    searchdir=`echo $pathlist | sed -e 's/:.*$//'`   # barf.
-    final=`echo $pathlist | grep :`
-    pathlist=`echo $pathlist | sed -e 's/^[^:]*://'` # where's my (cdr)?
-
-    # does $searchdir contain an executable called `patch'?
-    if test -f ${searchdir}/patch -o -h ${searchdir}/patch; then
-        if test -x ${searchdir}/patch; then
-            # run `patch --version`
-            output=`${searchdir}/patch --version 2>/dev/null`
-            if test $? != 0; then
-                # `patch --version` returned error; keep looking.
-                continue
-            fi
-            # else, look for "GNU" in the output.
-            if test "`echo $output | grep GNU`" != "";  then
-                gnu_patch_path="${searchdir}/patch"
-                break
-            fi
-        fi
-    fi
+for searchdir in $PATH; do
+    # does $searchdir contain an executable called either `gpatch' or `patch'?
+    for name in gpatch patch; do
+	patch=$searchdir/$name
+	if test -x $patch; then
+	    # run `patch --version`
+	    if $patch --version 2>&1 | grep -qw GNU; then
+		echo $patch
+		exit
+	    fi
+	fi
+    done
 done
 
-echo $gnu_patch_path
+echo ""
