6ad2df604775c1476e974437097eb447fb443320
Karl Fogel <kfogel@apache.org>
2001-11-28 18:37:47 +0000

    Skeleton for new RA `get_file' function:
    * subversion/include/svn_ra.h (svn_ra_plugin_t): New func `get_file'.
    
    * subversion/libsvn_ra_dav/session.c (dav_plugin): Add
    svn_ra_dav__get_file.
    
    * subversion/libsvn_ra_dav/ra_dav.h (svn_ra_dav__get_file): New
    prototype.
    
    * subversion/libsvn_ra_local/ra_plugin.c (get_file): New function.
    (ra_local_plugin): Add get_file.
    
    * subversion/libsvn_ra_dav/fetch.c (svn_ra_dav__get_file): New
    function skeleton.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@840627 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_ra_dav/session.c
+++ subversion/libsvn_ra_dav/session.c
@@ -227,6 +227,7 @@ static const svn_ra_plugin_t dav_plugin = {
   svn_ra_dav__get_latest_revnum,
   svn_ra_dav__get_dated_revision,
   svn_ra_dav__get_commit_editor,
+  svn_ra_dav__get_file,
   svn_ra_dav__do_checkout,
   svn_ra_dav__do_update,
   svn_ra_dav__do_status,
