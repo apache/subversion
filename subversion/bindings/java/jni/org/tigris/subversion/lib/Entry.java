6213ac4219772cd96bdf253582fd5b3a961eedac
xela <xela@unknown>
2001-11-28 08:42:41 +0000

    now Entry.java reflects svn_wc_entry_t
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@840618 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/java/jni/org/tigris/subversion/lib/Entry.java
+++ subversion/bindings/java/jni/org/tigris/subversion/lib/Entry.java
@@ -16,6 +16,9 @@ package org.tigris.subversion.lib;
  * ====================================================================
  **/
 
+import java.util.Date;
+import java.util.Hashtable;
+
 public class Entry {
     public final static int SCHEDULE_NORMAL=0;
     public final static int SCHEDULE_ADD=1;
@@ -33,20 +36,27 @@ public class Entry {
     public final static int NODEKIND_DIR = 2;
     public final static int NODEKIND_UNKNOWN = 3;
 
+    public final long revision;
     public final String url;
-    public final int revision;
     public final int nodeKind;
     public final int schedule;
     public final int existence;
+    public final Date text_time;
+    public final Date prop_time;
+    public final Hashtable attributes;
 
-    public Entry(String url, int revision, 
-		 int nodeKind, int schedule, int existence)
+    public Entry( long _revision, String _url, int _nodeKind, 
+                  int _schedule, int _existence, Date _text_time, 
+                  Date _prop_time, Hashtable _attributes )
 	{
-	    this.url = url;
-	    this.revision = revision;
-	    this.nodeKind = nodeKind;
-	    this.schedule = schedule;
-	    this.existence = existence;
+	    url = _url;
+	    revision = _revision;
+	    nodeKind = _nodeKind;
+	    schedule = _schedule;
+	    existence = _existence;
+            text_time = _text_time;
+            prop_time = _prop_time;
+            attributes = _attributes;
 	}
 }
 
