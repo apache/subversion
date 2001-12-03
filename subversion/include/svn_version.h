122d8529e86d538e3f9583ccce5c65278f3bec8a
C. Michael Pilato <cmpilato@apache.org>
2001-12-03 16:59:07 +0000

    * subversion/include/svn_version.h
      (SVN_VER_MINOR): Upped the minor revision to 7!
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@840661 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_version.h
+++ subversion/include/svn_version.h
@@ -37,7 +37,7 @@ extern "C" {
 /* Minor version number.
    Modify when new functionality is added or new interfaces are
    defined, but all changes are backward compatible. */
-#define SVN_VER_MINOR      6
+#define SVN_VER_MINOR      7
 
 /* Patch number.
    Modify for every released patch. */
