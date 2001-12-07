b87f2e67f747b065dee0a6074489f3104c458254
xela <dev-null@apache.org>
2001-12-07 22:09:14 +0000

    Extended the java utility functions. Now there are functions that implement set accessor function for several datatypes.
    Function prefix for set accessor function:
    "j__set_"
    
    Right now we do have these functions:
    * j__set_boolean
    * j__set_int
    * j__set_long
    * j__set_object
    
    Affected files:
    * j.h
    * j.c
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@840697 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/java/jni/j.c
+++ subversion/bindings/java/jni/j.c
@@ -16,7 +16,11 @@
  * ====================================================================
  */
 
+/*** Includes ***/
 #include <jni.h>
+#include "global.h"
+
+/*** Code ***/
 
 jclass
 j__get_class(JNIEnv *env, jboolean *hasException, 
@@ -24,6 +28,10 @@ j__get_class(JNIEnv *env, jboolean *hasException,
 {
   jclass result = NULL;
   jboolean _hasException = JNI_FALSE;
+
+#ifdef SVN_JNI__VERBOSE
+  fprintf(stderr,"j__get_class(classname=%s)\n", className);
+#endif
 
   result = (*env)->FindClass(env, className);
   if( result == NULL )
@@ -47,8 +55,26 @@ j__get_method(JNIEnv *env, jboolean *hasException,
   jmethodID result = NULL;
   jboolean _hasException = JNI_FALSE;
 
+#ifdef SVN_JNI__VERBOSE
+  fprintf(stderr, "j__get_method(" 
+          "methodName='%s';"
+          "methodSignature='%s';",
+          methodName, methodSignature);
+  if( class == NULL )
+    {
+      fprintf(stderr, "class=NULL;");
+    }
+#endif
+
   result = (*env)->GetMethodID(env, class, methodName, 
                                methodSignature);
+#ifdef SVN_JNI__VERBOSE
+  if( result == NULL )
+    {
+      fprintf(stderr, "hasException;");
+    }
+  fprintf(stderr, ")\n");
+#endif
 
   if( result == NULL )
     {
@@ -63,9 +89,245 @@ j__get_method(JNIEnv *env, jboolean *hasException,
   return result;
 }
 
+void j__set_int(JNIEnv *env, jboolean *hasException,
+                char *className, char *methodName,
+                jobject obj, jint value)
+{
+  jclass clazz = NULL;
+  jmethodID methodID = NULL;
+  jboolean _hasException = JNI_FALSE;
+
+#ifdef SVN_JNI__VERBOSE
+  fprintf(stderr, "\nentry__set_int("
+          "className='%s'; methodName='%s'; value=%d)\n{\n",
+          className, methodName, value);
+#endif
+
+  /*
+   * needed references:
+   * - class
+   * - method
+   * = 2 
+   */
+  if( (*env)->PushLocalFrame(env, 2) < 0 )
+    {
+      _hasException = JNI_TRUE;
+    }
+  else
+    {
+      clazz = j__get_class(env, &_hasException, className);
+
+      if( !_hasException )
+        {
+          methodID = j__get_method(env, &_hasException,
+                                   clazz, methodName, "(I)V");
+        }
+
+      if( !_hasException )
+        {
+          (*env)->CallVoidMethod(env, obj, methodID, value);
+
+          _hasException = (*env)->ExceptionCheck(env);
+        }
+
+      (*env)->PopLocalFrame(env, NULL);
+    }
+
+#ifdef SVN_JNI__VERBOSE
+  if( _hasException )
+    {
+      fprintf(stderr, "hasException;");
+    }
+  fprintf(stderr, "\n}\n");
+#endif
+
+  if( hasException != NULL )
+    {
+      *hasException = _hasException;
+    }
+}
+
+void j__set_long(JNIEnv *env, jboolean *hasException,
+                 char *className, char *methodName,
+                 jobject obj, jlong value)
+{
+  jclass clazz = NULL;
+  jmethodID methodID = NULL;
+  jboolean _hasException = JNI_FALSE;
+
+#ifdef SVN_JNI__VERBOSE
+  fprintf(stderr, "\nentry__set_long("
+          "className='%s'; methodName='%s'; value=%d)\n{\n",
+          className, methodName, value);
+#endif
+
+  /*
+   * needed references:
+   * - class
+   * - method
+   * = 2 
+   */
+  if( (*env)->PushLocalFrame(env, 2) < 0 )
+    {
+      _hasException = JNI_TRUE;
+    }
+  else
+    {
+      clazz = j__get_class(env, &_hasException, className);
+
+      if( !_hasException )
+        {
+          methodID = j__get_method(env, &_hasException,
+                                   clazz, methodName, "(J)V");
+        }
+
+      if( !_hasException )
+        {
+          (*env)->CallVoidMethod(env, obj, methodID, value);
+
+          _hasException = (*env)->ExceptionCheck(env);
+        }
+
+      (*env)->PopLocalFrame(env, NULL);
+    }
+
+#ifdef SVN_JNI__VERBOSE
+  if( _hasException )
+    {
+      fprintf(stderr, "hasException;");
+    }
+  fprintf(stderr, "\n}\n");
+#endif
+
+  if( hasException != NULL )
+    {
+      *hasException = _hasException;
+    }
+}
+
+void j__set_boolean(JNIEnv *env, jboolean *hasException,
+                    char *className, char *methodName,
+                    jobject obj, jboolean value)
+{
+  jclass clazz = NULL;
+  jmethodID methodID = NULL;
+  jboolean _hasException = JNI_FALSE;
+
+#ifdef SVN_JNI__VERBOSE
+  fprintf(stderr, "\nentry__set_boolean("
+          "className='%s'; methodName='%s'; value=%d)\n{\n",
+          className, methodName, value);
+#endif
+
+  /*
+   * needed references:
+   * - class
+   * - method
+   * = 2 
+   */
+  if( (*env)->PushLocalFrame(env, 2) < 0 )
+    {
+      _hasException = JNI_TRUE;
+    }
+  else
+    {
+      clazz = j__get_class(env, &_hasException, className);
+
+      if( !_hasException )
+        {
+          methodID = j__get_method(env, &_hasException,
+                                   clazz, methodName, "(Z)V");
+        }
+
+      if( !_hasException )
+        {
+          (*env)->CallVoidMethod(env, obj, methodID, value);
+
+          _hasException = (*env)->ExceptionCheck(env);
+        }
+
+      (*env)->PopLocalFrame(env, NULL);
+    }
+
+#ifdef SVN_JNI__VERBOSE
+  if( _hasException )
+    {
+      fprintf(stderr, "hasException;");
+    }
+  fprintf(stderr, "\n}\n");
+#endif
+
+  if( hasException != NULL )
+    {
+      *hasException = _hasException;
+    }
+}
+
+void j__set_object(JNIEnv *env, jboolean *hasException,
+                   char *className, char *methodName,
+                   char *methodSig,
+                   jobject obj, jobject value)
+{
+  jclass clazz = NULL;
+  jmethodID methodID = NULL;
+  jboolean _hasException = JNI_FALSE;
+
+#ifdef SVN_JNI__VERBOSE
+  fprintf(stderr, "\nentry__set_object("
+          "className='%s'; methodName='%s'; "
+          "methodSig=%s;"
+          ")\n{\n",
+          className, methodName, methodSig);
+#endif
+
+  /*
+   * needed references:
+   * - class
+   * - method
+   * = 2 
+   */
+  if( (*env)->PushLocalFrame(env, 2) < 0 )
+    {
+      _hasException = JNI_TRUE;
+    }
+  else
+    {
+      clazz = j__get_class(env, &_hasException, className);
+
+      if( !_hasException )
+        {
+          methodID = j__get_method(env, &_hasException,
+                                   clazz, methodName, methodSig);
+        }
+
+      if( !_hasException )
+        {
+          (*env)->CallVoidMethod(env, obj, methodID, value);
+
+          _hasException = (*env)->ExceptionCheck(env);
+        }
+
+      (*env)->PopLocalFrame(env, NULL);
+    }
+
+#ifdef SVN_JNI__VERBOSE
+  if( _hasException )
+    {
+      fprintf(stderr, "hasException;");
+    }
+  fprintf(stderr, "\n}\n");
+#endif
+
+  if( hasException != NULL )
+    {
+      *hasException = _hasException;
+    }
+}
+
 /* 
  * local variables:
  * eval: (load-file "../../../svn-dev.el")
  * end: 
  */
+
 
