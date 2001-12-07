3831ed0a2873fbaf69d601bf9452a52af1deed64
xela <xela@unknown>
2001-12-07 22:03:01 +0000

    Changed the interface of the following classes.
    A lot of people complained about the public
    accessible variables instead of having accessor
    functions.
    
    So now there are accessor functions for
    every single attribute. Also, there are no
    complex constructors anymore, that initialize
    the class at once. Maybe this feature will come
    back later...
    
    *subversion/bindings/java/jni/org/tigris/subversion/lib/Entry.java
    *subversion/bindings/java/jni/org/tigris/subversion/lib/Status.java
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@840695 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/java/jni/org/tigris/subversion/lib/Status.java
+++ subversion/bindings/java/jni/org/tigris/subversion/lib/Status.java
@@ -19,42 +19,112 @@ package org.tigris.subversion.lib;
 
 public class Status 
 {
-    public final Entry entry;
-    public final Revision repos_rev;
-    public final StatusKind text_status;
-    public final StatusKind prop_status;
-    public final boolean locked;
-    public final StatusKind repos_text_status;
-    public final StatusKind repos_prop_status;
+    private Entry entry = null;
+    private Revision reposRev = null;
+    private StatusKind textStatus = null;
+    private StatusKind propStatus = null;
+    private boolean locked = false;
+    private StatusKind reposTextStatus = null;
+    private StatusKind reposPropStatus = null;
 
-    public Status(Entry entry, Revision repos_rev,
-		  StatusKind text_status,
-		  StatusKind prop_status,
-		  boolean locked,
-		  StatusKind repos_text_status,
-		  StatusKind repos_prop_status)
-	{
-	    this.entry = entry;
-	    this.repos_rev = repos_rev;
-	    this.text_status = text_status;
-	    this.prop_status = prop_status;
-	    this.locked = locked;
-	    this.repos_text_status = repos_text_status;
-	    this.repos_prop_status = repos_prop_status;
-	}
-    public Status(Entry entry, int repos_rev,
-		  int text_status, int prop_status,
-		  boolean locked, 
-		  int repos_text_status,
-		  int repos_prop_status)
-	{
-	    this(entry, new Revision(repos_rev),
-		   new StatusKind(text_status),
-		   new StatusKind(prop_status),
-		   locked,
-		   new StatusKind(repos_text_status),
-		   new StatusKind(repos_prop_status));
-	}
+    public Status()
+        {
+        }
+
+    public void setEntry(Entry _entry)
+        {
+            entry = _entry;
+        }
+    
+    public Entry getEntry()
+        {
+            return entry;
+        }
+
+    public void setReposRev(Revision _reposRev)
+        {
+            reposRev = _reposRev;
+        }
+
+    public void setReposRev(long _reposRev)
+        {
+            reposRev = new Revision(_reposRev);
+        }
+
+    public Revision getReposRev()
+        {
+            return reposRev;
+        }
+
+    public void setTextStatus(StatusKind _textStatus)
+        {
+            textStatus = _textStatus;
+        }
+
+    public void setTextStatus(int _textStatus)
+        {
+            textStatus = new StatusKind(_textStatus);
+        }
+
+    public StatusKind getTextStatus()
+        {
+            return textStatus;
+        }
+
+    public void setPropStatus(StatusKind _propStatus)
+        {
+            propStatus = _propStatus;
+        }
+
+    public void setPropStatus(int _propStatus)
+        {
+            propStatus = new StatusKind(_propStatus);
+        }
+
+    public StatusKind getPropStatus()
+        {
+            return propStatus;
+        }
+
+    public void setLocked(boolean _locked)
+        {
+            locked = _locked;
+        }
+
+    public boolean getLocked()
+        {
+            return locked;
+        }
+
+    public void setReposTextStatus(StatusKind _reposTextStatus)
+        {
+            reposTextStatus = _reposTextStatus;
+        }
+
+    public void setReposTextStatus(int _reposTextStatus)
+        {
+            reposTextStatus = new StatusKind(_reposTextStatus);
+        }
+
+    public StatusKind getReposTextStatus()
+        {
+            return reposTextStatus;
+        }
+
+    public void setReposPropStatus(StatusKind _reposPropStatus)
+        {
+            reposPropStatus = _reposPropStatus;
+        }
+
+    public void setReposPropStatus(int _reposPropStatus)
+        {
+            reposPropStatus = new StatusKind(_reposPropStatus);
+        }
+
+    public StatusKind getReposPropStatus()
+        {
+            return reposPropStatus;
+        }
 }
 
 /* 
