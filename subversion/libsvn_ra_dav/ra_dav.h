e30df8a27feeb05e5696ec7e5da1f5f81955f69f
joe <joe@unknown>
2001-12-11 23:09:30 +0000

    * libsvn_ra_dav/util.c (svn_ra_dav__convert_error): New function, converts a neon error into a svn_error_t *.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@840708 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_ra_dav/ra_dav.h
+++ subversion/libsvn_ra_dav/ra_dav.h
@@ -351,6 +351,14 @@ svn_error_t *
 svn_ra_dav__maybe_store_auth_info (svn_ra_session_t *ras);
 
 
+/* Create an error object for an error from neon in the given session,
+   where the return code from neon was RETCODE, and CONTEXT describes
+   what was being attempted. */
+svn_error_t *svn_ra_dav__convert_error(ne_session *sess,
+                                       const char *context,
+                                       int retcode,
+                                       apr_pool_t *pool);
+
 #endif  /* RA_DAV_H */
 
 
