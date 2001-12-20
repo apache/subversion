ad874f8da9da02cf33ad8a271e3cc86ad029b38f
xela <xela@unknown>
2001-12-20 20:51:15 +0000

    Removed item.c/item.h because they were obsolete.
    * build.sh (OBJS): removed item.c
    * main.c (#include): removed item.h
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@840773 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/java/jni/build.sh
+++ subversion/bindings/java/jni/build.sh
@@ -15,6 +15,6 @@ LIBS="-lapr -ldl -lmm -lcrypt -lpthread -lsvn_subr -lneon -lsvn_delta -lexpat -l
 INCLUDE="-I$JAVA_HOME/include -I$JAVA_HOME/include/linux -I../../../include"
 CC_ARGS="$INCLUDE $LIBS -pthread -shared"
 NATIVE_CLASS_NAME="svn_jni"
-OBJS="main.c date.c misc.c status.c item.c hashtable.c string.c j.c entry.c clientimpl_status.c"
+OBJS="main.c date.c misc.c status.c hashtable.c string.c j.c entry.c clientimpl_status.c"
 CC_ARGS="$CC_ARGS -o lib$NATIVE_CLASS_NAME.so $OBJS"
 cc $CC_ARGS
