45ca2b2404afb5c8765d07669fabca05ba7e7eb7
Greg Stein <gstein@apache.org>
2002-01-11 07:59:08 +0000

    Moved some functions from auth.c to ra.c. NO CHANGES were made to the functions during the move.
    Note: this breaks the compilation since get_authenticator is not defined
    in ra.c.  Will fix in the next commit.
    
    * ra.c (svn_client__get_ra_callbacks, open_admin_tmp_file, open_tmp_file):
        moved here from auth.c
    
    * auth.c (svn_client__get_ra_callbacks, open_admin_tmp_file, open_tmp_file):
        moved to ra.c
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@840912 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/ra.c
+++ subversion/libsvn_client/ra.c
@@ -22,10 +22,75 @@
 
 #include "svn_ra.h"
 #include "svn_client.h"
+#include "svn_path.h"
 
 #include "client.h"
 
 
+static svn_error_t *
+open_admin_tmp_file (apr_file_t **fp,
+                     void *callback_baton)
+{
+  svn_client_auth_baton_t *cb = 
+    (svn_client_auth_baton_t *) callback_baton;
+  
+  SVN_ERR (svn_wc_create_tmp_file (fp, cb->path, cb->pool));
+
+  return SVN_NO_ERROR;
+}
+
+
+static svn_error_t *
+open_tmp_file (apr_file_t **fp,
+               void *callback_baton)
+{
+  svn_client_auth_baton_t *cb = (svn_client_auth_baton_t *) callback_baton;
+  svn_stringbuf_t *truepath = svn_stringbuf_dup (cb->path, cb->pool);
+  svn_stringbuf_t *ignored_filename;
+
+  /* Tack on a made-up filename. */
+  svn_path_add_component_nts (truepath, "tempfile", svn_path_local_style);
+
+  /* Open a unique file;  use APR_DELONCLOSE. */  
+  SVN_ERR (svn_io_open_unique_file (fp, &ignored_filename,
+                                    truepath, ".tmp", TRUE, cb->pool));
+
+  return SVN_NO_ERROR;
+}
+
+
+svn_error_t *
+svn_client__get_ra_callbacks (svn_ra_callbacks_t **callbacks,
+                              void **callback_baton,
+                              svn_client_auth_baton_t *auth_baton,
+                              svn_stringbuf_t *path,
+                              svn_boolean_t do_store,
+                              svn_boolean_t use_admin,
+                              apr_pool_t *pool)
+{
+  svn_ra_callbacks_t *cbtable = 
+    (svn_ra_callbacks_t *) apr_pcalloc (pool, sizeof(*cbtable));
+  
+  cbtable->open_tmp_file = use_admin ? open_admin_tmp_file : open_tmp_file;
+  cbtable->get_authenticator = get_authenticator;
+
+  /* Just copy the PATH and DO_STORE into the baton, so callbacks can
+     see them later. */
+  auth_baton->path = path;
+  auth_baton->do_store = do_store;
+  
+  /* This is humorous; at present, we use the application-provided
+     auth_baton as the baton for whole the callbacks-vtable!  This
+     might not always be so.  For now, it's just easier that
+     svn_client_auth_baton_t be shared by the application and client
+     both, rather than wrapping one baton in another. */
+  *callback_baton = auth_baton;
+
+  *callbacks = cbtable;
+  return SVN_NO_ERROR;
+}
+
+
 svn_error_t * svn_client__open_ra_session (void **session_baton,
                                            const svn_ra_plugin_t *ra_lib,
                                            svn_stringbuf_t *repos_URL,
