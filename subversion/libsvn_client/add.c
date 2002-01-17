e973e857f366adb177653c9676105bdd2bd0f040
C. Michael Pilato <cmpilato@apache.org>
2002-01-17 03:14:20 +0000

    Code factorization ala Stein.
    * subversion/libsvn_client/client.h
    
      (svn_client__make_commit_info): New function.
    
    * subversion/libsvn_client/commit.c
    
      (svn_client__make_commit_info): New function.
    
      (send_to_repos): Use new svn_client__make_commit_info function.
      Also, in the XML case, the passed-in revision is used in the
      commit_info structure.
    
    * subversion/libsvn_client/delete.c
    
      (svn_client_delete): Use new svn_client__make_commit_info function.
    
    * subversion/libsvn_client/add.c
    
      (svn_client_mkdir): Use new svn_client__make_commit_info function.
    
    * subversion/libsvn_client/copy.c
    
      (wc_to_repos_copy, repos_to_repos_copy): Use new svn_client__make_commit_info
      function.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@840999 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/add.c
+++ subversion/libsvn_client/add.c
@@ -186,21 +186,11 @@ svn_client_mkdir (svn_client_commit_info_t **commit_info,
       SVN_ERR (editor->close_directory (dir_baton));
       SVN_ERR (editor->close_edit (edit_baton));
 
-      /* Allocate (and populate) the commit_info */
-      if ((committed_date != NULL) 
-          || (committed_author != NULL) 
-          || (SVN_IS_VALID_REVNUM (committed_rev)))
-        {
-          svn_client_commit_info_t *info;
-          
-          info = apr_pcalloc (pool, sizeof (**commit_info));
-          if (committed_date)
-            info->date = apr_pstrdup (pool, committed_date);
-          if (committed_author)
-            info->date = apr_pstrdup (pool, committed_author);
-          info->revision = committed_rev;
-          *commit_info = info;
-        }
+      /* Fill in the commit_info structure. */
+      *commit_info = svn_client__make_commit_info (committed_rev,
+                                                   committed_author,
+                                                   committed_date,
+                                                   pool);
 
       /* Free the RA session. */
       SVN_ERR (ra_lib->close (session));
