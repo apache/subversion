63161b319669d24dcba0e5bd74cd1e1ce25cd84b
xela <xela@unknown>
2002-01-18 09:55:48 +0000

    Renamed "tests/jni_vector.c" to "nativewrapper.c"
    * tests/jni_vector.c : deleted
    * tests/nativewrapper.c : added
    
    New Java wrappers for functions in "date.h"
    
    * tests/nativewrapper.c(Java_NativeWrapper_dateCreate)
    * tests/Nativewrapper.java(dateCreate)
    
    Inserted Javadoc tags
    
    * tests/Nativewrapper.java(vectorCreate, vectorAdd)
    
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@841015 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/java/jni/tests/NativeWrapper.java
+++ subversion/bindings/java/jni/tests/NativeWrapper.java
@@ -15,16 +15,47 @@
  *
  */
 import java.util.Vector;
+import java.util.Date;
 
+/**
+ * This classed is used for the unit tests. All of the C helper
+ * functions for the Java Subversion binding should be reached
+ * with this class. 
+ *
+ * Sometimes this is not possible, because
+ * the class needs non-trivial native parameters. In this case
+ * either simple type parameters are used or the methode
+ * here designs a special case with no parameter
+ */
 public class NativeWrapper
 {
     static
 	{
-	    System.loadLibrary("svn_jni_tests");
+	    System.loadLibrary("svn_jni_nativewrapper");
 	}
 
+    /**
+     * Calls the function "vector__create" (vector.h)
+     *
+     * @return new, empty Vector instance
+     */
     public static native Vector vectorCreate();
+
+    /**
+     * Calls the function "vector__add" (vector.h)
+     *
+     * @param vector instance of a vector that should be used for
+     *               the operation
+     * @param object
+     */
     public static native void vectorAdd(Vector vector, Object object);
+
+    /**
+     * Create a new date from a long value.
+     *
+     * @param date milliseconds since Januar 1, 1970 00:00
+     */
+    public static native Date dateCreate(long date);
 }
 
 /* 
