989c0b82f5eb1b776aef6b00fed5414da400439b
xela <xela@unknown>
2002-01-18 19:29:54 +0000

    Prior the compilation of the Java classes, adapt CLASSPATH variable to our paths (relative), so there are no explicit parameters for javac necessary
    * build-java.sh
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@841024 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/java/jni/build-java.sh
+++ subversion/bindings/java/jni/build-java.sh
@@ -5,17 +5,21 @@
 # will be replaced soon with some real makefile 
 # thing...
 
+CLASSPATH=$CLASSPATH:.:tests
+
 # first step: compile all of the java sources
-javac -classpath . org/tigris/subversion/*.java
-javac -classpath . org/tigris/subversion/lib/*.java
-javac -classpath . *.java
-javac -classpath $CLASSPATH:.:tests tests/*.java
+javac org/tigris/subversion/*.java
+javac org/tigris/subversion/lib/*.java
+javac *.java
+javac tests/*.java
 
 # second step: build the JNI header
-javah -classpath . -o svn_jni.h org.tigris.subversion.lib.ClientImpl
-javah -classpath tests -o tests/nativewrapper.h NativeWrapper
+javah -o svn_jni.h org.tigris.subversion.lib.ClientImpl
+javah -o tests/nativewrapper.h NativeWrapper
 
 # third step: give some extra information
 echo "to run the tests, start them with:"
 echo "run-tests.sh"
+
+
 
