839be06e6b209d397db41184c52bfe7c2e4076fe
Garrett Rooney <rooneg@apache.org>
2002-01-29 01:52:23 +0000

    * subversion/tests/libsvn_subr/target-test.sh   Instead of trying to assign to PWD and masking the error if it fails, just   change the variable name to CURRENT_DIR.
    Requested By: gstein
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@841173 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/libsvn_subr/target-test.sh
+++ subversion/tests/libsvn_subr/target-test.sh
@@ -2,10 +2,7 @@
 
 FAILED="no"
 
-# PWD is a read only variable in some /bin/sh's, so we would get an error here, 
-# fortunately, those /bin/sh's also allow you to just read $PWD to get the 
-# current working directory, so we can safely ignore the problem and continue.
-PWD=`pwd` 2>/dev/null
+CURRENT_DIR=`pwd`
 
 if test -d "z"; then
     :
@@ -23,7 +20,7 @@ else
     touch z/A/file
 fi
 
-EXPECTED="${PWD}/z: A, D, G, "
+EXPECTED="${CURRENT_DIR}/z: A, D, G, "
 GOT=`./target-test z/A/B z/A z/A/C z/D/E z/D/F z/D z/G z/G/H z/G/I`
 if [ $? != 0 ]; then
   echo "FAIL: target-test  1: normal use (non-null return)"
@@ -37,7 +34,7 @@ else
   fi
 fi
 
-EXPECTED="${PWD}/z/A: "
+EXPECTED="${CURRENT_DIR}/z/A: "
 GOT=`./target-test z/A z/A z/A z/A`
 if [ $? != 0 ]; then
   echo "FAIL: target-test  2: identical dirs (non-null return)"
@@ -51,7 +48,7 @@ else
   fi
 fi
 
-EXPECTED="${PWD}/z/A: file, "
+EXPECTED="${CURRENT_DIR}/z/A: file, "
 GOT=`./target-test z/A/file z/A/file z/A/file z/A/file`
 if [ $? != 0 ]; then
   echo "FAIL: target-test  3: identical files (non-null return)"
@@ -65,7 +62,7 @@ else
   fi
 fi
 
-EXPECTED="${PWD}/z/A: "
+EXPECTED="${CURRENT_DIR}/z/A: "
 GOT=`./target-test z/A`
 if [ $? != 0 ]; then
   echo "FAIL: target-test  4: single dir (non-null return)"
@@ -79,7 +76,7 @@ else
   fi
 fi
 
-EXPECTED="${PWD}/z/A: file, "
+EXPECTED="${CURRENT_DIR}/z/A: file, "
 GOT=`./target-test z/A/file`
 if [ $? != 0 ]; then
   echo "FAIL: target-test  5: single file (non-null return)"
