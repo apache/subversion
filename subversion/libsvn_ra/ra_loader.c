5db743df7d0b90779b415dfd565ba8c4425666b2
Karl Fogel <kfogel@apache.org>
2002-02-08 19:46:32 +0000

    Remove spurious leading whitespace in copyright notice.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@841296 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_ra/ra_loader.c
+++ subversion/libsvn_ra/ra_loader.c
@@ -1,7 +1,7 @@
 /*
  * ra_loader.c:  logic for loading different RA library implementations
  *
-      * ====================================================================
+ * ====================================================================
  * Copyright (c) 2000-2002 CollabNet.  All rights reserved.
  *
  * This software is licensed as described in the file COPYING, which
