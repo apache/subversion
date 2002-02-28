14f01b19c1a2b4acecfc26ecf7e2b105ab6c631b
Greg Stein <gstein@apache.org>
2002-02-28 02:21:39 +0000

    Switch over to the "duplicate keys" design for the 'strings' table.
    COMPATABILITY NOTE: this requires an update to existing
    repositories. The strings table needs to have its "duplicates" flag
    enabled. See the 'tools/enable-dupkey.sh' script to fix up existing
    repositories.
    
    * libsvn_fs/strings-table.c
      (svn_fs__open_strings_table): set the flags on the db to enable
        duplicate keys.
      (locate_key): new function to allocate a cursor, locate the first
        record of data for a key, and return its length.
      (get_next_length): use the cursor to get the length of the next
        record of data [for the key]
      (svn_fs__string_read): use locate_key and get_next_length to locate
        the data record for the requested offset. return whatever data is
        available in that data record, or the requested length (whichever
        is less). note that this changes the semantics to "return some
        amount" rather than "return all requested"
      (get_key_and_bump): new function containing code factored out of
        svn_fs__string_append; it gets the current 'next-key' value and
        bumps the value in the database. it has also been updated to deal
        with the new 'put' semantics of databases with dup keys.
      (svn_fs__string_append): just shove another record into the database
      (svn_fs__string_clear): we have to delete prior contents (all the
        data records associated with the key) since we can't just 'put' a
        zero-length value over the top of the old.
      (svn_fs__string_size): revamped to total all the data records for
        the given key.
      (svn_fs__string_copy): revamp. rather than reading and appending to
        a new record, we just copy all the records to the new key.
    
    * libsvn_fs/strings-table.h (svn_fs__string_read): update the docs to
        better reflect reality, and the fact that the function can now
        (legally) return less than requested.
    
    * tests/libsvn_fs/strings-reps-test.c (verify_expected_record): print
        more information when an expected size is not met. adjust call to
        svn_fs__string_read() to compensate for not necessarily getting
        all the data.
    
    * tools/enable-dupkey.sh: new tool to convert an existing database
        over to the new "duplicate keys" format.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@841458 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs/strings-table.c
+++ subversion/libsvn_fs/strings-table.c
@@ -36,6 +36,11 @@ svn_fs__open_strings_table (DB **strings_p,
   DB *strings;
 
   DB_ERR (db_create (&strings, env, 0));
+
+  /* Enable duplicate keys. This allows the data to be spread out across
+     multiple records. Note: this must occur before ->open().  */
+  DB_ERR (strings->set_flags (strings, DB_DUP));
+
   DB_ERR (strings->open (strings, "strings", 0, DB_BTREE,
                        create ? (DB_CREATE | DB_EXCL) : 0,
                        0666));
@@ -60,6 +65,101 @@ svn_fs__open_strings_table (DB **strings_p,
 
 /*** Storing and retrieving strings.  ***/
 
+static svn_error_t *
+locate_key (apr_size_t *length,
+            DBC **cursor,
+            DBT *query,
+            svn_fs_t *fs,
+            trail_t *trail)
+{
+  int db_err;
+  DBT result;
+
+  SVN_ERR (DB_WRAP (fs, "creating cursor for reading a string",
+                    fs->strings->cursor (fs->strings, trail->db_txn,
+                                         cursor, 0)));
+
+  /* Set up the DBT for reading the length of the record. */
+  svn_fs__clear_dbt (&result);
+  result.ulen = 0;
+  result.flags |= DB_DBT_USERMEM;
+
+  /* Advance the cursor to the key that we're looking for. */
+  db_err = (*cursor)->c_get (*cursor, query, &result, DB_SET);
+
+  /* We don't need to svn_fs__track_dbt() the result, because nothing
+     was allocated in it. */
+
+  /* If there's no such node, return an appropriately specific error.  */
+  if (db_err == DB_NOTFOUND)
+    {
+      (*cursor)->c_close (*cursor);
+      return svn_error_createf
+        (SVN_ERR_FS_NO_SUCH_STRING, 0, 0, fs->pool,
+         "locate_key: no such string `%s'", query->data);
+    }
+  if (db_err)
+    {
+      DBT rerun;
+
+      if (db_err != ENOMEM)
+        {
+          (*cursor)->c_close (*cursor);
+          return DB_WRAP (fs, "could not move cursor", db_err);
+        }
+
+      /* We got an ENOMEM (typical since we have a zero length buf), so
+         we need to re-run the operation to make it happen. */
+      svn_fs__clear_dbt (&rerun);
+      rerun.flags |= DB_DBT_USERMEM | DB_DBT_PARTIAL;
+      SVN_ERR (DB_WRAP (fs, "rerunning cursor move",
+                        (*cursor)->c_get (*cursor, query, &rerun, DB_SET)));
+    }
+
+  /* ### this cast might not be safe? */
+  *length = (apr_size_t) result.size;
+
+  return SVN_NO_ERROR;
+}
+
+static int
+get_next_length (apr_size_t *length, DBC *cursor, DBT *query)
+{
+  DBT result;
+  int db_err;
+
+  /* Set up the DBT for reading the length of the record. */
+  svn_fs__clear_dbt (&result);
+  result.ulen = 0;
+  result.flags |= DB_DBT_USERMEM;
+
+  /* Note: this may change the QUERY DBT, but that's okay: we're going
+     to be sticking with the same key anyways.  */
+  db_err = cursor->c_get (cursor, query, &result, DB_NEXT_DUP);
+
+  /* Note that we exit on DB_NOTFOUND. The caller uses that to end a loop. */
+  if (db_err)
+    {
+      DBT rerun;
+
+      if (db_err != ENOMEM)
+        {
+          cursor->c_close (cursor);
+          return db_err;
+        }
+
+      /* We got an ENOMEM (typical since we have a zero length buf), so
+         we need to re-run the operation to make it happen. */
+      svn_fs__clear_dbt (&rerun);
+      rerun.flags |= DB_DBT_USERMEM | DB_DBT_PARTIAL;
+      db_err = cursor->c_get (cursor, query, &rerun, DB_NEXT_DUP);
+    }
+
+  /* ### this cast might not be safe? */
+  *length = (apr_size_t) result.size;
+  return db_err;
+}
+
 svn_error_t *
 svn_fs__string_read (svn_fs_t *fs,
                      const char *key,
@@ -70,53 +170,112 @@ svn_fs__string_read (svn_fs_t *fs,
 {
   int db_err;
   DBT query, result;
+  DBC *cursor;
+  apr_size_t length;
 
+  svn_fs__str_to_dbt (&query, (char *) key);
+
+  SVN_ERR (locate_key (&length, &cursor, &query, fs, trail));
+
+  /* Seek through the records for this key, trying to find the record that
+     includes OFFSET. Note that we don't require reading from more than
+     one record since we're allowed to return partial reads.  */
+  while (length <= offset)
+    {
+      offset -= length;
+
+      db_err = get_next_length (&length, cursor, &query);
+
+      /* No more records? They tried to read past the end. */
+      if (db_err == DB_NOTFOUND)
+        {
+          *len = 0;
+          return SVN_NO_ERROR;
+        }
+      if (db_err)
+        return DB_WRAP (fs, "reading string", db_err);
+    }
+
+  /* The current record contains OFFSET. Fetch the contents now. Note that
+     OFFSET has been moved to be relative to this record. The length could
+     quite easily extend past this record, but no big deal. We also keep
+     the DB_DBT_PARTIAL to read little pieces at this location.  */
   svn_fs__clear_dbt (&result);
   result.data = buf;
   result.ulen = *len;
   result.doff = offset;
   result.dlen = *len;
   result.flags |= (DB_DBT_USERMEM | DB_DBT_PARTIAL);
+  db_err = cursor->c_get (cursor, &query, &result, DB_CURRENT);
+  if (db_err)
+    goto cursor_error;
 
-  db_err = fs->strings->get (fs->strings, trail->db_txn,
-                             svn_fs__str_to_dbt (&query, (char *) key),
-                             &result, 0);
+  /* Done with the cursor. */
+  SVN_ERR (DB_WRAP (fs, "closing string-reading cursor",
+                    cursor->c_close (cursor)));
 
-  /* If there's no such node, return an appropriately specific error.  */
-  if (db_err == DB_NOTFOUND)
-    return svn_error_createf
-      (SVN_ERR_FS_NO_SUCH_STRING, 0, 0, fs->pool,
-       "svn_fs__string_read: no such string `%s'", key);
-
-  /* Handle any other error conditions.  */
-  SVN_ERR (DB_WRAP (fs, "reading string", db_err));
-
-  {
-    /* ### ugly hack!!  
-
-       Thanks to what is believed to be a bug in Berkeley DB 3.2.9,
-       reading off the end of this end (if it is stored in a
-       B_OVERFLOW page, as BDB defines it) will not return the proper
-       value in results.size, so for now we will hack around that by
-       calculating the legal sizes for ourselves. 
-
-       This section of code *should* just be:
-
-           *len = result.size;
-       
-       and is certainly intended to someday return to that. */
-    apr_size_t size;
-
-    SVN_ERR (svn_fs__string_size (&size, fs, key, trail));
-    if (offset >= size)
-      *len = 0;
-    else if (*len > (size - offset))
-      *len = size - offset;
-  }
-
+  *len = result.size;
   return SVN_NO_ERROR;
+
+cursor_error:
+  /* An error occurred somewhere. Close the cursor and return the error. */
+  cursor->c_close (cursor);
+  return DB_WRAP (fs, "reading string", db_err);
 }
 
+
+/* Get the current 'next-key' value and bump the record. */
+static svn_error_t *
+get_key_and_bump (svn_fs_t *fs, const char **key, trail_t *trail)
+{
+  DBC *cursor;
+  char next_key[200];
+  apr_size_t key_len;
+  int db_err;
+  DBT query;
+  DBT result;
+
+  /* ### todo: see issue #409 for why bumping the key as part of this
+     trail is problematic. */
+
+  /* Open a cursor and move it to the 'next-key' value. We can then fetch
+     the contents and use the cursor to overwrite those contents. Since
+     this database allows duplicates, we can't do an arbitrary 'put' to
+     write the new value -- that would append, not overwrite.  */
+
+  SVN_ERR (DB_WRAP (fs, "creating cursor for reading a string",
+                    fs->strings->cursor (fs->strings, trail->db_txn,
+                                         &cursor, 0)));
+
+  /* Advance the cursor to 'next-key' and read it. */
+
+  db_err = cursor->c_get (cursor,
+                          svn_fs__str_to_dbt (&query,
+                                              (char *) svn_fs__next_key_key),
+                          svn_fs__result_dbt (&result),
+                          DB_SET);
+  if (db_err)
+    {
+      cursor->c_close (cursor);
+      return DB_WRAP (fs, "getting next-key value", db_err);
+    }
+
+  svn_fs__track_dbt (&result, trail->pool);
+  *key = apr_pstrndup (trail->pool, result.data, result.size);
+
+  /* Bump to future key. */
+  key_len = result.size;
+  svn_fs__next_key (result.data, &key_len, next_key);
+
+  /* Shove the new key back into the database, at the cursor position. */
+  db_err = cursor->c_put (cursor, &query,
+                          svn_fs__str_to_dbt (&result, (char *) next_key),
+                          DB_CURRENT);
+
+  cursor->c_close (cursor);
+
+  return DB_WRAP (fs, "bumping next string key", db_err);
+}
 
 svn_error_t *
 svn_fs__string_append (svn_fs_t *fs,
@@ -126,61 +285,21 @@ svn_fs__string_append (svn_fs_t *fs,
                        trail_t *trail)
 {
   DBT query, result;
-  apr_size_t offset = 0;
-  int db_err;
 
   /* If the passed-in key is NULL, we graciously generate a new string
      using the value of the `next-key' record in the strings table. */
   if (*key == NULL)
     {
-      char next_key[200];
-      apr_size_t key_len;
-
-      /* ### todo: see issue #409 for why bumping the key as part of this
-         trail is problematic. */
-
-      /* Get the current value associated with `next-key'.  */
-      svn_fs__str_to_dbt (&query, (char *) svn_fs__next_key_key);
-      SVN_ERR (DB_WRAP (fs, "allocating new string (getting next-key)",
-                        fs->strings->get (fs->strings,
-                                          trail->db_txn,
-                                          &query,
-                                          svn_fs__result_dbt (&result),
-                                          0)));
-
-      svn_fs__track_dbt (&result, trail->pool);
-      *key = apr_pstrndup (trail->pool, result.data, result.size);
-
-      /* Bump to future key. */
-      key_len = result.size;
-      svn_fs__next_key (result.data, &key_len, next_key);
-      db_err = fs->strings->put
-        (fs->strings, trail->db_txn,
-         svn_fs__str_to_dbt (&query, (char *) svn_fs__next_key_key),
-         svn_fs__str_to_dbt (&result, (char *) next_key),
-         0);
-
-      SVN_ERR (DB_WRAP (fs, "bumping next string key", db_err));
-    }
-  else
-    {
-      /* Get the current size of the record. */
-      SVN_ERR (svn_fs__string_size (&offset, fs, *key, trail));
+      SVN_ERR (get_key_and_bump (fs, key, trail));
     }
 
-  /* Append to it. */
-  svn_fs__clear_dbt (&result);
-  result.data = (char *) buf;
-  result.size = len;
-  result.doff = offset;
-  result.dlen = len;
-  result.flags |= (DB_DBT_USERMEM | DB_DBT_PARTIAL);
-
+  /* Store a new record into the database. */
   SVN_ERR (DB_WRAP (fs, "appending string",
                     fs->strings->put
                     (fs->strings, trail->db_txn,
                      svn_fs__str_to_dbt (&query, (char *) (*key)),
-                     &result, 0)));
+                     svn_fs__set_dbt (&result, (void *) buf, len),
+                     0)));
 
   return SVN_NO_ERROR;
 }
@@ -194,14 +313,10 @@ svn_fs__string_clear (svn_fs_t *fs,
   int db_err;
   DBT query, result;
 
-  svn_fs__clear_dbt (&result);
-  result.data = 0;
-  result.size = 0;
-  result.flags |= DB_DBT_USERMEM;
+  svn_fs__str_to_dbt (&query, (char *)key);
 
-  db_err = fs->strings->put (fs->strings, trail->db_txn,
-                             svn_fs__str_to_dbt (&query, (char *) key),
-                             &result, 0);
+  /* Torch the prior contents */
+  db_err = fs->strings->del (fs->strings, trail->db_txn, &query, 0);
 
   /* If there's no such node, return an appropriately specific error.  */
   if (db_err == DB_NOTFOUND)
@@ -212,7 +327,15 @@ svn_fs__string_clear (svn_fs_t *fs,
   /* Handle any other error conditions.  */
   SVN_ERR (DB_WRAP (fs, "clearing string", db_err));
 
-  return SVN_NO_ERROR;
+  /* Shove empty data back in for this key. */
+  svn_fs__clear_dbt (&result);
+  result.data = 0;
+  result.size = 0;
+  result.flags |= DB_DBT_USERMEM;
+
+  return DB_WRAP (fs, "storing empty contents",
+                  fs->strings->put (fs->strings, trail->db_txn,
+                                    &query, &result, 0));
 }
 
 
@@ -223,32 +346,33 @@ svn_fs__string_size (apr_size_t *size,
                      trail_t *trail)
 {
   int db_err;
-  DBT query, result;
+  DBT query;
+  DBC *cursor;
+  apr_size_t length;
+  apr_size_t total;
 
-  svn_fs__clear_dbt (&result);
-  result.ulen = 0;
-  result.flags |= DB_DBT_USERMEM;
+  svn_fs__str_to_dbt (&query, (char *) key);
 
-  db_err = fs->strings->get (fs->strings, trail->db_txn,
-                             svn_fs__str_to_dbt (&query, (char *) key),
-                             &result, 0);
+  SVN_ERR (locate_key (&length, &cursor, &query, fs, trail));
 
-  /* We don't need to svn_fs__track_dbt() the result, because nothing
-     was allocated in it. */
+  total = length;
+  while (1)
+    {
+      db_err = get_next_length (&length, cursor, &query);
 
-  /* If there's no such node, return an appropriately specific error.  */
-  if (db_err == DB_NOTFOUND)
-    return svn_error_createf
-      (SVN_ERR_FS_NO_SUCH_STRING, 0, 0, fs->pool,
-       "svn_fs__string_size: no such string `%s'", key);
+      /* No more records? Then return the total length. */
+      if (db_err == DB_NOTFOUND)
+        {
+          *size = total;
+          return SVN_NO_ERROR;
+        }
+      if (db_err)
+        return DB_WRAP (fs, "fetching string length", db_err);
 
-  if (db_err && db_err != ENOMEM)
-    return DB_WRAP (fs, "reading string", db_err);
+      total += length;
+    }
 
-  /* kff todo: how can we know this cast is safe? */
-  *size = (apr_size_t) result.size;
-
-  return SVN_NO_ERROR;
+  /* NOTREACHED */
 }
 
 
@@ -282,27 +406,62 @@ svn_fs__string_copy (svn_fs_t *fs,
                      const char *key,
                      trail_t *trail)
 {
-  apr_size_t size, offset;
-  char buf[10000];
+  DBT query;
+  DBT result;
+  DBT copykey;
+  DBC *cursor;
+  int db_err;
 
-  offset = 0;
-  *new_key = NULL;
+  SVN_ERR (get_key_and_bump (fs, new_key, trail));
+
+  SVN_ERR (DB_WRAP (fs, "creating cursor for reading a string",
+                    fs->strings->cursor (fs->strings, trail->db_txn,
+                                         &cursor, 0)));
+
+  svn_fs__str_to_dbt (&query, (char *) key);
+  svn_fs__str_to_dbt (&copykey, (char *) *new_key);
+
+  svn_fs__clear_dbt (&result);
+
+  /* Move to the first record and fetch its data (under BDB's mem mgmt). */
+  db_err = cursor->c_get (cursor, &query, &result, DB_SET);
+  if (db_err)
+    {
+      cursor->c_close (cursor);
+      return DB_WRAP (fs, "getting next-key value", db_err);
+    }
+
   while (1)
     {
-      /* Reset size to maximum chunk size. */
-      size = sizeof (buf);
+      /* ### can we pass a BDB-provided buffer to another BDB function?
+         ### they are supposed to have a duration up to certain points
+         ### of calling back into BDB, but I'm not sure what the exact
+         ### rules are. it is definitely nicer to use BDB buffers here
+         ### to simplify things and reduce copies, but... hrm.
+      */
 
-      /* Read SIZE bytes from the record, starting at OFFSET. */
-      SVN_ERR (svn_fs__string_read (fs, key, buf, offset, &size, trail));
+      /* Write the data to the database */
+      db_err = fs->strings->put (fs->strings, trail->db_txn,
+                                 &copykey, &result, 0);
+      if (db_err)
+        {
+          cursor->c_close (cursor);
+          return DB_WRAP (fs, "writing copied data", db_err);
+        }
 
-      /* Now, write SIZE bytes into the new record. */
-      SVN_ERR (svn_fs__string_append (fs, new_key, size, buf, trail));
-      offset += size;
-
-      /* If we didn't get the whole chunk, we must be finished. */
-      if (size < sizeof (buf))
+      /* Read the next chunk. Terminate loop if we're done. */
+      svn_fs__clear_dbt (&result);
+      db_err = cursor->c_get (cursor, &query, &result, DB_NEXT_DUP);
+      if (db_err == DB_NOTFOUND)
         break;
+      if (db_err)
+        {
+          cursor->c_close (cursor);
+          return DB_WRAP (fs, "fetching string data for a copy", db_err);
+        }
     }
+
+  cursor->c_close (cursor);
 
   return SVN_NO_ERROR;
 }
---
+++ tools/enable-dupkeys.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+#
+# USAGE: enable-dupkey.sh REPOS_PATH
+#
+# This script will upgrade an existing repository to enable "duplicates"
+# in the 'strings' table. The addition of that flag was added in r####
+# of Subversion. All old/existing databases need to be updated.
+#
+# Your PATH should refer the BerkeleyDB 4.0 tools (make sure the 4.0
+# versions are found before any others). Typically, you can set this
+# up without needing to permanent modify your PATH; use the following:
+#
+#    $ PATH=/usr/local/BerkeleyDB.4.0:$PATH ./enable-dupkey.sh REPOS_PATH
+#
+# Note that REPOS_PATH refers to a Subversion repository, not the 'db'
+# directory inside the repository.
+#
+# Also note: the original 'strings' table will be preserved as 'strings.bak'
+#
+
+
+if test "$#" != 1; then
+    echo "USAGE: $0 REPOS_PATH"
+    echo "  See the comments in the header of this file for more information."
+    exit 1
+fi
+
+#
+# first, verify that we have the proper versions of the tools
+#
+
+VSN=4.0.14
+
+v="`db_dump -V`"
+tmp="`echo $v | grep $VSN`"
+if test -z "$tmp"; then
+    echo "$VSN of db_dump is required. You are running:"
+    echo "    $v"
+    echo ""
+    echo "Make sure your path finds the 4.0 versions of the tools."
+    exit 1
+fi
+
+v="`db_load -V`"
+tmp="`echo $v | grep $VSN`"
+if test -z "$tmp"; then
+    echo "$VSN of db_load is required. You are running:"
+    echo "    $v"
+    echo ""
+    echo "Make sure your path finds the 4.0 versions of the tools."
+    exit 1
+fi
+
+
+#
+# Figure out the paths and check some basic conditions
+#
+
+strings_old="$1/db/strings"
+strings_new="./strings.new"
+
+if test ! -r "$strings_old"; then
+    echo "ERROR: Could not read '$strings_old'. Please correct."
+    exit 1
+fi
+
+db_dir="`cd $1/db && pwd`"
+this_dir="`pwd`"
+if test "$db_dir" = "$this_dir"; then
+    echo "ERROR: Current directory can not be '$1/db'."
+    echo "       Please switch to a different directory to perform the conversion."
+    exit 1
+fi
+
+
+#
+# Do the conversion!
+#
+
+echo "Converting '$strings_old' to '$strings_new' ..."
+db_dump "$strings_old" | db_load -c duplicates=1 "$strings_new"
+
+echo "Preserving '$strings_old' as '$strings_old.bak' ..."
+mv "$strings_old" "${strings_old}.bak"
+
+echo "Moving '$strings_new' to '$strings_old' ..."
+mv "$strings_new" "$strings_old"
+
+echo "Done."
