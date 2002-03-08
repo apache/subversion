7841b440cd73d12734feae2a68f0bbc9c198a442
Greg Stein <gstein@apache.org>
2002-03-08 01:51:52 +0000

    Update the versioning stuff, in preparation for the 0.10.0 release.
    * svn_version.h:
    
      (SVN_VER_STRINGIFY): removed in favor of APR_STRINGIFY() from
        apr_version.h
    
      (SVN_VER_NAME): removed; won't ever be used.
      
      (SVN_VER_TAG): set to "dev build". this gets reset by the
        distribution process.
    
      (SVN_VER_MINOR): set to 10 for the next release
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@841533 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_version.h
+++ subversion/include/svn_version.h
@@ -15,14 +15,14 @@
  * ====================================================================
  */
 
+#ifndef SVN_VERSION_H
+#define SVN_VERSION_H
 
-
+#include <apr_version.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
-
-#ifndef SVN_VERSION_H
-#define SVN_VERSION_H
 
 
 /* Symbols that define the version number.
@@ -37,61 +37,41 @@ extern "C" {
 /* Minor version number.
    Modify when new functionality is added or new interfaces are
    defined, but all changes are backward compatible. */
-#define SVN_VER_MINOR      9
+#define SVN_VER_MINOR      10
 
 /* Patch number.
    Modify for every released patch. */
 #define SVN_VER_MICRO      0
 
 
-/* Descriptive name: If a version number isn't appropriate (e.g., for
-   pre-1.0 milestone versions), use this string for the version name
-   instead. Otherwise, this symbol should be #undef'd. */
-#undef SVN_VER_NAME
-/* #define SVN_VER_NAME       "M6" */
+/* Version tag: a string describing the version.
+   This tag remains "dev build" in the repository so that we can always
+   see from "svn --version" that the software has been built from the
+   repository rather than a "blessed" distribution.
 
-
-/* Version tag: A string describing the of the version.
-   Typical values are "alpha", "beta", or "rcN", for release candidate N.
-   This symbol may be #undef'd for major releases; for development
-   snapshots, it should be the number of the snapshot's revision. */
-#undef SVN_VER_TAG
-/* #define SVN_VER_TAG        "rc2" */
+   During the distribution process, we automatically replace this text
+   with something like "r1504".  */
+#define SVN_VER_TAG        "dev build"
 
 
 
 /* Version strings composed from the above definitions. */
 
 
-/* Yet another stringification macro. */
-#define SVN_VER_STRINGIFY(X) SVN_VER_REALLY_STRINGIFY(X)
-#define SVN_VER_REALLY_STRINGIFY(X) #X
-
-
 /* Version number */
-#ifndef SVN_VER_NAME
-#  define SVN_VER_NUMBER   SVN_VER_STRINGIFY(SVN_VER_MAJOR) \
-                           "." SVN_VER_STRINGIFY(SVN_VER_MINOR) \
-                           "." SVN_VER_STRINGIFY(SVN_VER_MICRO)
-#else
-#  define SVN_VER_NUMBER   SVN_VER_NAME
-#endif /* SVN_VER_NAME */
-
+#define SVN_VER_NUMBER   APR_STRINGIFY(SVN_VER_MAJOR) \
+                         "." APR_STRINGIFY(SVN_VER_MINOR) \
+                         "." APR_STRINGIFY(SVN_VER_MICRO)
 
 /* Complete version string */
-#ifdef SVN_VER_TAG
-#  define SVN_VERSION      SVN_VER_NUMBER " (" SVN_VER_TAG ")"
-#else
-#  define SVN_VERSION      SVN_VER_NUMBER
-#endif /* SVN_VER_TAG */
+#define SVN_VERSION      SVN_VER_NUMBER " (" SVN_VER_TAG ")"
 
-
-
-#endif /* SVN_VERSION_H */
 
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
+
+#endif /* SVN_VERSION_H */
 
 
 /* --------------------------------------------------------------
