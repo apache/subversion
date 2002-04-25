b17c22b3ff33e913159f36c9fcba13b2c5ad7281
Sander Striker <striker@apache.org>
2002-04-25 10:34:12 +0000

    * build/buildcheck.sh
      Update our build requirements to require neon 0.19.4.
    
    
    * INSTALL
    
      (section I): Update the documentation to reflect that
        our version requirements are 0.19.4 for neon and
        2.0.36 for Apache.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@841854 13f79535-47bb-0310-9956-ffa450edef68

--- build/buildcheck.sh
+++ build/buildcheck.sh
@@ -53,8 +53,8 @@ echo "buildcheck: libtool version $lt_pversion (ok)"
 #--------------------------------------------------------------------------
 # check for the correct version of Neon
 #
-NEON_WANTED_REGEX=0.19.[2-3]
-NEON_LATEST_WORKING_VER=0.19.3
+NEON_WANTED_REGEX=0.19.4
+NEON_LATEST_WORKING_VER=0.19.4
 NEON_URL="http://www.webdav.org/neon/neon-${NEON_LATEST_WORKING_VER}.tar.gz"
 if test -d ./neon; then
   NEON_VERSION="`./ac-helpers/get-neon-ver.sh neon`"
