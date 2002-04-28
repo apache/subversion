c17e6aca8a613a6f736d135523f495a4ec9d23d7
Garrett Rooney <rooneg@apache.org>
2002-04-28 23:31:14 +0000

    To satisfy those who want their ChangeLog's in GNU style rather than svn style, here's a script to munge the output of 'svn log' into something that approaches GNU ChangeLog style.
    If someone is really motivated, they could grab some code from cvs2cl.pl and 
    make this do line wrapping and formatting, but this is at least a start.
    
    This closes (barely) issue #610, although there are several useful suggestions
    in the issue that a motivated individual could take and add to this script.
    
    * tools/dev/gnuify-changelog.pl
      new file.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@841895 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tools/dev/gnuify-changelog.pl
@@ -0,0 +1,71 @@
+#!/usr/bin/perl -w
+
+# a script to munge the output of 'svn log' into something approaching the 
+# style of a GNU ChangeLog.
+#
+# to use this, just fill in the 'hackers' hash with the usernames and 
+# name/emails of the people who work on your project, go to the top level 
+# of your working copy, and run:
+#
+# $ svn log | /path/to/gnuify-changelog.pl > ChangeLog
+
+%hackers = ( "jimb"        => 'Jim Blandy <jimb@redhat.com>',
+             "sussman"     => 'Ben Collins-Sussman <sussman@collab.net>',
+             "kfogel"      => 'Karl Fogel <kfogel@collab.net>',
+             "gstein"      => 'Greg Stein <gstein@lyra.org>',
+             "brane"       => 'Branko Cibej <brane@xbc.nu>',
+             "joe"         => 'Joe Orton <joe@light.plus.com>',
+             "ghudson"     => 'Greg Hudson <ghudson@mit.edu>',
+             "lefty"       => 'Lee P. W. Burgess <lefty@red-bean.com>',
+             "fitz"        => 'Brian Fitzpatrick <fitz@red-bean.com>',
+             "mab"         => 'Matthew Braithwaite <matt@braithwaite.net>',
+             "daniel"      => 'Daniel Stenberg <daniel@haxx.se>',
+             "mmurphy"     => 'Mark Murphy <mmurphy@collab.net>',
+             "cmpilato"    => 'C. Michael Pilato <cmpilato@collab.net>',
+             "kevin"       => 'Kevin Pilch-Bisson <kevin@pilch-bisson.net>',
+             "philip"      => 'Philip Martin <philip@codematters.co.uk>',
+             "jerenkrantz" => 'Justin Erenkrantz <jerenkrantz@apache.org>',
+             "rooneg"      => 'Garrett Rooney <rooneg@electricjellyfish.net>',
+             "bcollins"    => 'Ben Collins <bcollins@debian.org>',
+             "blair"       => 'Blair Zajac <blair@orcaware.com>',
+             "striker"     => 'Sander Striker <striker@apache.org>',
+             "XelaRellum"  => 'Alexander Mueller <alex@littleblue.de>',
+             "yoshiki"     => 'Yoshiki Hayashi <yoshiki@xemacs.org>',
+             "david"       => 'David Summers <david@summersoft.fay.ar.us>',
+             "rassilon"    => 'Bill Tutt <rassilon@lyra.org>',
+             "kbohling"    => 'Kirby C. Bohling <kbohling@birddog.com>', );
+
+%months = ( "Jan" => "01",
+            "Feb" => "02",
+            "Mar" => "03",
+            "Apr" => "04",
+            "May" => "05",
+            "Jun" => "06",
+            "Jul" => "07",
+            "Aug" => "08",
+            "Sep" => "09",
+            "Oct" => "10",
+            "Nov" => "11",
+            "Dec" => "12", );
+
+$parse_next_line = 0;
+
+while (<>) {
+  # axe windows style line endings, since we should try to be consistent, and 
+  # the repos has both styles in it's log entries.
+  $_ =~ s/\r\n$/\n/;
+
+  if (/^-+$/) {
+    # we're at the start of a log entry, so we need to parse the next line
+    $parse_next_line = 1;
+  } elsif ($parse_next_line) {
+    # transform from svn style to GNU style
+    $parse_next_line = 0;
+
+    @parts = split (/ /, $_);
+
+    print "$parts[8]-$months{$parts[7]}-$parts[6] $hackers{$parts[3]}\n";
+  } else {
+    print "\t$_";
+  }
+}
