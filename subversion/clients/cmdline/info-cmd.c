2fb922c6b7d0c3b60f4ece8154693d566c22dd82
C. Michael Pilato <cmpilato@apache.org>
2002-04-28 21:07:59 +0000

    * subversion/clients/cmdline/main.c   (svn_cl__cmd_table): Change usage for `info' to reflect an optional first target.
    * subversion/clients/cmdline/info-cmd.c
      (svn_cl__info): Allow implict `.' target.  Rearrange some logic in
    this function.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@841893 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/clients/cmdline/info-cmd.c
+++ subversion/clients/cmdline/info-cmd.c
@@ -57,114 +57,112 @@ svn_cl__info (apr_getopt_t *os,
 
   targets = svn_cl__args_to_target_array (os, opt_state, FALSE, pool);
 
-  if (targets->nelts == 0)
-    return svn_error_create (SVN_ERR_CL_INSUFFICIENT_ARGS, 0, 0, pool, "");
+  /* Add "." if user passed 0 arguments. */
+  svn_cl__push_implicit_dot_target (targets, pool);
 
   for (i = 0; i < targets->nelts; i++)
     {
       svn_stringbuf_t *target = ((svn_stringbuf_t **) (targets->elts))[i];
       svn_wc_entry_t *entry;
 
-      SVN_ERR (svn_wc_entry (&entry, target, pool));
+      printf ("Path: %s\n", target->data);
 
+      SVN_ERR (svn_wc_entry (&entry, target, pool));
       if (! entry)
         {
-          printf ("'%s' is not a versioned resource.\n", target->data);
+          /* Print non-versioned message and extra newline separator. */
+          printf ("(Not a versioned resource)\n\n");
+          continue;
         }
-      else
+
+      /* Note: we have to be paranoid about checking that these are
+         valid, since svn_wc_entry() doesn't fill them in if they
+         aren't in the entries file. */
+
+      if ((entry->name) 
+          && strcmp (entry->name->data, SVN_WC_ENTRY_THIS_DIR))
+        printf ("Name: %s\n", entry->name->data);
+      
+      if (entry->url)
+        printf ("Url: %s\n", entry->url->data);
+          
+      if (entry->repos)
+        printf ("Repository: %s\n", entry->repos->data);
+
+      if (SVN_IS_VALID_REVNUM (entry->revision))
+        printf ("Revision: %ld\n", entry->revision);
+
+      switch (entry->kind) 
         {
-          /* note: we have to be paranoid about checking that these are valid,
-             since svn_wc_entry() doesn't fill them in if they aren't in the 
-             entries file. */
-
-          if (entry->name)
-            printf ("Name: %s\n", entry->name->data);
-
-          if (entry->url)
-            printf ("Url: %s\n", entry->url->data);
-
-          if (entry->repos)
-            printf ("Repository: %s\n", entry->repos->data);
-
-          if (SVN_IS_VALID_REVNUM (entry->revision))
-            printf ("Revision: %ld\n", entry->revision);
-
-          if (entry->kind)
-            {
-              switch (entry->kind) {
-              case svn_node_file:
-                printf ("Node Kind: file\n");
-                break;
-
-              case svn_node_dir:
-                printf ("Node Kind: directory\n");
-                break;
-
-              case svn_node_unknown:
-                printf ("Node Kind: unknown\n");
-                break;
-
-              case svn_node_none:
-                printf ("Node Kind: none\n");
-                break;
-
-              default:
-                break;
-              }
-            }
-
-          if (entry->schedule)
-            {
-              switch (entry->schedule) {
-              case svn_wc_schedule_normal:
-                printf ("Schedule: normal\n");
-                break;
-
-              case svn_wc_schedule_add:
-                printf ("Schedule: add\n");
-                break;
-
-              case svn_wc_schedule_delete:
-                printf ("Schedule: delete\n");
-                break;
-
-              case svn_wc_schedule_replace:
-                printf ("Schedule: replace\n");
-                break;
-
-              default:
-                break;
-              }
-            }
-
-          if (entry->copied)
-            {
-              if (entry->copyfrom_url)
-                printf ("Copied From Url: %s\n", entry->copyfrom_url->data);
-
-              if (SVN_IS_VALID_REVNUM (entry->copyfrom_rev))
-                printf ("Copied From Rev: %ld\n", entry->copyfrom_rev);
-            }
-
-          if (entry->cmt_author)
-            printf ("Last Changed Author: %s\n", entry->cmt_author->data);
-
-          if (SVN_IS_VALID_REVNUM (entry->cmt_rev))
-            printf ("Last Changed Rev: %ld\n", entry->cmt_rev);
-
-          if (entry->cmt_date)
-            svn_cl__info_print_time (entry->cmt_date, "Last Changed Date");
-
-          if (entry->text_time)
-            svn_cl__info_print_time (entry->text_time, "Text Last Updated");
-
-          if (entry->prop_time)
-            svn_cl__info_print_time (entry->prop_time,
-                                     "Properties Last Updated");
-
-          if (entry->checksum)
-            printf ("Checksum: %s\n", entry->checksum->data);
+        case svn_node_file:
+          printf ("Node Kind: file\n");
+          break;
+          
+        case svn_node_dir:
+          printf ("Node Kind: directory\n");
+          break;
+          
+        case svn_node_none:
+          printf ("Node Kind: none\n");
+          break;
+          
+        case svn_node_unknown:
+        default:
+          printf ("Node Kind: unknown\n");
+          break;
         }
+
+      switch (entry->schedule) 
+        {
+        case svn_wc_schedule_normal:
+          printf ("Schedule: normal\n");
+          break;
+          
+        case svn_wc_schedule_add:
+          printf ("Schedule: add\n");
+          break;
+          
+        case svn_wc_schedule_delete:
+          printf ("Schedule: delete\n");
+          break;
+          
+        case svn_wc_schedule_replace:
+          printf ("Schedule: replace\n");
+          break;
+          
+        default:
+          break;
+        }
+
+      if (entry->copied)
+        {
+          if (entry->copyfrom_url)
+            printf ("Copied From Url: %s\n", entry->copyfrom_url->data);
+
+          if (SVN_IS_VALID_REVNUM (entry->copyfrom_rev))
+            printf ("Copied From Rev: %ld\n", entry->copyfrom_rev);
+        }
+
+      if (entry->cmt_author)
+        printf ("Last Changed Author: %s\n", entry->cmt_author->data);
+
+      if (SVN_IS_VALID_REVNUM (entry->cmt_rev))
+        printf ("Last Changed Rev: %ld\n", entry->cmt_rev);
+
+      if (entry->cmt_date)
+        svn_cl__info_print_time (entry->cmt_date, "Last Changed Date");
+
+      if (entry->text_time)
+        svn_cl__info_print_time (entry->text_time, "Text Last Updated");
+
+      if (entry->prop_time)
+        svn_cl__info_print_time (entry->prop_time, "Properties Last Updated");
+
+      if (entry->checksum)
+        printf ("Checksum: %s\n", entry->checksum->data);
+
+      /* Print extra newline separator. */
+      printf ("\n");
     }
 
   return SVN_NO_ERROR;
