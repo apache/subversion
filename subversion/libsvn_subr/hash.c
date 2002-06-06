427f22630768b5e2287ad4ef5ccb654baa354e31
Karl Fogel <kfogel@apache.org>
2002-06-06 16:13:28 +0000

    Score two for Stein-o-Vision:
    * subversion/libsvn_subr/hash.c
      (svn_unpack_bytestring): Remove one useless cast, and one incorrect
      cast.  Note that there's still some suspicious casting going on in
      this file, though, see svn_hash_write.
    
    *** NOTE: this commit also shows the addition of svn_hash_diff(),
    *** which actually happened in r2092.  For why this happened, see
    *** http://subversion.tigris.org/issues/show_bug.cgi?id=771#desc5.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@842176 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/hash.c
+++ subversion/libsvn_subr/hash.c
@@ -76,11 +76,11 @@
 apr_size_t 
 svn_unpack_bytestring (char **returndata, void *value)
 {
-  svn_stringbuf_t *valstring = (svn_stringbuf_t *) value;
+  svn_stringbuf_t *valstring = value;
 
   *returndata = valstring->data;
 
-  return (size_t) valstring->len;
+  return valstring->len;
 }
 
 
@@ -306,6 +306,47 @@ svn_hash_read (apr_hash_t *hash,
     } /* while (1) */
 
 }
+
+
+svn_error_t *
+svn_hash_diff (apr_hash_t *hash_a,
+               apr_hash_t *hash_b,
+               svn_hash_diff_func_t diff_func,
+               void *diff_func_baton,
+               apr_pool_t *pool)
+{
+  apr_hash_index_t *hi;
+
+  for (hi = apr_hash_first (pool, hash_a); hi; hi = apr_hash_next (hi))
+    {
+      const void *key;
+      apr_ssize_t klen;
+      
+      apr_hash_this (hi, &key, &klen, NULL);
+
+      if (apr_hash_get (hash_b, key, klen))
+        SVN_ERR ((*diff_func) (key, klen, svn_hash_diff_key_both,
+                               diff_func_baton));
+      else
+        SVN_ERR ((*diff_func) (key, klen, svn_hash_diff_key_a,
+                               diff_func_baton));
+    }
+
+  for (hi = apr_hash_first (pool, hash_b); hi; hi = apr_hash_next (hi))
+    {
+      const void *key;
+      apr_ssize_t klen;
+      
+      apr_hash_this (hi, &key, &klen, NULL);
+
+      if (! apr_hash_get (hash_a, key, klen))
+        SVN_ERR ((*diff_func) (key, klen, svn_hash_diff_key_b,
+                               diff_func_baton));
+    }
+
+  return SVN_NO_ERROR;
+}
+
 
 
 
