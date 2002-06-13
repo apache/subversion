f5244e162ae7a9e4f194fb33571eded441449836
Garrett Rooney <rooneg@apache.org>
2002-06-13 01:51:23 +0000

    By popular demand, remove the 'svn rollback' command.  There are a number of opinions as to what the semantics of the command should be, and even if we could decide on the semantics, there are non-trivial issues with dealing with mixed-revision working copies.  All the functionality is available via the merge command anyway, so the consensus is that it is better to have the user specifically say what they want via 'svn merge' than to have them be confused by the behavior of 'svn rollback'.
    * subversion/clients/cmdline/cl.h
      remove the declaration of svn_cl__rollback.
    
    * subversion/clients/cmdline/rollback-cmd.c
      remove this file.
    
    * subversion/clients/cmdline/main.c
      (svn_cl__cmd_table): remove the 'rollback' command's entry.
    
    * subversion/tests/clients/cmdline/getopt_tests_data/svn_help_stdout
    * subversion/tests/clients/cmdline/getopt_tests_data/svn_stderr
    * subversion/tests/clients/cmdline/getopt_tests_data/svn--help_stdout
      remove the rollback command from the getopt tests.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@842252 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/clients/cmdline/main.c
+++ subversion/clients/cmdline/main.c
@@ -303,11 +303,6 @@ const svn_cl__cmd_desc_t svn_cl__cmd_table[] =
     "    to be committed again.\n",
     {svn_cl__targets_opt} },
  
-  { "rollback", svn_cl__rollback, {0},
-    "Revert a specific revision in the working copy.\n"
-    "usage: rollback -r REVNUM [TARGETS]\n\n",
-    {'r', svn_cl__targets_opt} },
- 
   { "status", svn_cl__status, {"stat", "st"},
     "Print the status of working copy files and directories.\n"
     "usage: svn status [TARGETS]\n\n"
