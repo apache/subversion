7bb037ad961549fe50fc93b2cbb212b75b59691b
Blair Zajac <blair@apache.org>
2002-06-18 01:14:07 +0000

    * build/buildcheck.sh
      Require neon 0.21.2.  A bug in neon 0.21.1 was found by Justin
      Erenkrantz using svn with mod_deflate if zlib was used for
      on-the-wire compression.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@842338 13f79535-47bb-0310-9956-ffa450edef68

--- build/buildcheck.sh
+++ build/buildcheck.sh
@@ -55,7 +55,7 @@ echo "buildcheck: libtool version $lt_pversion (ok)"
 #--------------------------------------------------------------------------
 # check for the correct version of Neon
 #
-NEON_WANTED_REGEX=0.21.[12]
+NEON_WANTED_REGEX=0.21.2
 NEON_LATEST_WORKING_VER=0.21.2
 NEON_URL="http://www.webdav.org/neon/neon-${NEON_LATEST_WORKING_VER}.tar.gz"
 NEON_TEST_REGEX="$NEON_WANTED_REGEX"
