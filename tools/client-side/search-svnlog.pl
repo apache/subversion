fd7e72d42716b14240219d229b4dd0e6af60d27a
Blair Zajac <blair@apache.org>
2002-07-02 18:04:04 +0000

    Some Perl script cleanups.
    * tools/client-side/search-svnlog.pl:
      (usage): New subroutine.  Print the script's usage and what it does.
      (main):
        Replace custom command line processing code with Getopt::Long.
        Do better checking of variable values by using if (defined $var)
          instead of if ($var) which fails is $var is a file named `0'.
        Print $! if open()'s fail.
        Explicitly close the `svn log' pipe or the log filename and check
          for an error.
        Print the script's name when it wants to die.
        Add CollabNet copyright.
      
    * tools/hook-scripts/commit-email.pl:
      Add the svn:executable property.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@842472 13f79535-47bb-0310-9956-ffa450edef68

--- tools/client-side/search-svnlog.pl
+++ tools/client-side/search-svnlog.pl
@@ -1,47 +1,53 @@
 #!/usr/bin/perl -w
 
-### Show log messages matching a certain pattern.  Usage:
-###
-###    search-svnlog.pl REGEXP [ -f LOGFILE ]
-###
-### It will print only log messages matching REGEXP.  If -f LOGFILE is
-### passed, then instead of running "svn log", the log data will be
-### read from LOGFILE (which should be in the same format as the
-### output of "svn log").
-### 
-### Note:
-### In the future, this may take pathnames and/or revision numbers as
-### arguments.  Then it will need to do real argument parsing.
-### Personally, I'm not going to bother with that right now; it's
-### useful enough as is.
+# ====================================================================
+# Show log messages matching a certain pattern.  Usage:
+#
+#    search-svnlog.pl [-f LOGFILE] REGEXP
+#
+# It will print only log messages matching REGEXP.  If -f LOGFILE is
+# passed, then instead of running "svn log", the log data will be
+# read from LOGFILE (which should be in the same format as the
+# output of "svn log").
+#
+# Note:
+# In the future, this may take pathnames and/or revision numbers as
+# arguments.
+#
+# ====================================================================
+# Copyright (c) 2000-2002 CollabNet.  All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.  The terms
+# are also available at http://subversion.tigris.org/license-1.html.
+# If newer versions of this license are posted there, you may use a
+# newer version instead, at your option.
+#
+# This software consists of voluntary contributions made by many
+# individuals.  For exact contribution history, see the revision
+# history and logs, available at http://subversion.tigris.org/.
+# ====================================================================
 
 use strict;
+use Getopt::Long;
 
-my $filter = shift || die ("Usage: $0 REGEXP\n");
+my $log_file;
 
-# ### Could do real option parsing here...
-my $dash_f = shift;
-my $logfile;
-if (defined ($dash_f) && ($dash_f eq "-f")) {
-  $logfile = shift;
-  die ("-f option needs argument") if (! $logfile);
-}
-elsif (defined ($dash_f)) {
-  die ("unknown option ${dash_f}");
-}
-    
+GetOptions('file=s' => \$log_file) or
+  &usage;
+
+&usage("$0: too few arguments") unless @ARGV;
+&usage("$0: too many arguments") if @ARGV > 1;
+
+my $filter = shift;
 
 my $log_cmd = "svn log";
 
-my $log_separator =
-  "------------------------------------------------------------------------\n";
+my $log_separator = '-' x 72 . "\n";
 
-if ($logfile) {
-  open (LOGDATA, "<$logfile") or die ("Unable to open \"$logfile\".\n");
-}
-else {
-  open (LOGDATA, "$log_cmd |") or die ("Unable to run \"$log_cmd\".\n");
-}
+my $open_string = defined $log_file ? $log_file : "$log_cmd |";
+open(LOGDATA, $open_string) or
+  die "$0: cannot open `$open_string' for reading: $!\n";
 
 my $this_entry_accum = "";
 my $this_rev = -1;
@@ -59,7 +65,7 @@ while (<LOGDATA>)
   {
     if (! ($_ eq $log_separator))
     {
-      die ("Wrong number of lines for log message!\n${this_entry_accum}\n");
+      die "$0: wrong number of lines for log message!\n${this_entry_accum}\n";
     }
 
     if ($this_entry_accum =~ /$filter/og)
@@ -73,11 +79,26 @@ while (<LOGDATA>)
   }
   elsif ($this_lines < 0)
   {
-    die ("Line weirdness parsing log.\n");
+    die "$0: line weirdness parsing log.\n";
   }
   else   # Must be inside a message, continue accumulating.
   {
     $this_entry_accum .= $_;
     $this_lines--;
   }
+}
+
+close(LOGDATA) or
+  die "$0: closing `$open_string' failed: $!\n";
+
+exit 0;
+
+sub usage {
+  warn "@_\n" if @_;
+  die "usage: $0: [-f LOGFILE] REGEXP\n",
+      "This script will print only log messages matching REGEXP by\n",
+      "running `svn log' in the current working directory.  If\n",
+      "-f LOGFILE is passed, then instead of running `svn log', the\n",
+      "log data will be read from LOGFILE (which should be in the\n",
+      "same format as the output of `svn log').\n";
 }
