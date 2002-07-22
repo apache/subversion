781b989777c9d41e6b664b1868350c1ede61df3b
Karl Fogel <kfogel@apache.org>
2002-07-22 01:41:01 +0000

    * subversion/tests/clients/cmdline/module_tests.py   (externals_test_setup): Init correct expected status.
    Now module_tests.py passes again.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@842690 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/clients/cmdline/module_tests.py
+++ subversion/tests/clients/cmdline/module_tests.py
@@ -184,7 +184,7 @@ def externals_test_setup(sbox):
 
   expected_status = svntest.actions.get_virginal_state(wc_init_dir, 5)
   expected_status.tweak(repos_rev=6)
-  expected_status.tweak('A/B', 'A/D', wc_rev=6)
+  expected_status.tweak('A/B', 'A/D', wc_rev=6, status='__')
 
   return svntest.actions.run_and_verify_commit(wc_init_dir,
                                                expected_output,
