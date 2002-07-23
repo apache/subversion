daf55bf1185fa0d4fc4d9b3fc10f3fbf492eaaab
Karl Fogel <kfogel@apache.org>
2002-07-23 22:23:12 +0000

    Mark Alpha:
    * CHANGES: Predict the new revision.
    
    * subversion/include/svn_version.h
      (SVN_VER_MINOR, SVN_VER_MICRO): Bump to 14 and 0 respectively.
    
    * www/project_status.html: Update Alpha.
    
    * www/project_faq.html, www/project_source.html: Checkout no longer
      needs the "-d" option.  Adjust accordingly.
    
    [Actual Alpha was 2667; see intervening commits for why.]
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@842732 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_version.h
+++ subversion/include/svn_version.h
@@ -43,11 +43,11 @@ extern "C" {
 /* Minor version number.
    Modify when new functionality is added or new interfaces are
    defined, but all changes are backward compatible. */
-#define SVN_VER_MINOR      13
+#define SVN_VER_MINOR      14
 
 /* Patch number.
    Modify for every released patch. */
-#define SVN_VER_MICRO      2
+#define SVN_VER_MICRO      0
 
 
 /* Version tag: a string describing the version.
