c176270f9a47ee464a6dc67fae73fd9c6adb2111
Karl Fogel <kfogel@apache.org>
2002-07-29 20:29:58 +0000

    * subversion/libsvn_subr/validate.c: Oops, forgot to add this in   previous revision.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@842849 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subversion/libsvn_subr/validate.c
@@ -0,0 +1,69 @@
+/*
+ * validate.c:  validation routines
+ *
+ * ====================================================================
+ * Copyright (c) 2000-2002 CollabNet.  All rights reserved.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at http://subversion.tigris.org/license-1.html.
+ * If newer versions of this license are posted there, you may use a
+ * newer version instead, at your option.
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals.  For exact contribution history, see the revision
+ * history and logs, available at http://subversion.tigris.org/.
+ * ====================================================================
+ */
+
+/* ==================================================================== */
+
+
+
+/*** Includes. ***/
+
+#include <apr_lib.h>
+#define APR_WANT_STRFUNC
+#include <apr_want.h>
+
+#include "svn_string.h"
+#include "svn_pools.h"
+#include "svn_error.h"
+#include "svn_props.h"
+
+
+
+/*** Code. ***/
+
+svn_error_t *
+svn_validate_mime_type (const char *mime_type, apr_pool_t *pool)
+{
+  if (strchr (mime_type, '/') == NULL)
+    return svn_error_createf
+      (SVN_ERR_BAD_MIME_TYPE, 0, NULL, pool
+       "Mime type \"%s\" missing '/'\n", mime_type);
+
+  {
+    /* Could just take an optional `len' arg and avoid this strlen.
+       Many callers have the length anyway.  But is such a
+       micro-optimization worth the extra interface noise?  Is it
+       even worth this comment?? */
+    char c = mime_type[strlen(mime_type) - 1];
+    
+    /* Man page seems to claim that isalnum is always a function.
+       But I don't trust it. */
+    if (! apr_isalnum (c))
+      return svn_error_createf
+        (SVN_ERR_BAD_MIME_TYPE, 0, NULL, pool
+         "Mime type \"%s\" ends with non-alphanumeric.\n", mime_type);
+  }
+
+  return SVN_NO_ERROR;
+}
+
+
+
+/* 
+ * local variables:
+ * eval: (load-file "../../tools/dev/svn-dev.el")
+ * end: */
