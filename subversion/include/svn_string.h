71b196ea17de9715e2e21a0b6d4a71bd18e5bbac
Philip Martin <philip@apache.org>
2002-08-26 10:37:41 +0000

    Use STR->len rather than -1 to indicate no whitespace, making first_non_whitespace consistent with find_char_backward.
    * subversion/include/svn_string.h (svn_string_first_non_whitespace,
      svn_stringbuf_first_non_whitespace): Make return type apr_size_t and
      use STR->len rather than -1.
    
    * subversion/libsvn_subr/svn_string.c
      (svn_string_first_non_whitespace, svn_stringbuf_first_non_whitespace): Make
      return type apr_size_t and use STR->len rather than -1.
      (svn_string_strip_whitespace, svn_stringbuf_strip_whitespace): Use
      apr_size_t for offset.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@843135 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_string.h
+++ subversion/include/svn_string.h
@@ -129,8 +129,9 @@ svn_boolean_t svn_string_compare (const svn_string_t *str1,
 
 /** convenience routines **/
 
-/* Return offset of first non-whitespace character in STR, or -1 if none. */
-apr_ssize_t svn_string_first_non_whitespace (const svn_string_t *str);
+/* Return offset of first non-whitespace character in STR, or return
+   STR->len if none. */
+apr_size_t svn_string_first_non_whitespace (const svn_string_t *str);
 
 /* Strips whitespace from both sides of STR (modified in place). */
 void svn_string_strip_whitespace (svn_string_t *str);
@@ -211,8 +212,9 @@ svn_boolean_t svn_stringbuf_compare (const svn_stringbuf_t *str1,
 
 /** convenience routines **/
 
-/* Return offset of first non-whitespace character in STR, or -1 if none. */
-apr_ssize_t svn_stringbuf_first_non_whitespace (const svn_stringbuf_t *str);
+/* Return offset of first non-whitespace character in STR, or return
+   STR->len if none. */
+apr_size_t svn_stringbuf_first_non_whitespace (const svn_stringbuf_t *str);
 
 /* Strips whitespace from both sides of STR (modified in place). */
 void svn_stringbuf_strip_whitespace (svn_stringbuf_t *str);
