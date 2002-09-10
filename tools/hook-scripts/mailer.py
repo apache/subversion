321c70ecc15c546c63c39502377548eb598d1439
Greg Stein <gstein@apache.org>
2002-09-10 03:47:16 +0000

    Continued work on the commit mailer script.
    The mailer.conf file is now processed. The script also looks in a
    couple default places for it, or whether the cmdline args say it is.
    Some internal refactoring occurred for eventual diff generation and
    for writing to arbitrary output streams (e.g. a pipe into the mail
    delivery program).
    
    * mailer.py:
      (class Repository): new class to hold various bits of state
        corresponding to the repository that we're working against.
      (class Config): new class to parse and hold configuration info.
      (class _sub_section): empty class for holding values as named
        attributes.
      (generate_content): new function, with the body factored out of
        main(). it now takes an output stream rather than assuming stdout
        and print'ing. it also builds a list of all affected paths and
        calls generate_diff() to generate (diff) output for them.
      (generate_list): renamed from print_list(). now takes an output
        stream.
      (generate_diff): new function, mostly stubbed, for generating diff
        output.
      (main): rewritten to use the new Config and Repository classes, and
        to refactor the output generation into generate_content().
      (__main__ idiom): rebuilt to have an optional config file, and to
        attempt to search for the file, if not provided, in a couple
        standard locations.
    
    * mailer.conf.dist: an example mailer.conf file. still quite empty.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@843226 13f79535-47bb-0310-9956-ffa450edef68

--- tools/hook-scripts/mailer.py
+++ tools/hook-scripts/mailer.py
@@ -2,7 +2,7 @@
 #
 # mailer.py: send email describing a commit
 #
-# USAGE: mailer.py CONFIG-FILE REPOS-DIR REVISION
+# USAGE: mailer.py REPOS-DIR REVISION [CONFIG-FILE]
 #
 #   Using CONFIG-FILE, deliver an email describing the changes between
 #   REV and REV-1 for the repository REPOS.
@@ -16,24 +16,16 @@ from svn import fs, util, delta, _repos
 
 
 def main(pool, config_fname, repos_dir, rev):
-#  cfg = load_config(config_fname)
+  cfg = Config(config_fname)
 
-  db_path = os.path.join(repos_dir, 'db')
-  if not os.path.exists(db_path):
-    db_path = repos_dir
+  repos = Repository(repos_dir, rev, pool)
 
-  fs_ptr = fs.new(pool)
-  fs.open_berkeley(fs_ptr, db_path)
-
-  root_prev = fs.revision_root(fs_ptr, rev-1, pool)
-  root_this = fs.revision_root(fs_ptr, rev, pool)
-
-  editor = ChangeCollector(root_prev)
+  editor = ChangeCollector(repos.root_prev)
 
   e_ptr, e_baton = delta.make_editor(editor, pool)
   wrap_editor, wrap_baton = delta.svn_delta_compat_wrap(e_ptr, e_baton, pool)
 
-  _repos.svn_repos_dir_delta(root_prev, '', None, root_this, '',
+  _repos.svn_repos_dir_delta(repos.root_prev, '', None, repos.root_this, '',
                              wrap_editor, wrap_baton,
                              0,  # text_deltas
                              1,  # recurse
@@ -41,33 +33,99 @@ def main(pool, config_fname, repos_dir, rev):
                              1,  # use_copy_history
                              pool)
 
-  date = fs.revision_prop(fs_ptr, rev, util.SVN_PROP_REVISION_DATE, pool)
+  ### pipe it to sendmail rather than stdout
+  generate_content(sys.stdout, repos, editor, pool)
+
+
+def generate_content(output, repos, editor, pool):
+
+  date = repos.get_rev_prop(util.SVN_PROP_REVISION_DATE)
   ### reformat the date
 
-  ### print for now. soon: spool into mailer.
-  print 'Author:', fs.revision_prop(fs_ptr, rev,
-                                    util.SVN_PROP_REVISION_AUTHOR, pool)
-  print 'Date:', date
-  print 'New Revision:', rev
-  print
-  print_list('Added', editor.adds)
-  print_list('Removed', editor.deletes)
-  print_list('Modified', editor.changes)
-  print 'Log:'
-  print fs.revision_prop(fs_ptr, rev, util.SVN_PROP_REVISION_LOG, pool) or ''
+  output.write('Author: %s\nDate: %s\nNew Revision: %s\n\n'
+               % (repos.get_rev_prop(util.SVN_PROP_REVISION_AUTHOR),
+                  date,
+                  repos.rev))
+
+  generate_list(output, 'Added', editor.adds)
+  generate_list(output, 'Removed', editor.deletes)
+  generate_list(output, 'Modified', editor.changes)
+
+  output.write('Log:\n%s\n'
+               % (repos.get_rev_prop(util.SVN_PROP_REVISION_LOG) or ''))
+
+  # build a complete list of affected dirs/files
+  paths = editor.adds.keys() + editor.deletes.keys() + editor.changes.keys()
+  paths.sort()
+
+  for path in paths:
+    if editor.adds.has_key(path):
+      src = None
+      dst = path
+      info = editor.adds[path]
+    elif editor.deletes.has_key(path):
+      src = path
+      dst = None
+      info = editor.deletes[path]
+    else:
+      src = dst = path
+      info = editor.changes[path]
+
+    generate_diff(output, repos, src, dst, info, pool)
 
   ### print diffs. watch for binary files.
 
 
-def print_list(header, fnames):
+def generate_list(output, header, fnames):
   if fnames:
-    print header + ':'
-    for fname, (item_type, prop_changes, copy_info) in fnames.items():
-      ### should print prop_changes and copy_info here
+    output.write('%s:\n' % header)
+    items = fnames.items()
+    items.sort()
+    for fname, (item_type, prop_changes, copy_info) in items:
+      ### should print prop_changes?, copy_info, and binary? here
+      ### hmm. don't have binary right now.
       if item_type == ChangeCollector.DIR:
-        print '   ' + fname + '/'
+        output.write('   %s/\n' % fname)
       else:
-        print '   ' + fname
+        output.write('   %s\n' % fname)
+
+
+def generate_diff(output, repos, src, dst,
+                  (item_type, prop_changes, copy_info), pool):
+  if copy_info and copy_info[0]:
+    assert (not src) and dst  # it was ADDED
+
+    copyfrom_path = copy_info[0]
+    if copyfrom_path[0] == '/':
+      # remove the leading slash for consistency with other paths
+      copyfrom_path = copyfrom_path[1:]
+
+    output.write('Copied: %s (from rev %d, %s)\n'
+                 % (dst, copy_info[1], copy_info[0]))
+
+  print src, dst, item_type, prop_changes, copy_info
+
+
+class Repository:
+  "Hold roots and other information about the repository."
+
+  def __init__(self, repos_dir, rev, pool):
+    self.repos_dir = repos_dir
+    self.rev = rev
+    self.pool = pool
+
+    db_path = os.path.join(repos_dir, 'db')
+    if not os.path.exists(db_path):
+      db_path = repos_dir
+
+    self.fs_ptr = fs.new(pool)
+    fs.open_berkeley(self.fs_ptr, db_path)
+
+    self.root_prev = fs.revision_root(self.fs_ptr, rev-1, pool)
+    self.root_this = fs.revision_root(self.fs_ptr, rev, pool)
+
+  def get_rev_prop(self, propname):
+    return fs.revision_prop(self.fs_ptr, self.rev, propname, self.pool)
 
 
 class ChangeCollector(delta.Editor):
@@ -153,19 +211,49 @@ class ChangeCollector(delta.Editor):
                                    ]
 
 
-def load_config(fname):
-  cp = ConfigParser.ConfigParser()
-  cp.read(fname)
+class Config:
+  def __init__(self, fname):
+    cp = ConfigParser.ConfigParser()
+    cp.read(fname)
 
-  ### figure out config and construct the right obs...
+    for section in cp.sections():
+      if not hasattr(self, section):
+        setattr(self, section, _sub_section())
+      section_ob = getattr(self, section)
+      for option in cp.options(section):
+        # get the raw value -- we use the same format for *our* interpolation
+        value = cp.get(section, option, raw=1)
+        setattr(section_ob, option, value)
 
-  return cp
+
+class _sub_section:
+  pass
+
+
+class MissingConfig(Exception):
+  pass
 
 
 if __name__ == '__main__':
-  if len(sys.argv) != 4:
-    sys.stderr.write('USAGE: %s CONFIG-FILE REPOS-DIR REVISION\n')
+  if len(sys.argv) < 3 or len(sys.argv) > 4:
+    sys.stderr.write('USAGE: %s REPOS-DIR REVISION [CONFIG-FILE]\n')
     sys.exit(1)
 
+  repos_dir = sys.argv[1]
+  revision = int(sys.argv[2])
+
+  if len(sys.argv) == 3:
+    # default to REPOS-DIR/conf/mailer.conf
+    config_fname = os.path.join(repos_dir, 'conf', 'mailer.conf')
+    if not os.path.exists(config_fname):
+      # okay. look for 'mailer.conf' as a sibling of this script
+      config_fname = os.path.join(os.path.dirname(sys.argv[0]), 'mailer.conf')
+  else:
+    # the config file was explicitly provided
+    config_fname = sys.argv[3]
+
+  if not os.path.exists(config_fname):
+    raise MissingConfig(config_fname)
+
   ### run some validation on these params
-  util.run_app(main, sys.argv[1], sys.argv[2], int(sys.argv[3]))
+  util.run_app(main, config_fname, repos_dir, revision)
