5f2cf69d2e38e2805480c11cfa8a931b8b7ac095
Blair Zajac <blair@apache.org>
2002-09-18 21:05:13 +0000

    Enable builds against Neon 0.23.4.
    * build/buildcheck.sh:
      Require any version of Neon matching 0.23.?.
    
    * INSTALL:
      Update to use Neon 0.23.4.
    
    * CHANGES:
      Mention Neon 0.23.4.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@843261 13f79535-47bb-0310-9956-ffa450edef68

--- build/buildcheck.sh
+++ build/buildcheck.sh
@@ -55,8 +55,8 @@ echo "buildcheck: libtool version $lt_pversion (ok)"
 #--------------------------------------------------------------------------
 # check for the correct version of Neon
 #
-NEON_WANTED_REGEX=0.23.[1-3]
-NEON_LATEST_WORKING_VER=0.23.3
+NEON_WANTED_REGEX=0.23.?
+NEON_LATEST_WORKING_VER=0.23.4
 NEON_URL="http://www.webdav.org/neon/neon-${NEON_LATEST_WORKING_VER}.tar.gz"
 NEON_TEST_REGEX="$NEON_WANTED_REGEX"
 if test "$NEON_CHECK_CONTROL" = "--disable-neon-version-check"; then
