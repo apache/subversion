debb7b19c7456aa14d1d8dc2e4b2f01d18d529fd
Blair Zajac <blair@apache.org>
2002-10-08 20:07:40 +0000

    Update to use Neon 0.23.5.
    * build/buildcheck.sh:
      Mention Neon 0.23.5 as latest version.
    
    * INSTALL:
      Update to use Neon 0.23.5.
    
    * CHANGES:
      Mention Neon 0.23.5.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@843407 13f79535-47bb-0310-9956-ffa450edef68

--- build/buildcheck.sh
+++ build/buildcheck.sh
@@ -56,7 +56,7 @@ echo "buildcheck: libtool version $lt_pversion (ok)"
 # check for the correct version of Neon
 #
 NEON_WANTED_REGEX=0.23.?
-NEON_LATEST_WORKING_VER=0.23.4
+NEON_LATEST_WORKING_VER=0.23.5
 NEON_URL="http://www.webdav.org/neon/neon-${NEON_LATEST_WORKING_VER}.tar.gz"
 NEON_TEST_REGEX="$NEON_WANTED_REGEX"
 if test "$NEON_CHECK_CONTROL" = "--disable-neon-version-check"; then
