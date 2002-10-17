6c194f123684163a14b3ef128e3f5b3ef9ef322a
Greg Hudson <ghudson@apache.org>
2002-10-17 15:05:03 +0000

    Implement ra_dav support for getting a revision by date.  DeltaV provides no way of doing this, so we use a custom report.
    * mod_dav_svn/version.c
      (dav_svn__drev_report): New function to deliver a dated-rev report.
      (dav_svn_deliver_report): Recognize dated-rev report.
    * libsvn_ra_dav/ra_dav.h (ELEM_dated_rev_report): New constant.
    * libsvn_ra_dav/fetch.c
      (svn_ra_dav__get_dated_revision): Implement by making the custom
      report and parsing the answer.  Returns a reasonable error message
      if the server doesn't support the new report.
      (drev_report_elements, drev_validate_element, drev_start_element,
      drev_end_element): New helper functions for the dated-rev
      implementation.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@843458 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/mod_dav_svn/version.c
+++ subversion/mod_dav_svn/version.c
@@ -26,6 +26,7 @@
 #include "svn_xml.h"
 #include "svn_repos.h"
 #include "svn_dav.h"
+#include "svn_time.h"
 
 #include "dav_svn.h"
 
@@ -474,6 +475,61 @@ static int dav_svn_report_label_header_allowed(const apr_xml_doc *doc)
   return 0;
 }
 
+/* Respond to a S:dated-rev-report request.  The request contains a
+ * DAV:creationdate element giving the requested date; the response
+ * contains a DAV:version-name element giving the most recent revision
+ * as of that date. */
+static dav_error * dav_svn__drev_report(const dav_resource *resource,
+                                        const apr_xml_doc *doc,
+                                        ap_filter_t *output)
+{
+  apr_xml_elem *child;
+  int ns;
+  apr_time_t tm = (apr_time_t) -1;
+  svn_revnum_t rev;
+  apr_bucket_brigade *bb;
+
+  /* Find the DAV:creationdate element and get the requested time from it. */
+  ns = dav_svn_find_ns(doc->namespaces, "DAV:");
+  if (ns != -1)
+    {
+      for (child = doc->root->first_child; child != NULL; child = child->next)
+        {
+          if (child->ns != ns || strcmp(child->name, "creationdate") != 0)
+            continue;
+          /* If this fails, we'll notice below. */
+          svn_time_from_nts(&tm, child->first_cdata.first->text,
+                            resource->pool);
+        }
+    }
+
+  if (tm == (apr_time_t) -1)
+    {
+      return dav_new_error(resource->pool, HTTP_BAD_REQUEST, 0,
+                           "The request does not contain a valid "
+                           "'DAV:creationdate' element.");
+    }
+
+  /* Do the actual work of finding the revision by date. */
+  if (svn_repos_dated_revision(&rev, resource->info->repos->repos, tm,
+                               resource->pool) != SVN_NO_ERROR)
+    {
+      return dav_new_error(resource->pool, HTTP_INTERNAL_SERVER_ERROR, 0,
+                           "Could not access revision times.");
+    }
+
+  bb = apr_brigade_create(resource->pool, output->c->bucket_alloc);
+  ap_fprintf(output, bb,
+             DAV_XML_HEADER DEBUG_CR
+             "<S:dated-rev-report xmlns:S=\"" SVN_XML_NAMESPACE "\" "
+             "xmlns:D=\"DAV:\">" DEBUG_CR
+             "<D:version-name>%" SVN_REVNUM_T_FMT "</D:version-name>"
+             "</S:dated-rev-report>", rev);
+  ap_fflush(output, bb);
+
+  return NULL;
+}
+
 static dav_error *dav_svn_deliver_report(request_rec *r,
                                          const dav_resource *resource,
                                          const apr_xml_doc *doc,
@@ -489,9 +545,13 @@ static dav_error *dav_svn_deliver_report(request_rec *r,
         {
           return dav_svn__update_report(resource, doc, output);
         }
-      if (strcmp(doc->root->name, "log-report") == 0)
+      else if (strcmp(doc->root->name, "log-report") == 0)
         {
           return dav_svn__log_report(resource, doc, output);
+        }
+      else if (strcmp(doc->root->name, "dated-rev-report") == 0)
+        {
+          return dav_svn__drev_report(resource, doc, output);
         }
     }
 
