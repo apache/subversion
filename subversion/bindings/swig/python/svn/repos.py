c8481a9e27e88367423217ab461f4d5f37021cc9
C. Michael Pilato <cmpilato@apache.org>
2002-10-30 05:08:04 +0000

    * subversion/bindings/swig/python/svn/repos.py * subversion/bindings/swig/python/svn/wc.py * subversion/bindings/swig/python/svn/client.py * subversion/bindings/swig/python/svn/ra.py   Add wimpy new wrappers for the rest of the shared-object python   modules for naming consistancy and possible future expansion.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@843645 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subversion/bindings/swig/python/svn/repos.py
@@ -0,0 +1,17 @@
+#
+# repos.py : various utilities for interacting with the _repos module
+#
+######################################################################
+#
+# Copyright (c) 2000-2001 CollabNet.  All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.  The terms
+# are also available at http://subversion.tigris.org/license-1.html.
+# If newer versions of this license are posted there, you may use a
+# newer version instead, at your option.
+#
+######################################################################
+#
+
+from _repos import *
