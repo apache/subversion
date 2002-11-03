08f8127b27dd843161b21eb2e7182c44aadea3db
Greg Hudson <ghudson@apache.org>
2002-11-03 17:31:05 +0000

    Migrate svn_ra_local__checkout to svn_repos_checkout.  Essentially no adaptation required, since svn_ra_local__checkout didn't use the ra_local session structure.
    * libsvn_ra_local/ra_local.h (svn_ra_local_checkout): Remove declaration.
    * include/svn_repos.h (svn_repos_checkout): Add declaration.
    
    * libsvn_ra_local/checkout.c: Remove.
    * libsvn_repos/checkout.c: Add (with history).
      Change exported function name and included headers, nothing else.
    
    * libsvn_ra_local/ra_plugin.c (svn_ra_local__do_checkout): Use
      new name for svn_repos_checkout.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@843700 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_ra_local/ra_plugin.c
+++ subversion/libsvn_ra_local/ra_plugin.c
@@ -391,12 +391,12 @@ svn_ra_local__do_checkout (void *session_baton,
   else
     revnum_to_fetch = revision;
 
-  SVN_ERR (svn_ra_local__checkout (sbaton->fs,
-                                   revnum_to_fetch,
-                                   recurse,
-                                   sbaton->repository_URL,
-                                   sbaton->fs_path,
-                                   editor, edit_baton, sbaton->pool));
+  SVN_ERR (svn_repos_checkout (sbaton->fs,
+                               revnum_to_fetch,
+                               recurse,
+                               sbaton->repository_URL,
+                               sbaton->fs_path,
+                               editor, edit_baton, sbaton->pool));
 
   return SVN_NO_ERROR;
 }
