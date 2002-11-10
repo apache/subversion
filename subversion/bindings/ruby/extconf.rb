2672fd3ab698964599569c47dadee38b32a37411
Garrett Rooney <rooneg@apache.org>
2002-11-10 23:43:47 +0000

    * subversion/bindings/ruby/extconf.rb   bring this somewhat up to date.  the ruby bindings are still way behind the   times with regard to almost all of subversion, but it's hard to fix the real   problems if you don't have a makefile, so this seemed like as good a place   to start as any.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@843794 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/ruby/extconf.rb
+++ subversion/bindings/ruby/extconf.rb
@@ -19,6 +19,15 @@ $CFLAGS << ' -I. '
 $LDFLAGS << `apr-config --ldflags`.chop
 $LOCAL_LIBS << `apr-config --libs`.chop
 $CFLAGS << `apr-config --cflags`.chop
+$CFLAGS << `apr-config --includes`.chop
+
+$LDFLAGS << `svn-config --ldflags`.chop
+$CFLAGS << `svn-config --cflags`.chop
+$CFLAGS << `svn-config --includes`.chop
+
+# ick...  svn-config doesn't seem to be adding in the directory the subversion 
+# includes are in, so lets work around that for now.
+$CFLAGS << ' -I' + `svn-config --prefix`.chop + '/include/subversion-1'
 
 # Linux needs -lpthread.
 if PLATFORM =~ /linux/ && $CFLAGS =~ /-pthread/ then
@@ -26,17 +35,27 @@ if PLATFORM =~ /linux/ && $CFLAGS =~ /-pthread/ then
 end
 
 # Extra libraries needed to compile
-libraries = %w{apr svn_subr svn_delta svn_client svn_wc svn_ra}
-libraries.each do |lib| 
-  unless have_library(lib, nil)
+libraries = [
+              ['apr-0',        'apr_initialize'],
+              ['aprutil-0',    'apr_bucket_alloc'],
+              ['svn_subr-1',   'svn_pool_create'],
+              ['svn_delta-1',  'svn_txdelta_next_window'],
+              ['svn_client-1', 'svn_client_commit'],
+              ['svn_wc-1',     'svn_wc_entry'],
+              ['svn_ra-1',     'svn_ra_get_ra_library'],
+]
+
+libraries.each do |lib,func| 
+  unless have_library(lib, func)
     puts "You seem to be missing the #{lib} library.\nI can't compile the "+
       "svn library without this." 
     exit(1)
   end
 end
+
 # These aren't required, but we'll link them if we have them
-have_library('svn_fs')
-have_library('svn_repos')
+have_library('svn_fs-1', 'svn_fs_new')
+have_library('svn_repos-1', 'svn_repos_open')
 
 with_config('svn')
 
