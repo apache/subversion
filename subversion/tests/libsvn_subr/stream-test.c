3a941cca668ff606fcc6403f97112a339db98e17
C. Michael Pilato <cmpilato@apache.org>
2002-11-14 19:10:40 +0000

    Helping sussman with issue #896 by providing a stream-from-string interface.  He's abstracting out some code that currently reads from a file.  That code now needs to operate on data from a file or from a string.  So, of course, it will now read from an svn_stream_t.
    * subversion/include/svn_io.h
      (svn_stream_from_string): New.
    
    * subversion/libsvn_subr/io.c
      (struct string_stream_baton, read_handler_string,
        svn_stream_from_string): New.
    
    * subversion/tests/libsvn_subr/stream-test.c
      New test suite file (with a single test, for now).
    
    * subversion/build.conf
      (stream-test): New section for new test.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@843863 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subversion/tests/libsvn_subr/stream-test.c
@@ -0,0 +1,101 @@
+/*
+ * stream-test.c -- test the stream functions
+ *
+ * ====================================================================
+ * Copyright (c) 2000-2002 CollabNet.  All rights reserved.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at http://subversion.tigris.org/license-1.html.
+ * If newer versions of this license are posted there, you may use a
+ * newer version instead, at your option.
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals.  For exact contribution history, see the revision
+ * history and logs, available at http://subversion.tigris.org/.
+ * ====================================================================
+ */
+
+#include <stdio.h>
+#include <svn_io.h>
+#include <apr_general.h>
+#include "svn_test.h"
+
+
+static svn_error_t *
+test_stream_from_string (const char **msg,
+                         svn_boolean_t msg_only,
+                         apr_pool_t *pool)
+{
+  int i;
+
+#define NUM_TEST_STRINGS 5
+#define TEST_BUF_SIZE 10
+
+  static const char * const strings[NUM_TEST_STRINGS] = { 
+    /* 0 */
+    NULL,
+    /* 1 */
+    "",
+    /* 2 */
+    "This is a string.",
+    /* 3 */
+    "This is, by comparison to the previous string, a much longer string.",
+    /* 4 */
+    "And if you thought that last string was long, you just wait until "
+    "I'm finished here.  I mean, how can a string really claim to be long "
+    "when it fits on a single line of 80-columns?  Give me a break. "
+    "Now, I'm not saying that I'm the longest string out there--far from "
+    "it--but I feel that it is safe to assume that I'm far longer than my "
+    "peers.  And that demands some amount of respect, wouldn't you say?"
+  };
+  
+  *msg = "test svn_stream_from_string";
+
+  if (msg_only)
+    return SVN_NO_ERROR;
+
+  for (i = 0; i < NUM_TEST_STRINGS; i++)
+    {
+      svn_stream_t *stream;
+      char buffer[TEST_BUF_SIZE];
+      svn_stringbuf_t *stringbuf;
+      int orig_len;
+      apr_size_t len;
+
+      orig_len = strings[i] ? strlen (strings[i]) : 0;
+      stream = svn_stream_from_string (strings[i], orig_len, pool);
+      stringbuf = svn_stringbuf_create ("", pool);
+      len = TEST_BUF_SIZE;
+      while (len == TEST_BUF_SIZE)
+        {
+          /* Read a chunk ... */
+          SVN_ERR (svn_stream_read (stream, buffer, &len));
+
+          /* ... and append the chunk to the stringbuf. */
+          svn_stringbuf_appendbytes (stringbuf, buffer, len);
+        }
+      
+      if (orig_len != stringbuf->len)
+        return svn_error_create (SVN_ERR_TEST_FAILED, 0, NULL,
+                                 "Result had unexpected length.");
+      if (strings[i] && (strcmp (stringbuf->data, strings[i]) != 0))
+        return svn_error_create (SVN_ERR_TEST_FAILED, 0, NULL,
+                                 "Result had unexpected contents.");
+    }
+
+#undef NUM_TEST_STRINGS
+#undef TEST_BUF_SIZE
+
+  return SVN_NO_ERROR;
+}
+
+
+/* The test table.  */
+
+struct svn_test_descriptor_t test_funcs[] =
+  {
+    SVN_TEST_NULL,
+    SVN_TEST_PASS (test_stream_from_string),
+    SVN_TEST_NULL
+  };
