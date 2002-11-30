fcbe7de97655d0b6845f59a57b62fc124d99d2ab
Garrett Rooney <rooneg@apache.org>
2002-11-30 21:40:40 +0000

    translate eol's in 'svn cat' when appropriate.  this requires moving some of the code for parsing the svn:eol-style property out of libsvn_wc and into libsvn_subr.
    * subversion/libsvn_wc/translate.h
      (enum svn_wc__eol_style): move to svn_subst.h and rename.
      (svn_wc__get_eol_style): take an svn_subst_eol_style_t.
      (svn_wc__eol_style_from_value): moved to libsvn_subr and renamed.
    
    * subversion/libsvn_wc/translate.c
      (svn_wc__get_eol_style): adjust arguments.  use the new
       svn_subst_eol_style_from_value instead of svn_wc__eol_style_from_value.
      (svn_wc__style_from_value): moved to libsvn_subr and renamed.
      (all others): change svn_wc__eol_style enum values to new svn_subst names.
    
    * subversion/include/svn_subst.h
      (svn_subst_eol_style_t): was enum svn_wc__eol_style.
      (svn_subst_eol_style_from_value): moved from libsvn_wc and renamed.
    
    * subversion/libsvn_subr/subst.c
      (svn_subst_eol_style_from_value): was svn_wc__eol_style_from_value.
    
    * subversion/libsvn_client/cat.c
      (svn_client_cat): translate eol's when appropriate.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@844020 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/cat.c
+++ subversion/libsvn_client/cat.c
@@ -25,9 +25,9 @@
 #include "svn_client.h"
 #include "svn_string.h"
 #include "svn_error.h"
+#include "svn_subst.h"
 #include "svn_io.h"
 #include "client.h"
-
 
 
 /*** Code. ***/
@@ -43,6 +43,9 @@ svn_client_cat (svn_stream_t* out,
   void *ra_baton, *session;
   svn_revnum_t rev;
   svn_node_kind_t url_kind;
+  svn_string_t *mime_type;
+  svn_string_t *eol_style;
+  apr_hash_t *props;
 
   /* Get the RA library that handles URL. */
   SVN_ERR (svn_ra_init_ra_libs (&ra_baton, pool));
@@ -66,13 +69,60 @@ svn_client_cat (svn_stream_t* out,
     return svn_error_createf(SVN_ERR_CLIENT_IS_DIRECTORY, 0, NULL,
                              "URL \"%s\" refers to directory", url);
 
-  /* Get the file */
-  SVN_ERR (ra_lib->get_file(session, "", rev, out, NULL, NULL));
+  /* Grab some properties we need to know in order to figure out if anything 
+     special needs to be done with this file. */
+  SVN_ERR (ra_lib->get_file (session, "", rev, NULL, NULL, &props));
+
+  mime_type = apr_hash_get (props, SVN_PROP_MIME_TYPE, APR_HASH_KEY_STRING);
+  eol_style = apr_hash_get (props, SVN_PROP_EOL_STYLE, APR_HASH_KEY_STRING);
+
+  /* FIXME: Someday we should also check the keywords property and if it's 
+   * set do keyword expansion, but that's a fair amount of work. */
+
+  if (mime_type && svn_mime_type_is_binary (mime_type->data)
+      || ! eol_style)
+    {
+      /* Either it's a binary file, or it's a text file with no special eol 
+         style. */
+      SVN_ERR (ra_lib->get_file(session, "", rev, out, NULL, NULL));
+    }
+  else
+    {
+      svn_subst_keywords_t *kw = NULL;
+      svn_subst_eol_style_t style;
+      const char *tmp_filename;
+      svn_stream_t *tmp_stream;
+      apr_file_t *tmp_file;
+      apr_status_t apr_err;
+      apr_off_t off = 0;
+      const char *eol;
+
+      /* grab a temporary file to write the target to. */
+      SVN_ERR (svn_io_open_unique_file (&tmp_file, &tmp_filename, "", ".tmp", 
+                                        TRUE, pool));
+
+      tmp_stream = svn_stream_from_aprfile (tmp_file, pool);
+
+      SVN_ERR (ra_lib->get_file(session, "", rev, tmp_stream, NULL, NULL));
+
+      /* rewind our stream. */
+      apr_err = apr_file_seek (tmp_file, APR_SET, &off);
+      if (apr_err)
+        return svn_error_createf (apr_err, 0, NULL, "seek failed on '%s'.",
+                                  tmp_filename);
+
+      /* FIXME: set the kw to the appropriate value as found in the keywords 
+         property before translating it. */
+
+      svn_subst_eol_style_from_value (&style, &eol, eol_style->data);
+
+      SVN_ERR (svn_subst_translate_stream (tmp_stream, out, eol, FALSE, kw,
+                                           TRUE));
+
+      SVN_ERR (svn_stream_close (tmp_stream));
+    }
 
   SVN_ERR (ra_lib->close (session));
-
-  /* ### FIXME: do keyword expansion, newline translation?  Perhaps
-     ### make it optional?  */
 
   return SVN_NO_ERROR;
 }
