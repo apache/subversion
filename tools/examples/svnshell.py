a848a334feb25ead148f2a6959c1dbaae9408b02
C. Michael Pilato <cmpilato@apache.org>
2002-12-19 08:05:05 +0000

    * tools/examples/svnshell.py   Add new 'propcat' command.  Thanks to Greg Stein for the inadvertant   inspiration.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@844239 13f79535-47bb-0310-9956-ffa450edef68

--- tools/examples/svnshell.py
+++ tools/examples/svnshell.py
@@ -47,6 +47,7 @@ class SVNShell:
     print "  exit         : exit the shell"
     print "  ls [PATH]    : list the contents of the current directory"
     print "  lstxns       : list the transactions available for browsing"
+    print "  propcat      : list the properties of FILE or DIR"
     print "  setrev REV   : set the current revision to browse"
     print "  settxn TXN   : set the current transaction to browse"
     print "  youngest     : list the youngest browsable revision number"
@@ -160,6 +161,26 @@ class SVNShell:
         counter = 0
     print ""
     util.svn_pool_clear(self.taskpool)
+    
+  def cmd_propcat(self, *args):
+    """dump the properties of a file or dir"""
+    args = args[0]
+    if not len(args):
+      print "You must supply a path."
+      return
+    catpath = self._parse_path(args[0])
+    kind = fs.check_path(self.root, catpath, self.taskpool)
+    if kind == util.svn_node_none:
+      print "Path '%s' does not exist." % catpath
+      return
+    plist = fs.node_proplist(self.root, catpath, self.taskpool)
+    for pkey in plist.keys():
+      pval = plist[pkey]
+      print 'K ' + str(len(pkey))
+      print pkey
+      print 'P ' + str(len(pval))
+      print pval
+    print 'PROPS-END'
     
   def cmd_setrev(self, *args):
     """set the current revision to view"""
