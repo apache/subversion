0d8c897ca25d4197c062b77e4431b6043b125d9c
Karl Fogel <kfogel@apache.org>
2002-12-21 03:28:20 +0000

    Fix issue #1028: 'svn log -rCOMMITTED foo' was broken.
    * subversion/libsvn_client/client.h, subversion/libsvn_client/revisions.c
      (svn_client__revision_is_local): New function.
    
    * subversion/libsvn_client/log.c
      (svn_client_log): Handle revision keywords as reasonably as can.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@844260 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/log.c
+++ subversion/libsvn_client/log.c
@@ -158,54 +158,137 @@ svn_client_log (svn_client_auth_baton_t *auth_baton,
                                             auth_baton, pool));
     }
 
-  /* Get the revisions based on the users "hints".  */
-  SVN_ERR (svn_client__get_revision_number
-           (&start_revnum, ra_lib, session, start, base_name, pool));
-  SVN_ERR (svn_client__get_revision_number
-           (&end_revnum, ra_lib, session, end, base_name, pool));
-
-  err = ra_lib->get_log (session,
-                         condensed_targets,
-                         start_revnum,
-                         end_revnum,
-                         discover_changed_paths,
-                         strict_node_history,
-                         receiver,
-                         receiver_baton);
-  
-  /* Special case: If there have been no commits, we'll get an error
-   * for requesting log of a revision higher than 0.  But the
-   * default behavior of "svn log" is to give revisions HEAD through
-   * 1, on the assumption that HEAD >= 1.
+  /* It's a bit complex to correctly handle the special revision words
+   * such as "BASE", "COMMITTED", and "PREV".  For example, if the
+   * user runs
    *
-   * So if we got that error for that reason, and it looks like the
-   * user was just depending on the defaults (rather than explicitly
-   * requesting the log for revision 1), then we don't error.  Instead
-   * we just invoke the receiver manually on a hand-constructed log
-   * message for revision 0.
+   *   $ svn log -rCOMMITTED foo.txt bar.c
    *
-   * See also http://subversion.tigris.org/issues/show_bug.cgi?id=692.
+   * which committed rev should be used?  The younger of the two?  The
+   * first one?  Should we just error?
+   *
+   * None of the above, I think.  Rather, the committed rev of each
+   * target in turn should be used.  This is what most users would
+   * expect, and is the most useful interpretation.  Of course, this
+   * goes for the other dynamic (i.e., local) revision words too.
+   *
+   * Note that the code to do this is a bit more complex than a simple
+   * loop, because the user might run
+   *
+   *    $ svn log -rCOMMITTED:42 foo.txt bar.c
+   *
+   * in which case we want to avoid recomputing the static revision on
+   * every iteration.
    */
-  if (err && (err->apr_err == SVN_ERR_FS_NO_SUCH_REVISION)
-      && (start->kind == svn_opt_revision_head)
-      && ((end->kind == svn_opt_revision_number)
-          && (end->value.number == 1)))
-    {
-      svn_revnum_t youngest_rev;
-      
-      SVN_ERR (ra_lib->get_latest_revnum (session, &youngest_rev));
-      if (youngest_rev == 0)
-        {
-          err = SVN_NO_ERROR;
+  {
+    svn_boolean_t start_is_local = svn_client__revision_is_local (start);
+    svn_boolean_t end_is_local = svn_client__revision_is_local (end);
 
-          /* Log receivers are free to handle revision 0 specially... But
-             just in case some don't, we make up a message here. */
-          SVN_ERR (receiver (receiver_baton,
-                             NULL, 0, "", "", "No commits in repository.",
-                             pool));
-        }
-    }
+    if (! start_is_local)
+      SVN_ERR (svn_client__get_revision_number
+               (&start_revnum, ra_lib, session, start, base_name, pool));
 
+    if (! end_is_local)
+      SVN_ERR (svn_client__get_revision_number
+               (&end_revnum, ra_lib, session, end, base_name, pool));
+
+    if (start_is_local || end_is_local)
+      {
+        /* ### FIXME: At least one revision is locally dynamic, that
+         * is, we're in a case similar to one of these:
+         *
+         *   $ svn log -rCOMMITTED foo.txt bar.c
+         *   $ svn log -rCOMMITTED:42 foo.txt bar.c
+         *
+         * We'll to iterate over each target in turn, getting the logs
+         * for the named range.  This means that certain revisions may
+         * be printed out more than once.  I think that's okay
+         * behavior, since the sense of the command is that one wants
+         * a particular range of logs for *this* file, then another
+         * range for *that* file, and so on.  But we should at
+         * probably some sort of separator header between the log
+         * groups.  Of course, libsvn_client can't just print stuff
+         * out -- it has to take a callback from the client to do
+         * that.  So we need to define that callback interface, then
+         * have the command line client pass one down here.
+         *
+         * In any case, at least it will behave uncontroversially when
+         * passed only one argument, which I would think is the common
+         * case when passing a local dynamic revision word.
+         */
+
+        int i;
+
+        for (i = 0; i < targets->nelts; i++)
+          {
+            const char *target = ((const char **)targets->elts)[i];
+
+            if (start_is_local)
+              SVN_ERR (svn_client__get_revision_number
+                       (&start_revnum, ra_lib, session, start, target, pool));
+            
+            if (end_is_local)
+              SVN_ERR (svn_client__get_revision_number
+                       (&end_revnum, ra_lib, session, end, target, pool));
+
+            err = ra_lib->get_log (session,
+                                   condensed_targets,
+                                   start_revnum,
+                                   end_revnum,
+                                   discover_changed_paths,
+                                   strict_node_history,
+                                   receiver,
+                                   receiver_baton);
+            if (err)
+              break;
+          }
+      }
+    else  /* both revisions are static, so no loop needed */
+      {
+        err = ra_lib->get_log (session,
+                               condensed_targets,
+                               start_revnum,
+                               end_revnum,
+                               discover_changed_paths,
+                               strict_node_history,
+                               receiver,
+                               receiver_baton);
+      }
+    
+    /* Special case: If there have been no commits, we'll get an error
+     * for requesting log of a revision higher than 0.  But the
+     * default behavior of "svn log" is to give revisions HEAD through
+     * 1, on the assumption that HEAD >= 1.
+     *
+     * So if we got that error for that reason, and it looks like the
+     * user was just depending on the defaults (rather than explicitly
+     * requesting the log for revision 1), then we don't error.  Instead
+     * we just invoke the receiver manually on a hand-constructed log
+     * message for revision 0.
+     *
+     * See also http://subversion.tigris.org/issues/show_bug.cgi?id=692.
+     */
+    if (err && (err->apr_err == SVN_ERR_FS_NO_SUCH_REVISION)
+        && (start->kind == svn_opt_revision_head)
+        && ((end->kind == svn_opt_revision_number)
+            && (end->value.number == 1)))
+      {
+        svn_revnum_t youngest_rev;
+        
+        SVN_ERR (ra_lib->get_latest_revnum (session, &youngest_rev));
+        if (youngest_rev == 0)
+          {
+            err = SVN_NO_ERROR;
+            
+            /* Log receivers are free to handle revision 0 specially... But
+               just in case some don't, we make up a message here. */
+            SVN_ERR (receiver (receiver_baton,
+                               NULL, 0, "", "", "No commits in repository.",
+                               pool));
+          }
+      }
+  }
+  
   /* We're done with the RA session. */
   SVN_ERR (ra_lib->close (session));
 
