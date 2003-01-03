f6de2f0e9dbd2908c060ea05b814a5628a7eb270
Ben Collins-Sussman <sussman@apache.org>
2003-01-03 20:39:30 +0000

    Merge /trunk/subversion/mod_dav_svn changes r4132:4243 into autoversion branch.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/autoversion@844318 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/mod_dav_svn/dav_svn.h
+++ subversion/mod_dav_svn/dav_svn.h
@@ -2,7 +2,7 @@
  * dav_svn.h: types, functions, macros for the DAV/SVN Apache module
  *
  * ====================================================================
- * Copyright (c) 2000-2002 CollabNet.  All rights reserved.
+ * Copyright (c) 2000-2003 CollabNet.  All rights reserved.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
