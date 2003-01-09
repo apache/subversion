e8ce3a7889cd419af752531e5ccfd527b618e014
Greg Stein <gstein@apache.org>
2003-01-09 11:58:56 +0000

    Add a regression test for the mailer.py script.
    * hook-scripts/tests: new directory
    
    * hook-scripts/tests/mailer-init.sh: initialize a repos for the
        testing run.
    
    * hook-scripts/tests/mailer-t1.sh: first test. generates "mail" for
        every revision, with the intent of comparing that to a known,
        reference output.
    
    * hook-scripts/tests/mailer-t1.output: the reference output
    
    * hook-scripts/tests/mailer-tweak.py: tweak the recorded dates on the
        revisions. now we don't have to worry about working around
        floating dates in the output.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@844377 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tools/hook-scripts/tests/mailer-t1.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# mailer-t1.sh: test #1 for the mailer.py script
+#
+# This test generates "email" for each revision in the repository,
+# concatenating them into one big blob, which is then compared against
+# a known output.
+#
+# Note: mailer-tweak.py must have been run to make the test outputs
+#       consistent and reproducible
+#
+# USAGE: ./mailer-t1.sh REPOS MAILER-SCRIPT
+#
+
+glom=mailer-t1.current
+rm -f $glom
+
+youngest="`svnlook youngest $1`"
+for rev in `python -c "import string; print string.join(map(str, range(1,$youngest+1)))"`; do
+  $2 $1 $rev >> $glom
+done
+
+echo "current mailer.py output in: $glom"
+
+echo diff -q mailer-t1.output $glom
+diff -q mailer-t1.output $glom
