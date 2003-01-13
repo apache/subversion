8e11df1b6e46643cf0a9857e8e0cd5418579dc2f
Karl Fogel <kfogel@apache.org>
2003-01-13 19:58:50 +0000

    For issue #689: Move more md5 stuff into the md5 library.
    * subversion/libsvn_fs/reps-strings.c
      (empty_digest, checksums_match): Moved to the new svn_md5 library
        and renamed, see below.
    
    * subversion/include/svn_md5.h, subversion/libsvn_subr/md5.c
      (svn_md5_empty_string_digest, svn_md5_digests_match): Moved here
        from from reps-strings.c and renamed, see above.
    
    All callers changed.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@844441 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_md5.h
+++ subversion/include/svn_md5.h
@@ -26,8 +26,20 @@
 
 
 
+/* The MD5 digest for the empty string. */
+extern const char svn_md5_empty_string_digest[];
+
+
 /** Return the hex representation of @a digest, which must be
  * MD5_DIGESTSIZE bytes long.  Allocate the string in @a pool.
  */
 const char *svn_md5_digest_to_cstring (unsigned char digest[],
                                        apr_pool_t *pool);
+
+
+/* Compare digests @a d1 and @a d2, each MD5_DIGESTSIZE bytes long.
+ * If neither is all zeros, and they do not match, then return false;
+ * else return true.
+ */
+svn_boolean_t svn_md5_digests_match (unsigned const char d1[],
+                                     unsigned const char d2[]);
