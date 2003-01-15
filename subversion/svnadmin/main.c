14912ea38e150e823570a89724e1fc374605c805
Ben Collins-Sussman <sussman@apache.org>
2003-01-15 20:25:18 +0000

    Fix two small annoyances with 'svnadmin recover'.
    * repos.c (svn_repos_recover):  if the db lockfile is already
      unlocked, don't freak out.  move on to the recovery step.
    
    * main.c (subcommand_recover):  change phrasing, so people no longer
      think they're waiting to get a lock.  make it clear they're waiting
      for recovery, not locking.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@844470 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svnadmin/main.c
+++ subversion/svnadmin/main.c
@@ -412,8 +412,8 @@ subcommand_recover (apr_getopt_t *os, void *baton, apr_pool_t *pool)
   svn_repos_t *repos;
   struct svnadmin_opt_state *opt_state = baton;
 
-  printf ("Acquiring exclusive lock on repository db, and running "
-          "recovery procedures.\nPlease stand by...");
+  printf ("Acquiring exclusive lock on repository db.\n"
+          "Recovery is running, please stand by...");
   fflush(stdout);
 
   SVN_ERR (svn_repos_recover (opt_state->repository_path, pool));
