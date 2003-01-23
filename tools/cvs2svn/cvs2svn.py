e19bdab54ea8bf60d671fa1d018355829fad3809
Greg Stein <gstein@apache.org>
2003-01-23 12:29:36 +0000

    Get cvs2svn working against some of the latest API changes. Pass new, additional arguments to svn_fs_apply_textdelta() and svn_repos_create().
    * cvs2svn/cvs2svn.py
      (Commit.commit): add checksum arguments (None) to the
        svn_fs_apply_textdelta call.
      (pass4): pass template arguments (None) to svn_repos_create(0
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@844597 13f79535-47bb-0310-9956-ffa450edef68

--- tools/cvs2svn/cvs2svn.py
+++ tools/cvs2svn/cvs2svn.py
@@ -363,7 +363,7 @@ class Commit:
       else:
         created_file = 0
 
-      handler, baton = fs.apply_textdelta(root, repos_path, f_pool)
+      handler, baton = fs.apply_textdelta(root, repos_path, None, None, f_pool)
 
       # figure out the real file path for "co"
       try:
@@ -550,7 +550,7 @@ def pass4(ctx):
   # create the target repository
   if not ctx.dry_run:
     if ctx.create_repos:
-      t_repos = repos.svn_repos_create(ctx.target, ctx.pool)
+      t_repos = repos.svn_repos_create(ctx.target, None, None, ctx.pool)
     else:
       t_repos = repos.svn_repos_open(ctx.target, ctx.pool)
     t_fs = repos.svn_repos_fs(t_repos)
