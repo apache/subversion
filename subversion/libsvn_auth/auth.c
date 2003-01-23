2b3bfb4849f75597acb37975192d5cc9ed9e3c54
Ben Collins-Sussman <sussman@apache.org>
2003-01-23 15:01:57 +0000

    Continue issue #724 (libsvn_auth).
    * svn_auth.h (svn_auth_provider_t):  tweak docstring.
    
    * auth.c: add docs describing the 'table' analogy, fix formatting to
      match our tab-style... and my favorite GNU-style too.  :-)
    
      (struct svn_auth_baton_t):  add a pool argument.  rename 'kinds'
      field to 'tables' to match analogy.
    
      (svn_auth_open):  this is easy, so implement it.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@844599 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_auth/auth.c
+++ subversion/libsvn_auth/auth.c
@@ -24,76 +24,118 @@
 #include "svn_error.h"
 #include "svn_auth.h"
 
+/* The good way to think of this machinery is as a set of tables.
 
+   - Each type of credentials selects a single table.
+
+   - In a given table, each row is a 'provider' capable of returning
+     the same type of credentials.  Each column represents a
+     provider's repeated attempts to provide credentials.
+
+   When the caller asks for a particular type of credentials, the
+   machinery in this file walks over the appropriate table.  It starts
+   with the first provider (first row), and calls first_credentials()
+   to get the first set of credentials (first column).  If the caller
+   is unhappy with the credentials, then each subsequent call to
+   next_credentials() traverses the row from left to right.  If the
+   provider returns NULL at any point, then we go to the next provider
+   (row).  We continue this way until every provider is used up, or
+   until the client is happy with the returned credentials.
+
+   Note that the caller cannot see the table traversal, and thus has
+   no idea when we switch providers.
+*/
+
+
+/* A provider object. */
 typedef struct
 {
-    const svn_auth_provider_t *vtable;
-    void * provider_baton;
+  const svn_auth_provider_t *vtable;
+  void *provider_baton;
+
 } provider_t;
 
+/* This effectively defines a single table.  Every provider in this
+   array returns the same kind of credentials. */
 typedef struct
 {
-    apr_array_header_t *providers; /* (ordered) array of provider_t */
-
+  apr_array_header_t *providers; /* (ordered) array of provider_t */
+  
 } provider_set_t;
 
 
+/* The auth baton contains all of the tables. */
 struct svn_auth_baton_t
 {
-    apr_hash_t *kinds;  /* map cred_kind -> provider_set */
+  apr_hash_t *tables;  /* maps cred_kind -> provider_set */
+  apr_pool_t *pool;    /* the pool I'm allocated in. */
+
 };
 
+/* Abstracted iteration baton */
 struct svn_auth_iterstate_t
 {
-    const char *cred_kind;
-    int provider_idx;
-    void *provider_iter_baton;
+  const char *cred_kind;      /* the table being traversed */
+  int provider_idx;           /* the provider (row) being searched */
+  void *provider_iter_baton;  /* the provider's own iteration context */
+
 };
 
 
-svn_error_t * svn_auth_open(svn_auth_baton_t **baton,
+svn_error_t * 
+svn_auth_open (svn_auth_baton_t **auth_baton,
+               apr_pool_t *pool)
+{
+  svn_auth_baton_t *ab;
+
+  ab = apr_pcalloc (pool, sizeof (*ab));
+  ab->tables = apr_hash_make (pool);
+  ab->pool = pool;
+
+  *auth_baton = ab;
+  return SVN_NO_ERROR;
+}
+
+svn_error_t *
+svn_auth_register_provider (svn_auth_baton_t *baton,
+                            int order,
+                            const svn_auth_provider_t *vtable,
+                            void *provider_baton,
                             apr_pool_t *pool)
 {
-    abort();
-    return SVN_NO_ERROR;
-}
-
-svn_error_t * svn_auth_register_provider(svn_auth_baton_t *baton,
-                                         int order,
-                                         const svn_auth_provider_t *vtable,
-                                         void *provider_baton,
-                                         apr_pool_t *pool)
-{
-    abort();
-    return SVN_NO_ERROR;
+  abort();
+  return SVN_NO_ERROR;
 }
 
 
-svn_error_t * svn_auth_first_credentials(void **credentials,
-                                         svn_auth_iterstate_t **state,
-                                         const char *cred_kind,
-                                         svn_auth_baton_t *baton,
-                                         apr_pool_t *pool)
+svn_error_t *
+svn_auth_first_credentials (void **credentials,
+                            svn_auth_iterstate_t **state,
+                            const char *cred_kind,
+                            svn_auth_baton_t *baton,
+                            apr_pool_t *pool)
 {
-    abort();
-    return SVN_NO_ERROR;
+  abort();
+  return SVN_NO_ERROR;
 }
 
 
-svn_error_t * svn_auth_next_credentials(void **credentials,
-                                        svn_auth_iterstate_t *state,
-                                        apr_pool_t *pool)
+svn_error_t *
+svn_auth_next_credentials (void **credentials,
+                           svn_auth_iterstate_t *state,
+                           apr_pool_t *pool)
 {
-    abort();
-    return SVN_NO_ERROR;
+  abort();
+  return SVN_NO_ERROR;
 }
 
 
-svn_error_t * svn_auth_save_credentials(const char *cred_kind,
-                                        void *credentials,
-                                        svn_auth_baton_t *baton,
-                                        apr_pool_t *pool)
+svn_error_t *
+svn_auth_save_credentials (const char *cred_kind,
+                           void *credentials,
+                           svn_auth_baton_t *baton,
+                           apr_pool_t *pool)
 {
-    abort();
-    return SVN_NO_ERROR;
+  abort();
+  return SVN_NO_ERROR;
 }
