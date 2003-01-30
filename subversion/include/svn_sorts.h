0eda3ddd5a4c943c44997e26348ab6db491882dd
C. Michael Pilato <cmpilato@apache.org>
2003-01-30 18:23:43 +0000

    * subversion/include/svn_sorts.h   (apr_hash_sorted_keys): Add efficiency warning.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@844743 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_sorts.h
+++ subversion/include/svn_sorts.h
@@ -112,8 +112,13 @@ apr_hash_sorted_keys (apr_hash_t *ht,
  * 
  * Return a pointer to an empty slot at the head of array @a arr.
  *
+ * Note that if your application does not strictly need the new
+ * element to be at the head of the array, consider using the much
+ * more efficient function apr_array_push() (which will add your new
+ * element at the tail of the array.
+ *
  * NOTE:
- * This function should go over to APR.
+ * This function should go over to APR. 
  */
 void *apr_array_prepend (apr_array_header_t *arr);
 #endif /* apr_array_prepend */
