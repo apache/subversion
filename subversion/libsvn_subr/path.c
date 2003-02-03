21dc876316844e77a87d6e6f6eb18db45ec414c9
Philip Martin <philip@apache.org>
2003-02-03 01:09:59 +0000

    Fix issue 1122, recognise "/" and "/foo" as parent-child.
    * subversion/libsvn_subr/path.c (svn_path_is_child):  Handle a parent
      path that is "/".
    
    * subversion/tests/libsvn_subr/path-test.c (test_path_is_child): Test
      "/" behaviour.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@844779 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/path.c
+++ subversion/libsvn_subr/path.c
@@ -522,29 +522,36 @@ svn_path_is_child (const char *path1,
   /* assert (is_canonical (path2, strlen (path2)));  ### Expensive strlen */
 
   /* Allow "" and "foo" to be parent/child */
-  if (SVN_PATH_IS_EMPTY (path1)                /* "" is the parent  */
-      &&
-      ! (SVN_PATH_IS_EMPTY (path2)             /* "" not a child    */
-         || path2[0] == '/'))                  /* "/foo" not a child */
-    return apr_pstrdup (pool, path2);
-
-  if (path1[0] != '/' && path2[0] == '/')
-    return NULL;
-
-  /* Reach the end of at least one of the paths. */
-  for (i = 0; path1[i] && path2[i]; i++)
+  if (SVN_PATH_IS_EMPTY (path1))               /* "" is the parent  */
     {
-      if (path1[i] != path2[i])
+      if (SVN_PATH_IS_EMPTY (path2)            /* "" not a child    */
+          || path2[0] == '/')                  /* "/foo" not a child */
         return NULL;
+      else
+        return apr_pstrdup (pool, path2);      /* everything else is child */
     }
 
-  /* The only possibility for it being child here is when path1 is now blank,
-   * and path 2 is the beginning of a directory.
-   */
+  /* Reach the end of at least one of the paths.  How should we handle
+     things like path1:"foo///bar" and path2:"foo/bar/baz"?  It doesn't
+     appear to arise in the current Subversion code, it's not clear to me
+     if they should be parent/child or not. */
+  for (i = 0; path1[i] && path2[i]; i++)
+    if (path1[i] != path2[i])
+      return NULL;
 
-  if (path1[i] == '\0' && path2[i] == '/')
+  /* There are two cases that are parent/child
+          ...      path1[i] == '\0'
+          .../foo  path2[i] == '/'
+      or
+          /        path1[i] == '\0'
+          /foo     path2[i] != '/'
+  */
+  if (path1[i] == '\0' && path2[i])
     {
-      return apr_pstrdup (pool, path2 + i + 1);
+      if (path2[i] == '/')
+        return apr_pstrdup (pool, path2 + i + 1);
+      else if (i == 1 && path1[0] == '/')
+        return apr_pstrdup (pool, path2 + 1);
     }
 
   /* Otherwise, path2 isn't a child. */
