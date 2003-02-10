3898201e4b9d73a694c850b2b5351569f7026667
C. Michael Pilato <cmpilato@apache.org>
2003-02-10 07:19:23 +0000

    * subversion/libsvn_fs/bdb/uuids-table.c   (svn_fs__bdb_get_uuid, svn_fs__bdb_set_uuid): Indentation fixes only     -- no logical changes.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@844888 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs/bdb/uuids-table.c
+++ subversion/libsvn_fs/bdb/uuids-table.c
@@ -90,25 +90,25 @@ svn_error_t *svn_fs__bdb_get_uuid (svn_fs_t *fs,
                                    const char **uuid,
                                    trail_t *trail)
 {
-    char buffer[APR_UUID_FORMATTED_LENGTH + 1];
-    DB *uuids = fs->uuids;
-    DBT key;
-    DBT value;
+  char buffer[APR_UUID_FORMATTED_LENGTH + 1];
+  DB *uuids = fs->uuids;
+  DBT key;
+  DBT value;
 
-    svn_fs__clear_dbt (&key);
-    key.data = &idx;
-    key.size = sizeof (idx);
-
-    svn_fs__clear_dbt (&value);
-    value.data = buffer;
-    value.size = sizeof (buffer) - 1;
-
-    SVN_ERR (BDB_WRAP (fs, "get repository uuid",
-                       uuids->get (uuids, trail->db_txn, &key, &value, 0)));
-
-    *uuid = apr_pstrmemdup (trail->pool, value.data, value.size + 1);
-
-    return SVN_NO_ERROR;
+  svn_fs__clear_dbt (&key);
+  key.data = &idx;
+  key.size = sizeof (idx);
+  
+  svn_fs__clear_dbt (&value);
+  value.data = buffer;
+  value.size = sizeof (buffer) - 1;
+  
+  SVN_ERR (BDB_WRAP (fs, "get repository uuid",
+                     uuids->get (uuids, trail->db_txn, &key, &value, 0)));
+  
+  *uuid = apr_pstrmemdup (trail->pool, value.data, value.size + 1);
+  
+  return SVN_NO_ERROR;
 }
 
 svn_error_t *svn_fs__bdb_set_uuid (svn_fs_t *fs,
@@ -116,21 +116,20 @@ svn_error_t *svn_fs__bdb_set_uuid (svn_fs_t *fs,
                                    const char *uuid,
                                    trail_t *trail)
 {
-    DB *uuids = fs->uuids;
-    DBT key;
-    DBT value;
-
-    svn_fs__clear_dbt (&key);
-    key.data = &idx;
-    key.size = sizeof (idx);
-
-    svn_fs__clear_dbt (&value);
-    value.size = strlen (uuid);
-    value.data = apr_pstrmemdup (trail->pool, uuid, value.size + 1);
-     
-    SVN_ERR (BDB_WRAP (fs,
-                       "set repository uuid",
-                       uuids->put (uuids, trail->db_txn, &key, &value, 0)));
-
-    return SVN_NO_ERROR;
+  DB *uuids = fs->uuids;
+  DBT key;
+  DBT value;
+  
+  svn_fs__clear_dbt (&key);
+  key.data = &idx;
+  key.size = sizeof (idx);
+  
+  svn_fs__clear_dbt (&value);
+  value.size = strlen (uuid);
+  value.data = apr_pstrmemdup (trail->pool, uuid, value.size + 1);
+  
+  SVN_ERR (BDB_WRAP (fs, "set repository uuid",
+                     uuids->put (uuids, trail->db_txn, &key, &value, 0)));
+  
+  return SVN_NO_ERROR;
 }
