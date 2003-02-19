37f9df1fda78548d5503f7f76d296c6e9ef72bdd
mprice <dev-null@apache.org>
2003-02-19 17:56:12 +0000

    Update CHANGES and svn_version.h for release 0.18.0
    * subversion/include/svn_version.h: bump version number
    
    * CHANGES: Add entries for 0.18.0.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@845033 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_version.h
+++ subversion/include/svn_version.h
@@ -51,13 +51,13 @@ extern "C" {
  * Modify when new functionality is added or new interfaces are
  * defined, but all changes are backward compatible.
  */
-#define SVN_VER_MINOR      17
+#define SVN_VER_MINOR      18
 
 /** Patch number.
  *
  * Modify for every released patch.
  */
-#define SVN_VER_MICRO      1
+#define SVN_VER_MICRO      0
 
 /** Library version number.
  *
