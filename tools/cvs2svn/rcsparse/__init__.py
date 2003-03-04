ceaa5e1d162ab16dcfe871690a1ad08727200e6f
Karl Fogel <kfogel@apache.org>
2003-03-04 20:20:14 +0000

    Fix the rcsparse situation as much as can, update documentation:
    * rcsparse/: New directory, taken from latest ViewCVS.
    
    * rcsparse.py: Removed.
    
    * tools/cvs2svn/README: Apply Michael Wood's patch about the RCS 'co'
        dependency, and describe the rcsparse situation.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@845275 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tools/cvs2svn/rcsparse/__init__.py
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2000-2002 The ViewCVS Group. All Rights Reserved.
+#
+# By using this file, you agree to the terms and conditions set forth in
+# the LICENSE.html file which can be found at the top level of the ViewCVS
+# distribution or at http://viewcvs.sourceforge.net/license-1.html.
+#
+# Contact information:
+#   Greg Stein, PO Box 760, Palo Alto, CA, 94302
+#   gstein@lyra.org, http://viewcvs.sourceforge.net/
+#
+# -----------------------------------------------------------------------
+#
+# This software is being maintained as part of the ViewCVS project.
+# Information is available at:
+#    http://viewcvs.sourceforge.net/
+#
+# -----------------------------------------------------------------------
+
+"""This package provides parsing tools for RCS files."""
+
+from common import *
+
+try:
+  from tparse import parse
+except ImportError:
+  try:
+    from texttools import Parser
+  except ImportError:
+    from default import Parser
+
+  def parse(file, sink):
+    return Parser().parse(file, sink)
