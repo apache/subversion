47bc6db6fc29c9d84e8f05e6a908571c15f85e6a
C. Michael Pilato <cmpilato@apache.org>
2003-03-18 18:10:34 +0000

    Add a --strict option to 'svn propget', and get some binary property testing in place.
    * subversion/clients/cmdline/main.c
      (svn_cl__cmd_table): Add the --strict option to propget, and
        describe that subcommand's output a little better.  Also, clean up
        some other usage strings that ended in double-newlines (we only
        need one at the end of usage strings)
    
    * subversion/clients/cmdline/propget-cmd.c
      (stream_write): New helper.
      (svn_cl__propget): Use binary streams for output, and handle --strict.
    
    * subversion/tests/clients/cmdline/prop_tests.py
      (check_prop): Move this helper into the global context, and make it
        use --strict so that newlines aren't appended to the property values.
      (prop_value_conversion): check_prop() is now global, and we don't
        need the extra newlines on the property values now.
      (binary_props): New test.
      (test_list): Add binary_props() which currently doesn't run over DAV.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@845448 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/clients/cmdline/propget-cmd.c
+++ subversion/clients/cmdline/propget-cmd.c
@@ -35,6 +35,24 @@
 
 /*** Code. ***/
 
+static svn_error_t *
+stream_write (svn_stream_t *out,
+              const char *data,
+              apr_size_t len)
+{
+  apr_size_t write_len = len;
+
+  /* We're gonna bail on an incomplete write here only because we know
+     that this stream is really stdout, which should never be blocking
+     on us. */
+  SVN_ERR (svn_stream_write (out, data, &write_len));
+  if (write_len != len)
+    return svn_error_create (SVN_ERR_STREAM_UNEXPECTED_EOF, NULL,
+                             "Error writing to stream");
+  return SVN_NO_ERROR;
+}
+
+
 /* This implements the `svn_opt_subcommand_t' interface. */
 svn_error_t *
 svn_cl__propget (apr_getopt_t *os,
@@ -45,6 +63,9 @@ svn_cl__propget (apr_getopt_t *os,
   svn_client_ctx_t *ctx = ((svn_cl__cmd_baton_t *) baton)->ctx;
   const char *pname, *pname_utf8;
   apr_array_header_t *args, *targets;
+  apr_status_t status;
+  apr_file_t *std_out;
+  svn_stream_t *out;
   int i;
 
   /* PNAME is first argument (and PNAME_UTF8 will be a UTF-8 version
@@ -59,9 +80,15 @@ svn_cl__propget (apr_getopt_t *os,
                                          &(opt_state->start_revision),
                                          &(opt_state->end_revision),
                                          FALSE, pool));
-
+  
   /* Add "." if user passed 0 file arguments */
   svn_opt_push_implicit_dot_target (targets, pool);
+
+  /* Open a handle and stream to stdout. */
+  status = apr_file_open_stdout (&std_out, pool);
+  if (!APR_STATUS_IS_SUCCESS (status))
+    return svn_error_create (status, NULL, "Error opening stdout.");
+  out = svn_stream_from_aprfile (std_out, pool);
 
   if (opt_state->revprop)  /* operate on a revprop */
     {
@@ -86,16 +113,16 @@ svn_cl__propget (apr_getopt_t *os,
       if (URL == NULL)
         return svn_error_create(SVN_ERR_UNVERSIONED_RESOURCE, NULL,
                                 "Either a URL or versioned item is required.");
-  
+      
       /* Let libsvn_client do the real work. */
       SVN_ERR (svn_client_revprop_get (pname_utf8, &propval,
                                        URL, &(opt_state->start_revision),
                                        &rev, ctx, pool));
-
+      
       if (propval != NULL)
         {
           svn_string_t *printable_val = propval;
-
+          
           /* If this is a special Subversion property, it is stored as
              UTF8 and LF, so convert to the native locale and eol-style. */
           
@@ -103,7 +130,9 @@ svn_cl__propget (apr_getopt_t *os,
             SVN_ERR (svn_subst_detranslate_string (&printable_val, propval,
                                                    pool));
           
-          printf ("%s\n", printable_val->data);
+          SVN_ERR (stream_write (out, printable_val->data, 
+                                 printable_val->len));
+          SVN_ERR (stream_write (out, "\n", 1));
         }
     }
   else  /* operate on a normal, versioned property (not a revprop) */
@@ -119,9 +148,14 @@ svn_cl__propget (apr_getopt_t *os,
                                        &(opt_state->start_revision),
                                        opt_state->recursive, ctx, pool));
           
-          print_filenames = (opt_state->recursive || targets->nelts > 1
-                             || apr_hash_count (props) > 1);
-          
+          /* Any time there is more than one thing to print, or where
+             the path associated with a printed thing is not obvious,
+             we'll print filenames.  That is, unless we've been told
+             not to do so with the --strict option. */
+          print_filenames = ((opt_state->recursive || targets->nelts > 1
+                              || apr_hash_count (props) > 1)
+                             && (! opt_state->strict));
+            
           for (hi = apr_hash_first (pool, props); hi; hi = apr_hash_next (hi))
             {
               const void *key;
@@ -137,20 +171,20 @@ svn_cl__propget (apr_getopt_t *os,
               /* If this is a special Subversion property, it is stored as
                  UTF8, so convert to the native format. */
               if (svn_prop_needs_translation (pname_utf8))
-                SVN_ERR (svn_subst_detranslate_string (&propval, propval,
-                                                       pool));
+              SVN_ERR (svn_subst_detranslate_string (&propval, propval,
+                                                     pool));
 
-              /* ### this won't handle binary property values */
               if (print_filenames) 
                 {
                   SVN_ERR (svn_utf_cstring_from_utf8 (&filename_native,
                                                       filename, pool));
-                  printf ("%s - %s\n", filename_native, propval->data);
+                  SVN_ERR (stream_write (out, filename_native,
+                                         strlen (filename_native)));
+                  SVN_ERR (stream_write (out, " - ", 3));
                 } 
-              else 
-                {
-                  printf ("%s\n", propval->data);
-                }
+              SVN_ERR (stream_write (out, propval->data, propval->len));
+              if (! opt_state->strict)
+                SVN_ERR (stream_write (out, "\n", 1));
             }
         }
     }
