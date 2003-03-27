fb34eee782f3ab866f1e584f5fe4adbca56216ee
kraai <kraai@unknown>
2003-03-27 16:29:58 +0000

    Reported by Andreas J. Koenig.
    * subversion/clients/cmdline/ls-cmd.c
      (svn_cl__ls): Default to '.' if no targets are specified.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@845559 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/clients/cmdline/ls-cmd.c
+++ subversion/clients/cmdline/ls-cmd.c
@@ -126,9 +126,8 @@ svn_cl__ls (apr_getopt_t *os,
                                          &(opt_state->end_revision),
                                          FALSE, pool));
 
-  /* Give me arguments or give me death! */
-  if (targets->nelts == 0)
-    return svn_error_create (SVN_ERR_CL_INSUFFICIENT_ARGS, NULL, "");
+  /* Add "." if user passed 0 arguments */
+  svn_opt_push_implicit_dot_target (targets, pool);
 
   /* For each target, try to list it. */
   for (i = 0; i < targets->nelts; i++)
