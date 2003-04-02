d02d3af3cc978808463a6c4c01655facc411a959
Greg Stein <gstein@apache.org>
2003-04-02 01:06:13 +0000

    This tool consumes some data that I extracted from the SVN IssueZilla database. It can produce a simple textual summary, or with the help of gnuplot, can produce a nifty graph.
    The intent is to use this as an example for more interesting types of
    data extraction. More scripts or other reductions of the data at
    tigris.org are welcome.
    
    * tools/dev/iz/find-fix.py: new script. it is quite a hacky first cut
        intended to just get a graph done.
    
    * tools/dev/iz/defect.dem: another rough cut at a gnuplot input file
        for graphing defects over time.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@845594 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tools/dev/iz/find-fix.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python
+#
+# find-fix.py: produce a find/fix report for Subversion's IZ database
+#
+# This can be run over the data file found at:
+#   http://subversion.tigris.org/iz-data/query-set-1.tsv
+#
+# also, see:
+#   http://subversion.tigris.org/iz-data/README
+#
+
+import string
+import time
+import operator
+
+
+_types = [
+  'DEFECT',
+  'TASK',
+  'FEATURE',
+  'ENHANCEMENT',
+  'PATCH',
+  ]
+
+ONE_WEEK = 7 * 24 * 60 * 60
+
+_milestone_filter = [
+  'Post-1.0',
+  'cvs2svn-1.0',
+  'cvs2svn-opt',
+  'inapplicable',
+  ]
+
+
+def summary(datafile, d_start, d_end):
+  "Prints a summary of activity within a specified date range."
+
+  data = load_data(datafile)
+
+  found, fixed = extract(data, d_start, d_end)
+
+  t_found = t_fixed = t_rem = 0
+  for t in _types:
+    print '%12s:  found=%3d  fixed=%3d  remaining=%3d' \
+          % (t, found[t], fixed[t], found[t] - fixed[t])
+    t_found = t_found + found[t]
+    t_fixed = t_fixed + fixed[t]
+    t_rem = t_rem + found[t] - fixed[t]
+
+  print '-' * 55
+  print '%12s:  found=%3d  fixed=%3d  remaining=%3d' \
+        % ('totals', t_found, t_fixed, t_rem)
+
+
+def plot(datafile, outbase):
+  "Generates data files intended for use by gnuplot."
+
+  data = load_data(datafile)
+
+  t_min = 1L<<32
+  for issue in data:
+    if issue.created < t_min:
+      t_min = issue.created
+
+  # break the time up into a tuple, then back up to Sunday
+  t_start = time.localtime(t_min)
+  t_start = time.mktime((t_start[0], t_start[1], t_start[2] - t_start[6] - 1,
+                         0, 0, 0, 0, 0, 0))
+
+  plots = { }
+  for t in _types:
+    # for each issue type, we will record per-week stats, compute a moving
+    # average of the find/fix delta, and track the number of open issues
+    plots[t] = [ [ ], MovingAverage(), 0 ]
+
+  week = 0
+  for date in range(t_start, time.time(), ONE_WEEK):
+    ### this is quite inefficient, as we could just sort by date, but
+    ### I'm being lazy
+    found, fixed = extract(data, date, date + ONE_WEEK - 1)
+
+    for t in _types:
+      per_week, avg, open_issues = plots[t]
+      delta = found[t] - fixed[t]
+      per_week.append((week, date,
+                       found[t], -fixed[t], avg.add(delta), open_issues))
+      plots[t][2] = open_issues + delta
+
+    week = week + 1
+
+  for t in _types:
+    week_data = plots[t][0]
+    write_file(week_data, outbase, t, 'found', 2)
+    write_file(week_data, outbase, t, 'fixed', 3)
+    write_file(week_data, outbase, t, 'avg', 4)
+    write_file(week_data, outbase, t, 'open', 5)
+
+
+def write_file(week_data, base, type, tag, idx):
+  f = open('%s.%s.%s' % (base, tag, type), 'w')
+  for info in week_data:
+    f.write('%s %s # %s\n' % (info[0], info[idx], time.ctime(info[1])))
+
+
+class MovingAverage:
+  "Helper class to compute moving averages."
+  def __init__(self, n=4):
+    self.n = n
+    self.data = [ 0 ] * n
+  def add(self, value):
+    self.data.pop(0)
+    self.data.append(float(value) / self.n)
+    return self.avg()
+  def avg(self):
+    return reduce(operator.add, self.data)
+
+
+def extract(data, d_start, d_end):
+  "Extract found/fixed counts for each issue type within the data range."
+
+  found = { }
+  fixed = { }
+  for t in _types:
+    found[t] = fixed[t] = 0
+
+  for issue in data:
+    # filter out post-1.0 issues
+    if issue.milestone in _milestone_filter:
+      continue
+
+    # record the found/fixed counts
+    if d_start <= issue.created <= d_end:
+      found[issue.type] = found[issue.type] + 1
+    if d_start <= issue.resolved <= d_end:
+      fixed[issue.type] = fixed[issue.type] + 1
+
+  return found, fixed
+
+
+def load_data(datafile):
+  "Return a list of Issue objects for the specified data."
+  return map(Issue, open(datafile).readlines())
+
+
+class Issue:
+  "Represents a single issue from the exported IssueZilla data."
+
+  def __init__(self, line):
+    row = string.split(string.strip(line), '\t')
+
+    self.id = int(row[0])
+    self.type = row[1]
+    self.reporter = row[2]
+    if row[3] == 'NULL':
+      self.assigned = None
+    else:
+      self.assigned = row[3]
+    self.milestone = row[4]
+    self.created = parse_time(row[5])
+    self.resolution = row[7]
+    if not self.resolution:
+      # If the resolution is empty, then force the resolved date to None.
+      # When an issue is reopened, there will still be activity showing
+      # a "RESOLVED", thus we get a resolved date. But we simply want to
+      # ignore that date.
+      self.resolved = None
+    else:
+      self.resolved = parse_time(row[6])
+    self.summary = row[8]
+
+
+def parse_time(t):
+  "Convert an exported MySQL timestamp into seconds since the epoch."
+
+  if t == 'NULL':
+    return None
+  try:
+    return time.mktime(time.strptime(t, '%Y-%m-%d %H:%M:%S'))
+  except ValueError:
+    print 'ERROR: bad time value:', t
+    raise
+
+
+if __name__ == '__main__':
+  import sys
+
+  if len(sys.argv) == 3:
+    plot(sys.argv[1], sys.argv[2])
+  else:
+    summary(sys.argv[1],
+            time.mktime(time.strptime(sys.argv[2], '%Y-%m-%d')),
+            time.mktime(time.strptime(sys.argv[3], '%Y-%m-%d')),
+            )
