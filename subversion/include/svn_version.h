9a62b033723497928db646c4c5532814d5b05b84
mprice <dev-null@apache.org>
2003-05-16 21:03:47 +0000

    * subversion/include/svn_version.h: bump version to 0.23.0
     * CHANGES: put in 0.23.0 template -- fill in later
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@846030 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_version.h
+++ subversion/include/svn_version.h
@@ -51,13 +51,13 @@ extern "C" {
  * Modify when new functionality is added or new interfaces are
  * defined, but all changes are backward compatible.
  */
-#define SVN_VER_MINOR      22
+#define SVN_VER_MINOR      23
 
 /** Patch number.
  *
  * Modify for every released patch.
  */
-#define SVN_VER_MICRO      2
+#define SVN_VER_MICRO      0
 
 /** Library version number.
  *
