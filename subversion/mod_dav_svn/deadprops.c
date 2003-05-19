481595e4d8277df7daa2b2e81bacf72b4c9f3ddd
C. Michael Pilato <cmpilato@apache.org>
2003-05-19 15:59:14 +0000

    Turn on binary property support in the server->client direction. Clients will learn to transmit in the next milestone, and then this issue (issue #1015) will be (finally) finished.
    * subversion/mod_dav_svn/deadprops.c
      (dav_svn_db_output_value): No longer conditionalize the binary prop 
        support. 
    
    * subversion/tests/clients/cmdline/prop_tests.py
      (test_list): No longer skip 'binary_props' test.  Note that that
        test still has a workaround for making it actually run over
        ra_dav, which will also be removed in the next milestone.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@846055 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/mod_dav_svn/deadprops.c
+++ subversion/mod_dav_svn/deadprops.c
@@ -262,7 +262,6 @@ static dav_error *dav_svn_db_output_value(dav_db *db,
 
       /* Ensure XML-safety of our property values before sending them
          across the wire. */
-#ifdef SVN_DAV_FEATURE_BINARY_PROPS
       if (! svn_xml_is_xml_safe(propval->data, propval->len))
         {
           propval = (svn_string_t *)svn_base64_encode_string(propval, pool);
@@ -270,7 +269,6 @@ static dav_error *dav_svn_db_output_value(dav_db *db,
           encoding = apr_pstrcat(pool, " V:encoding=\"base64\"", NULL);
         }
       else
-#endif
         {    
           svn_stringbuf_t *xmlval = NULL;
           svn_xml_escape_cdata_string(&xmlval, propval, pool);
