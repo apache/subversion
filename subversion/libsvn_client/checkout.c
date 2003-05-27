5581e72f28c52ce855290342cc8093dc705fef12
Ben Collins-Sussman <sussman@apache.org>
2003-05-27 18:06:41 +0000

    Followup to restartable checkouts:  make 'svn checkout' resume a checkout, not just 'svn update'.  This is much more user-intuitive.
    * libsvn_client/checkout.c (svn_client__checkout_internal): if the
      checkout directory already exists, then either A) procede normally
      if its unversioned, B) error if it's versioned with a non-matching
      URL, or C) run 'svn up' if the URL matches.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@846140 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/checkout.c
+++ subversion/libsvn_client/checkout.c
@@ -54,7 +54,7 @@ svn_client__checkout_internal (const char *URL,
   const svn_delta_editor_t *checkout_editor;
   void *checkout_edit_baton;
   svn_wc_traversal_info_t *traversal_info = svn_wc_init_traversal_info (pool);
-  svn_error_t *err;
+  svn_error_t *err = NULL;
   svn_revnum_t revnum;
   svn_boolean_t sleep_here = FALSE;
   svn_boolean_t *use_sleep = timestamp_sleep ? timestamp_sleep : &sleep_here;
@@ -99,6 +99,7 @@ svn_client__checkout_internal (const char *URL,
     {
       void *ra_baton, *session;
       svn_ra_plugin_t *ra_lib;
+      svn_node_kind_t kind;
 
       /* Get the RA vtable that matches URL. */
       SVN_ERR (svn_ra_init_ra_libs (&ra_baton, pool));
@@ -114,15 +115,70 @@ svn_client__checkout_internal (const char *URL,
       SVN_ERR (svn_client__get_revision_number
                (&revnum, ra_lib, session, revision, path, pool));
 
-      /* Bootstrap: create an incomplete working-copy root dir.  Its
-         entries file should only have an entry for THIS_DIR with a
-         URL, revnum, and an 'incomplete' flag.  */
-      SVN_ERR (svn_io_make_dir_recursively (path, pool));
-      SVN_ERR (svn_wc_ensure_adm (path, URL, revnum, pool));
+      
+      SVN_ERR (svn_io_check_path (path, &kind, pool));
 
-      /* Have update flesh everything out. */
-      err = svn_client_update (path, revision, recurse, ctx, pool);
-    
+      if (kind == svn_node_none)
+        {
+          /* Bootstrap: create an incomplete working-copy root dir.  Its
+             entries file should only have an entry for THIS_DIR with a
+             URL, revnum, and an 'incomplete' flag.  */
+          SVN_ERR (svn_io_make_dir_recursively (path, pool));          
+          SVN_ERR (svn_wc_ensure_adm (path, URL, revnum, pool));
+          
+          /* Have update fix the incompleteness. */
+          err = svn_client_update (path, revision, recurse, ctx, pool);
+        }
+      else if (kind == svn_node_dir)
+        {
+          int wc_format;
+          const svn_wc_entry_t *entry;
+          svn_wc_adm_access_t *adm_access;
+
+          SVN_ERR (svn_wc_check_wc (path, &wc_format, pool));
+          if (! wc_format)
+            {
+              /* Make the unversioned directory into a versioned one. */
+              SVN_ERR (svn_wc_ensure_adm (path, URL, revnum, pool));
+              err = svn_client_update (path, revision, recurse, ctx, pool);
+              goto done;
+            }
+
+          /* Get PATH's entry. */
+          SVN_ERR (svn_wc_adm_open (&adm_access, NULL, path,
+                                    FALSE, FALSE, pool));
+          SVN_ERR (svn_wc_entry (&entry, path, adm_access, FALSE, pool));
+          SVN_ERR (svn_wc_adm_close (adm_access));
+
+          /* If PATH's existing url matches the incoming one, then
+             just update.  This allows 'svn co' to restart an
+             interrupted checkout. */
+          if (entry->url && (strcmp (entry->url, URL) == 0))
+            {
+              err = svn_client_update (path, revision, recurse, ctx, pool);
+            }
+          else
+            {
+              const char *errmsg;
+              errmsg = apr_psprintf 
+                (pool,
+                 "'%s' is already a working copy for a different url", path);
+              if (entry->incomplete)
+                errmsg = apr_pstrcat
+                  (pool, errmsg, "; run 'svn update' to complete it.", NULL);
+
+              return svn_error_create (SVN_ERR_WC_OBSTRUCTED_UPDATE, NULL,
+                                       errmsg);
+            }
+        }
+      else
+        {
+          return svn_error_createf (SVN_ERR_WC_NODE_KIND_CHANGE, NULL,
+                                    "'%s' is already a file/something else.",
+                                    path);
+        }
+
+    done:
       if (err)
         {
           /* Don't rely on the error handling to handle the sleep later, do
