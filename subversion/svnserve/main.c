c54aaf2414fd0fe1148c151b8553d9f551961df6
Philip Martin <philip@apache.org>
2003-06-02 13:14:45 +0000

    Add multi-threading to svnserve.  Whether threads are used is a run-time decision.  Platforms with threads but not fork (Windows) will use threads by default, platforms with both threads and fork (Unix) will use fork by default.
    * subversion/svnserve/main.c
      (enum connection_handling_mode, struct serve_thread_t, serve_thread): New.
      (main): Add multi-thread mode.  Add -F/-T/-S options to select fork,
      multi-thread or single-thread mode.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@846184 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svnserve/main.c
+++ subversion/svnserve/main.c
@@ -26,6 +26,7 @@
 #include <apr_getopt.h>
 #include <apr_network_io.h>
 #include <apr_signal.h>
+#include <apr_thread_proc.h>
 
 #include "svn_types.h"
 #include "svn_string.h"
@@ -37,11 +38,49 @@
 
 #include "server.h"
 
+/* The strategy for handling incoming connections.  Some of these may be
+   unavailable due to platform limitations. */
+enum connection_handling_mode {
+  connection_mode_fork,   /* Create a process per connection */
+  connection_mode_thread, /* Create a thread per connection */
+  connection_mode_single  /* One connection at a time in this process */
+};
+
+#if APR_HAS_FORK
+#if APR_HAS_THREADS
+
+#define CONNECTION_DEFAULT connection_mode_fork
+#define CONNECTION_USAGE "[-F|-T|-S] "
+#define CONNECTION_OPT "FTS"
+
+#else /* ! APR_HAS_THREADS */
+
+#define CONNECTION_DEFAULT connection_mode_fork
+#define CONNECTION_USAGE "[-F|-S] "
+#define CONNECTION_OPT "FS"
+
+#endif /* ! APR_HAS_THREADS */
+#elif APR_HAS_THREADS /* and ! APR_HAS_FORK */
+
+#define CONNECTION_DEFAULT connection_mode_thread
+#define CONNECTION_USAGE "[-T|-S] "
+#define CONNECTION_OPT "TS"
+
+#else /* ! APR_HAS_THREADS and ! APR_HAS_FORK */
+
+#define CONNECTION_DEFAULT connection_mode_single
+/* No point having option -S if that is the only available mode */
+#define CONNECTION_USAGE
+#define CONNECTION_OPT
+
+#endif
+
 static void usage(const char *progname)
 {
   if (!progname)
     progname = "svn-server";
-  fprintf(stderr, "Usage: %s [-X|-d|-t|-R] [-r root]\n", progname);
+  fprintf(stderr,
+          "Usage: %s [-X|-d|-t|-R] " CONNECTION_USAGE "[-r root]\n", progname);
   exit(1);
 }
 
@@ -66,6 +105,26 @@ static apr_status_t redirect_stdout(void *arg)
   return apr_file_dup2(out_file, err_file, pool);
 }
 
+/* "Arguments" passed from the main thread to the connection thread */
+struct serve_thread_t {
+  const char *root;
+  svn_ra_svn_conn_t *conn;
+  svn_boolean_t read_only;
+  apr_pool_t *pool;
+};
+
+#if APR_HAS_THREADS
+static void * APR_THREAD_FUNC serve_thread(apr_thread_t *tid, void *data)
+{
+  struct serve_thread_t *d = data;
+
+  svn_error_clear(serve(d->conn, d->root, FALSE, d->read_only, d->pool));
+  svn_pool_destroy(d->pool);
+
+  return NULL;
+}
+#endif
+
 int main(int argc, const char *const *argv)
 {
   svn_boolean_t listen_once = FALSE, daemon_mode = FALSE, tunnel_mode = FALSE;
@@ -81,9 +140,17 @@ int main(int argc, const char *const *argv)
   const char *arg, *root = "/";
   apr_status_t status;
   svn_ra_svn_conn_t *conn;
-#if APR_HAS_FORK
   apr_proc_t proc;
+#if APR_HAS_THREADS
+  apr_threadattr_t *tattr;
+  apr_thread_t *tid;
 #endif
+  struct serve_thread_t *thread_data;
+  enum connection_handling_mode handling_mode = CONNECTION_DEFAULT;
+
+  /* Silence some "false-alarm" unitialized warnings from gcc */
+  thread_data = NULL;
+  connection_pool = NULL;
 
   /* Initialize the app. */
   if (svn_cmdline_init ("svn", stderr) != EXIT_SUCCESS)
@@ -96,7 +163,7 @@ int main(int argc, const char *const *argv)
 
   while (1)
     {
-      status = apr_getopt(os, "dtXr:R", &opt, &arg);
+      status = apr_getopt(os, "dtXr:R" CONNECTION_OPT, &opt, &arg);
       if (APR_STATUS_IS_EOF(status))
         break;
       if (status != APR_SUCCESS)
@@ -123,6 +190,18 @@ int main(int argc, const char *const *argv)
 
         case 'R':
           read_only = TRUE;
+          break;
+
+        case 'F':
+          handling_mode = connection_mode_fork;
+          break;
+
+        case 'T':
+          handling_mode = connection_mode_thread;
+          break;
+
+        case 'S':
+          handling_mode = connection_mode_single;
           break;
         }
     }
@@ -163,28 +242,42 @@ int main(int argc, const char *const *argv)
 
   apr_listen(sock, 7);
 
-#if APR_HAS_FORK
-  if (!listen_once)
+  if (!listen_once && handling_mode != connection_mode_single)
     apr_proc_detach(APR_PROC_DETACH_DAEMONIZE);
 
   apr_signal(SIGCHLD, sigchld_handler);
-#endif
 
-  connection_pool = svn_pool_create(pool);
+  if (handling_mode != connection_mode_thread)
+    connection_pool = svn_pool_create(pool);
+
   while (1)
     {
-      /* Clear the pool for each iteration. */
-      apr_pool_clear(connection_pool);
+      if (handling_mode == connection_mode_thread)
+        {
+          /* The server never blocks to join threads so it cannot clean up
+             after each thread, thus each thread needs a separate pool that
+             it can clear when it is finished. */
+          connection_pool = svn_pool_create(NULL);
+          thread_data = apr_palloc(connection_pool, sizeof(*thread_data));
+        }
+      else
+        /* Clear the pool for each iteration. */
+        apr_pool_clear(connection_pool);
 
       status = apr_accept(&usock, sock, connection_pool);
-#if APR_HAS_FORK
-      /* Collect any zombie child processes. */
-      while (apr_proc_wait_all_procs(&proc, NULL, NULL, APR_NOWAIT,
-                                     connection_pool) == APR_CHILD_DONE)
-        ;
-#endif
+      if (handling_mode == connection_mode_fork)
+        {
+          /* Collect any zombie child processes. */
+          while (apr_proc_wait_all_procs(&proc, NULL, NULL, APR_NOWAIT,
+                                         connection_pool) == APR_CHILD_DONE)
+            ;
+        }
       if (APR_STATUS_IS_EINTR(status))
-        continue;
+        {
+          if (handling_mode == connection_mode_thread)
+            svn_pool_destroy(connection_pool);
+          continue;
+        }
       if (status)
         {
           fprintf(stderr, "Can't accept client connection: %s\n",
@@ -207,30 +300,66 @@ int main(int argc, const char *const *argv)
           exit(0);
         }
 
-      /* ### We should try to use threads when APR_HAS_FORK isn't
-       * defined (such as on Windows). */
-#if APR_HAS_FORK
-      status = apr_proc_fork(&proc, connection_pool);
-      if (status == APR_INCHILD)
+      switch (handling_mode)
         {
-          svn_error_clear(serve(conn, root, FALSE, read_only,
-                                connection_pool));
-          apr_socket_close(usock);
-          exit(0);
-        }
-      else if (status == APR_INPARENT)
-        {
-          apr_socket_close(usock);
-        }
-      else
-        {
-          /* Log an error, when we support logging. */
-          apr_socket_close(usock);
-        }
-#else
-      /* Serve one connection at a time. */
-      svn_error_clear(serve(conn, root, FALSE, read_only, connection_pool));
+        case connection_mode_fork:
+          status = apr_proc_fork(&proc, connection_pool);
+          if (status == APR_INCHILD)
+            {
+              svn_error_clear(serve(conn, root, FALSE, read_only,
+                                    connection_pool));
+              apr_socket_close(usock);
+              exit(0);
+            }
+          else if (status == APR_INPARENT)
+            {
+              apr_socket_close(usock);
+            }
+          else
+            {
+              /* Log an error, when we support logging. */
+              apr_socket_close(usock);
+            }
+          break;
+
+        case connection_mode_thread:
+          /* Create a detached thread for each connection.  That's not a
+             particularly sophisticated strategy for a threaded server, it's
+             little different from forking one process per connection. */
+#if APR_HAS_THREADS
+          status = apr_threadattr_create(&tattr, connection_pool);
+          if (status)
+            {
+              fprintf(stderr, "Can't create threadattr: %s\n",
+                      apr_strerror(status, errbuf, sizeof(errbuf)));
+              exit(1);
+            }
+          status = apr_threadattr_detach_set(tattr, 1);
+          if (status)
+            {
+              fprintf(stderr, "Can't set detached state: %s\n",
+                      apr_strerror(status, errbuf, sizeof(errbuf)));
+              exit(1);
+            }
+          thread_data->conn = conn;
+          thread_data->root = root;
+          thread_data->read_only = read_only;
+          thread_data->pool = connection_pool;
+          status = apr_thread_create(&tid, tattr, serve_thread, thread_data,
+                                     connection_pool);
+          if (status)
+            {
+              fprintf(stderr, "Can't create thread: %s\n",
+                      apr_strerror(status, errbuf, sizeof(errbuf)));
+              exit(1);
+            }
 #endif
+          break;
+
+        case connection_mode_single:
+          /* Serve one connection at a time. */
+          svn_error_clear(serve(conn, root, FALSE, read_only, connection_pool));
+        }
     }
 
   return 0;
