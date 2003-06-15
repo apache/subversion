72b7429100d320e577c78ac673b5f12dfcd25d56
mprice <mprice@unknown>
2003-06-15 18:31:19 +0000

    * subversion/include/svn_version.h: bump version number.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/release-0.24.0@846306 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_version.h
+++ subversion/include/svn_version.h
@@ -51,7 +51,7 @@ extern "C" {
  * Modify when new functionality is added or new interfaces are
  * defined, but all changes are backward compatible.
  */
-#define SVN_VER_MINOR      23
+#define SVN_VER_MINOR      24
 
 /** Patch number.
  *
