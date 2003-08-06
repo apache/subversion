b61199ffde072338f1f47e99d2d3c79043b281c5
Jostein Chr Andersen <josander@apache.org>
2003-08-06 14:30:56 +0000

    Finished issue #1133: MSVCP60.DLL missing when running svn client
    The installer will warn the user and add (and tell the user about) a Start Menu
    item (HTML file) which helps the user to download and install the VC Runtime
    files if MSVCP60.DLL is missing during installation.
    
    * packages\win32-innosetup\isx_globals.pas:
       Deleted, was not in use.
    * packages\win32-innosetup\isx_main.pas:
       Added routines for for handling issue #1133. This file was emty but the
       header.
    * packages\win32-innosetup\missing_msvcp60dll.html:
       Contains info and links for dowloading and installing VC6 runtime files.
    * packages\win32-innosetup\svn.iss:
      (Files): Installing "missing_msvcp60dll.html" if MSVCP60.DLL are missing.
      (Icons): Adding "missing_msvcp60dll.html" to the Start Menu if MSVCP60.DLL are
               missing.
      (Code):  Are using isx_main.pas for Pascal Script routines.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@846730 13f79535-47bb-0310-9956-ffa450edef68

--- packages/win32-innosetup/isx_main.pas
+++ packages/win32-innosetup/isx_main.pas
@@ -1,4 +1,4 @@
-(* svn_isx.pas: Pascal ISX routines for Inno Setup Windows installer.
+(* isx_main.pas: Pascal ISX routines for Inno Setup Windows installer.
  * ====================================================================
  * Copyright (c) 2000-2003 CollabNet.  All rights reserved.
  *
@@ -14,3 +14,84 @@
  * =================================================================== *)
 
 // ****************************************************************************
+// Global variables
+var
+    // Visual C++ 6.0 Runtime file related stuff
+    g_bMsVcpNotFound: Boolean;
+
+// Constants
+const
+    // Visual C++ 6.0 Runtime file related stuff
+    FILE_MSVCPDLL = 'msvcp60.dll';
+    URL_VCREDIST = 'http://support.microsoft.com/support/kb/articles/Q259/4/03.ASP';
+    URL_VCREDIST_ENU = 'http://download.microsoft.com/download/vc60pro/Update/1/W9XNT4/EN-US/VC6RedistSetup_enu.exe';
+    URL_VCREDIST_DEU = 'http://download.microsoft.com/download/vc60pro/Update/2/W9XNT4/EN-US/VC6RedistSetup_deu.exe';
+    URL_VCREDIST_JPN = 'http://download.microsoft.com/download/vc60pro/update/3/w9xnt4/en-us/VC6RedistSetup_jpn.exe';
+
+
+// ****************************************************************************
+// Name:    VCRuntimeDownLoadNow
+// Purpose: Decide if we want to download FILE_MSVCPDLL or just make links on
+//          on the start menu for later download and installing.
+//          Returns True if Yes and False if No
+function VCRuntimeDownLoadInfo(): Boolean;
+var
+    sMsg: String;
+
+begin
+	sMsg :='The file ' + FILE_MSVCPDLL + ' was not found in the system folder. Subversion needs this' + #13#10 +
+           'file in order to function.' + #13#10#13#10 +
+           'Please, go to the Subversion entry in the Start Menu after the installation' + #13#10 +
+           'and read the ''Download and install ' + FILE_MSVCPDLL + ''' item.'  + #13#10#13#10;
+
+    MsgBox(sMsg, mbInformation, MB_OK);
+
+    Result := True;
+end;
+
+// ****************************************************************************
+// Name:    VCRuntimeNotFound
+// Purpose: Checks if FILE_MSVCPDLL don't excists.
+//          Returns True if Yes and False if No
+function VCRuntimeNotFound(): Boolean;
+var
+    sSysDir: String;
+
+begin
+    sSysDir := ExpandConstant('{sys}');
+
+    if FileExists(sSysDir + '\' + FILE_MSVCPDLL) then
+    begin
+        g_bMsVcpNotFound := False;
+    end else begin
+        g_bMsVcpNotFound := True;
+    end;
+    
+    Result:= g_bMsVcpNotFound;
+end;
+
+
+// ****************************************************************************
+// The rest happends in the build-in ISX functions (See ISX help file for help
+// about function names).
+
+function InitializeSetup(): Boolean;
+
+begin
+
+    //Initialize some global variables
+    g_bMsVcpNotFound := VCRuntimeNotFound;
+
+    Result := True;
+end;
+
+function NextButtonClick(CurPage: Integer): Boolean;
+begin
+    if (CurPage = wpSelectComponents) and g_bMsVcpNotFound then
+    begin
+        VCRuntimeDownLoadInfo();
+	end;
+
+    Result := True;
+end;
+
