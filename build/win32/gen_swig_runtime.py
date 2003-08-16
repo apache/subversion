345647792f464980a518235eb2ea690aea850ed6
rey4 <rey4@unknown>
2003-08-16 23:18:58 +0000

    Changed Visual Studio builds of SWIG bindings to dynamically link with the SWIG runtime instead of statically linking with it. (Makefile builds already do this). This is needed because the SWIG runtime functions read and write global data. When static linking is used each module gets its own incomplete copy of the global data, now data is shared and accessible to all modules.
    * build.conf
        new target: swig_runtime
    
    * build/win32/gen_swig_runtime.py:
        new python script which generates swig runtime code for
        various languages
    
    * build/generator/msvc_dsp.ezt
    * build/generator/vcnet_vcproj.ezt
        pass -c option to swig. new custom build step to generate
        swig runtime source
    
    * build/generator/gen_base.py
        (SWIGLibrary.__init__): changed signature and moved some
          functionality into TargetSWIG.add_dependencies
    
        (SWIGRuntimeLibrary): new DependencyNode class
    
        (TargetSWIG.add_dependencies): changes for new SWIGLibrary
          constructor.
    
        (TargetSWIGRuntime, _build_types): add new Target class
    
    * build/generator/gen_win.py
        (Generator.write_project, Generator.adjust_win_depends):
          added code to handle writing project files for swig
          runtime libraries
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@846836 13f79535-47bb-0310-9956-ffa450edef68

---
+++ build/win32/gen_swig_runtime.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+import os, sys, string
+
+class Literal:
+  def __init__(self, value):
+    self.value = value
+
+  def write(self, out):
+    out.write(self.value);
+
+class File:
+  def __init__(self, *path_parts):
+    self.path = os.path.join(*path_parts)
+
+  def write(self, out):
+    fp = open(os.path.join(SWIG_LIB, self.path), 'rt')
+    try:
+      while 1:
+        chunk = fp.read(4096)
+        if not chunk: break
+        out.write(chunk)
+    finally:
+      fp.close()
+
+languages = {
+
+  'tcl':      (File('common.swg'),
+               File('tcl', 'swigtcl8.swg')),
+
+  'python':   (Literal('#include "Python.h"\n'),
+               File('common.swg'),
+               File('python', 'pyrun.swg')),
+
+  'perl':     (File('common.swg'),
+               File('perl5', 'perlrun.swg')),
+
+  'ruby':     (File('common.swg'),
+               File('ruby', 'rubyhead.swg'),
+               File('ruby', 'rubydef.swg')),
+
+  'guile':    (File('guile', 'guiledec.swg'),
+               File('guile', 'guile.swg')),
+
+  'mzscheme': (File('mzscheme', 'mzschemedec.swg'),
+               File('mzscheme', 'mzscheme.swg')),
+
+  'php':      (File('common.swg'),
+               File('php4', 'php4run.swg')),
+
+  'ocaml':    (File('ocaml', 'libswigocaml.swg')),
+
+  'pike':     (File('common.swg'),
+               File('pike', 'pikerun.swg')),
+
+  'chicken':  (Literal('#include "chicken.h"\n'),
+               File('common.swg'),
+               File('chicken', 'chickenrun.swg')),
+
+}
+
+if __name__ == "__main__":
+  if len(sys.argv) != 3:
+    print >> sys.stderr, 'Usage: %s language output.c' % sys.argv[0]
+    sys.exit(1)
+
+  language = sys.argv[1]
+  output = sys.argv[2]
+
+  fp = os.popen('swig -swiglib', 'r')
+  try:
+    SWIG_LIB = string.rstrip(fp.readline())
+  finally:
+    fp.close()
+
+  if not SWIG_LIB:
+    print >> sys.stderr, "Error: `swig -swiglib` returned nothing, unable to"
+    print >> sys.stderr, "detect swig library directory. Make sure swig is on"
+    print >> sys.stderr, "the standard path."
+    sys.exit(1)
+
+  contents = languages[language]
+  fp = open(output, 'wt')
+  try:
+    for c in contents:
+      c.write(fp)
+  finally:
+    fp.close()
