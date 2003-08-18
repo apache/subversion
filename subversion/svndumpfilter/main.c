430ed57954818809fb399a8d35fb0bdfe6296885
Sander Striker <striker@apache.org>
2003-08-18 16:39:06 +0000

    Cleanup and minor performance improvements.
    * subversion/include/svn_pools.h
    
      (svn_pool_create_ex): New function.  Simple wrapper for apr_pool_create_ex.
        Sets a default abort_fn, which calls abort().
    
      (svn_pool_create): Turn this into a simple define wrapper for
        svn_pool_create_ex.
    
      (svn_pool_clear, svn_pool_destroy): Turn into simple define wrappers
        for their apr counterparts.  These exist for completeness.
    
    
    * subversion/libsvn_subr/pool.c
    
      (svn_pool_create_ex): New function.  Simple wrapper for apr_pool_create_ex.
        Sets a default abort_fn, which calls abort().
    
      (svn_pool_create_ex_debug): New function.  Binary compatibility wrapper.
    
      (svn_pool_create, svn_pool_create_debug, svn_pool_clear,
       svn_pool_clear_debug): Removed.
    
    
    * subversion/clients/cmdline/main.c
    * subversion/svnadmin/main.c
    * subversion/svndumpfilter/main.c
    * subversion/svnlook/main.c
    * subversion/svnversion/main.c
    
      (main): Create a mutexless allocator and use that for our allocations.
        This is safe because all these applications are single threaded.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@846855 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svndumpfilter/main.c
+++ subversion/svndumpfilter/main.c
@@ -854,6 +854,7 @@ main (int argc, const char * const *argv)
 {
   svn_error_t *err;
   apr_status_t apr_err;
+  apr_allocator_t *allocator;
   apr_pool_t *pool;
 
   const svn_opt_subcommand_desc_t *subcommand = NULL;
@@ -868,9 +869,15 @@ main (int argc, const char * const *argv)
   if (svn_cmdline_init ("svndumpfilter", stderr) != EXIT_SUCCESS)
     return EXIT_FAILURE;
 
-  /* Create our top-level pool. */
-  pool = svn_pool_create (NULL);
+  /* Create our top-level pool.  Use a seperate mutexless allocator,
+   * given this application is single threaded.
+   */
+ if (apr_allocator_create (&allocator))
+   return EXIT_FAILURE;
 
+  pool = svn_pool_create_ex (NULL, allocator);
+  apr_allocator_set_owner (allocator, pool);
+		  
   if (argc <= 1)
     {
       subcommand_help (NULL, NULL, pool);
