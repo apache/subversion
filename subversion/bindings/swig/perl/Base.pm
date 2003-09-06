96a11498657a8158a7656059ac02f438ab871ba9
clkao <clkao@unknown>
2003-09-06 11:39:58 +0000

    Document SVN::Base and slightly modify the bootstrapping behavior.
    * perl/Base.pm:
      Add POD.
      (import): boostrap the _ module unless its ISA is set to Dynaloader.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@847070 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/swig/perl/Base.pm
+++ subversion/bindings/swig/perl/Base.pm
@@ -1,8 +1,55 @@
 package SVN::Base;
 
+=head1 NAME
+
+SVN::Base - Base class for importing symbols for svn modules
+
+=head1 SYNOPSIS
+
+    # Load the svn_ra_* functions into the SVN::Ra namespace.
+    pacakge SVN::Ra;
+    use SVN::Base qw(Ra svn_ra_);
+
+    # Load svn_config_t structure accessors in the magic namcespace
+    # provided by swig, so we could use it returned by other functions
+    package _p_svn_config_t;
+    use SVN::Base qw(Core svn_config_);
+
+=head1 DESCRIPTION
+
+SVN::Base is a module importing the subversion perl bindings raw
+symbols created by swig, into proper namespace and make them easier to
+use.
+
+It will also find the accessors for members of a C struct, create an
+simpler accessor function like C<$data-E<gt>field()> and
+C<$data-E<gt>field($new_value)>.
+
+Once you understand the convention of subversion functions in perl
+bindings, you could look at the subversion api and write them in perl.
+The API is available in the source header files or online at
+http://svn.collab.net/svn-doxygen/.
+
+=head1 INTERNALS
+
+The perl bindings of swig wraps raw functions into different perl
+modules, for example, SVN::_Core, SVN::_Repos. Upon import, SVN::Base
+bootstrap the requested module if it's not yet loaded, and iterate
+over the symbols provided in that module, it them puts the function
+with prefix trimmed in the namespace of the caller for this import.
+
+=head1 CAVEATS
+
+SVN::Base consider a function as structure member accessor if it is
+postfixed ``_get'' or ``_set''. Real functions with this postfixes
+will need extra handling.
+
+=cut
+
 sub import {
     my (undef, $pkg, $prefix) = @_;
-    unless (defined %{"SVN::_${pkg}::"}) {
+    unless (${"SVN::_${pkg}::ISA"}[0] eq 'DynaLoader') {
+	warn "boostrapping $pkg";
 	@{"SVN::_${pkg}::ISA"} = qw(DynaLoader);
 	eval qq'
 package SVN::_$pkg;
@@ -34,5 +81,25 @@ bootstrap SVN::_$pkg;
     }
 
 }
+
+=head1 AUTHORS
+
+Chia-liang Kao E<lt>clkao@clkao.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2003 CollabNet.  All rights reserved.
+
+This software is licensed as described in the file COPYING, which you
+should have received as part of this distribution.  The terms are also
+available at http://subversion.tigris.org/license-1.html.  If newer
+versions of this license are posted there, you may use a newer version
+instead, at your option.
+
+This software consists of voluntary contributions made by many
+individuals.  For exact contribution history, see the revision history
+and logs, available at http://subversion.tigris.org/.
+
+=cut
 
 1;
