dde1e247ae482666f0974ab667f672326754fad1
C. Michael Pilato <cmpilato@apache.org>
2003-10-04 21:03:24 +0000

    My eyes are going to burn right out of my skull if I have to keep looking at this code that can't decide if it wants to have spaces before parens or not.  Well, I'll make the decision -- it wants spaces before parens, just like the rest of the code in these modules.
    * subversion/libsvn_wc/relocate.c
      Mostly just whitespace changes.  Also...
      (svn_wc_relocate): Oops -- fix parameter order in recursive call!
    
    * subversion/libsvn_client/relocate.c
      Mostly just whitespace changes.  Also...
      (struct validator_baton_t): Was typedef baton_t.
      (relocate_error): Was error().
      (validator_func): Was validator().  Add some commentary.
      (svn_client_relocate): Add some commentary.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@847374 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/relocate.c
+++ subversion/libsvn_wc/relocate.c
@@ -45,33 +45,34 @@ svn_wc_relocate (const char *path,
   apr_hash_t *entries = NULL;
   apr_hash_index_t *hi;
   int from_len;
+  svn_wc_entry_t *entry;
 
-  SVN_ERR(svn_io_check_path(path, &kind, pool));
+  SVN_ERR (svn_io_check_path (path, &kind, pool));
 
-  from_len = strlen(from);
+  from_len = strlen (from);
 
-  SVN_ERR(svn_wc_entries_read(&entries, adm_access, TRUE, pool));
+  SVN_ERR (svn_wc_entries_read (&entries, adm_access, TRUE, pool));
 
   if (kind == svn_node_file)
     {
-      const char *base = svn_path_basename(path, pool);
-      svn_wc_entry_t *entry = apr_hash_get(entries, base, APR_HASH_KEY_STRING);
-      if (!entry)
-        return svn_error_create(SVN_ERR_ENTRY_NOT_FOUND, NULL,
-                                "missing entry");
+      const char *base = svn_path_basename (path, pool);
 
-      if (!entry->url)
-        return svn_error_createf(SVN_ERR_ENTRY_MISSING_URL, NULL,
-                                 "entry '%s' has no URL", path);
+      entry = apr_hash_get (entries, base, APR_HASH_KEY_STRING);
+      if (! entry)
+        return svn_error_create (SVN_ERR_ENTRY_NOT_FOUND, NULL, 
+                                 "missing entry");
+      if (! entry->url)
+        return svn_error_createf (SVN_ERR_ENTRY_MISSING_URL, NULL,
+                                  "entry '%s' has no URL", path);
 
-      if (!strncmp(entry->url, from, from_len))
+      if (! strncmp (entry->url, from, from_len))
         {
-          char *url = apr_psprintf(svn_wc_adm_access_pool(adm_access),
-                                   "%s%s", to, entry->url + from_len);
+          const char *url = apr_psprintf (svn_wc_adm_access_pool (adm_access),
+                                          "%s%s", to, entry->url + from_len);
           if (entry->uuid)
-            SVN_ERR(validator(validator_baton, entry->uuid, url));
+            SVN_ERR (validator (validator_baton, entry->uuid, url));
           entry->url = url;
-          SVN_ERR(svn_wc__entries_write (entries, adm_access, pool));
+          SVN_ERR (svn_wc__entries_write (entries, adm_access, pool));
         }
 
       return SVN_NO_ERROR;
@@ -81,59 +82,52 @@ svn_wc_relocate (const char *path,
      of all of the other entries.  This is technically cheating, but it
      significantly cuts down on the number of expensive validations the
      validator has to do. */
-  {
-    svn_wc_entry_t *entry = apr_hash_get(entries,
-                                         SVN_WC_ENTRY_THIS_DIR,
-                                         APR_HASH_KEY_STRING);
-    if (entry->url &&
-        (strncmp(entry->url, from, from_len) == 0)) 
-      {
-        char *url = apr_psprintf(svn_wc_adm_access_pool(adm_access),
-                                 "%s%s", to, entry->url + from_len);
-        if (entry->uuid)
-          SVN_ERR(validator(validator_baton, entry->uuid, url));
-        entry->url = url;
-      }
-  }
+  entry = apr_hash_get (entries, SVN_WC_ENTRY_THIS_DIR, APR_HASH_KEY_STRING);
+  if (entry->url && (strncmp (entry->url, from, from_len) == 0)) 
+    {
+      const char *url = apr_psprintf (svn_wc_adm_access_pool (adm_access),
+                                      "%s%s", to, entry->url + from_len);
+      if (entry->uuid)
+        SVN_ERR (validator (validator_baton, entry->uuid, url));
+      entry->url = url;
+    }
 
-  for (hi = apr_hash_first(pool, entries); hi; hi = apr_hash_next(hi))
+  for (hi = apr_hash_first (pool, entries); hi; hi = apr_hash_next (hi))
     {
       const void *key;
       void *val;
-      svn_wc_entry_t *entry;
 
-      apr_hash_this(hi, &key, NULL, &val);
+      apr_hash_this (hi, &key, NULL, &val);
       entry = val;
 
-      if (strcmp(key, SVN_WC_ENTRY_THIS_DIR) == 0)
+      if (strcmp (key, SVN_WC_ENTRY_THIS_DIR) == 0)
         continue;
 
-      if (recurse
-          && (entry->kind == svn_node_dir))
+      if (recurse && (entry->kind == svn_node_dir))
         {
           svn_wc_adm_access_t *subdir_access;
           const char *subdir = svn_path_join (path, key, pool);
-          if (svn_wc__adm_missing(adm_access, subdir))
+          if (svn_wc__adm_missing (adm_access, subdir))
             continue;
-          SVN_ERR(svn_wc_adm_retrieve(&subdir_access, adm_access, subdir,
-                                      pool));
-          SVN_ERR(svn_wc_relocate(subdir, subdir_access, from, to,
-                                  recurse, validator_baton, validator, pool));
+          SVN_ERR (svn_wc_adm_retrieve (&subdir_access, adm_access, 
+                                        subdir, pool));
+          SVN_ERR (svn_wc_relocate (subdir, subdir_access, from, to,
+                                    recurse, validator, 
+                                    validator_baton, pool));
         }
 
-      if (entry->url &&
-          (strncmp(entry->url, from, from_len) == 0)) 
+      if (entry->url && (strncmp (entry->url, from, from_len) == 0)) 
         {
-          char *url = apr_psprintf(svn_wc_adm_access_pool(adm_access),
-                                   "%s%s", to, entry->url + from_len);
+          char *url = apr_psprintf (svn_wc_adm_access_pool (adm_access),
+                                    "%s%s", to, entry->url + from_len);
           if (entry->uuid)
-            SVN_ERR(validator(validator_baton, entry->uuid, url));
+            SVN_ERR (validator (validator_baton, entry->uuid, url));
           entry->url = url;
         }
     }
 
-  SVN_ERR(svn_wc__remove_wcprops (adm_access, FALSE, pool));
-  SVN_ERR(svn_wc__entries_write (entries, adm_access, pool));
+  SVN_ERR (svn_wc__remove_wcprops (adm_access, FALSE, pool));
+  SVN_ERR (svn_wc__entries_write (entries, adm_access, pool));
   return SVN_NO_ERROR;
 }
 
