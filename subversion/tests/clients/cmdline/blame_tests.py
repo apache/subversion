e8ea33c0e880468d05134b6abcd373dd72b5d8b2
Julian Foad <julianfoad@apache.org>
2003-12-16 00:38:18 +0000

    Add a first regression test for "svn blame".
    * build.conf
      (fs-test-scripts): Add the new test file "blame_tests.py".
    
    * subversion/tests/clients/cmdline/blame_tests.py
      New file.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@848082 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subversion/tests/clients/cmdline/blame_tests.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+#
+#  blame_tests.py:  testing line-by-line annotation.
+#
+#  Subversion is a tool for revision control. 
+#  See http://subversion.tigris.org for more information.
+#    
+# ====================================================================
+# Copyright (c) 2000-2003 CollabNet.  All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.  The terms
+# are also available at http://subversion.tigris.org/license-1.html.
+# If newer versions of this license are posted there, you may use a
+# newer version instead, at your option.
+#
+######################################################################
+
+# General modules
+import os
+
+# Our testing module
+import svntest
+
+
+# (abbreviation)
+Skip = svntest.testcase.Skip
+XFail = svntest.testcase.XFail
+Item = svntest.wc.StateItem
+
+ 
+######################################################################
+# Tests
+#
+#   Each test must return on success or raise on failure.
+
+
+#----------------------------------------------------------------------
+
+def blame_space_in_name(sbox):
+  "annotate a file whose name contains a space"
+  sbox.build()
+  
+  file_path = os.path.join(sbox.wc_dir, 'space in name')
+  svntest.main.file_append(file_path, "Hello\n")
+  svntest.main.run_svn(None, 'add', file_path)
+  svntest.main.run_svn(None, 'ci',
+                       '--username', svntest.main.wc_author,
+                       '--password', svntest.main.wc_passwd,
+                       '-m', '', file_path)
+
+  svntest.main.run_svn(None, 'blame', file_path)
+
+
+########################################################################
+# Run the tests
+
+
+# list all tests here, starting with None:
+test_list = [ None,
+              XFail(blame_space_in_name)
+             ]
+
+if __name__ == '__main__':
+  svntest.main.run_tests(test_list)
+  # NOTREACHED
+
+
+### End of file.
