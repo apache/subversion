0706b9496c0315aacb9ef2c605f4918b76f4b014
Patrick Mayweg <pmayweg@apache.org>
2003-12-17 07:53:57 +0000

    As a followup r8015, build a better interface to the may_save parameter and pass it to java. * native/Prompter.cpp native/Prompter.h     add the may_save parameter to various methods and add support for new     java interface o.t.s.j.PromptUserPassword3, which will use this     parameter really.
    * native/svnjavahl.dsp
        compile new java interface o.t.s.j.PromptUserPassword3
    
    * src/org/tigris/subversion/javahl/PromptUserPassword2.java
        add missing copyright statement
    
    * src/org/tigris/subversion/javahl/PromptUserPassword3.java
        new interface to be implemented
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@848094 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/java/javahl/src/org/tigris/subversion/javahl/PromptUserPassword2.java
+++ subversion/bindings/java/javahl/src/org/tigris/subversion/javahl/PromptUserPassword2.java
@@ -1,12 +1,22 @@
+/**
+ * @copyright
+ * ====================================================================
+ * Copyright (c) 2003 CollabNet.  All rights reserved.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at http://subversion.tigris.org/license-1.html.
+ * If newer versions of this license are posted there, you may use a
+ * newer version instead, at your option.
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals.  For exact contribution history, see the revision
+ * history and logs, available at http://subversion.tigris.org/.
+ * ====================================================================
+ * @endcopyright
+ */
 package org.tigris.subversion.javahl;
 
-/**
- * Created by IntelliJ IDEA.
- * User: patrick
- * Date: Oct 18, 2003
- * Time: 9:14:34 AM
- * To change this template use Options | File Templates.
- */
 public interface PromptUserPassword2 extends PromptUserPassword
 {
     public static final int Reject = 0;
