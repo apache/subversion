88173dab4bacae46fca9a7241412188bd62e140d
Karl Fogel <kfogel@apache.org>
2004-01-11 22:32:27 +0000

    Fix recidivist error message:
    * subversion/clients/cmdline/export-cmd.c
      (svn_cl__export): Correct a misspelling noticed by Ben Reser,
        uncapitalize a miscapitalized word, and don't end with a period.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@848319 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/clients/cmdline/export-cmd.c
+++ subversion/clients/cmdline/export-cmd.c
@@ -73,8 +73,8 @@ svn_cl__export (apr_getopt_t *os,
                            opt_state->force, ctx, pool);
   if (err && err->apr_err == SVN_ERR_WC_OBSTRUCTED_UPDATE && !opt_state->force)
     SVN_ERR_W (err,
-               "Destination directory exists; Please remove "
-               "the directory or use --force to ovewrite.");
+               "Destination directory exists; please remove "
+               "the directory or use --force to overwrite");
   else
     SVN_ERR (err);
 
