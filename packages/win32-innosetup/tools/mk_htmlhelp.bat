836e90ac4db653f08470044b1b6a6aa004cb91e7
Jostein Chr Andersen <josander@apache.org>
2004-01-18 21:07:58 +0000

    Several packages (among this: Inno Setup itself and XML progs) are updated to newer versions. Inno Setup (IS) and Inno Setup Extensions (ISX) are now merged since its version 4. So any previous references, variables, functions or calls who was for ISX are now changed to IS
    * packages\win32-innosetup\tools\svnpath\main.c:
      (main) Previous Comment about ISX are changed to IX
    
    * packages\win32-innosetup\tools\mk_distro.pl:
      (MakeSetup): Anything that references to ISX are changed to IS
      (PathSetupOut): Ditto
    
    * packages\win32-innosetup\tools\mk_htmlhelp.bat:
      Extensive cleanups - Splitting long lines into smaller chunks
    
    * packages\win32-innosetup\isx_main.pas,
    * packages\win32-innosetup\is_main.pas:
    Renamed from isx_main.pas to is_main.pas
    
    * packages\win32-innosetup\svn.iss,
    * packages\win32-innosetup\templates\paths_inno_src.iss:
      Anything related to ISX are changed to IS
    
    * packages\win32-innosetup\Readme.txt:
      -Set propertys svn:keyword to LastChangedDate
      -Reflecting all the packages updates.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@848450 13f79535-47bb-0310-9956-ffa450edef68

--- packages/win32-innosetup/tools/mk_htmlhelp.bat
+++ packages/win32-innosetup/tools/mk_htmlhelp.bat
@@ -1,10 +1,15 @@
 @ECHO OFF
 SET BOOKNAME=book
 cd %BOOKNAME%
-SET XLSPARAMS=--param suppress.navigation 0 --param htmlhelp.hhc.binary 1 --param htmlhelp.show.advanced.search 1
-SET XLSPARAMS=%XLSPARAMS% --param htmlhelp.chm '%BOOKNAME%.chm' --param htmlhelp.hhp '%BOOKNAME%.hhp'
-SET XLSPARAMS=%XLSPARAMS% --param htmlhelp.hhk '%BOOKNAME%.hhk' --param htmlhelp.hhc '%BOOKNAME%.hhc'
-SET XLSPARAMS=%XLSPARAMS% --param html.stylesheet.type 'text/css' --param html.stylesheet '../svn-doc.css'
+SET XLSPARAMS=--param suppress.navigation 0 --param htmlhelp.hhc.binary 1
+SET XLSPARAMS=%XLSPARAMS% --param htmlhelp.show.advanced.search 1
+SET XLSPARAMS=%XLSPARAMS% --param htmlhelp.chm '%BOOKNAME%.chm'
+SET XLSPARAMS=%XLSPARAMS% --param htmlhelp.hhp '%BOOKNAME%.hhp'
+SET XLSPARAMS=%XLSPARAMS% --param htmlhelp.hhk '%BOOKNAME%.hhk'
+SET XLSPARAMS=%XLSPARAMS% --param htmlhelp.hhc '%BOOKNAME%.hhc'
+SET XLSPARAMS=%XLSPARAMS% --param html.stylesheet.type 'text/css'
+SET XLSPARAMS=%XLSPARAMS% --param html.stylesheet '../svn-doc.css'
+
 xsltproc %XLSPARAMS% ..\tools\xsl\htmlhelp\htmlhelp.xsl %BOOKNAME%.xml
 
 SET FOO=name=\"Local\" value=\"
@@ -14,10 +19,16 @@ cd ..
 
 SET BOOKNAME=misc-docs
 cd %BOOKNAME%
-SET XLSPARAMS=--param suppress.navigation 0 --param htmlhelp.hhc.binary 1 --param htmlhelp.show.advanced.search 1
-SET XLSPARAMS=%XLSPARAMS% --param htmlhelp.chm '%BOOKNAME%.chm' --param htmlhelp.hhp '%BOOKNAME%.hhp'
-SET XLSPARAMS=%XLSPARAMS% --param htmlhelp.hhk '%BOOKNAME%.hhk' --param htmlhelp.hhc '%BOOKNAME%.hhc'
-SET XLSPARAMS=%XLSPARAMS% --param html.stylesheet.type 'text/css' --param html.stylesheet '../svn-doc.css'
+SET XLSPARAMS=--param suppress.navigation 0 --param htmlhelp.hhc.binary 1
+SET XLSPARAMS=%XLSPARAMS% --param htmlhelp.hhc.show.root 0
+SET XLSPARAMS=%XLSPARAMS% --param htmlhelp.show.advanced.search 1
+SET XLSPARAMS=%XLSPARAMS% --param htmlhelp.chm '%BOOKNAME%.chm'
+SET XLSPARAMS=%XLSPARAMS% --param htmlhelp.hhp '%BOOKNAME%.hhp'
+SET XLSPARAMS=%XLSPARAMS% --param htmlhelp.hhk '%BOOKNAME%.hhk'
+SET XLSPARAMS=%XLSPARAMS% --param htmlhelp.hhc '%BOOKNAME%.hhc'
+SET XLSPARAMS=%XLSPARAMS% --param html.stylesheet.type 'text/css'
+SET XLSPARAMS=%XLSPARAMS% --param html.stylesheet '../svn-doc.css'
+
 xsltproc %XLSPARAMS% ..\tools\xsl\htmlhelp\htmlhelp.xsl %BOOKNAME%.xml
 
 SET FOO=name=\"Local\" value=\"
