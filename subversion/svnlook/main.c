7d18acf5764f7696cee2cf80a5c328803382ddf3
ringstrom <ringstrom@unknown>
2004-01-19 09:30:17 +0000

    Fix SIGTERM/SIGHUP typo found by YAMAZAKI Makoto.
    * subversion/svnadmin/main.c
    * subversion/svnlook/main.c
    * subversion/clients/cmdline/main.c
    
      (main): Fix SIGTERM/SIGHUP typo.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@848458 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svnlook/main.c
+++ subversion/svnlook/main.c
@@ -2012,7 +2012,7 @@ main (int argc, const char * const *argv)
   /* Set up our cancellation support. */
   apr_signal (SIGINT, signal_handler);
 #ifdef SIGHUP
-  apr_signal (SIGTERM, signal_handler);
+  apr_signal (SIGHUP, signal_handler);
 #endif
 #ifdef SIGTERM
   apr_signal (SIGTERM, signal_handler);
