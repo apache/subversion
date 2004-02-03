a0cd4f02cf6f5561fb91d6a4d98ce04ebe47e66f
C. Michael Pilato <cmpilato@apache.org>
2004-02-03 17:15:55 +0000

    Improve the dump-db.py cvs2svn debugging tool.
    * tools/cvs2svn/dump-db.py
      (usage): Fix usage.
      (main): Make this script able to take multiple input files, and
        drastically improve the formatting (at the cost of some speed, but
        whatever, this is a debugging tool).
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@848626 13f79535-47bb-0310-9956-ffa450edef68

--- tools/cvs2svn/dump-db.py
+++ tools/cvs2svn/dump-db.py
@@ -3,12 +3,13 @@
 import anydbm
 import marshal
 import sys
+import os.path
 
 
 def usage():
-  sys.stderr.write("Usage: %s DB-FILE\n\n" % sys.argv[0])
-  sys.stderr.write("Dump .db database files created by cvs2svn (for\n")
-  sys.stderr.write("debugging purposes, generally).\n")
+  cmd = sys.argv[0]
+  sys.stderr.write("Usage: %s DBFILE [...]\n\n" % os.path.basename(cmd))
+  sys.stderr.write("Dump .db database files created by cvs2svn.\n")
   sys.exit(1)
 
   
@@ -16,15 +17,22 @@ def main():
   argc = len(sys.argv)
   if argc < 2:
     usage()
-  db_file = anydbm.open(sys.argv[1], 'r')
-  keys = db_file.keys()
-  keys.sort()
-  for key in keys:
-    value = marshal.loads(db_file[key])
-    print 'KEY: ' + key
-    print 'VAL: ' + str(value)
+  for db_file in sys.argv[1:]:
+    print '*** ' + db_file + ' ***'
     print ''
-
+    db_file = anydbm.open(sys.argv[1], 'r')
+    keys = db_file.keys()
+    keys.sort()
+    longest_len = 0
+    for key in keys:
+      this_len = len(key)
+      if this_len > longest_len:
+        longest_len = this_len
+    for key in keys:
+      this_len = len(key)
+      value = str(marshal.loads(db_file[key]))
+      print ' ' * (longest_len - this_len) + key + ' : ' + value
+    print ''
 
 if __name__ == "__main__":
   main()
