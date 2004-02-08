8fdc1099ffa3adda4b101227931194d379ff4ac0
Philip Martin <philip@apache.org>
2004-02-08 22:18:36 +0000

    Detect invalid UTF-8 inputs when the native encoding is UTF-8.
    * build.conf: Add utf-test.
    
    * win-tests.py: Add utf-test.exe.
    
    * subversion/libsvn_subr/utf.c
      (invalid_utf8, check_utf8, check_cstring_utf8): New functions.
      (svn_utf_stringbuf_to_utf8, svn_utf_string_to_utf8,
       svn_utf_cstring_to_utf8, svn_utf_cstring_to_utf8_ex): Add validity
       check.
    
    * subversion/libsvn_subr/utf_impl.h
      (svn_utf__last_valid, svn_utf__is_valid, svn_utf__cstring_is_valid,
       svn_utf__last_valid2): New functions.
    
    * subversion/libsvn_subr/utf_validate.c: New file.
    
    * subversion/tests/libsvn_subr/utf-test.c: New file.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@848655 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/utf_impl.h
+++ subversion/libsvn_subr/utf_impl.h
@@ -38,6 +38,33 @@ const char *svn_utf__cstring_from_utf8_fuzzy (const char *src,
                                                apr_pool_t *));
 
 
+/* Return a pointer to the first character after the last valid UTF-8
+ * multi-byte character in the string SRC of length LEN.  If SRC is a valid
+ * UTF-8 the return value will point to the byte after SRC+LEN, otherwise
+ * it will point to the start of the first invalid multi-byte character.
+ * In either case all the characters between SRC and the return pointer are
+ * valid UTF-8.
+ */
+const char *svn_utf__last_valid(const char *src, apr_size_t len);
+
+/* Return TRUE if the string SRC of length LEN is a valid UTF-8 encoding
+ * according to the rules laid down by the Unicode 4.0 standard, FALSE
+ * otherwise.  This function is faster than svn_utf__last_valid.
+ */
+svn_boolean_t svn_utf__is_valid (const char *src, apr_size_t len);
+
+/* As for svn_utf__is_valid but SRC is NULL terminated. */
+svn_boolean_t svn_utf__cstring_is_valid (const char *src);
+
+/* As for svn_utf__last_valid but uses a different implementation without
+   lookup tables.  It avoids the table memory use (about 400 bytes) but the
+   function is longer (about 200 bytes extra) and likely to be slower when
+   the string is valid.  If the string is invalid this function may be
+   faster since it returns immediately rather than continuing to the end of
+   the string.  The main reason this function exists is to test the table
+   driven implementation.  */
+const char *svn_utf__last_valid2 (const char *src, apr_size_t len);
+
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
