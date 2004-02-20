138b8f7ee3d9c2495939872ae9b91220fb09b679
Jostein Chr Andersen <josander@apache.org>
2004-02-20 21:10:01 +0000

    * subversion/include/svn_version.h: Bumping the 1.0.0-beta1 version
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/1.0.0-beta1@848871 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_version.h
+++ subversion/include/svn_version.h
@@ -83,7 +83,7 @@ extern "C" {
  *
  * Always change this at the same time as SVN_VER_NUMTAG.
  */
-#define SVN_VER_TAG        " (dev build)"
+#define SVN_VER_TAG        "beta"
 
 
 /** Number tag: a string describing the version.
@@ -98,7 +98,7 @@ extern "C" {
  *
  * Always change this at the same time as SVN_VER_TAG.
  */
-#define SVN_VER_NUMTAG     "-dev"
+#define SVN_VER_NUMTAG     "-beta"
 
 
 /** Revision number: The repository revision number of this release.
