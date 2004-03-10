533db6208a5f8fc1415be3524c458adfae7ab2fd
breser <dev-null@apache.org>
2004-03-10 20:14:20 +0000

    Fix duplicate images directory in tarball.
    Approved by bliss
    
    * dist.sh
      Do not copy doc/book/book/images into the sandbox when building the
      tarball.  It should already be there from the export.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@849043 13f79535-47bb-0310-9956-ffa450edef68

--- dist.sh
+++ dist.sh
@@ -138,10 +138,6 @@ do
    cp "$name" "$DISTPATH/$name"
 done
 
-echo "Copying images into sandbox..."
-cp -r "doc/book/book/images/" "$DISTPATH/doc/book/book/images/"
-find "$DISTPATH" -type d -name .svn | xargs rm -rf
-
 cat > "$DISTPATH/ChangeLog.CVS" <<EOF
 The old CVS ChangeLog is kept at 
 
