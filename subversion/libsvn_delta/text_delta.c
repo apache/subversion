1d317cac2acd9cdfb81d6235564437e91e938bfc
ringstrom <ringstrom@unknown>
2004-04-05 18:47:52 +0000

    Revert r9280 and r9284 because it's not OK to change the value of a public header constant that is potatially used as a buffer size by an API user as pointed out by Greg Stein.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@849362 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_delta/text_delta.c
+++ subversion/libsvn_delta/text_delta.c
@@ -221,7 +221,7 @@ svn_txdelta (svn_txdelta_stream_t **stream,
   (*stream)->target = target;
   (*stream)->more = TRUE;
   (*stream)->pos = 0;
-  (*stream)->buf = apr_palloc (pool, 2 * SVN_DELTA_WINDOW_SIZE);
+  (*stream)->buf = apr_palloc (pool, 2 * SVN_STREAM_CHUNK_SIZE);
 
   /* Initialize MD5 digest calculation. */
   apr_md5_init (&((*stream)->context));
@@ -256,8 +256,8 @@ svn_txdelta_next_window (svn_txdelta_window_t **window,
                          apr_pool_t *pool)
 {
   svn_error_t *err;
-  apr_size_t source_len = SVN_DELTA_WINDOW_SIZE;
-  apr_size_t target_len = SVN_DELTA_WINDOW_SIZE;
+  apr_size_t source_len = SVN_STREAM_CHUNK_SIZE;
+  apr_size_t target_len = SVN_STREAM_CHUNK_SIZE;
   svn_txdelta__ops_baton_t build_baton = { 0 };
   
   /* Read the source stream. */
