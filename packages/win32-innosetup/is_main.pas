95bbef34749eff49fb5636e50972822500e36f87
Jostein Chr Andersen <josander@apache.org>
2004-04-09 09:37:59 +0000

    ï»¿The installer now handles the Apache Server for the NT variations of Windows if a Apache Service is running.
    NOTE: This is for installation only. The uninstall part for Apache will come
    later:
     -It Stops and uninstalls the Service
     -Copying modules and the Berkeley DB to Apache's modules directory
     -Editing Apache's configuration file (httpd.conf) if needed.
     -Reinstalls the service and starts the server.
     -Checks that the minimum version of an Apache service is running and warns the
      user if needed.
    
    * packages\win32-innosetup\Post.txt,
    * packages\win32-innosetup\Post.rtf:
      -Format change to Rich Text and renamed from Post.txt to Post.rtf
    
    * packages\win32-innosetup\Pre.rtf:
      Removed Apache info, the setup are doing it all.
    
    * packages\win32-innosetup\is_main.pas:
      So much changes that it's almost a complete rewrite. Here's the big picture:
      -Taking care of the Apache Server when running as a service.
      -Code Cleanups
      -Improvements of IS's Event Handling
    
    * packages\win32-innosetup\svn.iss:
      (Setup): -Changed Compression from lzma/max to lzma (defaults to max anyway)
               -InfoAfterFile now reads Post.rtf (and not Post.txt anymore)
      (Tasks): -Added the apachehandler task which is enabled if Apache should be
                handled.
      (Files): -We let svn_dynamics.iss and the pre proccessor (ISPP) deal with the
                name of the Berkeley dll. This name is something that changes from
                time to time.
    
    * packages\win32-innosetup\Readme.txt:
      -We now have to deal with making of binaries with diffrent compilers which
       means diffrent run time files and dynamic data.
      -4.2.1 is now the recommended version of Inno Setup (this prepares us for
       dealing with multi language setups).
    
    * packages\win32-innosetup\templates\svn_dynamics.iss:
      -Added Template variable name the Berkeley dll: libdb_dll
      -Added Template variable name for required Apache version: apache_ver_min
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@849379 13f79535-47bb-0310-9956-ffa450edef68

--- packages/win32-innosetup/is_main.pas
+++ packages/win32-innosetup/is_main.pas
@@ -1,4 +1,4 @@
-(* isx_main.pas: Pascal ISX routines for Inno Setup Windows installer.
+(* isx_main.pas: Pascal Scripts routines for Inno Setup Windows installer.
  * ====================================================================
  * Copyright (c) 2000-2004 CollabNet.  All rights reserved.
  *
@@ -16,13 +16,17 @@
 // ****************************************************************************
 // Global variables
 var
-    // Visual C++ 6.0 Runtime file related
-    g_bMsVcpNotFound: Boolean;
-    
-    // shfolder.dll related
-    g_bShFolderNotFound: Boolean;
+    // Required dlls
+    g_bMsVcpNotFound: Boolean;                // Visual C++ 6.0 Runtimes
+    g_bShFolderNotFound: Boolean;             // shfolder.dll
 
-// Constants
+    // Apache
+    g_bHandleApache: Boolean;
+    g_sApachePath: String;
+    g_sApachePathBin: String;
+    g_sApachePathConf: String;
+    g_sApachePathModules: String;
+
 const
     // Visual C++ 6.0 Runtime file related
     FILE_MSVCPDLL = 'msvcp60.dll';
@@ -32,6 +36,377 @@ const
     FILE_SHFOLDERDLL = 'shfolder.dll';
     URL_SHFREDIST = 'http://download.microsoft.com/download/platformsdk/Redist/5.50.4027.300/W9XNT4/EN-US/shfinst.EXE';
 
+    // Apache
+    REG_KEY_APACHE_SERVICE = 'SYSTEM\CurrentControlSet\Services\Apache2';
+    APACHE_VER_MIN = '{#= apache_ver_min}';
+
+    // Status codes for modules in httpd.conf
+    STATUS_NONE = 0;
+    STATUS_DISABLED = 1;
+    STATUS_ENABLED = 2;
+
+// ****************************************************************************
+// Name:    ApachePathParent
+// Purpose: Returns the path of Apache parent folder.
+function ApachePathParent(): String;
+var
+    sApachePathParent: String;
+    iLengthString: Integer;
+    iPosK: Integer;
+begin
+
+    if RegKeyExists(HKLM, REG_KEY_APACHE_SERVICE) and UsingWinNT then
+    begin
+        // Set g_sApachePathBin
+        RegQueryStringValue(HKLM, REG_KEY_APACHE_SERVICE,
+                            'ImagePath', sApachePathParent);
+
+        // Remove the run command and strip quotes away from g_sApachePathBin
+        iPosK := Pos('-k', sApachePathParent);
+        iLengthString := Length(sApachePathParent);
+        Delete(sApachePathParent, iLengthString - (iLengthString - iPosK), iPosK);
+        sApachePathParent := RemoveQuotes(sApachePathParent);
+
+        // Strip basename twice so only the Apache parent path is left
+        sApachePathParent := ExtractFileDir(sApachePathParent);
+        sApachePathParent := ExtractFileDir(sApachePathParent);
+    end;
+
+    // Function variables
+    Result := sApachePathParent;
+end;
+
+// ****************************************************************************
+// Name:    ApacheTask
+// Purpose: Decide if we should handle the Apache Server or not
+function ApacheTask(): Boolean;
+begin
+    Result:= UsingWinNT and not (ApachePathParent = '');
+end;
+
+// ****************************************************************************
+// Name:    ApacheBinFound
+// Purpose: Checks if bin\apache.exe excists in Apache's parent folder .
+//          Returns True if Yes and False if No
+function ApacheBinFound(): Boolean;
+var
+    sApacheBinary: String;
+begin
+    sApacheBinary := ApachePathParent() + '\bin\apache.exe';
+
+    if FileExists(sApacheBinary) then
+    begin
+        Result:= True;
+    end else begin
+        Result:= False;
+    end;
+end;
+
+// ****************************************************************************
+// Name:    ApacheServiceUninstall
+// Purpose: Stopping and uninstalling the Apache Service
+procedure ApacheServiceUninstall();
+var
+    bRetVal: Boolean;
+    ErrorCode: Integer;
+begin
+    // Stop and uninstall the Apache service
+    bRetVal := InstExec('cmd.exe', '/C apache -k stop', g_sApachePathBin,
+                             True, False, SW_HIDE, ErrorCode);
+    bRetVal := InstExec('cmd.exe', '/C apache -k uninstall', g_sApachePathBin,
+                             True, False, SW_HIDE, ErrorCode);
+end;
+
+// ****************************************************************************
+// Name:    ApacheServiceInstall
+// Purpose: Installing and starting the Apache Service
+procedure ApacheServiceInstall();
+var
+    bRetVal: Boolean;
+    ErrorCode: Integer;
+begin
+    // Install and start the Apache service
+	 	bRetVal := InstExec('cmd.exe', '/C apache -k install', g_sApachePathBin,
+                        True, False, SW_HIDE, ErrorCode);
+
+    bRetVal := InstExec('cmd.exe', '/C apache -k start', g_sApachePathBin,
+                        True, False, SW_HIDE, ErrorCode);
+end;
+
+
+// ****************************************************************************
+// Name:    ApacheModuleStatus
+// Purpose: Identifying if a module is in a string and returning it's status
+Function ApacheModuleStatus(sLine: String): Integer;
+var
+	iStatus: Integer;
+	iPosSharp, iPosModule: Integer;
+begin
+    iStatus:= STATUS_NONE;
+    iPosSharp := Pos('#', sLine);
+    iPosModule := Pos('LoadModule ', sLine);
+
+    if Pos('foo_module ', sLine) = 0 then
+    begin
+        if (iPosSharp > 0) and (iPosModule > iPosSharp) then
+        begin
+            iStatus := STATUS_DISABLED;
+        end else
+        begin
+            iStatus := STATUS_ENABLED;
+        end;
+    end;
+
+    Result := iStatus;
+end;
+
+// ****************************************************************************
+// Name:    ApacheModuleName
+// Purpose: Extracting and returning a module name from a string
+Function ApacheModuleName(sLine: String): String;
+var
+    iPosModNameStart, iPosModNameEnd: Integer;
+    iCharNum: Integer;
+    sModuleName : String;
+begin
+    iPosModNameStart := (Pos('modules/mod_', sLine) + 12);
+    iPosModNameEnd := (Pos('.so', sLine) - 1);
+
+    sModuleName := '';
+    iCharNum := iPosModNameStart;
+
+    for iCharNum := iPosModNameStart to iPosModNameEnd do
+    begin
+        sModuleName := sModuleName + StrGet(sLine, iCharNum);
+    end;
+
+    sModuleName := sModuleName + '_module';
+
+    Result := sModuleName;
+end;
+
+// ****************************************************************************
+// Name:    ApacheConfFileEdit
+// Purpose: Checking if the httpd.conf (Subversion related modules) file
+//          if needed.
+procedure ApacheConfFileEdit(aHttpdConf: TArrayOfString;
+                             iPosFileModules, iPosFileModulesPost,
+                             iPosModDav, iStatusModDav,
+                             iPosModDavSvn, iStatusModDavSvn,
+                             iPosModAuthzSvn, iStatusModAuthzSvn: Integer);
+var
+    sConfFileName: String;
+    sLoadModDav, sLoadModDavSvn, sLoadModAuthzSvn: String;
+begin
+    sConfFileName:= g_sApachePathConf + '\httpd.conf';
+
+    sLoadModDav := 'LoadModule dav_module modules/mod_dav.so';
+    sLoadModDavSvn := 'LoadModule dav_svn_module modules/mod_dav_svn.so';
+    sLoadModAuthzSvn := 'LoadModule authz_svn_module modules/mod_authz_svn.so';
+
+    //Backup the current httpd.conf
+    FileCopy (sConfFileName, sConfFileName + '-svn-' + GetDateString + '.bak', False)
+
+    // Add the modules if they're not there
+    if (iStatusModDav = STATUS_NONE) then
+    begin
+        if iPosModDav = 0 then
+        begin
+            iPosModDav := iPosFileModules + 10;
+        end;
+
+        aHttpdConf[iPosModDav] := aHttpdConf[iPosModDav] + #13#10 + sLoadModDav;
+    end;
+
+    if (iStatusModDavSvn = STATUS_NONE) then
+        aHttpdConf[iPosFileModulesPost -1] := aHttpdConf[iPosFileModulesPost -1] + #13#10 +
+                                              sLoadModDavSvn;
+
+    if (iStatusModAuthzSvn = STATUS_NONE) then
+        aHttpdConf[iPosFileModulesPost -1] := aHttpdConf[iPosFileModulesPost -1] + #13#10 +
+                                              sLoadModAuthzSvn;
+
+    // Enable modules if disabled ********************************
+    if (iStatusModDav = STATUS_DISABLED) then
+        aHttpdConf[iPosModDav] := sLoadModDav;
+
+    if (iStatusModDavSvn = STATUS_DISABLED) then
+        aHttpdConf[iPosModDavSvn] := sLoadModDavSvn;
+
+    if (iStatusModAuthzSvn = STATUS_DISABLED) then
+        aHttpdConf[iPosModAuthzSvn] := sLoadModAuthzSvn;
+
+    SaveStringsToFile(sConfFileName, aHttpdConf, False);
+end;
+
+// ****************************************************************************
+// Name:    ApacheConfFileHandle
+// Purpose: Checking if the httpd.conf (Subversion related modules) file should
+//          be edited and sets some data for further proccessing if needed.
+procedure ApacheConfFileHandle();
+var
+    aHttpdConf : TArrayOfString;
+
+    sConfFileName: String;
+    sCurrentLine: String;
+
+    iLineNum, iArrayLen: Integer;
+
+    iPosFileModules, iPosFileModulesPost : Integer;
+    iPosModDav, iPosModDavSvn, iPosModAuthzSvn: Integer;
+    iStatusModDav, iStatusModDavSvn, iStatusModAuthzSvn: Integer;
+begin
+    iStatusModDav := STATUS_NONE;
+    iStatusModDavSvn := STATUS_NONE;
+    iStatusModAuthzSvn := STATUS_NONE;
+    
+    sConfFileName:= g_sApachePathConf + '\httpd.conf';
+
+    //Load the httpd.conf to the aHttpdConf array  and init vars
+    LoadStringsFromFile(sConfFileName, aHttpdConf);
+
+    iArrayLen := GetArrayLength(aHttpdConf)-1;
+
+    // Check httpd.conf line by line
+    for iLineNum := 0 to iArrayLen do
+    begin
+        sCurrentLine := aHttpdConf[iLineNum];
+
+        // Get module status and file data with help of the modules
+        if (Pos('LoadModule ', sCurrentLine) > 0 ) then
+        begin
+            if (ApacheModuleStatus(sCurrentLine) > 0) then
+            begin
+                if iPosFileModules = 0 then
+                begin
+                    iPosFileModules := iLineNum;
+                end;
+
+                iPosFileModulesPost := iLineNum + 1;
+            end;
+
+            //Decide placements and status of modules --------
+
+            // dav_module: If we (for some reason) don't find dav_module then
+            // we'll try to set the placement with help of cgi_module and make
+            // sure that we do it _before_ a dav_fs_module.
+            if ApacheModuleName(sCurrentLine) = 'dav_module' then
+            begin
+                iPosModDav := iLineNum;
+                iStatusModDav := ApacheModuleStatus(sCurrentLine);
+            end;
+
+            if (ApacheModuleName(sCurrentLine) = 'cgi_module') and
+               ((iStatusModDav = STATUS_NONE) and (iPosModDav = 0)) then
+                   iPosModDav := iLineNum + 1;
+
+            if (ApacheModuleName(sCurrentLine) = 'dav_fs_module') and
+              (iStatusModDav = STATUS_NONE) then
+                   iPosModDav := iLineNum - 1;
+
+            // dav_svn_module:
+            if ApacheModuleName(sCurrentLine) = 'dav_svn_module' then
+            begin
+                iPosModDavSvn := iLineNum;
+                iStatusModDavSvn := ApacheModuleStatus(sCurrentLine);
+            end;
+
+            // authz_svn_module:
+            if ApacheModuleName(sCurrentLine) = 'authz_svn_module' then
+            begin
+                iPosModAuthzSvn := iLineNum;
+                iStatusModAuthzSvn := ApacheModuleStatus(sCurrentLine);
+            end;
+        end;
+    end;
+
+    // Edit httpd.conf if needed.
+    if (iStatusModDav + iStatusModDavSvn + iStatusModAuthzSvn) <>
+      (STATUS_ENABLED * 3) then
+    begin
+        ApacheConfFileEdit (aHttpdConf,
+                            iPosFileModules, iPosFileModulesPost,
+                            iPosModDav, iStatusModDav,
+                            iPosModDavSvn, iStatusModDavSvn,
+                            iPosModAuthzSvn, iStatusModAuthzSvn);
+    end;
+end;
+
+// ****************************************************************************
+// Name:    ApacheVersion
+// Purpose: Returns apache.exe's version with the last number stripped.
+function ApacheVersion(): String;
+var
+    sApacheVersion: String;
+begin
+    GetVersionNumbersString(g_sApachePathBin + '\apache.exe' ,sApacheVersion);
+    Delete(sApacheVersion, 7, 2);
+    Result := sApacheVersion;
+end;
+
+// ****************************************************************************
+// Name:    VerifyApache
+// Purpose: Finding/Setting Apache paths and version info
+procedure VerifyApache();
+var
+    sMsg: String;
+    sApacheVersion: String;
+begin
+    g_bHandleApache := True;
+
+    // Set/check the Apache paths
+    g_sApachePath := ApachePathParent;
+
+    // apache.exe
+    if g_sApachePathBin = '' then
+        g_sApachePathBin := g_sApachePath + '\bin';
+
+    if not FileExists(g_sApachePathBin + '\apache.exe') then
+    begin
+        sMsg := 'Could not find ''apache.exe'' in the system. Please, browse' +
+                ' to the folder where the Apache binary is.';
+        BrowseForFolder(sMsg , g_sApachePathBin, false);
+    end;
+
+    // httpd.conf
+    if g_sApachePathConf = '' then
+        g_sApachePathConf := g_sApachePath + '\conf';
+
+    if not FileExists(g_sApachePathConf + '\httpd.conf') then
+    begin
+        sMsg := 'Could not find ''httpd.conf'' in the system. Please, browse' +
+                ' to the folder where configuration file is.';
+        BrowseForFolder(sMsg, g_sApachePathConf, false);
+    end;
+
+    // Modules folder
+    if g_sApachePathModules = '' then
+        g_sApachePathModules := g_sApachePath + '\modules';
+
+    if not DirExists(g_sApachePathModules) then
+    begin
+        sMsg := 'Could not find the ''modules'' folder in the system. Please,' +
+                ' browse to the folder where the Apache modules is.';
+        BrowseForFolder(sMsg, g_sApachePathModules, false);
+    end;
+
+    // Check that we have the required Apache version and warn the user if
+    // needed
+    sApacheVersion := ApacheVersion;
+
+    if CompareStr(sApacheVersion, APACHE_VER_MIN) < 0 then
+    begin
+        sMsg :=
+            'WARNING: Apache http server version ' + sApacheVersion  + ' is detected and the'    + #13#10 +
+            'Subversion modules are built for the ' + APACHE_VER_MIN + ' version of the server.' + #13#10#13#10 +
+            'You are strongly encouraged to quit this setup and upgrade your'                    + #13#10 +
+            'apache server first, or go back to this setup''s ''Additional Tasks'''              + #13#10 +
+            'dialog box and uncheck the task of installing the Apache modules.'                  + #13#10;
+
+            MsgBox(sMsg, mbError, MB_OK);
+    end;
+end;
+
 // ****************************************************************************
 // Name:    ShFolderDllNotFound
 // Purpose: Checks if FILE_SHFOLDERDLL don't excists.
@@ -39,7 +414,6 @@ const
 function ShFolderDllNotFound(): Boolean;
 var
     sSysDir: String;
-
 begin
     sSysDir := ExpandConstant('{sys}');
 
@@ -49,7 +423,7 @@ begin
     end else begin
         g_bShFolderNotFound := True;
     end;
-    
+
     Result:= g_bShFolderNotFound;
 end;
 
@@ -63,9 +437,7 @@ var
     sFile: string;
     sDocument: string;
     sMsg: String;
-
 begin
-
     sItThem := ' it';
     sFile := ' file';
     sDocument := ' document';
@@ -79,19 +451,15 @@ begin
     end;
 
     if (g_bMsVcpNotFound and not g_bShFolderNotFound) then
-    begin
         sSysfiles := FILE_MSVCPDLL;
-    end;
 
     if (g_bShFolderNotFound and not g_bMsVcpNotFound) then
-    begin
         sSysfiles := FILE_SHFOLDERDLL;
-    end;
 
-	sMsg :='The' + sFile + ' ' + sSysFiles + ' was not found in the system.' + #13#10#13#10 +
+    sMsg :='The' + sFile + ' ' + sSysFiles + ' was not found in the system.'                 + #13#10#13#10 +
            'Please, go to the Subversion entry in the Start Menu after the installation and' + #13#10 +
-           'read the ''Download and install''' + sDocument + ' for ' + sSysfiles + '.' + #13#10#13#10 +
-           'Subversion will not work without this' + sFile + '.' + #13#10#13#10;
+           'read the ''Download and install''' + sDocument + ' for ' + sSysfiles + '.'       + #13#10#13#10 +
+           'Subversion will not work without this' + sFile + '.'                             + #13#10#13#10;
 
     MsgBox(sMsg, mbInformation, MB_OK);
 end;
@@ -103,7 +471,6 @@ end;
 function VCRuntimeNotFound(): Boolean;
 var
     sSysDir: String;
-
 begin
     sSysDir := ExpandConstant('{sys}');
 
@@ -113,32 +480,51 @@ begin
     end else begin
         g_bMsVcpNotFound := True;
     end;
-    
+
     Result:= g_bMsVcpNotFound;
 end;
 
 // ****************************************************************************
-// The rest happends in the build-in ISX functions (See ISX help file for help
-// about function names).
+// The rest is build-in functions/events. (See Inno help file for  info about
+// function/event names).
 function InitializeSetup(): Boolean;
-
 begin
     //Initialize some global variables
     g_bMsVcpNotFound := VCRuntimeNotFound;
     g_bShFolderNotFound := ShFolderDllNotFound;
+    g_bHandleApache:= False;
 
-    // Function variables
     Result := True;
+end;
+
+procedure CurPageChanged(CurStep: Integer);
+begin
+    case CurStep of
+        wpReady:      // Event after selected tasks
+            if (ShouldProcessEntry('', 'apachehandler') = srYes) then
+                VerifyApache;
+
+        wpInstalling: // Event before setup is copying destination files
+            if g_bHandleApache then
+                ApacheServiceUninstall;
+    end;
+end;
+
+procedure CurStepChanged(CurStep: Integer);
+begin
+    // Event after setup has copyed destination files
+    if (CurStep = wpInfoBefore) and g_bHandleApache then
+    begin;
+        ApacheConfFileHandle;
+        ApacheServiceInstall;
+    end;
 end;
 
 function NextButtonClick(CurPage: Integer): Boolean;
 begin
-    if (CurPage = wpSelectComponents) and 
-       (g_bMsVcpNotFound or g_bShFolderNotFound) then
-    begin
-        SysFilesDownLoadInfo();
-	end;
+    if (CurPage = wpSelectComponents) then
+        if (g_bMsVcpNotFound or g_bShFolderNotFound) then
+            SysFilesDownLoadInfo();
 
     Result := True;
 end;
-
