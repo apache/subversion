4225ca3168226bdd67676f64f7a8c0f7751b1896
Max Oliver Bowsher <maxb@apache.org>
2004-07-31 22:11:08 +0000

    Explicitly link the python bindings against libpython.
    * Makefile.in: New substitution variable, SWIG_PY_LIBS.
    * build.conf: Add SWIG_PY_LIBS to the libsvn_swig_py link.
    * build/ac-macros/swig.m4: Set and substitute SWIG_PY_LIBS.
    * build/get-py-info.py: New --libs option to provide SWIG_PY_LIBS.
      Teach how to escape OS X -framework options so libtool doesn't eat them.
      Teach how to add extra library arguments on Cygwin, like distutils does.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@850524 13f79535-47bb-0310-9956-ffa450edef68

--- build/get-py-info.py
+++ build/get-py-info.py
@@ -9,6 +9,7 @@
 
 import sys
 import os
+import string
 from distutils import sysconfig
 
 def usage():
@@ -18,6 +19,7 @@ def usage():
   print "    --includes : return -I include flags"
   print "    --compile  : return a compile command"
   print "    --link     : return a link command"
+  print "    --libs     : return just the library options for linking"
   sys.exit(1)
 
 if len(sys.argv) != 2:
@@ -40,9 +42,40 @@ if sys.argv[1] == '--compile':
   print cc, opt, ccshared
   sys.exit(0)
 
+def ldshared_process(just_libs = None):
+  ldshared = sysconfig.get_config_var('LDSHARED')
+  ldshared_elems = string.split(ldshared, " ")
+  libs_elems = []
+  for i in range(len(ldshared_elems)):
+    if ldshared_elems[i] == '-framework':
+      ldshared_elems[i] = '-Wl,' + ldshared_elems[i]
+      ldshared_elems[i+1] = '-Wl,' + ldshared_elems[i+1]
+      libs_elems.append(ldshared_elems[i])
+      libs_elems.append(ldshared_elems[i+1])
+    elif ldshared_elems[i][:2] == '-L':
+      if ldshared_elems[i][:3] != '-L:':
+        libs_elems.append(ldshared_elems[i])
+    elif ldshared_elems[i][:2] == '-l':
+      libs_elems.append(ldshared_elems[i])
+  if sys.platform[:6] == 'cygwin':
+    py_Lopt = "-L" + os.path.join(sys.prefix, "lib", "python" +
+        sysconfig.get_python_version(), "config")
+    py_lopt = "-lpython" + sysconfig.get_python_version()
+    libs_elems.append(py_Lopt)
+    libs_elems.append(py_lopt)
+    ldshared_elems.append(py_Lopt)
+    ldshared_elems.append(py_lopt)
+  if just_libs:
+    return string.join(libs_elems, " ")
+  else:
+    return string.join(ldshared_elems, " ")
+
 if sys.argv[1] == '--link':
-  ### why the hell is this a list?!
-  print sysconfig.get_config_vars('LDSHARED')[0]
+  print ldshared_process()
+  sys.exit(0)
+
+if sys.argv[1] == '--libs':
+  print ldshared_process(just_libs = 1)
   sys.exit(0)
 
 usage()
