d6a015b7fbd94150dd569fc5537ac367f5dd56b3
Greg Hudson <ghudson@apache.org>
2004-08-13 16:44:52 +0000

    Work around the directory creation bug in the APR shipped with httpd 2.0.50.  Pulling on this thread also means fixing some character encoding issues with the config dir.
    * subversion/libsvn_subr/io.c
      (svn_io_copy_recursively, svn_io_make_dir_recursively): Use
       svn_io_dir_make instead of apr_dir_make.
      (dir_make): If APR_OS_DEFAULT encompasses APR_WSTICKY, and we are
       passed APR_OS_DEFAULT, unset the three bits erroneously included
       in APR_OS_DEFAULT.
    * subversion/svnlook/main.c
      (create_unique_tmpdir): Use svn_io_dir_make instead of apr_dir_make.
    * subversion/libsvn_subr/config_file.c
      (svn_config__user_config_path): UTF8-encode the homedir from APR.
      (ensure_auth_dirs, svn_config_ensure): Use svn_io_dir_make instead
       of apr_dir_make, and svn_io_file_open instead of apr_file_open.
    * subversion/clients/cmdline/main.c
      (main): UTF8-encode the config dir if one is specified.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@850690 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svnlook/main.c
+++ subversion/svnlook/main.c
@@ -1260,7 +1260,6 @@ do_changed (svnlook_ctxt_t *c, apr_pool_t *pool)
 static svn_error_t *
 create_unique_tmpdir (const char **name, apr_pool_t *pool)
 {
-  const char *unique_name_apr;
   const char *unique_name;
   const char *sys_tmp_dir;
   const char *base_apr;
@@ -1273,27 +1272,21 @@ create_unique_tmpdir (const char **name, apr_pool_t *pool)
 
   for (i = 1; i <= 99999; i++)
     {
-      apr_status_t apr_err;
+      svn_error_t *err;
 
-      unique_name_apr = apr_psprintf (pool, "%s.%u", base_apr, i);
-      apr_err = apr_dir_make (unique_name_apr, APR_OS_DEFAULT, pool);
+      unique_name = apr_psprintf (pool, "%s.%u", base_apr, i);
+      err = svn_io_dir_make (unique_name, APR_OS_DEFAULT, pool);
 
-      if (APR_STATUS_IS_EEXIST (apr_err))
-        continue;
-
-      SVN_ERR (svn_path_cstring_to_utf8 (&unique_name, unique_name_apr, pool));
-
-      if (apr_err)
-        {
-          *name = NULL;
-          return svn_error_wrap_apr (apr_err, _("Can't create directory '%s'"),
-                                     unique_name);
-        }
-      else
+      if (!err)
         {
           *name = unique_name;
           return SVN_NO_ERROR;
         }
+
+      if (! APR_STATUS_IS_EEXIST (err->apr_err))
+        return err;
+
+      svn_error_clear (err);
     }
 
   *name = NULL;
