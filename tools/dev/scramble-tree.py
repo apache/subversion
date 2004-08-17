02ac5bda30c0b5a3e3f86f35dc8881a8fdf32075
C. Michael Pilato <cmpilato@apache.org>
2004-08-17 20:13:42 +0000

    Teach scramble-tree.py to use Subversion, give it a dry-run mode, and clean up a bunch of stuff.
    Suggested by: Elchan Aliev <aelchan@collab.net>
    
    * tools/dev/scramble-tree.py
      (hashDir.walker_callback, walker_callback): Whitespace mods only.
      (Scrambler.__init__): Add 'use_svn' and 'dry_run' parameters.
      (Scrambler._mod_append_to_file): Was append_to_file, and now takes
        an explicit path (instead of expecting self.path).  Do nothing in
        dry_run mode.
      (Scrambler._mod_delete_file): Was delete_file, and now takes an
        explicit path (instead of expecting self.path).  Also, if use_svn,
        use svn.  But do nothing in dry_run mode.
      (Scrambler.shrink_list): Moved into...
      (Scrambler._mod_remove_from_file): ...here.  Which was
        remove_from_file.  And which now takes an explicit path (instead
        of expecting self.path).  But which does nothing in dry_run mode.
      (Scrambler.munge_file): Pass path to the mod function.
      (Scrambler.maybe_add_file): If use_svn, use svn.  Unless in dry_run
        mode, in which case, do nothing.
      (usage): Update usage message.
      (main): Parse new options and stuff.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@850745 13f79535-47bb-0310-9956-ffa450edef68

--- tools/dev/scramble-tree.py
+++ tools/dev/scramble-tree.py
@@ -34,12 +34,12 @@
 
 import os
 import sys
+import getopt
 import random
 import md5
 import base64
 
 class hashDir:
-
   """Given a directory, creates a string containing all directories
   and files under that directory (sorted alphanumerically) and makes a
   base64-encoded md5 hash of the resulting string.  Call
@@ -49,121 +49,140 @@ class hashDir:
     self.allfiles = []
     os.path.walk(rootdir, self.walker_callback, len(rootdir))
 
-
   def gen_seed(self):
     # Return a base64-encoded (kinda ... strip the '==\n' from the
     # end) MD5 hash of sorted tree listing.
     self.allfiles.sort()
     return base64.encodestring(md5.md5(''.join(self.allfiles)).digest())[:-3]
 
-    
   def walker_callback(self, baselen, dirname, fnames):
-    if ((dirname == '.svn')
-        or (dirname == 'CVS')):
+    if ((dirname == '.svn') or (dirname == 'CVS')):
       return
-
     self.allfiles.append(dirname[baselen:])
-
     for filename in fnames:
       path = os.path.join(dirname, filename)
       if not os.path.isdir(path):
         self.allfiles.append(path[baselen:])
 
 
-
 class Scrambler:
-  def __init__(self, seed):
+  def __init__(self, seed, use_svn, dry_run):
+    self.use_svn = use_svn
+    self.dry_run = dry_run
     self.greeking = """
 ======================================================================
 This is some text that was inserted into this file by the lovely and
 talented scramble-tree.py script.
 ======================================================================
 """
-    self.file_modders = [self.append_to_file,
-                         self.append_to_file,
-                         self.append_to_file,                         
-                         self.remove_from_file,
-                         self.remove_from_file,
-                         self.remove_from_file,
-                         self.delete_file,
+    self.file_modders = [self._mod_append_to_file,
+                         self._mod_append_to_file,
+                         self._mod_append_to_file,                         
+                         self._mod_remove_from_file,
+                         self._mod_remove_from_file,
+                         self._mod_remove_from_file,
+                         self._mod_delete_file,
                          ]
     self.rand = random.Random(seed)
 
-
-  def shrink_list(self, list):
-    # remove 5 random lines
-    if len(list) < 6:
-      return list
-    for i in range(5): # TODO remove a random number of lines in a range
-      j = self.rand.randrange(len(list) - 1)
-      del list[j]
-    return list
-
-
-  def append_to_file(self):
-    print 'append_to_file:', self.path
-    fh = open(self.path, "a")
+  def _mod_append_to_file(self, path):
+    print 'append_to_file:', path
+    if self.dry_run:
+      return
+    fh = open(path, "a")
     fh.write(self.greeking)
     fh.close()
 
+  def _mod_remove_from_file(self, path):
+    print 'remove_from_file:', path
+    if self.dry_run:
+      return
+    def _shrink_list(list):
+      # remove 5 random lines
+      if len(list) < 6:
+        return list
+      for i in range(5): # TODO remove a random number of lines in a range
+        j = self.rand.randrange(len(list) - 1)
+        del list[j]
+      return list
+    lines = _shrink_list(open(path, "r").readlines())
+    open(path, "w").writelines(lines)
 
-  def remove_from_file(self):
-    print 'remove_from_file:', self.path
-    lines = self.shrink_list(open(self.path, "r").readlines())
-    open(self.path, "w").writelines(lines)
-
-
-  def delete_file(self):
-    print 'delete_file:', self.path
-    os.remove(self.path)
-
+  def _mod_delete_file(self, path):
+    print 'delete_file:', path
+    if self.dry_run:
+      return
+    os.remove(path)
+    if use_svn:
+      os.system('svn rm --quiet --force ' + path)
 
   def munge_file(self, path):
-    self.path = path
     # Only do something 33% of the time
     if self.rand.randrange(3):
-      self.rand.choice(self.file_modders)()
-
+      self.rand.choice(self.file_modders)(path)
 
   def maybe_add_file(self, dir):
     if self.rand.randrange(3) == 2:
       path = os.path.join(dir, 'newfile.txt')
       print "maybe_add_file:", path
+      if self.dry_run:
+        return
       open(path, 'w').write(self.greeking)
+      if use_svn:
+        os.system('svn add --quiet ' + path)
 
 
-
-def usage():
-  print "Usage:", sys.argv[0], "<directory> [SEED]"
-  sys.exit(255)
+def usage(retcode=255):
+  print 'Usage: %s [OPTIONS] DIRECTORY' % (sys.argv[0])
+  print ''
+  print 'Options:'
+  print '    --seed ARG  : Use seed ARG to scramble the tree.'
+  print '    --use-svn   : Use Subversion (as "svn") to perform file additions'
+  print '                  and removals.'
+  print '    --dry-run   : Don\'t actually change the disk.'
+  sys.exit(retcode)
 
 
 def walker_callback(scrambler, dirname, fnames):
-  if ((dirname.find('.svn') != -1)
-      or dirname.find('CVS') != -1):
+  if ((dirname.find('.svn') != -1) or dirname.find('CVS') != -1):
     return
-
   scrambler.maybe_add_file(dirname)
   for filename in fnames:
     path = os.path.join(dirname, filename)
     if not os.path.isdir(path):
       scrambler.munge_file(path)
 
-if __name__ == '__main__':
-  # If we have no ARG, exit
-  argc = len(sys.argv)
-  if argc < 2 or argc > 3:
-    usage()
-    sys.exit(254)
 
-  rootdir = sys.argv[1]
-  if argc == 2:
+if __name__ == '__main__':
+  seed = None
+  use_svn = 0
+  dry_run = 0
+
+  # Mm... option parsing.
+  optlist, args = getopt.getopt(sys.argv[1:], "h",
+                                ['seed=', 'use-svn', 'help', 'dry-run'])
+  for opt, arg in optlist:
+    if opt == '--help' or opt == '-h':
+      usage(0)
+    if opt == '--seed':
+      seed = arg
+    if opt == '--use-svn':
+      use_svn = 1
+    if opt == '--dry-run':
+      dry_run = 1
+
+  # We need at least a path to work with, here.
+  argc = len(args)
+  if argc < 1 or argc > 1:
+    usage()
+  rootdir = args[0]
+
+  # If a seed wasn't provide, calculate one.
+  if seed is None:
     seed = hashDir(rootdir).gen_seed()
-  else:
-    seed = sys.argv[2]
-  print 'Using seed: ' + seed
-  scrambler = Scrambler(seed)
+  scrambler = Scrambler(seed, use_svn, dry_run)
   
   # Fire up the treewalker
+  print 'SEED: ' + seed
+  
   os.path.walk(rootdir, walker_callback, scrambler)
-
