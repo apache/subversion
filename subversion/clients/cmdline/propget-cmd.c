735f8cae245b2dbe9c34e390f46eb45703c752f4
lundblad <dev-null@apache.org>
2004-10-05 18:50:54 +0000

    Fix silly mistake in r11202. (Thanks brane.)
    * clients/cmdline/propget-cmd.c (svn_cl__propget): When writing a newline,
      print the whole APR_EOL_STR instead of just the first character.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@851317 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/clients/cmdline/propget-cmd.c
+++ subversion/clients/cmdline/propget-cmd.c
@@ -129,7 +129,7 @@ svn_cl__propget (apr_getopt_t *os,
           
           SVN_ERR (stream_write (out, printable_val->data, 
                                  printable_val->len));
-          SVN_ERR (stream_write (out, APR_EOL_STR, 1));
+          SVN_ERR (stream_write (out, APR_EOL_STR, strlen (APR_EOL_STR)));
         }
     }
   else  /* operate on a normal, versioned property (not a revprop) */
@@ -199,7 +199,8 @@ svn_cl__propget (apr_getopt_t *os,
 
               SVN_ERR (stream_write (out, propval->data, propval->len));
               if (! opt_state->strict)
-                SVN_ERR (stream_write (out, APR_EOL_STR, 1));
+                SVN_ERR (stream_write (out, APR_EOL_STR,
+                                       strlen (APR_EOL_STR)));
             }
         }
       svn_pool_destroy (subpool);
