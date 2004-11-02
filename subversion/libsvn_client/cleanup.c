6452daee701d843342e3379d7e129a5f7d551de4
Philip Martin <philip@apache.org>
2004-11-02 20:12:15 +0000

    * subversion/libsvn_client/cleanup.c (svn_client_cleanup): Don't   bother generating SVN_ERR_WC_NOT_DIRECTORY as svn_wc_cleanup will   do it.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@851796 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/cleanup.c
+++ subversion/libsvn_client/cleanup.c
@@ -41,18 +41,11 @@ svn_client_cleanup (const char *dir,
                     svn_client_ctx_t *ctx,
                     apr_pool_t *pool)
 {
-  svn_node_kind_t kind;
   const char *diff3_cmd;
   svn_config_t *cfg = ctx->config
     ? apr_hash_get (ctx->config, SVN_CONFIG_CATEGORY_CONFIG,  
                     APR_HASH_KEY_STRING)
     : NULL;
-
-  SVN_ERR (svn_io_check_path (dir, &kind, pool));
-  if (kind != svn_node_dir)
-    return svn_error_createf (SVN_ERR_WC_NOT_DIRECTORY, NULL,
-                              _("Cannot cleanup '%s': not a directory"),
-                              dir);
 
   svn_config_get (cfg, &diff3_cmd, SVN_CONFIG_SECTION_HELPERS,
                   SVN_CONFIG_OPTION_DIFF3_CMD, NULL);
