0034a03cc6eb43dec20758e9f03f5596fb46105a
Julian Foad <julianfoad@apache.org>
2004-11-14 00:13:50 +0000

    Stop casting away const-ness where we don't need to.
    * subversion/libsvn_subr/io.c
      (test_tempdir): No need for "*path" to be const.
    
    * subversion/mod_authz_svn/mod_authz_svn.c
      (req_check_access): Modify the data before (not after) assigning it to
      a pointer-to-const, so a cast is not needed.
    
    * subversion/tests/libsvn_fs_base/skel-test.c
      (parse_cstr): "*str" can be const...
      (parse_invalid_atoms): so this no longer needs a cast.
    
    * subversion/tests/libsvn_fs_base/strings-reps-test.c
      (write_new_rep, write_rep, read_rep, delete_rep): Casts not needed.
    
    * subversion/tests/libsvn_subr/config-test.c
      (test1): The local string variables can be const (and can also be more
        local), so the casts are not needed.  One cast was not needed anyway.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@851965 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/mod_authz_svn/mod_authz_svn.c
+++ subversion/mod_authz_svn/mod_authz_svn.c
@@ -395,8 +395,8 @@ static int req_check_access(request_rec *r,
 
         apr_uri_parse(r->pool, dest_uri, &parsed_dest_uri);
 
+        ap_unescape_url(parsed_dest_uri.path);
         dest_uri = parsed_dest_uri.path;
-        ap_unescape_url((char *)dest_uri);
         if (strncmp(dest_uri, conf->base_path, strlen(conf->base_path))) {
             /* If it is not the same location, then we don't allow it.
              * XXX: Instead we could compare repository uuids, but that
