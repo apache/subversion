854a2c2a53bae82d4258b14f0fd5ccfb3530d918
lundblad <lundblad@unknown>
2004-11-25 21:06:55 +0000

    Follow-up to r12038.  Silly copy error.
    * subversion/include/svn_client.h (svn_client_diff_peg): Remove one of the two
      identical delcarations.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@852114 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_client.h
+++ subversion/include/svn_client.h
@@ -951,19 +951,6 @@ svn_error_t *svn_client_diff_peg (const apr_array_header_t *diff_options,
                                   svn_client_ctx_t *ctx,
                                   apr_pool_t *pool);
 
-svn_error_t *svn_client_diff_peg (const apr_array_header_t *diff_options,
-                                  const char *path,
-                                  const svn_opt_revision_t *peg_revision,
-                                  const svn_opt_revision_t *start_revision,
-                                  const svn_opt_revision_t *end_revision,
-                                  svn_boolean_t recurse,
-                                  svn_boolean_t ignore_ancestry,
-                                  svn_boolean_t no_diff_deleted,
-                                  apr_file_t *outfile,
-                                  apr_file_t *errfile,
-                                  svn_client_ctx_t *ctx,
-                                  apr_pool_t *pool);
-
 /** Merge changes from @a source1/@a revision1 to @a source2/@a revision2 into 
  * the working-copy path @a target_wcpath.
  *
