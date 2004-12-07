33e98085b9acb863982a3bd76f7070d9e1abcf3d
Philip Martin <philip@apache.org>
2004-12-07 19:37:14 +0000

    * subversion/libsvn_subr/utf.c (invalid_utf8): Use a single error string   to allow translation.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@852298 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/utf.c
+++ subversion/libsvn_subr/utf.c
@@ -429,7 +429,7 @@ static svn_error_t *
 invalid_utf8 (const char *data, apr_size_t len, apr_pool_t *pool)
 {
   const char *last = svn_utf__last_valid (data, len);
-  const char *msg = _("Valid UTF-8 data\n(hex:");
+  const char *valid_txt = "", *invalid_txt = "";
   int i, valid, invalid;
 
   /* We will display at most 24 valid octets (this may split a leading
@@ -438,22 +438,23 @@ invalid_utf8 (const char *data, apr_size_t len, apr_pool_t *pool)
   if (valid > 24)
     valid = 24;
   for (i = 0; i < valid; ++i)
-    msg = apr_pstrcat (pool, msg, apr_psprintf (pool, " %02x",
-                                                (unsigned char)last[i-valid]),
-                       NULL);
-  msg = apr_pstrcat (pool, msg,
-                     _(")\nfollowed by invalid UTF-8 sequence\n(hex:"), NULL);
+    valid_txt = apr_pstrcat (pool, valid_txt,
+                             apr_psprintf (pool, " %02x",
+                                           (unsigned char)last[i-valid]), NULL);
 
   /* 4 invalid octets will guarantee that the faulty octet is displayed */
   invalid = data + len - last;
   if (invalid > 4)
     invalid = 4;
   for (i = 0; i < invalid; ++i)
-    msg = apr_pstrcat (pool, msg, apr_psprintf (pool, " %02x",
-                                                (unsigned char)last[i]), NULL);
-  msg = apr_pstrcat (pool, msg, _(")"), NULL);
+    invalid_txt = apr_pstrcat (pool, invalid_txt,
+                               apr_psprintf (pool, " %02x",
+                                             (unsigned char)last[i]), NULL);
 
-  return svn_error_create (APR_EINVAL, NULL, msg);
+  return svn_error_createf (APR_EINVAL, NULL,
+                            _("Valid UTF-8 data\n(hex:%s)\n"
+                              "followed by invalid UTF-8 sequence\n(hex:%s)"),
+                            valid_txt, invalid_txt);
 }
 
 /* Verify that the sequence DATA of length LEN is valid UTF-8 */
