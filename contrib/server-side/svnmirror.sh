019d9096b7e394528b4ca4b357810b6bff10223b
Max Oliver Bowsher <maxb@apache.org>
2004-12-18 23:50:58 +0000

    Contrib update.
    Patch by: Markus Rückert <darix@irssi.org>
    
    * contrib/server-side/svnmirror.sh
        + added DUMPPARAMS to specify the params for the svnadmin dump call
        + added note about svnadmin 1.1 and "--deltas" cmdline option
        * made the script POSIX sh-compatible
          (zsh in native-mode does not work atm!)
        * changed handling of default settings
        * created config file has all lines commented out now
        + check if necessary values are set
        + added note about current users
        * fixed documentation in the default config
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@852462 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/server-side/svnmirror.sh
+++ contrib/server-side/svnmirror.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 #######################################################################
 #
 # svnmirror.sh
 #
-VERSION="0.0.4"
+VERSION="0.0.5"
 #
 # This script syncs changes from a developer repository to a
 #             _READONLY_public_repository_
@@ -37,52 +37,65 @@ VERSION="0.0.4"
 #
 # Authors:
 #    - Martin Furter <mf@rola.ch>
-#    - Marcus Rückert <darix@irssi.org>
+#    - Marcus RÃ¼ckert <darix@irssi.org>
+#    - Joerg Sonnenberger <joerg@bec.de>
 #
-#   with suggestions from:
-#        - tberman (#svn at freenode)
-#          - he actually needed such a script :)
-#        - Erik Huelsmann <e.huelsmann@gmx.net>
-#          - the little if for remote version :)
-#          - status reply
-#        - Ben Collins-Sussman <sussman@collab.net>
-#          - for some help with the svn commands
-#        - Bjørn Magnus Mathisen <epic@generation.no>
-#          - for pointing out i forgot to replace one ssh with $LSSH
-# TODO:
-#   - remove bashisms and port to plain old bourne shell
+#    with suggestions from:
+#       - tberman (#svn at freenode)
+#         - he actually needed such a script :)
+#       - Erik Huelsmann <e.huelsmann@gmx.net>
+#         - the little if for remote version :)
+#         - status reply
+#       - Ben Collins-Sussman <sussman@collab.net>
+#         - for some help with the svn commands
+#       - BjÃ¸rn Magnus Mathisen <epic@generation.no>
+#         - for pointing out i forgot to replace one ssh with $LSSH
+#
+# Users:
+#    our biggest users atm are Mono Project and MonoDevelop
+#    Mono Team currently mirrors the repos every 30 minutes!:)
+#    see http://www.mono-project.com/contributing/anonsvn.html
 #
 # License:
-#   The same as svn itself. for latest version check:
+#    The same as svn itself. for latest version check:
 #    http://svn.collab.net/repos/svn/trunk/subversion/LICENSE
 #
 # Thanks to the subversion team for their great work.
 #
 # Links:
-#
-#    If you do not like my solution check:
+#    If you do not like our solution check:
 #       - svnpush
-#         +  http://svn.collab.net/repos/svn/trunk/contrib/client-side/svn-push/svn-push.c
+#         + http://svn.collab.net/repos/svn/trunk/contrib/client-side/svn-push/svn-push.c
 #       - svn replicate
 #         + https://open.datacore.ch/read-only/
 #       - SVN::Mirror and SVN::Web
 #         + http://svn.elixus.org/repos/member/clkao/
 #         + http://svn.elixus.org/svnweb/repos/browse/member/clkao/ 
 #
-#
 # Changes:
 #
+#  0.0.5
+#    + added DUMPPARAMS to specify the params for the svnadmin dump call
+#    + added note about svnadmin 1.1 and "--deltas" cmdline option
+#    * made the script POSIX sh-compatible
+#      (zsh in native-mode does not work atm!)
+#    * changed handling of default settings
+#    * created config file has all lines commented out now
+#    + check if necessary values are set
+#    + added note about current users
+#    * fixed documentation in the default config
+#
 #  0.0.4
-#    added comandline switch -v which shows the version.
-#    using markers now to find the config so it's not twice in this script
-#    added/changed some more documentation
+#    + added comandline switch -v which shows the version.
+#    + using markers now to find the config so it's not twice in this script
+#    + added/changed some more documentation
 #
 #  0.0.3
-#    added comandline switches: -C (create config) -f (read config file)
+#    + added comandline switches: -C (create config) -f (read config file)
 #      -h (help) -q (quiet).
 #
 #  0.0.2
-#    initial version
+#    * initial version
 #
 #######################################################################
 #
@@ -97,6 +110,8 @@ VERSION="0.0.4"
 # Everything starting with "R" refers to the remote host
 # with "L" to the local host
 #
+# the required variables are LREPOS, RREPOS and RHOST
+#
 #######################################################################
 #
 # Mode:
@@ -104,40 +119,65 @@ VERSION="0.0.4"
 # push == Mirror from local repository to remote (readonly) repository
 # pull == Mirror from remote repository to local (readonly) repository
 #
-MODE="push"
+DFLT_MODE="push"
+
+#
+# keychain = path to keychain sh file
+# If the variable is not set, the script tries to read
+# "$HOME/.keychain/`uname -n`-sh", if it is readable.
+#
+DFLT_KEYCHAIN=""
+
+#
+# DUMPPARMS
+#
+# see "svnadmin help dump" for it.
+# default is "--incremental"
+# 
+# if you use svn 1.1 on both you should add "--deltas"
+# it should speed up the transfer on slow lines.
+#
+DFLT_DUMPPARAMS="--incremental"
 
 # Absolute path of svnlook on the local host.
-LSVNLOOK="/usr/bin/svnlook"
+DFLT_LSVNLOOK="/usr/bin/svnlook"
+
 # Absolute path of svnadmin on the local host.
-LSVNADMIN="/usr/bin/svnadmin"
+DFLT_LSVNADMIN="/usr/bin/svnadmin"
+
 # Absolute path to the repository on the local host.
-LREPOS="/path/to/repos"
+# REQUIRED
+DFLT_LREPOS="/path/to/repos"
 
 # Absolute path of ssh on the local host.
 #   '-c blowfish -C' to speed up the transfer a bit :)
-LSSH="/usr/bin/ssh -c blowfish -C"
+DFLT_LSSH="/usr/bin/ssh -c blowfish -C"
 
 # Name or IP of the remote host.
-RHOST="host"
-# UNIX username on the remote host.
-RUSER="user"
+# REQUIRED
+DFLT_RHOST="host"
 
+# UNIX username on the remote host.
+# defaults to the local username.
+DFLT_RUSER="user"
 
 # Absolute path of svnlook on the remote host.
-RSVNLOOK="/usr/bin/svnlook"
+DFLT_RSVNLOOK="/usr/bin/svnlook"
+
 # Absolute path of svnadmin on the remote host.
-RSVNADMIN="/usr/bin/svnadmin"
+DFLT_RSVNADMIN="/usr/bin/svnadmin"
 
 # Absolute path to the repository on the remote host.
-RREPOS="/path/to/repos"
+# REQUIRED
+DFLT_RREPOS="/path/to/repos"
 
 # Additional commands you want to run before loading the dump in to the repos
 # e.g. setting umask or change group via newgrp.
 #
 # Make sure the last char is a ";".
 #
-LADDITIONAL=""
-RADDITIONAL=""
+DFLT_LADDITIONAL=""
+DFLT_RADDITIONAL=""
 
 #_CONFIG_END_MARKER
 #######################################################################
@@ -150,22 +190,22 @@ create_config()
 {
     CFGFILE="$1"
     if [ -f "${CFGFILE}" ]; then
-        echo "config file '${CFGFILE}' allready exists."
+        echo "config file '${CFGFILE}' already exists." >&2
         exit 1
     fi
     SVNMIRROR="$0"
     if [ ! -f "$0" ]; then
-        echo blorg
         SVNMIRROR=`which "$0"`
         if [ $? -ne 0 ]; then
-            echo "could not locate $0"
+            echo "could not locate $0" >&2
             exit 1
         fi
     fi
     STARTLINE=`grep -n "^#_CONFIG_START_MARKER" "$SVNMIRROR" | sed 's/:.*$//'`
     ENDLINE=`grep -n "^#_CONFIG_END_MARKER" "$SVNMIRROR" | sed 's/:.*$//'`
-    LINECOUNT=`expr $ENDLINE - $STARTLINE`
-    head -n $ENDLINE "$SVNMIRROR" | tail -$LINECOUNT | \
+    ENDLINE=`expr ${ENDLINE} - 1`
+    LINECOUNT=`expr ${ENDLINE} - ${STARTLINE}`
+    head -n ${ENDLINE} "$SVNMIRROR" | tail -$LINECOUNT | sed -e 's/^#/##/' -e 's/^DFLT_/# /g' | \
         sed 's/default config/config/' > "$CFGFILE"
 }
 
@@ -190,6 +230,8 @@ show_help()
     echo "  -q              quiet (-q -q for really quiet)"
     echo "  -v              show version"
     echo ""
+    echo "Note: For -f, configfile is relative to the current PATH settings"
+    echo ""
 }
 
 CONFIGREAD=false
@@ -203,13 +245,13 @@ while getopts C:f:hqv OPTION; do
             ;;
         f)
             if [ ${CONFIGREAD} = true ]; then
-                echo "config allready read" >&2
+                echo "config already read" >&2
                 exit 1
             elif [ ! -r "${OPTARG}" ]; then
                 echo "cannot read config file '${OPTARG}'" >&2
                 exit 1
             else
-                source ${OPTARG}
+                . ${OPTARG}
             fi
             ;;
         h)
@@ -236,19 +278,38 @@ done
 
 #######################################################################
 #
+# add default values
+#
+#######################################################################
+MODE="${MODE:-$DFLT_MODE}"
+DUMPPARAMS="${DUMPPARAMS:-$DFLT_DUMPPARAMS}"
+LSVNLOOK="${LSVNLOOK:-$DFLT_LSVNLOOK}"
+LSVNADMIN="${LSVNADMIN:-$DFLT_LSVNADMIN}"
+LSSH="${LSSH:-$DFLT_LSSH}"
+RUSER="${RUSER:-$USER}"
+RSVNLOOK="${RSVNLOOK:-$DFLT_RSVNLOOK}"
+RSVNADMIN="${RSVNADMIN:-$DFLT_RSVNADMIN}"
+LADDITIONAL="${LADDITIONAL:-$DFLT_LADDITIONAL}"
+RADDITIONAL="${RADDITIONAL:-$DFLT_RADDITIONAL}"
+
+echo "${LREPOS:?Required variable LREPOS is not set in config file}" > /dev/null
+echo "${RREPOS:?Required variable RREPOS is not set in config file}" > /dev/null
+echo "${RHOST:?Required variable RHOST is not set in config file}" > /dev/null
+
+KEYCHAIN="${KEYCHAIN:-$HOME/.keychain/`uname -n`-sh}"
+[ -n "${KEYCHAIN}" -a -r "${KEYCHAIN}" ] && . ${KEYCHAIN}
+
+#######################################################################
+#
 # the actual script
 #
 #######################################################################
 #
-# Uncomment this line if you want to use keychain.
-# You maybe have to tweak the path to the script.
-#
-# source ~/.keychain/$(uname -n)-sh
 
 #
 # getting version of the remote repository
 #
-RVERSION=$(${LSSH} ${RUSER}@${RHOST} ${RSVNLOOK} youngest ${RREPOS})
+RVERSION=`${LSSH} ${RUSER}@${RHOST} ${RSVNLOOK} youngest ${RREPOS}`
 if [ -z "${RVERSION}" ] ; then
     echo "getting version of remote repository failed" >&2
     exit 1
@@ -258,7 +319,7 @@ fi
 # getting version of the local repository
 #
 ${LADDITIONAL}
-LVERSION=$(${LSVNLOOK} youngest ${LREPOS})
+LVERSION=`${LSVNLOOK} youngest ${LREPOS}`
 if [ -z "${LVERSION}" ] ; then
     echo "getting version of local repository failed" >&2
     exit 1
@@ -268,8 +329,8 @@ fi
 # compare revision numbers
 #
 if [ ${RVERSION} -eq ${LVERSION} ] ; then
-    [ $VERBOSE = true ] && \
-        echo "both repositories are already at ${LVERSION}"
+    [ ${VERBOSE} = true ] && \
+        echo "both repositories are already at ${LVERSION}" >&2
     exit 0
 fi
 
@@ -277,38 +338,40 @@ fi
 # syncing
 #
 RC=0
-if [ $MODE = "push" ] ; then
+if [ ${MODE} = "push" ] ; then
     if [ ${RVERSION} -gt ${LVERSION} ] ; then
         echo "revision of remote repos is higher than local one" >&2
         exit 1
     fi
-    REVRANGE="$((${RVERSION}+1)):${LVERSION}"
-    [ $VERBOSE = true ] && \
+    REVRANGE="`expr ${RVERSION} + 1`:${LVERSION}"
+    [ ${VERBOSE} = true ] && \
         echo -n "syncing r${REVRANGE} to ${RHOST} ";
-    ${LSVNADMIN} dump ${QUIET} --incremental -r${REVRANGE} ${LREPOS} | \
-    ${LSSH} $RUSER@$RHOST "${RADDITIONAL} ${RSVNADMIN} load ${QUIET} ${RREPOS}" || \
+    ${LSVNADMIN} dump ${QUIET} ${DUMPPARAMS} -r${REVRANGE} ${LREPOS} | \
+    ${LSSH} ${RUSER}@${RHOST} "${RADDITIONAL} ${RSVNADMIN} load ${QUIET} ${RREPOS}" || \
     RC=1
-elif [ $MODE = "pull" ] ; then
+elif [ ${MODE} = "pull" ] ; then
     if [ ${LVERSION} -gt ${RVERSION} ] ; then
         echo "revision of local repos is higher than remote one" >&2
         exit 1
     fi
-    REVRANGE="$((${LVERSION}+1)):${RVERSION}"
-    [ $VERBOSE = true ] && \
+    REVRANGE="`expr ${LVERSION} + 1`:${RVERSION}"
+    [ ${VERBOSE} = true ] && \
         echo -n "syncing r${REVRANGE} from ${RHOST} ";
     ${LSSH} ${RUSER}@${RHOST} \
-    "${RADDITIONAL} ${RSVNADMIN} dump ${QUIET} --incremental -r${REVRANGE} ${RREPOS}" | \
+    "${RADDITIONAL} ${RSVNADMIN} dump ${QUIET} ${DUMPPARAMS} -r${REVRANGE} ${RREPOS}" | \
     ${LSVNADMIN} load ${QUIET} ${LREPOS} || \
     RC=1
 else
     echo "invalid mode \"${MODE}\" specified!" >&2
     exit 1
 fi
-if [ $RC -ne 0 ]; then
+if [ ${RC} -ne 0 ]; then
     echo "failed" >&2
 else
-    [ $VERBOSE = true ] && \
+    [ ${VERBOSE} = true ] && \
         echo "successfull completed."
 fi
-exit $RC
+exit ${RC}
+
+
 
