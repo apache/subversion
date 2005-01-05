936fc152be68a0456c0da2b5485ec4e839c3d093
Max Oliver Bowsher <maxb@apache.org>
2005-01-05 14:51:32 +0000

    * build/generator/gen_win.py   (argv_to_command_string, escape_shell_arg): Remove comments suggesting that     this code is a duplicate of some elsewhere, since the target of that link     is about to change in ways we do not wish to copy here.   (argv_to_command_string): Delete, since it was only here to make a block of     code an identical copy/paste from elsewhere.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@852669 13f79535-47bb-0310-9956-ffa450edef68

--- build/generator/gen_win.py
+++ build/generator/gen_win.py
@@ -867,11 +867,8 @@ class ProjectItem:
     vars(self).update(kw)
 
 # ============================================================================
-# Reusable code segment. This code is duplicated in the several locations.
-# This is NOT the master copy.
-# The master copy is: subversion/subversion/bindings/swig/python/svn/core.py
-#  
-# Please keep all copies in sync.
+# This is a cut-down and modified version code from:
+#   subversion/subversion/bindings/swig/python/svn/core.py
 #
 if sys.platform == "win32":
   _escape_shell_arg_re = re.compile(r'(\\+)(\"|$)')
@@ -888,33 +885,10 @@ if sys.platform == "win32":
     arg = '"' + string.replace(arg, '"', '"^""') + '"'
     return arg
 
-
-  def argv_to_command_string(argv):
-    """Flatten a list of command line arguments into a command string.
-
-    The resulting command string is expected to be passed to the system
-    shell which os functions like popen() and system() invoke internally.
-    """
-
-    # According cmd's usage notes (cmd /?), it parses the command line by
-    # "seeing if the first character is a quote character and if so, stripping
-    # the leading character and removing the last quote character."
-    # So to prevent the argument string from being changed we add an extra set
-    # of quotes around it here.
-    return '"' + string.join(map(escape_shell_arg, argv), " ") + '"'
-
 else:
   def escape_shell_arg(str):
     return "'" + string.replace(str, "'", "'\\''") + "'"
 
-  def argv_to_command_string(argv):
-    """Flatten a list of command line arguments into a command string.
-
-    The resulting command string is expected to be passed to the system
-    shell which os functions like popen() and system() invoke internally.
-    """
-
-    return string.join(map(escape_shell_arg, argv), " ")
 # ============================================================================
 
 FILTER_LIBS = 1
