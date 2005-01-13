eefe0ffbed75b8decff53eece59179c166164d37
jaa <dev-null@apache.org>
2005-01-13 14:34:30 +0000

    Implement bindings testing with the svntest framework.
    At the moment following targets are tested:
    shared:
      swig-pl: build, check
      swig-py: build, install
      java-hl: build, install, check
    
    static:
      none
    
    * tools/test-scripts/svntest/svntest.sh
        Call svntest-bindings.sh from here.
    
    * tools/test-scripts/svntest/svntest-run.sh
        Removed RAM-disk checking from here, it is a function 
        'reinitialize_ramdisk' on the svntest-config.sh. We need its functionality
        with bindings.
    
    * tools/test-scripts/svntest/svntest-config.sh
        (SVN_NAME, SVN_REPO, APR_NAME, APR_REPO, APU_NAME, APR_REPO,
         HTTPD_NAME, HTTPD_REPO):  Use environment variables for these if they were
         set (the defaults are still the same).
        (TEST_BDB, TEST_FSFS): ditto.
        (TEST_BINDINGS_SWIG_PERL,
         TEST_BINDINGS_JAVAHL,
         TEST_BINDINGS_SWIG_PYHTON): New variables for controlling whether to test
           these bindings or not.
        (REVISION): New variable, this is the revision of the SVN repository.
        (reinitialize_ramdisk): New function, the code is moved from 
           'svntest-run.sh' file.
    
    * tools/test-scripts/svntest/svntest-sendmail-generic.sh
        A new file (copied from svntest-sendmail.sh), abstract generic email sending
        to here. We need this functionality with bindings.
    
    * tools/test-scripts/svntest/svntest-bindings-generic.sh
        A new file (copied from svntest-rebuild.sh).
        Build, install and test bindings here. "Install" and "check" are optional 
        targets.
        
    * tools/test-scripts/svntest/svntest-sendmail.sh
        Remove away generic email sending from here, and move it to the 
        'svntest-sendmail-generic.sh'.
        
    * tools/test-scripts/svntest/svntest-bindings.sh
        A new file, this is a driver script for actual bindings tests.
        (send_bindings_email): A new local function, for sending bindings status 
           email.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@852772 13f79535-47bb-0310-9956-ffa450edef68

--- tools/test-scripts/svntest/svntest-sendmail.sh
+++ tools/test-scripts/svntest/svntest-sendmail.sh
@@ -48,12 +48,13 @@ EOF
     exit 1
 fi
 
+SUBJECT="svn $REVPREFIX$REV: $BUILD_STAT ($TEST)"
 # Send the No-Op mail
 if [ "$BUILD_STAT" = "NOOP" ]
 then
     $SENDMAIL -t <<EOF
 From: $FROM
-Subject: svn $REVPREFIX$REV: NOOP ($TEST)
+Subject: $SUBJECT
 To: $TO
 
 $REVPREFIX$REV: There is nothing to test.
@@ -62,43 +63,15 @@ EOF
 fi
 
 # Send the status mail
-MAILFILE="/tmp/svntest.$$"
-NEXT_PART="NextPart-$$"
 TESTS_LOG_FILE="$LOG_FILE_DIR/tests.$BUILD_TYPE.$RA_TYPE.$FS_TYPE.log.gz"
-$CAT <<EOF > "$MAILFILE"
-From: $FROM
-Subject: svn $REVPREFIX$REV: $BUILD_STAT ($TEST)
-Reply-To: $REPLY_TO
-To: $TO
-EOF
+
 if [ "$BUILD_STAT" = "PASS" -o ! -f "$TESTS_LOG_FILE" ]
 then
-    echo "" >> "$MAILFILE"
-    $CAT "$LOG_FILE" >> "$MAILFILE"
+    $EXEC_PATH/svntest-sendmail-generic.sh "$TO" "$REPLY_TO" "$SUBJECT" \
+        "$LOG_FILE"
 else
-    $CAT <<EOF >> "$MAILFILE"
-MIME-Version: 1.0
-Content-Type: multipart/mixed; boundary="----------=_$NEXT_PART"
-
-This is a multi-part message in MIME format.
-------------=_$NEXT_PART
-Content-Type: text/plain; charset=us-ascii
-Content-Transfer-Encoding: 8bit
-
-EOF
-    $CAT "$LOG_FILE" >> "$MAILFILE"
-    $CAT <<EOF >> "$MAILFILE"
-------------=_$NEXT_PART
-Content-Type: application/x-gzip; name="tests.log.gz"
-Content-Transfer-Encoding: base64
-Content-Disposition: inline; filename="tests.log.gz"
-
-EOF
-    $BASE64_E < "$TESTS_LOG_FILE" >> "$MAILFILE"
-    $RM_F "$TESTS_LOG_FILE"
-    $CAT <<EOF >> "$MAILFILE"
-------------=_$NEXT_PART--
-EOF
+    $EXEC_PATH/svntest-sendmail-generic.sh "$TO" "$REPLY_TO" "$SUBJECT" \
+        "$LOG_FILE" "$TESTS_LOG_FILE"
 fi
-$SENDMAIL -t < "$MAILFILE"
-$RM_F "$MAILFILE"
+
+$RM_F "$TESTS_LOG_FILE"
