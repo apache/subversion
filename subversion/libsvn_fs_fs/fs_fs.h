3ea1c09e0b4de09e343cc0e9cca8f5f699de3a45
Brian Fitzpatrick <fitz@apache.org>
2005-01-14 20:07:49 +0000

    Whitespace fixups, no code changes.
    * libsvn_fs_fs/fs_fs.h: No space after *.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/locking@852809 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_fs/fs_fs.h
+++ subversion/libsvn_fs_fs/fs_fs.h
@@ -330,16 +330,16 @@ svn_error_t *svn_fs_fs__txn_changes_fetch (apr_hash_t **changes,
    directory to its final location NEW_FILENAME in the repository.  On
    Unix, match the permissions of the new file to the permissions of
    PERMS_REFERENCE.  Temporary allocations are from POOL. */
-svn_error_t * svn_fs_fs__move_into_place (const char *old_filename, 
-                                          const char *new_filename,
-                                          const char *perms_reference, 
-                                          apr_pool_t *pool);
+svn_error_t *svn_fs_fs__move_into_place (const char *old_filename, 
+                                         const char *new_filename,
+                                         const char *perms_reference, 
+                                         apr_pool_t *pool);
 
 /* Match the perms on FILENAME to the PERMS_REFERENCE file if we're
    not on a win32 system.  On win32, this is a no-op. */
-svn_error_t * svn_fs_fs__dup_perms (const char *filename,
-                                    const char *perms_reference,
-                                    apr_pool_t *pool);
+svn_error_t *svn_fs_fs__dup_perms (const char *filename,
+                                   const char *perms_reference,
+                                   apr_pool_t *pool);
 
 /* Return the path to the file containing revision REV in FS.
    Allocate the new char * from POOL. */
