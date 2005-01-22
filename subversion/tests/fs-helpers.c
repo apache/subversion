c0e0a48ed5853324f1cb323ec7046ee9111a3784
Max Oliver Bowsher <maxb@apache.org>
2005-01-22 12:54:33 +0000

    Fix the build when Berkeley DB is not available.
    Patch by: Alex Waugh <alex@alexwaugh.com>
    
    * build.conf (libsvn_test_fs): Remove unnecessary dependency on libsvn_fs_base
        and hence BDB.
    * subversion/tests/fs-helpers.c: Remove unused #includes.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@852893 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/fs-helpers.c
+++ subversion/tests/fs-helpers.c
@@ -26,13 +26,6 @@
 #include "svn_test.h"
 #include "fs-helpers.h"
 
-#include "../libsvn_fs_base/fs.h"
-#include "../libsvn_fs_base/dag.h"
-#include "../libsvn_fs_base/trail.h"
-
-#include "../libsvn_fs_base/bdb/rev-table.h"
-#include "../libsvn_fs_base/bdb/nodes-table.h"
-
 
 /*-------------------------------------------------------------------*/
 
