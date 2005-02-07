8508862bf0aa346abb54c7533b2cbdaadcfaeb99
Karl Fogel <kfogel@apache.org>
2005-02-07 20:02:05 +0000

    Follow up to r12904 with minor formatting and doc fixes.
    * subversion/mod_dav_svn/dav_svn.h
      (dav_svn_convert_err): Lose space between asterisk and open paren.
      (dav_svn__test_canonical): Add missing close-quote in doc string.
        Lose space between asterisk and open paren, which probably derived
        from the bogus example of dav_svn_convert_err in the first place.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@853009 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/mod_dav_svn/dav_svn.h
+++ subversion/mod_dav_svn/dav_svn.h
@@ -311,13 +311,13 @@ const char *dav_svn_get_xslt_uri(request_rec *r);
    NOTE: MESSAGE needs to hang around for the lifetime of the error since
    the current implementation doesn't copy it!  Lots of callers pass static
    string constant. */
-dav_error * dav_svn_convert_err(svn_error_t *serr, int status,
-                                const char *message, apr_pool_t *pool);
+dav_error *dav_svn_convert_err(svn_error_t *serr, int status,
+                               const char *message, apr_pool_t *pool);
 
 /* Test PATH for canonicalness (defined as "what won't make the
-   svn_path_* functions immediately explode), returning an
+   svn_path_* functions immediately explode"), returning an
    HTTP_BAD_REQUEST error tag if the test fails. */
-dav_error * dav_svn__test_canonical(const char *path, apr_pool_t *pool);
+dav_error *dav_svn__test_canonical(const char *path, apr_pool_t *pool);
 
 /* activity functions for looking up, storing, and deleting
    ACTIVITY->TXN mappings */
