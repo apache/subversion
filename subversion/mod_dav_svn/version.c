2ee31b830afd8a136c71b58e98a6de2802f24a7e
Ben Collins-Sussman <sussman@apache.org>
2005-02-08 21:40:54 +0000

    Locking branch:  implement server half of new svn_ra_get_locks() REPORT.
    * subversion/mod_dav_svn/version.c 
      (avail_reports):              ummmm... we support 6 reports now, not 2.  :-)
      (dav_svn_deliver_report):     recognize new 'get-locks-report' request.
      (dav_svn__get_locks_report):  new function.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/locking@853020 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/mod_dav_svn/version.c
+++ subversion/mod_dav_svn/version.c
@@ -32,6 +32,7 @@
 #include "svn_pools.h"
 #include "svn_props.h"
 #include "svn_dav.h"
+#include "svn_base64.h"
 
 #include "dav_svn.h"
 
@@ -41,6 +42,10 @@
 static const dav_report_elem avail_reports[] = {
   { SVN_XML_NAMESPACE, "update-report" },
   { SVN_XML_NAMESPACE, "log-report" },
+  { SVN_XML_NAMESPACE, "dated-rev-report" },
+  { SVN_XML_NAMESPACE, "get-locations" },
+  { SVN_XML_NAMESPACE, "file-revs-report" },
+  { SVN_XML_NAMESPACE, "get-locks-report" },
   { NULL },
 };
 
@@ -889,6 +894,169 @@ static dav_error * dav_svn__drev_report(const dav_resource *resource,
   return derr;
 }
 
+
+/* Respond to a get-locks-report request.  See description of this
+   report in libsvn_ra_dav/fetch.c.  */
+static dav_error * dav_svn__get_locks_report(const dav_resource *resource,
+                                             const apr_xml_doc *doc,
+                                             ap_filter_t *output)
+{
+  apr_bucket_brigade *bb;
+  svn_error_t *err;
+  apr_status_t apr_err;
+  apr_hash_t *locks;
+  dav_svn_authz_read_baton arb;
+  apr_hash_index_t *hi;
+  apr_pool_t *subpool;
+
+  /* The request URI should be a public one representing an fs path. */
+  if ((! resource->info->repos_path)
+      || (! resource->info->repos->repos))
+    return dav_new_error(resource->pool, HTTP_BAD_REQUEST, 0,
+                         "get-locks-report run on resource which doesn't "
+                         "represent a path within a repository.");
+
+  arb.r = resource->info->r;
+  arb.repos = resource->info->repos;
+
+  /* Fetch the locks, but allow authz_read checks to happen on each. */
+  if ((err = svn_repos_fs_get_locks(&locks,
+                                    resource->info->repos->repos,
+                                    resource->info->repos_path,
+                                    dav_svn_authz_read, &arb,
+                                    resource->pool)) != SVN_NO_ERROR)
+    return dav_svn_convert_err(err, HTTP_INTERNAL_SERVER_ERROR,
+                               err->message, resource->pool);      
+
+  bb = apr_brigade_create(resource->pool, output->c->bucket_alloc);
+
+  /* start sending report */
+  apr_err = ap_fprintf(output, bb,
+                       DAV_XML_HEADER DEBUG_CR
+                       "<S:get-locks-report xmlns:S=\"" SVN_XML_NAMESPACE "\" "
+                       "xmlns:D=\"DAV:\">" DEBUG_CR);
+  if (apr_err)
+    return dav_svn_convert_err(svn_error_create(apr_err, 0, NULL),
+                               HTTP_INTERNAL_SERVER_ERROR,
+                               "Error writing REPORT response.",
+                               resource->pool);
+
+  /* stream the locks */
+  subpool = svn_pool_create(resource->pool);
+  for (hi = apr_hash_first(resource->pool, locks); hi; hi = apr_hash_next(hi))
+    {
+      const void *key;
+      void *val;
+      const svn_lock_t *lock;
+      const char *path_quoted, *token_quoted;
+      const char *creation_str, *expiration_str;
+      const char *owner_to_send, *comment_to_send;
+      svn_boolean_t owner_base64 = FALSE, comment_base64 = FALSE;
+
+      svn_pool_clear(subpool);
+      apr_hash_this(hi, &key, NULL, &val);
+      lock = val;
+      
+      path_quoted = apr_xml_quote_string(subpool, lock->path, 1);
+      token_quoted = apr_xml_quote_string(subpool, lock->token, 1);
+      creation_str = svn_time_to_cstring(lock->creation_date, subpool);
+
+      apr_err = ap_fprintf(output, bb,
+                           "<S:lock>" DEBUG_CR
+                           "<S:path>%s</S:path>" DEBUG_CR
+                           "<S:token>%s</S:token>" DEBUG_CR
+                           "<S:creationdate>%s</S:creationdate>" DEBUG_CR,
+                           path_quoted, token_quoted, creation_str);
+      if (apr_err)
+        return dav_svn_convert_err(svn_error_create(apr_err, 0, NULL),
+                                   HTTP_INTERNAL_SERVER_ERROR,
+                                   "Error writing REPORT response.",
+                                   resource->pool);
+      
+      if (lock->expiration_date)
+        {
+          expiration_str = svn_time_to_cstring(lock->expiration_date, subpool);
+          apr_err = ap_fprintf(output, bb,
+                               "<S:expirationdate>%s</S:expirationdate>"
+                               DEBUG_CR, expiration_str);
+          if (apr_err)
+            return dav_svn_convert_err(svn_error_create(apr_err, 0, NULL),
+                                       HTTP_INTERNAL_SERVER_ERROR,
+                                       "Error writing REPORT response.",
+                                       resource->pool);
+        }
+
+      if (svn_xml_is_xml_safe(lock->owner, strlen(lock->owner)))
+        {
+          owner_to_send = apr_xml_quote_string(subpool, lock->owner, 1);
+        }
+      else
+        {
+          svn_string_t owner_string;
+          const svn_string_t *encoded_owner;
+
+          owner_string.data = lock->owner;
+          owner_string.len = strlen(lock->owner);         
+          encoded_owner = svn_base64_encode_string(&owner_string, subpool);
+          owner_to_send = encoded_owner->data;
+          owner_base64 = TRUE;
+        }
+
+      if (svn_xml_is_xml_safe(lock->comment, strlen(lock->comment)))
+        {
+          comment_to_send = apr_xml_quote_string(subpool, lock->comment, 1);
+        }
+      else
+        {
+          svn_string_t comment_string;
+          const svn_string_t *encoded_comment;
+
+          comment_string.data = lock->comment;
+          comment_string.len = strlen(lock->comment);         
+          encoded_comment = svn_base64_encode_string(&comment_string, subpool);
+          comment_to_send = encoded_comment->data;
+          comment_base64 = TRUE;
+        }
+
+      apr_err = ap_fprintf(output, bb,
+                           "<S:owner %s>%s</S:owner>" DEBUG_CR
+                           "<S:comment %s>%s</S:comment>" DEBUG_CR
+                           "<S:/lock>" DEBUG_CR,
+                           owner_base64 ? "encoding=\"base64\"" : "",
+                           owner_to_send,
+                           comment_base64 ? "encoding=\"base64\"" : "",
+                           comment_to_send);
+      if (apr_err)
+        return dav_svn_convert_err(svn_error_create(apr_err, 0, NULL),
+                                   HTTP_INTERNAL_SERVER_ERROR,
+                                   "Error writing REPORT response.",
+                                   resource->pool);
+    } /* end of hash loop */
+  svn_pool_destroy(subpool);
+
+  /* finish the report */
+  apr_err = ap_fprintf(output, bb,
+                       DAV_XML_HEADER DEBUG_CR
+                       "</S:get-locks-report>");
+  if (apr_err)
+    return dav_svn_convert_err(svn_error_create(apr_err, 0, NULL),
+                               HTTP_INTERNAL_SERVER_ERROR,
+                               "Error writing REPORT response.",
+                               resource->pool);
+
+  /* Flush the contents of the brigade (returning an error only if we
+     don't already have one). */
+  if ((apr_err = ap_fflush(output, bb)))
+    return dav_svn_convert_err(svn_error_create(apr_err, 0, NULL),
+                               HTTP_INTERNAL_SERVER_ERROR,
+                               "Error flushing brigade.",
+                               resource->pool);
+
+  return NULL;
+}
+
+
+
 static apr_status_t send_get_locations_report(ap_filter_t *output,
                                               apr_bucket_brigade *bb,
                                               const dav_resource *resource,
@@ -1066,6 +1234,10 @@ static dav_error *dav_svn_deliver_report(request_rec *r,
       else if (strcmp(doc->root->name, "file-revs-report") == 0)
         {
           return dav_svn__file_revs_report(resource, doc, output);
+        }
+      else if (strcmp(doc->root->name, "get-locks-report") == 0)
+        {
+          return dav_svn__get_locks_report(resource, doc, output);
         }
     }
 
