51e729f70b61d9c25c6d90837b716419c2939916
Brian Fitzpatrick <fitz@apache.org>
2005-03-11 20:13:24 +0000

    Documentation tweak at Greg Hudson's suggestion.
    * subversion/include/svn_error.h (svn_error_is_lock_error,
      svn_error_is_unlock_error): Remove comments that state err cannot be
      NULL since that's assumed.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/locking@853454 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_error.h
+++ subversion/include/svn_error.h
@@ -222,16 +222,14 @@ void svn_handle_warning (FILE *stream, svn_error_t *error);
  * @since New in 1.2.  
  *
  * Return TRUE if @a err is an error specifically related to locking a
- * path in the repository, FALSE otherwise.  @a err may not be
- * NULL. */
+ * path in the repository, FALSE otherwise. */
 svn_boolean_t svn_error_is_lock_error (svn_error_t *err);
 
 /* 
  * @since New in 1.2.  
  *
  * Return TRUE if @a err is an error specifically related to unlocking
- * a path in the repository, FALSE otherwise.  @a err may not be
- * NULL. */
+ * a path in the repository, FALSE otherwise. */
 svn_boolean_t svn_error_is_unlock_error (svn_error_t *err);
 
 
