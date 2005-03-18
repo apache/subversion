3442a0bdfa455d4b9f6fbed59e1ad7ee6884363b
Ben Collins-Sussman <sussman@apache.org>
2005-03-18 23:54:41 +0000

    Locking branch:  make lock_tests.py #1 pass over ra_dav.
    * subversion/tests/clients/cmdline/lock_tests.py 
      (lock_file):  look for both ra_dav and ra_local/ra_svn error messages.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/locking@853560 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/clients/cmdline/lock_tests.py
+++ subversion/tests/clients/cmdline/lock_tests.py
@@ -66,7 +66,7 @@ def lock_file(sbox):
                                      '-m', '', file_path)
 
   # --- Meanwhile, in our other working copy... ---
-  err_re = ".*User jconstant does not own lock on path.*"
+  err_re = "((.*User jconstant does not own lock on path.*)|(.*423 Locked.*))"
 
   svntest.main.run_svn(None, 'update', wc_b)
   # -- Try to change a file --
@@ -89,6 +89,9 @@ def lock_file(sbox):
   # -- Try to change a property --
   # change the locked file's properties
   svntest.main.run_svn(None, 'propset', 'sneakyuser', 'Sally', file_path_b)
+
+  err_re = "((.*User jconstant does not own lock on path.*)\
+             |(.*At least one property change failed.*))"
 
   # attempt (and fail) to commit as user Sally
   svntest.actions.run_and_verify_commit (wc_b, None, None, err_re,
