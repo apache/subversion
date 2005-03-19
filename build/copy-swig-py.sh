78ab05e45f7ce5381ebf263a0e4489cd194a1c77
Max Oliver Bowsher <maxb@apache.org>
2005-03-19 20:12:32 +0000

    Fix issue #1131. DESTDIR path erroneously embedded in Python compiled bytecode. Patch by Olaf Hering.
    * Makefile.in (INSTALL_EXTRA_SWIG_PY):
        Pass DESTDIR seperately to copy-swig-py.sh.
    * build/copy-swig-py.sh:
        Handle DESTDIR properly and tell the Python bytecode compiler about it.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@853586 13f79535-47bb-0310-9956-ffa450edef68

--- build/copy-swig-py.sh
+++ build/copy-swig-py.sh
@@ -2,29 +2,26 @@
 #
 # copy-swig-py.sh: copy the Python bindings' .py files to the install locn
 #
-# USAGE: copy-swig-py.sh PYTHON INSTALL SOURCE_DIR DEST_DIR
+# USAGE: copy-swig-py.sh PYTHON INSTALL SOURCE_DIR TARGET_DIR DESTDIR
 #
 
-if test "$#" != 4; then
-  echo USAGE: $0 PYTHON INSTALL SOURCE_DIR DEST_DIR
-  exit 1
+if test "$#" != 5; then
+  echo USAGE: $0 PYTHON INSTALL SOURCE_DIR TARGET_DIR DESTDIR
 fi
 
 pyprog="$1"
 install="$2"
 srcdir="$3"
-dstdir="$4"
+instdir="$4"
+destdir="$5"
 
 # cd to the source directory so that we can get filenames rather than paths
 cd "$srcdir"
 
 # copy all the .py files to the target location
 for file in *.py; do
-  $install "$file" "${dstdir}/${file}"
+  $install "$file" "${destdir}${instdir}/${file}"
 done
 
-# figure out where the precompiling script is located
-script="`\"$pyprog\" -c 'import compileall; print compileall.__file__'`"
-
 # precompile all of the files
-"$pyprog" "$script" "$dstdir"
+"$pyprog" -c "import compileall; compileall.compile_dir(\"${destdir}${instdir}\",1,\"${instdir}\")"
