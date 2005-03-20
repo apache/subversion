083a8931f23ed1c7055e5617966700b6f436fe26
lundblad <dev-null@apache.org>
2005-03-20 00:12:53 +0000

    Last part of the notify API redesign.  Make the cmdline client use the new API.
    * subversion/clients/cmdline/cl.h (svn_cl__get_notifier): Change to new
      notify func type.  Callers updated.
    
    * subversion/clients/cmdline/notify.c (notify_baton): Remove pool field.
      (notify): Change signature to new notify func type.  Used pool passed
      by caller instead of pool of baton.  Rewrite everything to use the notify
      struct.  Reindent.
      (svn_cl__get_notifier): Change to new notify func.  Don't create a pool
      for the baton.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@853594 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/clients/cmdline/merge-cmd.c
+++ subversion/clients/cmdline/merge-cmd.c
@@ -160,8 +160,8 @@ svn_cl__merge (apr_getopt_t *os,
     opt_state->end_revision.kind = svn_opt_revision_head;
 
   if (! opt_state->quiet)
-    svn_cl__get_notifier (&ctx->notify_func, &ctx->notify_baton, FALSE, FALSE,
-                          FALSE, pool);
+    svn_cl__get_notifier (&ctx->notify_func2, &ctx->notify_baton2, FALSE,
+                          FALSE, FALSE, pool);
 
   if (using_alternate_syntax)
     {
