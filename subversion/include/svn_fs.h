04f816598ccd2adcf1fc0cbe1a84d0ca98f7875a
Julian Foad <julianfoad@apache.org>
2005-04-07 15:37:38 +0000

    Rename a locking error code for clarity.
    * subversion/include/svn_error_codes.h
      (SVN_ERR_FS_PATH_LOCKED): Rename to ...
      (SVN_ERR_FS_PATH_ALREADY_LOCKED): New name; change message accordingly.
    
    * subversion/libsvn_fs_base/err.c
    * subversion/libsvn_fs_base/err.h
      (svn_fs_base__err_path_locked): Rename to ...
      (svn_fs_base__err_path_already_locked): New name; change message accordingly.
    
    * subversion/libsvn_fs_fs/err.c
    * subversion/libsvn_fs_fs/err.h
      (svn_fs_fs__err_path_locked): Rename to ...
      (svn_fs_fs__err_path_already_locked): New name; change message accordingly.
    
    * subversion/include/svn_error.h (SVN_ERR_IS_LOCK_ERROR)
    * subversion/include/svn_fs.h (svn_fs_lock)
    * subversion/libsvn_fs_base/lock.c (txn_body_lock)
    * subversion/libsvn_fs_fs/lock.c (lock_body)
    * subversion/libsvn_ra_dav/commit.c (commit_delete_entry)
      Update call sites and comments accordingly.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@854085 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_fs.h
+++ subversion/include/svn_fs.h
@@ -1509,7 +1509,7 @@ svn_error_t *svn_fs_set_uuid (svn_fs_t *fs,
  * @a comment is optional: it's either an xml-escapable UTF8 string
  * which describes the lock, or it is @c NULL.
  *
- * If path is already locked, then return @c SVN_ERR_FS_PATH_LOCKED,
+ * If path is already locked, then return @c SVN_ERR_FS_PATH_ALREADY_LOCKED,
  * unless @a steal_lock is true, in which case "steal" the existing
  * lock, even if the FS access-context's username does not match the
  * current lock's owner: delete the existing lock on @a path, and
