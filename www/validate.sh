c434f5d1ccdead06a9307737dae9edd6591726cd
Max Oliver Bowsher <maxb@apache.org>
2005-04-08 00:27:08 +0000

    Extend the www validation script to accept "all" as a parameter, and produce a report of the validity of all html files in the directory.
    * www/validate.sh: Add support for "all" parameter.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@854102 13f79535-47bb-0310-9956-ffa450edef68

--- www/validate.sh
+++ www/validate.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
+WWWDIR="`dirname \"$0\"`"
+
+if [ "$1" = "all" ]; then
+  WARNFILE=".validation-warnings.$$.tmp"
+  for f in "$WWWDIR"/*.html; do
+    if ! grep -F -q '<!DOCTYPE' "$f"; then
+      RESULT='No <!DOCTYPE'
+    else
+      if "$WWWDIR/validate.sh" "$f" 2>"$WARNFILE"; then
+        RESULT='\033[32mvalid\033[0m'
+      else
+        WARNLINES="`cat \"$WARNFILE\" | wc -l`"
+        RESULT='\033[31;1mINVALID ('"$WARNLINES"')\033[0m'
+      fi
+    fi
+    echo -e "$f: $RESULT"
+  done
+  rm -f "$WARNFILE"
+  exit 0
+fi
+
 export SP_CHARSET_FIXED=YES
 export SP_ENCODING=XML
 
@@ -8,8 +29,6 @@ export SGML_CATALOG_FILES="/usr/share/OpenSP/xml.soc"
 
 # Much of this script is concerned with setting up a local cache of the
 # DTD and files required by the DTD, so they are not re-downloaded every time.
-
-WWWDIR="`dirname \"$0\"`"
 
 ensure ()
 {
