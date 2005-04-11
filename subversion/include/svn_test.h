2045225d1baf0c5129cbb77683289960c46aabfd
Max Oliver Bowsher <maxb@apache.org>
2005-04-11 16:14:42 +0000

    There is nothing whatsoever public about svn_test.h.
    * subversion/include/svn_test.h: De-doxygenate.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@854192 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_test.h
+++ subversion/include/svn_test.h
@@ -1,5 +1,4 @@
-/**
- * @copyright
+/*
  * ====================================================================
  * Copyright (c) 2000-2004 CollabNet.  All rights reserved.
  *
@@ -13,10 +12,6 @@
  * individuals.  For exact contribution history, see the revision
  * history and logs, available at http://subversion.tigris.org/.
  * ====================================================================
- * @endcopyright
- *
- * @file svn_test.h
- * @brief public interfaces for test programs
  */
 
 #ifndef SVN_TEST_H
@@ -35,22 +30,22 @@ extern "C" {
 #endif /* __cplusplus */
 
 
-/** Baton for any arguments that need to be passed from main() to svn
-    test functions  */
-typedef struct svn_test_opts_t
+/* Baton for any arguments that need to be passed from main() to svn
+ * test functions  */
+  typedef struct svn_test_opts_t
 {
-  /** Description of the fs backend that should be used for testing. */
+  /* Description of the fs backend that should be used for testing. */
   const char *fs_type;
   /* Add future "arguments" here. */
 } svn_test_opts_t;
 
-/** Prototype for test driver functions. */
+/* Prototype for test driver functions. */
 typedef svn_error_t* (*svn_test_driver_t) (const char **msg, 
                                            svn_boolean_t msg_only,
                                            svn_test_opts_t *opts,
                                            apr_pool_t *pool);
 
-/** Test modes. */
+/* Test modes. */
 enum svn_test_mode_t
   {
     svn_test_pass,
@@ -58,36 +53,36 @@ enum svn_test_mode_t
     svn_test_skip
   };
 
-/** Each test gets a test descriptor, holding the function and other
+/* Each test gets a test descriptor, holding the function and other
  * associated data. */
 struct svn_test_descriptor_t
 {
-  /** A pointer to the test driver function. */
+  /* A pointer to the test driver function. */
   svn_test_driver_t func;
 
-  /** Is the test marked XFAIL? */
+  /* Is the test marked XFAIL? */
   enum svn_test_mode_t mode;
 };
 
-/** All Subversion test programs include an array of @c svn_test_descriptor_t's
+/* All Subversion test programs include an array of @c svn_test_descriptor_t's
  * (all of our sub-tests) that begins and ends with a @c SVN_TEST_NULL entry.
  */
 extern struct svn_test_descriptor_t test_funcs[];
 
-/** A null initializer for the test descriptor. */
+/* A null initializer for the test descriptor. */
 #define SVN_TEST_NULL  {NULL, 0}
 
-/** Initializer for PASS tests */
+/* Initializer for PASS tests */
 #define SVN_TEST_PASS(func)  {func, svn_test_pass}
 
-/** Initializer for XFAIL tests */
+/* Initializer for XFAIL tests */
 #define SVN_TEST_XFAIL(func) {func, svn_test_xfail}
 
-/** Initializer for SKIP tests */
+/* Initializer for SKIP tests */
 #define SVN_TEST_SKIP(func, p) {func, ((p) ? svn_test_skip : svn_test_pass)}
 
 
-/** Return a pseudo-random number based on @a seed, and modify @a seed.
+/* Return a pseudo-random number based on @a seed, and modify @a seed.
  *
  * This is a "good" pseudo-random number generator, intended to replace 
  * all those "bad" rand() implementations out there.
@@ -95,72 +90,71 @@ extern struct svn_test_descriptor_t test_funcs[];
 apr_uint32_t svn_test_rand (apr_uint32_t *seed);
 
 
-/** Add @a path to the test cleanup list.
- */
+/* Add @a path to the test cleanup list.  */
 void svn_test_add_dir_cleanup (const char *path);
 
 
 
-/** Set @a *editor and @a *edit_baton to an editor that prints its 
- * arguments to @a out_stream.  The edit starts at @a path, that is, 
+/* Set *EDITOR and *EDIT_BATON to an editor that prints its 
+ * arguments to OUT_STREAM.  The edit starts at PATH, that is, 
  * @a path will be prepended to the appropriate paths in the output.
- * Allocate the editor in @a pool.
+ * Allocate the editor in POOL.
  *
- * @a editor_name is a name for the editor, a string that will be
- * prepended to the editor output as shown below.  @a editor_name may
+ * EDITOR_NAME is a name for the editor, a string that will be
+ * prepended to the editor output as shown below.  EDITOR_NAME may
  * be the empty string (but it may not be null).
  *
- * @a verbose is a flag for specifying whether or not your want all the
- * nitty gritty details displayed.  When @a verbose is @c FALSE, each 
+ * VERBOSE is a flag for specifying whether or not your want all the
+ * nitty gritty details displayed.  When VERBOSE is FALSE, each 
  * editor function will print only a one-line summary. 
  *
- * @a indentation is the number of spaces to indent by at each level; use
+ * INDENTATION is the number of spaces to indent by at each level; use
  * 0 for no indentation.  The indent level is always the same for a
  * given call (i.e, stack frame).
  * 
  * SOME EXAMPLES
  *
  * With an indentation of 3, editor name of "COMMIT-TEST" and with
- * verbose = @c TRUE
+ * VERBOSE = TRUE
  *
- *<pre> [COMMIT-TEST] open_root (wc)
- * base_revision: 1</pre>
- *<pre>    [COMMIT-TEST] open_directory (wc/A)
+ * [COMMIT-TEST] open_root (wc)
+ * base_revision: 1
+ *    [COMMIT-TEST] open_directory (wc/A)
  *    parent: wc
- *    base_revision: 1</pre>
- *<pre>       [COMMIT-TEST] delete_entry (wc/A/B)</pre>
- *<pre>       [COMMIT-TEST] open_file (wc/A/mu)
+ *    base_revision: 1
+ *       [COMMIT-TEST] delete_entry (wc/A/B)
+ *       [COMMIT-TEST] open_file (wc/A/mu)
  *       parent: wc/A
- *       base_revision: 1</pre>
- *<pre>          [COMMIT-TEST] change_file_prop (wc/A/mu)
+ *       base_revision: 1
+ *          [COMMIT-TEST] change_file_prop (wc/A/mu)
  *          name: foo
- *          value: bar</pre>
- *<pre>       [COMMIT-TEST] close_file (wc/A/mu)</pre>
- *<pre>    [COMMIT-TEST] close_directory (wc/A)</pre>
- *<pre>    [COMMIT-TEST] add_file (wc/zeta)
+ *          value: bar
+ *       [COMMIT-TEST] close_file (wc/A/mu)
+ *    [COMMIT-TEST] close_directory (wc/A)
+ *    [COMMIT-TEST] add_file (wc/zeta)
  *    parent: wc
  *    copyfrom_path: 
- *    copyfrom_revision: 0</pre>
- *<pre>    [COMMIT-TEST] open_file (wc/iota)
+ *    copyfrom_revision: 0
+ *    [COMMIT-TEST] open_file (wc/iota)
  *    parent: wc
- *    base_revision: 1</pre>
- *<pre> [COMMIT-TEST] close_directory (wc)</pre>
- *<pre>       [COMMIT-TEST] apply_textdelta (wc/iota)</pre>
- *<pre>          [COMMIT-TEST] window_handler (2 ops)
+ *    base_revision: 1
+ * [COMMIT-TEST] close_directory (wc)
+ *       [COMMIT-TEST] apply_textdelta (wc/iota)
+ *          [COMMIT-TEST] window_handler (2 ops)
  *          (1) new text: length 11
- *          (2) source text: offset 0, length 0</pre>
- *<pre>          [COMMIT-TEST] window_handler (EOT)</pre>
- *<pre>    [COMMIT-TEST] close_file (wc/iota)</pre>
- *<pre>       [COMMIT-TEST] apply_textdelta (wc/zeta)</pre>
- *<pre>          [COMMIT-TEST] window_handler (1 ops)
- *          (1) new text: length 11</pre>
- *<pre>          [COMMIT-TEST] window_handler (EOT)</pre>
- *<pre>    [COMMIT-TEST] close_file (wc/zeta)</pre>
- *<pre> [COMMIT-TEST] close_edit</pre>
+ *          (2) source text: offset 0, length 0
+ *          [COMMIT-TEST] window_handler (EOT)
+ *    [COMMIT-TEST] close_file (wc/iota)
+ *       [COMMIT-TEST] apply_textdelta (wc/zeta)
+ *          [COMMIT-TEST] window_handler (1 ops)
+ *          (1) new text: length 11
+ *          [COMMIT-TEST] window_handler (EOT)
+ *    [COMMIT-TEST] close_file (wc/zeta)
+ * [COMMIT-TEST] close_edit
  *  
  * The same example as above, but with verbose = @c FALSE
  *
- *<pre> [COMMIT-TEST] open_root (wc)
+ * [COMMIT-TEST] open_root (wc)
  *    [COMMIT-TEST] open_directory (wc/A)
  *       [COMMIT-TEST] delete_entry (wc/A/B)
  *       [COMMIT-TEST] open_file (wc/A/mu)
@@ -174,9 +168,8 @@ void svn_test_add_dir_cleanup (const char *path);
  *    [COMMIT-TEST] close_file (wc/iota)
  *       [COMMIT-TEST] apply_textdelta (wc/zeta)
  *    [COMMIT-TEST] close_file (wc/zeta)
- * [COMMIT-TEST] close_edit</pre>
+ * [COMMIT-TEST] close_edit
  *
- * This is implemented in tests/libsvn_test_editor.la
  */
 svn_error_t *svn_test_get_editor (const svn_delta_editor_t **editor,
                                   void **edit_baton,
