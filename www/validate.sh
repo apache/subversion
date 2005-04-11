c5c5a7c8c634d9e0872b74da28d3d2fcd94395bc
Max Oliver Bowsher <maxb@apache.org>
2005-04-11 23:17:22 +0000

    More www validation tweaks.
    * www (svn:ignore): Add *.soc and *.dcl .
    * www/validate.sh: Remove dependency on hardcoded path to XML SGML decl,
        by hardcoding an URL, and downloading it.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@854209 13f79535-47bb-0310-9956-ffa450edef68

--- www/validate.sh
+++ www/validate.sh
@@ -22,12 +22,6 @@ if [ "$1" = "all" ]; then
   exit 0
 fi
 
-export SP_CHARSET_FIXED=YES
-export SP_ENCODING=XML
-
-# This path is system specific. Anyone know a good way of making this portable?
-export SGML_CATALOG_FILES="/usr/share/OpenSP/xml.soc"
-
 # Much of this script is concerned with setting up a local cache of the
 # DTD and files required by the DTD, so they are not re-downloaded every time.
 
@@ -42,14 +36,28 @@ ensure "http://www.w3.org/TR/xhtml1/DTD/xhtml-lat1.ent"
 ensure "http://www.w3.org/TR/xhtml1/DTD/xhtml-symbol.ent"
 ensure "http://www.w3.org/TR/xhtml1/DTD/xhtml-special.ent"
 
+# Do not need these, but the URLs are here for uncommenting if you like
+#ensure "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd"
+#ensure "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"
+#ensure "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"
+
+# If you have ongmls installed, then you almost certainly have a SGML
+# declaration for XML installed already, but there is no cross-platform way of
+# knowing where.  So, instead, just download one, since we had to download
+# other things anyway.
+ensure "http://www.w3.org/TR/xhtml1/DTD/xhtml.soc"
+ensure "http://www.w3.org/TR/xhtml1/DTD/xhtml1.dcl"
+
 if test ! -f "$WWWDIR/catalog"; then
   echo '
 OVERRIDE YES
 PUBLIC "-//Tigris//DTD XHTML 1.0 Transitional//EN" "tigris_transitional.dtd"
-PUBLIC "-//W3C//ENTITIES Latin 1 for XHTML//EN" "xhtml-lat1.ent"
-PUBLIC "-//W3C//ENTITIES Symbols for XHTML//EN" "xhtml-symbol.ent"
-PUBLIC "-//W3C//ENTITIES Special for XHTML//EN" "xhtml-special.ent"
 ' > "$WWWDIR/catalog"
 fi
+
+export SP_CHARSET_FIXED=YES
+export SP_ENCODING=XML
+
+export SGML_CATALOG_FILES="$WWWDIR/xhtml.soc"
 
 onsgmls -wxml -wno-inclusion -ges "$1"
