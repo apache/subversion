5eacd367b07f0080d758b41834340c8597086d86
Max Oliver Bowsher <maxb@apache.org>
2005-04-12 16:16:18 +0000

    * www/validate.sh: Support xmllint as an alternative to onsgmls.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@854221 13f79535-47bb-0310-9956-ffa450edef68

--- www/validate.sh
+++ www/validate.sh
@@ -1,26 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
+# Yes, this script is aching to be rewritten in Python.
+# It was never supposed to grow this big!
 
 WWWDIR="`dirname \"$0\"`"
-
-if [ "$1" = "all" ]; then
-  WARNFILE=".validation-warnings.$$.tmp"
-  for f in "$WWWDIR"/*.html; do
-    if ! grep -F -q '<!DOCTYPE' "$f"; then
-      RESULT='No <!DOCTYPE'
-    else
-      if "$WWWDIR/validate.sh" "$f" 2>"$WARNFILE"; then
-        RESULT='\033[32mvalid\033[0m'
-      else
-        WARNLINES="`cat \"$WARNFILE\" | wc -l`"
-        RESULT='\033[31;1mINVALID ('"$WARNLINES"')\033[0m'
-      fi
-      cat "$WARNFILE"
-    fi
-    echo -e "$f: $RESULT"
-  done
-  rm -f "$WARNFILE"
-  exit 0
-fi
 
 # Much of this script is concerned with setting up a local cache of the
 # DTD and files required by the DTD, so they are not re-downloaded every time.
@@ -48,16 +31,76 @@ ensure "http://www.w3.org/TR/xhtml1/DTD/xhtml-special.ent"
 ensure "http://www.w3.org/TR/xhtml1/DTD/xhtml.soc"
 ensure "http://www.w3.org/TR/xhtml1/DTD/xhtml1.dcl"
 
-if test ! -f "$WWWDIR/catalog"; then
+# Tigris DTD uses a SGML feature not compatible with a standard XML processor.
+# Fortunately, it is not a major feature - so remove it:
+if test ! -f "$WWWDIR/tigris_transitionalxml.dtd"; then
+  sed -e 's/<!ELEMENT a %a.content; +(br)>/<!ELEMENT a %a.content;>/' \
+    < "$WWWDIR/tigris_transitional.dtd" > "$WWWDIR/tigris_transitionalxml.dtd"
+fi
+
+if test ! -f "$WWWDIR/tigris.soc"; then
   echo '
 OVERRIDE YES
-PUBLIC "-//Tigris//DTD XHTML 1.0 Transitional//EN" "tigris_transitional.dtd"
-' > "$WWWDIR/catalog"
+PUBLIC "-//Tigris//DTD XHTML 1.0 Transitional//EN" "tigris_transitionalxml.dtd"
+' > "$WWWDIR/tigris.soc"
 fi
 
 export SP_CHARSET_FIXED=YES
 export SP_ENCODING=XML
 
-export SGML_CATALOG_FILES="$WWWDIR/xhtml.soc"
+export SGML_CATALOG_FILES="$WWWDIR/xhtml.soc:$WWWDIR/tigris.soc"
 
-onsgmls -wxml -wno-inclusion -ges "$1"
+if [ -z "$XML_VALIDATOR" ]; then
+  if [ "`type -p onsgmls`" != "" ]; then
+    export XML_VALIDATOR="onsgmls"
+  else
+    if [ "`type -p xmllint`" != "" ]; then
+      export XML_VALIDATOR="xmllint"
+    else
+      echo "No XML validator found!" >&2
+      exit 1
+    fi
+  fi
+fi
+
+echo "Selected XML validator: '$XML_VALIDATOR'"
+
+validate ()
+{
+  case $XML_VALIDATOR in
+    onsgmls)
+    onsgmls -wxml -wno-inclusion -ges "$1"
+    ;;
+    xmllint)
+    xmllint --nonet --noout --valid --catalogs "$1"
+    ;;
+    *)
+    echo "Internal error - unknown XML validator '$XML_VALIDATOR'!" >&2
+    exit 1
+    ;;
+  esac
+}
+
+if [ "$1" = "all" ]; then
+  WARNFILE=".validation-warnings.$$.tmp"
+  for f in "$WWWDIR"/*.html; do
+    if ! grep -F -q '<!DOCTYPE' "$f"; then
+      RESULT='No <!DOCTYPE'
+    else
+      validate "$f" 2>"$WARNFILE"
+      if [ $? -eq 0 ]; then
+        RESULT='\033[32mvalid\033[0m'
+      else
+        WARNLINES="`cat \"$WARNFILE\" | wc -l`"
+        RESULT='\033[31;1mINVALID ('"$WARNLINES"')\033[0m'
+      fi
+      cat "$WARNFILE"
+    fi
+    echo -e "$f: $RESULT"
+  done
+  rm -f "$WARNFILE"
+  exit 0
+fi
+
+validate "$1"
+
