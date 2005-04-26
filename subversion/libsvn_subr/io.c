5190660b261263b12c9dd5bebff60b37017ea694
Daniel Rall <dlr@apache.org>
2005-04-26 17:06:25 +0000

    * subversion/libsvn_subr/io.c   (apr_dir_is_empty): Removed inline comment asking about the   APR_STATUS_IS_SUCCESS macro.  It was removed by Justin in APR's 1.0   release, and as such should not be used by Subversion.  Ryan Bloom   and Greg Stein break it down here:
      http://www.mail-archive.com/dev@httpd.apache.org/msg21757.html
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@854526 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/io.c
+++ subversion/libsvn_subr/io.c
@@ -2696,9 +2696,6 @@ apr_dir_is_empty (const char *dir, apr_pool_t *pool)
   apr_err = apr_dir_open (&dir_handle, dir, pool);
   if (apr_err != APR_SUCCESS)
     return apr_err;
-      
-  /* ### What is the gospel on the APR_STATUS_IS_SUCCESS macro these
-     days? :-) */
 
   for (apr_err = apr_dir_read (&finfo, APR_FINFO_NAME, dir_handle);
        apr_err == APR_SUCCESS;
