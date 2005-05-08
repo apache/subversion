03834b86d427f14a5f0f53d0053e51d5a5995cf8
Max Oliver Bowsher <maxb@apache.org>
2005-05-08 14:19:24 +0000

    Restore testsuite Python 2.0 compatibility.
    * subversion/tests/clients/cmdline/svntest/actions.py (run_and_verify_svn):
        Use a loop instead of a nested function.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@854702 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/clients/cmdline/svntest/actions.py
+++ subversion/tests/clients/cmdline/svntest/actions.py
@@ -169,7 +169,9 @@ def run_and_verify_svn(message, expected_stdout, expected_stderr, *varargs):
 
   out, err = main.run_svn(want_err, *varargs)
 
-  def handle_output(expected, actual, output_type, raisable):
+  for (expected, actual, output_type, raisable) in (
+      (expected_stdout, out, 'stdout', SVNExpectedStdout),
+      (expected_stderr, err, 'stderr', SVNExpectedStderr)):
     if type(expected) is type([]):
       compare_and_display_lines(message, output_type.upper(), expected, actual)
     elif type(expected) is type(''):
@@ -181,8 +183,6 @@ def run_and_verify_svn(message, expected_stdout, expected_stderr, *varargs):
     elif expected is not None:
       raise SVNIncorrectDatatype("Unexpected type for %s data" % output_type)
 
-  handle_output(expected_stdout, out, 'stdout', SVNExpectedStdout)
-  handle_output(expected_stderr, err, 'stderr', SVNExpectedStderr)
   return out, err
 
 
