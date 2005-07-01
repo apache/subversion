d736743a6cd44b148b899c111cd47accf51307e4
Paul Burba <pburba@apache.org>
2005-07-01 12:35:01 +0000

    Merge r14790:15139 from /branches/1.2.x to /branches/ebcdic/1.2.x
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/ebcdic@855282 13f79535-47bb-0310-9956-ffa450edef68

--- 1.2.x/subversion/libsvn_fs_fs/fs_fs.c
+++ 1.2.x/subversion/libsvn_fs_fs/fs_fs.c
@@ -2175,7 +2175,7 @@ fold_change (apr_hash_t *changes,
           old_change->text_mod = change->text_mod;
           old_change->prop_mod = change->prop_mod;
           if (change->copyfrom_rev == SVN_INVALID_REVNUM)
-            copyfrom_string = NULL;
+            copyfrom_string = apr_pstrdup (copyfrom_pool, "");
           else
             {
               copyfrom_string = APR_PSPRINTF2 (copyfrom_pool,
@@ -2214,7 +2214,7 @@ fold_change (apr_hash_t *changes,
                                            change->copyfrom_path);
         }
       else
-        copyfrom_string = NULL;
+        copyfrom_string = apr_pstrdup (copyfrom_pool, "");
       path = apr_pstrdup (pool, change->path);
     }
 
