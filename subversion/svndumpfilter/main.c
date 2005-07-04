f9780cb0dd4b16c876424ea1bb3da9629c49b067
Max Oliver Bowsher <maxb@apache.org>
2005-07-04 11:50:07 +0000

    Fix crash of svn, svnadmin, svndumpfilter and svnlook when more than SVN_OPT_MAX_OPTIONS options were passed. Reported by David James.
    * subversion/clients/cmdline/ain.c:
    * subversion/svnadmin/main.c:
    * subversion/svndumpfilter/main.c:
    * subversion/svnlook/main.c:
      (main): Change received_opts from a plain C array to an APR array, and use
        APR's auto-extending push function.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@855325 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svndumpfilter/main.c
+++ subversion/svndumpfilter/main.c
@@ -1067,8 +1067,8 @@ main (int argc, const char * const *argv)
   struct svndumpfilter_opt_state opt_state;
   apr_getopt_t *os;
   int opt_id;
-  int received_opts[SVN_OPT_MAX_OPTIONS];
-  int i, num_opts = 0;
+  apr_array_header_t *received_opts;
+  int i;
 
 
   /* Initialize the app. */
@@ -1092,6 +1092,8 @@ main (int argc, const char * const *argv)
     return error_exit (err, pool);
 
   /* Initialize the FS library. */
+  received_opts = apr_array_make (pool, SVN_OPT_MAX_OPTIONS, sizeof (int));
+
   err = svn_fs_initialize (pool);
   if (err)
     return error_exit (err, pool);
@@ -1127,8 +1129,7 @@ main (int argc, const char * const *argv)
         }
 
       /* Stash the option code in an array before parsing it. */
-      received_opts[num_opts] = opt_id;
-      num_opts++;
+      APR_ARRAY_PUSH (received_opts, int) = opt_id;
 
       switch (opt_id)
         {
@@ -1234,9 +1235,9 @@ main (int argc, const char * const *argv)
 
 
   /* Check that the subcommand wasn't passed any inappropriate options. */
-  for (i = 0; i < num_opts; i++)
+  for (i = 0; i < received_opts->nelts; i++)
     {
-      opt_id = received_opts[i];
+      opt_id = APR_ARRAY_IDX (received_opts, i, int);
 
       /* All commands implicitly accept --help, so just skip over this
          when we see it. Note that we don't want to include this option
