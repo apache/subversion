165bdabeb9b16a93d42f40c66c7210cb416d8862
C. Michael Pilato <cmpilato@apache.org>
2005-07-07 08:05:06 +0000

    Add comments describing the errorcodes Subversion expects.
    * contrib/client-side/diffwrap.bat,
    * contrib/client-side/diff3wrap.bat
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@855358 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/client-side/diff3wrap.bat
+++ contrib/client-side/diff3wrap.bat
@@ -18,4 +18,5 @@ REM your merge program).
 
 REM After performing the merge, this script needs to print the contents
 REM of the merged file to stdout.  Do that in whatever way you see fit.
-
+REM Return an errorcode of 0 on successful merge, 1 if unresolved conflicts
+REM remain in the result.  Any other errorcode will be treated as fatal.
