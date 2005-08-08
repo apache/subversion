54c3649498beff8a0a8d3fd469b87daf81eb443e
Daniel Rall <dlr@apache.org>
2005-08-08 16:21:36 +0000

    [in subversion/bindings/java/javahl/src/org/tigris/subversion/javahl/] * SVNClientInterface.java * SVNClient.java   (streamFileContent): Updated JavaDoc with a hint as to how to    produce an InputStream.
    Suggested by: Conor MacNeill
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@855707 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/java/javahl/src/org/tigris/subversion/javahl/SVNClientInterface.java
+++ subversion/bindings/java/javahl/src/org/tigris/subversion/javahl/SVNClientInterface.java
@@ -713,13 +713,17 @@ public interface SVNClientInterface
     byte[] fileContent(String path, Revision revision, Revision pegRevision)
             throws ClientException;
     /**
-     * Stream file content to the given output stream
-     * @param path      the path of the file
-     * @param revision  the revision to retrieve
-     * @param pegRevision the revision to interpret path
-     * @param bufferSize the size of buffer to use during streaming
-     * @param  the stream to which content is written
+     * Write the file's content to the specified output stream.  If
+     * you need an InputStream, use a
+     * PipedInputStream/PipedOutputStream combination.
+     *
+     * @param path        the path of the file
+     * @param revision    the revision to retrieve
+     * @param pegRevision the revision at which to interpret the path
+     * @param the stream to write the file's content to
      * @throws ClientException
+     * @see <a href="http://java.sun.com/j2se/1.4.2/docs/api/java/io/PipedOutputStream.html">PipedOutputStream</a>
+     * @see <a href="http://java.sun.com/j2se/1.4.2/docs/api/java/io/PipedInputStream.html">PipedInputStream</a>
      */
     void streamFileContent(String path, Revision revision, Revision pegRevision,
                            int bufferSize, OutputStream stream) 
