45cbd87ab329c60e53cb1bf09591bea65f7c1ec7
danderson <dev-null@apache.org>
2005-09-04 14:28:44 +0000

    Fix the C example programs still using the legacy RA interface to eliminate a potential crash due to a missing callback function.  Also make these programs compile without warnings on -Wall.
    * tools/examples/svnput.c, tools/examples/headrev.c: Add missing svn
        includes.
      (open_tmp_file): New function.
      (main): Add open_tmp_file to the callback table passed to the RA
        layer.  Remove unused variables.
    
    * tools/examples/getlocks_test.c: Same as above, plus...
      (my_commit_callback): Remove function, not used.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@856115 13f79535-47bb-0310-9956-ffa450edef68

--- tools/examples/headrev.c
+++ tools/examples/headrev.c
@@ -29,6 +29,8 @@
 #include "svn_pools.h"
 #include "svn_config.h"
 #include "svn_fs.h"
+#include "svn_path.h"
+#include "svn_cmdline.h"
 
 
 /* Display a prompt and read a one-line response into the provided buffer,
@@ -109,14 +111,33 @@ my_username_prompt_callback (svn_auth_cred_username_t **cred,
 }
 
 
+/* A callback function used when the RA layer needs a handle to a
+   temporary file.  This is a reduced version of the callback used in
+   the official svn cmdline client. */
+static svn_error_t *
+open_tmp_file (apr_file_t **fp,
+               void *callback_baton,
+               apr_pool_t *pool)
+{
+  const char *path;
+  const char *ignored_filename;
+
+  SVN_ERR (svn_io_temp_dir (&path, pool));
+  path = svn_path_join (path, "tempfile", pool);
+
+  /* Open a unique file, with delete-on-close set. */
+  SVN_ERR (svn_io_open_unique_file (fp, &ignored_filename,
+                                    path, ".tmp", TRUE, pool));
+
+  return SVN_NO_ERROR;
+}
+
+
 int
 main (int argc, const char **argv)
 {
   apr_pool_t *pool;
   svn_error_t *err;
-  apr_hash_t *dirents;
-  apr_hash_index_t *hi;
-  svn_client_ctx_t *ctx;
   const char *URL;
   svn_ra_plugin_t *ra_lib;  
   void *session, *ra_baton;
@@ -180,6 +201,7 @@ main (int argc, const char **argv)
   /* Create a table of callbacks for the RA session, mostly nonexistent. */
   cbtable = apr_pcalloc (pool, sizeof(*cbtable));
   cbtable->auth_baton = auth_baton;
+  cbtable->open_tmp_file = open_tmp_file;
 
   /* Now do the real work. */
 
