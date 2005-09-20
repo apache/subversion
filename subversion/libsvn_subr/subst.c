8a24aaad855efb2b1f0e7b5205902bd9070f7c98
Daniel Rall <dlr@apache.org>
2005-09-20 20:24:44 +0000

    * subversion/libsvn_subr/subst.c   (svn_subst_keywords_differ2): Revert r16172, which was entirely    incorrect.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@856247 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/subst.c
+++ subversion/libsvn_subr/subst.c
@@ -735,8 +735,11 @@ svn_subst_keywords_differ2 (apr_hash_t *a,
   a_count = (a == NULL) ? 0 : apr_hash_count (a);
   b_count = (b == NULL) ? 0 : apr_hash_count (b);
 
-  if (a_count != b_count || a_count == 0)
+  if (a_count != b_count)
     return TRUE;
+
+  if (a_count == 0)
+    return FALSE;
 
   /* The hashes are both non-NULL, and have the same number of items.
    * We must check that every item of A is present in B. */
