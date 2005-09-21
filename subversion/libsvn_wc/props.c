44f4178193e70431a8431ad938f2c1b1ec10625c
Daniel Rall <dlr@apache.org>
2005-09-21 00:09:54 +0000

    * subversion/libsvn_wc/props.h   (svn_wc__has_magic_property): Doc string tweak to improve clarity.
    * subversion/libsvn_wc/props.c
      (svn_wc__has_magic_property): Don't treat the return value of
       strcmp() as a boolean value, and eliminate unnecessary parens.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@856252 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/props.c
+++ subversion/libsvn_wc/props.c
@@ -2120,11 +2120,11 @@ svn_wc__has_magic_property (apr_array_header_t *properties)
     {
       svn_prop_t *property = &APR_ARRAY_IDX (properties, i, svn_prop_t);
 
-      if ((! strcmp (property->name, SVN_PROP_EXECUTABLE))
-          || (! strcmp (property->name, SVN_PROP_KEYWORDS))
-          || (! strcmp (property->name, SVN_PROP_EOL_STYLE))
-          || (! strcmp (property->name, SVN_PROP_SPECIAL))
-          || (! strcmp (property->name, SVN_PROP_NEEDS_LOCK)))
+      if (strcmp (property->name, SVN_PROP_EXECUTABLE) == 0
+          || strcmp (property->name, SVN_PROP_KEYWORDS) == 0
+          || strcmp (property->name, SVN_PROP_EOL_STYLE) == 0
+          || strcmp (property->name, SVN_PROP_SPECIAL) == 0
+          || strcmp (property->name, SVN_PROP_NEEDS_LOCK) == 0)
         return TRUE;
     }
   return FALSE;
