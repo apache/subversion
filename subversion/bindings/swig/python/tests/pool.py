afcf09929cc2ddfb920dc35225fae965a36fa02b
djames <djames@unknown>
2005-09-25 23:40:35 +0000

    Update Python bindings to track pool ownership of struct members.
    * subversion/bindings/swig/include/proxy.swg
      (__setattr__): Save _parent_pool and _member_dicts information for
      struct members.
      (__getattr__): Restore _parent_pool and _member_dicts information
      for struct members.
    * subversion/bindings/swig/python/tests/pool.py
      (PoolTestCase.test_struct_members): New test case. Check that parent
      pools are set correctly on struct accesses.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@856322 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/swig/python/tests/pool.py
+++ subversion/bindings/swig/python/tests/pool.py
@@ -1,5 +1,6 @@
 from svn.core import *
 import svn.core
+import svn.client
 import libsvn.core
 import unittest
 import weakref
@@ -17,6 +18,13 @@ class PoolTestCase(unittest.TestCase):
     """Assert that the specified value is None"""
     return self.assertEqual(value, None);
   
+  def test_struct_members(self):
+    """Check that parent pools are set correctly on struct accesses"""
+    client_ctx = svn.client.svn_client_create_context()
+    config = svn.core.svn_config_get_config(None)
+    client_ctx.config = config
+    self.assertEqual(client_ctx.config._parent_pool, config._parent_pool)
+ 
   def test_pool(self):
     # Create pools
     parent_pool = Pool()
