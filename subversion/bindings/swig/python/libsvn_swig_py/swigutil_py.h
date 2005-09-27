29e6c85799763105d140dd605c9291c35f27ae2b
Branko Cibej <brane@apache.org>
2005-09-27 13:02:13 +0000

    Build the Python bindings utility library as a DLL on Windows.
    Patch by: Marc Haesen <Marc.Haesen@telindus.be>
    (Tweaked by me.)
    
    * build.conf (libsvn_swig_py): Change msvc-static to 'no'.
    * subversion/bindings/swig/python/libsvn_swig_py/swigutil_py.c
      (SVN_SWIG_SWIGUTIL_PY_C): New define.
    * subversion/bindings/swig/python/libsvn_swig_py/swigutil_py.h
      (SVN_SWIG_SWIGUTIL_EXPORT): New define for DLL export magic on Windows.
       Use it in every public declaratio in this header.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@856376 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/swig/python/libsvn_swig_py/swigutil_py.h
+++ subversion/bindings/swig/python/libsvn_swig_py/swigutil_py.h
@@ -34,6 +34,17 @@
 #include "svn_client.h"
 #include "svn_repos.h"
 
+/* Define DLL export magic on Windows. */
+#ifdef WIN32
+#  ifdef SVN_SWIG_SWIGUTIL_PY_C
+#    define SVN_SWIG_SWIGUTIL_EXPORT __declspec(dllexport)
+#  else
+#    define SVN_SWIG_SWIGUTIL_EXPORT __declspec(dllimport)
+#  endif
+#else
+#  define SVN_SWIG_SWIGUTIL_EXPORT
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
@@ -41,70 +52,89 @@ extern "C" {
 
 
 /* Functions to manage python's global interpreter lock */
+SVN_SWIG_SWIGUTIL_EXPORT
 void svn_swig_py_release_py_lock(void);
+
+SVN_SWIG_SWIGUTIL_EXPORT
 void svn_swig_py_acquire_py_lock(void);
 
 
 /*** Automatic Pool Management Functions ***/
+SVN_SWIG_SWIGUTIL_EXPORT
 extern int _global_svn_swig_py_is_local_pool;
 
 /* Set the application pool */
+SVN_SWIG_SWIGUTIL_EXPORT
 void svn_swig_py_set_application_pool(PyObject *py_pool, apr_pool_t *pool);
 
 /* Clear the application pool */
+SVN_SWIG_SWIGUTIL_EXPORT
 void svn_swig_py_clear_application_pool(void);
 
 /* Get the application pool */
+SVN_SWIG_SWIGUTIL_EXPORT
 void svn_swig_get_application_pool(PyObject **py_pool, apr_pool_t **pool);
 
 /* Register cleanup function */
+SVN_SWIG_SWIGUTIL_EXPORT
 PyObject * svn_swig_py_register_cleanup(PyObject *py_pool, apr_pool_t *pool);
 
 
 /*** SWIG Wrappers ***/
 
 /* Wrapper for SWIG_NewPointerObj */
+SVN_SWIG_SWIGUTIL_EXPORT
 PyObject *svn_swig_NewPointerObj(void *obj, swig_type_info *type, 
                                  PyObject *pool);
 
 /* Wrapper for SWIG_ConvertPtr */
+SVN_SWIG_SWIGUTIL_EXPORT
 int svn_swig_ConvertPtr(PyObject *input, void **obj, swig_type_info *type);
 
 /* Wrapper for SWIG_MustGetPtr */
+SVN_SWIG_SWIGUTIL_EXPORT
 void *svn_swig_MustGetPtr(void *input, swig_type_info *type, int argnum,
                           PyObject **py_pool);
 
 /*** Functions to expose a custom SubversionException ***/
 
 /* register a new subversion exception class */
+SVN_SWIG_SWIGUTIL_EXPORT
 PyObject *svn_swig_py_register_exception(void);
 
 /* get the object which represents the subversion exception class */
+SVN_SWIG_SWIGUTIL_EXPORT
 PyObject *svn_swig_py_exception_type(void);
 
 /* raise a subversion exception, created from a normal subversion error */
+SVN_SWIG_SWIGUTIL_EXPORT
 void svn_swig_py_svn_exception(svn_error_t *err);
 
 
 
 /* helper function to convert an apr_hash_t* (char* -> svnstring_t*) to
    a Python dict */
+SVN_SWIG_SWIGUTIL_EXPORT
 PyObject *svn_swig_py_prophash_to_dict(apr_hash_t *hash);
 
 /* helper function to convert an apr_hash_t* (svn_revnum_t* -> const
    char *) to a Python dict */
+SVN_SWIG_SWIGUTIL_EXPORT
 PyObject *svn_swig_py_locationhash_to_dict(apr_hash_t *hash);
 
 /* convert a hash of 'const char *' -> TYPE into a Python dict */
+SVN_SWIG_SWIGUTIL_EXPORT
 PyObject *svn_swig_py_convert_hash(apr_hash_t *hash, swig_type_info *type, 
                                    PyObject *py_pool);
 
 /* helper function to convert a 'char **' into a Python list of string
    objects */
+SVN_SWIG_SWIGUTIL_EXPORT
 PyObject *svn_swig_py_c_strings_to_list(char **strings);
 
 /* helper function to convert an array of 'const char *' to a Python list
    of string objects */
+SVN_SWIG_SWIGUTIL_EXPORT
 PyObject *svn_swig_py_array_to_list(const apr_array_header_t *strings);
 
 /* helper function to convert an array of 'svn_revnum_t' to a Python list
@@ -116,12 +146,14 @@ PyObject *svn_swig_py_revarray_to_list(const apr_array_header_t *revs);
 /* helper function to convert a Python dictionary mapping strings to
    strings into an apr_hash_t mapping const char *'s to const char *'s,
    allocated in POOL. */
+SVN_SWIG_SWIGUTIL_EXPORT
 apr_hash_t *svn_swig_py_stringhash_from_dict(PyObject *dict,
                                              apr_pool_t *pool);
 
 /* helper function to convert a Python dictionary mapping strings to
    strings into an apr_hash_t mapping const char *'s to svn_string_t's,
    allocated in POOL. */
+SVN_SWIG_SWIGUTIL_EXPORT
 apr_hash_t *svn_swig_py_prophash_from_dict(PyObject *dict,
                                            apr_pool_t *pool);
 
@@ -130,27 +162,33 @@ apr_hash_t *svn_swig_py_prophash_from_dict(PyObject *dict,
    objects must remain alive -- the values are not copied. This is
    appropriate for incoming arguments which are defined to last the
    duration of the function's execution.  */
+SVN_SWIG_SWIGUTIL_EXPORT
 const apr_array_header_t *svn_swig_py_strings_to_array(PyObject *source,
                                                        apr_pool_t *pool);
 
 /* like svn_swig_py_strings_to_array(), but for array's of 'svn_revnum_t's. */
+SVN_SWIG_SWIGUTIL_EXPORT
 const apr_array_header_t *svn_swig_py_revnums_to_array(PyObject *source,
                                                        apr_pool_t *pool);
 
 /* make an editor that "thunks" from C callbacks up to Python */
+SVN_SWIG_SWIGUTIL_EXPORT
 void svn_swig_py_make_editor(const svn_delta_editor_t **editor,
                              void **edit_baton,
                              PyObject *py_editor,
                              apr_pool_t *pool);
 
+SVN_SWIG_SWIGUTIL_EXPORT
 apr_file_t *svn_swig_py_make_file(PyObject *py_file,
                                   apr_pool_t *pool);
 
+SVN_SWIG_SWIGUTIL_EXPORT
 svn_stream_t *svn_swig_py_make_stream(PyObject *py_io,
                                       apr_pool_t *pool);
 
 /* a notify function that executes a Python function that is passed in
    via the baton argument */
+SVN_SWIG_SWIGUTIL_EXPORT
 void svn_swig_py_notify_func(void *baton,
                              const char *path,
                              svn_wc_notify_action_t action,
@@ -162,20 +200,24 @@ void svn_swig_py_notify_func(void *baton,
 
 /* a status function that executes a Python function that is passed in
    via the baton argument */
+SVN_SWIG_SWIGUTIL_EXPORT
 void svn_swig_py_status_func(void *baton,
                              const char *path,
                              svn_wc_status_t *status);
 
 /* a cancel function that executes a Python function passed in via the
    cancel_baton argument. */
+SVN_SWIG_SWIGUTIL_EXPORT
 svn_error_t *svn_swig_py_cancel_func(void *cancel_baton);
 
 /* thunked fs get_locks function */
+SVN_SWIG_SWIGUTIL_EXPORT
 svn_error_t *svn_swig_py_fs_get_locks_func (void *baton, 
                                             svn_lock_t *lock, 
                                             apr_pool_t *pool);
 
 /* thunked commit log fetcher */
+SVN_SWIG_SWIGUTIL_EXPORT
 svn_error_t *svn_swig_py_get_commit_log_func(const char **log_msg,
                                              const char **tmp_file,
                                              apr_array_header_t *commit_items,
@@ -183,6 +225,7 @@ svn_error_t *svn_swig_py_get_commit_log_func(const char **log_msg,
                                              apr_pool_t *pool);
 
 /* thunked repos authz callback function */
+SVN_SWIG_SWIGUTIL_EXPORT
 svn_error_t *svn_swig_py_repos_authz_func(svn_boolean_t *allowed,
                                           svn_fs_root_t *root,
                                           const char *path,
@@ -190,12 +233,14 @@ svn_error_t *svn_swig_py_repos_authz_func(svn_boolean_t *allowed,
                                           apr_pool_t *pool);
 
 /* thunked history callback function */
+SVN_SWIG_SWIGUTIL_EXPORT
 svn_error_t *svn_swig_py_repos_history_func(void *baton,
                                             const char *path,
                                             svn_revnum_t revision,
                                             apr_pool_t *pool);
 
 /* thunked log receiver function */
+SVN_SWIG_SWIGUTIL_EXPORT
 svn_error_t *svn_swig_py_log_receiver(void *py_receiver,
                                       apr_hash_t *changed_paths,
                                       svn_revnum_t rev,
@@ -205,6 +250,7 @@ svn_error_t *svn_swig_py_log_receiver(void *py_receiver,
                                       apr_pool_t *pool);
 
 /* thunked blame receiver function */
+SVN_SWIG_SWIGUTIL_EXPORT
 svn_error_t *svn_swig_py_client_blame_receiver_func(void *baton,
                                                     apr_int64_t line_no,
                                                     svn_revnum_t revision,
@@ -214,6 +260,7 @@ svn_error_t *svn_swig_py_client_blame_receiver_func(void *baton,
                                                     apr_pool_t *pool);
 
 /* auth provider callbacks */
+SVN_SWIG_SWIGUTIL_EXPORT
 svn_error_t *svn_swig_py_auth_simple_prompt_func(
     svn_auth_cred_simple_t **cred,
     void *baton,
@@ -222,6 +269,7 @@ svn_error_t *svn_swig_py_auth_simple_prompt_func(
     svn_boolean_t may_save,
     apr_pool_t *pool);
 
+SVN_SWIG_SWIGUTIL_EXPORT
 svn_error_t *svn_swig_py_auth_username_prompt_func(
     svn_auth_cred_username_t **cred,
     void *baton,
@@ -229,6 +277,7 @@ svn_error_t *svn_swig_py_auth_username_prompt_func(
     svn_boolean_t may_save,
     apr_pool_t *pool);
 
+SVN_SWIG_SWIGUTIL_EXPORT
 svn_error_t *svn_swig_py_auth_ssl_server_trust_prompt_func(
     svn_auth_cred_ssl_server_trust_t **cred,
     void *baton,
@@ -238,6 +287,7 @@ svn_error_t *svn_swig_py_auth_ssl_server_trust_prompt_func(
     svn_boolean_t may_save,
     apr_pool_t *pool);
 
+SVN_SWIG_SWIGUTIL_EXPORT
 svn_error_t *svn_swig_py_auth_ssl_client_cert_prompt_func(
     svn_auth_cred_ssl_client_cert_t **cred,
     void *baton,
@@ -245,6 +295,7 @@ svn_error_t *svn_swig_py_auth_ssl_client_cert_prompt_func(
     svn_boolean_t may_save,
     apr_pool_t *pool);
 
+SVN_SWIG_SWIGUTIL_EXPORT
 svn_error_t *svn_swig_py_auth_ssl_client_cert_pw_prompt_func(
     svn_auth_cred_ssl_client_cert_pw_t **cred,
     void *baton,
