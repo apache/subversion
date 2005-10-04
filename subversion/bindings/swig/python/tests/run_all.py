484eac88ad48f2d2f71f163f310f5cd04d3e10e2
Max Oliver Bowsher <maxb@apache.org>
2005-10-04 11:18:24 +0000

    Merge r16378 from trunk to 1.3.x branch.
    Make the Python bindings testsuite be able to find the needed shared
    libraries on Cygwin. Needed to compensate for Windows' complete lack of
    library RPATHs.
    
    Approved by: +1: maxb, +0: djames
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/1.3.x@856516 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/swig/python/tests/run_all.py
+++ subversion/bindings/swig/python/tests/run_all.py
@@ -2,6 +2,18 @@ import sys, os
 bindir = os.path.dirname(sys.argv[0])
 sys.path[0:0] = [ os.getcwd(), "%s/.libs" % os.getcwd(), \
                   "%s/.." % bindir, "%s/../.libs" % bindir ]
+
+# OSes without RPATH support are going to have to do things here to make
+# the correct shared libraries be found.
+if sys.platform == 'cygwin':
+  import glob
+  svndir = os.path.dirname(os.path.dirname(os.path.dirname(os.getcwd())))
+  libpath = os.getenv("PATH").split(":")
+  libpath.insert(0, "%s/libsvn_swig_py/.libs" % os.getcwd())
+  for libdir in glob.glob("%s/libsvn_*" % svndir):
+    libpath.insert(0, "%s/.libs" % (libdir))
+  os.putenv("PATH", ":".join(libpath))
+
 import unittest
 import pool
 import trac.versioncontrol.tests
