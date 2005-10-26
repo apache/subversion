78392d065bc78f93c0752c91a61a4bf1d0b7c3dd
lundblad <dev-null@apache.org>
2005-10-26 11:48:05 +0000

    Fix switch --relocate, where it could (still) lead to an invalid repository root in the entries file.  This is fixing a corner case for issue #1859.
    A relocation such as
    svn switch --relocate http://h/r http://h/r2/a
    (where /a was not part of thre repository root) didn't fail as it should.
    
    * subversion/include/svn_wc.h
      (svn_wc_relocation_validator2): New typedef.
      (svn_wc_relocation_validator): Deprecate.
      (svn_wc_relocate2): New function declaration.
      (svn_wc_relocate): Deprecate.
    
    * subversion/libsvn_wc/relocate.c
      (relocate_entry): Change signature to use non-deprecated typedef.
      Make sure that the new repository root really is a repository root.
      (svn_wc_relocate2): New.  Copied from svn_wc_relocate, adjusting the
      signature.
      (compat_baton, compat_validator): New.
      (svn_wc_relocate): New wrapper.
    
    * subversion/libsvn_client/relocate.c
      (url_uuid_t): New struct.
      (validator_baton_t): Change url_uuids member from hash table to array.  (It
      was scanned sequentially anyway.)
      (relocate_error): Remove, code moved inside validator_func.
      (validator_func): Rework to validate repository root and be more
      straightforward.  Implement non-deprecated API.
      (svn_client_relocate): Create an array instead of hash table in the baton.
      Use non-deprecated API.
    
    * subversion/tests/clients/cmdline/switch_tests.py
      (relocate_beyond_repos_root): Extend to catch the bug we're fixing.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@857105 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/relocate.c
+++ subversion/libsvn_client/relocate.c
@@ -26,6 +26,7 @@
 #include "svn_client.h"
 #include "svn_pools.h"
 #include "svn_error.h"
+#include "svn_path.h"
 #include "client.h"
 
 #include "svn_private_config.h"
@@ -33,83 +34,79 @@
 
 /*** Code. ***/
 
+/* Repository root and UUID for a repository. */
+struct url_uuid_t
+{
+  const char *root;
+  const char *uuid;
+};
+
 struct validator_baton_t
 {
   svn_client_ctx_t *ctx;
   const char *path;
-  apr_hash_t *url_uuids;
+  apr_array_header_t *url_uuids;
   apr_pool_t *pool;
 
 };
 
 
 static svn_error_t *
-relocate_error (const char *url,
-                const char *actual_uuid,
-                const char *expected_uuid)
-{
-  return svn_error_createf 
-    (SVN_ERR_CLIENT_INVALID_RELOCATION, NULL,
-     _("The repository at '%s' has uuid '%s', but the WC has '%s'"),
-     url, actual_uuid, expected_uuid);
-}
-
-
-static svn_error_t *
 validator_func (void *baton, 
                 const char *uuid, 
-                const char *url)
+                const char *url,
+                svn_boolean_t root,
+                apr_pool_t *pool)
 {
-  svn_ra_session_t *ra_session;
   struct validator_baton_t *b = baton;
-  const char *ra_uuid;
+  struct url_uuid_t *url_uuid = NULL;
 
-  apr_hash_t *uuids = b->url_uuids;
-  apr_pool_t *pool = b->pool;
+  apr_array_header_t *uuids = b->url_uuids;
+  int i;
 
-  if (apr_hash_count (uuids) != 0)
+  for (i = 0; i < uuids->nelts; ++i)
     {
-      apr_hash_index_t *hi;
-      for (hi = apr_hash_first (pool, uuids); hi; hi = apr_hash_next (hi))
+      struct url_uuid_t *uu = &APR_ARRAY_IDX (uuids, i,
+                                                   struct url_uuid_t);
+      if (svn_path_is_ancestor (uu->root, url))
         {
-          const void *key;
-          void *val;
-          const char *item_url;
-          const char *item_uuid;
-
-          apr_hash_this (hi, &key, NULL, &val);
-          item_url = key;
-          item_uuid = val;
-
-          if (strncmp (item_url, url, strlen (item_url)) != 0)
-            continue;
-
-          if (strcmp (uuid, item_uuid) == 0)
-            return SVN_NO_ERROR;
-
-          return relocate_error (item_url, item_uuid, uuid);
+          url_uuid = uu;
+          break;
         }
     }
-
 
   /* We use an RA session in a subpool to get the UUID of the
      repository at the new URL so we can force the RA session to close
      by destroying the subpool. */
-  {
-    apr_pool_t *subpool = svn_pool_create (pool); 
-    SVN_ERR (svn_client__open_ra_session_internal (&ra_session, url, NULL,
-                                                   NULL, NULL, FALSE, TRUE,
-                                                   b->ctx, subpool));
-    SVN_ERR (svn_ra_get_uuid (ra_session, &ra_uuid, subpool));
-    ra_uuid = apr_pstrdup (pool, ra_uuid);
-    svn_pool_destroy (subpool);
-  }
+  if (! url_uuid)
+    {
+      apr_pool_t *subpool = svn_pool_create (pool); 
+      svn_ra_session_t *ra_session;
+      const char *ra_uuid, *ra_root;
+      SVN_ERR (svn_client__open_ra_session_internal (&ra_session, url, NULL,
+                                                     NULL, NULL, FALSE, TRUE,
+                                                     b->ctx, subpool));
+      SVN_ERR (svn_ra_get_uuid (ra_session, &ra_uuid, subpool));
+      SVN_ERR (svn_ra_get_repos_root (ra_session, &ra_root, subpool));
+      url_uuid = &APR_ARRAY_PUSH (uuids, struct url_uuid_t);
+      url_uuid->root = apr_pstrdup (b->pool, ra_root);
+      url_uuid->uuid = apr_pstrdup (b->pool, ra_uuid);
+      svn_pool_destroy (subpool);
+    }
+
+  /* Make sure the url is a repository root if desired. */
+  if (root
+      && strcmp (url, url_uuid->root) != 0)
+    return svn_error_createf (SVN_ERR_CLIENT_INVALID_RELOCATION, NULL,
+                                  _("'%s' is not the root of the repository"),
+                                  url);
 
   /* Make sure the UUIDs match. */
-  if (strcmp (uuid, ra_uuid))
-    return relocate_error (url, ra_uuid, uuid);
-
-  apr_hash_set (uuids, url, APR_HASH_KEY_STRING, ra_uuid);
+  if (uuid && strcmp (uuid, url_uuid->uuid) != 0)
+    return svn_error_createf
+      (SVN_ERR_CLIENT_INVALID_RELOCATION, NULL,
+       _("The repository at '%s' has uuid '%s', but the WC has '%s'"),
+       url, uuid, url_uuid->uuid);
 
   return SVN_NO_ERROR;
 }
@@ -134,10 +131,10 @@ svn_client_relocate (const char *path,
   /* Now, populate our validator callback baton, and call the relocate code. */
   vb.ctx = ctx;
   vb.path = path;
-  vb.url_uuids = apr_hash_make (pool);
+  vb.url_uuids = apr_array_make (pool, 1, sizeof (struct url_uuid_t));
   vb.pool = pool;
-  SVN_ERR (svn_wc_relocate (path, adm_access, from, to,
-                            recurse, validator_func, &vb, pool));
+  SVN_ERR (svn_wc_relocate2 (path, adm_access, from, to,
+                             recurse, validator_func, &vb, pool));
 
   /* All done.  Clean up, and move on out. */
   SVN_ERR (svn_wc_adm_close (adm_access));
