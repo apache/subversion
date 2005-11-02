9e26a6d30f978307c7cfde7c819dcf50344e0e99
Philip Martin <philip@apache.org>
2005-11-02 21:28:20 +0000

    Fix buffer overflow encountered when expanding a long keyword value.
    * subversion/libsvn_subr/subst.c (translate_keyword_subst): Allow for the
      keyword itself when truncating expanded values.
    
    * subversion/tests/clients/cmdline/trans_tests.py
      (cat_keyword_expansion): Test truncation.
    
    Patch by: Ken Case <kc@omnigroup.com>
              philip
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@857223 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/subst.c
+++ subversion/libsvn_subr/subst.c
@@ -493,8 +493,8 @@ translate_keyword_subst (char *buf,
               apr_size_t vallen = value->len;
 
               /* "$keyword: value $" */
-              if (vallen > (SVN_KEYWORD_MAX_LEN - 5))
-                vallen = SVN_KEYWORD_MAX_LEN - 5;
+              if (vallen > (SVN_KEYWORD_MAX_LEN - 5 - keyword_len))
+                vallen = SVN_KEYWORD_MAX_LEN - 5 - keyword_len;
               strncpy (buf_ptr + 2, value->data, vallen);
               buf_ptr[2 + vallen] = ' ';
               buf_ptr[2 + vallen + 1] = '$';
