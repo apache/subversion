f9123c01d11e25969c6a5c38c47fc039ff49ecba
djames <djames@unknown>
2005-11-10 00:40:44 +0000

    Simplify implementation of enable_revprop_changes by using get_pre_revprop_change_hook_path and create_python_hook_script.
    * subversion/tests/clients/cmdline/svntest/actions.py
      (enable_revprop_changes): Simplify implementation.
    
    * svntest/main.py
      (get_pre_revprop_change_hook_path): New function.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@857358 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/clients/cmdline/svntest/actions.py
+++ subversion/tests/clients/cmdline/svntest/actions.py
@@ -808,19 +808,11 @@ def lock_admin_dir(wc_dir):
   path = os.path.join(wc_dir, main.get_admin_name(), 'lock')
   main.file_append(path, "stop looking!")
 
-def enable_revprop_changes(repos_dir):
+def enable_revprop_changes(repo_dir):
   """Enable revprop changes in a repository REPOS_DIR by creating a
 pre-revprop-change hook script and (if appropriate) making it executable."""
-  if os.name == 'posix':
-    hook = os.path.join(repos_dir,
-                        'hooks', 'pre-revprop-change')
-    main.file_append(hook, "#!/bin/sh\n\nexit 0\n")
-    os.chmod(hook, 0755)
-  elif sys.platform == 'win32':
-    hook = os.path.join(repos_dir,
-                        'hooks', 'pre-revprop-change.bat')
-    main.file_append(hook, "@exit 0\n")
 
-
+  hook_path = main.get_pre_revprop_change_hook_path (repo_dir)
+  main.create_python_hook_script (hook_path, 'import sys; sys.exit(0)')
 
 ### End of file.
