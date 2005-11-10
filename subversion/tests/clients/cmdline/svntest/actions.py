f90944d84344af7549bebdfb14e1c0a7d8e5a52f
djames <djames@unknown>
2005-11-10 01:20:17 +0000

    Rename "disable_commits" function to "create_failing_post_commit_hook", to better reflect its purpose. Followup to r17285.
    * subversion/tests/clients/cmdline/svntest/actions.py
      (create_failing_post_commit_hook): Rename from "disable_commits". Update
      output text to reflect purpose of function.
    
    * subversion/tests/clients/cmdline/commit_tests.py
      (post_commit_hook_test): Update usage of "create_failing_post_commit_hook".
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@857361 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/clients/cmdline/svntest/actions.py
+++ subversion/tests/clients/cmdline/svntest/actions.py
@@ -815,12 +815,12 @@ pre-revprop-change hook script and (if appropriate) making it executable."""
   hook_path = main.get_pre_revprop_change_hook_path (repo_dir)
   main.create_python_hook_script (hook_path, 'import sys; sys.exit(0)')
 
-def disable_commits(repo_dir):
+def create_failing_post_commit_hook(repo_dir):
   """Disable commits in a repository REPOS_DIR by creating a post-commit hook
 script which always reports errors."""
 
   hook_path = main.get_post_commit_hook_path (repo_dir)
   main.create_python_hook_script (hook_path, 'import sys; '
-    'print >>sys.stderr, "Committing has been disabled"; '
+    'print >>sys.stderr, "Post-commit hook failed"; '
     'sys.exit(1)')
 ### End of file.
