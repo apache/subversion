9844d993dd0272cd54c1be044ca5a524ed84bb0d
lundblad <lundblad@unknown>
2005-11-15 22:07:05 +0000

    On wc-propcaching branch: Implement automatic WC format upgrading for the format changes on this branch so far.
    * subversion/libsvn_wc/lock.c (introduce_propcaching): New function.
      (maybe_upgrade_format): Introduce propcaching if the WC didn't have it
      before.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/wc-propcaching@857441 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/lock.c
+++ subversion/libsvn_wc/lock.c
@@ -28,6 +28,8 @@
 #include "adm_files.h"
 #include "lock.h"
 #include "questions.h"
+#include "props.h"
+#include "log.h"
 
 #include "svn_private_config.h"
 
@@ -89,6 +91,47 @@ static svn_error_t *
 do_close (svn_wc_adm_access_t *adm_access, svn_boolean_t preserve_lock);
 
 
+/* Write, to LOG_ACCUM, log entries to convert an old WC that did not have
+   propcaching into a WC that uses propcaching.  Do this conversion for
+   the directory of ADM_ACCESS and its file children.  Use POOL for 
+   temporary allocations.  */
+static svn_error_t *
+introduce_propcaching (svn_stringbuf_t *log_accum,
+                       svn_wc_adm_access_t *adm_access,
+                       apr_pool_t *pool)
+{
+  apr_hash_t *entries;
+  apr_hash_index_t *hi;
+  apr_pool_t *subpool = svn_pool_create (pool);
+  
+  SVN_ERR (svn_wc_entries_read (&entries, adm_access, FALSE, pool));
+
+  /* Reinstall the properties for each file and this dir; subdirs are handled
+     when they're opened. */
+  for (hi = apr_hash_first (pool, entries); hi; hi = apr_hash_next (hi))
+    {
+      void *val;
+      const svn_wc_entry_t *entry;
+      apr_hash_t *base_props, *props;
+
+      apr_hash_this (hi, NULL, NULL, &val);
+      entry = val;
+
+      if (entry->kind != svn_node_file
+          && strcmp (entry->name, SVN_WC_ENTRY_THIS_DIR) != 0)
+        continue;
+
+      svn_pool_clear (subpool);
+      
+      SVN_ERR (svn_wc__load_props (&base_props, &props, adm_access,
+                                   entry->name, subpool));
+      SVN_ERR (svn_wc__install_props (&log_accum, adm_access, entry->name,
+                                      base_props, props, TRUE, subpool));
+    }
+
+  return SVN_NO_ERROR;
+}
+
 /* Maybe upgrade the working copy directory represented by ADM_ACCESS
    to the latest 'SVN_WC__VERSION'.  ADM_ACCESS must contain a write
    lock.  Use POOL for all temporary allocation.
@@ -106,20 +149,48 @@ maybe_upgrade_format (svn_wc_adm_access_t *adm_access, apr_pool_t *pool)
                                  adm_access->path,
                                  pool));
 
-  /* ### TODO: upgrade from format 5 to format 6. */
-  /* ### Upgrading currently disabled. */
-
-  return SVN_NO_ERROR;
-
   /* We can upgrade all formats that are accepted by
      svn_wc__check_format. */
   if (adm_access->wc_format != SVN_WC__VERSION)
     {
       const char *path = svn_wc__adm_path (adm_access->path, FALSE, pool,
                                            SVN_WC__ADM_FORMAT, NULL);
+      svn_boolean_t convert_to_propcaching =
+        (adm_access->wc_format <= SVN_WC__NO_PROPCACHING_VERSION);
 
-      SVN_ERR (svn_io_write_version_file (path, SVN_WC__VERSION, pool));
+      /* Convert an old WC that doesn't use propcaching. */
+      if (convert_to_propcaching)
+        {
+          svn_boolean_t cleanup_required;
+          svn_stringbuf_t *log_accum = svn_stringbuf_create ("", pool);
+          const char *tmp_path;
+
+          /* Don't try to mess with the WC if there are old log files left. */
+          SVN_ERR (svn_wc__adm_is_cleanup_required (&cleanup_required,
+                                                    adm_access, pool));
+          if (cleanup_required)
+            return SVN_NO_ERROR;
+
+          /* First, loggily upgrade the format file. */
+          SVN_ERR (svn_io_open_unique_file2 (NULL, &tmp_path, path, ".tmp",
+                                             svn_io_file_del_none, pool));
+          SVN_ERR (svn_io_write_version_file (tmp_path, SVN_WC__VERSION,
+                                              pool));
+          SVN_ERR (svn_wc__loggy_move (&log_accum, NULL, adm_access,
+                                       svn_path_is_child (adm_access->path,
+                                                          tmp_path, pool),
+                                       svn_path_is_child (adm_access->path,
+                                                          path, pool),
+                                       FALSE, pool));
+          SVN_ERR (introduce_propcaching (log_accum, adm_access, pool));
+          SVN_ERR (svn_wc__write_log (adm_access, 0, log_accum, pool));
+          SVN_ERR (svn_wc__run_log (adm_access, NULL, pool));
+        }
+      else
+        SVN_ERR (svn_io_write_version_file (path, SVN_WC__VERSION, pool));
+
       adm_access->wc_format = SVN_WC__VERSION;
+
     }
 
   return SVN_NO_ERROR;
