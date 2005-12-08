8d1ce0d59a82ec4e4921a90b05a56e871fab9b89
Ivan Zhakov <ivan@apache.org>
2005-12-08 11:21:11 +0000

    Eliminate intermediate build_neon.bat on Windows.
    * build.conf
      (neon): Change project file location to neon's directory.
    
    * build/generator/build_neon.ezt: Remove file.
    
    * build/generator/gen_win.py
      (__init__): Remove build_neon.bat file generation.
      (write_neon_project_file): Change neon project file location.
      Pass additional parameters to template.
    
    * build/generator/neon.dsp.ezt
    * build/generator/neon.vcproj.ezt: Change command line to call
      nmake directly instead of build_neon.bat. Replace absolute 
      paths to relative.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@857765 13f79535-47bb-0310-9956-ffa450edef68

--- build/generator/gen_win.py
+++ build/generator/gen_win.py
@@ -167,17 +167,6 @@ class WinGeneratorBase(GeneratorBase):
       bat = os.path.join('build', 'win32', 'build_zlib.bat')
       self.write_with_template(bat, 'build_zlib.ezt', data)
 
-    # Generate the build_neon.bat file
-    data = {'neon_path': os.path.abspath(self.neon_path),
-            'expat_path': os.path.join(os.path.abspath(self.apr_util_path),
-                                       'xml', 'expat', 'lib'),
-            'zlib_path': self.zlib_path
-                         and os.path.abspath(self.zlib_path),
-            'openssl_path': self.openssl_path
-                            and os.path.abspath(self.openssl_path)}
-    bat = os.path.join('build', 'win32', 'build_neon.bat')
-    self.write_with_template(bat, 'build_neon.ezt', data)
-
     # Generate the build_locale.bat file
     pofiles = []
     if self.enable_nls:
@@ -450,6 +439,8 @@ class WinGeneratorBase(GeneratorBase):
       path = self.apr_util_path + target.external_project[8:]
     elif target.external_project[:4] == 'apr/':
       path = self.apr_path + target.external_project[3:]
+    elif target.external_project[:5] == 'neon/':
+      path = self.neon_path + target.external_project[4:]
     else:
       path = target.external_project
 
@@ -767,12 +758,19 @@ class WinGeneratorBase(GeneratorBase):
 
   def write_neon_project_file(self, name):
     neon_path = os.path.abspath(self.neon_path)
-    self.move_proj_file(os.path.join('build', 'win32'), name,
-                        (('neon_path', neon_path),
-                         ('neon_sources',
+    self.move_proj_file(self.neon_path, name,
+                        (('neon_sources',
                           glob.glob(os.path.join(neon_path, 'src', '*.c'))),
                          ('neon_headers',
                           glob.glob(os.path.join(neon_path, 'src', '*.h'))),
+                         ('expat_path',
+                          os.path.join(os.path.abspath(self.apr_util_path),
+                                       'xml', 'expat', 'lib')),
+                         ('zlib_path', self.zlib_path 
+                                       and os.path.abspath(self.zlib_path)),
+                         ('openssl_path',
+                          self.openssl_path
+                            and os.path.abspath(self.openssl_path)),
                         ))
 
   def move_proj_file(self, path, name, params=()):
