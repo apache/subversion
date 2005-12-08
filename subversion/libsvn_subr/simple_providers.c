6a2207cd5794f082997e5706eb09349b5f54572b
Daniel Rall <dlr@apache.org>
2005-12-08 02:08:56 +0000

    * subversion/libsvn_subr/simple_providers.c   Add in-line comments about several known issues with OS X Keychain   support, including lack of TTY prompting, prompting even when   SecKeychainSetUserInteractionAllowed(FALSE) was called, and a   concurrency-related race condition with that same API.
    Patch by: me
              mbk
    Suggested by: me
                  Wilfredo Sánchez <wsanchez@apache.org>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@857760 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/simple_providers.c
+++ subversion/libsvn_subr/simple_providers.c
@@ -733,6 +733,25 @@ svn_auth_get_windows_simple_provider (svn_auth_provider_object_t **provider,
 #if SVN_HAVE_KEYCHAIN_SERVICES
 #include <Security/Security.h>
 
+/*
+ * XXX (2005-12-07): If no GUI is available (e.g. over a SSH session),
+ * you won't be prompted for credentials with which to unlock your
+ * keychain.  Apple recognizes lack of TTY prompting as a known
+ * problem.
+ *
+ *
+ * XXX (2005-12-07): SecKeychainSetUserInteractionAllowed(FALSE) does
+ * not appear to actually prevent all user interaction.  Specifically,
+ * if the executable changes (for example, if it is rebuilt), the
+ * system prompts the user to okay the use of the new executable.
+ *
+ * Worse than that, the interactivity setting is global per app (not
+ * process/thread), meaning that there is a race condition in the
+ * implementation below between calls to
+ * SecKeychainSetUserInteractionAllowed() when multiple instances of
+ * the same Subversion auth provider-based app run concurrently.
+ */
+
 /* Implementation of password_set_t that stores the password
    in the OS X KeyChain. */
 static svn_boolean_t
