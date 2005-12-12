77bde3f40357acc673390e2628597207acaa8dc7
lundblad <lundblad@unknown>
2005-12-12 22:59:56 +0000

    Merge r17665, r17666 (by merging the 1.3.x-r17665) to the 1.3.x branch.
    Avoid corrupting WC when adding repos entry attribute, or when switching
    with an inconsistent URL.
    
    Votes:
    +1: lundblad, rooneg, kfogel
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/1.3.x@857826 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/clients/cmdline/switch_tests.py
+++ subversion/tests/clients/cmdline/switch_tests.py
@@ -971,6 +971,35 @@ def relocate_beyond_repos_root(sbox):
                                      'info', '-rHEAD', A_wc_dir)
                                      
 
+# Check that switch can't change the repository root.
+def switch_change_repos_root(sbox):
+  "switch shouldn't allow changing repos root"
+  sbox.build()
+
+  wc_dir = sbox.wc_dir
+  repo_url = sbox.repo_url
+  other_repo_url = repo_url
+
+  # Strip trailing slashes and add something bogus to that other URL.
+  while other_repo_url[-1] == '/':
+    other_repos_url = other_repo_url[:-1]
+  other_repo_url = other_repo_url + "_bogus"
+
+  other_A_url = other_repo_url +  "/A"
+  A_wc_dir = os.path.join(wc_dir, "A")
+
+  # A switch that changes the repo root part of the URL shouldn't work.
+  svntest.actions.run_and_verify_svn(None, None,
+                                     ".*not the same repository.*",
+                                     'switch',
+                                     other_A_url, A_wc_dir)
+
+  # Make sure we didn't break the WC.
+  expected_status = svntest.actions.get_virginal_state(wc_dir, 1)
+  svntest.actions.run_and_verify_status(wc_dir, expected_status)
+                                        
+
+
 ########################################################################
 # Run the tests
 
@@ -993,6 +1022,7 @@ test_list = [ None,
               obstructed_switch,
               XFail(commit_mods_below_switch),
               relocate_beyond_repos_root,
+              switch_change_repos_root,
              ]
 
 if __name__ == '__main__':
