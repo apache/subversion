94a53f91af7bd42d51d5650cc8a42ea30d0af383
djames <dev-null@apache.org>
2005-12-24 18:38:45 +0000

    Checkout SWIG libraries and build external runtimes one at a time, so as to be more friendly to parallel make. Move functionality for checking out SWIG libraries into checkout_swig_header.py.
    * build.conf
      (swig-checkout-files): New configuration value. Defines which SWIG headers
      should be checked out from the SWIG library.
    
    * build/generator/gen_make.py, build/generator/gen_win.py
      (Generator.write): Checkout SWIG headers using new checkout_swig_header
      module.
    
    * build/generator/swig/__init__.py
      (Generator.__init__): Read swig-checkout-files config variable.
      (Generator.checkout): Remove function. Move into checkout_swig_header.py.
    
    * build/generator/swig/external_runtime.py
      (Generator.write): Rewrite function. Build external runtime for each
      language.
      (Generator.write_makefile_rules): Pass language into $(GEN_SWIG_RUNTIME).
      Add $(SWIG_CHECKOUT_FILES) as dependency.
      (Generator.write_external_runtime): Remove code for checking out SWIG
      libraries. Moved to checkout_swig_header.py.
    
    * build/generator/swig/checkout_swig_header.py: New file. Checkout files from
      the SWIG library into Subversion's proxy directory.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@858007 13f79535-47bb-0310-9956-ffa450edef68

--- build/generator/swig/external_runtime.py
+++ build/generator/swig/external_runtime.py
@@ -16,9 +16,9 @@ class Generator(generator.swig.Generator):
   """Generate external runtime files for SWIG"""
 
   def write(self):
-    """Generate external runtime and long long fix"""
-    self.write_external_runtime()
-    self.write_long_long_fix()
+    """Generate external runtimes"""
+    for lang in self.langs:
+      self.write_external_runtime(lang)
 
   def write_makefile_rules(self, makefile):
     """Write the makefile rules for generating external runtimes"""
@@ -30,25 +30,13 @@ class Generator(generator.swig.Generator):
       out = self._output_file(lang)
       makefile.write(
         'autogen-swig-%s: %s\n' % (self.short[lang], out) +
-        '%s:\n' % out +
-        '\t$(GEN_SWIG_RUNTIME)\n\n'
+        '%s: $(SWIG_CHECKOUT_FILES)\n' % out +
+        '\t$(GEN_SWIG_RUNTIME) %s\n\n' % lang
       )
     makefile.write('\n')
 
-  def write_external_runtime(self):
+  def write_external_runtime(self, lang):
     """Generate external runtime header files for each SWIG language"""
-
-    # Gather necessary header files
-    self.checkout(".","swigrun.swg")
-    if self.version() == 103024:
-      self.checkout(".","common.swg")
-    self.checkout(".","runtime.swg")
-    self.checkout("python","pyrun.swg")
-    self.checkout("perl5","perlrun.swg")
-    self.checkout("ruby","rubydef.swg")
-    self.checkout("ruby", "rubyhead.swg")
-    if self.version() >= 103026:
-      self.checkout("ruby", "rubytracking.swg")
 
     # Runtime library names
     runtime_library = {
@@ -56,68 +44,47 @@ class Generator(generator.swig.Generator):
     }
 
     # Build runtime files
-    for lang in self.langs:
-      out = "%s/swig_%s_external_runtime.swg" % (self.proxy_dir, lang)
-      if self.version() == 103024:
-        out_file = open(out, "w")
-        out_file.write(open("%s/swigrun.swg" % self.proxy_dir).read())
-        out_file.write(open("%s/common.swg" % self.proxy_dir).read())
-        out_file.write(
-          open("%s/%s" % (self.proxy_dir, runtime_library[lang])).read())
-        if lang != "ruby":
-          out_file.write(open("%s/runtime.swg" % self.proxy_dir).read())
-        out_file.close()
-      else:
-        _exec.run("%s -%s -external-runtime %s" % (self.swig_path, lang, out))
+    out = self._output_file(lang)
+    if self.version() == 103024:
+      out_file = open(out, "w")
+      out_file.write(open("%s/swigrun.swg" % self.proxy_dir).read())
+      out_file.write(open("%s/common.swg" % self.proxy_dir).read())
+      out_file.write(
+        open("%s/%s" % (self.proxy_dir, runtime_library[lang])).read())
+      if lang != "ruby":
+        out_file.write(open("%s/runtime.swg" % self.proxy_dir).read())
+      out_file.close()
+    else:
+      _exec.run("%s -%s -external-runtime %s" % (self.swig_path, lang, out))
 
-      if lang == "ruby" and self.version() >= 103026:
-        # SWIG 1.3.26-27 should include rubytracking.swg in their
-        # external runtime, but they don't.
-        # TODO: Find out whether this bug will be fixed in later versions
-        # of SWIG.
-        runtime = open(out, "r").read()
-        tracking = open("%s/rubytracking.swg" % self.proxy_dir,"r").read()
-        out_file = open(out, "w")
-        out_file.write(tracking)
-        out_file.write(runtime)
-        out_file.close()
+    if lang == "ruby" and self.version() >= 103026:
+      # SWIG 1.3.26-27 should include rubytracking.swg in their
+      # external runtime, but they don't.
+      # TODO: Find out whether this bug will be fixed in later versions
+      # of SWIG.
+      runtime = open(out).read()
+      tracking = open("%s/rubytracking.swg" % self.proxy_dir).read();
+      out_file = open(out, "w")
+      out_file.write(tracking)
+      out_file.write(runtime)
+      out_file.close()
 
-      # SWIG 1.3.25 and earlier use the wrong number of arguments in calls to
-      # SWIG_GetModule. We fix this below.
-      if self.version() <= 103025:
-        for line in fileinput.input(out, inplace=1):
-          sys.stdout.write(
-            re.sub(r"SWIG_GetModule\(\)", "SWIG_GetModule(NULL)", line)
-          )
-
-  def write_long_long_fix(self):
-    """Hide the SWIG implementation of 'long long' converters so that
-       Visual C++ won't get confused by it."""
-
-    self.checkout("python","python.swg")
-
-    python_swg_filename = "%s/python.swg" % self.proxy_dir
-    python_swg = open(python_swg_filename).read()
-    file = open(python_swg_filename,"w")
-    file.write("""
-    %fragment("SWIG_AsVal_" {long long},"header") {
-    }
-    %fragment("SWIG_Check_" {long long},"header") {
-    }
-    %fragment("SWIG_From_" {long long},"header") {
-    }\n""")
-    file.write(python_swg)
-    file.close()
-
+    # SWIG 1.3.25 and earlier use the wrong number of arguments in calls to
+    # SWIG_GetModule. We fix this below.
+    if self.version() <= 103025:
+      for line in fileinput.input(out, inplace=1):
+        sys.stdout.write(
+          re.sub(r"SWIG_GetModule\(\)", "SWIG_GetModule(NULL)", line)
+        )
   def _output_file(self, lang):
     """Return the output filename of the runtime for the given language""" 
     return '%s/swig_%s_external_runtime.swg' % (self.proxy_dir, lang)
 
 
 if __name__ == "__main__":
-  if len(sys.argv) < 3:
+  if len(sys.argv) != 4:
     print "Usage: %s build.conf swig"
     print "Generates external runtime files for SWIG"
   else:
     gen = Generator(sys.argv[1], sys.argv[2])
-    gen.write()
+    gen.write_external_runtime(sys.argv[3])
