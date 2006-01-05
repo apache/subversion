d8c46821ebf228ffb39892dddb931be1337fba54
Max Oliver Bowsher <maxb@apache.org>
2006-01-05 00:25:43 +0000

    * contrib/client-side/svn-push/svn-push.c: Thoughrough overhaul.   Update copyright year. Miscellaneous formatting cleanup.   (my_commit_callback): Update to new api-rev svn_commit_callback2_t.   (open_tmp_file): New function.   (do_job): Fix pool argument to be in conventional last place.     Update many uses of deprecated APIs.     Add proper authentication support using svn_cmdline_setup_auth_baton.     Avoid allocating structures on the stack, because it makes the use of them     look unusual.   (main): Use conventional name 'pool' for apr_pool_t. General tidying up and     simplification.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@858057 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/client-side/svn-push/svn-push.c
+++ contrib/client-side/svn-push/svn-push.c
@@ -2,7 +2,7 @@
  * a different (networked) repository.
  *
  * ====================================================================
- * Copyright (c) 2000-2003 CollabNet.  All rights reserved.
+ * Copyright (c) 2000-2006 CollabNet.  All rights reserved.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
@@ -18,128 +18,117 @@
 #include <stdio.h>
 
 #include "svn_pools.h"
+#include "svn_path.h"
 #include "svn_ra.h"
 #include "svn_delta.h"
 #include "svn_config.h"
 #include "svn_cmdline.h"
 
+/* Implements svn_commit_callback2_t. */
 static svn_error_t *
-my_commit_callback (svn_revnum_t new_revision,
-		    const char *date, const char *author, void *baton)
+my_commit_callback (const svn_commit_info_t *ci, void *baton, apr_pool_t *pool)
 {
   printf ("Commiting Rev. %" SVN_REVNUM_T_FMT " at date \"%s\", by "
-          "author \"%s\"", new_revision, date, author);
-
+          "author \"%s\"\n", ci->revision, ci->date, ci->author);
+  if (ci->post_commit_err)
+    printf ("Post-commit Error: %s\n", ci->post_commit_err);
   return SVN_NO_ERROR;
 }
 
-svn_error_t *(*old_change_file_prop) (void *file_baton,
-				      const char *name,
-				      const svn_string_t * value,
-				      apr_pool_t * pool);
 
-svn_error_t *(*old_change_dir_prop) (void *dir_baton,
-				     const char *name,
-				     const svn_string_t * value,
-				     apr_pool_t * pool);
-
+/* Implements svn_ra_callbacks2_t.open_tmp_file */
 static svn_error_t *
-new_change_file_prop (void *file_baton,
-		      const char *name,
-		      const svn_string_t * value, apr_pool_t * pool)
+open_tmp_file (apr_file_t **fp, void *callback_baton, apr_pool_t *pool)
+{
+  const char *path;
+
+  SVN_ERR (svn_io_temp_dir (&path, pool));
+  path = svn_path_join (path, "tempfile", pool);
+  SVN_ERR (svn_io_open_unique_file (fp, NULL, path, ".tmp", TRUE, pool));
+
+  return SVN_NO_ERROR;
+}
+  
+
+svn_error_t *(*old_change_file_prop) (void *file_baton,
+                                      const char *name,
+                                      const svn_string_t *value,
+                                      apr_pool_t *pool);
+
+/* Implements svn_ra_callbacks2_t.change_file_prop */
+static svn_error_t *
+new_change_file_prop (void *file_baton, const char *name,
+                      const svn_string_t *value, apr_pool_t *pool)
 {
   if (svn_property_kind (NULL, name) != svn_prop_regular_kind)
-    {
-      /* Do nothing */
-      return SVN_NO_ERROR;
-    }
+    return SVN_NO_ERROR;
   else
     return old_change_file_prop (file_baton, name, value, pool);
 }
 
+
+svn_error_t *(*old_change_dir_prop) (void *dir_baton,
+                                     const char *name,
+                                     const svn_string_t *value,
+                                     apr_pool_t *pool);
+
+/* Implements svn_ra_callbacks2_t.change_dir_prop */
 static svn_error_t *
-new_change_dir_prop (void *dir_baton,
-		     const char *name,
-		     const svn_string_t * value, apr_pool_t * pool)
+new_change_dir_prop (void *dir_baton, const char *name,
+                     const svn_string_t *value, apr_pool_t *pool)
 {
   if (svn_property_kind (NULL, name) != svn_prop_regular_kind)
-    {
-      return SVN_NO_ERROR;	/* Do nothing */
-    }
+    return SVN_NO_ERROR;
   else
     return old_change_dir_prop (dir_baton, name, value, pool);
 }
 
 
-
 static svn_error_t *
-do_job (apr_pool_t * pool, const char *src_url, const char *dest_url,
-	int start_rev, int end_rev)
+do_job (const char *src_url, const char *dest_url, int start_rev, int end_rev,
+        apr_pool_t *pool)
 {
-  svn_ra_plugin_t *ra_src, *ra_dest;
-  void *ra_src_sess_baton, *ra_dest_sess_baton;
-  const svn_delta_editor_t *delta_editor;
-  svn_delta_editor_t my_delta_editor;
-  void *edit_baton;
-  void *ra_baton;
-  const svn_ra_reporter_t *reporter;
-  void *report_baton;
   apr_hash_t *config;
-  svn_ra_callbacks_t dest_callbacks;
-
-#if 0
+  svn_config_t *cfg;
   svn_auth_baton_t *ab;
-  apr_array_header_t *providers;
-#endif 
+  svn_ra_callbacks2_t *callbacks;
+  svn_ra_session_t *ra_src, *ra_dest;
+  const svn_delta_editor_t *editor;
+  svn_delta_editor_t *my_editor;
+  void *edit_baton;
+  const svn_ra_reporter2_t *reporter;
+  void *report_baton;
 
   SVN_ERR (svn_config_get_config (&config, NULL, pool));
+  cfg = apr_hash_get (config, SVN_CONFIG_CATEGORY_CONFIG, APR_HASH_KEY_STRING);
 
-  SVN_ERR (svn_ra_init_ra_libs (&ra_baton, pool));
+  SVN_ERR (svn_cmdline_setup_auth_baton (&ab, FALSE, NULL, NULL, NULL, FALSE,
+                                         cfg, NULL, NULL, pool));
+  SVN_ERR (svn_ra_create_callbacks (&callbacks, pool));
+  callbacks->open_tmp_file = open_tmp_file;
+  callbacks->auth_baton = ab;
 
-  SVN_ERR (svn_ra_get_ra_library (&ra_src, ra_baton, src_url, pool));
+  SVN_ERR (svn_ra_open2 (&ra_dest, dest_url, callbacks, NULL, config, pool));
+  SVN_ERR (svn_ra_open2 (&ra_src, src_url, callbacks, NULL, config, pool));
 
-  SVN_ERR (svn_ra_get_ra_library (&ra_dest, ra_baton, dest_url, pool));
+  SVN_ERR (svn_ra_get_commit_editor2 (ra_dest, &editor, &edit_baton,
+                                      "Hello World!", my_commit_callback,
+                                      NULL, NULL, TRUE, pool));
 
-  memset ((void *) &dest_callbacks, '\0', sizeof (dest_callbacks));
+  /* Create a copy of the editor so we can hook some calls. */
+  my_editor = apr_palloc (pool, sizeof (*my_editor));
+  *my_editor = *editor;
 
-#if 0
-  providers
-    = apr_array_make (pool, 10, sizeof (svn_auth_provider_object_t *));
+  /* Install the editor hooks. */
+  old_change_file_prop = editor->change_file_prop;
+  my_editor->change_file_prop = new_change_file_prop;
+  old_change_dir_prop = editor->change_dir_prop;
+  my_editor->change_dir_prop = new_change_dir_prop;
 
-
-  svn_auth_open (&ab, providers, pool);
-#endif
-
-  SVN_ERR (ra_dest->open (&ra_dest_sess_baton,
-			  dest_url, &dest_callbacks, NULL, config, pool));
-
-  SVN_ERR (ra_src->open (&ra_src_sess_baton,
-			 src_url, &dest_callbacks, NULL, config, pool));
-
-
-  SVN_ERR (ra_dest->get_commit_editor (ra_dest_sess_baton,
-				       &delta_editor,
-				       &edit_baton,
-				       "Hello World!",
-				       my_commit_callback, NULL, pool));
-
-  my_delta_editor = *delta_editor;
-
-  old_change_dir_prop = my_delta_editor.change_dir_prop;
-  my_delta_editor.change_dir_prop = new_change_dir_prop;
-
-  old_change_file_prop = my_delta_editor.change_file_prop;
-  my_delta_editor.change_file_prop = new_change_file_prop;
-
-  SVN_ERR (ra_src->do_diff (ra_src_sess_baton,
-			    &reporter,
-			    &report_baton,
-			    end_rev,
-			    "",
-			    1, 1, src_url, &my_delta_editor, edit_baton, pool));
-
-  SVN_ERR (reporter->set_path (report_baton, "", start_rev, 0, pool));
-
+  SVN_ERR (svn_ra_do_diff (ra_src, &reporter, &report_baton,
+                           end_rev, "", TRUE, TRUE, src_url,
+                           my_editor, edit_baton, pool));
+  SVN_ERR (reporter->set_path (report_baton, "", start_rev, 0, NULL, pool));
   SVN_ERR (reporter->finish_report (report_baton, pool));
 
   return SVN_NO_ERROR;
@@ -166,64 +155,38 @@ check_lib_versions (void)
 int
 main (int argc, char *argv[])
 {
-  apr_pool_t *top_pool;
+  apr_pool_t *pool;
   svn_error_t *error = NULL;
   int start_rev, end_rev;
   char *src_url, *dest_url;
 
-
-  /* Initialize the app.  Send all error messages to 'stderr'.  */
-  if (svn_cmdline_init ("minimal_client", stderr) != EXIT_SUCCESS)
+  if (svn_cmdline_init ("svn-push", stderr) != EXIT_SUCCESS)
     return EXIT_FAILURE;
 
-  top_pool = svn_pool_create (NULL);
-
-  /* Check library versions */
-  error = check_lib_versions ();
-  if (error)
+  if (argc != 5 ||
+      strcmp (argv[1], "-r") != 0 ||
+      sscanf (argv[2], "%i:%i", &start_rev, &end_rev) != 2)
     {
-      svn_handle_error2 (error, stderr, FALSE, "svn-push");
+      fprintf(stderr, "%s", "Usage : svn-push -r N:M SRC_URL DEST_URL\n");
       return EXIT_FAILURE;
-    }
-
-#define CMD_LINE_ERROR \
-        { \
-        fprintf(stderr, "%s",  \
-                "Usage : svn-push -r N:M [SRC_URL] [DEST_URL]\n"); \
-        goto error; \
-        } \
-
-  if (argc != 5)
-    {
-    CMD_LINE_ERROR
-    }
-
-  if (strcmp (argv[1], "-r"))
-    {
-    CMD_LINE_ERROR
-    }
-
-  if (sscanf (argv[2], "%i:%i", &start_rev, &end_rev) != 2)
-    {
-    CMD_LINE_ERROR
     }
 
   src_url = argv[3];
   dest_url = argv[4];
 
-  error = do_job (top_pool, src_url, dest_url, start_rev, end_rev);
+  pool = svn_pool_create (NULL);
 
-  if (error)
-    {
-      svn_handle_error2 (error, stderr, FALSE, "svn-push: ");
-      return EXIT_FAILURE;
-    }
+  /* Check library versions */
+  error = check_lib_versions ();
+  if (error) goto error;
 
+  error = do_job (src_url, dest_url, start_rev, end_rev, pool);
+  if (error) goto error;
+
+  svn_pool_destroy (pool);
+  return EXIT_SUCCESS;
 
 error:
-  svn_pool_destroy (top_pool);
-
-  return 0;
+  svn_handle_error2 (error, stderr, FALSE, "svn-push: ");
+  return EXIT_FAILURE;
 }
-
-
