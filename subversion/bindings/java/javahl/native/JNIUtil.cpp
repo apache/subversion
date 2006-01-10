bc1aee84688296461cc99a5b18b8e2df57c1a2fc
Daniel Rall <dlr@apache.org>
2006-01-10 22:42:45 +0000

    * subversion/bindings/java/javahl/native/JNIUtil.cpp   (JNIUtil::JNIGlobalInit): Remove tab characters introduced in r18054.
    Review by: lundblad
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@858129 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/java/javahl/native/JNIUtil.cpp
+++ subversion/bindings/java/javahl/native/JNIUtil.cpp
@@ -194,7 +194,7 @@ bool JNIUtil::JNIGlobalInit(JNIEnv *env)
         outbytes = outlength = 3 * (inwords + 1);
         utf8_path = (char *)apr_palloc (pool, outlength);
         apr_err = apr_conv_ucs2_to_utf8 ((const apr_wchar_t *) ucs2_path,
-					 &inwords, utf8_path, &outbytes);
+                                         &inwords, utf8_path, &outbytes);
         if (!apr_err && (inwords > 0 || outbytes == 0))
           apr_err = APR_INCOMPLETE;
         if (apr_err)
