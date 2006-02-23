98de44d553c00384d5c818a69a03096e8dec95b8
Philip Martin <philip@apache.org>
2006-02-23 23:50:38 +0000

    * subversion/mod_dav_svn/lock.c (unescape_xml): Cast to (void) not (void*).
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@858668 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/mod_dav_svn/lock.c
+++ subversion/mod_dav_svn/lock.c
@@ -139,7 +139,7 @@ unescape_xml(const char **output,
   if (apr_err)
     {
       char errbuf[1024];
-      (void *)apr_xml_parser_geterror(xml_parser, errbuf, sizeof(errbuf));
+      (void)apr_xml_parser_geterror(xml_parser, errbuf, sizeof(errbuf));
       return dav_new_error(pool, HTTP_INTERNAL_SERVER_ERROR,
                            DAV_ERR_LOCK_SAVE_LOCK, errbuf);
     }
