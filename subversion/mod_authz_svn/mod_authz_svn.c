3096c06a6d4ee995ad7131e4f5c4b43da7713181
Max Oliver Bowsher <maxb@apache.org>
2006-03-02 09:59:29 +0000

    Implement a new mod_authz_svn directive 'AuthzSVNNoAuthWhenAnonymousAllowed', which provides similar functionality to 'Satisfy Any', but without abusing the 'Satisfy' mechanism to do so.  The 'Satisfy' directive is deprecated in httpd-2.3.
    * subversion/mod_authz_svn/mod_authz_svn.c:
      Add extern APR optional fn ap_satisfies declaration to make compilation
      with httpd-2.3 work.
      (authz_svn_config_rec): New field no_auth_when_anon_ok.
      (authz_svn_cmds): New command "AuthzSVNNoAuthWhenAnonymousAllowed".
      (check_user_id): New hook function.
      (auth_checker): If the check_user_id has already done the heavy work,
        exit early.
      (register_hooks): Register the new hook.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@858754 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/mod_authz_svn/mod_authz_svn.c
+++ subversion/mod_authz_svn/mod_authz_svn.c
@@ -30,6 +30,7 @@
 #include <mod_dav.h>
 #if AP_MODULE_MAGIC_AT_LEAST(20060110,0)
 #include <mod_auth.h>
+extern APR_OPTIONAL_FN_TYPE(ap_satisfies) *ap_satisfies;
 #endif
 
 #include "mod_dav_svn.h"
@@ -44,6 +45,7 @@ extern module AP_MODULE_DECLARE_DATA authz_svn_module;
 typedef struct {
     int authoritative;
     int anonymous;
+    int no_auth_when_anon_ok;
     const char *base_path;
     const char *access_file;
 } authz_svn_config_rec;
@@ -83,6 +85,13 @@ static const command_rec authz_svn_cmds[] =
                  "directive, and (2) enforcement of the authorization "
                  "policy even when no 'Require' directives are present. "
                  "(default is On.)"),
+    AP_INIT_FLAG("AuthzSVNNoAuthWhenAnonymousAllowed", ap_set_flag_slot,
+                 (void *)APR_OFFSETOF(authz_svn_config_rec,
+                                      no_auth_when_anon_ok),
+                 OR_AUTHCFG,
+                 "Set to 'On' to suppress authentication and authorization "
+                 "for requests which anonymous users are allowed to perform. "
+                 "(default is Off.)"),
     { NULL }
 };
 
@@ -456,6 +465,32 @@ static int access_checker(request_rec *r)
     return OK;
 }
 
+static int check_user_id(request_rec *r)
+{
+    authz_svn_config_rec *conf = ap_get_module_config(r->per_dir_config,
+                                                      &authz_svn_module);
+    const char *repos_path;
+    const char *dest_repos_path = NULL;
+    int status;
+
+    /* We are not configured to run, or, an earlier module has already
+     * authenticated this request. */
+    if (!conf->access_file || !conf->no_auth_when_anon_ok || r->user)
+        return DECLINED;
+
+    /* If anon access is allowed, return OK, preventing later modules
+     * from issuing an HTTP_UNAUTHORIZED.  Also pass a note to our
+     * auth_checker hook that access has already been checked. */
+    status = req_check_access(r, conf, &repos_path, &dest_repos_path);
+    if (status == OK) {
+        apr_table_setn(r->notes, "authz_svn-anon-ok", (const char*)1);
+        log_access_verdict(APLOG_MARK, r, 1, repos_path, dest_repos_path);
+        return OK;
+    }
+
+    return status;
+}
+
 static int auth_checker(request_rec *r)
 {
     authz_svn_config_rec *conf = ap_get_module_config(r->per_dir_config,
@@ -468,6 +503,12 @@ static int auth_checker(request_rec *r)
     if (!conf->access_file)
         return DECLINED;
 
+    /* Previous hook (check_user_id) already did all the work,
+     * and, as a sanity check, r->user hasn't been set since then? */
+    if (!r->user && apr_table_get(r->notes, "authz_svn-anon-ok")) {
+        return OK;
+    }
+      
     status = req_check_access(r, conf, &repos_path, &dest_repos_path);
     if (status == DECLINED) {
         if (conf->authoritative) {
@@ -493,7 +534,13 @@ static int auth_checker(request_rec *r)
 
 static void register_hooks(apr_pool_t *p)
 {
+    static const char * const mod_ssl[] = { "mod_ssl.c", NULL };
+
     ap_hook_access_checker(access_checker, NULL, NULL, APR_HOOK_LAST);
+    /* Our check_user_id hook must be before any module which will return
+     * HTTP_UNAUTHORIZED (mod_auth_basic, etc.), but after mod_ssl, to
+     * give SSLOptions +FakeBasicAuth a chance to work. */
+    ap_hook_check_user_id(check_user_id, mod_ssl, NULL, APR_HOOK_FIRST);
     ap_hook_auth_checker(auth_checker, NULL, NULL, APR_HOOK_FIRST);
 }
 
