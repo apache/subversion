17bac4c44dc42ea2f235873801ef7d40a867c417
Branko Čibej <brane@apache.org>
2006-03-10 08:58:20 +0000

    Stop using BUFSIZ for file read buffers, because it's usually too small to be efficient.
    * subversion/libsvn_wc/entries.c (read_entries),
    * subversion/libsvn_wc/log.c (run_log),
    * subversion/libsvn_subr/io.c
      (svn_io_file_checksum, contents_identical_p, svn_stringbuf_from_aprfile),
    * subversion/svnlook/main.c (do_cat):
       Replace stack-allocated buffers of BUFSIZ size with dynamically-allocated
       buffers of size SVN__STREAM_CHUNK_SIZE.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@858883 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svnlook/main.c
+++ subversion/svnlook/main.c
@@ -1219,8 +1219,8 @@ do_cat(svnlook_ctxt_t *c, const char *path, apr_pool_t *pool)
   svn_fs_root_t *root;
   svn_node_kind_t kind;
   svn_stream_t *fstream, *stdout_stream;
-  char buf[BUFSIZ];
-  apr_size_t len = BUFSIZ;
+  char *buf = apr_palloc(pool, SVN__STREAM_CHUNK_SIZE);
+  apr_size_t len = SVN__STREAM_CHUNK_SIZE;
 
   SVN_ERR(get_root(&root, c, pool));
   SVN_ERR(verify_path(&kind, root, path, pool));
@@ -1239,7 +1239,7 @@ do_cat(svnlook_ctxt_t *c, const char *path, apr_pool_t *pool)
       SVN_ERR(svn_stream_read(fstream, buf, &len));
       SVN_ERR(svn_stream_write(stdout_stream, buf, &len));
     } 
-  while (len == BUFSIZ);
+  while (len == SVN__STREAM_CHUNK_SIZE);
 
   return SVN_NO_ERROR;
 }
