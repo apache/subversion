2d3b1387f9bbd6ff36b325874cdf76f71fdf119f
Blair Zajac <blair@apache.org>
2006-04-06 18:06:08 +0000

    Trim trailing whitespace.
    * contrib/client-side/svnmerge.py:
      Trim trailing whitespace.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@859281 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/client-side/svnmerge.py
+++ contrib/client-side/svnmerge.py
@@ -292,7 +292,7 @@ class RevisionLog:
             # The --verbose flag lets us grab merge tracking information
             # by looking at propchanges
             log_opts = "--verbose " + log_opts
-    
+
         # Read the log to look for revision numbers and merge-tracking info
         self.revs = []
         self.propchange_revs = []
@@ -610,7 +610,7 @@ def set_props(dir, name, props):
     if props:
         _run_propset(dir, name, props)
     else:
-        svn_command('propdel "%s" "%s"' % (name, dir)) 
+        svn_command('propdel "%s" "%s"' % (name, dir))
 
 def set_merge_props(dir, props):
     set_props(dir, opts["prop"], props)
