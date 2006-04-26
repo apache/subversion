985bd3eadc51990bf95692853846019fe7118132
lundblad <dev-null@apache.org>
2006-04-26 19:30:40 +0000

    In FSFS, decrease the server load somewhat in a common case where the filesystem has actually stored the delta the client is asking for.
    * subversion/libsvn_fs_fs/tree.c
      (fs_get_file_delta_stream): Call svn_fs_fs__dag_get_file_delta_stream.
    
    * subversion/libsvn_fs_fs/dag.h
      (svn_fs_fs__dag_get_file_delta_stream): Declare.
    
    * subversion/libsvn_fs_fs/fs_fs.c
      (create_rep_state): New function, factored out of ...
      (build_rep_list): ...here.  Use the above to get rep information.
      (delta_read_baton): New struct.
      (delta_read_next_window, delta_read_md5_digest,
      svn_fs_fs__get_file_delta_stream): New functions.
    
    * subversion/libsvn_fs_fs/fs_fs.h
      (svn_fs_fs__get_file_delta_stream): Declare.
    
    * subversion/libsvn_fs_fs/dag.c
      (svn_fs_fs__dag_get_file_delta_stream): New function.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@859530 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_fs/fs_fs.h
+++ subversion/libsvn_fs_fs/fs_fs.h
@@ -1,7 +1,7 @@
 /* fs_fs.h : interface to the native filesystem layer
  *
  * ====================================================================
- * Copyright (c) 2000-2004 CollabNet.  All rights reserved.
+ * Copyright (c) 2000-2004, 2006 CollabNet.  All rights reserved.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
@@ -84,6 +84,15 @@ svn_error_t *svn_fs_fs__get_contents(svn_stream_t **contents,
                                      svn_fs_t *fs,
                                      node_revision_t *noderev,
                                      apr_pool_t *pool);
+
+/* Set *STREAM_P to a delta stream turning the contents of the file SOURCE into
+   the contents of the FILE target, allocated in POOL.
+   If SOURCE is null, the empty string will be used. */
+svn_error_t *svn_fs_fs__get_file_delta_stream(svn_txdelta_stream_t **stream_p,
+                                              svn_fs_t *fs,
+                                              node_revision_t *source,
+                                              node_revision_t *target,
+                                              apr_pool_t *pool);
 
 /* Set *PROPLIST to be an apr_hash_t containing the property list of
    node-revision NODEREV as seen in filesystem FS.  Use POOL for
