c3c3a975c8b42af7d5494bdf8d220065af27944e
Max Oliver Bowsher <maxb@apache.org>
2006-05-09 21:33:21 +0000

    * subversion/tests/cmdline/svnlook_tests.py (run_svnlook): Add docstring.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@859669 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/svnlook_tests.py
+++ subversion/tests/cmdline/svnlook_tests.py
@@ -34,6 +34,8 @@ Item = svntest.wc.StateItem
 # Convenience functions to make writing more tests easier
 
 def run_svnlook(*varargs):
+  "Run svnlook with VARARGS, returns stdout as list of lines.
+  Raises Failure if any stderr messages."
   output, dummy_errput = svntest.main.run_command(svntest.main.svnlook_binary,
       0, 0, *varargs)
   return output
