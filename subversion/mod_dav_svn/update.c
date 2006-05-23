ae570458fbfefae828573e9f276bb46cafae243a
Garrett Rooney <rooneg@apache.org>
2006-05-23 18:10:30 +0000

    Some more xml encoding related fixes for mod_dav_svn.
    * subversion/mod_dav_svn/update.c
      (absent_helper): Encode the basename of the path.
      (upd_close_file): Don't pass elt directly to dav_svn__send_xml, it's not
       good practice to use generated strings as the format string in that sort
       of thing, even if it may be safe in this particular case.
    
    * subversion/mod_dav_svn/replay.c
      (change_dir_prop, change_file_prop): Encode the prop name.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@859860 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/mod_dav_svn/update.c
+++ subversion/mod_dav_svn/update.c
@@ -437,7 +437,10 @@ static svn_error_t * absent_helper(svn_boolean_t is_dir,
       const char *elt = apr_psprintf(pool,
                                      "<S:absent-%s name=\"%s\"/>" DEBUG_CR,
                                      DIR_OR_FILE(is_dir),
-                                     svn_path_basename(path, pool));
+                                     apr_xml_quote_string
+                                       (pool,
+                                        svn_path_basename(path, pool),
+                                        1));
       SVN_ERR(dav_svn__send_xml(uc->bb, uc->output, "%s", elt));
     }
 
@@ -1034,7 +1037,7 @@ static svn_error_t * upd_close_file(void *file_baton,
                          file->base_checksum ? " base-checksum=\"" : "",
                          file->base_checksum ? file->base_checksum : "",
                          file->base_checksum ? "\"" : "");
-      SVN_ERR(dav_svn__send_xml(file->uc->bb, file->uc->output, elt));
+      SVN_ERR(dav_svn__send_xml(file->uc->bb, file->uc->output, "%s", elt));
     }
 
   return close_helper(FALSE /* is_dir */, file);
