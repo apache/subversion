10027c44a4db35cf92b11e7e213d8f22e0e22810
Garrett Rooney <rooneg@apache.org>
2006-05-25 23:20:14 +0000

    Clean up the log --limit test a bit.  Follow up to r19753.
    * subversion/tests/log_tests.py
      (log_limit): Break some overly long lines.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@859903 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/log_tests.py
+++ subversion/tests/cmdline/log_tests.py
@@ -778,18 +778,16 @@ def log_limit(sbox):
   log_chain = parse_log_output (out)
   check_log_chain(log_chain, [1])
 
+  must_be_positive = ".*Argument to --limit must be positive.*"
+
   # error expected when limit <= 0
-  svntest.actions.run_and_verify_svn(None, None, ".*Argument to --limit must be positive.*",
-                                                'log', '--limit', '0',
-                                                '--revision', '1',
-                                                svntest.main.current_repo_url,
-                                                'A/B')
+  svntest.actions.run_and_verify_svn(None, None, must_be_positive,
+                                     'log', '--limit', '0', '--revision', '1',
+                                     svntest.main.current_repo_url, 'A/B')
                                                 
-  svntest.actions.run_and_verify_svn(None, None, ".*Argument to --limit must be positive.*",
-                                                'log', '--limit', '-1',
-                                                '--revision', '1',
-                                                svntest.main.current_repo_url,
-                                                'A/B')
+  svntest.actions.run_and_verify_svn(None, None, must_be_positive,
+                                     'log', '--limit', '-1', '--revision', '1',
+                                     svntest.main.current_repo_url, 'A/B')
                                                                                                 
 def log_base_peg(sbox):
   "run log on an @BASE target"
