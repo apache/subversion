3aa992633a7fe883812345f187db44ecc752c945
danderson <danderson@unknown>
2006-05-31 18:26:07 +0000

    Merge r19700, r19753, r19829 from trunk to the 1.4.x branch.
    Error out for limit arguments that are less than or equal to zero,
    since they don't make sense, plus a test for it.
    
    Votes:
    +1: rooneg, lundblad, danderson
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/1.4.x@859951 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/log_tests.py
+++ subversion/tests/cmdline/log_tests.py
@@ -778,6 +778,17 @@ def log_limit(sbox):
   log_chain = parse_log_output (out)
   check_log_chain(log_chain, [1])
 
+  must_be_positive = ".*Argument to --limit must be positive.*"
+
+  # error expected when limit <= 0
+  svntest.actions.run_and_verify_svn(None, None, must_be_positive,
+                                     'log', '--limit', '0', '--revision', '1',
+                                     svntest.main.current_repo_url, 'A/B')
+                                                
+  svntest.actions.run_and_verify_svn(None, None, must_be_positive,
+                                     'log', '--limit', '-1', '--revision', '1',
+                                     svntest.main.current_repo_url, 'A/B')
+                                                                                                
 def log_base_peg(sbox):
   "run log on an @BASE target"
   guarantee_repos_and_wc(sbox)
