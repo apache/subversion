e77868fe229e398e67f43c6ba19f2f97276760e8
Paul Burba <pburba@apache.org>
2006-05-31 18:42:24 +0000

    Merge r19869, r19875 from /svn/trunk to /svn/branches/ebcdic/v5r4/1.3.x.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/ebcdic@859955 13f79535-47bb-0310-9956-ffa450edef68

--- v5r4/1.3.x/subversion/libsvn_repos/hooks.c
+++ v5r4/1.3.x/subversion/libsvn_repos/hooks.c
@@ -302,7 +302,10 @@ run_hook_cmd (const char *name,
       /* If read() returned 0 then EOF was found and we are done reading
        * stderr. */
       if (rc == 0)
-        break;
+        {
+          script_output->data[script_output->len] = '\0';
+          break;
+        }
     }
 
   /* Close the read end of the stderr pipe. */
