9c6803309f7da3b3d830337d876410e540a8d345
Max Oliver Bowsher <maxb@apache.org>
2006-06-05 20:55:00 +0000

    * doc/design/: Delete. The HTML version is now primary.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@860020 13f79535-47bb-0310-9956-ffa450edef68

--- doc/design/to-html.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-COMPONENT_XML_FILES="goals.xml model.xml architecture.xml deltas.xml "\
-"client.xml protocol.xml server.xml license.xml"
-perl -i.bak1~ -pe 's/&/¬/g;' $COMPONENT_XML_FILES
-xsltproc --output design.html to-html.xsl design.xml
-perl -i.bak2~ -pe 's/¬/&/g;' $COMPONENT_XML_FILES design.html
-rm -f *.bak[12]~
