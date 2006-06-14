e0aa16e220ed631ecd0cfc07d76c05ace1c1dad2
Garrett Rooney <rooneg@apache.org>
2006-06-14 19:20:16 +0000

    More test coverage for the svn.wc Python bindings.
    Patch by: Jelmer Vernooij <jelmer@samba.org>
    
    * subversion/bindings/swig/python/tests/wc.py
      (setUp): Acquire write lock on wc
      (tearDown): Explicitly close wc
      (test_lock): Add tests for add_lock and remove_lock
      (test_version): Add test for version()
      (test_is_adm_dir): Add test for is_adm_dir()
      (test_get_adm_dir): Add test for get_adm_dir()
      (test_set_adm_dir): Add test for set_adm_dir()
      (test_init_traversal_info): Add test for init_traversal_info()
      (test_create_notify): Add test for create_notify()
      (test_check_wc): Add test for test_wc()
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@860176 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/swig/python/tests/wc.py
+++ subversion/bindings/swig/python/tests/wc.py
@@ -1,6 +1,7 @@
 import unittest, os, tempfile
 
 from svn import core, repos, wc, client
+from libsvn.core import SubversionException
 
 from trac.versioncontrol.tests.svn_fs import SubversionRepositoryTestSetup, \
   REPOS_PATH
@@ -28,10 +29,53 @@ class SubversionRepositoryTestCase(unittest.TestCase):
     client.checkout2(self.repos_url, self.path, rev, rev, True, True, 
             client_ctx)
 
-    self.wc = wc.adm_open3(None, self.path, False, 0, None)
+    self.wc = wc.adm_open3(None, self.path, True, 0, None)
 
   def test_entry(self):
       wc_entry = wc.entry(self.path, self.wc, True)
+
+  def test_lock(self):
+      lock = wc.add_lock(self.path, core.svn_lock_create(core.Pool()), self.wc)
+      self.assertEqual(True, wc.adm_locked(self.wc))
+      self.assertEqual(True, wc.locked(self.path))
+      wc.remove_lock(self.path, self.wc)
+
+  def test_version(self):
+      wc.version()
+
+  def test_access_path(self):
+      self.assertEqual(self.path, wc.adm_access_path(self.wc))
+
+  def test_is_adm_dir(self):
+      self.assertTrue(wc.is_adm_dir(".svn"))
+      self.assertFalse(wc.is_adm_dir(".foosvn"))
+
+  def test_get_adm_dir(self):
+      self.assertTrue(isinstance(wc.get_adm_dir(), basestring))
+
+  def test_set_adm_dir(self):
+      self.assertRaises(SubversionException, wc.set_adm_dir, ".foobar")
+      self.assertTrue(wc.is_adm_dir(".svn"))
+      self.assertFalse(wc.is_adm_dir("_svn"))
+      self.assertFalse(wc.is_adm_dir(".foobar"))
+      wc.set_adm_dir("_svn")
+      self.assertTrue(wc.is_adm_dir("_svn"))
+      self.assertEqual("_svn", wc.get_adm_dir())
+      wc.set_adm_dir(".svn")
+      self.assertFalse(wc.is_adm_dir("_svn"))
+      self.assertEqual(".svn", wc.get_adm_dir())
+
+  def test_init_traversal_info(self):
+      wc.init_traversal_info()
+
+  def test_create_notify(self):
+      wc.create_notify(self.path, wc.notify_add)
+
+  def test_check_wc(self):
+      self.assertTrue(wc.check_wc(self.path) > 0)
+
+  def tearDown(self):
+      wc.adm_close(self.wc)
 
 def suite():
     return unittest.makeSuite(SubversionRepositoryTestCase, 'test',
