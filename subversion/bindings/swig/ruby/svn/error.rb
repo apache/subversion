42387525b28b010c3946f98b8dd767874396d4da
kou <dev-null@apache.org>
2006-07-06 13:36:08 +0000

    Fix the Ruby bindings test failures caused in r20336.
    * subversion/bindings/swig/ruby/svn/error.rb:
      Ignore SVN_ERR_*_CATEGORY_START.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@860521 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/swig/ruby/svn/error.rb
+++ subversion/bindings/swig/ruby/svn/error.rb
@@ -7,15 +7,17 @@ module Svn
 
     Ext::Core.constants.each do |const_name|
       if /^SVN_ERR_(.*)/ =~ const_name
+        error_const_name = $1
+        next if /_CATEGORY_START\z/ =~ error_const_name
         value = Ext::Core.const_get(const_name)
-        module_eval(<<-EOC, __FILE__, __LINE__)
-          class #{$1} < Error
+        module_eval(<<-EOC, __FILE__, __LINE__ + 1)
+          class #{error_const_name} < Error
             def initialize(message="", file=nil, line=nil)
               super(#{value}, message, file, line)
             end
           end
         EOC
-        TABLE[value] = const_get($1)
+        TABLE[value] = const_get(error_const_name)
       end
     end
     
