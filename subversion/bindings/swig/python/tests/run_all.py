80d14249c87f483febd466792489fde495a64b0c
Max Oliver Bowsher <maxb@apache.org>
2006-07-12 19:29:56 +0000

    Python bindings: abstract the path setup code into a separate file, to facilitate starting an interactive interpreter using the uninstalled bindings.
    * subversion/bindings/swig/python/tests/setup_path.py:
      New file, copied from run_all.py, taking just the path setup parts.
      
    * subversion/bindings/swig/python/tests/run_all.py:
      Just import setup_path.py. Also, re-order the import statements to be in the
      same order as the suite.addTest() calls.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@860676 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/swig/python/tests/run_all.py
+++ subversion/bindings/swig/python/tests/run_all.py
@@ -1,25 +1,14 @@
-import sys, os
-bindir = os.path.dirname(sys.argv[0])
-sys.path[0:0] = [ bindir, os.getcwd(), "%s/.libs" % os.getcwd(), \
-                  "%s/.." % bindir, "%s/../.libs" % bindir ]
-
-# OSes without RPATH support are going to have to do things here to make
-# the correct shared libraries be found.
-if sys.platform == 'cygwin':
-  import glob
-  svndir = os.path.dirname(os.path.dirname(os.path.dirname(os.getcwd())))
-  libpath = os.getenv("PATH").split(":")
-  libpath.insert(0, "%s/libsvn_swig_py/.libs" % os.getcwd())
-  for libdir in glob.glob("%s/libsvn_*" % svndir):
-    libpath.insert(0, "%s/.libs" % (libdir))
-  os.putenv("PATH", ":".join(libpath))
-
+import sys
+import os
 import unittest
-import pool
-import repository
+
+import setup_path
+
 import client
+import pool
 import ra
 import wc
+import repository
 import trac.versioncontrol.tests
 
 # Run all tests
