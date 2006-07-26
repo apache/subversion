b7f9ac4adf30d53cad4a7012fdc29bd349781b04
Daniel Rall <dlr@apache.org>
2006-07-26 22:03:42 +0000

    On the merge-tracking branch: Attempt to clarify the doc string for an API in the libsvn_client-private header file.
    * subversion/libsvn_client/client.h
      (svn_client__path_relative_to_root): Be more explicit about the need
       for a non-null REPOS_ROOT or RA_SESSION param.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/merge-tracking@860947 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/client.h
+++ subversion/libsvn_client/client.h
@@ -187,9 +187,9 @@ svn_client__ra_session_from_path(svn_ra_session_t **ra_session_p,
    (REPOS_ROOT) in REL_PATH (URI-decoded).
 
    The remaining parameters are used to procure the repository root.
-   Either REPOS_ROOT or RA_SESSION may be NULL, but REPOS_ROOT or
-   ADM_ACCESS (which may also be NULL) should be passed when available
-   as an optimization (in that order of preference). */
+   Either REPOS_ROOT or RA_SESSION -- but not both -- may be NULL.
+   REPOS_ROOT or ADM_ACCESS (which may also be NULL) should be passed
+   when available as an optimization (in that order of preference). */
 svn_error_t *
 svn_client__path_relative_to_root(const char **rel_path,
                                   const char *path_or_url,
