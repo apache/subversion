c90a3baac01afdb16cf507c72ff9c87253a658cb
Bhuvaneswaran A <bhuvan@apache.org>
2006-08-25 15:15:13 +0000

    Approved by: cmpilato
    De-"string"-ify the script.
    
    * contrib/hook-scripts/check-case-insensitive.py
      (global): Do not import "string" library.
      (Editor.add_directory, Editor.add_file, CheckCase.showfile): Fix uses
      of deprecated string methods.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@861335 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/hook-scripts/check-case-insensitive.py
+++ contrib/hook-scripts/check-case-insensitive.py
@@ -60,7 +60,6 @@ if SVNLIB_DIR:
   sys.path.insert(0, SVNLIB_DIR)
 
 import os.path
-import string
 from svn import fs, core, repos, delta
 
 # Set this True for debug output.
@@ -183,7 +182,7 @@ class Editor(delta.Editor):
 
 
   def add_directory(self, path, *args):
-    lpath = string.lower(path.decode("utf-8"))
+    lpath = path.decode("utf-8").lower()
     if self.paths.has_key(lpath):
       self.paths[lpath] += 1
     else:
@@ -193,7 +192,7 @@ class Editor(delta.Editor):
   open_directory = add_directory
 
   def add_file(self, path, *args):
-    lpath = string.lower(path.decode("utf-8"))
+    lpath = path.decode("utf-8").lower()
     if self.paths.has_key(lpath):
       self.paths[lpath] += 1
     else:
@@ -288,10 +287,10 @@ class CheckCase:
     else:
       changedpath = changedroot + '/' + path
     for added in changed.added:
-      if (string.lower(added) == string.lower(changedpath)):
+      if (added.lower() == changedpath.lower()):
         return added
     for added in changed.addeddir:
-      if (string.lower(added) == string.lower(changedpath)):
+      if (added.lower() == changedpath.lower()):
         return added
     return ''
 
