d9c7f35b2d2e05927cad2168a84172fb4e83f7a2
Justin Erenkrantz <jerenkrantz@apache.org>
2006-08-25 16:26:11 +0000

    Update copyright years to reflect reality.
    * build/get-py-info.py:
      Update copyright year based on files actual history.
    
    Suggested by: Karl Fogel
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@861337 13f79535-47bb-0310-9956-ffa450edef68

--- build/get-py-info.py
+++ build/get-py-info.py
@@ -3,7 +3,7 @@
 #
 ######################################################################
 #
-# Copyright (c) 2000-2004 CollabNet.  All rights reserved.
+# Copyright (c) 2002-2006 CollabNet.  All rights reserved.
 #
 # This software is licensed as described in the file COPYING, which
 # you should have received as part of this distribution.  The terms
