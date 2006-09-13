441d262b14c20f714088a95589e9de56c58b54c7
Paul Burba <pburba@apache.org>
2006-09-13 14:29:38 +0000

    Follow-up to r21406 - Fix [bdb] x [dav] test failure in prop_test.py 7
    * subversion/tests/cmdline/prop_tests.py
      (commit_conflict_dirprops): Tweak reg exp to also handle DAV variant
      of expected error message.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@861553 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/prop_tests.py
+++ subversion/tests/cmdline/prop_tests.py
@@ -377,7 +377,9 @@ def commit_conflict_dirprops(sbox):
   svntest.main.run_svn(None, 'propset', 'foo', 'eek', wc_dir)
 
   svntest.actions.run_and_verify_commit(wc_dir, None, None,
-                                        "Out of date: '' in transaction",
+                                        "(Your file or directory '.*' is " \
+                                        "probably out-of-date)|" \
+                                        "(Out of date: '' in transaction)",
                                         None, None, None, None,
                                         wc_dir)
 
