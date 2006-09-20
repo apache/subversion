c9e97379f4163d70311d488f0e9964dd06d1e104
Daniel Rall <dlr@apache.org>
2006-09-20 21:47:17 +0000

    On the merge-tracking branch: Merge r21564 and r21570 from trunk, fixing compilation with pre-1.3.0 APR.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/merge-tracking@861646 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/hash.c
+++ subversion/libsvn_subr/hash.c
@@ -437,7 +437,9 @@ svn_hash_diff(apr_hash_t *hash_a,
 svn_error_t *
 svn_hash_clear(apr_hash_t *hash)
 {
-#if APR_VERSION_AT_LEAST(1, 3, 0)
+/* The APR_VERSION_AT_LEAST() macro is new in APR 1.3.0, which also
+   happens to be the minimum version we require. */
+#ifdef APR_VERSION_AT_LEAST /* 1.3.0 */
   apr_hash_clear(hash);
 #else
   apr_hash_index_t *hi;
