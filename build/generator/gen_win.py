2db13f48a29c5c74efef47b6a76017b760fd820e
djh <djh@unknown>
2006-09-21 01:35:52 +0000

    Add sqlite support to Windows build on merge tracking branch.
    * build/generator/gen_win.py
      (parse_options): Look for --with-sqlite option.
      (get_win_includes): Add sqlite inc directory to include directories.
      (get_win_lib_dirs): Add sqlite lib directory to library directories.
      (get_win_libs): Add sqlite library dependency as needed.
    
    * gen-make.py
      Add --with-sqlite option for Windows build.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/merge-tracking@861656 13f79535-47bb-0310-9956-ffa450edef68

--- build/generator/gen_win.py
+++ build/generator/gen_win.py
@@ -51,6 +51,7 @@ class WinGeneratorBase(GeneratorBase):
     self.swig_path = None
     self.vsnet_version = '7.00'
     self.vsnet_proj_ver = '7.00'
+    self.sqlite_path = None
     self.skip_sections = { 'mod_dav_svn': None,
                            'mod_authz_svn': None }
 
@@ -91,6 +92,8 @@ class WinGeneratorBase(GeneratorBase):
         self.zlib_path = val
       elif opt == '--with-swig':
         self.swig_path = val
+      elif opt == '--with-sqlite':
+        self.sqlite_path = val
       elif opt == '--with-sasl':
         self.sasl_path = val
       elif opt == '--with-openssl':
@@ -667,7 +670,10 @@ class WinGeneratorBase(GeneratorBase):
             or isinstance(target, gen_base.TargetSWIGLib)):
       fakeincludes.append(self.swig_libdir)
 
-    fakeincludes.append(self.apath(self.zlib_path))
+    fakeincludes.extend([
+                         self.apath(self.zlib_path),
+                         self.apath(self.sqlite_path, 'inc')
+                         ])
 
     if self.sasl_path:
       fakeincludes.append(self.apath(self.sasl_path, 'include'))
@@ -682,7 +688,8 @@ class WinGeneratorBase(GeneratorBase):
 
     fakelibdirs = [ self.apath(self.bdb_path, "lib"),
                     self.apath(self.neon_path),
-                    self.apath(self.zlib_path) ]
+                    self.apath(self.zlib_path),
+                    self.apath(self.sqlite_path, "lib") ]
     if self.sasl_path:
       fakelibdirs.append(self.apath(self.sasl_path, "lib"))
     if isinstance(target, gen_base.TargetApacheMod):
@@ -736,6 +743,9 @@ class WinGeneratorBase(GeneratorBase):
 
       if dep.external_lib == '$(NEON_LIBS)':
         nondeplibs.append(neonlib)
+        
+      if dep.external_lib == '$(SVN_SQLITE_LIBS)':
+        nondeplibs.append('sqlite3.lib')
         
       if dep.external_lib == '$(SVN_SASL_LIBS)':
         nondeplibs.append(sasllib)
