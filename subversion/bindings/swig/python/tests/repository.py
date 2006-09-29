b7c0584c80a47e2c81389b6848023a6d49136c08
djames <dev-null@apache.org>
2006-09-29 22:48:54 +0000

    * subversion/bindings/swig/python/tests/repository.py   (test_dump_fs2): New test case.
    Patch by: me
              Jeremy Whitlock <jcscoobyrs@gmail.com>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@861793 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/swig/python/tests/repository.py
+++ subversion/bindings/swig/python/tests/repository.py
@@ -1,5 +1,6 @@
-import unittest, os, setup_path
+import unittest, os, setup_path, StringIO
 from svn import core, repos, fs, delta
+import _core
 
 from trac.versioncontrol.tests.svn_fs import SubversionRepositoryTestSetup, \
   REPOS_PATH
@@ -34,7 +35,51 @@ class SubversionRepositoryTestCase(unittest.TestCase):
     for i in range(5):
       path = os.path.join(REPOS_PATH, "test" + str(i))
       repos.create(path, "", "", None, fs_config)
+
+  def test_dump_fs2(self):
+    """Test the dump_fs2 function"""
+
+    self.callback_calls = 0
+
+    def is_cancelled():
+      self.callback_calls += 1
+      return None
+
+    dumpstream = StringIO.StringIO()
+    feedbackstream = StringIO.StringIO()
+    repos.dump_fs2(self.repos, dumpstream, feedbackstream, 0, self.rev, 0, 0,
+                   is_cancelled)
+
+    # Check that we can dump stuff
+    dump = dumpstream.getvalue()
+    feedback = feedbackstream.getvalue()
+    expected_feedback = "* Dumped revision " + str(self.rev)
+    self.assert_("Node-path: trunk/README.txt" in dump)
+    self.assert_(expected_feedback in feedback)
+    self.assertEquals(self.callback_calls, 13)
+
+    # Check that the dump can be cancelled
+    self.assertRaises(_core.SubversionException, repos.dump_fs2,
+      self.repos, dumpstream, feedbackstream, 0, self.rev, 0, 0, lambda: 1)
+
+    dumpstream.close()
+    feedbackstream.close()
    
+    # Check that the dump fails when the dumpstream is closed
+    self.assertRaises(ValueError, repos.dump_fs2,
+      self.repos, dumpstream, feedbackstream, 0, self.rev, 0, 0, None)
+
+    # Check that the dump fails when we pass in None as the dumpstream or
+    # feedbackstream
+    # FIXME: We should probably return a ValueError instead, and say that
+    # the function does not accept NULL streams
+    self.assertRaises(AttributeError, repos.dump_fs2,
+      self.repos, None, None, 0, self.rev, 0, 0, None)
+
+    # FIXME: The Python bindings don't check for 'NULL' values for
+    #        svn_repos_t objects, so the following call segfaults
+    #repos.dump_fs2(None, None, None, 0, self.rev, 0, 0, None)
+
   def test_get_logs(self):
     """Test scope of get_logs callbacks"""
     logs = []
