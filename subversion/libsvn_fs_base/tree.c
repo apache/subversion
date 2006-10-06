ff42b4ae17d7738f410c128b32d2e6d3ba70c891
Daniel Rall <dlr@apache.org>
2006-10-06 22:49:46 +0000

    * subversion/libsvn_fs_base/tree.c   (base_dir_entries): Avoid unnecessary use of a sub-pool.
    Found by: philip
    Review by: philip
               rooneg
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@861892 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_base/tree.c
+++ subversion/libsvn_fs_base/tree.c
@@ -1465,8 +1465,7 @@ base_dir_entries(apr_hash_t **table_p,
   if (table)
     {
       apr_hash_index_t *hi;
-      apr_pool_t *subpool = svn_pool_create(pool);
-      for (hi = apr_hash_first(subpool, table); hi; hi = apr_hash_next(hi))
+      for (hi = apr_hash_first(pool, table); hi; hi = apr_hash_next(hi))
         {
           svn_fs_dirent_t *entry;
           struct node_kind_args nk_args;
