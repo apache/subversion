9bf261d33d702466d15bccc38fa67855fc172608
Justin Erenkrantz <jerenkrantz@apache.org>
2006-11-04 19:28:32 +0000

    ra_serf: Correctly retrieve the comment field from the lock.
    Now passes lock_tests #21 (examine the fields of a lock from a URL).
    
    * subversion/libsvn_ra_serf/locks.c
      (lock_state_e): Add COMMENT state.
      (lock_info_t): Remove owner, creation_date, lock_token, comment as they
      are now unused fields.
      (push_state, cdata_lock): Add COMMENT state.
      (start_lock): Map 'owner' DAV field to the COMMENT state.
      (end_lock): Fill the ->comment field in the lock.
      (create_lock_body): Use ->lock->comment instead of ->comment.
      (svn_ra_serf__lock): Do not set ->comment.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@862290 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_ra_serf/locks.c
+++ subversion/libsvn_ra_serf/locks.c
@@ -52,6 +52,7 @@ typedef enum {
   DEPTH,
   TIMEOUT,
   LOCK_TOKEN,
+  COMMENT,
 } lock_state_e;
 
 typedef struct {
@@ -65,11 +66,6 @@ typedef struct {
   const char *path;
 
   svn_lock_t *lock;
-
-  const char *owner;
-  const char *creation_date;
-  const char *lock_token;
-  const char *comment;
 
   svn_boolean_t force;
   svn_revnum_t revision;
@@ -102,6 +98,7 @@ push_state(svn_ra_serf__xml_parser_t *parser,
     case DEPTH:
     case TIMEOUT:
     case LOCK_TOKEN:
+    case COMMENT:
         parser->state->private = apr_pcalloc(parser->state->pool,
                                              sizeof(lock_prop_info_t));
         break;
@@ -161,6 +158,10 @@ start_lock(svn_ra_serf__xml_parser_t *parser,
       else if (strcmp(name.name, "locktoken") == 0)
         {
           push_state(parser, ctx, LOCK_TOKEN);
+        }
+      else if (strcmp(name.name, "owner") == 0)
+        {
+          push_state(parser, ctx, COMMENT);
         }
     }
   else if (state == LOCK_TYPE)
@@ -261,6 +262,17 @@ end_lock(svn_ra_serf__xml_parser_t *parser,
       /* We don't actually need the lock token. */
       svn_ra_serf__xml_pop_state(parser);
     }
+  else if (state == COMMENT &&
+           strcmp(name.name, "owner") == 0)
+    {
+      lock_prop_info_t *info = parser->state->private;
+
+      if (info->len)
+        {
+          ctx->lock->comment = apr_pstrndup(ctx->pool, info->data, info->len);
+        }
+      svn_ra_serf__xml_pop_state(parser);
+    }
 
   return SVN_NO_ERROR;
 }
@@ -285,6 +297,7 @@ cdata_lock(svn_ra_serf__xml_parser_t *parser,
     case DEPTH:
     case TIMEOUT:
     case LOCK_TOKEN:
+    case COMMENT:
         svn_ra_serf__expand_string(&info->data, &info->len,
                                    data, len, parser->state->pool);
         break;
@@ -437,13 +450,13 @@ create_lock_body(void *baton,
 
   svn_ra_serf__add_tag_buckets(buckets, "locktype", "<write/>", alloc);
 
-  if (ctx->comment)
+  if (ctx->lock->comment)
     {
       svn_stringbuf_t *xml_esc = NULL;
       svn_string_t val;
 
-      val.data = ctx->comment;
-      val.len = strlen(ctx->comment);
+      val.data = ctx->lock->comment;
+      val.len = strlen(ctx->lock->comment);
 
       svn_xml_escape_cdata_string(&xml_esc, &val, pool);
       svn_ra_serf__add_tag_buckets(buckets, "owner", xml_esc->data, alloc);
@@ -551,7 +564,6 @@ svn_ra_serf__lock(svn_ra_session_t *ra_session,
       lock_ctx->path = key;
       lock_ctx->revision = *((svn_revnum_t*)val);
       lock_ctx->lock = svn_lock_create(subpool);
-      lock_ctx->comment = comment;
       lock_ctx->lock->path = key;
       lock_ctx->lock->comment = comment;
 
