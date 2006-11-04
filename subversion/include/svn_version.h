e35ebe22163ed227f56b08b8788a330b01f0b829
Max Oliver Bowsher <maxb@apache.org>
2006-11-04 12:00:43 +0000

    Tagging release 1.4.2 with svn_version.h matching tarball.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/tags/1.4.2@862284 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_version.h
+++ subversion/include/svn_version.h
@@ -66,7 +66,7 @@ extern "C" {
  * 
  * @since New in 1.1.
  */
-#define SVN_VER_PATCH      1
+#define SVN_VER_PATCH      2
 
 
 /** @deprecated Provided for backward compatibility with the 1.0 API. */
@@ -89,7 +89,7 @@ extern "C" {
  *
  * Always change this at the same time as SVN_VER_NUMTAG.
  */
-#define SVN_VER_TAG        " (dev build)"
+#define SVN_VER_TAG        " (r22196)"
 
 
 /** Number tag: a string describing the version.
@@ -104,7 +104,7 @@ extern "C" {
  *
  * Always change this at the same time as SVN_VER_TAG.
  */
-#define SVN_VER_NUMTAG     "-dev"
+#define SVN_VER_NUMTAG     ""
 
 
 /** Revision number: The repository revision number of this release.
@@ -115,7 +115,7 @@ extern "C" {
  * When rolling a tarball, we automatically replace it with what we
  * guess to be the correct revision number.
  */
-#define SVN_VER_REVISION   0
+#define SVN_VER_REVISION   22196
 
 
 /* Version strings composed from the above definitions. */
