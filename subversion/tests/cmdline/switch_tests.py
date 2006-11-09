55e8a4621af8d1022ca7920b6d3c8b7b9d295398
Lieven Govaerts <lgo@apache.org>
2006-11-09 23:31:59 +0000

    Fixed incorrect test which was failing on the buildbot.
    * subversion/tests/cmdline/switch_tests.py
      (relocate_and_propset):  when copying the repos, copy from a 
       revision that actually exists (r1).
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@862325 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/switch_tests.py
+++ subversion/tests/cmdline/switch_tests.py
@@ -1094,7 +1094,7 @@ def relocate_and_propset(sbox):
 
   # Relocate
   other_repo_dir, other_repo_url = sbox.add_repo_path('other')
-  svntest.main.copy_repos(repo_dir, other_repo_dir, 2)
+  svntest.main.copy_repos(repo_dir, other_repo_dir, 1)
   svntest.main.safe_rmtree(repo_dir, 1)
   svntest.actions.run_and_verify_svn(None, None, [], 'switch', '--relocate',
                                      repo_url, other_repo_url, wc_dir)
