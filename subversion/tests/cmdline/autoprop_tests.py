1781bff66458e64d0412c14d9223314c5bf48c8c
Lieven Govaerts <lgo@apache.org>
2006-11-11 21:45:55 +0000

    Prepare the python tests to be run in parallel: Make sure that any resources we create during test setup are exclusively available to that test.
    * subversion/tests/cmdline/autoprop_tests.py
      (autoprops_test): make the config files exclusive to the test by adding
       the name of the test in the folder name.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@862354 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/autoprop_tests.py
+++ subversion/tests/cmdline/autoprop_tests.py
@@ -100,7 +100,7 @@ def autoprops_test(sbox, cmd, cfgenable, clienable, subdir):
   # some directories
   wc_dir = sbox.wc_dir
   tmp_dir = os.path.abspath(svntest.main.temp_dir)
-  config_dir = os.path.join(tmp_dir, 'autoprops_config')
+  config_dir = os.path.join(tmp_dir, 'autoprops_config_' + sbox.name)
   repos_url = sbox.repo_url
 
   # initialize parameters
