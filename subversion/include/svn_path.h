370ebaab315d970ecdb152d881e8e703c1224544
Daniel Rall <dlr@apache.org>
2006-11-28 17:09:47 +0000

    A follow-up to r21277 documenting when the svn_path_is_root() API was introduced.
    * subversion/include/svn_path.h
      (svn_path_is_root): Add @since tag.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@862553 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_path.h
+++ subversion/include/svn_path.h
@@ -180,6 +180,8 @@ int svn_path_is_empty(const char *path);
 /** Return TRUE if @a path is considered a root path on the platform at 
  * hand, amongst which '/' on all platforms or 'X:/', '\\\\?\\X:/', 
  * '\\\\.\\..', '\\\\server\\share' on Windows.
+ *
+ * @since New in 1.5.
  */
 svn_boolean_t svn_path_is_root(const char *path, apr_size_t len);
 
