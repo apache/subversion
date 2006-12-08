5498be5e6d661b558b4741ea2e0d0ff08d9cff74
C. Michael Pilato <cmpilato@apache.org>
2006-12-08 16:16:30 +0000

    Eliminate multiple instances of a rather annoying and roundabout way of asking for the base revision of a transaction root (get-base-rev-from-txn-from-name-from-txn-root) by introducing a new public API that does it all -- svn_fs_txn_root_base_revision(). Update places in the code that use the roundabout way so that they do so no longer.
    * subversion/include/svn_fs.h
      (svn_fs_txn_root_base_revision): New.
    
    * subversion/libsvn_fs/fs-loader.h
      (struct svn_fs_root_t): Update comment (and usage) of 'rev' member.
    
    * subversion/libsvn_fs/fs-loader.c
      (svn_fs_txn_root_base_revision): New.
    
    * subversion/libsvn_fs_fs/tree.c
      (make_txn_root): Add 'base_rev' parameter, and use it to populate
        the root's 'rev' member.
      (svn_fs_fs__txn_root): Update call to make_txn_root().
    
    * subversion/libsvn_fs_base/tree.c
      (make_txn_root): Add 'base_rev' parameter, and use it to populate
        the root's 'rev' member.
      (txn_body_txn_root): Update call to make_txn_root().
    
    * subversion/mod_dav_svn/authz.c
      (authz_read): Use new svn_fs_txn_root_base_revision() instead of the
        roundabout way.
    
    * subversion/libsvn_repos/delta.c
      (svn_repos_dir_delta): Use new svn_fs_txn_root_base_revision() instead of the
        roundabout way.
    
    * subversion/tests/libsvn_fs/fs-test.c
      (root_revisions): New test for svn_fs_txn_root_base_revision() and
        svn_fs_revision_root_revision().
      (test_funcs): Add reference to new test.
    
    * subversion/libsvn_repos/replay.c
      (svn_repos_replay2): use new svn_fs_txn_root_base_revision() instead
        of the roundabout way.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@862684 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/mod_dav_svn/authz.c
+++ subversion/mod_dav_svn/authz.c
@@ -143,12 +143,7 @@ authz_read(svn_boolean_t *allowed,
          revision is the same as its path in this txn. */
       if ((rev == SVN_INVALID_REVNUM) && (revpath == NULL))
         {
-          const char *txn_name;
-          svn_fs_txn_t *txn;
-
-          txn_name = svn_fs_txn_root_name(root, pool);
-          SVN_ERR(svn_fs_open_txn(&txn, svn_fs_root_fs(root), txn_name, pool));
-          rev = svn_fs_txn_base_revision(txn);
+          rev = svn_fs_txn_root_base_revision(root);
           revpath = path;
         }
     }
