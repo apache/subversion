e1b6a75222177930c8fc4b4118a9e6ce76942674
madanus <madanus@unknown>
2006-12-29 05:10:37 +0000

    On the incomplete-directories branch: Fix default depth for the info command.
    * subversion/svn/info-cmd.c
      (svn_cl__info): The default depth for the info command is one, and not
       infinity as r21829 thought.
    
    Approved by: jerenkrantz
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/incomplete-directories@862905 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svn/info-cmd.c
+++ subversion/svn/info-cmd.c
@@ -561,7 +561,7 @@ svn_cl__info(apr_getopt_t *os,
     }
   
   if (opt_state->depth == svn_depth_unknown)
-    opt_state->depth = svn_depth_infinity;
+    opt_state->depth = svn_depth_one;
 
   for (i = 0; i < targets->nelts; i++)
     {
