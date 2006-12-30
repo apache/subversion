5b5d238eadae096b4ec33f7f54a22348bd06d03d
Kouhei Sutou <kou@apache.org>
2006-12-30 06:01:39 +0000

    Support preparing test environment for non-installation test on Windows.
    * subversion/bindings/swig/ruby/test/run-test.rb: Use copy instead of
      symbolic link on Windows.
    
    Suggested by: Joe Swatosh
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@862911 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/swig/ruby/test/run-test.rb
+++ subversion/bindings/swig/ruby/test/run-test.rb
@@ -3,15 +3,27 @@
 require "test/unit"
 require "fileutils"
 
-ENV["PATH"] = File.join(Dir.pwd, "..", "..", "..", "svnserve") + ":" + ENV["PATH"]
-ext_dir = File.join(Dir.pwd, ".ext")
+test_dir = File.expand_path(File.join(File.dirname(__FILE__)))
+base_dir = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+top_dir = File.expand_path(File.join(base_dir, "..", "..", ".."))
+
+ext_dir = File.join(base_dir, ".ext")
 ext_svn_dir = File.join(ext_dir, "svn")
 FileUtils.mkdir_p(ext_svn_dir)
-FileUtils.ln_sf(File.join(Dir.pwd, ".libs"), File.join(ext_svn_dir, "ext"))
 at_exit {FileUtils.rm_rf(ext_dir)}
 
+if /cygwin|mingw|mswin32|bccwin32/.match(RUBY_PLATFORM)
+  ext_svn_ext_dir = File.join(ext_svn_dir, "ext")
+  FileUtils.mkdir_p(ext_svn_ext_dir)
+  FileUtils.cp(Dir.glob(File.join(base_dir, "*.dll"), ext_svn_ext_dir))
+else
+  ENV["PATH"] = "#{File.join(top_dir, 'subversion', 'svnserve')}:#{ENV['PATH']}"
+  FileUtils.ln_sf(File.join(base_dir, ".libs"), File.join(ext_svn_dir, "ext"))
+end
+
 $LOAD_PATH.unshift(ext_dir)
-$LOAD_PATH.unshift(Dir.pwd)
+$LOAD_PATH.unshift(base_dir)
+$LOAD_PATH.unshift(test_dir)
 
 require 'svn/core'
 Svn::Locale.set
