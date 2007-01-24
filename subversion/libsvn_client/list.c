db2d7a5269d443ce0d002d7c983b4ff1045ef917
Daniel Rall <dlr@apache.org>
2007-01-24 22:21:18 +0000

    A follow-up to r23204, fixing breakage in repos_diff.c, and tweaking usage of sub-pools.
    [ in subversion/libsvn_client/ ]
    
    * repos_diff.c
      (svn_client__get_diff_editor): Don't destroy local variable
       "subpool", as it's managed by our edit baton.
    
    * externals.c
      (switch_external): Add inline comment asking why we don't pass a
       sub-pool to svn_client__update_internal().
    
    * list.c
      (get_dir_contents): Rename local variable "subpool" to "iterpool",
       as per our conventions.
    
    Found by: lgo
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@863296 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/list.c
+++ subversion/libsvn_client/list.c
@@ -51,7 +51,7 @@ get_dir_contents(apr_uint32_t dirent_fields,
                  apr_pool_t *pool)
 {
   apr_hash_t *tmpdirents;
-  apr_pool_t *subpool = svn_pool_create(pool);
+  apr_pool_t *iterpool = svn_pool_create(pool);
   apr_array_header_t *array;
   int i;
 
@@ -71,27 +71,27 @@ get_dir_contents(apr_uint32_t dirent_fields,
       svn_dirent_t *the_ent = apr_hash_get(tmpdirents, item->key, item->klen);
       svn_lock_t *lock;
 
-      svn_pool_clear(subpool);
+      svn_pool_clear(iterpool);
 
-      path = svn_path_join(dir, item->key, subpool);
+      path = svn_path_join(dir, item->key, iterpool);
 
       if (locks)
         {
-          const char *abs_path = svn_path_join(fs_path, path, subpool);
+          const char *abs_path = svn_path_join(fs_path, path, iterpool);
           lock = apr_hash_get(locks, abs_path, APR_HASH_KEY_STRING);
         }
       else
         lock = NULL;
 
-      SVN_ERR(list_func(baton, path, the_ent, lock, fs_path, subpool));
+      SVN_ERR(list_func(baton, path, the_ent, lock, fs_path, iterpool));
 
       if (recurse && the_ent->kind == svn_node_dir)
         SVN_ERR(get_dir_contents(dirent_fields, path, rev,
                                  ra_session, locks, fs_path, recurse, ctx,
-                                 list_func, baton, subpool));
+                                 list_func, baton, iterpool));
     }
 
-  svn_pool_destroy(subpool);
+  svn_pool_destroy(iterpool);
   return SVN_NO_ERROR;
 }
 
