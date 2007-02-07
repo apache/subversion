3a29817403c87b8136de4c1daccc16ecb7115bb2
Blair Zajac <blair@apache.org>
2007-02-07 05:59:20 +0000

    * subversion/tests/cmdline/svnserveautocheck.sh:   Remove unnecessary single quotes.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@863436 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/svnserveautocheck.sh
+++ subversion/tests/cmdline/svnserveautocheck.sh
@@ -64,7 +64,7 @@ $SERVER_CMD -d -r $ABS_BUILDDIR/subversion/tests/cmdline \
             --listen-host 127.0.0.1 \
             --pid-file $SVNSERVE_PID &
 
-time make check 'BASE_URL=svn://127.0.0.1'
+time make check BASE_URL=svn://127.0.0.1
 r=$?
 
 really_cleanup
