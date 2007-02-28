f3a530f949581a93daf6e6048bf4113be26a65f5
Paul Burba <pburba@apache.org>
2007-02-28 00:53:17 +0000

    On the merge-tracking branch: Minor docstring fix
    * subversion/include/svn_repos.h
      (svn_repos_fs_get_merge_info): Fix docstring to match actual argument name.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/merge-tracking@863584 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_repos.h
+++ subversion/include/svn_repos.h
@@ -1060,7 +1060,7 @@ svn_repos_get_logs(svn_repos_t *repos,
 
 /**
  * Fetch the merge info for @a paths at @rev, and save it to @a
- * mergeinfo.  @a mergeinfo is a mapping of @c char * target paths
+ * mergeoutput.  @a mergeoutput is a mapping of @c char * target paths
  * (from @a paths) to textual (@c char *) representations of merge
  * info (as managed by svn_mergeinfo.h), or @c NULL if there is no
  * merge info visible or available.
