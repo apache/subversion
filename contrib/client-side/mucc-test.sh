0fee16c05d0203154ad5ab1a867f0bdc423d8b5b
C. Michael Pilato <cmpilato@apache.org>
2007-03-13 20:26:38 +0000

    More bug fixes for MUCC, and some tweaks to its test/exercise script.
    * contrib/client-side/mucc.c
      (get_operation): Make operation kinds default to 'svn_node_dir' (for 
        the sake getting OP_OPEN to work as expected).
      (execute): Correct the total bustedness of the "mv" operation
        handling I committed recently.
    
    * contrib/client-side/mucc-test.sh
      Update this test script to use -U (for shorter command-lines) and to
      use MUCC to do the initial file and directory creation that it
      didn't used to be able to do.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@863869 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/client-side/mucc-test.sh
+++ contrib/client-side/mucc-test.sh
@@ -5,8 +5,8 @@
 SVN=subversion/svn/svn
 MUCC=contrib/client-side/mucc
 SL=subversion/svnlook/svnlook
-#REPO=file://`pwd`/repo
-REPO=svn://localhost/repo
+REPO=file://`pwd`/repo
+#REPO=svn://localhost/repo
 #REPO=http://localhost:8888/obj/repo
 
 rm -rf repo
@@ -19,63 +19,78 @@ echo [general] >> repo/conf/svnserve.conf
 echo passwd-db = passwd >> repo/conf/svnserve.conf
 echo anon-access = write >> repo/conf/svnserve.conf
 
-$SVN mkdir -m "a directory" $REPO/foo
-$SVN import repo/README.txt -m "a file" $REPO/z.c
-$SL tree --full-paths repo /
-
 function stat()
 {
-  $SVN log -vrhead $REPO
+  $SVN log -vqrhead $REPO
   $SL tree --full-paths repo /
 }
 
-$MUCC cp 2 $REPO/z.c $REPO/foo/z.c \
-      cp 2 $REPO/foo $REPO/foo/bar
+
+$MUCC -U $REPO \
+      mkdir foo
 stat
 
-$MUCC cp 3 $REPO/foo $REPO/zig \
-      rm   $REPO/zig/bar \
-      mv   $REPO/foo $REPO/zig/zag
+$MUCC -U $REPO \
+      put /dev/null z.c
 stat
 
-$MUCC mv   $REPO/z.c $REPO/zig/zag/bar/y.c \
-      cp 2 $REPO/z.c $REPO/zig/zag/bar/x.c
+$MUCC -U $REPO \
+      cp 2 z.c foo/z.c \
+      cp 2 foo foo/bar
 stat
 
-$MUCC mv      $REPO/zig/zag/bar/y.c $REPO/zig/zag/bar/y%20y.c \
-      cp head $REPO/zig/zag/bar/y.c $REPO/zig/zag/bar/y%2520y.c 
+$MUCC -U $REPO \
+      cp 3 foo zig \
+      rm   zig/bar \
+      mv   foo zig/zag
 stat
 
-$MUCC mv      $REPO/zig/zag/bar/y%20y.c   $REPO/zig/zag/bar/z\ z1.c \
-      cp head $REPO/zig/zag/bar/y%2520y.c $REPO/zig/zag/bar/z%2520z.c \
-      cp head $REPO/zig/zag/bar/y\ y.c    $REPO/zig/zag/bar/z\ z2.c
+$MUCC -U $REPO \
+      mv   z.c zig/zag/bar/y.c \
+      cp 2 z.c zig/zag/bar/x.c
 stat
 
-$MUCC mv   $REPO/zig/zag $REPO/zig/foo \
-      rm   $REPO/zig/foo/bar/z\ z1.c \
-      rm   $REPO/zig/foo/bar/z%20z2.c \
-      rm   $REPO/zig/foo/bar/z%2520z.c \
-      cp 5 $REPO/zig/zag/bar/x.c $REPO/zig/foo/bar/z%20z1.c
+$MUCC -U $REPO \
+      mv      zig/zag/bar/y.c zig/zag/bar/y%20y.c \
+      cp head zig/zag/bar/y.c zig/zag/bar/y%2520y.c 
 stat
 
-$MUCC rm   $REPO/zig/foo/bar \
-      cp 8 $REPO/zig/z.c $REPO/zig/foo/bar
+$MUCC -U $REPO \
+      mv      zig/zag/bar/y%20y.c   zig/zag/bar/z\ z1.c \
+      cp head zig/zag/bar/y%2520y.c zig/zag/bar/z%2520z.c \
+      cp head zig/zag/bar/y\ y.c    zig/zag/bar/z\ z2.c
 stat
 
-$MUCC rm   $REPO/zig/foo/bar \
-      cp 8 $REPO/zig/foo/bar $REPO/zig/foo/bar \
-      rm   $REPO/zig/foo/bar/z%20z1.c
+$MUCC -U $REPO \
+      mv   zig/zag zig/foo \
+      rm   zig/foo/bar/z\ z1.c \
+      rm   zig/foo/bar/z%20z2.c \
+      rm   zig/foo/bar/z%2520z.c \
+      cp 5 zig/zag/bar/x.c zig/foo/bar/z%20z1.c
 stat
 
-$MUCC rm      $REPO/zig/foo \
-      cp head $REPO/zig/foo/bar $REPO/zig/foo
+$MUCC -U $REPO \
+      rm   zig/foo/bar \
+      cp 8 zig/z.c zig/foo/bar
 stat
 
-$MUCC rm   $REPO/zig \
-      cp 3 $REPO/foo $REPO/foo \
-      cp 3 $REPO/foo $REPO/foo/foo \
-      cp 3 $REPO/foo $REPO/foo/foo/foo \
-      rm   $REPO/foo/foo/bar \
-      rm   $REPO/foo/foo/foo/bar \
-      cp 3 $REPO/foo $REPO/foo/foo/foo/bar
+$MUCC -U $REPO \
+      rm   zig/foo/bar \
+      cp 8 zig/foo/bar zig/foo/bar \
+      rm   zig/foo/bar/z%20z1.c
+stat
+
+$MUCC -U $REPO \
+      rm      zig/foo \
+      cp head zig/foo/bar zig/foo
+stat
+
+$MUCC -U $REPO \
+      rm   zig \
+      cp 3 foo foo \
+      cp 3 foo foo/foo \
+      cp 3 foo foo/foo/foo \
+      rm   foo/foo/bar \
+      rm   foo/foo/foo/bar \
+      cp 3 foo foo/foo/foo/bar
 stat
