ac1709aa62e98970229e32508bc4d327df102a91
djames <djames@unknown>
2007-03-14 04:56:24 +0000

    Revert r23802 and r23803, which were intended to be committed to the artem-soc-work branch.
    Branch: trunk
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@863878 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/mod_dav_svn/authz.c
+++ subversion/mod_dav_svn/authz.c
@@ -18,12 +18,10 @@
 
 #include <http_request.h>
 #include <http_log.h>
-#include <http_protocol.h>
 
 #include "svn_pools.h"
 #include "svn_path.h"
 
-#include "svn_dav.h"
 #include "dav_svn.h"
 
 
@@ -60,31 +58,15 @@ allow_read(request_rec *r,
   /* Build a Version Resource uri representing (rev, path). */
   uri = dav_svn__build_uri(repos, uri_type, rev, path, FALSE, pool);
 
-  if (dav_svn__get_native_authz_file(r))
-    {
-      /* Do native auhorization lookup - read access */
-      dav_error *err = dav_svn__check_access(repos->repo_name,
-                                             path,
-                                             r,
-                                             svn_authz_read);
+  /* Check if GET would work against this uri. */
+  subreq = ap_sub_req_method_uri("GET", uri, r, r->output_filters);
 
-      if (! err)
+  if (subreq)
+    {
+      if (subreq->status == HTTP_OK)
         allowed = TRUE;
 
-      /* XXX: need to cleanup dav_error? */
-    }
-  else
-    {
-      /* Check if GET would work against this uri. */
-      subreq = ap_sub_req_method_uri("GET", uri, r, r->output_filters);
-
-      if (subreq)
-        {
-          if (subreq->status == HTTP_OK)
-            allowed = TRUE;
-
-          ap_destroy_sub_req(subreq);
-        }
+      ap_destroy_sub_req(subreq);
     }
 
   return allowed;
@@ -197,196 +179,4 @@ dav_svn__allow_read(const dav_resource *resource,
 {
   return allow_read(resource->info->r, resource->info->repos,
                     resource->info->repos_path, rev, pool);
-}
-
-
-/* Native path-based authorization */
-
-static int
-check_access(const char *repos_name,
-             const char *repos_path,
-             request_rec* r,
-             svn_repos_authz_access_t required_access)
-{
-  const char *authz_file = NULL;
-  svn_authz_t *access_conf = NULL;
-  svn_error_t *svn_err;
-  const char *cache_key;
-  void *user_data;
-  svn_boolean_t access_granted;
-  char errbuf[128];
-
-  /* If native authz is off, there's nothing to do. Return DONE
-   * instead of OK to indicate that no checks have really been done.
-   */
-  if (! dav_svn__get_native_authz_file(r))
-    return DONE;
-
-  authz_file = dav_svn__get_native_authz_file(r);
-  /* If access file had not been specified, the default
-     behavior is to allow access. 
-     XXX: is this right? */
-  if (authz_file == NULL)
-    return OK;
-
-  /* Retrieve/cache authorization file */
-  cache_key = apr_pstrcat(r->pool, "mod_dav_svn:", authz_file, NULL);
-  apr_pool_userdata_get(&user_data, cache_key, r->connection->pool);
-  access_conf = user_data;
-  if (access_conf == NULL)
-    {
-      svn_err = svn_repos_authz_read(&access_conf, authz_file,
-                                     TRUE, r->connection->pool);
-      if (svn_err)
-        {
-          ap_log_rerror(APLOG_MARK, APLOG_ERR,
-                        /* If it is an error code that APR can make sense
-                           of, then show it, otherwise, pass zero to avoid
-                           putting "APR does not understand this error code"
-                           in the error log. */
-                        ((svn_err->apr_err >= APR_OS_START_USERERR &&
-                          svn_err->apr_err < APR_OS_START_CANONERR) ?
-                         0 : svn_err->apr_err),
-                        r, "Failed to load the SVNNativeAuthzFile: %s",
-                        svn_err_best_message(svn_err,
-                                             errbuf, sizeof(errbuf)));
-          svn_error_clear(svn_err);
-
-          return DECLINED;
-        }
-
-      /* Cache the open repos for the next request on this connection */
-      apr_pool_userdata_set(access_conf, cache_key,
-                            NULL, r->connection->pool);
-  }
-
-  /* Perform authz access control. */
-  svn_err = svn_repos_authz_check_access(access_conf, repos_name,
-                                         repos_path, r->user,
-                                         required_access,
-                                         &access_granted,
-                                         r->pool);
-
-  if (svn_err)
-    {
-      ap_log_rerror(APLOG_MARK, APLOG_ERR,
-                    /* If it is an error code that APR can make
-                       sense of, then show it, otherwise, pass
-                       zero to avoid putting "APR does not
-                       understand this error code" in the error
-                       log. */
-                    ((svn_err->apr_err >= APR_OS_START_USERERR &&
-                      svn_err->apr_err < APR_OS_START_CANONERR) ?
-                     0 : svn_err->apr_err),
-                    r, "Failed to perform access control: %s",
-                    svn_err_best_message(svn_err, errbuf, sizeof(errbuf)));
-      svn_error_clear(svn_err);
-
-      return DECLINED;
-    }
-
-  if (! access_granted)
-    return DECLINED;
-
-  return OK;
-}
-
-
-/* Log a message indicating the access control decision made about a
-   request.  FILE and LINE should be supplied via the APLOG_MARK macro.
-   ALLOWED is boolean.  REPOS_PATH and DEST_REPOS_PATH are information
-   about the request.  DEST_REPOS_PATH may be NULL. */
-static void
-log_access_verdict(const char *file, int line,
-                   const request_rec *r,
-                   int allowed,
-                   const char *repos_path,
-                   svn_repos_authz_access_t required_access)
-{
-  int level = allowed ? APLOG_INFO : APLOG_ERR;
-  const char *verdict = allowed ? "granted" : "denied";
-
-  char access_str[4] = { 0, 0, 0, 0 };
-  int access_idx = 0;
-
-  if (required_access & svn_authz_read)
-    access_str[access_idx++] = 'r';
-
-  if (required_access & svn_authz_write)
-    access_str[access_idx++] = 'w';
-
-  if (required_access & svn_authz_recursive)
-    access_str[access_idx++] = 'R';
-
-  if (repos_path == NULL)
-    repos_path = "<global>";
-
-  if (r->user)
-  {
-    ap_log_rerror(file, line, level, 0, r,
-                  "[native] Access %s: '%s' %s %s %s", verdict, r->user,
-                  r->method, repos_path, access_str);
-  }
-  else
-  {
-    ap_log_rerror(file, line, level, 0, r,
-                  "[native] Access %s: - %s %s %s", verdict,
-                  r->method, repos_path, access_str);
-  }
-}
-
-
-dav_error *
-dav_svn__check_access(const char *repos_name,
-                      const char *repos_path,
-                      request_rec *r,
-                      svn_repos_authz_access_t required_access)
-{
-  int status;
-
-  status = check_access(repos_name, repos_path, r, required_access);
-
-  /* If no checks had been done, native authz is off, so don't log
-   * a possibly misleading authorization verdict.
-   */
-  if (status == DONE)
-    return NULL;
-
-  if(status == DECLINED)
-  {
-    log_access_verdict(APLOG_MARK, r, 0, repos_path, required_access);
-    ap_note_auth_failure(r); // XXX: need this?
-
-    // XXX: need better error message
-    return dav_svn__new_error_tag(r->pool, HTTP_FORBIDDEN, 0,
-                                  "Insufficient rights to access resource.",
-                                  SVN_DAV_ERROR_NAMESPACE,
-                                  SVN_DAV_ERROR_TAG);
-  }
-
-  log_access_verdict(APLOG_MARK, r, 1, repos_path, required_access);
-
-  return NULL;
-}
-
-
-dav_error *
-dav_svn__check_resource_access(const dav_resource *resource,
-                               const svn_repos_authz_access_t required_access)
-{
-  return dav_svn__check_access(resource->info->repos->repo_name,
-                               resource->info->repos_path,
-                               resource->info->r,
-                               required_access);
-}
-
-
-dav_error *
-dav_svn__check_global_access(const dav_resource *resource,
-                             const svn_repos_authz_access_t required_access)
-{
-  return dav_svn__check_access(resource->info->repos->repo_name,
-                               NULL, /* global access */
-                               resource->info->r,
-                               required_access);
 }
