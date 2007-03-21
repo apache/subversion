395edf84dcb80c355714e10bbde0d63169850ac8
Daniel Rall <dlr@apache.org>
2007-03-21 19:56:48 +0000

    On the sparse-directories branch: Fix depth parameter-related build breakage in JavaHL bindings.
    * subversion/bindings/java/javahl/native/SVNClient.cpp
      Include svn_types.h for the SVN_DEPTH_FROM_RECURSE() macro.
      (checkout, update, doSwitch): Convert RECURSE parameters to svn_depth_t.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/sparse-directories@864045 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/java/javahl/native/SVNClient.cpp
+++ subversion/bindings/java/javahl/native/SVNClient.cpp
@@ -34,6 +34,7 @@
 #include "JNIByteArray.h"
 #include "CommitMessage.h"
 #include "EnumMapper.h"
+#include "svn_types.h"
 #include "svn_client.h"
 #include "svn_sorts.h"
 #include "svn_time.h"
@@ -455,7 +456,7 @@ jlong SVNClient::checkout(const char *moduleName, const char *destPath,
                                path.c_str (),
                                pegRevision.revision (),
                                revision.revision (),
-                               recurse, 
+                               SVN_DEPTH_FROM_RECURSE(recurse),
                                ignoreExternals,
                                allowUnverObstructions,
                                ctx,
@@ -595,7 +596,7 @@ jlongArray SVNClient::update(Targets &targets, Revision &revision, bool recurse,
     }
     Err = svn_client_update3(&retval, array,
                              revision.revision(),
-                             recurse,
+                             SVN_DEPTH_FROM_RECURSE(recurse),
                              ignoreExternals,
                              allowUnverObstructions,
                              ctx,
@@ -899,7 +900,8 @@ jlong SVNClient::doSwitch(const char *path, const char *url,
     Err = svn_client_switch2(&retval, intPath.c_str (),
                              intUrl.c_str(),
                              revision.revision (),
-                             recurse, allowUnverObstructions,
+                             SVN_DEPTH_FROM_RECURSE(recurse),
+			     allowUnverObstructions,
                              ctx,
                              apr_pool);
 
