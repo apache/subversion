eb9ae0eae39516373ba2673d39d21bdb626d99d0
kou <dev-null@apache.org>
2007-03-22 07:11:24 +0000

    Follow-up r23955 and r23956.
    * subversion/bindings/swig/ruby/svn/client.rb:
      - Remove needless require and end.
      (Svn::Clienct::Context): Fix typos.
    
    Found by: Joe Swatosh
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@864073 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/swig/ruby/svn/client.rb
+++ subversion/bindings/swig/ruby/svn/client.rb
@@ -4,7 +4,6 @@ require "svn/error"
 require "svn/util"
 require "svn/core"
 require "svn/wc"
-require "svn/ra"
 require "svn/ext/client"
 
 module Svn
@@ -33,7 +32,6 @@ module Svn
           item
         end
       end
-     end
     end
 
     class Info
@@ -475,13 +473,13 @@ module Svn
         add_provider(Core.auth_get_simple_provider)
       end
 
-      if Core.respond_to?(:get_windows_simple_provider)
+      if Core.respond_to?(:auth_get_windows_simple_provider)
         def add_windows_simple_provider
           add_provider(Core.auth_get_windows_simple_provider)
         end
       end
       
-      if Core.respond_to?(:get_keychain_simple_provider)
+      if Core.respond_to?(:auth_get_keychain_simple_provider)
         def add_keychain_simple_provider
           add_provider(Core.auth_get_keychain_simple_provider)
         end
@@ -503,7 +501,7 @@ module Svn
         add_provider(Core.auth_get_ssl_server_trust_file_provider)
       end
 
-      if Core.respond_to?(:get_windows_ssl_server_trust_provider)
+      if Core.respond_to?(:auth_get_windows_ssl_server_trust_provider)
         def add_windows_ssl_server_trust_provider
           add_provider(Core.auth_get_windows_ssl_server_trust_provider)
         end
