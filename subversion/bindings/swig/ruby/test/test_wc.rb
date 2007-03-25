7c13daf21ef026b3b87c1bebfc6b877e01adeb2a
kou <dev-null@apache.org>
2007-03-25 08:24:47 +0000

    Support svn_wc_delete3().
    * subversion/bindings/swig/ruby/svn/wc.rb
      (Svn::Wc::AdmAccess#delete): Use Svn::Wc.delete3 instead of Svn::Wc.delete2.
    
    * subversion/bindings/swig/ruby/test/test_wc.rb
      (SvnWcTest#test_delete): New test.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@864186 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/swig/ruby/test/test_wc.rb
+++ subversion/bindings/swig/ruby/test/test_wc.rb
@@ -408,7 +408,33 @@ EOE
       assert_equal(Svn::Wc::STATUS_NONE, status.text_status)
     end
   end
-  
+
+  def test_delete
+    source = "source"
+    file = "file"
+    path = File.join(@wc_path, file)
+    log = "sample log"
+    ctx = make_context(log)
+
+    File.open(path, "w") {|f| f.print(source)}
+    ctx.add(path)
+    ctx.ci(@wc_path).revision
+
+    assert(File.exists?(path))
+    Svn::Wc::AdmAccess.open(nil, @wc_path, true, 5) do |access|
+      access.delete(path)
+    end
+    assert(!File.exists?(path))
+
+    ctx.revert(path)
+
+    assert(File.exists?(path))
+    Svn::Wc::AdmAccess.open(nil, @wc_path, true, 5) do |access|
+      access.delete(path, nil, nil, true)
+    end
+    assert(File.exists?(path))
+  end
+
   def test_locked
     assert(!Svn::Wc.locked?(@wc_path))
     Svn::Wc::AdmAccess.open(nil, @wc_path, true, -1) do |access|
