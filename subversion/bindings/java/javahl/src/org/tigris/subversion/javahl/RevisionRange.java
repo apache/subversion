a0dd4469e85a3248dbb8e46139eb8d6552ab7165
Daniel Rall <dlr@apache.org>
2007-03-27 23:53:46 +0000

    [ in subversion/bindings/java/javahl/ ]
    * src/org/tigris/subversion/javahl/RevisionRange.java
      (toString): Use char data type.
      (hashCode, equals, compareTo): Remove pointless, Eclipse-generated
       doc string.  For equals(), add JavaDoc, rename paramter from "obj"
       to "range", and reformat for clarity.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@864290 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/java/javahl/src/org/tigris/subversion/javahl/RevisionRange.java
+++ subversion/bindings/java/javahl/src/org/tigris/subversion/javahl/RevisionRange.java
@@ -54,7 +54,7 @@ public class RevisionRange implements Comparable
             if (from.equals(to))
                 return from.toString();
             else
-                return from.toString() + "-" + to.toString();
+                return from.toString() + '-' + to.toString();
         }
         return super.toString();
     }
@@ -69,10 +69,6 @@ public class RevisionRange implements Comparable
         return new Long(val);
     }
 
-    /*
-     * (non-Javadoc)
-     * @see java.lang.Object#hashCode()
-     */
     public int hashCode()
     {
         final int prime = 31;
@@ -82,35 +78,53 @@ public class RevisionRange implements Comparable
         return result;
     }
 
-    /*
-     * (non-Javadoc)
-     * @see java.lang.Object#equals(java.lang.Object)
+    /**
+     * @param range The RevisionRange to compare this object to.
      */
-    public boolean equals(Object obj)
+    public boolean equals(Object range)
     {
-        if (this == obj) return true;
-        if (!super.equals(obj)) return false;
-        if (getClass() != obj.getClass()) return false;
-        final RevisionRange other = (RevisionRange) obj;
+        if (this == range)
+            return true;
+        if (!super.equals(range))
+            return false;
+        if (getClass() != range.getClass())
+            return false;
+
+        final RevisionRange other = (RevisionRange) range;
+
         if (from == null)
         {
-            if (other.from != null) return false;
+            if (other.from != null)
+                return false;
         }
-        else if (!from.equals(other.from)) return false;
+        else if (!from.equals(other.from))
+        {
+            return false;
+        }
+
         if (to == null)
         {
-            if (other.to != null) return false;
+            if (other.to != null)
+                return false;
         }
-        else if (!to.equals(other.to)) return false;
+        else if (!to.equals(other.to))
+        {
+            return false;
+        }
+
         return true;
     }
 
-    public int compareTo(Object obj)
+    /**
+     * @param range The RevisionRange to compare this object to.
+     */
+    public int compareTo(Object range)
     {
-        if (this == obj) return 0;
-        return RevisionRange.getRevisionAsLong(this.getFromRevision()).compareTo(
-                                               RevisionRange.getRevisionAsLong(((RevisionRange) obj)
-                                                      .getFromRevision()));
+        if (this == range)
+            return 0;
 
+        Revision other = ((RevisionRange) range).getFromRevision();
+        return RevisionRange.getRevisionAsLong(this.getFromRevision())
+            .compareTo(RevisionRange.getRevisionAsLong(other));
     }
 }
