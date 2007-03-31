3393335c420c1c535e6d724cfa1379c7c525271d
Daniel Rall <dlr@apache.org>
2007-03-31 02:26:08 +0000

    [ in subversion/bindings/java/javahl/ ]
    * native/Revision.h
    * native/Revision.cpp
      (makeJRevision): Add API for creating a Java Revision object.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@864359 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/java/javahl/native/Revision.h
+++ subversion/bindings/java/javahl/native/Revision.h
@@ -36,12 +36,18 @@ private:
 public:
     static const svn_opt_revision_kind START;
     static const svn_opt_revision_kind HEAD;
+
     Revision(jobject jthis, bool headIfUnspecified = false,
              bool oneIfUnspecified = false);
     Revision(const svn_opt_revision_kind kind = svn_opt_revision_unspecified);
     ~Revision();
+
     const svn_opt_revision_t *revision() const;
 
+    /**
+     * Make a Revision Java object.
+     */
+    static jobject makeJRevision(svn_revnum_t rev);
 };
 // !defined(AFX_REVISION_H__BEAA0788_C9D9_4A67_B94E_761ABC68ACFE__INCLUDED_)
 #endif
