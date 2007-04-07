046c691e93e84a6e15e8e9fa294185ec53baa2b5
kou <dev-null@apache.org>
2007-04-07 03:08:49 +0000

    Add a script that generates RubyGem for the Ruby bindings.
    * build/win32/make_gem.rb: New file.
    
    Approved by: djh
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@864568 13f79535-47bb-0310-9956-ffa450edef68

---
+++ build/win32/make_gem.rb
@@ -0,0 +1,46 @@
+# -*- ruby -*-
+
+def usage
+  puts "#{$0} target_directory"
+end
+
+if ARGV.size < 1
+  usage
+  exit 1
+end
+
+target_dir = ARGV.shift
+
+if target_dir == "--help"
+  usage
+  exit
+end
+
+base_dir = File.expand_path(File.join(target_dir, "ruby"))
+$LOAD_PATH.unshift(File.join(base_dir, "ext"))
+$LOAD_PATH.unshift(File.join(base_dir, "lib"))
+
+require 'svn/core'
+
+gem_file = nil
+Dir.chdir(target_dir) do
+  require 'rubygems'
+  Gem.manage_gems
+
+  spec = Gem::Specification.new do |s|
+    s.name = "subversion"
+    s.date = Time.now
+    s.version = Svn::Core::VER_NUM
+    s.summary = "The Ruby bindings for Subversion."
+    s.email = "dev@subversion.tigris.org"
+    s.homepage = "http://subversion.tigris.org/"
+    s.description = s.summary
+    s.authors = ["Kouhei Sutou"]
+    s.files = Dir.glob(File.join("**", "*")).delete_if {|x| /\.gem$/i =~ x}
+    s.require_paths = ["ruby/ext", "ruby/lib"]
+    s.platform = Gem::Platform::WIN32
+  end
+
+  gem_file = File.expand_path(Gem::Builder.new(spec).build)
+end
+FileUtils.mv(gem_file, File.basename(gem_file))
