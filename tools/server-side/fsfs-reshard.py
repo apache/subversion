3ee734f82953dd51cdb7c3b69e525475c62c5e31
malcolm <malcolm@unknown>
2007-04-12 12:45:46 +0000

    * tools/server-side/fsfs-reshard.py   Prevent use, for now, and add a license block.
    Suggested by: cmpilato
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@864626 13f79535-47bb-0310-9956-ffa450edef68

--- tools/server-side/fsfs-reshard.py
+++ tools/server-side/fsfs-reshard.py
@@ -26,6 +26,19 @@
 # same 'large number of files in a directory' problem that sharding is
 # intended to solve.
 #
+# ====================================================================
+# Copyright (c) 2007 CollabNet.  All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.  The terms
+# are also available at http://subversion.tigris.org/license-1.html.
+# If newer versions of this license are posted there, you may use a
+# newer version instead, at your option.
+#
+# This software consists of voluntary contributions made by many
+# individuals.  For exact contribution history, see the revision
+# history and logs, available at http://subversion.tigris.org/.
+# ====================================================================
 
 import os, stat, sys
 
@@ -326,4 +339,7 @@ def main():
   sys.exit(0)
 
 if __name__ == '__main__':
+  raise Exception, \
+    """This script is unfinished and not ready to be used on live data.
+    Trust us."""
   main()
