c86970f3047c99dc472105594de86e9d8de596c2
Daniel Rall <dlr@apache.org>
2007-04-23 23:41:33 +0000

    * subversion/libsvn_wc/util.c   (svn_wc__path_switched): Use svn_wc__entry_versioned() to avoid    potential NULL pointer dereference.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@864819 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/util.c
+++ subversion/libsvn_wc/util.c
@@ -266,8 +266,8 @@ svn_wc__path_switched(const char *wc_path,
       return err;
     }
 
-  SVN_ERR(svn_wc_entry(&parent_entry, wc_parent_path, parent_adm_access,
-                       FALSE, pool));
+  SVN_ERR(svn_wc__entry_versioned(&parent_entry, wc_parent_path,
+                                  parent_adm_access, FALSE, pool));
   parent_child_url = svn_path_join(parent_entry->url,
                                    svn_path_basename(wc_path, pool), pool);
   *switched = strcmp(parent_child_url, entry->url);
