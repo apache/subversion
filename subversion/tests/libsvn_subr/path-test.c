9d8f9dd1935dbc606a0e7e1aa022575ea61c062b
C. Michael Pilato <cmpilato@apache.org>
2007-04-24 16:05:03 +0000

    Finish issue #2641 - svnadmin dump path ordering bug.
    Fix an issue with svn_path_compare_paths()'s ordering logic triggered
    by just the wrong combination of paths which use extended ASCII
    (high-bit-set) characters.
    
    * subversion/libsvn_subr/path.c
      (svn_path_compare_paths): Use an unsigned character comparison
        instead of a signed one.
    
    * subversion/tests/libsvn_subr/path-test.c
      (test_compare_paths): Extend this test with data that triggers the
        problem case.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@864829 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/libsvn_subr/path-test.c
+++ subversion/tests/libsvn_subr/path-test.c
@@ -1016,6 +1016,7 @@ test_compare_paths(const char **msg,
     { "/foo",         "/foo/bar",     -1},
     { "/foo",         "/foo/bar/boo", -1},
     { "foo",          "/foo",         1},
+    { "foo\xe0bar",   "foo",          1},
   };
 
   *msg = "test svn_path_compare_paths";
