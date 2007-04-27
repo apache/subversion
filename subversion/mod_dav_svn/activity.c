5671c4f85e311cd8c4f42f93b5532139de5eb3b6
epg <dev-null@apache.org>
2007-04-27 19:16:53 +0000

    Stop using apr_dbm to manage the DAV activities database which maps DAV activity ids to Subversion transaction names.  Instead, use a simple directory structure, with MD5 checksums of activity ids as file names and transaction names as the first line. Include the activity id on a second line, as a diagnostic.
    * subversion/mod_dav_svn/activity.c
      (ACTIVITY_DB): Change to 'dav/activities.d' so as not to conflict
        with the DBM file 'dav/activities'.
      (escape_activity): New helper to escape activity for safe use as a
        file name.
      (read_txn): New helper to return the txn from an activity file.
      (dav_svn__get_txn): Make this a simple wrapper around read_txn.
      (dav_svn__delete_activity): Use read_txn to get the transaction
        name; delete the file from the activities directory.
      (dav_svn__store_activity): Create the activities directory if it
        does not exist and create the activity file.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@864889 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/mod_dav_svn/activity.c
+++ subversion/mod_dav_svn/activity.c
@@ -16,11 +16,16 @@
  * ====================================================================
  */
 
-#include <apr_dbm.h>
+#include <errno.h>
+
+#include <apr_md5.h>
 
 #include <httpd.h>
 #include <mod_dav.h>
 
+#include "svn_error.h"
+#include "svn_io.h"
+#include "svn_md5.h"
 #include "svn_path.h"
 #include "svn_fs.h"
 #include "svn_repos.h"
@@ -28,47 +33,117 @@
 #include "dav_svn.h"
 
 
-#define ACTIVITY_DB "dav/activities"
+#define ACTIVITY_DB "dav/activities.d"
+
+
+/* Escape ACTIVITY_ID to be safely usable as a filename.  Simply
+   returns the MD5 checksum of the id.
+*/
+static const char *
+escape_activity(const char *activity_id, apr_pool_t *pool)
+{
+  unsigned char digest[APR_MD5_DIGESTSIZE];
+  apr_md5(digest, activity_id, strlen(activity_id));
+  return svn_md5_digest_to_cstring_display(digest, pool);
+}
+
+
+/* FSFS transaction ids:
+   19 bytes for svn_revnum_t (room for 32 or 64 bit values)
+   + 1 byte for '-'
+   + 5 bytes (up to 99999)
+   + 1 terminating null / newline
+   = 26 bytes
+   BDB transaction ids are base-36, bounded by bdb MAX_KEY_SIZE, but
+   realistically, 26 bytes ought to be enough.
+  */
+#define TXN_LEN 26
+
+/* Return the transaction name of the activity stored in file
+   PATHNAME, or NULL if PATHNAME cannot be read for any reason. */
+static const char *
+read_txn(const char *pathname, apr_pool_t *pool)
+{
+  apr_file_t *activity_file;
+  apr_pool_t *iterpool = svn_pool_create(pool);
+  apr_size_t len;
+  svn_error_t *err = SVN_NO_ERROR;
+  char *txn_name = apr_palloc(pool, TXN_LEN);
+  int i;
+
+  /* Try up to 10 times to read the txn name, retrying on ESTALE
+     (stale NFS file handle because of dav_svn__store_activity
+     renaming the activity file into place).
+   */
+  for (i = 0; i < 10; i++)
+    {
+      svn_error_clear(err);
+      svn_pool_clear(iterpool);
+
+      err = svn_io_file_open(&activity_file, pathname,
+                             APR_READ  | APR_BUFFERED,
+                             APR_OS_DEFAULT, iterpool);
+      if (err)
+        {
+#ifdef ESTALE
+          if (APR_TO_OS_ERROR(err->apr_err) == ESTALE)
+            /* Retry on ESTALE... */
+            continue;
+#endif
+          /* ...else bail. */
+          break;
+        }
+
+      len = TXN_LEN;
+      err = svn_io_read_length_line(activity_file, txn_name, &len, iterpool);
+      if (err)
+        {
+#ifdef ESTALE
+          if (APR_TO_OS_ERROR(err->apr_err) == ESTALE)
+            continue;
+#endif
+          break;
+        }
+
+      err = svn_io_file_close(activity_file, iterpool);
+#ifdef ESTALE
+      if (err)
+        {
+          if (APR_TO_OS_ERROR(err->apr_err) == ESTALE)
+            {
+              /* No retry, just completely ignore this ESTALE. */
+              svn_error_clear(err);
+              err = SVN_NO_ERROR;
+            }
+        }
+#endif
+
+      /* We have a txn_name or had a non-ESTALE close failure; either
+         way, we're finished. */
+      break;
+    }
+  svn_pool_destroy(iterpool);
+
+  /* ### let's just assume that any error means the
+     ### activity/transaction doesn't exist */
+  if (err)
+    {
+      svn_error_clear(err);
+      return NULL;
+    }
+
+  return txn_name;
+}
 
 
 const char *
 dav_svn__get_txn(const dav_svn_repos *repos, const char *activity_id)
 {
-  apr_dbm_t *dbm;
-  apr_status_t status;
   const char *pathname;
-  apr_datum_t key;
-  apr_datum_t value;
-  const char *txn_name = NULL;
-
-  pathname = svn_path_join(repos->fs_path, ACTIVITY_DB, repos->pool);
-  status = apr_dbm_open(&dbm, pathname, APR_DBM_READONLY, 
-                        APR_OS_DEFAULT, repos->pool);
-  if (status != APR_SUCCESS)
-    {
-      /* ### let's just assume that any error means the DB doesn't exist,
-         ### therefore, the activity/transaction doesn't exist */
-      return NULL;
-    }
-
-  key.dptr = (char *)activity_id;
-  key.dsize = strlen(activity_id) + 1;  /* null-term'd */
-  if (apr_dbm_exists(dbm, key))
-    {
-      status = apr_dbm_fetch(dbm, key, &value);
-      if (status != APR_SUCCESS)
-        {
-          /* ### again: assume failure means it doesn't exist */
-          apr_dbm_close(dbm);
-          return NULL;
-        }
-      txn_name = apr_pstrdup(repos->pool, value.dptr);   /* null-term'd */
-      apr_dbm_freedatum(dbm, value);
-    }
-
-  apr_dbm_close(dbm);
-
-  return txn_name;
+  pathname = svn_path_join_many(repos->pool, repos->fs_path, ACTIVITY_DB,
+                                escape_activity(activity_id, repos->pool),
+                                NULL);
+  return read_txn(pathname, repos->pool);
 }
 
 
@@ -76,11 +151,7 @@ dav_error *
 dav_svn__delete_activity(const dav_svn_repos *repos, const char *activity_id)
 {
   dav_error *err = NULL;
-  apr_dbm_t *dbm;
-  apr_status_t status;
   const char *pathname;
-  apr_datum_t key;
-  apr_datum_t value;
   svn_fs_txn_t *txn;
   const char *txn_name;
   svn_error_t *serr;
@@ -89,23 +160,12 @@ dav_svn__delete_activity(const dav_svn_repos *repos, const char *activity_id)
      404.  If the transaction is not present or is immutable, return a
      204.  For all other failures, return a 500. */
 
-  /* Open the activities database. */
-  pathname = svn_path_join(repos->fs_path, ACTIVITY_DB, repos->pool);
-  status = apr_dbm_open(&dbm, pathname, APR_DBM_READWRITE, 
-                        APR_OS_DEFAULT, repos->pool);
-  if (status != APR_SUCCESS)
-    return dav_new_error(repos->pool, HTTP_NOT_FOUND, 0,
-                         "could not open activities database.");
-
-  /* Get the activity from the activity database. */
-  key.dptr = (char *)activity_id;
-  key.dsize = strlen(activity_id) + 1;  /* null-term'd */
-  status = apr_dbm_fetch(dbm, key, &value);
-  if (status == APR_SUCCESS)
-    txn_name = value.dptr;
-  else
+  pathname = svn_path_join_many(repos->pool, repos->fs_path, ACTIVITY_DB,
+                                escape_activity(activity_id, repos->pool),
+                                NULL);
+  txn_name = read_txn(pathname, repos->pool);
+  if (txn_name == NULL)
     {
-      apr_dbm_close(dbm);
       return dav_new_error(repos->pool, HTTP_NOT_FOUND, 0,
                            "could not find activity.");
     }
@@ -131,7 +191,7 @@ dav_svn__delete_activity(const dav_svn_repos *repos, const char *activity_id)
               err = dav_svn__convert_err(serr, HTTP_INTERNAL_SERVER_ERROR,
                                          "could not open transaction.", 
                                          repos->pool);
-              goto cleanup;
+              return err;
             }
         }
       else
@@ -142,23 +202,17 @@ dav_svn__delete_activity(const dav_svn_repos *repos, const char *activity_id)
               err = dav_svn__convert_err(serr, HTTP_INTERNAL_SERVER_ERROR,
                                          "could not abort transaction.", 
                                          repos->pool);
-              goto cleanup;
+              return err;
             }
         }
     }
-  
-  /* Finally, we remove the activity from the activities database. */
-  status = apr_dbm_delete(dbm, key);
-  if (status)
-    {
-      err = dav_new_error(repos->pool, HTTP_INTERNAL_SERVER_ERROR, 0,
-                          "unable to remove activity.");
-      goto cleanup;
-    }
 
- cleanup:
-  apr_dbm_freedatum(dbm, value);
-  apr_dbm_close(dbm);
+  /* Finally, we remove the activity from the activities database. */
+  serr = svn_io_remove_file(pathname, repos->pool);
+  if (serr)
+    err = dav_svn__convert_err(serr, HTTP_INTERNAL_SERVER_ERROR,
+                               "unable to remove activity.",
+                               repos->pool);
 
   return err;
 }
@@ -169,37 +223,66 @@ dav_svn__store_activity(const dav_svn_repos *repos,
                         const char *activity_id,
                         const char *txn_name)
 {
-  apr_dbm_t *dbm;
-  apr_status_t status;
-  const char *pathname;
-  apr_datum_t key;
-  apr_datum_t value;
+  const char *final_path, *tmp_path, *activity_contents;
+  svn_error_t *err;
+  apr_file_t *activity_file;
 
-  pathname = svn_path_join(repos->fs_path, ACTIVITY_DB, repos->pool);
-  status = apr_dbm_open(&dbm, pathname, APR_DBM_RWCREATE, 
-                        APR_OS_DEFAULT, repos->pool);
-  if (status != APR_SUCCESS)
+  /* Create activities directory if it does not yet exist. */
+  err = svn_io_make_dir_recursively(svn_path_join_many(repos->pool,
+                                                       repos->fs_path,
+                                                       ACTIVITY_DB,
+                                                       NULL),
+                                    repos->pool);
+  if (err != NULL)
+    return dav_svn__convert_err(err, HTTP_INTERNAL_SERVER_ERROR,
+                                "could not initialize activity db.",
+                                repos->pool);
+
+  final_path = svn_path_join_many(repos->pool, repos->fs_path, ACTIVITY_DB,
+                                  escape_activity(activity_id, repos->pool),
+                                  NULL);
+  err = svn_io_open_unique_file2(&activity_file, &tmp_path, final_path,
+                                 ".tmp", svn_io_file_del_none, repos->pool);
+  if (err)
     {
-      svn_error_t *serr = svn_error_wrap_apr(status, "Can't open activity db");
-
+      svn_error_t *serr = svn_error_quick_wrap(err, "Can't open activity db");
       return dav_svn__convert_err(serr, HTTP_INTERNAL_SERVER_ERROR,
-                                  "could not open dbm files.", 
+                                  "could not open files.",
                                   repos->pool);
     }
 
-  key.dptr = (char *)activity_id;
-  key.dsize = strlen(activity_id) + 1;  /* null-term'd */
-  value.dptr = (char *)txn_name;
-  value.dsize = strlen(txn_name) + 1;   /* null-term'd */
-  status = apr_dbm_store(dbm, key, value);
-  apr_dbm_close(dbm);
-  if (status != APR_SUCCESS)
+  activity_contents = apr_psprintf(repos->pool, "%s\n%s\n",
+                                   txn_name, activity_id);
+  err = svn_io_file_write_full(activity_file, activity_contents,
+                               strlen(activity_contents), NULL, repos->pool);
+  if (err)
     {
-      svn_error_t *serr =
-        svn_error_wrap_apr(status, "Can't close activity db");
+      svn_error_t *serr = svn_error_quick_wrap(err,
+                                               "Can't write to activity db");
 
+      /* Try to remove the tmp file, but we already have an error... */
+      svn_error_clear(svn_io_file_close(activity_file, repos->pool));
+      svn_error_clear(svn_io_remove_file(tmp_path, repos->pool));
       return dav_svn__convert_err(serr, HTTP_INTERNAL_SERVER_ERROR,
-                                  "could not close dbm files.", 
+                                  "could not write files.",
+                                  repos->pool);
+    }
+
+  err = svn_io_file_close(activity_file, repos->pool);
+  if (err)
+    {
+      svn_error_clear(svn_io_remove_file(tmp_path, repos->pool));
+      return dav_svn__convert_err(err, HTTP_INTERNAL_SERVER_ERROR,
+                                  "could not close files.",
+                                  repos->pool);
+    }
+
+  err = svn_io_file_rename(tmp_path, final_path, repos->pool);
+  if (err)
+    {
+      svn_error_clear(svn_io_remove_file(tmp_path, repos->pool));
+      return dav_svn__convert_err(err, HTTP_INTERNAL_SERVER_ERROR,
+                                  "could not replace files.",
                                   repos->pool);
     }
 
