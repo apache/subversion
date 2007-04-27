46912926d3c66ca9f1167bcda7ff8d3da264db51
Paul Burba <pburba@apache.org>
2007-04-27 01:25:49 +0000

    Remove unnecessary argument from svn_wc__path_switched()
    We already have the entry, we open the access for it's parent.
    
    * subversion/include/private/svn_wc_private.h
    * subversion/libsvn_wc/util.c
      (svn_wc__path_switched): Remove adm_access arg.
    
    * subversion/libsvn_client/merge.c
      (get_wc_merge_info, svn_client__elide_mergeinfo): Update callers to
      svn_wc__path_switched.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@864882 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/private/svn_wc_private.h
+++ subversion/include/private/svn_wc_private.h
@@ -85,8 +85,8 @@ svn_error_t *svn_wc__props_modified(const char *path,
                                     apr_pool_t *pool);
 
 /** Given a @a wcpath with its accompanying @a entry, set @a *switched to
- * whether @a wcpath is switched (TRUE or FALSE).  @a adm_access must be an
- * access baton for @a wcpath.  All allocations are done in @a pool.
+ * whether @a wcpath is switched (TRUE or FALSE).  All allocations are
+ * done in @a pool.
  *
  * @since New in 1.5.
  */
@@ -94,7 +94,6 @@ svn_error_t *
 svn_wc__path_switched(const char *wcpath,
                       svn_boolean_t *switched,
                       const svn_wc_entry_t *entry,
-                      svn_wc_adm_access_t *adm_access,
                       apr_pool_t *pool);
 
 #ifdef __cplusplus
