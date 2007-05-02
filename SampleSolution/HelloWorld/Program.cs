539f0a23add6e5f859a04b9e3cdfa7e1177d6d2e
Ivan Zhakov <ivan@apache.org>
2007-05-02 10:18:24 +0000

    * SampleSolution: Accidental commit, later reverted in r24883.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@864955 13f79535-47bb-0310-9956-ffa450edef68

--- SampleSolution/HelloWorld/Program.cs
+++ SampleSolution/HelloWorld/Program.cs
@@ -14,7 +14,7 @@ namespace HelloWorld
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
