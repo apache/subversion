7100f5c44076eb35f7c62dcfeb96b480cfb273ad
Hyrum Kurt Wright <hwright@apache.org>
2007-05-09 17:27:51 +0000

    On the merge-sensitive-log branch: Add the '--merge-sensitive' option to 'svn log'.
    This isn't actually being used anywhere the log command yet.  I'm adding it
    now so that the soon-to-be-written tests fail for correctness reasons, not
    invalid commandline option reasons.
    
    * subversion/svn/main.c
      (svn_cl__options): Add the '--merge-sensitive' option with the '-g' shortcut.
      (svn_cl__cmd_table): Accept '--merge-sensitive' for 'svn log'.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/merge-sensitive-log@865049 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svn/main.c
+++ subversion/svn/main.c
@@ -202,6 +202,8 @@ const apr_getopt_option_t svn_cl__options[] =
                     N_("set revision property ARG in new revision\n"
                        "                             "
                        "using the name=value format")},
+  {"merge-sensitive", 'g', 0,
+                    N_("show additional information from merge history")},
   {0,               0, 0, 0}
 };
 
@@ -505,7 +507,7 @@ const svn_opt_subcommand_desc2_t svn_cl__cmd_table[] =
      "    svn log foo.c\n"
      "    svn log http://www.example.com/repo/project/foo.c\n"
      "    svn log http://www.example.com/repo/project foo.c bar.c\n"),
-    {'r', 'q', 'v', svn_cl__targets_opt, svn_cl__stop_on_copy_opt,
+    {'r', 'q', 'v', 'g', svn_cl__targets_opt, svn_cl__stop_on_copy_opt,
      svn_cl__incremental_opt, svn_cl__xml_opt, SVN_CL__AUTH_OPTIONS,
      svn_cl__config_dir_opt, svn_cl__limit_opt, svn_cl__changelist_opt} },
 
