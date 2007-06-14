1b4499f7e5ba3b5e6ab6b488e0822e656e7f96d3
Daniel Rall <dlr@apache.org>
2007-06-14 22:43:37 +0000

    * contrib/client-side/diff-wrap.sh   A wrapper for invoking a third-party diff program (e.g. Mac OS X opendiff)   from 'svn diff --diff-cmd=diff-wrap.sh ARGS... > /dev/null'.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@865482 13f79535-47bb-0310-9956-ffa450edef68

---
+++ contrib/client-side/diff-wrap.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# A wrapper for invoking a thrid-party diff program (e.g. Mac OS X opendiff)
+# from 'svn diff --diff-cmd=diff-wrap.sh ARGS... > /dev/null'.
+
+if [ $# -lt 2 ]; then
+    echo "usage: $0 [ignored args...] file1 file2" >&2
+    exit 1
+fi
+
+# Configure your favoriate diff program here.
+DIFF=opendiff
+
+# The last two arguments passed to this script are the paths to the files
+# to diff.
+while [ $# -gt 2 ]; do
+    shift
+done
+
+# Call the diff command (change the following line to make sense for your
+# merge program).
+$DIFF $*
