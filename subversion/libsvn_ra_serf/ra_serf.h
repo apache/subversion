0f0e696f3d18a6f36fd0df19d795d32c9e93cd94
Daniel Rall <dlr@apache.org>
2007-06-20 21:35:03 +0000

    * subversion/libsvn_ra_serf/ra_serf.h   Bump copyright year.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@865552 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_ra_serf/ra_serf.h
+++ subversion/libsvn_ra_serf/ra_serf.h
@@ -2,7 +2,7 @@
  * ra_serf.h : Private declarations for the Serf-based DAV RA module.
  *
  * ====================================================================
- * Copyright (c) 2006 CollabNet.  All rights reserved.
+ * Copyright (c) 2006-2007 CollabNet.  All rights reserved.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
