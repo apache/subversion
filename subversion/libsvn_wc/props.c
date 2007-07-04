2cfca06e6a6193a34c0c2dcf52a9aab21cc5544c
Kamesh J <kameshj@apache.org>
2007-07-04 18:50:49 +0000

    Fix the problem: Two people from 2 different working copies at revision rX trying to do a merge of rY and rZ independently from /branch/b1 to /trunk. Assume the first person commits(merge of rY), when second guy tries to do the commit(merge of rZ) he would get a out of date error. So second guy would do 'svn up'. svn up does not merge the 'svn:mergeinfo' correctly It just keeps rZ.
    * subversion/libsvn_wc/props.c
      (svn_wc__merge_props): Record the merged svn:mergeinfo on the target.
       
    Found by: Piet-Hein Peeters
    Reviewed by: dlr
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@865720 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/props.c
+++ subversion/libsvn_wc/props.c
@@ -830,6 +830,8 @@ svn_wc__merge_props(svn_wc_notify_state_t *state,
                       SVN_ERR(combine_forked_mergeinfo_props(&to_val, from_val,
                                                              working_val,
                                                              to_val, pool));
+                      apr_hash_set(working_props, propname, 
+                                   APR_HASH_KEY_STRING, to_val);
                       *state = svn_wc_notify_state_merged;
                     }
                   else
