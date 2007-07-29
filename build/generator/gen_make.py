04d7fb3c25ad202d8a11521131fb5261e3722591
djames <djames@unknown>
2007-07-29 04:37:30 +0000

    Restore the SWIG checkout system, which was deleted in r25740, because it is still needed for compatibility with SWIG 1.3.24 and SWIG 1.3.27. Instead, we will only remove the specific "long long" hacks which were obsoleted by r25740.
    * build.conf
      (swig-checkout-files): Restore.
    
    * build/generator/gen_make.py
      build/generator/gen_win.py:
      (Generator.write): Restore logic for calling swig_checkout_files.
    
    * build/generator/swig/__init__.py:
      (Generator.__init__): Restore logic for grabbing swig_checkout_files
      from build.conf.
    
    * build/generator/swig/checkout_swig_header.py:
      (): Restore deleted file.
      (checkout): Remove "long long" hacks which were obsoleted by r25740.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@865951 13f79535-47bb-0310-9956-ffa450edef68

--- build/generator/gen_make.py
+++ build/generator/gen_make.py
@@ -8,6 +8,7 @@ import string
 
 import gen_base
 import generator.swig.header_wrappers
+import generator.swig.checkout_swig_header
 import generator.swig.external_runtime
 import generator.util.executable
 _exec = generator.util.executable
@@ -133,6 +134,7 @@ class Generator(gen_base.GeneratorBase):
 
     if not self.release_mode:
       for swig in (generator.swig.header_wrappers,
+                   generator.swig.checkout_swig_header,
                    generator.swig.external_runtime):
         gen = swig.Generator(self.conf, "swig")
         gen.write_makefile_rules(self.ofile)
