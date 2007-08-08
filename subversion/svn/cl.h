e306d8076a5a71954b7664471c52595da763cf40
C. Michael Pilato <cmpilato@apache.org>
2007-08-08 13:16:29 +0000

    Rename an awkward-looking variable name (accept_), and rename svn_accept_default to svn_accept_none (because it indicates that no auto-accept value was chosen).
    These are matters of utmost criticality, to be sure.
    
    * subversion/include/svn_types.h
      (svn_accept_none): Was svn_accept_default.
    
    * subversion/svn/cl.h
      (svn_cl__opt_state_t): Rename 'accept_' to 'accept_which'.
    
    * subversion/svn/main.c
      (main): Track changes to variable and enum names.
    
    * subversion/libsvn_client/resolved.c
      (svn_client_resolved): Track changes to variable names.
    
    * subversion/libsvn_wc/adm_ops.c
      (resolve_conflict_on_entry): Rename 'accept_' to 'accept_which', and
        track changes to enum names.
      (svn_wc_resolved_conflict2): Track changes to enum names.
    
    * subversion/svn/resolved-cmd.c
      (svn_cl__resolved): Track changes to variable names.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@866058 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svn/cl.h
+++ subversion/svn/cl.h
@@ -159,7 +159,7 @@ typedef struct svn_cl__opt_state_t
   apr_hash_t *revprop_table;     /* table with revision properties to set */
   svn_boolean_t parents;         /* create intermediate directories */
   svn_boolean_t use_merge_history; /* use/display extra merge information */
-  svn_accept_t accept_;          /* automatically resolve conflict */
+  svn_accept_t accept_which;     /* automatically resolve conflict */
 
 } svn_cl__opt_state_t;
 
