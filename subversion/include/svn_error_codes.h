938cf0aa486e0b8a249b158a916e5f4278ee4cbc
Kamesh J <kameshj@apache.org>
2007-08-09 10:35:17 +0000

    Disallow 'switch --relocate' with --depth, since it is not a valid usecase.
     * subversion/include/svn_error_codes.h
       (SVN_ERR_CL_OBSCURE_ARGS): New error code.
    
     * subversion/svn/main.c
       (main): Check for combination of '--relocate' and '--depth' argument and
       report an error.
    
    Patch by: Senthil Kumaran <senthil@collab.net>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@866081 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_error_codes.h
+++ subversion/include/svn_error_codes.h
@@ -1115,6 +1115,10 @@ SVN_ERROR_START
              SVN_ERR_CL_CATEGORY_START + 9,
              "A log message was given where none was necessary")
 
+  SVN_ERRDEF(SVN_ERR_CL_OBSCURE_ARGS,
+             SVN_ERR_CL_CATEGORY_START + 10,
+             "The arguments are obscure and does not imply a valid usecase")
+
 SVN_ERROR_END
 
 
