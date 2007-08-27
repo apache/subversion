1c00568c1d8e8103ce21051798a5f73c4e605417
E W H Huelsmann <ehu@apache.org>
2007-08-27 20:15:37 +0000

    Replace svn_path_uri_encode()/svn_path_join() pairs with svn_path_url_add_component() which is designed to do the same thing.
    * libsvn_wc/adm_crawler.c (report_revisions_and_depths)
    * libsvn_wc/util.c (svn_wc__path_switched):
      Use svn_path_url_add_component().
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@866415 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/util.c
+++ subversion/libsvn_wc/util.c
@@ -239,7 +239,7 @@ svn_wc__path_switched(const char *wc_path,
                       const svn_wc_entry_t *entry,
                       apr_pool_t *pool)
 {
-  const char *wc_parent_path, *parent_child_url, *wc_basename;
+  const char *wc_parent_path, *parent_child_url;
   const svn_wc_entry_t *parent_entry;
   svn_wc_adm_access_t *parent_adm_access;
   svn_error_t *err;
@@ -271,10 +271,6 @@ svn_wc__path_switched(const char *wc_path,
   SVN_ERR(svn_wc__entry_versioned(&parent_entry, wc_parent_path,
                                   parent_adm_access, FALSE, pool));
   SVN_ERR(svn_wc_adm_close(parent_adm_access));
-  wc_basename = svn_path_basename(wc_path, pool);
-
-  if (!svn_path_is_uri_safe(wc_basename))
-    wc_basename = svn_path_uri_encode(wc_basename, pool);
 
   /* Without complete entries (and URLs) for WC_PATH and it's parent
      we return SVN_ERR_ENTRY_MISSING_URL. */
@@ -286,7 +282,9 @@ svn_wc__path_switched(const char *wc_path,
                                svn_path_local_style(no_url_path, pool));
     }
 
-  parent_child_url = svn_path_join(parent_entry->url, wc_basename, pool);
+  parent_child_url
+    = svn_path_add_url_component(parent_entry->url,
+                                 svn_path_basename(wc_path, pool), pool);
   *switched = strcmp(parent_child_url, entry->url) != 0;
 
   return SVN_NO_ERROR;
