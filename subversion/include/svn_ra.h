8513e67508620f94c93c6b9a4ee60fc758a95770
Karl Fogel <kfogel@apache.org>
2007-09-11 21:58:25 +0000

    * subversion/include/svn_ra.h   (svn_ra_do_update2): Move a "TODO(sd)" comment back to where it belongs,     following up to r26503.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@866615 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_ra.h
+++ subversion/include/svn_ra.h
@@ -826,12 +826,12 @@ svn_error_t *svn_ra_get_mergeinfo(svn_ra_session_t *session,
  *
  * Update the target only as deeply as @a depth indicates.
  *
+ * ### TODO(sd): Make sure the behavior described above is what happens.
+ *
  * If @a send_copyfrom_args is true, then ask the server to send
  * copyfrom arguments to add_file() and add_directory() when possible.
  * (Note: this means that any subsequent txdeltas coming from the
  * server are presumed to apply against the copied file!)
- *
- * ### TODO(sd): Make sure the behavior described above is what happens.
  *
  * The working copy will be updated to @a revision_to_update_to, or the
  * "latest" revision if this arg is invalid.
