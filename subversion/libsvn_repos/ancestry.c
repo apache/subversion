7aa4c661229d8deae05e1168febdc9561f789750
Hyrum Kurt Wright <hwright@apache.org>
2007-09-17 21:35:19 +0000

    On the ancestry-walk branch: When walking ancestry, discover merging revisions, notify consumers of them, and recursively walk merged revisions.
    * subversion/libsvn_repos/ancestry.c
      (get_merged_rev_mergeinfo): Implement.
      (walk_merged_history): Implement.
      (walk_range): New function.
      (svn_repos__walk_ancestry): Check for merging revisions, and report them.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/ancestry-walk@866718 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_repos/ancestry.c
+++ subversion/libsvn_repos/ancestry.c
@@ -50,6 +50,7 @@ struct path_info
 
 /*** Ancestry walking. ***/
 
+/* Return in *MERGEINFO the difference between PATH at REV and PATH at REV-1 */
 static svn_error_t *
 get_merged_rev_mergeinfo(apr_hash_t **mergeinfo,
                          svn_fs_t *fs,
@@ -57,13 +58,112 @@ get_merged_rev_mergeinfo(apr_hash_t **mergeinfo,
                          svn_revnum_t rev,
                          apr_pool_t *pool)
 {
-  *mergeinfo = apr_hash_make(pool);
+  apr_hash_t *curr_mergeinfo, *prev_mergeinfo, *changed, *deleted;
+
+  SVN_ERR(svn_repos__get_path_mergeinfo(&curr_mergeinfo, fs, path, rev, pool));
+  SVN_ERR(svn_repos__get_path_mergeinfo(&prev_mergeinfo, fs, path, rev - 1,
+                                        pool));
+  SVN_ERR(svn_mergeinfo_diff(&deleted, &changed, prev_mergeinfo, curr_mergeinfo,
+                             svn_rangelist_ignore_inheritance, pool));
+  SVN_ERR(svn_mergeinfo_merge(&changed, deleted,
+                              svn_rangelist_equal_inheritance, pool));
+
+  *mergeinfo = changed;
+
   return SVN_NO_ERROR;
 }
 
+/* Walk a single RANGE of revisions at PATH. */
 static svn_error_t *
-walk_merged_history(apr_pool_t *pool)
+walk_range(const char *path,
+           svn_merge_range_t *range,
+           svn_fs_t *fs,
+           const svn_repos__ancestry_callbacks_t *callbacks,
+           void *callbacks_baton,
+           svn_repos_authz_func_t authz_read_func,
+           void *authz_read_baton,
+           apr_pool_t *pool)
 {
+  svn_merge_range_t tmp_range = *range;
+  apr_pool_t *iterpool = svn_pool_create(pool);
+  
+  /* The main problem here is that we know we should look at PATH everywhere
+     in range, examining every revision can get expensive.  However, we can't
+     just use the history object, because the note at PATH could be replaced
+     in any given revision.  So, we compromise... */
+  do
+    {
+      svn_fs_root_t *root, *copy_root;
+      svn_revnum_t copy_rev;
+      const char *copy_path;
+
+      svn_pool_clear(iterpool);
+
+      SVN_ERR(svn_fs_revision_root(&root, fs, tmp_range.end, iterpool));
+      SVN_ERR(svn_fs_closest_copy(&copy_root, &copy_path, root, path,
+                                  iterpool));
+
+      /* If we don't have a copy, we know the entire range is for the same node.
+         If we do have a copy, see if it is older than the beginning of the
+         range. */
+      if (copy_root == NULL)
+        {
+          copy_rev = tmp_range.start;
+        }
+      else 
+        {
+          copy_rev = svn_fs_revision_root_revision(copy_root);
+          if (copy_rev < tmp_range.start)
+            copy_rev = tmp_range.start;
+        }
+
+      SVN_ERR(svn_repos__walk_ancestry(path, fs, copy_rev, tmp_range.end, TRUE,
+                                       FALSE, callbacks, callbacks_baton,
+                                       authz_read_func, authz_read_baton,
+                                       iterpool));
+
+      tmp_range.end = copy_rev - 1;
+    }
+  while (tmp_range.start <= tmp_range.end);
+
+  svn_pool_destroy(iterpool);
+
+  return SVN_NO_ERROR;
+}
+
+/* Walk merged history starting from PATH at REV. */
+static svn_error_t *
+walk_merged_history(const char *path,
+                    svn_revnum_t rev,
+                    svn_fs_t *fs,
+                    apr_hash_t *mergeinfo_diff,
+                    const svn_repos__ancestry_callbacks_t *callbacks,
+                    void *callbacks_baton,
+                    svn_repos_authz_func_t authz_read_func,
+                    void *authz_read_baton,
+                    apr_pool_t *pool)
+{
+  apr_hash_index_t *hi;
+
+  /* Determine the source of the merge. */
+  for (hi = apr_hash_first(pool, mergeinfo_diff); hi; hi=apr_hash_next(hi))
+    {
+      apr_array_header_t *rangelist;
+      const char *merged_path;
+      int i;
+
+      apr_hash_this(hi, (void *) &merged_path, NULL, (void *) &rangelist);
+      
+      for (i = 0; i < rangelist->nelts; i++)
+        {
+          svn_merge_range_t *range = APR_ARRAY_IDX(rangelist, i,
+                                                   svn_merge_range_t *);
+
+          SVN_ERR(walk_range(merged_path, range, fs, callbacks, callbacks_baton,
+                             authz_read_func, authz_read_baton, pool));
+        }
+    }
+
   return SVN_NO_ERROR;
 }
 
@@ -96,7 +196,6 @@ svn_repos__walk_ancestry(const char *end_path,
       svn_revnum_t rev;
       const char *path;
       svn_boolean_t halt;
-      apr_hash_t *mergeinfo;
 
       svn_pool_clear(iterpool);
 
@@ -130,17 +229,24 @@ svn_repos__walk_ancestry(const char *end_path,
        /* Check for merges */
       if (include_merges)
         {
+          apr_hash_t *mergeinfo;
           svn_boolean_t merging_rev = FALSE;
 
           SVN_ERR(get_merged_rev_mergeinfo(&mergeinfo, fs, path, rev, iterpool));
           if (apr_hash_count(mergeinfo) > 0)
-            {
-              /* First, check branchingness. */
-              merging_rev = TRUE;
-            }
+            merging_rev = TRUE;
 
+        /* If we have a merge, notify the consumer, and walk the merged
+           history. */
         if (merging_rev)
-          SVN_ERR(walk_merged_history(iterpool));
+          {
+            if (callbacks->found_merge)
+              SVN_ERR(callbacks->found_merge(callbacks_baton, path, rev, &halt,
+                                             iterpool));
+            SVN_ERR(walk_merged_history(path, rev, fs, mergeinfo, callbacks,
+                                        callbacks_baton, authz_read_func,
+                                        authz_read_baton, iterpool));
+          }
       }
 
       /* Swap the temporary pools. */
