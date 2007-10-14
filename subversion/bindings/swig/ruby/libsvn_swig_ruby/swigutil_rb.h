5d0dba45440f54a6c7fe1e6b1a976ffbf367c33d
Joe Swatosh <joeswatosh@apache.org>
2007-10-14 18:33:56 +0000

    Follow on to r27133 "Implement Issue #2818: Provide an API which allows for merging of arbitrary revision ranges as well as the command line enhancements to support the same."   This specifically is in response to the change to the change to the svn_client_merge_peg3 API.
    * subversion/bindings/swig/ruby/svn/client.rb
      (Svn::Client::Context#merge_peg2): New method to match the new API in 
      svn_client_merge_peg3.
      (Svn::Client::Context#merge_peg): Implement in terms of merge_peg2.
    
    * subversion/bindings/swig/ruby/libsvn_swig_ruby/swigutil_rb.c
    * subversion/bindings/swig/ruby/libsvn_swig_ruby/swigutil_rb.h
      (svn_swig_rb_array_to_apr_array_revision_range): New function to convert a 
      Ruby array of two element arrays into an apr_array_header_t of 
      svn_opt_revision_range_t.
    
    * subversion/bindings/swig/include/svn_containers.swg
      (typemap(in) apr_array_header_t *REVISION_RANGE_LIST): New typemap.
    
    * subversion/bindings/swig/svn_client.i
      (apply apr_array_header_t *REVISION_RANGE_LIST): New apply.
    
    Patch by: kou
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@867253 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/swig/ruby/libsvn_swig_ruby/swigutil_rb.h
+++ subversion/bindings/swig/ruby/libsvn_swig_ruby/swigutil_rb.h
@@ -153,6 +153,9 @@ apr_array_header_t *svn_swig_rb_array_to_apr_array_merge_range(VALUE array,
 SVN_RB_SWIG_SWIGUTIL_EXPORT
 apr_array_header_t *svn_swig_rb_array_to_apr_array_copy_source(VALUE array,
                                                                apr_pool_t *pool);
+SVN_RB_SWIG_SWIGUTIL_EXPORT
+apr_array_header_t *svn_swig_rb_array_to_apr_array_revision_range(VALUE array,
+                                                                  apr_pool_t *pool);
 
 SVN_RB_SWIG_SWIGUTIL_EXPORT
 apr_array_header_t *svn_swig_rb_to_apr_array_prop(VALUE array_or_hash,
