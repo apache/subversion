c2575999c5cc7695f7d74d1c497eec9c3b7f0355
C. Michael Pilato <cmpilato@apache.org>
2007-10-15 12:09:25 +0000

    Add initial ra-neon implementation of the get-location-segments RA API.
    * subversion/libsvn_ra_neon/ra_neon.h
      (ELEM_get_location_segments_report): New enum value.
    
    * subversion/libsvn_ra_neon/fetch.c
      (gls_report_elements, get_location_segments_baton_t,
       gls_start_element): New.
      (dav_svn__get_location_segments_report): Actually implement this function.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@867263 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_ra_neon/fetch.c
+++ subversion/libsvn_ra_neon/fetch.c
@@ -1382,6 +1382,87 @@ svn_ra_neon__get_locations(svn_ra_session_t *session,
 }
 
 
+/*
+ * Plan for processing the XML. The XML will be of the form:
+ *
+ * <S:get-location-segments-report xmlns...>
+ *     <S:location [path="..."] range_start="..." range_end="..."/>
+ *     ...
+ * </S:get-location-segments-report>
+ *
+ * We extract what we want at the start of <S:location>.
+ */
+
+/* Elements used in a get-locations response */
+static const svn_ra_neon__xml_elm_t gls_report_elements[] =
+{
+  { SVN_XML_NAMESPACE, "get-location-segments-report", 
+    ELEM_get_locations_report, 0 },
+  { SVN_XML_NAMESPACE, "location", ELEM_location, 0 },
+  { NULL }
+};
+
+typedef struct {
+  svn_location_segment_receiver_t receiver;
+  void *receiver_baton;
+  apr_pool_t *subpool;
+} get_location_segments_baton_t;
+
+/* This implements the `svn_ra_neon__startelem_cb_t' prototype. */
+static svn_error_t *
+gls_start_element(int *elem, void *userdata, int parent_state,
+                  const char *ns, const char *ln, const char **atts)
+{
+  get_location_segments_baton_t *baton = userdata;
+  const svn_ra_neon__xml_elm_t *elm;
+
+  /* Just skip unknown elements. */
+  if (! ((elm = svn_ra_neon__lookup_xml_elem(gls_report_elements, ns, ln))))
+    {
+      *elem = NE_XML_DECLINE;
+      return SVN_NO_ERROR;
+    }
+
+  if (parent_state == ELEM_get_location_segments_report
+      && elm->id == ELEM_location)
+    {
+      const char *rev_str;
+      svn_revnum_t range_start = SVN_INVALID_REVNUM;
+      svn_revnum_t range_end = SVN_INVALID_REVNUM;
+      const char *path = NULL;
+
+      path = svn_xml_get_attr_value("path", atts);
+      rev_str = svn_xml_get_attr_value("range-start", atts);
+      if (rev_str)
+        range_start = SVN_STR_TO_REV(rev_str);
+      rev_str = svn_xml_get_attr_value("range-end", atts);
+      if (rev_str)
+        range_end = SVN_STR_TO_REV(rev_str);
+
+      if (SVN_IS_VALID_REVNUM(range_start) && SVN_IS_VALID_REVNUM(range_end))
+        {
+          svn_location_segment_t *segment = apr_pcalloc(baton->subpool, 
+                                                        sizeof(*segment));
+          segment->path = path;
+          segment->range_start = range_start;
+          segment->range_end = range_end;
+          SVN_ERR(baton->receiver(segment, 
+                                  baton->receiver_baton, 
+                                  baton->subpool));
+          svn_pool_clear(baton->subpool);
+        }
+      else
+        {
+          return svn_error_create(SVN_ERR_RA_DAV_MALFORMED_DATA, NULL,
+                                  _("Expected valid revision range"));
+        }
+    }
+
+  *elem = elm->id;
+
+  return SVN_NO_ERROR;
+}
+
 svn_error_t *
 svn_ra_neon__get_location_segments(svn_ra_session_t *session,
                                    const char *path,
@@ -1390,8 +1471,71 @@ svn_ra_neon__get_location_segments(svn_ra_session_t *session,
                                    svn_location_segment_receiver_t receiver,
                                    void *receiver_baton,
                                    apr_pool_t *pool)
+
 {
-  return svn_error_create(SVN_ERR_RA_NOT_IMPLEMENTED, NULL, NULL);
+  svn_ra_neon__session_t *ras = session->priv;
+  svn_stringbuf_t *request_body;
+  svn_error_t *err;
+  get_location_segments_baton_t request_baton;
+  svn_string_t bc_url, bc_relative;
+  const char *bc;
+  int status_code = 0;
+
+  /* Build the request body. */
+  request_body = svn_stringbuf_create("", pool);
+  svn_stringbuf_appendcstr(request_body,
+                           "<?xml version=\"1.0\" encoding=\"utf-8\"?>" 
+                           DEBUG_CR "<S:get-location-segments xmlns:S=\"" 
+                           SVN_XML_NAMESPACE "\" xmlns:D=\"DAV:\">" DEBUG_CR);
+
+  /* Tack on the path... */
+  svn_stringbuf_appendcstr(request_body, "<S:path>");
+  svn_stringbuf_appendcstr(request_body, apr_xml_quote_string(pool, path, 0));
+  svn_stringbuf_appendcstr(request_body, "</S:path>" DEBUG_CR);
+
+  /* ...and maybe a start revision... */
+  if (SVN_IS_VALID_REVNUM(start_rev))
+    svn_stringbuf_appendcstr
+      (request_body, apr_psprintf(pool, 
+                                  "<S:start-revision>%ld</S:start-revision>" 
+                                  DEBUG_CR, start_rev));
+
+  /* ...and maybe an end revision. */
+  if (SVN_IS_VALID_REVNUM(end_rev))
+    svn_stringbuf_appendcstr
+      (request_body, apr_psprintf(pool, 
+                                  "<S:end-revision>%ld</S:end-revision>" 
+                                  DEBUG_CR, end_rev));
+
+  svn_stringbuf_appendcstr(request_body, "</S:get-locations>");
+
+  request_baton.receiver = receiver;
+  request_baton.receiver_baton = receiver_baton;
+  request_baton.subpool = svn_pool_create(pool);
+
+  /* ras's URL may not exist in HEAD, and thus it's not safe to send
+     it as the main argument to the REPORT request; it might cause
+     dav_get_resource() to choke on the server.  So instead, we pass a
+     baseline-collection URL, which we get from the peg revision.  */
+  SVN_ERR(svn_ra_neon__get_baseline_info(NULL, &bc_url, &bc_relative, NULL,
+                                         ras, ras->url->data,
+                                         start_rev, pool));
+  bc = svn_path_url_add_component(bc_url.data, bc_relative.data, pool);
+
+  err = svn_ra_neon__parsed_request(ras, "REPORT", bc,
+                                    request_body->data, NULL, NULL,
+                                    gls_start_element, NULL, NULL,
+                                    &request_baton, NULL, &status_code,
+                                    FALSE, pool);
+
+  /* Map status 501: Method Not Implemented to our not implemented error.
+     1.0.x servers and older don't support this report. */
+  if (status_code == 501)
+    return svn_error_create(SVN_ERR_RA_NOT_IMPLEMENTED, err,
+                            _("'get-location-segments' REPORT "
+                              "not implemented"));
+
+  return err;
 }
 
 
