6968a07d2f0a0cb9b102b4c93cdaaaf7a8a7e52d
Hyrum Kurt Wright <hwright@apache.org>
2007-10-15 21:32:12 +0000

    * subversion/bindings/javahl/src/org/tigris/subversion/javahl/ConflictResolverCallback.java   (resolve): A follow-up to r27186: fix the docstring to reflect the new class   name.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@867272 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/javahl/src/org/tigris/subversion/javahl/ConflictResolverCallback.java
+++ subversion/bindings/javahl/src/org/tigris/subversion/javahl/ConflictResolverCallback.java
@@ -37,9 +37,9 @@ public interface ConflictResolverCallback extends EventListener
      *
      * @param descrip A description of the conflict.
      * @return The result of any conflict resolution, from the {@link
-     * .Result} enum.
+     * .Choice} enum.
      * @throws SubversionException If an error occurs.
-     * @see .Result
+     * @see .Choice
      */
     public int resolve(ConflictDescriptor descrip)
         throws SubversionException;
