7feb616f39667ff0b506c1e391e4b4816740eecb
C. Michael Pilato <cmpilato@apache.org>
2007-10-15 12:05:22 +0000

    Add mod_dav_svn REPORT handler for get-location-segments RA API.
    * subversion/mod_dav_svn/version.c
      (deliver_report): Handle new get-location-segments REPORT request.
    
    * subversion/mod_dav_svn/dav_svn.h
      (dav_svn__get_location_segments_report): New prototype.
    
    * subversion/mod_dav_svn/reports/get-location-segments.c
      New file with get-location-segments REPORT handler implementation.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@867262 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/mod_dav_svn/version.c
+++ subversion/mod_dav_svn/version.c
@@ -967,6 +967,10 @@ deliver_report(request_rec *r,
         {
           return dav_svn__get_locations_report(resource, doc, output);
         }
+      else if (strcmp(doc->root->name, "get-location-segments") == 0)
+        {
+          return dav_svn__get_location_segments_report(resource, doc, output);
+        }
       else if (strcmp(doc->root->name, "file-revs-report") == 0)
         {
           return dav_svn__file_revs_report(resource, doc, output);
