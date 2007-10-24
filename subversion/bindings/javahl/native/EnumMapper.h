163881e289527ec55cea706cce23e663c7f6d028
Daniel Rall <dlr@apache.org>
2007-10-24 18:27:22 +0000

    JavaHL: Expose new APIs for property conflict resolution.
    * build.conf
      (private-includes): Add
       org_tigris_subversion_javahl_ConflictDescriptor_Kind.h.
    
    [ in subversion/bindings/javahl/ ]
    
    * src/org/tigris/subversion/javahl/ConflictDescriptor.java
      (propertyName, conflictKind): Add new fields.
      (ConflictDescriptor): Add corresponding new parameters to ctor, and
       account for renames.
      (theirPath, myPath): Rename from reposPath and userPath.
      (getTheirPath, getMyPath): Rename from getReposPath() and getUserPath().
      (Kind): Add new inner class corresponding to C enum.
    
    * native/ConflictResolverCallback.cpp
      (resolve): Handle new ConflictDescriptor fields.
    
    * native/EnumMapper.h
    * native/EnumMapper.cpp
      Include org_tigris_subversion_javahl_ConflictDescriptor_Kind.h in .cpp file.
      (mapConflictKind): Add new API.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@867429 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/javahl/native/EnumMapper.h
+++ subversion/bindings/javahl/native/EnumMapper.h
@@ -45,6 +45,7 @@ class EnumMapper
   static jint mapNotifyLockState(svn_wc_notify_lock_state_t state);
   static jint mapStatusKind(svn_wc_status_kind svnKind);
   static jint mapScheduleKind(svn_wc_schedule_t schedule);
+  static jint mapConflictKind(svn_wc_conflict_kind_t kind);
   static jint mapConflictAction(svn_wc_conflict_action_t action);
   static jint mapConflictReason(svn_wc_conflict_reason_t reason);
 };
