19653a4af899288bad1a7cb1e35152c292d9e712
Mark Phippard <markphip@apache.org>
2007-10-24 19:17:04 +0000

    JavaHL: Follow-up to r27355 - expose new APIs for property conflict resolution.
    [ in subversion/bindings/javahl/ ]
    
    * src/org/tigris/subversion/javahl/ConflictDescriptor.java
      (ConflictDescriptor): Change ordering of ctor args to match the order they
        are passed from the native library.  Rearranged code ordering to match.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@867430 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/javahl/src/org/tigris/subversion/javahl/ConflictDescriptor.java
+++ subversion/bindings/javahl/src/org/tigris/subversion/javahl/ConflictDescriptor.java
@@ -29,14 +29,14 @@ public class ConflictDescriptor
     private String path;
 
     /**
-     * @see .Kind
-     */
-    private int conflictKind;
-
-    /**
      * @see NodeKind
      */
     private int nodeKind;
+
+    /**
+     * @see .Kind
+     */
+    private int conflictKind;
 
     private String propertyName;
 
@@ -71,15 +71,15 @@ public class ConflictDescriptor
     private String myPath;
     private String mergedPath;
 
-    ConflictDescriptor(String path, int conflictKind, int nodeKind,
+    ConflictDescriptor(String path, int nodeKind, int conflictKind,
                        String propertyName, boolean isBinary, String mimeType,
                        int action, int reason,
                        String basePath, String theirPath,
                        String myPath, String mergedPath)
     {
         this.path = path;
-        this.conflictKind = conflictKind;
         this.nodeKind = nodeKind;
+        this.conflictKind = conflictKind;
         this.propertyName = propertyName;
         this.isBinary = isBinary;
         this.mimeType = mimeType;
@@ -97,19 +97,19 @@ public class ConflictDescriptor
     }
 
     /**
-     * @see .Kind
-     */
-    public int getKind()
-    {
-        return conflictKind;
-    }
-
-    /**
      * @see NodeKind
      */
     public int getNodeKind()
     {
         return nodeKind;
+    }
+
+    /**
+     * @see .Kind
+     */
+    public int getKind()
+    {
+        return conflictKind;
     }
 
     public String getPropertyName()
