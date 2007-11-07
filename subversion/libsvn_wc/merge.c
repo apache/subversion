655b62d493544f9cf00d96add5cb008a1b54d043
epg <epg@unknown>
2007-11-07 02:09:34 +0000

    Follow-up to r27253: Fix one last conflict-callback API violation.
    * subversion/libsvn_wc/merge.c
      (svn_wc__merge_internal): Raise error if the callback doesn't return
        a result_t object.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@867716 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/merge.c
+++ subversion/libsvn_wc/merge.c
@@ -671,13 +671,9 @@ svn_wc__merge_internal(svn_stringbuf_t **log_accum,
 
           SVN_ERR(conflict_func(&result, &cdesc, conflict_baton, pool));
           if (result == NULL)
-            {
-              /* If we were harsh, we'd throw an error here.
-                 Instead, we'll just pretend they want to postpone
-                 the conflict resolution. */
-              result = svn_wc_create_conflict_result(
-                               svn_wc_conflict_choose_postpone, NULL, pool);
-            }
+            return svn_error_create(SVN_ERR_WC_CONFLICT_RESOLVER_FAILURE,
+                                    NULL, _("Conflict callback violated API:"
+                                            " returned no results"));
 
           switch (result->choice)
             {
