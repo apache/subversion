34e68019cc97ae0ba8fee4745ca51556c0e95c76
Hyrum Kurt Wright <hwright@apache.org>
2007-11-07 19:12:19 +0000

    Add a bit of introductory text to our Doxygen docs.  The location of this text is somewhat arbitrary, it needs only live somewhere in the files which are processed by doxygen.  I couldn't find a way to include this text somewhere outside of the source tree, but I think it is valuable to have some front matter to our generated API documentation.
    * subversion/include/svn_client.h:
      Add a comment which includes introductory text for the generated API docs.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@867735 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_client.h
+++ subversion/include/svn_client.h
@@ -4191,3 +4191,18 @@ svn_client_open_ra_session(svn_ra_session_t **session,
 #endif /* __cplusplus */
 
 #endif  /* SVN_CLIENT_H */
+
+/* The following comment adds some text to the main doxygen page.  I've
+ * put it here, because I don't know of a better place.  Feel free to
+ * move it if a more suitable home can be found. */
+/**
+ * @mainpage Subversion Documentation
+ *
+ * This documentation covers the public APIs provided by the Subversion
+ * libraries.  It is intended mainly for programmers, both those working
+ * on Subversion itself, as well as developers of 3rd-party applications
+ * intending to use these APIs.  For more information about using Subversion,
+ * see the Subversion Book at http://svnbook.red-bean.com/.
+ *
+ * To learn more about Subversion, please visit http://subversion.tigris.org/.
+ */
