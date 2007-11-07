85fb7610b6dea753b1eb644dda51f65c15b481d1
Daniel Rall <dlr@apache.org>
2007-11-07 22:00:31 +0000

    A follow-up to r27661, moving Doxygen-specific descriptive text out of our public header files.
    * doc/doxygen.conf
      (INPUT): Add subversion/include/svn_doxygen.h.
    
    * subversion/include/svn_client.h
      (@mainpage): Move section into...
    
    * subversion/include/private/svn_doxygen.h
      (@mainpage): ...this new, empty file.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@867745 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subversion/include/private/svn_doxygen.h
@@ -0,0 +1,11 @@
+/**
+ * @mainpage Subversion Documentation
+ *
+ * This documentation covers the public APIs provided by the Subversion
+ * libraries.  It is intended mainly for programmers, both those working
+ * on Subversion itself, as well as developers of 3rd-party applications
+ * intending to use these APIs.  For more information about using Subversion,
+ * see the Subversion Book at http://svnbook.red-bean.com/.
+ *
+ * To learn more about Subversion, please visit http://subversion.tigris.org/.
+ */
