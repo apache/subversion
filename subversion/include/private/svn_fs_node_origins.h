5bf95f375b61c215253297294814968764ae1962
David Samuel Glasser <glasser@apache.org>
2007-11-15 23:28:07 +0000

    On the sqlite-node-origins branch:
    * subversion/include/private/svn_fs_node_origins.h
    * subversion/include/private/svn_fs_sqlite.h
      Change copyright on two headers I just made to be just 2007.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/sqlite-node-origins@867918 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/private/svn_fs_node_origins.h
+++ subversion/include/private/svn_fs_node_origins.h
@@ -3,7 +3,7 @@
  * be consumed by only fs_* libs; access to the node origin index.
  *
  * ====================================================================
- * Copyright (c) 2006-2007 CollabNet.  All rights reserved.
+ * Copyright (c) 2007 CollabNet.  All rights reserved.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
