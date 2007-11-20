83f920f191c6070cffc8a63ef7cad20b2a883cda
Kamesh J <kameshj@apache.org>
2007-11-20 14:31:52 +0000

    Docstring Fix.
    * subversion/libsvn_fs_util/mergeinfo-sqlite-index.c
      (get_mergeinfo_for_path): Fix the Docstring to document all parameters.
    
    Suggested by: kfogel
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@868019 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_util/mergeinfo-sqlite-index.c
+++ subversion/libsvn_fs_util/mergeinfo-sqlite-index.c
@@ -379,10 +379,11 @@ append_component_to_paths(apr_hash_t **output,
 }
 
 /* A helper for svn_fs_mergeinfo__get_mergeinfo() that retrieves
-   mergeinfo recursively (when INHERIT is svn_mergeinfo_inherited or 
-   svn_mergeinfo_nearest_ancestor) for a single path.  Pass NULL for RESULT 
-   if you only want CACHE to be updated.  Otherwise, both RESULT and CACHE
-   are updated with the appropriate mergeinfo for PATH. */
+   mergeinfo on PATH at REV from DB by taking care of elided mergeinfo
+   if INHERIT is svn_mergeinfo_inherited or svn_mergeinfo_nearest_ancestor.
+   Pass NULL for RESULT if you only want CACHE to be 
+   updated.  Otherwise, both RESULT and CACHE are updated with the appropriate
+   mergeinfo for PATH.  Perform all allocations in POOL. */
 static svn_error_t *
 get_mergeinfo_for_path(sqlite3 *db,
                        const char *path,
