5739fa590500890aa8b1f1f04c99b7c65018af59
C. Michael Pilato <cmpilato@apache.org>
2007-11-26 22:50:23 +0000

    Fix a bunch of copy tests that were failing because of a poorly rooted RA session.  In doing so, introduce a helper function I've wanted more times than I can count in recent weeks.
    * subversion/libsvn_client/client.h,
    * subversion/libsvn_client/ra.c
      (svn_client__ensure_ra_session_url): New.
    
    * subversion/libsvn_client/copy.c
      (calculate_target_mergeinfo): Temporarily point the RA session at
        the repository root URL when asking about mergeinfo.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@868122 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/ra.c
+++ subversion/libsvn_client/ra.c
@@ -457,6 +457,22 @@ svn_client__ra_session_from_path(svn_ra_session_t **ra_session_p,
 }
 
 
+svn_error_t *
+svn_client__ensure_ra_session_url(const char **old_session_url,
+                                  svn_ra_session_t *ra_session,
+                                  const char *session_url,
+                                  apr_pool_t *pool)
+{
+  *old_session_url = NULL;
+  SVN_ERR(svn_ra_get_session_url(ra_session, old_session_url, pool));
+  if (! session_url)
+    SVN_ERR(svn_ra_get_repos_root(ra_session, &session_url, pool));
+  if (strcmp(*old_session_url, session_url) != 0)
+    SVN_ERR(svn_ra_reparent(ra_session, session_url, pool));
+  return SVN_NO_ERROR;
+}
+
+
 
 /*** Repository Locations ***/
 
