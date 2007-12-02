ebc6d3dcfbc209be8183dc0be778a92610fc8a35
Lieven Govaerts <lgo@apache.org>
2007-12-02 17:30:08 +0000

    Fix the Windows build with IPv6 by including the required header files before <windows.h>.
    Patch by: Stefan Küng <tortoisesvn@gmail.com>
    
    * subversion/libsvn_ra_serf/util.c, 
    * subversion/libsvn_ra_serf/win32_auth_sspi.c
    * subversion/libsvn_subr/config_win.c
    * subversion/libsvn_subr/win32_xlate.c : Include required header files
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@868259 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/config_win.c
+++ subversion/libsvn_subr/config_win.c
@@ -23,7 +23,9 @@
 #ifdef WIN32
 #define WIN32_LEAN_AND_MEAN
 #ifdef APR_HAVE_IPV6
-#include <winsock2.h>
+#include "winsock2.h"
+#include "Ws2tcpip.h"
+#include "Wspiapi.h"
 #endif
 #include <windows.h>
 #include <shlobj.h>
