9e26439a8473546902f89476a5e49f425509d3d6
C. Michael Pilato <cmpilato@apache.org>
2007-12-05 18:51:57 +0000

    On the 'better-two-url-merges' branch, merge oustanding changes from trunk. (Merged /trunk:r28267-28278.)
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/better-two-url-merges@868353 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/prop_tests.py
+++ subversion/tests/cmdline/prop_tests.py
@@ -674,28 +674,28 @@ def inappropriate_props(sbox):
                                      path)
   svntest.actions.run_and_verify_svn('illegal grammar', None,
                                      "svn: Invalid revision number found "
-                                     "parsing 'one'\n",
+                                      "parsing 'one'\n",
                                      'propset', SVN_PROP_MERGE_INFO,
                                      '/trunk:one', path)
 
   # ...contain overlapping revision ranges
   svntest.actions.run_and_verify_svn('overlapping ranges', None,
                                      "svn: Parsing of overlapping revision "
-                                     "ranges '9-20' and '18-22' is not "
-                                     "supported\n",
+                                      "ranges '9-20' and '18-22' is not "
+                                      "supported\n",
                                      'propset', SVN_PROP_MERGE_INFO,
                                      '/branch:5-7,9-20,18-22', path)
 
   svntest.actions.run_and_verify_svn('overlapping ranges', None,
                                      "svn: Parsing of overlapping revision "
-                                     "ranges '3' and '3' is not supported\n",
+                                      "ranges '3' and '3' is not supported\n",
                                      'propset', SVN_PROP_MERGE_INFO,
                                      '/branch:3,3', path)
 
   # ...contain unordered revision ranges
   svntest.actions.run_and_verify_svn('unordered ranges', None,
                                      "svn: Unable to parse unordered "
-                                     "revision ranges '5' and '2-3'\n",
+                                      "revision ranges '5' and '2-3'\n",
                                      'propset', SVN_PROP_MERGE_INFO,
                                      '/featureX:5,2-3,9', path)
 
@@ -703,14 +703,14 @@ def inappropriate_props(sbox):
   #    equal to end revisions.
   svntest.actions.run_and_verify_svn('range start >= range end', None,
                                      "svn: Unable to parse reversed "
-                                     "revision range '20-5'\n",
+                                      "revision range '20-5'\n",
                                      'propset', SVN_PROP_MERGE_INFO,
                                      '/featureX:4,20-5', path)
 
   # ...contain paths mapped to empty revision ranges
   svntest.actions.run_and_verify_svn('empty ranges', None,
                                      "svn: Mergeinfo for '/trunk' maps to "
-                                     "an empty revision range\n",
+                                      "an empty revision range\n",
                                      'propset', SVN_PROP_MERGE_INFO,
                                      '/trunk:', path)
 
