03e117cfaeb89eb583808fcff94dbec394751bca
Karl Fogel <kfogel@apache.org>
2007-12-12 02:03:36 +0000

    On the 'reintegrate' branch:
    * subversion/include/svn_error_codes.h
      (SVN_ERR_CLIENT_NOT_READY_TO_MERGE): Reword, following up to r28428.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/reintegrate@868503 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_error_codes.h
+++ subversion/include/svn_error_codes.h
@@ -1018,7 +1018,7 @@ SVN_ERROR_START
   /** @since New in 1.5. */
   SVN_ERRDEF(SVN_ERR_CLIENT_NOT_READY_TO_MERGE,
              SVN_ERR_CLIENT_CATEGORY_START + 16,
-             "Working copy and branch not ready for whole-branch merge.")
+             "Working copy and merge source not ready for reintegration")
 
   /* misc errors */
 
