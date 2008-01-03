a7e6dd20ec0b670b04700287720510d1e6c5c49d
Hyrum Kurt Wright <hwright@apache.org>
2008-01-03 19:35:09 +0000

    Happy New Year!
    [ Additional instances of the license were updated in r31732. ]
    
    * www/license-1.html,
      www/license-for-python-bindings.html,
      www/design.html,
      subversion/bindings/swig/python/LICENSE_FOR_PYTHON_BINDINGS,
      packages/windows-innosetup/Pre.rtf
      Update copyright year in license notices.
    
    * subversion/libsvn_subr/opt.c
      (print_version_info): Update copyright year for the command-line utilities.
    
    * build/win32/svn.rc
      Update copyright year for the Windows binaries.
    
    * packages/windows-innosetup/svn.iss
      Update copyright year for the Windows installer.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@868810 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/opt.c
+++ subversion/libsvn_subr/opt.c
@@ -1100,7 +1100,7 @@ print_version_info(const char *pgm_name,
   SVN_ERR(svn_cmdline_printf(pool, _("%s, version %s\n"
                                      "   compiled %s, %s\n\n"), pgm_name,
                              SVN_VERSION, __DATE__, __TIME__));
-  SVN_ERR(svn_cmdline_fputs(_("Copyright (C) 2000-2007 CollabNet.\n"
+  SVN_ERR(svn_cmdline_fputs(_("Copyright (C) 2000-2008 CollabNet.\n"
                               "Subversion is open source software, see"
                               " http://subversion.tigris.org/\n"
                               "This product includes software developed by "
