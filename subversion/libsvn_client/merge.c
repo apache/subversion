0a22c1c725411ec7d7afd52a93b2590004555530
Kamesh J <kameshj@apache.org>
2008-01-14 16:14:47 +0000

    On the issue-2897 branch:
    Code cleanup.
    
    * subversion/libsvn_client/merge.c
      (drive_merge_report_editor): child's range info is already available as 
       'range_info' and its range as 'range' make use of it. And child range is
       non-NULL at this point of execution, so no need to re-affirm that.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/issue-2897@868963 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/merge.c
+++ subversion/libsvn_client/merge.c
@@ -2704,13 +2704,8 @@ drive_merge_report_editor(const char *target_wcpath,
                   svn_client__remaining_range_info_t *parent_range_info =
                     APR_ARRAY_IDX(parent->remaining_ranges, 0,
                                   svn_client__remaining_range_info_t *);
-                  svn_client__remaining_range_info_t *child_range_info =
-                    APR_ARRAY_IDX(child->remaining_ranges, 0,
-                                  svn_client__remaining_range_info_t *);
-                  if (parent_range_info && child_range_info
-                      && parent_range_info->range && child_range_info->range
-                      && parent_range_info->range->start == 
-                         child_range_info->range->start)
+                  if (parent_range_info && parent_range_info->range
+                      && parent_range_info->range->start == range->start)
                     continue;
                 }
             }
