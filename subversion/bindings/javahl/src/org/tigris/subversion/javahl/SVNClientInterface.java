e6ad52e2138b1472dc1f875d28b4ae077da90741
Hyrum Kurt Wright <hwright@apache.org>
2008-01-18 22:50:54 +0000

    JavaHL: Followup to r28951.  Add support for changelists to propertySet(), propertyCreate(), propertyRemove(), and properties().
    Note: This commit introduces a test failure in the JavaHL tests.  I'm not yet
    sure how the failure is related to these changes (though I seem to recall
    something similar happening in past changes to the properties interface).  I'm
    committing this now so others may have the chance to look at the failure also.
    
    [ in subversion/bindings/javahl/ ]
    * native/org_tigris_subversion_javahl_SVNClient.cpp
      (Java_org_tigris_subversion_javahl_SVNClient_properties,
       Java_org_tigris_subversion_javahl_SVNClient_propertySet,
       Java_org_tigris_subversion_javahl_SVNClient_propertyRemove):
       Add changelist parameter.
    
    * native/SVNClient.h,
      native/SVNClient.cpp
      (propertyRemove, propertySet, properties): Add changelist parameter, use it
       for the C APIs.
    
    * src/org/tigris/subversion/javahl/SVNClient.java,
      src/org/tigris/subversion/javahl/SVNClientSynchronized.java,
      src/org/tigris/subversion/javahl/SVNClientInterface.java
      (properties, propertyRemove, propertySet, propertyCreate): Add parameter.
    
    * tests/org/tigris/subversion/javahl/BasicTests.java:
      Update tests.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@869032 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/javahl/src/org/tigris/subversion/javahl/SVNClientInterface.java
+++ subversion/bindings/javahl/src/org/tigris/subversion/javahl/SVNClientInterface.java
@@ -1217,12 +1217,13 @@ public interface SVNClientInterface
      * @param revision    the revision of the item
      * @param pegRevision the revision to interpret path
      * @param depth       the depth to recurse into subdirectories
+     * @param changelists changelists to filter by
      * @param callback    the callback to use to return the properties
      * @throws ClientException
      * @since 1.5
      */
     void properties(String path, Revision revision, Revision pegRevision,
-                    int depth, ProplistCallback callback)
+                    int depth, String[] changelists, ProplistCallback callback)
             throws ClientException;
 
     /**
@@ -1292,12 +1293,13 @@ public interface SVNClientInterface
      * @param name    name of the property
      * @param value   new value of the property
      * @param depth   the depth to recurse into subdirectories
+     * @param changelists changelists to filter by
      * @param force   do not check if the value is valid
      * @throws ClientException
      * @since 1.5
      */
     void propertySet(String path, String name, String value, int depth,
-                     boolean force)
+                     String[] changelists, boolean force)
             throws ClientException;
 
     /**
@@ -1317,10 +1319,12 @@ public interface SVNClientInterface
      * @param path      path of the item
      * @param name      name of the property
      * @param depth     the depth to recurse into subdirectories
+     * @param changelists changelists to filter by
      * @throws ClientException
      * @since 1.5
      */
-    void propertyRemove(String path, String name, int depth)
+    void propertyRemove(String path, String name, int depth,
+                        String[] changelists)
             throws ClientException;
 
     /**
@@ -1392,12 +1396,13 @@ public interface SVNClientInterface
      * @param name    name of the property
      * @param value   new value of the property
      * @param depth   depth to set property on the subdirectories
+     * @param changelists changelists to filter by
      * @param force   do not check if the value is valid
      * @throws ClientException
      * @since 1.5
      */
     void propertyCreate(String path, String name, String value, int depth,
-                        boolean force)
+                        String[] changelists, boolean force)
             throws ClientException;
 
     /**
