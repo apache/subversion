32ed554f221c6ee5ea218643051f628c7f000bd1
Karl Fogel <kfogel@apache.org>
2008-02-02 06:00:59 +0000

    Documentation fixes.
    Patch by: Augie Fackler: <durin42@gmail.com>
              me
    
    * subversion/include/svn_opt.h
      (svn_opt_args_to_target_array3): Updated documentation to describe how the
        errors work, and make this the version against which the old is compared.
      (svn_opt_args_to_target_array2): Update the documentation so this describes
        how it is different from svn_opt_args_to_target_array3().
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@869237 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_opt.h
+++ subversion/include/svn_opt.h
@@ -477,27 +477,34 @@ svn_opt_resolve_revisions(svn_opt_revision_t *peg_rev,
  *
  * On each URL target, do some IRI-to-URI encoding and some
  * auto-escaping.  On each local path, canonicalize case and path
- * separators, and silently skip it if it has the same name as a
- * Subversion working copy administrative directory.
+ * separators.
  *
  * Allocate @a *targets_p and its elements in @a pool.
  *
- * @since New in 1.2.
- */
-svn_error_t *
-svn_opt_args_to_target_array2(apr_array_header_t **targets_p,
-                              apr_getopt_t *os,
-                              apr_array_header_t *known_targets,
-                              apr_pool_t *pool);
-
-/**
- * This is the same as svn_opt_args_to_target_array2() except that it returns
- * errors when a path has the same name as a working copy directory.
+ * If a path has the same name as a Subversion working copy
+ * administrative directory, return SVN_ERR_RESERVED_FILENAME_SPECIFIED;
+ * if multiple reserved paths are encountered, return a chain of
+ * errors, all of which are SVN_ERR_RESERVED_FILENAME_SPECIFIED.
+ * If this is the only type of error encountered, complete the
+ * operation before returning the error(s).
  *
  * @since New in 1.5.
  */
 svn_error_t *
 svn_opt_args_to_target_array3(apr_array_header_t **targets_p,
+                              apr_getopt_t *os,
+                              apr_array_header_t *known_targets,
+                              apr_pool_t *pool);
+
+/**
+ * This is the same as svn_opt_args_to_target_array3() except that it
+ * silently ignores paths that have the same name as a working copy
+ * administrative directory.
+ *
+ * @since New in 1.2.
+ */
+svn_error_t *
+svn_opt_args_to_target_array2(apr_array_header_t **targets_p,
                               apr_getopt_t *os,
                               apr_array_header_t *known_targets,
                               apr_pool_t *pool);
