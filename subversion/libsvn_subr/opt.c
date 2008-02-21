187317c3e045a03befe8220527d314490d1ccc49
epg <dev-null@apache.org>
2008-02-21 22:59:42 +0000

    Support --with-revprop in svnmucc.
    * contrib/client-side/svnmucc/svnmucc.c
      (commit_callback): Upgrade to svn_commit_callback2_t interface.
      (execute): Take revprops hash instead of log message argument and pass
        along to svn_ra_get_commit_editor3, not svn_ra_get_commit_editor.
      (usage): Document --with-revprop option.
      (main): Parse --with-revprop option, integrate with log message options,
        and pass along to execute.
    
    * subversion/libsvn_subr/opt.c
      (svn_opt_parse_revprop): Move parse_revprop from subversion/svn/main.c
        here; rename revprop_pair to revprop_spec, as it's not a pair
        of revprops.
    
    * subversion/include/svn_opt.h
      (svn_opt_parse_revprop): Declare.
    
    * subversion/svn/main.c
      (parse_revprop): Remove.
      (main): Use svn_opt_parse_revprop instead of parse_revprop.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@869566 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/opt.c
+++ subversion/libsvn_subr/opt.c
@@ -1104,6 +1104,44 @@ svn_opt_args_to_target_array(apr_array_header_t **targets_p,
 }
 
 
+svn_error_t *
+svn_opt_parse_revprop(apr_hash_t **revprop_table_p, const char *revprop_spec,
+                      apr_pool_t *pool)
+{
+  const char *sep, *propname;
+  svn_string_t *propval;
+
+  if (! *revprop_spec)
+    return svn_error_create(SVN_ERR_CL_ARG_PARSING_ERROR, NULL,
+                            _("Revision property pair is empty"));
+
+  if (! *revprop_table_p)
+    *revprop_table_p = apr_hash_make(pool);
+
+  sep = strchr(revprop_spec, '=');
+  if (sep)
+    {
+      propname = apr_pstrndup(pool, revprop_spec, sep - revprop_spec);
+      SVN_ERR(svn_utf_cstring_to_utf8(&propname, propname, pool));
+      propval = svn_string_create(sep + 1, pool);
+    }
+  else
+    {
+      SVN_ERR(svn_utf_cstring_to_utf8(&propname, revprop_spec, pool));
+      propval = svn_string_create("", pool);
+    }
+
+  if (!svn_prop_name_is_valid(propname))
+    return svn_error_createf(SVN_ERR_CLIENT_PROPERTY_NAME, NULL,
+                             _("'%s' is not a valid Subversion property name"),
+                             propname);
+
+  apr_hash_set(*revprop_table_p, propname, APR_HASH_KEY_STRING, propval);
+
+  return SVN_NO_ERROR;
+}
+
+
 /* Print version info for PGM_NAME.  If QUIET is  true, print in
  * brief.  Else if QUIET is not true, print the version more
  * verbosely, and if FOOTER is non-null, print it following the
