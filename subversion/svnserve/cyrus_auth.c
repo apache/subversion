aa99fc4171bbd77e1b7ccae86dcfaac9721012c5
Hyrum Kurt Wright <hwright@apache.org>
2008-03-07 04:09:46 +0000

    Merge r29659, r29663, r29664 from trunk:
      * r29659, r29663, r29664
        Fix error leaks in the svnserve Cyrus SASL implementation.
        Votes:
          +1: glasser (via dev mail), hwright
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/1.5.x@869834 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svnserve/cyrus_auth.c
+++ subversion/svnserve/cyrus_auth.c
@@ -132,13 +132,24 @@ fail_auth(svn_ra_svn_conn_t *conn, apr_pool_t *pool, sasl_conn_t *sasl_ctx)
   return svn_ra_svn_flush(conn, pool);
 }
 
+/* Like svn_ra_svn_write_cmd_failure, but also clears the given error
+   and sets it to SVN_NO_ERROR. */
+static svn_error_t *
+write_failure(svn_ra_svn_conn_t *conn, apr_pool_t *pool, svn_error_t **err_p)
+{
+  svn_error_t *write_err = svn_ra_svn_write_cmd_failure(conn, pool, *err_p);
+  svn_error_clear(*err_p);
+  *err_p = SVN_NO_ERROR;
+  return write_err;
+}
+
 /* Used if we run into a SASL error outside try_auth(). */
 static svn_error_t *
 fail_cmd(svn_ra_svn_conn_t *conn, apr_pool_t *pool, sasl_conn_t *sasl_ctx)
 {
   svn_error_t *err = svn_error_create(SVN_ERR_RA_NOT_AUTHORIZED, NULL,
                                       sasl_errdetail(sasl_ctx));
-  SVN_ERR(svn_ra_svn_write_cmd_failure(conn, pool, err));
+  SVN_ERR(write_failure(conn, pool, &err));
   return svn_ra_svn_flush(conn, pool);
 }
 
@@ -241,7 +252,7 @@ svn_error_t *cyrus_auth_request(svn_ra_svn_conn_t *conn,
   if (apr_err)
     {
       svn_error_t *err = svn_error_wrap_apr(apr_err, _("Can't get hostname"));
-      SVN_ERR(svn_ra_svn_write_cmd_failure(conn, pool, err));
+      SVN_ERR(write_failure(conn, pool, &err));
       return svn_ra_svn_flush(conn, pool);
     }
 
@@ -256,7 +267,7 @@ svn_error_t *cyrus_auth_request(svn_ra_svn_conn_t *conn,
     {
       svn_error_t *err = svn_error_create(SVN_ERR_RA_NOT_AUTHORIZED, NULL,
                                           sasl_errstring(result, NULL, NULL));
-      SVN_ERR(svn_ra_svn_write_cmd_failure(conn, pool, err));
+      SVN_ERR(write_failure(conn, pool, &err));
       return svn_ra_svn_flush(conn, pool);
     }
 
@@ -310,7 +321,7 @@ svn_error_t *cyrus_auth_request(svn_ra_svn_conn_t *conn,
       svn_error_t *err = svn_error_create(SVN_ERR_RA_NOT_AUTHORIZED, NULL,
                                           _("Could not obtain the list"
                                           " of SASL mechanisms"));
-      SVN_ERR(svn_ra_svn_write_cmd_failure(conn, pool, err));
+      SVN_ERR(write_failure(conn, pool, &err));
       return svn_ra_svn_flush(conn, pool);
     }
 
@@ -350,7 +361,7 @@ svn_error_t *cyrus_auth_request(svn_ra_svn_conn_t *conn,
           err = svn_error_create(SVN_ERR_RA_NOT_AUTHORIZED, NULL,
                                  _("Couldn't obtain the authenticated"
                                  " username"));
-          SVN_ERR(svn_ra_svn_write_cmd_failure(conn, pool, err));
+          SVN_ERR(write_failure(conn, pool, &err));
           return svn_ra_svn_flush(conn, pool);
         }
     }
