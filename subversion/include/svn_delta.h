f9e22783b8452608ac4b4114df9a87515bbcba08
Karl Fogel <kfogel@apache.org>
2008-04-03 18:38:01 +0000

    * subversion/include/svn_delta.h   (svn_delta_editor_t): Add note about how to construct these.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@870302 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_delta.h
+++ subversion/include/svn_delta.h
@@ -524,6 +524,10 @@ svn_error_t *svn_txdelta_skip_svndiff_window(apr_file_t *file,
 /** A structure full of callback functions the delta source will invoke
  * as it produces the delta.
  *
+ * Note: Don't try to allocate one of these yourself.  Instead, always
+ * use svn_delta_default_editor() or some other constructor, to ensure
+ * that unused slots are filled in with no-op functions.
+ *
  * <h3>Function Usage</h3>
  *
  * Here's how to use these functions to express a tree delta.
