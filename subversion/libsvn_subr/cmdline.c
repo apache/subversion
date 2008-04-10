c00ea1624a938f4cbcdb39be23b8be88b583838e
Lieven Govaerts <lgo@apache.org>
2008-04-10 08:59:02 +0000

    Import current state of the Gnome Keyring integration code.
    This is not going to be lots of code, I'll write a decent commit
    message when merging the branch back to trunk.
    
    What works:
    - keyring integration for password
    TODO:
    - fix configuration
    - add non-interactive support (as much as possible)
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/gnome-keyring@870559 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/cmdline.c
+++ subversion/libsvn_subr/cmdline.c
@@ -381,6 +381,10 @@ svn_cmdline_setup_auth_baton(svn_auth_baton_t **ab,
   svn_auth_get_keychain_simple_provider(&provider, pool);
   APR_ARRAY_PUSH(providers, svn_auth_provider_object_t *) = provider;
 #endif
+#ifdef SVN_HAVE_GNOME_KEYRING_SERVICES
+  svn_auth_get_gnome_keyring_simple_provider(&provider, pool);
+  APR_ARRAY_PUSH(providers, svn_auth_provider_object_t *) = provider;
+#endif
   svn_auth_get_simple_provider(&provider, pool);
   APR_ARRAY_PUSH(providers, svn_auth_provider_object_t *) = provider;
   svn_auth_get_username_provider(&provider, pool);
