4946542a5428733559ce9b7ed4e929df2ba8beb7
epg <epg@unknown>
2008-04-10 21:22:07 +0000

    * tools/server-side/test_svn_dav_log_parse.py   When reconstructing a davautocheck ops line, don't put the trailing     text back, as there should be none.  Now this test can detect     failures such as my failure to teach the parser about get-mergeinfo's     include-descendants boolean.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@870595 13f79535-47bb-0310-9956-ffa450edef68

--- tools/server-side/test_svn_dav_log_parse.py
+++ tools/server-side/test_svn_dav_log_parse.py
@@ -404,9 +404,10 @@ if __name__ == '__main__':
             leading = ' '.join(words[:4])
             action = ' '.join(words[4:])
             # Parse the action and write the reconstructed action to
-            # the temporary file.
-            trailing = parser.parse(action)
-            fp.write(leading + ' ' + parser.action + trailing + '\n')
+            # the temporary file.  Ignore the returned trailing text,
+            # as we have none in the davautocheck ops log.
+            parser.parse(action)
+            fp.write(leading + ' ' + parser.action + '\n')
         fp.close()
         # Check differences between original and reconstructed files
         # (should be identical).
