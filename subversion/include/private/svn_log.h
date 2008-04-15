ede8cd17d8b63ef11aac0d95ca4ba71f5a9b3074
epg <dev-null@apache.org>
2008-04-15 20:16:28 +0000

    * subversion/include/private/svn_log.h   Add simple doc strings for these functions.
    * TODO.branch
      Mark this item done.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/svnserve-logging@870689 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/private/svn_log.h
+++ subversion/include/private/svn_log.h
@@ -28,55 +28,115 @@
 extern "C" {
 #endif /* __cplusplus */
 
+/**
+ * Return a log string for a change-rev-prop action.
+ *
+ * @since New in 1.6.
+ */
 const char *
 svn_log__change_rev_prop(svn_revnum_t rev, const char *name, apr_pool_t *pool);
 
+/**
+ * Return a log string for a rev-proplist action.
+ *
+ * @since New in 1.6.
+ */
 const char *
 svn_log__rev_proplist(svn_revnum_t rev, apr_pool_t *pool);
 
+/**
+ * Return a log string for a rev-prop action.
+ *
+ * @since New in 1.6.
+ */
 const char *
 svn_log__rev_prop(svn_revnum_t rev, const char *name, apr_pool_t *pool);
 
+/**
+ * Return a log string for a commit action.
+ *
+ * @since New in 1.6.
+ */
 const char *
 svn_log__commit(svn_revnum_t rev, apr_pool_t *pool);
 
+/**
+ * Return a log string for a get-file action.
+ *
+ * @since New in 1.6.
+ */
 const char *
 svn_log__get_file(const char *path, svn_revnum_t rev,
                   svn_boolean_t want_contents, svn_boolean_t want_props,
                   apr_pool_t *pool);
 
+/**
+ * Return a log string for a get-dir action.
+ *
+ * @since New in 1.6.
+ */
 const char *
 svn_log__get_dir(const char *path, svn_revnum_t rev,
                  svn_boolean_t want_contents, svn_boolean_t want_props,
                  apr_uint64_t dirent_fields,
                  apr_pool_t *pool);
 
+/**
+ * Return a log string for a get-mergeinfo action.
+ *
+ * @since New in 1.6.
+ */
 const char *
 svn_log__get_mergeinfo(const apr_array_header_t *paths,
                        svn_mergeinfo_inheritance_t inherit,
                        svn_boolean_t include_descendants,
                        apr_pool_t *pool);
 
+/**
+ * Return a log string for a checkout/update action.
+ *
+ * @since New in 1.6.
+ */
 const char *
 svn_log__update(const char *path, svn_revnum_t rev, svn_depth_t depth,
                 svn_boolean_t send_copyfrom_args,
                 int entry_count, svn_boolean_t entry_is_empty,
                 apr_pool_t *pool);
 
+/**
+ * Return a log string for a switch action.
+ *
+ * @since New in 1.6.
+ */
 const char *
 svn_log__switch(const char *path, const char *dst_path, svn_revnum_t revnum,
                 svn_depth_t depth, apr_pool_t *pool);
 
+/**
+ * Return a log string for a status action.
+ *
+ * @since New in 1.6.
+ */
 const char *
 svn_log__status(const char *path, svn_revnum_t rev, svn_depth_t depth,
                 apr_pool_t *pool);
 
+/**
+ * Return a log string for a diff action.
+ *
+ * @since New in 1.6.
+ */
 const char *
 svn_log__diff(const char *path, svn_revnum_t from_revnum,
               const char *dst_path, svn_revnum_t revnum,
               svn_depth_t depth, svn_boolean_t ignore_ancestry,
               apr_pool_t *pool);
 
+/**
+ * Return a log string for a log action.
+ *
+ * @since New in 1.6.
+ */
 const char *
 svn_log__log(const apr_array_header_t *paths,
              svn_revnum_t start, svn_revnum_t end,
@@ -85,37 +145,79 @@ svn_log__log(const apr_array_header_t *paths,
              svn_boolean_t include_merged_revisions,
              const apr_array_header_t *revprops, apr_pool_t *pool);
 
+/**
+ * Return a log string for a get-locations action.
+ *
+ * @since New in 1.6.
+ */
 const char *
 svn_log__get_locations(const char *path, svn_revnum_t peg_revision,
                        const apr_array_header_t *location_revisions,
                        apr_pool_t *pool);
 
+/**
+ * Return a log string for a get-location-segments action.
+ *
+ * @since New in 1.6.
+ */
 const char *
 svn_log__get_location_segments(const char *path, svn_revnum_t peg_revision,
                                svn_revnum_t start, svn_revnum_t end,
                                apr_pool_t *pool);
 
+/**
+ * Return a log string for a get-file-revs action.
+ *
+ * @since New in 1.6.
+ */
 const char *
 svn_log__get_file_revs(const char *path, svn_revnum_t start, svn_revnum_t end,
                        svn_boolean_t include_merged_revisions,
                        apr_pool_t *pool);
 
+/**
+ * Return a log string for a lock action.
+ *
+ * @since New in 1.6.
+ */
 const char *
 svn_log__lock(const apr_array_header_t *paths, svn_boolean_t steal,
               apr_pool_t *pool);
 
+/**
+ * Return a log string for an unlock action.
+ *
+ * @since New in 1.6.
+ */
 const char *
 svn_log__unlock(const apr_array_header_t *paths, svn_boolean_t break_lock,
                 apr_pool_t *pool);
 
+/**
+ * Return a log string for a lock action on only one path; this is
+ * just a convenience wrapper around svn_log__lock().
+ *
+ * @since New in 1.6.
+ */
 const char *
 svn_log__lock_one_path(const char *path, svn_boolean_t steal,
                        apr_pool_t *pool);
 
+/**
+ * Return a log string for a unlock action on only one path; this is
+ * just a convenience wrapper around svn_log__unlock().
+ *
+ * @since New in 1.6.
+ */
 const char *
 svn_log__unlock_one_path(const char *path, svn_boolean_t break_lock,
                          apr_pool_t *pool);
 
+/**
+ * Return a log string for a replay action.
+ *
+ * @since New in 1.6.
+ */
 const char *
 svn_log__replay(const char *path, svn_revnum_t rev, apr_pool_t *pool);
 
