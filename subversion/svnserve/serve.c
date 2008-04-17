a08eba8e372034d3fac9c127f2901110e104e132
epg <dev-null@apache.org>
2008-04-17 23:35:26 +0000

    * subversion/svnserve/log-escape.c   Add file for log escaping functions copied from Apache httpd.
    * subversion/svnserve/serve.c
      Remove functions moved to log-escape.c
      (log_fs_warning): Hard-code 8192 but say where it came from.
    
    * subversion/svnserve/server.h
      (escape_errorlog_item): Add prototype.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/svnserve-logging@870741 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svnserve/serve.c
+++ subversion/svnserve/serve.c
@@ -161,104 +161,6 @@ static svn_error_t *get_fs_path(const char *repos_url, const char *url,
   return SVN_NO_ERROR;
 }
 
-/* copied from httpd-2.2.4/include/httpd.h */
-#define MAX_STRING_LEN 8192
-
-/* copied from httpd-2.2.4/server/util.c */
-/* c2x takes an unsigned, and expects the caller has guaranteed that
- * 0 <= what < 256... which usually means that you have to cast to
- * unsigned char first, because (unsigned)(char)(x) first goes through
- * signed extension to an int before the unsigned cast.
- *
- * The reason for this assumption is to assist gcc code generation --
- * the unsigned char -> unsigned extension is already done earlier in
- * both uses of this code, so there's no need to waste time doing it
- * again.
- */
-static const char c2x_table[] = "0123456789abcdef";
-
-/* copied from httpd-2.2.4/server/util.c */
-static APR_INLINE unsigned char *c2x(unsigned what, unsigned char prefix,
-                                     unsigned char *where)
-{
-#if APR_CHARSET_EBCDIC
-    what = apr_xlate_conv_byte(ap_hdrs_to_ascii, (unsigned char)what);
-#endif /*APR_CHARSET_EBCDIC*/
-    *where++ = prefix;
-    *where++ = c2x_table[what >> 4];
-    *where++ = c2x_table[what & 0xf];
-    return where;
-}
-
-/* copied from httpd-2.2.4/server/util.c */
-static apr_size_t escape_errorlog_item(char *dest, const char *source,
-                                               apr_size_t buflen)
-{
-    unsigned char *d, *ep;
-    const unsigned char *s;
-
-    if (!source || !buflen) { /* be safe */
-        return 0;
-    }
-
-    d = (unsigned char *)dest;
-    s = (const unsigned char *)source;
-    ep = d + buflen - 1;
-
-    for (; d < ep && *s; ++s) {
-
-        /* httpd-2.2.4/server/util.c has this:
-             if (TEST_CHAR(*s, T_ESCAPE_LOGITEM)) {
-           which does this same check with a fast lookup table.  Well,
-           mostly the same; we don't escape quotes, as that does.
-        */
-        if (*s && (!apr_isprint(*s) || *s == '\\' || apr_iscntrl(*s))) {
-            *d++ = '\\';
-            if (d >= ep) {
-                --d;
-                break;
-            }
-
-            switch(*s) {
-            case '\b':
-                *d++ = 'b';
-                break;
-            case '\n':
-                *d++ = 'n';
-                break;
-            case '\r':
-                *d++ = 'r';
-                break;
-            case '\t':
-                *d++ = 't';
-                break;
-            case '\v':
-                *d++ = 'v';
-                break;
-            case '\\':
-                *d++ = *s;
-                break;
-            case '"': /* no need for this in error log */
-                d[-1] = *s;
-                break;
-            default:
-                if (d >= ep - 2) {
-                    ep = --d; /* break the for loop as well */
-                    break;
-                }
-                c2x(*s, 'x', d);
-                d += 3;
-            }
-        }
-        else {
-            *d++ = *s;
-        }
-    }
-    *d = '\0';
-
-    return (d - (unsigned char *)dest);
-}
-
 static void
 log_fs_warning(void *baton, svn_error_t *err)
 {
@@ -267,7 +169,8 @@ log_fs_warning(void *baton, svn_error_t *err)
   svn_ra_svn_conn_t *conn = b->conn;
   const char *timestr, *remote_host, *user, *continuation;
   char errbuf[256];
-  char errstr[MAX_STRING_LEN];
+  /* 8192 from MAX_STRING_LEN in from httpd-2.2.4/include/httpd.h */
+  char errstr[8192];
 
   if (server->log_file == NULL)
     return;
