3d2e3e1089291a816a8d7e868dcf31c852c266ff
Arfrever Frehtes Taifersar Arahesis <arfrever@apache.org>
2008-05-11 17:35:57 +0000

    On the 'kwallet' branch: Fix build failure on Windows.
    * subversion/include/svn_auth.h
      (svn_auth_get_kwallet_simple_provider): Move from here ...
    * subversion/include/svn_auth_kwallet.h
      (subversion/include/svn_auth_kwallet.h): ... to here.
    
    * subversion/libsvn_subr_kwallet/kwallet.cpp: Include "svn_auth_kwallet.h".
    
    Tested by: danielsh
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/kwallet@871197 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subversion/include/svn_auth_kwallet.h
@@ -0,0 +1,54 @@
+/**
+ * @copyright
+ * ====================================================================
+ * Copyright (c) 2008 CollabNet.  All rights reserved.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at http://subversion.tigris.org/license-1.html.
+ * If newer versions of this license are posted there, you may use a
+ * newer version instead, at your option.
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals.  For exact contribution history, see the revision
+ * history and logs, available at http://subversion.tigris.org/.
+ * ====================================================================
+ * @endcopyright
+ *
+ * @file svn_auth_kwallet.h
+ * @brief Subversion's authentication system - KWallet
+ */
+
+#ifndef SVN_AUTH_KWALLET_H
+#define SVN_AUTH_KWALLET_H
+
+#include <apr_pools.h>
+
+#include "svn_auth.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+/**
+ * Create and return @a *provider, an authentication provider of type @c
+ * svn_auth_cred_simple_t that gets/sets information from the user's
+ * ~/.subversion configuration directory.  Allocate @a *provider in
+ * @a pool.
+ *
+ * This is like svn_client_get_simple_provider(), except that the
+ * password is stored in KWallet.
+ *
+ * @since New in 1.6
+ * @note This function actually works only on systems with libsvn_subr_kwallet
+ * and KWallet installed.
+ */
+svn_auth_simple_provider_func_t
+svn_auth_get_kwallet_simple_provider(svn_auth_provider_object_t **provider,
+                                     apr_pool_t *pool);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* SVN_AUTH_KWALLET_H */
