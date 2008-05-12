1c1d20d22f8f2b7307e7c36407332de4b81338bc
Daniel Rall <dlr@apache.org>
2008-05-12 20:24:33 +0000

    * subversion/libsvn_ra_serf/serf.c (svn_ra_serf__get_dir): Fix formatting.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@871217 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_ra_serf/serf.c
+++ subversion/libsvn_ra_serf/serf.c
@@ -1024,9 +1024,10 @@ svn_ra_serf__get_dir(svn_ra_session_t *ra_session,
                                          path, pool));
 
       /* If we don't have the latest revision, we have to fetch it. */
-      if (!SVN_IS_VALID_REVNUM(revision)) {
-        SVN_ERR(svn_ra_serf__get_latest_revnum(ra_session, &revision, pool));
-      }
+      if (!SVN_IS_VALID_REVNUM(revision))
+        {
+          SVN_ERR(svn_ra_serf__get_latest_revnum(ra_session, &revision, pool));
+        }
 
       SVN_ERR(svn_ra_serf__retrieve_props(props, session, session->conns[0],
                                           vcc_url, revision,
