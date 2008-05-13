70330de2161f289e081eba54e8fba1f1f6630f59
C. Michael Pilato <cmpilato@apache.org>
2008-05-13 17:23:37 +0000

    Use the natural history of the claimed merge sources as a filter to avoid storing bogus migrated mergeinfo.
    (Read:  "Too bad svnmerge.py is rename- and history-ignorant.")
    
    * contrib/client-side/svnmerge/svnmerge-migrate-history.py
      (mergeinfo2str): New convenience shortcut.
      (Migrator.flatten_prop): New helper function.
      (Migrator.convert_path_history): Use flatten_prop() for more
        readable output.  Now, when not in "naive mode", use the natural
        history of the merge sources to ensure only valid stuff gets into
        the final to-be-stored mergeinfo.
      (Migrator.get_natural_history): Now accepts a lower-bounds
        revision.  Callers updated.
    
    * contrib/client-side/svnmerge/svnmerge-migrate-test.sh
      Basically, rewrite this whole thing to not use bogus mergeinfo, and
      to allow for two passes -- one in naive-mode, one not.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@871227 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/client-side/svnmerge/svnmerge-migrate-history.py
+++ contrib/client-side/svnmerge/svnmerge-migrate-history.py
@@ -47,6 +47,9 @@ except ImportError, e:
         "location's svn-python directory." % e
   sys.exit(1)
 
+# Convenience shortcut.
+mergeinfo2str = svn.core.svn_mergeinfo_to_string
+
 # Pretend we have boolean data types for older Python versions.
 try:
   True
@@ -125,6 +128,9 @@ class Migrator:
       self.process_dir(root, revnum, leading_paths[i],
                        prefix[len(prefix) - 1] + ".*")
 
+  def flatten_prop(self, propval):
+    return '\\n'.join(propval.split('\n'))
+
   def process_dir(self, root, revnum, dir_path, pattern=None):
     "Recursively process children of DIR_PATH."
     dirents = svn.fs.dir_entries(root, dir_path)
@@ -150,14 +156,14 @@ class Migrator:
                                           svn.core.SVN_PROP_MERGEINFO)
     if mergeinfo_prop_val is not None:
       self.log("Discovered pre-existing Subversion mergeinfo of '%s'" \
-               % (mergeinfo_prop_val))
+               % (self.flatten_prop(mergeinfo_prop_val)))
       
     # Retrieve svnmerge.py's merge history meta data, and roll it into
     # Subversion 1.5 mergeinfo.
     integrated_prop_val = svn.fs.node_prop(root, path, "svnmerge-integrated")
     if integrated_prop_val is not None:
       self.log("Discovered svnmerge.py mergeinfo of '%s'" \
-               % (integrated_prop_val))
+               % (self.flatten_prop(integrated_prop_val)))
       
     ### LATER: We handle svnmerge-blocked by converting it into
     ### svn:mergeinfo, until revision blocking becomes available in
@@ -165,7 +171,7 @@ class Migrator:
     blocked_prop_val = svn.fs.node_prop(root, path, "svnmerge-blocked")
     if blocked_prop_val is not None:
       self.log("Discovered svnmerge.py blocked revisions of '%s'" \
-               % (blocked_prop_val))
+               % (self.flatten_prop(blocked_prop_val)))
 
     # Convert property values into real mergeinfo structures.
     svn_mergeinfo = None
@@ -178,41 +184,70 @@ class Migrator:
     new_mergeinfo = self.mergeinfo_merge(svn_mergeinfo, integrated_mergeinfo)
     new_mergeinfo = self.mergeinfo_merge(new_mergeinfo, blocked_mergeinfo)
 
-    # Unless we're doing a naive migration, start trying to cleanup
-    # after svnmerge.py's history-ignorant initialization.
-    if not self.naive_mode:
+    if new_mergeinfo is not None:
+      self.log("Combined mergeinfo is '%s'" \
+               % (self.flatten_prop(mergeinfo2str(new_mergeinfo))))
+      
+    # Unless we're doing a naive migration (or we've no, or only
+    # empty, mergeinfo anyway, start trying to cleanup after
+    # svnmerge.py's history-ignorant initialization.
+    if not self.naive_mode and new_mergeinfo:
 
       # We begin by subtracting the natural history of the merge
       # target from its own mergeinfo.
-      if new_mergeinfo is not None:
-        implicit_mergeinfo = self.get_natural_history(root, path)
-        implicit_mergeinfo_prop_val = \
-          svn.core.svn_mergeinfo_to_string(implicit_mergeinfo)
-        self.log("Subtracting natural mergeinfo of '%s'" \
-                 % (implicit_mergeinfo_prop_val))
-        new_mergeinfo = svn.core.svn_mergeinfo_remove(implicit_mergeinfo,
-                                                      new_mergeinfo)
+      rev = svn.fs.revision_root_revision(root)
+      implicit_mergeinfo = self.get_natural_history(path, rev)
+      self.log("Subtracting natural mergeinfo of '%s'" \
+               % (self.flatten_prop(mergeinfo2str(implicit_mergeinfo))))
+      new_mergeinfo = svn.core.svn_mergeinfo_remove(implicit_mergeinfo,
+                                                    new_mergeinfo)
+      self.log("Remaining mergeinfo is '%s'" \
+               % (self.flatten_prop(mergeinfo2str(new_mergeinfo))))
 
-      ### Unfortunately, svnmerge.py tends to initialize using
-      ### oft-bogus revision ranges like 1-SOMETHING when the merge
-      ### source didn't even exist in r1.  So if the natural history
-      ### of a branch begins in some revision other than r1, there's
-      ### still going to be cruft revisions left in NEW_MERGEINFO
-      ### after subtracting the natural history.
-      ###
-      ### Not really sure how to handle this correctly...
-        
+      # Unfortunately, svnmerge.py tends to initialize using oft-bogus
+      # revision ranges like 1-SOMETHING when the merge source didn't
+      # even exist in r1.  So if the natural history of a branch
+      # begins in some revision other than r1, there's still going to
+      # be cruft revisions left in NEW_MERGEINFO after subtracting the
+      # natural history.  So, we also examine the natural history of
+      # the merge sources, and use that as a filter for the explicit
+      # mergeinfo we've calculated so far.
+      self.log("Filtering mergeinfo by recontruction from source history ...")
+      filtered_mergeinfo = {}
+      for source_path, ranges in new_mergeinfo.items():
+        ### If by some chance it is the case that /path:RANGE1 and
+        ### /path:RANGE2 a) represent different lines of history, and
+        ### b) were combined into /path:RANGE1+RANGE2 (due to the
+        ### ranges being contiguous), we'll foul this up.  But the
+        ### chances are preeeeeeeetty slim.
+        for range in ranges:
+          try:
+            source_history = self.get_natural_history(source_path,
+                                                      range.end,
+                                                      range.start + 1)
+            self.log("... adding '%s'" \
+                     % (self.flatten_prop(mergeinfo2str(source_history))))
+            filtered_mergeinfo = \
+                svn.core.svn_mergeinfo_merge(filtered_mergeinfo,
+                                             source_history)
+          except svn.core.SubversionException, e:
+            if not (e.apr_err == svn.core.SVN_ERR_FS_NOT_FOUND
+                    or e.apr_err == svn.core.SVN_ERR_FS_NO_SUCH_REVISION):
+              raise
+      self.log("... done.")
+      new_mergeinfo = filtered_mergeinfo
 
     # Turn our to-be-written mergeinfo back into a property value.
     new_mergeinfo_prop_val = None
     if new_mergeinfo is not None:
-      new_mergeinfo_prop_val = svn.core.svn_mergeinfo_to_string(new_mergeinfo)
+      new_mergeinfo_prop_val = mergeinfo2str(new_mergeinfo)
       
     # If we need to change the value of the svn:mergeinfo property or
     # delete any svnmerge-* properties, let's do so.
     if (new_mergeinfo_prop_val != mergeinfo_prop_val) \
        or (integrated_prop_val is not None) \
        or (blocked_prop_val is not None):
+      
       # If this not a dry-run, begin a transaction in which we'll
       # manipulate merge-related properties.  Open the transaction root.
       if not self.dry_run:
@@ -222,7 +257,8 @@ class Migrator:
       # Manipulate the merge history.
       if new_mergeinfo_prop_val != mergeinfo_prop_val:
         self.log("Queuing change of %s to '%s'"
-                 % (svn.core.SVN_PROP_MERGEINFO, new_mergeinfo_prop_val))
+                 % (svn.core.SVN_PROP_MERGEINFO,
+                    self.flatten_prop(new_mergeinfo_prop_val)))
         if not self.dry_run:
           svn.fs.change_node_prop(root, path, svn.core.SVN_PROP_MERGEINFO,
                                   new_mergeinfo_prop_val)
@@ -283,18 +319,19 @@ class Migrator:
       return mergeinfo1
     return svn.core.svn_mergeinfo_merge(mergeinfo1, mergeinfo2)
 
-  def get_natural_history(self, root, path):
-    """Return the natural history of PATH in ROOT as mergeinfo.
+  def get_natural_history(self, path, rev,
+                          oldest_rev=svn.core.SVN_INVALID_REVNUM):
+    """Return the natural history of PATH in REV, between OLDEST_REV
+    and REV, as mergeinfo.  If OLDEST_REV is svn.core.SVN_INVALID_REVNUM,
+    all of PATH's history prior to REV will be returned.
     (Adapted from Subversion's svn_client__get_history_as_mergeinfo().)"""
+    
     location_segments = []
-    rev = svn.fs.revision_root_revision(root)
-
     def _allow_all(root, path, pool):
       return 1
     def _segment_receiver(segment, pool):
       location_segments.append(segment)
-    svn.repos.node_location_segments(self.repos, path, rev, rev,
-                                     svn.core.SVN_INVALID_REVNUM,
+    svn.repos.node_location_segments(self.repos, path, rev, rev, oldest_rev,
                                      _segment_receiver, _allow_all)
 
     # Translate location segments into merge sources and ranges.
