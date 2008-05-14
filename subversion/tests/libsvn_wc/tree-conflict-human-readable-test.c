e5cf7cc496c60c6f4529b65a3b20b8b3fad63c1c
Julian Foad <julianfoad@apache.org>
2008-05-14 11:26:08 +0000

    Fix some tree-conflict tests that were failing just because of a mis-match of expectations with the current implementation.
    * subversion/tests/libsvn_wc/tree-conflict-data-test.c
      (test_write_invalid_tree_conflicts): To specify an invalid action or
        reason, use a silly number instead of a symbolic constant that the
        implementation might now or in future handle as a valid argument.
    * subversion/tests/libsvn_wc/tree-conflict-human-readable-test.c
      (test_get_one_human_readable_tree_conflict_description,
       write_test_expected_output):
        Update the expected output text to match the current implementation.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/tree-conflicts@871235 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/libsvn_wc/tree-conflict-human-readable-test.c
+++ subversion/tests/libsvn_wc/tree-conflict-human-readable-test.c
@@ -53,7 +53,7 @@ test_get_one_human_readable_tree_conflict_description(const char **msg,
    * If subversion/libsvn_wc/tree_conflicts.c:new_tree_conflict_phrases()
    * is changed, don't forget to update this string!
    */
-  expected = "The update attempted to delete the file 'Foo.c'\n"
+  expected = "The update attempted to delete 'Foo.c'\n"
              "(possibly as part of a rename operation).\n"
              "You have edited 'Foo.c' locally.\n";
 
@@ -146,18 +146,18 @@ static struct svn_wc_conflict_description_t write_test_descriptions[] = {
  */
 static const char* write_test_expected_output =
   /* Test 1 */
-  "The update attempted to delete the file 'Foo.c'\n"
+  "The update attempted to delete 'Foo.c'\n"
   "(possibly as part of a rename operation).\n"
   "You have edited 'Foo.c' locally.\n"
   "\n"
   /* Test 2 */
-  "The update attempted to edit the file 'Foo.c'.\n"
+  "The update attempted to edit 'Foo.c'.\n"
   "You have deleted 'Foo.c' locally.\n"
   "Maybe you renamed it?\n"
   "\n"
   /* Test 3 */
-  "The merge attempted to edit the file 'Foo.c'.\n"
-  "The file 'Foo.c' does not exist locally.\n"
+  "The merge attempted to edit 'Foo.c'.\n"
+  "'Foo.c' does not exist locally.\n"
   "Maybe you renamed it? Or has it been"
   " renamed in the history of the branch\n"
   "you are merging into?\n"
