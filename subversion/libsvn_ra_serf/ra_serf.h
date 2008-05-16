da39b25a6dbd5226430cca2958ccc973c22f475d
Lieven Govaerts <lgo@apache.org>
2008-05-16 16:44:59 +0000

    ra_serf: Remove unneeded function private to ra_serf, as its code is almost a duplicate of another function.
    * subversion/libsvn_ra_serf/ra_serf.h
      (svn_ra_serf__search_for_base_props): Remove declaration.
    
    * subversion/libsvn_ra_serf/property.c
      (svn_ra_serf__search_for_base_props): Remove definition.
    
    * subversion/libsvn_ra_serf/blame.c
      (svn_ra_serf__get_file_revs): Reimplement using svn_ra_serf__discover_root
       instead of the now removed svn_ra_serf__search_for_base_props.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@871306 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_ra_serf/ra_serf.h
+++ subversion/libsvn_ra_serf/ra_serf.h
@@ -801,27 +801,6 @@ svn_ra_serf__wait_for_props(svn_ra_serf__propfind_context_t *prop_ctx,
                             svn_ra_serf__session_t *sess,
                             apr_pool_t *pool);
 
-/* Shared helper func: given a public URL which may not exist in HEAD,
-   use SESSION to search up parent directories until we can retrieve a
-   *PROPS (allocated in POOL) containing a standard set of base props:
-   {VCC, resourcetype, baseline-relative-path}.
-
-   Also return:
-   *MISSING_PATH (allocated in POOL), which is the trailing portion of
-     the URL that did not exist.  If an error occurs, *MISSING_PATH isn't
-     changed.
-   *REMAINING_PATH (allocated in POOL), which is the parent path on which
-     we found the PROPS.
-   */
-svn_error_t *
-svn_ra_serf__search_for_base_props(apr_hash_t *props,
-                                   const char **remaining_path,
-                                   const char **missing_path,
-                                   svn_ra_serf__session_t *session,
-                                   svn_ra_serf__connection_t *conn,
-                                   const char *url,
-                                   apr_pool_t *pool);
-
 /*
  * This is a blocking version of deliver_props.
  */
