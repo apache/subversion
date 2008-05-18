2608f088a82a2cb72459f107cba67a1d23bcdbff
arfrever <dev-null@apache.org>
2008-05-18 19:31:12 +0000

    * subversion/libsvn_subr/cmdline.c   (svn_cmdline_setup_auth_baton): Fix indentation.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@871364 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/cmdline.c
+++ subversion/libsvn_subr/cmdline.c
@@ -432,9 +432,9 @@ svn_cmdline_setup_auth_baton(svn_auth_baton_t **ab,
 #endif
 #ifdef SVN_HAVE_KWALLET
   if (get_auth_simple_provider(&provider, "kwallet", pool))
-  {
-    APR_ARRAY_PUSH(providers, svn_auth_provider_object_t *) = provider;
-  }
+    {
+      APR_ARRAY_PUSH(providers, svn_auth_provider_object_t *) = provider;
+    }
 #endif
   if (non_interactive == FALSE)
     {
