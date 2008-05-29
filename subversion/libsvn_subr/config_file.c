9ebb54aae7397f2420d7c1070cd2be023893e584
arfrever <dev-null@apache.org>
2008-05-29 20:25:39 +0000

    Follow-up to r31527:
    * subversion/libsvn_subr/config_file.c
      (svn_config_ensure): Fix indentation in the default 'config' file.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@871604 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/config_file.c
+++ subversion/libsvn_subr/config_file.c
@@ -906,10 +906,10 @@ svn_config_ensure(const char *config_dir, apr_pool_t *pool)
         "### delimited by spaces or commas. The order of values determines"  NL
         "### the order in which password stores are used."                   NL
         "### Valid password stores:"                                         NL
-        "###   gnome-keyring (Unix-like systems)"                            NL
-        "###   kwallet (Unix-like systems)"                                  NL
-        "###   keychain (Mac OS X)"                                          NL
-        "###   windows-cryptoapi (Windows)"                                  NL
+        "###   gnome-keyring        (Unix-like systems)"                     NL
+        "###   kwallet              (Unix-like systems)"                     NL
+        "###   keychain             (Mac OS X)"                              NL
+        "###   windows-cryptoapi    (Windows)"                               NL
 #ifdef SVN_HAVE_KEYCHAIN_SERVICES
         "# password-stores = keychain"                                       NL
 #elif defined(WIN32) && !defined(__MINGW32__)
