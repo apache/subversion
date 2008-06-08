91c694cc69de563774caa39bc00d865853154f93
Lieven Govaerts <lgo@apache.org>
2008-06-08 21:25:04 +0000

    ra_serf: Follow up r31633. Only ignore lock related errors, not other errors - like 403 Forbidden - that happen to show up on a lock request.
    * subversion/libsvn_ra_serf/locks.c
      (svn_ra_serf__lock): Only ignore the error it it's lock related.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@871712 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_ra_serf/locks.c
+++ subversion/libsvn_ra_serf/locks.c
@@ -646,6 +646,11 @@ svn_ra_serf__lock(svn_ra_session_t *ra_session,
       svn_error_clear(err);
       svn_error_clear(parser_ctx->error);
 
+      if (!SVN_ERR_IS_LOCK_ERROR(lock_ctx->error))
+        {
+          return lock_ctx->error;
+        }
+
       if (lock_func)
         err = lock_func(lock_baton, lock_ctx->path, TRUE, lock_ctx->lock,
                         lock_ctx->error, subpool);
