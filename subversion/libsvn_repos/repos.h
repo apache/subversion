eeb8c1ec7f076154d29b3e3e3b01017fa2765c81
C. Michael Pilato <cmpilato@apache.org>
2008-07-17 14:10:23 +0000

    Finish issue #3220 ('svn log -g' seems quite slow) and, as a bonus side-effect, also finish issue #3235 ('log -g' merge determination logic faulty).
    [Merge the entirety of the issue-3220-dev branch to trunk.]
    
    Prior to this change, the method used by 'log -g' for determining what
    merges were made in a revision was:
    
       - fetch all the mergeinfo found in REV
       - fetch all the mergeinfo found in REV-1
       - compare, contrast, and collapse the results
    
    The problems with this approach are two-fold.  First, it's slow, and
    it gets slower as more paths in a revision grow mergeinfo.  Secondly,
    it's just wrong.  Just because mergeinfo appears or disappears doesn't
    mean a merge was done -- perhaps a path that held some mergeinfo was
    merely deleted.
    
    This new approach is to look at only the paths changed in REV, and see
    if/how their mergeinfo changed.  This is much, much faster on average
    for a repository actually using mergeinfo, and much more accurate as
    well.
    
    * subversion/libsvn_repos/log.c
      (fs_mergeinfo_changed): New (pretty-generic) mergeinfo change
        calculation function.
      (get_combined_mergeinfo_changes): New, supercedes ...
      (get_combined_mergeinfo, get_merged_rev_mergeinfo): ...these, which
        are now removed.
      (do_logs): Use get_combined_mergeinfo_changes() instead of
        get_merged_rev_mergeinfo().
    
    * subversion/libsvn_repos/repos.h
      (svn_repos__prev_location): New prototype.
    
    * subversion/libsvn_repos/rev_hunt.c
      (svn_repos__prev_location): Was prev_location(), now module-public.
        Allow prev_rev, prev_path, and appeared_rev parameters to be NULL,
        in which case (for each) don't try to set their pointer values.
        Callers updated.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@872227 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_repos/repos.h
+++ subversion/libsvn_repos/repos.h
@@ -273,6 +273,28 @@ svn_repos__compare_files(svn_boolean_t *changed_p,
                          const char *path2,
                          apr_pool_t *pool);
 
+/* Set *PREV_PATH and *PREV_REV to the path and revision which
+   represent the location at which PATH in FS was located immediately
+   prior to REVISION iff there was a copy operation (to PATH or one of
+   its parent directories) between that previous location and
+   PATH@REVISION, and set *APPEARED_REV to the first revision in which
+   PATH@REVISION appeared at PATH as a result of that copy operation.
+
+   If there was no such copy operation in that portion
+   of PATH's history, set *PREV_PATH to NULL, and set *PREV_REV and
+   *APPEARED_REV to SVN_INVALID_REVNUM.  
+
+   NOTE: Any of PREV_PATH, PREV_REV, and APPEARED_REV may be NULL to
+   if that information is of no interest to the caller.  */
+svn_error_t *
+svn_repos__prev_location(svn_revnum_t *appeared_rev,
+                         const char **prev_path,
+                         svn_revnum_t *prev_rev,
+                         svn_fs_t *fs,
+                         svn_revnum_t revision,
+                         const char *path,
+                         apr_pool_t *pool);
+
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
