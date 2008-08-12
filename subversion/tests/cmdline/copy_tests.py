611145f997e59255bb49b3add2df557ad0a074ce
Daniel Shahaf <danielsh@apache.org>
2008-08-12 11:53:12 +0000

    Fix copy_tests #48: copy added uncommitted paths with props.
    Patch by: me
    
    * subversion/libsvn_wc/copy.c
      (copy_props): New function.
      (copy_added_file_administratively):
        Copy src's properties to target.  Add 'src_access' parameter.
        Callers (copy_added_dir_administratively and svn_wc_copy2) updated.
      (copy_added_dir_administratively):
        Copy src's properties to target.
    
    * subversion/tests/cmdline/copy_tests.py
      (copy_added_paths_with_props):
        Expect empty mergeinfo on copy targets.  Fix comment.
      (test_list):
        Remove XFail from copy_added_paths_with_props().
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@872522 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/copy_tests.py
+++ subversion/tests/cmdline/copy_tests.py
@@ -2574,7 +2574,7 @@ def copy_added_paths_with_props(sbox):
   svntest.tree.compare_trees("disk", actual_disk_tree,
                              expected_disk.old_tree())
 
-  # Copy added dir K to dir A/C
+  # Copy added dir I to dir A/C
   I_copy_path = os.path.join(wc_dir, 'A', 'C', 'I')
   svntest.actions.run_and_verify_svn(None, None, [], 'cp',
                                      I_path, I_copy_path)
@@ -2603,9 +2603,11 @@ def copy_added_paths_with_props(sbox):
 
   # Tweak expected disk tree
   expected_disk.add({
-    'A/C/upsilon' : Item(props={'foo' : 'bar'},
+    'A/C/upsilon' : Item(props={ 'foo' : 'bar',
+                                 SVN_PROP_MERGEINFO : '' },
                          contents="This is the file 'upsilon'\n"),
-    'A/C/I'       : Item(props={'foo' : 'bar'}),
+    'A/C/I'       : Item(props={ 'foo' : 'bar',
+                                 SVN_PROP_MERGEINFO : '' }),
     })
 
   svntest.actions.run_and_verify_commit(wc_dir,
@@ -3966,7 +3968,7 @@ test_list = [ None,
               move_file_back_and_forth,
               move_dir_back_and_forth,
               copy_move_added_paths,
-              XFail(copy_added_paths_with_props),
+              copy_added_paths_with_props,
               copy_added_paths_to_URL,
               move_to_relative_paths,
               move_from_relative_paths,
