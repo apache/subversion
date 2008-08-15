1068c8fc0323098b18de76b08a2f65e41d4b4518
Paul Burba <pburba@apache.org>
2008-08-15 14:13:38 +0000

    Doc string improvement for merge.c private function.
    * subversion/libsvn_client/merge.c
      (drive_merge_report_editor): Make the doc string a bit more thorough.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@872568 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/merge.c
+++ subversion/libsvn_client/merge.c
@@ -2782,10 +2782,31 @@ remove_absent_children(const char *target_wcpath,
 
 /* Helper for do_directory_merge().
 
-   Sets up the diff editor report and drives it by properly negating
-   subtree that could have a conflicting merge history.
+   Set up the diff editor report to merge URL1@REVISION1 to URL2@REVISION2
+   into TARGET_WCPATH and drive it.  Properly describe any subtrees of
+   TARGET_WCPATH that require only a subset of REVISION1:REVISION2 to be
+   merged -- These subtrees are described in CHILDREN_WITH_MERGEINFO, an
+   array of svn_client__merge_path_t *, see
+   'THE CHILDREN_WITH_MERGEINFO ARRAY' comment at the top of this file
+   for more info.  Note that it is possible that TARGET_WCPATH needs only
+   a subset of REVISION1:REVISION2 while its subtrees need the entire range.
 
-   MERGE_B->ra_session1 reflects URL1; MERGE_B->ra_session2 reflects URL2.
+   REVISION1 and REVISION2 must be bound by the set of remaining_ranges
+   fields in CHILDREN_WITH_MERGEINFO's elements, specifically:
+
+     1) For forward merges the oldest revision in all the remaining_ranges
+        must be equal to REVISION1 and the youngest revision in the *first*
+        range of all the remaining ranges must be equal to REVISION2.
+
+     2) For reverse merges the youngest revision in all the remaining_ranges
+        must be equal to REVISION1 and the oldest revision in the *first*
+        range of all the remaining ranges must be equal to REVISION2.
+
+   If IS_ROLLBACK is true this is a reverse merge, otherwise it is a
+   forward merge.  DEPTH, NOTIFY_B, ADM_ACCESS, and MERGE_B are cascasded from
+   do_directory_merge(), see that function for more info.  CALLBACKS are the
+   svn merge versions of the svn_wc_diff_callbacks3_t callbacks invoked by
+   the editor.
 
    If MERGE_B->sources_ancestral is set, then URL1@REVISION1 must be a
    historical ancestor of URL2@REVISION2, or vice-versa (see
