f793d46dc3c185f32377be81fe9feae9feadcbb2
Bert Huijben <rhuijben@apache.org>
2008-08-21 23:23:55 +0000

    Following up to r32623 and r32621: Updated the prototypes of the fsfs caching functions to full prototypes.
    * subversion/libsvn_fs_fs/dag.h
      (svn_fs_fs__dag_dup_for_cache, svn_fs_fs__dag_serialize,
       svn_fs_fs__dag_deserialize): Updated to full function prototype.
       
    * subversion/libsvn_fs_fs/fs_fs.h
      (svn_fs_fs__dir_entries_serialize,
       svn_fs_fs__dir_entries_deserialize): Updated to full function 
       prototype.
    
    * subversion/libsvn_fs_fs/caching.c
      (warn_on_cache_errors): Made a comment of the svn_cache_error_handler_t
       prototype.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@872700 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_fs/fs_fs.h
+++ subversion/libsvn_fs_fs/fs_fs.h
@@ -90,11 +90,21 @@ svn_error_t *svn_fs_fs__rev_get_root(svn_fs_id_t **root_id,
                                      svn_revnum_t rev,
                                      apr_pool_t *pool);
 
-/* Serialize a directory contents hash. */
-svn_cache_serialize_func_t svn_fs_fs__dir_entries_serialize;
+/* Serialize a directory contents hash.
+   Implements svn_cache_serialize_func_t */
+svn_error_t *
+svn_fs_fs__dir_entries_serialize(char **data,
+                                 apr_size_t *data_len,
+                                 void *in,
+                                 apr_pool_t *pool);
 
-/* Deserialize a directory contents hash. */
-svn_cache_deserialize_func_t svn_fs_fs__dir_entries_deserialize;
+/* Deserialize a directory contents hash. 
+   Implements svn_cache_deserialize_func_t */
+svn_error_t *
+svn_fs_fs__dir_entries_deserialize(void **out,
+                                   const char *data,
+                                   apr_size_t data_len,
+                                   apr_pool_t *pool);
 
 /* Set *ENTRIES to an apr_hash_t of dirent structs that contain the
    directory entries of node-revision NODEREV in filesystem FS.  The
