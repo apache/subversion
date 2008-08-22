620501d63295dc5e15a4b0dc4bb3bed0864037b2
Hyrum Kurt Wright <hwright@apache.org>
2008-08-22 17:40:42 +0000

    Don't allow repository creation inside of an existant repository.  This was first mentioned on the users list here:   http://svn.haxx.se/users/archive-2008-08/0800.shtml
    Suggested by: Steven J. Murdoch <svn+Steven.Murdoch@cl.cam.ac.uk>
    
    * subversion/libsvn_repos/repos.c
      (svn_repos_create): Check to see if we are in an existant repo before
        creating a new one.
    
    * subversion/tests/cmdline/svnadmin_tests.py
      (create_in_repo_subdir): New test.
      (test_list): Run the new test.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@872710 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_repos/repos.c
+++ subversion/libsvn_repos/repos.c
@@ -1192,6 +1192,7 @@ svn_repos_create(svn_repos_t **repos_p,
 {
   svn_repos_t *repos;
   svn_error_t *err;
+  const char *root_path;
 
   /* Allocate a repository object, filling in the format we will create. */
   repos = create_svn_repos_t(path, pool);
@@ -1209,6 +1210,13 @@ svn_repos_create(svn_repos_t **repos_p,
 
   if (! repos->fs_type)
     repos->fs_type = DEFAULT_FS_TYPE;
+
+  /* Don't create a repository inside another repository. */
+  root_path = svn_repos_find_root_path(path, pool);
+  if (root_path != NULL)
+    return svn_error_createf(SVN_ERR_REPOS_BAD_ARGS, NULL, _("'%s' is a "
+                              "subdirectory of an existing repository rooted "
+                              "at '%s'"), path, root_path);
 
   /* Create the various files and subdirectories for the repository. */
   SVN_ERR_W(create_repos_structure(repos, path, fs_config, pool),
