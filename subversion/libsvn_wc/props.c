d33af145a24734676391cbee2d2082ece46f5f1d
Blair Zajac <blair@apache.org>
2008-08-26 19:39:26 +0000

    Follow up to r32673, remove references to old- and new-style external definitions as we may add a new-new-style in the future; just refer to them as external definitions.
    Suggested by: cmpilato
    
    * subversion/libsvn_wc/props.c
      (svn_wc_parse_externals_description3)
        If an external uses an absolute URL where a directory path is
        expected, do not refer to the external as an old- or new-style
        external, just refer to it as an external definition.
    
    * subversion/tests/cmdline/externals_tests.py
      (disallow_propset_invalid_formatted_externals):
        Now that the old- and new-style externals generate the same error
        text, merge two for loops into one and update the regex to match
        the new text.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@872813 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/props.c
+++ subversion/libsvn_wc/props.c
@@ -3315,16 +3315,16 @@ svn_wc_parse_externals_description3(apr_array_header_t **externals_p,
         return svn_error_createf
           (SVN_ERR_CLIENT_INVALID_EXTERNALS_DESCRIPTION, NULL,
            _("Invalid %s property on '%s': "
-             "cannot use a URL '%s' as the target directory for a new style "
-             "external"),
+             "cannot use a URL '%s' as the target directory for an external "
+             "definition"),
            SVN_PROP_EXTERNALS, parent_directory_display, token1);
 
       if (1 == rev_idx && token0_is_url)
         return svn_error_createf
           (SVN_ERR_CLIENT_INVALID_EXTERNALS_DESCRIPTION, NULL,
            _("Invalid %s property on '%s': "
-             "cannot use a URL '%s' as the target directory for an old style "
-             "external"),
+             "cannot use a URL '%s' as the target directory for an external "
+             "definition"),
            SVN_PROP_EXTERNALS, parent_directory_display, token0);
 
       /* The appearence of -r N or -rN forces the type of external.
