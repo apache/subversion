c06662199afac67cc1c2db73c03efa675ced095b
Hyrum Kurt Wright <hwright@apache.org>
2008-09-02 16:54:19 +0000

    Make the wc property handling just a little bit saner by removing unused functions and duplicate function implementations.
    * subversion/libsvn_wc/props.c
      (svn_wc__props_modified): Remove.
      (svn_wc__has_prop_mods): Remove implementation, and just wrap
        modified_props() instead.
     
    * subversion/libsvn_wc/status.c
      (assemble_status): Use a library-private function instead of the public API.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@872929 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/status.c
+++ subversion/libsvn_wc/status.c
@@ -375,8 +375,8 @@ assemble_status(svn_wc_status2_t **status,
         final_prop_status = svn_wc_status_normal;
 
       /* If the entry has a property file, see if it has local changes. */
-      SVN_ERR(svn_wc_props_modified_p(&prop_modified_p, path, adm_access,
-                                      pool));
+      SVN_ERR(svn_wc__has_prop_mods(&prop_modified_p, path, adm_access,
+                                    pool));
 
 #ifdef HAVE_SYMLINK
       if (has_props)
