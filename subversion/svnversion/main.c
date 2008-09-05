c996f885ea5cefebf0315842303148c573510f36
Arfrever Frehtes Taifersar Arahesis <arfrever@apache.org>
2008-09-05 14:31:06 +0000

    Use svn_opt_print_help3() instead of deprecated svn_opt_print_help().
    * subversion/svnserve/main.c
      (version):
    * subversion/svnversion/main.c
      (version): Use svn_opt_print_help3() instead of deprecated
       svn_opt_print_help().
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@873002 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svnversion/main.c
+++ subversion/svnversion/main.c
@@ -29,8 +29,8 @@
 static svn_error_t *
 version(apr_pool_t *pool)
 {
-  return svn_opt_print_help(NULL, "svnversion", TRUE, FALSE, NULL, NULL,
-                            NULL, NULL, NULL, pool);
+  return svn_opt_print_help3(NULL, "svnversion", TRUE, FALSE, NULL, NULL,
+                             NULL, NULL, NULL, NULL, pool);
 }
 
 static void
