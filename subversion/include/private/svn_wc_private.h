05235f8639ade9f96caeb95ec0a4d89cf25564ae
Hyrum Kurt Wright <hwright@apache.org>
2008-09-08 21:26:28 +0000

    Semi-publicize a function which checks for interesting statii.
    * subversion/include/private/svn_wc_private.h
      (svn_wc__is_sendable_status): New.
    
    * subversion/libsvn_wc/status.c
      (is_sendable_status): Move from this...
      (svn_wc__is_sendable_status): ...to this.  Also, make the arguments
        independent of a particular flavor of baton.
      (handle_statii, close_directory): Update callers.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@873047 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/private/svn_wc_private.h
+++ subversion/include/private/svn_wc_private.h
@@ -126,6 +126,18 @@ svn_wc__versioned_file_modcheck(svn_boolean_t *modified_p,
                                 svn_boolean_t compare_textbases,
                                 apr_pool_t *pool);
 
+/**
+ * Return a boolean answer to the question "Is @a status something that
+ * should be reported?".  @a no_ignore and @a get_all are the same as
+ * svn_wc_get_status_editor4().
+ * 
+ * @since New in 1.6.
+ */
+svn_boolean_t
+svn_wc__is_sendable_status(svn_wc_status2_t *status,
+                           svn_boolean_t no_ignore,
+                           svn_boolean_t get_all);
+
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
