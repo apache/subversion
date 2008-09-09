5ae946df769f75a68ab5799dbc078ec841b44500
arfrever <dev-null@apache.org>
2008-09-09 22:41:17 +0000

    Fix some typos.
    * subversion/include/svn_wc.h
      (svn_wc_status_func2_t):
    * subversion/libsvn_fs_base/bdb/changes-table.c
      (svn_fs_bdb__changes_delete):
    * subversion/svn/log-cmd.c
      (log_entry_receiver):
    * notes/compatibility.txt
      (BINARY PROPERTIES):
    * notes/l10n-problems
      (Translations on the server):
    * notes/webdav-proxy:
    * www/merge-tracking/func-spec.html
      (ra-copy-move, wc-wc-copy-move):
    * www/svn_1.5_releasenotes.html
      (webdav-proxy-example): Fix some typos.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@873082 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svn/log-cmd.c
+++ subversion/svn/log-cmd.c
@@ -2,7 +2,7 @@
  * log-cmd.c -- Display log messages
  *
  * ====================================================================
- * Copyright (c) 2000-2007 CollabNet.  All rights reserved.
+ * Copyright (c) 2000-2008 CollabNet.  All rights reserved.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
@@ -295,7 +295,7 @@ log_entry_receiver(void *baton,
  *   errors with helpful hints about the -m and -F options.
  *
  * * subversion/libsvn_client/commit.c
- *   (svn_client_commit): Actually capture and propogate &quot;no external
+ *   (svn_client_commit): Actually capture and propagate &quot;no external
  *   editor&quot; errors.</msg>
  * </logentry>
  * </log>
