c0f3e5db9ced385002daf669ee9118c66d1102d6
Blair Zajac <blair@apache.org>
2008-09-09 05:58:44 +0000

    * subversion/libsvn_fs_fs/dag.h * subversion/libsvn_fs_fs/dag.c   (svn_fs_fs__dag_check_mutable):     Change the dag_node_t * argument to a const dag_node_t *.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@873068 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_fs/dag.c
+++ subversion/libsvn_fs_fs/dag.c
@@ -162,7 +162,7 @@ get_node_revision(node_revision_t **noderev_p,
 }
 
 
-svn_boolean_t svn_fs_fs__dag_check_mutable(dag_node_t *node)
+svn_boolean_t svn_fs_fs__dag_check_mutable(const dag_node_t *node)
 {
   return (svn_fs_fs__id_txn_id(svn_fs_fs__dag_get_id(node)) != NULL);
 }
