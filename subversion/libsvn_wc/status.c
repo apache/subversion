841f8ddb5839842739f32acc3156aebc6a1c74df
Greg Stein <gstein@apache.org>
2008-09-16 18:25:44 +0000

    Nit: tighten scope of local variable.
    * subversion/libsvn_wc/status.c:
      (close_edit): move tgt_access into a narrower scope.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@873177 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/status.c
+++ subversion/libsvn_wc/status.c
@@ -2008,7 +2008,6 @@ close_edit(void *edit_baton,
 
       if (kind == svn_node_dir)
         {
-          svn_wc_adm_access_t *tgt_access;
           const svn_wc_entry_t *tgt_entry;
 
           err = svn_wc_entry(&tgt_entry, full_path, eb->adm_access,
@@ -2027,6 +2026,8 @@ close_edit(void *edit_baton,
             }
           else
             {
+              svn_wc_adm_access_t *tgt_access;
+
               err = svn_wc_adm_retrieve(&tgt_access, eb->adm_access,
                                         full_path, pool);
               if (err) goto cleanup;
