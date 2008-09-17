584015d343564d419938d1f6f7daa913d61f268f
Julian Foad <julianfoad@apache.org>
2008-09-17 12:59:00 +0000

    Tweak a doc string. A follow-up to r33125.
    * subversion/include/svn_wc.h
      (svn_wc_get_tree_conflict): Add "@since" to doc string.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@873201 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_wc.h
+++ subversion/include/svn_wc.h
@@ -5125,7 +5125,10 @@ svn_wc_set_changelist(const char *path,
 /** Set @a *tree_conflict to a newly allocated @c svn_wc_conflict_description_t
  * structure describing the tree conflict state of @a victim_path, or to null
  * if @a victim_path is not in a state of tree conflict. @a adm_access is the
- * admin access baton for @a victim_path. Use @a pool for all allocations. */
+ * admin access baton for @a victim_path. Use @a pool for all allocations.
+ *
+ * @since New in 1.6.
+ */
 svn_error_t *
 svn_wc_get_tree_conflict(svn_wc_conflict_description_t **tree_conflict,
                          const char *victim_path,
