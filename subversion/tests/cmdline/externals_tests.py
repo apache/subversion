6bfa42d35ca95776353e8c19f597effec9d455b1
Blair Zajac <blair@apache.org>
2008-09-22 04:45:15 +0000

    * subversion/include/private/svn_wc_private.h, * subversion/libsvn_client/switch.c, * subversion/libsvn_wc/adm_ops.c, * subversion/svn/status.c, * subversion/tests/cmdline/externals_tests.py:   Follow up to r33229; bump the copyright years.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@873305 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/externals_tests.py
+++ subversion/tests/cmdline/externals_tests.py
@@ -6,7 +6,7 @@
 #  See http://subversion.tigris.org for more information.
 #
 # ====================================================================
-# Copyright (c) 2000-2007 CollabNet.  All rights reserved.
+# Copyright (c) 2000-2008 CollabNet.  All rights reserved.
 #
 # This software is licensed as described in the file COPYING, which
 # you should have received as part of this distribution.  The terms
