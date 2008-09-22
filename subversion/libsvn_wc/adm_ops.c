0ea3fbf0afb998b8f09dffc4bb84d94b802158d1
Blair Zajac <blair@apache.org>
2008-09-22 04:45:15 +0000

    * subversion/include/private/svn_wc_private.h, * subversion/libsvn_client/switch.c, * subversion/libsvn_wc/adm_ops.c, * subversion/svn/status.c, * subversion/tests/cmdline/externals_tests.py:   Follow up to r33229; bump the copyright years.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@873305 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/adm_ops.c
+++ subversion/libsvn_wc/adm_ops.c
@@ -7,7 +7,7 @@
  *            file in the working copy).
  *
  * ====================================================================
- * Copyright (c) 2000-2007 CollabNet.  All rights reserved.
+ * Copyright (c) 2000-2008 CollabNet.  All rights reserved.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
--- subversion/svn/status.c
+++ subversion/svn/status.c
@@ -2,7 +2,7 @@
  * status.c:  the command-line's portion of the "svn status" command
  *
  * ====================================================================
- * Copyright (c) 2000-2004 CollabNet.  All rights reserved.
+ * Copyright (c) 2000-2004,2008 CollabNet.  All rights reserved.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
