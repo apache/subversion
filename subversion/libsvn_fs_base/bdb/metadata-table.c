b19b3f0729957fdae69876ecf3808e4df3fe1a51
Hyrum Kurt Wright <hwright@apache.org>
2008-09-24 20:47:38 +0000

    Simplify the metadata table API by using a NULL value to delete a key in the table.
    Suggested by: cmpilato
    
    * subversion/libsvn_fs_base/bdb/metadata-table.h,
      subversion/libsvn_fs_base/bdb/metadata-table.c
      (svn_fs_bdb__metadata_add): Rename to...
      (svn_fs_bdb__metadata_set): ...this and add the functionality from ...
      (svn_fs_bdb__metadata_delete): ...this, which is now removed.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@873352 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_base/bdb/metadata-table.c
+++ subversion/libsvn_fs_base/bdb/metadata-table.c
@@ -61,7 +61,7 @@ svn_fs_bdb__open_metadata_table(DB **metadata_p,
 
 
 svn_error_t *
-svn_fs_bdb__metadata_add(svn_fs_t *fs,
+svn_fs_bdb__metadata_set(svn_fs_t *fs,
                          const char *key_str,
                          const char *val,
                          trail_t *trail,
@@ -71,11 +71,21 @@ svn_fs_bdb__metadata_add(svn_fs_t *fs,
   DBT key, value;
 
   svn_fs_base__str_to_dbt(&key, key_str);
-  svn_fs_base__str_to_dbt(&value, val);
-  svn_fs_base__trail_debug(trail, "metadata", "add");
-  return BDB_WRAP(fs, "storing metadata record",
-                  bfd->metadata->put(bfd->metadata, trail->db_txn,
-                                     &key, &value, 0));
+  if (val == NULL)
+    {
+      svn_fs_base__trail_debug(trail, "metadata", "del");
+      return BDB_WRAP(fs, "deleting metadata from 'metadata' table",
+                      bfd->metadata->del(bfd->metadata, trail->db_txn,
+                                         &key, 0));
+    }
+  else
+    {
+      svn_fs_base__str_to_dbt(&value, val);
+      svn_fs_base__trail_debug(trail, "metadata", "add");
+      return BDB_WRAP(fs, "storing metadata record",
+                      bfd->metadata->put(bfd->metadata, trail->db_txn,
+                                         &key, &value, 0));
+    }
 }
 
 
@@ -85,14 +95,6 @@ svn_fs_bdb__metadata_delete(svn_fs_t *fs,
                             trail_t *trail,
                             apr_pool_t *pool)
 {
-  base_fs_data_t *bfd = fs->fsap_data;
-  DBT key;
-
-  svn_fs_base__str_to_dbt(&key, key_str);
-  svn_fs_base__trail_debug(trail, "metadata", "del");
-  return BDB_WRAP(fs, "deleting metadata from 'metadata' table",
-                  bfd->metadata->del(bfd->metadata, trail->db_txn,
-                                     &key, 0));
 }
 
 
