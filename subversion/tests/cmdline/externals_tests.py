7b52583846f9a2ee8e85103188ee7d7b18f07f30
Blair Zajac <blair@apache.org>
2008-09-24 15:43:12 +0000

    Allow a file external to be placed into a directory external which doesn't already have an access baton for it.
    * subversion/include/svn_error_codes.h
      (SVN_ERR_RA_REPOS_ROOT_URL_MISMATCH):
        New error code.  I put this in SVN_ERR_RA to match the
        SVN_ERR_RA_UUID_MISMATCH error.
      
    * subversion/libsvn_client/externals.c
      (switch_file_external):
        If svn_wc_adm_retrieve() cannot get an access baton, then try to
          get an access baton using svn_wc_adm_open3() and if it succeeds,
          check that the repos root URL for the file external matches the
          repos root URL for the working copy the external is being placed
          into.
        Use target_adm_access instead of adm_access throughout the rest of
          the function.
    
    * subversion/tests/cmdline/externals_tests.py
      (can_place_file_external_into_dir_external):
        New unit test to check that a file external can be placed into a
        directory external from the same repository but not a different
        repository.
      (test_list):
        Add can_place_file_external_into_dir_external.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@873339 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/externals_tests.py
+++ subversion/tests/cmdline/externals_tests.py
@@ -1041,6 +1041,61 @@ def cannot_move_or_remove_file_externals(sbox):
 
   file(os.path.join(wc_dir, 'A', 'D', 'gamma')).close()
 
+#----------------------------------------------------------------------
+
+def can_place_file_external_into_dir_external(sbox):
+  "place a file external into a directory external"
+
+  external_url_for = externals_test_setup(sbox)
+  wc_dir = sbox.wc_dir
+  repo_url = sbox.repo_url
+  other_repo_url = repo_url + ".other"
+
+  # Checkout a working copy.
+  svntest.actions.run_and_verify_svn(None, None, [],
+                                     'checkout',
+                                     repo_url, wc_dir)
+
+  # Put a directory external into the same repository and then a file
+  # external into that.
+  ext = "^/A/D        A/D-copy\n" + \
+        "^/A/B/E/beta A/D-copy/G/beta\n"
+  change_external(wc_dir, ext)
+
+  # Bring the working copy up to date and check that the file the file
+  # external is switched to still exists.
+  svntest.actions.run_and_verify_svn(None, None, [],
+                                     'up',
+                                     repo_url, wc_dir)
+
+  beta1_path = os.path.join(wc_dir, 'A', 'B', 'E', 'beta')
+  f = file(beta1_path)
+  beta1_contents = f.read()
+  f.close()
+
+  beta2_path = os.path.join(wc_dir, 'A', 'D-copy', 'G', 'beta')
+  f = file(beta2_path)
+  beta2_contents = f.read()
+  f.close()
+
+  if beta1_contents != beta2_contents:
+      raise svntest.Failure("Contents of '%s' and '%s' do not match" %
+                            (beta1_path, beta2_path))
+
+  # Now have a directory external from one repository and a file
+  # external from another repository.  This should fail.
+  ext = other_repo_url + "/A/B C/exdir_B\n" + \
+        "^/A/B/E/beta C/exdir_B/beta\n"
+  change_external(os.path.join(wc_dir, 'A'), ext)
+
+  svntest.actions.run_and_verify_svn("Able to put file external in foreign wc",
+                                     None,
+                                     ".*Cannot insert a file external from "
+                                     ".*/beta' into a working copy "
+                                     ".*" + other_repo_url,
+                                     'up',
+                                     repo_url, wc_dir)
+
 ########################################################################
 # Run the tests
 
@@ -1062,6 +1117,7 @@ test_list = [ None,
               disallow_propset_invalid_formatted_externals,
               old_style_externals_ignore_peg_reg,
               cannot_move_or_remove_file_externals,
+              can_place_file_external_into_dir_external,
              ]
 
 if __name__ == '__main__':
