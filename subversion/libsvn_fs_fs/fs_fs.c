0825b84d625f42afe9ad2bb784bdcbd3e49677dc
Hyrum Kurt Wright <hwright@apache.org>
2008-10-01 17:43:54 +0000

    On the fs-rep-sharing branch: Fix a segfault arising from caching non-existant representations.
    * subversion/libsvn_fs_fs/fs_fs.c
      (write_final_rev): Don't cache a NULL data_rep.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/fs-rep-sharing@873458 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_fs/fs_fs.c
+++ subversion/libsvn_fs_fs/fs_fs.c
@@ -4935,8 +4935,9 @@ write_final_rev(const svn_fs_id_t **new_id_p,
                                    svn_fs_fs__fs_supports_mergeinfo(fs),
                                    pool));
 
-  /* Save the representation's hash in the rep cache. */
-  SVN_ERR(svn_fs_fs__set_rep_reference(fs, noderev->data_rep, pool));
+  /* Save the representations' hashes in the rep cache. */
+  if (noderev->data_rep)
+    SVN_ERR(svn_fs_fs__set_rep_reference(fs, noderev->data_rep, pool));
   if (noderev->prop_rep)
     SVN_ERR(svn_fs_fs__set_rep_reference(fs, noderev->prop_rep, pool));
 
