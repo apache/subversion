baad610085ecc42ec57c26894587d6c9cb12f9d2
Hyrum Kurt Wright <hwright@apache.org>
2008-10-18 13:11:59 +0000

    Merge the fs-rep-sharing branch to trunk.
    This gives us significant space savings on both bdb and fsfs.  See the
    branch for individual log messages.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@873804 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_error_codes.h
+++ subversion/include/svn_error_codes.h
@@ -673,6 +673,11 @@ SVN_ERROR_START
              SVN_ERR_FS_CATEGORY_START + 47,
              "Filesystem upgrade is not supported")
 
+  /** @since New in 1.6. */
+  SVN_ERRDEF(SVN_ERR_FS_NO_SUCH_CHECKSUM_REP,
+             SVN_ERR_FS_CATEGORY_START + 49,
+             "Filesystem has no such checksum-representation index record")
+
   /* repos errors */
 
   SVN_ERRDEF(SVN_ERR_REPOS_LOCKED,
