2c8f1dba5eef00bd7acec53d1861d1dccb0cd66b
Hyrum Kurt Wright <hwright@apache.org>
2008-10-21 09:57:52 +0000

    Some followup fixes to r33730.
    Suggested by: gstein
    
    * subversion/libsvn_fs_base/bdb/dbt.c
      (svn_fs_base__str_to_dbt): Simplify checksum size calculation.
    
    * subversion/libsvn_fs_base/dag.c
      (svn_fs_base__dag_set_proplist): Fix comment.  Also, simplify error
        checking.
      (svn_fs_base__dag_finalize_edits): Simplify error checking.
    
    * subversion/include/svn_error_codes.h
      (SVN_ERR_FS_NO_SUCH_CHECKSUM_REP): Change category offset.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@873875 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_error_codes.h
+++ subversion/include/svn_error_codes.h
@@ -675,7 +675,7 @@ SVN_ERROR_START
 
   /** @since New in 1.6. */
   SVN_ERRDEF(SVN_ERR_FS_NO_SUCH_CHECKSUM_REP,
-             SVN_ERR_FS_CATEGORY_START + 49,
+             SVN_ERR_FS_CATEGORY_START + 48,
              "Filesystem has no such checksum-representation index record")
 
   /* repos errors */
