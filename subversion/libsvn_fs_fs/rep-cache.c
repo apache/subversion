e0631ba220ec19a4b18ef801f0624219f5defed0
Hyrum Kurt Wright <hwright@apache.org>
2008-10-22 16:48:36 +0000

    * subversion/libsvn_fs_fs/rep-cache.c   (cleanup_db_apr): Use a loop for finalizing our prepared sqlite statements.     Followup to r33816.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@873924 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_fs/rep-cache.c
+++ subversion/libsvn_fs_fs/rep-cache.c
@@ -49,33 +49,21 @@ cleanup_db_apr(void *data)
   svn_fs_t *fs = data;
   fs_fs_data_t *ffd = fs->fsap_data;
   svn_error_t *err;
+  int i;
 
-  if (ffd->rep_cache.get_rep_stmt) 
-    {
-      err = svn_sqlite__finalize(ffd->rep_cache.get_rep_stmt);
-      if (err)
-        goto err_cleanup;
-    }
+  svn_sqlite__stmt_t *stmts[] = { ffd->rep_cache.get_rep_stmt,
+                                  ffd->rep_cache.set_rep_stmt,
+                                  ffd->rep_cache.inc_select_stmt,
+                                  ffd->rep_cache.inc_update_stmt };
 
-  if (ffd->rep_cache.set_rep_stmt) 
+  for (i = 0; i < 4; i++)
     {
-      err = svn_sqlite__finalize(ffd->rep_cache.set_rep_stmt);
-      if (err)
-        goto err_cleanup;
-    }
-
-  if (ffd->rep_cache.inc_select_stmt)
-    {
-      err = svn_sqlite__finalize(ffd->rep_cache.inc_select_stmt);
-      if (err)
-        goto err_cleanup;
-    }
-
-  if (ffd->rep_cache.inc_update_stmt)
-    {
-      err = svn_sqlite__finalize(ffd->rep_cache.inc_update_stmt);
-      if (err)
-        goto err_cleanup;
+      if (stmts[i])
+        {
+          err = svn_sqlite__finalize(stmts[i]);
+          if (err)
+            goto err_cleanup;
+        }
     }
 
   err = svn_sqlite__close(ffd->rep_cache.db, SVN_NO_ERROR);
