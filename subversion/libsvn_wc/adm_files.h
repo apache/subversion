d42742b644294e4d05ad848a0ddbdba363685485
Greg Stein <gstein@apache.org>
2008-10-23 11:39:17 +0000

    Get rid of the svn_wc__make_adm_thing() internal API, switching it to a file-local function. Convert a number of calls to extend_with_adm_name() over to svn_wc__adm_path().
    * subversion/libsvn_wc/adm_files.h:
      (svn_wc__make_adm_thing): remove
    
    * subversion/libsvn-wc/adm_files.c:
      (svn_wc__adm_area_exists, svn_wc__make_killme, svn_wc__remove_adm_file,
          make_empty_adm, svn_wc__adm_destroy, svn_wc__adm_cleanup_tmp_area):
        use svn_wc__adm_child()
      (svn_wc__make_adm_thing): renamed to ...
      (make_adm_subdir): ... this. useless params removed and the function was
        streamlined to just creating subdirs in the admin area.
      (svn_wc__check_killme): use svn_wc__adm_child(). avoid a stupid creation
        of svn_string_t and just strcmp() directly
      (init_adm_tmp_area): switch to make_adm_subdir()
      (init_adm): switch to make_adm_subdir() and svn_wc__adm_child()
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@873933 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/adm_files.h
+++ subversion/libsvn_wc/adm_files.h
@@ -46,15 +46,6 @@ svn_boolean_t svn_wc__adm_area_exists(const svn_wc_adm_access_t *adm_access,
                                       apr_pool_t *pool);
 
 
-/* Make `PATH/<adminstrative_subdir>/THING'. The caller should ensure
-   that a write lock is active for PATH. */
-svn_error_t *svn_wc__make_adm_thing(const char *path,
-                                    const char *thing,
-                                    svn_node_kind_t type,
-                                    apr_fileperms_t perms,
-                                    svn_boolean_t tmp,
-                                    apr_pool_t *scratch_pool);
-
 /* Create a killme file in the administrative area, indicating that the
    directory containing the administrative area should be removed.
 
