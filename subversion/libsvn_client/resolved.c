71f77e7d0433d3213e82ad46bac85e123e996a0d
Julian Foad <julianfoad@apache.org>
2008-11-11 15:09:42 +0000

    On the 'tc-resolve' branch: Continue with resolving per victim.
    * subversion/libsvn_client/resolved.c
      Include a required header file.
      (svn_client_resolve): Correct the levels-to-lock calculation.
    
    * subversion/libsvn_wc/adm_ops.c
      (resolve_found_entry_callback): Remove an unused variable.
    
    * subversion/tests/cmdline/commit_tests.py
      (tree_conflicts_resolved): Adjust for resolving per victim.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/tc-resolve@874209 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/resolved.c
+++ subversion/libsvn_client/resolved.c
@@ -26,6 +26,7 @@
 #include "svn_wc.h"
 #include "svn_client.h"
 #include "svn_error.h"
+#include "svn_path.h"
 #include "client.h"
 #include "private/svn_wc_private.h"
 
@@ -56,8 +57,8 @@ svn_client_resolve(const char *path,
   /* In order to resolve tree-conflicts on the target PATH, we need an
    * adm_access on its parent directory. The lock level then needs to extend
    * at least onto the immediate children. */
-  if (adm_lock_level == 0)
-    adm_lock_level = 1;
+  if (adm_lock_level >= 0)
+    adm_lock_level++;
   SVN_ERR(svn_wc_adm_probe_open3(&adm_access, NULL,
                                  svn_path_dirname(path, pool),
                                  TRUE,
