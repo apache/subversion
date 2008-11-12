8d6e036653063b0d383825516ec9f571650f40d8
Greg Stein <gstein@apache.org>
2008-11-12 10:57:56 +0000

    Update the 600 line svn_wc__merge_internal. SIX HUNDRED LINES. It used the old svn_io_open_unique_file2 seven times; these have been replaced by calls to the new svn_io_open_uniquely_named(). We want the merge (unique) files to have particular names.
    * subversion/libsvn_wc/merge.c:
      (svn_wc__merge_internal): switch to uniquely_named
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@874230 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/merge.c
+++ subversion/libsvn_wc/merge.c
@@ -286,6 +286,8 @@ svn_wc__merge_internal(svn_stringbuf_t **log_accum,
                        void *conflict_baton,
                        apr_pool_t *pool)
 {
+  const char *merge_dirpath;
+  const char *merge_filename;
   const char *tmp_target, *result_target, *working_text;
   const char *adm_path = svn_wc_adm_access_path(adm_access);
   apr_file_t *result_f;
@@ -303,6 +305,8 @@ svn_wc__merge_internal(svn_stringbuf_t **log_accum,
       *merge_outcome = svn_wc_merge_no_merge;
       return SVN_NO_ERROR;
     }
+
+  svn_path_split(merge_target, &merge_dirpath, &merge_filename, pool);
 
   /* Decide if the merge target is a text or binary file. */
   if ((mimeprop = get_prop(prop_diff, SVN_PROP_MIME_TYPE))
@@ -429,13 +433,15 @@ svn_wc__merge_internal(svn_stringbuf_t **log_accum,
               if (result->save_merged)
                 {
                   const char *edited_copy;
+
                   /* ### Should use preserved-conflict-file-exts. */
-                  SVN_ERR(svn_io_open_unique_file2(NULL,
-                                                   &edited_copy,
-                                                   merge_target,
-                                                   ".edited",
-                                                   svn_io_file_del_none,
-                                                   pool));
+                  SVN_ERR(svn_io_open_uniquely_named(NULL,
+                                                     &edited_copy,
+                                                     merge_dirpath,
+                                                     merge_filename,
+                                                     ".edited",
+                                                     svn_io_file_del_none,
+                                                     pool, pool));
                   SVN_ERR(svn_wc__loggy_copy(log_accum, adm_access,
                                              /* Look for callback's own
                                                 merged-file first: */
@@ -548,33 +554,36 @@ svn_wc__merge_internal(svn_stringbuf_t **log_accum,
 
           /* I miss Lisp. */
 
-          SVN_ERR(svn_io_open_unique_file2(NULL,
-                                           &left_copy,
-                                           merge_target,
-                                           left_label,
-                                           svn_io_file_del_none,
-                                           pool));
+          SVN_ERR(svn_io_open_uniquely_named(NULL,
+                                             &left_copy,
+                                             merge_dirpath,
+                                             merge_filename,
+                                             left_label,
+                                             svn_io_file_del_none,
+                                             pool, pool));
 
           /* Have I mentioned how much I miss Lisp? */
 
-          SVN_ERR(svn_io_open_unique_file2(NULL,
-                                           &right_copy,
-                                           merge_target,
-                                           right_label,
-                                           svn_io_file_del_none,
-                                           pool));
+          SVN_ERR(svn_io_open_uniquely_named(NULL,
+                                             &right_copy,
+                                             merge_dirpath,
+                                             merge_filename,
+                                             right_label,
+                                             svn_io_file_del_none,
+                                             pool, pool));
 
           /* Why, how much more pleasant to be forced to unroll my loops.
              If I'd been writing in Lisp, I might have mapped an inline
              lambda form over a list, or something equally disgusting.
              Thank goodness C was here to protect me! */
 
-          SVN_ERR(svn_io_open_unique_file2(NULL,
-                                           &target_copy,
-                                           merge_target,
-                                           target_label,
-                                           svn_io_file_del_none,
-                                           pool));
+          SVN_ERR(svn_io_open_uniquely_named(NULL,
+                                             &target_copy,
+                                             merge_dirpath,
+                                             merge_filename,
+                                             target_label,
+                                             svn_io_file_del_none,
+                                             pool, pool));
 
           /* We preserve all the files with keywords expanded and line
              endings in local (working) form. */
@@ -792,19 +801,21 @@ svn_wc__merge_internal(svn_stringbuf_t **log_accum,
         }
 
       /* reserve names for backups of left and right fulltexts */
-      SVN_ERR(svn_io_open_unique_file2(NULL,
-                                       &left_copy,
-                                       merge_target,
-                                       left_label,
-                                       svn_io_file_del_none,
-                                       pool));
+      SVN_ERR(svn_io_open_uniquely_named(NULL,
+                                         &left_copy,
+                                         merge_dirpath,
+                                         merge_filename,
+                                         left_label,
+                                         svn_io_file_del_none,
+                                         pool, pool));
 
-      SVN_ERR(svn_io_open_unique_file2(NULL,
-                                       &right_copy,
-                                       merge_target,
-                                       right_label,
-                                       svn_io_file_del_none,
-                                       pool));
+      SVN_ERR(svn_io_open_uniquely_named(NULL,
+                                         &right_copy,
+                                         merge_dirpath,
+                                         merge_filename,
+                                         right_label,
+                                         svn_io_file_del_none,
+                                         pool, pool));
 
       /* create the backup files */
       SVN_ERR(svn_io_copy_file(left,
@@ -818,12 +829,13 @@ svn_wc__merge_internal(svn_stringbuf_t **log_accum,
           /* Create a .mine file too */
           const char *mine_copy;
 
-          SVN_ERR(svn_io_open_unique_file2(NULL,
-                                           &mine_copy,
-                                           merge_target,
-                                           target_label,
-                                           svn_io_file_del_none,
-                                           pool));
+          SVN_ERR(svn_io_open_uniquely_named(NULL,
+                                             &mine_copy,
+                                             merge_dirpath,
+                                             merge_filename,
+                                             target_label,
+                                             svn_io_file_del_none,
+                                             pool, pool));
           SVN_ERR(svn_wc__loggy_move(log_accum,
                                      adm_access,
                                      tmp_target,
