41c1913340144bbe6ffeef65de7fc84ff73c548f
Bert Huiben <rhuijben@apache.org>
2008-11-14 15:28:13 +0000

    * subversion/libsvn_subr/auth.c   (svn_auth_get_platform_specific_provider): Following up on r34085, load     windows ssl server trust provider when asked for the ssl server trust     provider.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@874262 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/auth.c
+++ subversion/libsvn_subr/auth.c
@@ -464,7 +464,7 @@ svn_auth_get_platform_specific_provider(svn_auth_provider_object_t **provider,
       if (strcmp(provider_name, "windows") == 0 &&
           strcmp(provider_type, "ssl_server_trust") == 0)
         {
-          svn_auth_get_windows_simple_provider(provider, pool);
+          svn_auth_get_windows_ssl_server_trust_provider(provider, pool);
         }
 #endif
     }
