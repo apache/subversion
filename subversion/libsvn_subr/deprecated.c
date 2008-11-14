bd9444c7efda3abafbcff6d262a2f062a96fde90
Hyrum Kurt Wright <hwright@apache.org>
2008-11-14 18:52:14 +0000

    Move a deprecated function to the deprecated file (thus removing a warning).
    * subversion/libsvn_subr/io.c
      (svn_io_open_unique_file2, svn_io_open_unique_file): Move from here...
    
    * subversion/libsvn_subr/deprecated.c
      (svn_io_open_unique_file2, svn_io_open_unique_file): ...to here.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@874266 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/deprecated.c
+++ subversion/libsvn_subr/deprecated.c
@@ -480,3 +480,38 @@ svn_opt_print_generic_help(const char *header,
   svn_handle_error2(err, stderr, FALSE, "svn: ");
   svn_error_clear(err);
 }
+
+/*** From io.c ***/
+svn_error_t *
+svn_io_open_unique_file2(apr_file_t **file,
+                         const char **temp_path,
+                         const char *path,
+                         const char *suffix,
+                         svn_io_file_del_t delete_when,
+                         apr_pool_t *pool)
+{
+  const char *dirpath;
+  const char *filename;
+
+  svn_path_split(path, &dirpath, &filename, pool);
+  return svn_io_open_uniquely_named(file, temp_path,
+                                    dirpath, filename, suffix,
+                                    delete_when,
+                                    pool, pool);
+}
+
+svn_error_t *
+svn_io_open_unique_file(apr_file_t **file,
+                        const char **temp_path,
+                        const char *path,
+                        const char *suffix,
+                        svn_boolean_t delete_on_close,
+                        apr_pool_t *pool)
+{
+  return svn_io_open_unique_file2(file, temp_path,
+                                  path, suffix,
+                                  delete_on_close
+                                    ? svn_io_file_del_on_close
+                                    : svn_io_file_del_none,
+                                  pool);
+}
