25546d57d38affef37d96ba91fc7c5b560dfee97
Hyrum Kurt Wright <hwright@apache.org>
2008-11-23 04:13:53 +0000

    * subversion/mod_dav_svn/util.c   (dav_svn__build_uri): Remove another never-reached line.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@874427 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/mod_dav_svn/util.c
+++ subversion/mod_dav_svn/util.c
@@ -226,7 +226,6 @@ dav_svn__build_uri(const dav_svn_repos *repos,
     default:
       /* programmer error somewhere */
       SVN_ERR_MALFUNCTION_NO_RETURN();
-      return NULL;
     }
 
   /* NOTREACHED */
