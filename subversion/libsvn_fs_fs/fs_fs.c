63d3f2d32dad3c1893e87f4f59cb50e20a5f6805
Hyrum Kurt Wright <hwright@apache.org>
2008-12-08 19:23:22 +0000

    Since we default to TRUE for rep-sharing, make the commented-out option listed in the FSFS config file FALSE.
    * subversion/libsvn_fs_fs/fs_fs.c
      (write_config): Write the CONFIG_OPTION_ENABLE_REP_SHARING as false, since
        the default is true, and the line is commented out by default.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@874685 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_fs/fs_fs.c
+++ subversion/libsvn_fs_fs/fs_fs.c
@@ -1030,7 +1030,7 @@ write_config(svn_fs_t *fs,
 "### The following parameter enables rep-sharing in the repository.  It can" NL
 "### be switched on and off at will, but for best space-saving results"      NL
 "### should be enabled consistently over the life of the repository."        NL
-"### " CONFIG_OPTION_ENABLE_REP_SHARING " = true"                            NL
+"### " CONFIG_OPTION_ENABLE_REP_SHARING " = false"                           NL
 
 ;
 #undef NL
