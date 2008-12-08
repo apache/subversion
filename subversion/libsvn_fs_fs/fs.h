411ede64265f40e862d9da88f93a9632f64f5292
Hyrum Kurt Wright <hwright@apache.org>
2008-12-08 21:43:25 +0000

    Followup to r34588: Really uniquify representation strings by including the next_node_id as part of the uniquifier, and rename a structure member appropriately.
    * subversion/libsvn_fs_fs/fs_fs.c
      (read_rep_offsets, representation_string, svn_fs_fs__noderev_same_rep_key,
       rep_write_contents_close, svn_fs_fs__rep_copy): Update variable name.
      (svn_fs_fs__set_entry, rep_write_contents_close): Use the next node id to
        further uniquify the rep string within a txn.
    
    * subversion/libsvn_fs_fs/fs.h
      (representation_t): Rename orig_txn_id to uniquifier.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@874690 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_fs/fs.h
+++ subversion/libsvn_fs_fs/fs.h
@@ -297,8 +297,9 @@ typedef struct
 
   /* For rep-sharing, we need a way of uniquifying node-revs which share the
      same representation (see svn_fs_fs__noderev_same_rep_key() ).  So, we
-     store the original txn of the node rev (not the rep!) here. */
-  const char *orig_txn_id;
+     store the original txn of the node rev (not the rep!), along with some
+     intra-node uniqification content. */
+  const char *uniquifier;
 } representation_t;
 
 
