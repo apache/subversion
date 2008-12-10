773a69b1e56e50c847c5b45c2cff6c7de109bcaa
Julian Foad <julianfoad@apache.org>
2008-12-10 18:22:49 +0000

    Adjust test expectations to account for progress so far. This solves 1 of the 2 remaining test failures, leaving this one:
    FAIL:  update_tests.py 50: tree conflicts on update 2.3
    
    * subversion/tests/cmdline/update_tests.py
      (tree_conflicts_on_update_2_1): Tweak expectations.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/tc-issue-3334@874725 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/update_tests.py
+++ subversion/tests/cmdline/update_tests.py
@@ -4004,13 +4004,14 @@ def tree_conflicts_on_update_2_1(sbox):
 
   expected_status = deep_trees_status_local_leaf_edit
   expected_status.tweak(
-    'D/D1',
+    #'D/D1',
+    # The expectation on 'alpha' reflects partial progress on issue #3334.
     'F/alpha',
-    'DD/D1',
-    'DF/D1',
-    'DDD/D1',
-    'DDF/D1',
-    status='A ')
+    #'DD/D1',
+    #'DF/D1',
+    #'DDD/D1',
+    #'DDF/D1',
+    status='A ', copied='+', wc_rev='-')
 
   svntest.actions.deep_trees_run_tests_scheme_for_update(sbox,
     [ DeepTreesTestCase("local_leaf_edit_incoming_tree_del",
