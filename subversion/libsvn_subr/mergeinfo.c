658b2556646e1817243d9e0d74547544bd080b36
Julian Foad <julianfoad@apache.org>
2008-12-17 14:23:10 +0000

    On the 1.5.x-build-fixes branch: Revert r34741, as it was only needed to support two functions that were introduced in r34740 but removed in r34763.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/1.5.x-build-fixes@874838 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/mergeinfo.c
+++ subversion/libsvn_subr/mergeinfo.c
@@ -1209,14 +1209,11 @@ svn_rangelist_to_string(svn_string_t **output,
   return SVN_NO_ERROR;
 }
 
-/* Converts a mergeinfo INPUT to an unparsed mergeinfo in OUTPUT.  If PREFIX
-   is not NULL then prepend PREFIX to each line in OUTPUT.  If INPUT contains
-   no elements, return the empty string.
+/* Converts a mergeinfo @a input to an unparsed mergeinfo in @a
+ * output.  If @a input contains no elements, return the empty string.
  */
 static svn_error_t *
-mergeinfo_to_stringbuf(svn_stringbuf_t **output,
-                       svn_mergeinfo_t input,
-                       const char *prefix,
+mergeinfo_to_stringbuf(svn_stringbuf_t **output, svn_mergeinfo_t input,
                        apr_pool_t *pool)
 {
   *output = svn_stringbuf_create("", pool);
@@ -1234,8 +1231,7 @@ mergeinfo_to_stringbuf(svn_stringbuf_t **output,
 
           SVN_ERR(svn_rangelist_to_string(&revlist, elt.value, pool));
           svn_stringbuf_appendcstr(*output,
-                                   apr_psprintf(pool, "%s%s:%s",
-                                                prefix ? prefix : "",
+                                   apr_psprintf(pool, "%s:%s",
                                                 (char *) elt.key,
                                                 revlist->data));
           if (i < sorted->nelts - 1)
@@ -1253,7 +1249,7 @@ svn_mergeinfo_to_string(svn_string_t **output, svn_mergeinfo_t input,
   if (apr_hash_count(input) > 0)
     {
       svn_stringbuf_t *mergeinfo_buf;
-      SVN_ERR(mergeinfo_to_stringbuf(&mergeinfo_buf, input, NULL, pool));
+      SVN_ERR(mergeinfo_to_stringbuf(&mergeinfo_buf, input, pool));
       *output = svn_string_create_from_buf(mergeinfo_buf, pool);
     }
   else
