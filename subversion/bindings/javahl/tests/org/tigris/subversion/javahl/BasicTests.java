b652714a10cc0e8839559631a83ac5851178b3eb
Mark Phippard <markphip@apache.org>
2008-12-19 17:00:14 +0000

    Merge r34196
     * r34196
       JavaHL: Update tests to adjust move expectations of creating mergeinfo.
       Justification:
         Backport of r34184 changed move/copy behavior and tests needed to be
         updated.  This was done on trunk and was just not included in backport
         recommendation.
       Votes:
         +1: markphip, hwright
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/1.5.x@874925 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/javahl/tests/org/tigris/subversion/javahl/BasicTests.java
+++ subversion/bindings/javahl/tests/org/tigris/subversion/javahl/BasicTests.java
@@ -862,11 +862,9 @@ public class BasicTests extends SVNTests
             wc.addItem("A/B/F/" + fileName,
                        wc.getItemContent("A/B/E/" + fileName));
             wc.setItemWorkingCopyRevision("A/B/F/" + fileName, 2);
-            wc.setItemPropStatus("A/B/F/" + fileName, Status.Kind.normal);
             addExpectedCommitItem(thisTest.getWCPath(), thisTest.getUrl(),
                                   "A/B/F/" + fileName, NodeKind.file,
                                   CommitItemStateFlags.Add |
-                                  CommitItemStateFlags.PropMods |
                                   CommitItemStateFlags.IsCopy);
 
             wc.removeItem("A/B/E/" + fileName);
