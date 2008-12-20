a5ec5b7ba551e22ce2e153cc75088829c59ca572
Bert Huijben <rhuijben@apache.org>
2008-12-20 11:48:53 +0000

    * subversion/tests/cmdline/depth_tests.py   (upgrade_from_above): Clone the sandbox instead of trying to clean from     within the test. The cleaning code frequently failed to remove all files     when testing BDB via mod_dav_svn and svnserve. (Especially on Windows     where files can't be deleted while some process has a handle open)
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@874949 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/depth_tests.py
+++ subversion/tests/cmdline/depth_tests.py
@@ -1257,7 +1257,7 @@ def add_tree_with_depth(sbox):
 
 def upgrade_from_above(sbox):
   "upgrade a depth=empty wc from above"
-
+  
   # The bug was that 'svn up --set-depth=files' worked from within the
   # working copy, but not from without with working copy top given
   # as an argument.  Both ways would correctly cause 'iota' to
@@ -1270,6 +1270,8 @@ def upgrade_from_above(sbox):
   #   Subject: Problem upgrading working copy depth
   #   Date: Wed, 19 Sep 2007 23:15:24 +0700
   #   Message-ID: <46F14B1C.8010406@svnkit.com>
+
+  sbox2 = sbox.clone_dependent() 
 
   wc, ign_a, ign_b, ign_c = set_up_depthy_working_copies(sbox, empty=True)
 
@@ -1297,9 +1299,8 @@ def upgrade_from_above(sbox):
   finally:
     os.chdir(saved_cwd)
 
-  # Reset and do it again, this time from above the working copy.
-  svntest.main.safe_rmtree(wc)
-  wc, ign_a, ign_b, ign_c = set_up_depthy_working_copies(sbox, empty=True)
+  # Do it again, this time from above the working copy.
+  wc, ign_a, ign_b, ign_c = set_up_depthy_working_copies(sbox2, empty=True)
   expected_output = svntest.wc.State(wc, {
       'iota'    : Item(status='A '),
       })
