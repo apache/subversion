0b1bec5e364cd6485be710745abd96049ef7aa5e
Hyrum Kurt Wright <hwright@apache.org>
2008-12-24 16:16:19 +0000

    * tools/buildbot/slaves/x64-ubuntu/svncheck.sh:   Include the swig bindings in the list of targets to check.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@874999 13f79535-47bb-0310-9956-ffa450edef68

--- tools/buildbot/slaves/ubuntu-x64/svncheck.sh
+++ tools/buildbot/slaves/ubuntu-x64/svncheck.sh
@@ -16,10 +16,16 @@ test -e ../mount-ramdrive && ../mount-ramdrive
 echo "========= make check"
 make check FS_TYPE=$1 CLEANUP=1 || exit $?
 
-# echo "========= make check-swig-pl"
-# make check-swig-pl || exit $?
+echo "========= make check-javahl"
+make check-javahl || exit $?
 
-#echo "========= make check-swig-rb"
-#make check-swig-rb || exit $?
+echo "========= make check-swig-pl"
+make check-swig-pl || exit $?
+
+echo "========= make check-swig-py"
+make check-swig-py || exit $?
+
+echo "========= make check-swig-rb"
+make check-swig-rb || exit $?
 
 exit 0
