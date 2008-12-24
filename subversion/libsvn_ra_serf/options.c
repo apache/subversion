7724a1ec6781fd7363816f8984320aff59955339
Ben Collins-Sussman <sussman@apache.org>
2008-12-24 18:58:33 +0000

    On the 'http-protocol-v2' branch: Bugfix spotted by gstein.
    * subversion/libsvn_ra_serf/options.c 
      (capabilities_headers_iterator_callback): compare first 3 chars of key with "SVN".
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/http-protocol-v2@875002 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_ra_serf/options.c
+++ subversion/libsvn_ra_serf/options.c
@@ -311,7 +311,7 @@ capabilities_headers_iterator_callback(void *baton,
     }
 
   /* SVN-specific headers -- if present, server supports HTTP protocol v2 */
-  if (svn_cstring_casecmp(key, "svn") == 0)
+  if (strncmp(key, "SVN", 3) == 0)
     {
       if (svn_cstring_casecmp(key, SVN_DAV_ROOT_STUB_HEADER) == 0)
         orc->session->root_stub = apr_pstrdup(orc->session->pool, val);
