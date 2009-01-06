2c404ad7d89712800b859f476f8afd5c8d885eff
Paul Burba <pburba@apache.org>
2009-01-06 15:31:29 +0000

    Allow colons within mergeinfo path names.
    Previously a mergeinfo pathname containing colons produced a parsing error.
    
    Found by: Damien Deville <damien.deville@netasq.com>
    
    * subversion/libsvn_subr/mergeinfo.c
      (parse_pathname): Permit colons in the pathname.
    
    * subversion/tests/libsvn_subr/mergeinfo-test.c
      (NBR_MERGEINFO_VALS, mergeinfo_vals, mergeinfo_paths, mergeinfo_ranges):
      Add some test cases for test_parse_single_line_mergeinfo() which have
      pathnames with colons.
      (NBR_BROKEN_MERGEINFO_VALS, broken_mergeinfo_vals):
      Add some test cases for test_parse_broken_mergeinfo() which have
      pathnames with colons.
      (test_merge_mergeinfo): Add some colons to pathnames.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@875114 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/libsvn_subr/mergeinfo-test.c
+++ subversion/tests/libsvn_subr/mergeinfo-test.c
@@ -110,7 +110,8 @@ verify_mergeinfo_parse(const char *input,
    -> merge ranges. */
 static apr_hash_t *info1, *info2;
 
-#define NBR_MERGEINFO_VALS 5
+#define NBR_MERGEINFO_VALS 8
+
 /* Valid mergeinfo values. */
 static const char * const mergeinfo_vals[NBR_MERGEINFO_VALS] =
   {
@@ -118,7 +119,10 @@ static const char * const mergeinfo_vals[NBR_MERGEINFO_VALS] =
     "/trunk/foo:1-6",
     "/trunk: 5,7-9,10,11,13,14",
     "/trunk: 3-10,11*,13,14",
-    "/branch: 1,2-18*,33*"
+    "/branch: 1,2-18*,33*",
+    "patch-common::netasq-bpf.c:25381",
+    "patch-common_netasq-bpf.c::25381",
+    ":patch:common:netasq:bpf.c:25381"
   };
 /* Paths corresponding to mergeinfo_vals. */
 static const char * const mergeinfo_paths[NBR_MERGEINFO_VALS] =
@@ -127,7 +131,10 @@ static const char * const mergeinfo_paths[NBR_MERGEINFO_VALS] =
     "/trunk/foo",
     "/trunk",
     "/trunk",
-    "/branch"
+    "/branch",
+    "patch-common::netasq-bpf.c",
+    "patch-common_netasq-bpf.c:",
+    ":patch:common:netasq:bpf.c"
   };
 /* First ranges from the paths identified by mergeinfo_paths. */
 static svn_merge_range_t mergeinfo_ranges[NBR_MERGEINFO_VALS][MAX_NBR_RANGES] =
@@ -136,7 +143,10 @@ static svn_merge_range_t mergeinfo_ranges[NBR_MERGEINFO_VALS][MAX_NBR_RANGES] =
     { {0, 6,  TRUE} },
     { {4, 5,  TRUE}, { 6, 11, TRUE }, {12, 14, TRUE } },
     { {2, 10, TRUE}, {10, 11, FALSE}, {12, 14, TRUE } },
-    { {0, 1,  TRUE}, { 1, 18, FALSE}, {32, 33, FALSE} }
+    { {0, 1,  TRUE}, { 1, 18, FALSE}, {32, 33, FALSE} },
+    { {25380, 25381, TRUE } },
+    { {25380, 25381, TRUE } },
+    { {25380, 25381, TRUE } }
   };
 
 static svn_error_t *
@@ -246,7 +256,7 @@ test_parse_combine_rangeinfo(const char **msg,
 }
 
 
-#define NBR_BROKEN_MERGEINFO_VALS 38
+#define NBR_BROKEN_MERGEINFO_VALS 39
 /* Invalid mergeinfo values. */
 static const char * const broken_mergeinfo_vals[NBR_BROKEN_MERGEINFO_VALS] =
   {
@@ -291,6 +301,7 @@ static const char * const broken_mergeinfo_vals[NBR_BROKEN_MERGEINFO_VALS] =
     /* path mapped to range with no revisions */
     "/trunk:",
     "/trunk:2-9\n/branch:",
+    "::",
     /* No path */
     ":1-3",
     /* Invalid revisions */
@@ -621,9 +632,9 @@ test_merge_mergeinfo(const char **msg,
                         {12, 13, TRUE}, {13, 22, FALSE} } } } },
 
       /* Two paths all inheritable ranges */
-      { "/trunk: 3,5,7-9,10,11,13,14\n/fred:8-10",
-        "/trunk: 1-4,6\n/fred:9-12", 2,
-        { {"/trunk", { {0, 11, TRUE}, {12, 14, TRUE} } },
+      { "/trunk::1: 3,5,7-9,10,11,13,14\n/fred:8-10",
+        "/trunk::1: 1-4,6\n/fred:9-12", 2,
+        { {"/trunk::1", { {0, 11, TRUE}, {12, 14, TRUE} } },
           {"/fred",  { {7, 12, TRUE} } } } },
 
       /* Two paths all non-inheritable ranges */
@@ -641,11 +652,11 @@ test_merge_mergeinfo(const char **msg,
 
       /* A slew of different paths but no ranges to be merged */
       { "/trunk: 3,5-9*\n/betty: 2-4",
-        "/fred: 1-18\n/barney: 1,3-43", 4,
-        { {"/trunk",  { {2,  3, TRUE}, {4,  9, FALSE} } },
-          {"/betty",  { {1,  4, TRUE} } },
-          {"/barney", { {0,  1, TRUE}, {2, 43, TRUE} } },
-          {"/fred",   { {0, 18, TRUE} } } } }
+        "/fred: 1-18\n/:barney: 1,3-43", 4,
+        { {"/trunk",   { {2,  3, TRUE}, {4,  9, FALSE} } },
+          {"/betty",   { {1,  4, TRUE} } },
+          {"/:barney", { {0,  1, TRUE}, {2, 43, TRUE} } },
+          {"/fred",    { {0, 18, TRUE} } } } }
     };
 
   *msg = "merging of mergeinfo hashs";
