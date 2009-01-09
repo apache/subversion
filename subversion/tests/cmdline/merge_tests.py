d0582f03d0f1a11954652c80722d7325ede2aa8e
Kamesh J <kameshj@apache.org>
2009-01-09 13:19:51 +0000

    On issue-2897-take2:
    Merge r28621 from ^/branches/issue-2897
    
    Resolved conflicts on subversion/tests/cmdline/merge_tests.py
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/issue-2897-take2@875185 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/merge_tests.py
+++ subversion/tests/cmdline/merge_tests.py
@@ -15797,6 +15797,34 @@ def merge_non_reflective_text_and_prop_change(sbox):
                                        None, None, None, None, None, 1,
                                        dry_run = False)
   os.chdir(saved_cwd)
+  # Revert 
+  svntest.actions.run_and_verify_svn(None, None, [], 'revert', '-R', wc_dir)
+  #Redo the above merge with current working directory being a implicit
+  #target
+  os.chdir(ABE_path)
+  #TODO Duplicating above expected_* for the target '', should be
+  #smart enough here to reuse without duplicating.
+  expected_output = wc.State('', {
+    ''         : Item(status=' U'),
+    'alpha'    : Item(status='G '),
+    'beta'     : Item(status=' U'),
+    })
+  expected_status = wc.State('', {
+    ''         : Item(status=' M', wc_rev=7),
+    'alpha'    : Item(status='M ', wc_rev=7),
+    'beta'     : Item(status=' M', wc_rev=7),
+    })
+  expected_skip = wc.State('', {})
+
+  svntest.actions.run_and_verify_merge('', None, None,
+                                       sbox.repo_url + '/A/B/E_COPY',
+                                       expected_output,
+                                       expected_disk,
+                                       expected_status,
+                                       expected_skip,
+                                       None, None, None, None, None, 1,
+                                       dry_run = False)
+  os.chdir(saved_cwd)
 
 def merge_non_reflective_with_conflict(sbox):
   "merge non-reflective with conflict"
