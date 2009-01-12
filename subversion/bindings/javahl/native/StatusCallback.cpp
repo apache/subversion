0d535ac4f154cc83b02e386ece73a3485b7d0009
Hyrum Kurt Wright <hwright@apache.org>
2009-01-12 22:17:50 +0000

    "Curse my metal body, I wasn't fast enough!"
    Revert a bogus change in r35195 caused by muscle memory and a fast internet
    connection.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@875270 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/javahl/native/StatusCallback.cpp
+++ subversion/bindings/javahl/native/StatusCallback.cpp
@@ -143,7 +143,6 @@ StatusCallback::createJavaStatus(const char *path,
   jboolean jIsLocked = JNI_FALSE;
   jboolean jIsCopied = JNI_FALSE;
   jboolean jIsSwitched = JNI_FALSE;
-  jboolean jIsFileExternal = JNI_FALSE;
   jboolean jIsTreeConflicted = JNI_FALSE;
   jobject jConflictDescription = NULL;
   jstring jConflictOld = NULL;
@@ -174,7 +173,6 @@ StatusCallback::createJavaStatus(const char *path,
       jIsCopied = (status->copied == 1) ? JNI_TRUE: JNI_FALSE;
       jIsLocked = (status->locked == 1) ? JNI_TRUE: JNI_FALSE;
       jIsSwitched = (status->switched == 1) ? JNI_TRUE: JNI_FALSE;
-      jIsFileExternal = (status->file_external == 1) ? JNI_TRUE: JNI_FALSE;
       jConflictDescription = ConflictResolverCallback::createJConflictDescriptor(
                                                       status->tree_conflict);
       if (JNIUtil::isJavaExceptionThrown())
@@ -250,10 +248,10 @@ StatusCallback::createJavaStatus(const char *path,
                                jLastCommitAuthor, jTextType, jPropType,
                                jRepositoryTextType, jRepositoryPropType,
                                jIsLocked, jIsCopied, jIsTreeConflicted,
-                               jConflictDescription, jConflictOld, jConflictNew,
-                               jConflictWorking, jURLCopiedFrom,
-                               jRevisionCopiedFrom, jIsSwitched, jIsFileExternal,
-                               jLockToken, jLockOwner,
+                               jConflictDescription,
+                               jConflictOld, jConflictNew, jConflictWorking,
+                               jURLCopiedFrom, jRevisionCopiedFrom,
+                               jIsSwitched, jLockToken, jLockOwner,
                                jLockComment, jLockCreationDate, jLock,
                                jOODLastCmtRevision, jOODLastCmtDate,
                                jOODKind, jOODLastCmtAuthor, jChangelist);
