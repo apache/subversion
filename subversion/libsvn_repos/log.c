3ef056adfacac00f84d2ede07292b4f71446d4a8
Hyrum Kurt Wright <hwright@apache.org>
2009-01-16 15:25:54 +0000

    Followup to r35253: Update some callers of svn_fs_paths_changed() to use the new API.
    * subversion/libsvn_repos/log.c
      (svn_repos_check_revision_access, detect_changed, fs_mergeinfo_changed):
      Update to use the new API, along with the corresponding output datatype.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@875363 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_repos/log.c
+++ subversion/libsvn_repos/log.c
@@ -60,7 +60,7 @@ svn_repos_check_revision_access(svn_repos_revision_access_level_t *access_level,
 
   /* Fetch the changes associated with REVISION. */
   SVN_ERR(svn_fs_revision_root(&rev_root, fs, revision, pool));
-  SVN_ERR(svn_fs_paths_changed(&changes, rev_root, pool));
+  SVN_ERR(svn_fs_paths_changed2(&changes, rev_root, pool));
 
   /* No changed paths?  We're done. */
   if (apr_hash_count(changes) == 0)
@@ -73,7 +73,7 @@ svn_repos_check_revision_access(svn_repos_revision_access_level_t *access_level,
     {
       const void *key;
       void *val;
-      svn_fs_path_change_t *change;
+      svn_fs_path_change2_t *change;
       svn_boolean_t readable;
 
       svn_pool_clear(subpool);
@@ -181,7 +181,7 @@ detect_changed(apr_hash_t **changed,
   svn_boolean_t found_unreadable = FALSE;
 
   *changed = apr_hash_make(pool);
-  SVN_ERR(svn_fs_paths_changed(&changes, root, pool));
+  SVN_ERR(svn_fs_paths_changed2(&changes, root, pool));
 
   if (apr_hash_count(changes) == 0)
     /* No paths changed in this revision?  Uh, sure, I guess the
@@ -195,7 +195,7 @@ detect_changed(apr_hash_t **changed,
          here, so we'll live with the duplication. */
       const void *key;
       void *val;
-      svn_fs_path_change_t *change;
+      svn_fs_path_change2_t *change;
       const char *path;
       char action;
       svn_log_changed_path_t *item;
@@ -532,7 +532,7 @@ fs_mergeinfo_changed(svn_mergeinfo_catalog_t *deleted_mergeinfo_catalog,
   /* We're going to use the changed-paths information for REV to
      narrow down our search. */
   SVN_ERR(svn_fs_revision_root(&root, fs, rev, subpool));
-  SVN_ERR(svn_fs_paths_changed(&changes, root, subpool));
+  SVN_ERR(svn_fs_paths_changed2(&changes, root, subpool));
 
   /* No changed paths?  We're done. */
   if (apr_hash_count(changes) == 0)
@@ -549,7 +549,7 @@ fs_mergeinfo_changed(svn_mergeinfo_catalog_t *deleted_mergeinfo_catalog,
     {
       const void *key;
       void *val;
-      svn_fs_path_change_t *change;
+      svn_fs_path_change2_t *change;
       const char *changed_path, *base_path = NULL;
       svn_revnum_t base_rev = SVN_INVALID_REVNUM;
       svn_fs_root_t *base_root = NULL;
