c9a2f504f51a5fe6a0d34b04772bad3754f227c7
Bert Huiben <rhuijben@apache.org>
2009-01-21 22:55:14 +0000

    Following up on r33594, rename the svn_client_commit_item3_t constructor to svn_client_commit_item3_create() to make the function match the structure name like the other constructors.
    * subversion/include/svn_client.h
      (svn_client_commit_item3_t): Update comment.
      (svn_client_commit_item_create2): Rename to ...
      (svn_client_commit_item3_create): ... this.
    * subversion/libsvn_client/util.c:
      (svn_client_commit_item_create2): Rename to ...
      (svn_client_commit_item3_create): ... this.
      (svn_client_commit_item_create): Update caller.
    * subversion/libsvn_client/add.c
      (mkdir_urls):
    * subversion/libsvn_client/commit.c
      (svn_client_import3):
    * subversion/libsvn_client/commit_util.c
      (add_committable):
    * subversion/libsvn_client/copy.c
      (repos_to_repos_copy, wc_to_repos_copy):
    * subversion/libsvn_client/delete.c
      (delete_urls):
    * subversion/libsvn_client/prop_commands.c
      (propset_on_url):
        Update callers.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@875458 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/prop_commands.c
+++ subversion/libsvn_client/prop_commands.c
@@ -263,7 +263,7 @@ propset_on_url(svn_commit_info_t **commit_info_p,
       const char *tmp_file;
       apr_array_header_t *commit_items = apr_array_make(pool, 1, sizeof(item));
 
-      item = svn_client_commit_item_create2(pool);
+      item = svn_client_commit_item3_create(pool);
       item->url = target;
       item->state_flags = SVN_CLIENT_COMMIT_ITEM_PROP_MODS;
       APR_ARRAY_PUSH(commit_items, svn_client_commit_item3_t *) = item;
