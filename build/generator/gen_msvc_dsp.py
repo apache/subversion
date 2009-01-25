9fa0d64c2d7fed360b0ff379d06c39610bf6b3f1
Arfrever Frehtes Taifersar Arahesis <arfrever@apache.org>
2009-01-25 03:05:08 +0000

    Python 3 compatibility: Don't assing values to True/False keywords.
    * build/generator/gen_make.py:
    * build/generator/gen_msvc_dsp.py:
    * build/generator/gen_vcnet_vcproj.py:
    * subversion/bindings/swig/python/svn/repos.py:
    * subversion/tests/cmdline/svntest/__init__.py:
    * tools/client-side/change-svn-wc-format.py:
    * tools/dev/contribulyze.py:
    * tools/hook-scripts/log-police.py:
    * tools/hook-scripts/mailer/mailer.py: Don't assing values to True/False keywords.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@875525 13f79535-47bb-0310-9956-ffa450edef68

--- build/generator/gen_msvc_dsp.py
+++ build/generator/gen_msvc_dsp.py
@@ -126,11 +126,3 @@ class Generator(gen_win.WinGeneratorBase):
       }
 
     self.write_with_template('subversion_msvc.dsw', 'msvc_dsw.ezt', data)
-
-
-# compatibility with older Pythons:
-try:
-  True
-except NameError:
-  True = 1
-  False = 0
