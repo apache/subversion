5bc39a4b148e5d71b8fa7a0de05efa0b2ab82563
Hyrum Kurt Wright <hwright@apache.org>
2009-01-26 18:54:37 +0000

    JavaHL: Start to consolidate all of the 'makeJ' and 'createJ' functions into one class.
    * subversion/bindings/javahl/native/ConflictResolverCallback.cpp
      (resolve): Update caller.
      (createJConflictDescriptor, createJConflictVersion): Remove.
    
    * subversion/bindings/javahl/native/CreateJ.cpp:
      New.
    
    * subversion/bindings/javahl/native/StatusCallback.cpp
      (createJavaStatus): Update caller.
    
    * subversion/bindings/javahl/native/ConflictResolverCallback.h
      (createJConflictDescriptor, createJConflictVersion): Remove.
    
    * subversion/bindings/javahl/native/InfoCallback.cpp
      (createJavaInfo2): Update caller.
    
    * subversion/bindings/javahl/native/CreateJ.h:
      New.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@875548 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/javahl/native/ConflictResolverCallback.cpp
+++ subversion/bindings/javahl/native/ConflictResolverCallback.cpp
@@ -25,6 +25,7 @@
 #include "JNIUtil.h"
 #include "JNIStringHolder.h"
 #include "EnumMapper.h"
+#include "CreateJ.h"
 #include "ConflictResolverCallback.h"
 
 ConflictResolverCallback::ConflictResolverCallback(jobject jconflictResolver)
@@ -115,7 +116,7 @@ ConflictResolverCallback::resolve(svn_wc_conflict_result_t **result,
     }
 
   // Create an instance of the conflict descriptor.
-  jobject jdesc = createJConflictDescriptor(desc);
+  jobject jdesc = CreateJ::ConflictDescriptor(desc);
   if (JNIUtil::isJavaExceptionThrown())
     return SVN_NO_ERROR;
 
@@ -211,160 +212,4 @@ svn_wc_conflict_choice_t ConflictResolverCallback::javaChoiceToC(jint jchoice)
     case org_tigris_subversion_javahl_ConflictResult_chooseMerged:
       return svn_wc_conflict_choose_merged;
     }
-}
-
-jobject
-ConflictResolverCallback::createJConflictDescriptor(
-                                   const svn_wc_conflict_description_t *desc)
-{
-  JNIEnv *env = JNIUtil::getEnv();
-
-  if (desc == NULL)
-    return NULL;
-
-  // Create an instance of the conflict descriptor.
-  static jmethodID ctor = 0;
-  jclass clazz = env->FindClass(JAVA_PACKAGE "/ConflictDescriptor");
-  if (JNIUtil::isJavaExceptionThrown())
-    return NULL;
-
-  if (ctor == 0)
-    {
-      ctor = env->GetMethodID(clazz, "<init>", "(Ljava/lang/String;II"
-                              "Ljava/lang/String;ZLjava/lang/String;III"
-                              "Ljava/lang/String;Ljava/lang/String;"
-                              "Ljava/lang/String;Ljava/lang/String;"
-                              "L"JAVA_PACKAGE"/ConflictVersion;"
-                              "L"JAVA_PACKAGE"/ConflictVersion;)V");
-      if (JNIUtil::isJavaExceptionThrown() || ctor == 0)
-        return NULL;
-    }
-
-  jstring jpath = JNIUtil::makeJString(desc->path);
-  if (JNIUtil::isJavaExceptionThrown())
-    return NULL;
-  jstring jpropertyName = JNIUtil::makeJString(desc->property_name);
-  if (JNIUtil::isJavaExceptionThrown())
-    return NULL;
-  jstring jmimeType = JNIUtil::makeJString(desc->mime_type);
-  if (JNIUtil::isJavaExceptionThrown())
-    return NULL;
-  jstring jbasePath = JNIUtil::makeJString(desc->base_file);
-  if (JNIUtil::isJavaExceptionThrown())
-    return NULL;
-  jstring jreposPath = JNIUtil::makeJString(desc->their_file);
-  if (JNIUtil::isJavaExceptionThrown())
-    return NULL;
-  jstring juserPath = JNIUtil::makeJString(desc->my_file);
-  if (JNIUtil::isJavaExceptionThrown())
-    return NULL;
-  jstring jmergedPath = JNIUtil::makeJString(desc->merged_file);
-  if (JNIUtil::isJavaExceptionThrown())
-    return NULL;
-  jobject jsrcLeft = ConflictResolverCallback::createJConflictVersion(
-                                                      desc->src_left_version);
-  if (JNIUtil::isJavaExceptionThrown())
-    return NULL;
-  jobject jsrcRight = ConflictResolverCallback::createJConflictVersion(
-                                                      desc->src_right_version);
-  if (JNIUtil::isJavaExceptionThrown())
-    return NULL;
-
-  // Instantiate the conflict descriptor.
-  jobject jdesc = env->NewObject(clazz, ctor, jpath,
-                                 EnumMapper::mapConflictKind(desc->kind),
-                                 EnumMapper::mapNodeKind(desc->node_kind),
-                                 jpropertyName,
-                                 (jboolean) desc->is_binary, jmimeType,
-                                 EnumMapper::mapConflictAction(desc->action),
-                                 EnumMapper::mapConflictReason(desc->reason),
-                                 EnumMapper::mapOperation(desc->operation),
-                                 jbasePath, jreposPath, juserPath,
-                                 jmergedPath, jsrcLeft, jsrcRight);
-  if (JNIUtil::isJavaExceptionThrown())
-    return NULL;
-
-  env->DeleteLocalRef(clazz);
-  if (JNIUtil::isJavaExceptionThrown())
-    return NULL;
-
-  env->DeleteLocalRef(jpath);
-  if (JNIUtil::isJavaExceptionThrown())
-    return NULL;
-  env->DeleteLocalRef(jpropertyName);
-  if (JNIUtil::isJavaExceptionThrown())
-    return NULL;
-  env->DeleteLocalRef(jmimeType);
-  if (JNIUtil::isJavaExceptionThrown())
-    return NULL;
-  env->DeleteLocalRef(jbasePath);
-  if (JNIUtil::isJavaExceptionThrown())
-    return NULL;
-  env->DeleteLocalRef(jreposPath);
-  if (JNIUtil::isJavaExceptionThrown())
-    return NULL;
-  env->DeleteLocalRef(juserPath);
-  if (JNIUtil::isJavaExceptionThrown())
-    return NULL;
-  env->DeleteLocalRef(jmergedPath);
-  if (JNIUtil::isJavaExceptionThrown())
-    return NULL;
-  env->DeleteLocalRef(jsrcRight);
-  if (JNIUtil::isJavaExceptionThrown())
-    return NULL;
-  env->DeleteLocalRef(jsrcLeft);
-  if (JNIUtil::isJavaExceptionThrown())
-    return NULL;
-
-  return jdesc;
-}
-
-jobject
-ConflictResolverCallback::createJConflictVersion(
-                                    const svn_wc_conflict_version_t *version)
-{
-  JNIEnv *env = JNIUtil::getEnv();
-
-  if (version == NULL)
-    return NULL;
-
-  // Create an instance of the conflict descriptor.
-  static jmethodID ctor = 0;
-  jclass clazz = env->FindClass(JAVA_PACKAGE "/ConflictVersion");
-  if (JNIUtil::isJavaExceptionThrown())
-    return NULL;
-
-  if (ctor == 0)
-    {
-      ctor = env->GetMethodID(clazz, "<init>", "(Ljava/lang/String;J"
-                                               "Ljava/lang/String;I)V");
-      if (JNIUtil::isJavaExceptionThrown() || ctor == 0)
-        return NULL;
-    }
-
-  jstring jreposURL = JNIUtil::makeJString(version->repos_url);
-  if (JNIUtil::isJavaExceptionThrown())
-    return NULL;
-  jstring jpathInRepos = JNIUtil::makeJString(version->path_in_repos);
-  if (JNIUtil::isJavaExceptionThrown())
-    return NULL;
-
-  jobject jversion = env->NewObject(clazz, ctor, jreposURL,
-                                 (jlong)version->peg_rev, jpathInRepos,
-                                 EnumMapper::mapNodeKind(version->node_kind));
-  if (JNIUtil::isJavaExceptionThrown())
-    return NULL;
-
-  env->DeleteLocalRef(clazz);
-  if (JNIUtil::isJavaExceptionThrown())
-    return NULL;
-
-  env->DeleteLocalRef(jreposURL);
-  if (JNIUtil::isJavaExceptionThrown())
-    return NULL;
-  env->DeleteLocalRef(jpathInRepos);
-  if (JNIUtil::isJavaExceptionThrown())
-    return NULL;
-
-  return jversion;
 }
