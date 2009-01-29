d14e711e184c08b7e50153a0f7912c56d592fff2
Hyrum Kurt Wright <hwright@apache.org>
2009-01-29 01:11:26 +0000

    Change the 'kind' and 'depth' columns in the wc-ng metadata schema to text values, partly for human readability, and partly for consistency with existing ways of persisting these values.
    * subversion/libsvn_wc/wc-metadata.sql
      (BASE_NODE, WORKING_NODE): Make kind and depth TEXT type.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@875608 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/wc-metadata.sql
+++ subversion/libsvn_wc/wc-metadata.sql
@@ -78,7 +78,7 @@ CREATE TABLE BASE_NODE (
   revnum  INTEGER NOT NULL,
 
   /* file/dir/special. none says this node is NOT present at this REV. */
-  kind  INTEGER NOT NULL,
+  kind  TEXT NOT NULL,
 
   /* if this node is a file, then the checksum and its translated size
      (given the properties on this file) are specified by the following
@@ -92,7 +92,7 @@ CREATE TABLE BASE_NODE (
   changed_author  TEXT NOT NULL,
 
   /* NULL depth means "default" (typically svn_depth_infinity) */
-  depth  INTEGER,
+  depth  TEXT,
 
   /* ### Do we need this?  We've currently got various mod time APIs
      ### internal to libsvn_wc, but those might be used in answering some
@@ -147,7 +147,7 @@ CREATE TABLE WORKING_NODE (
        if a BASE_NODE exists at the same local_relpath, then this is a
        replaced item (possibly copied or moved here), which implies the
        base node should be deleted first. */
-  kind  INTEGER NOT NULL,
+  kind  TEXT NOT NULL,
 
   /* Where this node was copied from. Set only on the root of the copy,
      and implied for all children. */
@@ -176,7 +176,7 @@ CREATE TABLE WORKING_NODE (
   changed_author  TEXT,
 
   /* NULL depth means "default" (typically svn_depth_infinity) */
-  depth  INTEGER,
+  depth  TEXT,
 
   /* ### Do we need this?  We've currently got various mod time APIs
      ### internal to libsvn_wc, but those might be used in answering some
