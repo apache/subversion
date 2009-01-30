b6b11f701e2c1dfeb9ab8be736f713c9504f6106
arfrever <dev-null@apache.org>
2009-01-30 20:43:02 +0000

    Python 3 compatibility: Don't use 'cmp' argument of sort() in 'build' directory.
    * build/generator/gen_make.py:
    * build/generator/gen_msvc_dsp.py:
    * build/generator/gen_vcnet_vcproj.py:
    * build/generator/gen_win.py: Don't use 'cmp' argument of sort().
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@875674 13f79535-47bb-0310-9956-ffa450edef68

--- build/generator/gen_vcnet_vcproj.py
+++ build/generator/gen_vcnet_vcproj.py
@@ -226,7 +226,7 @@ class Generator(gen_win.WinGeneratorBase):
       target.path = target.path.rstrip('"')
       target.path = target.path.lstrip('"')
 
-    targets.sort(lambda x, y: cmp(x.name, y.name))
+    targets.sort(key = lambda x: x.name)
 
     configs = [ ]
     for i in range(len(self.configs)):
