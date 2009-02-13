4bc28acc96f838cc880dc8229b6157098e4cb936
Hyrum Kurt Wright <hwright@apache.org>
2009-02-13 15:34:40 +0000

    Revert r35748, in accordance with the sentiment described in this thread: http://svn.haxx.se/dev/archive-2009-02/0151.shtml
    [ There were accidental changes to subversion/tests/cmdline/commit_tess.py
      committed with the revert.  Those changes were themselves reverted in
      r35848. ]
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@875920 13f79535-47bb-0310-9956-ffa450edef68

--- build/generator/gen_win.py
+++ build/generator/gen_win.py
@@ -1389,7 +1389,7 @@ class WinGeneratorBase(GeneratorBase):
       os.path.join('subversion', 'libsvn_fs_fs', 'rep-cache-db'),
       ]
     for sql in sql_sources:
-      transform_sql.main(sql + '.sql', sql + '.sql_h')
+      transform_sql.main(sql + '.sql', sql + '.h')
     
 class ProjectItem:
   "A generic item class for holding sources info, config info, etc for a project"
