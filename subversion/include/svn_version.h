57b064936579f8134310d0148a00f70394ba9409
Hyrum Kurt Wright <hwright@apache.org>
2009-02-16 20:14:25 +0000

    Post-branch housekeeping.
    * subversion/include/svn_version.h: Increment minor version number.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@875964 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_version.h
+++ subversion/include/svn_version.h
@@ -57,7 +57,7 @@ extern "C" {
  * Modify when new functionality is added or new interfaces are
  * defined, but all changes are backward compatible.
  */
-#define SVN_VER_MINOR      6
+#define SVN_VER_MINOR      7
 
 /**
  * Patch number.
