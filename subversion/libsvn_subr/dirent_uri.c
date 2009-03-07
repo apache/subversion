d61294a420a82e4f642aea6723513de2ba103d05
Bert Huiben <rhuijben@apache.org>
2009-03-07 12:34:47 +0000

    * subversion/libsvn_subr/dirent_uri.c   (svn_uri_is_canonical): Update function to return false on UNC paths.     Code should use svn_dirent_*() (or svn_path_*()) on UNC paths. * subversion/tests/libsvn_subr/dirent_uri-test.c   (test_dirent_is_canonical): Verify that canonical paths are not changed     by canonicalizing them.   (test_uri_is_canonical): Verify that canonical paths are not changed     by canonicalizing them. Add a few UNC path test values.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@876457 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/dirent_uri.c
+++ subversion/libsvn_subr/dirent_uri.c
@@ -1429,9 +1429,6 @@ svn_uri_is_canonical(const char *uri, apr_pool_t *pool)
           ! (*(ptr+1) >= 'A' && *(ptr+1) <= 'Z') &&
           *(ptr+2) == ':')
         return FALSE;
-      /* if this is an UNC path, we have two '/' here */
-      if (*(ptr+1) == '/')
-        ptr++;
     }
 #endif /* WIN32 or Cygwin */
 
