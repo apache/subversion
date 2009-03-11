b07306e53b9df30890f72f7d3594931bc7c1c812
Greg Stein <gstein@apache.org>
2009-03-11 16:17:01 +0000

    Move the Sandbox class into its own module. It is going to grow quite a bit larger, so give it space.
    * subversion/tests/cmdline/svntest/sandbox.py: new module
      (Sandbox): moved from main.py. many references have svntest.main added
        to them. some minor whitespace fixes in default args.
      (_deferred_test_paths): moved from main.py
      (cleanup_deferred_test_paths): moved from main._cleanup_deferred_test_paths.
        no need to copy the global. we will keep the reference.
      (_cleanup_test_path): moved from main.py. added svntest.main prefix to
        the verbose_mode value.
    
    * subversion/tests/cmdline/svntest/__init__.py: import sandbox
    
    * subversion/tests/cmdline/svntest/main.py:
      (Sandbox, _deferred_test_paths, _cleanup_deferred_test_paths,
        _cleanup_test_path): moved to sandbox.py
      (TestRunner.run, _internal_run_tests, run_tests): add svntest.sandbox
        prefix to various references.
    
    * subversion/tests/cmdline/svntest/testcase.py: update a couple comments
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@876557 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subversion/tests/cmdline/svntest/sandbox.py
@@ -0,0 +1,143 @@
+#
+# sandbox.py :  tools for manipulating a test's working area ("a sandbox")
+#
+# ====================================================================
+# Copyright (c) 2009 CollabNet.  All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.  The terms
+# are also available at http://subversion.tigris.org/license-1.html.
+# If newer versions of this license are posted there, you may use a
+# newer version instead, at your option.
+#
+# This software consists of voluntary contributions made by many
+# individuals.  For exact contribution history, see the revision
+# history and logs, available at http://subversion.tigris.org/.
+# ====================================================================
+#
+
+import os
+import shutil
+import copy
+
+import svntest
+
+
+class Sandbox:
+  """Manages a sandbox (one or more repository/working copy pairs) for
+  a test to operate within."""
+
+  dependents = None
+
+  def __init__(self, module, idx):
+    self._set_name("%s-%d" % (module, idx))
+
+  def _set_name(self, name, read_only=False):
+    """A convenience method for renaming a sandbox, useful when
+    working with multiple repositories in the same unit test."""
+    if not name is None:
+      self.name = name
+    self.read_only = read_only
+    self.wc_dir = os.path.join(svntest.main.general_wc_dir, self.name)
+    if not read_only:
+      self.repo_dir = os.path.join(svntest.main.general_repo_dir, self.name)
+      self.repo_url = (svntest.main.test_area_url + '/'
+                       + svntest.main.pathname2url(self.repo_dir))
+    else:
+      self.repo_dir = svntest.main.pristine_dir
+      self.repo_url = svntest.main.pristine_url
+
+    ### TODO: Move this into to the build() method
+    # For dav tests we need a single authz file which must be present,
+    # so we recreate it each time a sandbox is created with some default
+    # contents.
+    if self.repo_url.startswith("http"):
+      # this dir doesn't exist out of the box, so we may have to make it
+      if not os.path.exists(work_dir):
+        os.makedirs(work_dir)
+      self.authz_file = os.path.join(work_dir, "authz")
+      open(self.authz_file, 'w').write("[/]\n* = rw\n")
+
+    # For svnserve tests we have a per-repository authz file, and it
+    # doesn't need to be there in order for things to work, so we don't
+    # have any default contents.
+    elif self.repo_url.startswith("svn"):
+      self.authz_file = os.path.join(self.repo_dir, "conf", "authz")
+
+    self.test_paths = [self.wc_dir, self.repo_dir]
+
+  def clone_dependent(self, copy_wc=False):
+    """A convenience method for creating a near-duplicate of this
+    sandbox, useful when working with multiple repositories in the
+    same unit test.  If COPY_WC is true, make an exact copy of this
+    sandbox's working copy at the new sandbox's working copy
+    directory.  Any necessary cleanup operations are triggered by
+    cleanup of the original sandbox."""
+
+    if not self.dependents:
+      self.dependents = []
+    clone = copy.deepcopy(self)
+    self.dependents.append(clone)
+    clone._set_name("%s-%d" % (self.name, len(self.dependents)))
+    if copy_wc:
+      self.add_test_path(clone.wc_dir)
+      shutil.copytree(self.wc_dir, clone.wc_dir, symlinks=True)
+    return clone
+
+  def build(self, name=None, create_wc=True, read_only=False):
+    self._set_name(name, read_only)
+    if svntest.actions.make_repo_and_wc(self, create_wc, read_only):
+      raise svntest.Failure("Could not build repository and sandbox '%s'"
+                            % self.name)
+
+  def add_test_path(self, path, remove=True):
+    self.test_paths.append(path)
+    if remove:
+      svntest.main.safe_rmtree(path)
+
+  def add_repo_path(self, suffix, remove=True):
+    path = (os.path.join(svntest.main.general_repo_dir, self.name)
+            + '.' + suffix)
+    url = svntest.main.test_area_url + '/' + svntest.main.pathname2url(path)
+    self.add_test_path(path, remove)
+    return path, url
+
+  def add_wc_path(self, suffix, remove=True):
+    path = self.wc_dir + '.' + suffix
+    self.add_test_path(path, remove)
+    return path
+
+  def cleanup_test_paths(self):
+    "Clean up detritus from this sandbox, and any dependents."
+    if self.dependents:
+      # Recursively cleanup any dependent sandboxes.
+      for sbox in self.dependents:
+        sbox.cleanup_test_paths()
+    # cleanup all test specific working copies and repositories
+    for path in self.test_paths:
+      if not path is pristine_dir:
+        _cleanup_test_path(path)
+
+
+_deferred_test_paths = []
+
+def cleanup_deferred_test_paths():
+  global _deferred_test_paths
+  test_paths = _deferred_test_paths
+  _deferred_test_paths = []
+  for path in test_paths:
+    _cleanup_test_path(path, True)
+
+
+def _cleanup_test_path(path, retrying=False):
+  if svntest.main.verbose_mode:
+    if retrying:
+      print("CLEANUP: RETRY: %s" % path)
+    else:
+      print("CLEANUP: %s" % path)
+  try:
+    svntest.main.safe_rmtree(path)
+  except:
+    if svntest.main.verbose_mode:
+      print("WARNING: cleanup failed, will try again later")
+    _deferred_test_paths.append(path)
