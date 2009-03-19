f9a3abb1609cbb7c77d699648c601b0c5465a5ea
Greg Stein <gstein@apache.org>
2009-03-19 14:05:42 +0000

    Try to minimize/centralize the number of places where we modify ENTRIES hash tables, and where we (manually) write them out to disk. This revision concentrates on the svn_wc__entry_remove() function.
    Every caller grabbed PARENT_DIR from an access baton, so we just pass that
    in directly (which we need anyways). Add a WRITE_TO_DISK parameter so this
    function can write the modification out to disk. Also, make the ENTRIES
    parameter optional -- we'll fetch the entries, do the removal, and then
    write the result to disk for this case.
    
    All callers updated.
    
    * subversion/libsvn_wc/entries.h:
      (svn_wc__entry_remove): adjust parameters and docstring
    
    * subversion/libsvn_wc/entries.c:
      (svn_wc__entry_remove): adjust parameters. if ENTRIES is not provided,
        then read them. temporarily remove all the DB code since the entries
        will get written at some point, and the names should be removed *then*
        rather than removed immediately. and lastly, if WRITE_TO_DISK is true,
        then write out the (modified) entries.
    
    * subversion/libsvn_wc/crop.c:
      (crop_children): adjust call to entry_remove, and write to disk.
    
    * subversion/libsvn_wc/adm_ops.c:
      (tweak_entries): adjust call to entry_remove, but don't write to disk
        since we'll do a batch-write at the end.
      (svn_wc_delete3): adjust call to entry_remove and write to disk.
      (revert_entry): adjust call, and write to disk
      (svn_wc_remove_from_revision_control): adjust call to entry_remove,
        taking advantage of not needing the entries at all. a later loop does,
        though, so move the declaration. adjust the other calls to
        entry_remove, with the appropriate WRITE_TO_DISK values.
    
    * subversion/libsvn_wc/update_editor.c:
      (complete_directory): adjust calls to entry_remove() and hold off on
        writing, in favor of a batch-write at the end.
      (do_entry_deletion): replace entries read/remove/write with a new call
        to entry_remove()
    
    * subversion/libsvn_wc/log.c:
      (log_do_delete_entry): replace a read/remove/write with a single
        call to entry_remove()
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@876752 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/adm_ops.c
+++ subversion/libsvn_wc/adm_ops.c
@@ -180,9 +180,10 @@ tweak_entries(svn_wc_adm_access_t *dirpath,
                   if (current_entry->schedule != svn_wc_schedule_add
                       && !excluded)
                     {
-                      SVN_ERR(svn_wc__entry_remove(
-                                     entries, svn_wc_adm_access_path(dirpath),
-                                     name, subpool));
+                      /* WRITE_TO_DISK is FALSE because we're batching
+                         these up. We'll write out the entries later.  */
+                      SVN_ERR(svn_wc__entry_remove(entries, dirpath, name,
+                                                   FALSE, subpool));
                       if (notify_func)
                         {
                           notify = svn_wc_create_notify(child_path,
@@ -1215,10 +1216,8 @@ svn_wc_delete3(const char *path,
                  with!  this means we're dealing with a missing item
                  that's scheduled for addition.  Easiest to just
                  remove the entry.  */
-              SVN_ERR(svn_wc__entry_remove(
-                            entries, svn_wc_adm_access_path(parent_access),
-                            base_name, pool));
-              SVN_ERR(svn_wc__entries_write(entries, parent_access, pool));
+              SVN_ERR(svn_wc__entry_remove(entries, parent_access, base_name,
+                                           TRUE, pool));
             }
         }
       else
@@ -2063,10 +2062,8 @@ revert_entry(svn_depth_t *depth,
                  adm_access, for which we definitely can't use the 'else'
                  code path (as it will remove the parent from version
                  control... (See issue 2425) */
-              SVN_ERR(svn_wc__entry_remove(
-                            entries, svn_wc_adm_access_path(parent_access),
-                            basey, pool));
-              SVN_ERR(svn_wc__entries_write(entries, parent_access, pool));
+              SVN_ERR(svn_wc__entry_remove(entries, parent_access, basey,
+                                           TRUE, pool));
             }
           else
             {
@@ -2414,7 +2411,6 @@ svn_wc_remove_from_revision_control(svn_wc_adm_access_t *adm_access,
   svn_error_t *err;
   svn_boolean_t is_file;
   svn_boolean_t left_something = FALSE;
-  apr_hash_t *entries = NULL;
   const char *full_path = apr_pstrdup(pool,
                                       svn_wc_adm_access_path(adm_access));
 
@@ -2458,10 +2454,7 @@ svn_wc_remove_from_revision_control(svn_wc_adm_access_t *adm_access,
                                    adm_access, pool));
 
       /* Remove NAME from PATH's entries file: */
-      SVN_ERR(svn_wc_entries_read(&entries, adm_access, TRUE, pool));
-      SVN_ERR(svn_wc__entry_remove(entries, svn_wc_adm_access_path(adm_access),
-                                   name, pool));
-      SVN_ERR(svn_wc__entries_write(entries, adm_access, pool));
+      SVN_ERR(svn_wc__entry_remove(NULL, adm_access, name, TRUE, pool));
 
       /* Remove text-base/NAME.svn-base */
       SVN_ERR(remove_file_if_present(svn_wc__text_base_path(full_path, FALSE,
@@ -2485,6 +2478,7 @@ svn_wc_remove_from_revision_control(svn_wc_adm_access_t *adm_access,
       apr_pool_t *subpool = svn_pool_create(pool);
       apr_hash_index_t *hi;
       svn_wc_entry_t incomplete_entry;
+      apr_hash_t *entries;
 
       /* ### sanity check:  check 2 places for DELETED flag? */
 
@@ -2559,10 +2553,17 @@ svn_wc_remove_from_revision_control(svn_wc_adm_access_t *adm_access,
                 {
                   /* The directory is either missing or excluded,
                      so don't try to recurse, just delete the
-                     entry in the parent directory. */
-                  SVN_ERR(svn_wc__entry_remove(
-                                entries, svn_wc_adm_access_path(adm_access),
-                                current_entry_name, subpool));
+                     entry in the parent directory.
+
+                     WRITE_TO_DISK is FALSE because we're just going to
+                     blow away the administrative data anyways.
+
+                     ### tho maybe we should write it out in case our
+                     ### removal is interrupted? get the partial state
+                     ### written to disk.  */
+                  SVN_ERR(svn_wc__entry_remove(entries, adm_access,
+                                               current_entry_name, FALSE,
+                                               subpool));
                 }
               else
                 {
@@ -2625,13 +2626,8 @@ svn_wc_remove_from_revision_control(svn_wc_adm_access_t *adm_access,
             dir_entry = apr_hash_get(parent_entries, base_name,
                                      APR_HASH_KEY_STRING);
             if (dir_entry->depth != svn_depth_exclude)
-              {
-                SVN_ERR(svn_wc__entry_remove(
-                        parent_entries, svn_wc_adm_access_path(parent_access),
-                        base_name, pool));
-                SVN_ERR(svn_wc__entries_write(parent_entries, parent_access, pool));
-
-              }
+              SVN_ERR(svn_wc__entry_remove(parent_entries, parent_access,
+                                           base_name, TRUE, pool));
           }
       }
 
