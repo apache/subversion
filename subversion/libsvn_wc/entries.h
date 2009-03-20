8fb9e48b821fd3ee60c6c7487d8fe2c34375a58a
Hyrum Kurt Wright <hwright@apache.org>
2009-03-20 05:31:25 +0000

    Hacky solution to a temporary problem: file externals storage in wc_db.
    The problem is that in order to populate the file externals fields of
    svn_wc_entry_t, we need to look at some properties.  However, properties
    have not yet been moved to the New World, and so retrieving them requires
    calls into the properties APIs in props.c.  *Those* APIs take paths, and fetch
    the appropriate entry before getting the properties, which causes a minor
    explosion of the mutually recursive kind.
    
    So, until such time as the properties are easily retrievable, we add yet
    another column to BASE_NODE, and do a backdoor handshake to get data in
    and out of it.
    
    * subversion/tests/libsvn_wc/db-test.c
      (data_loading_sql): Add the extra column.
    
    * subversion/libsvn_wc/old-and-busted.c
      (unserialize_file_externals, serialize_file_externals):  Rename from this...
      (svn_wc__unserialize_file_externals, svn_wc__serialize_file_externals):
        ...to this.
      (read_entry, svn_wc__write_entry_old): Update callers.
     
    * subversion/libsvn_wc/wc-metadata.sql
      (BASE_NODE): Add file_external column.
    
    * subversion/libsvn_wc/entries.c
      (statement_keys, statements): New statements.
      (read_entries): Go fetch the file external information and shove it in
        the entry.
      (write_entry): Possibly write the file external information.
    
    * subversion/libsvn_wc/entries.h
      (svn_wc__unserialize_file_externals, svn_wc__serialize_file_externals):
        Publicize.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@876768 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/entries.h
+++ subversion/libsvn_wc/entries.h
@@ -279,6 +279,35 @@ svn_error_t *
 svn_wc__resolve_to_defaults(apr_hash_t *entries,
                             apr_pool_t *pool);
 
+/* Parse a file external specification in the NULL terminated STR and
+   place the path in PATH_RESULT, the peg revision in PEG_REV_RESULT
+   and revision number in REV_RESULT.  STR may be NULL, in which case
+   PATH_RESULT will be set to NULL and both PEG_REV_RESULT and
+   REV_RESULT set to svn_opt_revision_unspecified.
+
+   The format that is read is the same as a working-copy path with a
+   peg revision; see svn_opt_parse_path(). */
+svn_error_t *
+svn_wc__unserialize_file_external(const char **path_result,
+                                  svn_opt_revision_t *peg_rev_result,
+                                  svn_opt_revision_t *rev_result,
+                                  const char *str,
+                                  apr_pool_t *pool);
+
+/* Serialize into STR the file external path, peg revision number and
+   the operative revision number into a format that
+   unserialize_file_external() can parse.  The format is
+     %{peg_rev}:%{rev}:%{path}
+   where a rev will either be HEAD or the string revision number.  If
+   PATH is NULL then STR will be set to NULL.  This method writes to a
+   string instead of a svn_stringbuf_t so that the string can be
+   protected by write_str(). */
+svn_error_t *
+svn_wc__serialize_file_external(const char **str,
+                                const char *path,
+                                const svn_opt_revision_t *peg_rev,
+                                const svn_opt_revision_t *rev,
+                                apr_pool_t *pool);
 
 #ifdef __cplusplus
 }
