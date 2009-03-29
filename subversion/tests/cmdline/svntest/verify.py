61fe5f6a5d5bc99435c545f8f52117e1e8456d52
arfrever <dev-null@apache.org>
2009-03-29 05:16:12 +0000

    Python 3 compatibility: Don't use __cmp__().
    * subversion/tests/cmdline/svntest/tree.py
      (SVNTreeNode.__cmp__): Delete.
      (SVNTreeNode.__eq__, SVNTreeNode.__ne__): New.
    
    * subversion/tests/cmdline/svntest/verify.py
      (ExpectedOutput.__cmp__): Delete.
      (ExpectedOutput.__eq__, ExpectedOutput.__ne__): New.
      (UnorderedOutput.__cmp__): Delete.
    
    * tools/dev/contribulyze.py
      (Contributor, LogMessage): Convert to new-style classes.
      (Contributor.__cmp__, Contributor.__hash__): Delete.
      (Contributor.__eq__, Contributor.__ne__, Contributor.__lt__,
       Contributor.__gt__): New.
      (LogMessage.__cmp__, LogMessage.__hash__): Delete.
      (LogMessage.__eq__, LogMessage.__ne__, LogMessage.__lt__,
       LogMessage.__gt__): New.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@876907 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/svntest/verify.py
+++ subversion/tests/cmdline/svntest/verify.py
@@ -93,7 +93,7 @@ class ExpectedOutput:
   def __str__(self):
     return str(self.output)
 
-  def __cmp__(self, other):
+  def __eq__(self, other):
     """Return whether SELF.output matches OTHER (which may be a list
     of newline-terminated lines, or a single string).  Either value
     may be None."""
@@ -116,9 +116,12 @@ class ExpectedOutput:
       is_match = False
 
     if is_match:
-      return 0
+      return True
     else:
-      return 1
+      return False
+
+  def __ne__(self, other):
+    return not self.__eq__(other)
 
   def is_equivalent_list(self, expected, actual):
     "Return whether EXPECTED and ACTUAL are equivalent."
@@ -216,14 +219,6 @@ class RegexOutput(ExpectedOutput):
 
 class UnorderedOutput(ExpectedOutput):
   """Marks unordered output, and performs comparisions."""
-
-  def __cmp__(self, other):
-    "Handle ValueError."
-    try:
-      return ExpectedOutput.__cmp__(self, other)
-    except ValueError:
-      return 1
-
   def is_equivalent_list(self, expected, actual):
     "Disregard the order of ACTUAL lines during comparison."
     if self.match_all:
