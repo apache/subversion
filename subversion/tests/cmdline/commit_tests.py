6241f6f175b01e45f5603431b94bb53fa0e6e1ed
Arfrever Frehtes Taifersar Arahesis <arfrever@apache.org>
2009-04-04 19:09:18 +0000

    * subversion/tests/cmdline/commit_tests.py   (from_wc_top_with_bad_editor): Use list comprehension instead of map().
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@877073 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/commit_tests.py
+++ subversion/tests/cmdline/commit_tests.py
@@ -6,7 +6,7 @@
 #  See http://subversion.tigris.org for more information.
 #
 # ====================================================================
-# Copyright (c) 2000-2008 CollabNet.  All rights reserved.
+# Copyright (c) 2000-2009 CollabNet.  All rights reserved.
 #
 # This software is licensed as described in the file COPYING, which
 # you should have received as part of this distribution.  The terms
@@ -1814,7 +1814,7 @@ def from_wc_top_with_bad_editor(sbox):
     None, svntest.verify.AnyOutput,
     'ci', '--editor-cmd', 'no_such-editor')
 
-  err = " ".join(map(str.strip, err))
+  err = " ".join([x.strip() for x in err])
   if not (re.match(".*no_such-editor.*", err)
           and re.match(".*Commit failed.*", err)):
     print("Commit failed, but not in the way expected.")
