79a96118e898fbeaea85b925ca87e3a9ed774128
Greg Stein <gstein@apache.org>
2009-04-10 01:38:29 +0000

    Shift some version checking around a bit. Rather than always looking at the directory in svn_wc_check_wc(), we construct a DB and ask *it* for the version information. In the future, svn_wc_check_wc() should arrange to get a DB from somewhere else to use since created one on its own will not cache anything.
    * subversion/libsvn_wc/wc_db.h:
      (svn_wc__db_version): removed
    
    * subversion/libsvn_wc/wc_db.c:
      (svn_wc_-db_version): renamed to ...
      (db_version): ... this.
      (svn_wc__db_temp_get_format): track rename
    
    * subversion/libsvn_wc/questions.c:
      (svn_wc_check_wc): construct a DB and ask it for the format number.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@877224 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/wc_db.h
+++ subversion/libsvn_wc/wc_db.h
@@ -256,18 +256,6 @@ svn_wc__db_open(svn_wc__db_t **db,
                 apr_pool_t *scratch_pool);
 
 
-/* This function answers a simple question: what format version of the wc
-   exists at PATH.  The reason it takes a PATH instead of an existing db
-   handle is because it may need to use legacy, pre-wc-ng methods to determine
-   what that version is, and such versions don't have any db to open. 
-   
-   If no working copy exists at PATH, return SVN_ERR_WC_MISSING. */
-svn_error_t *
-svn_wc__db_version(int *version,
-                   const char *path,
-                   apr_pool_t *scratch_pool);
-
-
 /**
  * Close DB.
  *
