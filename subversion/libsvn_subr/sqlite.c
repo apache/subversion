4c0d18b091281c1c1068162a1ed0ac455eee50d9
arfrever <dev-null@apache.org>
2009-04-12 15:14:29 +0000

    Fix Issue #3387: SQLite configuration fails since client application has already configured SQLite.
    * subversion/libsvn_subr/sqlite.c
      (init_sqlite): Ignore SQLITE_MISUSE return value of sqlite3_config().
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@877259 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/sqlite.c
+++ subversion/libsvn_subr/sqlite.c
@@ -597,8 +597,12 @@ init_sqlite(apr_pool_t *pool)
                               "thread-safe mode"));
 #endif
 #if SQLITE_VERSION_AT_LEAST(3,6,0)
-  SQLITE_ERR_MSG(sqlite3_config(SQLITE_CONFIG_MULTITHREAD),
-                 "Could not configure SQLite");
+  /* If SQLite has been already initialized, sqlite3_config() returns
+     SQLITE_MISUSE. */
+  int err = sqlite3_config(SQLITE_CONFIG_MULTITHREAD);
+  if (err != SQLITE_OK && err != SQLITE_MISUSE)
+    return svn_error_create(SQLITE_ERROR_CODE(err), NULL,
+                            "Could not configure SQLite");
   SQLITE_ERR_MSG(sqlite3_initialize(), "Could not initialize SQLite");
 #endif
 
