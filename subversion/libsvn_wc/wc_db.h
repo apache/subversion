3a9dd1a691574ac3d9ee3bd412619c3f2cab19af
Hyrum Kurt Wright <hwright@apache.org>
2009-04-14 17:46:15 +0000

    Variations on a r37260 theme: update references to the dav cache column and function names on wc_db.
    * subversion/libsvn_wc/wc_db.c
      (statement_keys, statements): Use the new column name.
      (svn_wc__db_base_set_wcprops): Rename from this...
      (svn_wc__db_base_set_dav_cache): ...to this.
      (svn_wc__db_base_get_wcprops): Rename from this...
      (svn_wc__db_base_get_dav_cache): ...to this.
    
    * subversion/libsvn_wc/wc_db.h
      (svn_wc__db_base_set_wcprops): Rename from this...
      (svn_wc__db_base_set_dav_cache): ...to this.
      (svn_wc__db_base_get_wcprops): Rename from this...
      (svn_wc__db_base_get_dav_cache): ...to this.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@877335 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/wc_db.h
+++ subversion/libsvn_wc/wc_db.h
@@ -607,23 +607,23 @@ svn_wc__db_base_get_children(const apr_array_header_t **children,
                              apr_pool_t *scratch_pool);
 
 
-/** Set the wcprops for LOCAL_ABSPATH to PROPS.  Use SCRATCH_POOL for
+/** Set the dav cache for LOCAL_ABSPATH to PROPS.  Use SCRATCH_POOL for
     temporary allocations. */
 svn_error_t *
-svn_wc__db_base_set_wcprops(svn_wc__db_t *db,
-                            const char *local_abspath,
-                            const apr_hash_t *props,
-                            apr_pool_t *scratch_pool);
+svn_wc__db_base_set_dav_cache(svn_wc__db_t *db,
+                              const char *local_abspath,
+                              const apr_hash_t *props,
+                              apr_pool_t *scratch_pool);
 
 
-/** Retrieve the wcprops for LOCAL_ABSPATH into *PROPS, allocated in
+/** Retrieve the dav cache for LOCAL_ABSPATH into *PROPS, allocated in
     RESULT_POOL.  Use SCRATCH_POOL for temporary allocations. */
 svn_error_t *
-svn_wc__db_base_get_wcprops(apr_hash_t **props,
-                            svn_wc__db_t *db,
-                            const char *local_abspath,
-                            apr_pool_t *result_pool,
-                            apr_pool_t *scratch_pool);
+svn_wc__db_base_get_dav_cache(apr_hash_t **props,
+                              svn_wc__db_t *db,
+                              const char *local_abspath,
+                              apr_pool_t *result_pool,
+                              apr_pool_t *scratch_pool);
 
 
 /* ### how to handle depth? empty != absent. thus, record depth on each
