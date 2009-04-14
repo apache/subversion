1e7e9bae81dd38df0f41cd274146a5746c29ea08
Hyrum Kurt Wright <hwright@apache.org>
2009-04-14 17:31:21 +0000

    * subversion/libsvn_wc/wc-metadata.sql   (BASE_NODE): Rename the wc_props column to dav_cache, since that's what it     is, and to remove confusion.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@877334 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/wc-metadata.sql
+++ subversion/libsvn_wc/wc-metadata.sql
@@ -129,9 +129,9 @@ CREATE TABLE BASE_NODE (
      have no information about the properties (a non-present node). */
   properties  BLOB,
 
-  /* serialized skel of this node's wcprops.  could be NULL if the
-     node does not have any wcprops. */
-  wc_props  BLOB,
+  /* serialized skel of this node's dav-cache.  could be NULL if the
+     node does not have any dav-cache. */
+  dav_cache  BLOB,
 
   /* this node is a directory, and all of its child nodes have not (yet)
      been created [for this revision number]. Note: boolean value. */
