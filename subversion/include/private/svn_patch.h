0f72a8fed96a34eeab96115c8ab594132cd5a860
Stefan Sperling <stsp@apache.org>
2009-04-14 16:08:00 +0000

    Move diff parsing functions from libsvn_subr to libsvn_diff.
    * subversion/libsvn_subr/patch.c: Remove an #include made unnecessary
      by moving ...
      (svn_patch__get_next_patch, svn_patch__get_next_hunk, parse_offset,
       parse_range, parse_hunk_header): ... these functions into ...
    
    * subversion/libsvn_diff/parse-diff.c: ... this new file, adding
      necessary #include directives to it, and ...
      (svn_diff__parse_next_patch, svn_diff__parse_next_hunk): ... renaming
       two of the moved functions to this.
    
    * subversion/include/private/svn_patch.h
      (svn_patch_t, svn_hunk_t, svn_patch__get_next_patch,
       svn_patch__get_next_hunk): Move these declarations into ...
    
    * subversion/include/private/svn_diff_private.h: ... this new file, and ...
      (svn_diff__parse_next_patch, svn_diff__parse_next_hunk): ... rename
       two of the moved declarations to this.
    
    Suggested by: gstein
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@877323 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/private/svn_patch.h
+++ subversion/include/private/svn_patch.h
@@ -29,60 +29,6 @@
 extern "C" {
 #endif /* __cplusplus */
 
-/* A single hunk inside a patch */
-typedef struct svn_hunk_t {
-  /* The hunk's text as it appeared in the patch file,
-   * without range information. */
-  const svn_string_t *diff_text;
-
-  /* The original and modified texts in the hunk range.
-   * Derived from the diff text. */
-  const svn_string_t *original_text;
-  const svn_string_t *modified_text;
-
-  /* Hunk ranges as they appeared in the patch file. */
-  svn_filesize_t original_start;
-  svn_filesize_t original_length;
-  svn_filesize_t modified_start;
-  svn_filesize_t modified_length;
-} svn_hunk_t;
-
-/* Data type to manage parsing of patches. */
-/* TODO: Should be made opaque when done with testing. */
-typedef struct svn_patch_t {
-  /* The patch file itself. */
-  apr_file_t *patch_file;
-
-  /* The old and new file names as retreived from the patch file. */
-  const char *old_filename;
-  const char *new_filename;
-
-  /* EOL string used in patch file. */
-  const char *eol_str;
-} svn_patch_t;
-
-/* Return the next *PATCH in PATCH_FILE. The patch file is assumed to
- * have consistent EOL-markers as specified in EOL_STR.
- * If no patch can be found, set *PATCH to NULL.
- * Allocate results in RESULT_POOL.
- * Use SCRATCH_POOL for all other allocations. */
-svn_error_t *
-svn_patch__get_next_patch(svn_patch_t **patch,
-                          apr_file_t *patch_file,
-                          const char *eol_str,
-                          apr_pool_t *result_pool,
-                          apr_pool_t *scratch_pool);
-
-/* Return the next *HUNK from a PATCH.
- * If no hunk can be found, set *HUNK to NULL.
- * Allocate results in RESULT_POOL.
- * Use SCRATCH_POOL for all other allocations. */
-svn_error_t *
-svn_patch__get_next_hunk(svn_hunk_t **hunk,
-                         svn_patch_t *patch,
-                         apr_pool_t *result_pool,
-                         apr_pool_t *scratch_pool);
-
 /* Output -- Writing */
 
 /* Append a command into @a target in a printf-like fashion.
