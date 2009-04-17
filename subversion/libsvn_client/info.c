1c24b372ef8ad9a2255d192f034c7df5b0bd3d97
Hyrum Kurt Wright <hwright@apache.org>
2009-04-17 15:43:25 +0000

    A better r37318, this time with correctness!
    * subversion/libsvn_client/info.c
      (same_resource_in_head): Some error handing cleanup.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@877404 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/info.c
+++ subversion/libsvn_client/info.c
@@ -405,18 +405,14 @@ same_resource_in_head(svn_boolean_t *same_p,
     {
       svn_error_clear(err);
       *same_p = FALSE;
+      return SVN_NO_ERROR;
     }
-  else if (err)
-    return err;
-  else
-    {
-      /* ### Currently, the URLs should always be equal, since we can't
-         ### walk forwards in history. */
-      if (strcmp(url, head_url) == 0)
-        *same_p = TRUE;
-      else
-        *same_p = FALSE;
-    }
+  else 
+    SVN_ERR(err);
+
+  /* ### Currently, the URLs should always be equal, since we can't
+     ### walk forwards in history. */
+  *same_p = (strcmp(url, head_url) == 0);
 
   return SVN_NO_ERROR;
 }
