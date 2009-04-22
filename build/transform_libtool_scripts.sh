88a1b3ea9f052b98d0109ff69f88c459c8ab3276
arfrever <dev-null@apache.org>
2009-04-22 12:56:17 +0000

    * build/transform_libtool_scripts.sh: Add some comments. Fix dependencies    of svnsync.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@877498 13f79535-47bb-0310-9956-ffa450edef68

--- build/transform_libtool_scripts.sh
+++ build/transform_libtool_scripts.sh
@@ -23,10 +23,12 @@ ra="ra $delta $ra_local $ra_neon $ra_serf $ra_svn $subr"
 wc="wc $delta $diff $subr"
 client="client $delta $diff $ra $subr $wc"
 
+# Variable 'libraries' containing names of variables corresponding to libraries
 libraries="auth_gnome_keyring auth_kwallet client delta diff fs fs_base fs_fs fs_util ra ra_local ra_neon ra_serf ra_svn repos subr wc"
 
 for library in $libraries; do
-  library_dependencies="$(echo -n $(for x in $(eval echo "\${${library}}"); do echo $x ; done | sort -u))"
+  # Delete duplicates in dependencies of libraries
+  library_dependencies="$(echo -n $(for x in $(eval echo "\$$library"); do echo $x; done | sort -u))"
   eval "$library=\$library_dependencies"
 done
 
@@ -36,53 +38,64 @@ svnadmin="$delta $fs $repos $subr"
 svndumpfilter="$delta $fs $repos $subr"
 svnlook="$delta $diff $fs $repos $subr"
 svnserve="$delta $fs $ra_svn $repos $subr"
-svnsync="$delta $ra $subr"
+svnsync="$auth_gnome_keyring $auth_kwallet $delta $ra $subr"
 entries_dump="$subr $wc"
 
+# Variable 'executables' containing names of variables corresponding to executables
 executables="svn svnadmin svndumpfilter svnlook svnserve svnsync entries_dump"
 
 for executable in $executables; do
+  # Set variables containing paths of executables
   if [ "$executable" != entries_dump ]; then
     eval "${executable}_path=subversion/$executable/$executable"
   else
     eval "${executable}_path=subversion/tests/cmdline/entries-dump"
   fi
-  executable_dependencies="$(echo -n $(for x in $(eval echo "\${${executable}}"); do echo $x ; done | sort -u))"
+  # Delete duplicates in dependencies of executables
+  executable_dependencies="$(echo -n $(for x in $(eval echo "\$$executable"); do echo $x; done | sort -u))"
   eval "$executable=\$executable_dependencies"
 done
 
 test_paths="$(find subversion/tests -name '*-test' ! -path '*/.libs/*')"
 for test in $test_paths; do
   test_path="$test"
+  # Dependencies of tests are based on names of directories containing tests
   test_library="$(echo $test | sed -e 's:^subversion/tests/libsvn_\([^/]*\)/.*:\1:')"
+  test_dependencies="$(eval echo "\$$test_library")"
+  # Set variables corresponding to tests and containing dependencies of tests
   test="$(echo $test | sed -e 's:^subversion/tests/libsvn_[^/]*/\(.*\):\1:' -e 's/-/_/g')"
-  test_dependencies="$(eval echo "\${${test_library}}")"
   eval "$test=\$test_dependencies"
+  # Set variables containing paths of tests
   eval "${test}_path=\$test_path"
+  # Set variable 'tests' containing names of variables corresponding to tests
   tests="$tests $test"
 done
 
+# auth-test dynamically loads libsvn_auth_gnome_keyring and libsvn_auth_kwallet libraries
 auth_test="auth_gnome_keyring auth_kwallet $auth_test"
 
+# Usage: sed_append LINE_NUMBER TEXT FILE
 sed_append()
 {
-  sed -e "$1a$2" "$libtool_script_path" > "$libtool_script_path.new"
-  mv -f "$libtool_script_path.new" "$libtool_script_path"
+  sed -e "$1a$2" "$3" > "$3.new"
+  mv -f "$3.new" "$3"
 }
 
+current_directory="$(pwd)"
 for libtool_script in $executables $tests; do
-  eval "libtool_script_path=\${${libtool_script}_path}"
+  eval "libtool_script_path=\$${libtool_script}_path"
   if [ -f "$libtool_script_path" ]; then
-    if grep LD_LIBRARY_PATH "$libtool_script_path" > /dev/null && ! grep LD_PRELOAD "$libtool_script_path" > /dev/null; then
+    if { grep LD_LIBRARY_PATH "$libtool_script_path" && ! grep LD_PRELOAD "$libtool_script_path"; } > /dev/null; then
       echo "Transforming $libtool_script_path"
-      libtool_script_dependencies="$(eval echo "\${${libtool_script}}")"
+      libtool_script_dependencies="$(eval echo "\$$libtool_script")"
       for libtool_script_dependency in $libtool_script_dependencies; do
-        libtool_script_library="$(pwd)/subversion/libsvn_$libtool_script_dependency/.libs/libsvn_$libtool_script_dependency-1.so"
+        libtool_script_library="$current_directory/subversion/libsvn_$libtool_script_dependency/.libs/libsvn_$libtool_script_dependency-1.so"
         [ -f "$libtool_script_library" ] && libtool_script_libraries="$libtool_script_libraries $libtool_script_library"
       done
       libtool_script_libraries="${libtool_script_libraries# *}"
-      sed_append 1 "LD_PRELOAD=\"$libtool_script_libraries\""
-      sed_append 2 "export LD_PRELOAD"
+      # Append definitions of LD_PRELOAD to libtool scripts
+      sed_append 1 "LD_PRELOAD=\"$libtool_script_libraries\"" "$libtool_script_path"
+      sed_append 2 "export LD_PRELOAD" "$libtool_script_path"
       chmod +x "$libtool_script_path"
     fi
   fi
