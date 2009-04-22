3d30a73ce585abffde8e098ade7a8c9195ef225f
Bert Huiben <rhuijben@apache.org>
2009-04-22 13:39:50 +0000

    * tests/cmdline/upgrade_tests.py   (replace_sbox_with_tarfile): Following up on r37425, fix the intended     rmtree to work on Windows instead of adding another rmtree.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@877500 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/upgrade_tests.py
+++ subversion/tests/cmdline/upgrade_tests.py
@@ -30,7 +30,7 @@ XFail = svntest.testcase.XFail
 
 def replace_sbox_with_tarfile(sbox, tar_filename):
   try:
-    shutil.rmtree(sbox.wc_dir)
+    svntest.main.safe_rmtree(sbox.wc_dir)
   except OSError, e:
     pass
 
@@ -40,10 +40,6 @@ def replace_sbox_with_tarfile(sbox, tar_filename):
   for member in t.getmembers():
     t.extract(member, svntest.main.temp_dir)
 
-  # Remove the directory (and the read only files inside) before moving,
-  # to support the Windows semantics of shutil.move
-  svntest.main.safe_rmtree(sbox.wc_dir)
-  
   shutil.move(os.path.join(svntest.main.temp_dir, tar_filename.split('.')[0]),
               sbox.wc_dir)
 
