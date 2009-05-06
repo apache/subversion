5573c9a3b875b4a2627d508382bbf7649b960522
Greg Stein <gstein@apache.org>
2009-05-06 13:58:32 +0000

    Use EZT to create build-outputs-e.mk. This is simply an *incremental* creation, making it easier to see how this was done and to verify that it creates *exactly* the same output. A future revision will remove the old generation code and just use EZT.
    * build/generator/gen_make.py:
      (): import StringIO. needed for some generation delegation. import ezt
      (Generator.write): put all of the info into a 'data' object, and then
        write out a build-outputs-e.mk at the end, based on a template fetched
        form the new templates/ directory
    
    * build/generator/templates/: new directory to hold templates
    
    * build/generator/templates/makefile.ezt: new directory for makefile-based
        generation. e.g. build-outputs.mk
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@877679 13f79535-47bb-0310-9956-ffa450edef68

--- build/generator/gen_make.py
+++ build/generator/gen_make.py
@@ -11,6 +11,18 @@ except ImportError:
   # Python <3.0
   import ConfigParser as configparser
 
+if sys.version_info[0] >= 3:
+  # Python >=3.0
+  from io import StringIO
+else:
+  # Python <3.0
+  try:
+    from cStringIO import StringIO
+  except ImportError:
+    from StringIO import StringIO
+
+import ezt
+
 import gen_base
 import generator.swig.header_wrappers
 import generator.swig.checkout_swig_header
@@ -64,6 +76,21 @@ class Generator(gen_base.GeneratorBase):
     install_deps.sort()
     install_sources.sort(key = lambda s: s.name)
 
+    class _eztdata(object):
+      def __init__(self, **kw):
+        vars(self).update(kw)
+
+    data = _eztdata(
+      modules=[ ],
+      swig_langs=[ ],
+      swig_c=[ ],
+      target=[ ],
+      itargets=[ ],
+      areas=[ ],
+      isources=[ ],
+      deps=[ ],
+      )
+
     ########################################
     self.begin_section('Global make variables')
 
@@ -89,6 +116,7 @@ class Generator(gen_base.GeneratorBase):
           deps.append(source.filename)
           link.append(build_path_join(retreat, source.filename))
 
+        data.modules.append(_eztdata(name=name, deps=deps, link=link))
         self.ofile.write('%s_DEPS = %s\n'
                          '%s_LINK = %s\n\n' % (name, ' '.join(deps),
                                                name, ' '.join(link)))
@@ -99,11 +127,16 @@ class Generator(gen_base.GeneratorBase):
 
     #   remove duplicate directories between targets and tests
     build_dirs = unique(self.target_dirs + script_dirs + self.swig_dirs)
+    data.build_dirs = build_dirs
 
     self.ofile.write('BUILD_DIRS = %s\n\n' % ' '.join(build_dirs))
 
     # write lists of test files
     # deps = all, progs = not including those marked "testing = skip"
+    data.bdb_test_deps = self.bdb_test_deps + self.bdb_scripts
+    data.bdb_test_progs = self.bdb_test_progs + self.bdb_scripts
+    data.test_deps = self.test_deps + self.scripts
+    data.test_progs = self.test_progs + self.scripts
     self.ofile.write('BDB_TEST_DEPS = %s\n\n' %
                      ' '.join(self.bdb_test_deps + self.bdb_scripts))
     self.ofile.write('BDB_TEST_PROGRAMS = %s\n\n' %
@@ -114,6 +147,7 @@ class Generator(gen_base.GeneratorBase):
                      ' '.join(self.test_progs + self.scripts))
 
     # write list of all manpages
+    data.manpages = self.manpages
     self.ofile.write('MANPAGES = %s\n\n' % ' '.join(self.manpages))
 
     # write a list of files to remove during "make clean"
@@ -129,6 +163,7 @@ class Generator(gen_base.GeneratorBase):
          and target.filename[-3:] != '.la':
         cfiles.append(target.filename)
     cfiles.sort()
+    data.cfiles = cfiles
     self.ofile.write('CLEAN_FILES = %s\n\n' % ' '.join(cfiles))
 
     # this is here because autogen-standalone needs it too
@@ -140,6 +175,7 @@ class Generator(gen_base.GeneratorBase):
         '  -I$(abs_builddir)/subversion/bindings/swig/proxy \\\n'
         '  $(SVN_APR_INCLUDES) $(SVN_APRUTIL_INCLUDES)\n\n')
 
+    data.release_mode = ezt.boolean(self.release_mode)
     if self.release_mode:
       self.ofile.write('RELEASE_MODE = 1\n\n')
 
@@ -147,11 +183,15 @@ class Generator(gen_base.GeneratorBase):
     self.begin_section('SWIG headers (wrappers and external runtimes)')
 
     if not self.release_mode:
+      swig_rules = StringIO()
       for swig in (generator.swig.header_wrappers,
                    generator.swig.checkout_swig_header,
                    generator.swig.external_runtime):
         gen = swig.Generator(self.conf, "swig")
-        gen.write_makefile_rules(self.ofile)
+        gen.write_makefile_rules(swig_rules)
+
+      data.swig_rules = swig_rules.getvalue()
+      self.ofile.write(swig_rules.getvalue())
 
     ########################################
     self.begin_section('SWIG autogen rules')
@@ -169,6 +209,8 @@ class Generator(gen_base.GeneratorBase):
       swig_lang_deps[lang].append(str(objname))
 
     for lang in self.swig.langs:
+      data.swig_langs.append(_eztdata(short=short[lang],
+                                      deps=swig_lang_deps[lang]))
       lang_deps = ' '.join(swig_lang_deps[lang])
       self.ofile.write(
         'autogen-swig-%s: %s\n' % (short[lang], lang_deps) +
@@ -179,13 +221,14 @@ class Generator(gen_base.GeneratorBase):
     ########################################
     self.begin_section('Rules to build SWIG .c files from .i files')
 
-    for objname, sources in swig_c_deps:
-      deps = ' '.join(map(str, sources))
-      source = str(sources[0])
-      source_dir = build_path_dirname(source)
-      opts = self.swig.opts[objname.lang]
-      if not self.release_mode:
-        self.ofile.write('%s: %s\n' % (objname, deps) +
+    if not self.release_mode:
+      for objname, sources in swig_c_deps:
+        deps = map(str, sources)
+        source = str(sources[0])
+        opts = self.swig.opts[objname.lang]
+        data.swig_c.append(_eztdata(c_file=str(objname), deps=deps, opts=opts,
+                                    source=source))
+        self.ofile.write('%s: %s\n' % (objname, ' '.join(deps)) +
           '\t$(SWIG) $(SWIG_INCLUDES) %s ' % opts +
           '-o $@ $(top_srcdir)/%s\n' % source
         )
@@ -261,17 +304,40 @@ class Generator(gen_base.GeneratorBase):
             deps.append(nonlib.filename)
 
       targ_varname = target.replace('-', '_')
-      objnames = ' '.join(build_path_strip(path, objects))
+      objnames = build_path_strip(path, objects)
+
+      ezt_target = _eztdata(name=target_ob.name,
+                            varname=targ_varname,
+                            path=path,
+                            install=None,
+                            add_deps=target_ob.add_deps,
+                            objects=objects,
+                            deps=deps,
+                            )
+      data.target.append(ezt_target)
+
+      if hasattr(target_ob, 'link_cmd'):
+        ezt_target.link_cmd = target_ob.link_cmd
+      if hasattr(target_ob, 'output_dir'):
+        ezt_target.output_dir = target_ob.output_dir
 
       # Output value of path variable
       self.ofile.write('%s_PATH = %s\n' % (targ_varname, path))
 
       # Add additional install dependencies if necessary
       if target_ob.add_install_deps:
+        ezt_target.install = target_ob.install
+        ezt_target.install_deps = target_ob.add_install_deps
         self.ofile.write('install-%s: %s\n'
           % (target_ob.install, target_ob.add_install_deps))
 
       if isinstance(target_ob, gen_base.TargetJava):
+        ezt_target.type = 'java'
+        ezt_target.headers = headers
+        ezt_target.sources = None
+        ezt_target.jar = None
+        ezt_target.classes = target_ob.classes
+
         self.ofile.write(
           '%s_HEADERS = %s\n'
           '%s_OBJECTS = %s\n'
@@ -288,6 +354,9 @@ class Generator(gen_base.GeneratorBase):
 
         # Build the headers from the header_classes with one 'javah' call
         if headers:
+          ezt_target.header_class_filenames = header_class_filenames
+          ezt_target.header_classes = header_classes
+
           self.ofile.write(
             '%s_CLASS_FILENAMES = %s\n'
             '%s_CLASSES = %s\n'
@@ -304,6 +373,8 @@ class Generator(gen_base.GeneratorBase):
 
         # Build the objects from the object_srcs with one 'javac' call
         if object_srcs:
+          ezt_target.sources = object_srcs
+
           self.ofile.write(
             '%s_SRC = %s\n'
             '$(%s_OBJECTS): $(%s_SRC)\n'
@@ -318,6 +389,10 @@ class Generator(gen_base.GeneratorBase):
         # Once the bytecodes have been compiled up, we produce the
         # JAR.
         if target_ob.jar:
+          ezt_target.jar_path = build_path_join(target_ob.classes,
+                                                target_ob.jar)
+          ezt_target.packages = target_ob.packages
+
           self.ofile.write('\n\t$(JAR) cf %s -C %s %s' %
                            (build_path_join(target_ob.classes, target_ob.jar),
                             target_ob.classes,
@@ -325,12 +400,25 @@ class Generator(gen_base.GeneratorBase):
 
         self.ofile.write('\n\n')
       elif isinstance(target_ob, gen_base.TargetI18N):
+        ezt_target.type = 'i18n'
         self.ofile.write(
           '%s_DEPS = %s %s\n'
           '%s: $(%s_DEPS)\n\n'
           % (targ_varname, target_ob.add_deps, ' '.join(objects + deps),
              target_ob.name, targ_varname))
       else:
+        ezt_target.type = 'n/a'
+        ezt_target.filename = target_ob.filename
+        ezt_target.path = path
+        if (isinstance(target_ob, gen_base.TargetLib)
+            and not target_ob.undefined_lib_symbols):
+          ezt_target.undefined_flag = '$(LT_NO_UNDEFINED)'
+        else:
+          ezt_target.undefined_flag = ''
+        ezt_target.libs = gen_base.unique(libs)
+        ezt_target.objnames = objnames
+        ezt_target.basename = build_path_basename(target_ob.filename)
+
         self.ofile.write(
           '%s_DEPS = %s %s\n'
           '%s_OBJECTS = %s\n'
@@ -338,7 +426,7 @@ class Generator(gen_base.GeneratorBase):
           '\tcd %s && %s -o %s %s $(%s_OBJECTS) %s $(LIBS)\n\n'
           % (targ_varname, target_ob.add_deps, ' '.join(objects + deps),
 
-             targ_varname, objnames,
+             targ_varname, ' '.join(objnames),
 
              target_ob.filename, targ_varname,
 
@@ -363,6 +451,7 @@ class Generator(gen_base.GeneratorBase):
       for t in i_targets:
         if hasattr(t, 'filename'):
           outputs.append(t.filename)
+      data.itargets.append(_eztdata(type=itype, outputs=outputs))
       self.ofile.write('%s: %s\n\n' % (itype, ' '.join(outputs)))
 
     ########################################
@@ -379,13 +468,19 @@ class Generator(gen_base.GeneratorBase):
       # get the output files for these targets, sorted in dependency order
       files = gen_base._sorted_files(self.graph, area)
 
+      ezt_area = _eztdata(type=area, files=[ ], extra_install=None)
+
       if area == 'apache-mod':
+        data.areas.append(ezt_area)
+
         self.ofile.write('install-mods-shared: %s\n' % (' '.join(files)))
         for file in files:
           # cd to dirname before install to work around libtool 1.4.2 bug.
           dirname, fname = build_path_splitfile(file)
           base, ext = os.path.splitext(fname)
           name = base.replace('mod_', '')
+          ezt_area.files.append(_eztdata(fullname=file, dirname=dirname,
+                                         name=name, filename=fname))
           self.ofile.write('\tcd %s ; '
                            '$(MKDIR) "$(APACHE_LIBEXECDIR)" ; '
                            '$(INSTALL_MOD_SHARED) -n %s %s\n'
@@ -393,17 +488,26 @@ class Generator(gen_base.GeneratorBase):
         self.ofile.write('\n')
 
       elif area != 'test' and area != 'bdb-test':
+        data.areas.append(ezt_area)
+
         area_var = area.replace('-', '_')
         upper_var = area_var.upper()
+        ezt_area.varname = area_var
+        ezt_area.uppervar = upper_var
+
         self.ofile.write('install-%s: %s\n'
                          '\t$(MKDIR) $(DESTDIR)$(%sdir)\n'
                          % (area, ' '.join(files), area_var))
         for file in files:
           # cd to dirname before install to work around libtool 1.4.2 bug.
           dirname, fname = build_path_splitfile(file)
+          ezt_file = _eztdata(dirname=dirname, fullname=file,
+                              filename=fname)
           if area == 'locale':
             lang, objext = os.path.splitext(fname)
             installdir = '$(DESTDIR)$(%sdir)/%s/LC_MESSAGES' % (area_var, lang)
+            ezt_file.installdir = installdir
+            ezt_file.objext = objext
             self.ofile.write('\t$(MKDIR) %s\n'
                              '\tcd %s ; $(INSTALL_%s) %s '
                              '%s/$(PACKAGE_NAME)%s\n'
@@ -411,14 +515,19 @@ class Generator(gen_base.GeneratorBase):
                                 dirname, upper_var, fname,
                                 installdir, objext))
           else:
+            ezt_file.install_fname = build_path_join('$(%sdir)' % area_var, fname)
             self.ofile.write('\tcd %s ; $(INSTALL_%s) %s $(DESTDIR)%s\n'
                              % (dirname, upper_var, fname,
                                 build_path_join('$(%sdir)' % area_var, fname)))
+
+          ezt_area.files.append(ezt_file)
+
         # certain areas require hooks for extra install rules defined
         # in Makefile.in
         ### we should turn AREA into an object, then test it instead of this
         if area[:5] == 'swig-' and area[-4:] != '-lib' or \
            area[:7] == 'javahl-':
+          ezt_area.extra_install = 'yes'
           self.ofile.write('\t$(INSTALL_EXTRA_%s)\n' % upper_var)
         self.ofile.write('\n')
 
@@ -427,6 +536,12 @@ class Generator(gen_base.GeneratorBase):
 
     includedir = build_path_join('$(includedir)',
                                  'subversion-%s' % self.version)
+    data.includes = [_eztdata(file=file,
+                              src=build_path_join('$(abs_srcdir)', file),
+                              dst=build_path_join(includedir,
+                                                  build_path_basename(file)))
+                      for file in self.includes]
+    data.includedir = includedir
     self.ofile.write('install-include: %s\n'
                      '\t$(MKDIR) $(DESTDIR)%s\n'
                      % (' '.join(self.includes), includedir))
@@ -443,6 +558,8 @@ class Generator(gen_base.GeneratorBase):
       if not isinstance(target, gen_base.TargetScript) and \
          not isinstance(target, gen_base.TargetJava) and \
          not isinstance(target, gen_base.TargetI18N):
+        data.isources.append(_eztdata(name=target.name,
+                                      filename=target.filename))
         self.ofile.write('%s: %s\n' % (target.name, target.filename))
 
     ########################################
@@ -454,11 +571,15 @@ class Generator(gen_base.GeneratorBase):
     obj_deps = sorted(self.graph.get_deps(gen_base.DT_OBJECT), key = lambda t: t[0].filename)
 
     for objname, sources in obj_deps:
-      deps = ' '.join(map(str, sources))
-      self.ofile.write('%s: %s\n' % (objname, deps))
+      deps = map(str, sources)
+      self.ofile.write('%s: %s\n' % (objname, ' '.join(deps)))
       cmd = objname.compile_cmd
+      dep = _eztdata(name=str(objname), deps=deps, cmd=cmd,
+                     source=str(sources[0]))
+      data.deps.append(dep)
       if cmd:
-        if not getattr(objname, 'source_generated', 0):
+        dep.generated = ezt.boolean(getattr(objname, 'source_generated', 0))
+        if not dep.generated:
           self.ofile.write('\t%s %s\n\n'
                            % (cmd, '$(canonicalized_srcdir)' + str(sources[0])))
         else:
@@ -468,6 +589,12 @@ class Generator(gen_base.GeneratorBase):
 
 
     self.ofile.close()
+
+    template = ezt.Template(os.path.join('build', 'generator', 'templates',
+                                         'makefile.ezt'),
+                            compress_whitespace=False)
+    template.generate(open('build-outputs-e.mk', 'w'), data)
+
     self.write_standalone()
 
   def write_standalone(self):
