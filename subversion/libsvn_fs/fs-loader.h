abcd34144187a6f38732648d995359d9251ed57b
Neels Hofmeyr <neels@apache.org>
2009-06-03 01:54:20 +0000

    * subversion/libsvn_fs/fs-loader.h (root_vtable_t): Comment.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@877987 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs/fs-loader.h
+++ subversion/libsvn_fs/fs-loader.h
@@ -213,7 +213,9 @@ typedef struct txn_vtable_t
    roots may not all have the same vtable, we need a rule to determine
    which root's vtable is used.  The rule is: if one of the roots is
    named "target", we use that root's vtable; otherwise, we use the
-   first root argument's vtable. */
+   first root argument's vtable.
+   These callbacks basically correspond to svn_fs_* functions
+   in include/svn_fs.h, see there for details. */
 typedef struct root_vtable_t
 {
   /* Determining what has changed in a root */
