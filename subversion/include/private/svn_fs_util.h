6ed3a59f2f63b6cf9e651fe6638b7b13ff0640fd
Hyrum Kurt Wright <hwright@apache.org>
2009-06-30 21:48:50 +0000

    A few more local_style deprecation replacements, this time in the filesystem layer.
    * subversion/include/private/svn_fs_util.h
      (SVN_FS__ALREADY_EXISTS),
    * subversion/libsvn_fs_fs/fs_fs.c
      (svn_fs_fs__path_rev_absolute, get_writable_proto_rev_body,
       check_format_file_buffer_numeric, read_format,
       svn_fs_fs__dup_perms),
    * subversion/libsvn_fs_fs/lock.c
      (write_digest_file, read_digest_file):
        Switch out svn_path_local_style() for svn_dirent_local_style().
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@878349 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/private/svn_fs_util.h
+++ subversion/include/private/svn_fs_util.h
@@ -75,12 +75,12 @@ svn_fs__check_fs(svn_fs_t *fs, svn_boolean_t expect_open);
     svn_error_createf                                                          \
       (SVN_ERR_FS_ALREADY_EXISTS, 0,                                           \
        _("File already exists: filesystem '%s', transaction '%s', path '%s'"), \
-       svn_path_local_style(root->fs->path, pool), root->txn, path_str)        \
+       svn_dirent_local_style(root->fs->path, pool), root->txn, path_str)      \
   :                                                                            \
     svn_error_createf                                                          \
       (SVN_ERR_FS_ALREADY_EXISTS, 0,                                           \
        _("File already exists: filesystem '%s', revision %ld, path '%s'"),     \
-       svn_path_local_style(root->fs->path, pool), root->rev, path_str)        \
+       svn_dirent_local_style(root->fs->path, pool), root->rev, path_str)      \
   )
 
 /* ROOT is of type svn_fs_root_t *. */
