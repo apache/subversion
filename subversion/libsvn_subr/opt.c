5defa8bd3a06411b886f7bbfd5b5ea4038d796a4
Hyrum Kurt Wright <hwright@apache.org>
2009-07-07 22:03:10 +0000

    Relicense Subversion under the Apache License, Version 2.0.
    * NOTICE: New.
    
    * LICENSE: New.
    
    * COPYING,
      subversion/LICENSE: Remove.
    
    * subversion/libsvn_subr/opt.c
      (svn_opt__print_version_info): Note that the product as a whole is
        copyrighted by the SVN Corp, and that it contains contributions from
        many people, as referenced in NOTICE.
    
    * subversion/bindings/swig/python/LICENSE_FOR_PYTHON_BINDINGS:
      Relicense the SVN parts under Apache 2.0.
    
    * everywhere:
      Change copyright notices in file headers to reflect the Apache 2.0 license.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@878444 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/opt.c
+++ subversion/libsvn_subr/opt.c
@@ -2,17 +2,22 @@
  * opt.c :  option and argument parsing for Subversion command lines
  *
  * ====================================================================
- * Copyright (c) 2000-2009 CollabNet.  All rights reserved.
+ *    Licensed to the Subversion Corporation (SVN Corp.) under one
+ *    or more contributor license agreements.  See the NOTICE file
+ *    distributed with this work for additional information
+ *    regarding copyright ownership.  The SVN Corp. licenses this file
+ *    to you under the Apache License, Version 2.0 (the
+ *    "License"); you may not use this file except in compliance
+ *    with the License.  You may obtain a copy of the License at
  *
- * This software is licensed as described in the file COPYING, which
- * you should have received as part of this distribution.  The terms
- * are also available at http://subversion.tigris.org/license-1.html.
- * If newer versions of this license are posted there, you may use a
- * newer version instead, at your option.
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * This software consists of voluntary contributions made by many
- * individuals.  For exact contribution history, see the revision
- * history and logs, available at http://subversion.tigris.org/.
+ *    Unless required by applicable law or agreed to in writing,
+ *    software distributed under the License is distributed on an
+ *    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *    KIND, either express or implied.  See the License for the
+ *    specific language governing permissions and limitations
+ *    under the License.
  * ====================================================================
  */
 
@@ -969,11 +974,12 @@ svn_opt__print_version_info(const char *pgm_name,
   SVN_ERR(svn_cmdline_printf(pool, _("%s, version %s\n"
                                      "   compiled %s, %s\n\n"), pgm_name,
                              SVN_VERSION, __DATE__, __TIME__));
-  SVN_ERR(svn_cmdline_fputs(_("Copyright (C) 2000-2009 CollabNet.\n"
+  SVN_ERR(svn_cmdline_fputs(_("Copyright (C) 2009 The Subversion Corporation.\n"
+                              "This software consists of"
+                              " contributions made by many people;\n"
+                              "see the NOTICE file for more information.\n"
                               "Subversion is open source software, see"
-                              " http://subversion.tigris.org/\n"
-                              "This product includes software developed by "
-                              "CollabNet (http://www.Collab.Net/).\n\n"),
+                              " http://subversion.tigris.org/\n\n"),
                             stdout, pool));
 
   if (footer)
