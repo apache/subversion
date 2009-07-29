e76895f37616e42ff390a51ed2c977c4dcffc005
Arfrever Frehtes Taifersar Arahesis <arfrever@apache.org>
2009-07-29 17:14:56 +0000

    * tools/dist/construct-rolling-environment.sh: Update versions of some    dependencies.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@878563 13f79535-47bb-0310-9956-ffa450edef68

--- tools/dist/construct-rolling-environment.sh
+++ tools/dist/construct-rolling-environment.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 set -e
 
-AUTOCONF=autoconf-2.63
+AUTOCONF=autoconf-2.64
 LIBTOOL=libtool-1.5.26
 SWIG=swig-1.3.36
 
-APR=apr-1.3.6
+APR=apr-1.3.7
 APR_UTIL=apr-util-1.3.8
 NEON=neon-0.28.5
 SERF=serf-0.3.0
 ZLIB=zlib-1.2.3
-SQLITE_VERSION=3.6.15
+SQLITE_VERSION=3.6.16
 SQLITE=sqlite-amalgamation-$SQLITE_VERSION
 
-HTTPD=httpd-2.2.11
+HTTPD=httpd-2.2.12
 HTTPD_OOPS=
 APR_ICONV=apr-iconv-1.2.1
 APR_ICONV_OOPS=
