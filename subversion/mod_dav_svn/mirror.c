a92836e5b2ac81f8006f4985a4b12df29ea11c3a
Kamesh J <kameshj@apache.org>
2009-08-04 11:07:06 +0000

    Follow-up to r38533
    * subversion/mod_dav_svn/mirror.c
      (dav_svn__location_in_filter, dav_svn__location_body_filter):
       Compare master_uri's PATH portion with 'root_dir' not with 
       master_uri itself.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@878625 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/mod_dav_svn/mirror.c
+++ subversion/mod_dav_svn/mirror.c
@@ -140,7 +140,7 @@ apr_status_t dav_svn__location_in_filter(ap_filter_t *f,
        (that is, if our root path matches that of the master server). */
     apr_uri_parse(r->pool, master_uri, &uri);
     root_dir = dav_svn__get_root_dir(r);
-    if (strcmp(master_uri, root_dir) == 0) {
+    if (strcmp(uri.path, root_dir) == 0) {
         ap_remove_input_filter(f);
         return ap_get_brigade(f->next, bb, mode, block, readbytes);
     }
@@ -252,7 +252,7 @@ apr_status_t dav_svn__location_body_filter(ap_filter_t *f,
        (that is, if our root path matches that of the master server). */
     apr_uri_parse(r->pool, master_uri, &uri);
     root_dir = dav_svn__get_root_dir(r);
-    if (strcmp(master_uri, root_dir) == 0) {
+    if (strcmp(uri.path, root_dir) == 0) {
         ap_remove_input_filter(f);
         return ap_pass_brigade(f->next, bb);
     }
