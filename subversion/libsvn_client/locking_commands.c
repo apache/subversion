ed1d44af2a3d5425b9f545de5e1b870d9988f741
Hyrum Kurt Wright <hwright@apache.org>
2009-08-13 13:18:45 +0000

    * subversion/libsvn_client/locking_commands.c   (organize_lock_targets): Use svn_wc__get_entry_versioned() in place of     svn_wc__entry_versioned().
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@878796 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/locking_commands.c
+++ subversion/libsvn_client/locking_commands.c
@@ -260,8 +260,9 @@ organize_lock_targets(const char **common_parent,
           abs_path = svn_path_join
             (svn_wc_adm_access_path(*parent_adm_access_p), target, subpool);
 
-          SVN_ERR(svn_wc__entry_versioned(&entry, abs_path,
-                                         *parent_adm_access_p, FALSE, subpool));
+          SVN_ERR(svn_wc__get_entry_versioned(&entry, ctx->wc_ctx, abs_path,
+                                              svn_node_unknown, FALSE, FALSE,
+                                              subpool, subpool));
 
           if (! entry->url)
             return svn_error_createf(SVN_ERR_ENTRY_MISSING_URL, NULL,
