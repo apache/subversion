84f81a04ccf88a43c1773bc1f0b265f887e2dcc4
C. Michael Pilato <cmpilato@apache.org>
2009-08-17 18:15:51 +0000

    Merge from trunk r38410, which contains some whitespace-in-error-message fixes.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/1.6.x-r38222@878855 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_auth_gnome_keyring/gnome_keyring.c
+++ subversion/libsvn_auth_gnome_keyring/gnome_keyring.c
@@ -408,7 +408,7 @@ simple_gnome_keyring_first_creds(void **credentials,
     {
       return svn_error_create(SVN_ERR_AUTHN_CREDS_UNAVAILABLE, NULL,
                               _("GNOME Keyring is locked and "
-                                " we are non-interactive"));
+                                "we are non-interactive"));
     }
   else
     {
@@ -464,7 +464,7 @@ simple_gnome_keyring_save_creds(svn_boolean_t *saved,
     {
       return svn_error_create(SVN_ERR_AUTHN_CREDS_NOT_SAVED, NULL,
                               _("GNOME Keyring is locked and "
-                                " we are non-interactive"));
+                                "we are non-interactive"));
     }
   else
     {
@@ -557,7 +557,7 @@ ssl_client_cert_pw_gnome_keyring_first_creds(void **credentials,
     {
       return svn_error_create(SVN_ERR_AUTHN_CREDS_UNAVAILABLE, NULL,
                               _("GNOME Keyring is locked and "
-                                " we are non-interactive"));
+                                "we are non-interactive"));
     }
   else
     {
@@ -614,7 +614,7 @@ ssl_client_cert_pw_gnome_keyring_save_creds(svn_boolean_t *saved,
     {
       return svn_error_create(SVN_ERR_AUTHN_CREDS_UNAVAILABLE, NULL,
                               _("GNOME Keyring is locked and "
-                                " we are non-interactive"));
+                                "we are non-interactive"));
     }
   else
     {
