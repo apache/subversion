cfa962da81fe88208cbe965f966b969c31c88542
Stefan Sperling <stsp@apache.org>
2009-08-27 20:35:11 +0000

    Create a new callback for svn_stream_readline() that can perform arbitrary transformations on a line before it is returned to the caller of svn_stream_readline().
    * subversion/include/svn_io.h
      (svn_io_line_transformer_cb_t): New callback type.
      (svn_stream_set_line_transformer_callback): Declare.
      (svn_stream_readline): Document the new line transformation feature.
    
    * subversion/libsvn_subr/stream.c
      (svn_stream_t): Add new field 'transformer_cb'.
      (svn_stream_create): Initialize the new field.
      (svn_stream_set_line_transformer_callback): New function to set the
        line transformer callback on a stream.
      (line_transformer): New helper function for svn_stream_readline().
      (svn_stream_readline): Invoke the line transformation callback if defined.
        Also, add a comment explaining that the iterpool is on purpose not
        destroyed right after the loop has exited.
    
    * subversion/tests/libsvn_subr/stream-tests.c
      (line_transformer): An implementation of svn_io_line_transformer_cb_t,
       reverses the supplied line.
      (test_stream_line_transformer): New test, testing line transformations
        on a stream.
      (test_stream_line_filter_and_transformer): New test, testing line
        transformations as well as line filtering on a stream.
      (test_funcs): Add the 2 new tests.
    
    Patch by: Daniel Näslund <daniel@longitudo.com>
              me
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@879047 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_io.h
+++ subversion/include/svn_io.h
@@ -705,6 +705,25 @@ typedef svn_error_t *(*svn_io_line_filter_cb_t)(svn_boolean_t *filtered,
                                                 const char *line,
                                                 apr_pool_t *scratch_pool);
 
+/** A callback function, invoked by svn_stream_readline(), which can perform
+ * arbitary transformations on the line before it is passed back to the caller
+ * of svn_stream_readline().
+ *
+ * Returns a transformed stringbuf in @a buf, allocated in @a result_pool.
+ * This callback gets invoked on lines which were not filtered by the
+ * line-filtering callback function.
+ *
+ * Implementations should always at least return an empty stringbuf.
+ * It is a fatal error if an implementation returns @a *buf as NULL.
+ *
+ * @see svn_stream_t, svn_io_line_filter_cb_t and svn_stream_readline().
+ * 
+ * @since New in 1.7. */
+typedef svn_error_t *(*svn_io_line_transformer_cb_t)(svn_stringbuf_t **buf,
+                                                     const char *line,
+                                                     apr_pool_t *result_pool,
+                                                     apr_pool_t *scratch_pool);
+
 /** Create a generic stream.  @see svn_stream_t. */
 svn_stream_t *
 svn_stream_create(void *baton,
@@ -741,6 +760,14 @@ svn_stream_set_reset(svn_stream_t *stream,
 void
 svn_stream_set_line_filter_callback(svn_stream_t *stream,
                                     svn_io_line_filter_cb_t line_filter_cb);
+
+/** Set @a streams's line-transforming callback function to
+ * @a line_transformer_cb.
+ * @since New in 1.7. */
+void
+svn_stream_set_line_transformer_callback(svn_stream_t *stream,
+                                         svn_io_line_transformer_cb_t 
+                                         line_transformer_cb);
 
 /** Create a stream that is empty for reading and infinite for writing. */
 svn_stream_t *
@@ -1030,6 +1057,13 @@ svn_stream_printf_from_utf8(svn_stream_t *stream,
  * if they pass the filtering decision of the callback function.
  * If end-of-file is encountered while reading the line and the line
  * is filtered, @a *stringbuf will be empty.
+ *
+ * If a line-transformer callback function was set on the stream using
+ * svn_stream_set_line_transformer_callback(), lines will be returned
+ * transformed, in a way determined by the callback.
+ *
+ * Note that the line-transformer callback gets called after the line-filter
+ * callback, not before.
  */
 svn_error_t *
 svn_stream_readline(svn_stream_t *stream,
