dce25a446ca0f336330a75a3be12c1c7d117eb7e
Stefan Sperling <stsp@apache.org>
2009-08-28 14:01:09 +0000

    Move functions concerned with EOL parsing from libsvn_subr/subst.c to libsvn_subr/eol.c. This allows further consolidation of code concerned with EOL-parsing instead of being all over the place.
    Note that functions concerned with the svn:eol-style property are not
    affected by this change.
    
    [ Note from the future: The change to build.conf was accidentally not
      committed, and was instead committed in r38984. ]
    * build.conf:
      (subst-test): Rename to ...
      (eol-test): ... this.
      (__ALL_TESTS__): Track renamed test.
      (msvc-export): Add private\svn_eol_private.h.
    
    * subversion/include/private/svn_eol_private.h: New.
    
    * subversion/libsvn_subr/subst.c,
      subversion/include/svn_subst.h
      (svn_subst_find_eol_start, svn_subst_detect_eol,
       svn_subst_detect_file_eol): Move these functions from here ...
    
    * subversion/libsvn_subr/eol.c,
      subversion/include/private/svn_eol_private.h
      (svn_eol_find_eol_start, svn_eol_detect_eol,
       svn_eol_detect_file_eol): ... to these new files and rename them.
    
    * subversion/libsvn_client/patch.c,
      subversion/libsvn_diff/diff-file.c: Track renamed functions.
    
    * subversion/tests/libsvn_subr/subst-test.c: Rename to ...
    
    * subversion/tests/libsvn_subr/eol-test.c: ... this, and track renamed
       functions.
    
    Patch by: Edmund Wong <ed{_AT_}kdtc.net>
    Suggested by: stsp
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@879057 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subversion/include/private/svn_eol_private.h
@@ -0,0 +1,86 @@
+/**
+ * @copyright
+ * ====================================================================
+ *    Licensed to the Subversion Corporation (SVN Corp.) under one
+ *    or more contributor license agreements.  See the NOTICE file
+ *    distributed with this work for additional information
+ *    regarding copyright ownership.  The SVN Corp. licenses this file
+ *    to you under the Apache License, Version 2.0 (the
+ *    "License"); you may not use this file except in compliance
+ *    with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing,
+ *    software distributed under the License is distributed on an
+ *    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *    KIND, either express or implied.  See the License for the
+ *    specific language governing permissions and limitations
+ *    under the License.
+ * ====================================================================
+ * @endcopyright
+ *
+ * @file svn_eol_private.h
+ * @brief Subversion's EOL functions - Internal routines
+ */
+
+#ifndef SVN_EOL_PRIVATE_H
+#define SVN_EOL_PRIVATE_H
+
+#include <apr_pools.h>
+#include <apr_hash.h>
+
+#include "svn_types.h"
+#include "svn_error.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+/* Generic EOL character helper routines */
+
+/* Look for the start of an end-of-line sequence (i.e. CR or LF)
+ * in the array pointed to by @a buf , of length @a len.
+ * If such a byte is found, return the pointer to it, else return NULL.
+ *
+ * @since New in 1.7
+ */
+char *
+svn_eol_find_eol_start(char *buf, apr_size_t len);
+
+/* Return the first eol marker found in [@a buf, @a endp) as a
+ * NUL-terminated string, or NULL if no eol marker is found.
+ *
+ * If the last valid character of @a buf is the first byte of a
+ * potentially two-byte eol sequence, just return that single-character
+ * sequence, that is, assume @a buf represents a CR-only or LF-only file.
+ * This is correct for callers that pass an entire file at once, and is
+ * no more likely to be incorrect than correct for any caller that
+ * doesn't.
+ *
+ * @since New in 1.7
+ */
+const char *
+svn_eol_detect_eol(char *buf, char *endp);
+
+/* Detect the EOL marker used in @a file and return it in @a *eol.
+ * If it cannot be detected, set @a *eol to NULL.
+ *
+ * The file is searched starting at the current file cursor position.
+ * The first EOL marker found will be returnd. So if the file has
+ * inconsistent EOL markers, this won't be detected.
+ *
+ * Upon return, the original file cursor position is always preserved,
+ * even if an error is thrown.
+ *
+ * Do temporary allocations in @a pool.
+ *
+ * @since New in 1.7 */
+svn_error_t *
+svn_eol_detect_file_eol(const char **eol, apr_file_t *file, apr_pool_t *pool);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* SVN_EOL_PRIVATE_H */
