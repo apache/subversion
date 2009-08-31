ba6f9b9eed2e779d9c12ce123fed243ecab82e30
Stefan Sperling <stsp@apache.org>
2009-08-31 22:22:37 +0000

    r39000, r39002, and r39007 were all just part of a bigger plot: Getting merge_test 132 ("replace that causes a tree-conflict") to pass for valid reasons.
    * subversion/tests/cmdline/merge_tests.py
      (merge_replace_causes_tree_conflict): Tweak test expectations slightly,
       a property conflict on a directory is flagged. We have no defined
       behaviour yet for the case where both a property and a tree conflict
       is flagged, so just pass the current behaviour of flagging both.
       Leave a comment asking whether this is OK for future archaeologists.
       Most importantly, remove XFail. This test now passes beautifully,
       all TCs are correctly recognized as "local edit, incoming replace
       upon merge".
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@879082 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/merge_tests.py
+++ subversion/tests/cmdline/merge_tests.py
@@ -16141,7 +16141,9 @@ def merge_replace_causes_tree_conflict(sbox):
   # svn st
   expected_status.tweak('A', status=' M')
   expected_status.tweak('A/D/G/pi', 'A/mu', status='M ', treeconflict='C')
-  expected_status.tweak('A/D/H', 'A/B/E', status=' M', treeconflict='C')
+  expected_status.tweak('A/D/H', status=' M', treeconflict='C')
+  ### A/B/E gets both a property and tree conflict flagged. Is this OK?
+  expected_status.tweak('A/B/E', status=' C', treeconflict='C')
 
   actions.run_and_verify_status(wc_dir, expected_status)
 
@@ -16442,7 +16444,7 @@ test_list = [ None,
                          server_has_mergeinfo),
               SkipUnless(multiple_reintegrates_from_the_same_branch,
                          server_has_mergeinfo),
-              XFail(merge_replace_causes_tree_conflict),
+              merge_replace_causes_tree_conflict,
               SkipUnless(handle_gaps_in_implicit_mergeinfo,
                          server_has_mergeinfo),
               XFail(copy_then_replace_via_merge),
