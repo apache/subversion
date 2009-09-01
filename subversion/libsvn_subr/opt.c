0743f35ae31d878e271489e0ed2c89b13b61e9ab
Hyrum Kurt Wright <hwright@apache.org>
2009-09-01 14:09:26 +0000

    In updating type information in r39026, I mistakenly used an unsigned type which we depended on rolling over to a negative value.  Rather than attempt to put an unsigned value into a signed variable and add casts everywhere, just use pointers (since that really what we're trying to emulate here), and be done with it.
    * subversion/libsvn_subr/opt.c
      (svn_opt__split_arg_at_peg_revision): Instead of using an index, use
        pointer comparisons.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@879102 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/opt.c
+++ subversion/libsvn_subr/opt.c
@@ -889,18 +889,19 @@ svn_opt__split_arg_at_peg_revision(const char **true_target,
                                    apr_pool_t *pool)
 {
   const char *peg_start = NULL; /* pointer to the peg revision, if any */
-  size_t j;
+  const char *ptr;
 
-  for (j = (strlen(utf8_target) - 1); j >= 0; --j)
+  for (ptr = (utf8_target + strlen(utf8_target) - 1); ptr >= utf8_target;
+        --ptr)
     {
       /* If we hit a path separator, stop looking.  This is OK
           only because our revision specifiers can't contain '/'. */
-      if (utf8_target[j] == '/')
+      if (*ptr == '/')
         break;
 
-      if (utf8_target[j] == '@')
+      if (*ptr == '@')
         {
-          peg_start = &utf8_target[j];
+          peg_start = ptr;
           break;
         }
     }
@@ -908,13 +909,13 @@ svn_opt__split_arg_at_peg_revision(const char **true_target,
   if (peg_start)
     {
       /* Error out if target is the empty string. */
-      if (j == 0)
+      if (ptr == utf8_target)
         return svn_error_createf(SVN_ERR_BAD_FILENAME, NULL,
                                  _("'%s' is just a peg revision. "
                                    "Maybe try '%s@' instead?"),
                                  utf8_target, utf8_target);
 
-      *true_target = apr_pstrmemdup(pool, utf8_target, j);
+      *true_target = apr_pstrmemdup(pool, utf8_target, ptr - utf8_target);
       if (peg_revision)
         *peg_revision = apr_pstrdup(pool, peg_start);
     }
