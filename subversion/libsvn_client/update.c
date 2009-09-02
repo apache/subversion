2d2a270b22b76ca095332d9afb573a75218b6504
Bert Huiben <rhuijben@apache.org>
2009-09-02 15:45:34 +0000

    Following up on r39073, make the notifications use svn_dirent_skip_ancestor()
    * subversion/libsvn_client/update.c
      (svn_client_update3): Apply workaround for basic_tests.py 4 behavior of
        calling svn update on a URL.
    
    * subversion/svn/notify.c
      (notify):
         Use new relative path helpers and rely on svn_dirent_local_style() for
         converting "" (canonical) to "." (not canonical).
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@879150 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/update.c
+++ subversion/libsvn_client/update.c
@@ -377,10 +377,26 @@ svn_client_update3(apr_array_header_t **result_revs,
           err = SVN_NO_ERROR;
           result_rev = SVN_INVALID_REVNUM;
           if (ctx->notify_func2)
-            (*ctx->notify_func2)(ctx->notify_baton2,
-                                 svn_wc_create_notify(path,
-                                                      svn_wc_notify_skip,
-                                                      subpool), subpool);
+            {
+              svn_wc_notify_t *notify;
+
+              if (svn_path_is_url(path))
+                {
+                  /* For some historic reason this user error is supported,
+                     and must provide correct notifications. */
+                  notify = svn_wc_create_notify_url(path,
+                                                    svn_wc_notify_skip,
+                                                    subpool);
+                }
+              else
+                {
+                  notify = svn_wc_create_notify(path,
+                                                svn_wc_notify_skip,
+                                                subpool);
+                }
+
+              (*ctx->notify_func2)(ctx->notify_baton2, notify, subpool);
+            }
         }
       if (result_revs)
         APR_ARRAY_PUSH(*result_revs, svn_revnum_t) = result_rev;
