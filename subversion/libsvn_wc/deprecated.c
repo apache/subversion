63c3444d41f09b32a4ccf048cc0ffcb87207fa77
Bert Huijben <rhuijben@apache.org>
2009-09-06 00:47:59 +0000

    [The deprecated.c part of this patch was reverted in r39155]
    Remove the usage of access batons from the status editor. This
    switches some entry functions to the new wc_db functions.
    
    * subversion/libsvn_wc/status.c
      (handle_dir_entry): Assume the passed entry is the full entry.
      (get_dir_status): Load childnodes directly from wc-db. Use an extra
         hash to collapse all status call updates in a single loop.
    
    * subversion/libsvn_wc/deprecated.c
      (svn_wc_get_status_editor4): Accidental commit of prototype change.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@879228 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/deprecated.c
+++ subversion/libsvn_wc/deprecated.c
@@ -1780,26 +1780,27 @@ svn_wc_get_status_editor4(const svn_delta_editor_t **editor,
 {
   struct status4_wrapper_baton *swb = apr_palloc(pool, sizeof(*swb));
   svn_wc_context_t *wc_ctx;
-  apr_pool_t *scratch_pool = svn_pool_create(pool);
-  svn_error_t *err;
 
   swb->old_func = status_func;
   swb->old_baton = status_baton;
 
   SVN_ERR(svn_wc__context_create_with_db(&wc_ctx, NULL /* config */,
                                          svn_wc__adm_get_db(anchor),
-                                         scratch_pool));
+                                         pool));
 
-  err = svn_wc_get_status_editor5(editor, edit_baton, set_locks_baton,
-                                  edit_revision, wc_ctx, anchor, target,
-                                  depth, get_all, no_ignore, ignore_patterns,
-                                  status4_wrapper_func, swb,
-                                  cancel_func, cancel_baton, traversal_info,
-                                  pool, scratch_pool);
+  SVN_ERR(svn_wc_get_status_editor5(editor, edit_baton, set_locks_baton,
+                                    edit_revision, wc_ctx,
+                                    svn_wc__adm_access_abspath(anchor),
+                                    svn_dirent_join(
+                                        svn_wc__adm_access_abspath(anchor),
+                                        target, pool),
+                                    depth, get_all, no_ignore, ignore_patterns,
+                                    status4_wrapper_func, swb,
+                                    cancel_func, cancel_baton, traversal_info,
+                                    pool, pool));
 
-  /* This destroys the context also. */
-  svn_pool_destroy(scratch_pool);
-  return err;
+  /* We can't destroy the context; the editor is a return value */
+  return SVN_NO_ERROR;
 }
 
 struct status_editor3_compat_baton
