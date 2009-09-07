14bc5b6078fdbe6ea317c6444e73b59edce1e27d
Hyrum Kurt Wright <hwright@apache.org>
2009-09-07 21:49:02 +0000

    Revert an over-eager, but bit too hasty r39172.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@879247 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/delete.c
+++ subversion/libsvn_client/delete.c
@@ -132,8 +132,7 @@ delete_urls(svn_commit_info_t **commit_info_p,
   apr_pool_t *subpool = svn_pool_create(pool);
 
   /* Condense our list of deletion targets. */
-  SVN_ERR(svn_dirent_condense_targets(&common, &targets, paths, TRUE, pool,
-                                      pool));
+  SVN_ERR(svn_path_condense_targets(&common, &targets, paths, TRUE, pool));
   if (! targets->nelts)
     {
       const char *bname;
