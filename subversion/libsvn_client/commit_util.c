28febf7d373803a3e22472608e17ab7a841eb04e
Hyrum Kurt Wright <hwright@apache.org>
2009-09-25 19:45:18 +0000

    * subversion/libsvn_client/commit_util.c   (svn_client__harvest_committables): Constify a variable.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@879683 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/commit_util.c
+++ subversion/libsvn_client/commit_util.c
@@ -996,7 +996,7 @@ svn_client__harvest_committables(apr_hash_t **committables,
        * conflicts error instead of a "not under version control". */
       if (err && (err->apr_err == SVN_ERR_ENTRY_NOT_FOUND))
         {
-          svn_wc_conflict_description2_t *conflict;
+          const svn_wc_conflict_description2_t *conflict;
           svn_wc__get_tree_conflict(&conflict, ctx->wc_ctx, target_abspath,
                                     pool, subpool);
           if (conflict != NULL)
