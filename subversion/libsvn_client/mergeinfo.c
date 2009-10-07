f944ad0c3f8e9085fcc2c00bf1c2a016e3b23f0c
Paul Burba <pburba@apache.org>
2009-10-07 18:34:06 +0000

    Doc string fix.
    * subversion/libsvn_client/mergeinfo.c
      (elide_mergeinfo): Update argument names and remove mention of
       non-existent ADM_ACCESS arg.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@879913 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/mergeinfo.c
+++ subversion/libsvn_client/mergeinfo.c
@@ -814,13 +814,12 @@ should_elide_mergeinfo(svn_boolean_t *elides,
 
 /* Helper for svn_client__elide_mergeinfo().
 
-   Given a working copy PATH, its mergeinfo hash CHILD_MERGEINFO, and
-   the mergeinfo of PATH's nearest ancestor PARENT_MERGEINFO, use
+   Given a working copy LOCAL_ABSPATH, its mergeinfo hash CHILD_MERGEINFO, and
+   the mergeinfo of LOCAL_ABSPATH's nearest ancestor PARENT_MERGEINFO, use
    should_elide_mergeinfo() to decide whether or not CHILD_MERGEINFO elides to
    PARENT_MERGEINFO; PATH_SUFFIX means the same as in that function.
 
-   If elision does occur, then update the mergeinfo for PATH (which is
-   the child) in the working copy via ADM_ACCESS appropriately.
+   If elision does occur, then remove the mergeinfo for LOCAL_ABSPATH.
 
    If CHILD_MERGEINFO is NULL, do nothing.
 
