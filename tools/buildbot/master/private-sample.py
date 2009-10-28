a96aa47c99b97b0bbf9737f032226916fea4fd9b
Lieven Govaerts <lgo@apache.org>
2009-10-28 14:34:24 +0000

    The Buildbot master configuration + customization files.
    Note 1: these are the actual files, no cleanup of extra comments added.
    
    Note 2: changes made to these files are *not* automatically propagated
            to the buildbot master server.
    
    * tools/buildbot/master/master.cfg: The configuration of the buildbot master.
    
    * tools/buildbot/master/private-sample.py: Specifies the format of 
       the file containing private information (paths, passwords).
    
    * tools/buildbot/master/SVNMailNotifier.py: Extends buildbot to send
       mails in the correct format to the svn-breakage list.
    
    * tools/buildbot/master/Feeder.py: RSS+ATOM feeds. This has been 
       integrated upstream in later versions of buildbot.
    
    * tools/buildbot/master/public_html/*: Public web interface files.
    * tools/buildbot/master/public_html/robots.txt
    * tools/buildbot/master/public_html/index.html
    * tools/buildbot/master/public_html/buildbot.css
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@880324 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tools/buildbot/master/private-sample.py
@@ -0,0 +1,12 @@
+# -*- python -*-
+
+svnbin="svn"
+smtp="smtp.example.com"
+htmlPort = 8010
+slavePortnum = 9989
+tryPort = 8031
+tryUser = "xxxx"
+tryPwd = "xxxx"
+slavePwd = "xxxx"
+hwrightPwd = "xxxx"
+aholst_slavePwd = "xxxx"
