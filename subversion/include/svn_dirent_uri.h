0bd9bd492b40fbec73b73a9154a7481da2f43036
Branko Čibej <brane@apache.org>
2009-10-31 12:32:51 +0000

    * subversion/include/svn_dirent_uri.h (svn_uri_join): Fix typo in docstring.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@880398 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_dirent_uri.h
+++ subversion/include/svn_dirent_uri.h
@@ -180,11 +180,11 @@ svn_relpath_join(const char *base,
                  apr_pool_t *pool);
 
 /** Join a valid base uri (@a base) with a relative path or uri
- * (@a component), allocating the result in @a pool. @a relpath need
+ * (@a component), allocating the result in @a pool. @a component need
  * not be a single component: it can be a relative path or a '/'
  * prefixed relative path to join component to the root path of @a base.
  *
- * If @a relpath is the empty path, then @a base will be copied and
+ * If @a component is the empty path, then @a base will be copied and
  * returned.
  *
  * If the @a component is an absolute uri, then it is copied and returned.
