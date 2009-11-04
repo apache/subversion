63d38045ea3798f4f65e771b936e23096551b968
Arfrever Frehtes Taifersar Arahesis <arfrever@apache.org>
2009-11-04 16:14:12 +0000

    * subversion/libsvn_subr/io.c   (svn_io_file_perms_set): Slightly change error message so that it's identical    to already translated error message in svn_io_copy_perms().
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@880447 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/io.c
+++ subversion/libsvn_subr/io.c
@@ -3651,7 +3651,7 @@ svn_io_file_perms_set(const char *fname, apr_fileperms_t perms,
 
   status = apr_file_perms_set(fname_apr, perms);
   if (status)
-    return svn_error_wrap_apr(status, _("Can't set permissions of '%s'"),
+    return svn_error_wrap_apr(status, _("Can't set permissions on '%s'"),
                               fname);
   else
     return SVN_NO_ERROR;
