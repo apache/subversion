3ed403e19d09cc04157271b39712bc03a0038b74
Stefan Sperling <stsp@apache.org>
2009-11-06 15:37:10 +0000

    Make 'svn patch' ignore "\ No newline at end of file" marker lines. They were interpreted as context and caused text conflicts.
    An open question is whether 'svn patch' should do something smart
    in the presence of such lines. Currently, applying a patch which only
    changes the last character of a file to a newline is a no-op, even if
    the target file does not end with a newline. However, this behaviour is
    encoded somewhere within svn_wc_merge4(), rather than the 'svn patch' code.
    
    * subversion/libsvn_diff/parse-diff.c
      (original_line_filter, modified_line_filter): Filter "\ No newline at
       end of file" lines.
      (parse_next_hunk): Disregard "\ No newline at end of file" lines.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@880477 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_diff/parse-diff.c
+++ subversion/libsvn_diff/parse-diff.c
@@ -167,21 +167,25 @@ parse_hunk_header(const char *header, svn_hunk_t *hunk, apr_pool_t *pool)
   return TRUE;
 }
 
-/* A stream line-filter which allows only original text from a hunk. */
+/* A stream line-filter which allows only original text from a hunk,
+ * and filters special lines. */
 static svn_error_t *
 original_line_filter(svn_boolean_t *filtered, const char *line, void *baton,
                      apr_pool_t *scratch_pool)
 {
-  *filtered = line[0] == '+';
+  *filtered = (line[0] == '+' ||
+               strcmp(line, "\\ No newline at end of file") == 0);
   return SVN_NO_ERROR;
 }
 
-/* A stream line-filter which allows only modified text from a hunk. */
+/* A stream line-filter which allows only modified text from a hunk,
+ * and filters special lines. */
 static svn_error_t *
 modified_line_filter(svn_boolean_t *filtered, const char *line, void *baton,
                      apr_pool_t *scratch_pool)
 {
-  *filtered = line[0] == '-';
+  *filtered = (line[0] == '-' ||
+               strcmp(line, "\\ No newline at end of file") == 0);
   return SVN_NO_ERROR;
 }
 
@@ -248,6 +252,11 @@ parse_next_hunk(svn_hunk_t **hunk,
       last_line = pos;
       SVN_ERR(svn_stream_readline(stream, &line, patch->eol_str, &eof,
                                   iterpool));
+
+      /* Skip lines which aren't part of the hunk text. */
+      if (strcmp(line->data, "\\ No newline at end of file") == 0)
+        continue;
+
       if (! eof)
         {
           /* Update line offset for next iteration.
