00e2f8c81226659fb1bfebda8ea19ec18c3732f5
neels <dev-null@apache.org>
2009-11-08 21:55:39 +0000

    * notes/tree-conflicts/all-tests.sh: Update list of tree-conflicts tests.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@880499 13f79535-47bb-0310-9956-ffa450edef68

--- notes/tree-conflicts/all-tests.sh
+++ notes/tree-conflicts/all-tests.sh
@@ -2,7 +2,7 @@
 # tree-conflicts handling. You can use this file as a shell script, just
 # go to your subversion/tests/cmdline directory and run this file.
 
-./update_tests.py "$@" 33 34 46:51
+./update_tests.py "$@" 33 34 46:54
 ./switch_tests.py "$@" 31:35
 ./merge_tests.py "$@" 101:103 111:121 128:129
 ./stat_tests.py "$@" 31
