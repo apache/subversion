9be26f4e1f087b545fff38020fcf0f175a2db5d5
Hyrum Kurt Wright <hwright@apache.org>
2009-11-16 19:07:17 +0000

    Test out my new and fancy ASF commit priviledges by changing the copyright wording in our license headers to reflect ownership by the ASF.
    * NOTICE:
      Change terminology to ASF, and update a link.
    
    * subversion/libsvn_subr/opt.c
      (svn_opt__print_version_info): Note that the product as a whole is
        copyrighted by the ASF, and update the project website.
    
    * everywhere:
      Change license text to reflect ASF ownership.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@880911 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_props.h
+++ subversion/include/svn_props.h
@@ -1,10 +1,10 @@
 /**
  * @copyright
  * ====================================================================
- *    Licensed to the Subversion Corporation (SVN Corp.) under one
+ *    Licensed to the Apache Software Foundation (ASF) under one
  *    or more contributor license agreements.  See the NOTICE file
  *    distributed with this work for additional information
- *    regarding copyright ownership.  The SVN Corp. licenses this file
+ *    regarding copyright ownership.  The ASF licenses this file
  *    to you under the Apache License, Version 2.0 (the
  *    "License"); you may not use this file except in compliance
  *    with the License.  You may obtain a copy of the License at
@@ -350,10 +350,10 @@ svn_prop_name_is_valid(const char *prop_name);
 /** Meta-data properties.
  *
  * ====================================================================
- *    Licensed to the Subversion Corporation (SVN Corp.) under one
+ *    Licensed to the Apache Software Foundation (ASF) under one
  *    or more contributor license agreements.  See the NOTICE file
  *    distributed with this work for additional information
- *    regarding copyright ownership.  The SVN Corp. licenses this file
+ *    regarding copyright ownership.  The ASF licenses this file
  *    to you under the Apache License, Version 2.0 (the
  *    "License"); you may not use this file except in compliance
  *    with the License.  You may obtain a copy of the License at
