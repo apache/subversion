ed4578296a67f1b6440ae788c0a57fbed5bf7417
Paul Burba <pburba@apache.org>
2009-12-01 18:16:33 +0000

    Add a test for issue #3115 'Case only renames resulting from merges don't work or break the WC on case-insensitive file systems'.
    * subversion/tests/cmdline/merge_tests.py
      (svntest.main.is_fs_case_insensitive): Imported.
    
      (committed_case_only_move_and_revert): New.
    
      (test_list): Add committed_case_only_move_and_revert.  Set as XFail when
       run on case-insensitive file systems.
    
    * subversion/tests/cmdline/svntest/main.py
      (is_fs_case_insensitive): New test configuration check function.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@885855 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/svntest/main.py
+++ subversion/tests/cmdline/svntest/main.py
@@ -1022,6 +1022,9 @@ def is_posix_os():
 def is_os_darwin():
   return sys.platform == 'darwin'
 
+def is_fs_case_insensitive():
+  return (is_os_darwin() or is_os_windows())
+
 def server_has_mergeinfo():
   _check_command_line_parsed()
   return server_minor_version >= 5
