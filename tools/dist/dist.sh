apache/subversion

commit 87aa7e8281b8e2dcb2446ca01dcb5a552e65655a

Don't ship our packages/ directory, per the results of this thread: http://svn.haxx.se/dev/archive-2009-12/0205.shtml
* tools/dist/rat-excludes:
  Exclude packages/ from the RAT report.

* tools/dist/dist.sh:
  Don't ship packages/.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@889381 13f79535-47bb-0310-9956-ffa450edef68

tools/dist/dist.sh
@@ -242,6 +242,10 @@ ${SVN:-svn} export -q $EXTRA_EXPORT_OPTIONS -r "$REVISION" \
 # (See http://svn.haxx.se/dev/archive-2009-04/0166.shtml for discussion.)
 rm -rf "$DISTPATH/contrib"
 
+# Remove packages/ from the tarball.
+# (See http://svn.haxx.se/dev/archive-2009-12/0205.shtml)
+rm -rf "$DISPATH/packages"
+
 install_dependency()
 {
   DEP_NAME=$1

tools/dist/rat-excludes
@@ -42,6 +42,7 @@ subversion/bindings/ctypes-python/TODO
 subversion/bindings/ctypes-python/test/test.dumpfile
 subversion/bindings/ctypes-python/csvn/__init__.py
 subversion/bindings/ctypes-python/csvn/ext/__init__.py
+packages/
 tools/dist/rat-excludes
 doc/doxygen.conf
 **/*.dump

commit 2245995272d2fa1c135b44eb0197685d9b61ab6a

* tools/dist/dist.sh:   Fix a typo introduced in r889381.
git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@889404 13f79535-47bb-0310-9956-ffa450edef68

tools/dist/dist.sh
@@ -244,7 +244,7 @@ rm -rf "$DISTPATH/contrib"
 
 # Remove packages/ from the tarball.
 # (See http://svn.haxx.se/dev/archive-2009-12/0205.shtml)
-rm -rf "$DISPATH/packages"
+rm -rf "$DISTPATH/packages"
 
 install_dependency()
 {