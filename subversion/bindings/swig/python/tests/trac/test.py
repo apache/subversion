6c79477f9e86cc4ff635a10bf3d273f492193507
Julian Foad <julianfoad@apache.org>
2009-12-17 12:48:09 +0000

    Move character-coding declaration lines in Python files back to the beginning of the file where they need to be, after a licence message was inserted before them in r882257. This fixes a failure to run the Python tests under Python 2.5, which I noticed on the macosx buildbot.
    * tools/hook-scripts/mailer/mailer.py
    * tools/hook-scripts/svnperms.py
    * subversion/bindings/swig/python/tests/trac/test.py
      Move the "coding" line back to the beginning, just after the she-bang line.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@891679 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/swig/python/tests/trac/test.py
+++ subversion/bindings/swig/python/tests/trac/test.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
 #
 #
 # Licensed to the Apache Software Foundation (ASF) under one
@@ -18,8 +20,6 @@
 # specific language governing permissions and limitations
 # under the License.
 #
-#
-# -*- coding: utf-8 -*-
 #
 # Copyright (C) 2003, 2004, 2005 Edgewall Software
 # Copyright (C) 2003, 2004, 2005 Jonas Borgström <jonas@edgewall.com>
