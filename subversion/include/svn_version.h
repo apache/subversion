5d7422017b812ba0a1fa2bb71c4f2199c6ff9b31
C. Michael Pilato <cmpilato@apache.org>
2010-01-22 03:35:35 +0000

    *** NOTE: This errant change was fully reverted in r901971 ***
    Post-release cleanups.
    
    * STATUS
      Bump version number.
    
    * subversion/include/svn_version.h
      (SVN_VER_PATCH): Bump to "10".
      (SVN_VER_TAG, SVN_VER_NUMTAG, SVN_VER_REVISION): Revert the effects
        of the somewhat strange r880128 so that builds from this branch
        claim to be dev builds, not tagged releases.  Weird.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/tags/1.6.6@901969 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_version.h
+++ subversion/include/svn_version.h
@@ -66,7 +66,7 @@ extern "C" {
  *
  * @since New in 1.1.
  */
-#define SVN_VER_PATCH      6
+#define SVN_VER_PATCH      10
 
 
 /** @deprecated Provided for backward compatibility with the 1.0 API. */
@@ -89,7 +89,7 @@ extern "C" {
  *
  * Always change this at the same time as SVN_VER_NUMTAG.
  */
-#define SVN_VER_TAG        " (r40053)"
+#define SVN_VER_TAG        " (dev build)"
 
 
 /** Number tag: a string describing the version.
@@ -104,7 +104,7 @@ extern "C" {
  *
  * Always change this at the same time as SVN_VER_TAG.
  */
-#define SVN_VER_NUMTAG     ""
+#define SVN_VER_NUMTAG     "-dev"
 
 
 /** Revision number: The repository revision number of this release.
@@ -115,7 +115,7 @@ extern "C" {
  * When rolling a tarball, we automatically replace it with what we
  * guess to be the correct revision number.
  */
-#define SVN_VER_REVISION   40053
+#define SVN_VER_REVISION   0
 
 
 /* Version strings composed from the above definitions. */
