693fab074bbffbf6615e3c62f826611d8be526f9
Hyrum Kurt Wright <hwright@apache.org>
2010-02-02 16:17:28 +0000

    JavaHL: Migrate the NativeResources and Version class native implementations to the new org.apache package, and implement the old versions as simple wrappers.
    [ in subversion/bindings/javahl/ ]
    * native/org_tigris_subversion_javahl_Version.cpp:
      Removed, copied to ...
    
    * native/org_apache_subversion_javahl_Version.cpp:
      ... this.
    
    * native/org_tigris_subversion_javahl_NativeResources.cpp:
      Removed, copied to ...
    
    * native/org_apache_subversion_javahl_NativeResources.cpp:
      ... this.
    
    * src/org/tigris/subversion/javahl/Version.java:
      Make this class wrap the version in org.apache.
    
    * src/org/tigris/subversion/javahl/NativeResources.java:
      Same.
    
    * src/org/apache/subversion/javahl/NativeResources.java:
      Make the class public so the old package can use it.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@905689 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/javahl/src/org/apache/subversion/javahl/NativeResources.java
+++ subversion/bindings/javahl/src/org/apache/subversion/javahl/NativeResources.java
@@ -27,7 +27,7 @@ package org.apache.subversion.javahl;
  * Handles activities related to management of native resouces
  * (e.g. loading of native libraries).
  */
-class NativeResources
+public class NativeResources
 {
     /**
      * Version information about the underlying native libraries.
