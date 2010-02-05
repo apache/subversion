737d6920ff82666c1d888acffe4e86b08e6c4674
C. Michael Pilato <cmpilato@apache.org>
2010-02-05 18:41:47 +0000

    On the '1.6.x-issue3390' branch, feign a backport of the issue 3390 fix by applying Daniel Näslund's backport patch, and then using 'svn merge --record-only' to claim that this was a merge of r877016 and r902093 from trunk.
    Patch by: Daniel Näslund <daniel{_AT_}longitudo.com>
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/1.6.x-issue3390@907043 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/switch.c
+++ subversion/libsvn_client/switch.c
@@ -218,15 +218,16 @@ svn_client__switch_internal(svn_revnum_t *result_rev,
      PATH.  When we call reporter->finish_report, the update_editor
      will be driven by svn_repos_dir_delta2.
 
-     We pass NULL for traversal_info because this is a switch, not an
-     update, and therefore we don't want to handle any externals
-     except the ones directly affected by the switch. */
+     We pass in a traversal_info for recording all externals. It
+     shouldn't be needed for a switch if it wasn't for the relative
+     externals of type '../path'. All of those must be resolved to 
+     the new location.  */
   err = svn_wc_crawl_revisions4(path, dir_access, reporter, report_baton,
                                 TRUE, depth, (! depth_is_sticky),
                                 (! server_supports_depth),
                                 use_commit_times,
                                 ctx->notify_func2, ctx->notify_baton2,
-                                NULL, /* no traversal info */
+                                traversal_info, 
                                 pool);
 
   if (err)
