apache/subversion

commit 9cdf09e22e9502cd421eb0fd540c30831de88bcd

Use the proper streamy function to read the pristine contents, and thereby fix a bungled commit in r898406.
* subversion/libsvn_wc/questions.c
  (svn_wc__internal_text_modified_p): Use svn_wc__get_pristine_contents() to
    fetch the pristine contents for the requested path.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@898492 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_wc/questions.c
@@ -266,7 +266,6 @@ svn_wc__internal_text_modified_p(svn_boolean_t *modified_p,
                                  apr_pool_t *scratch_pool)
 {
   svn_stream_t *pristine_stream;
-  svn_node_kind_t kind;
   svn_error_t *err;
   apr_finfo_t finfo;
 
@@ -359,27 +358,14 @@ svn_wc__internal_text_modified_p(svn_boolean_t *modified_p,
      yet committed. */
   /* We used to stat for the working base here, but we just give
      compare_and_verify a try; we'll check for errors afterwards */
-  SVN_ERR(svn_wc__db_pristine_read(&pristine_stream, db, local_abspath, NULL,
-                                   scratch_pool, scratch_pool));
-  /*if (err)
+  err = svn_wc__get_pristine_contents(&pristine_stream, db, local_abspath,
+                                      scratch_pool, scratch_pool);
+  if (err && APR_STATUS_IS_ENOENT(err->apr_err))
     {
-      svn_error_t *err2 = svn_io_check_path(textbase_abspath, &kind,
-                                            scratch_pool);
-      if (! err2 && kind != svn_node_file)
-        {
-          svn_error_clear(err);
-          *modified_p = TRUE;
-          return SVN_NO_ERROR;
-        }
-
-      if (err2)
-        {
-          svn_error_clear(err);
-          return err2;
-        }
-
-      return err;
-    }*/
+      svn_error_clear(err);
+      *modified_p = TRUE;
+      return SVN_NO_ERROR;
+    }
 
   /* Check all bytes, and verify checksum if requested. */
   SVN_ERR(compare_and_verify(modified_p, db, local_abspath, pristine_stream,

commit 27de2433033f5becb8f42db87c4ef67740511571

Following up on r89406 and r898492, resolve 10 Windows test failures by closing files after usage. Also remove an error leak introduced in r898492.
* subversion/libsvn_wc/questions.c
  (svn_wc__internal_text_modified_p): Close pristine stream and handle other
    errors than ENOENT instead of leaking them.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@907414 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_wc/questions.c
@@ -367,12 +367,15 @@ svn_wc__internal_text_modified_p(svn_boolean_t *modified_p,
       return SVN_NO_ERROR;
     }
 
-  /* Check all bytes, and verify checksum if requested. */
-  SVN_ERR(compare_and_verify(modified_p, db, local_abspath, pristine_stream,
-                             compare_textbases, force_comparison,
-                             scratch_pool));
+  SVN_ERR(err);
 
-  return SVN_NO_ERROR;
+  /* Check all bytes, and verify checksum if requested. */
+  err = compare_and_verify(modified_p, db, local_abspath, pristine_stream,
+                           compare_textbases, force_comparison,
+                           scratch_pool);
+
+  return svn_error_compose_create(err,
+                                  svn_stream_close(pristine_stream));
 }
 
 