db9d9ea0a4f73dff98509fecf27a811e122fbad8
Hyrum Kurt Wright <hwright@apache.org>
2010-03-11 20:45:32 +0000

    JavaHL: Fix build errors introduced by r922003.
    * subversion/bindings/javahl/native/LogMessageCallback.cpp
      (singleMessage): Use the updated hash index functions.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@922016 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/javahl/native/LogMessageCallback.cpp
+++ subversion/bindings/javahl/native/LogMessageCallback.cpp
@@ -114,9 +114,9 @@ LogMessageCallback::singleMessage(svn_log_entry_t *log_entry, apr_pool_t *pool)
            hi;
            hi = apr_hash_next(hi))
         {
-          const char *path = (const char *) svn_apr_hash_index_key(hi);
+          const char *path = (const char *) svn__apr_hash_index_key(hi);
           svn_log_changed_path2_t *log_item =
-                    (svn_log_changed_path2_t *) svn_apr_hash_index_val(hi);
+                    (svn_log_changed_path2_t *) svn__apr_hash_index_val(hi);
 
           jstring jpath = JNIUtil::makeJString(path);
           if (JNIUtil::isJavaExceptionThrown())
