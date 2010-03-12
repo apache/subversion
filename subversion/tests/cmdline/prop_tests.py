apache/subversion

commit 8c058c57490ee161a53bd869dcbd8e469cd8622a

Convert the loggy operations in svn_wc_delete4 into a work queue item.  This is a precursor to removing the svn_wc__entry_modify2 call.
* subversion/libsvn_wc/adm_ops.c
  (svn_wc_delete4): Use work queue.

* subversion/libsvn_wc/workqueue.h (svn_wc__wq_add_delete): New.

* subversion/libsvn_wc/workqueue.c
  (svn_wc__wq_add_delete): New.
  (run_delete): New, derived from svn_wc_delete4.
  (dispatch_table): Add OP_DELETE.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@909093 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_wc/workqueue.c
@@ -55,6 +55,7 @@
 #define OP_DELETION_POSTCOMMIT "deletion-postcommit"
 #define OP_POSTCOMMIT "postcommit"
 #define OP_INSTALL_PROPERTIES "install-properties"
+#define OP_DELETE "delete"
 
 
 struct work_item_dispatch {
@@ -1664,6 +1665,121 @@ svn_wc__wq_add_install_properties(svn_wc__db_t *db,
   return SVN_NO_ERROR;
 }
 
+/* ------------------------------------------------------------------------ */
+
+/* OP_DELETE */
+
+static svn_error_t *
+run_delete(svn_wc__db_t *db,
+           const svn_skel_t *work_item,
+           svn_cancel_func_t cancel_func,
+           void *cancel_baton,
+           apr_pool_t *scratch_pool)
+{
+  const svn_skel_t *arg = work_item->children->next;
+  const char *local_abspath;
+  svn_wc__db_kind_t kind;
+  svn_boolean_t was_added, was_copied, was_replaced, base_shadowed;
+  svn_wc_entry_t tmp_entry;
+
+  local_abspath = apr_pstrmemdup(scratch_pool, arg->data, arg->len);
+  arg = arg->next;
+  kind = svn_skel__parse_int(arg, scratch_pool);
+  arg = arg->next;
+  was_added = svn_skel__parse_int(arg, scratch_pool);
+  arg = arg->next;
+  was_copied = svn_skel__parse_int(arg, scratch_pool);
+  arg = arg->next;
+  was_replaced = svn_skel__parse_int(arg, scratch_pool);
+  arg = arg->next;
+  base_shadowed = svn_skel__parse_int(arg, scratch_pool);
+
+  /* Edit the entry to reflect the now deleted state.
+     entries.c:fold_entry() clears the values of copied, copyfrom_rev
+     and copyfrom_url. */
+  tmp_entry.schedule = svn_wc_schedule_delete;
+  SVN_ERR(svn_wc__entry_modify2(db, local_abspath, svn_node_unknown, TRUE,
+                                &tmp_entry, SVN_WC__ENTRY_MODIFY_SCHEDULE,
+                                scratch_pool));
+
+  if (was_replaced && was_copied)
+    {
+      const char *props_base, *props_revert;
+      svn_error_t *err;
+
+      SVN_ERR(svn_wc__prop_path(&props_base, local_abspath, kind,
+                                svn_wc__props_base, scratch_pool));
+      SVN_ERR(svn_wc__prop_path(&props_revert, local_abspath, kind,
+                                svn_wc__props_revert, scratch_pool));
+      err = svn_io_file_rename(props_base, props_revert, scratch_pool);
+      if (err && !APR_STATUS_IS_ENOENT(err->apr_err))
+        return svn_error_quick_wrap(err, _("Can't move source to dest"));
+      svn_error_clear(err);
+
+      if (kind != svn_wc__db_kind_dir)
+        {
+          const char *text_base, *text_revert;
+
+          SVN_ERR(svn_wc__text_base_path(&text_base, db, local_abspath,
+                                         FALSE, scratch_pool));
+
+          SVN_ERR(svn_wc__text_revert_path(&text_revert, db,
+                                           local_abspath, scratch_pool));
+          err = svn_io_file_rename(text_revert, text_base, scratch_pool);
+          if (err && !APR_STATUS_IS_ENOENT(err->apr_err))
+            return svn_error_quick_wrap(err, _("Can't move source to dest"));
+          svn_error_clear(err);
+        }
+    }
+  if (was_added)
+    {
+      const char *props_base, *props_working;
+      svn_error_t *err;
+
+      SVN_ERR(svn_wc__prop_path(&props_base, local_abspath, kind,
+                                svn_wc__props_base, scratch_pool));
+      SVN_ERR(svn_wc__prop_path(&props_working, local_abspath, kind,
+                                svn_wc__props_working, scratch_pool));
+      
+      err = svn_io_remove_file2(props_base, TRUE, scratch_pool);
+      if (err && !APR_STATUS_IS_ENOENT(err->apr_err))
+        return svn_error_quick_wrap(err, _("Can't move source to dest"));
+      svn_error_clear(err);
+      err = svn_io_remove_file2(props_working, TRUE, scratch_pool);
+      if (err && !APR_STATUS_IS_ENOENT(err->apr_err))
+        return svn_error_quick_wrap(err, _("Can't move source to dest"));
+      svn_error_clear(err);
+    }
+
+  return SVN_NO_ERROR;
+}
+
+svn_error_t *
+svn_wc__wq_add_delete(svn_wc__db_t *db,
+                      const char *parent_abspath,
+                      const char *local_abspath,
+                      svn_wc__db_kind_t kind,
+                      svn_boolean_t was_added,
+                      svn_boolean_t was_copied,
+                      svn_boolean_t was_replaced,
+                      svn_boolean_t base_shadowed,
+                      apr_pool_t *scratch_pool)
+{
+  svn_skel_t *work_item = svn_skel__make_empty_list(scratch_pool);
+
+  svn_skel__prepend_int(base_shadowed, work_item, scratch_pool);
+  svn_skel__prepend_int(was_replaced, work_item, scratch_pool);
+  svn_skel__prepend_int(was_copied, work_item, scratch_pool);
+  svn_skel__prepend_int(was_added, work_item, scratch_pool);
+  svn_skel__prepend_int(kind, work_item, scratch_pool);
+  svn_skel__prepend_str(local_abspath, work_item, scratch_pool);
+  svn_skel__prepend_str(OP_DELETE, work_item, scratch_pool);
+
+  SVN_ERR(svn_wc__db_wq_add(db, parent_abspath, work_item, scratch_pool));
+
+  return SVN_NO_ERROR;
+}
+
 
 /* ------------------------------------------------------------------------ */
 
@@ -1675,6 +1791,7 @@ static const struct work_item_dispatch dispatch_table[] = {
   { OP_DELETION_POSTCOMMIT, run_deletion_postcommit },
   { OP_POSTCOMMIT, run_postcommit },
   { OP_INSTALL_PROPERTIES, run_install_properties },
+  { OP_DELETE, run_delete },
 
   /* Sentinel.  */
   { NULL }

subversion/libsvn_wc/adm_ops.c
@@ -1123,7 +1123,6 @@ svn_wc_delete4(svn_wc_context_t *wc_ctx,
   svn_boolean_t was_copied = FALSE;
   svn_boolean_t was_deleted = FALSE; /* Silence a gcc uninitialized warning */
   svn_error_t *err;
-  const char *parent_abspath = svn_dirent_dirname(local_abspath, pool);
   svn_wc__db_status_t status;
   svn_wc__db_kind_t kind;
   svn_boolean_t base_shadowed;
@@ -1229,63 +1228,14 @@ svn_wc_delete4(svn_wc_context_t *wc_ctx,
 
   if (kind != svn_wc__db_kind_dir || !was_add || was_deleted)
     {
-      /* We need to mark this entry for deletion in its parent's entries
-         file, so we split off base_name from the parent path, then fold in
-         the addition of a delete flag. */
-      svn_stringbuf_t *log_accum = svn_stringbuf_create("", pool);
-      svn_wc_entry_t tmp_entry;
+      const char *parent_abspath = svn_dirent_dirname(local_abspath, pool);
 
-      /* Edit the entry to reflect the now deleted state.
-         entries.c:fold_entry() clears the values of copied, copyfrom_rev
-         and copyfrom_url. */
-      tmp_entry.schedule = svn_wc_schedule_delete;
-      SVN_ERR(svn_wc__loggy_entry_modify(&log_accum,
-                                         parent_abspath,
-                                         local_abspath, &tmp_entry,
-                                         SVN_WC__ENTRY_MODIFY_SCHEDULE,
-                                         pool, pool));
-      SVN_WC__FLUSH_LOG_ACCUM(db, parent_abspath, log_accum, pool);
+      SVN_ERR(svn_wc__wq_add_delete(wc_ctx->db, parent_abspath, local_abspath,
+                                    kind, was_add, was_copied, was_replace,
+                                    base_shadowed, pool));
 
-      /* is it a replacement with history? */
-      if (was_replace && was_copied)
-        {
-          const char *text_base, *text_revert;
-
-          SVN_ERR(svn_wc__text_base_path(&text_base, wc_ctx->db, local_abspath,
-                                         FALSE, pool));
-
-          SVN_ERR(svn_wc__text_revert_path(&text_revert, wc_ctx->db,
-                                           local_abspath, pool));
-
-          if (kind != svn_wc__db_kind_dir) /* Dirs don't have text-bases */
-            /* Restore the original text-base */
-            SVN_ERR(svn_wc__loggy_move(&log_accum,
-                                       parent_abspath,
-                                       text_revert, text_base,
-                                       pool, pool));
-          SVN_WC__FLUSH_LOG_ACCUM(db, parent_abspath, log_accum, pool);
-
-          SVN_ERR(svn_wc__loggy_revert_props_restore(&log_accum, wc_ctx->db,
-                                                     local_abspath,
-                                                     parent_abspath, pool));
-          SVN_WC__FLUSH_LOG_ACCUM(db, parent_abspath, log_accum, pool);
-        }
-      if (was_add)
-        {
-          SVN_ERR(svn_wc__loggy_props_delete(&log_accum, wc_ctx->db,
-                                             local_abspath, parent_abspath,
-                                             svn_wc__props_base, pool));
-          SVN_WC__FLUSH_LOG_ACCUM(db, parent_abspath, log_accum, pool);
-
-          SVN_ERR(svn_wc__loggy_props_delete(&log_accum, wc_ctx->db,
-                                             local_abspath, parent_abspath,
-                                             svn_wc__props_working, pool));
-          SVN_WC__FLUSH_LOG_ACCUM(db, parent_abspath, log_accum, pool);
-        }
-
-      SVN_ERR(svn_wc__wq_add_loggy(db, parent_abspath, log_accum, pool));
-
-      SVN_ERR(svn_wc__run_log2(db, parent_abspath, pool));
+      SVN_ERR(svn_wc__wq_run(db, parent_abspath, cancel_func, cancel_baton,
+                             pool));
     }
 
   /* Report the deletion to the caller. */

subversion/libsvn_wc/workqueue.h
@@ -108,6 +108,25 @@ svn_wc__wq_add_install_properties(svn_wc__db_t *db,
                                   svn_boolean_t force_base_install,
                                   apr_pool_t *scratch_pool);
 
+/* Add a work item to delete a node.
+
+   ### LOCAL_ABSPATH is the node to be deleted and the queue exists in
+   PARENT_ABSPATH (because when LOCAL_ABSPATH is a directory it might
+   not exist on disk).  This use of PARENT_ABSPATH is inherited from
+   the log file conversion but perhaps we don't need to use a work
+   queue when deleting a directory that does not exist on disk.
+ */
+svn_error_t *
+svn_wc__wq_add_delete(svn_wc__db_t *db,
+                      const char *parent_abspath,
+                      const char *local_abspath,
+                      svn_wc__db_kind_t kind,
+                      svn_boolean_t was_added,
+                      svn_boolean_t was_copied,
+                      svn_boolean_t was_replaced,
+                      svn_boolean_t base_shadowed,
+                      apr_pool_t *scratch_pool);
+
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */

commit 1f82a73d1e82985e3f9aaa62aab92127e1fde532

Fix a property handling bug introduced in r909093: a rename was switched to the wrong ordering. A test was added to demonstrate the old problem, and to verify the fix.
Some other rename/deletion simplifications were made.

* subversion/libsvn_wc/workqueue.c:
  (move_if_present): add docstring
  (run_delete): switch to using move_if_present, as it will do the ENOENT
    testing. remove the ENOENT testing from the remove_file2 calls, since
    it already has a parameter to do exactly that(!). fix the ordering of
    the rename (move_if_present) call for the properties. it was backwards
    -- we want to put the revert props back to the base props.

* subversion/tests/cmdline/prop_tests.py:
  (rm_of_replaced_files): new test to ensure props on a replaced file are
    restored after a deletion of the replacement
  (test_list): add the new test


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@922105 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_wc/workqueue.c
@@ -117,7 +117,9 @@ copy_and_translate(svn_wc__db_t *db,
 }
 
 
-/* */
+/* If SOURCE_ABSPATH is present, then move it to DEST_ABSPATH. Ignore any
+   ENOENT message for a missing source, which may indicate the move has
+   already been performed.  */
 static svn_error_t *
 move_if_present(const char *source_abspath,
                 const char *dest_abspath,
@@ -1757,16 +1759,12 @@ run_delete(svn_wc__db_t *db,
   if (was_replaced && was_copied)
     {
       const char *props_base, *props_revert;
-      svn_error_t *err;
 
       SVN_ERR(svn_wc__prop_path(&props_base, local_abspath, kind,
                                 svn_wc__props_base, scratch_pool));
       SVN_ERR(svn_wc__prop_path(&props_revert, local_abspath, kind,
                                 svn_wc__props_revert, scratch_pool));
-      err = svn_io_file_rename(props_base, props_revert, scratch_pool);
-      if (err && !APR_STATUS_IS_ENOENT(err->apr_err))
-        return svn_error_quick_wrap(err, _("Can't move source to dest"));
-      svn_error_clear(err);
+      SVN_ERR(move_if_present(props_revert, props_base, scratch_pool));
 
       if (kind != svn_wc__db_kind_dir)
         {
@@ -1774,33 +1772,22 @@ run_delete(svn_wc__db_t *db,
 
           SVN_ERR(svn_wc__text_base_path(&text_base, db, local_abspath,
                                          FALSE, scratch_pool));
-
           SVN_ERR(svn_wc__text_revert_path(&text_revert, db,
                                            local_abspath, scratch_pool));
-          err = svn_io_file_rename(text_revert, text_base, scratch_pool);
-          if (err && !APR_STATUS_IS_ENOENT(err->apr_err))
-            return svn_error_quick_wrap(err, _("Can't move source to dest"));
-          svn_error_clear(err);
+          SVN_ERR(move_if_present(text_revert, text_base, scratch_pool));
         }
     }
   if (was_added)
     {
       const char *props_base, *props_working;
-      svn_error_t *err;
 
       SVN_ERR(svn_wc__prop_path(&props_base, local_abspath, kind,
                                 svn_wc__props_base, scratch_pool));
       SVN_ERR(svn_wc__prop_path(&props_working, local_abspath, kind,
                                 svn_wc__props_working, scratch_pool));
 
-      err = svn_io_remove_file2(props_base, TRUE, scratch_pool);
-      if (err && !APR_STATUS_IS_ENOENT(err->apr_err))
-        return svn_error_quick_wrap(err, _("Can't move source to dest"));
-      svn_error_clear(err);
-      err = svn_io_remove_file2(props_working, TRUE, scratch_pool);
-      if (err && !APR_STATUS_IS_ENOENT(err->apr_err))
-        return svn_error_quick_wrap(err, _("Can't move source to dest"));
-      svn_error_clear(err);
+      SVN_ERR(svn_io_remove_file2(props_base, TRUE, scratch_pool));
+      SVN_ERR(svn_io_remove_file2(props_working, TRUE, scratch_pool));
     }
 
   return SVN_NO_ERROR;

subversion/tests/cmdline/prop_tests.py
@@ -1734,6 +1734,51 @@ def post_revprop_change_hook(sbox):
                                      'ps', '--revprop', '-r0', 'p', 'v',
                                      wc_dir)
 
+def rm_of_replaced_file(sbox):
+  """properties after a removal of a replaced file"""
+
+  sbox.build()
+  wc_dir = sbox.wc_dir
+
+  # Add some properties to iota and mu
+  iota_path = os.path.join(wc_dir, 'iota')
+  svntest.main.run_svn(None, 'propset', 'red', 'rojo', iota_path)
+  svntest.main.run_svn(None, 'propset', 'blue', 'lagoon', iota_path)
+
+  mu_path = os.path.join(wc_dir, 'A', 'mu')
+  svntest.main.run_svn(None, 'propset', 'yellow', 'submarine', mu_path)
+  svntest.main.run_svn(None, 'propset', 'orange', 'toothpick', mu_path)
+
+  svntest.main.run_svn(None, 'ci', '-m', 'log message', wc_dir)
+
+  # Copy iota over the top of mu
+  svntest.main.run_svn(None, 'rm', mu_path)
+  svntest.main.run_svn(None, 'cp', iota_path, mu_path)
+
+  expected_disk = svntest.main.greek_state.copy()
+  expected_disk.tweak('iota', props={'red': 'rojo', 'blue': 'lagoon'})
+  expected_disk.tweak('A/mu', props={'red': 'rojo', 'blue': 'lagoon'},
+                      contents="This is the file 'iota'.\n")
+  actual_disk_tree = svntest.tree.build_tree_from_wc(wc_dir, 1)
+  svntest.tree.compare_trees("disk", actual_disk_tree,
+                             expected_disk.old_tree())
+
+  # Remove the copy. Properties should go back to mu's original props.
+  svntest.main.run_svn(None, 'rm', '--force', mu_path)
+
+  exit_code, output, errput = svntest.main.run_svn(None,
+                                                   'proplist', '-v', mu_path)
+  expected_output = svntest.verify.UnorderedRegexOutput([
+      'Properties on',
+      '  yellow',
+      '    submarine',
+      '  orange',
+      '    toothpick',
+      ])
+  svntest.verify.compare_and_display_lines('message', 'label',
+                                           expected_output, output)
+  svntest.verify.verify_exit_code(None, exit_code, 0)
+
 
 ########################################################################
 # Run the tests
@@ -1775,6 +1820,7 @@ test_list = [ None,
               added_moved_file,
               delete_nonexistent_property,
               XFail(post_revprop_change_hook, svntest.main.is_ra_type_dav),
+              rm_of_replaced_file,
              ]
 
 if __name__ == '__main__':