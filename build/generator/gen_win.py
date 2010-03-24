4b61b66771be9dd5dfd0d9b1aace5f5389a9e2bd
Bert Huiben <rhuijben@apache.org>
2010-03-24 00:14:02 +0000

    Fix JavaHL dependency calculation on Windows.
    * build.conf
      (libsvnjavahl): Add dependency on javahl_callback_javah.
    
    * build/generator/gen_win.py
      (adjust_win_depends): Apply dependency transformations on
        javahl-callback-javah.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@926876 13f79535-47bb-0310-9956-ffa450edef68

--- build/generator/gen_win.py
+++ build/generator/gen_win.py
@@ -698,7 +698,7 @@ class WinGeneratorBase(GeneratorBase):
     #
     # This section parses those dependencies and adds them to the dependency list
     # for this target.
-    if name == 'javahl-javah' or name == 'libsvnjavahl':
+    if name == 'javahl-javah' or name == 'libsvnjavahl' or name == 'javahl-callback-javah':
       for dep in re.findall('\$\(([^\)]*)_DEPS\)', target.add_deps):
         dep = dep.replace('_', '-')
         depends.extend(self.sections[dep].get_targets())
