apache/subversion

commit 3df197e0d7c538d7ed812dde4528afcf712e10c8

Add some more usefull command output than 'Performing Custom Build' on compiling java files from Visual Studio/MSBuild.
* build/generator/gen_win.py
  (get_proj_sources): Fix scope of a few variables and generate a
     description to pass to the custom build.
* build/generator/templates/vcnet_vcproj.ezt
* build/generator/templates/vcnet_vcxproj.ezt
  (*): Apply custom description.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@926613 13f79535-47bb-0310-9956-ffa450edef68

build/generator/gen_win.py
@@ -480,9 +480,10 @@ class WinGeneratorBase(GeneratorBase):
       jar_exe = os.path.join(self.jdk_path, "bin", jar_exe)
 
     if not isinstance(target, gen_base.TargetProject):
-      cbuild = None
-      ctarget = None
       for source, object, reldir in self.get_win_sources(target):
+        cbuild = None
+        ctarget = None
+        cdesc = None
         if isinstance(target, gen_base.TargetJavaHeaders):
           classes = self.path(target.classes)
           if self.junit_path is not None:
@@ -496,6 +497,7 @@ class WinGeneratorBase(GeneratorBase):
                       self.quote(headers), classname)
 
           ctarget = self.path(object.filename_win)
+          cdesc = "Generating %s" % (object.filename_win)
 
         elif isinstance(target, gen_base.TargetJavaClasses):
           classes = targetdir = self.path(target.classes)
@@ -510,6 +512,7 @@ class WinGeneratorBase(GeneratorBase):
                                             targetdir, sourcepath)))
 
           ctarget = self.path(object.filename)
+          cdesc = "Compiling %s" % (source)
 
         rsrc = self.path(str(source))
         if quote_path and '-' in rsrc:
@@ -517,6 +520,7 @@ class WinGeneratorBase(GeneratorBase):
 
         sources.append(ProjectItem(path=rsrc, reldir=reldir, user_deps=[],
                                    custom_build=cbuild, custom_target=ctarget,
+                                   custom_desc=cdesc,
                                    extension=os.path.splitext(rsrc)[1]))
 
     if isinstance(target, gen_base.TargetJavaClasses) and target.jar:

build/generator/templates/vcnet_vcproj.ezt
@@ -128,7 +128,8 @@
 				<Tool
 					Name="VCCustomBuildTool"
 					CommandLine="[sources.custom_build]"
-					AdditionalDependencies="[for sources.user_deps]&quot;[sources.user_deps]&quot;;[end]"
+[is sources.custom_desc ""][else]					Description="[sources.custom_desc]"
+[end]					AdditionalDependencies="[for sources.user_deps]&quot;[sources.user_deps]&quot;;[end]"
 					Outputs="&quot;[sources.custom_target]&quot;"/>
 			</FileConfiguration>
 [end][end][end]

build/generator/templates/vcnet_vcxproj.ezt
@@ -91,7 +91,8 @@
 [for configs][for platforms]      <Command Condition="'$(Configuration)|$(Platform)'=='[configs.name]|[platforms]'">[sources.custom_build]</Command>
       <AdditionalInputs Condition="'$(Configuration)|$(Platform)'=='[configs.name]|[platforms]'">[for sources.user_deps][sources.user_deps];[end]%(AdditionalInputs)</AdditionalInputs>
       <Outputs Condition="'$(Configuration)|$(Platform)'=='[configs.name]|[platforms]'">[sources.custom_target];%(Outputs)</Outputs>
-[end][end]    </CustomBuild>
+[is sources.custom_desc ""][else]<Message Condition="'$(Configuration)|$(Platform)'=='[configs.name]|[platforms]'">[sources.custom_desc]</Message>
+[end][end][end]    </CustomBuild>
 [else][is sources.extension ".c"]    <ClCompile Include="[sources.path]" />
 [else][is sources.extension ".cpp"]    <ClCompile Include="[sources.path]" />
 [else]    <None Include="[sources.path]" />

commit 0b5dead873ed44d3e3e0d516ccc68a7a7cc1c687

Fix the swig build on Windows (broken since r926613) and use a simpler construct to check a prefix.
* build/generator/gen_win.py
  (get_proj_sources): Add description to swig custom build operations
  (get_proj_sources): Use .startswith() instead of a range check.

git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@930042 13f79535-47bb-0310-9956-ffa450edef68

build/generator/gen_win.py
@@ -562,10 +562,13 @@ class WinGeneratorBase(GeneratorBase):
 
                 cbuild = '%s %s -o %s $(InputPath)' \
                          % (self.swig_exe, " ".join(swig_options), cout)
+                         
+                cdesc = 'Generating %s' % cout
 
                 sources.append(ProjectItem(path=isrc, reldir=None,
                                            custom_build=cbuild,
                                            custom_target=csrc,
+                                           custom_desc=cdesc,
                                            user_deps=user_deps,
                                            extension=''))
 
@@ -698,7 +701,7 @@ class WinGeneratorBase(GeneratorBase):
     #
     # This section parses those dependencies and adds them to the dependency list
     # for this target.
-    if name[0:7] == 'javahl-' or name == 'libsvnjavahl':
+    if name.startswith('javahl') or name == 'libsvnjavahl':
       for dep in re.findall('\$\(([^\)]*)_DEPS\)', target.add_deps):
         dep = dep.replace('_', '-')
         depends.extend(self.sections[dep].get_targets())