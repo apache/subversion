apache/subversion

commit 50d6b8e1ffc695d70149f6a9e72a465e7199f0b0

Remove ENTRY_MODIFY_INCOMPLETE from entry_modify2(). Remove LOG_ACCUM from file attribute setting, and queue the work items directly.
* subversion/libsvn_wc/entries.h:
  (SVN_WC__ENTRY_MODIFY_INCOMPLETE): removed. no longer used.

* subversion/libsvn_wc/entries.c:
  (): remove a couple unused includes
  (fold_entry): don't fold entry->incomplete

* subversion/libsvn_wc/log.h:
  (svn_wc__loggy_maybe_set_executable, svn_wc__loggy_maybe_set_readonly):
    remove the LOG_ACCUM parameter, add a DB parameter. the work items
    will be queued immediately, rather than placed into LOG_ACCUM

* subversion/libsvn_wc/log.c:
  (svn_wc__loggy_maybe_set_executable, svn_wc__loggy_maybe_set_readonly):
    rejigger to queue immediately, rather than return the ops in a
    LOG_ACCUM paramter.

* subversion/libsvn_wc/merge.c:
  (svn_wc__internal_merge): assert that DRY_RUN=TRUE will produce no work
    items to queue. adjust file attr call signatures.
  (svn_wc_merge4): do the write_check early. assert that internal_merge
    queues its work. simplify the ending block, simply running the queue.

* subversion/libsvn_wc/adm_ops.c:
  (svn_wc_add4): avoid using SVN_WC__ENTRY_MODIFY_INCOMPLETE and use
    svn_wc__temp_op_set_working_incomplete instead.

* subversion/libsvn_wc/update_editor.c:
  (merge_file): remove LOCK_REMOVED parameter. shift this logic "outward"
    to the close_file function. after the __internal_merge call, assert
    that it has queued all log operations.
  (close_file): avoid passing LOCK_REMOVED param. shift the "set readonly"
    logic to here.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@930162 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_wc/entries.c
@@ -37,7 +37,6 @@
 
 #include "wc.h"
 #include "adm_files.h"
-#include "adm_ops.h"
 #include "entries.h"
 #include "lock.h"
 #include "tree_conflicts.h"
@@ -47,7 +46,6 @@
 #include "svn_private_config.h"
 #include "private/svn_wc_private.h"
 #include "private/svn_sqlite.h"
-#include "private/svn_skel.h"
 
 #define MAYBE_ALLOC(x,p) ((x) ? (x) : apr_pcalloc((p), sizeof(*(x))))
 
@@ -2564,10 +2562,6 @@ fold_entry(apr_hash_t *entries,
   /* Absent state */
   if (modify_flags & SVN_WC__ENTRY_MODIFY_ABSENT)
     cur_entry->absent = entry->absent;
-
-  /* Incomplete state */
-  if (modify_flags & SVN_WC__ENTRY_MODIFY_INCOMPLETE)
-    cur_entry->incomplete = entry->incomplete;
 
   /* Text/prop modification times */
   if (modify_flags & SVN_WC__ENTRY_MODIFY_TEXT_TIME)

subversion/libsvn_wc/adm_ops.c
@@ -1584,14 +1584,15 @@ svn_wc_add4(svn_wc_context_t *wc_ctx,
          This deletes the erroneous BASE_NODE for added directories and
          adds a WORKING_NODE. */
       modify_flags |= SVN_WC__ENTRY_MODIFY_FORCE;
-      modify_flags |= SVN_WC__ENTRY_MODIFY_INCOMPLETE;
       tmp_entry.schedule = is_replace
                            ? svn_wc_schedule_replace
                            : svn_wc_schedule_add;
-      tmp_entry.incomplete = FALSE;
       SVN_ERR(svn_wc__entry_modify2(db, local_abspath, svn_node_dir,
                                     FALSE /* parent_stub */,
                                     &tmp_entry, modify_flags, pool));
+
+      SVN_ERR(svn_wc__db_temp_op_set_working_incomplete(
+                db, local_abspath, FALSE, pool));
 
       if (copyfrom_url)
         {

subversion/libsvn_wc/entries.h
@@ -109,7 +109,6 @@ svn_error_t *svn_wc__atts_to_entry(svn_wc_entry_t **new_entry,
 #define SVN_WC__ENTRY_MODIFY_CONFLICT_WRK       APR_INT64_C(0x0000000000004000)
 #define SVN_WC__ENTRY_MODIFY_PREJFILE           APR_INT64_C(0x0000000000008000)
 /* OPEN */
-#define SVN_WC__ENTRY_MODIFY_INCOMPLETE         APR_INT64_C(0x0000000000100000)
 #define SVN_WC__ENTRY_MODIFY_ABSENT             APR_INT64_C(0x0000000000200000)
 /* OPEN */
 #define SVN_WC__ENTRY_MODIFY_WORKING_SIZE       APR_INT64_C(0x0000000100000000)

subversion/libsvn_wc/log.c
@@ -1062,44 +1062,46 @@ svn_wc__loggy_move(svn_stringbuf_t **log_accum,
 }
 
 svn_error_t *
-svn_wc__loggy_maybe_set_executable(svn_stringbuf_t **log_accum,
+svn_wc__loggy_maybe_set_executable(svn_wc__db_t *db,
                                    const char *adm_abspath,
                                    const char *path,
-                                   apr_pool_t *result_pool,
                                    apr_pool_t *scratch_pool)
 {
+  svn_stringbuf_t *log_accum = NULL;
   const char *loggy_path1;
 
   SVN_ERR(loggy_path(&loggy_path1, path, adm_abspath, scratch_pool));
-  svn_xml_make_open_tag(log_accum,
-                        result_pool,
+  svn_xml_make_open_tag(&log_accum,
+                        scratch_pool,
                         svn_xml_self_closing,
                         SVN_WC__LOG_MAYBE_EXECUTABLE,
                         SVN_WC__LOG_ATTR_NAME, loggy_path1,
                         NULL);
 
-  return SVN_NO_ERROR;
+  return svn_error_return(svn_wc__wq_add_loggy(db, adm_abspath, log_accum,
+                                               scratch_pool));
 }
 
 svn_error_t *
-svn_wc__loggy_maybe_set_readonly(svn_stringbuf_t **log_accum,
+svn_wc__loggy_maybe_set_readonly(svn_wc__db_t *db,
                                  const char *adm_abspath,
                                  const char *path,
-                                 apr_pool_t *result_pool,
                                  apr_pool_t *scratch_pool)
 {
+  svn_stringbuf_t *log_accum = NULL;
   const char *loggy_path1;
 
   SVN_ERR(loggy_path(&loggy_path1, path, adm_abspath, scratch_pool));
-  svn_xml_make_open_tag(log_accum,
-                        result_pool,
+  svn_xml_make_open_tag(&log_accum,
+                        scratch_pool,
                         svn_xml_self_closing,
                         SVN_WC__LOG_MAYBE_READONLY,
                         SVN_WC__LOG_ATTR_NAME,
                         loggy_path1,
                         NULL);
 
-  return SVN_NO_ERROR;
+  return svn_error_return(svn_wc__wq_add_loggy(db, adm_abspath, log_accum,
+                                               scratch_pool));
 }
 
 svn_error_t *

subversion/libsvn_wc/log.h
@@ -192,37 +192,35 @@ svn_wc__loggy_move(svn_stringbuf_t **log_accum,
 
 
 
-/* Extend **LOG_ACCUM with log instructions to set permissions of PATH
-   to 'executable' if it has the 'executable' property set.
+/* Queue instructions to set permissions of PATH to 'executable' if it has
+   the 'executable' property set.
+
    ADM_ABSPATH is the absolute path for the admin directory for PATH.
 
    The property is tested at log run time, within this log instruction.
 
-   Allocate *LOG_ACCUM in RESULT_POOL if it is NULL. Use SCRATCH_POOL for
-   temporary allocations.
+   Use SCRATCH_POOL for temporary allocations.
 */
 svn_error_t *
-svn_wc__loggy_maybe_set_executable(svn_stringbuf_t **log_accum,
+svn_wc__loggy_maybe_set_executable(svn_wc__db_t *db,
                                    const char *adm_abspath,
                                    const char *path,
-                                   apr_pool_t *result_pool,
                                    apr_pool_t *scratch_pool);
 
-/* Extend **LOG_ACCUM with log instructions to set permissions of PATH
-   to 'readonly' if it has the 'needs-lock' property set and there is
-   no lock for the file in the working copy.
+/* Queue instructions to set permissions of PATH to 'readonly' if it has
+   the 'needs-lock' property set and there is no lock for the file in the
+   working copy.
+
    ADM_ABSPATH is the absolute path for the admin directory for PATH.
 
    The tests are made at log run time, within this log instruction.
 
-   Allocate *LOG_ACCUM in RESULT_POOL if it is NULL. Use SCRATCH_POOL for
-   temporary allocations.
+   Use SCRATCH_POOL for temporary allocations.
 */
 svn_error_t *
-svn_wc__loggy_maybe_set_readonly(svn_stringbuf_t **log_accum,
+svn_wc__loggy_maybe_set_readonly(svn_wc__db_t *db,
                                  const char *adm_abspath,
                                  const char *path,
-                                 apr_pool_t *result_pool,
                                  apr_pool_t *scratch_pool);
 
 

commit 5899c7c4378f72fd8259a8a2f614fa9dfd4256d7

Fix the inheritance handling for entry->copyfrom_*. The mapping code was not properly considering the copyfrom data between "this" node and the ancestor nodes.
This fixes merge_tests 34 and 134 (broken by r930162), but proceeds to
break diff_tests 41 and merge_tests 8. Two steps forward...

* subversion/libsvn_wc/entries.c:
  (read_entries_new): rework the logic that analyzes copyfrom information
    on "this" node and the ancestor nodes. it was not (properly)
    considering that an ancestor may imply "this" node is part of a copy.

* subversion/tests/cmdline/switch_tests.py:
  (tree_conflicts_on_switch_2_1): note that we fail because an (A)dd is
    incorrectly reported as a (M)odify, due to issues in wc_db.
  (test_list): mark the above as an XFail

* subversion/tests/cmdline/update_tests.py:
  (tree_conflicts_on_update_2_1, tree_conflict_uc2_schedule_re_add): leave
    comments about the breakage due to wc_db issues.
  (test_list): mark the above two tests as XFail


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@931449 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_wc/entries.c
@@ -727,6 +727,8 @@ read_entries_new(apr_hash_t **result_entries,
                || status == svn_wc__db_status_obstructed_add)
         {
           svn_wc__db_status_t work_status;
+          const char *op_root_abspath;
+          const char *scanned_original_relpath;
           svn_revnum_t original_revision;
 
           /* For child nodes, pick up the parent's revision.  */
@@ -817,17 +819,19 @@ read_entries_new(apr_hash_t **result_entries,
             }
 
           SVN_ERR(svn_wc__db_scan_addition(&work_status,
-                                           NULL,
+                                           &op_root_abspath,
                                            &repos_relpath,
                                            &entry->repos,
                                            &entry->uuid,
-                                           NULL, NULL, NULL, &original_revision,
+                                           &scanned_original_relpath,
+                                           NULL, NULL, /* original_root|uuid */
+                                           &original_revision,
                                            db,
                                            entry_abspath,
                                            result_pool, iterpool));
-
+          SVN_DBG(("entry: work_status=%d  cmt_rev=%ld  relpath='%s'\n", work_status, entry->cmt_rev, original_repos_relpath));
           if (!SVN_IS_VALID_REVNUM(entry->cmt_rev)
-              && original_repos_relpath == NULL)
+              && scanned_original_relpath == NULL)
             {
               /* There is NOT a last-changed revision (last-changed date and
                  author may be unknown, but we can always check the rev).
@@ -850,106 +854,146 @@ read_entries_new(apr_hash_t **result_entries,
 
               /* Copied nodes need to mirror their copyfrom_rev, if they
                  don't have a revision of their own already. */
-              if (!SVN_IS_VALID_REVNUM(entry->revision))
+              if (!SVN_IS_VALID_REVNUM(entry->revision)
+                  || entry->revision == 0 /* added */)
                 entry->revision = original_revision;
             }
 
           /* Does this node have copyfrom_* information?  */
-          if (original_repos_relpath != NULL)
+          if (scanned_original_relpath != NULL)
             {
-              const char *parent_abspath;
-              svn_boolean_t set_copyfrom = TRUE;
-              svn_error_t *err;
-              const char *op_root_abspath;
-              const char *parent_repos_relpath;
-              const char *parent_root_url;
+              svn_boolean_t is_copied_child;
+              svn_boolean_t is_mixed_rev = FALSE;
 
               SVN_ERR_ASSERT(work_status == svn_wc__db_status_copied);
 
-              /* When we insert entries into the database, we will construct
-                 additional copyfrom records for mixed-revision copies. The
-                 old entries would simply record the different revision in
-                 the entry->revision field. That is not available within
-                 wc-ng, so additional copies are made (see the logic inside
-                 write_entry()). However, when reading these back *out* of
-                 the database, the additional copies look like new "Added"
-                 nodes rather than a simple mixed-rev working copy.
+              /* If this node inherits copyfrom information from an
+                 ancestor node, then it must be a copied child.  */
+              is_copied_child = (original_repos_relpath == NULL);
 
-                 That would be a behavior change if we did not compensate.
-                 If there is copyfrom information for this node, then the
-                 code below looks at the parent to detect if it *also* has
-                 copyfrom information, and if the copyfrom_url would align
-                 properly. If it *does*, then we omit storing copyfrom_url
-                 and copyfrom_rev (ie. inherit the copyfrom info like a
-                 normal child), and update entry->revision with the
-                 copyfrom_rev in order to (re)create the mixed-rev copied
-                 subtree that was originally presented for storage.  */
-
-              /* Get the copyfrom information from our parent.
-
-                 Note that the parent could be added/copied/moved-here. There
-                 is no way for it to be deleted/moved-away and have *this*
-                 node appear as copied.  */
-              parent_abspath = svn_dirent_dirname(entry_abspath, iterpool);
-              err = svn_wc__db_scan_addition(NULL,
-                                             &op_root_abspath,
-                                             NULL, NULL, NULL,
-                                             &parent_repos_relpath,
-                                             &parent_root_url,
-                                             NULL, NULL,
-                                             db,
-                                             parent_abspath,
-                                             iterpool, iterpool);
-              if (err)
+              /* If this node has copyfrom information on it, then it may
+                 be an actual copy-root, or it could be participating in
+                 a mixed-revision copied tree. So if we don't already know
+                 this is a copied child, then we need to look for this
+                 mixed-revision situation.  */
+              if (!is_copied_child)
                 {
-                  if (err->apr_err != SVN_ERR_WC_PATH_NOT_FOUND)
-                    return svn_error_return(err);
-                  svn_error_clear(err);
-                }
-              else if (parent_root_url != NULL
-                       && strcmp(original_root_url, parent_root_url) == 0)
-                {
-                  const char *relpath_to_entry = svn_dirent_is_child(
-                    op_root_abspath, entry_abspath, NULL);
-                  const char *entry_repos_relpath = svn_relpath_join(
-                    parent_repos_relpath, relpath_to_entry, iterpool);
+                  const char *parent_abspath;
+                  svn_error_t *err;
+                  const char *parent_repos_relpath;
+                  const char *parent_root_url;
 
-                  /* The copyfrom repos roots matched.
+                  /* When we insert entries into the database, we will
+                     construct additional copyfrom records for mixed-revision
+                     copies. The old entries would simply record the different
+                     revision in the entry->revision field. That is not
+                     available within wc-ng, so additional copies are made
+                     (see the logic inside write_entry()). However, when
+                     reading these back *out* of the database, the additional
+                     copies look like new "Added" nodes rather than a simple
+                     mixed-rev working copy.
 
-                     Now we look to see if the copyfrom path of the parent
-                     would align with our own path. If so, then it means
-                     this copyfrom was spontaneously created and inserted
-                     for mixed-rev purposes and can be eliminated without
-                     changing the semantics of a mixed-rev copied subtree.
+                     That would be a behavior change if we did not compensate.
+                     If there is copyfrom information for this node, then the
+                     code below looks at the parent to detect if it *also* has
+                     copyfrom information, and if the copyfrom_url would align
+                     properly. If it *does*, then we omit storing copyfrom_url
+                     and copyfrom_rev (ie. inherit the copyfrom info like a
+                     normal child), and update entry->revision with the
+                     copyfrom_rev in order to (re)create the mixed-rev copied
+                     subtree that was originally presented for storage.  */
 
-                     See notes/api-errata/wc003.txt for some additional
-                     detail, and potential issues.  */
-                  if (strcmp(entry_repos_relpath, original_repos_relpath) == 0)
+                  /* Get the copyfrom information from our parent.
+
+                     Note that the parent could be added/copied/moved-here.
+                     There is no way for it to be deleted/moved-away and
+                     have *this* node appear as copied.  */
+                  parent_abspath = svn_dirent_dirname(entry_abspath, iterpool);
+                  err = svn_wc__db_scan_addition(NULL,
+                                                 &op_root_abspath,
+                                                 NULL, NULL, NULL,
+                                                 &parent_repos_relpath,
+                                                 &parent_root_url,
+                                                 NULL, NULL,
+                                                 db,
+                                                 parent_abspath,
+                                                 iterpool, iterpool);
+                  if (err)
                     {
-                      /* Don't set the copyfrom_url and clear out the
-                         copyfrom_rev. Thus, this node becomes a child
-                         of a copied subtree (rather than its own root).  */
-                      set_copyfrom = FALSE;
-                      entry->copyfrom_rev = SVN_INVALID_REVNUM;
+                      if (err->apr_err != SVN_ERR_WC_PATH_NOT_FOUND)
+                        return svn_error_return(err);
+                      svn_error_clear(err);
+                    }
+                  else if (parent_root_url != NULL
+                           && strcmp(original_root_url, parent_root_url) == 0)
+                    {
+                      const char *relpath_to_entry = svn_dirent_is_child(
+                        op_root_abspath, entry_abspath, NULL);
+                      const char *entry_repos_relpath = svn_relpath_join(
+                        parent_repos_relpath, relpath_to_entry, iterpool);
 
-                      /* Children in a copied subtree are schedule normal
-                         since we don't plan to actually *do* anything with
-                         them. Their operation is implied by ancestors.  */
-                      entry->schedule = svn_wc_schedule_normal;
+                      /* The copyfrom repos roots matched.
 
-                      /* And *finally* we turn this entry into the mixed
-                         revision node that it was intended to be. This
-                         node's revision is taken from the copyfrom record
-                         that we spontaneously constructed.  */
-                      entry->revision = original_revision;
+                         Now we look to see if the copyfrom path of the parent
+                         would align with our own path. If so, then it means
+                         this copyfrom was spontaneously created and inserted
+                         for mixed-rev purposes and can be eliminated without
+                         changing the semantics of a mixed-rev copied subtree.
+
+                         See notes/api-errata/wc003.txt for some additional
+                         detail, and potential issues.  */
+                      if (strcmp(entry_repos_relpath,
+                                 original_repos_relpath) == 0)
+                        {
+                          is_copied_child = TRUE;
+                          is_mixed_rev = TRUE;
+                        }
                     }
                 }
 
-              if (set_copyfrom)
-                entry->copyfrom_url =
-                  svn_path_url_add_component2(original_root_url,
-                                              original_repos_relpath,
-                                              result_pool);
+              if (is_copied_child)
+                {
+                  /* We won't be settig the  copyfrom_url, yet need to
+                     clear out the copyfrom_rev. Thus, this node becomes a
+                     child of a copied subtree (rather than its own root).  */
+                  entry->copyfrom_rev = SVN_INVALID_REVNUM;
+
+                  /* Children in a copied subtree are schedule normal
+                     since we don't plan to actually *do* anything with
+                     them. Their operation is implied by ancestors.  */
+                  entry->schedule = svn_wc_schedule_normal;
+
+                  /* And *finally* we turn this entry into the mixed
+                     revision node that it was intended to be. This
+                     node's revision is taken from the copyfrom record
+                     that we spontaneously constructed.  */
+                  if (is_mixed_rev)
+                    entry->revision = original_revision;
+                }
+              else if (original_repos_relpath != NULL)
+                {
+                  entry->copyfrom_url =
+                    svn_path_url_add_component2(original_root_url,
+                                                original_repos_relpath,
+                                                result_pool);
+                }
+              else
+                {
+                  /* NOTE: if original_repos_relpath == NULL, then the
+                     second call to scan_addition() will not have occurred.
+                     Thus, this use of OP_ROOT_ABSPATH still contains the
+                     original value where we fetched a value for
+                     SCANNED_REPOS_RELPATH.  */
+                  const char *relpath_to_entry = svn_dirent_is_child(
+                    op_root_abspath, entry_abspath, NULL);
+                  const char *entry_repos_relpath = svn_relpath_join(
+                    scanned_original_relpath, relpath_to_entry, iterpool);
+
+                  entry->copyfrom_url =
+                    svn_path_url_add_component2(original_root_url,
+                                                entry_repos_relpath,
+                                                result_pool);
+                }
             }
         }
       else if (status == svn_wc__db_status_not_present)

subversion/tests/cmdline/switch_tests.py
@@ -2545,6 +2545,11 @@ def tree_conflicts_on_switch_2_1(sbox):
     },
   }
 
+  ### D/D1/delta is locally-added during leaf_edit. when tree_del executes,
+  ### it will delete D/D1, and the switch reschedules local D/D1 for
+  ### local-copy from its original revision. however, right now, we cannot
+  ### denote that delta is a local-add rather than a child of that D/D1 copy.
+  ### thus, it appears in the status output as a (M)odified child.
   svntest.actions.deep_trees_run_tests_scheme_for_switch(sbox,
     [ DeepTreesTestCase("local_leaf_edit_incoming_tree_del",
                         leaf_edit,
@@ -2939,7 +2944,7 @@ test_list = [ None,
               tolerate_local_mods,
               tree_conflicts_on_switch_1_1,
               tree_conflicts_on_switch_1_2,
-              tree_conflicts_on_switch_2_1,
+              XFail(tree_conflicts_on_switch_2_1),
               tree_conflicts_on_switch_2_2,
               tree_conflicts_on_switch_3,
               single_file_relocate,

subversion/tests/cmdline/update_tests.py
@@ -4544,6 +4544,11 @@ def tree_conflicts_on_update_2_1(sbox):
     },
   }
 
+  ### D/D1/delta is locally-added during leaf_edit. when tree_del executes,
+  ### it will delete D/D1, and the update reschedules local D/D1 for
+  ### local-copy from its original revision. however, right now, we cannot
+  ### denote that delta is a local-add rather than a child of that D/D1 copy.
+  ### thus, it appears in the status output as a (M)odified child.
   svntest.actions.deep_trees_run_tests_scheme_for_update(sbox,
     [ DeepTreesTestCase("local_leaf_edit_incoming_tree_del",
                         leaf_edit,
@@ -5152,6 +5157,10 @@ def tree_conflict_uc2_schedule_re_add(sbox):
 
   # The status of the new and old scenarios should be identical...
   expected_status = get_status(wc2)
+  ### The following fails, as of Apr 6, 2010. The problem is that A/new_file
+  ### has been *added* within a copy, yet the wc_db datastore cannot
+  ### differentiate this from a copied-child. As a result, new_file is
+  ### reported as a (M)odified node, rather than (A)dded.
   svntest.actions.run_and_verify_status(wc2, expected_status)
 
   # ...except for the revision of the root of the WC and iota, because
@@ -5516,13 +5525,13 @@ test_list = [ None,
               restarted_update_should_delete_dir_prop,
               tree_conflicts_on_update_1_1,
               tree_conflicts_on_update_1_2,
-              tree_conflicts_on_update_2_1,
+              XFail(tree_conflicts_on_update_2_1),
               tree_conflicts_on_update_2_2,
               XFail(tree_conflicts_on_update_2_3),
               tree_conflicts_on_update_3,
               update_moves_and_modifies_an_edited_file,
               tree_conflict_uc1_update_deleted_tree,
-              tree_conflict_uc2_schedule_re_add,
+              XFail(tree_conflict_uc2_schedule_re_add),
               set_deep_depth_on_target_with_shallow_children,
               update_wc_of_dir_to_rev_not_containing_this_dir,
               XFail(update_deleted_locked_files),