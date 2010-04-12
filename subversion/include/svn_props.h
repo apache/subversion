fe8063f7369ffeaefbd9fa9c968e092730062ef1
Greg Stein <gstein@apache.org>
2010-04-12 02:12:17 +0000

    Remove some sludge from svn_props.h
    * subversion/include/svn_props.h:
      (svn_categorize_props): remove crappy suggestion. we're not going to
        create more confusingly kinds of props.
      (...): remove extra license header
      (SVN_PROP_WC_PREFIX): use example.com rather than a (my) private domain
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@933076 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_props.h
+++ subversion/include/svn_props.h
@@ -189,11 +189,6 @@ svn_prop_needs_translation(const char *prop_name);
  * are uninterested.  If no props exist in a certain category, and the
  * property list argument for that category is non-NULL, then that
  * array will come back with <tt>->nelts == 0</tt>.
- *
- * ### Hmmm, maybe a better future interface is to return an array of
- *     arrays, where the index into the array represents the index
- *     into @c svn_prop_kind_t.  That way we can add more prop kinds
- *     in the future without changing this interface...
  */
 svn_error_t *
 svn_categorize_props(const apr_array_header_t *proplist,
@@ -349,25 +344,6 @@ svn_prop_name_is_valid(const char *prop_name);
 
 /** Meta-data properties.
  *
- * ====================================================================
- *    Licensed to the Apache Software Foundation (ASF) under one
- *    or more contributor license agreements.  See the NOTICE file
- *    distributed with this work for additional information
- *    regarding copyright ownership.  The ASF licenses this file
- *    to you under the Apache License, Version 2.0 (the
- *    "License"); you may not use this file except in compliance
- *    with the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing,
- *    software distributed under the License is distributed on an
- *    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *    KIND, either express or implied.  See the License for the
- *    specific language governing permissions and limitations
- *    under the License.
- * ====================================================================
- *
  * The following properties are used for storing meta-data about
  * individual entries in the meta-data branches of subversion,
  * see issue #1256 or browseable at
@@ -417,12 +393,12 @@ svn_prop_name_is_valid(const char *prop_name);
 
 /** The property name *prefix* that makes a property a "WC property".
  *
- * For example, WebDAV RA implementations might store a versioned-resource url as a WC
- * prop like this:
+ * For example, WebDAV RA implementations might store a versioned-resource
+ * url as a WC prop like this:
  *
  * @verbatim
       name = svn:wc:dav_url
-      val  = http://www.lyra.org/repos/452348/e.289 @endverbatim
+      val  = http://www.example.com/repos/452348/e.289 @endverbatim
  *
  * The client will try to protect WC props by warning users against
  * changing them.  The client will also send them back to the RA layer
