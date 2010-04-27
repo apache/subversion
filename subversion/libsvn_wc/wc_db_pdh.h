8914254440968299953f2f68b173fb9b44257764
Hyrum Kurt Wright <hwright@apache.org>
2010-04-27 14:51:33 +0000

    Add a new header file, and move the a few type declarations from wc_db.c to that header file.  Also, use from preprocessor magic to ensure that other files do not use these data structures.
    * subversion/libsvn_wc/wc_db.c
      Include the require define, and the new header file.
      (svn_wc__db_t, svn_wc__db_wcroot_t, svn_wc__db_pdh_t): Move from here...
    
    * subversion/libsvn_wc/wc_db_pdh.h:
      (svn_wc__db_t, svn_wc__db_wcroot_t, svn_wc__db_pdh_t): ...to here.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@938483 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subversion/libsvn_wc/wc_db_pdh.h
@@ -0,0 +1,109 @@
+/**
+ * @copyright
+ * ====================================================================
+ *    Licensed to the Apache Software Foundation (ASF) under one
+ *    or more contributor license agreements.  See the NOTICE file
+ *    distributed with this work for additional information
+ *    regarding copyright ownership.  The ASF licenses this file
+ *    to you under the Apache License, Version 2.0 (the
+ *    "License"); you may not use this file except in compliance
+ *    with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing,
+ *    software distributed under the License is distributed on an
+ *    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *    KIND, either express or implied.  See the License for the
+ *    specific language governing permissions and limitations
+ *    under the License.
+ * ====================================================================
+ * @endcopyright
+ */
+
+/* This file is not for general consumption; it should only be used by
+   wc_db.c. */
+#ifndef SVN_WC__I_AM_WC_DB
+#error "You should not be using these data structures directly"
+#endif /* SVN_WC__I_AM_WC_DB */
+
+#ifndef WC_DB_PDH_H
+#define WC_DB_PDH_H
+
+
+
+struct svn_wc__db_t {
+  /* What's the appropriate mode for this datastore? */
+  svn_wc__db_openmode_t mode;
+
+  /* We need the config whenever we run into a new WC directory, in order
+     to figure out where we should look for the corresponding datastore. */
+  svn_config_t *config;
+
+  /* Should we attempt to automatically upgrade the database when it is
+     opened, and found to be not-current?  */
+  svn_boolean_t auto_upgrade;
+
+  /* Should we ensure the WORK_QUEUE is empty when a WCROOT is opened?  */
+  svn_boolean_t enforce_empty_wq;
+
+  /* Map a given working copy directory to its relevant data.
+     const char *local_abspath -> svn_wc__db_pdh_t *pdh  */
+  apr_hash_t *dir_data;
+
+  /* As we grow the state of this DB, allocate that state here. */
+  apr_pool_t *state_pool;
+};
+
+/** Hold information about a WCROOT.
+ *
+ * This structure is referenced by all per-directory handles underneath it.
+ */
+typedef struct {
+  /* Location of this wcroot in the filesystem.  */
+  const char *abspath;
+
+  /* The SQLite database containing the metadata for everything in
+     this wcroot.  */
+  svn_sqlite__db_t *sdb;
+
+  /* The WCROOT.id for this directory (and all its children).  */
+  apr_int64_t wc_id;
+
+  /* The format of this wcroot's metadata storage (see wc.h). If the
+     format has not (yet) been determined, this will be UNKNOWN_FORMAT.  */
+  int format;
+
+} svn_wc__db_wcroot_t;
+
+/**  Pristine Directory Handle
+ *
+ * This structure records all the information that we need to deal with
+ * a given working copy directory.
+ */
+typedef struct svn_wc__db_pdh_t {
+  /* This (versioned) working copy directory is obstructing what *should*
+     be a file in the parent directory (according to its metadata).
+
+     Note: this PDH should probably be ignored (or not created).
+
+     ### obstruction is only possible with per-dir wc.db databases.  */
+  svn_boolean_t obstructed_file;
+
+  /* The absolute path to this working copy directory. */
+  const char *local_abspath;
+
+  /* What wcroot does this directory belong to?  */
+  svn_wc__db_wcroot_t *wcroot;
+
+  /* The parent directory's per-dir information. */
+  struct svn_wc__db_pdh_t *parent;
+
+  /* Whether this process owns a write-lock on this directory. */
+  svn_boolean_t locked;
+
+  /* Hold onto the old-style access baton that corresponds to this PDH.  */
+  svn_wc_adm_access_t *adm_access;
+} svn_wc__db_pdh_t;
+
+#endif /* WC_DB_PDH_H */
