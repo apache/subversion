apache/subversion

commit d8fdebb5331e0846769826e1921aab954af006d7

Change svn_wc_transmit_text_deltas3() so that it can store the new pristine text in the pristine text store.  In this version, it can store it the old way or the new way, as requested by the caller.
* subversion/include/svn_wc.h
  (svn_wc_transmit_text_deltas3): Document the new behaviour.

* subversion/libsvn_wc/adm_crawler.c
  (svn_wc__internal_transmit_text_deltas): If the SHA-1 checksum is
    requested, store a copy of the new pristine text in the pristine text
    store.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@938107 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_wc/adm_crawler.c
@@ -1060,6 +1060,7 @@ svn_wc__internal_transmit_text_deltas(const char **tempfile,
   svn_checksum_t *verify_checksum = NULL;  /* calc'd MD5 of BASE_STREAM */
   svn_checksum_t *local_md5_checksum;  /* calc'd MD5 of LOCAL_STREAM */
   svn_checksum_t *local_sha1_checksum;  /* calc'd SHA1 of LOCAL_STREAM */
+  const char *new_pristine_tmp_abspath;
   svn_error_t *err;
   svn_stream_t *base_stream;  /* delta source */
   svn_stream_t *local_stream;  /* delta target: LOCAL_ABSPATH transl. to NF */
@@ -1092,6 +1093,18 @@ svn_wc__internal_transmit_text_deltas(const char **tempfile,
          Note that the new text base file will be closed when the new stream
          is closed. */
       local_stream = copying_stream(local_stream, tempstream, scratch_pool);
+    }
+  if (new_text_base_sha1_checksum)
+    {
+      svn_stream_t *new_pristine_stream;
+
+      SVN_ERR(svn_wc__open_writable_base(&new_pristine_stream,
+                                         &new_pristine_tmp_abspath,
+                                         NULL, &local_sha1_checksum,
+                                         db, local_abspath,
+                                         scratch_pool, scratch_pool));
+      local_stream = copying_stream(local_stream, new_pristine_stream,
+                                    scratch_pool);
     }
 
   /* Set BASE_STREAM to a stream providing the base (source) content for the
@@ -1192,12 +1205,6 @@ svn_wc__internal_transmit_text_deltas(const char **tempfile,
                                     &handler, &wh_baton));
   }
 
-  if (new_text_base_sha1_checksum)
-    local_stream = svn_stream_checksummed2(local_stream,
-                                           &local_sha1_checksum,
-                                           NULL, svn_checksum_sha1, TRUE,
-                                           scratch_pool);
-
   /* Run diff processing, throwing windows at the handler. */
   err = svn_txdelta_run(base_stream, local_stream,
                         handler, wh_baton,
@@ -1264,8 +1271,14 @@ svn_wc__internal_transmit_text_deltas(const char **tempfile,
     *new_text_base_md5_checksum = svn_checksum_dup(local_md5_checksum,
                                                    result_pool);
   if (new_text_base_sha1_checksum)
-    *new_text_base_sha1_checksum = svn_checksum_dup(local_sha1_checksum,
-                                                    result_pool);
+    {
+      SVN_ERR(svn_wc__db_pristine_install(db, new_pristine_tmp_abspath,
+                                          local_sha1_checksum,
+                                          local_md5_checksum,
+                                          scratch_pool));
+      *new_text_base_sha1_checksum = svn_checksum_dup(local_sha1_checksum,
+                                                      result_pool);
+    }
 
   /* Close the file baton, and get outta here. */
   return editor->close_file(file_baton,

subversion/include/svn_wc.h
@@ -7151,7 +7151,10 @@ svn_wc_translated_stream(svn_stream_t **stream,
  * If @a new_text_base_md5_checksum is non-NULL, set
  * @a *new_text_base_md5_checksum to the MD5 checksum of (@a local_abspath
  * translated to repository-normal form), allocated in @a result_pool.
- * Similarly for @a new_text_base_sha1_checksum.
+ *
+ * If @a new_text_base_sha1_checksum in non-NULL, store a copy of (@a
+ * local_abspath translated to repository-normal form) in the pristine text
+ * store, and set @a *new_text_base_sha1_checksum to its SHA-1 checksum.
  *
  * If @a fulltext, send the untranslated copy of @a local_abspath through
  * @a editor as full-text; else send it as svndiff against the current text

commit d8d0597710c324199387e0c56f80d5bd67db364f

* subversion/libsvn_wc/adm_crawler.c   (svn_wc__internal_transmit_text_deltas): Wrap the attempt to install     a pristine text in subversion/libsvn_wc/update_editor.cifdef because the subdirectory it goes in     hasn't been created and because there's no code yet to use it or     to ever delete it. This fixes breakage caused by r938107.
git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@938436 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_wc/adm_crawler.c
@@ -1272,10 +1272,12 @@ svn_wc__internal_transmit_text_deltas(const char **tempfile,
                                                    result_pool);
   if (new_text_base_sha1_checksum)
     {
+#ifdef SVN_EXPERIMENTAL_PRISTINE
       SVN_ERR(svn_wc__db_pristine_install(db, new_pristine_tmp_abspath,
                                           local_sha1_checksum,
                                           local_md5_checksum,
                                           scratch_pool));
+#endif
       *new_text_base_sha1_checksum = svn_checksum_dup(local_sha1_checksum,
                                                       result_pool);
     }