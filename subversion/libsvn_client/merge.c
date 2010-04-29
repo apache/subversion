apache/subversion

commit 89c94dbb3f75d9513ba779769213c74fbdcf0fc8

Another entry_t down.
* subversion/libsvn_client/merge.c
  (obstructed_or_missing):
    Use wc-ng API instead of svn_wc__get_entry_versioned(), and much simplify
    this function in the process. Untangle, rinse and dry.
  (node_kind_working, node_kind_on_disk):
    Remove these ghastly two functions and incorporate into
    obstructed_or_missing(), the way it should always have been.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@939150 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_client/merge.c
@@ -367,57 +367,11 @@ is_path_conflicted_by_merge(merge_cmd_baton_t *merge_b)
           apr_hash_count(merge_b->conflicted_paths) > 0);
 }
 
-/* Return the node kind of the working version of local path PATH,
- * according to the WC metadata in ENTRY. If ENTRY is null, assume the node
- * is unversioned and so set the kind to 'none'.
- *
- * However, if this is a dry run, set *NODE_KIND to 'none' if the node would
- * already have been deleted by the merge if this were not a dry run. Use
- * MERGE_B to determine the dry-run details. */
-static svn_node_kind_t
-node_kind_working(const char *path,
-                  const merge_cmd_baton_t *merge_b,
-                  const svn_wc_entry_t *entry)
-{
-  if (!entry
-      || (entry->schedule == svn_wc_schedule_delete)
-      || (merge_b->dry_run && dry_run_deleted_p(merge_b, path))
-      || (entry->deleted && entry->schedule != svn_wc_schedule_add))
-    return svn_node_none;
-  else
-    return entry->kind;
-}
-
-/* Return the node kind that is found on disk at local path PATH.
- *
- * However, if this is a dry run, set *NODE_KIND to 'none' if the node would
- * already have been deleted by the merge if this were not a dry run. Use
- * MERGE_B to determine the dry-run details. */
-static svn_node_kind_t
-node_kind_on_disk(const char *path,
-                  const merge_cmd_baton_t *merge_b,
-                  apr_pool_t *pool)
-{
-  svn_error_t *err;
-  svn_node_kind_t node_kind;
-
-  err = svn_io_check_path(path, &node_kind, pool);
-  if (err)
-    {
-      svn_error_clear(err);
-      return svn_node_unknown;
-    }
-  else if (dry_run_deleted_p(merge_b, path))
-    return svn_node_none;
-  else
-    return node_kind;
-}
-
-/* Return a state indicating whether the WC metadata in ENTRY matches the
- * node kind on disk of the local path PATH. If ENTRY is null, assume the
- * node is unversioned. In the case of a dry-run merge, use the disk node
- * kind that would exist if it were not a dry run. Use MERGE_B to determine
- * the dry-run details.
+/* Return a state indicating whether the WC metadata matches the
+ * node kind on disk of the local path PATH.
+ * Use MERGE_B to determine the dry-run details; particularly, if a dry run
+ * noted that it deleted this path, assume matching node kinds (as if both
+ * kinds were svn_node_none).
  *
  *   - Return svn_wc_notify_state_inapplicable if the node kind matches.
  *   - Return 'obstructed' if there is a node on disk where none or a
@@ -430,55 +384,52 @@ obstructed_or_missing(const char *path,
                       apr_pool_t *pool)
 {
   svn_error_t *err;
-  const svn_wc_entry_t *entry = NULL;
-  svn_node_kind_t kind_expected, kind_on_disk, wc_kind;
+  svn_node_kind_t kind_expected, kind_on_disk;
   const char *local_abspath;
 
+  /* In a dry run, make as if nodes "deleted" by the dry run appear so. */
+  if (merge_b->dry_run && dry_run_deleted_p(merge_b, path))
+    return svn_wc_notify_state_inapplicable;
+
+  /* Since this function returns no svn_error_t, we make all errors look like
+   * no node found in the wc. */
   err = svn_dirent_get_absolute(&local_abspath, path, pool);
 
   if (!err)
-    err = svn_wc_read_kind(&wc_kind, merge_b->ctx->wc_ctx, local_abspath,
-                           FALSE, pool);
+    err = svn_wc_read_kind(&kind_expected, merge_b->ctx->wc_ctx,
+                           local_abspath, FALSE, pool);
 
   if (err)
     {
-      wc_kind = svn_node_none;
       svn_error_clear(err);
-      entry = NULL;
+      kind_expected = svn_node_none;
     }
-  else
+
+  /* If a node is deleted, we will still get its kind from the working copy.
+   * But to compare with disk state, we want to consider deleted nodes as
+   * svn_node_none instead of their original kind.
+   * ### Not necessary with central DB:
+   * Because deleted directories are expected to remain on disk until commit
+   * to keep the metadata available, we only do this for files, not dirs. */
+  if (kind_expected == svn_node_file)
     {
-      err = svn_wc__get_entry_versioned(&entry, merge_b->ctx->wc_ctx,
-                                        local_abspath, svn_node_unknown,
-                                        TRUE, FALSE, pool, pool);
-
+      svn_boolean_t is_deleted;
+      err = svn_wc__node_is_status_deleted(&is_deleted,
+                                           merge_b->ctx->wc_ctx,
+                                           local_abspath,
+                                           pool);
       if (err)
-        {
-          svn_error_clear(err);
-          entry = NULL;
-        }
+        svn_error_clear(err);
+      else if (is_deleted)
+        kind_expected = svn_node_none;
     }
 
-  if ((wc_kind == svn_node_dir || wc_kind == svn_node_file)
-      && !entry)
-    return svn_wc_notify_state_missing;
-
-  /* ### This check (and most of this function)
-         can be removed after we move to one DB */
-  /* svn_wc__get_entry_versioned ignores node kind errors, so check if we
-     didn't get the parent stub, instead of the real thing */
-  if (entry && entry->kind == svn_node_dir && *entry->name != '\0')
-    return svn_wc_notify_state_missing; /* Only found parent entry */
-
-  kind_expected = node_kind_working(path, merge_b, entry);
-  kind_on_disk = node_kind_on_disk(path, merge_b, pool);
-
-  /* If it's a sched-delete directory, change the expected kind to "dir"
-   * because the directory should not yet have gone from disk. */
-  if (entry && entry->kind == svn_node_dir
-      && entry->schedule == svn_wc_schedule_delete
-      && kind_on_disk == svn_node_dir)
-    kind_expected = svn_node_dir;
+  err = svn_io_check_path(path, &kind_on_disk, pool);
+  if (err)
+    {
+      svn_error_clear(err);
+      kind_on_disk = svn_node_unknown;
+    }
 
   if (kind_expected == kind_on_disk)
     return svn_wc_notify_state_inapplicable;

commit 295cc3877db31e7c584d27ab59c1bb39ee524b14

Fix merge behaviour for absent nodes, broken by r939150. Now, merge_authz_tests.py 1 passes again. Also tweak a path to local_abspath.
* subversion/libsvn_client/merge.c
  (obstructed_or_missing):
    Return absent nodes as 'missing'. Tweak: rather use LOCAL_ABSPATH to
    check the file system node kind. Also tweak comment.

* subversion/tests/cmdline/merge_authz_tests.py
  (mergeinfo_and_skipped_paths):
    Unmark XFAIL. Above changes fix the missing "skipped" notification.



git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@939474 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_client/merge.c
@@ -376,7 +376,8 @@ is_path_conflicted_by_merge(merge_cmd_baton_t *merge_b)
  *   - Return svn_wc_notify_state_inapplicable if the node kind matches.
  *   - Return 'obstructed' if there is a node on disk where none or a
  *     different kind is expected, or if the disk node cannot be read.
- *   - Return 'missing' if there is no node on disk but one is expected. */
+ *   - Return 'missing' if there is no node on disk but one is expected.
+ *     Also return 'missing' for absent nodes (not here due to authz).*/
 static svn_wc_notify_state_t
 obstructed_or_missing(const char *path,
                       const char *local_dir_abspath,
@@ -405,6 +406,20 @@ obstructed_or_missing(const char *path,
       kind_expected = svn_node_none;
     }
 
+  /* Report absent nodes as 'missing'. First of all, they count as 'hidden',
+   * and since we pass show_hidden == FALSE above, they will show up as
+   * 'none' here. */
+  if (kind_expected == svn_node_none)
+    {
+      svn_boolean_t is_absent;
+      err = svn_wc__node_is_status_absent(&is_absent, merge_b->ctx->wc_ctx,
+                                          local_abspath, pool);
+      if (err)
+        svn_error_clear(err);
+      else if (is_absent)
+        return svn_wc_notify_state_missing;
+    }
+
   /* If a node is deleted, we will still get its kind from the working copy.
    * But to compare with disk state, we want to consider deleted nodes as
    * svn_node_none instead of their original kind.
@@ -424,7 +439,7 @@ obstructed_or_missing(const char *path,
         kind_expected = svn_node_none;
     }
 
-  err = svn_io_check_path(path, &kind_on_disk, pool);
+  err = svn_io_check_path(local_abspath, &kind_on_disk, pool);
   if (err)
     {
       svn_error_clear(err);

subversion/tests/cmdline/merge_authz_tests.py
@@ -583,9 +583,7 @@ def merge_fails_if_subtree_is_deleted_on_src(sbox):
 
 # list all tests here, starting with None:
 test_list = [ None,
-              # Marked XFAIL because svn fails to emit a 'skipped' notice
-              # on the file 'psi'.
-              SkipUnless(Skip(XFail(mergeinfo_and_skipped_paths),
+              SkipUnless(Skip(mergeinfo_and_skipped_paths,
                               svntest.main.is_ra_type_file),
                          svntest.main.server_has_mergeinfo),
               SkipUnless(merge_fails_if_subtree_is_deleted_on_src,