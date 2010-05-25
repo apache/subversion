1def79fb196d579bb4789646978418e22ff6b185
Hyrum Kurt Wright <hwright@apache.org>
2010-05-25 19:19:50 +0000

    Followup to r948165 by initializing a variable.
    * subversion/libsvn_client/mergeinfo.c
      (location_from_path_and_rev): Make sure the local_abspath gets set.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@948183 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/mergeinfo.c
+++ subversion/libsvn_client/mergeinfo.c
@@ -1597,7 +1597,7 @@ location_from_path_and_rev(const char **url,
   svn_ra_session_t *ra_session;
   apr_pool_t *subpool = svn_pool_create(pool);
   svn_revnum_t rev;
-  const char *local_abspath;
+  const char *local_abspath = NULL;
 
   if (!svn_path_is_url(path_or_url))
     SVN_ERR(svn_dirent_get_absolute(&local_abspath, path_or_url, subpool));
