81d9fc1de784969273913e1bf38ad09cba205ed3
Greg Stein <gstein@apache.org>
2010-06-03 21:29:07 +0000

    * subversion/libsvn_wc/adm_ops.c:   (svn_wc__internal_remove_from_revision_control): tweak/fix comment
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@951164 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/adm_ops.c
+++ subversion/libsvn_wc/adm_ops.c
@@ -2488,7 +2488,7 @@ svn_wc__internal_remove_from_revision_control(svn_wc__db_t *db,
 
 #ifdef SVN_EXPERIMENTAL_PRISTINE
       /* Find the checksum(s) of the node's one or two pristine texts.  Note
-         that read_info() may give us the one from WORKING_NODE again. */
+         that read_info() may give us the one from BASE_NODE again. */
       err = svn_wc__db_base_get_info(NULL, NULL, NULL, NULL, NULL, NULL,
                                      NULL, NULL, NULL, NULL, NULL,
                                      &base_sha1_checksum,
