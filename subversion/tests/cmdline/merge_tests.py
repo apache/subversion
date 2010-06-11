272cf1571cae5158800edd3b57d0e3107326d7e1
Bert Huijben <rhuijben@apache.org>
2010-06-11 16:56:59 +0000

    Following up on r953765, fix some test expectations back to how they were before r937468. We can't assume what is and what isn't in a parent stub and all this will be guaranteed once we have a central db.
    * subversion/tests/cmdline/merge_tests.py
      (merge_into_missing): Update test expectations.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@953766 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/merge_tests.py
+++ subversion/tests/cmdline/merge_tests.py
@@ -1710,7 +1710,10 @@ def merge_into_missing(sbox):
   expected_status = wc.State(F_path, {
     ''      : Item(status='  ', wc_rev=1),
     'foo'   : Item(status='! ', wc_rev=2),
-    'Q'     : Item(status='! ', wc_rev='2', entry_rev='?'),
+    'Q'     : Item(status='! ', wc_rev='?'),
+# In some intermediate WC-NG state (since r937468) this was:
+#   'Q'     : Item(status='! ', wc_rev='2', entry_rev='?'),
+# but the expected value is now back what it was.
     })
   expected_skip = wc.State(F_path, {
     'Q'   : Item(),
@@ -1732,7 +1735,10 @@ def merge_into_missing(sbox):
   expected_status = wc.State(F_path, {
     ''      : Item(status=' M', wc_rev=1),
     'foo'   : Item(status='!M', wc_rev=2),
-    'Q'     : Item(status='! ', wc_rev='2', entry_rev='?'),
+    'Q'     : Item(status='! ', wc_rev='?'),
+# In some intermediate WC-NG state (since r937468) this was:
+#   'Q'     : Item(status='! ', wc_rev='2', entry_rev='?'),
+# but the expected value is now back what it was.
     })
   expected_mergeinfo_output = wc.State(F_path, {
     ''    : Item(status=' U'),
@@ -1763,7 +1769,10 @@ def merge_into_missing(sbox):
   expected_status.add({
     'A/B/F'     : Item(status=' M', wc_rev=1),
     'A/B/F/foo' : Item(status='!M', wc_rev=2),
-    'A/B/F/Q'   : Item(status='! ', wc_rev='2', entry_rev='?'),
+    'A/B/F/Q'   : Item(status='! ', wc_rev='?'),
+# In some intermediate WC-NG state (since r937468) this was:
+#  'A/B/F/Q'   : Item(status='! ', wc_rev='2', entry_rev='?'),
+# but the expected value is now back what it was.
     })
   svntest.actions.run_and_verify_status(wc_dir, expected_status)
 
