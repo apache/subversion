24faff3620e82461bf901d833f2e6365292651a7
Bert Huijben <rhuijben@apache.org>
2010-06-25 08:24:49 +0000

    * subversion/libsvn_fs_fs/fs_fs.c   (write_format): Remove some win32 specific magic that we already handle     in svn_io_file_rename(). We can safely do this without any real-world     performance impact as this function is only used for creating, copying     and upgrading repositories.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@957840 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_fs/fs_fs.c
+++ subversion/libsvn_fs_fs/fs_fs.c
@@ -1042,12 +1042,6 @@ write_format(const char *path, int format, int max_files_per_dir,
                                   contents->data, contents->len,
                                   svn_io_file_del_none, pool));
 
-#ifdef WIN32
-      /* make the destination writable, but only on Windows, because
-         Windows does not let us replace read-only files. */
-      SVN_ERR(svn_io_set_file_read_write(path, TRUE, pool));
-#endif /* WIN32 */
-
       /* rename the temp file as the real destination */
       SVN_ERR(svn_io_file_rename(path_tmp, path, pool));
     }
