b403ff16fd27f7a1eebf65719d384efcc245e76a
Julian Foad <julianfoad@apache.org>
2010-07-02 15:25:03 +0000

    Start stripping out code that is obsolete now that SVN_EXPERIMENTAL_PRISTINE is defined.
    * subversion/ibsvn_wc/adm_crawler.c,
      subversion/libsvn_wc/diff.c,
      subversion/libsvn_wc/copy.c
        Remove SVN_EXPERIMENTAL_PRISTINE ifdefs and the pre-pristine-store code
        chunks that were ifdef'd out.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@960024 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/diff.c
+++ subversion/libsvn_wc/diff.c
@@ -153,7 +153,6 @@ get_nearest_pristine_text_as_file(const char **result_abspath,
                                   apr_pool_t *result_pool,
                                   apr_pool_t *scratch_pool)
 {
-#ifdef SVN_EXPERIMENTAL_PRISTINE
   const svn_checksum_t *checksum;
 
   SVN_ERR(svn_wc__db_read_info(NULL, NULL, NULL, NULL, NULL, NULL,
@@ -186,34 +185,6 @@ get_nearest_pristine_text_as_file(const char **result_abspath,
                                            result_pool, scratch_pool));
       return SVN_NO_ERROR;
     }
-#else
-  svn_error_t *err;
-
-  err = svn_wc__text_base_path_to_read(result_abspath, db, local_abspath,
-                                       result_pool, scratch_pool);
-
-  if (err && err->apr_err == SVN_ERR_WC_PATH_UNEXPECTED_STATUS)
-    svn_error_clear(err);
-  else
-    return svn_error_return(err);
-
-  err = svn_wc__text_revert_path_to_read(result_abspath, db, local_abspath,
-                                         result_pool);
-
-  /* If there is no revert base to diff either, don't attempt to diff it.
-     ### This is a band-aid.
-     ### In WC-NG, files added within a copied subtree are marked "copied",
-     ### which will cause the code below to end up calling
-     ### eb->callbacks->file_changed() with a non-existent text-base.
-     ### Not sure how to properly tell apart a file added within a copied
-     ### subtree from a copied file. But eventually we'll have to get the
-     ### base text from the pristine store anyway and use tempfiles (or
-     ### streams, hopefully) for diffing, so this hack will just go away. */
-  if (err && err->apr_err == SVN_ERR_WC_PATH_UNEXPECTED_STATUS)
-    svn_error_clear(err);
-  else
-    return svn_error_return(err);
-#endif
 
   *result_abspath = NULL;
   return SVN_NO_ERROR;
