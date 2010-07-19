151d092c603fd1e3b6f8a544f59c1ea9ff1febaf
Bert Huijben <rhuijben@apache.org>
2010-07-19 13:14:42 +0000

    Following up on r874426, add some missing @since comments.
    * subversion/include/svn_repos.h
      (SVN_REPOS_DUMPFILE_TEXT_COPY_SOURCE_MD5,
       SVN_REPOS_DUMPFILE_TEXT_COPY_SOURCE_SHA1,
       SVN_REPOS_DUMPFILE_TEXT_CONTENT_MD5,
       SVN_REPOS_DUMPFILE_TEXT_CONTENT_SHA1,
       SVN_REPOS_DUMPFILE_TEXT_DELTA_BASE_MD5,
       SVN_REPOS_DUMPFILE_TEXT_DELTA_BASE_SHA1): Mark new in 1.6.
    
      (SVN_REPOS_DUMPFILE_TEXT_DELTA_BASE_CHECKSUM): Set @since back to 1.5,
        like before r874426.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@965468 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_repos.h
+++ subversion/include/svn_repos.h
@@ -2296,11 +2296,15 @@ svn_repos_node_from_baton(void *edit_baton);
 #define SVN_REPOS_DUMPFILE_NODE_ACTION               "Node-action"
 #define SVN_REPOS_DUMPFILE_NODE_COPYFROM_PATH        "Node-copyfrom-path"
 #define SVN_REPOS_DUMPFILE_NODE_COPYFROM_REV         "Node-copyfrom-rev"
+/** @since New in 1.6. */
 #define SVN_REPOS_DUMPFILE_TEXT_COPY_SOURCE_MD5      "Text-copy-source-md5"
+/** @since New in 1.6. */
 #define SVN_REPOS_DUMPFILE_TEXT_COPY_SOURCE_SHA1     "Text-copy-source-sha1"
 #define SVN_REPOS_DUMPFILE_TEXT_COPY_SOURCE_CHECKSUM \
                                         SVN_REPOS_DUMPFILE_TEXT_COPY_SOURCE_MD5
+/** @since New in 1.6. */
 #define SVN_REPOS_DUMPFILE_TEXT_CONTENT_MD5          "Text-content-md5"
+/** @since New in 1.6. */
 #define SVN_REPOS_DUMPFILE_TEXT_CONTENT_SHA1         "Text-content-sha1"
 #define SVN_REPOS_DUMPFILE_TEXT_CONTENT_CHECKSUM     \
                                         SVN_REPOS_DUMPFILE_TEXT_CONTENT_MD5
@@ -2312,11 +2316,11 @@ svn_repos_node_from_baton(void *edit_baton);
 #define SVN_REPOS_DUMPFILE_PROP_DELTA                "Prop-delta"
 /** @since New in 1.1. */
 #define SVN_REPOS_DUMPFILE_TEXT_DELTA                "Text-delta"
-/** @since New in 1.5. */
+/** @since New in 1.6. */
 #define SVN_REPOS_DUMPFILE_TEXT_DELTA_BASE_MD5       "Text-delta-base-md5"
 /** @since New in 1.6. */
 #define SVN_REPOS_DUMPFILE_TEXT_DELTA_BASE_SHA1      "Text-delta-base-sha1"
-/** @since New in 1.6. */
+/** @since New in 1.5. */
 #define SVN_REPOS_DUMPFILE_TEXT_DELTA_BASE_CHECKSUM  \
                                         SVN_REPOS_DUMPFILE_TEXT_DELTA_BASE_MD5
 
