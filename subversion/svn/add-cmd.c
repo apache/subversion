0df8e71a5415e04437f4016d806c472206c7a58f
Stefan Sperling <stsp@apache.org>
2010-07-20 15:34:11 +0000

    * subversion/svn/add-cmd.c,   subversion/svn/changelist-cmd.c:   (svn_cl__add, svn_cl__changelist): When complaining about URLs where paths    are expected, use the SVN_ERR_CL_ARG_PARSING_ERROR error code instead of    SVN_ERR_ILLEGAL_TARGET. This causes an additional line to be printed    asking the user to try 'svn help' for more information. Of course,    we won't use this error code in the client library, just in the CLI client.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@965882 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svn/add-cmd.c
+++ subversion/svn/add-cmd.c
@@ -71,7 +71,7 @@ svn_cl__add(apr_getopt_t *os,
       const char *target = APR_ARRAY_IDX(targets, i, const char *);
 
       if (svn_path_is_url(target))
-        return svn_error_return(svn_error_createf(SVN_ERR_ILLEGAL_TARGET,
+        return svn_error_return(svn_error_createf(SVN_ERR_CL_ARG_PARSING_ERROR,
                                                   NULL,
                                                   _("'%s' is not a local path"),
                                                   target));
