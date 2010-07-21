apache/subversion

commit 1e333f2455fe515d4d8ac5ae2de70b8a361484c5

* subversion/libsvn_wc/upgrade.c   (upgrade_to_wcng): When upgrading to format 18 or later from entry files,     move properties into wc-db.
git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@966279 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_wc/upgrade.c
@@ -1350,6 +1350,9 @@ upgrade_to_wcng(svn_wc__db_t *db,
 
 #if (SVN_WC__VERSION >= 17)
   SVN_ERR(migrate_text_bases(dir_abspath, sdb, iterpool));
+
+#if (SVN_WC__VERSION >= 18)
+  SVN_ERR(migrate_props(dir_abspath, sdb, old_format, iterpool));
 #endif
 
   /* All done. DB should finalize the upgrade process now.  */

commit 221690fccc8b77645b303b8bbb4a367dcec393bd

Fix the build failure introduced in r966279.  The project now builds, but the correctness of this fix may be suspect.
* subversion/libsvn_wc/upgrade.c
  (upgrade_to_wcng): Add #endif.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@966300 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_wc/upgrade.c
@@ -1350,6 +1350,7 @@ upgrade_to_wcng(svn_wc__db_t *db,
 
 #if (SVN_WC__VERSION >= 17)
   SVN_ERR(migrate_text_bases(dir_abspath, sdb, iterpool));
+#endif
 
 #if (SVN_WC__VERSION >= 18)
   SVN_ERR(migrate_props(dir_abspath, sdb, old_format, iterpool));