d6f41860a3deb01d819d1681a3c7c447de1c3eeb
Bert Huijben <rhuijben@apache.org>
2010-07-21 19:44:05 +0000

    * subversion/libsvn_wc/wc_db.c   (svn_wc__db_upgrade_apply_props): Store the base_props in WORKING_NODE,     not (never!) the revert props.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@966381 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/wc_db.c
+++ subversion/libsvn_wc/wc_db.c
@@ -6909,7 +6909,7 @@ svn_wc__db_upgrade_apply_props(svn_sqlite__db_t *sdb,
           SVN_ERR(svn_sqlite__get_statement(&stmt, sdb,
                                             STMT_UPDATE_WORKING_PROPS));
           SVN_ERR(svn_sqlite__bindf(stmt, "is", wc_id, local_relpath));
-          SVN_ERR(svn_sqlite__bind_properties(stmt, 3, revert_props,
+          SVN_ERR(svn_sqlite__bind_properties(stmt, 3, base_props,
                                               scratch_pool));
           SVN_ERR(svn_sqlite__update(&affected_rows, stmt));
 
