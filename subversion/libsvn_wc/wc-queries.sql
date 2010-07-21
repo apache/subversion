e1667f845b227e983eba8b8814e362677251a2f5
Philip Martin <philip@apache.org>
2010-07-21 17:14:27 +0000

    * subversion/libsvn_wc/wc-queries.sql   (STMT_SELECT_BASE_WORKING_NODE): Remove spurious semicolons.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@966321 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/wc-queries.sql
+++ subversion/libsvn_wc/wc-queries.sql
@@ -415,10 +415,10 @@ LIMIT 1
 
 -- STMT_SELECT_BASE_WORKING_NODE
 SELECT 1 FROM base_node
-  WHERE wc_id = ?1 AND local_relpath = ?2;
+  WHERE wc_id = ?1 AND local_relpath = ?2
 UNION ALL
 SELECT 1 FROM working_node
-  WHERE wc_id = ?1 AND local_relpath = ?2;
+  WHERE wc_id = ?1 AND local_relpath = ?2
 LIMIT 1
 
 -- STMT_DELETE_PRISTINE
