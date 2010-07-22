e09082dffdcc4b1d288c764467ad9b1bc52feec4
Hyrum Kurt Wright <hwright@apache.org>
2010-07-22 16:07:17 +0000

    Finish rev'ing the client commit APIs by making the propset API return commit info back through the client context callback.
    * subversion/svn/propdel-cmd.c
      (svn_cl__propdel): Use the updated API.
    
    * subversion/svn/propset-cmd.c
      (svn_cl__propset): Same.
    
    * subversion/svn/propedit-cmd.c
      (commit_info_baton, commit_info_handler): New.
      (svn_cl__propedit): Use the new API, and define a dummy callback to
        maintain output compat.
    
    * subversion/include/svn_client.h
      (svn_client_propset4): New.
      (svn_client_propset3): Deprecate.
    
    * subversion/libsvn_client/deprecated.c
      (svn_client_propset3): New wrapper.
    
    * subversion/libsvn_client/prop_commands.c
      (propset_on_url): Remove commit_info param, use the context callback and
        baton on commit.
      (svn_client_propset3): Remove.
      (svn_client_propset4): New.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@966720 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svn/propset-cmd.c
+++ subversion/svn/propset-cmd.c
@@ -175,12 +175,11 @@ svn_cl__propset(apr_getopt_t *os,
       for (i = 0; i < targets->nelts; i++)
         {
           const char *target = APR_ARRAY_IDX(targets, i, const char *);
-          svn_commit_info_t *commit_info;
 
           svn_pool_clear(iterpool);
           SVN_ERR(svn_cl__check_cancel(ctx->cancel_baton));
-          SVN_ERR(svn_cl__try(svn_client_propset3
-                              (&commit_info, pname_utf8, propval, target,
+          SVN_ERR(svn_cl__try(svn_client_propset4(
+                               pname_utf8, propval, target,
                                opt_state->depth, opt_state->force,
                                SVN_INVALID_REVNUM, opt_state->changelists,
                                NULL, ctx, iterpool),
