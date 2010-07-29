0a68dbe1272ff7ca68166195c74398c26e9eb55f
Ramkumar Ramachandra <artagnon@apache.org>
2010-07-29 19:30:39 +0000

    svnrdump: Redesign the API and workflow to use for loading
    Get a generalized commit editor along with related data, and then
    drive it with using the callback parser functions that get fired while
    parsing the diff using svn_repos_parse_dumpstream.
    
    * subversion/svnrdump/dumpfile_parser.c
    
      (new_revision_record, uuid_record, new_node_record,
      set_revision_property, set_node_property, delete_node_property,
      remove_node_props, set_fulltext, apply_textdelta, close_node,
      close_revision): Add new no-op functions to set in the
      svn_repos_parse_fns2_t.
    
      (new_revision_record, new_node_record): Add a small debugging aid:
      when these functions are called, print a message to stderr to
      indicate parsing progress.
    
      (apply_txdelta): Do just enough to perform like a no-op without
      crashing: set the handler/ handler_baton,
    
      (apply_window): Add some WIP code inside an #if 0 block and another
      no-op one outside. This is used by apply_textdelta to set the
      svn_txdelta_window_handler_t.
    
      (build_dumpfile_parser): Fill in information about the new callback
      functions into the svn_repos_parse_fns2_t. Do some memory
      allocations for various batons. This is still a heavy WIP, as most
      of the information in the batons isn't used. Also, don't actually
      call svn_repos_parse_dumpstream2; this function only builds the
      parser to use. Related to this, change the API to exclude the stream
      argument.
    
    * subversion/svnrdump/load_editor.c
    
      (drive_load_editor): Change API to take the editor/ edit_baton (to
      be fetched from get_load_editor) and stream to use with the
      dumpstream parser. Also add calls to build_dumpfile_parser and
      svn_repos_parse_dumpstream2 appropriately.
    
      (get_load_editor): Change the API to remove stream. The commit
      editor needs to know nothing about the stream from which the
      dumpfile will be parsed. Setup necessary batons and do the
      appropriate memory allocations to actually make a successful call to
      svn_ra_get_commit_editor3. Set the various arguments to return
      appropriately.
    
    * subversion/svnrdump/load_editor.h
    
      (apply_baton): New baton to be used by txdelta applier.
    
      (parse_baton): New baton to be used by some of the parse functions,
      mainly to fetch data to put into other more specific batons.
    
      (node_baton, revision_baton): Two new batons to be used by various
      parse functions.
    
      (commit_dir_baton): The directory baton to be used by the load
      editor; taken from commit.c. ### It looks very similar to the
      directory baton we use in dump_editor: can we use the same baton?
    
      (build_dumpfile_parser, drive_load_editor, get_load_editor): Track
      API changes.
    
    * subversion/svnrdump/svnrdump.c
    
      (load_revisions): Track the API and workflow changes.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@980547 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svnrdump/dumpfile_parser.c
+++ subversion/svnrdump/dumpfile_parser.c
@@ -6,21 +6,160 @@
 
 #include "load_editor.h"
 
+static svn_error_t *
+new_revision_record(void **revision_baton,
+		    apr_hash_t *headers,
+		    void *parse_baton,
+		    apr_pool_t *pool)
+{
+  struct revision_baton *rb;
+  rb = apr_pcalloc(pool, sizeof(*rb));
+  rb->pb = parse_baton;
+
+  fprintf(stderr, "new_revision_record called\n");
+
+  *revision_baton = rb;
+  return SVN_NO_ERROR;
+}
+
+static svn_error_t *
+uuid_record(const char *uuid,
+            void *parse_baton,
+            apr_pool_t *pool)
+{
+  return SVN_NO_ERROR;
+}
+
+static svn_error_t *
+new_node_record(void **node_baton,
+                apr_hash_t *headers,
+                void *revision_baton,
+                apr_pool_t *pool)
+{
+  struct node_baton *nb;
+  nb = apr_pcalloc(pool, sizeof(*nb));
+  nb->rb = revision_baton;
+
+  fprintf(stderr, "new_node_record called\n");
+
+  *node_baton = nb;
+  return SVN_NO_ERROR;
+}
+
+
+static svn_error_t *
+set_revision_property(void *baton,
+                      const char *name,
+                      const svn_string_t *value)
+{
+  return SVN_NO_ERROR;
+}
+
+static svn_error_t *
+set_node_property(void *baton,
+                  const char *name,
+                  const svn_string_t *value)
+{
+  return SVN_NO_ERROR;
+}
+
+static svn_error_t *
+delete_node_property(void *baton,
+                     const char *name)
+{
+  return SVN_NO_ERROR;
+}
+
+static svn_error_t *
+remove_node_props(void *baton)
+{
+  return SVN_NO_ERROR;
+}
+
+static svn_error_t *
+set_fulltext(svn_stream_t **stream,
+             void *node_baton)
+{
+  return SVN_NO_ERROR;
+}
+
+#if 0
+static svn_error_t *
+apply_window(svn_txdelta_window_t *window, void *baton)
+{
+  struct apply_baton *apply_baton;
+  apr_size_t tlen;
+  apply_baton = baton;
+  if (window == NULL)
+    return SVN_NO_ERROR;
+
+  tlen = window->tview_len;
+  apply_baton->target = apr_pcalloc(apply_baton->pool, tlen);
+  svn_txdelta_apply_instructions(window, apply_baton->source,
+                                 apply_baton->target, &tlen);
+  return SVN_NO_ERROR;
+}
+#endif
+
+static svn_error_t *
+apply_window(svn_txdelta_window_t *window, void *baton)
+{
+  return SVN_NO_ERROR;
+}
+
+static svn_error_t *
+apply_textdelta(svn_txdelta_window_handler_t *handler,
+                void **handler_baton,
+                void *node_baton)
+{
+  struct node_baton *nb;
+  nb = node_baton;
+  *handler = apply_window;
+  *handler_baton = nb->rb->pb->ab;
+  return SVN_NO_ERROR;
+}
+
+static svn_error_t *
+close_node(void *baton)
+{
+  return SVN_NO_ERROR;
+}
+
+static svn_error_t *
+close_revision(void *baton)
+{
+  return SVN_NO_ERROR;
+}
+
 svn_error_t *
 build_dumpfile_parser(const svn_repos_parse_fns2_t **parser,
                       void **parse_baton,
-                      svn_stream_t *stream,
                       apr_pool_t *pool)
 {
   svn_repos_parse_fns2_t *pf;
+  struct parse_baton *pb;
+  struct apply_baton *ab;
 
   pf = apr_pcalloc(pool, sizeof(*pf));
+  pf->new_revision_record = new_revision_record;
+  pf->uuid_record = uuid_record;
+  pf->new_node_record = new_node_record;
+  pf->set_revision_property = set_revision_property;
+  pf->set_node_property = set_node_property;
+  pf->delete_node_property = delete_node_property;
+  pf->remove_node_props = remove_node_props;
+  pf->set_fulltext = set_fulltext;
+  pf->apply_textdelta = apply_textdelta;
+  pf->close_node = close_node;
+  pf->close_revision = close_revision;
 
-  SVN_ERR(svn_repos_parse_dumpstream2(stream, pf, NULL,
-                                      NULL, NULL, pool));
+  pb = apr_pcalloc(pool, sizeof(*pb));
+  ab = apr_pcalloc(pool, sizeof(struct apply_baton));
+  ab->source = apr_pstrmemdup(pool, " ", sizeof(" "));
+  pb->ab = ab;
 
   *parser = pf;
-  *parse_baton = NULL;
+  *parse_baton = pb;
 
   return SVN_NO_ERROR;
 }
