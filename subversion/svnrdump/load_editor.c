c9a6d9bdefa887a2160defa81b7314c9e00a91a2
Ramkumar Ramachandra <artagnon@apache.org>
2010-07-31 12:00:21 +0000

    svnrdump: Restructure workflow to include revprops and handle r0
    Revprops are set at the time of creation of the commit_editor;
    therefore, every revision needs a corresponding
    commit_editor. Revprops are known only while parsing the dumpfile, so
    creating the commit_editor in advance is a mistake -- we must create
    it immediately after the revprops have been parsed in. Also, by virtue
    of the commit_editor being unable to rewrite any revisions, it cannot
    handle revision 0.
    
    Memory management has also changed due to the "one commit_editor per
    revision" design- within one revision, all allocations are done in a
    revision-specific pool in the parser_baton.
    
    * subversion/svnrdump/dumpfile_parser.c: Remove file and move all the
      code out into load_editor.c.
    
    * subversion/svnrdump/load_editor.c
    
      (build_dumpfile_parser, get_dumpstream_loader): Cosmetic
      rename. Change API to accept a session instead of editor/
      edit_baton. The editor/ edit_baton is created in new_node_record
      now. Add the code for setting up the parser and parse_baton.
    
      (drive_load_editor, get_load_editor): Remove both functions: There
      is no more load editor- just a dumpstream loader to be driven.
    
      (drive_dumpstream_loader): New function to simply call
      svn_repos_parse_dumpstream2 now.
    
      (new_revision_record): Don't open_root here since we don't have a
      commit_editor yet. Allocate memory to the revision-specific pool in
      the parser_baton pool and set the commit_editor/ commit_edit_baton
      to NULL.
    
      (uuid_record): Set the uuid field in parse_baton. Currenly unused.
    
      (new_node_record): If the commit_editor doesn't already exist, it
      means that this is the first node in the revision: take this
      opportunity to create the commit editor and call set_target_revision
      and open_root. Note that this commit editor will be used for all
      nodes in this revision. Also change the if-statement into a
      switch-case for the action, just like in the dumpfile_parser.
    
      (set_revision_property): Implement special handling for revision 0
      and stuff the datestamp into the parse_baton to use in
      close_revision.
    
      (set_node_property, delete_node_property, remove_node_props,
      set_fulltext, apply_textdelta, close_node): Import the relevant code
      from dumpfile_parser.c making minor semantic modifications where
      necessary.
    
      (close_revision): For revision 0, don't attempt to close a
      non-existant commit_editor; since revision 0 doesn't have any nodes,
      new_node_record is never fired and consequently, a commit_editor is
      never created. Also implement special handling for the datestamp
      after closing the revision (learnt from svnadmin).
    
    * subversion/svnrdump/load_editor.h
    
      (build_dumpfile_parser, get_dumpstream_loader): Track rename.
    
      (drive_load_editor, get_load_editor): Track removal.
    
      (drive_dumpstream_loader): Track new function creation.
    
      (revision_baton): Add a revprop_table to store the revprops before
      writing them.
    
      (parse_baton): Add session and uuid. Session is needed to make RA
      API calls outside the operation of the commit editor, and uuid is
      currently just set appropriately, not used anywhere.
    
    * subversion/svnrdump/svnrdump.c
    
      (load_revision): Track API changes.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@981039 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svnrdump/load_editor.c
+++ subversion/svnrdump/load_editor.c
@@ -22,11 +22,13 @@
  * ====================================================================
  */
 
+#include "svn_cmdline.h"
 #include "svn_pools.h"
 #include "svn_delta.h"
 #include "svn_repos.h"
+#include "svn_props.h"
 #include "svn_ra.h"
-#include "svn_cmdline.h"
+#include "svn_io.h"
 
 #include "load_editor.h"
 
@@ -43,42 +45,318 @@ commit_callback(const svn_commit_info_t *commit_info,
   return SVN_NO_ERROR;
 }
 
-svn_error_t *
-drive_load_editor(const svn_delta_editor_t *editor,
-                  void *edit_baton,
-                  svn_stream_t *stream,
-                  apr_pool_t *pool)
-{
-  const svn_repos_parse_fns2_t *parser;
-  void *pb;
 
-  SVN_ERR(build_dumpfile_parser(&parser, &pb, editor, edit_baton, pool));
-  SVN_ERR(svn_repos_parse_dumpstream2(stream, parser, pb,
-                                      NULL, NULL, pool));
+static svn_error_t *
+new_revision_record(void **revision_baton,
+		    apr_hash_t *headers,
+		    void *parse_baton,
+		    apr_pool_t *pool)
+{
+  struct revision_baton *rb;
+  struct parse_baton *pb;
+  apr_hash_index_t *hi;
+
+  rb = apr_pcalloc(pool, sizeof(*rb));
+  pb = parse_baton;
+  pb->pool = svn_pool_create(pool);
+  rb->pb = pb;
+
+  for (hi = apr_hash_first(pool, headers); hi; hi = apr_hash_next(hi))
+    {
+      const void *key;
+      void *val;
+      const char *hname, *hval;
+
+      apr_hash_this(hi, &key, NULL, &val);
+      hname = key;
+      hval = val;
+
+      if (strcmp(hname, SVN_REPOS_DUMPFILE_REVISION_NUMBER) == 0)
+        rb->rev = atoi(hval);
+    }
+  /* Set the commit_editor/ commit_edit_baton to NULL and wait for
+     them to be created in new_node_record */
+  
+  rb->pb->commit_editor = NULL;
+  rb->pb->commit_edit_baton = NULL;
+  rb->revprop_table = apr_hash_make(pb->pool);
+
+  *revision_baton = rb;
+  return SVN_NO_ERROR;
+}
+
+static svn_error_t *
+uuid_record(const char *uuid,
+            void *parse_baton,
+            apr_pool_t *pool)
+{
+  struct parse_baton *pb;
+  pb = parse_baton;
+  pb->uuid = apr_pstrdup(pool, uuid);
+  return SVN_NO_ERROR;
+}
+
+static svn_error_t *
+new_node_record(void **node_baton,
+                apr_hash_t *headers,
+                void *revision_baton,
+                apr_pool_t *pool)
+{
+  struct node_baton *nb;
+  struct revision_baton *rb;
+  apr_hash_index_t *hi;
+  void *file_baton;
+  void *child_baton;
+  const struct svn_delta_editor_t *commit_editor;
+  void *commit_edit_baton;
+  void *root_baton;
+
+  rb = revision_baton;
+  nb = apr_pcalloc(rb->pb->pool, sizeof(*nb));
+  nb->rb = rb;
+
+  commit_editor = rb->pb->commit_editor;
+  commit_edit_baton = rb->pb->commit_edit_baton;
+
+  /* If the creation of commit_editor is pending, create it now and
+     open_root on it */
+  if (!commit_editor) {
+      SVN_ERR(svn_ra_get_commit_editor3(rb->pb->session, &commit_editor,
+                                        &commit_edit_baton, rb->revprop_table,
+                                        commit_callback, NULL, NULL, FALSE,
+                                        rb->pb->pool));
+
+      rb->pb->commit_editor = commit_editor;
+      rb->pb->commit_edit_baton = commit_edit_baton;
+
+      SVN_ERR(commit_editor->set_target_revision(commit_edit_baton,
+                                                 rb->rev, rb->pb->pool));
+      SVN_ERR(commit_editor->open_root(commit_edit_baton, rb->rev,
+                                       rb->pb->pool, &root_baton));
+      rb->dir_baton = root_baton;
+
+  }
+
+  for (hi = apr_hash_first(rb->pb->pool, headers); hi; hi = apr_hash_next(hi))
+    {
+      const void *key;
+      void *val;
+      const char *hname, *hval;
+
+      apr_hash_this(hi, &key, NULL, &val);
+      hname = key;
+      hval = val;
+
+      /* Parse the different kinds of headers we can encounter and
+         stuff them into the node_baton for writing later */
+      if (strcmp(hname, SVN_REPOS_DUMPFILE_NODE_PATH) == 0)
+        nb->path = apr_pstrdup(rb->pb->pool, hval);
+      if (strcmp(hname, SVN_REPOS_DUMPFILE_NODE_KIND) == 0)
+        nb->kind = strcmp(hval, "file") == 0 ? svn_node_file : svn_node_dir;
+      if (strcmp(hname, SVN_REPOS_DUMPFILE_NODE_ACTION) == 0)
+        {
+          if (strcmp(hval, "add") == 0)
+            nb->action = svn_node_action_add;
+          if (strcmp(hval, "change") == 0)
+            nb->action = svn_node_action_change;
+          if (strcmp(hval, "delete") == 0)
+            nb->action = svn_node_action_delete;
+          if (strcmp(hval, "replace") == 0)
+            nb->action = svn_node_action_replace;
+        }
+      if (strcmp(hname, SVN_REPOS_DUMPFILE_NODE_COPYFROM_REV) == 0)
+        nb->copyfrom_rev = atoi(hval);
+      if (strcmp(hname, SVN_REPOS_DUMPFILE_NODE_COPYFROM_PATH) == 0)
+        nb->copyfrom_path = apr_pstrdup(rb->pb->pool, hval);
+    }
+
+  switch (nb->action)
+    {
+    case svn_node_action_add:
+      if (nb->kind == svn_node_file)
+        {
+          SVN_ERR(commit_editor->add_file(nb->path, nb->rb->dir_baton,
+                                          nb->copyfrom_path, nb->copyfrom_rev,
+                                          rb->pb->pool, &file_baton));
+          nb->file_baton = file_baton;
+        }
+      else if(nb->kind == svn_node_dir)
+        {
+          SVN_ERR(commit_editor->add_directory(nb->path, nb->rb->dir_baton,
+                                               nb->copyfrom_path, nb->copyfrom_rev,
+                                               rb->pb->pool, &child_baton));
+          nb->rb->dir_baton = child_baton;
+        }
+      break;
+    default:
+      break;
+    }
+
+  *node_baton = nb;
+  return SVN_NO_ERROR;
+}
+
+
+static svn_error_t *
+set_revision_property(void *baton,
+                      const char *name,
+                      const svn_string_t *value)
+{
+  struct revision_baton *rb;
+  rb = baton;
+
+  if (rb->rev > 0)
+    {
+      apr_hash_set(rb->revprop_table, apr_pstrdup(rb->pb->pool, name),
+                   APR_HASH_KEY_STRING, svn_string_dup(value, rb->pb->pool));
+    }
+  else
+    /* Special handling for revision 0; this is safe because the
+       commit_editor hasn't been created yet. */
+    svn_ra_change_rev_prop(rb->pb->session, rb->rev, name, value,
+                           rb->pb->pool);
+
+  /* Remember any datestamp that passes through!  (See comment in
+     close_revision() below.) */
+  if (! strcmp(name, SVN_PROP_REVISION_DATE))
+    rb->datestamp = svn_string_dup(value, rb->pb->pool);
 
   return SVN_NO_ERROR;
 }
 
-/* The load editor is a essentially a dumpfile parser connected to a
-   commit editor. It can be driven using drive_load_editor(). */
-svn_error_t *
-get_load_editor(const svn_delta_editor_t **editor,
-                void **edit_baton,
-                svn_ra_session_t *session,
-                apr_pool_t *pool)
+static svn_error_t *
+set_node_property(void *baton,
+                  const char *name,
+                  const svn_string_t *value)
 {
-  const svn_delta_editor_t *delta_editor;
-  apr_hash_t *revprop_table;
-  void *delta_edit_baton;
+  return SVN_NO_ERROR;
+}
 
-  revprop_table = apr_hash_make(pool);
- 
-  /* Call the commit editor and get back a delta_editor and delta_editor_baton */
-  SVN_ERR(svn_ra_get_commit_editor3(session, &delta_editor, &delta_edit_baton,
-                                    revprop_table, commit_callback,
-                                    NULL, NULL, FALSE, pool));
-  *editor = delta_editor;
-  *edit_baton = delta_edit_baton;
+static svn_error_t *
+delete_node_property(void *baton,
+                     const char *name)
+{
+  return SVN_NO_ERROR;
+}
+
+static svn_error_t *
+remove_node_props(void *baton)
+{
+  return SVN_NO_ERROR;
+}
+
+static svn_error_t *
+set_fulltext(svn_stream_t **stream,
+             void *node_baton)
+{
+  /* ### Not implemented */
+  return SVN_NO_ERROR;
+}
+
+static svn_error_t *
+apply_textdelta(svn_txdelta_window_handler_t *handler,
+                void **handler_baton,
+                void *node_baton)
+{
+  struct node_baton *nb;
+  const struct svn_delta_editor_t *commit_editor;
+  apr_pool_t *pool;
+  nb = node_baton;
+  commit_editor = nb->rb->pb->commit_editor;
+  pool = nb->rb->pb->pool;
+  return commit_editor->apply_textdelta(nb->file_baton, NULL, pool,
+                                        handler, handler_baton);
+}
+
+static svn_error_t *
+close_node(void *baton)
+{
+  struct node_baton *nb;
+  const struct svn_delta_editor_t *commit_editor;
+  apr_pool_t * pool;
+  nb = baton;
+  pool = nb->rb->pb->pool;
+  commit_editor = nb->rb->pb->commit_editor;
+  if (nb->kind == svn_node_file)
+    SVN_ERR(commit_editor->close_file(nb->file_baton, NULL,
+                                      pool));
+  else if (nb->kind == svn_node_dir)
+    SVN_ERR(commit_editor->close_directory(nb->rb->dir_baton,
+                                           pool));
+  return SVN_NO_ERROR;
+}
+
+static svn_error_t *
+close_revision(void *baton)
+{
+  struct revision_baton *rb;
+  const svn_delta_editor_t *commit_editor;
+  void *commit_edit_baton;
+  rb = baton;
+
+  commit_editor = rb->pb->commit_editor;
+  commit_edit_baton = rb->pb->commit_edit_baton;
+
+  /* Ignore revision 0 in the stream, since the commit editor can't
+     change a revision that's already written. */
+  if (rb->rev > 0)
+    SVN_ERR(commit_editor->close_edit(commit_edit_baton, rb->pb->pool));
+
+  /* svn_fs_commit_txn rewrites the datestamp property to the current
+     clock-time, and this is not desired. Rewrite it by hand after
+     closing the commit_editor. */
+  SVN_ERR(svn_ra_change_rev_prop(rb->pb->session, rb->rev,
+                                 SVN_PROP_REVISION_DATE,
+                                 rb->datestamp, rb->pb->pool));
+
+  svn_pool_destroy(rb->pb->pool);
+
+  return SVN_NO_ERROR;
+}
+
+svn_error_t *
+get_dumpstream_loader(const svn_repos_parse_fns2_t **parser,
+                      void **parse_baton,
+                      svn_ra_session_t *session,
+                      apr_pool_t *pool)
+{
+  svn_repos_parse_fns2_t *pf;
+  struct parse_baton *pb;
+
+  pf = apr_pcalloc(pool, sizeof(*pf));
+  pf->new_revision_record = new_revision_record;
+  pf->uuid_record = uuid_record;
+  pf->new_node_record = new_node_record;
+  pf->set_revision_property = set_revision_property;
+  pf->set_node_property = set_node_property;
+  pf->delete_node_property = delete_node_property;
+  pf->remove_node_props = remove_node_props;
+  pf->set_fulltext = set_fulltext;
+  pf->apply_textdelta = apply_textdelta;
+  pf->close_node = close_node;
+  pf->close_revision = close_revision;
+
+  pb = apr_pcalloc(pool, sizeof(*pb));
+  pb->session = session;
+
+  *parser = pf;
+  *parse_baton = pb;
+
+  return SVN_NO_ERROR;
+}
+
+svn_error_t *
+drive_dumpstream_loader(svn_stream_t *stream,
+                        const svn_repos_parse_fns2_t *parser,
+                        void *parse_baton,
+                        svn_ra_session_t *session,
+                        apr_pool_t *pool)
+{
+  void *pb;
+  pb = parse_baton;
+
+  SVN_ERR(svn_repos_parse_dumpstream2(stream, parser,parse_baton,
+                                      NULL, NULL, pool));
 
   return SVN_NO_ERROR;
 }
