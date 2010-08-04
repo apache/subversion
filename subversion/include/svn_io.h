9c1180fc807e4467aa8d43d8307c71535d6fa10a
Stefan Fuhrmann <stefan2@apache.org>
2010-08-04 20:22:10 +0000

    Upon second thought, svn_stream_from_aprfile3 should be renamed to svn_stream_from_cached_file_handle.
    * subversion/include/svn_io.h
      (svn_stream_from_cached_file_handle): renamed from svn_stream_from_aprfile3
    * subversion/libvn_subr/stream.c
      (svn_stream_from_cached_file_handle): renamed from svn_stream_from_aprfile3
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/performance@982391 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_io.h
+++ subversion/include/svn_io.h
@@ -926,23 +926,6 @@ svn_stream_from_aprfile2(apr_file_t *file,
 /* "forward-declare" svn_file_handle_cache__handle_t */
 struct svn_file_handle_cache__handle_t;
 
-/** Create a stream from a cached file handle.  For convenience, if @a file
- * is @c NULL, an empty stream created by svn_stream_empty() is returned.
- *
- * This function should normally be called with @a disown set to FALSE,
- * in which case closing the stream will also return the file handle to
- * the respective cache object.
- *
- * If @a disown is TRUE, the stream will disown the file handle, meaning 
- * that svn_stream_close() will not close the cached file handle.
- *
- * @since New in 1.7.
- */
-svn_stream_t *
-svn_stream_from_aprfile3(struct svn_file_handle_cache__handle_t *file,
-                         svn_boolean_t disown,
-                         apr_pool_t *pool);
-
 /** Similar to svn_stream_from_aprfile2(), except that the file will
  * always be disowned.
  *
@@ -956,6 +939,24 @@ SVN_DEPRECATED
 svn_stream_t *
 svn_stream_from_aprfile(apr_file_t *file,
                         apr_pool_t *pool);
+
+/** Create a stream from a cached file handle.  For convenience, if @a file
+ * is @c NULL, an empty stream created by svn_stream_empty() is returned.
+ *
+ * This function should normally be called with @a disown set to FALSE,
+ * in which case closing the stream will also return the file handle to
+ * the respective cache object.
+ *
+ * If @a disown is TRUE, the stream will disown the file handle, meaning 
+ * that svn_stream_close() will not close the cached file handle.
+ *
+ * @since New in 1.7.
+ */
+svn_stream_t *
+svn_stream_from_cached_file_handle
+    (struct svn_file_handle_cache__handle_t *file,
+     svn_boolean_t disown,
+     apr_pool_t *pool);
 
 /** Create a stream for reading from a range of an APR file.
  * The stream cannot be written to.
