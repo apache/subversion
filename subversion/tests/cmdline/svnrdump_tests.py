21413581579362a88cd819049502b9daf9b876ec
Ramkumar Ramachandra <artagnon@apache.org>
2010-08-06 13:17:31 +0000

    * subversion/tests/cmdline/svnrdump_tests.py   (test_list): Mark skeleton_load and copy_and_modify_load as Wimp   only for ra_dav. They pass with ra_svn now.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@982948 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/svnrdump_tests.py
+++ subversion/tests/cmdline/svnrdump_tests.py
@@ -175,10 +175,8 @@ test_list = [ None,
               basic_dump,
               revision_0_dump,
               revision_0_load,
-              XFail(XFail(skeleton_load, svntest.main.is_ra_type_dav),
-                    svntest.main.is_ra_type_svn),
-              XFail(XFail(copy_and_modify_load, svntest.main.is_ra_type_dav),
-                    svntest.main.is_ra_type_svn),
+              Wimp("Response code: 501", skeleton_load, svntest.main.is_ra_type_dav),
+              Wimp("Response code: 501", copy_and_modify_load, svntest.main.is_ra_type_dav),
               Wimp("Need to fix headers in RA layer", copy_and_modify_dump),
              ]
 
