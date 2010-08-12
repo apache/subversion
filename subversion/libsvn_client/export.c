79fb4c2dcbc9725df584cd8fb40af76164b05de8
Daniel Shahaf <danielsh@apache.org>
2010-08-12 19:49:20 +0000

    * subversion/libsvn_client/export.c   (add_file):  Add a fullstop at the end of a comment.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@984931 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/export.c
+++ subversion/libsvn_client/export.c
@@ -714,8 +714,8 @@ add_file(const char *path,
   struct file_baton *fb = apr_pcalloc(pool, sizeof(*fb));
   const char *full_path = svn_dirent_join(eb->root_path, path, pool);
 
-  /* path is not canonicalized, i.e. it may still contain spaces etc.
-   * but eb->root_url is */
+  /* PATH is not canonicalized, i.e. it may still contain spaces etc.
+   * but EB->root_url is. */
   const char *full_url = svn_path_url_add_component2(eb->root_url,
                                                      path,
                                                      pool);
