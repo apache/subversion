b505b91523b75515d47ade1a7b7cad677d7ae2b7
Stefan Fuhrmann <stefan2@apache.org>
2010-08-12 19:29:23 +0000

    Merge r983764 from branches/performance. Approved by: danielsh http://svn.haxx.se/dev/archive-2010-08/0217.shtml
    Fix an obvious typo in the path validation code.
    It produces false negatives, i.e. certain malformed URIs won't be detected.
    
    * subversion/libsvn_subr/dirent_uri.c
      (svn_uri_is_canonical): actually compare the chars following '%' instead
       of comparing '%'+1 and '%'+2.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@984926 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/dirent_uri.c
+++ subversion/libsvn_subr/dirent_uri.c
@@ -1901,11 +1901,11 @@ svn_uri_is_canonical(const char *uri, apr_pool_t *pool)
 
               /* Can't use apr_isxdigit() because lower case letters are
                  not in our canonical format */
-              if (((*(ptr+1) < '0' || (*ptr+1) > '9')) 
-                  && (*(ptr+1) < 'A' || (*ptr+1) > 'F'))
+              if (((*(ptr+1) < '0' || *(ptr+1) > '9')) 
+                  && (*(ptr+1) < 'A' || *(ptr+1) > 'F'))
                 return FALSE;
-              else if (((*(ptr+2) < '0' || (*ptr+2) > '9')) 
-                  && (*(ptr+2) < 'A' || (*ptr+2) > 'F'))
+              else if (((*(ptr+2) < '0' || *(ptr+2) > '9')) 
+                  && (*(ptr+2) < 'A' || *(ptr+2) > 'F'))
                 return FALSE;
 
               digitz[0] = *(++ptr);
