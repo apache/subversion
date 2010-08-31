39c7aeebf06716f57ad58b4b6d1357e4203a7622
Stefan Sperling <stsp@apache.org>
2010-08-31 19:15:09 +0000

    * subversion/libsvn_diff/parse-diff.c   (svn_diff_close_patch): Also close hunks from property patches, if any.    Requires a new SCRATCH_POOL argument to loop over the prop hash table.
    * subversion/include/svn_diff.h
      (svn_diff_close_patch): Update declaration.
    
    * subversion/libsvn_client/patch.c
      (apply_patches): Adjust caller.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@991297 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_diff/parse-diff.c
+++ subversion/libsvn_diff/parse-diff.c
@@ -1331,9 +1331,10 @@ svn_diff_parse_next_patch(svn_patch_t **patch,
 }
 
 svn_error_t *
-svn_diff_close_patch(const svn_patch_t *patch)
+svn_diff_close_patch(const svn_patch_t *patch, apr_pool_t *scratch_pool)
 {
   int i;
+  apr_hash_index_t *hi;
 
   for (i = 0; i < patch->hunks->nelts; i++)
     {
@@ -1341,6 +1342,23 @@ svn_diff_close_patch(const svn_patch_t *patch)
                                                   svn_diff_hunk_t *);
       SVN_ERR(close_hunk(hunk));
     }
+
+  for (hi = apr_hash_first(scratch_pool, patch->prop_patches);
+       hi;
+       hi = apr_hash_next(hi))
+    {
+          svn_prop_patch_t *prop_patch; 
+
+          prop_patch = svn__apr_hash_index_val(hi);
+
+          for (i = 0; i < prop_patch->hunks->nelts; i++)
+            {
+              const svn_diff_hunk_t *hunk;
+              
+              hunk = APR_ARRAY_IDX(prop_patch->hunks, i, svn_diff_hunk_t *);
+              SVN_ERR(close_hunk(hunk));
+            }
+    } 
 
   return SVN_NO_ERROR;
 }
