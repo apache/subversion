529d006865aece688eab8eab2830ad8dbce2f396
Philip Martin <philip@apache.org>
2010-09-01 13:38:51 +0000

    Fix some transient authz failures when running the dav tests in parallel.
     * subversion/tests/cmdline/svntest/sandbox.py
       (Sandbox._set_name): Use an atomic operation to update the authz file.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@991534 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/svntest/sandbox.py
+++ subversion/tests/cmdline/svntest/sandbox.py
@@ -57,13 +57,15 @@ class Sandbox:
     ### TODO: Move this into to the build() method
     # For dav tests we need a single authz file which must be present,
     # so we recreate it each time a sandbox is created with some default
-    # contents.
+    # contents, making sure that an empty file is never present
     if self.repo_url.startswith("http"):
       # this dir doesn't exist out of the box, so we may have to make it
       if not os.path.exists(svntest.main.work_dir):
         os.makedirs(svntest.main.work_dir)
       self.authz_file = os.path.join(svntest.main.work_dir, "authz")
-      open(self.authz_file, 'w').write("[/]\n* = rw\n")
+      tmp_authz_file = os.path.join(svntest.main.work_dir, "authz-" + self.name)
+      open(tmp_authz_file, 'w').write("[/]\n* = rw\n")
+      os.rename(tmp_authz_file, self.authz_file)
 
     # For svnserve tests we have a per-repository authz file, and it
     # doesn't need to be there in order for things to work, so we don't
