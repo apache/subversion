fff222a7d99504949006c94c461a01059748f70d
Julian Foad <julianfoad@apache.org>
2010-09-02 11:33:45 +0000

    * subversion/include/private/svn_wc_private.h   (svn_wc__node_is_status_obstructed, svn_wc__temp_mark_missing_not_present):     Delete, to fix the Windows build. (Limitation of 'extractor.py'.)
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@991894 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/private/svn_wc_private.h
+++ subversion/include/private/svn_wc_private.h
@@ -399,20 +399,6 @@ svn_wc__node_is_status_deleted(svn_boolean_t *is_deleted,
                                const char *local_abspath,
                                apr_pool_t *scratch_pool);
 
-#ifndef SVN_WC__SINGLE_DB
-/**
- * Set @a *is_obstructed to whether @a local_abspath is obstructed, using
- * @a wc_ctx.  If @a local_abspath is not in the working copy, return
- * @c SVN_ERR_WC_PATH_NOT_FOUND.  Use @a scratch_pool for all temporary
- * allocations.
- */
-svn_error_t *
-svn_wc__node_is_status_obstructed(svn_boolean_t *is_obstructed,
-                                  svn_wc_context_t *wc_ctx,
-                                  const char *local_abspath,
-                                  apr_pool_t *scratch_pool);
-#endif
-
 /**
  * Set @a *is_absent to whether @a local_abspath is absent, using
  * @a wc_ctx.  If @a local_abspath is not in the working copy, return
@@ -688,19 +674,6 @@ svn_wc__call_with_write_lock(svn_wc__with_write_lock_func_t func,
                              apr_pool_t *result_pool,
                              apr_pool_t *scratch_pool);
 
-
-#ifndef SVN_WC__SINGLE_DB
-/** Mark missing, deleted directory @a local_abspath as 'not-present'
- * in its parent's list of entries.
- *
- * Return #SVN_ERR_WC_PATH_FOUND if @a local_abspath isn't actually a
- * missing, deleted directory.
- */
-svn_error_t *
-svn_wc__temp_mark_missing_not_present(const char *local_abspath,
-                                      svn_wc_context_t *wc_ctx,
-                                      apr_pool_t *scratch_pool);
-#endif
 
 /**
  * Register @a local_abspath as a new file external aimed at
