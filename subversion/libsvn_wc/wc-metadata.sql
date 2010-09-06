0d067a0432fcbc8e7185a7a9032052f1789cb771
E W H Huelsmann <ehu@apache.org>
2010-09-06 11:12:01 +0000

    Add comment as suggested by rhuijben.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@992997 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/wc-metadata.sql
+++ subversion/libsvn_wc/wc-metadata.sql
@@ -800,7 +800,8 @@ CREATE TABLE NODES (
 
   /* parent's local_relpath for aggregating children of a given parent.
      this will be "" if the parent is the wcroot.  Since a wcroot will
-     never have a WORKING node the parent_relpath will never be null. */
+     never have a WORKING node the parent_relpath will never be null,
+     except when op_depth == 0 and the node is a wcroot. */
   /* ### would be nice to make this column NOT NULL.  */
   parent_relpath  TEXT,
 
