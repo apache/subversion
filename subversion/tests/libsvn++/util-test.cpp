267ae97e4751ad4294ba1edd9ead192106dbaba6
Hyrum Kurt Wright <hwright@apache.org>
2010-09-16 08:12:44 +0000

    On the object-model branch: Add a test suite for the C++ tests.  For easy of use, this is included in the same directory heirarchy as the standard C tests.
    * build.conf
      (util-cpp-test): New.
    
    * subversion/tests/libsvn++:
      New.
    
    * subversion/tests/libsvn++/util-test.cpp:
      New.
    
    * subversion/bindings/c++/test:
      Remove.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/object-model@997636 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subversion/tests/libsvn++/util-test.cpp
@@ -0,0 +1,49 @@
+/*
+ * util-test.c -- test the libsvn++ utilities
+ *
+ * ====================================================================
+ *    Licensed to the Apache Software Foundation (ASF) under one
+ *    or more contributor license agreements.  See the NOTICE file
+ *    distributed with this work for additional information
+ *    regarding copyright ownership.  The ASF licenses this file
+ *    to you under the Apache License, Version 2.0 (the
+ *    "License"); you may not use this file except in compliance
+ *    with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing,
+ *    software distributed under the License is distributed on an
+ *    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *    KIND, either express or implied.  See the License for the
+ *    specific language governing permissions and limitations
+ *    under the License.
+ * ====================================================================
+ */
+
+#include "../svn_test.h"
+
+#include "../../bindings/c++/Pool.h"
+
+using namespace SVN;
+
+static svn_error_t *
+test_pools(apr_pool_t *p)
+{
+  // We ignore the passed in pool, in favor of creating our own.
+  Pool pool;
+
+  pool.clear();
+
+  return SVN_NO_ERROR;
+}
+
+/* The test table.  */
+
+struct svn_test_descriptor_t test_funcs[] =
+  {
+    SVN_TEST_NULL,
+    SVN_TEST_PASS2(test_pools,
+                   "test Pool class"),
+    SVN_TEST_NULL
+  };
