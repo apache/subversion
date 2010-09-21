f99eb0b21569e019c3510281fce03f39d421a266
Philip Martin <philip@apache.org>
2010-09-21 10:30:47 +0000

    Make a simple SVN_WC__NODES_ONLY checkout work.
    * subversion/libsvn_wc/wc-queries.sql
      (STMT_DETERMINE_TREE_FOR_RECORDING_1): New.
    
    * subversion/libsvn_wc/wc_db.c
      (which_trees_exist): Query the NODES table.
      (svn_wc__db_temp_op_set_base_incomplete): Use the nodes result
       if SVN_WC__NODES_ONLY.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@999302 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/wc-queries.sql
+++ subversion/libsvn_wc/wc-queries.sql
@@ -650,6 +650,15 @@ SELECT 0 FROM BASE_NODE WHERE wc_id = ?1 AND local_relpath = ?2
 UNION
 SELECT 1 FROM WORKING_NODE WHERE wc_id = ?1 AND local_relpath = ?2;
 
+-- STMT_DETERMINE_TREE_FOR_RECORDING_1
+SELECT 0 FROM NODES WHERE wc_id = ?1 AND local_relpath = ?2 AND op_depth = 0
+UNION
+SELECT 1 FROM NODES WHERE wc_id = ?1 AND local_relpath = ?2
+  AND op_depth IN (SELECT op_depth FROM nodes
+                   WHERE wc_id = ?1 AND local_relpath = ?2
+                   ORDER BY op_depth DESC
+                   LIMIT 1);
+
 
 /* ### Why can't this query not just use the BASE repository
    location values, instead of taking 3 additional parameters?! */
