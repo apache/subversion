084e5b84ddb78f92f09f5d0a7dae88459a45f53e
Stefan Fuhrmann <stefan2@apache.org>
2010-09-30 00:01:45 +0000

    Merge r1001413 from the performance branch.
    Improve documentation on svn_stringbuf_appendbyte.
    
    Approved by: Hyrum K. Wright <hyrum_wright_at_mail.utexas.edu> 
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1002898 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/svn_string.c
+++ subversion/libsvn_subr/svn_string.c
@@ -388,27 +388,59 @@ svn_stringbuf_ensure(svn_stringbuf_t *str, apr_size_t minimum_size)
 }
 
 
+/* WARNING - Optimized code ahead! 
+ * This function has been hand-tuned for performance. Please read 
+ * the comments below before modifying the code.
+ */
 void
 svn_stringbuf_appendbyte(svn_stringbuf_t *str, char byte)
 {
+  char *dest;
+  apr_size_t old_len = str->len;
+
   /* In most cases, there will be pre-allocated memory left
    * to just write the new byte at the end of the used section
    * and terminate the string properly.
    */
-  apr_size_t old_len = str->len;
-  if (str->blocksize > old_len + 1)
+  if (str->blocksize < old_len + 1)
     {
-      char *dest = str->data;
+      /* The following read does not depend this write, so we
+       * can issue the write first to minimize register pressure:
+       * The value of old_len+1 is no longer needed; on most processors,
+       * dest[old_len+1] will be calculated implicitly as part of 
+       * the addressing scheme.
+       */
+      str->len = old_len+1;
 
+      /* Since the compiler cannot be sure that *src->data and *src
+       * don't overlap, we read src->data *once* before writing
+       * to *src->data. Replacing dest with str->data would force
+       * the compiler to read it again after the first byte.
+       */
+      dest = str->data;
+
+      /* If not already available in a register as per ABI, load
+       * "byte" into the register (e.g. the one freed from old_len+1),
+       * then write it to the string buffer and terminate it properly.
+       *
+       * Including the "byte" fetch, all operations so far could be
+       * issued at once and be scheduled at the CPU's descression.
+       * Most likely, no-one will soon depend on the data that will be 
+       * written in this function. So, no stalls there, either.
+       */
       dest[old_len] = byte;
       dest[old_len+1] = '\0';
-
-      str->len = old_len+1;
     }
   else
     {
       /* we need to re-allocate the string buffer
        * -> let the more generic implementation take care of that part
+       */
+
+      /* Depending on the ABI, "byte" is a register value. If we were
+       * to take its address directly, the compiler might decide to
+       * put in on the stack *unconditionally*, even if that would
+       * only be necessary for this block.
        */
       char b = byte;
       svn_stringbuf_appendbytes(str, &b, 1);
