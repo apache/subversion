2eddfcdd734f7b22999cc8fc3ecb606a68c9dd2a
Daniel Shahaf <danielsh@apache.org>
2010-10-25 20:00:56 +0000

    Follow-up to r1027245.  (It was already sitting in my wc when that revision was committed.)
    * subversion/svn/cl.h
      (svn_cl__opt_state_t.show_diff): Note option name in comment.
    
    * subversion/svn/log-cmd.c
      (log_receiver_baton): Ditto.
      
    * subversion/svn/main.c
      (opt_diff): Renamed from 'opt_show_diff'.
      (svn_cl__options, svn_cl__cmd_table, main): Track rename.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1027249 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svn/cl.h
+++ subversion/svn/cl.h
@@ -226,7 +226,7 @@ typedef struct svn_cl__opt_state_t
   svn_boolean_t reverse_diff;     /* reverse a diff (e.g. when patching) */
   svn_boolean_t ignore_whitespace; /* don't account for whitespace when
                                       patching */
-  svn_boolean_t show_diff;        /* produce diff output */
+  svn_boolean_t show_diff;        /* produce diff output (maps to --diff) */
   svn_boolean_t internal_diff;    /* override diff_cmd in config file */
   svn_boolean_t use_git_diff_format; /* Use git's extended diff format */
 } svn_cl__opt_state_t;
