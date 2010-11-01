apache/subversion

commit 5e3dc9200c8a6ad74b380c8c70e1319dbe979750

Make "svn export $repos/blah '.'" do what you think it would, namely, put a file named "blah" into the cwd.
* subversion/tests/cmdline/export_tests.py
  (export_to_explicit_cwd): New.
  (test_list): Run the new test.

* subversion/svn/export-cmd.c
  (svn_cl__export): If the explicit TO path is '.', treat it as if it weren't
    given.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@880559 13f79535-47bb-0310-9956-ffa450edef68

subversion/svn/export-cmd.c
@@ -70,10 +70,9 @@ svn_cl__export(apr_getopt_t *os,
 
   /* If only one target was given, split off the basename to use as
      the `to' path.  Else, a `to' path was supplied. */
-  if (targets->nelts == 1)
+  to = APR_ARRAY_IDX(targets, 1, const char *);
+  if (targets->nelts == 1 || strcmp(to, "") == 0)
     to = svn_path_uri_decode(svn_uri_basename(truefrom, pool), pool);
-  else
-    to = APR_ARRAY_IDX(targets, 1, const char *);
 
   if (! opt_state->quiet)
     SVN_ERR(svn_cl__get_notifier(&ctx->notify_func2, &ctx->notify_baton2,

subversion/tests/cmdline/export_tests.py
@@ -404,6 +404,24 @@ def export_HEADplus1_fails(sbox):
                                      'export', sbox.repo_url, sbox.wc_dir,
                                      '-r', 38956)
 
+def export_to_explicit_cwd(sbox):
+  "export a single file to '.'"
+  sbox.build(create_wc = False, read_only = True)
+
+  svntest.main.safe_rmtree(sbox.wc_dir)
+  expected_output = svntest.tree.build_generic_tree([
+      Item(status='A ').as_node_tuple('iota'),
+    ]).as_state()
+  expected_disk = svntest.tree.build_generic_tree([
+      Item(contents="This is the file 'iota'.\n").as_node_tuple('iota'),
+    ]).as_state()
+
+  os.mkdir(sbox.wc_dir)
+  os.chdir(sbox.wc_dir)
+  svntest.actions.run_and_verify_export(sbox.repo_url + '/iota',
+                                        '.', expected_output,
+                                        expected_disk)
+
 ########################################################################
 # Run the tests
 
@@ -427,6 +445,7 @@ test_list = [ None,
               export_with_state_deleted,
               export_creates_intermediate_folders,
               export_HEADplus1_fails,
+              export_to_explicit_cwd,
              ]
 
 if __name__ == '__main__':

commit 087b3a7edaf9e0be58ef360598f1bda9820d69e1

Fix issue 3727: Fix regression caused by r880559
* subversion/tests/cmdline/export_tests.py
  (export_to_current_dir): Remove XFail.

* subversion/svn/export-cmd.c
  (svn_cl__export): Move logic into subversion/libsvn_client/export.c

* subversion/include/svn_client.h
  (svn_client_export5): Update docstring.

* subversion/libsvn_client/export.c
  (svn_client_export5): If the TO path is empty when exporting an
   single file then use the basename of the URL.

Patch by: Noorul Islam K M <noorul{_AT_}collab.net>
Tweaked by: me


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1029751 13f79535-47bb-0310-9956-ffa450edef68

subversion/include/svn_client.h
@@ -4461,8 +4461,10 @@ svn_client_revprop_list(apr_hash_t **props,
  * @a from is either the path the working copy on disk, or a URL to the
  * repository you wish to export.
  *
- * @a to is the path to the directory where you wish to create the exported
- * tree.
+ * When exporting a directory @a to is the path to the directory where
+ * you wish to create the exported tree, when exporting a file it is
+ * the path of the file that will be created.  If @a to is the empty
+ * path the name of the file/directory in the repository will be used.
  *
  * @a peg_revision is the revision where the path is first looked up
  * when exporting from a repository.  If @a peg_revision->kind is

subversion/libsvn_client/export.c
@@ -993,6 +993,12 @@ svn_client_export5(svn_revnum_t *result_rev,
           apr_hash_index_t *hi;
           struct file_baton *fb = apr_pcalloc(pool, sizeof(*fb));
 
+          if (svn_path_is_empty(to))
+            {
+              to = svn_path_uri_decode(svn_uri_basename(from, NULL), pool);
+              eb->root_path = to;
+            }
+          
           /* Since you cannot actually root an editor at a file, we
            * manually drive a few functions of our editor. */
 

subversion/svn/export-cmd.c
@@ -79,10 +79,7 @@ svn_cl__export(apr_getopt_t *os,
     {
       to = APR_ARRAY_IDX(targets, 1, const char *);
 
-      /* If given the cwd, pretend we weren't given anything. */
-      if (strcmp("", to) == 0)
-        to = svn_path_uri_decode(svn_uri_basename(truefrom, pool), pool);
-      else
+      if (strcmp("", to) != 0)
         /* svn_cl__eat_peg_revisions() but only on one target */
         SVN_ERR(svn_opt__split_arg_at_peg_revision(&to, NULL, to, pool));
     }

subversion/tests/cmdline/export_tests.py
@@ -680,7 +680,7 @@ test_list = [ None,
               export_with_url_unsafe_characters,
               XFail(export_working_copy_with_depths),
               export_externals_with_native_eol,
-              XFail(export_to_current_dir),
+              export_to_current_dir,
              ]
 
 if __name__ == '__main__':