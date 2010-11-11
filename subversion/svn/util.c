9ae2c5c29faaafabfb1186b3c3a767e15463f154
Stefan Sperling <stsp@apache.org>
2010-11-11 12:34:56 +0000

    * subversion/svn/util.c   (svn_cl__print_commit_info): If you're gonna have one, you might as well     have another!
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1033914 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svn/util.c
+++ subversion/svn/util.c
@@ -73,7 +73,7 @@ svn_cl__print_commit_info(const svn_commit_info_t *commit_info,
   if (SVN_IS_VALID_REVNUM(commit_info->revision))
     SVN_ERR(svn_cmdline_printf(pool, _("\nCommitted revision %ld%s.\n"),
                                commit_info->revision,
-                               getenv("SVN_I_LOVE_PANGALACTIC_GARGLE_BLASTER")
+                               getenv("SVN_I_LOVE_PANGALACTIC_GARGLE_BLASTERS")
                                  && commit_info->revision == 42
                                  ?  _(" (the answer to life, the universe, "
                                       "and everything)")
