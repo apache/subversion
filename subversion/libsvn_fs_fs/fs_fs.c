f362b1327e9bc606103b1d62de76e30eaf257714
Stefan Fuhrmann <stefan2@apache.org>
2010-11-22 01:31:20 +0000

    File handle caching causes a potential svn_fs_fs__path_rev_absolute race condition if revision files get deleted during a FSFS session. Mention that in the doc string.
    * subversion/libsvn_fs_fs/fs_fs.c
      (svn_fs_fs__path_rev_absolute): update doc string
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/performance@1037586 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_fs/fs_fs.c
+++ subversion/libsvn_fs_fs/fs_fs.c
@@ -259,7 +259,10 @@ path_rev(svn_fs_t *fs, svn_revnum_t rev, apr_pool_t *pool)
 }
 
 /* Returns the path of REV in FS, whether in a pack file or not.
-   Allocate in POOL. */
+   Allocate in POOL.
+   NOTE. The file given by path is only guaranteed to actually
+   exist if it has not been deleted since this FSFS session has
+   been opened. */
 svn_error_t *
 svn_fs_fs__path_rev_absolute(const char **path,
                              svn_fs_t *fs,
