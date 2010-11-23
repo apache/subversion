3f200c3a3047e9b9d95717cab94142ad4affdb17
Hyrum Kurt Wright <hwright@apache.org>
2010-11-23 19:38:22 +0000

    * tools/dist/collect_sigs.py:   Add license blurb, as well as some ideas on future improvement.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1038297 13f79535-47bb-0310-9956-ffa450edef68

--- tools/dist/collect_sigs.py
+++ tools/dist/collect_sigs.py
@@ -1,4 +1,38 @@
 #!/usr/bin/env python
+#
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+#
+# A script intended to be useful in helping to collect signatures for a
+# release.  This is a pretty rough, and patches are welcome to improve it.
+#
+# Some thoughts about future improvement:
+#  * Store all sigs, etc, in a database
+#    - This helps with idempotence
+#    - Also allows display of per-file and per-release statistics
+#  * A download link on the page itself
+#  * Make the textarea access multiple signatures
+#  * Don't require the signer to specify which file they are signing
+#    - Just check the sign against all the files for that release
+#  * Make use of the python-gpg package (http://code.google.com/p/python-gnupg/)
+#
+
 
 import cgi
 import cgitb
