6e5b3f9208985b24d6dd2321b8754c83e9634481
Hyrum Kurt Wright <hwright@apache.org>
2010-11-23 14:18:07 +0000

    * subversion/libsvn_client/export.c:   Don't attempt to notify if we don't have a notification function.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1038126 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/export.c
+++ subversion/libsvn_client/export.c
@@ -531,13 +531,13 @@ copy_versioned_files(const char *from,
                                       pool));
 
       /* Notify. */
-      {
-        svn_wc_notify_t *notify = svn_wc_create_notify(to_abspath,
-                                                       svn_wc_notify_update_add,
-                                                       pool);
-        notify->kind = svn_node_file;
-        (ctx->notify_func2)(ctx->notify_baton2, notify, pool);
-      }
+      if (ctx->notify_func2)
+        {
+          svn_wc_notify_t *notify = svn_wc_create_notify(to_abspath,
+                                          svn_wc_notify_update_add, pool);
+          notify->kind = svn_node_file;
+          (ctx->notify_func2)(ctx->notify_baton2, notify, pool);
+        }
     }
 
   return SVN_NO_ERROR;
