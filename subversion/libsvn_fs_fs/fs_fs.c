d58cacf65b48c6bb9acf11e97b3a9d54d20e9c1e
Daniel Shahaf <danielsh@apache.org>
2010-12-02 23:13:12 +0000

    Change the returned error code in case of an error, and use that to prevent false errors in -DPACK_AFTER_EVERY_COMMIT-guarded code.
    * subversion/libsvn_fs_fs/fs_fs.c
      (pack_body):
        Return SVN_ERR_UNSUPPORTED_FEATURE instead of SVN_ERR_FS_UNSUPPORTED_FORMAT.
        (I assume the latter means "We don't know what to do with this format number", 
        as opposed to "We know this format and it doesn't support <this> feature".)
    
    * subversion/libsvn_fs/fs-loader.c
      (svn_fs_commit_txn):
        Within a PACK_AFTER_EVERY_COMMIT-guarded block, ignore "Filesystem too
        old; doesn't support packing" errors.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1041633 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_fs/fs_fs.c
+++ subversion/libsvn_fs_fs/fs_fs.c
@@ -7727,8 +7727,9 @@ pack_body(void *baton,
   /* If the repository isn't a new enough format, we don't support packing.
      Return a friendly error to that effect. */
   if (format < SVN_FS_FS__MIN_PACKED_FORMAT)
-    return svn_error_create(SVN_ERR_FS_UNSUPPORTED_FORMAT, NULL,
-      _("FS format too old to pack, please upgrade."));
+    return svn_error_createf(SVN_ERR_UNSUPPORTED_FEATURE, NULL,
+      _("FSFS format (%d) too old to pack, please upgrade."),
+      format);
 
   /* If we aren't using sharding, we can't do any packing, so quit. */
   if (!max_files_per_dir)
