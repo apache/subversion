3f3718b52f28fa520feee1c5b94590a23936ebf5
Julian Foad <julianfoad@apache.org>
2010-12-02 11:40:05 +0000

    * subversion/include/svn_io.h   (svn_io_dir_file_copy): Further clarify the doc string. A follow-up to     r1041102.
    Suggested by: danielsh
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1041336 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_io.h
+++ subversion/include/svn_io.h
@@ -692,11 +692,13 @@ svn_error_t *
 svn_io_file_flush_to_disk(apr_file_t *file,
                           apr_pool_t *pool);
 
-/** Copy the file whose basename or relative path is @a file within
- * directory @a src_path to the same basename or relative path within
- * directory @a dest_path.  Overwrite the destination if it already
- * exists.  Set the destination file's permissions to match those of
- * the source.  Use @a pool for memory allocations.
+/** Copy the file whose basename (or relative path) is @a file within
+ * directory @a src_path to the same basename (or relative path) within
+ * directory @a dest_path.  Overwrite the destination file if it already
+ * exists.  The destination directory (including any directory
+ * components in @a name) must already exist.  Set the destination
+ * file's permissions to match those of the source.  Use @a pool for
+ * memory allocations.
  */
 svn_error_t *
 svn_io_dir_file_copy(const char *src_path,
