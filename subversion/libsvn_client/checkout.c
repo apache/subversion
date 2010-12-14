apache/subversion

commit 3b92ec71f348cc77868fe4440636a24e949f4412

Code simplification.
* subversion/libsvn_client/checkout.c
  (initialize_area): Don't update the WC here, just initialize it.
  (svn_client__checkout_internal): Update the WC here, in all cases.

git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1030755 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_client/checkout.c
@@ -48,18 +48,12 @@
 /*** Public Interfaces. ***/
 
 static svn_error_t *
-initialize_area(svn_revnum_t *result_rev,
-                const char *local_abspath,
-                const svn_opt_revision_t *revision,
+initialize_area(const char *local_abspath,
                 const char *session_url,
                 const char *repos_root,
                 const char *uuid,
                 svn_revnum_t revnum,
                 svn_depth_t depth,
-                svn_boolean_t *use_sleep,
-                svn_boolean_t ignore_externals,
-                svn_boolean_t allow_unver_obstructions,
-                svn_boolean_t innercheckout,
                 svn_client_ctx_t *ctx,
                 apr_pool_t *pool)
 {
@@ -69,13 +63,7 @@ initialize_area(svn_revnum_t *result_rev,
   /* Make the unversioned directory into a versioned one.  */
   SVN_ERR(svn_wc_ensure_adm4(ctx->wc_ctx, local_abspath, session_url,
                              repos_root, uuid, revnum, depth, pool));
-  /* Have update fix the incompleteness. */
-  return svn_error_return(svn_client__update_internal(result_rev, local_abspath,
-                                                      revision, depth, TRUE,
-                                                      ignore_externals,
-                                                      allow_unver_obstructions,
-                                                      use_sleep, innercheckout,
-                                                      ctx, pool));
+  return SVN_NO_ERROR;
 }
 
 
@@ -187,10 +175,8 @@ svn_client__checkout_internal(svn_revnum_t *result_rev,
          entries file should only have an entry for THIS_DIR with a
          URL, revnum, and an 'incomplete' flag.  */
       SVN_ERR(svn_io_make_dir_recursively(local_abspath, pool));
-      err = initialize_area(result_rev, local_abspath, revision, session_url,
-                            repos_root, uuid, revnum, depth, use_sleep,
-                            ignore_externals, allow_unver_obstructions,
-                            innercheckout, ctx, pool);
+      err = initialize_area(local_abspath, session_url,
+                            repos_root, uuid, revnum, depth, ctx, pool);
     }
   else if (kind == svn_node_dir)
     {
@@ -200,10 +186,8 @@ svn_client__checkout_internal(svn_revnum_t *result_rev,
       SVN_ERR(svn_wc_check_wc2(&wc_format, ctx->wc_ctx, local_abspath, pool));
       if (! wc_format)
         {
-          err = initialize_area(result_rev, local_abspath, revision, session_url,
-                                repos_root, uuid, revnum, depth, use_sleep,
-                                ignore_externals, allow_unver_obstructions,
-                                innercheckout, ctx, pool);
+          err = initialize_area(local_abspath, session_url,
+                                repos_root, uuid, revnum, depth, ctx, pool);
         }
       else
         {
@@ -213,17 +197,8 @@ svn_client__checkout_internal(svn_revnum_t *result_rev,
 
           /* If PATH's existing URL matches the incoming one, then
              just update.  This allows 'svn co' to restart an
-             interrupted checkout. */
-          if (strcmp(entry_url, session_url) == 0)
-            {
-              err = svn_client__update_internal(result_rev, local_abspath,
-                                                revision, depth, TRUE,
-                                                ignore_externals,
-                                                allow_unver_obstructions,
-                                                use_sleep, innercheckout,
-                                                ctx, pool);
-            }
-          else
+             interrupted checkout.  Otherwise bail out. */
+          if (strcmp(entry_url, session_url) != 0)
             return svn_error_createf(
                           SVN_ERR_WC_OBSTRUCTED_UPDATE, NULL,
                           _("'%s' is already a working copy for a different URL;"
@@ -237,6 +212,14 @@ svn_client__checkout_internal(svn_revnum_t *result_rev,
                                _("'%s' already exists and is not a directory"),
                                svn_dirent_local_style(local_abspath, pool));
     }
+
+  /* Have update fix the incompleteness. */
+  err = svn_client__update_internal(result_rev, local_abspath,
+                                    revision, depth, TRUE,
+                                    ignore_externals,
+                                    allow_unver_obstructions,
+                                    use_sleep, innercheckout,
+                                    ctx, pool);
 
   if (err)
     {

commit 53256ab3e933d519f6693db107e8488bced39d15

* subversion/libsvn_client/checkout.c   (svn_client__checkout_internal): Fix an error leak that I introduced in     r1030755.
git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1049138 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_client/checkout.c
@@ -211,12 +211,15 @@ svn_client__checkout_internal(svn_revnum_t *result_rev,
     }
 
   /* Have update fix the incompleteness. */
-  err = svn_client__update_internal(result_rev, local_abspath,
-                                    revision, depth, TRUE,
-                                    ignore_externals,
-                                    allow_unver_obstructions,
-                                    use_sleep, innercheckout, FALSE,
-                                    ctx, pool);
+  if (! err)
+    {
+      err = svn_client__update_internal(result_rev, local_abspath,
+                                        revision, depth, TRUE,
+                                        ignore_externals,
+                                        allow_unver_obstructions,
+                                        use_sleep, innercheckout, FALSE,
+                                        ctx, pool);
+    }
 
   if (err)
     {