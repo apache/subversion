abbbc9f7892f0defcfae29ef92918b2c0215918c
C. Michael Pilato <cmpilato@apache.org>
2010-12-20 16:01:23 +0000

    Follow-up to r1050216.
    * subversion/libsvn_ra/util.c
      (svn_ra__release_operational_lock): Note who stole the lock, not
        just that it was stolen.
    
    Suggested by: danielsh
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1051157 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_ra/util.c
+++ subversion/libsvn_ra/util.c
@@ -103,8 +103,9 @@ svn_ra__release_operational_lock(svn_ra_session_t *session,
                                     be_atomic ? &mylocktoken : NULL, NULL,
                                     scratch_pool);
       if (is_atomicity_error(err))
-        return svn_error_quick_wrap(err,
-                                    _("Lock was stolen; unable to remove it"));
+        return svn_error_createf(err->apr_err, err,
+                                 _("Lock was stolen by '%s'; unable to "
+                                   "remove it"), reposlocktoken->data);
     }
 
   return SVN_NO_ERROR;
