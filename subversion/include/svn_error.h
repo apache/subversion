279314da3c5909a35eaa2fcd2fd4cb7e22cc8144
Blair Zajac <blair@apache.org>
2010-12-22 00:05:32 +0000

    Documentation addition.
    * subversion/include/svn_error.h
      (svn_error_purge_tracing):
        Document that the input svn_error_t * can be NULL or SVN_NO_ERROR.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1051703 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_error.h
+++ subversion/include/svn_error.h
@@ -317,7 +317,7 @@ svn_handle_warning(FILE *stream,
  * error tracing placeholders, and return the new top-level error
  * chain item.  @a ERR should be considered unusable after passing
  * through this function, but should *not* be cleared (as the returned
- * error is shares memory with @a ERR).
+ * error is shares memory with @a ERR).  @a ERR can be SVN_NO_ERROR.
  *
  * @since New in 1.7.
  */
