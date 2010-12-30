971755c76dd2f98d2c3dfe1ab595dc5f9fca8caf
Daniel Shahaf <danielsh@apache.org>
2010-12-30 21:18:10 +0000

    Name anonymous structs that my previous glob pattern verb(oversight)ed.
    See r1053996 for details.
    
    * subversion/bindings/swig/ruby/libsvn_swig_ruby/swigutil_rb.c,
      subversion/bindings/swig/python/libsvn_swig_py/swigutil_py.c,
      subversion/bindings/swig/perl/libsvn_swig_perl/swigutil_pl.c,
      contrib/server-side/mod_dontdothat/mod_dontdothat.c,
      contrib/server-side/svnstsw/src/main.c,
      contrib/server-side/svnstsw/src/fso_is_changeable.c,
      tools/client-side/svnmucc/svnmucc.c:
        As above.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1054001 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/swig/python/libsvn_swig_py/swigutil_py.c
+++ subversion/bindings/swig/python/libsvn_swig_py/swigutil_py.c
@@ -1474,7 +1474,7 @@ static svn_error_t *type_conversion_error(const char *datatype)
 /*** Editor Wrapping ***/
 
 /* this baton is used for the editor, directory, and file batons. */
-typedef struct {
+typedef struct item_baton {
   PyObject *editor;     /* the editor handling the callbacks */
   PyObject *baton;      /* the dir/file baton (or NULL for edit baton) */
 } item_baton;
