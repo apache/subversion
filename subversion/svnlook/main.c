39698e712e7a79dcee7ff33705e4717f73b3c990
Hyrum Kurt Wright <hwright@apache.org>
2011-01-03 19:25:25 +0000

    Remove an instance of specifying a non-static format string.
    * subversion/svnlook/main.c
      (display_prop_diffs): Rearrange some variables so as to use a static format
        string in a printf.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1054715 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svnlook/main.c
+++ subversion/svnlook/main.c
@@ -821,7 +821,7 @@ display_prop_diffs(const apr_array_header_t *prop_diffs,
 
   for (i = 0; i < prop_diffs->nelts; i++)
     {
-      const char *header_fmt;
+      const char *header_label;
       const svn_string_t *orig_value;
       const svn_prop_t *pc = &APR_ARRAY_IDX(prop_diffs, i, svn_prop_t);
 
@@ -833,12 +833,12 @@ display_prop_diffs(const apr_array_header_t *prop_diffs,
         orig_value = NULL;
 
       if (! orig_value)
-        header_fmt = "Added: %s\n";
+        header_label = "Added";
       else if (! pc->value)
-        header_fmt = "Deleted: %s\n";
+        header_label = "Deleted";
       else
-        header_fmt = "Modified: %s\n";
-      SVN_ERR(svn_cmdline_printf(pool, header_fmt, pc->name));
+        header_label = "Modified";
+      SVN_ERR(svn_cmdline_printf(pool, "%s: %s\n", header_label, pc->name));
 
       /* Flush stdout before we open a stream to it below. */
       SVN_ERR(svn_cmdline_fflush(stdout));
