apache/subversion

commit 0c6a2863ccaea75321ce0a8208eb97cca71b32cd

Fix syntax inconsistency and update comments.
* subversion/libsvn_client/locking_commands.c
  (organize_lock_targets): Fix syntax inconsistency by removing
    unnecessary braces from 'if' block. Update comments to reflect new
    function names used.

Patch by: Noorul Islam K M <noorul{_AT_}collab.net>


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1054989 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_client/locking_commands.c
@@ -191,18 +191,15 @@ organize_lock_targets(const char **common_parent_url,
 
   /* Get the common parent and all paths */
   if (url_mode)
-    {
       SVN_ERR(svn_uri_condense_targets(common_parent_url, &rel_targets,
                                        targets, TRUE, pool, pool));
-    }
   else
-    {
       SVN_ERR(svn_dirent_condense_targets(common_parent_url, &rel_targets,
                                           targets, TRUE, pool, pool));
-    }
 
-  /* svn_path_condense_targets leaves paths empty if TARGETS only had
-     1 member, so we special case that. */
+  /* svn_uri_condense_targets and svn_dirent_condense_targets leaves
+     URLs/paths empty if TARGETS only had 1 member, so we special case
+     that. */
   if (apr_is_empty_array(rel_targets))
     {
       const char *parent, *base;
@@ -273,8 +270,8 @@ organize_lock_targets(const char **common_parent_url,
       SVN_ERR(svn_uri_condense_targets(&common_url, &rel_urls, urls,
                                        FALSE, pool, pool));
 
-      /* svn_path_condense_targets leaves paths empty if TARGETS only had
-         1 member, so we special case that (again). */
+      /* svn_uri_condense_targets leaves URLs empty if TARGETS only
+         had 1 member, so we special case that (again). */
       if (apr_is_empty_array(rel_urls))
         {
           const char *base_name = svn_uri_basename(common_url, pool);

commit 2028e21291e7b6aaf79904a9873cb776dde6a2c4

Fix wrong indentation caused by r1054989.
* subversion/libsvn_client/locking_commands.c
  (organize_lock_targets): Fix wrong indentation.

Patch by: Noorul Islam K M <noorul{_AT_}collab.net>


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1055092 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_client/locking_commands.c
@@ -191,11 +191,11 @@ organize_lock_targets(const char **common_parent_url,
 
   /* Get the common parent and all paths */
   if (url_mode)
-      SVN_ERR(svn_uri_condense_targets(common_parent_url, &rel_targets,
-                                       targets, TRUE, pool, pool));
+    SVN_ERR(svn_uri_condense_targets(common_parent_url, &rel_targets,
+                                     targets, TRUE, pool, pool));
   else
-      SVN_ERR(svn_dirent_condense_targets(common_parent_url, &rel_targets,
-                                          targets, TRUE, pool, pool));
+    SVN_ERR(svn_dirent_condense_targets(common_parent_url, &rel_targets,
+                                        targets, TRUE, pool, pool));
 
   /* svn_uri_condense_targets and svn_dirent_condense_targets leaves
      URLs/paths empty if TARGETS only had 1 member, so we special case