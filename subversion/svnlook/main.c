98af3deda49724ad6a43447dbe0b1c8e3a14faf0
C. Michael Pilato <cmpilato@apache.org>
2011-01-05 15:01:52 +0000

    Teach the --version output from svnadmin, svnlook, svnserve, svnversion, svnsync, svndumpfilter, and svnrdump to accept --quiet, which forces display of *only* the version number (the way 'svn --version --quiet' behaves).
    * subversion/svndumpfilter/main.c
      (subcommand_help): Pass option baton 'quiet' state to
        svn_opt_print_help3().
      (main): Handle the --quiet option when supplied with --version.
    
    * subversion/svnversion/main.c
      (version): Add 'quiet' parameter, passed to svn_opt_print_help3().
      (main): Delay handling of --version until all options are parsed,
        and pass --quiet state to updated call to version().
    
    * subversion/svnadmin/main.c
      (subcommand_help): Pass option baton 'quiet' state to
        svn_opt_print_help3().
      (main): Handle the --quiet option when supplied with --version.
    
    * subversion/svnlook/main.c
      (options_table): Add support for --quiet/-q.
      (struct svnlook_opt_state): Add 'quiet' boolean member.
      (subcommand_help): Pass option baton 'quiet' state to
        svn_opt_print_help3().
      (main): Handled the --quiet/-q option in general, and specifically when
        supplied with --version.
    
    * subversion/svnsync/main.c
      (help_cmd): Pass option baton 'quiet' state to svn_opt_print_help3().
      (main): Handle the --quiet option when supplied with --version.
    
    * subversion/svnserve/main.c
      (svnserve__options): Add --quiet/-q option.
      (version): Add 'quiet' parameter, passed to svn_opt_print_help3().
      (main): Delay handling of --version until all options are parsed,
        and pass --quiet state to updated call to version().
    
    * subversion/svnrdump/svnrdump.c
      (version): Add 'quiet' parameter, passed to svn_opt_print_help3().
      (main): Handle the --quiet option when supplied with --version.
    
    Suggested by: kameshj
    Patch by: Prabhu Gnana Sundar <prabhugs@collab.net>
              (Tweaked by me.)
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1055478 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svnlook/main.c
+++ subversion/svnlook/main.c
@@ -176,6 +176,9 @@ static const apr_getopt_option_t options_table[] =
                        "                            "
                        "       Ignore changes in EOL style")},
 
+  {"quiet",             'q', 0,
+   N_("no progress (only errors) to stderr")},
+
   {0,                   0, 0, 0}
 };
 
@@ -310,6 +313,7 @@ struct svnlook_opt_state
   svn_boolean_t non_recursive;    /* --non-recursive */
   svn_boolean_t xml;              /* --xml */
   const char *extensions;         /* diff extension args (UTF-8!) */
+  svn_boolean_t quiet;            /* --quiet */
 };
 
 
@@ -2021,7 +2025,8 @@ subcommand_help(apr_getopt_t *os, void *baton, apr_pool_t *pool)
 
   SVN_ERR(svn_opt_print_help3(os, "svnlook",
                               opt_state ? opt_state->version : FALSE,
-                              FALSE, version_footer->data,
+                              opt_state ? opt_state->quiet : FALSE,
+                              version_footer->data,
                               header, cmd_table, options_table, NULL,
                               NULL, pool));
 
@@ -2326,6 +2331,10 @@ main(int argc, const char *argv[])
           opt_state.help = TRUE;
           break;
 
+        case 'q':
+          opt_state.quiet = TRUE;
+          break;
+
         case svnlook__revprop_opt:
           opt_state.revprop = TRUE;
           break;
@@ -2419,6 +2428,7 @@ main(int argc, const char *argv[])
               static const svn_opt_subcommand_desc2_t pseudo_cmd =
                 { "--version", subcommand_help, {0}, "",
                   {svnlook__version,  /* must accept its own option */
+                   'q',
                   } };
 
               subcommand = &pseudo_cmd;
