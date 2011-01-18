f33d98ea8d211f24dd12f3abed31ad208f13e4e5
C. Michael Pilato <cmpilato@apache.org>
2011-01-18 19:00:38 +0000

    On the 'uris-as-urls' branch, remove some soon-to-be-pointless functions.
    * trunk/subversion/include/svn_dirent_uri.h
      (svn_url_is_absolute, svn_url_join): Remove.
    
    * trunk/subversion/libsvn_subr/dirent_uri.c
      (svn_url_is_absolute, svn_url_join): Remove.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/uris-as-urls@1060539 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/dirent_uri.c
+++ subversion/libsvn_subr/dirent_uri.c
@@ -1213,55 +1213,6 @@ svn_relpath_join(const char *base,
 }
 
 char *
-svn_url_join(const char *base, const char *component, apr_pool_t *pool)
-{
-  apr_size_t blen = strlen(base);
-  apr_size_t clen = strlen(component);
-  char *path;
-
-  assert(svn_uri_is_canonical(base, pool));
-  assert(svn_uri_is_canonical(component, pool));
-
-  /* If either is empty return the other */
-  if (SVN_PATH_IS_EMPTY(base))
-    return apr_pmemdup(pool, component, clen + 1);
-  if (SVN_PATH_IS_EMPTY(component))
-    return apr_pmemdup(pool, base, blen + 1);
-
-  /* If the component is absolute, then return it.  */
-  if (svn_uri_is_absolute(component))
-    {
-      if (*component != '/')
-        return apr_pmemdup(pool, component, clen + 1);
-      else
-        {
-          /* The uri is not absolute enough; use only the root from base */
-          apr_size_t n = uri_schema_root_length(base, blen);
-
-          path = apr_palloc(pool, n + clen + 1);
-
-          if (n > 0)
-            memcpy(path, base, n);
-
-          memcpy(path + n, component, clen + 1); /* Include '\0' */
-
-          return path;
-        }
-    }
-
-  if (blen == 1 && base[0] == '/')
-    blen = 0; /* Ignore base, just return separator + component */
-
-  /* Construct the new, combined path. */
-  path = apr_palloc(pool, blen + 1 + clen + 1);
-  memcpy(path, base, blen);
-  path[blen] = '/';
-  memcpy(path + blen + 1, component, clen + 1);
-
-  return path;
-}
-
-char *
 svn_dirent_dirname(const char *dirent, apr_pool_t *pool)
 {
   apr_size_t len = strlen(dirent);
@@ -1639,17 +1590,6 @@ svn_dirent_is_absolute(const char *dirent)
 #endif /* SVN_USE_DOS_PATHS */
 
   return FALSE;
-}
-
-svn_boolean_t
-svn_url_is_absolute(const char *uri)
-{
-  /* uri is absolute if it starts with '/' */
-  if (uri && uri[0] == '/')
-    return TRUE;
-
-  /* URLs are absolute. */
-  return svn_path_is_url(uri);
 }
 
 svn_error_t *
