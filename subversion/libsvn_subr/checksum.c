587207c3d0cdc2c987594224e7bfdd8cc8684314
Hyrum Kurt Wright <hwright@apache.org>
2011-02-04 15:20:50 +0000

    Allow callers of svn_checksum_mismatch_err() to specify the first bit of their error message.  Also, remove a call to apr_psprintf when generating the error message.
    * subversion/libsvn_subr/checksum.c
      (svn_checksum_mismatch_err): Use varargs to generate the error message.
    
    * subversion/include/svn_checksum.h
      (svn_checksum_mismatch_err): Update parameters to accept a varargs message.
    
    * subversion/libsvn_client/export.c
      (close_file): Update caller.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1067195 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/checksum.c
+++ subversion/libsvn_subr/checksum.c
@@ -402,21 +402,28 @@ svn_checksum_size(const svn_checksum_t *checksum)
 }
 
 svn_error_t *
-svn_checksum_mismatch_err(const char *object_label,
-                          const svn_checksum_t *expected,
+svn_checksum_mismatch_err(const svn_checksum_t *expected,
                           const svn_checksum_t *actual,
-                          apr_pool_t *scratch_pool)
+                          apr_pool_t *scratch_pool,
+                          const char *fmt,
+                          ...)
 {
   if (!svn_checksum_match(expected, actual))
     {
+      va_list ap;
+      const char *desc;
+
+      va_start(ap, fmt);
+      desc = apr_pvsprintf(scratch_pool, fmt, ap);
+      va_end(ap);
+    
       return svn_error_createf(SVN_ERR_CHECKSUM_MISMATCH, NULL,
-             apr_psprintf(scratch_pool, "%s:\n%s\n%s\n",
-                          _("Checksum mismatch for '%s'\n"),
-                          _("   expected:  %s"),
-                          _("     actual:  %s")),
-             object_label,
-             svn_checksum_to_cstring_display(expected, scratch_pool),
-             svn_checksum_to_cstring_display(actual, scratch_pool));
+                               _("%s:\n"
+                                 "   expected:  %s\n"
+                                 "     actual:  %s\n"),
+                    desc,               
+                    svn_checksum_to_cstring_display(expected, scratch_pool),
+                    svn_checksum_to_cstring_display(actual, scratch_pool));
     }
   else
     return SVN_NO_ERROR;
