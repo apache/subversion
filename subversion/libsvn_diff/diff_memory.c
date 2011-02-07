36cada9ea8e0b498c6a5ab2071cb63d9f02f1926
Hyrum Kurt Wright <hwright@apache.org>
2011-02-07 15:12:22 +0000

    To assist swig, use pointer, rather than array, syntax when declaring a function parameter.
    * subversion/include/svn_diff.h
      (datasources_open): As above.
    
    * subversion/libsvn_diff/diff_memory.c
      (datasources_open): Same.
    
    * subversion/libsvn_diff/diff_file.c
      (datasources_open): Same.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1067960 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_diff/diff_memory.c
+++ subversion/libsvn_diff/diff_memory.c
@@ -91,7 +91,7 @@ datasource_to_index(svn_diff_datasource_e datasource)
 /* Implements svn_diff_fns2_t::datasources_open */
 static svn_error_t *
 datasources_open(void *baton, apr_off_t *prefix_lines,
-                 svn_diff_datasource_e datasources[],
+                 svn_diff_datasource_e *datasources,
                  apr_size_t datasources_len)
 {
   /* Do nothing: everything is already there and initialized to 0 */
