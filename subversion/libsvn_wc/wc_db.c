465000b05011dffbdd85b2028951e7e2133bf43c
Philip Martin <philip@apache.org>
2011-02-10 10:29:58 +0000

    The new revert is currently non-recursive so don't revert copies with children.
    * subversion/libsvn_wc/wc_db.c
      (op_revert_txn): Don't revert copies that have children.
    
    * subversion/libsvn_wc/wc_db.h
      (svn_wc__db_op_revert): Tweak docstring.
    
    * subversion/tests/libsvn_wc/op-depth-test.c
      (revert): Test revert on copies.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1069329 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/wc_db.c
+++ subversion/libsvn_wc/wc_db.c
@@ -3584,36 +3584,24 @@ op_revert_txn(void *baton, svn_sqlite__db_t *sdb, apr_pool_t *scratch_pool)
                                                         b->local_relpath,
                                                         scratch_pool));
 
-      /* Check for higher op-depth children */
-      SVN_ERR(svn_sqlite__get_statement(&stmt, b->pdh->wcroot->sdb,
-                                     STMT_SELECT_NODES_GE_OP_DEPTH_RECURSIVE));
-      SVN_ERR(svn_sqlite__bindf(stmt, "issi", b->pdh->wcroot->wc_id,
-                                b->local_relpath,
-                                construct_like_arg(b->local_relpath,
-                                                   scratch_pool),
-                                op_depth + 1));
-      SVN_ERR(svn_sqlite__step(&have_row, stmt));
-      if (have_row)
-        return svn_error_createf(SVN_ERR_WC_INVALID_OPERATION_DEPTH, NULL,
-                                 _("Can't revert tree change for '%s' without"
-                                   " reverting children"),
-                                 path_for_error_message(b->pdh->wcroot,
-                                                        b->local_relpath,
-                                                        scratch_pool));
-
       SVN_ERR(convert_to_working_status(&status, status));
 
       if (status == svn_wc__db_status_added)
         {
-          SVN_ERR(remove_children(b->pdh, b->local_relpath,
-                                  svn_wc__db_status_normal,
-                                  op_depth, scratch_pool));
-          SVN_ERR(remove_children(b->pdh, b->local_relpath,
-                                  svn_wc__db_status_not_present,
-                                  op_depth, scratch_pool));
-          SVN_ERR(remove_children(b->pdh, b->local_relpath,
-                                  svn_wc__db_status_incomplete,
-                                  op_depth, scratch_pool));
+          /* Check for children */
+          SVN_ERR(svn_sqlite__get_statement(&stmt, b->pdh->wcroot->sdb,
+                                            STMT_SELECT_NODE_CHILDREN));
+          SVN_ERR(svn_sqlite__bindf(stmt, "is", b->pdh->wcroot->wc_id,
+                                    b->local_relpath));
+          SVN_ERR(svn_sqlite__step(&have_row, stmt));
+          SVN_ERR(svn_sqlite__reset(stmt));
+          if (have_row)
+            return svn_error_createf(SVN_ERR_WC_INVALID_OPERATION_DEPTH, NULL,
+                                     _("Can't revert tree change for '%s'"
+                                       " without reverting children"),
+                                     path_for_error_message(b->pdh->wcroot,
+                                                            b->local_relpath,
+                                                            scratch_pool));
         }
       else if (status == svn_wc__db_status_deleted)
         {
