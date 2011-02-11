79dafe1f9f8c9e13c055953155ca0c664844a5ff
Ivan Zhakov <ivan@apache.org>
2011-02-11 10:52:05 +0000

    Optimize svn_string_t creation by allocating memory for svn_string_t structure and data in one chunk.
    * subversion/libsvn_subr/svn_string.c
      (svn_string_ncreate): Allocate memory for svn_string_t and data in one 
       chunk.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1069743 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/svn_string.c
+++ subversion/libsvn_subr/svn_string.c
@@ -135,9 +135,18 @@ create_string(const char *data, apr_size_t size,
 svn_string_t *
 svn_string_ncreate(const char *bytes, apr_size_t size, apr_pool_t *pool)
 {
+  void *mem;
   char *data;
+  svn_string_t *new_string;
 
-  data = apr_palloc(pool, size + 1);
+  /* Allocate memory for svn_string_t and data in one chunk. */
+  mem = apr_palloc(pool, sizeof(*new_string) + size + 1);
+  data = (char*)mem + sizeof(*new_string);
+
+  new_string = mem;
+  new_string->data = data;
+  new_string->len = size;
+
   memcpy(data, bytes, size);
 
   /* Null termination is the convention -- even if we suspect the data
@@ -145,8 +154,7 @@ svn_string_ncreate(const char *bytes, apr_size_t size, apr_pool_t *pool)
      call.  Heck, that's why they call it the caller! */
   data[size] = '\0';
 
-  /* wrap an svn_string_t around the new data */
-  return create_string(data, size, pool);
+  return new_string;
 }
 
 
