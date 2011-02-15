fb5cae6419c3f685fd5f885a22e38ab52ee678be
Hyrum Kurt Wright <hwright@apache.org>
2011-02-15 11:29:00 +0000

    Move the "check for mismatch" bit out of the svn_checkum_mismatch_err() function, so that we don't have to do path localization unconditionally.
    * subversion/libsvn_subr/checksum.c
      (svn_checksum_mismatch_err): Don't check for the mismatch, just create the
        error.
    
    * subversion/include/svn_checksum.h
      (svn_checksum_mismatch_err): Update docs.
    
    * subversion/libsvn_client/export.c
      (close_file): Restore checkum mismatch check here, but use the helper to
        create the error.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1070847 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_checksum.h
+++ subversion/include/svn_checksum.h
@@ -241,9 +241,12 @@ svn_checksum_size(const svn_checksum_t *checksum);
 
 
 /**
- * Return an error of type #SVN_ERR_CHECKSUM_MISMATCH if @a actual and 
- * @a expected checksums do not match, otherwise, return #SVN_NO_ERROR.
- * Use @a fmt, and the following parameters to populate the error message.
+ * Return an error of type #SVN_ERR_CHECKSUM_MISMATCH for @a actual and 
+ * @a expected checksums which do not match.  Use @a fmt, and the following
+ * parameters to populate the error message.
+ *
+ * @note This function does not actually check for the mismatch, it just
+ * constructs the error.
  *
  * @a scratch_pool is used for temporary allocations; the returned error
  * will be allocated in its own pool (as is typical).
