53ae15ad9dbf4e5151d7fbb1b3ec11d2f8703290
Daniel Shahaf <danielsh@apache.org>
2011-02-22 16:13:19 +0000

    Add a test of svn_subst_translate_string2() to the subst_translate test suite.
    As discussed at:
      http://thread.gmane.org/gmane.comp.version-control.subversion.devel/125782
        Message-ID: <AANLkTimo=NpF_FqB+PBPkeifXjbnsORGYkZ5npCAxyVz@mail.gmail.com>
    
    Patch by: Danny Trebbien <dtrebbien@gmail.com>
    
    * subversion/tests/libsvn_subr/subst_translate-test.c
      (locale.h, string.h): Include these.
      (ARRAY_LEN): New macro.
      (test_svn_subst_translate_string2_null_encoding_helper): New function. It is
        the core of the new svn_subst_translate_string2_null_encoding() test.
      (test_svn_subst_translate_string2_null_encoding): New test that tests
        svn_subst_translate_string2() with ENCODING set to NULL.
      (test_funcs): Add test_svn_subst_translate_string2_null_encoding().
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1073377 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/libsvn_subr/subst_translate-test.c
+++ subversion/tests/libsvn_subr/subst_translate-test.c
@@ -21,11 +21,16 @@
  * ====================================================================
  */
 
+#include <locale.h>
+#include <string.h>
+
 #include "../svn_test.h"
 
 #include "svn_types.h"
 #include "svn_string.h"
 #include "svn_subst.h"
+
+#define ARRAY_LEN(ary) ((sizeof (ary)) / (sizeof ((ary)[0])))
 
 /* Test inputs and expected output for svn_subst_translate_string2(). */
 struct translate_string2_data_t
@@ -97,6 +102,83 @@ test_svn_subst_translate_string2(apr_pool_t *pool)
     }
 
   return SVN_NO_ERROR;
+}
+
+/* The body of the svn_subst_translate_string2_null_encoding test. It should
+   only be called by test_svn_subst_translate_string2_null_encoding(), as this
+   code assumes that the process locale has been changed to a locale that uses
+   either CP-1252 or ISO-8859-1 for the default narrow string encoding. */
+static svn_error_t *
+test_svn_subst_translate_string2_null_encoding_helper(apr_pool_t *pool)
+{
+  {
+    svn_string_t *new_value = NULL;
+    svn_boolean_t translated_to_utf8 = FALSE;
+    svn_boolean_t translated_line_endings = TRUE;
+    /* 'Æ', which is 0xc6 in both ISO-8859-1 and Windows-1252 */
+    svn_string_t *source_string = svn_string_create("\xc6", pool);
+
+    SVN_ERR(svn_subst_translate_string2(&new_value, &translated_to_utf8,
+                                        &translated_line_endings,
+                                        source_string, NULL, FALSE,
+                                        pool, pool));
+    SVN_TEST_STRING_ASSERT(new_value->data, "\xc3\x86");
+    SVN_TEST_ASSERT(translated_to_utf8 == TRUE);
+    SVN_TEST_ASSERT(translated_line_endings == FALSE);
+  }
+
+  return SVN_NO_ERROR;
+}
+
+/* Test that when ENCODING is NULL, the system-default language encoding is used.
+
+   This is a wrapper of test_svn_subst_translate_string2_null_encoding_helper()
+   that ensures that the system-default character encoding is set to either
+   CP-1252 or ISO-8859-1 before test_svn_subst_translate_string2_null_encoding_helper()
+   is called, later restoring the original system-default character encoding. */
+static svn_error_t *
+test_svn_subst_translate_string2_null_encoding(apr_pool_t *pool)
+{
+  char orig_lc_all[256] = { '\0' };
+  svn_error_t *test_result;
+
+  const char *other_locales[] =
+    {
+      /* For Windows' msvcrt */
+      "English.1252", "German.1252", "French.1252",
+
+      /* For glibc */
+      "en_US.ISO-8859-1", "en_GB.ISO-8859-1", "de_DE.ISO-8859-1",
+
+      /* For OpenBSD's libc */
+      "en_US.ISO8859-1",  "en_GB.ISO8859-1",  "de_DE.ISO8859-1",
+
+      /* Must be last */
+      NULL
+    };
+  const char **other_locale;
+
+  strncpy(orig_lc_all, setlocale(LC_ALL, NULL), sizeof (orig_lc_all));
+
+  for (other_locale = other_locales; *other_locale != NULL; ++other_locale)
+  {
+    if (setlocale(LC_ALL, *other_locale))
+      break;
+  }
+
+  /* If the end of the list of other locales to try has been reached, then none
+     of the tested locales are installed, so the test must be skipped. */
+  if (*other_locale == NULL)
+    return svn_error_createf(SVN_ERR_TEST_SKIPPED, NULL,
+                             "Tried %d locales, but none are installed.",
+                             (int) (ARRAY_LEN(other_locales) - 1));
+
+  test_result = test_svn_subst_translate_string2_null_encoding_helper(pool);
+
+  /* Restore the original locale for category LC_ALL. */
+  SVN_TEST_ASSERT(setlocale(LC_ALL, orig_lc_all) != NULL);
+
+  return test_result;
 }
 
 static svn_error_t *
@@ -171,6 +253,8 @@ struct svn_test_descriptor_t test_funcs[] =
     SVN_TEST_NULL,
     SVN_TEST_PASS2(test_svn_subst_translate_string2,
                    "test svn_subst_translate_string2()"),
+    SVN_TEST_PASS2(test_svn_subst_translate_string2_null_encoding,
+                   "test svn_subst_translate_string2(encoding = NULL)"),
     SVN_TEST_PASS2(test_repairing_svn_subst_translate_string2,
                    "test repairing svn_subst_translate_string2()"),
     SVN_TEST_PASS2(test_svn_subst_translate_cstring2,
