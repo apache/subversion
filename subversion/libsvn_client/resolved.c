3700ce8423a91e5567feeee543830334a700e4d7
Greg Stein <gstein@apache.org>
2011-02-23 21:37:47 +0000

    Small change lying around...
    * subversion/libsvn_client/resolved.c:
      (svn_client_resolve): clarify some params
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1073955 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/resolved.c
+++ subversion/libsvn_client/resolved.c
@@ -56,7 +56,10 @@ svn_client_resolve(const char *path,
   SVN_ERR(svn_dirent_get_absolute(&local_abspath, path, pool));
 
   SVN_ERR(svn_wc_resolved_conflict5(ctx->wc_ctx, local_abspath,
-                                    depth, TRUE, "", TRUE,
+                                    depth,
+                                    TRUE /* resolve_text */,
+                                    "" /* resolve_prop (ALL props) */,
+                                    TRUE /* resolve_tree */,
                                     conflict_choice,
                                     ctx->cancel_func, ctx->cancel_baton,
                                     ctx->notify_func2, ctx->notify_baton2,
