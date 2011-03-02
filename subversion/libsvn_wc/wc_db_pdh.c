apache/subversion

commit 4f2af58e628a4fb66b74cc4d8ea31ab4ef6ff5d1

* subversion/libsvn_wc/wc_db_pdh.c   (pdh_parse_local_abspath): Followup to r1076093 by further allocating bits of     the PDH in the result pool, rather than the db->state_pool.
git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1076098 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_wc/wc_db_pdh.c
@@ -350,7 +350,7 @@ pdh_parse_local_abspath(db_pdh_t **pdh,
   if (wcroot != NULL)
     {
       *pdh = apr_pcalloc(result_pool, sizeof(**pdh));
-      (*pdh)->local_abspath = apr_pstrdup(db->state_pool, local_abspath);
+      (*pdh)->local_abspath = apr_pstrdup(result_pool, local_abspath);
       (*pdh)->wcroot = wcroot;
     }
   else
@@ -397,7 +397,7 @@ pdh_parse_local_abspath(db_pdh_t **pdh,
       if (wcroot != NULL)
         {
           *pdh = apr_pcalloc(result_pool, sizeof(**pdh));
-          (*pdh)->local_abspath = apr_pstrdup(db->state_pool, local_abspath);
+          (*pdh)->local_abspath = apr_pstrdup(result_pool, local_abspath);
           (*pdh)->wcroot = wcroot;
         }
 
@@ -437,7 +437,7 @@ pdh_parse_local_abspath(db_pdh_t **pdh,
   if (*pdh == NULL)
     {
       *pdh = apr_pcalloc(result_pool, sizeof(**pdh));
-      (*pdh)->local_abspath = apr_pstrdup(db->state_pool, local_abspath);
+      (*pdh)->local_abspath = apr_pstrdup(result_pool, local_abspath);
     }
   else
     {

commit 1c391510fc98b93ff41c11a08292935b0a8a0465

Fix a pool lifetime issue introduced in r1076098.
* subversion/libsvn_wc/wc_db_pdh.c:
  (pdh_parse_local_abspath): dup the hash key for dir_data into the proper
    pool.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1076161 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_wc/wc_db_pdh.c
@@ -559,8 +559,12 @@ pdh_parse_local_abspath(db_pdh_t **pdh,
   }
 
   /* The PDH is complete. Stash it into DB.  */
+  /* ### we may have the path in the right pool already (in another var),
+     ### but this PDH stuff is short-lived. just dup it (for now) into the
+     ### correct lifetime.  */
   apr_hash_set(db->dir_data,
-               (*pdh)->local_abspath, APR_HASH_KEY_STRING,
+               apr_pstrdup(db->state_pool, (*pdh)->local_abspath),
+               APR_HASH_KEY_STRING,
                (*pdh)->wcroot);
 
   /* Did we traverse up to parent directories?  */