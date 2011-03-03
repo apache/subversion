a53a65889a57228e39218000eb3a5195826392f7
Philip Martin <philip@apache.org>
2011-03-03 23:07:35 +0000

    Fix a memory problem identified by valgrind.
    * subversion/libsvn_repos/reporter.c
      (get_revision_info): Don't use stack memory for hash key.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1076872 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_repos/reporter.c
+++ subversion/libsvn_repos/reporter.c
@@ -484,7 +484,7 @@ get_revision_info(report_baton_t *b,
       info->author = author ? svn_string_dup(author, b->pool) : NULL;
 
       /* Cache it */
-      apr_hash_set(b->revision_infos, &rev, sizeof(rev), info);
+      apr_hash_set(b->revision_infos, &info->rev, sizeof(rev), info);
     }
 
   *revision_info = info;
