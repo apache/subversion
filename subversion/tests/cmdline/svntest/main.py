0d304c6d3fb449560499fc402a5e5ec357866c1c
Julian Foad <julianfoad@apache.org>
2011-03-11 08:57:26 +0000

    Make the Python test suite raise an error if any command it runs yields a non-zero exit code unexpectedly.  This extends the present check which already raises an error if the command produces any stderr output unexpectedly.
    * subversion/tests/cmdline/svntest/main.py
      (run_command_stdin): Raise an error if the exit code is non-zero.
      (run_command, run_svn): Update doc strings accordingly.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1080491 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/svntest/main.py
+++ subversion/tests/cmdline/svntest/main.py
@@ -335,7 +335,8 @@ def run_command(command, error_expected, binary_mode=0, *varargs):
   """Run COMMAND with VARARGS. Return exit code as int; stdout, stderr
   as lists of lines (including line terminators).  See run_command_stdin()
   for details.  If ERROR_EXPECTED is None, any stderr output will be
-  printed and will raise an exception."""
+  printed and any stderr output or a non-zero exit code will raise an
+  exception."""
 
   return run_command_stdin(command, error_expected, 0, binary_mode,
                            None, *varargs)
@@ -490,8 +491,8 @@ def run_command_stdin(command, error_expected, bufsize=0, binary_mode=0,
   Normalize Windows line endings of stdout and stderr if not BINARY_MODE.
   Return exit code as int; stdout, stderr as lists of lines (including
   line terminators).
-  If ERROR_EXPECTED is None, any stderr output will be printed and will
-  raise an exception."""
+  If ERROR_EXPECTED is None, any stderr output will be printed and any
+  stderr output or a non-zero exit code will raise an exception."""
 
   if options.verbose:
     start = time.time()
@@ -510,7 +511,7 @@ def run_command_stdin(command, error_expected, bufsize=0, binary_mode=0,
     for x in stderr_lines:
       sys.stdout.write(x)
 
-  if (not error_expected) and (stderr_lines):
+  if (not error_expected) and ((stderr_lines) or (exit_code != 0)):
     if not options.verbose:
       for x in stderr_lines:
         sys.stdout.write(x)
@@ -577,7 +578,8 @@ def _with_auth(args):
 def run_svn(error_expected, *varargs):
   """Run svn with VARARGS; return exit code as int; stdout, stderr as
   lists of lines (including line terminators).  If ERROR_EXPECTED is
-  None, any stderr output will be printed and will raise an exception.  If
+  None, any stderr output will be printed and any stderr output or a
+  non-zero exit code will raise an exception.  If
   you're just checking that something does/doesn't come out of
   stdout/stderr, you might want to use actions.run_and_verify_svn()."""
   return run_command(svn_binary, error_expected, 0,
