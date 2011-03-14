4d73a6b44e349eac3ac157f415a726926c1b8a97
Bert Huiben <rhuijben@apache.org>
2011-03-14 21:24:01 +0000

    Perform some converstions from db, abspath to wcroot, relpath in the post update bump handling.
    * subversion/libsvn_wc/wc_db.c
      (set_rev_relpath_baton): Move up to right above its single caller and store
        repos_id instead of url and uuid.
      (set_rev_relpath_txn): Use repos_id instead of url and uuid.
      (db_op_set_rev_and_repos_relpath): Moved up from
        svn_wc__db_temp_op_set_rev_and_repos_relpath and take wcroot, relpath
        and repos_id.
      (tweak_node): Update caller.
      (tweak_entries): Use a few wcroot apis instead of using the public db apis.
      (svn_wc__db_temp_op_set_rev_and_repos_relpath): Remove function.
    
    * subversion/libsvn_wc/wc_db.h
      (svn_wc__db_temp_op_set_rev_and_repos_relpath): Remove function.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1081569 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/wc_db.h
+++ subversion/libsvn_wc/wc_db.h
@@ -2479,21 +2479,6 @@ svn_wc__db_temp_op_set_property_conflict_marker_file(svn_wc__db_t *db,
                                                      const char *prej_basename,
                                                      apr_pool_t *scratch_pool);
 
-/* Sets a base nodes revision and/or repository relative path. If
-   LOCAL_ABSPATH's rev (REV) is valid, set is revision and if SET_REPOS_RELPATH
-   is TRUE set its repository relative path to REPOS_RELPATH (and make sure its
-   REPOS_ROOT_URL and REPOS_ROOT_UUID are still valid).
- */
-svn_error_t *
-svn_wc__db_temp_op_set_rev_and_repos_relpath(svn_wc__db_t *db,
-                                             const char *local_abspath,
-                                             svn_revnum_t rev,
-                                             svn_boolean_t set_repos_relpath,
-                                             const char *repos_relpath,
-                                             const char *repos_root_url,
-                                             const char *repos_uuid,
-                                             apr_pool_t *scratch_pool);
-
 /* Tweak a locally added existing directory LOCAL_ABSPATH to have a base
    node with incomplete status and revision REVISION instead. If
    REPOS_RELPATH is not NULL, apply REPOS_RELPATH, REPOS_ROOT_URL and
