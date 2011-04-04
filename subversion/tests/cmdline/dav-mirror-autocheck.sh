apache/subversion

commit f3173c71264359d7692767d28dc948090ee515fd

With the below apache configuration(See the <space> character "/svn 1/").
<Location "/svn 1/">
  DAV svn
  SVNParentPath /repositories
</Location>
<Location "/svn 2/">
  DAV svn
  SVNParentPath /repositories-slave
  SVNMasterURI "http://localhost/svn 1"
</Location>

Write through proxy is *not* happening and commit happens *directly* inside the slave.

* subversion/mod_dav_svn/mirror.c
(proxy_request_fixup): URI encode the to be proxied file name.
(dav_svn__proxy_request_fixup): r->unparsed_uri is in url encoded form while
root_dir is not in encoded form. So use r->uri to compare with root_dir.
(dav_svn__location_in_filter): URL Encode the 'find & replace' urls as
the request body has it in url encoded format.
(dav_svn__location_header_filter): Encode the master_uri as the response from
master has the Location header url encoded already. Set the outgoing Location
header url encoded.
(dav_svn__location_body_filter): URL Encode the 'find & replace' urls as
the response body has it in url encoded format.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@917523 13f79535-47bb-0310-9956-ffa450edef68

subversion/mod_dav_svn/mirror.c
@@ -45,8 +45,10 @@ static void proxy_request_fixup(request_rec *r,
 
     r->proxyreq = PROXYREQ_REVERSE;
     r->uri = r->unparsed_uri;
-    r->filename = apr_pstrcat(r->pool, "proxy:", master_uri,
-                              uri_segment, NULL);
+    r->filename = (char *) svn_path_uri_encode(apr_pstrcat(r->pool, "proxy:",
+                                                           master_uri,
+                                                           uri_segment,
+                                                           NULL), r->pool);
     r->handler = "proxy-server";
     ap_add_output_filter("LocationRewrite", NULL, r, r->connection);
     ap_add_output_filter("ReposRewrite", NULL, r, r->connection);
@@ -78,7 +80,7 @@ int dav_svn__proxy_request_fixup(request_rec *r)
            transaction tree resouces. */
         if (r->method_number == M_PROPFIND ||
             r->method_number == M_GET) {
-            if ((seg = ap_strstr(r->unparsed_uri, root_dir))) {
+            if ((seg = ap_strstr(r->uri, root_dir))) {
                 if (ap_strstr_c(seg, apr_pstrcat(r->pool, special_uri,
                                                  "/wrk/", NULL))
                     || ap_strstr_c(seg, apr_pstrcat(r->pool, special_uri,
@@ -95,7 +97,7 @@ int dav_svn__proxy_request_fixup(request_rec *r)
         /* If this is a write request aimed at a public URI (such as
            MERGE, LOCK, UNLOCK, etc.) or any as-yet-unhandled request
            using a "special URI", we have to doctor it a bit for proxying. */
-        seg = ap_strstr(r->unparsed_uri, root_dir);
+        seg = ap_strstr(r->uri, root_dir);
         if (seg && (r->method_number == M_MERGE ||
                     r->method_number == M_LOCK ||
                     r->method_number == M_UNLOCK ||
@@ -158,6 +160,11 @@ apr_status_t dav_svn__location_in_filter(ap_filter_t *f,
        ### PUT requests and properties in PROPPATCH requests.
        ### See issue #3445 for details. */
 
+    /* We are url encoding the current url and the master url
+       as incoming(from client) request body has it encoded already. */
+    canonicalized_uri = (char *) svn_path_uri_encode(canonicalized_uri,
+                                                     r->pool);
+    root_dir = (char *) svn_path_uri_encode(root_dir, r->pool);
     if (!f->ctx) {
         ctx = f->ctx = apr_pcalloc(r->pool, sizeof(*ctx));
         ctx->remotepath = canonicalized_uri;
@@ -216,6 +223,7 @@ apr_status_t dav_svn__location_header_filter(ap_filter_t *f,
     /* Don't filter if we're in a subrequest or we aren't setup to
        proxy anything. */
     master_uri = dav_svn__get_master_uri(r);
+    master_uri = (char *) svn_path_uri_encode(master_uri, r->pool);
     if (r->main || !master_uri) {
         ap_remove_output_filter(f);
         return ap_pass_brigade(f->next, bb);
@@ -233,6 +241,7 @@ apr_status_t dav_svn__location_header_filter(ap_filter_t *f,
                                                dav_svn__get_root_dir(r), "/",
                                                start_foo, NULL),
                                    r);
+        new_uri = (char *) svn_path_uri_encode(new_uri, r->pool);
         apr_table_set(r->headers_out, "Location", new_uri);
     }
     return ap_pass_brigade(f->next, bb);
@@ -274,6 +283,11 @@ apr_status_t dav_svn__location_body_filter(ap_filter_t *f,
        ### they return in the process of trying to do URI fix-ups.
        ### See issue #3445 for details. */
 
+    /* We are url encoding the current url and the master url
+       as incoming(from master) request body has it encoded already. */
+    canonicalized_uri = (char *) svn_path_uri_encode(canonicalized_uri,
+                                                     r->pool);
+    root_dir = (char *) svn_path_uri_encode(root_dir, r->pool);
     if (!f->ctx) {
         ctx = f->ctx = apr_pcalloc(r->pool, sizeof(*ctx));
         ctx->remotepath = canonicalized_uri;

commit 3a38a749f803d8c87fc365471f614bf01bce1e1b

Fix the regression issue triggered by r917523. The revision r917523 do some url encodings to the paths and uris which are not url-encoded. But there is one additional url-encoding of an uri which is already encoded. With this extra encoding, committing a path to slave which has space in it fails.
* subversion/tests/cmdline/dav-mirror-autocheck.sh
  Add a testcase for a regression issue triggered by r917523.

* subversion/mod_dav_svn/mirror.c
  (dav_svn__location_header_filter): Remove redundant url-encoding of new_uri.

Patch by: Vijayaguru G <vijay{_AT_}collab.net>                                     


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1088602 13f79535-47bb-0310-9956-ffa450edef68

subversion/mod_dav_svn/mirror.c
@@ -241,7 +241,6 @@ apr_status_t dav_svn__location_header_filter(ap_filter_t *f,
                                                dav_svn__get_root_dir(r), "/",
                                                start_foo, (char *)NULL),
                                    r);
-        new_uri = svn_path_uri_encode(new_uri, r->pool);
         apr_table_set(r->headers_out, "Location", new_uri);
     }
     return ap_pass_brigade(f->next, bb);

subversion/tests/cmdline/dav-mirror-autocheck.sh
@@ -416,10 +416,6 @@ $svnmucc rm "$BASE_URL/branch" cp 2 "$BASE_URL/trunk" "$BASE_URL/branch" put /de
 say "svn log on $BASE_URL : "
 $SVN --username jrandom --password rayjandom log -vq "$BASE_URL"
 
-# shut it down
-echo -n "${SCRIPT}: stopping httpd: "
-$HTTPD -f $HTTPD_CONFIG -k stop
-echo "."
 
 # verify result: should be at rev 4 in both repos
 # FIXME: do more rigorous verification here
@@ -438,5 +434,29 @@ fi
 
 say "PASS: master, slave are both at r4, as expected"
 
-exit 0
+# The following test case is for the regression issue triggered by r917523.
+# The revision r917523 do some url encodings to the paths and uris which are
+# not url-encoded. But there is one additional url-encoding of an uri which is
+# already encoded. With this extra encoding, committing a path to slave which
+# has space in it fails. Please see this thread
+# http://svn.haxx.se/dev/archive-2011-03/0641.shtml for more info.
 
+say "Test case for regression issue triggered by r917523"
+
+$svnmucc cp 2 "$BASE_URL/trunk" "$BASE_URL/branch new"
+$svnmucc put /dev/null "$BASE_URL/branch new/file" \
+--config-option servers:global:http-library=neon
+RETVAL=$?
+
+if [ $RETVAL -eq 0 ] ; then
+  say "PASS: committing a path which has space in it passes"
+else
+  say "FAIL: committing a path which has space in it fails as there are extra
+  url-encodings happening in server side"
+fi
+
+# shut it down
+echo -n "${SCRIPT}: stopping httpd: "
+$HTTPD -f $HTTPD_CONFIG -k stop
+echo "."
+exit 0