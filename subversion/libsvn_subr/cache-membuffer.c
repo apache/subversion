apache/subversion

commit 2e0ec894c9b533c3a93bc1856a71cbcff044225b

Clarify the content of the 'found' return parameter. The problem is that partial getters perform two lookups: the whole entry by key and then the sub-item defined by baton and the deserializer logic.
* subversion/include/private/svn_cache.h
  (svn_cache__get_partial): in docstring, clarify the meaning of 'found'
* subversion/libsvn_subr/cache-membuffer.c
  (membuffer_cache_get_partial): make usage of 'found' comply to
  API definition

git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1090788 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_subr/cache-membuffer.c
@@ -1179,6 +1179,7 @@ membuffer_cache_get_partial(svn_membuffer_t *cache,
                             const void *key,
                             apr_size_t key_len,
                             void **item,
+                            svn_boolean_t *found,
                             svn_cache__partial_getter_func_t deserializer,
                             void *baton,
                             DEBUG_CACHE_MEMBUFFER_TAG_ARG
@@ -1198,9 +1199,12 @@ membuffer_cache_get_partial(svn_membuffer_t *cache,
   if (entry == NULL)
     {
       *item = NULL;
+      *found = FALSE;
     }
   else
     {
+      *found = TRUE;
+
       entry->hit_count++;
       cache->hit_count++;
       cache->total_hits++;
@@ -1473,7 +1477,7 @@ svn_membuffer_cache_get_partial(void **value_p,
                                       baton,
                                       DEBUG_CACHE_MEMBUFFER_TAG
                                       pool));
-  *found = *value_p != NULL;
+
   return SVN_NO_ERROR;
 }
 

subversion/include/private/svn_cache.h
@@ -381,7 +381,9 @@ svn_cache__iter(svn_boolean_t *completed,
 
 /**
  * Similar to @ref svn_cache__set but will call a specific de-serialization
- * function @a func.
+ * function @a func. @a found will be set depending on whether the @a key
+ * has been found. Even if that reports @c TRUE, @a values may still return
+ * a @c NULL pointer depending on the logic inside @a func.
  */
 svn_error_t *
 svn_cache__get_partial(void **value,

commit 14efee33a228ebc47750f4a093a3c1be9402d19f

Fix build broken by r1090788.
* subversion/libsvn_subr/cache-membuffer.c
  (svn_membuffer_cache_get_partial): add missing parameter

git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1090790 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_subr/cache-membuffer.c
@@ -1473,6 +1473,7 @@ svn_membuffer_cache_get_partial(void **value_p,
                                       full_key,
                                       full_key_len,
                                       value_p,
+                                      found,
                                       func,
                                       baton,
                                       DEBUG_CACHE_MEMBUFFER_TAG