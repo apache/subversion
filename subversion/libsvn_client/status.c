apache/subversion

commit 102da76a910144624ed0f26692a1cb1910c4b307

Remove a few entry-like is_replaced helpers. These functions were used for the transition away from nodes while we still had a revert base, but they can't survive in a world with more than two layers.
* subversion/include/private/svn_wc_private.h
  (svn_wc__node_is_replaced): Remove function.

* subversion/libsvn_client/status.c
  (svn_client_status5): When we have a local addition or copy, everything
    below is deleted... Keep it simple.

* subversion/libsvn_wc/adm_ops.c
  (svn_wc__process_committed_internal): These problems are gone since we moved
    to single-db.

* subversion/libsvn_wc/node.c
  (svn_wc__internal_is_replaced,
   svn_wc__node_is_replaced): Remove these functions.

* subversion/libsvn_wc/wc.h
  (svn_wc__internal_is_replaced): Remove function.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1092294 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_client/status.c
@@ -420,20 +420,6 @@ svn_client_status5(svn_revnum_t *result_rev,
              repository.  (Note that "locally replaced" doesn't count
              as "added" in this case.)  */
 
-          /* ### FIXME:  WC-1 code here was just (! added).  Not sure
-             ### if this WC-NG approach matches semantically.  */
-          SVN_ERR(svn_wc__node_is_added(&added, ctx->wc_ctx,
-                                        dir_abspath, pool));
-          if (added)
-            {
-              svn_boolean_t replaced;
-
-              SVN_ERR(svn_wc__node_is_replaced(&replaced, ctx->wc_ctx,
-                                               dir_abspath, pool));
-              if (replaced)
-                added = FALSE;
-            }
-
           if (! added)
             sb.deleted_in_repos = TRUE;
 

subversion/include/private/svn_wc_private.h
@@ -482,20 +482,6 @@ svn_wc__node_is_added(svn_boolean_t *is_added,
                       apr_pool_t *scratch_pool);
 
 /**
- * Set @a *is_replaced to whether @a local_abspath is replaced, using
- * @a wc_ctx.  If @a local_abspath is not in the working copy, return
- * @c SVN_ERR_WC_PATH_NOT_FOUND.  Use @a scratch_pool for all temporary
- * allocations.
- *
- * NOTE: This corresponds directly to svn_wc_schedule_replace.
- */
-svn_error_t *
-svn_wc__node_is_replaced(svn_boolean_t *is_replaced,
-                         svn_wc_context_t *wc_ctx,
-                         const char *local_abspath,
-                         apr_pool_t *scratch_pool);
-
-/**
  * Get the base revision of @a local_abspath using @a wc_ctx.  If
  * @a local_abspath is not in the working copy, return
  * @c SVN_ERR_WC_PATH_NOT_FOUND.

subversion/libsvn_wc/adm_ops.c
@@ -297,22 +297,6 @@ svn_wc__process_committed_internal(svn_wc__db_t *db,
           sha1_checksum = NULL;
           if (kind != svn_wc__db_kind_dir)
             {
-              /* Suppress log creation for deleted entries in a replaced
-                 directory.  By the time any log we create here is run,
-                 those entries will already have been removed (as a result
-                 of running the log for the replaced directory that was
-                 created at the start of this function). */
-              if (status == svn_wc__db_status_deleted)
-                {
-                  svn_boolean_t replaced;
-
-                  SVN_ERR(svn_wc__internal_is_replaced(&replaced,
-                                                       db, local_abspath,
-                                                       iterpool));
-                  if (replaced)
-                    continue;
-                }
-
               if (queue != NULL)
                 {
                   const committed_queue_item_t *cqi

subversion/libsvn_wc/node.c
@@ -883,56 +883,6 @@ svn_wc__node_is_added(svn_boolean_t *is_added,
   return SVN_NO_ERROR;
 }
 
-
-/* Equivalent to the old notion of "entry->schedule == schedule_replace"  */
-svn_error_t *
-svn_wc__internal_is_replaced(svn_boolean_t *replaced,
-                             svn_wc__db_t *db,
-                             const char *local_abspath,
-                             apr_pool_t *scratch_pool)
-{
-  svn_wc__db_status_t status;
-  svn_boolean_t have_base;
-  svn_wc__db_status_t base_status;
-
-  SVN_ERR(svn_wc__db_read_info(
-            &status, NULL, NULL,
-            NULL, NULL, NULL, NULL, NULL, NULL,
-            NULL, NULL, NULL, NULL, NULL, NULL,
-            NULL, NULL, NULL, NULL,
-            NULL, &have_base, NULL,
-            NULL, NULL,
-            db, local_abspath,
-            scratch_pool, scratch_pool));
-  if (have_base)
-    SVN_ERR(svn_wc__db_base_get_info(&base_status, NULL, NULL,
-                                     NULL, NULL, NULL,
-                                     NULL, NULL, NULL,
-                                     NULL, NULL, NULL,
-                                     NULL, NULL, NULL, NULL,
-                                     db, local_abspath,
-                                     scratch_pool, scratch_pool));
-
-  *replaced = ((status == svn_wc__db_status_added)
-               && have_base
-               && base_status != svn_wc__db_status_not_present);
-
-  return SVN_NO_ERROR;
-}
-
-
-svn_error_t *
-svn_wc__node_is_replaced(svn_boolean_t *replaced,
-                         svn_wc_context_t *wc_ctx,
-                         const char *local_abspath,
-                         apr_pool_t *scratch_pool)
-{
-  return svn_error_return(svn_wc__internal_is_replaced(replaced, wc_ctx->db,
-                                                       local_abspath,
-                                                       scratch_pool));
-}
-
-
 svn_error_t *
 svn_wc__node_get_base_rev(svn_revnum_t *base_revision,
                           svn_wc_context_t *wc_ctx,

subversion/libsvn_wc/wc.h
@@ -592,13 +592,6 @@ svn_wc__internal_remove_from_revision_control(svn_wc__db_t *db,
                                               apr_pool_t *scratch_pool);
 
 
-/* Library-internal version of svn_wc__node_is_replaced(). */
-svn_error_t *
-svn_wc__internal_is_replaced(svn_boolean_t *replaced,
-                             svn_wc__db_t *db,
-                             const char *local_abspath,
-                             apr_pool_t *scratch_pool);
-
 /* Library-internal version of svn_wc__node_is_file_external(). */
 svn_error_t *
 svn_wc__internal_is_file_external(svn_boolean_t *file_external,

commit 1c779aafaafdd97ca557ec5e052eed16951eed78

While working on a test for the remote deleted, locally added block I accidentally broke in r1092294, fix a few issues.
* subversion/libsvn_client/status.c
  (tweak_status): Following up on r955542, tweak the node status instead of the
    text status.
  (svn_client_status_5): Following up on r1092294, reinsert the added check.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1092327 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_client/status.c
@@ -91,7 +91,7 @@ tweak_status(void *baton,
   if (sb->deleted_in_repos)
     {
       svn_wc_status3_t *new_status = svn_wc_dup_status3(status, scratch_pool);
-      new_status->repos_text_status = svn_wc_status_deleted;
+      new_status->repos_node_status = svn_wc_status_deleted;
       status = new_status;
     }
 
@@ -419,7 +419,8 @@ svn_client_status5(svn_revnum_t *result_rev,
              versioned, then it must have since been deleted from the
              repository.  (Note that "locally replaced" doesn't count
              as "added" in this case.)  */
-
+          SVN_ERR(svn_wc__node_is_added(&added, ctx->wc_ctx,
+                                        dir_abspath, pool));
           if (! added)
             sb.deleted_in_repos = TRUE;
 