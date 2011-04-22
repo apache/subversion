40ad164a613c74cd4277a59afbb5a083d796f4b2
Bert Huiben <rhuijben@apache.org>
2011-04-22 09:49:57 +0000

    * subversion/libsvn_wc/wc_db.c   (set_props_txn): Fix compilation. (Stupid diff tool)
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1095909 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/wc_db.c
+++ subversion/libsvn_wc/wc_db.c
@@ -3377,7 +3377,7 @@ set_props_txn(void *baton,
   SVN_ERR(set_actual_props(wcroot->wc_id, local_relpath,
                            spb->props, wcroot->sdb, scratch_pool));
 
-  SVN_ERR(add_work_items(wcroot->sdb, work_items, scratch_pool));
+  SVN_ERR(add_work_items(wcroot->sdb, spb->work_items, scratch_pool));
 
   return SVN_NO_ERROR;
 }
