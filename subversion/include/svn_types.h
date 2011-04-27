ba76a82748a827bbe59ed83babe3f585ad04c4c6
Hyrum Kurt Wright <hwright@apache.org>
2011-04-27 20:51:33 +0000

    Move a typedef to the real proper home.  Since svn_changelist_receiver_t is only driven by libsvn_wc, it makes sense to live in svn_wc.h.
    * subversion/include/svn_types.h
      (svn_changelist_receiver_t): Moved from here...
    
    * subversion/include/svn_wc.h
      (svn_changelist_receiver_t): ...to here.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1097234 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_types.h
+++ subversion/include/svn_types.h
@@ -1200,20 +1200,6 @@ typedef unsigned long svn_linenum_t;
 /** @} */
 
 
-/**
- * The callback type used by svn_client_get_changelists().
- *
- * On each invocation, @a path is a newly discovered member of the
- * changelist, and @a baton is a private function closure.
- *
- * @since New in 1.5.
- */
-typedef svn_error_t *(*svn_changelist_receiver_t) (void *baton,
-                                                   const char *path,
-                                                   const char *changelist,
-                                                   apr_pool_t *pool);
-
-
 /** Forward declaration, for the purpose of the #svn_info2_t struct. */
 struct svn_wc_info_t;
 
