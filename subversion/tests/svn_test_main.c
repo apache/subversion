1396e7def9a94c32eaa05547d27c2cf8d2444632
Bert Huiben <rhuijben@apache.org>
2011-05-02 16:50:05 +0000

    * subversion/tests/svn_test_main.c   (main): Setup the primary pool like we do in svn, to reduce memory     fragmentation a bit on tests using a lot of ram.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1098657 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/svn_test_main.c
+++ subversion/tests/svn_test_main.c
@@ -355,6 +355,7 @@ main(int argc, const char *argv[])
   const char *prog_name;
   int i;
   svn_boolean_t got_error = FALSE;
+  apr_allocator_t *allocator;
   apr_pool_t *pool, *test_pool;
   svn_boolean_t ran_a_test = FALSE;
   svn_boolean_t list_mode = FALSE;
@@ -377,8 +378,16 @@ main(int argc, const char *argv[])
       exit(1);
     }
 
-  /* set up the global pool */
-  pool = svn_pool_create(NULL);
+  /* set up the global pool.  Use a separate mutexless allocator,
+   * given this application is single threaded.
+   */
+  if (apr_allocator_create(&allocator))
+    return EXIT_FAILURE;
+
+  apr_allocator_max_free_set(allocator, SVN_ALLOCATOR_RECOMMENDED_MAX_FREE);
+
+  pool = svn_pool_create_ex(NULL, allocator);
+  apr_allocator_owner_set(allocator, pool);
 
   /* Remember the command line */
   test_argc = argc;
