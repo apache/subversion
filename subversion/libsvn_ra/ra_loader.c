4feed38ba82ea918b8ba7dd8275000bf63c5f3c7
Bert Huijben <rhuijben@apache.org>
2011-05-02 20:57:55 +0000

    * subversion/libsvn_ra/ra_loader.c   (svn_ra_get_path_relative_to_session,    svn_ra_get_path_relative_to_root): Partially revert r1096934, as      svn_uri_is_child already guarantees uri decoded results.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1098793 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_ra/ra_loader.c
+++ subversion/libsvn_ra/ra_loader.c
@@ -577,13 +577,11 @@ svn_error_t *svn_ra_get_path_relative_to_session(svn_ra_session_t *session,
     }
   else
     {
-      const char *relpath = svn_uri_is_child(sess_url, url, pool);
-      if (! *relpath)
+      *rel_path = svn_uri_is_child(sess_url, url, pool);
+      if (! *rel_path)
         return svn_error_createf(SVN_ERR_RA_ILLEGAL_URL, NULL,
                                  _("'%s' isn't a child of session URL '%s'"),
                                  url, sess_url);
-
-      *rel_path = svn_path_uri_decode(relpath, pool);
     }
   return SVN_NO_ERROR;
 }
@@ -601,14 +599,12 @@ svn_error_t *svn_ra_get_path_relative_to_root(svn_ra_session_t *session,
     }
   else
     {
-      const char *relpath = svn_uri_is_child(root_url, url, pool);
-      if (! *relpath)
+      *rel_path = svn_uri_is_child(root_url, url, pool);
+      if (! *rel_path)
         return svn_error_createf(SVN_ERR_RA_ILLEGAL_URL, NULL,
                                  _("'%s' isn't a child of repository root "
                                    "URL '%s'"),
                                  url, root_url);
-
-      *rel_path = svn_path_uri_decode(relpath, pool);
     }
 
   return SVN_NO_ERROR;
