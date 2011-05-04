c1082011d5b438366502306f24a4005c432c46a1
Paul Burba <pburba@apache.org>
2011-05-04 20:55:01 +0000

    Fix issue #3871 "'svn up' with subtree switched to root asserts".
    * subversion/libsvn_ra_local/ra_plugin.c
    
      (reporter_link_path): Don't assume url can't be the root of the repository.
    
    * subversion/tests/cmdline/switch_tests.py
    
      (up_to_old_rev_with_subtree_switched_to_root): Remove XFail decorator.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1099604 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_ra_local/ra_plugin.c
+++ subversion/libsvn_ra_local/ra_plugin.c
@@ -198,8 +198,11 @@ reporter_link_path(void *reporter_baton,
                                "'%s'"), url, rbaton->sess->repos_url);
 
   /* Skip the repos_url, but keep the last '/' to create an fspath */
-  fs_path = svn_uri_skip_ancestor(repos_url, url) - 1;
-  fs_path = svn_path_uri_decode(fs_path, pool);
+  fs_path = svn_uri_skip_ancestor(repos_url, url);
+  if (fs_path[0] == '\0')
+    fs_path = "/";  
+  else
+    fs_path = svn_path_uri_decode(--fs_path, pool);  
 
   return svn_repos_link_path3(rbaton->report_baton, path, fs_path, revision,
                               depth, start_empty, lock_token, pool);
