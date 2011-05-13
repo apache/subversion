d17e1f3b62c8ce65fd016b9e87c2ae20f42cd918
Bert Huijben <rhuijben@apache.org>
2011-05-13 23:03:08 +0000

    * subversion/libsvn_wc/wc.h   (SVN_WC__VERSION): Revert r1102917. Phew.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1102918 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/wc.h
+++ subversion/libsvn_wc/wc.h
@@ -153,7 +153,7 @@ extern "C" {
  * Please document any further format changes here.
  */
 
-#define SVN_WC__VERSION 29
+#define SVN_WC__VERSION 28
 
 
 /* Formats <= this have no concept of "revert text-base/props".  */
