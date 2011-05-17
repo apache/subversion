apache/subversion

commit 79604efedf78bbedfd615949c705d2e7833cafb0

Speed up UTF8 conversion, especially if APR_HAS_THREADS. Provide global
* subversion/libsvn_subr/utf.c
  (SVN_UTF_NTOU_XLATE_HANDLE, SVN_UTF_UTON_XLATE_HANDLE):
   make them identifiable by address
  (xlat_ntou_static_handle, xlat_uton_static_handle): 
   introduce global translation map pointers as 1st level cache
  (get_xlate_handle_node): match standard xlat types by address;
   try global map pointers first before falling back to the hash
  (put_xlate_handle_node): match standard xlat types by address;
   push old global maps to hash

git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1104160 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_subr/utf.c
@@ -29,6 +29,7 @@
 #include <apr_strings.h>
 #include <apr_lib.h>
 #include <apr_xlate.h>
+#include <apr_atomic.h>
 
 #include "svn_string.h"
 #include "svn_error.h"
@@ -42,9 +43,13 @@
 
 
 
-#define SVN_UTF_NTOU_XLATE_HANDLE "svn-utf-ntou-xlate-handle"
-#define SVN_UTF_UTON_XLATE_HANDLE "svn-utf-uton-xlate-handle"
-#define SVN_APR_UTF8_CHARSET "UTF-8"
+/* Use these static strings to maximize performance on standard conversions.
+ * Any strings on other locations are still valid, however.
+ */
+static const char *SVN_UTF_NTOU_XLATE_HANDLE = "svn-utf-ntou-xlate-handle";
+static const char *SVN_UTF_UTON_XLATE_HANDLE = "svn-utf-uton-xlate-handle";
+
+static const char *SVN_APR_UTF8_CHARSET = "UTF-8";
 
 #if APR_HAS_THREADS
 static apr_thread_mutex_t *xlate_handle_mutex = NULL;
@@ -79,6 +84,13 @@ typedef struct xlate_handle_node_t {
    memory leak. */
 static apr_hash_t *xlate_handle_hash = NULL;
 
+/* "1st level cache" to standard conversion maps. We may access these
+ * using atomic xchange ops, i.e. without further thread synchronization.
+ * If the respective item is NULL, fallback to hash lookup.
+ */
+static volatile void *xlat_ntou_static_handle = NULL;
+static volatile void *xlat_uton_static_handle = NULL;
+
 /* Clean up the xlate handle cache. */
 static apr_status_t
 xlate_cleanup(void *arg)
@@ -90,6 +102,10 @@ xlate_cleanup(void *arg)
   xlate_handle_mutex = NULL;
 #endif
   xlate_handle_hash = NULL;
+
+  /* ensure no stale objects get accessed */
+  xlat_ntou_static_handle = NULL;
+  xlat_uton_static_handle = NULL;
 
   return APR_SUCCESS;
 }
@@ -183,6 +199,19 @@ get_xlate_handle_node(xlate_handle_node_t **ret,
     {
       if (xlate_handle_hash)
         {
+          /* 1st level: global, static items */
+          if (userdata_key == SVN_UTF_NTOU_XLATE_HANDLE)
+            old_node = apr_atomic_xchgptr(&xlat_ntou_static_handle, NULL);
+          else if (userdata_key == SVN_UTF_UTON_XLATE_HANDLE)
+            old_node = apr_atomic_xchgptr(&xlat_uton_static_handle, NULL);
+
+          if (old_node && old_node->valid)
+            {
+              *ret = old_node;
+              return SVN_NO_ERROR;
+            }
+          
+          /* 2nd level: hash lookup */
 #if APR_HAS_THREADS
           apr_err = apr_thread_mutex_lock(xlate_handle_mutex);
           if (apr_err != APR_SUCCESS)
@@ -321,9 +350,20 @@ put_xlate_handle_node(xlate_handle_node_t *node,
   assert(node->next == NULL);
   if (!userdata_key)
     return;
+
+  /* push previous global node to the hash */
   if (xlate_handle_hash)
     {
       xlate_handle_node_t **node_p;
+
+      /* 1st level: global, static items */
+      if (userdata_key == SVN_UTF_NTOU_XLATE_HANDLE)
+        node = apr_atomic_xchgptr(&xlat_ntou_static_handle, node);
+      else if (userdata_key == SVN_UTF_UTON_XLATE_HANDLE)
+        node = apr_atomic_xchgptr(&xlat_uton_static_handle, node);
+      if (node == NULL)
+        return;
+
 #if APR_HAS_THREADS
       if (apr_thread_mutex_lock(xlate_handle_mutex) != APR_SUCCESS)
         SVN_ERR_MALFUNCTION_NO_RETURN();

commit 0661fe33a4df46da64080dd510ec124ba126fcf8

Fix breakage with pre-1.3 APRs, introduced in r1104160. Introduce a utility function that checks for APR version as well as threading settings and provides the suitable implementation for atomic pointer swaps.
* subversion/libsvn_subr/utf.c
  (atomic_swap): new utility function
  (get_xlate_handle_node, put_xlate_handle_node): call our utility instead
   of the APR 1.3+ function


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1104209 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_subr/utf.c
@@ -40,6 +40,7 @@
 #include "win32_xlate.h"
 
 #include "private/svn_utf_private.h"
+#include "private/svn_dep_compat.h"
 
 
 
@@ -174,6 +175,30 @@ get_xlate_key(const char *topage,
                      "-xlate-handle", (char *)NULL);
 }
 
+/* Atomically replace the content in *MEM with NEW_VALUE and return
+ * the previous content of *MEM. If atomicy cannot be guaranteed,
+ * *MEM will not be modified and NEW_VALUE is simply returned to
+ * the caller.
+ */
+static void*
+atomic_swap(volatile void **mem, void *new_value)
+{
+#if APR_HAS_THREADS
+#if APR_VERSION_AT_LEAST(1,3,0)
+   return apr_atomic_xchgptr(mem, new_value);
+#else
+   /* old APRs don't support atomic swaps. Simply return the
+    * input to the caller for further proccessing. */
+   return new_value;
+#endif
+#else
+   /* no threads - no sync. necessary */
+   void *old_value = *mem;
+   *mem = new_value;
+   return old_value;
+#endif
+}
+
 /* Set *RET to a handle node for converting from FROMPAGE to TOPAGE,
    creating the handle node if it doesn't exist in USERDATA_KEY.
    If a node is not cached and apr_xlate_open() returns APR_EINVAL or
@@ -201,9 +226,9 @@ get_xlate_handle_node(xlate_handle_node_t **ret,
         {
           /* 1st level: global, static items */
           if (userdata_key == SVN_UTF_NTOU_XLATE_HANDLE)
-            old_node = apr_atomic_xchgptr(&xlat_ntou_static_handle, NULL);
+            old_node = atomic_swap(&xlat_ntou_static_handle, NULL);
           else if (userdata_key == SVN_UTF_UTON_XLATE_HANDLE)
-            old_node = apr_atomic_xchgptr(&xlat_uton_static_handle, NULL);
+            old_node = atomic_swap(&xlat_uton_static_handle, NULL);
 
           if (old_node && old_node->valid)
             {
@@ -358,12 +383,12 @@ put_xlate_handle_node(xlate_handle_node_t *node,
 
       /* 1st level: global, static items */
       if (userdata_key == SVN_UTF_NTOU_XLATE_HANDLE)
-        node = apr_atomic_xchgptr(&xlat_ntou_static_handle, node);
+        node = atomic_swap(&xlat_ntou_static_handle, node);
       else if (userdata_key == SVN_UTF_UTON_XLATE_HANDLE)
-        node = apr_atomic_xchgptr(&xlat_uton_static_handle, node);
+        node = atomic_swap(&xlat_uton_static_handle, node);
       if (node == NULL)
         return;
-
+        
 #if APR_HAS_THREADS
       if (apr_thread_mutex_lock(xlate_handle_mutex) != APR_SUCCESS)
         SVN_ERR_MALFUNCTION_NO_RETURN();