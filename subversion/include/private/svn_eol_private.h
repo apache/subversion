9f0a98d75d9ce3bc8d3af08ee807cffc56beeb6e
Stefan Sperling <stsp@apache.org>
2011-05-31 22:56:23 +0000

    Remove svn_eol__detect_file_eol() which was added during 1.7 development but is now unused. It also had known bugs.
    * subversion/include/private/svn_eol_private.h
      (svn_eol__detect_file_eol): Remove declaration and docstring.
    
    * subversion/libsvn_subr/eol.c
      (svn_eol__detect_file_eol): Remove.
    
    * subversion/tests/libsvn_subr/eol-test.c: Remove this entire file since
       it was only testing svn_eol__detect_file_eol().
    
    * build.conf: Adjust for removed file. Hope I won't break the Windows build.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1129955 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/private/svn_eol_private.h
+++ subversion/include/private/svn_eol_private.h
@@ -72,31 +72,6 @@ svn_eol__find_eol_start(char *buf, apr_size_t len);
 const char *
 svn_eol__detect_eol(char *buf, apr_size_t len, char **eolp);
 
-/* Set @a *eol to the EOL marker used in @a file.
- * If it cannot be detected, set @a *eol to NULL.
- *
- * The file is searched starting at the current file cursor position.
- * The first EOL marker found will be returned. So if the file has
- * inconsistent EOL markers, this won't be detected.
- *
- * Upon return, the original file cursor position is always preserved,
- * even if an error is thrown.
- *
- * The string @a *eol is statically allocated.
- * Do temporary allocations in @a pool.
- *
- * ### [JAF] Apparent bug: implementation suffers from the "one byte of a
- *     two-byte sequence" issue noted for svn_eol__detect_eol().
- *
- * ### [JAF] Apparent bug: if file pointer is at EOF, returns "success"
- *     without initializing @a *eol.
- *
- * ### [JAF] Wouldn't it make more sense to accept a stream than a file?
- *
- * @since New in 1.7 */
-svn_error_t *
-svn_eol__detect_file_eol(const char **eol, apr_file_t *file, apr_pool_t *pool);
-
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
