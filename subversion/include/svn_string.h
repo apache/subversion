b56e5792890a6aa88b40f5586e911276f925ed7c
Stefan Fuhrmann <stefan2@apache.org>
2011-06-02 12:24:36 +0000

    Finally, make svn_string_from_stringbuf useful: This kind of conversion usually takes place at the end of some internal processing using variable sized strings and then returning a fixed-size result. So, it is acceptable to "consume" the source svn_stringbuf_t.
    * subversion/include/svn_string.h
      (svn_string_from_stringbuf): switch interface to modifiable structs
    * subversion/libsvn_subr/svn_string.c
      (svn_string_from_stringbuf): make it harder to mis-use in debug mode;
       adapt to API change
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1130512 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_string.h
+++ subversion/include/svn_string.h
@@ -179,10 +179,11 @@ svn_string_find_char_backward(const svn_string_t *str, char ch);
 
 /** Returns the @a svn_string_t information contained in the data and
  * len members of @a strbuf. This is effectively a typecast, converting
- * @a strbuf into an svn_string_t.
+ * @a strbuf into an svn_string_t. This first will become invalid and must
+ * not be accessed after this function returned.
  */
-const svn_string_t *
-svn_string_from_stringbuf(const svn_stringbuf_t *strbuf);
+svn_string_t *
+svn_string_from_stringbuf(svn_stringbuf_t *strbuf);
 /** @} */
 
 
