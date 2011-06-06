54cfa8875ad91241be607326872277ead75c68ab
Julian Foad <julianfoad@apache.org>
2011-06-06 10:42:27 +0000

    Deprecate svn_info_dup(), as the svn_info_t structure is deprecated.
    * subversion/include/svn_client.h
      (svn_info_dup): Mark as deprecated.
    
    * subversion/libsvn_client/info.c
      (svn_info_dup): Move from here ...
    
    * subversion/libsvn_client/deprecated.c
      (svn_info_dup): ... to here.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1132588 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/deprecated.c
+++ subversion/libsvn_client/deprecated.c
@@ -2084,6 +2084,42 @@ svn_client_checkout(svn_revnum_t *result_rev,
 
 /*** From info.c ***/
 
+svn_info_t *
+svn_info_dup(const svn_info_t *info, apr_pool_t *pool)
+{
+  svn_info_t *dupinfo = apr_palloc(pool, sizeof(*dupinfo));
+
+  /* Perform a trivial copy ... */
+  *dupinfo = *info;
+
+  /* ...and then re-copy stuff that needs to be duped into our pool. */
+  if (info->URL)
+    dupinfo->URL = apr_pstrdup(pool, info->URL);
+  if (info->repos_root_URL)
+    dupinfo->repos_root_URL = apr_pstrdup(pool, info->repos_root_URL);
+  if (info->repos_UUID)
+    dupinfo->repos_UUID = apr_pstrdup(pool, info->repos_UUID);
+  if (info->last_changed_author)
+    dupinfo->last_changed_author = apr_pstrdup(pool,
+                                               info->last_changed_author);
+  if (info->lock)
+    dupinfo->lock = svn_lock_dup(info->lock, pool);
+  if (info->copyfrom_url)
+    dupinfo->copyfrom_url = apr_pstrdup(pool, info->copyfrom_url);
+  if (info->checksum)
+    dupinfo->checksum = apr_pstrdup(pool, info->checksum);
+  if (info->conflict_old)
+    dupinfo->conflict_old = apr_pstrdup(pool, info->conflict_old);
+  if (info->conflict_new)
+    dupinfo->conflict_new = apr_pstrdup(pool, info->conflict_new);
+  if (info->conflict_wrk)
+    dupinfo->conflict_wrk = apr_pstrdup(pool, info->conflict_wrk);
+  if (info->prejfile)
+    dupinfo->prejfile = apr_pstrdup(pool, info->prejfile);
+
+  return dupinfo;
+}
+
 /* Convert an svn_info2_t to an svn_info_t, doing shallow copies of objects. */
 static svn_error_t *
 info_from_info2(svn_info_t **new_info,
