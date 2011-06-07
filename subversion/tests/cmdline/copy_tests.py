42dde9a05e028e6b9f1c98718f33052f3e4c03cd
Bert Huiben <rhuijben@apache.org>
2011-06-07 10:43:07 +0000

    Reimplement most logic of r1132834 in svn_wc_move(). This removes the conflict marker files (if they exist) after moving a node.
    This patch doesn't fix the issue #3899 testcase as this resolves the copy to
    working instead of the expected mine.
    (I'm not sure which behavior we really want. I think <=1.6 used working by
     ignoring the marker files)
    
    * subversion/libsvn_wc/copy.c
      (remove_node_conflict_markers): New function.
      (remove_all_conflict_markers): New function.
      (svn_wc_move): Remove conflict markers after renaming the directory.
    
    * subversion/tests/cmdline/copy_tests.py
      (copy_and_move_conflicts): Handle test failure on Windows
        (similar to r1132731).
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1132948 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/copy_tests.py
+++ subversion/tests/cmdline/copy_tests.py
@@ -5307,8 +5307,9 @@ def copy_and_move_conflicts(sbox):
 
   # Resolve one text conflict via marker file deletion.
   os.remove(wc('A/B/E/beta.merge-left.r3'))
-  os.rename(wc('A/B/E/beta.merge-right.r4'), wc('A/B/E/beta'))
   os.remove(wc('A/B/E/beta.working'))
+  os.remove(wc('A/B/E/beta'))
+  os.rename(wc('A/B/E/beta.merge-right.r4'), wc('A/B/E/beta'))
 
   # Prepare for local copies and moves.
   sbox.simple_mkdir('copy-dest')
