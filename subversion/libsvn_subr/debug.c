d5633de4cb2770aba0ef3d6c850e2e982fa8fe8b
Stefan Sperling <stsp@apache.org>
2011-06-09 13:31:11 +0000

    Revert r1133865 because Windows compilers are too stupid to know about __func__.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1133866 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/debug.c
+++ subversion/libsvn_subr/debug.c
@@ -43,8 +43,7 @@ quiet_mode(void)
 
 
 void
-svn_dbg__preamble(const char *file, long line, const char *func,
-                  FILE *output)
+svn_dbg__preamble(const char *file, long line, FILE *output)
 {
   debug_output = output;
 
@@ -60,7 +59,7 @@ svn_dbg__preamble(const char *file, long line, const char *func,
       else
         ++slash;
 
-      fprintf(output, "DBG: %s:%4ld:%s(): ", slash, line, func);
+      fprintf(output, "DBG: %s:%4ld: ", slash, line);
     }
 }
 
