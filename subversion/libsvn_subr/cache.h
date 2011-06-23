0735cc204b55a8c8942ae987ee33b50b02915825
Daniel Shahaf <danielsh@apache.org>
2011-06-23 14:32:05 +0000

    * subversion/libsvn_subr/cache.h   (svn_cache__t, svn_cache__vtable_t): Point to the respective semi-public APIs for docstrings.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1138900 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/cache.h
+++ subversion/libsvn_subr/cache.h
@@ -31,26 +31,31 @@ extern "C" {
 #endif /* __cplusplus */
 
 typedef struct svn_cache__vtable_t {
+  /* See svn_cache__get(). */
   svn_error_t *(*get)(void **value,
                       svn_boolean_t *found,
                       void *cache_implementation,
                       const void *key,
                       apr_pool_t *result_pool);
 
+  /* See svn_cache__set(). */
   svn_error_t *(*set)(void *cache_implementation,
                       const void *key,
                       void *value,
                       apr_pool_t *scratch_pool);
 
+  /* See svn_cache__iter(). */
   svn_error_t *(*iter)(svn_boolean_t *completed,
                        void *cache_implementation,
                        svn_iter_apr_hash_cb_t func,
                        void *baton,
                        apr_pool_t *scratch_pool);
 
+  /* See svn_cache__is_cachable(). */
   svn_boolean_t (*is_cachable)(void *cache_implementation,
                                apr_size_t size);
 
+  /* See svn_cache__get_partial(). */
   svn_error_t *(*get_partial)(void **value,
                               svn_boolean_t *found,
                               void *cache_implementation,
@@ -58,12 +63,15 @@ typedef struct svn_cache__vtable_t {
                               svn_cache__partial_getter_func_t func,
                               void *baton,
                               apr_pool_t *result_pool);
+
+  /* See svn_cache__set_partial(). */
   svn_error_t *(*set_partial)(void *cache_implementation,
                               const void *key,
                               svn_cache__partial_setter_func_t func,
                               void *baton,
                               apr_pool_t *scratch_pool);
 
+  /* See svn_cache__get_info(). */
   svn_error_t *(*get_info)(void *cache_implementation,
                            svn_cache__info_t *info,
                            svn_boolean_t reset,
@@ -73,6 +81,7 @@ typedef struct svn_cache__vtable_t {
 struct svn_cache__t {
   const svn_cache__vtable_t *vtable;
 
+  /* See svn_cache__set_error_handler(). */
   svn_cache__error_handler_t error_handler;
   void *error_baton;
 
