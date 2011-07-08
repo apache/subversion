5de1c8f55e786ce4c2502e54f3ab7b650ad2fdf0
Julian Foad <julianfoad@apache.org>
2011-07-08 17:35:24 +0000

    Change a comment about removing code into a comment documenting the behaviour, because the consensus is to leave the code as it is.
    * subversion/libsvn_ra_neon/options.c
      (SVN_IGNORE_V2_ENV_VAR): Add a doc string here.
      (parse_capabilities): Remove the '###' comment from here. Correct the
        indentation.
    
    * subversion/libsvn_ra_serf/options.c
      (SVN_IGNORE_V2_ENV_VAR): Add a doc string here.
      (capabilities_headers_iterator_callback): Remove the '###' comment from
        here.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1144396 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_ra_neon/options.c
+++ subversion/libsvn_ra_neon/options.c
@@ -34,6 +34,8 @@
 #include "ra_neon.h"
 
 
+/* In a debug build, setting this environment variable to "yes" will force
+   the client to speak v1, even if the server is capable of speaking v2. */
 #define SVN_IGNORE_V2_ENV_VAR "SVN_I_LIKE_LATENCY_SO_IGNORE_HTTPV2"
 
 static const svn_ra_neon__xml_elm_t options_elements[] =
@@ -242,17 +244,13 @@ parse_capabilities(ne_request *req,
   if ((val = ne_get_response_header(req, SVN_DAV_ME_RESOURCE_HEADER)))
     {
 #ifdef SVN_DEBUG
-      /* ### This section is throw in here for development use.  It
-         ### allows devs the chance to force the client to speak v1,
-         ### even if the server is capable of speaking v2.  We should
-         ### probably remove it before 1.7 goes final. */
       char *ignore_v2_env_var = getenv(SVN_IGNORE_V2_ENV_VAR);
 
       if (! (ignore_v2_env_var
              && apr_strnatcasecmp(ignore_v2_env_var, "yes") == 0))
         ras->me_resource = apr_pstrdup(ras->pool, val);
 #else
-        ras->me_resource = apr_pstrdup(ras->pool, val);
+      ras->me_resource = apr_pstrdup(ras->pool, val);
 #endif
     }
   if ((val = ne_get_response_header(req, SVN_DAV_REV_ROOT_STUB_HEADER)))
