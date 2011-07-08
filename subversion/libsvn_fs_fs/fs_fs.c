79b83040baae5929a6d2179de4f49784f04fba78
Daniel Shahaf <danielsh@apache.org>
2011-07-08 20:38:24 +0000

    On the revprop-packing branch:
    Refactor the 'Is a dev format FS' check to be suitable for trunk.
    
    * subversion/libsvn_fs_fs/fs_fs.c
      (check_format): Check for blacklist format numbers before checking
        SVN_FS_FS__FORMAT_NUMBER.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/revprop-packing@1144499 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_fs/fs_fs.c
+++ subversion/libsvn_fs_fs/fs_fs.c
@@ -1076,19 +1076,19 @@ write_format(const char *path, int format, int max_files_per_dir,
 static svn_error_t *
 check_format(int format)
 {
+  /* Blacklist.  These formats may be either younger or older than
+     SVN_FS_FS__FORMAT_NUMBER, but we don't support them. */
+  if (format == SVN_FS_FS__PACKED_REVPROP_SQLITE_DEV_FORMAT)
+    return svn_error_createf(SVN_ERR_FS_UNSUPPORTED_FORMAT, NULL,
+                             _("Found format '%d', only created by "
+                               "unreleased dev builds; see "
+                               "http://subversion.apache.org"
+                               "/docs/release-notes/1.7#revprop-packing"),
+                             format);
+
   /* We support all formats from 1-current simultaneously */
   if (1 <= format && format <= SVN_FS_FS__FORMAT_NUMBER)
-    {
-      if (format == SVN_FS_FS__PACKED_REVPROP_SQLITE_DEV_FORMAT)
-        return svn_error_createf(SVN_ERR_FS_UNSUPPORTED_FORMAT, NULL,
-                                 _("Found format '%d', only created by "
-                                   "unreleased dev builds; dump using "
-                                   "1.7.0-alpha3, or upgrade using "
-                                   "tools/dev/unwritten.py"),
-                                 format);
-      else
-        return SVN_NO_ERROR;
-    }
+    return SVN_NO_ERROR;
 
   return svn_error_createf(SVN_ERR_FS_UNSUPPORTED_FORMAT, NULL,
      _("Expected FS format between '1' and '%d'; found format '%d'"),
