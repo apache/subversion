c1a60877b751ccfb841b9132cb885ddf8a330fcf
Hyrum Kurt Wright <hwright@apache.org>
2011-07-11 16:43:09 +0000

    Tagging release 1.7.0-alpha3 with svn_version.h matching tarball.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/tags/1.7.0-alpha3@1145236 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_version.h
+++ subversion/include/svn_version.h
@@ -94,7 +94,7 @@ extern "C" {
  *
  * Always change this at the same time as SVN_VER_NUMTAG.
  */
-#define SVN_VER_TAG        " (under development)"
+#define SVN_VER_TAG        " (Alpha 3)"
 
 
 /** Number tag: a string describing the version.
@@ -109,7 +109,7 @@ extern "C" {
  *
  * Always change this at the same time as SVN_VER_TAG.
  */
-#define SVN_VER_NUMTAG     "-dev"
+#define SVN_VER_NUMTAG     "-alpha3"
 
 
 /** Revision number: The repository revision number of this release.
