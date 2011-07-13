f2e52cc62fc6ea8fa9e3a7150c9e4c1388a1f952
Daniel Shahaf <danielsh@apache.org>
2011-07-13 22:41:39 +0000

    Two tweaks to the verify invocation logic:
    * subversion/libsvn_repos/dump.c
      (svn_repos_verify_fs2):
        Verify global/auxiliary data before verifying revisions.
        (If it fails, 'svnadmin dump' can still be used to at least
        sanity check the revisions data.)
    
        Also, only verify global/auxiliary data when verifying r0.  Plain
        'svnadmin verify' will trigger that, and 'svnadmin verify -rN' shouldn't
        go through this (expensive) check.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1146507 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_repos/dump.c
+++ subversion/libsvn_repos/dump.c
@@ -1282,6 +1282,11 @@ svn_repos_verify_fs2(svn_repos_t *repos,
                                "(youngest revision is %ld)"),
                              end_rev, youngest);
 
+  /* Verify global/auxiliary data before verifying revisions. */
+  if (start_rev == 0)
+    SVN_ERR(svn_fs_verify(svn_fs_path(fs, pool), cancel_func, cancel_baton,
+                          pool));
+
   /* Create a notify object that we can reuse within the loop. */
   if (notify_func)
     notify = svn_repos_notify_create(svn_repos_notify_verify_rev_end,
@@ -1332,9 +1337,6 @@ svn_repos_verify_fs2(svn_repos_t *repos,
       notify = svn_repos_notify_create(svn_repos_notify_verify_end, iterpool);
       notify_func(notify_baton, notify, iterpool);
     }
-
-  SVN_ERR(svn_fs_verify(svn_fs_path(fs, pool), cancel_func, cancel_baton,
-                        pool));
 
   /* Per-backend verification. */
   svn_pool_destroy(iterpool);
