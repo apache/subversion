02043a1132fbddd88cc0b33b01f2ff5f073bb8cf
Julian Foad <julianfoad@apache.org>
2011-07-14 09:51:57 +0000

    * subversion/include/svn_client.h   (svn_client_info2_t): On the wc_info member, remove 'struct' keyword for     consistency, and mark the doc string for Doxygen.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1146620 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_client.h
+++ subversion/include/svn_client.h
@@ -5390,8 +5390,8 @@ typedef struct svn_client_info2_t
   /** An exclusive lock, if present.  Could be either local or remote. */
   const svn_lock_t *lock;
 
-  /* Possible information about the working copy, NULL if not valid. */
-  const struct svn_wc_info_t *wc_info;
+  /** Possible information about the working copy, NULL if not valid. */
+  const svn_wc_info_t *wc_info;
 
 } svn_client_info2_t;
 
