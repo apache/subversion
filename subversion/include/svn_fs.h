5511f8d9307343118d87f9fe77afeb66d4b5c611
Daniel Shahaf <danielsh@apache.org>
2011-07-15 08:17:03 +0000

    On the fs-progress branch, tweak a docstring.
    * subversion/include/svn_fs.h
      (svn_fs_progress_notify_func_t):
        Define the invalid value for TOTAL.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/fs-progress@1147009 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_fs.h
+++ subversion/include/svn_fs.h
@@ -257,7 +257,8 @@ typedef struct svn_fs_progress_info_t svn_fs_progress_info_t;
  * Callback function type for progress notification.
  *
  * @a progress is the number of steps already completed, out of @a total steps.
- * @a info must be @c NULL in Subversion 1.8.  @a baton is the callback baton.
+ * @a total may be -1 if it's unknown or unavailable. @a info must be @c NULL
+ * in Subversion 1.8.  @a baton is the callback baton.
  *
  * @note @a total may be a best-effort estimate.
  *
