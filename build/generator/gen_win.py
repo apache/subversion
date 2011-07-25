2830d8a40844e6ee2e77f2354fa01fa9629827e3
Bert Huiben <rhuijben@apache.org>
2011-07-25 18:48:20 +0000

    * build/generator/gen_win.py   (parse_options): Don't generate a libsvn_auth_gpg_agent project on Windows.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1150842 13f79535-47bb-0310-9956-ffa450edef68

--- build/generator/gen_win.py
+++ build/generator/gen_win.py
@@ -91,7 +91,8 @@ class GeneratorBase(gen_base.GeneratorBase):
     self.skip_sections = { 'mod_dav_svn': None,
                            'mod_authz_svn': None,
                            'libsvn_auth_kwallet': None,
-                           'libsvn_auth_gnome_keyring': None }
+                           'libsvn_auth_gnome_keyring': None,
+                           'libsvn_auth_gpg_agent': None }
 
     # Instrumentation options
     self.disable_shared = None
