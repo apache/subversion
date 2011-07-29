928878b2a1f080fcd460c269646613776acbbbdf
Stefan Sperling <stsp@apache.org>
2011-07-29 16:53:01 +0000

    Temporarily revert r1152293 and r1152294. r1152293 is causing test failures.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1152300 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/wc-queries.sql
+++ subversion/libsvn_wc/wc-queries.sql
@@ -1342,10 +1342,6 @@ WHERE wc_id = ?1 AND local_relpath = ?2
    (SELECT MAX(op_depth) FROM nodes
     WHERE wc_id = ?1 AND local_relpath = ?2 AND op_depth > 0)
 
--- STMT_SELECT_MOVED_HERE_CHILDREN
-SELECT moved_to, local_relpath FROM nodes_current
-WHERE wc_id = ?1 AND moved_to > ?2 || '/'
-
 /* ------------------------------------------------------------------------- */
 
 /* Queries for verification. */
