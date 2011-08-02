apache/subversion

commit 666b0fa29b381e6b3685acea190d683a30e64202

Properly skip checking out file externals from a different repository than intended. This fixes the XFail test committed in r1153106.
* subversion/libsvn_client/externals.c
  (handle_external_item_change): Check if the repository roots match to avoid
    corrupting the working copy state.

* subversion/tests/cmdline/externals_tests.py
  (file_externals_different_repos): Remove XFail marker. Add relocate and
    second update to test more scenarios.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1153110 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_client/externals.c
@@ -898,6 +898,11 @@ handle_external_item_change(const struct external_change_baton_t *eb,
                    scratch_pool));
           break;
         case svn_node_file:
+          if (strcmp(eb->repos_root_url, ra_cache.repos_root_url))
+            return svn_error_createf(SVN_ERR_UNSUPPORTED_FEATURE, NULL,
+                      _("Unsupported external: "
+                        "url of file external '%s' is not in repository '%s'"),
+                      new_url, eb->repos_root_url);
           SVN_ERR(switch_file_external(local_abspath,
                                        new_url,
                                        &new_item->peg_revision,

subversion/tests/cmdline/externals_tests.py
@@ -1877,7 +1877,6 @@ def exclude_externals(sbox):
                                         None, None, None, None, False,
                                         '--set-depth', 'infinity', wc_dir)
 
-@XFail()
 def file_externals_different_repos(sbox):
   "update file externals via different url"
 
@@ -1906,7 +1905,21 @@ def file_externals_different_repos(sbox):
   # The externals from r2 should fail, but currently pass.
   # This creates a wc.db inconsistency
   svntest.actions.run_and_verify_update(wc_dir,
-                                        expected_output, None, None)
+                                        expected_output, None, None,
+                                        'svn: warning: W200007: Unsupported.*')
+
+  svntest.actions.run_and_verify_svn(None, None, [],
+                                     'relocate', r1_url, r2_url, wc_dir)
+
+
+  expected_output = svntest.wc.State(wc_dir, {
+    'r2-e-1'            : Item(status='A '),
+    'r2-e-2'            : Item(status='A '),
+  })
+
+  svntest.actions.run_and_verify_update(wc_dir,
+                                        expected_output, None, None,
+                                        '.*svn: warning: W170000:.*')
 
 
 ########################################################################

commit 3080dbc5dc635b458e21674c7d5e8a04ec35b950

Following up on r1153110, apply the same 'same repository' check on updating file externals. This resolves the non-ra_local failures caused by r1153110.
* subversion/libsvn_client/externals.c
  (handle_external_item_change): Add check.

* subversion/tests/cmdline/externals_tests.py
  (file_externals_different_repos): Update expected result.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1153149 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_client/externals.c
@@ -952,6 +952,11 @@ handle_external_item_change(const struct external_change_baton_t *eb,
                                       scratch_pool));
           break;
         case svn_node_file:
+          if (strcmp(eb->repos_root_url, ra_cache.repos_root_url))
+            return svn_error_createf(SVN_ERR_UNSUPPORTED_FEATURE, NULL,
+                      _("Unsupported external: "
+                        "url of file external '%s' is not in repository '%s'"),
+                      new_url, eb->repos_root_url);
           SVN_ERR(switch_file_external(local_abspath,
                                        new_url,
                                        &new_item->peg_revision,

subversion/tests/cmdline/externals_tests.py
@@ -1919,7 +1919,7 @@ def file_externals_different_repos(sbox):
 
   svntest.actions.run_and_verify_update(wc_dir,
                                         expected_output, None, None,
-                                        '.*svn: warning: W170000:.*')
+                                        'svn: warning: W200007: Unsupported.*')
 
 
 ########################################################################