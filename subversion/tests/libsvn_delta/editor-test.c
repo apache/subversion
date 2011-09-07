43b8dc39dff38938f30ee1af3ea2bd0b01a856fa
Hyrum Kurt Wright <hwright@apache.org>
2011-09-07 19:36:01 +0000

    A checkpoint commit of work on the Ev2 compat code.  This extends the tests and compat code a bit.
    * subversion/tests/libsvn_delta/editor-test.c
      (SET_STR): New.
      (editor_from_delta_editor_test): Extend this test with some prop editing.
     
    * subversion/libsvn_delta/compat.c
      (edit_baton, file_baton): Add a few members.
      (action, path_action, prop_args): New.
      (add_action): New.
      (ev2_add_directory, ev2_add_file, ev2_open_file, ev2_change_file_prop):
        Queue up a few actions for processing.
      (ev2_close_edit): Implement post-pass processing.
      (svn_delta_from_editor): Populate a few baton members.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1166332 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/libsvn_delta/editor-test.c
+++ subversion/tests/libsvn_delta/editor-test.c
@@ -27,9 +27,12 @@
 
 #include "svn_types.h"
 #include "svn_error.h"
+#include "svn_pools.h"
 #include "svn_editor.h"
 
 #include "../svn_test_fs.h"
+
+#define SET_STR(ps, s) ((ps)->data = (s), (ps)->len = strlen(s))
 
 /* We use svn_repos APIs in some of these tests simply for convenience. */
 
@@ -201,38 +204,78 @@ editor_from_delta_editor_test(const svn_test_opts_t *opts,
   svn_repos_t *repos;
   svn_fs_t *fs;
   svn_fs_txn_t *txn;
-  svn_fs_root_t *revision_root, *txn_root, *base_root;
+  svn_fs_root_t *txn_root;
   svn_revnum_t youngest_rev;
-  void *dedit_baton;
-  svn_delta_editor_t *deditor;
-  svn_editor_t *editor;
+  apr_pool_t *iterpool;
+  svn_string_t s1;
+  int i;
 
   /* Create a filesystem and repository. */
   SVN_ERR(svn_test__create_repos(&repos, "ev2-from-delta-editor-test",
                                  opts, pool));
   fs = svn_repos_fs(repos);
 
-  /* Prepare a txn to receive the greek tree. */
-  SVN_ERR(svn_fs_begin_txn(&txn, fs, 0, pool));
-  SVN_ERR(svn_fs_txn_root(&txn_root, txn, pool));
+  {
+    /* Prepare a txn to receive the greek tree. */
+    SVN_ERR(svn_fs_begin_txn2(&txn, fs, 0, 0, pool));
+    SVN_ERR(svn_fs_txn_root(&txn_root, txn, pool));
 
-  /* Create and commit the greek tree. */
-  SVN_ERR(svn_test__create_greek_tree(txn_root, pool));
-  SVN_ERR(svn_repos_fs_commit_txn(NULL, repos, &youngest_rev, txn, pool));
-  SVN_TEST_ASSERT(SVN_IS_VALID_REVNUM(youngest_rev));
+    /* Create and commit the greek tree. */
+    SVN_ERR(svn_test__create_greek_tree(txn_root, pool));
+    SVN_ERR(svn_repos_fs_commit_txn(NULL, repos, &youngest_rev, txn, pool));
+    SVN_TEST_ASSERT(SVN_IS_VALID_REVNUM(youngest_rev));
+  }
 
-  SVN_ERR(svn_fs_revision_root(&revision_root, fs, youngest_rev, pool));
-  SVN_ERR(svn_fs_revision_root(&base_root, fs, 0, pool));
+  {
+    /* Now chuck in some prop changes. */
+    const char *props[4][2] = {
+        { "If you change to meet a frown,", "Do not let it stay." },
+        { "Quickly turn it upside down,", "And smile that frown away." },
+        { "No one likes a frowny face,", "Change it for a smile," },
+        { "Make the world a better place,", "By smiling all the while!" }
+      };
 
-  /* Construct our editor, and from it a delta editor. */
-  SVN_ERR(get_noop_editor(&editor, NULL, NULL, NULL, pool, pool));
-  SVN_ERR(svn_delta_from_editor(&deditor, &dedit_baton, editor, pool));
+    SVN_ERR(svn_fs_begin_txn2(&txn, fs, 1, 0, pool));
+    SVN_ERR(svn_fs_txn_root(&txn_root, txn, pool));
 
-  SVN_ERR(svn_repos_replay2(revision_root, "", SVN_INVALID_REVNUM, TRUE,
-                            deditor, dedit_baton, NULL, NULL, pool));
+    for (i = 0; i < 4; i++)
+      {
+        SET_STR(&s1, props[i][1]);
+        SVN_ERR(svn_fs_change_node_prop(txn_root, "iota",
+                                        props[i][0], &s1, pool));
+      }
 
-  /* Close the edit. */
-  SVN_ERR(deditor->close_edit(dedit_baton, pool));
+    SVN_ERR(svn_repos_fs_commit_txn(NULL, repos, &youngest_rev, txn, pool));
+    SVN_TEST_ASSERT(SVN_IS_VALID_REVNUM(youngest_rev));
+  }
+
+  /* Now, let's look at each revision. */
+  iterpool = svn_pool_create(pool);
+  for (i = 0; i < youngest_rev; i++)
+    {
+      svn_fs_root_t *revision_root;
+      void *dedit_baton;
+      svn_delta_editor_t *deditor;
+      svn_editor_t *editor;
+
+      svn_pool_clear(iterpool);
+
+      SVN_ERR(svn_fs_revision_root(&revision_root, fs, i, iterpool));
+
+      /* Construct our editor, and from it a delta editor. */
+      SVN_ERR(get_noop_editor(&editor, NULL, NULL, NULL, iterpool, iterpool));
+      SVN_ERR(svn_delta_from_editor(&deditor, &dedit_baton, editor, iterpool));
+
+      SVN_ERR(svn_repos_replay2(revision_root, "", SVN_INVALID_REVNUM, TRUE,
+                                deditor, dedit_baton, NULL, NULL, iterpool));
+
+      /* Close the edit. */
+      SVN_ERR(deditor->close_edit(dedit_baton, iterpool));
+
+      /* ### This is the point at which we should probably verify something,
+       * ### but right now, we're just pleased if the tests don't crash. */
+    }
+  svn_pool_destroy(iterpool);
 
   return SVN_NO_ERROR;
 }
