a143c103cc0587af2fa67e524afa80f069d42856
Daniel Shahaf <danielsh@apache.org>
2011-09-20 13:09:37 +0000

    * subversion/libsvn_fs_base/locks-table.c   (svn_fs_bdb__locks_get): Move a comment to its correct place.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1173129 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_base/bdb/locks-table.c
+++ subversion/libsvn_fs_base/bdb/locks-table.c
@@ -248,11 +248,11 @@ svn_fs_bdb__locks_get(svn_fs_t *fs,
   db_err = svn_bdb_dbc_get(cursor, &key, svn_fs_base__result_dbt(&value),
                            DB_SET_RANGE);
 
-  /* As long as the prefix of the returned KEY matches LOOKUP_PATH we
-     know it is either LOOKUP_PATH or a decendant thereof.  */
   if (!svn_fspath__is_root(path, strlen(path)))
     lookup_path = apr_pstrcat(pool, path, "/", (char *)NULL);
 
+  /* As long as the prefix of the returned KEY matches LOOKUP_PATH we
+     know it is either LOOKUP_PATH or a decendant thereof.  */
   while ((! db_err)
          && strncmp(lookup_path, key.data, strlen(lookup_path)) == 0)
     {
