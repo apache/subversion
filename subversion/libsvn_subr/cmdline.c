15ba7e19b6390ab868ab634a88dd6bd376420eb6
Philip Martin <philip@apache.org>
2011-10-12 18:30:04 +0000

    * subversion/libsvn_subr/cmdline.c   (svn_cmdline_init): Use standard function for better error reporting,    tweak comment about DSO lifetime.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1182513 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/cmdline.c
+++ subversion/libsvn_subr/cmdline.c
@@ -73,6 +73,7 @@ svn_cmdline_init(const char *progname, FILE *error_stream)
   apr_status_t status;
   apr_pool_t *pool;
   svn_error_t *err;
+  char prefix_buf[64];  /* 64 is probably bigger than most program names */
 
 #ifndef WIN32
   {
@@ -197,11 +198,17 @@ svn_cmdline_init(const char *progname, FILE *error_stream)
       return EXIT_FAILURE;
     }
 
-  /* This has to happen before any pools are created. */
+  strncpy(prefix_buf, progname, sizeof(prefix_buf) - 3);
+  prefix_buf[sizeof(prefix_buf) - 3] = '\0';
+  strcat(prefix_buf, ": ");
+
+  /* DSO pool must be created before any other pools used by the
+     application so that pool cleanup doesn't unload DSOs too
+     early. */
   if ((err = svn_dso_initialize2()))
     {
-      if (error_stream && err->message)
-        fprintf(error_stream, "%s", err->message);
+      if (error_stream)
+        svn_handle_error2(err, error_stream, TRUE, prefix_buf);
 
       svn_error_clear(err);
       return EXIT_FAILURE;
@@ -223,8 +230,8 @@ svn_cmdline_init(const char *progname, FILE *error_stream)
 
   if ((err = svn_nls_init()))
     {
-      if (error_stream && err->message)
-        fprintf(error_stream, "%s", err->message);
+      if (error_stream)
+        svn_handle_error2(err, error_stream, TRUE, prefix_buf);
 
       svn_error_clear(err);
       return EXIT_FAILURE;
