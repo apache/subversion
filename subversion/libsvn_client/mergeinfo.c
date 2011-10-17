0a0307e70fc052057d19307b71e3b91ff9b0be00
Hyrum Kurt Wright <hwright@apache.org>
2011-10-17 20:43:16 +0000

    * subversion/libsvn_client/mergeinfo.c   (svn_client_mergeinfo_log): Use svn_mergeinfo_merge2().
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1185365 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/mergeinfo.c
+++ subversion/libsvn_client/mergeinfo.c
@@ -1905,9 +1905,9 @@ svn_client_mergeinfo_log(svn_boolean_t finding_merged,
                                            subtree_history,
                                            subtree_source_history, TRUE,
                                            scratch_pool, iterpool));
-          SVN_ERR(svn_mergeinfo_merge(subtree_mergeinfo,
-                                      merged_via_history,
-                                      scratch_pool));
+          SVN_ERR(svn_mergeinfo_merge2(subtree_mergeinfo,
+                                       merged_via_history,
+                                       scratch_pool, scratch_pool));
         }
 
       SVN_ERR(svn_mergeinfo_inheritable2(&subtree_inheritable_mergeinfo,
