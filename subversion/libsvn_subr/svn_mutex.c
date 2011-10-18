ed7333ad3375273e8b4d1ab19ccab58c9d403404
Stefan Fuhrmann <stefan2@apache.org>
2011-10-18 21:05:24 +0000

    Set the mutex pointer to a defined value even if synchronization is not supported and the pointer will never actually be accessed.
    * subversion/libsvn_subr/svn_mutex.c
      (svn_mutex__init): NULL the mutex if APR has no threads
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1185860 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/svn_mutex.c
+++ subversion/libsvn_subr/svn_mutex.c
@@ -29,8 +29,12 @@ svn_mutex__init(svn_mutex__t **mutex_p,
                 svn_boolean_t enable_mutex, 
                 apr_pool_t *result_pool)
 {
-#if APR_HAS_THREADS
+  // always initialize the mutex pointer, even though it is not
+  // strictly necessary if APR_HAS_THREADS has not been set
+
   *mutex_p = NULL;
+
+#if APR_HAS_THREADS
   if (enable_mutex)
     {
       apr_thread_mutex_t *apr_mutex;
