281048cee47d4f3e3c94c9fc71ca9c03336a51ec
Julian Foad <julianfoad@apache.org>
2011-10-19 14:39:38 +0000

    * subversion/libsvn_client/mergeinfo.h   (svn_client__parse_mergeinfo): Tiny doc string clarification.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1186243 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/mergeinfo.h
+++ subversion/libsvn_client/mergeinfo.h
@@ -287,7 +287,7 @@ svn_client__get_history_as_mergeinfo(svn_mergeinfo_t *mergeinfo_p,
                                      apr_pool_t *pool);
 
 /* Parse any explicit mergeinfo on LOCAL_ABSPATH and store it in
-   MERGEINFO.  If no record of any mergeinfo exists, set MERGEINFO to NULL.
+   *MERGEINFO.  If no record of any mergeinfo exists, set *MERGEINFO to NULL.
    Does not acount for inherited mergeinfo. */
 svn_error_t *
 svn_client__parse_mergeinfo(svn_mergeinfo_t *mergeinfo,
