bcaa3c16c120817843e5bc73edcb6f72d8ccdfc3
Hyrum Kurt Wright <hwright@apache.org>
2011-11-21 16:00:39 +0000

    Merge r1204478 from trunk:
     * r1204478
       Avoid using pool memory after it has been freed in client merge code.
       Justification:
         Undefined behaviour.
       Votes:
         +1: philip, rhuijben, hwright
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/1.7.x@1204566 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/merge.c
+++ subversion/libsvn_client/merge.c
@@ -555,6 +555,7 @@ tree_conflict(merge_cmd_baton_t *merge_b,
 
       if (merge_b->conflicted_paths == NULL)
         merge_b->conflicted_paths = apr_hash_make(merge_b->pool);
+      victim_abspath = apr_pstrdup(merge_b->pool, victim_abspath);
 
       apr_hash_set(merge_b->conflicted_paths, victim_abspath,
                    APR_HASH_KEY_STRING, victim_abspath);
@@ -596,6 +597,7 @@ tree_conflict_on_add(merge_cmd_baton_t *merge_b,
 
       if (merge_b->conflicted_paths == NULL)
         merge_b->conflicted_paths = apr_hash_make(merge_b->pool);
+      victim_abspath = apr_pstrdup(merge_b->pool, victim_abspath);
 
       apr_hash_set(merge_b->conflicted_paths, victim_abspath,
                    APR_HASH_KEY_STRING, victim_abspath);
@@ -626,6 +628,7 @@ tree_conflict_on_add(merge_cmd_baton_t *merge_b,
 
       if (merge_b->conflicted_paths == NULL)
         merge_b->conflicted_paths = apr_hash_make(merge_b->pool);
+      victim_abspath = apr_pstrdup(merge_b->pool, victim_abspath);
 
       apr_hash_set(merge_b->conflicted_paths, victim_abspath,
                    APR_HASH_KEY_STRING, victim_abspath);
