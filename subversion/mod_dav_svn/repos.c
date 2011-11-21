7b1e9444dcb8b4ce60fc47c6dc5d5ee107ee971d
Philip Martin <philip@apache.org>
2011-11-21 19:56:50 +0000

    Followup to r1204610.
    * subversion/mod_dav_svn/repos.c (log_warning): Purge tracing from chain.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1204673 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/mod_dav_svn/repos.c
+++ subversion/mod_dav_svn/repos.c
@@ -1161,6 +1161,7 @@ create_private_resource(const dav_resource *base,
 static void log_warning(void *baton, svn_error_t *err)
 {
   request_rec *r = baton;
+  const char *continuation = "";
 
   /* ### hmm. the FS is cleaned up at request cleanup time. "r" might
      ### not really be valid. we should probably put the FS into a
@@ -1170,9 +1171,13 @@ static void log_warning(void *baton, svn_error_t *err)
      ### of our functions ... ??
   */
 
+  /* Not showing file/line so no point in tracing */
+  err = svn_error_purge_tracing(err);
   while (err)
     {
-      ap_log_rerror(APLOG_MARK, APLOG_ERR, APR_EGENERAL, r, "%s", err->message);
+      ap_log_rerror(APLOG_MARK, APLOG_ERR, APR_EGENERAL, r, "%s%s",
+                    continuation, err->message);
+      continuation = "-";
       err = err->child;
     }
 }
