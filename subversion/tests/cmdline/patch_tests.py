4950d5f14f86baa8cbaeabd50d287d3345a1e52e
Stefan Sperling <stsp@apache.org>
2011-11-28 22:34:49 +0000

    Make the diff parser handle comment lines indicating missing EOLs if such comments appear within the diff text of a hunk, not just at the very end of the hunk's diff text.
    Reported by: Peter Balogh
    http://svn.haxx.se/users/archive-2011-11/0523.shtml
    
    * subversion/libsvn_diff/parse-diff.c
      (parse_next_hunk): The '\ No newline at end of ...' comments may appear
       not only at the very end of a hunk text, but also at the end of the
       original version (the last line starting with '-') or at the end of the
       modified version (the last line starting with '+') of the hunk text.
       Properly account for this by keeping track of the last byte in the
       original and modified hunk texts independently.
    
    * subversion/tests/cmdline/patch_tests.py
      (patch_target_no_eol_at_eof, test_list): New test.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1207663 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/patch_tests.py
+++ subversion/tests/cmdline/patch_tests.py
@@ -3988,6 +3988,63 @@ def patch_delete_and_skip(sbox):
                                        1, # check-props
                                        1) # dry-run
 
+def patch_target_no_eol_at_eof(sbox):
+  "patch target with no eol at eof"
+
+  sbox.build()
+  wc_dir = sbox.wc_dir
+
+  patch_file_path = make_patch_path(sbox)
+  iota_path = os.path.join(wc_dir, 'iota')
+
+  iota_contents = [
+    "This is the file iota."
+  ]
+
+  svntest.main.file_write(iota_path, ''.join(iota_contents))
+  expected_output = svntest.wc.State(wc_dir, {
+    'iota'  : Item(verb='Sending'),
+    })
+  expected_status = svntest.actions.get_virginal_state(wc_dir, 1)
+  expected_status.tweak('iota', wc_rev=2)
+  svntest.actions.run_and_verify_commit(wc_dir, expected_output,
+                                        expected_status, None, wc_dir)
+  unidiff_patch = [
+    "--- iota\t(revision 1)\n",
+    "+++ iota\t(working copy)\n",
+    "@@ -1,7 +1,7 @@\n",
+    "-This is the file iota.\n"
+    "\\ No newline at end of file\n",
+    "+It is the file 'iota'.\n",
+    "\\ No newline at end of file\n",
+  ]
+
+  svntest.main.file_write(patch_file_path, ''.join(unidiff_patch))
+
+  iota_contents = [
+    "It is the file 'iota'."
+  ]
+  expected_output = [
+    'U         %s\n' % os.path.join(wc_dir, 'iota'),
+  ]
+
+  expected_disk = svntest.main.greek_state.copy()
+  expected_disk.tweak('iota', contents=''.join(iota_contents))
+
+  expected_status = svntest.actions.get_virginal_state(wc_dir, 1)
+  expected_status.tweak('iota', status='M ', wc_rev=2)
+
+  expected_skip = wc.State('', { })
+
+  svntest.actions.run_and_verify_patch(wc_dir, os.path.abspath(patch_file_path),
+                                       expected_output,
+                                       expected_disk,
+                                       expected_status,
+                                       expected_skip,
+                                       None, # expected err
+                                       1, # check-props
+                                       1) # dry-run
+
 ########################################################################
 #Run the tests
 
@@ -4030,6 +4087,7 @@ test_list = [ None,
               patch_reversed_add_with_props2,
               patch_dev_null,
               patch_delete_and_skip,
+              patch_target_no_eol_at_eof,
             ]
 
 if __name__ == '__main__':
