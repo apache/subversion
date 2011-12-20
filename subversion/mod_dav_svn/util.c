31906b71c10bcaf4cf821a00d75ffcca9c33fb78
Philip Martin <philip@apache.org>
2011-12-20 15:02:00 +0000

    Fix issue 4086, log reason for POST request failure.
    * subversion/mod_dav_svn/repos.c
      (dav_svn__method_post): Log error.
    
    * subversion/mod_dav_svn/dav_svn.h
      (dav_svn__log_err): New.
    
    * subversion/mod_dav_svn/util.c
      (dav_svn__log_err): New.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1221303 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/mod_dav_svn/util.c
+++ subversion/mod_dav_svn/util.c
@@ -620,6 +620,30 @@ dav_svn__final_flush_or_error(request_rec *r,
   return derr;
 }
 
+void dav_svn__log_err(request_rec *r,
+                      dav_error *err,
+                      int level)
+{
+    dav_error *errscan;
+
+    /* Log the errors */
+    /* ### should have a directive to log the first or all */
+    for (errscan = err; errscan != NULL; errscan = errscan->prev) {
+        if (errscan->desc == NULL)
+            continue;
+
+        if (errscan->save_errno != 0) {
+            errno = errscan->save_errno;
+            ap_log_rerror(APLOG_MARK, level, errno, r, "%s  [%d, #%d]",
+                          errscan->desc, errscan->status, errscan->error_id);
+        }
+        else {
+            ap_log_rerror(APLOG_MARK, level, 0, r,
+                          "%s  [%d, #%d]",
+                          errscan->desc, errscan->status, errscan->error_id);
+        }
+    }
+}
 
 int
 dav_svn__error_response_tag(request_rec *r,
