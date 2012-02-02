22e6ef55e72995af4ac9e732b745cf91ad37e6e6
Stefan Sperling <stsp@apache.org>
2012-02-02 15:19:34 +0000

    On the 1.7.x-issue4059 branch, merge r1146762, r1201419, r1227237, r1227250, r1227352, r1227372, r1227384, r1227385, r1239382, r1239631, and r1239655 from trunk, resolving conflicts.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/1.7.x-issue4059@1239665 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/mergeinfo.c
+++ subversion/libsvn_client/mergeinfo.c
@@ -612,7 +612,7 @@ svn_client__get_wc_or_repos_mergeinfo_catalog(
      a URL and without that we cannot get accurate mergeinfo for
      TARGET_WCPATH. */
   SVN_ERR(svn_wc__node_get_origin(NULL, &target_rev, &repos_relpath,
-                                  &repos_root, NULL,
+                                  &repos_root, NULL, NULL,
                                   ctx->wc_ctx, local_abspath, FALSE,
                                   scratch_pool, scratch_pool));
 
@@ -1124,7 +1124,7 @@ get_mergeinfo(svn_mergeinfo_catalog_t *mergeinfo_catalog,
                                       scratch_pool));
 
       SVN_ERR(svn_wc__node_get_origin(NULL, &rev, &repos_relpath,
-                                      &repos_root_url, NULL,
+                                      &repos_root_url, NULL, NULL,
                                       ctx->wc_ctx, local_abspath, FALSE,
                                       scratch_pool, scratch_pool));
 
