e303813a964deadf35a3f86f4ec0fc58c8965b9b
Hyrum Kurt Wright <hwright@apache.org>
2012-02-07 21:22:52 +0000

    Revert r1241608, since it now tests for an impossible condition.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1241635 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_delta/compat.c
+++ subversion/libsvn_delta/compat.c
@@ -986,8 +986,6 @@ struct editor_baton
   struct operation root;
   svn_boolean_t *make_abs_paths;
 
-  svn_boolean_t driven;
-
   apr_hash_t *paths;
   apr_pool_t *edit_pool;
 };
@@ -1666,7 +1664,6 @@ complete_cb(void *baton,
 
   /* Drive the tree we've created. */
   err = drive_root(&eb->root, eb->deditor, *eb->make_abs_paths, scratch_pool);
-  eb->driven = TRUE;
   if (!err)
      {
        err = svn_error_compose_create(err, eb->deditor->close_edit(
@@ -1686,16 +1683,14 @@ abort_cb(void *baton,
          apr_pool_t *scratch_pool)
 {
   struct editor_baton *eb = baton;
-  svn_error_t *err = SVN_NO_ERROR;
+  svn_error_t *err;
   svn_error_t *err2;
 
   /* We still need to drive anything we collected in the editor to this
      point. */
 
   /* Drive the tree we've created. */
-  if (!eb->driven)
-    err = drive_root(&eb->root, eb->deditor, *eb->make_abs_paths,
-                     scratch_pool);
+  err = drive_root(&eb->root, eb->deditor, *eb->make_abs_paths, scratch_pool);
 
   err2 = eb->deditor->abort_edit(eb->dedit_baton, scratch_pool);
 
