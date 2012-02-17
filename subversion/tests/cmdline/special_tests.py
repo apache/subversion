b5b1387a4c99ef94fb9a10d174b94c3a95200529
Daniel Shahaf <danielsh@apache.org>
2012-02-17 01:05:20 +0000

    Add a regression test for issue #4119.
    * subversion/tests/cmdline/special_tests.py
      (cat_added_symlink): New, XFail.
      (test_list): Run it.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1245278 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/special_tests.py
+++ subversion/tests/cmdline/special_tests.py
@@ -1054,6 +1054,19 @@ def externals_as_symlink_targets(sbox):
 
   sbox.simple_commit()
     
+@XFail()
+@Issue(4119)
+@SkipUnless(svntest.main.is_posix_os)
+def cat_added_symlink(sbox):
+  "cat added symlink"
+
+  sbox.build(read_only = True)
+
+  kappa_path = sbox.ospath('kappa')
+  os.symlink('iota', kappa_path)
+  sbox.simple_add('kappa')
+  svntest.actions.run_and_verify_svn(None, "link iota", [],
+                                     "cat", kappa_path)
 
 ########################################################################
 # Run the tests
@@ -1084,6 +1097,7 @@ test_list = [ None,
               update_symlink,
               replace_symlinks,
               externals_as_symlink_targets,
+              cat_added_symlink,
              ]
 
 if __name__ == '__main__':
