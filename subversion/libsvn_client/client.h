76720e506223a9a72c2e6bba7cf8cb5ebc74b542
Hyrum Kurt Wright <hwright@apache.org>
2012-02-21 20:43:35 +0000

    Ev2 shims: Instead of installing callbacks generally for every ra session, do so only for a commit.  Also, implement the various client-side shim callbacks.
    * subversion/libsvn_client/ra.c
      (svn_client__open_ra_session_internal): Don't register the shims.
    
    * subversion/libsvn_client/util.c
      (shim_callbacks_baton): New.
      (fetch_props_func, fetch_kind_func, fetch_base_func): Implement.
      (svn_client__get_shim_callbacks): Add a baton, and a few params.
    
    * subversion/libsvn_client/commit.c
      (get_ra_editor): Register the callbacks prior to getting a commit editor.
    
    * subversion/libsvn_client/client.h
      (svn_client__get_shim_callbacks): Add a few params.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1291997 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/client.h
+++ subversion/libsvn_client/client.h
@@ -1075,7 +1075,9 @@ svn_client__get_normalized_stream(svn_stream_t **normal_stream,
 
 /* Return a set of callbacks to use with the Ev2 shims. */
 svn_delta_shim_callbacks_t *
-svn_client__get_shim_callbacks(apr_pool_t *result_pool);
+svn_client__get_shim_callbacks(svn_wc_context_t *wc_ctx,
+                               const char *anchor_abspath,
+                               apr_pool_t *result_pool);
 
 /* Return true if KIND is a revision kind that is dependent on the working
  * copy. Otherwise, return false. */
