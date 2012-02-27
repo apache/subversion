12e76d9892a24dc28c865e24f3e7adc4e4bb4cea
Daniel Shahaf <danielsh@apache.org>
2012-02-27 14:19:15 +0000

    Start documenting mod_dav_svn's and svnserve's different expectations from the REPOS_NAME in authz files rules.
    * subversion/include/svn_repos.h
      (svn_repos_authz_check_access):
        Document how @a REPOS_NAME is used currently.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1294167 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_repos.h
+++ subversion/include/svn_repos.h
@@ -3027,6 +3027,10 @@ svn_repos_authz_read(svn_authz_t **authz_p,
  * For compatibility with 1.6, and earlier, @a repos_name can be NULL
  * in which case it is equivalent to a @a repos_name of "".
  *
+ * @note Presently, @a repos_name must byte-for-byte match the repos_name
+ * specified in the authz file; it is treated as an opaque string, and not
+ * as a dirent.
+ *
  * @since New in 1.3.
  */
 svn_error_t *
