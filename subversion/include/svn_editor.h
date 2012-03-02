8897f5a5c8795928f6f4e1dff1f543beb19b8c67
Hyrum Kurt Wright <hwright@apache.org>
2012-03-02 13:29:43 +0000

    * subversion/include/svn_editor.h:   Complete a sentence started in r1296034.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1296198 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_editor.h
+++ subversion/include/svn_editor.h
@@ -383,7 +383,7 @@ extern "C" {
  */
 typedef struct svn_editor_t svn_editor_t;
 
-/** The kind of the checksum to be used throughout the 
+/** The kind of the checksum to be used throughout the #svn_editor_t APIs.
  *
  * @note ### This may change before Ev2 is official released, so just like
  * everything else in this file, please don't rely upon it until then.
