d5acef05a9ae878b084aa35b757590a3ef5fa057
Joe Schaefer <joes@apache.org>
2012-03-13 17:01:23 +0000

    * trunk/tools/server-side/svnpubsub/svnpubsub/client.py handle connect() exceptions a bit cleaner
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1300237 13f79535-47bb-0310-9956-ffa450edef68

--- tools/server-side/svnpubsub/svnpubsub/client.py
+++ tools/server-side/svnpubsub/svnpubsub/client.py
@@ -74,14 +74,11 @@ class Client(asynchat.async_chat):
     self.skipping_headers = True
 
     self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-    while True:
-      try:
-        self.connect((host, port))
-        break
-      except:
-        self.event_callback('connect failed, reconnecting in %d seconds'
-                            % (RECONNECT_DELAY))
-        time.sleep(RECONNECT_DELAY)
+    try:
+      self.connect((host, port))
+    except:
+      self.event_callback('error')
+      return
         
     ### should we allow for repository restrictions?
     self.push('GET /commits/xml HTTP/1.0\r\n\r\n')
