apache/subversion

commit fb2e52220c2c581dad43aaef6b8f0e5cf7c697c2

Convert svntest.wc to logging, rather than print() (or pprint).
* subversion/tests/cmdline/svntest/wc.py:
  Use the logging module to output error conditions.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1299950 13f79535-47bb-0310-9956-ffa450edef68

subversion/tests/cmdline/svntest/wc.py
@@ -27,8 +27,12 @@ import os
 import sys
 import re
 import urllib
+import logging
+import pprint
 
 import svntest
+
+logger = logging.getLogger()
 
 
 #
@@ -362,9 +366,8 @@ class State:
     if 0:
       check = tree.as_state()
       if self != check:
-        import pprint
-        pprint.pprint(self.desc)
-        pprint.pprint(check.desc)
+        logger.warn(pprint.pformat(self.desc))
+        logger.warn(pprint.pformat(check.desc))
         # STATE -> TREE -> STATE is lossy.
         # In many cases, TREE -> STATE -> TREE is not.
         # Even though our conversion from a TREE has lost some information, we
@@ -894,21 +897,25 @@ def display_nodes(label, path, expected, actual):
   'Display two nodes, expected and actual.'
   expected = item_to_node(path, expected)
   actual = item_to_node(path, actual)
-  print("=============================================================")
-  print("Expected '%s' and actual '%s' in %s tree are different!"
-        % (expected.name, actual.name, label))
-  print("=============================================================")
-  print("EXPECTED NODE TO BE:")
-  print("=============================================================")
-  expected.pprint()
-  print("=============================================================")
-  print("ACTUAL NODE FOUND:")
-  print("=============================================================")
-  actual.pprint()
+
+  output = [
+        "=============================================================\n",
+        "Expected '%s' and actual '%s' in %s tree are different!\n"
+                % (expected.name, actual.name, label),
+        "=============================================================\n",
+        "EXPECTED NODE TO BE:\n",
+        "=============================================================\n",
+        pprint.pformat(expected), '\n',
+        "=============================================================\n",
+        "ACTUAL NODE FOUND:\n",
+        "=============================================================\n",
+        pprint.pformat(actual), '\n',
+    ]
+  logger.warn(''.join(output))
 
 ### yanked from tree.py
 def default_singleton_handler(description, path, item):
   node = item_to_node(path, item)
-  print("Couldn't find node '%s' in %s tree" % (node.name, description))
-  node.pprint()
+  logger.warn("Couldn't find node '%s' in %s tree" % (node.name, description))
+  logger.warn(pprint.pformat(node))
   raise svntest.tree.SVNTreeUnequal

commit 5d63779223d359dfd047c4c25f7388b1e9519ace

Fix pretty-printing of some datastructures broken in r1299950.
It turns out that we have a few datastructures with pprint() methods in the
Python test suite, which don't really interface with the pprint module, although
they sound like they should.

* subversion/tests/cmdline/svntest/wc.py
  (display_nodes, default_singleton_handler): Use StringIO to build output to
    provide to the logger.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1299956 13f79535-47bb-0310-9956-ffa450edef68

subversion/tests/cmdline/svntest/wc.py
@@ -29,6 +29,7 @@ import re
 import urllib
 import logging
 import pprint
+import cStringIO as StringIO
 
 import svntest
 
@@ -898,24 +899,28 @@ def display_nodes(label, path, expected, actual):
   expected = item_to_node(path, expected)
   actual = item_to_node(path, actual)
 
-  output = [
-        "=============================================================\n",
-        "Expected '%s' and actual '%s' in %s tree are different!\n"
-                % (expected.name, actual.name, label),
-        "=============================================================\n",
-        "EXPECTED NODE TO BE:\n",
-        "=============================================================\n",
-        pprint.pformat(expected), '\n',
-        "=============================================================\n",
-        "ACTUAL NODE FOUND:\n",
-        "=============================================================\n",
-        pprint.pformat(actual), '\n',
-    ]
-  logger.warn(''.join(output))
+  o = StringIO()
+  o.write("=============================================================\n")
+  o.write("Expected '%s' and actual '%s' in %s tree are different!\n"
+                % (expected.name, actual.name, label))
+  o.write("=============================================================\n")
+  o.write("EXPECTED NODE TO BE:\n")
+  o.write("=============================================================\n")
+  expected.pprint(o)
+  o.write("=============================================================\n")
+  o.write("ACTUAL NODE FOUND:\n")
+  o.write("=============================================================\n")
+  actual.pprint(o)
+
+  logger.warn(o.getValue())
+  o.close()
 
 ### yanked from tree.py
 def default_singleton_handler(description, path, item):
   node = item_to_node(path, item)
   logger.warn("Couldn't find node '%s' in %s tree" % (node.name, description))
-  logger.warn(pprint.pformat(node))
+  o = StringIO()
+  node.pprint(o)
+  logger.warn(o.getValue())
+  o.close()
   raise svntest.tree.SVNTreeUnequal