2cb4cd4b3245892d9ed70699726b67662d59e825
Daniel Shahaf <danielsh@apache.org>
2012-04-14 13:39:39 +0000

    * tools/dist/release.py: Fix indentation.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1326102 13f79535-47bb-0310-9956-ffa450edef68

--- tools/dist/release.py
+++ tools/dist/release.py
@@ -481,7 +481,7 @@ def post_candidates(args):
     proc = subprocess.Popen(['svn', 'import', '-m',
                              'Add %s candidate release artifacts' 
                                % args.version.base,
-                            get_deploydir(args.base_dir), dist_dev_url])
+                             get_deploydir(args.base_dir), dist_dev_url])
     (stdout, stderr) = proc.communicate()
     proc.wait()
 
