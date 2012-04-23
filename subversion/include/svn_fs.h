19738a3644596cd1b6a54dac5d89a0ffa6c5de99
Greg Stein <gstein@apache.org>
2012-04-23 05:36:47 +0000

    Initial sketch for an Ev2 editor that can modify an FS txn.
    * subversion/include/svn_fs.h:
      (svn_fs_editor_create, svn_fs_editor_create_for): new declarations
    
    * subversion/libsvn_fs/editor.c:
      (): new file
      (struct edit_baton): baton for the editor
      (make_editor): utility function to wrap an Ev2 editor around a txn
      (svn_fs_editor_create): create a txn and wrap it in an Ev2 editor
      (svn_fs_editor_create_for): create an Ev2 editor for a given txn
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1329070 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_fs.h
+++ subversion/include/svn_fs.h
@@ -39,6 +39,7 @@
 #include "svn_io.h"
 #include "svn_mergeinfo.h"
 #include "svn_checksum.h"
+#include "svn_editor.h"
 
 
 #ifdef __cplusplus
@@ -1001,6 +1002,38 @@ svn_error_t *
 svn_fs_change_txn_props(svn_fs_txn_t *txn,
                         const apr_array_header_t *props,
                         apr_pool_t *pool);
+
+/** @} */
+
+
+/** Editors
+ *
+ * ### docco
+ *
+ * @defgroup svn_fs_editor Transaction editors
+ * @{
+ */
+
+svn_error_t *
+svn_fs_editor_create(svn_editor_t **editor,
+                     const char **txn_name,
+                     svn_fs_t *fs,
+                     svn_revnum_t revision,
+                     apr_uint32_t flags,
+                     svn_cancel_func_t cancel_func,
+                     void *cancel_baton,
+                     apr_pool_t *result_pool,
+                     apr_pool_t *scratch_pool);
+
+
+svn_error_t *
+svn_fs_editor_create_for(svn_editor_t **editor,
+                         svn_fs_t *fs,
+                         const char *txn_name,
+                         svn_cancel_func_t cancel_func,
+                         void *cancel_baton,
+                         apr_pool_t *result_pool,
+                         apr_pool_t *scratch_pool);
 
 /** @} */
 
