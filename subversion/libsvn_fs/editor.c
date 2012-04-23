efc1735a1d80a74f16ffc4e6583b78793bc7dd8f
Greg Stein <gstein@apache.org>
2012-04-23 06:51:34 +0000

    Fix stupid build break in r1329083.
    * subversion/libsvn_fs/editor.c:
      (make_editor): take a NO_AUTOCOMMIT parameter
      (svn_fs_editor_create): compute NO_AUTOCOMMIT based on flags, and
        pass it to make_editor().
      (svn_fs_editor_create_for): by definition, the txn is not being
        auto-committed, so keep it that way. pass TRUE to make_editor()
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1329085 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs/editor.c
+++ subversion/libsvn_fs/editor.c
@@ -41,6 +41,7 @@ struct edit_baton {
 static svn_error_t *
 make_editor(svn_editor_t **editor,
             svn_fs_txn_t *txn,
+            svn_boolean_t no_autocommit,
             svn_cancel_func_t cancel_func,
             void *cancel_baton,
             apr_pool_t *result_pool,
@@ -64,7 +65,7 @@ make_editor(svn_editor_t **editor,
   struct edit_baton *eb = apr_palloc(result_pool, sizeof(*eb));
 
   eb->txn = txn;
-  eb->no_autocommit = (flags & SVN_FS_TXN_NO_AUTOCOMMIT) != 0;
+  eb->no_autocommit = no_autocommit;
 
   SVN_ERR(svn_editor_create(editor, eb, cancel_func, cancel_baton,
                             result_pool, scratch_pool));
@@ -86,10 +87,11 @@ svn_fs_editor_create(svn_editor_t **editor,
                      apr_pool_t *scratch_pool)
 {
   svn_fs_txn_t *txn;
+  svn_boolean_t no_autocommit = (flags & SVN_FS_TXN_NO_AUTOCOMMIT) != 0;
 
   SVN_ERR(svn_fs_begin_txn2(&txn, fs, revision, flags, result_pool));
   SVN_ERR(svn_fs_txn_name(txn_name, txn, result_pool));
-  return svn_error_trace(make_editor(editor, txn,
+  return svn_error_trace(make_editor(editor, txn, no_autocommit,
                                      cancel_func, cancel_baton,
                                      result_pool, scratch_pool));
 }
@@ -107,7 +109,7 @@ svn_fs_editor_create_for(svn_editor_t **editor,
   svn_fs_txn_t *txn;
 
   SVN_ERR(svn_fs_open_txn(&txn, fs, txn_name, result_pool));
-  return svn_error_trace(make_editor(editor, txn,
+  return svn_error_trace(make_editor(editor, txn, TRUE /* no_autocommit */,
                                      cancel_func, cancel_baton,
                                      result_pool, scratch_pool));
 }
