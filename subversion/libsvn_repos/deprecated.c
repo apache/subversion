36f848411d7496f6066e47d3609ae58df3c8322c
Greg Stein <gstein@apache.org>
2012-04-23 06:53:40 +0000

    Deprecate svn_repos_fs_begin_txn_for_update(). It does not seem to be used, and we certainly don't need it.
    * subversion/include/svn_repos.h:
      (svn_repos_fs_begin_txn_for_update): mark as deprecated
    
    * subversion/libsvn_repos/fs-wrap.c:
      (svn_repos_fs_begin_txn_for_update): move to deprecated.c
    
    * subversion/libsvn_repos/deprecated.c:
      (svn_repos_fs_begin_txn_for_update): moved here from fs-wrap.c
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1329089 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_repos/deprecated.c
+++ subversion/libsvn_repos/deprecated.c
@@ -878,3 +878,32 @@ svn_repos_get_fs_build_parser(const svn_repos_parser_fns_t **parser_callbacks,
   *parser_callbacks = fns_from_fns2(fns2, pool);
   return SVN_NO_ERROR;
 }
+
+
+svn_error_t *
+svn_repos_fs_begin_txn_for_update(svn_fs_txn_t **txn_p,
+                                  svn_repos_t *repos,
+                                  svn_revnum_t rev,
+                                  const char *author,
+                                  apr_pool_t *pool)
+{
+  /* ### someday, we might run a read-hook here. */
+
+  /* Begin the transaction. */
+  SVN_ERR(svn_fs_begin_txn2(txn_p, repos->fs, rev, 0, pool));
+
+  /* We pass the author to the filesystem by adding it as a property
+     on the txn. */
+
+  /* User (author). */
+  if (author)
+    {
+      svn_string_t val;
+      val.data = author;
+      val.len = strlen(author);
+      SVN_ERR(svn_fs_change_txn_prop(*txn_p, SVN_PROP_REVISION_AUTHOR,
+                                     &val, pool));
+    }
+
+  return SVN_NO_ERROR;
+}
