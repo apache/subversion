49287bdeff7dfcdce7663993c07e84e9a84d9fd3
Greg Stein <gstein@apache.org>
2012-04-24 06:07:18 +0000

    Start sketching out the FS editor. In particular, the add_* callbacks.
    * subversion/libsvn_fs/editor.c:
      (struct edit_baton): add CANCEL_FUNC/BATON, which is needed when
        copying streams. remember the pool the TXN lives in, and keep a
        member for an open ROOT associated to the TXN.
      (FSPATH): helper function to transform a repos_relpath into an fspath
      (get_root): open or re-use the txn's root
      (add_new_props): for new nodes, it is easy to apply a set of
        properties (we don't need to worry about delets). this little
        helper applies the new properties.
      (add_directory_cb): flesh out with code to add a directory in the FS
      (add_file_cb): new code to add a file in the FS
      (add_symlink_cb): some incomplete sketchy code for adding a symlink
      (add_absent_cb): not allowed. return an error
      (complete_cb): close any open root. leave some todo comments
      (abort_cb): close any open root. abort the txn.
      (make_editor): remember some more stuff in the baton
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1329563 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs/editor.c
+++ subversion/libsvn_fs/editor.c
@@ -29,15 +29,77 @@
 #include "svn_editor.h"
 #include "svn_fs.h"
 
+#include "svn_private_config.h"
+
 #include "fs-loader.h"
 
 
 struct edit_baton {
+  /* The transaction associated with this editor.  */
   svn_fs_txn_t *txn;
+
+  /* Should the transaction be committed when complete_cb() is invoked?  */
   svn_boolean_t autocommit;
+
+  /* We sometimes need the cancellation beyond what svn_editor_t provides  */
+  svn_cancel_func_t cancel_func;
+  void *cancel_baton;
+
+  /* The pool that the txn lives within. When we create a ROOT, it will
+     be allocated within a subpool of this. The root will be closed in
+     complete/abort and that subpool will be destroyed.
+
+     This pool SHOULD NOT be used for any allocations.  */
+  apr_pool_t *txn_pool;
+
+  /* This is the root from the txn. Use get_root() to fetch/create this
+     member as appropriate.  */
+  svn_fs_root_t *root;
 };
 
+#define FSPATH(relpath, pool) apr_pstrcat(pool, "/", relpath, NULL)
 #define UNUSED(x) ((void)(x))
+
+
+static svn_error_t *
+get_root(svn_fs_root_t **root,
+         struct edit_baton *eb)
+{
+  if (eb->root == NULL)
+    SVN_ERR(svn_fs_txn_root(&eb->root, eb->txn, eb->txn_pool));
+  *root = eb->root;
+  return SVN_NO_ERROR;
+}
+
+
+/* Apply each property in PROPS to the node at FSPATH in ROOT.  */
+static svn_error_t *
+add_new_props(svn_fs_root_t *root,
+              const char *fspath,
+              apr_hash_t *props,
+              apr_pool_t *scratch_pool)
+{
+  apr_pool_t *iterpool = svn_pool_create(scratch_pool);
+  apr_hash_index_t *hi;
+
+  /* ### it would be nice to have svn_fs_set_node_props(). but since we
+     ### don't... add each property to the node. this is a new node, so
+     ### we don't need to worry about deleting props. just adding.  */
+
+  for (hi = apr_hash_first(scratch_pool, props); hi;
+       hi = apr_hash_next(hi))
+    {
+      const char *name = svn__apr_hash_index_key(hi);
+      const svn_string_t *value = svn__apr_hash_index_val(hi);
+
+      svn_pool_clear(iterpool);
+
+      SVN_ERR(svn_fs_change_node_prop(root, fspath, name, value, iterpool));
+    }
+
+  svn_pool_destroy(iterpool);
+  return SVN_NO_ERROR;
+}
 
 
 /* This implements svn_editor_cb_add_directory_t */
@@ -50,8 +112,20 @@ add_directory_cb(void *baton,
                  apr_pool_t *scratch_pool)
 {
   struct edit_baton *eb = baton;
+  const char *fspath = FSPATH(relpath, scratch_pool);
+  svn_fs_root_t *root;
 
-  UNUSED(eb); SVN__NOT_IMPLEMENTED();
+  /* Note: we ignore CHILDREN. We have no "incomplete" state to worry about,
+     so we don't need to be aware of what children will be created.  */
+
+  SVN_ERR(get_root(&root, eb));
+
+  /* ### validate REPLACES_REV  */
+
+  SVN_ERR(svn_fs_make_dir(root, fspath, scratch_pool));
+  SVN_ERR(add_new_props(root, fspath, props, scratch_pool));
+
+  return SVN_NO_ERROR;
 }
 
 
@@ -66,8 +140,30 @@ add_file_cb(void *baton,
             apr_pool_t *scratch_pool)
 {
   struct edit_baton *eb = baton;
+  const char *fspath = FSPATH(relpath, scratch_pool);
+  svn_fs_root_t *root;
+  svn_stream_t *fs_contents;
 
-  UNUSED(eb); SVN__NOT_IMPLEMENTED();
+  SVN_ERR(get_root(&root, eb));
+
+  /* ### do something with CHECKSUM  */
+  /* ### validate REPLACES_REV  */
+
+  SVN_ERR(svn_fs_make_file(root, fspath, scratch_pool));
+
+  /* ### We probably don't have an MD5 checksum, so no digest is available
+     ### for svn_fs_apply_text() to validate. It would be nice to have an
+     ### FS API that takes our CONTENTS/CHECKSUM pair (and PROPS!).  */
+  SVN_ERR(svn_fs_apply_text(&fs_contents, root, fspath,
+                            NULL /* result_checksum */,
+                            scratch_pool));
+  SVN_ERR(svn_stream_copy3(contents, fs_contents,
+                           eb->cancel_func, eb->cancel_baton,
+                           scratch_pool));
+
+  SVN_ERR(add_new_props(root, fspath, props, scratch_pool));
+
+  return SVN_NO_ERROR;
 }
 
 
@@ -81,8 +177,28 @@ add_symlink_cb(void *baton,
                apr_pool_t *scratch_pool)
 {
   struct edit_baton *eb = baton;
+  const char *fspath = FSPATH(relpath, scratch_pool);
+  svn_fs_root_t *root;
+  svn_stream_t *fs_contents;
 
-  UNUSED(eb); SVN__NOT_IMPLEMENTED();
+  SVN_ERR(get_root(&root, eb));
+
+  /* ### validate REPLACES_REV  */
+
+  /* ### we probably need to construct a file with specific contents
+     ### (until the FS grows some symlink APIs)  */
+#if 0
+  SVN_ERR(svn_fs_make_file(root, fspath, scratch_pool));
+  SVN_ERR(svn_fs_apply_text(&fs_contents, root, fspath,
+                            NULL /* result_checksum */,
+                            scratch_pool));
+  /* ### SVN_ERR(svn_stream_printf(fs_contents, ..., scratch_pool));  */
+
+  SVN_ERR(add_new_props(root, fspath, props, scratch_pool));
+#endif
+  UNUSED(fspath); UNUSED(fs_contents);
+
+  SVN__NOT_IMPLEMENTED();
 }
 
 
@@ -94,9 +210,11 @@ add_absent_cb(void *baton,
               svn_revnum_t replaces_rev,
               apr_pool_t *scratch_pool)
 {
-  struct edit_baton *eb = baton;
-
-  UNUSED(eb); SVN__NOT_IMPLEMENTED();
+  /* This is a programming error. Code should not attempt to create these
+     kinds of nodes within the FS.  */
+  return svn_error_create(
+           SVN_ERR_UNSUPPORTED_FEATURE, NULL,
+           N_("The filesystem does not support 'absent' nodes"));
 }
 
 
@@ -208,7 +326,16 @@ complete_cb(void *baton,
 {
   struct edit_baton *eb = baton;
 
-  UNUSED(eb); SVN__NOT_IMPLEMENTED();
+  if (eb->root != NULL)
+    {
+      svn_fs_close_root(eb->root);
+      eb->root = NULL;
+    }
+
+  /* ### check AUTOCOMMIT  */
+  /* ### how to return the results of the commit? eg. revision  */
+
+  return SVN_NO_ERROR;
 }
 
 
@@ -219,7 +346,16 @@ abort_cb(void *baton,
 {
   struct edit_baton *eb = baton;
 
-  UNUSED(eb); SVN__NOT_IMPLEMENTED();
+  if (eb->root != NULL)
+    {
+      svn_fs_close_root(eb->root);
+      eb->root = NULL;
+    }
+
+  /* ### should we examine the error and attempt svn_fs_purge_txn() ?  */
+  SVN_ERR(svn_fs_abort_txn(eb->txn, scratch_pool));
+
+  return SVN_NO_ERROR;
 }
 
 
@@ -247,10 +383,13 @@ make_editor(svn_editor_t **editor,
     complete_cb,
     abort_cb
   };
-  struct edit_baton *eb = apr_palloc(result_pool, sizeof(*eb));
+  struct edit_baton *eb = apr_pcalloc(result_pool, sizeof(*eb));
 
   eb->txn = txn;
   eb->autocommit = autocommit;
+  eb->cancel_func = cancel_func;
+  eb->cancel_baton = cancel_baton;
+  eb->txn_pool = result_pool;
 
   SVN_ERR(svn_editor_create(editor, eb, cancel_func, cancel_baton,
                             result_pool, scratch_pool));
