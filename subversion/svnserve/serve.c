apache/subversion

commit c9e931c74a82ee38d6c30b4543c43c799f8d697a

Backport r1306111, reimplementing the serve.c part.
git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/1.7.x-r1306111@1306303 13f79535-47bb-0310-9956-ffa450edef68

subversion/svnserve/serve.c
@@ -1623,10 +1623,16 @@ static svn_error_t *get_dir(svn_ra_svn_conn_t *conn, apr_pool_t *pool,
     {
       for (hi = apr_hash_first(pool, entries); hi; hi = apr_hash_next(hi))
         {
+          static const char *missing_date = svn_time_to_cstring(0, pool);
           const char *name = svn__apr_hash_index_key(hi);
           svn_dirent_t *entry = svn__apr_hash_index_val(hi);
 
-          cdate = (entry->time == (time_t) -1) ? NULL
+          /* The client does not properly handle a missing CDATE. For                              
+             interoperability purposes, we must fill in some junk.                                 
+                                                                                                   
+             See libsvn_ra_svn/client.c:ra_svn_get_dir()  */                                       
+          cdate = (entry->time == (time_t) -1)
+            ? missing_date
             : svn_time_to_cstring(entry->time, pool);
           SVN_ERR(svn_ra_svn_write_tuple(conn, pool, "cwnbr(?c)(?c)", name,
                                          svn_node_kind_to_word(entry->kind),

subversion/libsvn_ra_svn/client.c
@@ -1152,7 +1152,18 @@ static svn_error_t *ra_svn_get_dir(svn_ra_session_t *session,
       dirent->size = size;/* FIXME: svn_filesize_t */
       dirent->has_props = has_props;
       dirent->created_rev = crev;
-      SVN_ERR(svn_time_from_cstring(&dirent->time, cdate, pool));
+      /* NOTE: the tuple's format string says CDATE may be NULL. But this
+         function does not allow that. The server has always sent us some
+         random date, however, so this just happens to work. But let's
+         be wary of servers that are (improperly) fixed to send NULL.
+
+         Note: they should NOT be "fixed" to send NULL, as that would break
+         any older clients which received that NULL. But we may as well
+         be defensive against a malicous server.  */
+      if (cdate == NULL)
+        dirent->time = 0;
+      else
+        SVN_ERR(svn_time_from_cstring(&dirent->time, cdate, pool));
       dirent->last_author = cauthor;
       apr_hash_set(*dirents, name, APR_HASH_KEY_STRING, dirent);
     }

commit 1534ddad835fc830e29b5e388b1472dc14d75bc0

On the 1.7.x-r1306111, fix a whitespace issue introduced in r1306303.
* subversion/svnserve/serve.c
  (get_dir): Trim excessive trailing whitespace.


git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/1.7.x-r1306111@1330149 13f79535-47bb-0310-9956-ffa450edef68

subversion/svnserve/serve.c
@@ -1627,10 +1627,9 @@ static svn_error_t *get_dir(svn_ra_svn_conn_t *conn, apr_pool_t *pool,
           const char *name = svn__apr_hash_index_key(hi);
           svn_dirent_t *entry = svn__apr_hash_index_val(hi);
 
-          /* The client does not properly handle a missing CDATE. For                              
-             interoperability purposes, we must fill in some junk.                                 
-                                                                                                   
-             See libsvn_ra_svn/client.c:ra_svn_get_dir()  */                                       
+          /* The client does not properly handle a missing CDATE. For
+             interoperability purposes, we must fill in some junk.
+             See libsvn_ra_svn/client.c:ra_svn_get_dir()  */
           cdate = (entry->time == (time_t) -1)
             ? missing_date
             : svn_time_to_cstring(entry->time, pool);