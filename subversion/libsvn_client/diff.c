66074ace4d8a697248b5b63a0a3a67266e39c7a4
Stefan Sperling <stsp@apache.org>
2012-05-14 17:58:21 +0000

    * subversion/libsvn_client/diff.c   (diff_repos_repos_added_or_deleted_file): Avoid direct use of the temporary    file handle by deleting the temporary file upon pool cleanup instead of when    the file is closed. This allows us to flush buffers to disk by closing the    'content' stream rather than calling flush() on the file handle.
    Suggested by: rhuijben
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1338314 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/diff.c
+++ subversion/libsvn_client/diff.c
@@ -2341,21 +2341,16 @@ diff_repos_repos_added_or_deleted_file(const char *target,
                                        svn_ra_session_t *ra_session,
                                        apr_pool_t *scratch_pool)
 {
-  apr_file_t *file;
   const char *file_abspath;
   svn_stream_t *content;
   apr_hash_t *prop_hash;
 
-  /* ### There is no way to flush content to disk without access to the bare
-   * ### file handle so we re-implement svn_stream_open_unique() here.
-   * ### Do we need something like svn_stream_flush()? */
-  SVN_ERR(svn_io_open_unique_file3(&file, &file_abspath, NULL,
-                                   svn_io_file_del_on_close,
-                                   scratch_pool, scratch_pool));
-  content = svn_stream_from_aprfile2(file, FALSE, scratch_pool);
+  SVN_ERR(svn_stream_open_unique(&content, &file_abspath, NULL,
+                                 svn_io_file_del_on_pool_cleanup,
+                                 scratch_pool, scratch_pool));
   SVN_ERR(svn_ra_get_file(ra_session, target, peg_revision, content, NULL,
                           &prop_hash, scratch_pool));
-  SVN_ERR(svn_io_file_flush_to_disk(file, scratch_pool));
+  SVN_ERR(svn_stream_close(content));
 
   if (show_deletion)
     {
@@ -2381,8 +2376,6 @@ diff_repos_repos_added_or_deleted_file(const char *target,
                                     NULL, callback_baton, scratch_pool));
     }
     
-  SVN_ERR(svn_stream_close(content));
-
   return SVN_NO_ERROR;
 }
 
