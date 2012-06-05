5f4b7ff7b9e723745d7500d84d9550006c14c8cb
Bert Huijben <rhuijben@apache.org>
2012-06-05 11:25:13 +0000

    Remove now unused svn_wc__wq_build_record_fileinfo() from the internal api, but keep the operation for compatibility with items currently in wc.db.
    * subversion/libsvn_wc/workqueue.c
      (svn_wc__wq_build_record_fileinfo): Remove function.
    
    * subversion/libsvn_wc/workqueue.h
      (svn_wc__wq_build_record_fileinfo): Remove function.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1346350 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/workqueue.c
+++ subversion/libsvn_wc/workqueue.c
@@ -1254,32 +1254,6 @@ run_record_fileinfo(svn_wc__db_t *db,
                                                  scratch_pool));
 }
 
-
-svn_error_t *
-svn_wc__wq_build_record_fileinfo(svn_skel_t **work_item,
-                                 svn_wc__db_t *db,
-                                 const char *local_abspath,
-                                 apr_time_t set_time,
-                                 apr_pool_t *result_pool,
-                                 apr_pool_t *scratch_pool)
-{
-  const char *local_relpath;
-  *work_item = svn_skel__make_empty_list(result_pool);
-
-  SVN_ERR_ASSERT(svn_dirent_is_absolute(local_abspath));
-
-  SVN_ERR(svn_wc__db_to_relpath(&local_relpath, db, local_abspath,
-                                local_abspath, result_pool, scratch_pool));
-
-  if (set_time)
-   svn_skel__prepend_int(set_time, *work_item, result_pool);
-
-  svn_skel__prepend_str(local_relpath, *work_item, result_pool);
-  svn_skel__prepend_str(OP_RECORD_FILEINFO, *work_item, result_pool);
-
-  return SVN_NO_ERROR;
-}
-
 /* ------------------------------------------------------------------------ */
 
 /* OP_TMP_SET_TEXT_CONFLICT_MARKERS  */
