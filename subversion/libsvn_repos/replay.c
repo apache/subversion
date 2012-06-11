apache/subversion

commit 8e0309bcc13630859f8289f6ef01667eeec3886f

Remove the a now-trivial function, as well as a superfluous else clause, in replay revs.
* subversion/libsvn_repos/replay.c
  (is_within_base_path): Remove.
  (path_driver_cb_func): Update caller.
  (svn_repos_replay2): Update caller, drop else clause.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1349002 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_repos/replay.c
@@ -338,12 +338,6 @@ add_subdir(svn_fs_root_t *source_root,
   return SVN_NO_ERROR;
 }
 
-static svn_boolean_t
-is_within_base_path(const char *path, const char *base_path)
-{
-  return svn_relpath_skip_ancestor(base_path, path) != NULL;
-}
-
 /* Given PATH deleted under ROOT, return in READABLE whether the path was
    readable prior to the deletion.  Consult COPIES (a stack of 'struct
    copy_info') and AUTHZ_READ_FUNC. */
@@ -567,7 +561,7 @@ path_driver_cb_func(void **dir_baton,
          all. */
       if (copyfrom_path
           && ((! src_readable)
-              || (! is_within_base_path(copyfrom_path + 1, base_path))
+              || (svn_relpath_skip_ancestor(base_path, copyfrom_path + 1) == NULL)
               || (cb->low_water_mark > copyfrom_rev)))
         {
           copyfrom_path = NULL;
@@ -851,14 +845,7 @@ svn_repos_replay2(svn_fs_root_t *root,
 
           /* If the base_path doesn't match the top directory of this path
              we don't want anything to do with it... */
-          if (is_within_base_path(path, base_path))
-            {
-              APR_ARRAY_PUSH(paths, const char *) = path;
-              apr_hash_set(changed_paths, path, keylen, change);
-            }
-          /* ...unless this was a change to one of the parent directories of
-             base_path. */
-          else if (is_within_base_path(base_path, path))
+          if (svn_relpath_skip_ancestor(base_path, path) != NULL)
             {
               APR_ARRAY_PUSH(paths, const char *) = path;
               apr_hash_set(changed_paths, path, keylen, change);

commit 07ab67ce5b1e0a2633f6fd56cc23cd6447e4b64d

Fix bug introduced in r1349002.
* subversion/libsvn_repos/replay.c
  (svn_repos_replay2): Re-add an else clause which wasn't superfluous in
    r1349002.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1349017 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_repos/replay.c
@@ -850,6 +850,13 @@ svn_repos_replay2(svn_fs_root_t *root,
               APR_ARRAY_PUSH(paths, const char *) = path;
               apr_hash_set(changed_paths, path, keylen, change);
             }
+          /* ...unless this was a change to one of the parent directories of
+             base_path. */
+          else if (svn_relpath_skip_ancestor(path, base_path) != NULL)
+            {
+              APR_ARRAY_PUSH(paths, const char *) = path;
+              apr_hash_set(changed_paths, path, keylen, change);
+            }
         }
     }
 