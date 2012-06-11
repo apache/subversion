cedcc2b39f3f4dc022d31bbff8a2d4267fa6a52e
Stefan Fuhrmann <stefan2@apache.org>
2012-06-11 13:35:31 +0000

    Update callers to svn_stringbuf_ensure() to no longer add one byte to make room for the terminating NUL as this is already being taken care of by the called function.
    * subversion/libsvn_ra_neon/merge.c
      (svn_ra_neon__assemble_locktoken_body): remove extra +1
    * subversion/libsvn_ra_svn/marshal.c
      (read_string): ditto
    * subversion/libsvn_subr/base64.c
      (decode_bytes): ditto
    * subversion/libsvn_subr/path.c
      (illegal_path_escape): ditto
    * subversion/libsvn_subr/skel.c
      (unparse): ditto
    * subversion/libsvn_subr/stream.c
      (stream_readline_chunky): ditto
    * subversion/libsvn_subr/string.c
      (svn_stringbuf_set, svn_stringbuf_appendbytes): ditto
    * subversion/libsvn_subr/temp_serializer.c
      (align_buffer_end): ditto
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1348841 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/stream.c
+++ subversion/libsvn_subr/stream.c
@@ -380,7 +380,7 @@ stream_readline_chunky(svn_stringbuf_t **stringbuf,
       {
         /* Append the next chunk to the string read so far.
          */
-        svn_stringbuf_ensure(str, str->len + LINE_CHUNK_SIZE + 1);
+        svn_stringbuf_ensure(str, str->len + LINE_CHUNK_SIZE);
         numbytes = LINE_CHUNK_SIZE;
         SVN_ERR(svn_stream_read(stream, str->data + str->len, &numbytes));
         str->len += numbytes;
