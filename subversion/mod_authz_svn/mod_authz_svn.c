7be4c62d77cd2a9aa033bd376cc9c7925da801d6
Stefan Fuhrmann <stefan2@apache.org>
2012-06-13 21:22:24 +0000

    Silence more GCC conversion warnings (-Wconversion).
    * subversion/libsvn_subr/adler32.c
      (svn__adler32): adler32 returns always 32 bits
    * subversion/libsvn_subr/cache.c
      (svn_cache__format_info): do all calculations in double
    * subversion/libsvn_subr/cache-membuffer.c
      (is_group_initialized, initialize_group): explicitly cast to bytes
      (svn_cache__membuffer_cache_create): test for overflow before casting
    * subversion/libsvn_subr/string.c
      (svn__ui64toa): result of % will fit into 32 bits
    * subversion/mod_authz_svn/mod_authz_svn.c
      (convert_case): explicitly cast back to char
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1350021 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/mod_authz_svn/mod_authz_svn.c
+++ subversion/mod_authz_svn/mod_authz_svn.c
@@ -235,7 +235,7 @@ convert_case(char *text, svn_boolean_t to_uppercase)
   char *c = text;
   while (*c)
     {
-      *c = (to_uppercase ? apr_toupper(*c) : apr_tolower(*c));
+      *c = (char)(to_uppercase ? apr_toupper(*c) : apr_tolower(*c));
       ++c;
     }
 }
