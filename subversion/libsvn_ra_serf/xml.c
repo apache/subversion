95be45f804245fbb7920d48769500d08f37da98d
Greg Stein <gstein@apache.org>
2012-06-14 09:28:53 +0000

    Implement the attribute gathering function in the XML v2 parsing code.
    * subversion/libsvn_ra_serf/xml.c:
      (svn_ra_serf__xml_gather_since): implement
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1350158 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_ra_serf/xml.c
+++ subversion/libsvn_ra_serf/xml.c
@@ -483,12 +483,35 @@ svn_ra_serf__xml_gather_since(svn_ra_serf__xml_estate_t *xes,
                               int stop_state)
 {
   apr_hash_t *data;
+  apr_pool_t *pool;
 
   ensure_pool(xes);
+  pool = xes->state_pool;
 
-  data = apr_hash_make(xes->state_pool);
+  data = apr_hash_make(pool);
 
-  /* ### gather data  */
+  for (; xes != NULL; xes = xes->prev)
+    {
+      if (xes->attrs != NULL)
+        {
+          apr_hash_index_t *hi;
+
+          for (hi = apr_hash_first(pool, xes->attrs); hi;
+               hi = apr_hash_next(hi))
+            {
+              const void *key;
+              apr_ssize_t klen;
+              void *val;
+
+              /* Parent name/value lifetimes are at least as long as POOL.  */
+              apr_hash_this(hi, &key, &klen, &val);
+              apr_hash_set(data, key, klen, val);
+            }
+        }
+
+      if (xes->state == stop_state)
+        break;
+    }
 
   return data;
 }
