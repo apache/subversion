7a3785af128e4aeff35d47ad1034d54d56d5aa67
Vladimir Berezniker <vmpn@apache.org>
2012-06-21 04:06:11 +0000

    On the javahl-ra branch:
    JavaHL: Factor out common context to be shared between SVNClient and SVNRa
    classes
    
    [ in subversion/bindings/javahl/native ]
    
    * RaSharedContext.cpp: Update whitespace to be compliant with project standards
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/javahl-ra@1352403 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/javahl/native/RaSharedContext.cpp
+++ subversion/bindings/javahl/native/RaSharedContext.cpp
@@ -46,40 +46,40 @@ void
 RaSharedContext::attachJavaObject(jobject contextHolder, const char *contextClassType,
     const char *contextFieldName, jfieldID * ctxFieldID)
 {
-    JNIEnv *env = JNIUtil::getEnv();
+  JNIEnv *env = JNIUtil::getEnv();
 
-    /* Grab a global reference to the Java object embedded in the parent Java
-       object. */
-    if ((*ctxFieldID) == 0)
+  /* Grab a global reference to the Java object embedded in the parent Java
+   object. */
+  if ((*ctxFieldID) == 0)
     {
-        jclass clazz = env->GetObjectClass(contextHolder);
-        if (JNIUtil::isJavaExceptionThrown())
+      jclass clazz = env->GetObjectClass(contextHolder);
+      if (JNIUtil::isJavaExceptionThrown())
         return;
 
-        (*ctxFieldID) = env->GetFieldID(clazz, contextFieldName, contextClassType);
-        if (JNIUtil::isJavaExceptionThrown() || (*ctxFieldID) == 0)
-            return;
+      (*ctxFieldID) = env->GetFieldID(clazz, contextFieldName, contextClassType);
+      if (JNIUtil::isJavaExceptionThrown() || (*ctxFieldID) == 0)
+        return;
 
-        env->DeleteLocalRef(clazz);
+      env->DeleteLocalRef(clazz);
     }
 
-    jobject jctx = env->GetObjectField(contextHolder, (*ctxFieldID));
-    if (JNIUtil::isJavaExceptionThrown())
-        return;
+  jobject jctx = env->GetObjectField(contextHolder, (*ctxFieldID));
+  if (JNIUtil::isJavaExceptionThrown())
+    return;
 
-    m_jctx = env->NewGlobalRef(jctx);
-    if (JNIUtil::isJavaExceptionThrown())
-        return;
+  m_jctx = env->NewGlobalRef(jctx);
+  if (JNIUtil::isJavaExceptionThrown())
+    return;
 
-    env->DeleteLocalRef(jctx);
+  env->DeleteLocalRef(jctx);
 }
 
 RaSharedContext::~RaSharedContext()
 {
-    delete m_prompter;
+  delete m_prompter;
 
-    JNIEnv *env = JNIUtil::getEnv();
-    env->DeleteGlobalRef(m_jctx);
+  JNIEnv *env = JNIUtil::getEnv();
+  env->DeleteGlobalRef(m_jctx);
 }
 
 apr_hash_t *
@@ -90,14 +90,14 @@ RaSharedContext::getConfigData()
       JNIUtil::throwNullPointerException("pool is null");
     }
 
-    if (m_config == NULL)
-      {
-        const char *configDir = m_configDir.c_str();
-        if (m_configDir.empty())
-            configDir = NULL;
-        SVN_JNI_ERR(
+  if (m_config == NULL)
+    {
+      const char *configDir = m_configDir.c_str();
+      if (m_configDir.empty())
+        configDir = NULL;
+      SVN_JNI_ERR(
           svn_config_get_config(&m_config, configDir, m_pool->getPool()), NULL);
-      }
+    }
 
   return m_config;
 }
@@ -115,150 +115,142 @@ RaSharedContext::getAuthBaton(SVN::Pool &in_pool)
       return NULL;
     }
 
-    svn_config_t *config = (svn_config_t *) apr_hash_get(configData,
-                                                         SVN_CONFIG_CATEGORY_CONFIG,
-                                                         APR_HASH_KEY_STRING);
+  svn_config_t *config = (svn_config_t *) apr_hash_get(configData,
+      SVN_CONFIG_CATEGORY_CONFIG, APR_HASH_KEY_STRING);
 
+  /* The whole list of registered providers */
+  apr_array_header_t *providers;
 
-    /* The whole list of registered providers */
-    apr_array_header_t *providers;
+  /* Populate the registered providers with the platform-specific providers */
+  SVN_JNI_ERR(
+      svn_auth_get_platform_specific_client_providers(&providers, config, pool),
+      NULL);
 
-    /* Populate the registered providers with the platform-specific providers */
-    SVN_JNI_ERR(svn_auth_get_platform_specific_client_providers(&providers,
-                                                                config,
-                                                                pool),
-                NULL);
+  /* Use the prompter (if available) to prompt for password and cert
+   * caching. */
+  svn_auth_plaintext_prompt_func_t plaintext_prompt_func = NULL;
+  void *plaintext_prompt_baton = NULL;
+  svn_auth_plaintext_passphrase_prompt_func_t plaintext_passphrase_prompt_func;
+  void *plaintext_passphrase_prompt_baton = NULL;
 
-    /* Use the prompter (if available) to prompt for password and cert
-     * caching. */
-    svn_auth_plaintext_prompt_func_t plaintext_prompt_func = NULL;
-    void *plaintext_prompt_baton = NULL;
-    svn_auth_plaintext_passphrase_prompt_func_t plaintext_passphrase_prompt_func;
-    void *plaintext_passphrase_prompt_baton = NULL;
-
-    if (m_prompter != NULL)
+  if (m_prompter != NULL)
     {
-        plaintext_prompt_func = Prompter::plaintext_prompt;
-        plaintext_prompt_baton = m_prompter;
-        plaintext_passphrase_prompt_func = Prompter::plaintext_passphrase_prompt;
-        plaintext_passphrase_prompt_baton = m_prompter;
+      plaintext_prompt_func = Prompter::plaintext_prompt;
+      plaintext_prompt_baton = m_prompter;
+      plaintext_passphrase_prompt_func = Prompter::plaintext_passphrase_prompt;
+      plaintext_passphrase_prompt_baton = m_prompter;
     }
 
-    /* The main disk-caching auth providers, for both
-     * 'username/password' creds and 'username' creds.  */
-    svn_auth_provider_object_t *provider;
+  /* The main disk-caching auth providers, for both
+   * 'username/password' creds and 'username' creds.  */
+  svn_auth_provider_object_t *provider;
 
-    svn_auth_get_simple_provider2(&provider, plaintext_prompt_func,
-                                  plaintext_prompt_baton, pool);
+  svn_auth_get_simple_provider2(&provider, plaintext_prompt_func,
+      plaintext_prompt_baton, pool);
+  APR_ARRAY_PUSH(providers, svn_auth_provider_object_t *) = provider;
+
+  svn_auth_get_username_provider(&provider, pool);
+  APR_ARRAY_PUSH(providers, svn_auth_provider_object_t *) = provider;
+
+  /* The server-cert, client-cert, and client-cert-password providers. */
+  SVN_JNI_ERR(
+      svn_auth_get_platform_specific_provider(&provider, "windows", "ssl_server_trust", pool),
+      NULL);
+
+  if (provider)
     APR_ARRAY_PUSH(providers, svn_auth_provider_object_t *) = provider;
 
-    svn_auth_get_username_provider(&provider, pool);
-    APR_ARRAY_PUSH(providers, svn_auth_provider_object_t *) = provider;
+  svn_auth_get_ssl_server_trust_file_provider(&provider, pool);
+  APR_ARRAY_PUSH(providers, svn_auth_provider_object_t *) = provider;
+  svn_auth_get_ssl_client_cert_file_provider(&provider, pool);
+  APR_ARRAY_PUSH(providers, svn_auth_provider_object_t *) = provider;
+  svn_auth_get_ssl_client_cert_pw_file_provider2(&provider,
+      plaintext_passphrase_prompt_func, plaintext_passphrase_prompt_baton,
+      pool);
+  APR_ARRAY_PUSH(providers, svn_auth_provider_object_t *) = provider;
 
-    /* The server-cert, client-cert, and client-cert-password providers. */
-    SVN_JNI_ERR(svn_auth_get_platform_specific_provider(&provider,
-                                                        "windows",
-                                                        "ssl_server_trust",
-                                                        pool),
-                NULL);
-
-    if (provider)
-        APR_ARRAY_PUSH(providers, svn_auth_provider_object_t *) = provider;
-
-    svn_auth_get_ssl_server_trust_file_provider(&provider, pool);
-    APR_ARRAY_PUSH(providers, svn_auth_provider_object_t *) = provider;
-    svn_auth_get_ssl_client_cert_file_provider(&provider, pool);
-    APR_ARRAY_PUSH(providers, svn_auth_provider_object_t *) = provider;
-    svn_auth_get_ssl_client_cert_pw_file_provider2(&provider,
-                        plaintext_passphrase_prompt_func,
-                        plaintext_passphrase_prompt_baton, pool);
-    APR_ARRAY_PUSH(providers, svn_auth_provider_object_t *) = provider;
-
-    if (m_prompter != NULL)
+  if (m_prompter != NULL)
     {
-        /* Two basic prompt providers: username/password, and just username.*/
-        provider = m_prompter->getProviderSimple(in_pool);
+      /* Two basic prompt providers: username/password, and just username.*/
+      provider = m_prompter->getProviderSimple(in_pool);
 
-        APR_ARRAY_PUSH(providers, svn_auth_provider_object_t *) = provider;
+      APR_ARRAY_PUSH(providers, svn_auth_provider_object_t *) = provider;
 
-        provider = m_prompter->getProviderUsername(in_pool);
-        APR_ARRAY_PUSH(providers, svn_auth_provider_object_t *) = provider;
+      provider = m_prompter->getProviderUsername(in_pool);
+      APR_ARRAY_PUSH(providers, svn_auth_provider_object_t *) = provider;
 
-        /* Three ssl prompt providers, for server-certs, client-certs,
-         * and client-cert-passphrases.  */
-        provider = m_prompter->getProviderServerSSLTrust(in_pool);
-        APR_ARRAY_PUSH(providers, svn_auth_provider_object_t *) = provider;
+      /* Three ssl prompt providers, for server-certs, client-certs,
+       * and client-cert-passphrases.  */
+      provider = m_prompter->getProviderServerSSLTrust(in_pool);
+      APR_ARRAY_PUSH(providers, svn_auth_provider_object_t *) = provider;
 
-        provider = m_prompter->getProviderClientSSL(in_pool);
-        APR_ARRAY_PUSH(providers, svn_auth_provider_object_t *) = provider;
+      provider = m_prompter->getProviderClientSSL(in_pool);
+      APR_ARRAY_PUSH(providers, svn_auth_provider_object_t *) = provider;
 
-        provider = m_prompter->getProviderClientSSLPassword(in_pool);
-        APR_ARRAY_PUSH(providers, svn_auth_provider_object_t *) = provider;
+      provider = m_prompter->getProviderClientSSLPassword(in_pool);
+      APR_ARRAY_PUSH(providers, svn_auth_provider_object_t *) = provider;
     }
 
-    /* Build an authentication baton to give to libsvn_client. */
-    svn_auth_open(&ab, providers, pool);
+  /* Build an authentication baton to give to libsvn_client. */
+  svn_auth_open(&ab, providers, pool);
 
-    /* Place any default --username or --password credentials into the
-     * auth_baton's run-time parameter hash.  ### Same with --no-auth-cache? */
-    if (!m_userName.empty())
-        svn_auth_set_parameter(ab, SVN_AUTH_PARAM_DEFAULT_USERNAME,
-                               apr_pstrdup(in_pool.getPool(),
-                                           m_userName.c_str()));
-    if (!m_passWord.empty())
-        svn_auth_set_parameter(ab, SVN_AUTH_PARAM_DEFAULT_PASSWORD,
-                               apr_pstrdup(in_pool.getPool(),
-                                           m_passWord.c_str()));
-    /* Store where to retrieve authentication data? */
-    if (!m_configDir.empty())
-        svn_auth_set_parameter(ab, SVN_AUTH_PARAM_CONFIG_DIR,
-                               apr_pstrdup(in_pool.getPool(),
-                                           m_configDir.c_str()));
-    return ab;
+  /* Place any default --username or --password credentials into the
+   * auth_baton's run-time parameter hash.  ### Same with --no-auth-cache? */
+  if (!m_userName.empty())
+    svn_auth_set_parameter(ab, SVN_AUTH_PARAM_DEFAULT_USERNAME,
+        apr_pstrdup(in_pool.getPool(), m_userName.c_str()));
+  if (!m_passWord.empty())
+    svn_auth_set_parameter(ab, SVN_AUTH_PARAM_DEFAULT_PASSWORD,
+        apr_pstrdup(in_pool.getPool(), m_passWord.c_str()));
+  /* Store where to retrieve authentication data? */
+  if (!m_configDir.empty())
+    svn_auth_set_parameter(ab, SVN_AUTH_PARAM_CONFIG_DIR,
+        apr_pstrdup(in_pool.getPool(), m_configDir.c_str()));
+  return ab;
 }
 
 void
 RaSharedContext::username(const char *pi_username)
 {
-    m_userName = (pi_username == NULL ? "" : pi_username);
+  m_userName = (pi_username == NULL ? "" : pi_username);
 }
 
 void
 RaSharedContext::password(const char *pi_password)
 {
-    m_passWord = (pi_password == NULL ? "" : pi_password);
+  m_passWord = (pi_password == NULL ? "" : pi_password);
 }
 
 void
 RaSharedContext::setPrompt(Prompter *prompter)
 {
-    delete m_prompter;
-    m_prompter = prompter;
+  delete m_prompter;
+  m_prompter = prompter;
 }
 
 void
 RaSharedContext::setConfigDirectory(const char *configDir)
 {
-    // A change to the config directory may necessitate creation of
-    // the config templates.
-    SVN::Pool requestPool;
-    SVN_JNI_ERR(svn_config_ensure(configDir, requestPool.getPool()), );
+  // A change to the config directory may necessitate creation of
+  // the config templates.
+  SVN::Pool requestPool;
+  SVN_JNI_ERR(svn_config_ensure(configDir, requestPool.getPool()), );
 
-    m_configDir = (configDir == NULL ? "" : configDir);
+  m_configDir = (configDir == NULL ? "" : configDir);
 
-    m_config = NULL;
+  m_config = NULL;
 }
 
 const char *
 RaSharedContext::getConfigDirectory() const
 {
-    return m_configDir.c_str();
+  return m_configDir.c_str();
 }
 
 void
 RaSharedContext::cancelOperation()
 {
-    m_cancelOperation = true;
+  m_cancelOperation = true;
 }
 
 void
@@ -276,16 +268,16 @@ RaSharedContext::isCancelledOperation()
 svn_error_t *
 RaSharedContext::checkCancel(void *cancelBaton)
 {
-    RaSharedContext *that = (RaSharedContext *) cancelBaton;
-    if (that->isCancelledOperation())
-        return svn_error_create(SVN_ERR_CANCELLED, NULL, _("Operation cancelled"));
-    else
-        return SVN_NO_ERROR;
+  RaSharedContext *that = (RaSharedContext *) cancelBaton;
+  if (that->isCancelledOperation())
+    return svn_error_create(SVN_ERR_CANCELLED, NULL, _("Operation cancelled"));
+  else
+    return SVN_NO_ERROR;
 }
 
 void
-RaSharedContext::progress(apr_off_t progressVal, apr_off_t total,
-                        void *baton, apr_pool_t *pool)
+RaSharedContext::progress(apr_off_t progressVal, apr_off_t total, void *baton,
+    apr_pool_t *pool)
 {
   jobject jctx = (jobject) baton;
   JNIEnv *env = JNIUtil::getEnv();
@@ -303,7 +295,7 @@ RaSharedContext::progress(apr_off_t progressVal, apr_off_t total,
         POP_AND_RETURN_NOTHING();
 
       mid = env->GetMethodID(clazz, "onProgress",
-                             "(L"JAVA_PACKAGE"/ProgressEvent;)V");
+          "(L"JAVA_PACKAGE"/ProgressEvent;)V");
       if (JNIUtil::isJavaExceptionThrown() || mid == 0)
         POP_AND_RETURN_NOTHING();
     }
@@ -321,8 +313,8 @@ RaSharedContext::progress(apr_off_t progressVal, apr_off_t total,
     }
 
   // Call the Java method.
-  jobject jevent = env->NewObject(clazz, midCT,
-                                  (jlong) progressVal, (jlong) total);
+  jobject jevent = env->NewObject(clazz, midCT, (jlong) progressVal,
+      (jlong) total);
   if (JNIUtil::isJavaExceptionThrown())
     POP_AND_RETURN_NOTHING();
 
