b4623ab6dbc0f168443f256a460ade0356d8923c
Vladimir Berezniker <vmpn@apache.org>
2012-06-21 23:48:44 +0000

    On the javahl-ra branch:
    JavaHL: Update whitespace to be compliant with project standards
    
    [ in subversion/bindings/javahl/native ]
    
    * SVNBase.cpp, 
      SVNBase.h
    
    [ in subversion/bindings/javahl/src/org/tigris/subversion/javahl/ ]
    
    * JNIObject.java
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/javahl-ra@1352727 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/javahl/native/SVNBase.h
+++ subversion/bindings/javahl/native/SVNBase.h
@@ -32,72 +32,72 @@
 
 class SVNBase
 {
- public:
-  SVNBase();
-  virtual ~SVNBase();
+  public:
+    SVNBase();
+    virtual ~SVNBase();
 
-  /**
-   * Return @c this as a @c jlong.
-   *
-   * @since 1.4.0
-   */
-  jlong getCppAddr() const;
+    /**
+     * Return @c this as a @c jlong.
+     *
+     * @since 1.4.0
+     */
+    jlong getCppAddr() const;
 
-  /**
-   * Deletes this C++ peer object, and clears the memory address of
-   * the corresponding Java object @a jthis which points to it.
-   *
-   * @since 1.4.0
-   */
-  virtual void dispose(jobject jthis) = 0;
+    /**
+     * Deletes this C++ peer object, and clears the memory address of
+     * the corresponding Java object @a jthis which points to it.
+     *
+     * @since 1.4.0
+     */
+    virtual void dispose(jobject jthis) = 0;
 
-  /**
-   * This method should never be called, as @c dispose() should be
-   * called explicitly.  When @c dispose() fails to be called, this
-   * method assures that this C++ peer object has been enqueued for
-   * deletion.
-   *
-   * @since 1.4.0
-   */
-  void finalize();
+    /**
+     * This method should never be called, as @c dispose() should be
+     * called explicitly.  When @c dispose() fails to be called, this
+     * method assures that this C++ peer object has been enqueued for
+     * deletion.
+     *
+     * @since 1.4.0
+     */
+    void finalize();
 
- protected:
-  /**
-   * Return the value of the @c cppAddr instance field from the @a
-   * jthis Java object, or @c 0 if an error occurs, or the address
-   * otherwise can't be determined.  @a fid is expected to point to
-   * 0 if not already known, in which case it's looked up using @a
-   * className.
-   *
-   * @since 1.4.0
-   */
-  static jlong findCppAddrForJObject(jobject jthis, jfieldID *fid,
-                                     const char *className);
+  protected:
+    /**
+     * Return the value of the @c cppAddr instance field from the @a
+     * jthis Java object, or @c 0 if an error occurs, or the address
+     * otherwise can't be determined.  @a fid is expected to point to
+     * 0 if not already known, in which case it's looked up using @a
+     * className.
+     *
+     * @since 1.4.0
+     */
+    static jlong findCppAddrForJObject(jobject jthis, jfieldID *fid,
+        const char *className);
 
-  /**
-   * Deletes @c this, then attempts to null out the @c jthis.cppAddr
-   * instance field on the corresponding Java object.
-   *
-   * @since 1.4.0
-   */
-  void dispose(jobject jthis, jfieldID *fid, const char *className);
+    /**
+     * Deletes @c this, then attempts to null out the @c jthis.cppAddr
+     * instance field on the corresponding Java object.
+     *
+     * @since 1.4.0
+     */
+    void dispose(jobject jthis, jfieldID *fid, const char *className);
 
-  /**
-   * Instantiates java object attached to this base object
-   */
-  jobject createCppBoundObject(const char *clazzName);
+    /**
+     * Instantiates java object attached to this base object
+     */
+    jobject createCppBoundObject(const char *clazzName);
 
- private:
-  /**
-   * If the value pointed to by @a fid is zero, find the @c jfieldID
-   * for the @c cppAddr instance field of @c className.
-   *
-   * @since 1.4.0
-   */
-  static void findCppAddrFieldID(jfieldID *fid, const char *className,
-                                 JNIEnv *env);
+  private:
+    /**
+     * If the value pointed to by @a fid is zero, find the @c jfieldID
+     * for the @c cppAddr instance field of @c className.
+     *
+     * @since 1.4.0
+     */
+    static void findCppAddrFieldID(jfieldID *fid, const char *className,
+        JNIEnv *env);
 
-protected:
+  protected:
     SVN::Pool pool;
 };
 
--- subversion/bindings/javahl/src/org/apache/subversion/javahl/JNIObject.java
+++ subversion/bindings/javahl/src/org/apache/subversion/javahl/JNIObject.java
@@ -24,18 +24,17 @@
 package org.apache.subversion.javahl;
 
 /**
- *  This class is used internally by the JavaHL implementation and not
- *  considered part part of the public API. 
+ * This class is used internally by the JavaHL implementation and not considered
+ * part part of the public API.
  */
 public abstract class JNIObject
 {
     /**
-     * slot for the address of the native peer. 
-     * The JNI code controls this field. If it is set to 0 then 
-     * underlying JNI object has been freed
+     * slot for the address of the native peer. The JNI code controls this
+     * field. If it is set to 0 then underlying JNI object has been freed
      */
     protected long cppAddr;
-    
+
     protected JNIObject(long cppAddr)
     {
         this.cppAddr = cppAddr;
