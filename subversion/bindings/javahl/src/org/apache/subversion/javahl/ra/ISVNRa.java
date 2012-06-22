f01f8c7570954fffb13b2aa8cb80951a2de223f4
Vladimir Berezniker <vmpn@apache.org>
2012-06-22 01:41:24 +0000

    On the javahl-ra branch:
    JavaHL: Update whitespace to be compliant with project standards
    
    [ in subversion/bindings/javahl/src/org/tigris/subversion/javahl/ra ]
    
    * ISVNRa.java,
      SVNRa.java
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/javahl-ra@1352742 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/javahl/src/org/apache/subversion/javahl/ra/ISVNRa.java
+++ subversion/bindings/javahl/src/org/apache/subversion/javahl/ra/ISVNRa.java
@@ -47,17 +47,17 @@ public interface ISVNRa
      * @return latest revision
      */
     public long getLatestRevision();
-    
+
     /**
-     * @param date      The date
-     * @return          The latest revision at date
+     * @param date
+     *            The date
+     * @return The latest revision at date
      */
-    public long getDatedRevision(Date date)
-        throws SubversionException;
+    public long getDatedRevision(Date date) throws SubversionException;
 
     public Map<String, Lock> getLocks(String path, Depth depth)
-        throws SubversionException;
+            throws SubversionException;
 
     public NodeKind checkPath(String path, Revision revision)
-        throws SubversionException;
+            throws SubversionException;
 }
