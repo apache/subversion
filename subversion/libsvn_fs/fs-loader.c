693c0ae5dcba6fc5bbbf218d316e1ea66e643053
Philip Martin <philip@apache.org>
2012-07-17 13:17:34 +0000

    * subversion/libsvn_fs/fs-loader.c   (load_module): Only allow alphanumeric characters in name.
    Suggested by: ivan
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1362480 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs/fs-loader.c
+++ subversion/libsvn_fs/fs-loader.c
@@ -110,6 +110,15 @@ load_module(fs_init_func_t *initfunc, const char *name, apr_pool_t *pool)
     const char *libname;
     const char *funcname;
     apr_status_t status;
+    apr_size_t i;
+
+    /* Demand a simple alphanumeric name so that the generated DSO
+       name is sensible. */
+    for (i = 0; i < strlen(name); ++i)
+      if (!svn_ctype_isalnum(name[i]))
+        return svn_error_createf(SVN_ERR_FS_UNKNOWN_FS_TYPE, NULL,
+                                 _("Invalid name for FS type '%s'"),
+                                 name);
 
     libname = apr_psprintf(pool, "libsvn_fs_%s-%d.so.0",
                            name, SVN_VER_MAJOR);
