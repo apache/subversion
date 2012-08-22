89137c449033d93c0e9b3bf5bb2348e579a799e5
C. Michael Pilato <cmpilato@apache.org>
2012-08-22 19:19:48 +0000

    Tweak the 'start-commit' hook to run *after* txn creation and initial revprop population, and to receive as a new command-line argument the txn-id (so that scripts can examine those revprops).
    * subversion/libsvn_repos/repos.h,
    * subversion/libsvn_repos/hooks.c
      (svn_repos__hooks_start_commit): Add 'txn_name' parameter, the value
        of which is passed as the new final argument to the 'start-commit'
        hook.
    
    * subversion/libsvn_repos/commit.c
      (svn_repos__get_commit_ev2): Update call to svn_repos__hooks_start_commit(),
        and now run it after setting txnprops.
    
    * subversion/libsvn_repos/fs-wrap.c
      (svn_repos_fs_begin_txn_for_commit2): Fetch the transaction name.
        Update call to svn_repos__hooks_start_commit(), and now run it
        after setting txnprops.
    
    * subversion/libsvn_repos/repos.c
      (create_hooks): Update 'start-commit' hook template text to reflect
        changes.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1376201 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_repos/repos.h
+++ subversion/libsvn_repos/repos.h
@@ -153,14 +153,19 @@ struct svn_repos_t
    allocations.  If the hook fails, return SVN_ERR_REPOS_HOOK_FAILURE.
 
    USER is the authenticated name of the user starting the commit.
+
    CAPABILITIES is a list of 'const char *' capability names (using
    SVN_RA_CAPABILITY_*) that the client has self-reported.  Note that
    there is no guarantee the client is telling the truth: the hook
-   should not make security assumptions based on the capabilities. */
+   should not make security assumptions based on the capabilities.
+
+   TXN_NAME is the name of the commit transaction that's just been
+   created. */
 svn_error_t *
 svn_repos__hooks_start_commit(svn_repos_t *repos,
                               const char *user,
                               const apr_array_header_t *capabilities,
+                              const char *txn_name,
                               apr_pool_t *pool);
 
 /* Run the pre-commit hook for REPOS.  Use POOL for any temporary
