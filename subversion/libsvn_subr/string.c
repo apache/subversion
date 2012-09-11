7eeac8fbf87bab87d8267159564513adb02c6ff8
Stefan Fuhrmann <stefan2@apache.org>
2012-09-11 00:19:54 +0000

    Add stringbuf functions for insertion, removal and replacement of substrings.
    * subversion/include/svn_string.h
      (svn_stringbuf_insert,
       svn_stringbuf_remove,
       svn_stringbuf_replace): declare new API
    * subversion/libsvn_subr/string.c
      (svn_stringbuf_insert,
       svn_stringbuf_remove,
       svn_stringbuf_replace): implement new API
    * subversion/tests/libsvn_subr/string-test.c
      (expect_stringbuf_equal): new utility function
      (test_stringbuf_insert,
       test_stringbuf_remove,
       test_stringbuf_replace): new tests
      (test_funcs): register them
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1383181 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/string.c
+++ subversion/libsvn_subr/string.c
@@ -578,6 +578,80 @@ svn_stringbuf_appendcstr(svn_stringbuf_t *targetstr, const char *cstr)
   svn_stringbuf_appendbytes(targetstr, cstr, strlen(cstr));
 }
 
+void
+svn_stringbuf_insert(svn_stringbuf_t *str,
+                     apr_size_t pos,
+                     const char *bytes,
+                     apr_size_t count)
+{
+  if (bytes + count > str->data && bytes < str->data + str->blocksize)
+    {
+      /* special case: BYTES overlaps with this string -> copy the source */
+      const char *temp = apr_pstrndup(str->pool, bytes, count);
+      svn_stringbuf_insert(str, pos, temp, count);
+    }
+  else
+    {
+      if (pos > str->len)
+        pos = str->len;
+
+      svn_stringbuf_ensure(str, str->len + count);
+      memmove(str->data + pos + count, str->data + pos, str->len - pos + 1);
+      memcpy(str->data + pos, bytes, count);
+
+      str->len += count;
+    }
+}
+
+void
+svn_stringbuf_remove(svn_stringbuf_t *str,
+                     apr_size_t pos,
+                     apr_size_t count)
+{
+  if (pos > str->len)
+    pos = str->len;
+  if (pos + count > str->len)
+    count = str->len - pos;
+
+  memmove(str->data + pos, str->data + pos + count, str->len - pos - count + 1);
+  str->len -= count;
+}
+
+void
+svn_stringbuf_replace(svn_stringbuf_t *str,
+                      apr_size_t pos,
+                      apr_size_t old_count,
+                      const char *bytes,
+                      apr_size_t new_count)
+{
+  if (bytes + new_count > str->data && bytes < str->data + str->blocksize)
+    {
+      /* special case: BYTES overlaps with this string -> copy the source */
+      const char *temp = apr_pstrndup(str->pool, bytes, new_count);
+      svn_stringbuf_replace(str, pos, old_count, temp, new_count);
+    }
+  else
+    {
+      if (pos > str->len)
+        pos = str->len;
+      if (pos + old_count > str->len)
+        old_count = str->len - pos;
+
+      if (old_count < new_count)
+        {
+          apr_size_t delta = new_count - old_count;
+          svn_stringbuf_ensure(str, str->len + delta);
+        }
+
+      if (old_count != new_count)
+        memmove(str->data + pos + new_count, str->data + pos + old_count,
+                str->len - pos - old_count + 1);
+
+      memcpy(str->data + pos, bytes, new_count);
+      str->len += new_count - old_count;
+    }
+}
+
 
 svn_stringbuf_t *
 svn_stringbuf_dup(const svn_stringbuf_t *original_string, apr_pool_t *pool)
