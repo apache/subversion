3850f1b9987f5b3a12fa58b784c1f9c13a1b8a1c
Stefan Fuhrmann <stefan2@apache.org>
2012-10-01 16:19:08 +0000

    * subversion/trunk/subversion/include/svn_ra_svn.h   (svn_ra_svn_create_conn2): fix deprecation statement
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1392425 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_ra_svn.h
+++ subversion/include/svn_ra_svn.h
@@ -253,7 +253,7 @@ svn_ra_svn_conn_t *svn_ra_svn_create_conn3(apr_socket_t *sock,
  *
  * @since New in 1.7.
  *
- * @deprecated Provided for backward compatibility with the 1.8 API.
+ * @deprecated Provided for backward compatibility with the 1.7 API.
  */
 svn_ra_svn_conn_t *
 svn_ra_svn_create_conn2(apr_socket_t *sock,
