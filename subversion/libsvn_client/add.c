apache/subversion

commit d175c726638d77af3de20a247efdb5e35c384df3

Following up on r1394748, apply some correctness and performance tweaks.
* subversion/libsvn_client/add.c
  (svn_client_add4): Check working copy root using a function that doesn't
    handle switched paths as a working copy root. Handle 'modern' error codes
    like the original function did. As this function just uses the wc_db node
    cache for its result we can just call it on a file without a performance
    penalty. Use wcroot result to avoid add parents check.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1395027 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_client/add.c
@@ -671,8 +671,7 @@ svn_client_add4(const char *path,
   const char *parent_abspath;
   const char *local_abspath;
   const char *existing_parent_abspath;
-  svn_node_kind_t disk_kind;
-  svn_boolean_t is_wc_root = FALSE;
+  svn_boolean_t is_wc_root;
   svn_error_t *err;
 
   if (svn_path_is_url(path))
@@ -687,24 +686,19 @@ svn_client_add4(const char *path,
      svn_wc_is_wc_root2() will return TRUE even if LOCAL_ABSPATH is a
      *symlink* to a working copy root, which is a scenario we want to
      treat differently.  */
-  SVN_ERR(svn_io_check_path(local_abspath, &disk_kind, pool));
-  if (disk_kind == svn_node_dir)
+  err = svn_wc__strictly_is_wc_root(&is_wc_root, ctx->wc_ctx,
+                                    local_abspath, pool);
+  if (err)
     {
-      err = svn_wc_is_wc_root2(&is_wc_root, ctx->wc_ctx,
-                               local_abspath, pool);
-      if (err)
+      if (err->apr_err != SVN_ERR_WC_PATH_NOT_FOUND
+          && err->apr_err == SVN_ERR_WC_NOT_WORKING_COPY)
         {
-          if (err->apr_err == SVN_ERR_ENTRY_NOT_FOUND)
-            {
-              svn_error_clear(err);
-              err = SVN_NO_ERROR;
-              is_wc_root = FALSE;
-            }
-          else
-            {
-              return err;
-            }
+          return svn_error_trace(err);
         }
+
+      svn_error_clear(err);
+      err = NULL; /* SVN_NO_ERROR */
+      is_wc_root = FALSE;
     }
   if (is_wc_root && (! force))
     {
@@ -726,7 +720,7 @@ svn_client_add4(const char *path,
     parent_abspath = svn_dirent_dirname(local_abspath, pool);
 
   existing_parent_abspath = NULL;
-  if (add_parents)
+  if (add_parents && !is_wc_root)
     {
       apr_pool_t *subpool;
       const char *existing_parent_abspath2;

commit 05d980a3c018e5028ccac7803163f8db48e07e7a

Remove the no longer necessary hack that selectively allowed adding working copy roots when the current directory was that working copy root. Integrate this with the new wcroot check code to simplify the result.
Also properly handle svn add --force on symlinks (regression in r1395027)

* subversion/libsvn_client/add.c
  (svn_client_add4): Remove a documented 'hack' that is no longer necessary
    since we now always allow adding an already versioned directory.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1395082 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_client/add.c
@@ -698,7 +698,7 @@ svn_client_add4(const char *path,
       err = NULL; /* SVN_NO_ERROR */
       is_wc_root = FALSE;
     }
-  if (is_wc_root && (! force))
+  if (is_wc_root)
     {
 #ifdef HAVE_SYMLINK
       svn_node_kind_t disk_kind;
@@ -713,20 +713,17 @@ svn_client_add4(const char *path,
         is_wc_root = FALSE;
       else
 #endif
-        return svn_error_createf(SVN_ERR_ENTRY_EXISTS, NULL,
+        {
+          if (! force)
+            return svn_error_createf(
+                                 SVN_ERR_ENTRY_EXISTS, NULL,
                                  _("'%s' is already under version control"),
                                  svn_dirent_local_style(local_abspath, pool));
+        }
     }
-  
-  /* ### this is a hack.
-     ### before we switched to absolute paths, if a user tried to do
-     ### 'svn add .', PATH would be "" and PARENT_PATH would also be "",
-     ### thus emulating the behavior below.  Now that we are using
-     ### absolute paths, svn_dirent_dirname() doesn't behave the same way
-     ### w.r.t. '.', so we need to include the following hack.  This
-     ### behavior is tested in schedule_tests-11. */
-  if (path[0] == 0)
-    parent_abspath = local_abspath;
+
+  if (is_wc_root)
+    parent_abspath = local_abspath; /* We will only add children */
   else
     parent_abspath = svn_dirent_dirname(local_abspath, pool);
 
@@ -747,9 +744,7 @@ svn_client_add4(const char *path,
   SVN_WC__CALL_WITH_WRITE_LOCK(
     add(local_abspath, depth, force, no_ignore, existing_parent_abspath,
         ctx, pool),
-    ctx->wc_ctx,
-    is_wc_root ? local_abspath
-               : (existing_parent_abspath ? existing_parent_abspath 
+    ctx->wc_ctx, (existing_parent_abspath ? existing_parent_abspath 
                                           : parent_abspath),
     FALSE /* lock_anchor */, pool);
   return SVN_NO_ERROR;