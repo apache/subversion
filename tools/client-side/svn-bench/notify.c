20db2713343ec8bb9560546fd7893bf47848ceef
Philip Martin <philip@apache.org>
2012-10-08 14:11:44 +0000

    * tools/client-side/svn-bench/notify.c   (svn_cl__print_conflict_stats): Remove as unused.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1395576 13f79535-47bb-0310-9956-ffa450edef68

--- tools/client-side/svn-bench/notify.c
+++ tools/client-side/svn-bench/notify.c
@@ -65,43 +65,6 @@ struct notify_baton
 };
 
 
-svn_error_t *
-svn_cl__print_conflict_stats(void *notify_baton, apr_pool_t *pool)
-{
-  struct notify_baton *nb = notify_baton;
-  unsigned int text_conflicts;
-  unsigned int prop_conflicts;
-  unsigned int tree_conflicts;
-  unsigned int skipped_paths;
-
-  text_conflicts = nb->text_conflicts;
-  prop_conflicts = nb->prop_conflicts;
-  tree_conflicts = nb->tree_conflicts;
-  skipped_paths = nb->skipped_paths;
-
-  if (text_conflicts > 0 || prop_conflicts > 0
-    || tree_conflicts > 0 || skipped_paths > 0)
-      SVN_ERR(svn_cmdline_printf(pool, "%s", _("Summary of conflicts:\n")));
-
-  if (text_conflicts > 0)
-    SVN_ERR(svn_cmdline_printf
-      (pool, _("  Text conflicts: %u\n"), text_conflicts));
-
-  if (prop_conflicts > 0)
-    SVN_ERR(svn_cmdline_printf
-      (pool, _("  Property conflicts: %u\n"), prop_conflicts));
-
-  if (tree_conflicts > 0)
-    SVN_ERR(svn_cmdline_printf
-      (pool, _("  Tree conflicts: %u\n"), tree_conflicts));
-
-  if (skipped_paths > 0)
-    SVN_ERR(svn_cmdline_printf
-      (pool, _("  Skipped paths: %u\n"), skipped_paths));
-
-  return SVN_NO_ERROR;
-}
-
 /* Add a conflicted path to the list of conflicted paths stored
  * in the notify baton. */
 static void
