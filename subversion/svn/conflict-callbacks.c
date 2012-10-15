apache/subversion

commit 385fd691db6ce2a787641e7a381ea1c4acfb80b0

Easter egg.
* subversion/svn/conflict-callbacks.c
  (handle_text_conflict): Use ASCII art rather than :-[A-Z].

git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1398147 13f79535-47bb-0310-9956-ffa450edef68

subversion/svn/conflict-callbacks.c
@@ -477,7 +477,8 @@ handle_text_conflict(svn_wc_conflict_result_t *result,
           if (performed_edit)
             knows_something = TRUE;
         }
-      else if (strcmp(answer, "m") == 0 || strcmp(answer, ":-M") == 0)
+      else if (strcmp(answer, "m") == 0 || strcmp(answer, "=>-") == 0
+               || strcmp(answer, ":>.") == 0)
         {
           if (desc->kind != svn_wc_conflict_kind_text)
             {

commit 5d4cece919475181f0d1f47518e4589fe7ecaba0

* subversion/svn/conflict-callbacks.c   (handle_text_conflict): Restore ability to run the merge tool with a smiley    face at the conflict prompt. Fixes regression introduced in r1398147 :-)
git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1398304 13f79535-47bb-0310-9956-ffa450edef68

subversion/svn/conflict-callbacks.c
@@ -477,8 +477,8 @@ handle_text_conflict(svn_wc_conflict_result_t *result,
           if (performed_edit)
             knows_something = TRUE;
         }
-      else if (strcmp(answer, "m") == 0 || strcmp(answer, "=>-") == 0
-               || strcmp(answer, ":>.") == 0)
+      else if (strcmp(answer, "m") == 0 || strcmp(answer, ":-g") ||
+               strcmp(answer, "=>-") == 0 || strcmp(answer, ":>.") == 0)
         {
           if (desc->kind != svn_wc_conflict_kind_text)
             {