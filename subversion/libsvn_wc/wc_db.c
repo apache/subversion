4924c6d7addee338e77047e12289c19e0eaa4ee7
Bert Huijben <rhuijben@apache.org>
2012-11-08 11:43:48 +0000

    Turn svn_wc__db_temp_op_make_copy() in a proper db operation, by renaming and adding conflicts and work_items arguments. This in preparation for adding another caller in my next patch and possibly another one later on.
    * subversion/libsvn_wc/wc_db.c
      (db_base_remove): Update caller. Remove comment.
      (make_copy_baton_t): Add 2 variables.
      (make_copy_txn): Handle conflicts and work items. Move struct out of loop.
      (svn_wc__db_temp_op_make_copy): Rename to ...
      (svn_wc__db_op_make_copy): ... this, add arguments and fill struct.
    
    * subversion/libsvn_wc/wc_db.h
      (svn_wc__db_temp_op_make_copy): Rename to ...
      (svn_wc__db_op_make_copy): ... this and add arguments.
    
    * subversion/tests/libsvn_wc/op-depth-test.c
      (temp_op_make_copy): Update caller.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1407021 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/wc_db.c
+++ subversion/libsvn_wc/wc_db.c
@@ -2074,16 +2074,15 @@ db_base_remove(void *baton,
                                             wcroot, local_relpath,
                                             scratch_pool, scratch_pool));
 
-  /* ### This function should be turned into a helper of this function,
-         as this is the only valid caller */
   if (status == svn_wc__db_status_normal
       && rb->keep_as_working)
     {
-      SVN_ERR(svn_wc__db_temp_op_make_copy(rb->db,
-                                           svn_dirent_join(wcroot->abspath,
-                                                           local_relpath,
-                                                           scratch_pool),
-                                           scratch_pool));
+      SVN_ERR(svn_wc__db_op_make_copy(rb->db,
+                                      svn_dirent_join(wcroot->abspath,
+                                                      local_relpath,
+                                                      scratch_pool),
+                                      NULL, NULL,
+                                      scratch_pool));
       keep_working = TRUE;
     }
   else
@@ -13085,6 +13084,8 @@ svn_wc__db_temp_op_start_directory_update(svn_wc__db_t *db,
 struct make_copy_baton_t
 {
   int op_depth;
+  const svn_skel_t *conflicts;
+  const svn_skel_t *work_items;
 };
 
 
@@ -13133,6 +13134,7 @@ make_copy_txn(void *baton,
   svn_boolean_t add_working_base_deleted = FALSE;
   svn_boolean_t remove_working = FALSE;
   const apr_array_header_t *children;
+  struct make_copy_baton_t cbt;
   apr_pool_t *iterpool = svn_pool_create(scratch_pool);
   int i;
 
@@ -13197,17 +13199,19 @@ make_copy_txn(void *baton,
   SVN_ERR(gather_repo_children(&children, wcroot, local_relpath,
                                0, scratch_pool, iterpool));
 
+  cbt.op_depth = mcb->op_depth;
+  cbt.conflicts = NULL;
+  cbt.work_items = NULL;
+
   for (i = 0; i < children->nelts; i++)
     {
       const char *name = APR_ARRAY_IDX(children, i, const char *);
-      struct make_copy_baton_t cbt;
+      
       const char *copy_relpath;
 
       svn_pool_clear(iterpool);
 
       copy_relpath = svn_relpath_join(local_relpath, name, iterpool);
-
-      cbt.op_depth = mcb->op_depth;
 
       SVN_ERR(make_copy_txn(&cbt, wcroot, copy_relpath, iterpool));
     }
@@ -13216,6 +13220,11 @@ make_copy_txn(void *baton,
                                                 iterpool),
                                                 svn_depth_empty, iterpool));
 
+  if (mcb->conflicts)
+    SVN_ERR(mark_conflict(wcroot, local_relpath, mcb->conflicts, iterpool));
+
+  SVN_ERR(add_work_items(wcroot->sdb, mcb->work_items, iterpool));
+
   svn_pool_destroy(iterpool);
 
   return SVN_NO_ERROR;
@@ -13223,9 +13232,11 @@ make_copy_txn(void *baton,
 
 
 svn_error_t *
-svn_wc__db_temp_op_make_copy(svn_wc__db_t *db,
-                             const char *local_abspath,
-                             apr_pool_t *scratch_pool)
+svn_wc__db_op_make_copy(svn_wc__db_t *db,
+                        const char *local_abspath,
+                        const svn_skel_t *conflicts,
+                        const svn_skel_t *work_items,
+                        apr_pool_t *scratch_pool)
 {
   svn_wc__db_wcroot_t *wcroot;
   const char *local_relpath;
@@ -13258,6 +13269,8 @@ svn_wc__db_temp_op_make_copy(svn_wc__db_t *db,
   SVN_ERR(catch_copy_of_server_excluded(wcroot, local_relpath, scratch_pool));
 
   mcb.op_depth = relpath_depth(local_relpath);
+  mcb.conflicts = conflicts;
+  mcb.work_items = work_items;
 
   SVN_ERR(svn_wc__db_with_txn(wcroot, local_relpath, make_copy_txn, &mcb,
                               scratch_pool));
