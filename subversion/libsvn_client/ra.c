92380d448ebd99c4bcadcd7394bd56ad62833d76
Bert Huijben <rhuijben@apache.org>
2012-11-13 14:53:12 +0000

    * subversion/libsvn_client/ra.c   (svn_client__ra_provide_base,    svn_client__ra_provide_props): Obtain the same value using a better      documented function
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1408760 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/ra.c
+++ subversion/libsvn_client/ra.c
@@ -1033,9 +1033,9 @@ svn_client__ra_provide_base(svn_stream_t **contents,
     {
       /* The pristine contents refer to the BASE, or to the pristine of
          a copy/move to this location. Fetch the correct revision.  */
-      SVN_ERR(svn_wc__node_get_commit_base(revision, NULL, NULL, NULL,
-                                           reb->wc_ctx, local_abspath,
-                                           scratch_pool, scratch_pool));
+      SVN_ERR(svn_wc__node_get_origin(NULL, revision, NULL, NULL, NULL, NULL,
+                                      reb->wc_ctx, local_abspath, FALSE,
+                                      scratch_pool, scratch_pool));
     }
 
   return SVN_NO_ERROR;
@@ -1078,9 +1078,9 @@ svn_client__ra_provide_props(apr_hash_t **props,
     {
       /* The pristine props refer to the BASE, or to the pristine props of
          a copy/move to this location. Fetch the correct revision.  */
-      SVN_ERR(svn_wc__node_get_commit_base(revision, NULL, NULL, NULL,
-                                           reb->wc_ctx, local_abspath,
-                                           scratch_pool, scratch_pool));
+      SVN_ERR(svn_wc__node_get_origin(NULL, revision, NULL, NULL, NULL, NULL,
+                                      reb->wc_ctx, local_abspath, FALSE,
+                                      scratch_pool, scratch_pool));
     }
 
   return SVN_NO_ERROR;
