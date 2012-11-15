32ab6649d8dacf955f340fb2db5397b27d847b66
Neels Hofmeyr <neels@apache.org>
2012-11-15 16:57:05 +0000

    Implement issue #4227: Externals: Multiple definitions for the same path; limitation: only check for duplicates within the same svn:externals prop.
    * subversion/libsvn_wc/externals.c
      (svn_wc_parse_externals_description3):
        Return an error when DESC contains the same WC target twice (or more). For
        that purpose, always compose the list of items, regardless of whether it
        will be returned or not. Side effect: if EXTERNALS_P is nonzero, and if
        parsing of DESC returns an error, 
    
    * subversion/tests/cmdline/externals_tests.py
      (duplicate_targets): New test for above.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1409883 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/externals_tests.py
+++ subversion/tests/cmdline/externals_tests.py
@@ -2878,6 +2878,68 @@ def url_to_wc_copy_of_externals(sbox):
     "OUTPUT", expected_stdout, [], 0, 'copy', repo_url + '/A/C',
     sbox.ospath('External-WC-to-URL-Copy'))
 
+@Issue(4227)
+def duplicate_targets(sbox):
+  "local path appears twice in one svn:external prop"
+
+  if False:
+    svntest.factory.make(sbox, r"""
+      svn ps svn:externals "^/A/B/E barf\n^/A/B/E barf" .
+      svn ps svn:externals "^/A/B/E barf\n^/A/D/G barf" .
+      svn ps svn:externals "^/A/B/E barf/.\n^/A/D/G ./barf" .
+      svn ps svn:externals "^/A/B/E ././barf\n^/A/D/G .//barf" .
+      svn pl -v .
+      svn ps svn:externals "^/A/B/E ok" .
+      svn pl -v .
+      """)
+
+  sbox.build()
+  wc_dir = sbox.wc_dir
+  abs_wc_dir = os.path.abspath(sbox.wc_dir)
+
+  expected_stderr = verify.RegexOutput(
+    ".*Invalid svn:externals property on '" + abs_wc_dir +
+    "': target 'barf' appears more than once\n",
+    match_all=False)
+
+  # svn ps svn:externals "^/A/B/E barf\n^/A/B/E barf" .
+  actions.run_and_verify_svn2('OUTPUT', [], expected_stderr, 1, 'ps',
+    'svn:externals', '^/A/B/E barf\n^/A/B/E barf', wc_dir)
+
+  # svn ps svn:externals "^/A/B/E barf\n^/A/D/G barf" .
+  actions.run_and_verify_svn2('OUTPUT', [], expected_stderr, 1, 'ps',
+    'svn:externals', '^/A/B/E barf\n^/A/D/G barf', wc_dir)
+
+  # svn ps svn:externals "^/A/B/E barf/.\n^/A/D/G ./barf" .
+  actions.run_and_verify_svn2('OUTPUT', [], expected_stderr, 1, 'ps',
+    'svn:externals', '^/A/B/E barf/.\n^/A/D/G ./barf', wc_dir)
+
+  # svn ps svn:externals "^/A/B/E ././barf\n^/A/D/G .//barf" .
+  actions.run_and_verify_svn2('OUTPUT', [], expected_stderr, 1, 'ps',
+    'svn:externals', '^/A/B/E ././barf\n^/A/D/G .//barf', wc_dir)
+
+  # svn pl -v .
+  actions.run_and_verify_svn2('OUTPUT', [], [], 0, 'pl', '-v',
+    wc_dir)
+
+  # svn ps svn:externals "^/A/B/E ok" .
+  expected_stdout = ["property 'svn:externals' set on '" + wc_dir + "'\n"]
+
+  actions.run_and_verify_svn2('OUTPUT', expected_stdout, [], 0, 'ps',
+    'svn:externals', '^/A/B/E ok', wc_dir)
+
+  # svn pl -v .
+  expected_stdout = [
+    "Properties on '" + wc_dir + "':\n",
+    '  svn:externals\n',
+    '    ^/A/B/E ok\n',
+    '    \n',
+  ]
+
+  actions.run_and_verify_svn2('OUTPUT', expected_stdout, [], 0, 'pl', '-v',
+    wc_dir)
+
+
 ########################################################################
 # Run the tests
 
@@ -2925,6 +2987,7 @@ test_list = [ None,
               remap_file_external_with_prop_del,
               dir_external_with_dash_r_only,
               url_to_wc_copy_of_externals,
+              duplicate_targets,
              ]
 
 if __name__ == '__main__':
