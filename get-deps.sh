49b3c1b803991cbecaa166ba936ca41f2b2f44d1
Daniel Shahaf <danielsh@apache.org>
2012-12-13 18:31:40 +0000

    * get-deps.sh   (SQLITE_VERSION): Fix filename determination by fixing printf varargs.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1421418 13f79535-47bb-0310-9956-ffa450edef68

--- get-deps.sh
+++ get-deps.sh
@@ -27,8 +27,9 @@ APR=apr-1.4.6
 APR_UTIL=apr-util-1.4.1
 SERF=serf-1.0.1
 ZLIB=zlib-1.2.7
-SQLITE_VERSION=3.7.12
-SQLITE=sqlite-amalgamation-$(printf %u%02u%02u%02u $(echo $SQLITE_VERSION | sed -e "s/\./ /g"))
+SQLITE_VERSION=3.7.12.0
+3071200
+SQLITE=sqlite-amalgamation-$(printf %u%02u%02u%02u $(echo $SQLITE_VERSION | sed -e 's/\./ /g'))
 
 HTTPD=httpd-2.2.22
 APR_ICONV=apr-iconv-1.2.1
