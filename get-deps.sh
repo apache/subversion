a81c15befd3c1508a06762f0029cfafda1cb1352
Ben Reser <breser@apache.org>
2012-12-14 05:52:37 +0000

    Remove some neon references still lingering about.
    * get-deps.sh
      (get_deps): No reason to warn that we won't be downloading neon if the neon
        directory exists, since we won't be downloading neon anyway.
    
    * build/win32
      (svn:ignore): build_neon.bat shouldn't be being produced.  Let people realize
        they can delete this obsolete file.
    
    * subversion/bindings/swig/python/tests/ra.py
      (test_namestring): ra_neon is gone and ra_svn supports get_client_string so
        allow the test to run against svn:// and make the comment accurate.
    
    * subversion/bindings/swig/perl/native/Core.pm
      (SVN::Auth::SSL::OTHER): Update documentation to not refer to a specific
        http lib.
    
    * subversion/bindings/swig/perl/native/Makefile.PL.in
      (@ldpaths): Don't include libsvn_ra_neon in the library paths.
    
    * aclocal.m4: Don't include the neon.m4 file.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1421676 13f79535-47bb-0310-9956-ffa450edef68

--- get-deps.sh
+++ get-deps.sh
@@ -107,7 +107,7 @@ get_sqlite() {
 get_deps() {
     mkdir -p $TEMPDIR
 
-    for i in neon zlib serf sqlite-amalgamation apr apr-util; do
+    for i in zlib serf sqlite-amalgamation apr apr-util; do
       if [ -d $i ]; then
         echo "Local directory '$i' already exists; the downloaded copy won't be used" >&2
       fi
