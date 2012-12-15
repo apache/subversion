5caf35eb96a7ac05d69c873c0efb62e5698f004c
C. Michael Pilato <cmpilato@apache.org>
2012-12-15 09:03:23 +0000

    Fix issue #4101 ("'svnrdump dump -rN:M <URL>/subpath' strips subpath in first rev only") by teaching the special-case code in svnrdump used to handle the first revision of non-incremental range-based dump which doesn't begin with r0 to:
       1. generate dump stream commands for adding the missing parent tree
          structure, and
    
       2. use full repository relpaths for the Node-path: headers.
    
    NOTE: I'm a bit torn about whether I should have done #1 above.
          Perhaps that bit of the functionality should be tied to a
          --parents options?
    
    * subversion/svnrdump/svnrdump.h
      (svn_rdump__get_dump_editor, svn_rdump__get_dump_editor_v2): Add
        edit_root_relpath' parameter.
    
    * subversion/svnrdump/svnrdump.c
      (replay_revstart, replay_revstart_v2): Update call to
        svn_rdump__get_dump_editor()
      (dump_initial_full_revision): Calculate the source repository
        relpath, passing that to an updated call to svn_rdump__get_dump_editor()
    
    * subversion/svnrdump/dump_editor.c
      (dump_edit_baton): Add 'edit_root_relpath' member.
      (dump_node): Prepend the edit root relpath to the dump stream path
        if necessary.
      (dump_mkdir): New helper function.
      (open_root): If 'edit_root_relpath' is non-NULL, use dump_mkdir() to
        generate dump-stream additions for the dump source directory and
        its parents.
      (svn_rdump__get_dump_editor): Add 'edit_root_relpath' parameter,
        and stuff it into the dump edit baton.
    
    * subversion/tests/cmdline/svnrdump_tests_data/root-range.expected.dump,
    * subversion/tests/cmdline/svnrdump_tests_data/trunk-A-range.expected.dump,
    * subversion/tests/cmdline/svnrdump_tests_data/trunk-only-range.expected.dump
      New test data.
    
    * subversion/tests/cmdline/svnrdump_tests.py
      (range_dump, only_trunk_range_dump, only_trunk_A_range_dump): New tests.
      (test_list): Add references to new tests.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1422206 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svnrdump/svnrdump.h
+++ subversion/svnrdump/svnrdump.h
@@ -39,9 +39,13 @@ extern "C" {
 
 /**
  * Get a dump editor @a editor along with a @a edit_baton allocated in
- * @a pool.  The editor will write output to @a stream.  Use @a
- * cancel_func and @a cancel_baton to check for user cancellation of
- * the operation (for timely-but-safe termination).
+ * @a pool.  The editor will write output to @a stream.
+ *
+ * @a edit_root_relpath is the relative repository path of the drive
+ * which will happen on @a *editor.
+ *
+ * Use @a cancel_func and @a cancel_baton to check for user
+ * cancellation of the operation (for timely-but-safe termination).
  */
 svn_error_t *
 svn_rdump__get_dump_editor(const svn_delta_editor_t **editor,
@@ -49,6 +53,7 @@ svn_rdump__get_dump_editor(const svn_delta_editor_t **editor,
                            svn_revnum_t revision,
                            svn_stream_t *stream,
                            svn_ra_session_t *ra_session,
+                           const char *edit_root_relpath,
                            svn_cancel_func_t cancel_func,
                            void *cancel_baton,
                            apr_pool_t *pool);
@@ -59,6 +64,7 @@ svn_rdump__get_dump_editor_v2(svn_editor_t **editor,
                               svn_revnum_t revision,
                               svn_stream_t *stream,
                               svn_ra_session_t *ra_session,
+                              const char *edit_root_relpath,
                               svn_cancel_func_t cancel_func,
                               void *cancel_baton,
                               apr_pool_t *scratch_pool,
