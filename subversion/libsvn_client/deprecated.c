5a09c177382c046f93bd934c1833e80301af393f
Bert Huijben <rhuijben@apache.org>
2012-12-20 16:07:51 +0000

    * subversion/libsvn_client/commit.c   (svn_client_commit5): Move this function...
    * subversion/libsvn_client/deprecated.c
      (svn_client_commit5): ... to this file.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1424557 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/deprecated.c
+++ subversion/libsvn_client/deprecated.c
@@ -512,6 +512,26 @@ downgrade_commit_copied_notify_func(void *baton,
     b->orig_notify_func2(b->orig_notify_baton2, notify, pool);
 }
 
+svn_error_t *
+svn_client_commit5(const apr_array_header_t *targets,
+                   svn_depth_t depth,
+                   svn_boolean_t keep_locks,
+                   svn_boolean_t keep_changelists,
+                   svn_boolean_t commit_as_operations,
+                   const apr_array_header_t *changelists,
+                   const apr_hash_t *revprop_table,
+                   svn_commit_callback2_t commit_callback,
+                   void *commit_baton,
+                   svn_client_ctx_t *ctx,
+                   apr_pool_t *pool)
+{
+  return svn_client_commit6(targets, depth, keep_locks, keep_changelists,
+                            commit_as_operations,
+                            TRUE,  /* include_file_externals */
+                            FALSE, /* include_dir_externals */
+                            changelists, revprop_table, commit_callback,
+                            commit_baton, ctx, pool);
+}
 
 svn_error_t *
 svn_client_commit4(svn_commit_info_t **commit_info_p,
