17f46e55760f7228ba01b4d9f37c422e32912dd5
Julian Foad <julianfoad@apache.org>
2012-12-21 21:13:51 +0000

    * subversion/libsvn_wc/wc_db.c   (svn_wc__db_extend_parent_delete): In the doc string, add a note     asking what the KIND and OP_DEPTH parameters are for.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1425147 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/wc_db.c
+++ subversion/libsvn_wc/wc_db.c
@@ -588,6 +588,12 @@ blank_ibb(insert_base_baton_t *pibb)
 
 /* Extend any delete of the parent of LOCAL_RELPATH to LOCAL_RELPATH.
 
+   ### What about KIND and OP_DEPTH?  KIND ought to be redundant; I'm
+       discussing on dev@ whether we can let that be null for presence
+       == base-deleted.  OP_DEPTH is the op-depth of what, and why?
+       It is used to select the lowest working node higher than OP_DEPTH,
+       so, in terms of the API, OP_DEPTH means ...?
+
    Given a wc:
 
               0         1         2         3         4
