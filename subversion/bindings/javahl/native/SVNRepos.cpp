f035ec0c1c31d93520ea0a96390ff3cc21f604af
Vladimir Berezniker <vmpn@apache.org>
2012-12-27 02:23:45 +0000

    On the javahl-ra branch:
    Bring up-to-date with JavaHL changes on trunk@1405529
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/javahl-ra@1426101 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/javahl/native/SVNRepos.cpp
+++ subversion/bindings/javahl/native/SVNRepos.cpp
@@ -169,7 +169,7 @@ void SVNRepos::deltify(File &path, Revision &revStart, Revision &revEnd)
   for (revision = start; revision <= end; ++revision)
     {
       revisionPool.clear();
-      SVN_JNI_ERR(svn_fs_deltify_revision (fs, revision, revisionPool.getPool()),
+      SVN_JNI_ERR(svn_fs_deltify_revision(fs, revision, revisionPool.getPool()),
                   );
     }
 
@@ -273,7 +273,7 @@ void SVNRepos::hotcopy(File &path, File &targetPath,
 }
 
 static void
-list_dblogs (File &path, MessageReceiver &receiver, bool only_unused)
+list_dblogs(File &path, MessageReceiver &receiver, bool only_unused)
 {
   SVN::Pool requestPool;
   apr_array_header_t *logfiles;
@@ -297,7 +297,7 @@ list_dblogs (File &path, MessageReceiver &receiver, bool only_unused)
       log_utf8 = svn_dirent_join(path.getInternalStyle(requestPool),
                                  APR_ARRAY_IDX(logfiles, i, const char *),
                                  requestPool.getPool());
-      log_utf8 = svn_dirent_local_style (log_utf8, requestPool.getPool());
+      log_utf8 = svn_dirent_local_style(log_utf8, requestPool.getPool());
       receiver.receiveMessage(log_utf8);
     }
 }
@@ -365,13 +365,13 @@ void SVNRepos::lstxns(File &path, MessageReceiver &messageReceiver)
 
   SVN_JNI_ERR(svn_repos_open2(&repos, path.getInternalStyle(requestPool),
                               NULL, requestPool.getPool()), );
-  fs = svn_repos_fs (repos);
+  fs = svn_repos_fs(repos);
   SVN_JNI_ERR(svn_fs_list_transactions(&txns, fs, requestPool.getPool()), );
 
   /* Loop, printing revisions. */
   for (int i = 0; i < txns->nelts; ++i)
     {
-      messageReceiver.receiveMessage(APR_ARRAY_IDX (txns, i, const char *));
+      messageReceiver.receiveMessage(APR_ARRAY_IDX(txns, i, const char *));
     }
 
 
@@ -402,7 +402,7 @@ jlong SVNRepos::recover(File &path, ReposNotifyCallback *notifyCallback)
    * recovery actually worked. */
   SVN_JNI_ERR(svn_repos_open2(&repos, path.getInternalStyle(requestPool),
                               NULL, requestPool.getPool()), -1);
-  SVN_JNI_ERR(svn_fs_youngest_rev(&youngest_rev, svn_repos_fs (repos),
+  SVN_JNI_ERR(svn_fs_youngest_rev(&youngest_rev, svn_repos_fs(repos),
                                   requestPool.getPool()),
               -1);
   return youngest_rev;
@@ -426,13 +426,13 @@ void SVNRepos::rmtxns(File &path, StringArray &transactions)
 
   SVN_JNI_ERR(svn_repos_open2(&repos, path.getInternalStyle(requestPool),
                               NULL, requestPool.getPool()), );
-  fs = svn_repos_fs (repos);
+  fs = svn_repos_fs(repos);
 
   args = transactions.array(requestPool);
   /* All the rest of the arguments are transaction names. */
   for (i = 0; i < args->nelts; ++i)
     {
-      const char *txn_name = APR_ARRAY_IDX (args, i, const char *);
+      const char *txn_name = APR_ARRAY_IDX(args, i, const char *);
       svn_error_t *err;
 
       /* Try to open the txn.  If that succeeds, try to abort it. */
@@ -446,7 +446,7 @@ void SVNRepos::rmtxns(File &path, StringArray &transactions)
        * all.  */
       if (err && (err->apr_err == SVN_ERR_FS_TRANSACTION_DEAD))
         {
-          svn_error_clear (err);
+          svn_error_clear(err);
           err = svn_fs_purge_txn(fs, txn_name, transactionPool.getPool());
         }
 
@@ -500,7 +500,7 @@ void SVNRepos::setRevProp(File &path, Revision &revision,
     }
   else
     {
-      svn_fs_t *fs = svn_repos_fs (repos);
+      svn_fs_t *fs = svn_repos_fs(repos);
       err = svn_fs_change_rev_prop2(fs, revision.revision()->value.number,
                                     propName, NULL, propValStr,
                                     requestPool.getPool());
@@ -557,7 +557,7 @@ SVNRepos::verify(File &path, Revision &revisionStart, Revision &revisionEnd,
    * with no interest in the output. */
   SVN_JNI_ERR(svn_repos_open2(&repos, path.getInternalStyle(requestPool),
                               NULL, requestPool.getPool()), );
-  SVN_JNI_ERR(svn_fs_youngest_rev(&youngest, svn_repos_fs (repos),
+  SVN_JNI_ERR(svn_fs_youngest_rev(&youngest, svn_repos_fs(repos),
                                   requestPool.getPool()), );
 
   /* Find the revision numbers at which to start and end. */
@@ -661,12 +661,12 @@ jobject SVNRepos::lslocks(File &path, svn_depth_t depth)
 
   std::vector<jobject> jlocks;
 
-  for (hi = apr_hash_first (requestPool.getPool(), locks);
+  for (hi = apr_hash_first(requestPool.getPool(), locks);
        hi;
-       hi = apr_hash_next (hi))
+       hi = apr_hash_next(hi))
     {
       void *val;
-      apr_hash_this (hi, NULL, NULL, &val);
+      apr_hash_this(hi, NULL, NULL, &val);
       svn_lock_t *lock = (svn_lock_t *)val;
       jobject jLock = CreateJ::Lock(lock);
 
@@ -693,7 +693,7 @@ void SVNRepos::rmlocks(File &path, StringArray &locks)
 
   SVN_JNI_ERR(svn_repos_open2(&repos, path.getInternalStyle(requestPool),
                               NULL, requestPool.getPool()), );
-  fs = svn_repos_fs (repos);
+  fs = svn_repos_fs(repos);
   const char *username;
 
   /* svn_fs_unlock() demands that some username be associated with the
@@ -702,12 +702,12 @@ void SVNRepos::rmlocks(File &path, StringArray &locks)
     apr_uid_t uid;
     apr_gid_t gid;
     char *un;
-    if (apr_uid_current (&uid, &gid, requestPool.getPool()) == APR_SUCCESS &&
-        apr_uid_name_get (&un, uid, requestPool.getPool()) == APR_SUCCESS)
+    if (apr_uid_current(&uid, &gid, requestPool.getPool()) == APR_SUCCESS &&
+        apr_uid_name_get(&un, uid, requestPool.getPool()) == APR_SUCCESS)
       {
         svn_error_t *err = svn_utf_cstring_to_utf8(&username, un,
                                                    requestPool.getPool());
-        svn_error_clear (err);
+        svn_error_clear(err);
         if (err)
           username = "administrator";
       }
@@ -723,7 +723,7 @@ void SVNRepos::rmlocks(File &path, StringArray &locks)
   const apr_array_header_t *args = locks.array(requestPool);
   for (int i = 0; i < args->nelts; ++i)
     {
-      const char *lock_path = APR_ARRAY_IDX (args, i, const char *);
+      const char *lock_path = APR_ARRAY_IDX(args, i, const char *);
       svn_lock_t *lock;
 
       /* Fetch the path's svn_lock_t. */
@@ -734,13 +734,13 @@ void SVNRepos::rmlocks(File &path, StringArray &locks)
         continue;
 
       /* Now forcibly destroy the lock. */
-      err = svn_fs_unlock (fs, lock_path,
-                           lock->token, 1 /* force */, subpool.getPool());
+      err = svn_fs_unlock(fs, lock_path,
+                          lock->token, 1 /* force */, subpool.getPool());
       if (err)
         goto move_on;
 
     move_on:
-      svn_error_clear (err);
+      svn_error_clear(err);
       subpool.clear();
     }
 
