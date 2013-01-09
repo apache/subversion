5bca0c773e3417f8932bc441baf0e20df500f3d2
Julian Foad <julianfoad@apache.org>
2013-01-09 20:10:47 +0000

    * subversion/libsvn_ra_svn/client.c   (optbool_to_tristate): Fix the logic, following breakage in r1431017.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1431040 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_ra_svn/client.c
+++ subversion/libsvn_ra_svn/client.c
@@ -1304,9 +1304,9 @@ static svn_error_t *ra_svn_get_dir(svn_ra_session_t *session,
 static svn_tristate_t
 optbool_to_tristate(apr_uint64_t v)
 {
-  if (v)
+  if (v == TRUE)  /* not just non-zero but exactly equal to 'TRUE' */
     return svn_tristate_true;
-  if (!v)
+  if (v == FALSE)
     return svn_tristate_false;
 
   return svn_tristate_unknown; /* Contains SVN_RA_SVN_UNSPECIFIED_NUMBER */
