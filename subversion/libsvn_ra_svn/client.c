apache/subversion

commit 448ebcaeeb42502867e77aa4012b756aa0d1f0dd

Coding style patch: in logical expressions, instead of comparing with arithmetic constants use boolean operations. I.e. replace
<expr> == TRUE with <expr>  and
<expr> == FALSE with !<expr>

* subversion/libsvn_delta/svndiff.c
  (send_simple_insertion_window,
   window_handler): change condition style
* subversion/libsvn_fs_base/fs.c
  (svn_fs_base__clean_logs): ditto
* subversion/libsvn_fs_fs/fs_fs.c
  (representation_string,
   create_rep_state_body, 
   build_rep_list): ditto
* subversion/libsvn_ra_serf/locks.c
  (handle_lock): ditto
* subversion/libsvn_ra_serf/property.c
  (create_propfind_body): same here,
* subversion/libsvn_ra_serf/update.c
  (cancel_fetch, 
   handle_fetch,
   end_report,
   finish_report): here,
* subversion/libsvn_ra_serf/util.c
  (svn_ra_serf__handle_xml_parser): here,
* subversion/libsvn_ra_svn/client.c
  (optbool_to_tristate): here, 
* subversion/libsvn_repos/authz.c
  (authz_parse_section): and here.
* subversion/libsvn_repos/fs-wrap.c
  (svn_repos__validate_prop): here as well
* subversion/libsvn_subr/cmdline.c
  (svn_cmdline_create_auth_baton): and here
* subversion/libsvn_subr/deprecated.c
  (print_command_info): here
* subversion/libsvn_subr/opt.c
  (print_command_info2): more of the same
* subversion/libsvn_subr/subst.c
  (translate_chunk): ditto
* subversion/libsvn_subr/win32_crashrpt.c
  (write_var_values,
   svn__unhandled_exception_filter): ditto
* subversion/svn/svn.c
  (sub_main): ditto
* subversion/svnsync/svnsync.c
  (filter_props): and more of the
* subversion/tests/libsvn_subr/stream-test.c
  (test_readonly): same thing
* subversion/tests/libsvn_subr/string-test.c
  (test21): ditto
* subversion/tests/libsvn_subr/subst_translate-test.c
  (test_svn_subst_translate_string2_null_encode): here
* subversion/tests/libsvn_wc/db-test.c
  (test_getting_info,
   test_working_info): and here
* tools/client-side/svn-bench/svn-bench.c
  (sub_main): and finally here

subversion/libsvn_ra_svn/client.c
@@ -1304,9 +1304,9 @@ static svn_error_t *ra_svn_get_dir(svn_ra_session_t *session,
 static svn_tristate_t
 optbool_to_tristate(apr_uint64_t v)
 {
-  if (v == TRUE)
+  if (v)
     return svn_tristate_true;
-  if (v == FALSE)
+  if (!v)
     return svn_tristate_false;
 
   return svn_tristate_unknown; /* Contains SVN_RA_SVN_UNSPECIFIED_NUMBER */

subversion/libsvn_delta/svndiff.c
@@ -203,7 +203,7 @@ send_simple_insertion_window(svn_txdelta_window_t *window,
   assert(window->ops[0].offset == 0);
 
   /* write stream header if necessary */
-  if (eb->header_done == FALSE)
+  if (!eb->header_done)
     {
       eb->header_done = TRUE;
       headers[0] = 'S';
@@ -269,7 +269,7 @@ window_handler(svn_txdelta_window_t *window, void *baton)
     return svn_error_trace(send_simple_insertion_window(window, eb));
 
   /* Make sure we write the header.  */
-  if (eb->header_done == FALSE)
+  if (!eb->header_done)
     {
       char svnver[4] = {'S','V','N','\0'};
       len = 4;

subversion/libsvn_fs_base/fs.c
@@ -1016,7 +1016,7 @@ svn_fs_base__clean_logs(const char *live_path,
                                                  sub_pool));
 
           /* If log files do not match, go to the next log file. */
-          if (files_match == FALSE)
+          if (!files_match)
             continue;
         }
 

subversion/libsvn_fs_fs/fs_fs.c
@@ -2527,7 +2527,7 @@ representation_string(representation_t *rep,
     return "-1";
 
 #define DISPLAY_MAYBE_NULL_CHECKSUM(checksum)          \
-  ((may_be_corrupt == FALSE || (checksum) != NULL)     \
+  ((!may_be_corrupt || (checksum) != NULL)     \
    ? svn_checksum_to_cstring_display((checksum), pool) \
    : "(null)")
 
@@ -4431,7 +4431,7 @@ create_rep_state_body(struct rep_state **rep_state,
   *rep_state = rs;
   *rep_args = ra;
 
-  if (ra->is_delta == FALSE)
+  if (!ra->is_delta)
     /* This is a plaintext, so just return the current rep_state. */
     return SVN_NO_ERROR;
 
@@ -4784,7 +4784,7 @@ build_rep_list(apr_array_header_t **list,
           return SVN_NO_ERROR;
         }
 
-      if (rep_args->is_delta == FALSE)
+      if (!rep_args->is_delta)
         {
           /* This is a plaintext, so just return the current rep_state. */
           *src_state = rs;

subversion/libsvn_ra_serf/locks.c
@@ -261,7 +261,7 @@ handle_lock(serf_request_t *request,
                                request, response, ctx->handler, pool));
     }
 
-  if (ctx->read_headers == FALSE)
+  if (!ctx->read_headers)
     {
       serf_bucket_t *headers;
       const char *val;

subversion/libsvn_ra_serf/property.c
@@ -526,7 +526,7 @@ create_propfind_body(serf_bucket_t **bkt,
     }
 
   /* If we're not doing an allprop, add <prop> tags. */
-  if (requested_allprop == FALSE)
+  if (!requested_allprop)
     {
       tmp = SERF_BUCKET_SIMPLE_STRING_LEN("<prop>",
                                           sizeof("<prop>")-1,
@@ -540,7 +540,7 @@ create_propfind_body(serf_bucket_t **bkt,
 
   serf_bucket_aggregate_prepend(body_bkt, tmp);
 
-  if (requested_allprop == FALSE)
+  if (!requested_allprop)
     {
       tmp = SERF_BUCKET_SIMPLE_STRING_LEN("</prop>",
                                           sizeof("</prop>")-1,

subversion/libsvn_ra_serf/update.c
@@ -935,7 +935,7 @@ cancel_fetch(serf_request_t *request,
        */
       if (fetch_ctx->read_headers)
         {
-          if (fetch_ctx->aborted_read == FALSE && fetch_ctx->read_size)
+          if (!fetch_ctx->aborted_read && fetch_ctx->read_size)
             {
               fetch_ctx->aborted_read = TRUE;
               fetch_ctx->aborted_read_size = fetch_ctx->read_size;
@@ -1089,7 +1089,7 @@ handle_fetch(serf_request_t *request,
   /* ### new field. make sure we didn't miss some initialization.  */
   SVN_ERR_ASSERT(fetch_ctx->handler != NULL);
 
-  if (fetch_ctx->read_headers == FALSE)
+  if (!fetch_ctx->read_headers)
     {
       serf_bucket_t *hdrs;
       const char *val;
@@ -2252,7 +2252,7 @@ end_report(svn_ra_serf__xml_parser_t *parser,
       info->lock_token = apr_hash_get(ctx->lock_path_tokens, info->name,
                                       APR_HASH_KEY_STRING);
 
-      if (info->lock_token && info->fetch_props == FALSE)
+      if (info->lock_token && !info->fetch_props)
         info->fetch_props = TRUE;
 
       /* If possible, we'd like to fetch only a delta against a
@@ -3066,7 +3066,7 @@ finish_report(void *report_baton,
     {
       /* Ensure that we opened and closed our root dir and that we closed
        * all of our children. */
-      if (report->closed_root == FALSE && report->root_dir != NULL)
+      if (!report->closed_root && report->root_dir != NULL)
         {
           SVN_ERR(close_all_dirs(report->root_dir));
         }

subversion/libsvn_ra_serf/util.c
@@ -1559,7 +1559,7 @@ svn_ra_serf__handle_xml_parser(serf_request_t *request,
     }
 
   /* Woo-hoo.  Nothing here to see.  */
-  if (sl.code == 404 && ctx->ignore_errors == FALSE)
+  if (sl.code == 404 && !ctx->ignore_errors)
     {
       err = handle_server_error(request, response, pool);
 

commit 32e5999b993b6c7a379e4aa8ca7e822a17571ef6

* subversion/libsvn_ra_svn/client.c   (optbool_to_tristate): Fix the logic, following breakage in r1431017.
git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1431040 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_ra_svn/client.c
@@ -1304,9 +1304,9 @@ static svn_error_t *ra_svn_get_dir(svn_ra_session_t *session,
 static svn_tristate_t
 optbool_to_tristate(apr_uint64_t v)
 {
-  if (v)
+  if (v == TRUE)  /* not just non-zero but exactly equal to 'TRUE' */
     return svn_tristate_true;
-  if (!v)
+  if (v == FALSE)
     return svn_tristate_false;
 
   return svn_tristate_unknown; /* Contains SVN_RA_SVN_UNSPECIFIED_NUMBER */