7ee060aa22507c6f095063c6d99c6e3f3c14efbe
Philip Martin <philip@apache.org>
2013-01-10 10:54:59 +0000

    Select a smaller set of candidate move pairs.
    * subversion/libsvn_wc/wc-queries.sql
      (STMT_SELECT_MOVED_PAIR3): Make op-depth a parameter.
    
    * subversion/libsvn_wc/wc_db_update_move.c
      (bump_moved_away): Bind additional parameter.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1431270 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/wc-queries.sql
+++ subversion/libsvn_wc/wc-queries.sql
@@ -1423,7 +1423,7 @@ WHERE wc_id = ?1
 SELECT local_relpath, moved_to, op_depth FROM nodes
 WHERE wc_id = ?1
   AND (local_relpath = ?2 OR IS_STRICT_DESCENDANT_OF(local_relpath, ?2))
-  AND op_depth > 0
+  AND op_depth > ?3
   AND moved_to IS NOT NULL
 
 -- STMT_HAS_LAYER_BETWEEN
