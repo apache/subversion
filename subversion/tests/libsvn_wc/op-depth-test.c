4e90d290af4b35d24fcdb72ec26182203880a957
Philip Martin <philip@apache.org>
2013-02-04 17:00:16 +0000

    Layered moved-to doesn't work yet.
    * subversion/tests/libsvn_wc/op-depth-test.c
      (layered_moved_to): Extend.
      (test_funcs): Mark layered_moved_to XFAIL.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1442177 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/libsvn_wc/op-depth-test.c
+++ subversion/tests/libsvn_wc/op-depth-test.c
@@ -6035,40 +6035,52 @@ layered_moved_to(const svn_test_opts_t *opts, apr_pool_t *pool)
   SVN_ERR(sbox_wc_mkdir(&b, "A"));
   SVN_ERR(sbox_wc_mkdir(&b, "A/B"));
   SVN_ERR(sbox_wc_mkdir(&b, "A/B/C"));
-  SVN_ERR(sbox_wc_mkdir(&b, "B"));
-  SVN_ERR(sbox_wc_mkdir(&b, "B/C"));
+  SVN_ERR(sbox_wc_mkdir(&b, "A/B/C/D"));
+  SVN_ERR(sbox_wc_mkdir(&b, "A/B/C/D/E"));
+  SVN_ERR(sbox_wc_mkdir(&b, "C"));
+  SVN_ERR(sbox_wc_mkdir(&b, "C/D"));
+  SVN_ERR(sbox_wc_mkdir(&b, "C/D/E"));
   SVN_ERR(sbox_wc_commit(&b, ""));
-  SVN_ERR(sbox_wc_propset(&b, "property", "value", "A/B/C"));
-  SVN_ERR(sbox_wc_propset(&b, "property", "value", "B/C"));
+  SVN_ERR(sbox_wc_propset(&b, "property", "value", "A/B/C/D/E"));
+  SVN_ERR(sbox_wc_propset(&b, "property", "value", "C/D/E"));
   SVN_ERR(sbox_wc_commit(&b, ""));
   SVN_ERR(sbox_wc_update(&b, "", 1));
 
   SVN_ERR(sbox_wc_move(&b, "A", "X"));
-  SVN_ERR(sbox_wc_move(&b, "X/B/C", "C2"));
-  SVN_ERR(sbox_wc_delete(&b, "X/B"));
-  SVN_ERR(sbox_wc_move(&b, "B", "X/B"));
-  SVN_ERR(sbox_wc_move(&b, "X/B/C", "C3"));
+  SVN_ERR(sbox_wc_move(&b, "X/B/C/D/E", "E2"));
+  SVN_ERR(sbox_wc_delete(&b, "X/B/C"));
+  SVN_ERR(sbox_wc_move(&b, "C", "X/B/C"));
+  SVN_ERR(sbox_wc_move(&b, "X/B/C/D/E", "E3"));
   {
     nodes_row_t nodes[] = {
-      {0, "",        "normal",       1, ""},
-      {0, "A",       "normal",       1, "A"},
-      {0, "A/B",     "normal",       1, "A/B"},
-      {0, "A/B/C",   "normal",       1, "A/B/C"},
-      {0, "B",       "normal",       1, "B"},
-      {0, "B/C",     "normal",       1, "B/C"},
-      {1, "A",       "base-deleted", NO_COPY_FROM, "X"},
-      {1, "A/B",     "base-deleted", NO_COPY_FROM},
-      {1, "A/B/C",   "base-deleted", NO_COPY_FROM},
-      {1, "B",       "base-deleted", NO_COPY_FROM, "X/B"},
-      {1, "B/C",     "base-deleted", NO_COPY_FROM},
-      {1, "X",       "normal",       1, "A", MOVED_HERE},
-      {1, "X/B",     "normal",       1, "A/B", MOVED_HERE},
-      {1, "X/B/C",   "normal",       1, "A/B/C", MOVED_HERE},
-      {2, "X/B",     "normal",       1, "B", MOVED_HERE},
-      {2, "X/B/C",   "normal",       1, "B/C", FALSE, "C2", TRUE},
-      {3, "X/B/C",   "base-deleted", NO_COPY_FROM, "C3"},
-      {1, "C2",      "normal",       1, "A/B/C", MOVED_HERE},
-      {1, "C3",      "normal",       1, "B/C", MOVED_HERE},
+      {0, "",            "normal",       1, ""},
+      {0, "A",           "normal",       1, "A"},
+      {0, "A/B",         "normal",       1, "A/B"},
+      {0, "A/B/C",       "normal",       1, "A/B/C"},
+      {0, "A/B/C/D",     "normal",       1, "A/B/C/D"},
+      {0, "A/B/C/D/E",   "normal",       1, "A/B/C/D/E"},
+      {0, "C",           "normal",       1, "C"},
+      {0, "C/D",         "normal",       1, "C/D"},
+      {0, "C/D/E",       "normal",       1, "C/D/E"},
+      {1, "A",           "base-deleted", NO_COPY_FROM, "X"},
+      {1, "A/B",         "base-deleted", NO_COPY_FROM},
+      {1, "A/B/C",       "base-deleted", NO_COPY_FROM},
+      {1, "A/B/C/D",     "base-deleted", NO_COPY_FROM},
+      {1, "A/B/C/D/E",   "base-deleted", NO_COPY_FROM},
+      {1, "C",           "base-deleted", NO_COPY_FROM, "X/B/C"},
+      {1, "C/D",         "base-deleted", NO_COPY_FROM},
+      {1, "C/D/E",       "base-deleted", NO_COPY_FROM},
+      {1, "X",           "normal",       1, "A", MOVED_HERE},
+      {1, "X/B",         "normal",       1, "A/B", MOVED_HERE},
+      {1, "X/B/C",       "normal",       1, "A/B/C", MOVED_HERE},
+      {1, "X/B/C/D",     "normal",       1, "A/B/C/D", MOVED_HERE},
+      {1, "X/B/C/D/E",   "normal",       1, "A/B/C/D/E", MOVED_HERE},
+      {3, "X/B/C",       "normal",       1, "C", MOVED_HERE},
+      {3, "X/B/C/D",     "normal",       1, "C/D", MOVED_HERE},
+      {3, "X/B/C/D/E",   "normal",       1, "C/D/E", FALSE, "E2", TRUE},
+      {5, "X/B/C/D/E",   "base-deleted", NO_COPY_FROM, "E3"},
+      {1, "E2",          "normal",       1, "A/B/C/D/E", MOVED_HERE},
+      {1, "E3",          "normal",       1, "C/D/E", MOVED_HERE},
       {0}
     };
     SVN_ERR(check_db_rows(&b, "", nodes));
@@ -6076,29 +6088,39 @@ layered_moved_to(const svn_test_opts_t *opts, apr_pool_t *pool)
 
   SVN_ERR(sbox_wc_update(&b, "A", 2));
   SVN_ERR(sbox_wc_resolve(&b, "A", svn_wc_conflict_choose_mine_conflict));
-  SVN_ERR(sbox_wc_resolve(&b, "X/B", svn_wc_conflict_choose_merged));
-  SVN_ERR(sbox_wc_resolve(&b, "X/B/C", svn_wc_conflict_choose_mine_conflict));
+  SVN_ERR(sbox_wc_resolve(&b, "X/B/C", svn_wc_conflict_choose_merged));
+  SVN_ERR(sbox_wc_resolve(&b, "X/B/C/D/E",
+                          svn_wc_conflict_choose_mine_conflict));
   {
     nodes_row_t nodes[] = {
-      {0, "",        "normal",       1, ""},
-      {0, "A",       "normal",       2, "A"},
-      {0, "A/B",     "normal",       2, "A/B"},
-      {0, "A/B/C",   "normal",       2, "A/B/C"},
-      {0, "B",       "normal",       1, "B"},
-      {0, "B/C",     "normal",       1, "B/C"},
-      {1, "A",       "base-deleted", NO_COPY_FROM, "X"},
-      {1, "A/B",     "base-deleted", NO_COPY_FROM},
-      {1, "A/B/C",   "base-deleted", NO_COPY_FROM},
-      {1, "B",       "base-deleted", NO_COPY_FROM, "X/B"},
-      {1, "B/C",     "base-deleted", NO_COPY_FROM},
-      {1, "X",       "normal",       2, "A", MOVED_HERE},
-      {1, "X/B",     "normal",       2, "A/B", MOVED_HERE},
-      {1, "X/B/C",   "normal",       2, "A/B/C", MOVED_HERE},
-      {2, "X/B",     "normal",       1, "B", MOVED_HERE},
-      {2, "X/B/C",   "normal",       1, "B/C", FALSE, "C2", TRUE},
-      {3, "X/B/C",   "base-deleted", NO_COPY_FROM, "C3"},
-      {1, "C2",      "normal",       2, "A/B/C", MOVED_HERE},
-      {1, "C3",      "normal",       1, "B/C", MOVED_HERE},
+      {0, "",            "normal",       1, ""},
+      {0, "A",           "normal",       2, "A"},
+      {0, "A/B",         "normal",       2, "A/B"},
+      {0, "A/B/C",       "normal",       2, "A/B/C"},
+      {0, "A/B/C/D",     "normal",       2, "A/B/C/D"},
+      {0, "A/B/C/D/E",   "normal",       2, "A/B/C/D/E"},
+      {0, "C",           "normal",       1, "C"},
+      {0, "C/D",         "normal",       1, "C/D"},
+      {0, "C/D/E",       "normal",       1, "C/D/E"},
+      {1, "A",           "base-deleted", NO_COPY_FROM, "X"},
+      {1, "A/B",         "base-deleted", NO_COPY_FROM},
+      {1, "A/B/C",       "base-deleted", NO_COPY_FROM},
+      {1, "A/B/C/D",     "base-deleted", NO_COPY_FROM},
+      {1, "A/B/C/D/E",   "base-deleted", NO_COPY_FROM},
+      {1, "C",           "base-deleted", NO_COPY_FROM, "X/B/C"},
+      {1, "C/D",         "base-deleted", NO_COPY_FROM},
+      {1, "C/D/E",       "base-deleted", NO_COPY_FROM},
+      {1, "X",           "normal",       2, "A", MOVED_HERE},
+      {1, "X/B",         "normal",       2, "A/B", MOVED_HERE},
+      {1, "X/B/C",       "normal",       2, "A/B/C", MOVED_HERE},
+      {1, "X/B/C/D",     "normal",       2, "A/B/C/D", MOVED_HERE},
+      {1, "X/B/C/D/E",   "normal",       2, "A/B/C/D/E", MOVED_HERE},
+      {3, "X/B/C",       "normal",       1, "C", MOVED_HERE},
+      {3, "X/B/C/D",     "normal",       1, "C/D", MOVED_HERE},
+      {3, "X/B/C/D/E",   "normal",       1, "C/D/E", FALSE, "E2", TRUE},
+      {5, "X/B/C/D/E",   "base-deleted", NO_COPY_FROM, "E3"},
+      {1, "E2",          "normal",       2, "A/B/C/D/E", MOVED_HERE},
+      {1, "E3",          "normal",       1, "C/D/E", MOVED_HERE},
       {0}
     };
     SVN_ERR(check_db_rows(&b, "", nodes));
@@ -6223,7 +6245,7 @@ struct svn_test_descriptor_t test_funcs[] =
                        "switch_move"),
     SVN_TEST_OPTS_PASS(move_replace,
                        "move_replace"),
-    SVN_TEST_OPTS_PASS(layered_moved_to,
+    SVN_TEST_OPTS_XFAIL(layered_moved_to,
                        "layered_moved_to"),
     SVN_TEST_NULL
   };
