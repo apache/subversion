cad64239d30dba8bebd9f9574ebf43e06802f007
Ivan Zhakov <ivan@apache.org>
2013-02-05 11:55:17 +0000

    Do not send empty text-delta for equal files in update reporter.
    * subversion/libsvn_repos/reporter.c
      (delta_files): Always use svn_repos__compare_files() function to check 
       that file content is changed regardless of IGNORE_ANCESTRY flag. 
       IGNORE_ANCESTRY flag is already handled on caller side in update_entry() 
       function.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1442555 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_repos/reporter.c
+++ subversion/libsvn_repos/reporter.c
@@ -687,18 +687,15 @@ delta_files(report_baton_t *b, void *file_baton, svn_revnum_t s_rev,
     {
       SVN_ERR(get_source_root(b, &s_root, s_rev));
 
-      /* Is this delta calculation worth our time?  If we are ignoring
-         ancestry, then our editor implementor isn't concerned by the
-         theoretical differences between "has contents which have not
-         changed with respect to" and "has the same actual contents
-         as".  We'll do everything we can to avoid transmitting even
-         an empty text-delta in that case.  */
-      if (b->ignore_ancestry)
-        SVN_ERR(svn_repos__compare_files(&changed, b->t_root, t_path,
-                                         s_root, s_path, pool));
-      else
-        SVN_ERR(svn_fs_contents_changed(&changed, b->t_root, t_path, s_root,
-                                        s_path, pool));
+      /* Is this delta calculation worth our time? We'll do everything we can
+         to avoid transmitting even an empty text-delta in that case.
+         It doesn't matter if we are ignoring ancestry or not: we're not
+         interested in theoretical differences between "has contents which
+         have not changed with respect to" and "has the same actual contents
+         as" when sending text-delta. */
+      SVN_ERR(svn_repos__compare_files(&changed, b->t_root, t_path,
+                                       s_root, s_path, pool));
+
       if (!changed)
         return SVN_NO_ERROR;
 
