9dca0acff260ab4c5e3089e64eae3438438d8d9e
Ben Reser <breser@apache.org>
2013-02-13 16:21:35 +0000

    Cleaner solution to the problem fixed in r1445267.
    * subversion/bindings/swig/perl/libsvn_swig_perl/swigutil_pl.c
      (svn_swig_pl_set_revision): Return a svn_opt_revision_t instead of using a
        pointer to a pointer.
    
    * subversion/bindings/swig/perl/libsvn_swig_perl/swigutil_pl.h
      (svn_swig_pl_set_revision): Update declaration.
    
    * subversion/bindings/swig/include/svn_types.swg
      (svn_opt_revision_t): Update typemap to match changes to
        svn_swig_pl_set_revision().
    
    Patch by: Roderich Schupp <roderich.schupp{_AT_}gmail.com>
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1445705 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/swig/perl/libsvn_swig_perl/swigutil_pl.h
+++ subversion/bindings/swig/perl/libsvn_swig_perl/swigutil_pl.h
@@ -106,7 +106,7 @@ SV *svn_swig_pl_convert_array(const apr_array_header_t *array,
 
 SV *svn_swig_pl_revnums_to_list(const apr_array_header_t *array);
 
-void svn_swig_pl_set_revision(svn_opt_revision_t **rev, SV *source);
+svn_opt_revision_t *svn_swig_pl_set_revision(svn_opt_revision_t *rev, SV *source);
 
 /* thunked log receiver function.  */
 svn_error_t * svn_swig_pl_thunk_log_receiver(void *py_receiver,
