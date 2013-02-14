73b70458eeb3118f9e4bf077808e1034c2ab9c6a
Philip Martin <philip@apache.org>
2013-02-14 00:12:04 +0000

    * subversion/libsvn_wc/wc_db_update_move.c (bump_moved_away): Tweak comment.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1445996 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/wc_db_update_move.c
+++ subversion/libsvn_wc/wc_db_update_move.c
@@ -2101,7 +2101,9 @@ bump_moved_away(svn_wc__db_wcroot_t *wcroot,
             err = depth_sufficient_to_bump(&can_bump, src_relpath, wcroot,
                                            src_depth, scratch_pool);
           else
-            can_bump = TRUE; /* ### OK? With FALSE many op-depth tests fail */
+            /* Having chosen to bump an entire BASE tree move we
+               always have sufficient depth to bump subtree moves. */
+            can_bump = TRUE;
 
           if (!err)
             {
