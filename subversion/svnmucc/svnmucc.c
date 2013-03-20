059f7296eb35d6151580b615afbfb88d69b7621d
Philip Martin <philip@apache.org>
2013-03-20 18:14:10 +0000

    * subversion/svnmucc/svnmucc.c (usage): Typo in help text.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1458988 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svnmucc/svnmucc.c
+++ subversion/svnmucc/svnmucc.c
@@ -950,7 +950,7 @@ usage(apr_pool_t *pool, int exit_val)
       "  -r [--revision] ARG    : use revision ARG as baseline for changes\n"
       "  --with-revprop ARG     : set revision property in the following format:\n"
       "                               NAME[=VALUE]\n"
-      "  --non-interactive      : do no interactive prompting (default it to\n"
+      "  --non-interactive      : do no interactive prompting (default is to\n"
       "                           prompt only if standard input is a terminal)\n"
       "  --force-interactive    : do interactive propmting even if standard\n"
       "                           input is not a terminal\n"
