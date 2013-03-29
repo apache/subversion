28d8e095744a44c0476ffda963b229c3ac437a52
Ben Reser <breser@apache.org>
2013-03-29 04:34:10 +0000

    Bump 1.7.x branch to 1.7.10.
    * STATUS,
      subversion/include/svn_version.h: Update to 1.7.10
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/1.7.x@1462397 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_version.h
+++ subversion/include/svn_version.h
@@ -71,7 +71,7 @@ extern "C" {
  *
  * @since New in 1.1.
  */
-#define SVN_VER_PATCH      9
+#define SVN_VER_PATCH      10
 
 
 /** @deprecated Provided for backward compatibility with the 1.0 API. */
