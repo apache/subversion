0036e264c73f53963d027558e8013f5f09fb5c2e
Philip Martin <philip@apache.org>
2013-04-11 10:35:34 +0000

    * subversion/libsvn_subr/subst.c   (svn_subst_build_keywords): Move deprecated function ...
    * subversion/libsvn_subr/deprecated.c
      (svn_subst_build_keywords): ... here.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1466830 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/deprecated.c
+++ subversion/libsvn_subr/deprecated.c
@@ -1234,3 +1234,48 @@ svn_utf_initialize(apr_pool_t *pool)
 {
   svn_utf_initialize2(pool, FALSE);
 }
+
+svn_error_t *
+svn_subst_build_keywords(svn_subst_keywords_t *kw,
+                         const char *keywords_val,
+                         const char *rev,
+                         const char *url,
+                         apr_time_t date,
+                         const char *author,
+                         apr_pool_t *pool)
+{
+  apr_hash_t *kwhash;
+  const svn_string_t *val;
+
+  SVN_ERR(svn_subst_build_keywords2(&kwhash, keywords_val, rev,
+                                    url, date, author, pool));
+
+  /* The behaviour of pre-1.3 svn_subst_build_keywords, which we are
+   * replicating here, is to write to a slot in the svn_subst_keywords_t
+   * only if the relevant keyword was present in keywords_val, otherwise
+   * leaving that slot untouched. */
+
+  val = svn_hash_gets(kwhash, SVN_KEYWORD_REVISION_LONG);
+  if (val)
+    kw->revision = val;
+
+  val = svn_hash_gets(kwhash, SVN_KEYWORD_DATE_LONG);
+  if (val)
+    kw->date = val;
+
+  val = svn_hash_gets(kwhash, SVN_KEYWORD_AUTHOR_LONG);
+  if (val)
+    kw->author = val;
+
+  val = svn_hash_gets(kwhash, SVN_KEYWORD_URL_LONG);
+  if (val)
+    kw->url = val;
+
+  val = svn_hash_gets(kwhash, SVN_KEYWORD_ID);
+  if (val)
+    kw->id = val;
+
+  return SVN_NO_ERROR;
+}
+
+
--- subversion/libsvn_subr/subst.c
+++ subversion/libsvn_subr/subst.c
@@ -276,49 +276,6 @@ keyword_printf(const char *fmt,
   return svn_stringbuf__morph_into_string(value);
 }
 
-svn_error_t *
-svn_subst_build_keywords(svn_subst_keywords_t *kw,
-                         const char *keywords_val,
-                         const char *rev,
-                         const char *url,
-                         apr_time_t date,
-                         const char *author,
-                         apr_pool_t *pool)
-{
-  apr_hash_t *kwhash;
-  const svn_string_t *val;
-
-  SVN_ERR(svn_subst_build_keywords2(&kwhash, keywords_val, rev,
-                                    url, date, author, pool));
-
-  /* The behaviour of pre-1.3 svn_subst_build_keywords, which we are
-   * replicating here, is to write to a slot in the svn_subst_keywords_t
-   * only if the relevant keyword was present in keywords_val, otherwise
-   * leaving that slot untouched. */
-
-  val = svn_hash_gets(kwhash, SVN_KEYWORD_REVISION_LONG);
-  if (val)
-    kw->revision = val;
-
-  val = svn_hash_gets(kwhash, SVN_KEYWORD_DATE_LONG);
-  if (val)
-    kw->date = val;
-
-  val = svn_hash_gets(kwhash, SVN_KEYWORD_AUTHOR_LONG);
-  if (val)
-    kw->author = val;
-
-  val = svn_hash_gets(kwhash, SVN_KEYWORD_URL_LONG);
-  if (val)
-    kw->url = val;
-
-  val = svn_hash_gets(kwhash, SVN_KEYWORD_ID);
-  if (val)
-    kw->id = val;
-
-  return SVN_NO_ERROR;
-}
-
 static svn_error_t *
 build_keywords(apr_hash_t **kw,
                svn_boolean_t expand_custom_keywords,
