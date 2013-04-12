226d6b9c6d316fda053eebf9d651ac903c617e8c
Philip Martin <philip@apache.org>
2013-04-12 10:58:49 +0000

    Fix issue 4350, long keyword expansion.
    * subversion/libsvn_subr/subst.c
      (translate_keyword_subst): Don't expand overlong keywords.
    
    * subversion/tests/libsvn_subr/subst_translate-test.c
      (test_funcs): Mark test_svn_subst_long_keywords PASS.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1467237 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/libsvn_subr/subst_translate-test.c
+++ subversion/tests/libsvn_subr/subst_translate-test.c
@@ -498,7 +498,7 @@ struct svn_test_descriptor_t test_funcs[] =
                    "test svn_subst_build_keywords3()"),
     SVN_TEST_PASS2(test_svn_subst_truncated_keywords,
                    "test truncated keywords (issue 4349)"),
-    SVN_TEST_XFAIL2(test_svn_subst_long_keywords,
+    SVN_TEST_PASS2(test_svn_subst_long_keywords,
                    "test long keywords (issue 4350)"),
     SVN_TEST_NULL
   };
