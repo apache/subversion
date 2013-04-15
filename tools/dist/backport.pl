6dfc9d156560b9ff4d5411fb123a9c9219d064ed
Daniel Shahaf <danielsh@apache.org>
2013-04-15 17:27:16 +0000

    * tools/dist/backport.pl   (merge): Fix a Vim regexp.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1468162 13f79535-47bb-0310-9956-ffa450edef68

--- tools/dist/backport.pl
+++ tools/dist/backport.pl
@@ -78,7 +78,7 @@ sub merge {
 
   if ($entry{branch}) {
     # NOTE: This doesn't escape the branch into the pattern.
-    $pattern = sprintf '\V\(%s branch(es)?\|branches\/%s\|Branch(es)?:\n *%s\)', $entry{branch}, $entry{branch}, $entry{branch};
+    $pattern = sprintf '\V\(%s branch(es)?\|branches\/%s\|Branch\(es\)\?: \*\n\? \*%s\)', $entry{branch}, $entry{branch}, $entry{branch};
     $mergeargs = "--reintegrate $BRANCHES/$entry{branch}";
     print $logmsg_fh "Reintegrate the $entry{header}:";
     print $logmsg_fh "";
