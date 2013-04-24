apache/subversion

commit be66de8c672ffc942ad20bce60aa8425125c879b

* tools/dist/backport.pl   (handle_entry): Don't offer a subshell if no merge was done.
git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1471525 13f79535-47bb-0310-9956-ffa450edef68

tools/dist/backport.pl
@@ -238,12 +238,13 @@ sub handle_entry {
     print "";
     print "Vetoes found!" if @vetoes;
 
-    merge %entry if prompt 'Go ahead?';
-    system($ENV{SHELL} // "/bin/sh") == 0
-      or warn "Creating an interactive subshell failed ($?): $!"
-      if prompt "Shall I open a subshell?";
-    # Don't revert.  The next merge() call will do that anyway, or maybe the
-    # user did in his interactive shell.
+    if (prompt 'Go ahead?') {
+      system($ENV{SHELL} // "/bin/sh") == 0
+        or warn "Creating an interactive subshell failed ($?): $!"
+        if prompt "Shall I open a subshell?";
+      # Don't revert.  The next merge() call will do that anyway, or maybe the
+      # user did in his interactive shell.
+    }
   }
 
   # TODO: merge() changes ./STATUS, which we're reading below, but

commit 5cc232ae6706c3c7928f2e364fbe5d85db45779f

* tools/dist/backport.pl   (handle_entry): Fix a bug introduced in r1471525 for interactive mode.
git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1471536 13f79535-47bb-0310-9956-ffa450edef68

tools/dist/backport.pl
@@ -239,6 +239,7 @@ sub handle_entry {
     print "Vetoes found!" if @vetoes;
 
     if (prompt 'Go ahead?') {
+      merge %entry;
       system($ENV{SHELL} // "/bin/sh") == 0
         or warn "Creating an interactive subshell failed ($?): $!"
         if prompt "Shall I open a subshell?";