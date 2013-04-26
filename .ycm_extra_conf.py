738bb4f310faddf2e5c7471223e9fc5db4584a35
Ben Reser <breser@apache.org>
2013-04-26 22:07:04 +0000

    Add some comments to the YouCompleteMe changes.
    * .ycm_extra_conf.py: Add header mentioning the source and copyright.
    
    * Makefile.in: Comment explaining purpose of compile-commands target.
    
    Suggested by: julianf
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1476416 13f79535-47bb-0310-9956-ffa450edef68

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,3 +1,23 @@
+# Configuration file for YouCompleteMe vim plugin to allow the plugin
+# to determine the compile flags.  This file is based on:
+# https://github.com/Valloric/YouCompleteMe/blob/master/cpp/ycm/.ycm_extra_conf.py
+#
+# YouCompleteMe's Copyright:
+# Copyright (C) 2011, 2012  Strahinja Val Markovic  <val@markovic.io>
+#
+# YouCompleteMe is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import ycm_core
 from clang_helpers import PrepareClangFlags
