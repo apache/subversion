87417edfa286b7b37c633751532b40266b4dc4cc
svn-role <dev-null@apache.org>
2013-04-30 04:01:16 +0000

    Merge the r1470904 group from trunk:
     * r1470904, r1471744, r1475963, r1476155, r1476181, r1476254
       Tweak an upgrade Sql statement to be easier to understand and a tiny bit
       faster
       Justification:
         The old variant had some theoretical upgrade problems.
       Notes:
         r1470904, r1471744, r1475963 are a series of tweaks to the
         STMT_UPGRADE_31_SELECT_WCROOT_NODES query.  r1476155 is a new
         upgrade test.  r1476181, r1476254 are for 1.6 upgrades.
       Votes:
         +1: philip, pburba, rhuijben
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/1.8.x@1477465 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/wc-metadata.sql
+++ subversion/libsvn_wc/wc-metadata.sql
@@ -840,17 +840,13 @@ PRAGMA user_version = 31;
 SELECT l.wc_id, l.local_relpath FROM nodes as l
 LEFT OUTER JOIN nodes as r
 ON l.wc_id = r.wc_id
-   AND l.repos_id = r.repos_id
    AND r.local_relpath = l.parent_relpath
-WHERE (l.local_relpath = '' AND l.repos_path != '')
-   OR (l.op_depth = 0
-       AND l.local_relpath != ''
-       AND l.repos_path != ltrim(r.repos_path
-                                 || '/'
-                                 || ltrim(substr(l.local_relpath,
-                                                 length(l.parent_relpath) + 1),
-                                          '/'),
-                                 '/'))
+   AND r.op_depth = 0
+WHERE l.op_depth = 0
+  AND l.repos_path != ''
+  AND ((l.repos_id IS NOT r.repos_id)
+       OR (l.repos_path IS NOT RELPATH_SKIP_JOIN(r.local_relpath, r.repos_path, l.local_relpath)))
+
 
 /* ------------------------------------------------------------------------- */
 /* Format 32 ....  */
