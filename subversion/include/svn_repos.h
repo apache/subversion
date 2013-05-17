b80932004f1af022250cbc92e24ef962e2848f1d
Ben Reser <breser@apache.org>
2013-05-17 20:27:27 +0000

    * subversion/include/svn_repos.h   (svn_repos_load_uuid): Add documentation for the enum values.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1483977 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_repos.h
+++ subversion/include/svn_repos.h
@@ -69,8 +69,11 @@ enum svn_node_action
 /** The different policies for processing the UUID in the dumpfile. */
 enum svn_repos_load_uuid
 {
+  /** only update uuid if the repos has no revisions. */
   svn_repos_load_uuid_default,
+  /** never update uuid. */
   svn_repos_load_uuid_ignore,
+  /** always update uuid. */
   svn_repos_load_uuid_force
 };
 
