c13379710d9422b18201543def3352d63bbc9a67
Ben Reser <breser@apache.org>
2013-05-17 22:28:05 +0000

    * subversion/include/mod_authz_svn.h   (AUTHZ_SVN__SUBREQ_BYPASS_PROV_GRP, AUTHZ_SVN__SUBREQ_BYPASS_PROV_NAME,    AUTHZ_SVN__SUBREQ_BYPASS_PROV_VER, authz_svn__subreq_bypass_func_t):     Provide documentation.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1484016 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/mod_authz_svn.h
+++ subversion/include/mod_authz_svn.h
@@ -36,9 +36,20 @@ extern "C" {
 /*
  * mod_dav_svn to mod_authz_svn bypass mechanism
  */
+/** Provider group for subrequest bypass */
 #define AUTHZ_SVN__SUBREQ_BYPASS_PROV_GRP "dav2authz_subreq_bypass"
+/** Provider name for subrequest bypass */
 #define AUTHZ_SVN__SUBREQ_BYPASS_PROV_NAME "mod_authz_svn_subreq_bypass"
+/** Provider version for subrequest bypass */
 #define AUTHZ_SVN__SUBREQ_BYPASS_PROV_VER "00.00a"
+/** Provider to allow mod_dav_svn to bypass the generation of an apache
+ * request when checking GET access from "mod_dav_svn/auth.c".
+ *
+ * Uses @a r @a repos_path and @a repos_name to determine if the user
+ * making the request is authorized.
+ *
+ * If the access is allowed returns @c OK or @c HTTP_FORBIDDEN if it is not.
+ */
 typedef int (*authz_svn__subreq_bypass_func_t)(request_rec *r,
                                               const char *repos_path,
                                               const char *repos_name);
