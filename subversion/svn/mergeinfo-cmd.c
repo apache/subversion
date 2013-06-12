e9cfc266f5cfa76e703e35524eb0b4b95fe2e0da
Stefan Sperling <stsp@apache.org>
2013-06-12 12:27:05 +0000

    * subversion/svn/mergeinfo-cmd.c   (svn_cl__mergeinfo): Error out if --log option is given but --show-revs is     not given, as already done for other options, such as --depth.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1492172 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svn/mergeinfo-cmd.c
+++ subversion/svn/mergeinfo-cmd.c
@@ -439,6 +439,11 @@ svn_cl__mergeinfo(apr_getopt_t *os,
         return svn_error_create(SVN_ERR_CL_ARG_PARSING_ERROR, NULL,
                                 _("Depth specification options valid only "
                                   "with --show-revs option"));
+      if (opt_state->mergeinfo_log)
+        return svn_error_create(SVN_ERR_CL_ARG_PARSING_ERROR, NULL,
+                                _("--log option valid only with "
+                                  "--show-revs option"));
+
 
       SVN_ERR(mergeinfo_summary(source, &src_peg_revision,
                                 target, &tgt_peg_revision,
