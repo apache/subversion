e850a0d739fd185066ef92bf080da3ea4b35b480
Johan Corveleyn <jcorvel@apache.org>
2013-06-14 15:21:58 +0000

    * subversion/tests/cmdline/blame_tests.py   (blame_youngest_to_oldest): Extend blame range to -r3:0, to make the test    hit a revision before the target existed. Marking XFail.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1493111 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/blame_tests.py
+++ subversion/tests/cmdline/blame_tests.py
@@ -957,6 +957,7 @@ def blame_eol_handling(sbox):
 
 
 @SkipUnless(svntest.main.server_has_reverse_get_file_revs)
+@XFail()
 def blame_youngest_to_oldest(sbox):
   "blame_youngest_to_oldest"
 
@@ -977,7 +978,7 @@ def blame_youngest_to_oldest(sbox):
         '     %d    jrandom %s\n' % (2, orig_line[:-1]),
   ]
   svntest.actions.run_and_verify_svn(None, expected_output, [],
-                                     'blame', '-r3:1', iota)
+                                     'blame', '-r3:0', iota)
 
 ########################################################################
 # Run the tests
