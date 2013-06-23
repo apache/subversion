07521a2acaa13439c232cf3eadd8c1a57da95351
Lieven Govaerts <lgo@apache.org>
2013-06-23 17:14:58 +0000

    Fix a crash in TSVN when receiving a property update on a directory with local property modifications. (See: http://svn.haxx.se/dev/archive-2013-06/0543.shtml)
    * subversion/libsvn_wc/update_editor.c
      (close_directory): Use eb->cancel_baton instead of eb->conflict_baton for the
          cancel_baton parameter in the call to svn_wc__conflict_invoke_resolver.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1495850 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/update_editor.c
+++ subversion/libsvn_wc/update_editor.c
@@ -2928,7 +2928,7 @@ close_directory(void *dir_baton,
                                              eb->conflict_func,
                                              eb->conflict_baton,
                                              eb->cancel_func,
-                                             eb->conflict_baton,
+                                             eb->cancel_baton,
                                              scratch_pool));
 
   /* Notify of any prop changes on this directory -- but do nothing if
