6f4b6fe158db40e0d6c92008e8a72a0921180c74
Branko Čibej <brane@apache.org>
2013-07-02 16:58:46 +0000

    Remove the Ev2 rotate interface from JavaHL until its fate has been decided.
    [in subversion/bindings/javahl/native]
    * CommitEditor.h, CommitEditor.cpp (CommitEditor::rotate): Put in comment.
    * org_apache_subversion_javahl_remote_CommitEditor.cpp
      (Java_org_apache_subversion_javahl_remote_CommitEditor_rotate): Likewise.
    
    [in subversion/bindings/javahl/src/org/apache/subversion/javahl]
    * ISVNEditor.java, remote/CommitEditor.java (ISVNEditor.rotate): And here.
    
    [in subversion/bindings/javahl/tests/org/apache/subversion/javahl]
    * SVNRemoteTests.java (SVNRemoteTests.testEditorNotImplemented):
       Coment out the bits related to ISVNEditor.rotate.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1499016 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/javahl/native/CommitEditor.h
+++ subversion/bindings/javahl/native/CommitEditor.h
@@ -81,7 +81,7 @@ public:
             jstring jdst_relpath, jlong jreplaces_revision);
   void move(jstring jsrc_relpath, jlong jsrc_revision,
             jstring jdst_relpath, jlong jreplaces_revision);
-  void rotate(jobject jelements);
+  // void rotate(jobject jelements);
   void complete();
   void abort();
 
