905528fc2584e8f1b5a06d1dd26b3fc78e16d99f
Bert Huijben <rhuijben@apache.org>
2013-07-03 16:48:23 +0000

    * subversion/svn/svn.c   (sub_main): Also flush stdout when returning an error, for the same reason     as why we flush on success.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1499470 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svn/svn.c
+++ subversion/svn/svn.c
@@ -2990,6 +2990,10 @@ sub_main(int argc, const char *argv[], apr_pool_t *pool)
                          "Subversion"));
         }
 
+      /* Ensure that stdout is flushed, so the user will see any write errors.
+         This makes sure that output is not silently lost. */
+      SVN_INT_ERR(svn_cmdline_fflush(stdout));
+
       return EXIT_ERROR(err);
     }
   else
