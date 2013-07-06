628cb4a55bb41b2fd7a1a9f79f427cad8972a047
Branko Cibej <brane@apache.org>
2013-07-06 13:17:59 +0000

    Fixed the JavaHL RA implementation, infrastructure and tests so that they all pass via ra_serf and ra_svn (on a local connection with trunk servers).
    [in subversion/bindings/javahl/native]
    * JNIUtil.cpp (JNIUtil::makeJByteArray): Check for null svn_string_t.
    
    * CommitEditor.h
      (CommitEditor::provide_base_cb, CommitEditor::provide_props_cb): New methods.
    * CommitEditor.cpp (open_callback_session): Extracted code for opening the
       secondary session that suuports the Ev2 commit editor callbakcs from
       CommitEditor::get_copysrc_kind_cb.
      (CommitEditor::provide_base_cb, CommitEditor::provide_props_cb): Implement.
      (CommitEditor::CommitEditor): Use the new callbacks.
    
    [in subversion/bindings/javahl/tests/org/apache/subversion/javahl]
    * BasicTests.java (BasicTests.CountingProgressListener): New private class;
       hoisted from BasicTests.testDataTransferProgressReport to avoid
       throwing exceptions through native callbacks, which clutter up the
       output during the test run.
      (BasicTests.testDataTransferProgressReport): Use the new callback class.
    
    * SVNRemoteTests.java
      (SVNRemoteTests.testChangeRevpropAtomic): Check list of allowed error codes.
      (SVNRemoteTests.testGetFile, SVNRemoteTests.testGetDirectory): Expect wcprops.
      (SVNRemoteTests.testEditorSetFileContents): New test case; covers a code path
       that had not been tested before.
      (SVNRemoteTests.testEditorSetFileProps): Replaced by testEditorSetFileContents.
      (SVNRemoteTests.RemoteStatusReceiver): Make StatInfo array sortable.
      (SVNRemoteTests.testPropchangeStatus,
       SVNRemoteTests.testDeletedStatus): Sort status entries before checking
       results because ra_serf sends them in the opposite order than ra_local.
    * SVNTests.java (SVNTests.prompt): Return true; we do have credentials info.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1500255 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/javahl/tests/org/apache/subversion/javahl/SVNTests.java
+++ subversion/bindings/javahl/tests/org/apache/subversion/javahl/SVNTests.java
@@ -324,12 +324,12 @@ class SVNTests extends TestCase
 
         public boolean prompt(String realm, String username)
         {
-            return false;
+            return true;
         }
 
         public boolean prompt(String realm, String username, boolean maySave)
         {
-            return false;
+            return true;
         }
 
         public String askQuestion(String realm, String question,
