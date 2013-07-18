63301babea9a279412d42ff9bad57f33561ab7b1
Daniel Shahaf <danielsh@apache.org>
2013-07-18 06:17:26 +0000

    backport.pl conflicts mode: diff the conflicted files, to allow seeing the reason for the conflict in the bot output.
    * tools/dist/backport.pl
      (handle_entry): Print a diff of files in @conflicts.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1504372 13f79535-47bb-0310-9956-ffa450edef68

--- tools/dist/backport.pl
+++ tools/dist/backport.pl
@@ -541,13 +541,14 @@ sub handle_entry {
         merge %entry;
 
         my $output = `$SVN status`;
-        my (@conflicts) = ($output =~ m#^(?:C|.C|...C).*/(.*)#mg);
+        my (@conflicts) = ($output =~ m#^(?:C...|.C..|...C)...\s(.*)#mg);
         if (@conflicts and !$entry{depends}) {
           $ERRORS{$entry{id}} //= "Conflicts merging the $entry{header}: "
-                                  . (join ', ', @conflicts);
+                                  . (join ', ', map m#.*/(.*)#, @conflicts);
           say STDERR "Conflicts merging the $entry{header}!";
           say STDERR "";
           say STDERR $output;
+          system "$SVN diff -- @conflicts";
         } elsif (!@conflicts and $entry{depends}) {
           # Not a warning since svn-role may commit the dependency without
           # also committing the dependent in hte same pass.
