be429a6ff732486d688df3c98f76d459ac32f67b
Bert Huijben <rhuijben@apache.org>
2013-07-21 18:49:44 +0000

    * subversion/bindings/swig/ruby/libsvn_swig_ruby/swigutil_rb__pre_ruby.h   (HAVE_STDINT_H): Add enough replacements from <stdint.h> to make the     ruby 1.9 build on VS2008 happy.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1505443 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/swig/ruby/libsvn_swig_ruby/swigutil_rb__pre_ruby.h
+++ subversion/bindings/swig/ruby/libsvn_swig_ruby/swigutil_rb__pre_ruby.h
@@ -71,6 +71,15 @@ typedef long ssize_t;
 /* Visual C++ >= 2010 has <stdint.h> */
 #if _MSC_VER < 1600
 #undef HAVE_STDINT_H
+
+typedef signed __int8      int8_t;
+typedef signed __int16     int16_t;
+typedef signed __int32     int32_t;
+typedef signed __int64     int64_t;
+typedef unsigned __int8    uint8_t;
+typedef unsigned __int16   uint16_t;
+typedef unsigned __int32   uint32_t;
+typedef unsigned __int64   uint64_t;
 #endif
 
 /* Visual C++ >= 2013 has <inttypes.h> */
