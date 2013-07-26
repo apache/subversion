97f793fe31bae25ef1a44b56114489bfaa605de6
Branko Cibej <brane@apache.org>
2013-07-26 08:42:08 +0000

    Introduce a pool wrapper for the iteration pool idiom in the C++HL APR wrapper.
    * subversion/bindings/cxxhl/src/aprwrap/pool.hpp
      (IterationPool): New class; a pool wrapper that hides its API.
      (Pool::Pool): Make non-default constructor explicit and accept a non-const
       reference to the parent pool instead of a const pointer.
      (Pool::Iteration): New nested class; a proxy for the pool in IterationPool.
    * subversion/bindings/cxxhl/src/exception.cpp
      (Error::compile_messages): Use IterationPool and Pool::Iteration in the loop.
    * subversion/bindings/cxxhl/tests/test_aprwrap.cpp
      (CreateSubpool): Adapt to changed pool constructor signature.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1507217 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/cxxhl/src/aprwrap/pool.hpp
+++ subversion/bindings/cxxhl/src/aprwrap/pool.hpp
@@ -37,6 +37,9 @@ namespace subversion {
 namespace cxxhl {
 namespace apr {
 
+// Forward declaration
+class IterationPool;
+
 /**
  * Encapsulates an APR pool.
  */
@@ -53,24 +56,33 @@ public:
   /**
    * Create a pool as a child of @a parent.
    */
-  Pool(const Pool* parent) throw()
-    : m_pool(svn_pool_create(parent->m_pool))
+  explicit Pool(Pool& parent) throw()
+    : m_pool(svn_pool_create(parent.m_pool))
     {}
 
   /**
    * Destroy the pool.
    */
-  ~Pool() throw() { svn_pool_destroy(m_pool); }
+  ~Pool() throw()
+    {
+      svn_pool_destroy(m_pool);
+    }
 
   /**
    * Clear the pool.
    */
-  void clear() throw() { apr_pool_clear(m_pool); }
+  void clear() throw()
+    {
+      apr_pool_clear(m_pool);
+    }
 
   /**
    * Retuurn a pool pointer that can be used by the C APIs.
    */
-  apr_pool_t* get() const throw() { return m_pool; }
+  apr_pool_t* get() const throw()
+    {
+      return m_pool;
+    }
 
   /**
    * Allocate space for @a count elements of type @a T from the pool.
@@ -95,7 +107,87 @@ public:
 private:
   static apr_pool_t* get_root_pool();
   apr_pool_t* const m_pool;
+
+public:
+  /**
+   * Pool proxy used for iteration scratch pools.
+   *
+   * Construct this object inside a loop body in order to clear the
+   * proxied pool on every iteration.
+   */
+  class Iteration
+  {
+  public:
+    /**
+     * The constructor clears the proxied pool.
+     */
+    explicit Iteration(IterationPool& iterbase) throw();
+
+    /**
+     * Returns a reference to the proxied pool.
+     */
+    Pool& pool() const throw()
+      {
+        return m_pool;
+      }
+
+    /**
+     * Proxy method for Pool::get
+     */
+    apr_pool_t* get() const throw()
+      {
+        return m_pool.get();
+      }
+
+    /**
+     * Proxy method for Pool::alloc
+     */
+    template<typename T>
+    T* alloc(std::size_t count) throw()
+      {
+        return m_pool.alloc<T>(count);
+      }
+
+    /**
+     * Proxy method for Pool::allocz
+     */
+    template<typename T>
+    T* allocz(std::size_t count) throw()
+      {
+        return m_pool.allocz<T>(count);
+      }
+
+  private:
+    Pool& m_pool;
+  };
 };
+
+/**
+ * Pool wrapper that hides the pool implementation, except for construction.
+ *
+ * Construct this object outside a loop body, then within the body,
+ * use Pool::Iteration to access the wrapped pool.
+ */
+class IterationPool
+{
+public:
+  IterationPool() {}
+
+  explicit IterationPool(Pool& parent) throw()
+    : m_pool(parent)
+    {}
+
+private:
+  friend class Pool::Iteration;
+  Pool m_pool;
+};
+
+// Pool::Iteration constructor implementation
+inline Pool::Iteration::Iteration(IterationPool& iterbase) throw()
+  : m_pool(iterbase.m_pool)
+{
+  m_pool.clear();
+}
 
 } // namespace apr
 } // namespace cxxhl
