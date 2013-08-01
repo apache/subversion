c0256a213766e2a96b7e4850ffaea85428048ee6
Daniel Shahaf <danielsh@apache.org>
2013-08-01 12:15:02 +0000

    Follow-up to r1509186:
    * subversion/include/svn_config.h
      (svn_config_walk_auth_data): Note the birth date of the new behaviour.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1509196 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_config.h
+++ subversion/include/svn_config.h
@@ -740,6 +740,8 @@ typedef svn_error_t *
  * config directory location. If it is NULL, the default location
  * is used.
  *
+ * @note @a config_dir may only be NULL in 1.8.2 and later.
+ *
  * @note Removing credentials from the config-based disk store will
  * not purge them from any open svn_auth_baton_t instance.  Consider
  * using svn_auth_forget_credentials() -- from the @a cleanup_func,
