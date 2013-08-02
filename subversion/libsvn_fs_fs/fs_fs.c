35a9917ef6788a97a225a9830ba059e6bc94826b
Stefan Fuhrmann <stefan2@apache.org>
2013-08-02 11:13:41 +0000

    On the log-addressing branch:  Follow-up to r1509609.
    * subversion/libsvn_fs_fs/fs_fs.c
      (read_format): fix typo commentary and error string;
                     fix missing indirection in error condition;
                     fix parameters provided to error message
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/log-addressing@1509627 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_fs/fs_fs.c
+++ subversion/libsvn_fs_fs/fs_fs.c
@@ -355,14 +355,14 @@ read_format(int *pformat,
          svn_dirent_local_style(path, pool), buf->data);
     }
 
-  /* Non-shared repositories never use logical addressing.
+  /* Non-sharded repositories never use logical addressing.
    * If the format file is inconsistent in that respect, something
    * probably went wrong.
    */
-  if (min_log_addressing_rev != SVN_INVALID_REVNUM && !*max_files_per_dir)
+  if (*min_log_addressing_rev != SVN_INVALID_REVNUM && !*max_files_per_dir)
     return svn_error_createf(SVN_ERR_BAD_VERSION_FILE_FORMAT, NULL,
-       _("'%s' specifies logical addressing for a non-shared repository"),
-       svn_dirent_local_style(path, pool), buf->data);
+       _("'%s' specifies logical addressing for a non-sharded repository"),
+       svn_dirent_local_style(path, pool));
 
   return SVN_NO_ERROR;
 }
