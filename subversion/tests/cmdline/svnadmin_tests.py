51886ea11f8cbf7a3391d6186f000a090e1d7f0a
Stefan Fuhrmann <stefan2@apache.org>
2013-08-09 13:41:24 +0000

    'svnadmin hotcopy --incremental' is currently broken for FSFS format 6 repositories due to unpacked revprops not being cleaned up after copying revprop packs.
    This patch will cause the respective test use format 6 and FAIL.
    
    * subversion/tests/cmdline/svnadmin_tests.py
      (hotcopy_incremental_packed): use latest repo format
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1512300 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/svnadmin_tests.py
+++ subversion/tests/cmdline/svnadmin_tests.py
@@ -1635,7 +1635,7 @@ def hotcopy_incremental_packed(sbox):
   cwd = os.getcwd()
   # Configure two files per shard to trigger packing
   format_file = open(os.path.join(sbox.repo_dir, 'db', 'format'), 'wb')
-  format_file.write("4\nlayout sharded 2\n")
+  format_file.write("6\nlayout sharded 2\n")
   format_file.close()
 
   # Pack revisions 0 and 1.
