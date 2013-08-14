365b5750e20c28e64458caf5b341d4cbc536fa78
Ben Reser <breser@apache.org>
2013-08-14 20:29:45 +0000

    Adjust gen_make.py to handle wrapped add-deps.
    * build.conf: Revert temporary fix from r1514020.
    
    * build/generator/gen_make.py
      (Generator.write): Make the add_deps value in the _eztdata be a list.
    
    * build/generator/templates/build-outputs.mk.ezt: Treat target.add_deps as a list.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1514030 13f79535-47bb-0310-9956-ffa450edef68

--- build/generator/gen_make.py
+++ build/generator/gen_make.py
@@ -238,6 +238,7 @@ class Generator(gen_base.GeneratorBase):
       header_class_filenames = [ ]
       deps = [ ]
       libs = [ ]
+      add_deps = target_ob.add_deps.split()
 
       for link_dep in self.graph.get_sources(gen_base.DT_LINK, target_ob.name):
         if isinstance(link_dep, gen_base.TargetJava):
@@ -288,7 +289,7 @@ class Generator(gen_base.GeneratorBase):
                             varname=targ_varname,
                             path=path,
                             install=None,
-                            add_deps=target_ob.add_deps,
+                            add_deps=add_deps,
                             objects=objects,
                             deps=deps,
                             when=target_ob.when,
