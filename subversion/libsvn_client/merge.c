6feae96d7c6e634a9dde40eba32a9f1adf59c5b8
svn-role <dev-null@apache.org>
2013-10-10 04:02:06 +0000

    Merge r1523666 from trunk:
     * r1523666
       Fix path corruption in the calculation of reintegration merges.
       Justification:
         User reported path corruption. Possibly leading to invalid merge
         suggestions. See http://svn.haxx.se/users/archive-2013-09/0116.shtml
       Notes:
         This appears to be a regression in 1.8 as 1.7 has a similar check.
         To reproduce the problem, run in a working copy of 1.8.x:
             svn merge --reintegrate ^/subversion/branches/1.7.x
         and look for bad paths in 'Missing ranges' lines, e.g. '.../svnmucc.'
         which should be '.../svnmucc.c'.
       Votes:
         +1: rhuijben, stsp, stefan2
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/1.8.x@1530855 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/merge.c
+++ subversion/libsvn_client/merge.c
@@ -10716,7 +10716,7 @@ log_find_operative_revs(void *baton,
 
           suffix = svn_relpath_skip_ancestor(subtree_missing_this_rev,
                                              source_rel_path);
-          if (suffix)
+          if (suffix && suffix[0] != '\0')
             {
               missing_path = apr_pstrmemdup(pool, path,
                                             strlen(path) - strlen(suffix) - 1);
