7fe6db4191346700d3966844d12ffdd060574d7f
Stefan Fuhrmann <stefan2@apache.org>
2013-10-16 19:14:57 +0000

    Fix an access violation caused by incorrect pointer use.
    * subversion/libsvn_repos/dump.c
      (verify_directory_entry): the cache returns a pointer to data -
                                not the data directly
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1532866 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_repos/dump.c
+++ subversion/libsvn_repos/dump.c
@@ -1313,7 +1313,7 @@ verify_directory_entry(void *baton, const void *key, apr_ssize_t klen,
      type defined in the DIRENT. */
   if (db->edit_baton->verified_dirents_cache)
     {
-      svn_node_kind_t kind;
+      svn_node_kind_t *kind;
       svn_boolean_t found;
       unparsed_id = svn_fs_unparse_id(dirent->id, pool);
 
@@ -1323,7 +1323,7 @@ verify_directory_entry(void *baton, const void *key, apr_ssize_t klen,
 
       if (found)
         {
-          if (kind == dirent->kind)
+          if (*kind == dirent->kind)
             return SVN_NO_ERROR;
           else
             {
