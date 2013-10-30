162687881238f9ff91c4dc5b7ff862b12efe7ed2
Bert Huijben <rhuijben@apache.org>
2013-10-30 18:02:02 +0000

    Merge r1537147,1537190,1537193 from trunk excluding the config-test.c changes
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/1.8.x-r1537147@1537216 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/svn_test_main.c
+++ subversion/tests/svn_test_main.c
@@ -475,6 +475,10 @@ main(int argc, const char *argv[])
         case fstype_opt:
           opts.fs_type = apr_pstrdup(pool, opt_arg);
           break;
+        case srcdir_opt:
+          SVN_INT_ERR(svn_utf_cstring_to_utf8(&opts.srcdir, opt_arg, pool));
+          opts.srcdir = svn_dirent_internal_style(opts.srcdir, pool);
+          break;
         case list_opt:
           list_mode = TRUE;
           break;
