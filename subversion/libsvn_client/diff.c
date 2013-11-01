eb2865062dd8f88d168250219e4ef0c711b3f638
Gabriela Gibson <gbg@apache.org>
2013-11-01 23:26:09 +0000

    On the invoke-diff-cmd-feature branch: Fix variable declaration and initialization.  Update help info.
    * subversion/libsvn_client/diff.c
    
      (set_up_diff_cmd_and_options): Fix variable declaration and
        intialization.
    
    * subversion/libsvn_subr/config_file.c
    
      (invoke-diff-cmd): Update help info.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/invoke-diff-cmd-feature@1538088 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/diff.c
+++ subversion/libsvn_client/diff.c
@@ -2484,9 +2484,7 @@ set_up_diff_cmd_and_options(struct diff_cmd_baton *diff_cmd_baton,
   /* old style diff_cmd has precedence in config file */
   if (config)
     {
-      svn_config_t *cfg;
-      
-      cfg = svn_hash_gets(config, SVN_CONFIG_CATEGORY_CONFIG);
+      svn_config_t *cfg = svn_hash_gets(config, SVN_CONFIG_CATEGORY_CONFIG);
       
       svn_config_get(cfg, &diff_cmd, SVN_CONFIG_SECTION_HELPERS,
                      SVN_CONFIG_OPTION_DIFF_CMD, NULL);
@@ -2510,9 +2508,7 @@ set_up_diff_cmd_and_options(struct diff_cmd_baton *diff_cmd_baton,
     {
       if (config) /* check if there is a invoke_diff_cmd in the config file */
 	{
-	  svn_config_t *cfg;
-          
-          cfg = svn_hash_gets(config, SVN_CONFIG_CATEGORY_CONFIG);
+	  svn_config_t *cfg = svn_hash_gets(config, SVN_CONFIG_CATEGORY_CONFIG);
 	  diff_cmd_baton->diff_cmd = NULL; 
 	  svn_config_get(cfg, &diff_cmd, SVN_CONFIG_SECTION_HELPERS,
 			 SVN_CONFIG_OPTION_INVOKE_DIFF_CMD, NULL);
@@ -2528,11 +2524,9 @@ set_up_diff_cmd_and_options(struct diff_cmd_baton *diff_cmd_baton,
   /* If there was a command, arrange options to pass to it. */
   if (diff_cmd_baton->diff_cmd)
     {
-      const char **argv;
-      int argc;
+      const char **argv = NULL;
+      int argc = options->nelts;
 
-      argc = options->nelts;
-      argv = NULL;
       if (argc)
         {
           int i;
