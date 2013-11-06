b215fd0377387a2b18da3b715b76cc364c96cefc
Branko Čibej <brane@apache.org>
2013-11-06 03:44:55 +0000

    Make JavaHL tests pass when the test work directory is on a RAM disk.
    * subversion/bindings/javahl/tests/org/apache/subversion/javahl/BasicTests.java
      (BasicTests.MyChangelistCallback.doChangelist): Canonicalize the path
       because the test case that uses this callback does the same.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1539230 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/javahl/tests/org/apache/subversion/javahl/BasicTests.java
+++ subversion/bindings/javahl/tests/org/apache/subversion/javahl/BasicTests.java
@@ -3965,6 +3965,7 @@ public class BasicTests extends SVNTests
     {
         public void doChangelist(String path, String changelist)
         {
+            path = fileToSVNPath(new File(path), true);
             if (super.containsKey(path))
             {
                 // Append the changelist to the existing list
