1005303b1b8c31ee6cd6b3ffb99a8e471338655b
Stefan Fuhrmann <stefan2@apache.org>
2013-12-03 16:19:52 +0000

    * subversion/tests/svn_test_main.c   (main): C tests should accept 1.9 as an explicit compat version -           just like the Python tests do.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1547458 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/svn_test_main.c
+++ subversion/tests/svn_test_main.c
@@ -788,7 +788,7 @@ main(int argc, const char *argv[])
                 exit(1);
               }
             if ((opts.server_minor_version < 3)
-                || (opts.server_minor_version > 8))
+                || (opts.server_minor_version > 9))
               {
                 fprintf(stderr, "FAIL: Invalid minor version given\n");
                 exit(1);
