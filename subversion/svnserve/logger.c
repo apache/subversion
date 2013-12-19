ab76a5a58abe8abeda704399d344bf5e7b2feace
Bert Huijben <rhuijben@apache.org>
2013-12-19 08:27:56 +0000

    * subversion/svnserve/logger.c   (logger__log_error): Use a real memcpy instead of an obfuscated one.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1552250 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svnserve/logger.c
+++ subversion/svnserve/logger.c
@@ -149,8 +149,7 @@ logger__log_error(logger_t *logger,
           if (len > sizeof(errstr) - sizeof(APR_EOL_STR)) {
             len = sizeof(errstr) - sizeof(APR_EOL_STR);
           }
-          len += apr_snprintf(errstr + len, sizeof(APR_EOL_STR),
-                              "%s", APR_EOL_STR);
+          memcpy(errstr + len, APR_EOL_STR, sizeof(APR_EOL_STR));
           svn_error_clear(svn_stream_write(logger->stream, errstr, &len));
 
           continuation = "-";
