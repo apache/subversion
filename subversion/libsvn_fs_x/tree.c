e54855b8f005d97a61ae8efd3c358b617e8b1776
Stefan Fuhrmann <stefan2@apache.org>
2014-01-05 20:23:44 +0000

    Follow-up to r1555491: Apply a similar patch to FSX.
    * subversion/libsvn_fs_x/tree.c
      (make_txn_root): Use the correct pool.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1555625 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_x/tree.c
+++ subversion/libsvn_fs_x/tree.c
@@ -4363,7 +4363,7 @@ make_txn_root(svn_fs_root_t **root_p,
 
      Note that we cannot put those caches in frd because that content
      fs root object is not available where we would need it. */
-  SVN_ERR(svn_fs_x__initialize_txn_caches(fs, root->txn, pool));
+  SVN_ERR(svn_fs_x__initialize_txn_caches(fs, root->txn, root->pool));
 
   root->fsap_data = frd;
 
