apache/subversion

commit 4e4dc9fa771f10603ec68a848da788c21370238d

Merge the r1547866 group from trunk:
 * r1547866, r1547873, r1548097, r1548170
   Fix issue 3437, rep-cache.db created without group write bit
   Justification:
     Can prevent rep-cache from being used unless admin takes action.
   Votes:
     +1: philip, rhuijben, stefan2


git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/1.8.x@1561444 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_fs_fs/rep-cache.c
@@ -81,8 +81,33 @@ open_rep_cache(void *baton,
   int version;
 
   /* Open (or create) the sqlite database.  It will be automatically
-     closed when fs->pool is destoyed. */
+     closed when fs->pool is destoyed.  */
   db_path = path_rep_cache_db(fs->path, pool);
+#ifndef WIN32
+  {
+    /* We want to extend the permissions that apply to the repository
+       as a whole when creating a new rep cache and not simply default
+       to umask. */
+    svn_boolean_t exists;
+
+    SVN_ERR(svn_fs_fs__exists_rep_cache(&exists, fs, pool));
+    if (!exists)
+      {
+        const char *current = svn_fs_fs__path_current(fs, pool);
+        svn_error_t *err = svn_io_file_create_empty(db_path, pool);
+
+        if (err && !APR_STATUS_IS_EEXIST(err->apr_err))
+          /* A real error. */
+          return svn_error_trace(err);
+        else if (err)
+          /* Some other thread/process created the file. */
+          svn_error_clear(err);
+        else
+          /* We created the file. */
+          SVN_ERR(svn_io_copy_perms(current, db_path, pool));
+      }
+  }
+#endif
   SVN_ERR(svn_sqlite__open(&sdb, db_path,
                            svn_sqlite__mode_rwcreate, statements,
                            0, NULL,

subversion/libsvn_subr/sqlite.c
@@ -778,6 +778,21 @@ internal_open(sqlite3 **db3, const char *path, svn_sqlite__mode_t mode,
        somebody initialized SQLite before us it is needed anyway.  */
     flags |= SQLITE_OPEN_NOMUTEX;
 
+#if !defined(WIN32) && !defined(SVN_SQLITE_INLINE)
+    if (mode == svn_sqlite__mode_rwcreate)
+      {
+        svn_node_kind_t kind;
+
+        /* Create the file before SQLite to avoid any permissions
+           problems with an SQLite build that uses the default
+           SQLITE_DEFAULT_FILE_PERMISSIONS of 644 modified by umask.
+           We simply want umask permissions. */
+        SVN_ERR(svn_io_check_path(path, &kind, scratch_pool));
+        if (kind == svn_node_none)
+          SVN_ERR(svn_io_file_create_empty(path, scratch_pool));
+      }
+#endif
+
     /* Open the database. Note that a handle is returned, even when an error
        occurs (except for out-of-memory); thus, we can safely use it to
        extract an error message and construct an svn_error_t. */

STATUS
@@ -264,13 +264,6 @@ Veto-blocked changes:
 Approved changes:
 =================
 
- * r1547866, r1547873, r1548097, r1548170
-   Fix issue 3437, rep-cache.db created without group write bit
-   Justification:
-     Can prevent rep-cache from being used unless admin takes action.
-   Votes:
-     +1: philip, rhuijben, stefan2
-
  * r1548105
    mod_dav_svn: Fix SVNAllowBulkUpdates directive merging.
    Justification:

subversion/libsvn_subr/sqlite3wrapper.c
@@ -50,6 +50,7 @@
 #      undef inline
 #    endif
 #  endif
+#  define SQLITE_DEFAULT_FILE_PERMISSIONS 0666
 #  include <sqlite3.c>
 #  if __GNUC__ > 4 || (__GNUC__ == 4 && (__GNUC_MINOR__ >= 6))
 #    pragma GCC diagnostic pop

subversion/libsvn_wc/wc_db_util.c
@@ -136,22 +136,6 @@ svn_wc__db_util_open_db(svn_sqlite__db_t **sdb,
                                  svn_dirent_local_style(sdb_abspath,
                                                         scratch_pool));
     }
-#ifndef WIN32
-  else
-    {
-      apr_file_t *f;
-
-      /* A standard SQLite build creates a DB with mode 644 ^ !umask
-         which means the file doesn't have group/world write access
-         even when umask allows it. By ensuring the file exists before
-         SQLite gets involved we give it the permissions allowed by
-         umask. */
-      SVN_ERR(svn_io_file_open(&f, sdb_abspath,
-                               (APR_READ | APR_WRITE | APR_CREATE),
-                               APR_OS_DEFAULT, scratch_pool));
-      SVN_ERR(svn_io_file_close(f, scratch_pool));
-    }
-#endif
 
   SVN_ERR(svn_sqlite__open(sdb, sdb_abspath, smode,
                            my_statements ? my_statements : statements,

commit 00dd57292f8e45997d159ec974f5d1fc0c08b054

On the 1.8.x branch: Make everything compile again after the r1547866 group backport broke it in r1561444.
Approved +1 on IRC by stefan2, rhuijben, breser.

* subversion/libsvn_fs_fs/rep-cache.c
  (open_rep_cache): Instead of svn_io_file_create_empty,
                    use the generic file creation function
                    that is actually available in 1.8.x.

* subversion/libsvn_subr/sqlite.c
  (internal_open): Ditto.

git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/1.8.x@1561543 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_fs_fs/rep-cache.c
@@ -94,7 +94,7 @@ open_rep_cache(void *baton,
     if (!exists)
       {
         const char *current = svn_fs_fs__path_current(fs, pool);
-        svn_error_t *err = svn_io_file_create_empty(db_path, pool);
+        svn_error_t *err = svn_io_file_create(db_path, "", pool);
 
         if (err && !APR_STATUS_IS_EEXIST(err->apr_err))
           /* A real error. */

subversion/libsvn_subr/sqlite.c
@@ -789,7 +789,7 @@ internal_open(sqlite3 **db3, const char *path, svn_sqlite__mode_t mode,
            We simply want umask permissions. */
         SVN_ERR(svn_io_check_path(path, &kind, scratch_pool));
         if (kind == svn_node_none)
-          SVN_ERR(svn_io_file_create_empty(path, scratch_pool));
+          SVN_ERR(svn_io_file_create(path, "", scratch_pool));
       }
 #endif
 