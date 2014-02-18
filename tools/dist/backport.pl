apache/subversion

commit 91ec3353f626f562d25103eb4d6353682767cfb8

Fix backport script to not require Branch: header.
This makes it so that if the revision line has the branch on it and there is
no Branch: header the merge will complete rather than failing.

[ This change was wrong, because it made $pattern too liberal.  (It made
  $pattern start with unanchored, optional groups, which caused $pattern
  to match any entry that mentions the victim branch, not only the entry that
  nominates that branch.)

  This was fixed in r1569235. ]

* tools/dist/backport.pl
  (merge): Make the branch header prefix optional when searching for the entry
    to remove before committing.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1569068 13f79535-47bb-0310-9956-ffa450edef68

tools/dist/backport.pl
@@ -235,7 +235,7 @@ sub merge {
       map { sprintf '\[%s%s]', $_, ($_ x ($_ eq '\\')) }
       split //,
       $entry{branch};
-    $pattern = sprintf '\VBranch: \*\n\? \*\(\.\*\/branches\/\)\?%s',
+    $pattern = sprintf '\V\(Branch: \*\n\?\)\? \*\(\.\*\/branches\/\)\?%s',
                  $vim_escaped_branch;
     if ($SVNvsn >= 1_008_000) {
       $mergeargs = "$BRANCHES/$entry{branch}";

commit 23ab8da9a431007b2088589ca2ffc2887a4010f9

backport.pl: Get rid of an ugly hack.
This also fixes a bug introduced in r1569068 (which see for details).

* tools/dist/backport.pl
  (parse_entry):
    Store the paragraph number (relative to the file) in the parsed entry.
  (merge):
    Use the paragraph number to delete the entry.

git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1569235 13f79535-47bb-0310-9956-ffa450edef68

tools/dist/backport.pl
@@ -223,20 +223,13 @@ sub merge {
   $MERGED_SOMETHING++;
 
   my ($logmsg_fh, $logmsg_filename) = tempfile();
-  my ($mergeargs, $pattern);
+  my ($mergeargs);
 
   # Include the time so it's easier to find the interesting backups.
   my $backupfile = strftime "backport_pl.%Y%m%d-%H%M%S.$$.tmp", localtime;
   die if -s $backupfile;
 
   if ($entry{branch}) {
-    my $vim_escaped_branch = 
-      join "",
-      map { sprintf '\[%s%s]', $_, ($_ x ($_ eq '\\')) }
-      split //,
-      $entry{branch};
-    $pattern = sprintf '\V\(Branch: \*\n\?\)\? \*\(\.\*\/branches\/\)\?%s',
-                 $vim_escaped_branch;
     if ($SVNvsn >= 1_008_000) {
       $mergeargs = "$BRANCHES/$entry{branch}";
       say $logmsg_fh "Merge $entry{header}:";
@@ -246,7 +239,6 @@ sub merge {
     }
     say $logmsg_fh "";
   } elsif (@{$entry{revisions}}) {
-    $pattern = '^ *[*] \V' . 'r' . $entry{revisions}->[0];
     $mergeargs = join " ",
       ($entry{accept} ? "--accept=$entry{accept}" : ()),
       (map { "-c$_" } @{$entry{revisions}}),
@@ -294,7 +286,7 @@ if $sh[$MAY_COMMIT]; then
   # Remove the approved entry.  The sentinel guarantees the right number of blank
   # lines is removed, which prevents spurious '--renormalize' commits tomorrow.
   echo "sentinel" >> $STATUS
-  $VIM -e -s -n -N -i NONE -u NONE -c '/$pattern/normal! dap' -c wq $STATUS
+  $VIM -e -s -n -N -i NONE -u NONE -c ':0normal! $entry{parno}\x{7d}kdap' -c wq $STATUS
   $VIM -e -s -n -N -i NONE -u NONE -c '\$d' -c wq $STATUS
   $SVNq commit -F $logmsg_filename
 elif ! $sh[$YES]; then
@@ -454,6 +446,7 @@ sub parse_entry {
     accept => $accept,
     raw => $raw,
     digest => digest_entry($raw),
+    parno => ($.),
   );
 }
 