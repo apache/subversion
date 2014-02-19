apache/subversion

commit 0a4da1fe3b0de4cd0cb72e668794217dcc583da5

Make 'svn mergeinfo' account for non-inheritable revision ranges.
See http://svn.haxx.se/dev/archive-2009-08/0291.shtml.

* subversion/include/private/svn_mergeinfo_private.h
  (svn_mergeinfo__set_inheritance): New.

* subversion/include/svn_mergeinfo.h
  (svn_rangelist_inheritable2, svn_mergeinfo_inheritable2): New.

* subversion/libsvn_client/mergeinfo.c
  (filter_log_entry_with_rangelist): Handle non-inheritable ranges.
  (svn_client_mergeinfo_log_merged, svn_client_mergeinfo_log_eligible): Pass
  both fully and *partially* merged or available revisions to
  logs_for_mergeinfo_rangelist().

* subversion/libsvn_subr/mergeinfo.c
  (svn_mergeinfo__set_inheritance, svn_mergeinfo_inheritable2,
  svn_rangelist_inheritable2): New.

* subversion/svn/mergeinfo-cmd.c
  (print_log_rev): Print the '*' decorator for partially merged/available
  revisions.

* subversion/tests/cmdline/svntest/actions.py
  (run_and_verify_mergeinfo): Change expected_output parameter to list of
  strings.

* subversion/tests/cmdline/mergeinfo_tests.py
  (mergeinfo, explicit_mergeinfo_source, mergeinfo_non_source): Update calls
  to run_and_verify_mergeinfo, passing strings instead of ints.
  (non_inheritable_mergeinfo): As above, plus some tweaks to test the new
  functionality.

git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@878906 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_subr/mergeinfo.c
@@ -741,6 +741,33 @@ svn_rangelist__set_inheritance(apr_array_header_t *rangelist,
   return;
 }
 
+void
+svn_mergeinfo__set_inheritance(svn_mergeinfo_t mergeinfo,
+                               svn_boolean_t inheritable,
+                               apr_pool_t *scratch_pool)
+{
+  if (mergeinfo)
+    {
+      apr_hash_index_t *hi;
+
+      for (hi = apr_hash_first(scratch_pool, mergeinfo);
+           hi;
+           hi = apr_hash_next(hi))
+        {
+          apr_array_header_t *rangelist;
+          const void *path;
+          void *val;
+          apr_hash_this(hi, &path, NULL, &val);
+
+          rangelist = apr_hash_get(mergeinfo, path, APR_HASH_KEY_STRING);
+
+          if (rangelist)
+            svn_rangelist__set_inheritance(rangelist, inheritable);
+        }
+    }
+  return;
+}
+
 /* Either remove any overlapping ranges described by ERASER from
    WHITEBOARD (when DO_REMOVE is TRUE), or capture the overlap, and
    place the remaining or overlapping ranges in OUTPUT. */
@@ -1370,6 +1397,48 @@ svn_mergeinfo_dup(svn_mergeinfo_t mergeinfo, apr_pool_t *pool)
 }
 
 svn_error_t *
+svn_mergeinfo_inheritable2(svn_mergeinfo_t *output,
+                           svn_mergeinfo_t mergeinfo,
+                           const char *path,
+                           svn_revnum_t start,
+                           svn_revnum_t end,
+                           svn_boolean_t inheritable,
+                           apr_pool_t *result_pool,
+                           apr_pool_t *scratch_pool)
+{
+  apr_hash_index_t *hi;
+  const void *key;
+  apr_ssize_t keylen;
+  void *rangelist;
+
+  svn_mergeinfo_t inheritable_mergeinfo = apr_hash_make(result_pool);
+  for (hi = apr_hash_first(scratch_pool, mergeinfo);
+       hi;
+       hi = apr_hash_next(hi))
+    {
+      apr_array_header_t *inheritable_rangelist;
+      apr_hash_this(hi, &key, &keylen, &rangelist);
+      if (!path || svn_path_compare_paths(path, (const char *)key) == 0)
+        SVN_ERR(svn_rangelist_inheritable2(&inheritable_rangelist,
+                                           (apr_array_header_t *) rangelist,
+                                           start, end, inheritable,
+                                           result_pool, scratch_pool));
+      else
+        inheritable_rangelist =
+          svn_rangelist_dup((apr_array_header_t *)rangelist, result_pool);
+
+      /* Only add this rangelist if some ranges remain.  A rangelist with
+         a path mapped to an empty rangelist is not syntactically valid */
+      if (inheritable_rangelist->nelts)
+        apr_hash_set(inheritable_mergeinfo,
+                     apr_pstrmemdup(result_pool, key, keylen), keylen,
+                     inheritable_rangelist);
+    }
+  *output = inheritable_mergeinfo;
+  return SVN_NO_ERROR;
+}
+
+svn_error_t *
 svn_mergeinfo_inheritable(svn_mergeinfo_t *output,
                           svn_mergeinfo_t mergeinfo,
                           const char *path,
@@ -1406,14 +1475,17 @@ svn_mergeinfo_inheritable(svn_mergeinfo_t *output,
   return SVN_NO_ERROR;
 }
 
+
 svn_error_t *
-svn_rangelist_inheritable(apr_array_header_t **inheritable_rangelist,
-                          apr_array_header_t *rangelist,
-                          svn_revnum_t start,
-                          svn_revnum_t end,
-                          apr_pool_t *pool)
+svn_rangelist_inheritable2(apr_array_header_t **inheritable_rangelist,
+                           apr_array_header_t *rangelist,
+                           svn_revnum_t start,
+                           svn_revnum_t end,
+                           svn_boolean_t inheritable,
+                           apr_pool_t *result_pool,
+                           apr_pool_t *scratch_pool)
 {
-  *inheritable_rangelist = apr_array_make(pool, 1,
+  *inheritable_rangelist = apr_array_make(result_pool, 1,
                                           sizeof(svn_merge_range_t *));
   if (rangelist->nelts)
     {
@@ -1427,10 +1499,10 @@ svn_rangelist_inheritable(apr_array_header_t **inheritable_rangelist,
             {
               svn_merge_range_t *range = APR_ARRAY_IDX(rangelist, i,
                                                        svn_merge_range_t *);
-              if (range->inheritable)
+              if (range->inheritable == inheritable)
                 {
                   svn_merge_range_t *inheritable_range =
-                    apr_palloc(pool, sizeof(*inheritable_range));
+                    apr_palloc(result_pool, sizeof(*inheritable_range));
                   inheritable_range->start = range->start;
                   inheritable_range->end = range->end;
                   inheritable_range->inheritable = TRUE;
@@ -1444,12 +1516,12 @@ svn_rangelist_inheritable(apr_array_header_t **inheritable_rangelist,
           /* We want only the non-inheritable ranges bound by START
              and END removed. */
           apr_array_header_t *ranges_inheritable =
-            apr_array_make(pool, 0, sizeof(svn_merge_range_t *));
-          svn_merge_range_t *range = apr_palloc(pool, sizeof(*range));
+            apr_array_make(scratch_pool, 0, sizeof(svn_merge_range_t *));
+          svn_merge_range_t *range = apr_palloc(scratch_pool, sizeof(*range));
 
           range->start = start;
           range->end = end;
-          range->inheritable = FALSE;
+          range->inheritable = inheritable;
           APR_ARRAY_PUSH(ranges_inheritable, svn_merge_range_t *) = range;
 
           if (rangelist->nelts)
@@ -1457,10 +1529,21 @@ svn_rangelist_inheritable(apr_array_header_t **inheritable_rangelist,
                                          ranges_inheritable,
                                          rangelist,
                                          TRUE,
-                                         pool));
+                                         result_pool));
         }
     }
   return SVN_NO_ERROR;
+}
+
+svn_error_t *
+svn_rangelist_inheritable(apr_array_header_t **inheritable_rangelist,
+                          apr_array_header_t *rangelist,
+                          svn_revnum_t start,
+                          svn_revnum_t end,
+                          apr_pool_t *pool)
+{
+  return svn_rangelist_inheritable2(inheritable_rangelist, rangelist,
+                                    start, end, TRUE, pool, pool);
 }
 
 svn_boolean_t

subversion/libsvn_client/mergeinfo.c
@@ -980,13 +980,16 @@ filter_log_entry_with_rangelist(void *baton,
   range->end = log_entry->revision;
   range->inheritable = TRUE;
   APR_ARRAY_PUSH(this_rangelist, svn_merge_range_t *) = range;
+
+  /* Don't consider inheritance, we'll deal with non-inheritable
+     mergeinfo in the log_receiver callback. */
   SVN_ERR(svn_rangelist_intersect(&intersection, fleb->rangelist,
-                                  this_rangelist, TRUE, pool));
+                                  this_rangelist, FALSE, pool));
   if (! (intersection && intersection->nelts))
     return SVN_NO_ERROR;
 
   SVN_ERR_ASSERT(intersection->nelts == 1);
-  return fleb->log_receiver(fleb->log_receiver_baton, log_entry, pool);
+  return fleb->log_receiver(fleb->rangelist, log_entry, pool);
 }
 
 static svn_error_t *
@@ -1123,7 +1126,12 @@ svn_client_mergeinfo_log_merged(const char *path_or_url,
                                 apr_pool_t *pool)
 {
   const char *repos_root, *log_target = NULL, *merge_source_url;
-  svn_mergeinfo_t tgt_mergeinfo, source_history, mergeinfo;
+  svn_mergeinfo_t tgt_mergeinfo;
+  svn_mergeinfo_t source_history;
+  svn_mergeinfo_t merged;
+  svn_mergeinfo_t mergeinfo_noninheritable;
+  svn_mergeinfo_t tgt_inheritable_mergeinfo;
+  svn_mergeinfo_t tgt_noninheritable_mergeinfo;
   apr_array_header_t *rangelist;
   svn_opt_revision_t *real_src_peg_revision;
   apr_hash_index_t *hi;
@@ -1151,8 +1159,45 @@ svn_client_mergeinfo_log_merged(const char *path_or_url,
                                                SVN_INVALID_REVNUM,
                                                SVN_INVALID_REVNUM,
                                                NULL, ctx, pool));
-  SVN_ERR(svn_mergeinfo_intersect2(&mergeinfo, tgt_mergeinfo,
+
+  /* svn_client__get_history_as_mergeinfo() will give us mergeinfo with all
+     inheritable ranges, since history has no concept of non-inheritability.
+     TGT_MERGEINFO might have non-inheritable ranges however, indicating
+     that a range is only partially merged.  We need to keep track of both! */
+
+  /* Separate TGT_MERGEINFO into its inheritable and non-inheritable
+     parts. */
+  SVN_ERR(svn_mergeinfo_inheritable2(&tgt_inheritable_mergeinfo,
+                                     tgt_mergeinfo, NULL,
+                                     SVN_INVALID_REVNUM,
+                                     SVN_INVALID_REVNUM,
+                                     TRUE, pool, pool));
+  SVN_ERR(svn_mergeinfo_inheritable2(&tgt_noninheritable_mergeinfo,
+                                     tgt_mergeinfo, NULL,
+                                     SVN_INVALID_REVNUM,
+                                     SVN_INVALID_REVNUM,
+                                     FALSE, pool, pool));
+
+  /* Find the intersection of the non-inheritable part of TGT_MERGEINFO
+     and SOURCE_HISTORY.  svn_mergeinfo_intersect2() won't consider
+     non-inheritable and inheritable ranges intersecting unless we ignore
+     inheritance, but in doing so the the resulting intersection has
+     all inheritable ranges.  To get around this we set the inheritance
+     on the result to all non-inheritable. */
+  SVN_ERR(svn_mergeinfo_intersect2(&mergeinfo_noninheritable,
+                                   tgt_noninheritable_mergeinfo,
                                    source_history, FALSE, pool, pool));
+  svn_mergeinfo__set_inheritance(mergeinfo_noninheritable, FALSE, pool);
+
+  /* Find the intersection of the inheritable part of TGT_MERGEINFO
+     and SOURCE_HISTORY. */
+  SVN_ERR(svn_mergeinfo_intersect2(&merged, tgt_inheritable_mergeinfo,
+                                   source_history, FALSE, pool, pool));
+
+  /* Merge the inheritable and non-inheritable intersections back together.
+     This results in mergeinfo that describes both revisions that are fully
+     merged as well as those that are only partially merged to PATH_OR_URL. */
+  SVN_ERR(svn_mergeinfo_merge(merged, mergeinfo_noninheritable, pool));
 
   /* Step 3: Now, we iterate over the eligible paths/rangelists to
      find the youngest revision (and its associated path).  Because
@@ -1163,7 +1208,7 @@ svn_client_mergeinfo_log_merged(const char *path_or_url,
      we can filter out no-op merge revisions.  While here, we'll
      collapse our rangelists into a single one.  */
   rangelist = apr_array_make(pool, 64, sizeof(svn_merge_range_t *));
-  for (hi = apr_hash_first(pool, mergeinfo); hi; hi = apr_hash_next(hi))
+  for (hi = apr_hash_first(pool, merged); hi; hi = apr_hash_next(hi))
     {
       const char *key = svn_apr_hash_index_key(hi);
       apr_array_header_t *list = svn_apr_hash_index_val(hi);
@@ -1248,7 +1293,12 @@ svn_client_mergeinfo_log_eligible(const char *path_or_url,
                                   svn_client_ctx_t *ctx,
                                   apr_pool_t *pool)
 {
-  svn_mergeinfo_t mergeinfo, history, source_history, available;
+  svn_mergeinfo_t mergeinfo;
+  svn_mergeinfo_t mergeinfo_noninheritable;
+  svn_mergeinfo_t history;
+  svn_mergeinfo_t source_history;
+  svn_mergeinfo_t available;
+  svn_mergeinfo_t available_noninheritable;
   apr_hash_index_t *hi;
   svn_ra_session_t *ra_session;
   svn_opt_revision_t *real_src_peg_revision;
@@ -1291,12 +1341,39 @@ svn_client_mergeinfo_log_eligible(const char *path_or_url,
                                                real_src_peg_revision,
                                                SVN_INVALID_REVNUM,
                                                SVN_INVALID_REVNUM,
-                                               ra_session, ctx, pool));
+                                               ra_session, ctx, sesspool));
 
-  /* Now, we want to remove from the possible mergeinfo
-     (SOURCE_HISTORY) the merges already present in our PATH_OR_URL. */
+/* svn_client__get_history_as_mergeinfo() will give us mergeinfo with all
+   inheritable ranges, since history has no concept of non-inheritability.
+   MERGEINFO might have non-inheritable ranges however, indicating
+   that a range is only partially merged.  We need to keep track of both! */
+
+  /* Get the non-inheritable part of MERGEINFO. */
+  SVN_ERR(svn_mergeinfo_inheritable2(&mergeinfo_noninheritable,
+                                     mergeinfo, NULL,
+                                     SVN_INVALID_REVNUM,
+                                     SVN_INVALID_REVNUM,
+                                     FALSE, pool, sesspool));
+
+  /* Find the intersection of the non-inheritable part of MERGEINFO
+     and SOURCE_HISTORY.  svn_mergeinfo_intersect2() won't consider
+     non-inheritable and inheritable ranges intersecting unless we ignore
+     inheritance, but in doing so the the resulting intersection has
+     all inheritable ranges.  To get around this we set the inheritance on
+     the result to all non-inheritable, leaving us with what has been
+     partially merged to PATH_OR_URL*/
+  SVN_ERR(svn_mergeinfo_intersect2(&available_noninheritable,
+                                   mergeinfo_noninheritable,
+                                   source_history,
+                                   FALSE, pool, sesspool));
+  svn_mergeinfo__set_inheritance(available_noninheritable, FALSE, pool);
+
+  /* Find any part of SOURCE_HISTORY which has not been merged *at all*
+     to PATH_OR_URL and then merge in the parts which are partially
+     merged. */
   SVN_ERR(svn_mergeinfo_remove2(&available, mergeinfo, source_history,
                                 FALSE, pool, sesspool));
+  SVN_ERR(svn_mergeinfo_merge(available, available_noninheritable, pool));
 
   svn_pool_destroy(sesspool);
 

subversion/include/private/svn_mergeinfo_private.h
@@ -44,6 +44,14 @@ void
 svn_rangelist__set_inheritance(apr_array_header_t *rangelist,
                                svn_boolean_t inheritable);
 
+/* Set inheritability of all rangelists in MERGEINFO to INHERITABLE.
+   If MERGEINFO is NULL do nothing.  If a rangelist in MERGEINFO is
+   NULL leave it alone. */
+void
+svn_mergeinfo__set_inheritance(svn_mergeinfo_t mergeinfo,
+                               svn_boolean_t inheritable,
+                               apr_pool_t *scratch_pool);
+
 /* Return whether INFO1 and INFO2 are equal in *IS_EQUAL.
 
    CONSIDER_INERITANCE determines how the rangelists in the two

subversion/include/svn_mergeinfo.h
@@ -368,11 +368,26 @@ svn_rangelist_to_string(svn_string_t **output,
                         apr_pool_t *pool);
 
 /** Return a deep copy of @c svn_merge_range_t *'s in @a rangelist excluding
- * all non-inheritable @c svn_merge_range_t.  If @a start and @a end are valid
- * revisions and @a start is less than or equal to @a end, then exclude only the
- * non-inheritable revision ranges that intersect inclusively with the range
- * defined by @a start and @a end.  If @a rangelist contains no elements, return
- * an empty array.  Allocate the copy in @a pool.
+ * all non-inheritable @c svn_merge_range_t if @a inheritable is TRUE or
+ * excluding all inheritable @c svn_merge_range_t otherwise.  If @a start and
+ * @a end are valid revisions and @a start is less than or equal to @a end,
+ * then exclude only the non-inheritable revision ranges that intersect
+ * inclusively with the range defined by @a start and @a end.  If
+ * @a rangelist contains no elements, return an empty array.  Allocate the
+ * copy in @a result_pool, use @a scratch_pool for temporary allocations.
+ *
+ * @since New in 1.7.
+ */
+svn_error_t *
+svn_rangelist_inheritable2(apr_array_header_t **inheritable_rangelist,
+                           apr_array_header_t *rangelist,
+                           svn_revnum_t start,
+                           svn_revnum_t end,
+                           svn_boolean_t inheritable,
+                           apr_pool_t *result_pool,
+                           apr_pool_t *scratch_pool);
+
+/** Like svn_rangelist_inheritable2, but always finds inheritable ranges.
  *
  * @since New in 1.5.
  */
@@ -384,14 +399,30 @@ svn_rangelist_inheritable(apr_array_header_t **inheritable_rangelist,
                           apr_pool_t *pool);
 
 /** Return a deep copy of @a mergeinfo, excluding all non-inheritable
- * @c svn_merge_range_t.  If @a start and @a end are valid revisions
- * and @a start is less than or equal to @a end, then exclude only the
- * non-inheritable revisions that intersect inclusively with the range
- * defined by @a start and @a end.  If @a path is not NULL remove
- * non-inheritable ranges only for @a path.  If all ranges are removed
- * for a given path then remove that path as well.  If all paths are
- * removed or @a rangelist is empty then set @a *inheritable_rangelist
- * to an empty array.  Allocate the copy in @a pool.
+ * @c svn_merge_range_t if @a inheritable is TRUE or excluding all
+ * inheritable @c svn_merge_range_t otherwise.  If @a start and @a end
+ * are valid revisions and @a start is less than or equal to @a end,
+ * then exclude only the non-inheritable revisions that intersect
+ * inclusively with the range defined by @a start and @a end.  If @a path
+ * is not NULL remove non-inheritable ranges only for @a path.  If all
+ * ranges are removed for a given path then remove that path as well.
+ * If all paths are removed or @a rangelist is empty then set
+ * @a *inheritable_rangelist to an empty array.  Allocate the copy in
+ * @a result_pool, use @a scratch_pool for temporary allocations.
+ *
+ * @since New in 1.7.
+ */
+svn_error_t *
+svn_mergeinfo_inheritable2(svn_mergeinfo_t *inheritable_mergeinfo,
+                           svn_mergeinfo_t mergeinfo,
+                           const char *path,
+                           svn_revnum_t start,
+                           svn_revnum_t end,
+                           svn_boolean_t inheritable,
+                           apr_pool_t *result_pool,
+                           apr_pool_t *scratch_pool);
+
+/** Like svn_mergeinfo_inheritable2, but always finds inheritable mergeinfo.
  *
  * @since New in 1.5.
  */

subversion/svn/mergeinfo-cmd.c
@@ -42,13 +42,34 @@
 /*** Code. ***/
 
 /* Implements the svn_log_entry_receiver_t interface.  BATON is a
-   pointer to a mergeinfo rangelist array. */
+   pointer to a rangelist which contains LOG_ENTRY->REV. */
 static svn_error_t *
 print_log_rev(void *baton,
               svn_log_entry_t *log_entry,
               apr_pool_t *pool)
 {
-  svn_cmdline_printf(pool, "r%ld\n", log_entry->revision);
+  apr_array_header_t *intersecting_rangelist;
+  apr_array_header_t *baton_rangelist = baton;
+  apr_array_header_t *rangelist =
+    apr_array_make(pool, 1, sizeof(svn_merge_range_t *));
+  svn_merge_range_t *range = apr_pcalloc(pool, sizeof(*range));
+
+  /* Create a rangelist representing LOG_ENTRY->REV and see if it itersects
+     with BATON when considering inheritance.  If it does then print the
+     range otherwise print the range but with the non-inheritable decorator
+     '*'. */
+  range->start = log_entry->revision - 1;
+  range->end = log_entry->revision;
+  range->inheritable = TRUE;
+  APR_ARRAY_PUSH(rangelist, svn_merge_range_t *) = range;
+  SVN_ERR(svn_rangelist_intersect(&intersecting_rangelist, rangelist,
+                                  baton_rangelist, TRUE, pool));
+
+  if (intersecting_rangelist->nelts)
+    svn_cmdline_printf(pool, "r%ld\n", log_entry->revision);
+  else
+    svn_cmdline_printf(pool, "r%ld*\n", log_entry->revision);
+
   return SVN_NO_ERROR;
 }
 

subversion/tests/cmdline/mergeinfo_tests.py
@@ -77,7 +77,7 @@ def mergeinfo(sbox):
   svntest.actions.run_and_verify_svn(None, None, [], "merge", "-c", "1",
                                      "--record-only", sbox.repo_url, wc_dir)
   svntest.actions.run_and_verify_mergeinfo(adjust_error_for_server_version(""),
-                                           [1], sbox.repo_url, wc_dir)
+                                           ['1'], sbox.repo_url, wc_dir)
 
 def explicit_mergeinfo_source(sbox):
   "'mergeinfo' with source selection"
@@ -100,13 +100,13 @@ def explicit_mergeinfo_source(sbox):
 
   # Check using each of our recorded merge sources (as paths and URLs).
   svntest.actions.run_and_verify_mergeinfo(adjust_error_for_server_version(""),
-                                           [1], B_url, H_path)
+                                           ['1'], B_url, H_path)
   svntest.actions.run_and_verify_mergeinfo(adjust_error_for_server_version(""),
-                                           [1], B_path, H_path)
+                                           ['1'], B_path, H_path)
   svntest.actions.run_and_verify_mergeinfo(adjust_error_for_server_version(""),
-                                           [1], G_url, H_path)
+                                           ['1'], G_url, H_path)
   svntest.actions.run_and_verify_mergeinfo(adjust_error_for_server_version(""),
-                                           [1], G_path, H_path)
+                                           ['1'], G_path, H_path)
 
 def mergeinfo_non_source(sbox):
   "'mergeinfo' with uninteresting source selection"
@@ -129,7 +129,7 @@ def mergeinfo_non_source(sbox):
 
   # Check on a source we haven't "merged" from.
   svntest.actions.run_and_verify_mergeinfo(adjust_error_for_server_version(""),
-                                           [2], H2_url, H_path)
+                                           ['2'], H2_url, H_path)
 
 #----------------------------------------------------------------------
 # Issue #3138
@@ -199,22 +199,22 @@ def non_inheritable_mergeinfo(sbox):
   # Despite being non-inheritable, r6 should still show as merged to A_COPY
   # and not eligible for merging.
   svntest.actions.run_and_verify_mergeinfo(adjust_error_for_server_version(""),
-                                           [4,6],
+                                           ['4','6*'],
                                            sbox.repo_url + '/A',
                                            A_COPY_path)
   svntest.actions.run_and_verify_mergeinfo(adjust_error_for_server_version(""),
-                                           [3,5],
+                                           ['3','5','6*'],
                                            sbox.repo_url + '/A',
                                            A_COPY_path,
                                            '--show-revs', 'eligible')
   # But if we drop down to A_COPY/D, r6 should show as eligible because it
   # was only merged into A_COPY, no deeper.
   svntest.actions.run_and_verify_mergeinfo(adjust_error_for_server_version(""),
-                                           [4],
+                                           ['4'],
                                            sbox.repo_url + '/A/D',
                                            D_COPY_path)
   svntest.actions.run_and_verify_mergeinfo(adjust_error_for_server_version(""),
-                                           [3,6],
+                                           ['3','6'],
                                            sbox.repo_url + '/A/D',
                                            D_COPY_path,
                                            '--show-revs', 'eligible')

subversion/tests/cmdline/svntest/actions.py
@@ -1058,8 +1058,9 @@ def run_and_verify_mergeinfo(error_re_string = None,
                              expected_output = [],
                              *args):
   """Run 'svn mergeinfo ARGS', and compare the result against
-  EXPECTED_OUTPUT, a list of revisions expected in the output.
-  Raise an exception if an unexpected output is encountered."""
+  EXPECTED_OUTPUT, a list of string representations of revisions
+  expected in the output.  Raise an exception if an unexpected
+  output is encountered."""
 
   mergeinfo_command = ["mergeinfo"]
   mergeinfo_command.extend(args)
@@ -1072,9 +1073,8 @@ def run_and_verify_mergeinfo(error_re_string = None,
     verify.verify_outputs(None, None, err, None, expected_err)
     return
 
-  out = sorted([_f for _f in [int(x.rstrip()[1:]) for x in out] if _f])
+  out = sorted([_f for _f in [x.rstrip()[1:] for x in out] if _f])
   expected_output.sort()
-
   extra_out = []
   if out != expected_output:
     exp_hash = dict.fromkeys(expected_output)

commit c31716d915626f8cf007d928391bf2064121cced

Fix a mergeinfo calculation bug introduced in r878906. The bug had no adverse effect in the current trunk Subversion code.
The function svn_rangelist_inheritable2() was changing the inheritability of
the remaining non-inheritable ranges to TRUE when removing all inheritable
ranges from a rangelist. I found this by code inspection. The only caller
that exercises this code path in current trunk Subversion code is
svn_client__mergeinfo_log(), via svn_mergeinfo_inheritable2(inheritable=
FALSE), and it ignores inheritance on the result so there is no net effect.

* subversion/libsvn_subr/mergeinfo.c
  (svn_rangelist_inheritable2): Don't change the inheritability of remaining
    ranges.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1569731 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_subr/mergeinfo.c
@@ -2103,7 +2103,7 @@ svn_rangelist_inheritable2(svn_rangelist_t **inheritable_rangelist,
                     apr_palloc(result_pool, sizeof(*inheritable_range));
                   inheritable_range->start = range->start;
                   inheritable_range->end = range->end;
-                  inheritable_range->inheritable = TRUE;
+                  inheritable_range->inheritable = range->inheritable;
                   APR_ARRAY_PUSH(*inheritable_rangelist,
                                  svn_merge_range_t *) = range;
                 }