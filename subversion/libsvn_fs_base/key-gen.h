e3c73ed10af446f3c088627393b1e31075fc85a8
Ben Reser <breser@apache.org>
2014-02-21 23:47:44 +0000

    Just remove svn_fs_base__key_compare().
    The function has different semantics than strcmp, but this doesn't matter for
    how we use it.  Leaving the macro around essentially means we need to document
    strcmp again which is silly.
    
    * subversion/libsvn_fs_base/key-gen.h
      (svn_fs_base__key_compare): Remove.
    
    * subversion/libsvn_fs_base/revs-txns.c
      (delete_txn_tree): Update callers to use strcmp directly.
    
    * subversion/libsvn_fs_base/tree.c
      (get_copy_inheritance, txn_body_copied_from, txn_body_history_prev): ...
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1570753 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_base/key-gen.h
+++ subversion/libsvn_fs_base/key-gen.h
@@ -78,13 +78,6 @@ extern "C" {
 void svn_fs_base__next_key(const char *this, apr_size_t *len, char *next);
 
 
-/* Compare two strings A and B as base-36 alphanumeric keys.
- *
- * Return an integer value less than zero, zero, or greater than zero if A is
- * less than, equal to, or greater than B, respectively.
- */
-#define svn_fs_base__key_compare strcmp
-
 /* Compare two strings A and B as base-36 alphanumber keys.
  *
  * Return TRUE iff both keys are NULL or both keys have the same
