8e2ae1a835ad3f54c5798a2baefe62d2f8810b40
Bert Huijben <rhuijben@apache.org>
2014-02-26 21:29:22 +0000

    Following up on r1572253, bump serf requirement on Windows.
    * build/generator/gen_msvc_dsp.py
    * build/generator/gen_vcnet_vcproj.py
      Don't generate serf and zlib projects.
    
    * build/generator/gen_win.py
      Don't create build_zlib.bat
      Stop filtering serf and zlib projects in specific cases.
      
    * build/generator/gen_win_dependencies.py
      Remove special is_src support that was only needed for
      this ugly zlib/serf dependency case.
      Bump minimal required serf version.
      
    * build/generator/templates/build_zlib.ezt
    * build/generator/templates/serf.dsp.ezt
    * build/generator/templates/serf.vcproj.ezt
    * build/generator/templates/serf.vcxproj.ezt
    * build/generator/templates/zlib.dsp.ezt
    * build/generator/templates/zlib.vcproj.ezt
    * build/generator/templates/zlib.vcxproj.ezt
      Remove unused templates.
      
    * build.conf
      Make serf and zlib normal external projects.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1572265 13f79535-47bb-0310-9956-ffa450edef68

--- build/generator/gen_msvc_dsp.py
+++ build/generator/gen_msvc_dsp.py
@@ -127,8 +127,6 @@ class Generator(gen_win.WinGeneratorBase):
                         (
                           ('project_guid', self.makeguid('svn_locale')),
                         ))
-    self.write_zlib_project_file('zlib.dsp')
-    self.write_serf_project_file('serf.dsp')
     install_targets = self.get_install_targets()
 
     targets = [ ]
