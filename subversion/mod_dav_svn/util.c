320420f0e6fc32b32264e2f858e3c0c32481cb20
Ben Reser <breser@apache.org>
2014-02-26 22:34:39 +0000

    Remove svn__strtoff() which was used for pre 1.0 APR compat.
    * subversion/include/private/svn_string_private.h
      (svn_strtoff): Remove.
    
    * subversion/libsvn_subr/string.c
      (svn__strtoff): Remove.
    
    * subversion/mod_dav_svn/util.c
      (): No longer include private/svn_string_private.h
      (request_body_to_string): Use apr_strtoff() directly.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1572311 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/mod_dav_svn/util.c
+++ subversion/mod_dav_svn/util.c
@@ -37,7 +37,6 @@
 
 #include "dav_svn.h"
 #include "private/svn_fspath.h"
-#include "private/svn_string_private.h"
 
 dav_error *
 dav_svn__new_error(apr_pool_t *pool,
@@ -750,7 +749,7 @@ request_body_to_string(svn_string_t **request_str,
   content_length_str = apr_table_get(r->headers_in, "Content-Length");
   if (content_length_str)
     {
-      if (svn__strtoff(&content_length, content_length_str, &endp, 10)
+      if (apr_strtoff(&content_length, content_length_str, &endp, 10)
           || endp == content_length_str || *endp || content_length < 0)
         {
           ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, "Invalid Content-Length");
