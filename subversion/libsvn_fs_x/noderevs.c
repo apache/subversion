apache/subversion

commit 348dc931855f51f83ae243e6bc4cd7c5e57966a8

For TXN IDs in FSX, use a single integer instead of a two-part struct. This allows for packing the TXN ID into the same struct that holds the revision info for committed data.
Most of this commit is code churn replacing (pointers to) structs with
the new integer value.  Similarly, NULL values cannot be used anymore
and must be replaced by the new SVN_FS_X__INVALID_TXN_ID constant.

* subversion/libsvn_fs_x/id.h
  (svn_fs_x__txn_id_t): Define the new data type for TXN IDs. 
  (SVN_FS_X__INVALID_TXN_ID): Define the corresponding NULL constant.
  (svn_fs_x__id_txn_parse,
   svn_fs_x__id_txn_unparse): Drop.  These will be replaced by new
                              private API functions in utils.h
  (svn_fs_x__id_txn_used,
   svn_fs_x__id_txn_reset, 
   svn_fs_x__txn_id_t svn_fs_x__id_txn_id,
   svn_fs_x__id_txn_create_root,
   svn_fs_x__id_txn_create): Switch TXN_ID type in these APIs
                             from struct to integer type.

* subversion/libsvn_fs_x/id.c
  (fs_x__id_t): Use the new data type to store the TXN_ID member.
  (txn_id_parse): Simplify calling the new utils API function.
  (svn_fs_x__id_txn_used,
   svn_fs_x__id_txn_reset): Update function signature and "no value" handling.
  (svn_fs_x__id_txn_parse,
   svn_fs_x__id_txn_unparse): Drop.
  (svn_fs_x__id_txn_id,
   svn_fs_x__id_is_txn): Update function signatures and TXN_ID references. 
  (svn_fs_x__id_unparse): Update serializer to write the TXN_ID as a single
                          value instead of a value pair.
  (svn_fs_x__id_eq): The struct has become smaller. 
  (svn_fs_x__id_check_related): TXN_ID-based checks are now simple.
  (svn_fs_x__id_txn_create_root,
   svn_fs_id_t *svn_fs_x__id_create_root): Update function signatures and
                                           TXN_ID references.

  (svn_fs_x__id_create_root,
   svn_fs_x__id_rev_create,
   svn_fs_x__id_parse): Update TXN_ID references and "no value" handling.


* subversion/libsvn_fs_x/util.h
  (svn_fs_x__txn_name,

subversion/libsvn_fs_x/noderevs.c
@@ -97,7 +97,7 @@ typedef struct shared_representation_t
      intra-node uniqification content. */
   struct
   {
-    svn_fs_x__id_part_t txn_id;
+    svn_fs_x__txn_id_t txn_id;
     apr_uint64_t number;
   } uniquifier;
 
@@ -584,7 +584,7 @@ write_reps(svn_packed__int_stream_t *rep_stream,
       svn_packed__add_uint(rep_stream, rep->item_index);
       svn_packed__add_uint(rep_stream, rep->size);
       svn_packed__add_uint(rep_stream, rep->expanded_size);
-      
+
       svn_packed__add_bytes(digest_stream,
                             (const char *)rep->md5_digest,
                             sizeof(rep->md5_digest));
@@ -656,11 +656,11 @@ svn_fs_x__write_noderevs_container(svn_stream_t *stream,
         = &APR_ARRAY_IDX(container->noderevs, i, binary_noderev_t);
 
       svn_packed__add_uint(noderevs_stream, noderev->flags);
-      
+
       svn_packed__add_uint(noderevs_stream, noderev->id);
       svn_packed__add_uint(noderevs_stream, noderev->predecessor_id);
       svn_packed__add_uint(noderevs_stream, noderev->predecessor_count);
-      
+
       svn_packed__add_uint(noderevs_stream, noderev->copyfrom_path);
       svn_packed__add_int(noderevs_stream, noderev->copyfrom_rev);
       svn_packed__add_uint(noderevs_stream, noderev->copyroot_path);
@@ -670,9 +670,7 @@ svn_fs_x__write_noderevs_container(svn_stream_t *stream,
       svn_packed__add_uint(noderevs_stream, noderev->data_rep.representation);
 
       svn_packed__add_int(noderevs_stream,
-                          noderev->data_rep.uniquifier.txn_id.revision);
-      svn_packed__add_uint(noderevs_stream,
-                           noderev->data_rep.uniquifier.txn_id.number);
+                          noderev->data_rep.uniquifier.txn_id);
       svn_packed__add_uint(noderevs_stream,
                            noderev->data_rep.uniquifier.number);
 
@@ -802,7 +800,7 @@ svn_fs_x__read_noderevs_container(svn_fs_x__noderevs_t **container,
 
       APR_ARRAY_PUSH(noderevs->ids, binary_id_t) = id;
     }
-    
+
   /* read rep arrays */
   SVN_ERR(read_reps(&noderevs->data_reps, data_reps_stream, digests_stream,
                     result_pool));
@@ -833,10 +831,8 @@ svn_fs_x__read_noderevs_container(svn_fs_x__noderevs_t **container,
       noderev.data_rep.representation
         = (int)svn_packed__get_uint(noderevs_stream);
 
-      noderev.data_rep.uniquifier.txn_id.revision
-        = (svn_revnum_t)svn_packed__get_int(noderevs_stream);
-      noderev.data_rep.uniquifier.txn_id.number
-        = svn_packed__get_uint(noderevs_stream);
+      noderev.data_rep.uniquifier.txn_id
+        = svn_packed__get_int(noderevs_stream);
       noderev.data_rep.uniquifier.number
         = svn_packed__get_uint(noderevs_stream);
 

subversion/libsvn_fs_x/cached_data.c
@@ -219,7 +219,8 @@ open_and_seek_revision(apr_file_t **file,
   SVN_ERR(svn_fs_x__ensure_revision_exists(rev, fs, pool));
 
   SVN_ERR(svn_fs_x__open_pack_or_rev_file(&rev_file, fs, rev, pool));
-  SVN_ERR(svn_fs_x__item_offset(&offset, &sub_item, fs, rev, NULL, item,
+  SVN_ERR(svn_fs_x__item_offset(&offset, &sub_item, fs, rev,
+                                SVN_FS_X__INVALID_TXN_ID, item,
                                 pool));
   SVN_ERR(aligned_seek(fs, rev_file, NULL, offset, pool));
 
@@ -242,12 +243,12 @@ open_and_seek_transaction(apr_file_t **file,
   apr_uint32_t sub_item = 0;
 
   SVN_ERR(svn_io_file_open(&rev_file,
-                           svn_fs_x__path_txn_proto_rev(fs, &rep->txn_id,
+                           svn_fs_x__path_txn_proto_rev(fs, rep->txn_id,
                                                         pool),
                            APR_READ | APR_BUFFERED, APR_OS_DEFAULT, pool));
 
   SVN_ERR(svn_fs_x__item_offset(&offset, &sub_item, fs, SVN_INVALID_REVNUM,
-                                &rep->txn_id, rep->item_index, pool));
+                                rep->txn_id, rep->item_index, pool));
   SVN_ERR(aligned_seek(fs, rev_file, NULL, offset, pool));
 
   *file = rev_file;
@@ -264,7 +265,7 @@ open_and_seek_representation(apr_file_t **file_p,
                              representation_t *rep,
                              apr_pool_t *pool)
 {
-  if (! svn_fs_x__id_txn_used(&rep->txn_id))
+  if (! svn_fs_x__id_txn_used(rep->txn_id))
     return open_and_seek_revision(file_p, fs, rep->revision, rep->item_index,
                                   pool);
   else
@@ -335,7 +336,8 @@ get_node_revision_body(node_revision_t **noderev_p,
           apr_off_t offset;
           apr_uint32_t sub_item;
           SVN_ERR(svn_fs_x__item_offset(&offset, &sub_item, fs,
-                                        rev_item->revision, NULL,
+                                        rev_item->revision,
+                                        SVN_FS_X__INVALID_TXN_ID,
                                         rev_item->number, pool));
           key.revision = svn_fs_x__packed_base_rev(fs, rev_item->revision);
           key.second = offset;
@@ -440,7 +442,8 @@ svn_fs_x__get_mergeinfo_count(apr_int64_t *count,
           svn_boolean_t is_cached;
 
           SVN_ERR(svn_fs_x__item_offset(&offset, &sub_item, fs,
-                                        rev_item->revision, NULL,
+                                        rev_item->revision,
+                                        SVN_FS_X__INVALID_TXN_ID,
                                         rev_item->number, pool));
           key.revision = svn_fs_x__packed_base_rev(fs, rev_item->revision);
           key.second = offset;
@@ -571,7 +574,7 @@ create_rep_state_body(rep_state_t **rep_state,
   rs->ver = -1;
   rs->start = -1;
 
-  if (ffd->rep_header_cache && !svn_fs_x__id_txn_used(&rep->txn_id))
+  if (ffd->rep_header_cache && !svn_fs_x__id_txn_used(rep->txn_id))
     SVN_ERR(svn_cache__get((void **) &rh, &is_cached,
                            ffd->rep_header_cache, &key, pool));
 
@@ -599,13 +602,14 @@ create_rep_state_body(rep_state_t **rep_state,
       /* we will need the on-disk location for non-txn reps */
       apr_off_t offset;
       apr_uint32_t sub_item;
-      if (! svn_fs_x__id_txn_used(&rep->txn_id))
+      if (! svn_fs_x__id_txn_used(rep->txn_id))
         SVN_ERR(svn_fs_x__item_offset(&offset, &sub_item,
-                                      fs, rep->revision, NULL,
+                                      fs, rep->revision,
+                                      SVN_FS_X__INVALID_TXN_ID,
                                       rep->item_index, pool));
 
       /* is rep stored in some star-deltified container? */
-      if (! svn_fs_x__id_txn_used(&rep->txn_id))
+      if (! svn_fs_x__id_txn_used(rep->txn_id))
         {
           svn_boolean_t in_container = TRUE;
           if (sub_item == 0)
@@ -665,7 +669,7 @@ create_rep_state_body(rep_state_t **rep_state,
       SVN_ERR(svn_fs_x__read_rep_header(&rh, rs->file->stream, pool));
       SVN_ERR(svn_fs_x__get_file_offset(&rs->start, rs->file->file, pool));
 
-      if (! svn_fs_x__id_txn_used(&rep->txn_id))
+      if (! svn_fs_x__id_txn_used(rep->txn_id))
         {
           SVN_ERR(block_read(NULL, fs, rep->revision, rep->item_index,
                               rs->file->file, pool, pool));
@@ -1105,7 +1109,7 @@ build_rep_list(apr_array_header_t **list,
 
       /* for txn reps and containered reps, there won't be a cached
        * combined window */
-      if (!svn_fs_x__id_txn_used(&rep.txn_id)
+      if (!svn_fs_x__id_txn_used(rep.txn_id)
           && rep_header->type != svn_fs_x__rep_container)
         SVN_ERR(get_cached_combined_window(window_p, rs, &is_cached, pool));
 
@@ -1211,7 +1215,8 @@ auto_set_start_offset(rep_state_t *rs, apr_pool_t *pool)
   if (rs->start == -1)
     {
       SVN_ERR(svn_fs_x__item_offset(&rs->start, &rs->sub_item,
-                                    rs->file->fs, rs->revision, NULL,
+                                    rs->file->fs, rs->revision,
+                                    SVN_FS_X__INVALID_TXN_ID,
                                     rs->item_index, pool));
       rs->start += rs->header_size;
     }
@@ -1502,7 +1507,7 @@ init_rep_state(rep_state_t *rs,
   SVN_ERR_ASSERT(entry->type >= SVN_FS_X__ITEM_TYPE_FILE_REP
                  && entry->type <= SVN_FS_X__ITEM_TYPE_DIR_PROPS);
   SVN_ERR_ASSERT(entry->item_count == 1);
-  
+
   shared_file->file = file;
   shared_file->stream = stream;
   shared_file->fs = fs;
@@ -1960,7 +1965,7 @@ get_dir_contents(apr_hash_t *entries,
 {
   svn_stream_t *contents;
 
-  if (noderev->data_rep && svn_fs_x__id_txn_used(&noderev->data_rep->txn_id))
+  if (noderev->data_rep && svn_fs_x__id_txn_used(noderev->data_rep->txn_id))
     {
       const char *filename
         = svn_fs_x__path_txn_node_children(fs, noderev->id, pool);
@@ -2094,7 +2099,7 @@ locate_dir_cache(svn_fs_t *fs,
              A NULL key causes a cache miss. */
           *key = NULL;
         }
-      
+
       return ffd->dir_cache;
     }
 }
@@ -2198,7 +2203,7 @@ svn_fs_x__get_proplist(apr_hash_t **proplist_p,
   apr_hash_t *proplist;
   svn_stream_t *stream;
 
-  if (noderev->prop_rep && svn_fs_x__id_txn_used(&noderev->prop_rep->txn_id))
+  if (noderev->prop_rep && svn_fs_x__id_txn_used(noderev->prop_rep->txn_id))
     {
       const char *filename
         = svn_fs_x__path_txn_node_props(fs, noderev->id, pool);
@@ -2264,7 +2269,8 @@ svn_fs_x__get_changes(apr_array_header_t **changes,
       apr_uint32_t sub_item;
       pair_cache_key_t key;
 
-      SVN_ERR(svn_fs_x__item_offset(&offset, &sub_item, fs, rev, NULL,
+      SVN_ERR(svn_fs_x__item_offset(&offset, &sub_item, fs, rev,
+                                    SVN_FS_X__INVALID_TXN_ID,
                                     SVN_FS_X__ITEM_INDEX_CHANGES, pool));
       key.revision = svn_fs_x__packed_base_rev(fs, rev);
       key.second = offset;
@@ -2621,11 +2627,12 @@ block_read(void **result,
 
   /* don't try this on transaction protorev files */
   SVN_ERR_ASSERT(SVN_IS_VALID_REVNUM(revision));
-  
+
   /* index lookup: find the OFFSET of the item we *must* read plus (in the
    * "do-while" block) the list of items in the same block. */
   SVN_ERR(svn_fs_x__item_offset(&wanted_offset, &wanted_sub_item, fs,
-                                revision, NULL, item_index, iterpool));
+                                revision, SVN_FS_X__INVALID_TXN_ID,
+                                item_index, iterpool));
 
   offset = wanted_offset;
   do

subversion/libsvn_fs_x/changes.c
@@ -81,7 +81,9 @@ typedef struct binary_change_t
    * Empty, if REV_ID is not "used". */
   svn_fs_x__id_part_t node_id;
   svn_fs_x__id_part_t copy_id;
-  svn_fs_x__id_part_t rev_id;  /* txn ID if CHANGE_TXN_NODE set in FLAGS */
+  svn_fs_x__id_part_t rev_id;  /* if CHANGE_TXN_NODE set in FLAGS:
+                                  revision = SVN_INVALID_REVNUM
+                                  number = TXN_ID */
 
 } binary_change_t;
 
@@ -181,13 +183,20 @@ append_change(svn_fs_x__changes_t *changes,
     {
       binary_change.node_id = *svn_fs_x__id_node_id(info->node_rev_id);
       binary_change.copy_id = *svn_fs_x__id_copy_id(info->node_rev_id);
-      binary_change.rev_id = is_txn_id
-                           ? *svn_fs_x__id_txn_id(info->node_rev_id)
-                           : *svn_fs_x__id_rev_item(info->node_rev_id);
+      if (is_txn_id)
+        {
+          binary_change.rev_id.revision = SVN_INVALID_REVNUM;
+          binary_change.rev_id.number = svn_fs_x__id_txn_id(info->node_rev_id);
+        }
+      else
+        {
+          binary_change.rev_id = *svn_fs_x__id_rev_item(info->node_rev_id);
+        }
     }
   else
     {
-      svn_fs_x__id_txn_reset(&binary_change.rev_id);
+      binary_change.rev_id.revision = SVN_INVALID_REVNUM;
+      svn_fs_x__id_txn_reset((svn_fs_x__txn_id_t *)&binary_change.rev_id.number);
     }
 
   APR_ARRAY_PUSH(changes->changes, binary_change_t) = binary_change;
@@ -274,14 +283,15 @@ svn_fs_x__changes_get_list(apr_array_header_t **list,
                                                      &change->path.len,
                                                      pool);
 
-      if (svn_fs_x__id_txn_used(&binary_change->rev_id))
+      if (binary_change->flags & CHANGE_TXN_NODE)
         info->node_rev_id
-          = (binary_change->flags & CHANGE_TXN_NODE)
-          ? svn_fs_x__id_txn_create(&binary_change->node_id,
+          = svn_fs_x__id_txn_create(&binary_change->node_id,
                                     &binary_change->copy_id,
-                                    &binary_change->rev_id,
-                                    pool)
-          : svn_fs_x__id_rev_create(&binary_change->node_id,
+                                    binary_change->rev_id.number,
+                                    pool);
+      else if (binary_change->rev_id.revision != SVN_INVALID_REVNUM)
+        info->node_rev_id
+          = svn_fs_x__id_rev_create(&binary_change->node_id,
                                     &binary_change->copy_id,
                                     &binary_change->rev_id,
                                     pool);
@@ -545,14 +555,15 @@ svn_fs_x__changes_get_list_func(void **out,
         = svn_fs_x__string_table_get_func(paths, binary_change->path,
                                           &change->path.len, pool);
 
-      if (svn_fs_x__id_txn_used(&binary_change->rev_id))
+      if (binary_change->flags & CHANGE_TXN_NODE)
         info->node_rev_id
-          = (binary_change->flags & CHANGE_TXN_NODE)
-          ? svn_fs_x__id_txn_create(&binary_change->node_id,
+          = svn_fs_x__id_txn_create(&binary_change->node_id,
                                     &binary_change->copy_id,
-                                    &binary_change->rev_id,
-                                    pool)
-          : svn_fs_x__id_rev_create(&binary_change->node_id,
+                                    binary_change->rev_id.number,
+                                    pool);
+      else if (binary_change->rev_id.revision != SVN_INVALID_REVNUM)
+        info->node_rev_id
+          = svn_fs_x__id_rev_create(&binary_change->node_id,
                                     &binary_change->copy_id,
                                     &binary_change->rev_id,
                                     pool);

subversion/libsvn_fs_x/dag.c
@@ -334,7 +334,7 @@ set_entry(dag_node_t *parent,
           const char *name,
           const svn_fs_id_t *id,
           svn_node_kind_t kind,
-          const svn_fs_x__id_part_t *txn_id,
+          svn_fs_x__txn_id_t txn_id,
           apr_pool_t *pool)
 {
   node_revision_t *parent_noderev;
@@ -361,7 +361,7 @@ make_entry(dag_node_t **child_p,
            const char *parent_path,
            const char *name,
            svn_boolean_t is_dir,
-           const svn_fs_x__id_part_t *txn_id,
+           svn_fs_x__txn_id_t txn_id,
            apr_pool_t *pool)
 {
   const svn_fs_id_t *new_node_id;
@@ -455,7 +455,7 @@ svn_fs_x__dag_set_entry(dag_node_t *node,
                         const char *entry_name,
                         const svn_fs_id_t *id,
                         svn_node_kind_t kind,
-                        const svn_fs_x__id_part_t *txn_id,
+                        svn_fs_x__txn_id_t txn_id,
                         apr_pool_t *pool)
 {
   /* Check it's a directory. */
@@ -632,7 +632,7 @@ svn_fs_x__dag_revision_root(dag_node_t **node_p,
 svn_error_t *
 svn_fs_x__dag_txn_root(dag_node_t **node_p,
                        svn_fs_t *fs,
-                       const svn_fs_x__id_part_t *txn_id,
+                       svn_fs_x__txn_id_t txn_id,
                        apr_pool_t *pool)
 {
   const svn_fs_id_t *root_id, *ignored;
@@ -645,8 +645,8 @@ svn_fs_x__dag_txn_root(dag_node_t **node_p,
 svn_error_t *
 svn_fs_x__dag_txn_base_root(dag_node_t **node_p,
                             svn_fs_t *fs,
-                            const svn_fs_x__id_part_t *txn_id,
-                             apr_pool_t *pool)
+                            svn_fs_x__txn_id_t txn_id,
+                            apr_pool_t *pool)
 {
   const svn_fs_id_t *base_root_id, *ignored;
 
@@ -661,7 +661,7 @@ svn_fs_x__dag_clone_child(dag_node_t **child_p,
                           const char *parent_path,
                           const char *name,
                           const svn_fs_x__id_part_t *copy_id,
-                          const svn_fs_x__id_part_t *txn_id,
+                          svn_fs_x__txn_id_t txn_id,
                           svn_boolean_t is_parent_copyroot,
                           apr_pool_t *pool)
 {
@@ -734,7 +734,7 @@ svn_fs_x__dag_clone_child(dag_node_t **child_p,
 svn_error_t *
 svn_fs_x__dag_clone_root(dag_node_t **root_p,
                          svn_fs_t *fs,
-                         const svn_fs_x__id_part_t *txn_id,
+                         svn_fs_x__txn_id_t txn_id,
                          apr_pool_t *pool)
 {
   const svn_fs_id_t *base_root_id, *root_id;
@@ -761,7 +761,7 @@ svn_fs_x__dag_clone_root(dag_node_t **root_p,
 svn_error_t *
 svn_fs_x__dag_delete(dag_node_t *parent,
                      const char *name,
-                     const svn_fs_x__id_part_t *txn_id,
+                     svn_fs_x__txn_id_t txn_id,
                      apr_pool_t *pool)
 {
   node_revision_t *parent_noderev;
@@ -884,7 +884,7 @@ svn_fs_x__dag_make_file(dag_node_t **child_p,
                         dag_node_t *parent,
                         const char *parent_path,
                         const char *name,
-                        const svn_fs_x__id_part_t *txn_id,
+                        svn_fs_x__txn_id_t txn_id,
                         apr_pool_t *pool)
 {
   /* Call our little helper function */
@@ -897,7 +897,7 @@ svn_fs_x__dag_make_dir(dag_node_t **child_p,
                        dag_node_t *parent,
                        const char *parent_path,
                        const char *name,
-                       const svn_fs_x__id_part_t *txn_id,
+                       svn_fs_x__txn_id_t txn_id,
                        apr_pool_t *pool)
 {
   /* Call our little helper function */
@@ -1203,7 +1203,7 @@ svn_fs_x__dag_copy(dag_node_t *to_node,
                    svn_boolean_t preserve_history,
                    svn_revnum_t from_rev,
                    const char *from_path,
-                   const svn_fs_x__id_part_t *txn_id,
+                   svn_fs_x__txn_id_t txn_id,
                    apr_pool_t *pool)
 {
   const svn_fs_id_t *id;

subversion/libsvn_fs_x/dag.h
@@ -226,7 +226,7 @@ svn_error_t *svn_fs_x__dag_revision_root(dag_node_t **node_p,
    for a transaction, call svn_fs_x__dag_clone_root.  */
 svn_error_t *svn_fs_x__dag_txn_root(dag_node_t **node_p,
                                     svn_fs_t *fs,
-                                    const svn_fs_x__id_part_t *txn_id,
+                                    svn_fs_x__txn_id_t txn_id,
                                     apr_pool_t *pool);
 
 
@@ -234,7 +234,7 @@ svn_error_t *svn_fs_x__dag_txn_root(dag_node_t **node_p,
    allocating from POOL.  Allocate the node in TRAIL->pool.  */
 svn_error_t *svn_fs_x__dag_txn_base_root(dag_node_t **node_p,
                                          svn_fs_t *fs,
-                                         const svn_fs_x__id_part_t *txn_id,
+                                         svn_fs_x__txn_id_t txn_id,
                                          apr_pool_t *pool);
 
 
@@ -244,7 +244,7 @@ svn_error_t *svn_fs_x__dag_txn_base_root(dag_node_t **node_p,
    root directory clone.  Allocate *ROOT_P in POOL.  */
 svn_error_t *svn_fs_x__dag_clone_root(dag_node_t **root_p,
                                       svn_fs_t *fs,
-                                      const svn_fs_x__id_part_t *txn_id,
+                                      svn_fs_x__txn_id_t txn_id,
                                       apr_pool_t *pool);
 
 
@@ -298,7 +298,7 @@ svn_error_t *svn_fs_x__dag_set_entry(dag_node_t *node,
                                      const char *entry_name,
                                      const svn_fs_id_t *id,
                                      svn_node_kind_t kind,
-                                     const svn_fs_x__id_part_t *txn_id,
+                                     svn_fs_x__txn_id_t txn_id,
                                      apr_pool_t *pool);
 
 
@@ -326,7 +326,7 @@ svn_error_t *svn_fs_x__dag_clone_child(dag_node_t **child_p,
                                        const char *parent_path,
                                        const char *name,
                                        const svn_fs_x__id_part_t *copy_id,
-                                       const svn_fs_x__id_part_t *txn_id,
+                                       svn_fs_x__txn_id_t txn_id,
                                        svn_boolean_t is_parent_copyroot,
                                        apr_pool_t *pool);
 
@@ -345,7 +345,7 @@ svn_error_t *svn_fs_x__dag_clone_child(dag_node_t **child_p,
  */
 svn_error_t *svn_fs_x__dag_delete(dag_node_t *parent,
                                   const char *name,
-                                  const svn_fs_x__id_part_t *txn_id,
+                                  svn_fs_x__txn_id_t txn_id,
                                   apr_pool_t *pool);
 
 
@@ -388,7 +388,7 @@ svn_error_t *svn_fs_x__dag_make_dir(dag_node_t **child_p,
                                     dag_node_t *parent,
                                     const char *parent_path,
                                     const char *name,
-                                    const svn_fs_x__id_part_t *txn_id,
+                                    svn_fs_x__txn_id_t txn_id,
                                     apr_pool_t *pool);
 
 
@@ -499,7 +499,7 @@ svn_error_t *svn_fs_x__dag_make_file(dag_node_t **child_p,
                                      dag_node_t *parent,
                                      const char *parent_path,
                                      const char *name,
-                                     const svn_fs_x__id_part_t *txn_id,
+                                     svn_fs_x__txn_id_t txn_id,
                                      apr_pool_t *pool);
 
 
@@ -526,7 +526,7 @@ svn_error_t *svn_fs_x__dag_copy(dag_node_t *to_node,
                                 svn_boolean_t preserve_history,
                                 svn_revnum_t from_rev,
                                 const char *from_path,
-                                const svn_fs_x__id_part_t *txn_id,
+                                svn_fs_x__txn_id_t txn_id,
                                 apr_pool_t *pool);
 
 

subversion/libsvn_fs_x/fs.h
@@ -134,7 +134,7 @@ typedef struct fs_x_shared_txn_data_t
   struct fs_x_shared_txn_data_t *next;
 
   /* ID of this transaction. */
-  svn_fs_x__id_part_t txn_id;
+  svn_fs_x__txn_id_t txn_id;
 
   /* Whether the transaction's prototype revision file is locked for
      writing by any thread in this process (including the current
@@ -462,7 +462,7 @@ typedef struct representation_t
   svn_filesize_t expanded_size;
 
   /* Is this representation a transaction? */
-  svn_fs_x__id_part_t txn_id;
+  svn_fs_x__txn_id_t txn_id;
 
   /* For rep-sharing, we need a way of uniquifying node-revs which share the
      same representation (see svn_fs_x__noderev_same_rep_key() ).  So, we
@@ -470,7 +470,7 @@ typedef struct representation_t
      intra-node uniqification content. */
   struct
   {
-    svn_fs_x__id_part_t txn_id;
+    svn_fs_x__txn_id_t txn_id;
     apr_uint64_t number;
   } uniquifier;
 } representation_t;

commit 5a8ca1ccc0cfe4c4aa22ec3f12597d1d8e288fa7

Fix test failures with FSX caused by r1567023.
* subversion/libsvn_fs_x/noderevs.c
  (svn_fs_x__write_noderevs_container): We forgot to reduce the number
                                        of sub-streams when we reduced
                                        the number of struct members.

git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1575642 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_fs_x/noderevs.c
@@ -588,7 +588,7 @@ svn_fs_x__write_noderevs_container(svn_stream_t *stream,
     svn_packed__create_int_substream(ids_stream, TRUE, FALSE);
 
   svn_packed__create_int_substream(noderevs_stream, FALSE, FALSE);
-  for (i = 0; i < 12; ++i)
+  for (i = 0; i < 11; ++i)
     svn_packed__create_int_substream(noderevs_stream, TRUE, FALSE);
 
   /* serialize ids array */