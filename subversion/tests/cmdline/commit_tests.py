1dfb999f5aed5215648ffdffa4ab6ba54e10ccd7
Philip Martin <philip@apache.org>
2014-03-14 23:10:05 +0000

    * subversion/tests/cmdline/commit_tests.py   (commit_mergeinfo_ood): Mark with issue number.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1577755 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/commit_tests.py
+++ subversion/tests/cmdline/commit_tests.py
@@ -3073,6 +3073,7 @@ def commit_deep_deleted(sbox):
                                         sbox.ospath('AA'),
                                         sbox.ospath('A'))
 
+@Issue(4480)
 @XFail(svntest.main.is_ra_type_dav)
 def commit_mergeinfo_ood(sbox):
   "commit of mergeinfo that should cause out of date"
