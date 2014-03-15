apache/subversion

commit 0f50ee9bd2315dfd7f030dcd6895677dfbff9942

Fix some typos.
* CHANGES:
* contrib/cgi/mirror_dir_through_svn.cgi:
* contrib/client-side/emacs/psvn.el:
* contrib/client-side/svn2cl/svn2cl.xsl:
* contrib/hook-scripts/remove-zombie-locks.py:
* contrib/hook-scripts/syntax-check.sh:
* contrib/server-side/svnstsw/configure.ac:
* notes/assurance.txt:
* notes/fs-improvements.txt:
* notes/knobs:
* notes/merge-tracking/func-spec.html:
* notes/merge-tracking/summit-survey.html:
* notes/wc-ng/design:
* notes/wc-ng/locking:
* notes/wc-ng/nodes:
* subversion/bindings/ctypes-python/csvn/core/__init__.py:
* subversion/bindings/ctypes-python/examples/example.py:
* subversion/bindings/javahl/src/org/apache/subversion/javahl/ConflictResult.java:
* subversion/bindings/javahl/src/org/apache/subversion/javahl/ISVNClient.java:
* subversion/bindings/javahl/src/org/apache/subversion/javahl/SVNClient.java:
* subversion/bindings/javahl/src/org/apache/subversion/javahl/SVNRepos.java:
* subversion/bindings/javahl/src/org/tigris/subversion/javahl/ConflictResult.java:
* subversion/bindings/javahl/src/org/tigris/subversion/javahl/PropertyData.java:
* subversion/bindings/javahl/src/org/tigris/subversion/javahl/SVNAdmin.java:
* subversion/bindings/javahl/src/org/tigris/subversion/javahl/SVNClient.java:
* subversion/bindings/javahl/src/org/tigris/subversion/javahl/SVNClientInterface.java:
* subversion/bindings/javahl/tests/org/apache/subversion/javahl/BasicTests.java:
* subversion/bindings/javahl/tests/org/tigris/subversion/javahl/BasicTests.java:
* subversion/bindings/swig/include/proxy_apr.swg:
* subversion/bindings/swig/perl/native/Client.pm:
* subversion/bindings/swig/perl/native/t/3client.t:
* subversion/bindings/swig/python/libsvn_swig_py/swigutil_py.c:
* subversion/include/private/svn_diff_tree.h:
* subversion/include/private/svn_io_private.h:
* subversion/include/private/svn_ra_svn_private.h:
* subversion/include/private/svn_string_private.h:
* subversion/include/svn_io.h:
* subversion/include/svn_iter.h:

subversion/libsvn_client/merge.c
@@ -2419,7 +2419,7 @@ merge_file_deleted(const char *relpath,
    When *SKIP is TRUE, the diff driver avoids work on getting the details
    for the closing callbacks.
 
-   The SKIP and SKIP_DESCENDANTS work independantly.
+   The SKIP and SKIP_DESCENDANTS work independently.
  */
 static svn_error_t *
 merge_dir_opened(void **new_dir_baton,
@@ -3133,7 +3133,7 @@ merge_dir_deleted(const char *relpath,
          descendants we haven't walked?
 
          Note that we aren't interested in changes, as we already verified
-         changes in the paths touched by the merge. And the existance of
+         changes in the paths touched by the merge. And the existence of
          other paths is enough to mark the directory edited */
       err = svn_wc_walk_status(merge_b->ctx->wc_ctx, local_abspath,
                                svn_depth_infinity, TRUE /* get-all */,

subversion/tests/cmdline/merge_tests.py
@@ -3223,7 +3223,7 @@ def merge_file_replace_to_mixed_rev_wc(sbox):
   # Normally we'd simply update the whole working copy, but since that would
   # defeat the purpose of this test (see the comment below), instead we'll
   # update only "." using --depth empty.  This preserves the intent of the
-  # orginal mixed-rev test for this issue, but allows the merge tracking
+  # original mixed-rev test for this issue, but allows the merge tracking
   # logic to consider r3 as valid for reverse merging.
   svntest.actions.run_and_verify_svn(None, None, [],
                                      'up', '--depth', 'empty', wc_dir)
@@ -9563,7 +9563,7 @@ def new_subtrees_should_not_break_merge(sbox):
   wc_status.tweak(wc_rev=9)
 
   # Yet another test for issue #3067.  Merge -rX:Y, where X>Y (reverse merge)
-  # and the merge target has a subtree that came into existance at some rev
+  # and the merge target has a subtree that came into existence at some rev
   # N where X < N < Y.  This merge should simply delete the subtree.
   #
   # For this test merge -r9:2 to A_COPY.  This should revert all the merges
@@ -11818,7 +11818,7 @@ def subtree_source_missing_in_requested_range(sbox):
                                 psi_COPY_path : '/A/D/H/psi:3,8' })
 
   # Now test a reverse merge where part of the requested range postdates
-  # a subtree's existance.  Merge -r12:1 to A_COPY.  This should revert
+  # a subtree's existence.  Merge -r12:1 to A_COPY.  This should revert
   # all of the merges done thus far.  The fact that A/D/H/psi no longer
   # exists after r7 shouldn't break the subtree merge into A_COPY/D/H/psi.
   # A_COPY/D/H/psi should simply have r3 reverse merged.  No paths under

commit 20dcba4fb7eafc9f3679ad37f3e0491b1854a076

Merge r1577812,r1577813, resolving a trivial text conflict caused by r1573006
git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/1.8.x-r1577812@1577815 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_client/merge.c
@@ -3062,7 +3062,6 @@ merge_dir_deleted(const char *relpath,
   struct merge_dir_baton_t *db = dir_baton;
   const char *local_abspath = svn_dirent_join(merge_b->target->abspath,
                                               relpath, scratch_pool);
-  struct dir_delete_baton_t *delb;
   svn_boolean_t same;
   apr_hash_t *working_props;
 
@@ -3093,66 +3092,71 @@ merge_dir_deleted(const char *relpath,
                             scratch_pool, scratch_pool));
 
   if (merge_b->force_delete)
-    same = TRUE;
+    {
+      /* In this legacy mode we just assume that a directory delete
+         matches any directory. db->delete_state is NULL */
+      same = TRUE;
+    }
   else
     {
+      struct dir_delete_baton_t *delb;
+
       /* Compare the properties */
       SVN_ERR(properties_same_p(&same, left_props, working_props,
                                 scratch_pool));
-    }
+      delb = db->delete_state;
+      assert(delb != NULL);
 
-  delb = db->delete_state;
-  assert(delb != NULL);
-
-  if (! same)
-    {
-      delb->found_edit = TRUE;
-    }
-  else
-    {
-      store_path(delb->compared_abspaths, local_abspath);
-    }
-
-  if (delb->del_root != db)
-    return SVN_NO_ERROR;
-
-  if (delb->found_edit)
-    same = FALSE;
-  else if (merge_b->force_delete)
-    same = TRUE;
-  else
-    {
-      apr_array_header_t *ignores;
-      svn_error_t *err;
-      same = TRUE;
-
-      SVN_ERR(svn_wc_get_default_ignores(&ignores, merge_b->ctx->config,
-                                         scratch_pool));
-
-      /* None of the descendants was modified, but maybe there are
-         descendants we haven't walked?
-
-         Note that we aren't interested in changes, as we already verified
-         changes in the paths touched by the merge. And the existance of
-         other paths is enough to mark the directory edited */
-      err = svn_wc_walk_status(merge_b->ctx->wc_ctx, local_abspath,
-                               svn_depth_infinity, TRUE /* get-all */,
-                               FALSE /* no-ignore */,
-                               TRUE /* ignore-text-mods */, ignores,
-                               verify_touched_by_del_check, delb,
-                               merge_b->ctx->cancel_func,
-                               merge_b->ctx->cancel_baton,
-                               scratch_pool);
-
-      if (err)
+      if (! same)
         {
-          if (err->apr_err != SVN_ERR_CEASE_INVOCATION)
-            return svn_error_trace(err);
-
-          svn_error_clear(err);
+          delb->found_edit = TRUE;
+        }
+      else
+        {
+          store_path(delb->compared_abspaths, local_abspath);
         }
 
-      same = ! delb->found_edit;
+      if (delb->del_root != db)
+        return SVN_NO_ERROR;
+
+      if (delb->found_edit)
+        same = FALSE;
+      else if (merge_b->force_delete)
+        same = TRUE;
+      else
+        {
+          apr_array_header_t *ignores;
+          svn_error_t *err;
+          same = TRUE;
+
+          SVN_ERR(svn_wc_get_default_ignores(&ignores, merge_b->ctx->config,
+                                             scratch_pool));
+
+          /* None of the descendants was modified, but maybe there are
+             descendants we haven't walked?
+
+             Note that we aren't interested in changes, as we already verified
+             changes in the paths touched by the merge. And the existence of
+             other paths is enough to mark the directory edited */
+          err = svn_wc_walk_status(merge_b->ctx->wc_ctx, local_abspath,
+                                   svn_depth_infinity, TRUE /* get-all */,
+                                   FALSE /* no-ignore */,
+                                   TRUE /* ignore-text-mods */, ignores,
+                                   verify_touched_by_del_check, delb,
+                                   merge_b->ctx->cancel_func,
+                                   merge_b->ctx->cancel_baton,
+                                   scratch_pool);
+
+          if (err)
+            {
+              if (err->apr_err != SVN_ERR_CEASE_INVOCATION)
+                return svn_error_trace(err);
+
+              svn_error_clear(err);
+            }
+
+          same = ! delb->found_edit;
+        }
     }
 
   if (same && !merge_b->dry_run)

subversion/tests/cmdline/merge_tests.py
@@ -19143,6 +19143,30 @@ def merge_to_empty_target_merge_to_infinite_target(sbox):
   # Commit the merge.
   #sbox.simple_commit()
 
+def merge_dir_delete_force(sbox):
+  "merge a directory delete with --force"
+
+  sbox.build()
+
+  sbox.simple_rm('A/D/G')
+  sbox.simple_commit() # r2
+
+  sbox.simple_update(revision=1)
+
+  # Just merging r2 on r1 succeeds
+  svntest.actions.run_and_verify_svn(sbox.wc_dir, None, [],
+                                     'merge', '-c2', '^/', sbox.wc_dir,
+                                     '--ignore-ancestry')
+
+  # Bring working copy to r1 again
+  svntest.actions.run_and_verify_svn(sbox.wc_dir, None, [],
+                                     'revert', '-R', sbox.wc_dir)
+
+  # But when using --force this same merge caused a segfault in 1.8.0-1.8.8
+  svntest.actions.run_and_verify_svn(sbox.wc_dir, None, [],
+                                     'merge', '-c2', '^/', sbox.wc_dir,
+                                     '--ignore-ancestry', '--force')
+
 ########################################################################
 # Run the tests
 
@@ -19288,6 +19312,7 @@ test_list = [ None,
               single_editor_drive_merge_notifications,
               conflicted_split_merge_with_resolve,
               merge_to_empty_target_merge_to_infinite_target,
+              merge_dir_delete_force,
              ]
 
 if __name__ == '__main__':