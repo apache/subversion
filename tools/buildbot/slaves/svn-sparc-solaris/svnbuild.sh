1783b0bf078e511d69af569682d5bdc9ddb4822f
Philip Martin <philip@apache.org>
2014-03-18 19:18:50 +0000

    Initial layout for svn-sparc-solaris buildbot slave.
    * tools/buildbot/slaves/svn-sparc-solaris: New.
    * tools/buildbot/slaves/svn-sparc-solaris/svnbuild.sh: New.
    * tools/buildbot/slaves/svn-sparc-solaris/svncheck.sh: New.
    * tools/buildbot/slaves/svn-sparc-solaris/svnclean.sh: New.
    * tools/buildbot/slaves/svn-sparc-solaris/svnenv.sh: New.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1579021 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tools/buildbot/slaves/svn-sparc-solaris/svnbuild.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing,
+#  software distributed under the License is distributed on an
+#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#  KIND, either express or implied.  See the License for the
+#  specific language governing permissions and limitations
+#  under the License.
+
+set -x
+. ../svnenv.sh
+
+echo "============ autogen.sh"
+./autogen.sh || exit $?
+
+echo "============ configure"
+./configure CC='cc -m64' \
+  --with-apr=/export/home/wandisco/buildbot/install \
+  --with-apr-util=/export/home/wandisco/buildbot/install \
+  --with-serf=/export/home/wandisco/buildbot/install \
+  --with-apxs=/export/home/wandisco/buildbot/install/bin/apxs \
+  --with-sqlite=/export/home/wandisco/buildbot/sqlite-amalgamation-3071501/sqlite3.c \
+  --enable-optimize \
+  --disable-static \
+  || exit $?
+
+echo "============ make"
+make -j30 || exit $?
+
+exit 0
