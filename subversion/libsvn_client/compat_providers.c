f669a35d5ab39460e71173f3885c3ae850bc7cad
Bert Huijben <rhuijben@apache.org>
2014-03-27 21:14:22 +0000

    * subversion/libsvn_client/compat_providers.c   (svn_client_get_windows_simple_provider):     Revert accidental remove of this function in r1582467     (where I originally moved this function to deprecated.c, but reverted      that part in the final version).
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1582478 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/compat_providers.c
+++ subversion/libsvn_client/compat_providers.c
@@ -66,6 +66,15 @@ void svn_client_get_simple_provider(svn_auth_provider_object_t **provider,
   svn_auth_get_simple_provider2(provider, NULL, NULL, pool);
 }
 
+#if defined(WIN32) && !defined(__MINGW32__)
+void
+svn_client_get_windows_simple_provider(svn_auth_provider_object_t **provider,
+                                       apr_pool_t *pool)
+{
+  svn_auth_get_windows_simple_provider(provider, pool);
+}
+#endif /* WIN32 */
+
 void svn_client_get_username_provider(svn_auth_provider_object_t **provider,
                                       apr_pool_t *pool)
 {
