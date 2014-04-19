612c9d2fe63625b5b48834fb7395e8c6b416085d
Stefan Fuhrmann <stefan2@apache.org>
2014-04-19 22:40:11 +0000

    Follow-up to r1588651: Missed one update causing crash.
    * subversion/libsvn_fs_fs/caching.c
      (svn_fs_fs__initialize_caches): Another cache instance that need
                                      FS->POOL lifetime.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1588713 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_fs/caching.c
+++ subversion/libsvn_fs_fs/caching.c
@@ -457,7 +457,7 @@ svn_fs_fs__initialize_caches(svn_fs_t *fs,
                        fs->pool));
 
   /* 1st level DAG node cache */
-  ffd->dag_node_cache = svn_fs_fs__create_dag_cache(pool);
+  ffd->dag_node_cache = svn_fs_fs__create_dag_cache(fs->pool);
 
   /* Very rough estimate: 1K per directory. */
   SVN_ERR(create_cache(&(ffd->dir_cache),
