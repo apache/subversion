82fb34b19b0fa5f69d6a10e723d4a16e250791bf
Stefan Fuhrmann <stefan2@apache.org>
2014-04-23 21:03:28 +0000

    Reverting r1589262 as we can't have a normalized repository path on all platforms.
    Because source and target of a hotcopy share the same UUID, they also
    share the same mutexes.  Attempting to acquire the same locks on them
    will result in deadlocks on some systems.  Therefore, we can only take
    out the write lock on the dest repo (which is 1.8 and earlier behavior).
    
    * subversion/libsvn_fs_fs/fs.c
      (fs_serialized_init): Remove path component from hash key.
    
    * subversion/libsvn_fs_fs/hotcopy.c
      (svn_fs_fs__hotcopy): Acquire only the write lock on DST_FS, which
                            allows us to also get the pack lock on SRC_FS
                            without deadlocks.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1589518 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_fs/fs.c
+++ subversion/libsvn_fs_fs/fs.c
@@ -77,7 +77,6 @@ fs_serialized_init(svn_fs_t *fs, apr_pool_t *common_pool, apr_pool_t *pool)
 
   SVN_ERR_ASSERT(fs->uuid);
   key = apr_pstrcat(pool, SVN_FSFS_SHARED_USERDATA_PREFIX, fs->uuid,
-                    svn_fs__canonicalize_abspath(fs->path, pool),
                     SVN_VA_NULL);
   status = apr_pool_userdata_get(&val, key, common_pool);
   if (status)
