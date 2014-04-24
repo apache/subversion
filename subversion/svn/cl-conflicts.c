apache/subversion

commit 6a36306fac02e6e55019a95fb3dcf65419eca909

When the command line client shows tree conflict information, be more explicit about the fact that an incoming deletion might be part of an incoming move, and that a missing target during a merge might have been deleted or moved in the merge target's history. The current wording is a common source of user confusion. I'm not really sure if this change helps users a lot but we'll see.
If we ever grow support for incoming moves, we'll need to update
these descriptions again anyway.

* subversion/svn/cl-conflicts.c
  (local_reason_str): Add 'operation' parameter, and print
   print additional hints about possible causes if the
   operation is a merge.
  (incoming_action_str): Any incoming delete might be part of
   an incoming move, so say "incoming delete or move".

* subversion/tests/cmdline/stat_tests.py
  (status_with_tree_conflicts): Adjust expected output.

* subversion/tests/cmdline/switch_tests.py
  (tree_conflicts_on_switch_1_2,
   tree_conflicts_on_switch_2_1,
   tree_conflicts_on_switch_2_2,
   tree_conflicts_on_switch_3): Adjust expected output.

* subversion/tests/cmdline/update_tests.py
  (tree_conflicts_on_update_1_2,
   tree_conflicts_on_update_2_1,
   tree_conflicts_on_update_2_2,
   tree_conflicts_on_update_3): Adjust expected output.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1589662 13f79535-47bb-0310-9956-ffa450edef68

subversion/svn/cl-conflicts.c
@@ -69,7 +69,8 @@ static const svn_token_map_t map_conflict_kind_xml[] =
 /* Return a localised string representation of the local part of a conflict;
    NULL for non-localised odd cases. */
 static const char *
-local_reason_str(svn_node_kind_t kind, svn_wc_conflict_reason_t reason)
+local_reason_str(svn_node_kind_t kind, svn_wc_conflict_reason_t reason,
+                 svn_wc_operation_t operation)
 {
   switch (kind)
     {
@@ -83,7 +84,10 @@ local_reason_str(svn_node_kind_t kind, svn_wc_conflict_reason_t reason)
           case svn_wc_conflict_reason_deleted:
             return _("local file delete");
           case svn_wc_conflict_reason_missing:
-            return _("local file missing");
+            if (operation == svn_wc_operation_merge)
+              return _("local file missing (deleted or moved away?)");
+            else
+              return _("local file missing");
           case svn_wc_conflict_reason_unversioned:
             return _("local file unversioned");
           case svn_wc_conflict_reason_added:
@@ -106,7 +110,10 @@ local_reason_str(svn_node_kind_t kind, svn_wc_conflict_reason_t reason)
           case svn_wc_conflict_reason_deleted:
             return _("local dir delete");
           case svn_wc_conflict_reason_missing:
-            return _("local dir missing");
+            if (operation == svn_wc_operation_merge)
+              return _("local dir missing (deleted or moved away?)");
+            else
+              return _("local dir missing");
           case svn_wc_conflict_reason_unversioned:
             return _("local dir unversioned");
           case svn_wc_conflict_reason_added:
@@ -142,7 +149,7 @@ incoming_action_str(svn_node_kind_t kind, svn_wc_conflict_action_t action)
             case svn_wc_conflict_action_add:
               return _("incoming file add");
             case svn_wc_conflict_action_delete:
-              return _("incoming file delete");
+              return _("incoming file delete or move");
             case svn_wc_conflict_action_replace:
               return _("incoming file replace");
           }
@@ -155,7 +162,7 @@ incoming_action_str(svn_node_kind_t kind, svn_wc_conflict_action_t action)
             case svn_wc_conflict_action_add:
               return _("incoming dir add");
             case svn_wc_conflict_action_delete:
-              return _("incoming dir delete");
+              return _("incoming dir delete or move");
             case svn_wc_conflict_action_replace:
               return _("incoming dir replace");
           }
@@ -267,7 +274,8 @@ svn_cl__get_human_readable_tree_conflict_description(
         incoming_kind = conflict->src_right_version->node_kind;
     }
 
-  reason = local_reason_str(conflict->node_kind, conflict->reason);
+  reason = local_reason_str(conflict->node_kind, conflict->reason,
+                            conflict->operation);
   action = incoming_action_str(incoming_kind, conflict->action);
   operation = operation_str(conflict->operation);
   SVN_ERR_ASSERT(operation);

subversion/tests/cmdline/stat_tests.py
@@ -1747,11 +1747,11 @@ def status_with_tree_conflicts(sbox):
   # check status of G
   expected = svntest.verify.UnorderedOutput(
          ["A  +  C %s\n" % rho,
-          "      >   local file edit, incoming file delete upon update\n",
+          "      >   local file edit, incoming file delete or move upon update\n",
           "D     C %s\n" % pi,
           "      >   local file delete, incoming file edit upon update\n",
           "!     C %s\n" % tau,
-          "      >   local file delete, incoming file delete upon update\n",
+          "      >   local file delete, incoming file delete or move upon update\n",
          ] + svntest.main.summary_of_conflicts(tree_conflicts=3))
 
   svntest.actions.run_and_verify_svn(None,
@@ -1765,9 +1765,9 @@ def status_with_tree_conflicts(sbox):
           "D     C          2        2 jrandom      %s\n" % pi,
           "      >   local file delete, incoming file edit upon update\n",
           "A  +  C          -        1 jrandom      %s\n" % rho,
-          "      >   local file edit, incoming file delete upon update\n",
+          "      >   local file edit, incoming file delete or move upon update\n",
           "!     C                                  %s\n" % tau,
-          "      >   local file delete, incoming file delete upon update\n",
+          "      >   local file delete, incoming file delete or move upon update\n",
          ] + svntest.main.summary_of_conflicts(tree_conflicts=3))
 
   svntest.actions.run_and_verify_svn(None,

subversion/tests/cmdline/switch_tests.py
@@ -2248,7 +2248,7 @@ def tree_conflicts_on_switch_1_2(sbox):
   expected_info = {
     'F/alpha' : {
       'Tree conflict' :
-        '^local file delete, incoming file delete upon switch'
+        '^local file delete, incoming file delete or move upon switch'
         + ' Source  left: .file.*/F/alpha@2'
         + ' Source right: .none.*(/F/alpha@3)?$',
     },
@@ -2266,7 +2266,7 @@ def tree_conflicts_on_switch_1_2(sbox):
     },
     'D/D1' : {
       'Tree conflict' :
-        '^local dir delete, incoming dir delete upon switch'
+        '^local dir delete, incoming dir delete or move upon switch'
         + ' Source  left: .dir.*/D/D1@2'
         + ' Source right: .none.*(/D/D1@3)?$',
     },
@@ -2331,37 +2331,37 @@ def tree_conflicts_on_switch_2_1(sbox):
   expected_info = {
     'F/alpha' : {
       'Tree conflict' :
-        '^local file edit, incoming file delete upon switch'
+        '^local file edit, incoming file delete or move upon switch'
         + ' Source  left: .file.*/F/alpha@2'
         + ' Source right: .none.*(/F/alpha@3)?$',
     },
     'DF/D1' : {
       'Tree conflict' :
-        '^local dir edit, incoming dir delete upon switch'
+        '^local dir edit, incoming dir delete or move upon switch'
         + ' Source  left: .dir.*/DF/D1@2'
         + ' Source right: .none.*(/DF/D1@3)?$',
     },
     'DDF/D1' : {
       'Tree conflict' :
-        '^local dir edit, incoming dir delete upon switch'
+        '^local dir edit, incoming dir delete or move upon switch'
         + ' Source  left: .dir.*/DDF/D1@2'
         + ' Source right: .none.*(/DDF/D1@3)?$',
     },
     'D/D1' : {
       'Tree conflict' :
-        '^local dir edit, incoming dir delete upon switch'
+        '^local dir edit, incoming dir delete or move upon switch'
         + ' Source  left: .dir.*/D/D1@2'
         + ' Source right: .none.*(/D/D1@3)?$',
     },
     'DD/D1' : {
       'Tree conflict' :
-        '^local dir edit, incoming dir delete upon switch'
+        '^local dir edit, incoming dir delete or move upon switch'
         + ' Source  left: .dir.*/DD/D1@2'
         + ' Source right: .none.*(/DD/D1@3)?$',
     },
     'DDD/D1' : {
       'Tree conflict' :
-        '^local dir edit, incoming dir delete upon switch'
+        '^local dir edit, incoming dir delete or move upon switch'
         + ' Source  left: .dir.*/DDD/D1@2'
         + ' Source right: .none.*(/DDD/D1@3)?$',
     },
@@ -2428,37 +2428,37 @@ def tree_conflicts_on_switch_2_2(sbox):
   expected_info = {
     'F/alpha' : {
       'Tree conflict' :
-        '^local file delete, incoming file delete upon switch'
+        '^local file delete, incoming file delete or move upon switch'
         + ' Source  left: .file.*/F/alpha@2'
         + ' Source right: .none.*(/F/alpha@3)?$',
     },
     'DF/D1' : {
       'Tree conflict' :
-        '^local dir delete, incoming dir delete upon switch'
+        '^local dir delete, incoming dir delete or move upon switch'
         + ' Source  left: .dir.*/DF/D1@2'
         + ' Source right: .none.*(/DF/D1@3)?$',
     },
     'DDF/D1' : {
       'Tree conflict' :
-        '^local dir delete, incoming dir delete upon switch'
+        '^local dir delete, incoming dir delete or move upon switch'
         + ' Source  left: .dir.*/DDF/D1@2'
         + ' Source right: .none.*(/DDF/D1@3)?$',
     },
     'D/D1' : {
       'Tree conflict' :
-        '^local dir delete, incoming dir delete upon switch'
+        '^local dir delete, incoming dir delete or move upon switch'
         + ' Source  left: .dir.*/D/D1@2'
         + ' Source right: .none.*(/D/D1@3)?$',
     },
     'DD/D1' : {
       'Tree conflict' :
-        '^local dir delete, incoming dir delete upon switch'
+        '^local dir delete, incoming dir delete or move upon switch'
         + ' Source  left: .dir.*/DD/D1@2'
         + ' Source right: .none.*(/DD/D1@3)?$',
     },
     'DDD/D1' : {
       'Tree conflict' :
-        '^local dir delete, incoming dir delete upon switch'
+        '^local dir delete, incoming dir delete or move upon switch'
         + ' Source  left: .dir.*/DDD/D1@2'
         + ' Source right: .none.*(/DDD/D1@3)?$',
     },
@@ -2515,37 +2515,37 @@ def tree_conflicts_on_switch_3(sbox):
   expected_info = {
     'F/alpha' : {
       'Tree conflict' :
-        '^local file delete, incoming file delete upon switch'
+        '^local file delete, incoming file delete or move upon switch'
         + ' Source  left: .file.*/F/alpha@2'
         + ' Source right: .none.*(/F/alpha@3)?$',
     },
     'DF/D1' : {
       'Tree conflict' :
-        '^local dir delete, incoming dir delete upon switch'
+        '^local dir delete, incoming dir delete or move upon switch'
         + ' Source  left: .dir.*/DF/D1@2'
         + ' Source right: .none.*(/DF/D1@3)?$',
     },
     'DDF/D1' : {
       'Tree conflict' :
-        '^local dir delete, incoming dir delete upon switch'
+        '^local dir delete, incoming dir delete or move upon switch'
         + ' Source  left: .dir.*/DDF/D1@2'
         + ' Source right: .none.*(/DDF/D1@3)?$',
     },
     'D/D1' : {
       'Tree conflict' :
-        '^local dir delete, incoming dir delete upon switch'
+        '^local dir delete, incoming dir delete or move upon switch'
         + ' Source  left: .dir.*/D/D1@2'
         + ' Source right: .none.*(/D/D1@3)?$',
     },
     'DD/D1' : {
       'Tree conflict' :
-        '^local dir delete, incoming dir delete upon switch'
+        '^local dir delete, incoming dir delete or move upon switch'
         + ' Source  left: .dir.*/DD/D1@2'
         + ' Source right: .none.*(/DD/D1@3)?$',
     },
     'DDD/D1' : {
       'Tree conflict' :
-        '^local dir delete, incoming dir delete upon switch'
+        '^local dir delete, incoming dir delete or move upon switch'
         + ' Source  left: .dir.*/DDD/D1@2'
         + ' Source right: .none.*(/DDD/D1@3)?$',
     },

subversion/tests/cmdline/update_tests.py
@@ -4238,7 +4238,7 @@ def tree_conflicts_on_update_1_2(sbox):
   expected_info = {
     'F/alpha' : {
       'Tree conflict' :
-        '^local file delete, incoming file delete upon update'
+        '^local file delete, incoming file delete or move upon update'
         + ' Source  left: .file.*/F/alpha@2'
         + ' Source right: .none.*(/F/alpha@3)?$',
     },
@@ -4256,7 +4256,7 @@ def tree_conflicts_on_update_1_2(sbox):
     },
     'D/D1' : {
       'Tree conflict' :
-        '^local dir delete, incoming dir delete upon update'
+        '^local dir delete, incoming dir delete or move upon update'
         + ' Source  left: .dir.*/D/D1@2'
         + ' Source right: .none.*(/D/D1@3)?$',
     },
@@ -4321,37 +4321,37 @@ def tree_conflicts_on_update_2_1(sbox):
   expected_info = {
     'F/alpha' : {
       'Tree conflict' :
-        '^local file edit, incoming file delete upon update'
+        '^local file edit, incoming file delete or move upon update'
         + ' Source  left: .file.*/F/alpha@2'
         + ' Source right: .none.*(/F/alpha@3)?$',
     },
     'DF/D1' : {
       'Tree conflict' :
-        '^local dir edit, incoming dir delete upon update'
+        '^local dir edit, incoming dir delete or move upon update'
         + ' Source  left: .dir.*/DF/D1@2'
         + ' Source right: .none.*(/DF/D1@3)?$',
     },
     'DDF/D1' : {
       'Tree conflict' :
-        '^local dir edit, incoming dir delete upon update'
+        '^local dir edit, incoming dir delete or move upon update'
         + ' Source  left: .dir.*/DDF/D1@2'
         + ' Source right: .none.*(/DDF/D1@3)?$',
     },
     'D/D1' : {
       'Tree conflict' :
-        '^local dir edit, incoming dir delete upon update'
+        '^local dir edit, incoming dir delete or move upon update'
         + ' Source  left: .dir.*/D/D1@2'
         + ' Source right: .none.*(/D/D1@3)?$',
     },
     'DD/D1' : {
       'Tree conflict' :
-        '^local dir edit, incoming dir delete upon update'
+        '^local dir edit, incoming dir delete or move upon update'
         + ' Source  left: .dir.*/DD/D1@2'
         + ' Source right: .none.*(/DD/D1@3)?$',
     },
     'DDD/D1' : {
       'Tree conflict' :
-        '^local dir edit, incoming dir delete upon update'
+        '^local dir edit, incoming dir delete or move upon update'
         + ' Source  left: .dir.*/DDD/D1@2'
         + ' Source right: .none.*(/DDD/D1@3)?$',
     },
@@ -4426,37 +4426,37 @@ def tree_conflicts_on_update_2_2(sbox):
   expected_info = {
     'F/alpha' : {
       'Tree conflict' :
-        '^local file delete, incoming file delete upon update'
+        '^local file delete, incoming file delete or move upon update'
         + ' Source  left: .file.*/F/alpha@2'
         + ' Source right: .none.*(/F/alpha@3)?$',
     },
     'DF/D1' : {
       'Tree conflict' :
-        '^local dir delete, incoming dir delete upon update'
+        '^local dir delete, incoming dir delete or move upon update'
         + ' Source  left: .dir.*/DF/D1@2'
         + ' Source right: .none.*(/DF/D1@3)?$',
     },
     'DDF/D1' : {
       'Tree conflict' :
-        '^local dir delete, incoming dir delete upon update'
+        '^local dir delete, incoming dir delete or move upon update'
         + ' Source  left: .dir.*/DDF/D1@2'
         + ' Source right: .none.*(/DDF/D1@3)?$',
     },
     'D/D1' : {
       'Tree conflict' :
-        '^local dir delete, incoming dir delete upon update'
+        '^local dir delete, incoming dir delete or move upon update'
         + ' Source  left: .dir.*/D/D1@2'
         + ' Source right: .none.*(/D/D1@3)?$',
     },
     'DD/D1' : {
       'Tree conflict' :
-        '^local dir delete, incoming dir delete upon update'
+        '^local dir delete, incoming dir delete or move upon update'
         + ' Source  left: .dir.*/DD/D1@2'
         + ' Source right: .none.*(/DD/D1@3)?$',
     },
     'DDD/D1' : {
       'Tree conflict' :
-        '^local dir delete, incoming dir delete upon update'
+        '^local dir delete, incoming dir delete or move upon update'
         + ' Source  left: .dir.*/DDD/D1@2'
         + ' Source right: .none.*(/DDD/D1@3)?$',
     },
@@ -4584,37 +4584,37 @@ def tree_conflicts_on_update_3(sbox):
   expected_info = {
     'F/alpha' : {
       'Tree conflict' :
-        '^local file delete, incoming file delete upon update'
+        '^local file delete, incoming file delete or move upon update'
         + ' Source  left: .file.*/F/alpha@2'
         + ' Source right: .none.*(/F/alpha@3)?$',
     },
     'DF/D1' : {
       'Tree conflict' :
-        '^local dir delete, incoming dir delete upon update'
+        '^local dir delete, incoming dir delete or move upon update'
         + ' Source  left: .dir.*/DF/D1@2'
         + ' Source right: .none.*(/DF/D1@3)?$',
     },
     'DDF/D1' : {
       'Tree conflict' :
-        '^local dir delete, incoming dir delete upon update'
+        '^local dir delete, incoming dir delete or move upon update'
         + ' Source  left: .dir.*/DDF/D1@2'
         + ' Source right: .none.*(/DDF/D1@3)?$',
     },
     'D/D1' : {
       'Tree conflict' :
-        '^local dir delete, incoming dir delete upon update'
+        '^local dir delete, incoming dir delete or move upon update'
         + ' Source  left: .dir.*/D/D1@2'
         + ' Source right: .none.*(/D/D1@3)?$',
     },
     'DD/D1' : {
       'Tree conflict' :
-        '^local dir delete, incoming dir delete upon update'
+        '^local dir delete, incoming dir delete or move upon update'
         + ' Source  left: .dir.*/DD/D1@2'
         + ' Source right: .none.*(/DD/D1@3)?$',
     },
     'DDD/D1' : {
       'Tree conflict' :
-        '^local dir delete, incoming dir delete upon update'
+        '^local dir delete, incoming dir delete or move upon update'
         + ' Source  left: .dir.*/DDD/D1@2'
         + ' Source right: .none.*(/DDD/D1@3)?$',
     },

commit 1b6dfe34a9035076eaf80e8161284e07be52b779

* subversion/svn/cl-conflicts.c   (local_reason_str): Use an easier to read syntax in    the new description for 'local delete'.
* subversion/tests/cmdline/tree_conflict_tests.py
  (actual_only_node_behaviour): Adjust expected output.
   Also fixes a test failure accidentally introduced by r1589662.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1589691 13f79535-47bb-0310-9956-ffa450edef68

subversion/svn/cl-conflicts.c
@@ -85,7 +85,7 @@ local_reason_str(svn_node_kind_t kind, svn_wc_conflict_reason_t reason,
             return _("local file delete");
           case svn_wc_conflict_reason_missing:
             if (operation == svn_wc_operation_merge)
-              return _("local file missing (deleted or moved away?)");
+              return _("local file missing or deleted or moved away");
             else
               return _("local file missing");
           case svn_wc_conflict_reason_unversioned:
@@ -111,7 +111,7 @@ local_reason_str(svn_node_kind_t kind, svn_wc_conflict_reason_t reason,
             return _("local dir delete");
           case svn_wc_conflict_reason_missing:
             if (operation == svn_wc_operation_merge)
-              return _("local dir missing (deleted or moved away?)");
+              return _("local dir missing or deleted or moved away");
             else
               return _("local dir missing");
           case svn_wc_conflict_reason_unversioned:

subversion/tests/cmdline/tree_conflict_tests.py
@@ -1255,7 +1255,7 @@ def actual_only_node_behaviour(sbox):
 
   # info
   expected_info = {
-    'Tree conflict': 'local file missing, incoming file edit upon merge.*',
+    'Tree conflict': 'local file missing or deleted or moved away, incoming file edit upon merge.*',
     'Name': 'foo',
     'Schedule': 'normal',
     'Node Kind': 'none',