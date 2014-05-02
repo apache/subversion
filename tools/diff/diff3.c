f157e0fbab6e4a3dce394c67d44c8b6615f95b77
Daniel Shahaf <danielsh@apache.org>
2014-05-02 23:26:57 +0000

    * tools/diff/diff3.c   (main): Fix labels of original and mine.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1592108 13f79535-47bb-0310-9956-ffa450edef68

--- tools/diff/diff3.c
+++ tools/diff/diff3.c
@@ -131,9 +131,9 @@ int main(int argc, const char *argv[])
           }
         case 'L':
           if (!conflict_modified)
-            conflict_modified = apr_pstrcat(pool, "||||||| ", arg, SVN_VA_NULL);
+            conflict_modified = apr_pstrcat(pool, "<<<<<<< ", arg, SVN_VA_NULL);
           else if (!conflict_original)
-            conflict_original = apr_pstrcat(pool, "<<<<<<< ", arg, SVN_VA_NULL);
+            conflict_original = apr_pstrcat(pool, "||||||| ", arg, SVN_VA_NULL);
           else if (!conflict_latest)
             conflict_latest = apr_pstrcat(pool, ">>>>>>> ", arg, SVN_VA_NULL);
           else
