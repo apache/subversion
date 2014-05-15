3db9c96762eb55e9f6538cc2e8fcdf6ec871ab89
Stefan Fuhrmann <stefan2@apache.org>
2014-05-15 10:48:37 +0000

    Introduce FSFS expert tool 'svnfsfs'.  It is intended to grow various FSFS-specific commands in the future - like recreating repcache.db etc.
    For now, it provides two commands to read (dump) and write (load)
    format 7 index information.  With these, corrupted repositories can
    be manipulated / fixed by hand or script.
    
    The former fsfs-stats tool becomes a sub-command as well.
    
    * build.conf
      (svnfsfs): Define new target for the new tool.
      (__ALL__): Add new tool as dependency.
      (__MORE__): Remove fsfs-stats dependency.
      (fsfs-stats): Drop superseded target.
    
    * subversion/include/svn_error_codes.h
      (SVN_ERR_FS_UNSUPPORTED_TYPE,
       SVN_ERR_INVALID_INPUT): Declare new error types to be used by the
                               new tool.
    
    * tools/server-side/fsfs-stats.c
      (): Removed, non-boilerplate sources moved to ./svnfsfs/stats-cmd.c
    
    * tools/server-side/svnfsfs
      (): New directory for the new tool; ignore new binary.
    
    * tools/server-side/svnfsfs/svnfsfs.h
      (): New file containing the tool globals.
    
    * tools/server-side/svnfsfs/svnfsfs.c
      (): Boilerplate command line interface, command and parameter definition.
    
    * tools/server-side/svnfsfs/dump-index-cmd.c
      (): New 'dump-index' sub-command implementation.
    
    * tools/server-side/svnfsfs/load-index-cmd.c
      (): New 'load-index' sub-command implementation.
    
    * tools/server-side/svnfsfs/stats-cmd.c
      (): New 'stats' sub-command implementation.  Code taken from former
          stand-alone tool. 
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1594860 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tools/server-side/svnfsfs/dump-index-cmd.c
@@ -0,0 +1,134 @@
+/* dump-index-cmd.c -- implements the dump-index sub-command.
+ *
+ * ====================================================================
+ *    Licensed to the Apache Software Foundation (ASF) under one
+ *    or more contributor license agreements.  See the NOTICE file
+ *    distributed with this work for additional information
+ *    regarding copyright ownership.  The ASF licenses this file
+ *    to you under the Apache License, Version 2.0 (the
+ *    "License"); you may not use this file except in compliance
+ *    with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing,
+ *    software distributed under the License is distributed on an
+ *    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *    KIND, either express or implied.  See the License for the
+ *    specific language governing permissions and limitations
+ *    under the License.
+ * ====================================================================
+ */
+
+#include "svn_dirent_uri.h"
+#include "svn_pools.h"
+
+#include "../../../subversion/libsvn_fs_fs/fs.h"
+#include "../../../subversion/libsvn_fs_fs/index.h"
+#include "../../../subversion/libsvn_fs_fs/rev_file.h"
+#include "../../../subversion/libsvn_fs_fs/util.h"
+#include "../../../subversion/libsvn_fs/fs-loader.h"
+
+#include "svnfsfs.h"
+
+/* Return the 8 digit hex string for FNVV1, allocated in POOL.
+ */
+static const char *
+fnv1_to_string(apr_uint32_t fnv1,
+               apr_pool_t *pool)
+{
+  /* Construct a checksum object containing FNV1. */
+  svn_checksum_t checksum = { NULL, svn_checksum_fnv1a_32 };
+  apr_uint32_t digest = htonl(fnv1);
+  checksum.digest = (const unsigned char *)&digest;
+
+  /* Convert the digest to hex. */
+  return svn_checksum_to_cstring_display(&checksum, pool);
+}
+
+/* Map svn_fs_fs__p2l_entry_t.type to C string. */
+static const char *item_type_str[]
+  = {"none ", "frep ", "drep ", "fprop", "dprop", "node ", "chgs ", "rep  "};
+
+/* Read the repository at PATH beginning with revision START_REVISION and
+ * return the result in *FS.  Allocate caches with MEMSIZE bytes total
+ * capacity.  Use POOL for non-cache allocations.
+ */
+static svn_error_t *
+dump_index(const char *path,
+           svn_revnum_t revision,
+           apr_pool_t *pool)
+{
+  svn_fs_fs__revision_file_t *rev_file;
+  svn_fs_t *fs;
+  fs_fs_data_t *ffd;
+  int i;
+  apr_off_t offset, max_offset;
+  apr_pool_t *iterpool = svn_pool_create(pool);
+
+  /* Check repository type and open it. */
+  SVN_ERR(open_fs(&fs, path, pool));
+  ffd = fs->fsap_data;
+
+  /* Check the FS format. */
+  if (! svn_fs_fs__use_log_addressing(fs, revision))
+    return svn_error_create(SVN_ERR_FS_UNSUPPORTED_FORMAT, NULL, NULL);
+
+  /* Revision & index file access object. */
+  SVN_ERR(svn_fs_fs__open_pack_or_rev_file(&rev_file, fs, revision, pool));
+
+  /* Offset range to cover. */
+  SVN_ERR(svn_fs_fs__p2l_get_max_offset(&max_offset, fs, rev_file, revision,
+                                        pool));
+
+  /* Write header line. */
+  printf("       Start       Length Type   Revision     Item Checksum\n");
+
+  /* Walk through all P2L index entries in offset order. */
+  for (offset = 0; offset < max_offset; )
+    {
+      apr_array_header_t *entries;
+
+      /* Read entries for the next block.  There will be no overlaps since
+       * we start at the first offset not covered. */
+      svn_pool_clear(iterpool);
+      SVN_ERR(svn_fs_fs__p2l_index_lookup(&entries, fs, rev_file, revision,
+                                          offset, ffd->p2l_page_size,
+                                          iterpool));
+
+      /* Print entries for this block, one line per entry. */
+      for (i = 0; i < entries->nelts && offset < max_offset; ++i)
+        {
+          const svn_fs_fs__p2l_entry_t *entry
+            = &APR_ARRAY_IDX(entries, i, const svn_fs_fs__p2l_entry_t);
+          const char *type_str
+            = entry->type < (sizeof(item_type_str) / sizeof(item_type_str[0]))
+            ? item_type_str[entry->type]
+            : "???";
+
+          offset = entry->offset + entry->size;
+
+          printf("%12" APR_UINT64_T_HEX_FMT " %12" APR_UINT64_T_HEX_FMT
+                 " %s %9ld %8" APR_UINT64_T_HEX_FMT " %s\n",
+                 (apr_uint64_t)entry->offset, (apr_uint64_t)entry->size,
+                 type_str, entry->item.revision, entry->item.number,
+                 fnv1_to_string(entry->fnv1_checksum, iterpool));
+        }
+    }
+
+  svn_pool_destroy(iterpool);
+
+  return SVN_NO_ERROR;
+}
+
+/* This implements `svn_opt_subcommand_t'. */
+svn_error_t *
+subcommand__dump_index(apr_getopt_t *os, void *baton, apr_pool_t *pool)
+{
+  svnfsfs__opt_state *opt_state = baton;
+
+  SVN_ERR(dump_index(opt_state->repository_path,
+                     opt_state->start_revision.value.number, pool));
+
+  return SVN_NO_ERROR;
+}
---
+++ tools/server-side/svnfsfs/load-index-cmd.c
@@ -0,0 +1,390 @@
+/* load-index-cmd.c -- implements the dump-index sub-command.
+ *
+ * ====================================================================
+ *    Licensed to the Apache Software Foundation (ASF) under one
+ *    or more contributor license agreements.  See the NOTICE file
+ *    distributed with this work for additional information
+ *    regarding copyright ownership.  The ASF licenses this file
+ *    to you under the Apache License, Version 2.0 (the
+ *    "License"); you may not use this file except in compliance
+ *    with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing,
+ *    software distributed under the License is distributed on an
+ *    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *    KIND, either express or implied.  See the License for the
+ *    specific language governing permissions and limitations
+ *    under the License.
+ * ====================================================================
+ */
+
+#include "svn_ctype.h"
+#include "svn_dirent_uri.h"
+#include "svn_io.h"
+#include "svn_pools.h"
+
+#include "private/svn_sorts_private.h"
+
+#include "../../../subversion/libsvn_fs_fs/index.h"
+#include "../../../subversion/libsvn_fs_fs/util.h"
+
+#include "svn_private_config.h"
+
+#include "svnfsfs.h"
+
+/* Calculate the FNV1 checksum over the offset range in REV_FILE, covered by
+ * ENTRY.  Store the result in ENTRY->FNV1_CHECKSUM.  Use POOL for temporary
+ * allocations. */
+static svn_error_t *
+calc_fnv1(svn_fs_fs__p2l_entry_t *entry,
+          svn_fs_fs__revision_file_t *rev_file,
+          apr_pool_t *pool)
+{
+  unsigned char buffer[4096];
+  svn_checksum_t *checksum;
+  svn_checksum_ctx_t *context
+    = svn_checksum_ctx_create(svn_checksum_fnv1a_32x4, pool);
+  apr_off_t size = entry->size;
+
+  /* Special rules apply to unused sections / items.  The data must be a
+   * sequence of NUL bytes (not checked here) and the checksum is fixed to 0.
+   */
+  if (entry->type == SVN_FS_FS__ITEM_TYPE_UNUSED)
+    {
+      entry->fnv1_checksum = 0;
+      return SVN_NO_ERROR;
+    }
+
+  /* Read the block and feed it to the checksum calculator. */
+  SVN_ERR(svn_io_file_seek(rev_file->file, APR_SET, &entry->offset, pool));
+  while (size > 0)
+    {
+      apr_size_t to_read = size > sizeof(buffer)
+                         ? sizeof(buffer)
+                         : (apr_size_t)size;
+      SVN_ERR(svn_io_file_read_full2(rev_file->file, buffer, to_read, NULL,
+                                     NULL, pool));
+      SVN_ERR(svn_checksum_update(context, buffer, to_read));
+      size -= to_read;
+    }
+
+  /* Store final checksum in ENTRY. */
+  SVN_ERR(svn_checksum_final(&checksum, context, pool));
+  entry->fnv1_checksum = ntohl(*(const apr_uint32_t *)checksum->digest);
+
+  return SVN_NO_ERROR;
+}
+
+/* In FS, (re-)write the P2L index for the revision file starting at
+ * REVISION.  All entries to write are of type svn_fs_fs__p2l_entry_t* and
+ * given in ENTRIES.  The FVN1 checksums are not taken from ENTRIES but are
+ * begin calculated as we go.  Use POOL for temporary allocations.
+ */
+static svn_error_t *
+write_p2l_index(svn_fs_t *fs,
+                svn_revnum_t revision,
+                apr_array_header_t *entries,
+                apr_pool_t *pool)
+{
+  apr_file_t *proto_index;
+  const char *protoname, *filename;
+
+  /* Use a subpool for immediate temp file cleanup at the end of this
+   * function. */
+  apr_pool_t *subpool = svn_pool_create(pool);
+  apr_pool_t *iterpool = svn_pool_create(pool);
+  svn_fs_fs__revision_file_t *rev_file;
+  int i;
+
+  /* Create a proto-index file. */
+  SVN_ERR(svn_io_open_unique_file3(NULL, &protoname, NULL,
+                                   svn_io_file_del_on_pool_cleanup,
+                                   subpool, subpool));
+  SVN_ERR(svn_fs_fs__p2l_proto_index_open(&proto_index, protoname, subpool));
+
+  /* Write ENTRIES to proto-index file and calculate checksums as we go. */
+  SVN_ERR(svn_fs_fs__open_pack_or_rev_file(&rev_file, fs, revision, pool));
+  for (i = 0; i < entries->nelts; ++i)
+    {
+      svn_fs_fs__p2l_entry_t *entry
+        = APR_ARRAY_IDX(entries, i, svn_fs_fs__p2l_entry_t *);
+      svn_pool_clear(iterpool);
+
+      SVN_ERR(calc_fnv1(entry, rev_file, iterpool));
+      SVN_ERR(svn_fs_fs__p2l_proto_index_add_entry(proto_index, entry,
+                                                   iterpool));
+    }
+
+  /* Convert proto-index into final index and move it into position.
+   * Note that REV_FILE contains the start revision of the shard file if it
+   * has been packed while REVISION may be somewhere in the middle.  For
+   * non-packed shards, they will have identical values. */
+  SVN_ERR(svn_io_file_flush(proto_index, iterpool));
+  filename = svn_fs_fs__path_p2l_index(fs, rev_file->start_revision,
+                                       rev_file->is_packed, subpool);
+  SVN_ERR(svn_fs_fs__p2l_index_create(fs, filename, protoname,
+                                      rev_file->start_revision, subpool));
+
+  /* Temp file cleanup. */
+  svn_pool_destroy(iterpool);
+  svn_pool_destroy(subpool);
+
+  return SVN_NO_ERROR;
+}
+
+/* A svn_sort__array compatible comparator function, sorting the
+ * svn_fs_fs__p2l_entry_t** given in LHS, RHS by revision. */
+static int
+compare_p2l_entry_revision(const void *lhs,
+                           const void *rhs)
+{
+  const svn_fs_fs__p2l_entry_t *lhs_entry
+    =*(const svn_fs_fs__p2l_entry_t **)lhs;
+  const svn_fs_fs__p2l_entry_t *rhs_entry
+    =*(const svn_fs_fs__p2l_entry_t **)rhs;
+
+  if (lhs_entry->item.revision < rhs_entry->item.revision)
+    return -1;
+
+  return lhs_entry->item.revision == rhs_entry->item.revision ? 0 : 1;
+}
+
+/* In FS, (re-)write the L2P index with all entries taken from ENTRIES.
+ * The revisions found in ENTRIES define the index file to write.  The
+ * entries are given as svn_fs_fs__p2l_entry_t* (sic!) and the ENTRIES
+ * array will be reordered.  Use POOL for temporary allocations.
+ */
+static svn_error_t *
+write_l2p_index(svn_fs_t *fs,
+                apr_array_header_t *entries,
+                apr_pool_t *pool)
+{
+  apr_file_t *proto_index;
+  const char *protoname, *filename;
+
+  /* Use a subpool for immediate temp file cleanup at the end of this
+   * function. */
+  apr_pool_t *subpool = svn_pool_create(pool);
+  apr_pool_t *iterpool = svn_pool_create(pool);
+  int i;
+  svn_revnum_t last_revision = SVN_INVALID_REVNUM;
+  svn_revnum_t revision = SVN_INVALID_REVNUM;
+
+  /* L2P index must be written in revision order.
+   * Sort ENTRIES accordingly. */
+  svn_sort__array(entries, compare_p2l_entry_revision);
+
+  /* Find the first revision in the index
+   * (must exist since no truly empty revs are allowed). */
+  for (i = 0; i < entries->nelts && !SVN_IS_VALID_REVNUM(revision); ++i)
+    revision = APR_ARRAY_IDX(entries, i, const svn_fs_fs__p2l_entry_t *)
+               ->item.revision;
+
+  /* Create the temporary proto-rev file. */
+  SVN_ERR(svn_io_open_unique_file3(NULL, &protoname, NULL,
+                                   svn_io_file_del_on_pool_cleanup,
+                                   subpool, subpool));
+  SVN_ERR(svn_fs_fs__l2p_proto_index_open(&proto_index, protoname, subpool));
+
+  /*  Write all entries. */
+  for (i = 0; i < entries->nelts; ++i)
+    {
+      const svn_fs_fs__p2l_entry_t *entry
+        = APR_ARRAY_IDX(entries, i, const svn_fs_fs__p2l_entry_t *);
+      svn_pool_clear(iterpool);
+
+      if (entry->type == SVN_FS_FS__ITEM_TYPE_UNUSED)
+        continue;
+
+      if (last_revision != entry->item.revision)
+        {
+          SVN_ERR(svn_fs_fs__l2p_proto_index_add_revision(proto_index, pool));
+          last_revision = entry->item.revision;
+        }
+
+      SVN_ERR(svn_fs_fs__l2p_proto_index_add_entry(proto_index,
+                                                   entry->offset,
+                                                   entry->item.number,
+                                                   iterpool));
+    }
+
+  /* Convert proto-index into final index and move it into position. */
+  SVN_ERR(svn_io_file_flush(proto_index, iterpool));
+  filename = svn_fs_fs__path_l2p_index(fs, revision,
+                                       svn_fs_fs__is_packed_rev(fs, revision),
+                                       subpool);
+  SVN_ERR(svn_fs_fs__l2p_index_create(fs, filename, protoname, revision,
+                                      subpool));
+
+  /* Temp file cleanup. */
+  svn_pool_destroy(iterpool);
+  svn_pool_destroy(subpool);
+
+  return SVN_NO_ERROR;
+}
+
+/* Map svn_fs_fs__p2l_entry_t.type to C string. */
+static const char *item_type_str[]
+  = {"none", "frep", "drep", "fprop", "dprop", "node", "chgs", "rep"};
+
+/* Reverse lookup in ITEM_TYPE_STR: Set *TYPE to the index that contains STR.
+ * Return an error for invalid strings. */
+static svn_error_t *
+str_to_item_type(unsigned *type,
+                 const char *str)
+{
+  unsigned i;
+  for (i = 0; i < sizeof(item_type_str) / sizeof(item_type_str[0]); ++i)
+    if (strcmp(item_type_str[i], str) == 0)
+      {
+        *type = i;
+        return SVN_NO_ERROR;
+      }
+
+  return svn_error_createf(SVN_ERR_BAD_TOKEN, NULL,
+                           _("Unknown item type '%s'"), str);
+}
+
+/* Parse the hex string given as const char * at IDX in TOKENS and return
+ * its value in *VALUE_P.  Check for index overflows and non-hex chars.
+ */
+static svn_error_t *
+token_to_i64(apr_int64_t *value_p,
+             apr_array_header_t *tokens,
+             int idx)
+{
+  const char *hex;
+  char *end;
+  apr_int64_t value;
+
+  /* Tell the user when there is not enough information. */
+  SVN_ERR_ASSERT(idx >= 0);
+  if (tokens->nelts <= idx)
+    return svn_error_createf(SVN_ERR_INVALID_INPUT, NULL,
+                             _("%i columns needed, %i provided"),
+                             idx + 1, tokens->nelts);
+
+  /* hex -> int conversion */
+  hex = APR_ARRAY_IDX(tokens, idx, const char *);
+  value = apr_strtoi64(hex, &end, 16);
+
+  /* Has the whole token be parsed without error? */
+  if (errno || *end != '\0')
+    return svn_error_createf(SVN_ERR_INVALID_INPUT, NULL,
+                             _("%s is not a value HEX string"), hex);
+
+  *value_p = value;
+  return SVN_NO_ERROR;
+}
+
+/* Parse the P2L entry given as space separated values in LINE and return it
+ * in *ENTRY.  Ignore extra columns.  Allocate the result in RESULT_POOL and
+ * use SCRATCH_POOL for temporaries.
+ */
+static svn_error_t *
+parse_index_line(svn_fs_fs__p2l_entry_t **entry,
+                 svn_stringbuf_t *line,
+                 apr_pool_t *result_pool,
+                 apr_pool_t *scratch_pool)
+{
+  apr_array_header_t *tokens = svn_cstring_split(line->data, " ", TRUE,
+                                                 scratch_pool);
+  svn_fs_fs__p2l_entry_t *result = apr_pcalloc(result_pool, sizeof(*result));
+
+  /* Parse the hex columns. */
+  SVN_ERR(token_to_i64(&result->offset, tokens, 0));
+  SVN_ERR(token_to_i64(&result->size, tokens, 1));
+  SVN_ERR(token_to_i64(&result->item.number, tokens, 4));
+
+  /* We now know that there were at least 5 columns.
+   * Parse the non-hex columns without index check. */
+  SVN_ERR(str_to_item_type(&result->type,
+                           APR_ARRAY_IDX(tokens, 2, const char *)));
+  SVN_ERR(svn_revnum_parse(&result->item.revision,
+                           APR_ARRAY_IDX(tokens, 3, const char *), NULL));
+
+  *entry = result;
+  return SVN_NO_ERROR;
+}
+
+/* Parse the space separated P2L index table from INPUT, one entry per line.
+ * Rewrite the respective index files in PATH.  Allocate from POOL. */
+static svn_error_t *
+load_index(const char *path,
+           svn_stream_t *input,
+           apr_pool_t *pool)
+{
+  svn_fs_t *fs;
+  svn_revnum_t revision = SVN_INVALID_REVNUM;
+  apr_array_header_t *entries = apr_array_make(pool, 16, sizeof(void*));
+  apr_pool_t *iterpool = svn_pool_create(pool);
+
+  /* Check repository type and open it. */
+  SVN_ERR(open_fs(&fs, path, pool));
+
+  while (TRUE)
+    {
+      svn_stringbuf_t *line;
+      svn_fs_fs__p2l_entry_t *entry;
+      svn_boolean_t eol;
+
+      /* Get the next line from the input and stop if there is none. */
+      svn_pool_clear(iterpool);
+      svn_stream_readline(input, &line, "\n", &eol, iterpool);
+      if (eol)
+        break;
+
+      /* Skip header line(s).  They contain the sub-string [Ss]tart. */
+      if (strstr(line->data, "tart"))
+        continue;
+
+      /* Ignore empty lines (mostly trailing ones but we don't really care).
+       */
+      svn_stringbuf_strip_whitespace(line);
+      if (line->len == 0)
+        continue;
+
+      /* Parse the entry and append it to ENTRIES. */
+      SVN_ERR(parse_index_line(&entry, line, pool, iterpool));
+      APR_ARRAY_PUSH(entries, svn_fs_fs__p2l_entry_t *) = entry;
+
+      /* There should be at least one item that is not empty.
+       * Get a revision from (probably inside) the respective shard. */
+      if (   revision == SVN_INVALID_REVNUM
+          && entry->item.revision != SVN_INVALID_REVNUM)
+        {
+          revision = entry->item.revision;
+
+          /* Check the FS format number. */
+          if (! svn_fs_fs__use_log_addressing(fs, revision))
+            return svn_error_create(SVN_ERR_FS_UNSUPPORTED_FORMAT, NULL, NULL);
+        }
+
+    }
+
+  /* Treat an empty array as a no-op instead error. */
+  if (entries->nelts != 0)
+    {
+      SVN_ERR(write_p2l_index(fs, revision, entries, pool));
+      SVN_ERR(write_l2p_index(fs, entries, pool));
+    }
+
+  svn_pool_destroy(iterpool);
+
+  return SVN_NO_ERROR;
+}
+
+/* This implements `svn_opt_subcommand_t'. */
+svn_error_t *
+subcommand__load_index(apr_getopt_t *os, void *baton, apr_pool_t *pool)
+{
+  svnfsfs__opt_state *opt_state = baton;
+  svn_stream_t *input;
+
+  SVN_ERR(svn_stream_for_stdin(&input, pool));
+  SVN_ERR(load_index(opt_state->repository_path, input, pool));
+
+  return SVN_NO_ERROR;
+}
