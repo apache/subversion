162450d9a07c069a098fe5421ed9991dfde6cadd
Stefan Sperling <stsp@apache.org>
2014-05-28 18:08:52 +0000

    For issue #3747, 'svn patch' support of svn:mergeinfo, make it possible to parse svn:mergeinfo diffs regardless of i18n.
    * subversion/libsvn_diff/util.c
      (display_mergeinfo_diff): Print a hunk header line for mergeinfo diffs.
       The header indicates the number of reverse-merges and forward-merges in
       the diff. Before this change, this information was only printed as part
       of output which is translated for i18n. This made it impossible to reliably
       parse svn:mergeinfo diffs from patch files.
       Futher changes to the diff parser and svn patch are coming soon.
    
    * subversion/tests/cmdline/merge_tests.py
      (merge_in_new_file_and_diff): Adjust expected output.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1598092 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/merge_tests.py
+++ subversion/tests/cmdline/merge_tests.py
@@ -1342,6 +1342,7 @@ def merge_in_new_file_and_diff(sbox):
     "Property changes on: " + url_branch_path + "\n",
     "___________________________________________________________________\n",
     "Added: " + SVN_PROP_MERGEINFO + "\n",
+    "## -0,0 +0,1 ##\n",
     "   Merged /A/B/E:r2-3\n",
   ]
   svntest.actions.run_and_verify_svn(None, expected_output, [], 'diff',
