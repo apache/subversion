cc85ed226b73b471ce01344e0bee028c630bdec1
Branko Cibej <brane@apache.org>
2014-06-09 12:00:31 +0000

    Sketch out a utility API for managing credentials from JavaHL.
    [in subversion/bindings/javahl]
    * src/org/apache/subversion/javahl/SVNUtil.java
      (SVNUtil.Credential): New; abstract base class for credential types.
      (SVNUtil.UsernameCredential,
       SVNUtil.UsernameCredential,
       SVNUtil.SSLServerCertCredential,
       SVNUtil.SSLClientCertPassphraseCredential): New; concrete credentiall types.
      (SVNUtil.getCredential,
       SVNUtil.removeCredential,
       SVNUtil.addCredential,
       SVNUtil.searchCredentials,
       SVNUtil.deleteCredentials): New; credentials management methods.
    
    * src/org/apache/subversion/javahl/callback/AuthnCallback.java
      (AuthnCallback.SSLServerCertFailures.getFailures): New.
    
    * src/org/apache/subversion/javahl/util/ConfigLib.java
      (ConfigLib.getCredential,
       ConfigLib.removeCredential,
       ConfigLib.addCredential,
       ConfigLib.searchCredentials,
       ConfigLib.deleteCredentials): New; credentials management methods.
      (ConfigLib.nativeGetCredential,
       ConfigLib.nativeRemoveCredential,
       ConfigLib.nativeAddCredential,
       ConfigLib.iterateCredentials):
       New; native method declarations for credentials management.
    
    * native/org_apache_subversion_javahl_util_ConfigLib.cpp:
       Reimplement using the new jniwrapper infrastructure.
      (Java_org_apache_subversion_javahl_util_ConfigLib_enableNativeCredentialsStore,
       Java_org_apache_subversion_javahl_util_ConfigLib_disableNativeCredentialsStore,
       Java_org_apache_subversion_javahl_util_ConfigLib_isNativeCredentialsStoreEnabled):
       Change implementation to user jniwrapper.
      (Java_org_apache_subversion_javahl_util_ConfigLib_nativeGetCredential,
       Java_org_apache_subversion_javahl_util_ConfigLib_nativeRemoveCredential,
       Java_org_apache_subversion_javahl_util_ConfigLib_nativeAddCredential,
       Java_org_apache_subversion_javahl_util_ConfigLib_iterateCredentials):
       New; native methods for credentials management.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1601352 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/javahl/src/org/apache/subversion/javahl/util/ConfigLib.java
+++ subversion/bindings/javahl/src/org/apache/subversion/javahl/util/ConfigLib.java
@@ -23,14 +23,17 @@
 
 package org.apache.subversion.javahl.util;
 
+import org.apache.subversion.javahl.callback.*;
+
 import org.apache.subversion.javahl.SVNUtil;
 import org.apache.subversion.javahl.ClientException;
 import org.apache.subversion.javahl.NativeResources;
 
-import org.apache.subversion.javahl.callback.*;
+import java.util.List;
 
 /**
- * Provides global configuration knobs.
+ * Provides global configuration knobs and
+ * Encapsulates utility functions for authentication credentials management.
  * @since 1.9
  */
 public class ConfigLib
@@ -54,4 +57,118 @@ public class ConfigLib
     /** @see SVNUtil.isNativeCredentialsStoreEnabled */
     public native boolean isNativeCredentialsStoreEnabled()
         throws ClientException;
+
+    //
+    // Credentials management
+    //
+
+    /** @see SVNUtil.getCredential */
+    public SVNUtil.Credential getCredential(String configDir,
+                                            SVNUtil.Credential.Kind kind,
+                                            String realm)
+        throws ClientException
+    {
+        return nativeGetCredential(configDir, kind.toString(), realm);
+    }
+
+    /** @see SVNUtil.removeCredential */
+    public SVNUtil.Credential removeCredential(String configDir,
+                                               SVNUtil.Credential.Kind kind,
+                                               String realm)
+        throws ClientException
+    {
+        return nativeRemoveCredential(configDir, kind.toString(), realm);
+    }
+
+    /** @see SVNUtil.addCredential */
+    public SVNUtil.Credential addCredential(String configDir,
+                                            SVNUtil.Credential credential,
+                                            boolean replace)
+        throws ClientException
+    {
+        final AuthnCallback.SSLServerCertInfo sci = credential.getServerCertInfo();
+        final AuthnCallback.SSLServerCertFailures scf = credential.getServerCertFailures();
+        return nativeAddCredential(configDir,
+                                   credential.getKind().toString(),
+                                   credential.getRealm(),
+                                   credential.getUsername(),
+                                   credential.getPassword(),
+                                   (sci != null ? sci.getHostname() : null),
+                                   (sci != null ? sci.getFingerprint() : null),
+                                   (sci != null ? sci.getValidFrom() : null),
+                                   (sci != null ? sci.getValidUntil() : null),
+                                   (sci != null ? sci.getIssuer() : null),
+                                   (sci != null ? sci.getDER() : null),
+                                   (scf != null ? scf.getFailures() : 0),
+                                   credential.getClientCertPassphrase());
+    }
+
+    /** @see SVNUtil.searchCredentials */
+    public List<SVNUtil.Credential>
+        searchCredentials(String configDir,
+                          SVNUtil.Credential.Kind kind,
+                          String realmPattern,
+                          String usernamePattern,
+                          String hostnamePattern,
+                          String textPattern)
+        throws ClientException
+    {
+        return iterateCredentials(
+            false, configDir, kind.toString(), realmPattern,
+            usernamePattern, hostnamePattern, textPattern);
+    }
+
+    /** @see SVNUtil.deleteCredentials */
+    public List<SVNUtil.Credential>
+        deleteCredentials(String configDir,
+                          SVNUtil.Credential.Kind kind,
+                          String realmPattern,
+                          String usernamePattern,
+                          String hostnamePattern,
+                          String textPattern)
+        throws ClientException
+    {
+        return iterateCredentials(
+            true, configDir, kind.toString(), realmPattern,
+            usernamePattern, hostnamePattern, textPattern);
+    }
+
+    private native SVNUtil.Credential
+        nativeGetCredential(String configDir,
+                               String kind,
+                               String realm)
+        throws ClientException;
+
+    private native SVNUtil.Credential
+        nativeRemoveCredential(String configDir,
+                               String kind,
+                               String realm)
+        throws ClientException;
+
+    private native SVNUtil.Credential
+        nativeAddCredential(String configDir,
+                            String kind,
+                            String realm,
+                            String username,
+                            String password,
+                            String serverCertHostname,
+                            String serverCertFingerprint,
+                            String serverCertValidFrom,
+                            String serverCertValidUntil,
+                            String serverCertIssuer,
+                            String serverCertDER,
+                            int serverCertFailures,
+                            String clientCertPassphrase)
+        throws ClientException;
+
+    private native List<SVNUtil.Credential>
+        iterateCredentials(boolean deleteMatching,
+                           String configDir,
+                           String kind,
+                           String realmPattern,
+                           String usernamePattern,
+                           String hostnamePattern,
+                           String textPattern)
+        throws ClientException;
 }
+
