3c17a92d7767afaac203c7f793101fefd471374c
Ivan Zhakov <ivan@apache.org>
2014-06-20 10:29:49 +0000

    On remove-log-addressing branch: Further cleanup now unused code.
    * subversion/libsvn_fs_fs/util.c
      (svn_fs_fs__path_txn_item_index): Remove implementation.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/remove-log-addressing@1604129 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_fs/util.c
+++ subversion/libsvn_fs_fs/util.c
@@ -236,15 +236,6 @@ svn_fs_fs__path_txn_dir(svn_fs_t *fs,
 }
 
 const char *
-svn_fs_fs__path_txn_item_index(svn_fs_t *fs,
-                               const svn_fs_fs__id_part_t *txn_id,
-                               apr_pool_t *pool)
-{
-  return svn_dirent_join(svn_fs_fs__path_txn_dir(fs, txn_id, pool),
-                         PATH_TXN_ITEM_INDEX, pool);
-}
-
-const char *
 svn_fs_fs__path_txn_proto_rev(svn_fs_t *fs,
                               const svn_fs_fs__id_part_t *txn_id,
                               apr_pool_t *pool)
