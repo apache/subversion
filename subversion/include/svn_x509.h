6c6822d76f0cb25785a262c558318e096178acc0
Stefan Sperling <stsp@apache.org>
2014-06-24 21:23:41 +0000

    On the svn-auth-x509 branch, make the X509 parser API public.
    * subversion/include/svn_x509.h: New. Declares svn_x509_parse_cert().
    
    * subversion/libsvn_subr/x509.h
      (svn_x509_parse_cert): Remove declaration moved to public API.
       Also remove declaration of x509parse_dn_gets.
    
    * subversion/libsvn_subr/x509parse.c
      (x509parse_dn_gets): Make static and move up in the file.
    
    * subversion/svn/auth-cmd.c: Include svn_x509.h instead of libsvn_subr/x509.h.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/svn-auth-x509@1605200 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subversion/include/svn_x509.h
@@ -0,0 +1,54 @@
+/**
+ * @copyright
+ * ====================================================================
+ *    Licensed to the Apache Software Foundation (ASF) under one
+ *    or more contributor license agreements.  See the NOTICE file
+ *    distributed with this work for additional information
+ *    regarding copyright ownership.  The ASF licenses this file
+ *    to you under the Apache License, Version 2.0 (the
+ *    "License"); you may not use this file except in compliance
+ *    with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing,
+ *    software distributed under the License is distributed on an
+ *    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *    KIND, either express or implied.  See the License for the
+ *    specific language governing permissions and limitations
+ *    under the License.
+ * ====================================================================
+ * @endcopyright
+ *
+ * @file svn_x509.h
+ * @brief Subversion's X509 parser
+ */
+
+#ifndef SVN_X509_H
+#define SVN_X509_H
+
+/* Hash keys for certificate information returned by svn_x509_parse_cert().
+ * @since New in 1.9 */
+#define SVN_X509_CERTINFO_KEY_ISSUER      "issuer"
+#define SVN_X509_CERTINFO_KEY_VALID_FROM  "valid-from"
+#define SVN_X509_CERTINFO_KEY_VALID_TO    "valid-to"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Parse x509 @a der certificate data from @a buf with length @a buflen
+ * and return certificate information in @a *cert, allocated in
+ * @a result_pool. The certinfo hash contains values of type
+ * 'const char *' keyed by SVN_X509_CERTINFO_KEY_* macros. */
+svn_error_t *
+svn_x509_parse_cert(apr_hash_t **certinfo,
+                    const char *buf,
+                    int buflen,
+                    apr_pool_t *result_pool,
+                    apr_pool_t *scratch_pool);
+
+#ifdef __cplusplus
+}
+#endif
+#endif        /* SVN_X509_H */
