09d6b8cffad45acbf7993854aed4894f836b68bb
Stefan Fuhrmann <stefan2@apache.org>
2014-06-27 15:53:28 +0000

    Follow-up r1606132:
    * tools/server-side/svnfsfs/stats-cmd.c
      (read_log_rev_or_packfile): Use a result pool with appropriate lifetime.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1606137 13f79535-47bb-0310-9956-ffa450edef68

--- tools/server-side/svnfsfs/stats-cmd.c
+++ tools/server-side/svnfsfs/stats-cmd.c
@@ -1400,7 +1400,7 @@ read_log_rev_or_packfile(fs_t *fs,
 
   /* open the pack / rev file that is covered by the p2l index */
   SVN_ERR(svn_fs_fs__open_pack_or_rev_file(&rev_file, fs->fs, base,
-                                           iterpool, iterpool));
+                                           localpool, iterpool));
   SVN_ERR(svn_fs_fs__p2l_get_max_offset(&max_offset, fs->fs, rev_file,
                                         base, localpool));
 
