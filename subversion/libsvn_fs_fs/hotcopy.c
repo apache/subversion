32e2ce5b8240d8359990fd2916adb95555bed6d9
Branko Čibej <brane@apache.org>
2014-07-28 04:52:13 +0000

    * subversion/libsvn_fs_fs/hotcopy.c   (hotcopy_body): Fix strange comment block that was introduced in r1612810.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1613872 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_fs/hotcopy.c
+++ subversion/libsvn_fs_fs/hotcopy.c
@@ -987,7 +987,7 @@ hotcopy_body(void *baton, apr_pool_t *pool)
   if (dst_ffd->format >= SVN_FS_FS__MIN_REP_SHARING_FORMAT)
     {
       /* Copy the rep cache and then remove entries for revisions
-      /* that did not make it into the destination. */
+       * that did not make it into the destination. */
       src_subdir = svn_dirent_join(src_fs->path, REP_CACHE_DB_NAME, pool);
       dst_subdir = svn_dirent_join(dst_fs->path, REP_CACHE_DB_NAME, pool);
       SVN_ERR(svn_io_check_path(src_subdir, &kind, pool));
