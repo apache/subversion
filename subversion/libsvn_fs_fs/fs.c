2bdf7e6b1eac2a3676223436e916a5eafecef385
Bert Huijben <rhuijben@apache.org>
2014-09-29 11:35:51 +0000

    Add error tracing in two functions that try to remove a repository.
    * subversion/libsvn_fs_fs/fs.c
      (fs_delete_fs): Add error tracing.
    
    * subversion/libsvn_repos/repos.c
      (svn_repos_delete): Add error tracing.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1628161 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_fs/fs.c
+++ subversion/libsvn_fs_fs/fs.c
@@ -496,7 +496,7 @@ fs_delete_fs(const char *path,
              apr_pool_t *pool)
 {
   /* Remove everything. */
-  return svn_io_remove_dir2(path, FALSE, NULL, NULL, pool);
+  return svn_error_trace(svn_io_remove_dir2(path, FALSE, NULL, NULL, pool));
 }
 
 static const svn_version_t *
