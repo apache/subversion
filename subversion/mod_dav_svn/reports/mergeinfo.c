8dc70408da171338c5a9c3ba080bf9398f2e96f1
Ben Reser <breser@apache.org>
2014-12-05 22:18:06 +0000

    Merge r1643409 from trunk:
     * r1643409
       Correctly reject requests with missing repos paths.
       Justification:
         These requests aren't valid and should result in an error.
       Votes:
         +1: breser, philip, kotkov
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/1.8.x@1643454 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/mod_dav_svn/reports/mergeinfo.c
+++ subversion/mod_dav_svn/reports/mergeinfo.c
@@ -67,6 +67,9 @@ dav_svn__get_mergeinfo_report(const dav_resource *resource,
     = apr_array_make(resource->pool, 0, sizeof(const char *));
 
   /* Sanity check. */
+  if (!resource->info->repos_path)
+    return dav_svn__new_error(resource->pool, HTTP_BAD_REQUEST, 0,
+                              "The request does not specify a repository path");
   ns = dav_svn__find_ns(doc->namespaces, SVN_XML_NAMESPACE);
   if (ns == -1)
     {
