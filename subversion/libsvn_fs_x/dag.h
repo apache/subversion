831cb15e88c0abea5c11e84e278c3550cb720f41
Stefan Fuhrmann <stefan2@apache.org>
2015-01-06 16:36:20 +0000

    Get rid of a small non-static DAG function in FSX and merge it with its sole caller.
    * subversion/libsvn_fs_x/dag.h
      (svn_fs_x__dag_dir_entries): Remove.
    
    * subversion/libsvn_fs_x/dag.c
      (dir_entry_id_from_node): Instead of calling it, insert the logic of ...
      (svn_fs_x__dag_dir_entries): ... this function.  Delete it.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1649868 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_x/dag.h
+++ subversion/libsvn_fs_x/dag.h
@@ -319,19 +319,6 @@ svn_fs_x__dag_dir_entries(apr_array_header_t **entries_p,
                           dag_node_t *node,
                           apr_pool_t *pool);
 
-/* Fetches the NODE's entries and returns a copy of the entry selected
-   by the key value given in NAME and set *DIRENT to a copy of that
-   entry. If such entry was found, the copy will be allocated in
-   RESULT_POOL.  Temporary data will be used in SCRATCH_POOL.
-   Otherwise, the *DIRENT will be set to NULL.
- */
-/* ### This function is currently only called from dag.c. */
-svn_error_t * svn_fs_x__dag_dir_entry(svn_fs_x__dirent_t **dirent,
-                                      dag_node_t *node,
-                                      const char* name,
-                                      apr_pool_t *result_pool,
-                                      apr_pool_t *scratch_pool);
-
 /* Set ENTRY_NAME in NODE to point to ID (with kind KIND), allocating
    from POOL.  NODE must be a mutable directory.  ID can refer to a
    mutable or immutable node.  If ENTRY_NAME does not exist, it will
