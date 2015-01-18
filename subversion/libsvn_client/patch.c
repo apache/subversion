e0ce4c0468064e42fc0bc4dc5ecbb350f8e741b1
Philip Martin <philip@apache.org>
2015-01-18 11:52:51 +0000

    * subversion/libsvn_client/patch.c   (sort_matched_hunks): Followup to r1644599, really implement a total order.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1652744 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/patch.c
+++ subversion/libsvn_client/patch.c
@@ -2213,26 +2213,36 @@ sort_matched_hunks(const void *a, const void *b)
   const hunk_info_t *item2 = *((const hunk_info_t * const *)b);
   svn_boolean_t matched1 = !item1->rejected && !item1->already_applied;
   svn_boolean_t matched2 = !item2->rejected && !item2->already_applied;
+  svn_linenum_t original1, original2;
 
   if (matched1 && matched2)
     {
       /* Both match so use order matched in file. */
       if (item1->matched_line > item2->matched_line)
         return 1;
+      else if (item1->matched_line == item2->matched_line)
+        return 0;
+      else
+        return -1;
     }
   else if (matched2)
     /* Only second matches, put it before first. */
     return 1;
-  else
-    {
-      /* Neither matches, sort by original_start. */
-      if (svn_diff_hunk_get_original_start(item1->hunk)
-          > svn_diff_hunk_get_original_start(item2->hunk))
-        return 1;
-    }
+  else if (matched1)
+    /* Only first matches, put it before second. */
+    return -1;
 
-  return -1;
+  /* Neither matches, sort by original_start. */
+  original1 = svn_diff_hunk_get_original_start(item1->hunk);
+  original2 = svn_diff_hunk_get_original_start(item2->hunk);
+  if (original1 > original2)
+    return 1;
+  else if (original1 == original2)
+    return 0;
+  else
+    return -1;
 }
+
 
 /* Apply a PATCH to a working copy at ABS_WC_PATH and put the result
  * into temporary files, to be installed in the working copy later.
