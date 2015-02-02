41d1ef743d0673e65f4d84b9288e3bd9df49c0b5
Branko Čibej <brane@apache.org>
2015-02-02 15:24:16 +0000

    Introduce a private libsvn_client context structure that stores context information that should not be part of the public API.
    * subversion/include/svn_client.h
      (svn_client_ctx_t): Remove the 'progress' field, which should be private.
    
    * subversion/libsvn_client/client.h
      (client_ctx_t): New; the private context struct.
       Contains the equivalent of the 'progress' field.
      (svn_client__get_private_ctx): New prototype.
    * subversion/libsvn_client/ctx.c: Include stddef.h and client.h.
      (CLIENT_CTX_MAGIC): New; magic number for the private context.
      (svn_client__get_private_ctx): Implement.
      (svn_client_create_context2): Allocate and initialize the private
       context structure, with the public context embedded within it.
    * subversion/libsvn_client/ra.c
      (progress_func): Use the total progress counter from the private context.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1656488 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/client.h
+++ subversion/libsvn_client/client.h
@@ -45,6 +45,35 @@
 extern "C" {
 #endif /* __cplusplus */
 
+
+/* Private client context.
+ *
+ * This is what is actually allocated by svn_client_create_context2(),
+ * which then returns the address of the public_ctx member. */
+typedef struct client_ctx_t
+{
+  /* Reserved field, always zero, to detect misuse of the private
+     context as a public client context. */
+  apr_uint64_t magic_null;
+
+  /* Reserved field, always set to a known magic number, to identify
+     this struct as the private client context. */
+  apr_uint64_t magic_id;
+
+  /* Total number of bytes transferred over network across all RA sessions. */
+  apr_off_t total_progress;
+
+  /* The public context. */
+  svn_client_ctx_t public_ctx;
+} client_ctx_t;
+
+
+/* Given a public client context CTX, return the private context
+   within which it is allocated. */
+client_ctx_t *
+svn_client__get_private_ctx(svn_client_ctx_t *ctx);
+
+
 /* Set *REVNUM to the revision number identified by REVISION.
 
    If REVISION->kind is svn_opt_revision_number, just use
