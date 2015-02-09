apache/subversion

commit 14dad3f061d059e0d44c1a06dac12823f101613a

Within libsvn_repos get_log functionality, pass the list of wanted revprops around as an array of svn_string_t* instead of const char*.  The added length info allows for more effient functions to be used.  Do that.
* subversion/libsvn_repos/log.c
  (fill_log_entry): try an optimistic match between received and requested
                    revprops; adapt to using svn_string_t *
  (svn_repos_get_logs4): copy & convert the incoming array


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1483570 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_repos/log.c
@@ -1100,21 +1100,47 @@ fill_log_entry(svn_log_entry_t *log_entry,
         }
       else
         {
-          /* Requested only some revprops... */
           int i;
-          for (i = 0; i < revprops->nelts; i++)
+
+          /* Requested only some revprops... */
+          
+          /* often only the standard revprops got requested and delivered.
+             In that case, we can simply pass the hash on. */
+          if (revprops->nelts == apr_hash_count(r_props) && !censor_revprops)
             {
-              char *name = APR_ARRAY_IDX(revprops, i, char *);
-              svn_string_t *value = svn_hash_gets(r_props, name);
-              if (censor_revprops
-                  && !(strcmp(name, SVN_PROP_REVISION_AUTHOR) == 0
-                       || strcmp(name, SVN_PROP_REVISION_DATE) == 0))
-                /* ... but we can only return author/date. */
-                continue;
-              if (log_entry->revprops == NULL)
-                log_entry->revprops = svn_hash__make(pool);
-              svn_hash_sets(log_entry->revprops, name, value);
+              log_entry->revprops = r_props;
+              for (i = 0; i < revprops->nelts; i++)
+                {
+                  const svn_string_t *name
+                    = APR_ARRAY_IDX(revprops, i, const svn_string_t *);
+                  if (!apr_hash_get(r_props, name->data, name->len))
+                    {
+                      /* hash does not match list of revprops we want */
+                      log_entry->revprops = NULL;
+                      break;
+                    }
+                }
             }
+
+          /* slow, revprop-by-revprop filtering */
+          if (log_entry->revprops == NULL)
+            for (i = 0; i < revprops->nelts; i++)
+              {
+                const svn_string_t *name
+                  = APR_ARRAY_IDX(revprops, i, const svn_string_t *);
+                svn_string_t *value
+                  = apr_hash_get(r_props, name->data, name->len);
+                if (censor_revprops
+                    && !(strncmp(name->data, SVN_PROP_REVISION_AUTHOR,
+                                 name->len) == 0
+                         || strncmp(name->data, SVN_PROP_REVISION_DATE,
+                                    name->len) == 0))
+                  /* ... but we can only return author/date. */
+                  continue;
+                if (log_entry->revprops == NULL)
+                  log_entry->revprops = svn_hash__make(pool);
+                apr_hash_set(log_entry->revprops, name->data, name->len, value);
+              }
         }
     }
 
@@ -2256,6 +2282,19 @@ svn_repos_get_logs4(svn_repos_t *repos,
   svn_boolean_t descending_order;
   svn_mergeinfo_t paths_history_mergeinfo = NULL;
 
+  if (revprops)
+    {
+      int i;
+      apr_array_header_t *new_revprops
+        = apr_array_make(pool, revprops->nelts, sizeof(svn_string_t *));
+
+      for (i = 0; i < revprops->nelts; ++i)
+        APR_ARRAY_PUSH(new_revprops, svn_string_t *)
+          = svn_string_create(APR_ARRAY_IDX(revprops, i, const char *), pool);
+
+      revprops = new_revprops;
+    }
+  
   /* Setup log range. */
   SVN_ERR(svn_fs_youngest_rev(&head, fs, pool));
 

commit 879b07ee5c4ebe991c077ab1438a41a75bd68445

Fix information leak introduced in r1483570. See http://mail-archives.apache.org/mod_mbox/subversion-dev/201502.mbox/%3CCAP_GPNiVRRxt5pL%3DukODSPrs1REjO4VvAhzomFKfcMYYRbvxPQ%40mail.gmail.com%3E
* subversion/libsvn_repos/log.c
  (fill_log_entry): Use the correct method to compare a svn_string_t.

Found by: kotkov


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1658439 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_repos/log.c
@@ -1144,7 +1144,16 @@ fill_log_entry(svn_log_entry_t *log_entry,
           int i;
 
           /* Requested only some revprops... */
-          
+
+          /* Make "svn:author" and "svn:date" available as svn_string_t
+             for efficient comparison via svn_string_compare().  Note that
+             we want static initialization here and must therefore emulate
+             strlen(x) by sizeof(x)-1. */
+          static const svn_string_t svn_prop_revision_author
+            = {SVN_PROP_REVISION_AUTHOR, sizeof(SVN_PROP_REVISION_AUTHOR)-1};
+          static const svn_string_t svn_prop_revision_date
+            = {SVN_PROP_REVISION_DATE, sizeof(SVN_PROP_REVISION_DATE)-1};
+
           /* often only the standard revprops got requested and delivered.
              In that case, we can simply pass the hash on. */
           if (revprops->nelts == apr_hash_count(r_props) && !censor_revprops)
@@ -1172,10 +1181,8 @@ fill_log_entry(svn_log_entry_t *log_entry,
                 svn_string_t *value
                   = apr_hash_get(r_props, name->data, name->len);
                 if (censor_revprops
-                    && !(strncmp(name->data, SVN_PROP_REVISION_AUTHOR,
-                                 name->len) == 0
-                         || strncmp(name->data, SVN_PROP_REVISION_DATE,
-                                    name->len) == 0))
+                    && !svn_string_compare(name, &svn_prop_revision_author)
+                    && !svn_string_compare(name, &svn_prop_revision_date))
                   /* ... but we can only return author/date. */
                   continue;
                 if (log_entry->revprops == NULL)