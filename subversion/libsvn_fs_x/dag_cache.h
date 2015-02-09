3a16dd4368f03a5a2e6a43fcb6ff2c6173b95c4c
Stefan Fuhrmann <stefan2@apache.org>
2015-02-09 17:55:28 +0000

    On the fsx-1.10 branch:  Make the previous parent_path_t part of the internal DAG cache API.
    * subversion/libsvn_fs_x/dag_cache.h
      (svn_fs_x__copy_id_inherit_t,
       svn_fs_x__dag_path_t): Rename and move definition here from tree.c .
    
    * subversion/libsvn_fs_x/tree.c
      (copy_id_inherit_t,
       parent_path_t): Remove here.
      (parent_path_path,
       parent_path_relpath,
       get_copy_inheritance,
       make_parent_path,
       open_path,
       make_path_mutable,
       svn_fs_x__get_dag_node,
       increment_mergeinfo_up_tree, 
       x_change_node_prop,
       x_make_dir,
       x_delete_node,
       copy_helper,
       x_make_file,
       apply_textdelta,
       apply_text,
       find_youngest_copyroot,
       x_closest_copy,
       history_prev,
       get_mergeinfo_for_path_internal): Update users.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/fsx-1.10@1658484 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_x/dag_cache.h
+++ subversion/libsvn_fs_x/dag_cache.h
@@ -33,6 +33,47 @@ extern "C" {
 svn_fs_x__dag_cache_t*
 svn_fs_x__create_dag_cache(apr_pool_t *result_pool);
 
+/* Flag type used in svn_fs_x__dag_path_t to determine where the
+   respective node got its copy ID from. */
+typedef enum svn_fs_x__copy_id_inherit_t
+{
+  svn_fs_x__copy_id_inherit_unknown = 0,
+  svn_fs_x__copy_id_inherit_self,
+  svn_fs_x__copy_id_inherit_parent,
+  svn_fs_x__copy_id_inherit_new
+
+} svn_fs_x__copy_id_inherit_t;
+
+
+/* A linked list representing the path from a node up to a root
+   directory.  We use this for cloning, and for operations that need
+   to deal with both a node and its parent directory.  For example, a
+   `delete' operation needs to know that the node actually exists, but
+   also needs to change the parent directory.  */
+typedef struct svn_fs_x__dag_path_t
+{
+  /* A node along the path.  This could be the final node, one of its
+     parents, or the root.  Every parent path ends with an element for
+     the root directory.  */
+  dag_node_t *node;
+
+  /* The name NODE has in its parent directory.  This is zero for the
+     root directory, which (obviously) has no name in its parent.  */
+  char *entry;
+
+  /* The parent of NODE, or zero if NODE is the root directory.  */
+  struct svn_fs_x__dag_path_t *parent;
+
+  /* The copy ID inheritance style. */
+  svn_fs_x__copy_id_inherit_t copy_inherit;
+
+  /* If copy ID inheritance style is copy_id_inherit_new, this is the
+     path which should be implicitly copied; otherwise, this is NULL. */
+  const char *copy_src_path;
+
+} svn_fs_x__dag_path_t;
+
+
 /* Open the node identified by PATH in ROOT.  Set DAG_NODE_P to the
    node we find, allocated in POOL.  Return the error
    SVN_ERR_FS_NOT_FOUND if this node doesn't exist.
