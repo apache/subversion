05185a0f00be6859f5b5c3551dc43f46787740ab
svn-role <svn-role@unknown>
2015-02-18 04:00:22 +0000

    Merge the r1544302 group from trunk:
     * r1544302, r1544303
       Add HTTPd lock file configuration for testing against HTTPd 2.4+.
       Justification:
         Without this change, the davautocheck tests do not work on some
         systems where HTTPd 2.4+ is the default; OSX 10.10 is one such case.
       Notes:
         As far as I'm concerned, this blocks 1.7.19 and 1.8.11 since the
         tests won't run on a current version of a supported OS.
       Votes:
         +1: brane, breser, philip
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/1.7.x@1660561 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/davautocheck.sh
+++ subversion/tests/cmdline/davautocheck.sh
@@ -309,9 +309,17 @@ User                $(id -un)
 Group               $(id -gn)
 __EOF__
 else
+HTTPD_LOCK="$HTTPD_ROOT/lock"
+mkdir "$HTTPD_LOCK" \
+  || fail "couldn't create lock directory '$HTTPD_LOCK'"
   cat >> "$HTTPD_CFG" <<__EOF__
-# TODO: maybe uncomment this for prefork,worker MPMs only?
-# Mutex file:lock mpm-accept
+# worker and prefork MUST have a mpm-accept lockfile in 2.3.0+
+<IfModule worker.c>
+  Mutex "file:$HTTPD_LOCK" mpm-accept
+</IfModule>
+<IfModule prefork.c>
+  Mutex "file:$HTTPD_LOCK" mpm-accept
+</IfModule>
 __EOF__
 fi
 
