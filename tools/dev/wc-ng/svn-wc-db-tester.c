005a21cce91002680dcbb0f8ef4ac6d1d76d514b
Bert Huijben <rhuijben@apache.org>
2015-02-19 13:17:33 +0000

    Following up on r1660874, fix some relative paths.
    * tools/dev/wc-ng/svn-wc-db-tester.c
      (includes): Fix reference to libsvn_wc internal headers to avoid gen-make
        warning. (Somehow the references did work!)
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1660876 13f79535-47bb-0310-9956-ffa450edef68

--- tools/dev/wc-ng/svn-wc-db-tester.c
+++ tools/dev/wc-ng/svn-wc-db-tester.c
@@ -34,8 +34,8 @@
 #include "private/svn_wc_private.h"
 #include "private/svn_cmdline_private.h"
 
-#include "../../subversion/libsvn_wc/wc.h"
-#include "../../subversion/libsvn_wc/wc_db.h"
+#include "../../../subversion/libsvn_wc/wc.h"
+#include "../../../subversion/libsvn_wc/wc_db.h"
 
 #include "svn_private_config.h"
 
