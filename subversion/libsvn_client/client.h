b72bacb2eca1674113e64d99d98010981afc5ac1
Bert Huijben <rhuijben@apache.org>
2015-02-21 01:46:46 +0000

    * subversion/libsvn_client/add.c   (svn_client__get_inherited_ignores): Remove unused function.
    * subversion/libsvn_client/client.h
      (svn_client__get_inherited_ignores): Remove function.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1661266 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/client.h
+++ subversion/libsvn_client/client.h
@@ -455,17 +455,6 @@ svn_error_t *svn_client__get_all_auto_props(apr_hash_t **autoprops,
                                             apr_pool_t *result_pool,
                                             apr_pool_t *scratch_pool);
 
-/* Get a list of ignore patterns defined by the svn:global-ignores
-   properties set on, or inherited by, PATH_OR_URL.  Store the collected
-   patterns as const char * elements in the array *IGNORES.  Allocate
-   *IGNORES and its contents in RESULT_POOL.  Use  SCRATCH_POOL for
-   temporary allocations. */
-svn_error_t *svn_client__get_inherited_ignores(apr_array_header_t **ignores,
-                                               const char *path_or_url,
-                                               svn_client_ctx_t *ctx,
-                                               apr_pool_t *result_pool,
-                                               apr_pool_t *scratch_pool);
-
 /* The main logic for client deletion from a working copy. Deletes PATH
    from CTX->WC_CTX.  If PATH (or any item below a directory PATH) is
    modified the delete will fail and return an error unless FORCE or KEEP_LOCAL
