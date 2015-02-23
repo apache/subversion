9ccd3d74e7980e92b013c76e0fce9acde02d79be
Julian Foad <julianfoad@apache.org>
2015-02-23 17:52:31 +0000

    Tweak documentation following r1659863.
    * subversion/include/svn_repos.h
      (svn_repos_get_commit_editor5): Document that commit_info.repos_root is
        null. Tweak @since to make another change more visible.
    
    * subversion/libsvn_repos/commit.c
      (invoke_commit_cb): Adjust comment.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1661726 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_repos/commit.c
+++ subversion/libsvn_repos/commit.c
@@ -201,7 +201,7 @@ invoke_commit_cb(svn_commit_callback2_t commit_cb,
   commit_info->date = date ? date->data : NULL;
   commit_info->author = author ? author->data : NULL;
   commit_info->post_commit_err = post_commit_errstr;
-  /* commit_info->repos_root = NULL; ### Fixed up in ra-local handling */
+  /* commit_info->repos_root is not set by the repos layer, only by RA layers */
 
   return svn_error_trace(commit_cb(commit_info, commit_baton, scratch_pool));
 }
