fcc0ade0b9fd7c52355ae5390f414867ca9f54ac
Branko Čibej <brane@apache.org>
2015-02-25 15:13:16 +0000

    Fix a shadow warning.
    * subversion/libsvn_wc/wc_db_update_move.c
      (break_moved_away): Rename parameter src_op_depth to parent_src_op_depth.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1662241 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/wc_db_update_move.c
+++ subversion/libsvn_wc/wc_db_update_move.c
@@ -2487,7 +2487,7 @@ static svn_error_t *
 break_moved_away(svn_wc__db_wcroot_t *wcroot,
                  svn_wc__db_t *db,
                  const char *local_relpath,
-                 int src_op_depth,
+                 int parent_src_op_depth,
                  apr_pool_t *scratch_pool)
 {
   svn_sqlite__stmt_t *stmt;
@@ -2501,7 +2501,7 @@ break_moved_away(svn_wc__db_wcroot_t *wcroot,
   SVN_ERR(svn_sqlite__get_statement(&stmt, wcroot->sdb,
                                     STMT_SELECT_MOVED_DESCENDANTS_SRC));
   SVN_ERR(svn_sqlite__bindf(stmt, "isd", wcroot->wc_id, local_relpath,
-                                         src_op_depth));
+                            parent_src_op_depth));
   SVN_ERR(svn_sqlite__step(&have_row, stmt));
 
   iterpool = svn_pool_create(scratch_pool);
