3b15c4763fef19d58b4b70156211f407dc65d6e3
Daniel Shahaf <danielsh@apache.org>
2015-02-28 21:22:59 +0000

    info --show-item: Change 'last-changed-rev' to 'last-changed-revision'.
    * subversion/svn/info-cmd.c
      (info_item_map): Change item name.
    
    * subversion/svn/svn.c
      (svn_cl__cmd_table."info"): Update documentation.
    
    * subversion/tests/cmdline/info_tests.py
      (info_item_simple_multiple, info_item_url, info_item_uncommmitted):
        Update tests.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1663003 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/info_tests.py
+++ subversion/tests/cmdline/info_tests.py
@@ -658,7 +658,7 @@ def info_item_simple_multiple(sbox):
 
   svntest.actions.run_and_verify_svn(
     r'^1\s+\S+(/|\\)info_tests-\d+(/|\\)[^/\\]+$', [],
-    'info', '--show-item=last-changed-rev',
+    'info', '--show-item=last-changed-revision',
     sbox.ospath('A'), sbox.ospath('iota'))
 
 
@@ -669,13 +669,13 @@ def info_item_url(sbox):
 
   svntest.actions.run_and_verify_svn(
     '1', [],
-    'info', '--show-item=last-changed-rev',
+    'info', '--show-item=last-changed-revision',
     sbox.repo_url)
 
 
   svntest.actions.run_and_verify_svn(
     r'^1\s+[^/:]+://.+/repos/[^/]+$', [],
-    'info', '--show-item=last-changed-rev',
+    'info', '--show-item=last-changed-revision',
     sbox.repo_url + '/A', sbox.repo_url + '/iota')
 
 
@@ -697,7 +697,7 @@ def info_item_uncommmitted(sbox):
 
   svntest.actions.run_and_verify_svn(
     '', [],
-    'info', '--show-item=last-changed-rev',
+    'info', '--show-item=last-changed-revision',
     sbox.ospath('newfile'))
 
   svntest.actions.run_and_verify_svn(
