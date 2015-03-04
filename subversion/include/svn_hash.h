7e9fdbe273c4bef41170ca73f27bf104dc76f6c6
Julian Foad <julianfoad@apache.org>
2015-03-04 09:30:37 +0000

    * subversion/include/svn_hash.h   (svn_hash_sets): Rename from svn_hash_gets, to fix r1663896.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1663908 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_hash.h
+++ subversion/include/svn_hash.h
@@ -268,7 +268,7 @@ svn_hash__sets(apr_hash_t *ht, const char *key, const void *value);
 #define svn_hash_sets(ht, key, val) \
             svn_hash__sets(ht, key, val)
 #else
-#define svn_hash_gets(ht, key) \
+#define svn_hash_sets(ht, key, val) \
             apr_hash_set(ht, svn_hash__key, APR_HASH_KEY_STRING, val)
 #endif
 
