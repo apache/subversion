apache/subversion

commit bc982ddd7be3102b3892a74f49b61d54b83ccca6

Redefine the svn_hash_sets macro to match the way svn_hash_gets is defined, thus avoiding a backward compatibility problem. A follow-up to r1663760 and r1663780.
Found by: danielsh

* subversion/include/svn_hash.h
  (svn_hash__sets): New function.
  (svn_hash_sets): In debug mode, call svn_hash__sets; in release mode, call
    apr_hash_set directly.

* subversion/libsvn_subr/hash.c
  (svn_hash__sets): New function.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1663896 13f79535-47bb-0310-9956-ffa450edef68

subversion/include/svn_hash.h
@@ -239,13 +239,14 @@ svn_hash_from_cstring_keys(apr_hash_t **hash,
                            const apr_array_header_t *keys,
                            apr_pool_t *pool);
 
-/* In debug builds, the svn_hash_gets macro forwards the parameters
- * through this function in order to have parameter type checking,
- * particularly for the key. The svn_hash_sets macro gets parameter
- * type checking in a different way, by using an in-line assignment.
+/* In debug builds, the svn_hash_gets and svn_hash_sets macros forward their
+ * parameters through these functions in order to gain type checking for the
+ * 'key' parameter which the basic apr_hash_* APIs declare only as 'void *'.
  */
 void *
 svn_hash__gets(apr_hash_t *ht, const char *key);
+void
+svn_hash__sets(apr_hash_t *ht, const char *key, const void *value);
 
 /** Shortcut for apr_hash_get() with a const char * key.
  *
@@ -263,11 +264,13 @@ svn_hash__gets(apr_hash_t *ht, const char *key);
  *
  * @since New in 1.8.
  */
-#define svn_hash_sets(ht, key, val)                                         \
-  do {                                                                      \
-    const char *svn_hash__key = (key);                                      \
-    apr_hash_set(ht, svn_hash__key, APR_HASH_KEY_STRING, val);              \
-  } while (0)
+#ifdef SVN_DEBUG
+#define svn_hash_sets(ht, key, val) \
+            svn_hash__sets(ht, key, val)
+#else
+#define svn_hash_gets(ht, key) \
+            apr_hash_set(ht, svn_hash__key, APR_HASH_KEY_STRING, val)
+#endif
 
 /** @} */
 

subversion/libsvn_subr/hash.c
@@ -567,6 +567,13 @@ svn_hash__gets(apr_hash_t *ht, const char *key)
 }
 
 
+void
+svn_hash__sets(apr_hash_t *ht, const char *key, const void *val)
+{
+  return apr_hash_set(ht, key, APR_HASH_KEY_STRING, val);
+}
+
+
 
 /*** Specialized getter APIs ***/
 

commit 2043d908b26b7ebe8b02b73e2d94adaee567a8c3

* subversion/include/svn_hash.h   (svn_hash_sets): Fix remaining breakage from r1663896.
git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1663917 13f79535-47bb-0310-9956-ffa450edef68

subversion/include/svn_hash.h
@@ -269,7 +269,7 @@ svn_hash__sets(apr_hash_t *ht, const char *key, const void *value);
             svn_hash__sets(ht, key, val)
 #else
 #define svn_hash_sets(ht, key, val) \
-            apr_hash_set(ht, svn_hash__key, APR_HASH_KEY_STRING, val)
+            apr_hash_set(ht, key, APR_HASH_KEY_STRING, val)
 #endif
 
 /** @} */