414c9404f1102cbaa283e4d1ba5a9fa5ce418543
Philip Martin <philip@apache.org>
2015-03-11 12:00:56 +0000

    * subversion/tests/cmdline/upgrade_tests.py   (auto_analyze): Get SQLite return value where needed.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1665852 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/upgrade_tests.py
+++ subversion/tests/cmdline/upgrade_tests.py
@@ -1448,13 +1448,13 @@ def auto_analyze(sbox):
                             '52ec7e4b-e5f0-451d-829f-f05d5571b4ab')
 
   # Don't use svn to do relocate as that will add the table.
-  val = svntest.wc.sqlite_exec(sbox.wc_dir,
-                               "update repository "
-                               "set root ='" + sbox.repo_url + "'")
-  val = svntest.wc.sqlite_exec(sbox.wc_dir,
+  svntest.wc.sqlite_exec(sbox.wc_dir,
+                         "update repository "
+                         "set root ='" + sbox.repo_url + "'")
+  val = svntest.wc.sqlite_stmt(sbox.wc_dir,
                                "select 1 from sqlite_master "
                                "where name = 'sqlite_stat1'")
-  if val != None:
+  if val != []:
     raise svntest.Failure("initial state failed")
 
   # Make working copy read-only (but not wc_dir itself as
