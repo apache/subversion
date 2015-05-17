0884304b6b737cdd2e632a6614066b123bba010d
Daniel Shahaf <danielsh@apache.org>
2015-05-17 23:22:03 +0000

    Follow-up to r1679169: Further extend the unit test.
    * subversion/tests/libsvn_fs/fs-test.c
      (noop_freeze_func): Transform into..
      (freeze_func): .. this.
      (freeze_and_commit): Incorporate a failed freeze into the test scenario.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1679909 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/libsvn_fs/fs-test.c
+++ subversion/tests/libsvn_fs/fs-test.c
@@ -6891,11 +6891,12 @@ test_internal_txn_props(const svn_test_opts_t *opts,
   return SVN_NO_ERROR;
 }
 
+/* A freeze function that expects an 'svn_error_t *' baton, and returns it. */
 /* This function implements svn_fs_freeze_func_t. */
 static svn_error_t *
-noop_freeze_func(void *baton, apr_pool_t *pool)
+freeze_func(void *baton, apr_pool_t *pool)
 {
-  return SVN_NO_ERROR;
+  return baton;
 }
 
 static svn_error_t *
@@ -6926,10 +6927,14 @@ freeze_and_commit(const svn_test_opts_t *opts,
   SVN_ERR(test_commit_txn(&new_rev, txn, NULL, subpool));
 
   /* Freeze and unfreeze. */
-  SVN_ERR(svn_fs_freeze(fs, noop_freeze_func, NULL, pool));
+  SVN_ERR(svn_fs_freeze(fs, freeze_func, SVN_NO_ERROR, pool));
 
-  /* And the same once again, for good measure. */
-  SVN_ERR(svn_fs_freeze(fs, noop_freeze_func, NULL, pool));
+  /* Freeze again, but have freeze_func fail. */
+    {
+      svn_error_t *err = svn_error_create(APR_EGENERAL, NULL, NULL);
+      SVN_TEST_ASSERT_ERROR(svn_fs_freeze(fs, freeze_func, err, pool),
+                            err->apr_err);
+    }
 
   /* Make some commit using same FS instance. */
   SVN_ERR(svn_fs_begin_txn(&txn, fs, new_rev, pool));
