5f4944907dd0f4224513818558d8fe30e2141be7
Stefan Fuhrmann <stefan2@apache.org>
2015-08-12 12:34:46 +0000

    On the svn-mergeinfo-normalizer branch: Only remove branches as "obsolete" if we find an explicit deletion for them after their last point of creation.
    If branches are relatively old and inactive, they may simply not contain all
    sub-paths that the potential merge target has.  However, we would still need
    them in case there will be a catch-up merge on those branches in the future.
    
    * tools/client-side/svn-mergeinfo-normalizer/mergeinfo-normalizer.h
      (svn_min__find_copy): Declare new lookup function.
    
    * tools/client-side/svn-mergeinfo-normalizer/log.c
      (svn_min__find_copy): Implement it.
    
    * tools/client-side/svn-mergeinfo-normalizer/logic.c
      (remove_obsolete_line): No longer treat paths that never existed as
                              deleted / "obsolete".
      (remove_obsolete_lines,
       remove_lines,
       normalize): Update callers to provide the extra LOG parameter.
      (show_obsoletes_summary): Use the same logic as above to determine the
                                deletion rev. Say "missing" instead of "deleted"
                                branches.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/svn-mergeinfo-normalizer@1695493 13f79535-47bb-0310-9956-ffa450edef68

--- tools/client-side/svn-mergeinfo-normalizer/mergeinfo-normalizer.h
+++ tools/client-side/svn-mergeinfo-normalizer/mergeinfo-normalizer.h
@@ -200,6 +200,13 @@ svn_min__find_deletions(svn_min__log_t *log,
                         apr_pool_t *result_pool,
                         apr_pool_t *scratch_pool);
 
+svn_revnum_t
+svn_min__find_copy(svn_min__log_t *log,
+                   const char *path,
+                   svn_revnum_t start_rev,
+                   svn_revnum_t end_rev,
+                   apr_pool_t *scratch_pool);
+
 apr_array_header_t *
 svn_min__get_history(svn_min__log_t *log,
                      const char *path,
