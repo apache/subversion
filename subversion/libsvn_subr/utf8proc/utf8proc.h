703faf8ba6ff698a2edce4d6b855ea7744282842
Bert Huijben <rhuijben@apache.org>
2015-09-15 15:56:14 +0000

    Use similar detection for a few system headers on Windows and other platforms, to avoid problems whenever we want to reuse this code and to make it easier to fix Windows specifics in one place.
    * configure.ac
      Check for stdint.h instead of the less likely to exist inttypes.h, where
      we use the include of stdint.h of.
    
    * subversion/libsvn_subr/utf8proc/utf8proc.h
      Use standard defines for the headers we need.
    
    * subversion/svn_private_config.hw
      Declare defines for Visual Studio.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1703238 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/utf8proc/utf8proc.h
+++ subversion/libsvn_subr/utf8proc/utf8proc.h
@@ -91,34 +91,26 @@
 
 #include <stdlib.h>
 #include <sys/types.h>
+
+#ifdef HAVE_STDINT_H
+#include <stdint.h>
+#else
+#include <apr.h>
+#endif
+
+#ifdef HAVE_STDBOOL_H
+#include <stdbool.h>
+#elif !defined(__cplusplus)
+typedef uint8_t bool;
+enum { false, true };
+#endif
+
 #ifdef _MSC_VER
-# if _MSC_VER >= 1600
-#   include <stdint.h>
-# else
-typedef signed char int8_t;
-typedef unsigned char uint8_t;
-typedef short int16_t;
-typedef unsigned short uint16_t;
-typedef int int32_t;
-# endif
-# if _MSC_VER >= 1800
-#   include <stdbool.h>
-# else
-typedef unsigned char bool;
-enum {false, true};
-# endif
 # ifdef _WIN64
 #   define ssize_t __int64
 # else
 #   define ssize_t int
 # endif
-#elif defined(HAVE_STDBOOL_H) && defined(HAVE_INTTYPES_H)
-#include <stdbool.h>
-#include <inttypes.h>
-#else
-#include <apr.h>
-typedef uint8_t bool;
-enum {false, true};
 #endif
 #include <limits.h>
 
