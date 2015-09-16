3ccf3a81dbd181f4e00e9dc1cd4eb2a998c5dea9
svn-role <svn-role@unknown>
2015-09-16 04:00:23 +0000

    Merge the r1698312 group from trunk:
     * r1698312, r1702310
       Correctly report empty representations in 'svnfsfs stats'.
       Justification:
         The current code displays the "0" bracket in histograms as
         "-2147483648 .. < 1" instead of "0 .. < 1".  Technically correct
         but annoying.  UI change only.
       Votes:
         +1: rhuijben, stefan2, ivan
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/1.9.x@1703327 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/svnfsfs_tests.py
+++ subversion/tests/cmdline/svnfsfs_tests.py
@@ -173,6 +173,15 @@ def load_index_sharded(sbox):
   svntest.actions.run_and_verify_svnadmin(expected_output, [],
                                           "verify", sbox.repo_dir)
 
+@SkipUnless(svntest.main.is_fs_type_fsfs)
+def test_stats_on_empty_repo(sbox):
+  "stats on empty repo shall not crash"
+
+  sbox.build(create_wc=False, empty=True)
+
+  exit_code, output, errput = \
+    svntest.actions.run_and_verify_svnfsfs(None, [], 'stats', sbox.repo_dir)
+
 ########################################################################
 # Run the tests
 
@@ -180,6 +189,7 @@ def load_index_sharded(sbox):
 # list all tests here, starting with None:
 test_list = [ None,
               load_index_sharded,
+              test_stats_on_empty_repo,
              ]
 
 if __name__ == '__main__':
