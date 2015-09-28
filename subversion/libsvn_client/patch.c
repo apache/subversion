329fea1f405adc0ac178883d8872e5e732f68d2b
Bert Huijben <rhuijben@apache.org>
2015-09-28 17:43:25 +0000

    Stop applying some kinds of property changes during 'svn patch' that should have been stopped by a conflict.
    Properties were deleted even if the original value didn't match. This
    catches some hidden bugs in the svn:executable handling.
    
    * subversion/include/private/svn_diff_private.h
      (svn_diff_hunk__create_adds_single_line,
       svn_diff_hunk__create_deletes_single_line): Constify argument.
    
    * subversion/libsvn_client/patch.c
      (prop_patch_target_t): Add boolean.
      (apply_one_patch): Always push diff items to allow notifying conflicts.
        Set skip if nothing changed
      (install_patched_prop_targets): If skipping the property don't change its
        value.
    
    * subversion/libsvn_diff/parse-diff.c
      (svn_diff_hunk_t,
       svn_diff_binary_patch_t): Constify pointer.
      (svn_diff_hunk__create_adds_single_line,
       svn_diff_hunk__create_deletes_single_line): Create reversed hunks, to allow
         reading them back as not reversed.
    
    * subversion/tests/cmdline/patch_tests.py
      (patch_prop_madness): Tweak expectations. Expect conflicts on property
        mismatches, but (currently) not on final EOL only changes.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1705733 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/patch.c
+++ subversion/libsvn_client/patch.c
@@ -153,6 +153,9 @@ typedef struct prop_patch_target_t {
    * ### Should we use flags instead since we're not using all enum values? */
   svn_diff_operation_kind_t operation;
 
+  /* When true the property change won't be applied */
+  svn_boolean_t skipped;
+
   /* ### Here we'll add flags telling if the prop was added, deleted,
    * ### had_rejects, had_local_mods prior to patching and so on. */
 } prop_patch_target_t;
@@ -2622,8 +2625,7 @@ apply_one_patch(patch_target_t **patch_target, svn_patch_t *patch,
                             TRUE /* is_prop_hunk */,
                             cancel_func, cancel_baton,
                             result_pool, iterpool));
-      if (! hi->already_applied)
-        APR_ARRAY_PUSH(prop_target->content->hunks, hunk_info_t *) = hi;
+      APR_ARRAY_PUSH(prop_target->content->hunks, hunk_info_t *) = hi;
     }
 
   /* Apply or reject property hunks. */
@@ -2632,6 +2634,7 @@ apply_one_patch(patch_target_t **patch_target, svn_patch_t *patch,
        hash_index = apr_hash_next(hash_index))
     {
       prop_patch_target_t *prop_target;
+      svn_boolean_t applied_one = FALSE;
 
       prop_target = apr_hash_this_val(hash_index);
 
@@ -2653,24 +2656,30 @@ apply_one_patch(patch_target_t **patch_target, svn_patch_t *patch,
                                 prop_target->name,
                                 iterpool));
           else
-            SVN_ERR(apply_hunk(target, prop_target->content, hi,
-                               prop_target->name,
-                               iterpool));
+            {
+              SVN_ERR(apply_hunk(target, prop_target->content, hi,
+                                 prop_target->name,
+                                 iterpool));
+              applied_one = TRUE;
+            }
         }
 
-        if (prop_target->content->existed)
-          {
-            /* Copy any remaining lines to target. */
-            SVN_ERR(copy_lines_to_target(prop_target->content, 0,
-                                         scratch_pool));
-            if (! prop_target->content->eof)
-              {
-                /* We could not copy the entire target property to the
-                 * temporary file, and would truncate the target if we
-                 * copied the temporary file on top of it. Skip this target.  */
-                target->skipped = TRUE;
-              }
-          }
+      if (!applied_one)
+        prop_target->skipped = TRUE;
+
+      if (applied_one && prop_target->content->existed)
+        {
+          /* Copy any remaining lines to target. */
+          SVN_ERR(copy_lines_to_target(prop_target->content, 0,
+                                       scratch_pool));
+          if (! prop_target->content->eof)
+            {
+              /* We could not copy the entire target property to the
+               * temporary file, and would truncate the target if we
+               * copied the temporary file on top of it. Skip this target.  */
+              target->skipped = TRUE;
+            }
+        }
       }
 
   svn_pool_destroy(iterpool);
@@ -3101,6 +3110,9 @@ install_patched_prop_targets(patch_target_t *target,
 
       if (ctx->cancel_func)
         SVN_ERR(ctx->cancel_func(ctx->cancel_baton));
+
+      if (prop_target->skipped)
+        continue;
 
       /* For a deleted prop we only set the value to NULL. */
       if (prop_target->operation == svn_diff_op_deleted)
