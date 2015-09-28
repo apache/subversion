d3fb377a74b8df27e73e767447c6031e5bbf82c4
Bert Huijben <rhuijben@apache.org>
2015-09-28 12:18:22 +0000

    Following up on r1705391, r1705503, create the fake diff hunks in a form that represents the properties in their canonical format, and fix some issues when applying svn:executable changes in a reverse diff.
    * subversion/include/private/svn_diff_private.h
      (svn_diff_hunk__create_adds_single_line,
       svn_diff_hunk__create_deletes_single_line): Tweak comments.
    
    * subversion/libsvn_diff/parse-diff.c
      (add_or_delete_single_line): Update how not having a final EOL is recorded.
      (svn_diff_parse_next_patch): Handle reversing mode change diffs.
    
    * subversion/tests/cmdline/patch_tests.py
      (patch_adds_executability_nocontents): Extend test.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1705663 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/private/svn_diff_private.h
+++ subversion/include/private/svn_diff_private.h
@@ -113,8 +113,8 @@ svn_diff__display_prop_diffs(svn_stream_t *outstream,
                              void *cancel_baton,
                              apr_pool_t *scratch_pool);
 
-/** Create a hunk object that adds a single line.  Return the new object
- * in @a *hunk.
+/** Create a hunk object that adds a single line without newline.  Return the
+ * new object in @a *hunk.
  *
  * @a line is the added text, without a trailing newline.
  *
@@ -127,8 +127,8 @@ svn_diff_hunk__create_adds_single_line(svn_diff_hunk_t **hunk,
                                        apr_pool_t *result_pool,
                                        apr_pool_t *scratch_pool);
 
-/** Create a hunk object that deletes a single line.  Return the new object
- * in @a *hunk.
+/** Create a hunk object that deletes a single line without newline.  Return
+ * the new object in @a *hunk.
  *
  * @a line is the deleted text, without a trailing newline.
  *
