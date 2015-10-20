82324f913f3b026a750c09f90e10abf2d145765b
Julian Foad <julianfoad@apache.org>
2015-10-20 16:27:17 +0000

    On the 'move-tracking-2' branch: Re-apply a fix made in r1708949 and lost in r1708994.
    * subversion/libsvn_delta/branch.c
      (branch_txn_open_branch): Don't assert what isn't always true.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/move-tracking-2@1709638 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_delta/branch.c
+++ subversion/libsvn_delta/branch.c
@@ -147,8 +147,6 @@ branch_txn_open_branch(svn_branch_txn_t *txn,
     = svn_branch_txn_get_branch_by_id(txn, *new_branch_id_p, scratch_pool);
   if (new_branch)
     {
-      SVN_ERR_ASSERT(predecessor->rev == new_branch->predecessor->rev);
-      SVN_ERR_ASSERT(strcmp(predecessor->bid, new_branch->predecessor->bid) == 0);
       SVN_ERR_ASSERT(root_eid == svn_branch_root_eid(new_branch));
       return SVN_NO_ERROR;
     }
