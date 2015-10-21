apache/subversion

commit 18c85bb583cb7f6a859ca0e0e50dabbc18823548

On the 'move-tracking-2' branch: Allow an output pointer to be null.
* subversion/libsvn_delta/branch.c
  (branch_txn_open_branch,
   branch_txn_branch): Allow the output pointer to be null.

git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/move-tracking-2@1709777 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_delta/branch.c
@@ -138,13 +138,14 @@ branch_txn_open_branch(svn_branch_txn_t *txn,
                        apr_pool_t *result_pool,
                        apr_pool_t *scratch_pool)
 {
+  const char *new_branch_id;
   svn_branch_state_t *new_branch;
 
   /* if the subbranch already exists, just return its bid */
-  *new_branch_id_p
+  new_branch_id
     = svn_branch_id_nest(outer_branch_id, outer_eid, result_pool);
   new_branch
-    = svn_branch_txn_get_branch_by_id(txn, *new_branch_id_p, scratch_pool);
+    = svn_branch_txn_get_branch_by_id(txn, new_branch_id, scratch_pool);
   if (new_branch)
     {
       SVN_ERR_ASSERT(root_eid == svn_branch_root_eid(new_branch));
@@ -152,9 +153,12 @@ branch_txn_open_branch(svn_branch_txn_t *txn,
     }
 
   new_branch = svn_branch_txn_add_new_branch(txn,
-                                             *new_branch_id_p,
+                                             new_branch_id,
                                              predecessor,
                                              root_eid, scratch_pool);
+
+  if (new_branch_id_p)
+    *new_branch_id_p = new_branch_id;
   return SVN_NO_ERROR;
 }
 
@@ -172,6 +176,7 @@ branch_txn_branch(svn_branch_txn_t *txn,
   svn_branch_state_t *new_branch;
   svn_branch_state_t *from_branch;
   svn_element_tree_t *from_subtree;
+  const char *new_branch_id;
 
   SVN_ERR(branch_in_rev_or_txn(&from_branch, from, txn, scratch_pool));
   /* Source branch must exist */
@@ -194,17 +199,20 @@ branch_txn_branch(svn_branch_txn_t *txn,
                                from->rev, from->bid, from->eid);
     }
 
-  *new_branch_id_p
+  new_branch_id
     = svn_branch_id_nest(outer_branch_id, outer_eid, result_pool);
   predecessor = svn_branch_rev_bid_create(from->rev, from->bid, scratch_pool);
   new_branch = svn_branch_txn_add_new_branch(txn,
-                                             *new_branch_id_p,
+                                             new_branch_id,
                                              predecessor,
                                              from->eid, scratch_pool);
 
   /* Populate the mapping from the 'from' source */
   SVN_ERR(svn_branch_instantiate_elements(new_branch, from_subtree,
                                           scratch_pool));
+
+  if (new_branch_id_p)
+    *new_branch_id_p = new_branch_id;
   return SVN_NO_ERROR;
 }
 

commit 946b73167896d85ac45b8d061dee5a5a1c99ee06

On the 'move-tracking-2' branch: Fix a bug introduced in r1709777.
* subversion/libsvn_delta/branch.c
  (branch_txn_open_branch): Ensure the new branch id output is always
    provided (if requested), by avoiding an early return.

git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/move-tracking-2@1709782 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_delta/branch.c
@@ -149,13 +149,14 @@ branch_txn_open_branch(svn_branch_txn_t *txn,
   if (new_branch)
     {
       SVN_ERR_ASSERT(root_eid == svn_branch_root_eid(new_branch));
-      return SVN_NO_ERROR;
     }
-
-  new_branch = svn_branch_txn_add_new_branch(txn,
-                                             new_branch_id,
-                                             predecessor,
-                                             root_eid, scratch_pool);
+  else
+    {
+      new_branch = svn_branch_txn_add_new_branch(txn,
+                                                 new_branch_id,
+                                                 predecessor,
+                                                 root_eid, scratch_pool);
+    }
 
   if (new_branch_id_p)
     *new_branch_id_p = new_branch_id;