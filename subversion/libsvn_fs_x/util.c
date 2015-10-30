fd5ac519bc56817e19e5957a08878550ea4a90b9
Stefan Fuhrmann <stefan2@apache.org>
2015-10-30 10:39:54 +0000

    Re-enable proper fsync'ing in FSX for renames on non-POSIX platforms.
    * subversion/libsvn_fs_x/util.c
      (svn_fs_x__move_into_place): Use the batching infrastructure on POSIX only.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1711443 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_x/util.c
+++ subversion/libsvn_fs_x/util.c
@@ -732,14 +732,24 @@ svn_fs_x__move_into_place(const char *old_filename,
   /* Copying permissions is a no-op on WIN32. */
   SVN_ERR(svn_io_copy_perms(perms_reference, old_filename, scratch_pool));
 
-  /* TODO: teach svn_fs_x__batch_fsync_t how to properly rename on Win32. */
-
+  /* We use specific 'fsyncing move' Win32 API calls on Windows while the
+   * directory update fsync is POSIX-only.  Moreover, there tend to be only
+   * a few moved files (1 or 2) per batch.
+   *
+   * Therefore, we use the platform-optimized "immediate" fsyncs on all
+   * non-POSIX platforms and the "scheduled" fsyncs on POSIX only.
+   */
+#if defined(SVN_ON_POSIX)
   /* Move the file into place. */
   SVN_ERR(svn_io_file_rename2(old_filename, new_filename, FALSE,
                               scratch_pool));
 
   /* Schedule for synchronization. */
   SVN_ERR(svn_fs_x__batch_fsync_new_path(batch, new_filename, scratch_pool));
+#else
+  SVN_ERR(svn_io_file_rename2(old_filename, new_filename, TRUE,
+                              scratch_pool));
+#endif
 
   return SVN_NO_ERROR;
 }
