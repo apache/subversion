84681809823a48d0b18f9c3ec53dea183416134c
Ivan Zhakov <ivan@apache.org>
2015-10-30 17:21:14 +0000

    Implement svn_base64_encode2() with BREAK_LINES flag to control whether insert newlines periodically or not. The old svn_base64_encode() was inserting newlines unconditionally.
    * subversion/include/svn_base64.h
      (svn_base64_encode2): New function declaration.
      (svn_base64_encode): Deprecate.  
    
    * subversion/libsvn_subr/base64.c
      (encode_baton): Add BREAK_LINES field.
      (encode_data, finish_encoding_data): Use EB->BREAK_LINES flag.
      (svn_base64_encode2): Revv from svn_base64_encode(). Add BREAK_LINES.
       argument and save it in ENCODE_BATON.
    
    * subversion/libsvn_subr/deprecated.c
      (svn_base64_encode): Call svn_base64_encode2() with BREAK_LINES=TRUE.
    
    * subversion/mod_dav_svn/util.c
      (dav_svn__make_base64_output_stream): Use svn_base64_encode2() with
       BREAK_LINES=TRUE.
    
    * subversion/tests/libsvn_delta/svndiff-test.c
      (main): Use svn_base64_encode2() with BREAK_LINES=TRUE.
    
    * subversion/tests/libsvn_subr/stream-test.c
      (test_stream_base64, test_stream_base64_2): Test svn_base64_encode2() with
       BREAK_LINES=TRUE and BREAK_LINES=FALSE.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1711517 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/deprecated.c
+++ subversion/libsvn_subr/deprecated.c
@@ -46,6 +46,7 @@
 #include "svn_utf.h"
 #include "svn_xml.h"
 #include "svn_auth.h"
+#include "svn_base64.h"
 
 #include "opt.h"
 #include "auth.h"
@@ -1573,4 +1574,11 @@ svn_cmdline_create_auth_baton(svn_auth_baton_t **ab,
                                                         cancel_func,
                                                         cancel_baton,
                                                         pool));
+}
+
+/*** From base64.c ***/
+svn_stream_t *
+svn_base64_encode(svn_stream_t *output, apr_pool_t *pool)
+{
+  return svn_base64_encode2(output, TRUE, pool);
 }
