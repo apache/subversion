751d6ff8acfcb42c8f06c9daa557fb04a22a4274
Julian Foad <julianfoad@apache.org>
2015-11-12 13:58:57 +0000

    On the 'move-tracking-2' branch: Update a comment, following the renames of r1713980.
    * subversion/libsvn_wc/wc.h
      (svn_wc__fetch_func): Update the doc string.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/move-tracking-2@1714049 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/wc.h
+++ subversion/libsvn_wc/wc.h
@@ -693,7 +693,7 @@ struct svn_wc__shim_fetch_baton_t
 /* Using a BATON of struct svn_wc__shim_fetch_baton_t, return
  * KIND/PROPS/FILE_TEXT for REPOS_RELPATH. REVISION is unused.
  *
- * Implements svn_editor3__shim_fetch_func_t. */
+ * Implements svn_branch__compat_fetch_func_t. */
 svn_error_t *
 svn_wc__fetch_func(svn_node_kind_t *kind,
                    apr_hash_t **props,
