35f00f619dd9ded99b51a0a866dd27ebb64bd730
Bert Huijben <rhuijben@apache.org>
2015-12-02 18:41:02 +0000

    On the ra-git branch: Handle a few more nodes as related to avoid an endless loop in the delta editor when checking out a repository root.
    * subversion/libsvn_fs_git/git-revroot.c
      (fs_git_node_relation): Handle special dir cases.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/ra-git@1717667 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_git/git-revroot.c
+++ subversion/libsvn_fs_git/git-revroot.c
@@ -658,18 +658,23 @@ fs_git_node_relation(svn_fs_node_relation_t *relation,
   SVN_ERR(find_branch(&commit_a, &relpath_a, root_a, path_a, scratch_pool));
   SVN_ERR(find_branch(&commit_b, &relpath_b, root_b, path_b, scratch_pool));
 
-  if (!(commit_a && commit_b))
-    {
-      *relation = svn_fs_node_unrelated;
-      return SVN_NO_ERROR;
-    }
-  else if ((*relpath_a == '\0') || (*relpath_b == '\0'))
+  if ((*relpath_a == '\0') || (*relpath_b == '\0'))
     {
       if ((*relpath_a == '\0') && (*relpath_b == '\0'))
         *relation = svn_fs_node_common_ancestor;
       else
         *relation = svn_fs_node_unrelated;
 
+      return SVN_NO_ERROR;
+    }
+  else if (!(commit_a && commit_b))
+    {
+      if (root_a->rev == root_b->rev && !strcmp(path_a, path_b))
+        *relation = svn_fs_node_unchanged;
+      else if (!commit_a && !commit_b && !strcmp(path_a, path_b))
+        *relation = svn_fs_node_common_ancestor;
+      else
+        *relation = svn_fs_node_unrelated;
       return SVN_NO_ERROR;
     }
 
