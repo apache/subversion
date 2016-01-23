3b5f2127bca7fd0affb5f7ab99bee1677ad642ff
Stefan Fuhrmann <stefan2@apache.org>
2016-01-23 11:03:27 +0000

    Follow-up on r1726004: Make sure to always set the OOD info in FSX.
    * subversion/libsvn_fs_x/temp_serializer.c
      (svn_fs_x__extract_dir_entry): This function has an early-out code path,
                                     so we must set the OOD field earlier.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1726376 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs_x/temp_serializer.c
+++ subversion/libsvn_fs_x/temp_serializer.c
@@ -839,6 +839,10 @@ svn_fs_x__extract_dir_entry(void **out,
   const apr_uint32_t *lengths =
     svn_temp_deserializer__ptr(data, (const void *const *)&dir_data->lengths);
 
+  /* Before we return, make sure we tell the caller this data is even still
+     relevant. */
+  b->out_of_date = dir_data->txn_filesize != b->txn_filesize;
+
   /* Special case: Early out for empty directories.
      That simplifies tests further down the road. */
   *out = NULL;
@@ -867,8 +871,6 @@ svn_fs_x__extract_dir_entry(void **out,
 
   /* de-serialize that entry or return NULL, if no match has been found.
    * Be sure to check that the directory contents is still up-to-date. */
-  b->out_of_date = dir_data->txn_filesize != b->txn_filesize;
-
   if (found && !b->out_of_date)
     {
       const svn_fs_x__dirent_t *source =
