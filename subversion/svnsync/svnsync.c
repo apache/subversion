203a6f9e473fd02a8b74ffd465684630294121a0
Philip Martin <philip@apache.org>
2016-01-29 18:55:22 +0000

    * subversion/svn/svn.c * subversion/svnadmin/svnadmin.c * subversion/svnbench/svnbench.c * subversion/svnfsfs/svnfsfs.c * subversion/svnlook/svnlook.c * subversion/svnrdump/svnrdump.c * subversion/svnsync/svnsync.c   (main): Exit via SIGINT when SIGINT is caught.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1727621 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svnsync/svnsync.c
+++ subversion/svnsync/svnsync.c
@@ -46,6 +46,11 @@
 #include <apr_signal.h>
 #include <apr_uuid.h>
 
+#ifndef WIN32
+#include <signal.h>
+#include <unistd.h>
+#endif
+
 static svn_opt_subcommand_t initialize_cmd,
                             synchronize_cmd,
                             copy_revprops_cmd,
@@ -2470,5 +2475,15 @@ main(int argc, const char *argv[])
     }
 
   svn_pool_destroy(pool);
+
+#ifndef WIN32
+  /* If cancelled by SIGINT then attempt to exit via SIGINT.  This
+     allows the shell to use WIFSIGNALED and WTERMSIG to detect the
+     SIGINT.  See http://www.cons.org/cracauer/sigint.html  */
+  if (cancelled && apr_signal(SIGINT, SIG_DFL) == APR_SUCCESS)
+    /* No APR support for getpid() so cannot use apr_proc_kill(). */
+    kill(getpid(), SIGINT);
+#endif
+
   return exit_code;
 }
