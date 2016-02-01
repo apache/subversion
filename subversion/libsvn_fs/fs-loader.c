092d30529c4fdacaf32cd65bc987ee57b111a2bb
Stefan Fuhrmann <stefan2@apache.org>
2016-02-01 19:57:35 +0000

    Follow-up to r1727838: (Hopefully) fix the cyclic library dependecy between lib_fs and lib_fs_util that broke the Windows build.
    Reported by: rhuijben
    
    * subversion/include/private/svn_fs_util.h
      (svn_fs__get_deleted_node): Move declaration from here ...
    
    * subversion/include/private/svn_fs_private.h
      (svn_fs__get_deleted_node): ... to here. Use Doxygen comments like in the
                                  rest of that file.
    
    * subversion/libsvn_fs_util/fs-util.c
      (svn_fs__get_deleted_node): Move implementation from here ...
    
    * subversion/libsvn_fs/fs-loader.c
      (svn_fs__get_deleted_node): ... to here.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1727993 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_fs/fs-loader.c
+++ subversion/libsvn_fs/fs-loader.c
@@ -45,6 +45,7 @@
 #include "private/svn_atomic.h"
 #include "private/svn_fs_private.h"
 #include "private/svn_fs_util.h"
+#include "private/svn_fspath.h"
 #include "private/svn_utf_private.h"
 #include "private/svn_mutex.h"
 #include "private/svn_subr_private.h"
@@ -1669,6 +1670,64 @@ svn_fs_get_file_delta_stream(svn_txdelta_stream_t **stream_p,
                            stream_p,
                            source_root, source_path,
                            target_root, target_path, pool));
+}
+
+svn_error_t *
+svn_fs__get_deleted_node(svn_fs_root_t **node_root,
+                         const char **node_path,
+                         svn_fs_root_t *root,
+                         const char *path,
+                         apr_pool_t *result_pool,
+                         apr_pool_t *scratch_pool)
+{
+  const char *parent_path, *name;
+  svn_fs_root_t *copy_root;
+  const char *copy_path;
+
+  /* History traversal does not work with transaction roots.
+   * Therefore, do it "by hand". */
+
+  /* If the parent got copied in ROOT, PATH got copied with it.
+   * Otherwise, we will find the node at PATH in the revision prior to ROOT.
+   */
+  svn_fspath__split(&parent_path, &name, path, scratch_pool);
+  SVN_ERR(svn_fs_closest_copy(&copy_root, &copy_path, root, parent_path,
+                              scratch_pool));
+
+  /* Copied in ROOT? */
+  if (   copy_root
+      && (   svn_fs_revision_root_revision(copy_root)
+          == svn_fs_revision_root_revision(root)))
+    {
+      svn_revnum_t copyfrom_rev;
+      const char *copyfrom_path;
+      const char *rel_path;
+      SVN_ERR(svn_fs_copied_from(&copyfrom_rev, &copyfrom_path,
+                                 copy_root, copy_path, scratch_pool));
+
+      SVN_ERR(svn_fs_revision_root(node_root, svn_fs_root_fs(root),
+                                   copyfrom_rev, result_pool));
+      rel_path = svn_fspath__skip_ancestor(copy_path, path);
+      *node_path = svn_fspath__join(copyfrom_path, rel_path, result_pool);
+    }
+  else
+    {
+      svn_revnum_t revision;
+      svn_revnum_t previous_rev;
+
+      /* Determine the latest revision before ROOT. */
+      revision = svn_fs_revision_root_revision(root);
+      if (SVN_IS_VALID_REVNUM(revision))
+        previous_rev = revision - 1;
+      else
+        previous_rev = svn_fs_txn_root_base_revision(root);
+
+      SVN_ERR(svn_fs_revision_root(node_root, svn_fs_root_fs(root),
+                                   previous_rev, result_pool));
+      *node_path = apr_pstrdup(result_pool, path);
+    }
+
+  return SVN_NO_ERROR;
 }
 
 svn_error_t *
