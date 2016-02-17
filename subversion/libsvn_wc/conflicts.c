a8af0c6e1417e394655ac764599e0ed7f9711f06
Stefan Sperling <stsp@apache.org>
2016-02-17 21:05:14 +0000

    * subversion/libsvn_wc/conflicts.c   (svn_wc__conflict_tree_update_raise_moved_away): Ensure that conflict action    has the expected value.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1730944 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/conflicts.c
+++ subversion/libsvn_wc/conflicts.c
@@ -3524,6 +3524,12 @@ svn_wc__conflict_tree_update_raise_moved_away(svn_wc_context_t *wc_ctx,
                              svn_token__to_word(reason_map, reason),
                              svn_dirent_local_style(local_abspath,
                                                      scratch_pool));
+  if (action != svn_wc_conflict_action_edit)
+    return svn_error_createf(SVN_ERR_WC_CONFLICT_RESOLVER_FAILURE, NULL,
+                             _("Unexpected conflict action '%s' on '%s'"),
+                             svn_token__to_word(action_map, action),
+                             svn_dirent_local_style(local_abspath,
+                                                    scratch_pool));
 
   /* Raise local moved-away vs. incoming edit conflicts on any children
    * moved out of this directory, and leave this directory as-is.
