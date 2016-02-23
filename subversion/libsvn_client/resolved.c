4c26fd45262aba13167fda1d40dc6096b8a80a94
Stefan Sperling <stsp@apache.org>
2016-02-23 11:03:00 +0000

    Continue breaking up the massive resolve_tree_conflict() function into smaller per-resolution option handlers.
    This repairs stat_test 45 (broken since r1731832).
    
    * subversion/libsvn_client/resolved.c
      (resolve_update_raise_moved_away): New helper function.
      (svn_client_conflict_tree_get_resolution_options): Use new helper.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1731837 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/resolved.c
+++ subversion/libsvn_client/resolved.c
@@ -942,6 +942,39 @@ resolve_update_break_moved_away(svn_client_conflict_option_t *option,
 
 /* Implements conflict_option_resolve_func_t. */
 static svn_error_t *
+resolve_update_raise_moved_away(svn_client_conflict_option_t *option,
+                                svn_client_conflict_t *conflict,
+                                apr_pool_t *scratch_pool)
+{
+  const char *local_abspath;
+  const char *lock_abspath;
+  svn_client_ctx_t *ctx = conflict->ctx;
+  svn_error_t *err;
+
+  local_abspath = svn_client_conflict_get_local_abspath(conflict);
+
+  SVN_ERR(svn_wc__acquire_write_lock_for_resolve(&lock_abspath, ctx->wc_ctx,
+                                                 local_abspath,
+                                                 scratch_pool, scratch_pool));
+  err = svn_wc__conflict_tree_update_raise_moved_away(ctx->wc_ctx,
+                                                      local_abspath,
+                                                      ctx->cancel_func,
+                                                      ctx->cancel_baton,
+                                                      ctx->notify_func2,
+                                                      ctx->notify_baton2,
+                                                      scratch_pool);
+  err = svn_error_compose_create(err, svn_wc__release_write_lock(ctx->wc_ctx,
+                                                                 lock_abspath,
+                                                                 scratch_pool));
+  SVN_ERR(err);
+
+  conflict->resolution_tree = svn_client_conflict_option_get_id(option);
+
+  return SVN_NO_ERROR;
+}
+
+/* Implements conflict_option_resolve_func_t. */
+static svn_error_t *
 resolve_update_moved_away_node(svn_client_conflict_option_t *option,
                                svn_client_conflict_t *conflict,
                                apr_pool_t *scratch_pool)
@@ -1314,7 +1347,7 @@ svn_client_conflict_tree_get_resolution_options(apr_array_header_t **options,
               option->description =
                 _("prepare for updating moved-away children, if any");
               option->conflict = conflict;
-              option->do_resolve_func = resolve_update_break_moved_away;
+              option->do_resolve_func = resolve_update_raise_moved_away;
               APR_ARRAY_PUSH((*options), const svn_client_conflict_option_t *) =
                 option;
             }
