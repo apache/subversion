2d0b02fcfcefb1a8b043fa60e2d576357f9145be
Stefan Sperling <stsp@apache.org>
2016-02-24 10:38:59 +0000

    Replace calls to svn_client_conflict_from_wc_description2_t() with calls to svn_client_conflict_get(). These calls are equivalent, except that the latter needs a client context and a local abspath. The abspath is taken from svn_wc_conflict_description2_t for now, since that's where our info/status APIs put it. The context can be passed down from callers.
    * subversion/svn/info-cmd.c
      (print_info_baton_t): Add 'ctx' client context member.
      (print_info_xml, print_info): Use svn_client_conflict_get(). 
      (svn_cl__info): Wire up 'ctx' in baton.
    
    * subversion/svn/status.c
      (print_status): Use svn_client_conflict_get().
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1732059 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svn/status.c
+++ subversion/svn/status.c
@@ -289,9 +289,8 @@ print_status(const char *target_abspath,
                                             local_abspath, pool, pool));
           SVN_ERR_ASSERT(desc2 != NULL);
 
-          SVN_ERR(svn_client_conflict_from_wc_description2_t(&tree_conflict,
-                                                             desc2,
-                                                             pool, pool));
+          SVN_ERR(svn_client_conflict_get(&tree_conflict, desc2->local_abspath,
+                                          ctx, pool, pool));
           tree_status_code = 'C';
           SVN_ERR(svn_cl__get_human_readable_tree_conflict_description(
                             &desc, tree_conflict, pool));
