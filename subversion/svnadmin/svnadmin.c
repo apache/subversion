84ac49107a62143515e2b41c5ded2ffc0e2533d7
Stefan Fuhrmann <stefan2@apache.org>
2016-05-28 16:51:16 +0000

    Add support for FSX-specific info to 'svnadmin info'.
    * subversion/svnadmin/svnadmin.c
      (subcommand_info): Add a special case for FSX repositories just like we
                         did for FSFS.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1745905 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svnadmin/svnadmin.c
+++ subversion/svnadmin/svnadmin.c
@@ -2246,6 +2246,19 @@ subcommand_info(apr_getopt_t *os, void *baton, apr_pool_t *pool)
         else
           SVN_ERR(svn_cmdline_printf(pool, _("FSFS Logical Addressing: no\n")));
       }
+    else if (!strcmp(info->fs_type, SVN_FS_TYPE_FSX))
+      {
+        const svn_fs_fsx_info_t *fsx_info = (const void *)info;
+
+        const int shard_size = fsx_info->shard_size;
+        const long shards_packed = fsx_info->min_unpacked_rev / shard_size;
+        long shards_full = (head_rev + 1) / shard_size;
+
+        SVN_ERR(svn_cmdline_printf(pool, _("FSX Shard Size: %d\n"),
+                                   shard_size));
+        SVN_ERR(svn_cmdline_printf(pool, _("FSX Shards Packed: %ld/%ld\n"),
+                                   shards_packed, shards_full));
+      }
   }
 
   {
