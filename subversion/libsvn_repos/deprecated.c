802d4ca6c009d0057f849578eee108abbbd33857
Stefan Fuhrmann <stefan2@apache.org>
2016-06-05 14:58:17 +0000

    Deprecate svn_repos_get_logs4.
    Small twist here:  The ra_local log() implementation needs to expose an
    repos_log4-esque API.  So, we re-use all the shim code that adapts log5
    onto log4 by exposing the latter through a new private API.
    
    * subversion/include/svn_repos.h
      (svn_repos_get_logs4): Depricate.
    
    * subversion/include/private/svn_repos_private.h
      (svn_repos__get_logs_compat): Declare new private API.
    
    * subversion/libsvn_repos/log.c
      (log_entry_receiver_baton_t,
       path_change_kind_to_char,
       log4_path_change_receiver,
       log4_entry_receiver,
       svn_repos_get_logs4): Move these log5/log4 shims from here ...
    
    * subversion/libsvn_repos/compat.c
      (): ... to this new file.
      (svn_repos_get_logs4): Rename this ...
      (svn_repos__get_logs_compat): ... to that.
    
    * subversion/libsvn_repos/deprecated.c
      (svn_repos_get_logs4): Add trivial implementation.
    
    * subversion/libsvn_ra_local/ra_plugin.c
      (svn_ra_local__get_log): Call the new private API instead of the
                               deprecated one.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1746926 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_repos/deprecated.c
+++ subversion/libsvn_repos/deprecated.c
@@ -35,6 +35,8 @@
 
 #include "repos.h"
 
+#include "private/svn_repos_private.h"
+
 
 
 
@@ -505,6 +507,30 @@ svn_repos_fs_get_locks(apr_hash_t **locks,
 
 
 /*** From logs.c ***/
+svn_error_t *
+svn_repos_get_logs4(svn_repos_t *repos,
+                    const apr_array_header_t *paths,
+                    svn_revnum_t start,
+                    svn_revnum_t end,
+                    int limit,
+                    svn_boolean_t discover_changed_paths,
+                    svn_boolean_t strict_node_history,
+                    svn_boolean_t include_merged_revisions,
+                    const apr_array_header_t *revprops,
+                    svn_repos_authz_func_t authz_read_func,
+                    void *authz_read_baton,
+                    svn_log_entry_receiver_t receiver,
+                    void *receiver_baton,
+                    apr_pool_t *pool)
+{
+  return svn_repos__get_logs_compat(repos, paths, start, end, limit,
+                                    discover_changed_paths,
+                                    strict_node_history,
+                                    include_merged_revisions, revprops,
+                                    authz_read_func, authz_read_baton,
+                                    receiver, receiver_baton, pool);
+}
+
 svn_error_t *
 svn_repos_get_logs3(svn_repos_t *repos,
                     const apr_array_header_t *paths,
