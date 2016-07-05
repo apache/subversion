099bd4a7dec594c0accf9317ba81701a7615f145
Stefan Sperling <stsp@apache.org>
2016-07-05 17:06:41 +0000

    Improve resolver support for incoming file add vs local file add upon update.
    Add the "(R) delete my file and replace it with incoming file" option and
    improves the "(i) ignore incoming addition" option, and refine related
    option descriptions.
    
    * subversion/libsvn_client/conflicts.c
      (verify_local_state_for_incoming_add_upon_update): New helper function.
       Checks the local WC state for conditions we expect after an update flagged
       an add vs add conflict.
      (resolve_incoming_add_ignore): Use new helper to check conditions before
       marking the conflict resolved.
      (resolve_update_incoming_added_file_replace): New resolution function.
      (configure_option_incoming_add_ignore): Add specific option descriptions
       for the update and switch cases.
      (configure_option_incoming_added_file_replace): Apply option to 'svn update'.
    
    * subversion/svn/conflict-callbacks.c
      (builtin_resolver_options): Tweak "R" short option description to cover
       the update operation case as well as merge.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1751509 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svn/conflict-callbacks.c
+++ subversion/svn/conflict-callbacks.c
@@ -426,7 +426,7 @@ static const resolver_option_t builtin_resolver_options[] =
   /* Options for incoming file add vs local file add upon merge. */
   { "m", N_("merge the files"), NULL,
     svn_client_conflict_option_incoming_added_file_text_merge },
-  { "R", N_("replace my file with incoming file"), NULL,
+  { "R", N_("delete my file and replace it with incoming file"), NULL,
     svn_client_conflict_option_incoming_added_file_replace },
   { "M", N_("replace my file with incoming file and merge the files"), NULL,
     svn_client_conflict_option_incoming_added_file_replace_and_merge },
@@ -434,7 +434,7 @@ static const resolver_option_t builtin_resolver_options[] =
   /* Options for incoming dir add vs local dir add upon merge. */
   { "m", N_("merge the directories"), NULL,
     svn_client_conflict_option_incoming_added_dir_merge },
-  { "R", N_("replace my directory with incoming directory"), NULL,
+  { "R", N_("delete my directory and replace it with incoming directory"), NULL,
     svn_client_conflict_option_incoming_added_dir_replace },
   { "M", N_("replace my directory with incoming directory and merge"), NULL,
     svn_client_conflict_option_incoming_added_dir_replace_and_merge },
