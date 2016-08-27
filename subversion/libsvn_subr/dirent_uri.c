9a23a56f5de8f741c5e366d0ae3be183871e137d
Stefan Sperling <stsp@apache.org>
2016-08-27 21:02:27 +0000

    Fix issue #4652 which shows how to trigger an assertion failure in svn_dirent_get_absolute() by passing invalid input on the command line.
    Report a proper error message instead.
    
    * subversion/libsvn_subr/dirent_uri.c
      (svn_dirent_get_absolute): If the caller passed a URL, return an error.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1758069 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_subr/dirent_uri.c
+++ subversion/libsvn_subr/dirent_uri.c
@@ -1598,7 +1598,10 @@ svn_dirent_get_absolute(const char **pabsolute,
   apr_status_t apr_err;
   const char *path_apr;
 
-  SVN_ERR_ASSERT(! svn_path_is_url(relative));
+  if (svn_path_is_url(relative))
+    return svn_error_createf(SVN_ERR_BAD_FILENAME, NULL,
+                             _("Local path expected but '%s' looks like a URL"),
+                             relative);
 
   /* Merge the current working directory with the relative dirent. */
   SVN_ERR(svn_path_cstring_from_utf8(&path_apr, relative, pool));
