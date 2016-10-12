7368ef2a3eb6c67ace208cc1df26ec03e612f959
Stefan Fuhrmann <stefan2@apache.org>
2016-10-12 15:38:15 +0000

    On the authzperf branch:
    * subversion/libsvn_repos/authz.c
      (add_next_node): Refine commentary. No functional change.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/authzperf@1764482 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_repos/authz.c
+++ subversion/libsvn_repos/authz.c
@@ -952,7 +952,8 @@ add_next_node(lookup_state_t *state,
     {
       /* The rule with the highest sequence number is the one that applies.
        * Not all nodes that we are following have rules that apply directly
-       * to this path but only some deep sub-node. */
+       * to this path but are mere intermediates that may only have some
+       * matching deep sub-node. */
       combine_access(&state->rights, &node->rights);
 
       /* The rule tree node can be seen as an overlay of all the nodes that
@@ -964,8 +965,8 @@ add_next_node(lookup_state_t *state,
       /* NODE is now enlisted as a (potential) match for the next segment. */
       APR_ARRAY_PUSH(state->next, node_t *) = node;
 
-      /* Variable length sub-segment sequences apply to the same node due
-       * to matching empty sequences as well. */
+      /* Variable length sub-segment sequences apply to the same node as
+       * they match empty sequences as well. */
       if (node->pattern_sub_nodes && node->pattern_sub_nodes->any_var)
         {
           node = node->pattern_sub_nodes->any_var;
