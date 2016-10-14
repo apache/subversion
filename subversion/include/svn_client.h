e21feb3429b7f530511729f4274c2cb33ba11d40
Ivan Zhakov <ivan@apache.org>
2016-10-14 14:36:39 +0000

    Improve C90 compatibility by removing trailing commas in enum lists as well as converting a variadic macro stub with an empty function.
    * subversion/include/svn_client.h
    * subversion/include/svn_wc.h
    * subversion/libsvn_subr/cache-membuffer.c
    * subversion/svn/svn.c
    * subversion/svnmucc/svnmucc.c
      (svn_client_config_option_id_t, svn_wc_notify_action_t,
       prefix_pool_create, svn_cl__longopt_t, sub_main): Remove trailing commas
       in enum lists.
    
    * tools/dev/svnmover/linenoise/linenouse.c
      (lndebug): Replace variadic macro stub with an empty variadic function.
    
    Patch by: Patrick Steinhardt <ps{_AT_}pks.im>
    (Log message tweaked by me.)
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1764919 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_client.h
+++ subversion/include/svn_client.h
@@ -4427,7 +4427,7 @@ typedef enum svn_client_conflict_option_id_t {
   svn_client_conflict_option_incoming_move_dir_merge,
 
   /* Options for local move vs incoming edit on merge. */
-  svn_client_conflict_option_local_move_file_text_merge,
+  svn_client_conflict_option_local_move_file_text_merge
 } svn_client_conflict_option_id_t;
 
 /**
