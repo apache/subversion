3362253039ec6d1ef14f643c53dbaaf4b0adeebf
Stefan Fuhrmann <stefan2@apache.org>
2017-01-02 11:42:53 +0000

    Fix build on systems without multi-thread support.
    * subversion/libsvn_repos/authz.c
      (synchronized_authz_initialize): Don't try to access mutexes when the
                                       system does not support them.
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1776921 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_repos/authz.c
+++ subversion/libsvn_repos/authz.c
@@ -134,8 +134,12 @@ static svn_atomic_t authz_pool_initialized = FALSE;
 static svn_error_t *
 synchronized_authz_initialize(void *baton, apr_pool_t *pool)
 {
+#if APR_HAS_THREADS
   svn_boolean_t multi_threaded
     = apr_allocator_mutex_get(apr_pool_allocator_get(pool)) != NULL;
+#else
+  svn_boolean_t multi_threaded = FALSE;
+#endif
 
   SVN_ERR(svn_object_pool__create(&authz_pool, multi_threaded, pool));
   SVN_ERR(svn_object_pool__create(&filtered_pool, multi_threaded, pool));
