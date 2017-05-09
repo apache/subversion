20ade90a5080f4979b1e7bac085201f9db4a777f
Stefan Sperling <stsp@apache.org>
2017-05-09 15:33:37 +0000

    Correct a spelling mistake in an error code name.
    * subversion/include/svn_error_codes.h
      (SVN_ERR_FS_AMBIUGOUS_CHECKSUM_REP): Rename to ...
      (SVN_ERR_FS_AMBIGUOUS_CHECKSUM_REP): ... this.
    
    * subversion/libsvn_fs_fs/transaction.c,
      subversion/libsvn_fs_x/transaction.c
      (get_shared_rep): Track rename.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1794598 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/include/svn_error_codes.h
+++ subversion/include/svn_error_codes.h
@@ -879,7 +879,7 @@ SVN_ERROR_START
              "Property list is corrupt.")
 
   /** @since New in 1.10. */
-  SVN_ERRDEF(SVN_ERR_FS_AMBIUGOUS_CHECKSUM_REP,
+  SVN_ERRDEF(SVN_ERR_FS_AMBIGUOUS_CHECKSUM_REP,
              SVN_ERR_FS_CATEGORY_START + 67,
              "Content checksums supposedly match but content does not.")
 
