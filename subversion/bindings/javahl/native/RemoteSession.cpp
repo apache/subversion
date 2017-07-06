59e4f31d18e8cf6c9fe8e7da5ba912ad286c11cb
Philip Martin <philip@apache.org>
2017-07-06 20:56:14 +0000

    Add exception checks to some of the JavaHL native code to avoid JVM warnings about JNI problems of the form:
      WARNING in native method: JNI call made without checking exceptions when required to
     
    * subversion/bindings/javahl/native/Array.cpp
      (Array::init): Add exception check.
    
    * subversion/bindings/javahl/native/CreateJ.cpp
      (CreateJ::Mergeinfo): Add exception check.
    
    * subversion/bindings/javahl/native/Iterator.cpp
      (init_iterator, Iterator::next): Add exception check.
    
    * subversion/bindings/javahl/native/OperationContext.cpp
      (OperationContext::closeTunnel): Add exception check.
    
    * subversion/bindings/javahl/native/RemoteSession.cpp
      (build_string_array, long_iterable_to_revnum_array,
       location_hash_to_map): Add exception check.
    
    * subversion/bindings/javahl/native/RevisionRangeList.cpp
      (RevisionRangeList::RevisionRangeList): Add exception check.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1801108 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/bindings/javahl/native/RemoteSession.cpp
+++ subversion/bindings/javahl/native/RemoteSession.cpp
@@ -646,6 +646,8 @@ build_string_array(const Iterator& iter,
     {
       const char* element;
       jstring jitem = (jstring)iter.next();
+      if (JNIUtil::isExceptionThrown())
+        return NULL;
       if (contains_relpaths)
         {
           Relpath item(jitem, pool);
@@ -959,7 +961,10 @@ long_iterable_to_revnum_array(jobject jlong_iterable, apr_pool_t* pool)
   Iterator iter(jlong_iterable);
   while (iter.hasNext())
     {
-      const jlong entry = env->CallLongMethod(iter.next(), mid);
+      jobject next = iter.next();
+      if (JNIUtil::isExceptionThrown())
+        return NULL;
+      const jlong entry = env->CallLongMethod(next, mid);
       if (JNIUtil::isExceptionThrown())
         return NULL;
       APR_ARRAY_PUSH(array, svn_revnum_t) = svn_revnum_t(entry);
@@ -971,6 +976,8 @@ jobject
 location_hash_to_map(apr_hash_t* locations, apr_pool_t* scratch_pool)
 {
   JNIEnv* env = JNIUtil::getEnv();
+  if (JNIUtil::isExceptionThrown())
+    return NULL;
 
   jclass long_cls = env->FindClass("java/lang/Long");
   if (JNIUtil::isExceptionThrown())
