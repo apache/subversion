apache/subversion

commit 747a15c142ed78c87862d80036b7eff27d3e9c7c

Allow building against OpenSSL 1.1.0 on Windows by properly detect the library name, that starting with 1.1.0 now matches that on other platforms.
* build/generator/gen_win_dependencies.py
  (*): Accidentally added an 'f' at start of file. Fixed in r1814725.
  (_find_openssl): Use new library names when possible.
  
* build.conf
  Use standard dependency notation on Windows to support dynamic names.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1814724 13f79535-47bb-0310-9956-ffa450edef68

build/generator/gen_win_dependencies.py
@@ -1,4 +1,4 @@
-#
+f#
 #
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
@@ -878,16 +878,22 @@ class GenDependenciesBase(gen_base.GeneratorBase):
                int(vermatch.group(4)))
     openssl_version = vermatch.group(1)
 
+    libcrypto = 'libcrypto'
+    libssl = 'libssl'
+    if version < (1, 1, 0):
+      libcrypto = 'libeay32'
+      libssl = 'ssleay32'
+
     self._libraries['openssl'] = SVNCommonLibrary('openssl', inc_dir, lib_dir,
-                                                  'ssleay32.lib',
+                                                  '%s.lib' % (libssl,),
                                                   openssl_version,
-                                                  dll_name='ssleay32.dll',
+                                                  dll_name='%s.dll' % (libssl,),
                                                   dll_dir=bin_dir)
 
-    self._libraries['libeay32'] = SVNCommonLibrary('openssl', inc_dir, lib_dir,
-                                                    'libeay32.lib',
+    self._libraries['libcrypto'] = SVNCommonLibrary('openssl', inc_dir, lib_dir,
+                                                    '%s.lib' % (libcrypto,),
                                                     openssl_version,
-                                                    dll_name='libeay32.dll',
+                                                    dll_name='%s.dll' % (libcrypto,),
                                                     dll_dir=bin_dir)
 
   def _find_perl(self, show_warnings):

build.conf
@@ -1471,8 +1471,7 @@ external-lib = $(SVN_SASL_LIBS)
 
 [openssl]
 type = lib
-external-lib = $(SVN_OPENSSL_LIBS)
-msvc-libs = ssleay32.lib libeay32.lib
+external-lib = $(SVN_OPENSSL_LIBS) $(SVN_LIBCRYPTO_LIBS)
 
 [intl]
 type = lib

commit 5532e2d6a6566ecbef041f5486347eae90bf0a3b

* build/generator/gen_win_dependencies.py   Fix typo at start of file introduced in r1814724.
git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1814725 13f79535-47bb-0310-9956-ffa450edef68

build/generator/gen_win_dependencies.py
@@ -1,4 +1,4 @@
-f#
+#
 #
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file