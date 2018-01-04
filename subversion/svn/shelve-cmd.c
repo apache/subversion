b489298b98bafe918b8a86a20c8ace61b892ae6f
Julian Foad <julianfoad@apache.org>
2018-01-04 09:59:20 +0000

    Let 'svn shelves' accept '-q' for consistency with 'svn shelve --list -q'.
    * subversion/svn/svn.c
      (svn_cl__cmd_table): Let 'svn shelves' accept '-q'.
    
    * subversion/svn/shelve-cmd.c
      (svn_cl__shelves): Pass the 'quiet' option through.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1820047 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svn/shelve-cmd.c
+++ subversion/svn/shelve-cmd.c
@@ -316,6 +316,7 @@ svn_cl__shelves(apr_getopt_t *os,
                 void *baton,
                 apr_pool_t *pool)
 {
+  svn_cl__opt_state_t *opt_state = ((svn_cl__cmd_baton_t *) baton)->opt_state;
   svn_client_ctx_t *ctx = ((svn_cl__cmd_baton_t *) baton)->ctx;
   const char *local_abspath;
 
@@ -324,7 +325,8 @@ svn_cl__shelves(apr_getopt_t *os,
     return svn_error_create(SVN_ERR_CL_ARG_PARSING_ERROR, 0, NULL);
 
   SVN_ERR(svn_dirent_get_absolute(&local_abspath, "", pool));
-  SVN_ERR(shelves_list(local_abspath, TRUE /*diffstat*/, ctx, pool));
+  SVN_ERR(shelves_list(local_abspath, ! opt_state->quiet /*diffstat*/,
+                       ctx, pool));
 
   return SVN_NO_ERROR;
 }
