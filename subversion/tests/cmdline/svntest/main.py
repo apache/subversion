7aee002a7c2c87b58000fc74eb3baf2d63b7a2b5
Branko Čibej <brane@apache.org>
2018-02-25 13:22:55 +0000

    Return a better error message when connecting to an URL on a server that does not implement the DAV protocol.
    * subversion/libsvn_ra_serf/options.c
      (options_context_t): New member received_dav_header.
      (capabilities_headers_iterator_callback):
       Set it if the HTTP response contains the DAV: header.
      (options_response_handler):
       Bail out early with a specific message if the server doesn't support DAV.
    
    * subversion/tests/cmdline/dav_tests.py: New test fixture.
    * subversion/tests/cmdline/svntest/main.py
      (other_dav_root_dir, non_dav_root_dir): New global variable.
      (execute_tests): Initialize other_dav_root_dir and non_dav_root_dir.
    * subversion/tests/cmdline/svntest/actions.py
      (setup_pristine_greek_repository):
       Create the directories for other_dav_root_dir and non_dav_root_dir.
    
    * subversion/tests/cmdline/davautocheck.sh:
       Load mod_dav_fs and configure httpd for dav_tests.py.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1825302 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/svntest/main.py
+++ subversion/tests/cmdline/svntest/main.py
@@ -224,6 +224,10 @@ SVN_PROP_INHERITABLE_IGNORES = "svn:global-ignores"
 general_repo_dir = os.path.join(work_dir, "repositories")
 general_wc_dir = os.path.join(work_dir, "working_copies")
 
+# Directories used for DAV tests
+other_dav_root_dir = os.path.join(work_dir, "fsdavroot")
+non_dav_root_dir = os.path.join(work_dir, "nodavroot")
+
 # temp directory in which we will create our 'pristine' local
 # repository and other scratch data.  This should be removed when we
 # quit and when we startup.
@@ -2336,6 +2340,8 @@ def execute_tests(test_list, serial_only = False, test_name = None,
 
   global pristine_url
   global pristine_greek_repos_url
+  global other_dav_root_url
+  global non_dav_root_url
   global svn_binary
   global svnadmin_binary
   global svnlook_binary
@@ -2414,6 +2420,10 @@ def execute_tests(test_list, serial_only = False, test_name = None,
                                 svntest.wc.svn_uri_quote(
                                   pristine_greek_repos_dir.replace(
                                       os.path.sep, '/'))
+
+  other_dav_root_url = options.test_area_url + '/fsdavroot'
+  non_dav_root_url = options.test_area_url + '/nodavroot'
+
 
   if options.use_jsvn:
     if options.svn_bin is None:
