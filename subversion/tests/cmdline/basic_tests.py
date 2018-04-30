abc24978a0a549981a3ba0fd1dc0281ed333f513
Julian Foad <julianfoad@apache.org>
2018-04-30 16:39:58 +0000

    Fix pattern-matching of the top level path in a listing with search.
      svn list -v PATH --search=PATTERN
    
    The output for the top level PATH will now be included when a
    PATTERN matches the last component of its path-within-repository,
    or the empty string for the repository root path. This is consistent with
    matching all other paths in the listing.
    
    Until now, and since the feature was introduced in r1767186, the top
    level path was reported only when a pattern matched the entire
    path-within-repository, starting with "/", of PATH's *parent* directory.
    For example,
      svn ls -v ^/subversion/trunk/README --search=PATTERN
    reported the top-level path only if PATTERN matched
    "/subversion/trunk".
    
    Note that "-v" is specified in these examples because otherwise "svn"
    omits the top-level path when it is a directory.
    
    * subversion/libsvn_client/list.c
      (list_internal): Match the basename, not the dirname, of the top level
        path.
    
    * subversion/libsvn_repos/list.c
      (svn_repos_list): Same.
    
    * subversion/tests/cmdline/basic_tests.py
      (filtered_ls): Extend to test both server- and client-side code paths.
      (filtered_ls_top_level_path): New test.
      (test_list): Run it.
    
    * subversion/tests/libsvn_repos/repos-test.c
      (test_list): Expect the top level path also to match.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1830599 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/basic_tests.py
+++ subversion/tests/cmdline/basic_tests.py
@@ -3121,24 +3121,27 @@ def filtered_ls(sbox):
   sbox.build(read_only=True)
   path = sbox.repo_url + "/A/D"
 
-  # check plain info
-  expected = [ "H/omega\n",
-               "gamma\n" ]
+  # with and without externals, because without externals on a 1.10+ server
+  # a server-side code path is used
+  for extra_opts in [ [], ['--include-externals'] ]:
 
-  exit_code, output, error = svntest.actions.run_and_verify_svn(
-    expected, [], 'ls', path, '--depth=infinity', '--search=*a')
+    expected = [ "H/omega\n",
+                 "gamma\n" ]
 
-  # check case-insensitivity
-  exit_code, output, error = svntest.actions.run_and_verify_svn(
-    expected, [], 'ls', path, '--depth=infinity', '--search=*A')
+    exit_code, output, error = svntest.actions.run_and_verify_svn(
+      expected, [], 'ls', path, '--depth=infinity', '--search=*a', *extra_opts)
 
-  expected = [ "H/\n" ]
-  exit_code, output, error = svntest.actions.run_and_verify_svn(
-    expected, [], 'ls', path, '--depth=infinity', '--search=h')
+    # check case-insensitivity
+    exit_code, output, error = svntest.actions.run_and_verify_svn(
+      expected, [], 'ls', path, '--depth=infinity', '--search=*A', *extra_opts)
 
-  # we don't match full paths
-  exit_code, output, error = svntest.actions.run_and_verify_svn(
-    [], [], 'ls', path, '--depth=infinity', '--search=*/*')
+    expected = [ "H/\n" ]
+    exit_code, output, error = svntest.actions.run_and_verify_svn(
+      expected, [], 'ls', path, '--depth=infinity', '--search=h', *extra_opts)
+
+    # we don't match full paths
+    exit_code, output, error = svntest.actions.run_and_verify_svn(
+      [], [], 'ls', path, '--depth=infinity', '--search=*/*', *extra_opts)
 
 @Issue(4700)
 @XFail(svntest.main.is_fs_type_fsx)
@@ -3196,6 +3199,36 @@ def null_prop_update_last_changed_revision(sbox):
   svntest.actions.run_and_verify_svn(["2\n"], [],
                                      'info', sbox.path('iota'),
                                      '--show-item', 'last-changed-revision')
+
+@Skip(svntest.main.is_os_windows)
+def filtered_ls_top_level_path(sbox):
+  "filtered 'svn ls' top level path"
+
+  sbox.build(read_only=True, create_wc=False)
+  d_path = sbox.repo_url + "/A/B"
+  f_path = sbox.repo_url + "/A/B/lambda"
+
+  d_expected = svntest.verify.RegexListOutput([
+                 r".* \./",  # expect '*B*' to match its name which is 'B'
+                 r".* E/beta",
+                 r".* lambda" ])
+  f_expected = [ "lambda\n" ]
+
+  # with and without externals, because without externals on a 1.10+ server
+  # a server-side code path is used
+  for extra_opts in [ [], ['--include-externals'] ]:
+
+    exit_code, output, error = svntest.actions.run_and_verify_svn(
+      d_expected, [], 'ls', '-v', d_path, '-R', '--search=*B*', *extra_opts)
+
+    exit_code, output, error = svntest.actions.run_and_verify_svn(
+      f_expected, [], 'ls', f_path, '--search=lambda', *extra_opts)
+
+    # we don't match full paths, even for the top level path
+    exit_code, output, error = svntest.actions.run_and_verify_svn(
+      [], [], 'ls', '-v', d_path, '-R', '--search=*/*', *extra_opts)
+    exit_code, output, error = svntest.actions.run_and_verify_svn(
+      [], [], 'ls', f_path, '--search=*/*', *extra_opts)
 
 
 ########################################################################
@@ -3271,6 +3304,7 @@ test_list = [ None,
               filtered_ls,
               null_update_last_changed_revision,
               null_prop_update_last_changed_revision,
+              filtered_ls_top_level_path,
              ]
 
 if __name__ == '__main__':
