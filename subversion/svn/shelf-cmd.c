894d172571fdaae5a32acf2d2662427eb4d256ee
Philip Martin <philip@apache.org>
2018-06-20 19:02:37 +0000

    Allow "svn x-shelf-list" from outside the working copy.
    * subversion/svn/shelf-cmd.c
      (svn_cl__shelf_list): Accept path arguments, supply default '.' path.
    
    * subversion/svn/svn.c
      (svn_cl__cmd_table): Update help text.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1833935 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svn/shelf-cmd.c
+++ subversion/svn/shelf-cmd.c
@@ -31,6 +31,7 @@
 #include "svn_hash.h"
 #include "svn_path.h"
 #include "svn_props.h"
+#include "svn_pools.h"
 #include "svn_utf.h"
 
 #include "cl.h"
@@ -900,16 +901,25 @@ svn_cl__shelf_list(apr_getopt_t *os,
 {
   svn_cl__opt_state_t *opt_state = ((svn_cl__cmd_baton_t *) baton)->opt_state;
   svn_client_ctx_t *ctx = ((svn_cl__cmd_baton_t *) baton)->ctx;
-  const char *local_abspath;
+  apr_array_header_t *targets = NULL;
+  apr_pool_t *subpool = svn_pool_create(pool);
+  int i;
 
-  /* There should be no remaining arguments. */
-  if (os->ind < os->argc)
-    return svn_error_create(SVN_ERR_CL_ARG_PARSING_ERROR, 0, NULL);
+  SVN_ERR(svn_cl__args_to_target_array_print_reserved(&targets, os,
+                                                      opt_state->targets,
+                                                      ctx, FALSE, pool));
+  /* Add "." if user passed 0 arguments */
+  svn_opt_push_implicit_dot_target(targets, pool);
 
-  SVN_ERR(svn_dirent_get_absolute(&local_abspath, "", pool));
-  SVN_ERR(shelves_list(local_abspath,
-                       opt_state->quiet,
-                       ctx, pool));
+  for (i = 0; i < targets->nelts; ++i)
+    {
+      const char *target = APR_ARRAY_IDX(targets, i, const char *);
+
+      svn_pool_clear(subpool);
+      SVN_ERR(shelves_list(target,
+                           opt_state->quiet,
+                           ctx, pool));
+    }
 
   return SVN_NO_ERROR;
 }
