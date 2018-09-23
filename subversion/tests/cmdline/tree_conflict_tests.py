bf648bff9181e42d9fa5c6fedd2b65311fc9c35b
Stefan Sperling <stsp@apache.org>
2018-09-23 11:08:18 +0000

    Follow-up to r1841736:
    * subversion/tests/cmdline/tree_conflict_tests.py
      (local_missing_dir_endless_loop): Restore trailing newline in file content.
       Use main.file_append_binary() to avoid newline translation on Windows.
    
    Suggested by: brane
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1841743 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/tree_conflict_tests.py
+++ subversion/tests/cmdline/tree_conflict_tests.py
@@ -1518,7 +1518,7 @@ def local_missing_dir_endless_loop(sbox):
   sbox.simple_move('A/B', 'A/B2')
   sbox.simple_commit()
   sbox.simple_update()
-  main.file_append(sbox.ospath("A/B2/lambda"), "This is more content.")
+  main.file_append_binary(sbox.ospath("A/B2/lambda"), "This is more content.\n")
   sbox.simple_commit()
   sbox.simple_update()
 
@@ -1544,7 +1544,7 @@ interactive-conflicts = true
   contents = open(sbox.ospath('A1/B/lambda'), 'rb').readlines()
   svntest.verify.compare_and_display_lines(
     "A1/B/lambda has unexpectected contents", sbox.ospath("A1/B/lambda"),
-    [ "This is the file 'lambda'.\n", "This is more content."], contents)
+    [ "This is the file 'lambda'.\n", "This is more content.\n"], contents)
 
 
 #######################################################################
