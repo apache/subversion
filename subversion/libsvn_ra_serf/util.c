befc607647c0342cbc00448ec7698295254673de
svn-role <svn-role@unknown>
2018-10-02 04:00:05 +0000

    Merge the r1836762 group from trunk:
     * r1836762, r1836802
       Store the HTTPS client cert password.
       Justification:
         Restores a feature lost when we switched from neon to serf.
       Votes:
         +1: philip, rhuijben, stsp
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/1.10.x@1842578 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_ra_serf/util.c
+++ subversion/libsvn_ra_serf/util.c
@@ -756,6 +756,9 @@ handle_client_cert_pw(void *data,
 
     if (creds)
       {
+        /* At this stage we are unable to check whether the password
+           is correct; if it is incorrect serf will fail to establish
+           an SSL connection and will return a generic SSL error. */
         svn_auth_cred_ssl_client_cert_pw_t *pw_creds;
         pw_creds = creds;
         *password = pw_creds->password;
@@ -1444,6 +1447,23 @@ handle_response(serf_request_t *request,
   handler->reading_body = TRUE;
 
  process_body:
+
+  /* A client cert file password was obtained and worked (any HTTP
+     response means that the SSL connection was established.) */
+  if (handler->conn->ssl_client_pw_auth_state)
+    {
+      SVN_ERR(svn_auth_save_credentials(handler->conn->ssl_client_pw_auth_state,
+                                        handler->session->pool));
+      handler->conn->ssl_client_pw_auth_state = NULL;
+    }
+  if (handler->conn->ssl_client_auth_state)
+    {
+      /* The cert file provider doesn't have any code to save creds so
+         this is currently a no-op. */
+      SVN_ERR(svn_auth_save_credentials(handler->conn->ssl_client_auth_state,
+                                        handler->session->pool));
+      handler->conn->ssl_client_auth_state = NULL;
+    }
 
   /* We've been instructed to ignore the body. Drain whatever is present.  */
   if (handler->discard_body)
