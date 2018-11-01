29ef463a0d797597eac5fc0408180e6860bb86a1
Branko Čibej <brane@apache.org>
2018-11-01 07:50:18 +0000

    [In tools/buildbot/slaves/svn-x64-macosx] * svnbuild-bindings.sh: Ignore annoying linker warnings from out-of-date libraries on  macOS 10.13.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1845403 13f79535-47bb-0310-9956-ffa450edef68

--- tools/buildbot/slaves/svn-x64-macosx/svnbuild-bindings.sh
+++ tools/buildbot/slaves/svn-x64-macosx/svnbuild-bindings.sh
@@ -25,21 +25,17 @@ scripts=$(cd $(dirname "$0") && pwd)
 . ${scripts}/setenv.sh
 
 #
-# Step 4: build swig-py
+# Step 4: build bindings
 #
 
-echo "============ make swig-py"
-cd ${absbld}
-make swig-py
+build_bindings() {
+    echo "============ make $1"
+    cd ${absbld}
+    make $1 2>&1 \
+        | grep -v '^ld: [w]arning: text-based stub file.*Falling back to library file for linking. *$'
+}
 
-echo "============ make swig-pl"
-cd ${absbld}
-make swig-pl
-
-echo "============ make swig-rb"
-cd ${absbld}
-make swig-rb
-
-echo "============ make javahl"
-cd ${absbld}
-make javahl
+build_bindings swig-py
+build_bindings swig-pl
+build_bindings swig-rb
+build_bindings javahl
