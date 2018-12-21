be25271171f1c398254e0750ac9aed3fe9f4d24a
Branko Čibej <brane@apache.org>
2018-12-21 12:37:37 +0000

    Remove compatibility shared pointer wrappers from SVN++.
    * subversion/bindings/cxx/include/svnxx/_compat.hpp: Removed.
    * subversion/bindings/cxx/include/svnxx/noncopyable.hpp: New.
      (class noncopyable): Moved definitoin here from _compat.hpp and into
       the 'detail' namespace.
    
    * subversion/bindings/cxx/include/svnxx/exception.hpp,
    * subversion/bindings/cxx/src/exception.cpp:
       Use 'std::shared_ptr' instead of the wrapper in the 'compat' namespace.
    
    * subversion/bindings/cxx/src/aprwrap/pool.hpp:
       Use 'detail::noncopyable' instead of 'compat::noncopyable'.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1849463 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subversion/bindings/cxx/include/svnxx/noncopyable.hpp
@@ -0,0 +1,72 @@
+/**
+ * @copyright
+ * ====================================================================
+ *    Licensed to the Apache Software Foundation (ASF) under one
+ *    or more contributor license agreements.  See the NOTICE file
+ *    distributed with this work for additional information
+ *    regarding copyright ownership.  The ASF licenses this file
+ *    to you under the Apache License, Version 2.0 (the
+ *    "License"); you may not use this file except in compliance
+ *    with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing,
+ *    software distributed under the License is distributed on an
+ *    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *    KIND, either express or implied.  See the License for the
+ *    specific language governing permissions and limitations
+ *    under the License.
+ * ====================================================================
+ * @endcopyright
+ */
+
+#ifndef __cplusplus
+#error "This is a C++ header file."
+#endif
+
+#ifndef SVNXX_NONCOPYABLE_HPP
+#define SVNXX_NONCOPYABLE_HPP
+
+#if defined(SVNXX_USE_BOOST) && !defined(DOXYGEN)
+#include <boost/noncopyable.hpp>
+#endif
+
+namespace apache {
+namespace subversion {
+namespace svnxx {
+namespace detail {
+
+#if defined(SVNXX_USE_BOOST) && !defined(DOXYGEN)
+using boost::noncopyable;
+#else
+namespace noncopyable_ {
+
+/**
+ * @brief Base class for non-copyable objects.
+ *
+ * Objects of classes derived from @c noncopyable cannot be copyed,
+ * but can used as rvalue references and with <tt>std::move</tt>.
+ *
+ * @note Use @e private inheritance to avoid polymorphism traps!
+ */
+class noncopyable
+{
+protected:
+  constexpr noncopyable() = default;
+  ~noncopyable() = default;
+private:
+  noncopyable(const noncopyable&) = delete;
+  noncopyable& operator=(const noncopyable&) = delete;
+};
+} // namespace noncopyable_
+
+using noncopyable = noncopyable_::noncopyable;
+#endif // SVNXX_USE_BOOST
+
+} // namespace detail
+} // namespace svnxx
+} // namespace subversion
+} // namespace apache
+
+#endif  // SVNXX_NONCOPYABLE_HPP
