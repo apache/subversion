2bac17b0848258dbf262bcb6839cc43a0545cce9
Stefan Sperling <stsp@apache.org>
2020-03-26 23:24:54 +0000

    Fix autogen.sh SWIG python bindings generation in --release mode.
    This was accidentally broken in r1869354 (merge swig-py3 branch to trunk).
    
    * build/generator/gen_make.py: When generating autogen-standlone.mk, set
      SWIG_PY_OPTS to "-python". Otherwise swig gets invoked without any
      language options and SWIGPYTHON ends up being undefined. Swig is then
      unable to parse subversion/bindings/swig/include/svn_containers.swg
      because that file relies on the macro definition "hash_argout_typemap"
      which only takes effect if SWIGPYTHON is defined by swig.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1875742 13f79535-47bb-0310-9956-ffa450edef68

--- build/generator/gen_make.py
+++ build/generator/gen_make.py
@@ -511,6 +511,7 @@ class Generator(gen_base.GeneratorBase):
     standalone.write('top_srcdir = .\n')
     standalone.write('top_builddir = .\n')
     standalone.write('SWIG = swig\n')
+    standalone.write('SWIG_PY_OPTS = -python\n')
     standalone.write('PYTHON = ' + sys.executable + '\n')
     standalone.write('\n')
     standalone.write(open("build-outputs.mk","r").read())
