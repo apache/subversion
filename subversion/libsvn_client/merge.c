677e66a3cdd74d05f36dad8996df675ed155f33e
Julian Foad <julianfoad@apache.org>
2020-06-25 16:35:14 +0000

    Fix issue #4859 "Merge removing a folder with non-inheritable mergeinfo -> E155023: can't set properties: invalid status for updating properties".
    The code was attempting to set mergeinfo on a path that is being deleted by
    the merge.  This happened only in certain unusual cases such as when
    deleting a folder that had non-inheritable mergeinfo and had children.
    
    This patch fixes the problem by removing the 'children_with_mergeinfo'
    entries for sub-paths of a deleted folder.
    
    * subversion/libsvn_client/merge.c
      (merge_cmd_baton_t): Allow modifying 'children_with_mergeinfo'.
      (record_update_delete): Remove from 'children_with_mergeinfo' all paths
        in the subtree.
    
    * subversion/tests/cmdline/merge_tests.py
      (merge_deleted_folder_with_mergeinfo): Remove 'XFail'.
      (merge_deleted_folder_with_mergeinfo_2): New test.
      (test_list): Add the new test.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1879192 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_client/merge.c
+++ subversion/libsvn_client/merge.c
@@ -264,7 +264,7 @@ typedef struct merge_cmd_baton_t {
 
   /* Reference to the one-and-only CHILDREN_WITH_MERGEINFO (see global
      comment) or a similar list for single-file-merges */
-  const apr_array_header_t *children_with_mergeinfo;
+  apr_array_header_t *children_with_mergeinfo;
 
   svn_client_ctx_t *ctx;              /* Client context for callbacks, etc. */
 
@@ -1543,6 +1543,25 @@ record_update_delete(merge_cmd_baton_t *merge_b,
 
       svn_hash_sets(parent_db->pending_deletes, dup_abspath,
                     svn_node_kind_to_word(kind));
+    }
+
+  /* Note in children_with_mergeinfo that all paths in this subtree are
+   * being deleted, to avoid trying to set mergeinfo on them later. */
+  if (merge_b->children_with_mergeinfo)
+    {
+      int i;
+
+      for (i = 0; i < merge_b->children_with_mergeinfo->nelts; i++)
+        {
+          svn_client__merge_path_t *child
+            = APR_ARRAY_IDX(merge_b->children_with_mergeinfo, i,
+                            svn_client__merge_path_t *);
+
+          if (svn_dirent_is_ancestor(local_abspath, child->abspath))
+            {
+              SVN_ERR(svn_sort__array_delete2(merge_b->children_with_mergeinfo, i--, 1));
+            }
+        }
     }
 
   return SVN_NO_ERROR;
