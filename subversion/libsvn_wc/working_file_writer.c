67321a1c704ed46fa6d53f307328b76a5c2af76b
Evgeny Kotkov <kotkov@apache.org>
2021-03-04 13:28:21 +0000

    * subversion/libsvn_wc/working_file_writer.c   (svn_wc__working_file_writer_open): With r1887173, we no longer have to    account for the difference between an empty and a null keyword set, so    just use the KEYWORDS as is.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1887175 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/libsvn_wc/working_file_writer.c
+++ subversion/libsvn_wc/working_file_writer.c
@@ -73,9 +73,6 @@ svn_wc__working_file_writer_open(svn_wc__working_file_writer_t **writer_p,
   svn_stream_t *install_stream;
   svn_stream_t *write_stream;
 
-  if (keywords && apr_hash_count(keywords) <= 0)
-    keywords = NULL;
-
   SVN_ERR(svn_stream__create_for_install(&install_stream, tmp_abspath,
                                          result_pool, scratch_pool));
 
