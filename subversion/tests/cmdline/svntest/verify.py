3daa7325fbbbd11042ecd7203b6b5fcba1974d83
Julian Foad <julianfoad@apache.org>
2022-02-18 15:11:45 +0000

    Fix a Python2-ism in the test suite.
    This code broke under Python3 when trying to display test failure
    diagnostics.
    
    * subversion/tests/cmdline/svntest/verify.py
      (RegexListOutput): s/map(None,)/itertools.zip_longest()/
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1898185 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/svntest/verify.py
+++ subversion/tests/cmdline/svntest/verify.py
@@ -28,6 +28,7 @@ import re, sys
 from difflib import unified_diff, ndiff
 import pprint
 import logging
+import itertools
 
 import svntest
 
@@ -295,7 +296,7 @@ class RegexListOutput(ExpectedOutput):
       if len(self.expected) != len(actual):
         logger.warn('# Expected %d lines; actual %d lines' %
                     (len(self.expected), len(actual)))
-      for e, a in map(None, self.expected_res, actual):
+      for e, a in itertools.zip_longest(self.expected_res, actual):
         if e is not None and a is not None and regex_fullmatch(e, a):
           logger.warn("|  " + a.rstrip())
         else:
