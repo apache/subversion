ea56fd34fbfbfce5e0c1c077ec131a57a6d7e7e0
Julian Foad <julianfoad@apache.org>
2022-03-02 11:04:31 +0000

    Multi-WC-format: simplify supported versions display.
    Change the display format in 'svn --version' from:
    
      | * compatible with Subversion v1.8 to v1.15 (WC format 31)
      | * compatible with Subversion v1.15 (WC format 32)
    
    to:
    
      | * WC format 31, compatible with Subversion v1.8 and newer
      | * WC format 32, compatible with Subversion v1.15 and newer
    
    
    * subversion/svn/help-cmd.c
      (print_supported_wc_formats): Change as above.
    
    * subversion/tests/cmdline/getopt_tests_data/svn--version--verbose_stdout,
      subversion/tests/cmdline/getopt_tests_data/svn--version_stdout
      Update expected output for tests.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1898524 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/svn/help-cmd.c
+++ subversion/svn/help-cmd.c
@@ -54,28 +54,12 @@ print_supported_wc_formats(svn_stringbuf_t *output,
   for (i = 0; wc_formats[i].version_min; i++)
     {
       const svn_client_wc_format_t *v = &wc_formats[i];
-      const char *s;
-
-      if (v->version_min->major == v->version_min->major &&
-          v->version_min->minor == v->version_max->minor)
-        {
-          s = apr_psprintf(
-                pool,
-                _("%scompatible with Subversion v%d.%d (WC format %d)\n"),
-                prefix,
-                v->version_min->major, v->version_min->minor,
-                v->wc_format);
-        }
-      else
-        {
-          s = apr_psprintf(
-                pool,
-                _("%scompatible with Subversion v%d.%d to v%d.%d (WC format %d)\n"),
-                prefix,
-                v->version_min->major, v->version_min->minor,
-                v->version_max->major, v->version_max->minor,
-                v->wc_format);
-        }
+      const svn_version_t *ver = v->version_min;
+      const char *s
+        = apr_psprintf(
+            pool,
+            _("%sWC format %d, compatible with Subversion v%d.%d and newer\n"),
+            prefix, v->wc_format, ver->major, ver->minor);
 
       svn_stringbuf_appendcstr(output, s);
     }
