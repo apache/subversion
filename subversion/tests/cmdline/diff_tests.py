08b2ac2bfdd15527e613104f7e762f242bec9638
Julian Foad <julianfoad@apache.org>
2022-03-02 14:12:33 +0000

    On the 'pristines-on-demand-on-mwf' branch: fix disabled tests.
    * subversion/tests/cmdline/diff_tests.py
      (diff_external_diffcmd): Re-enable. Allow path to the pristine version to
        be a temporary file.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/pristines-on-demand-on-mwf@1898528 13f79535-47bb-0310-9956-ffa450edef68

--- subversion/tests/cmdline/diff_tests.py
+++ subversion/tests/cmdline/diff_tests.py
@@ -3063,7 +3063,6 @@ def diff_wrong_extension_type(sbox):
                                      'diff', '-x', sbox.wc_dir, '-r', '1')
 
 # Check the order of the arguments for an external diff tool
-@Wimp("Relies on wc.text_base_path()")
 def diff_external_diffcmd(sbox):
   "svn diff --diff-cmd provides the correct arguments"
 
@@ -3083,15 +3082,15 @@ def diff_external_diffcmd(sbox):
   if sys.platform == 'win32':
     diff_script_path = "%s.bat" % diff_script_path
 
-  expected_output = svntest.verify.ExpectedOutput([
+  expected_output = svntest.verify.RegexListOutput([
     "Index: iota\n",
     "===================================================================\n",
     "-u\n",
     "-L\n",
-    "iota\t(revision 1)\n",
+    r"iota\t\(revision 1\)\n",
     "-L\n",
-    "iota\t(working copy)\n",
-    os.path.abspath(svntest.wc.text_base_path("iota")) + "\n",
+    r"iota\t\(working copy\)\n",
+    os.path.abspath(svntest.main.get_admin_name()) + '.*' + "\n",
     os.path.abspath("iota") + "\n"])
 
   # Check that the output of diff corresponds with the expected arguments,
