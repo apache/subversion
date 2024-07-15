8db00b0d5319965ad352a531d26a23abef402845
Timofei Zhakov <rinrab@apache.org>
2024-07-15 15:01:37 +0000

    On the 'cmake' branch: Follow-up to r1919222: also rename SVN_BUILD_ to SVN_ENABLE_ options in enable conditions for targets which depend on the modules.
    git-svn-id: https://svn.apache.org/repos/asf/subversion/branches/cmake@1919259 13f79535-47bb-0310-9956-ffa450edef68

--- build/generator/gen_cmake.py
+++ build/generator/gen_cmake.py
@@ -137,7 +137,7 @@ class Generator(gen_base.GeneratorBase):
           else:
             if dep.name in ["libsvn_ra_local", "libsvn_ra_serf", "libsvn_ra_svn",
                             "libsvn_fs_base", "libsvn_fs_fs", "libsvn_fs_x"]:
-              enable_condition.append("SVN_BUILD_" + get_module_name(dep.name))
+              enable_condition.append("SVN_ENABLE_" + get_module_name(dep.name))
 
             libs.append(dep.name)
         elif isinstance(dep, gen_base.ObjectFile):
