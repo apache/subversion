50d239a87e7e3f548a6a49039b2e9b284a001bb5
Timofei Zhakov <rinrab@apache.org>
2024-09-19 15:02:40 +0000

    ra-modules loading: Factor-out init and compat init functions from libsvn_ra/ra_loader.h header into libsvn_ra_*/ra_init.h include file for each module. Then include those headers into it to have them declared in the loader.
    This is only a little refactoring for future change and doesn't includes any
    functional changes. After, the those new headers will be exported from DLLs
    on Windows and will allow us to build the RA modules in shared configuration.
    
    * subversion/libsvn_ra/ra_loader.h
      (svn_ra_local__init,
       svn_ra_svn__init,
       svn_ra_serf__init): Remove declarations.
      (): Include libsvn_ra_local/ra_init.h, libsvn_ra_serf/ra_init.h
       libsvn_ra_svn/ra_init.h for those init functions.
    
    * subversion/libsvn_ra_local/ra_init.h,
      subversion/libsvn_ra_serf/ra_init.h,
      subversion/libsvn_ra_svn/ra_init.h: New headers with init and compat init
      functions; partially copied from lisvn_fs_*/fs_init.h.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1920786 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subversion/libsvn_ra_local/ra_init.h
@@ -0,0 +1,41 @@
+/*
+ * libsvn_ra_local/ra_init.h:  Exported function of libsvn_ra_local
+ *
+ * ====================================================================
+ *    Licensed to the Apache Software Foundation (ASF) under one
+ *    or more contributor license agreements.  See the NOTICE file
+ *    distributed with this work for additional information
+ *    regarding copyright ownership.  The ASF licenses this file
+ *    to you under the Apache License, Version 2.0 (the
+ *    "License"); you may not use this file except in compliance
+ *    with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing,
+ *    software distributed under the License is distributed on an
+ *    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *    KIND, either express or implied.  See the License for the
+ *    specific language governing permissions and limitations
+ *    under the License.
+ * ====================================================================
+ */
+
+#ifndef LIBSVN_RA_RA_LOADER_H
+#error Please include libsvn_ra/ra_loader.h instead of this file
+#else
+
+svn_error_t *svn_ra_local__init(const svn_version_t *loader_version,
+                                const svn_ra__vtable_t **vtable,
+                                apr_pool_t *pool);
+
+/** Initialize libsvn_ra_local.
+ *
+ * @deprecated Provided for backward compatibility with the 1.1 API. */
+SVN_DEPRECATED
+svn_error_t *
+svn_ra_local_init(int abi_version,
+                  apr_pool_t *pool,
+                  apr_hash_t *hash);
+
+#endif
