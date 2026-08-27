apache/subversion

commit 3fc7e73dd6407c1b3166e58bbef81517e41f742d

Reduce the number of reallocations needed when working with string buffers. Valgrind shows that the my__realloc function is called an awful lot of times during common svn actions.
This change introduces a new function that makes it easy to create a string
buffer with an estimated buffer size.

* subversion/include/svn_string.h
  (svn_stringbuf_create_ensure): New function declaration.
* subversion/libsvn_subr/svn_string.c
  (svn_stringbuf_create_ensure): New function definition. Creates an empty
   stringbuf with the requested buffer size.
  (create_stringbuf): Take a minimum buffer size argument.
  (svn_stringbuf_ncreate): Use the new function svn_stringbuf_create_ensure as
   base.
  (svn_stringbuf_createv): Update the call to create_stringbuf.

* subversion/libsvn_subr/xml.c
  (xml_escape_attr, svn_xml_make_open_tag_hash): Estimate the size of the 
   target string, use that to create a new string buffer.

* subversion/libsvn_subr/io.c
  (svn_stringbuf_from_aprfile): Idem.

* subversion/libsvn_subr/stream.c
  (svn_stream_readline): Idem.

* subversion/libsvn_subr/utf.c
  (convert_to_stringbuf): Idem.

* subversion/libsvn_subr/path.c
  (uri_escape, svn_path_uri_decode, illegal_path_escape): Idem.

* subversion/libsvn_ra_serf/update.c
  (start_report): Idem.


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@869736 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_subr/utf.c
@@ -471,8 +471,12 @@ convert_to_stringbuf(xlate_handle_node_t *node,
   apr_size_t destlen = buflen;
   char *destbuf;
 
-  /* Initialize *DEST to an empty stringbuf. */
-  *dest = svn_stringbuf_create("", pool);
+  /* Initialize *DEST to an empty stringbuf.
+     A 1:2 ratio of input bytes to output bytes (as assigned above)
+     should be enough for most translations, and if it turns out not
+     to be enough, we'll grow the buffer again, sizing it based on a
+     1:3 ratio of the remainder of the string. */
+  *dest = svn_stringbuf_create_ensure(buflen + 1, pool);
   destbuf = (*dest)->data;
 
   /* Not only does it not make sense to convert an empty string, but
@@ -482,13 +486,6 @@ convert_to_stringbuf(xlate_handle_node_t *node,
 
   do
     {
-      /* A 1:2 ratio of input bytes to output bytes (as assigned above)
-         should be enough for most translations, and if it turns out not
-         to be enough, we'll grow the buffer again, sizing it based on a
-         1:3 ratio of the remainder of the string. */
-
-      svn_stringbuf_ensure(*dest, buflen + 1);
-
       /* Set up state variables for xlate. */
       destlen = buflen - (*dest)->len;
 

subversion/include/svn_string.h
@@ -186,6 +186,15 @@ svn_stringbuf_t *svn_stringbuf_create(const char *cstring,
 svn_stringbuf_t *svn_stringbuf_ncreate(const char *bytes,
                                        apr_size_t size,
                                        apr_pool_t *pool);
+/** Create a new empty bytestring with at least @a minimum_size bytes of
+ * space available in the memory block.
+ *
+ * (@a minimum_size should include space for the terminating NULL character.)
+ *
+ * @since New in 1.6.
+ */
+svn_stringbuf_t *svn_stringbuf_create_ensure(apr_size_t minimum_size,
+                                             apr_pool_t *pool);
 
 /** Create a new stringbuf with the contents of the given string */
 svn_stringbuf_t *svn_stringbuf_create_from_string(const svn_string_t *str,

subversion/libsvn_ra_serf/update.c
@@ -1258,7 +1258,8 @@ start_report(svn_ra_serf__xml_parser_t *parser,
       info->fetch_props = TRUE;
 
       info->dir->base_name = "";
-      info->dir->name_buf = svn_stringbuf_create("", info->pool);
+      /* Create empty stringbuf with estimated max. path size. */
+      info->dir->name_buf = svn_stringbuf_create_ensure(256, info->pool);
       info->dir->name = info->dir->name_buf->data;
 
       info->base_name = info->dir->base_name;

subversion/libsvn_subr/io.c
@@ -1678,7 +1678,8 @@ svn_stringbuf_from_aprfile(svn_stringbuf_t **result,
 {
   apr_size_t len;
   svn_error_t *err;
-  svn_stringbuf_t *res = svn_stringbuf_create("", pool);
+  svn_stringbuf_t *res = svn_stringbuf_create_ensure(SVN__STREAM_CHUNK_SIZE,
+                                                     pool);
   char *buf = apr_palloc(pool, SVN__STREAM_CHUNK_SIZE);
 
   /* XXX: We should check the incoming data for being of type binary. */

subversion/libsvn_subr/path.c
@@ -965,7 +965,7 @@ uri_escape(const char *path, const char table[], apr_pool_t *pool)
   apr_size_t i, copied = 0;
   int c;
 
-  retstr = svn_stringbuf_create("", pool);
+  retstr = svn_stringbuf_create_ensure(strlen(path), pool);
   for (i = 0; path[i]; i++)
     {
       c = (unsigned char)path[i];
@@ -1090,10 +1090,8 @@ svn_path_uri_decode(const char *path, apr_pool_t *pool)
   apr_size_t i;
   svn_boolean_t query_start = FALSE;
 
-  retstr = svn_stringbuf_create("", pool);
-
   /* avoid repeated realloc */
-  svn_stringbuf_ensure(retstr, strlen(path) + 1);
+  retstr = svn_stringbuf_create_ensure(strlen(path) + 1, pool);
 
   retstr->len = 0;
   for (i = 0; path[i]; i++)
@@ -1370,7 +1368,8 @@ illegal_path_escape(const char *path, apr_pool_t *pool)
   apr_size_t i, copied = 0;
   int c;
 
-  retstr = svn_stringbuf_create("", pool);
+  /* Create stringbuf with estimated buffer size. */
+  retstr = svn_stringbuf_create_ensure(strlen(path), pool);
   for (i = 0; path[i]; i++)
     {
       c = (unsigned char)path[i];

subversion/libsvn_subr/stream.c
@@ -168,13 +168,11 @@ svn_stream_readline(svn_stream_t *stream,
   apr_size_t numbytes;
   const char *match;
   char c;
-  svn_stringbuf_t *str = svn_stringbuf_create("", pool);
-
   /* Since we're reading one character at a time, let's at least
      optimize for the 90% case.  90% of the time, we can avoid the
      stringbuf ever having to realloc() itself if we start it out at
      80 chars.  */
-  svn_stringbuf_ensure(str, 80);
+  svn_stringbuf_t *str = svn_stringbuf_create_ensure(80, pool);
 
   match = eol;
   while (*match)

subversion/libsvn_subr/svn_string.c
@@ -220,7 +220,7 @@ svn_string_find_char_backward(const svn_string_t *str, char ch)
 /* svn_stringbuf functions */
 
 static svn_stringbuf_t *
-create_stringbuf(char *data, apr_size_t size, apr_pool_t *pool)
+create_stringbuf(char *data, apr_size_t size, apr_size_t blocksize, apr_pool_t *pool)
 {
   svn_stringbuf_t *new_string;
 
@@ -228,27 +228,37 @@ create_stringbuf(char *data, apr_size_t size, apr_pool_t *pool)
 
   new_string->data = data;
   new_string->len = size;
-  new_string->blocksize = size + 1; /* we know there is a null-term */
+  new_string->blocksize = blocksize;
   new_string->pool = pool;
 
   return new_string;
 }
 
 svn_stringbuf_t *
+svn_stringbuf_create_ensure(apr_size_t blocksize, apr_pool_t *pool)
+{
+  char *data = apr_palloc(pool, blocksize);
+
+  data[0] = '\0';
+
+  /* wrap an svn_stringbuf_t around the new data buffer. */
+  return create_stringbuf(data, 0, blocksize, pool);
+}
+
+svn_stringbuf_t *
 svn_stringbuf_ncreate(const char *bytes, apr_size_t size, apr_pool_t *pool)
 {
-  char *data;
+  svn_stringbuf_t *strbuf = svn_stringbuf_create_ensure(size + 1, pool);
 
-  data = apr_palloc(pool, size + 1);
-  memcpy(data, bytes, size);
+  memcpy(strbuf->data, bytes, size);
 
   /* Null termination is the convention -- even if we suspect the data
      to be binary, it's not up to us to decide, it's the caller's
      call.  Heck, that's why they call it the caller! */
-  data[size] = '\0';
+  strbuf->data[size] = '\0';
+  strbuf->len = size;
 
-  /* wrap an svn_stringbuf_t around the new data */
-  return create_stringbuf(data, size, pool);
+  return strbuf;
 }
 
 
@@ -270,9 +280,10 @@ svn_stringbuf_t *
 svn_stringbuf_createv(apr_pool_t *pool, const char *fmt, va_list ap)
 {
   char *data = apr_pvsprintf(pool, fmt, ap);
+  apr_size_t size = strlen(data);
 
   /* wrap an svn_stringbuf_t around the new data */
-  return create_stringbuf(data, strlen(data), pool);
+  return create_stringbuf(data, size, size + 1, pool);
 }
 
 

subversion/libsvn_subr/xml.c
@@ -153,7 +153,7 @@ xml_escape_attr(svn_stringbuf_t **outstr,
   const char *p = data, *q;
 
   if (*outstr == NULL)
-    *outstr = svn_stringbuf_create("", pool);
+    *outstr = svn_stringbuf_create_ensure(len, pool);
 
   while (1)
     {
@@ -560,9 +560,10 @@ svn_xml_make_open_tag_hash(svn_stringbuf_t **str,
                            apr_hash_t *attributes)
 {
   apr_hash_index_t *hi;
+  apr_size_t est_size = strlen(tagname) + 4 + apr_hash_count(attributes) * 30;
 
   if (*str == NULL)
-    *str = svn_stringbuf_create("", pool);
+    *str = svn_stringbuf_create_ensure(est_size, pool);
 
   svn_stringbuf_appendcstr(*str, "<");
   svn_stringbuf_appendcstr(*str, tagname);

commit 3803eab28bffe833b96dc1678a9f344c4a5f9b74

Fix a potential memory corruption in utf.c:convert_to_stringbuf().
It was decided privatly by the PMC not to treat this as a security issue
because this bug is not exploitable in a resonable setup [1]. The only way to
actually crash the program would be by something like:

[[[
$ python -c 'print( "\x86" * 100000 )' > commit
$ svn ci --encoding windows-1251 -F commit
# should segfault
]]]

The reason be that convert_to_stringbuf allocates twice the amount of memory
for dest buffer than src, but it has a retry loop in case the conversion failed
which updates the length but does not expand the buffer. This will cause the
program to segfault with encodings, in which certain 3-byte wide characters are
represented by a single byte.

This code has been broken 18 years ago since r869736 (in which
svn_stringbuf_ensure() call was removed).

* subversion/libsvn_subr/utf.c
  (convert_to_stringbuf): Call svn_stringbuf_ensure() 
* subversion/tests/libsvn_subr/utf-test.c
  (test_utf_cstring_to_utf8_ex2_CVE): New test.
  (test_funcs): Run new test.

[1] https://lists.apache.org/thread/56qkrznbco93ymg1n4lmgzz99bhx0kx9 
    (private, requires login)


git-svn-id: https://svn.apache.org/repos/asf/subversion/trunk@1937538 13f79535-47bb-0310-9956-ffa450edef68

subversion/libsvn_subr/utf.c
@@ -545,6 +545,7 @@ convert_to_stringbuf(xlate_handle_node_t *node,
                                for all characters in the buffer, 4 is
                                maximum character size (currently) */
 
+      svn_stringbuf_ensure(*dest, buflen);
 
     } while (apr_err == APR_SUCCESS && srclen != 0);
 #endif

subversion/tests/libsvn_subr/utf-test.c
@@ -256,6 +256,27 @@ test_utf_cstring_to_utf8_ex2(apr_pool_t *pool)
   return SVN_NO_ERROR;
 }
 
+static svn_error_t *
+test_utf_cstring_to_utf8_ex2_CVE(apr_pool_t *pool)
+{
+  apr_size_t i;
+  svn_stringbuf_t *src = svn_stringbuf_create_empty(pool);
+  svn_stringbuf_t *expected = svn_stringbuf_create_empty(pool);
+  const char *dest;
+
+  for (i = 0; i < 10000; i++)
+    {
+      svn_stringbuf_appendcstr(src, "\xfb");
+      svn_stringbuf_appendcstr(expected, "\xe2\x88\x9a");
+    }
+
+  SVN_ERR(svn_utf_cstring_to_utf8_ex2(&dest, src->data, "cp866", pool));
+
+  SVN_TEST_ASSERT(strcmp(dest, expected->data) == 0);
+
+  return SVN_NO_ERROR;
+}
+
 /* Test conversion to different codepages from utf8. */
 static svn_error_t *
 test_utf_cstring_from_utf8_ex2(apr_pool_t *pool)
@@ -1318,6 +1339,8 @@ test_utf8_align(apr_pool_t *pool)
   return SVN_NO_ERROR;
 }
 
+
+
 
 /* The test table.  */
 
@@ -1332,6 +1355,8 @@ static struct svn_test_descriptor_t test_funcs[] =
                    "test last_valid/last_valid2"),
     SVN_TEST_PASS2(test_utf_cstring_to_utf8_ex2,
                    "test svn_utf_cstring_to_utf8_ex2"),
+    SVN_TEST_PASS2(test_utf_cstring_to_utf8_ex2_CVE,
+                   "test svn_utf_cstring_to_utf8_ex2 CVE"),
     SVN_TEST_PASS2(test_utf_cstring_from_utf8_ex2,
                    "test svn_utf_cstring_from_utf8_ex2"),
     SVN_TEST_PASS2(test_utf_collated_compare,